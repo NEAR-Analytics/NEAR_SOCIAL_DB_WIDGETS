9f75b08fc0a9caff9eb92d2cc3c45eaacce9137b
ilblackdragon <ilblackdragon>
2022-11-08 22:48:53 -0500

    Update IdeasList by root.near at 2022-11-08 22:48:53.815

--- root.near/IdeasList/source_code.jsx
+++ root.near/IdeasList/source_code.jsx
@@ -7,6 +7,8 @@ return (
     {ideas.map((idea) => (
       <div>
         <h3>{idea.name}</h3>
+        <p>Submitted: {idea.submitter_id}</p>
+        <p>Reviewer: {idea.reviewer_id}</p>
         <p>{idea.description}</p>
         <p>Amount: {idea.amount / 1000000000000000000000000} NEAR</p>
         <a href={`#/root.near/widget/CreateSubmission?idea_id=${idea.idea_id}`}>
