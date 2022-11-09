df140542feef259be3c296ba00ffe04d427d1d5b
evgenykuzyakov <evgenykuzyakov>
2022-11-09 02:17:57 -0500

    Update ProfileLine by mob.near at 2022-11-09 02:17:57.696

---
+++ mob.near/ProfileLine/source_code.jsx
@@ -0,0 +1,36 @@
+const accountId = props.accountId ?? context.accountId;
+const link = props.link ?? true;
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+const inner = (
+  <>
+    <Widget
+      src="mob.near/widget/ProfileImage"
+      props={{
+        style: { width: "1.5em", height: "1.5em" },
+        profile,
+        accountId,
+        className: "d-inline-block",
+        imageClassName: "rounded w-100 h-100 align-top",
+      }}
+    />
+    {profile.name || "No-name profile"}
+    <span className="text-muted">@{accountId}</span>
+  </>
+);
+
+return link ? (
+  <a
+    href={
+      link !== true
+        ? link
+        : `#/mob.near/widget/ProfilePage?accountId=${accountId}`
+    }
+    className="link-dark text-truncate"
+  >
+    {inner}
+  </a>
+) : (
+  <span className="text-truncate">{inner}</span>
+);
