c594ec51776ef4c6a5c76297e3484397fc111f31
ilblackdragon <ilblackdragon>
2022-11-08 22:41:10 -0500

    Update IdeasList by root.near at 2022-11-08 22:41:10.475

--- root.near/IdeasList/source_code.jsx
+++ root.near/IdeasList/source_code.jsx
@@ -3,8 +3,14 @@ const ideas = Near.view("nearideas.near", "get_ideas");
 return (
   <div>
     <h2>Ideas</h2>
+    <a href="#/root.near/widget/AddIdea">Add idea</a>
     {ideas.map((idea) => (
-      <div>{idea.name}</div>
+      <div>
+        <h3>{idea.name}</h3>
+        <p>{idea.description}</p>
+        <p>Amount: {idea.amount}</p>
+        <a href="#/root.near/widget/CreateSubmission">Create submission</a>
+      </div>
     ))}
   </div>
 );
