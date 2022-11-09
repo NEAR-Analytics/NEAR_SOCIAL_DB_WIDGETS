486b04bea9b992d12798ec70f31f8ef7e205f8dc
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-11-09 00:42:47 -0500

    Update PollQuestion by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-11-09 00:42:47.911

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/PollQuestion/source_code.jsx
@@ -0,0 +1,41 @@
+const accountId = context.accountId;
+
+if (!accountId) {
+  return "Please sign in with NEAR wallet to add a new blog entry";
+}
+
+initState({
+  question: "",
+});
+
+const entry = {
+  question: state.question,
+};
+
+const handleChange = (event) => {
+  State.update({ content: event.target.value });
+};
+
+let qId = Date.now() + accountId;
+
+return (
+  <div className="row mb-3">
+    <div>
+      <h4>Question</h4>
+    </div>
+    <input className="mb-2" value={state.question} />
+
+    <CommitButton
+      data={{
+        post: {
+          poll_question: {
+            question: entry.question,
+            questionId: qId,
+          },
+        },
+      }}
+    >
+      Submit
+    </CommitButton>
+  </div>
+);
