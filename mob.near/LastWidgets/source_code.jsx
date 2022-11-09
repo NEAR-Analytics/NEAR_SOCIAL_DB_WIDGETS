f20ad000dffc3482a66968b2d59c4ed0d33681f5
evgenykuzyakov <evgenykuzyakov>
2022-11-09 02:00:29 -0500

    Update LastWidgets by mob.near at 2022-11-09 02:00:29.791

--- mob.near/LastWidgets/source_code.jsx
+++ mob.near/LastWidgets/source_code.jsx
@@ -58,10 +58,28 @@ if (JSON.stringify(data) !== JSON.stringify(state.data || {})) {
 }
 
 return (
-  <div className="px-2 mx-auto" style={{ maxWidth: "42em" }}>
-    <Widget
-      src="mob.near/widget/ItemFeed"
-      props={{ items: state.allItems, renderItem }}
-    />
-  </div>
+  <>
+    {props.accountId && (
+      <div className="mb-2">
+        Filtered by account
+        <a
+          href="#/mob.near/widget/LastWidgets"
+          className="btn btn-outline-primary"
+        >
+          <Widget
+            src="mob.near/widget/ProfileLine"
+            props={{ accountId, link: false }}
+          />
+          <i class="bi bi-x-square"></i>
+        </a>
+      </div>
+    )}
+
+    <div className="px-2 mx-auto" style={{ maxWidth: "42em" }}>
+      <Widget
+        src="mob.near/widget/ItemFeed"
+        props={{ items: state.allItems, renderItem }}
+      />
+    </div>
+  </>
 );
