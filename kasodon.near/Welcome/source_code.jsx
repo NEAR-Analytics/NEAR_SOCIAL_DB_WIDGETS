ed0cd308460d814fdfc5462b38817eb9d844a45e
https://github.com/kasodon <https://github.com/kasodon>
2022-11-10 04:14:16 -0500

    Update Welcome by kasodon.near at 2022-11-10 04:14:16.063

--- kasodon.near/Welcome/source_code.jsx
+++ kasodon.near/Welcome/source_code.jsx
@@ -1,58 +1,171 @@
 return (
-  <>
-    <div className="row mb-3">
-      <h2>Welcome to Near Social!</h2>
+  <div className="container my-4 text-sm-start text-md-start text-lg-start text-center">
+    <div style={{ borderRadius: "12px" }} className="row py-2 mb-2">
+      <div style={{}} className="col-12">
+        <h2
+          style={{
+            fontFamily: "Tahoma, sans-serif",
+            fontWeight: "bolder",
+            margin: "0",
+            color: "#414449",
+          }}
+        >
+          Welcome to{" "}
+          <span style={{ color: "#25A18E" }}>
+            <i>NEAR</i>
+          </span>{" "}
+          Social!
+        </h2>
+      </div>
+    </div>
+    <div
+      style={{ borderRadius: "12px", fontFamily: "Tahoma, sans-serif" }}
+      className="row py-2"
+    >
+      <div className="col-12 font-weight-bold mb-4">
+        {context.accountId && (
+          <div>
+            <Widget src="mob.near/widget/ProfileOnboarding" />
+            <Widget
+              src="mob.near/widget/Profile"
+              props={{ accountId: context.accountId }}
+            />
+          </div>
+        )}
+      </div>
+      <div className="col-12 font-weight-bold">
+        {context.accountId && (
+          <div>
+            <Widget
+              src="mob.near/widget/Profile"
+              props={{ accountId: "self.social.near" }}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+    <div
+      style={{ fontFamily: "Tahoma, sans-serif", fontWeight: "bold" }}
+      className="row gy-4 mt-4"
+    >
+      <div className="col-12">
+        <a
+          style={{
+            textDecoration: "none",
+            color: "#FAF8D4",
+            background: "#11151C",
+            padding: "10px 22px",
+            borderRadius: "8px",
+          }}
+          href="https://thewiki.near.page/near.social"
+        >
+          Learn More &rarr;
+        </a>
+      </div>
+      <div className="col-12">
+        <a
+          style={{
+            textDecoration: "none",
+            color: "#FAF8D4",
+            background: "#11151C",
+            padding: "10px 22px",
+            borderRadius: "8px",
+          }}
+          href="https://t.me/NearSocial"
+        >
+          Ask Questions &rarr;
+        </a>
+      </div>
+      <div className="col-12">
+        <a
+          style={{
+            textDecoration: "none",
+            color: "#FAF8D4",
+            background: "#11151C",
+            padding: "10px 22px",
+            borderRadius: "8px",
+          }}
+          href="#/mob.near/widget/Explorer"
+        >
+          Explore All Data &rarr;
+        </a>
+      </div>
+      <div className="col-12">
+        <a
+          style={{
+            textDecoration: "none",
+            color: "#FAF8D4",
+            background: "#11151C",
+            padding: "10px 22px",
+            borderRadius: "8px",
+          }}
+          href="#/mob.near/widget/AllWidgets"
+        >
+          Explore All widgets &rarr;
+        </a>
+      </div>
+      <div className="col-12">
+        <a
+          style={{
+            textDecoration: "none",
+            color: "#FAF8D4",
+            background: "#11151C",
+            padding: "10px 22px",
+            borderRadius: "8px",
+          }}
+          href="#/mob.near/widget/LastWidgets"
+        >
+          Last Updated widgets &rarr;
+        </a>
+      </div>
+      <div className="col-12">
+        <a
+          style={{
+            textDecoration: "none",
+            color: "#FAF8D4",
+            background: "#11151C",
+            padding: "10px 22px",
+            borderRadius: "8px",
+          }}
+          href="#/mob.near/widget/HomepageEditor"
+        >
+          Edit This Homepage &rarr;
+        </a>
+      </div>
+      <div className="col-12">
+        <a
+          style={{
+            textDecoration: "none",
+            color: "#FAF8D4",
+            background: "#11151C",
+            padding: "10px 22px",
+            borderRadius: "8px",
+          }}
+          href="#/mob.near/widget/Memes"
+        >
+          View All Memes &rarr;
+        </a>
+      </div>
+    </div>
+    <div className="mb-4">
       {context.accountId && (
         <div>
           <Widget src="mob.near/widget/ProfileOnboarding" />
           <Widget
-            src="mob.near/widget/Profile"
+            src="kasodon.near/widget/Profile"
             props={{ accountId: context.accountId }}
           />
         </div>
       )}
     </div>
-
-    <div className="row mb-3">
-      <h4>Get involved</h4>
-      <div className="mb-3">
-        <Widget
-          src="mob.near/widget/Profile"
-          props={{ accountId: "self.social.near" }}
-        />
-      </div>
-      <div className="mb-1">
-        Learn more:{" "}
-        <a href="https://thewiki.near.page/near.social">
-          https://thewiki.near.page/near.social
-        </a>
-      </div>
-      <div className="mb-1">
-        Questions? Join telegram group:{" "}
-        <a href="https://t.me/NearSocial">https://t.me/NearSocial</a>
-      </div>
-      <div className="mb-1">
-        Explore data: <a href="#/mob.near/widget/Explorer">Explorer</a>
-      </div>
-      <div className="mb-1">
-        Explore all widgets:{" "}
-        <a href="#/mob.near/widget/AllWidgets">AllWidgets</a>
-      </div>
-      <div className="mb-1">
-        Recently updated widgets:{" "}
-        <a href="#/mob.near/widget/LastWidgets">LastWidgets</a>
-      </div>
-      <div className="mb-1">
-        Edit your homepage:{" "}
-        <a href="#/mob.near/widget/HomepageEditor">HomepageEditor</a>
-      </div>
-      <div className="mb-1">
-        <a href="#/mob.near/widget/Memes">Memes mEmEs MeMeS</a>
-      </div>
-    </div>
     <div className="row mb-3">
       <div>
-        <h4>Recently joined</h4>
+        <h4
+          style={{ fontFamily: "Tahoma, sans-serif", fontWeight: "bold" }}
+          className="text-capitalize fw-bold"
+        >
+          Recently joined
+        </h4>
         <Widget src="mob.near/widget/LastProfilesImages" />
       </div>
     </div>
@@ -60,9 +173,11 @@ return (
       Object.keys(Social.keys(`${context.accountId}/widget/*`) || {}).length >
         0 && (
         <div className="row mb-3">
-          <h4>Your widgets</h4>
+          <h4 style={{ fontFamily: "Tahoma, sans-serif", fontWeight: "bold" }}>
+            Your widgets
+          </h4>
           <Widget src="mob.near/widget/Widgets" />
         </div>
       )}
-  </>
+  </div>
 );
