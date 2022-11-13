4a6941e58e92c75ae8179936e8a65923e74434d7
silkking.near <silkking.near>
2022-11-13 17:47:45 -0500

    Update Test by silkking.near at 2022-11-13 17:47:45.171

--- silkking.near/Test/source_code.jsx
+++ silkking.near/Test/source_code.jsx
@@ -1,13 +1,25 @@
 // Returns the account Id from the logged person
 const accountId = context.accountId;
 
-const data = Social.keys(`${accountId}/post/poll_question`, "final", {
+// Returns JSON with profile data from the accountId
+const profile = Social.getr(`${accountId}/profile`);
+
+// Returns the widgets contents (not committed data) from the user in accountId
+const widgets = Social.get(`${accountId}/widget/*`);
+
+// Returns the Test widget contents (not committed data) from the user in accountId
+const testWidget = Social.getr(`${accountId}/post/Test`);
+
+// Returns a JSON object with only one key that is accountId
+// After that, contains many keys, containing each other path that has been posted like "widgets", "post", "profile", "test"
+// Inside each key, there is an array contaning numbers, most likely the block heights that have any post with that key
+const data = Social.keys(`${accountId}/*`, "final", {
   return_type: "History",
 });
 
 return (
   <div>
     {JSON.stringify(data)}
-    <CommitButton data={test}>No</CommitButton>
+    <CommitButton data={{ test: "test" }}>No</CommitButton>
   </div>
 );
