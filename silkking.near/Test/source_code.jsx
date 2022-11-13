8fb9e3ed40e704d9c66f25a02fb8ea4dcb9c2644
silkking.near <silkking.near>
2022-11-13 17:22:54 -0500

    Update Test by silkking.near at 2022-11-13 17:22:54.776

--- silkking.near/Test/source_code.jsx
+++ silkking.near/Test/source_code.jsx
@@ -1,4 +1,25 @@
 // Returns the account Id from the logged person
 const accountId = context.accountId;
 
-return <div>Hello World</div>;
+const data = Social.keys(`${accountId}/post/poll_question`, "final", {
+  return_type: "History",
+});
+
+return (
+  <div>
+    {JSON.stringify(data)}
+    <CommitButton
+      data={{
+        post: {
+          entry: {
+            question: entry.question,
+            yes: entry.yes,
+            no: entry.no + 1,
+          },
+        },
+      }}
+    >
+      No
+    </CommitButton>
+  </div>
+);
