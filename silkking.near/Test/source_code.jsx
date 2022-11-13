d799e59c4242f4a0e4101188d05dc0fbb000d174
silkking.near <silkking.near>
2022-11-13 17:23:13 -0500

    Update Test by silkking.near at 2022-11-13 17:23:13.769

--- silkking.near/Test/source_code.jsx
+++ silkking.near/Test/source_code.jsx
@@ -8,18 +8,6 @@ const data = Social.keys(`${accountId}/post/poll_question`, "final", {
 return (
   <div>
     {JSON.stringify(data)}
-    <CommitButton
-      data={{
-        post: {
-          entry: {
-            question: entry.question,
-            yes: entry.yes,
-            no: entry.no + 1,
-          },
-        },
-      }}
-    >
-      No
-    </CommitButton>
+    <CommitButton data={test}>No</CommitButton>
   </div>
 );
