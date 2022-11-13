598a06e67b12ebfa8222b6927fb6f6193245abdf
https://github.com/kasodon <https://github.com/kasodon>
2022-11-13 17:02:32 -0500

    Update ProfileImage by kasodon.near at 2022-11-13 17:02:32.961

--- kasodon.near/ProfileImage/source_code.jsx
+++ kasodon.near/ProfileImage/source_code.jsx
@@ -16,7 +16,7 @@ const image = profile.image;
 const title = `${name} @${accountId}`;
 
 return (
-  <div className={className} title={title}>
+  <>
     <Widget
       src="mob.near/widget/Image"
       props={{
@@ -25,8 +25,9 @@ return (
         className: imageClassName,
         style: imageStyle,
         thumbnail,
-        fallbackUrl: "https://thewiki.io/static/media/sasha_anon.6ba19561.png",
+        fallbackUrl:
+          "https://images.unsplash.com/photo-1528731708534-816fe59f90cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
       }}
     />
-  </div>
+  </>
 );
