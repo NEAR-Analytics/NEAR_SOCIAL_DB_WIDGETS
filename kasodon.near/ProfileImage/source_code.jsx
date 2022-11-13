113a97a57e7886d6f3585bb486469a69613a8fd5
https://github.com/kasodon <https://github.com/kasodon>
2022-11-13 17:06:24 -0500

    Update ProfileImage by kasodon.near at 2022-11-13 17:06:24.800

--- kasodon.near/ProfileImage/source_code.jsx
+++ kasodon.near/ProfileImage/source_code.jsx
@@ -23,7 +23,6 @@ return (
         image,
         alt: title,
         className: imageClassName,
-        style: imageStyle,
         thumbnail,
         fallbackUrl:
           "https://images.unsplash.com/photo-1528731708534-816fe59f90cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
