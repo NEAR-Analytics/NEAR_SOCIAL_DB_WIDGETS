f4ff2fb4239759edda7b69f7fe22ed67348e930f
ilblackdragon <ilblackdragon>
2022-11-14 22:51:27 -0500

    Update IdeasList by root.near at 2022-11-14 22:51:27.499

--- root.near/IdeasList/source_code.jsx
+++ root.near/IdeasList/source_code.jsx
@@ -1,12 +1,5 @@
 const ideas = Near.view("nearideas.near", "get_ideas");
 
-const onReportClick = (idea_id) => {
-  Near.call("nearideas.near", "review", {
-    idea_id: parseInt(idea_id),
-    status: "Reported",
-  });
-};
-
 return (
   <div>
     <h2>Ideas</h2>
@@ -21,35 +14,7 @@ return (
     {ideas
       ? ideas.map((idea) => {
           if (idea.status == "Reported") return "";
-          return (
-            <div>
-              <h3>
-                {idea.name} ({idea.status})
-              </h3>
-              <p>Submitted: {idea.submitter_id}</p>
-              <p>Reviewer: {idea.reviewer_id}</p>
-              <p>{idea.description}</p>
-              <p>Amount: {idea.amount / 1000000000000000000000000} NEAR</p>
-              <p>
-                <a
-                  className="btn btn-outline-primary ms-2"
-                  href={`#/root.near/widget/CreateSubmission?idea_id=${idea.idea_id}`}
-                >
-                  Create submission
-                </a>
-                {idea.reviewer_id === context.accountId ? (
-                  <a
-                    className="btn btn-outline-primary ms-2"
-                    onClick={() => onReportClick(idea.idea_id)}
-                  >
-                    Report
-                  </a>
-                ) : (
-                  ""
-                )}
-              </p>
-            </div>
-          );
+          return <Widget src="root.near/widget/Idea" props={{ idea }} />;
         })
       : ""}
   </div>
