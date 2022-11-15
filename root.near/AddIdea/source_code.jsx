843b4473ca9d01abdf119875007b2ed041aad9ce
ilblackdragon <ilblackdragon>
2022-11-14 22:38:29 -0500

    Update AddIdea by root.near at 2022-11-14 22:38:29.662

--- root.near/AddIdea/source_code.jsx
+++ root.near/AddIdea/source_code.jsx
@@ -8,17 +8,17 @@ const onClick = () => {
 };
 
 const handleChange = (event) => {
-  State.update({ content: event.target.value });
+  State.update({ description: event.target.value });
 };
 
 return (
   <div>
     <h2>Add Idea</h2>
     <p>
-      Title: <input type="text" />
+      Title: <input type="text" value={state.name} />
     </p>
     <p>Description:</p>
-    <textarea value={state.content} onChange={handleChange} />
+    <textarea value={state.description} onChange={handleChange} />
     <p>
       <a className="btn btn-outline-primary ms-2" onClick={onClick}>
         Submit
