25c4752e046856de58af2b9d5520ec8f1d15bea5
urbanite.near <urbanite.near>
2022-11-17 17:48:21 -0500

    Update IdeasList by urbanite.near at 2022-11-17 17:48:21.201

---
+++ urbanite.near/IdeasList/source_code.jsx
@@ -0,0 +1,30 @@
+const ideas = Near.view("nearideas.near", "get_ideas");
+
+return (
+  <div>
+    <h2>Ideas</h2>
+    <a
+      className="btn btn-outline-primary ms-2"
+      href="#/root.near/widget/AddIdea"
+    >
+      Add idea
+    </a>
+    {ideas.map((idea) => (
+      <div>
+        <h3>
+          {idea.name} ({idea.status})
+        </h3>
+        <p>Submitted: {idea.submitter_id}</p>
+        <p>Reviewer: {idea.reviewer_id}</p>
+        <p>{idea.description}</p>
+        <p>Amount: {idea.amount / 1000000000000000000000000} NEAR</p>
+        <a
+          className="btn btn-outline-primary ms-2"
+          href={`#/root.near/widget/CreateSubmission?idea_id=${idea.idea_id}`}
+        >
+          Create submission
+        </a>
+      </div>
+    ))}
+  </div>
+);
