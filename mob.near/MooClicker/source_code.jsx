b1c4636d32604a01708e0b9d5c7e412234d99b5d
evgenykuzyakov <evgenykuzyakov>
2022-11-17 22:41:34 -0500

    Update MooClicker by mob.near at 2022-11-17 22:41:34.297

---
+++ mob.near/MooClicker/source_code.jsx
@@ -0,0 +1,31 @@
+const moos = Social.index("moo", "moo-click");
+
+return (
+  <div>
+    <div className="mb-2">
+      <CommitButton
+        data={{
+          index: {
+            moo: JSON.stringify(
+              {
+                key: "moo-click",
+                value: Date.now(),
+              },
+              undefined,
+              0
+            ),
+          },
+        }}
+      >
+        Moo
+      </CommitButton>
+    </div>
+    {moos &&
+      moos.map(({ accountId, blockHeight, value }) => (
+        <Widget
+          src="mob.near/widget/ProfileImage"
+          props={{ accountId, className: "d-inline-block overflow-hidden" }}
+        />
+      ))}
+  </div>
+);
