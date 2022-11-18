f7caa343acdd17bd87721ba5211c40b6aee762d8
evgenykuzyakov <evgenykuzyakov>
2022-11-17 22:54:28 -0500

    Update MooClicker by mob.near at 2022-11-17 22:54:28.763

--- mob.near/MooClicker/source_code.jsx
+++ mob.near/MooClicker/source_code.jsx
@@ -1,8 +1,9 @@
 const moos = Social.index("moo", "moo-click");
+const counter = {};
 
 return (
   <div>
-    <div className="mb-2">
+    <div className="mb-4">
       <CommitButton
         data={{
           index: {
@@ -20,17 +21,28 @@ return (
         Moo
       </CommitButton>
     </div>
-    <div className="d-flex gap-1">
+    <div className="d-flex gap-3 flex-row-reverse">
       {moos &&
-        moos.map(({ accountId, blockHeight, value }) => (
-          <Widget
-            src="mob.near/widget/ProfileImage"
-            props={{
-              accountId,
-              className: "d-inline-block overflow-hidden",
-            }}
-          />
-        ))}
+        moos.map(({ accountId, blockHeight, value }) => {
+          counter[accountId] = (counter[accountId] || 0) + 1;
+          return (
+            <div className="position-relative">
+              <Widget
+                src="mob.near/widget/ProfileImage"
+                props={{
+                  accountId,
+                  className: "d-inline-block overflow-hidden",
+                }}
+              />
+              <span
+                class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success"
+                style={{ zIndex: 1 }}
+              >
+                {counter[accountId]}
+              </span>
+            </div>
+          );
+        })}
     </div>
   </div>
 );
