b7744dbc917c6e6bca19d0f8e5cc1cf1590b1707
evgenykuzyakov <evgenykuzyakov>
2022-11-18 20:26:24 -0500

    Update WidgetIcons by mob.near at 2022-11-18 20:26:24.300

---
+++ mob.near/WidgetIcons/source_code.jsx
@@ -0,0 +1,84 @@
+const accountId = props.accountId;
+const tag = props.tag;
+
+let keys = `${accountId ?? "*"}/widget/*`;
+
+if (tag) {
+  const taggedWidgets = Social.keys(
+    `${accountId ?? "*"}/widget/*/metadata/tags/${tag}`,
+    "final"
+  );
+
+  if (taggedWidgets === null) {
+    return "Loading tags";
+  }
+
+  keys = Object.entries(taggedWidgets)
+    .map((kv) => Object.keys(kv[1].widget).map((w) => `${kv[0]}/widget/${w}`))
+    .flat();
+
+  if (!keys.length) {
+    return `No widgets found by tag #${tag}`;
+  }
+}
+
+const data = Social.keys(keys, "final", {
+  return_type: "BlockHeight",
+});
+
+if (data === null) {
+  return "Loading widgets";
+}
+
+const processData = (data) => {
+  const accounts = Object.entries(data);
+
+  const allItems = accounts
+    .map((account) => {
+      const accountId = account[0];
+      return Object.entries(account[1].widget).map((kv) => ({
+        accountId,
+        widgetName: kv[0],
+        blockHeight: kv[1],
+      }));
+    })
+    .flat();
+
+  allItems.sort((a, b) => b.blockHeight - a.blockHeight);
+  return allItems;
+};
+
+const renderTag = (tag, tagBadge) => (
+  <a href={makeLink(accountId, tag)}>{tagBadge}</a>
+);
+
+const renderItem = (a) => {
+  return (
+    <a
+      href={`#/${a.accountId}/widget/${a.widgetName}`}
+      className="text-decoration-none"
+      key={JSON.stringify(a)}
+    >
+      <Widget
+        src="mob.near/widget/WidgetImage"
+        props={{
+          accountId: a.accountId,
+          widgetName: a.widgetName,
+        }}
+      />
+    </a>
+  );
+};
+
+if (JSON.stringify(data) !== JSON.stringify(state.data || {})) {
+  State.update({
+    data,
+    allItems: processData(data),
+  });
+}
+
+return (
+  <div className="d-flex flex-wrap gap-3 m-3">
+    {state.allItems.map(renderItem)}
+  </div>
+);
