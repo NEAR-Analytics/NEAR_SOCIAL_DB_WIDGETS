a70ea9032fdd9bf631bcbc7151fa975a5d86ab88
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-11-19 01:00:06 -0500

    Update answer_poll-comment-container by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-11-19 01:00:06.546

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll-comment-container/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll-comment-container/source_code.jsx
@@ -1,10 +1,12 @@
-const answerData = props.answerData;
+const answer = props.answer;
+const answerTimeStamp = props.answerTimeStamp;
+const userName = props.accountId ?? "User name missed";
 
 return (
   <div className="flex-column mt-2">
     <small className="ps-1 text-nowrap text-muted ms-auto">
       <i className="bi bi-clock me-1"></i>
-      {timeAgo(Date.now() - answerTimestamp)}
+      {timeAgo(Date.now() - answerTimeStamp)}
     </small>
     <div className="flex-row">
       <span className="fw-bold">{userName}</span>
