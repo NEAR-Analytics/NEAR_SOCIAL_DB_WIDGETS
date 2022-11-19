c7d6640c24e3be496542c230747caffa34f6f84f
evgenykuzyakov <evgenykuzyakov>
2022-11-18 21:25:34 -0500

    Update LastProfilesImages by mob.near at 2022-11-18 21:25:34.851

---
+++ mob.near/LastProfilesImages/source_code.jsx
@@ -0,0 +1,40 @@
+const data = Social.keys("*/profile", "final");
+
+const lastN = props.lastN || 21;
+
+if (!data) {
+  return "Loading";
+}
+
+let accounts = Object.entries(data);
+const numAccounts = accounts.length;
+accounts = accounts.slice(numAccounts - lastN, numAccounts);
+
+const allWidgets = [];
+
+for (let i = 0; i < accounts.length; ++i) {
+  const accountId = accounts[i][0];
+
+  allWidgets.push(
+    <a
+      href={`#/mob.near/widget/ProfilePage?accountId=${accountId}`}
+      className="text-decoration-none"
+    >
+      <Widget
+        src="mob.near/widget/ProfileImage"
+        props={{
+          accountId,
+          tooltip: true,
+          className: "d-inline-block overflow-hidden",
+        }}
+      />
+    </a>
+  );
+}
+
+return (
+  <div>
+    <div>{allWidgets}</div>
+    <div>Total {numAccounts} profiles</div>
+  </div>
+);
