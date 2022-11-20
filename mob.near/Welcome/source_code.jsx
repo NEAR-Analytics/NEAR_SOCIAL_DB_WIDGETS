360d5d88110b8fec64594f125a9b374be80e9523
evgenykuzyakov <evgenykuzyakov>
2022-11-20 02:24:00 -0500

    Update Welcome by mob.near at 2022-11-20 02:24:00.901

--- mob.near/Welcome/source_code.jsx
+++ mob.near/Welcome/source_code.jsx
@@ -40,32 +40,34 @@ return (
           >
             Documentation
           </a>
+        </div>
+        <div className="mb-2 d-flex gap-2 flex-wrap">
           <a
-            className="btn btn-outline-secondary"
+            className="btn btn-outline-secondary border-0"
             href="#/mob.near/widget/ProfilePage?accountId=self.social.near"
           >
             <i className="bi bi-person-circle"></i>
           </a>
           <a
-            className="btn btn-outline-secondary"
+            className="btn btn-outline-secondary border-0"
             href="https://t.me/NearSocial"
           >
             <i className="bi bi-telegram"></i>
           </a>
           <a
-            className="btn btn-outline-secondary"
+            className="btn btn-outline-secondary border-0"
             href="https://github.com/NearSocial"
           >
             <i className="bi bi-github"></i>
           </a>
           <a
-            className="btn btn-outline-secondary"
+            className="btn btn-outline-secondary border-0"
             href="https://twitter.com/NearSocial_"
           >
             <i className="bi bi-twitter"></i>
           </a>
           <a
-            className="btn btn-outline-secondary"
+            className="btn btn-outline-secondary border-0"
             href="https://thewiki.near.page/near.social"
           >
             <i className="bi bi-wikipedia"></i>
