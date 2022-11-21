3b48d30790fd8f9116274e31fd24c73498b9f669
ciocan <ciocan>
2022-11-21 14:23:05 -0500

    Update burrow by ciocan.near at 2022-11-21 14:23:05.023

--- ciocan.near/burrow/source_code.jsx
+++ ciocan.near/burrow/source_code.jsx
@@ -1,19 +1,61 @@
-async function getAssets() {
+function power(x, y) {
+  if (y === 0) {
+    return 1;
+  } else if (y % 2 === 0) {
+    return power(x, parseInt(y / 2)) * power(x, parseInt(y / 2));
+  } else {
+    return x * power(x, parseInt(y / 2)) * power(x, parseInt(y / 2));
+  }
+}
+
+function getAssets() {
   const assets = Near.view("contract.main.burrow.near", "get_assets_paged");
   const tokenIds = assets.map(([id]) => id);
 
-  const assetsDetailed = await Promise.all(
-    tokenIds.map((token_id) =>
-      Near.view("contract.main.burrow.near", "get_asset", {
-        args: { token_id },
-      })
-    )
+  const assetsDetailed = tokenIds.map((token_id) =>
+    Near.view("contract.main.burrow.near", "get_asset", { token_id })
   );
 
-  return assetsDetailed;
+  const metadata = tokenIds.map((token_id) =>
+    Near.view(token_id, "ft_metadata")
+  );
+
+  const config = Near.view("contract.main.burrow.near", "get_config");
+  const prices = Near.view(config["oracle_account_id"], "get_price_data");
+
+  const refPricesResponse = fetch(
+    "https://raw.githubusercontent.com/NearDeFi/token-prices/main/ref-prices.json"
+  );
+  const refPrices = JSON.parse(refPricesResponse.body);
+
+  return assetsDetailed.map((asset, i) => {
+    const price = prices.prices.find((p) => p.asset_id === asset.token_id);
+    const decimals = parseInt(price.price.decimals) - metadata[i].decimals;
+    const usd = price.price.multiplier / power(10, decimals);
+
+    return {
+      ...asset,
+      metadata: metadata[i],
+      price: {
+        ...price.price,
+        usd: usd ? usd : refPrices[asset.token_id].price,
+      },
+    };
+  });
 }
 
-const x = await getAssets();
-console.log(x);
+const assets = getAssets();
+console.log(assets);
 
-return <div>Hello Burrow</div>;
+const allAssets = assets.map((asset) => (
+  <li>
+    <span>{asset.metadata.symbol}</span>
+  </li>
+));
+
+return (
+  <ul>
+    <h4>Assets</h4>
+    {allAssets}
+  </ul>
+);
