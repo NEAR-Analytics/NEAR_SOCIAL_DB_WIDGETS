14777ea1f71f8b0f969c0ad0c1d3a83461972646
ciocan <ciocan>
2022-11-21 13:37:06 -0500

    Update burrow by ciocan.near at 2022-11-21 13:37:06.614

--- ciocan.near/burrow/source_code.jsx
+++ ciocan.near/burrow/source_code.jsx
@@ -1,4 +1,19 @@
-const x = Near.view("contract.main.burrow.near", "get_config");
+async function getAssets() {
+  const assets = Near.view("contract.main.burrow.near", "get_assets_paged");
+  const tokenIds = assets.map(([id]) => id);
+
+  const assetsDetailed = await Promise.all(
+    tokenIds.map((token_id) =>
+      Near.view("contract.main.burrow.near", "get_asset", {
+        args: { token_id },
+      })
+    )
+  );
+
+  return assetsDetailed;
+}
+
+const x = await getAssets();
 console.log(x);
 
 return <div>Hello Burrow</div>;
