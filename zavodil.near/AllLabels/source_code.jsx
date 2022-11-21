cfe8c3db9126e90ee08be7b06404f0e4abebb537
zavodil <zavodil>
2022-11-21 01:44:17 -0500

    Update AllLabels by zavodil.near at 2022-11-21 01:44:17.384

---
+++ zavodil.near/AllLabels/source_code.jsx
@@ -0,0 +1,43 @@
+// AllLabels
+const ownerId = "zavodil.near";
+const appName = "nametag";
+
+let tag = props.tag ?? "*";
+const data = Social.keys(`*/${appName}/*/tags/${tag}`, "final");
+
+if (!data) {
+  return "Loading";
+}
+
+const contracts = {};
+
+Object.values(data).forEach((account) => {
+  Object.keys(account[appName]).forEach((contract) => {
+    contracts[contract] = true;
+  });
+});
+
+const allWidgets = Object.keys(contracts).map((accountId) => {
+  return (
+    <div className="mb-2 card">
+      <div className="card-body">
+        <div className="text-truncate">
+          <Widget src={`mob.near/widget/ProfileLine`} props={{ accountId }} />
+        </div>
+        <Widget src={`${ownerId}/widget/PublicTags`} props={{ accountId }} />
+      </div>
+    </div>
+  );
+});
+
+return (
+  <>
+    <Widget
+      src={`mob.near/widget/PublicTagEditor`}
+      key={`public-tag-editor-${props.accountId}`}
+      props={{ contractId: props.accountId }}
+    />
+    <hr />
+    {allWidgets}
+  </>
+);
