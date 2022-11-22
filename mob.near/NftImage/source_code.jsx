2126230ead804fc2de4585e1c9df26b55c9e0e9c
evgenykuzyakov <evgenykuzyakov>
2022-11-21 23:48:03 -0500

    Update NftImage by mob.near at 2022-11-21 23:48:03.284

--- mob.near/NftImage/source_code.jsx
+++ mob.near/NftImage/source_code.jsx
@@ -53,7 +53,7 @@ if (nftMetadata && nftToken) {
 const replaceIpfs = () => {
   if (!state.imageUrl && imageUrl) {
     const match =
-      /^https?:\/\/[^\/]+\/ipfs\/(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})(\/.*)?$/g.exec(
+      /^(?:https?:\/\/[^\/]+\/ipfs\/)?(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})(\/.*)?$/g.exec(
         imageUrl
       );
     if (match) {
