dea0939c339f187858d898256983a9e1b7f89941
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-11-23 16:19:25 -0500

    Update answer_poll by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-11-23 16:19:25.890

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll/source_code.jsx
@@ -58,7 +58,7 @@ let answersData = Object.keys(answerDataFromBlockHeight).map((key) => {
   };
 });
 
-// console.log("answData: ", answersData);
+console.log("answData: ", answersData);
 
 const haveThisUserAlreadyVoted = () => {
   if (answersData.length == 0) {
@@ -87,7 +87,7 @@ const loadComments = () => {
   for (let i = 0; i < answersData.length; i++) {
     // console.log("test: ", answersData[i].accountId);
     let answer = Social.get(
-      `*/post/answer__poll/${questionBlockHeight}/user_answers`
+      `${answersData[i].accountId}/post/answer__poll/${questionBlockHeight}/user_answers`
     );
 
     console.log("answer: ", answer);
