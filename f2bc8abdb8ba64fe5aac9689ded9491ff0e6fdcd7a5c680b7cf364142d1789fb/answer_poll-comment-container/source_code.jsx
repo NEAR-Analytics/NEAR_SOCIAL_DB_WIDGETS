e935827621279275af0e939e6303a4249a0f2ba0
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-11-22 22:11:19 -0500

    Update answer_poll-comment-container by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-11-22 22:11:19.717

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll-comment-container/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll-comment-container/source_code.jsx
@@ -2,6 +2,15 @@ const answer = props.answer;
 const answerTimeStamp = props.answerTimeStamp;
 const userName = props.accountId ?? "User name missed";
 
+const timeAgo = (diffSec) =>
+  diffSec < 60000
+    ? `${(diffSec / 1000) | 0} seconds ago`
+    : diffSec < 3600000
+    ? `${(diffSec / 60000) | 0} minutes ago`
+    : diffSec < 86400000
+    ? `${(diffSec / 3600000) | 0} hours ago`
+    : `${(diffSec / 86400000) | 0} days ago`;
+
 return (
   <div className="flex-column mt-2">
     <small className="ps-1 text-nowrap text-muted ms-auto">
@@ -9,7 +18,7 @@ return (
       {timeAgo(Date.now() - answerTimeStamp)}
     </small>
     <div className="flex-row">
-      <span className="fw-bold">{userName}</span>
+      <span className="fw-bold">{userName} :</span>
       <span>{comment}</span>
     </div>
   </div>
