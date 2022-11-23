568a64c013ba6c9c2f80a96a8e5e24035463acd8
zavodil <zavodil>
2022-11-23 14:32:52 -0500

    Update AllLabels by zavodil.near at 2022-11-23 14:32:52.458

--- zavodil.near/AllLabels/source_code.jsx
+++ zavodil.near/AllLabels/source_code.jsx
@@ -22,9 +22,9 @@ const allWidgets = Object.keys(contracts).map((accountId) => {
     <div className="mb-2 card">
       <div className="card-body">
         <div className="text-truncate">
-          <Widget src={`mob.near/widget/ProfileLine`} props={{ accountId }} />
+          <Widget src={`${ownerId}/widget/ProfileLine`} props={{ accountId }} />
         </div>
-        <Widget src={`mob.near/widget/PublicTags`} props={{ accountId }} />
+        <Widget src={`${ownerId}/widget/PublicTags`} props={{ accountId }} />
       </div>
     </div>
   );
@@ -33,11 +33,15 @@ const allWidgets = Object.keys(contracts).map((accountId) => {
 return (
   <>
     <Widget
-      src={`mob.near/widget/PublicTagEditor`}
+      src={`${ownerId}/widget/PublicTagEditor`}
       key={`public-tag-editor-${props.accountId}`}
       props={{ contractId: props.accountId }}
     />
     <hr />
     {allWidgets}
+    <hr />
+    {tag !== "*" && (
+      <Widget src={`${ownerId}/widget/LabelDetails`} props={{ tag: tag }} />
+    )}
   </>
 );
