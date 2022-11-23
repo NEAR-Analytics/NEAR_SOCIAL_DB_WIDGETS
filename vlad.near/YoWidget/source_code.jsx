72523d8aebaa1768ec795a03c0adbdd69d981058
vgrichina <vgrichina>
2022-11-23 00:37:35 -0500

    Update YoWidget by vlad.near at 2022-11-23 00:37:35.369

--- vlad.near/YoWidget/source_code.jsx
+++ vlad.near/YoWidget/source_code.jsx
@@ -1,5 +1,34 @@
+const accountId = props.accountId ?? "*";
+
+const data = Social.keys(`${accountId}/post/yo`, "final", {
+  return_type: "History",
+});
+
+if (!data) {
+  return "Loading";
+}
+
+const processData = (data) => {
+  const accounts = Object.entries(data);
+
+  const allMemes = accounts
+    .map((account) => {
+      const accountId = account[0];
+      const blockHeights = account[1].post.meme;
+      return blockHeights.map((blockHeight) => ({
+        accountId,
+        blockHeight,
+      }));
+    })
+    .flat();
+
+  allMemes.sort((a, b) => b.blockHeight - a.blockHeight);
+  return allMemes;
+};
+
 return (
   <div>
     <button>yo!</button>
+    {data ? <div></div> : "Loading"}
   </div>
 );
