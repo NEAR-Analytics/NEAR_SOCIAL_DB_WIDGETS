a9bddd083f8e0dcea3450dfa8842c47ef89c0ba4
evgenykuzyakov <evgenykuzyakov>
2022-11-23 01:20:32 -0500

    Update YourNFTs by mob.near at 2022-11-23 01:20:32.870

--- mob.near/YourNFTs/source_code.jsx
+++ mob.near/YourNFTs/source_code.jsx
@@ -1,4 +1,9 @@
 const accountId = props.accountId || context.accountId;
+const thumbnails = props.thumbnails;
+
+if (!accountId) {
+  return <></>;
+}
 
 const f = fetch(
   `https://api.kitwallet.app/account/${accountId}/likelyNFTsFromBlock`
@@ -11,12 +16,16 @@ if (!f.ok) {
 const allNfts = f.body.list;
 
 return (
-  <>
+  <div className="d-flex gap-1 flex-wrap">
     {allNfts.map((contractId) => (
       <Widget
-        src="mob.near/widget/NftCollection"
+        src={
+          thumbnails
+            ? "mob.near/widget/NftCollectionThumbs"
+            : "mob.near/widget/NftCollection"
+        }
         props={{ accountId, contractId }}
       />
     ))}
-  </>
+  </div>
 );
