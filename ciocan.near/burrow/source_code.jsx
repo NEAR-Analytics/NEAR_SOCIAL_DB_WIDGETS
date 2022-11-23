4d5690a5ccbc40095a9bbf043e0c43dddf73d00f
ciocan <ciocan>
2022-11-23 14:02:12 -0500

    Update burrow by ciocan.near at 2022-11-23 14:02:12.414

--- ciocan.near/burrow/source_code.jsx
+++ ciocan.near/burrow/source_code.jsx
@@ -1,3 +1,21 @@
+const toAPY = (v) => Math.round(v * 100) / 100;
+
+const shrinkToken = (value, decimals, fixed) => {
+  return new Big(value).div(new Big(10).pow(decimals)).toFixed(fixed);
+};
+
+const toUsd = (balance, asset) =>
+  asset.price?.usd
+    ? Number(
+        shrinkToken(
+          balance,
+          asset.metadata.decimals + asset.config.extra_decimals
+        )
+      ) * asset.price.usd
+    : 0;
+
+const sumReducer = (sum, a) => sum + a;
+
 function power(x, y) {
   if (y === 0) {
     return 1;
@@ -30,32 +48,192 @@ function getAssets() {
 
   return assetsDetailed.map((asset, i) => {
     const price = prices.prices.find((p) => p.asset_id === asset.token_id);
-    const decimals = parseInt(price.price.decimals) - metadata[i].decimals;
+    const decimals =
+      parseInt(price.price.decimals || 0) - parseInt(metadata[i].decimals);
     const usd = price.price.multiplier / power(10, decimals);
+
+    // console.log(
+    //   metadata[i].symbol,
+    //   shrinkToken(
+    //     asset.supplied.balance,
+    //     asset.config.extra_decimals + metadata[i].decimals
+    //   )
+    // );
 
     return {
       ...asset,
       metadata: metadata[i],
       price: {
         ...price.price,
-        usd: usd ? usd : refPrices[asset.token_id].price,
+        usd: usd ? usd : parseFloat(refPrices[asset.token_id].price),
       },
     };
   });
 }
 
+// ------ rewards ------
+const getTotalBalance = (assets, source) =>
+  assets
+    .map((asset) => {
+      const netTvlMultiplier = asset.config.net_tvl_multiplier / 10000;
+      return (
+        toUsd(asset[source].balance, asset) * netTvlMultiplier +
+        (source === "supplied"
+          ? toUsd(asset.reserved, asset) * netTvlMultiplier
+          : 0)
+      );
+    })
+    .reduce(sumReducer, 0);
+
+const getNetLiquidityAPY = (assets) => {
+  const netLiquidityFarm = Near.view(
+    "contract.main.burrow.near",
+    "get_asset_farm",
+    { farm_id: "NetTvl" }
+  );
+
+  const totalDailyNetLiquidityRewards = Object.entries(netLiquidityFarm.rewards)
+    .map(([rewardTokenId, farm]) => {
+      const rewardAsset = assets.find((a) => a.token_id === rewardTokenId);
+      const assetDecimals =
+        rewardAsset.metadata.decimals + rewardAsset.config.extra_decimals;
+      const dailyAmount = Number(
+        shrinkToken(farm.reward_per_day, assetDecimals)
+      );
+      return (
+        dailyAmount *
+        rewardAsset.price.usd *
+        (rewardAsset.config.net_tvl_multiplier / 10000)
+      );
+    })
+    .reduce(sumReducer, 0);
+
+  const supplied = getTotalBalance(assets, "supplied");
+  const borrowed = getTotalBalance(assets, "borrowed");
+
+  const totalProtocolLiquidity = supplied - borrowed;
+  const netLiquidtyAPY =
+    ((totalDailyNetLiquidityRewards * 365) / totalProtocolLiquidity) * 100;
+
+  const rewardTokens = Object.entries(netLiquidityFarm.rewards).map(
+    ([rewardTokenId]) => rewardTokenId
+  );
+
+  return [netLiquidtyAPY, rewardTokens];
+};
+
+const unique = (value, index, self) => {
+  return self.indexOf(value) === index;
+};
+
+const getRewards = (assets) => {
+  const [apyRewardTvl, rewardTokensTVL] = getNetLiquidityAPY(assets);
+
+  const rewards = assets.map((asset) => {
+    const apyBase = asset["supply_apr"] * 100;
+    const apyBaseBorrow = asset["borrow_apr"] * 100;
+    const tokenId = asset.token_id;
+    const totalSupplyUsd = toUsd(asset.supplied.balance, asset);
+    const totalBorrowUsd = toUsd(asset.borrowed.balance, asset);
+
+    const suppliedFarmRewards =
+      asset.farms.find((farm) => farm.farm_id.Supplied === tokenId)?.rewards ||
+      {};
+
+    const rewardTokens = Object.entries(suppliedFarmRewards)
+      .map(([rewardTokenId]) => rewardTokenId)
+      .concat(rewardTokensTVL)
+      .filter(unique);
+
+    const apyRewards = Object.entries(suppliedFarmRewards).map(
+      ([rewardTokenId, reward]) => {
+        const rewardAsset = assets.find((a) => a.token_id === rewardTokenId);
+        const decimals =
+          rewardAsset.metadata.decimals + rewardAsset.config.extra_decimals;
+        const price = rewardAsset.price?.usd || 0;
+        return (
+          new Big(reward.reward_per_day)
+            .div(new Big(10).pow(decimals))
+            .mul(365)
+            .mul(price)
+            .div(totalSupplyUsd)
+            .mul(100)
+            .toNumber() || 0
+        );
+      }
+    );
+
+    const apyReward = apyRewards.reduce(sumReducer, 0);
+
+    const borrowedFarmRewards =
+      asset.farms.find((farm) => farm.farm_id.Borrowed === tokenId)?.rewards ||
+      {};
+
+    const rewardTokensBorrow = Object.entries(borrowedFarmRewards).map(
+      ([rewardTokenId]) => rewardTokenId
+    );
+
+    const apyRewardBorrow = Object.entries(borrowedFarmRewards)
+      .map(([rewardTokenId, reward]) => {
+        const rewardAsset = assets.find((a) => a.token_id === rewardTokenId);
+        const decimals =
+          rewardAsset.metadata.decimals + rewardAsset.config.extra_decimals;
+        const price = rewardAsset.price?.usd || 0;
+
+        if (!totalBorrowUsd) return 0;
+
+        return (
+          new Big(reward.reward_per_day)
+            .div(new Big(10).pow(decimals))
+            .mul(365)
+            .mul(price)
+            .div(totalBorrowUsd)
+            .mul(100)
+            .toNumber() || 0
+        );
+      })
+      .reduce(sumReducer, 0);
+
+    return {
+      token_id: asset.token_id,
+      chain: "NEAR",
+      project: "Burrow",
+      symbol: asset.metadata.symbol,
+      tvlUsd: totalSupplyUsd - totalBorrowUsd,
+      apyReward,
+      apyRewardTvl,
+      apyBase,
+      rewardTokens,
+      totalSupplyUsd,
+      totalBorrowUsd,
+      apyBaseBorrow,
+      apyRewardBorrow,
+      rewardTokensBorrow,
+      ltv: asset.config.volatility_ratio,
+    };
+  });
+
+  return rewards;
+};
+
 const assets = getAssets();
 console.log(assets);
+const rewards = getRewards(assets);
+console.log(rewards);
 
-const allAssets = assets.map((asset) => (
-  <li>
-    <span>{asset.metadata.symbol}</span>
-  </li>
-));
+const allAssets = assets.map((asset) => {
+  const r = rewards.find((a) => a.token_id === asset.token_id);
+  return (
+    <li>
+      <span>{asset.metadata.symbol}</span>
+      <span>({toAPY(r.apyBase + r.apyRewardTvl + r.apyReward)}% APY)</span>
+    </li>
+  );
+});
 
 return (
   <ul>
-    <h4>Assets</h4>
+    <h4>Burrow supplied assets</h4>
     {allAssets}
   </ul>
 );
