5aa366bc888b34cd961590e652bbbc80840b90bc
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-11-23 15:56:19 -0500

    Update answer_poll by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-11-23 15:56:19.159

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll/source_code.jsx
@@ -58,7 +58,7 @@ let answersData = Object.keys(answerDataFromBlockHeight).map((key) => {
   };
 });
 
-console.log("answData: ", answersData);
+// console.log("answData: ", answersData);
 
 const haveThisUserAlreadyVoted = () => {
   if (answersData.length == 0) {
@@ -90,7 +90,7 @@ const loadComments = () => {
       `${answersData[i].accountId}/post/answer__poll/${answersData[i].blockHeightOfAnswer}/user_answers`
     );
 
-    // console.log("answer: ", answer);
+    console.log("answer: ", answer);
 
     let answerTimeStamp = Social.get(
       `${answersData[i].accountId}/post/answer__poll/${answersData[i].blockHeightOfAnswer}/answer_timestamps`
