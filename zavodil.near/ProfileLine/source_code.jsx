07a244b2a728c63602d83ed95503888315f88913
zavodil <zavodil>
2022-11-23 14:26:52 -0500

    Update ProfileLine by zavodil.near at 2022-11-23 14:26:52.918

---
+++ zavodil.near/ProfileLine/source_code.jsx
@@ -0,0 +1,40 @@
+//ProfileLine
+const accountId = props.accountId ?? context.accountId;
+const link = props.link ?? true;
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+const inner = (
+  <>
+    <Widget
+      src="mob.near/widget/ProfileImage"
+      props={{
+        style: { width: "1.5em", height: "1.5em" },
+        profile,
+        accountId,
+        className: "d-inline-block",
+        imageClassName: "rounded w-100 h-100 align-top",
+      }}
+    />
+    <span>
+      {profile.name || "No-name profile"}
+      <span className="text-muted">@{accountId}</span>
+    </span>
+  </>
+);
+
+return link ? (
+  <a
+    href={
+      link !== true
+        ? link
+        : `#/mob.near/widget/ProfilePage?accountId=${accountId}`
+    }
+    className="link-dark text-truncate"
+    style={{ TextDecoration: none }}
+  >
+    {inner}
+  </a>
+) : (
+  <span className="text-truncate">{inner}</span>
+);
