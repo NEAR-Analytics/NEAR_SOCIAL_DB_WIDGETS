aebf42195d40d4d4b5040759bf4fc68e4c03bc4e
evgenykuzyakov <evgenykuzyakov>
2022-11-23 23:16:48 -0500

    Update FollowPage by mob.near at 2022-11-23 23:16:48.927

---
+++ mob.near/FollowPage/source_code.jsx
@@ -0,0 +1,19 @@
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+
+return (
+  <div className="py-1 px-1">
+    <div className="mx-auto">
+      <Widget
+        src="mob.near/widget/ProfileLarge"
+        props={{ accountId, profile, link: true }}
+      />
+
+      <div className="mt-3">
+        <Widget src="mob.near/widget/FollowTabs" props={{ accountId, tab }} />
+      </div>
+    </div>
+  </div>
+);
