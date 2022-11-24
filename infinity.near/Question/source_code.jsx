21bc4cfff6d2cf05f8f29315541475a0f06c2b88
jlwaugh <jlwaugh>
2022-11-24 16:43:32 -0500

    Update Question by infinity.near at 2022-11-24 16:43:32.526

--- infinity.near/Question/source_code.jsx
+++ infinity.near/Question/source_code.jsx
@@ -1,26 +1,20 @@
 const accountId = context.accountId;
 
 if (!accountId) {
-  return "Please connect your NEAR wallet to add a question :)";
+  return "Please connect your NEAR wallet to add questions :)";
 }
 
 State.init({
   question: { title: "", description: "" },
-  img: {},
   done: false,
 });
 
 const question = {
   title: state.question.title,
   description: state.question.description,
-  image: {},
 };
 
-if (state.img.cid) {
-  question.image.ipfs_cid = state.img.cid;
-}
-
-const hasQuestion = question.image.ipfs_cid || question.title;
+const hasQuestion = question.title;
 
 return (
   <div className="row mb-3">
@@ -31,54 +25,38 @@ return (
     <div className="mb-2">
       Question:
       <br />
-      <IpfsImageUpload image={state.img} />
     </div>
-    {question.image.ipfs_cid && (
-      <div className="mb-2">
-        Title <span className="text-secondary">(optional)</span>
-        <input type="text" value={state.question.title} />
-      </div>
-    )}
-    {state.question.title && (
-      <div className="mb-2">
-        Description <span className="text-secondary">(optional)</span>
-        <input type="text" value={state.question.description} />
-      </div>
-    )}
     <div className="mb-2">
-      {hasQuestion ? (
-        <CommitButton
-          data={{ post: { question } }}
-          onCommit={() => {
-            State.update({
-              question: { title: "", description: "" },
-              img: {},
-              done: true,
-            });
-          }}
-        >
-          Post question
-        </CommitButton>
-      ) : (
-        !props.noPrevQuestion && (
-          <a
-            className="btn btn-outline-primary"
-            href={`#/infinity.near/widget/Question?accountId=${accountId}`}
-          >
-            View your last question
-          </a>
-        )
-      )}
+      Title <span className="text-secondary">(optional)</span>
+      <input type="text" value={state.question.title} />
+    </div>
+    <div className="mb-2">
+      Description <span className="text-secondary">(optional)</span>
+      <input type="text" value={state.question.description} />
+    </div>
+    <div className="mb-2">
+      <CommitButton
+        data={{ post: { question } }}
+        onCommit={() => {
+          State.update({
+            question: { title: "", description: "" },
+            img: {},
+            done: true,
+          });
+        }}
+      >
+        Post question
+      </CommitButton>
+      <a
+        className="btn btn-outline-primary"
+        href={`#/infinity.near/widget/Question?accountId=${accountId}`}
+      >
+        View your last question
+      </a>
     </div>
     <hr />
     {state.done && !hasQuestion && (
       <div className="alert alert-success">Success!</div>
-    )}
-    {(hasQuestion || !props.noPrevQuestion) && (
-      <Widget
-        src="infinity.near/widget/Question"
-        props={{ question: hasQuestion ? question : undefined }}
-      />
     )}
   </div>
 );
