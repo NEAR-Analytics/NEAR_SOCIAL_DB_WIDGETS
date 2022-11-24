77edf9704f6f9b4f87d68afc2a8175df5318387b
evgenykuzyakov <evgenykuzyakov>
2022-11-23 23:05:44 -0500

    Update ProfilePage by mob.near at 2022-11-23 23:05:44.764

--- mob.near/ProfilePage/source_code.jsx
+++ mob.near/ProfilePage/source_code.jsx
@@ -16,65 +16,9 @@ const showEditButton =
   accountId === context.accountId;
 
 const name = profile.name || "No-name profile";
-const description = profile.description;
 const image = profile.image;
 const backgroundImage = profile.backgroundImage;
 const tags = Object.keys(profile.tags ?? {});
-
-const linktree = Object.entries(profile.linktree ?? {});
-const linktreeElements = {
-  website: {
-    prefix: "https://",
-    icon: "bi-globe2",
-  },
-  github: {
-    prefix: "https://github.com/",
-    icon: "bi-github",
-  },
-  twitter: {
-    prefix: "https://twitter.com/",
-    icon: "bi-twitter",
-  },
-  telegram: {
-    prefix: "https://t.me/",
-    icon: "bi-telegram",
-  },
-};
-
-const linktreeObjects = linktree.map((o) => {
-  const key = o[0];
-  let value = o[1];
-  if (!value) {
-    return null;
-  }
-  const e = linktreeElements[key];
-  if (e.prefix) {
-    value = value && value.replace(e.prefix, "");
-  }
-  const icon = e.icon ? (
-    <i className={`bi ${e.icon ?? ""} text-secondary me-1`}></i>
-  ) : (
-    ""
-  );
-  return e.prefix ? (
-    <div>
-      <a href={`${e.prefix}${value}`}>
-        {icon}
-        {value}
-      </a>
-    </div>
-  ) : (
-    <div>
-      {key}: {icon}
-      {value}
-    </div>
-  );
-});
-
-State.init({
-  loadNFT: false,
-  loadWidgets: false,
-});
 
 return (
   <div className="py-1 px-1">
@@ -151,7 +95,12 @@ return (
                 />
               </div>
             </div>
-            <div style={{ minWidth: "12rem" }}>{linktreeObjects}</div>
+            <div style={{ minWidth: "12rem" }}>
+              <Widget
+                src="mob.near/widget/LinkTree"
+                props={{ linktree: profile.linktree }}
+              />
+            </div>
           </div>
 
           {tags.length > 0 && (
@@ -186,90 +135,10 @@ return (
       </div>
 
       <div className="mt-3">
-        <ul
-          className="nav nav-pills nav-fill mb-4"
-          id="pills-tab"
-          role="tablist"
-        >
-          <li className="nav-item" role="presentation">
-            <button
-              className="nav-link active"
-              id="pills-bio-tab"
-              data-bs-toggle="pill"
-              data-bs-target="#pills-bio"
-              type="button"
-              role="tab"
-              aria-controls="pills-bio"
-              aria-selected="true"
-            >
-              Bio
-            </button>
-          </li>
-          <li className="nav-item" role="presentation">
-            <button
-              className="nav-link"
-              id="pills-nft-tab"
-              data-bs-toggle="pill"
-              data-bs-target="#pills-nft"
-              type="button"
-              role="tab"
-              aria-controls="pills-nft"
-              aria-selected="false"
-              onClick={() => {
-                !state.loadNFT && State.update({ loadNFT: true });
-              }}
-            >
-              NFTs
-            </button>
-          </li>
-          <li className="nav-item" role="presentation">
-            <button
-              className="nav-link"
-              id="pills-widget-tab"
-              data-bs-toggle="pill"
-              data-bs-target="#pills-widget"
-              type="button"
-              role="tab"
-              aria-controls="pills-widget"
-              aria-selected="false"
-              onClick={() => {
-                !state.loadWidgets && State.update({ loadWidgets: true });
-              }}
-            >
-              Widgets
-            </button>
-          </li>
-        </ul>
-        <div className="tab-content" id="pills-tabContent">
-          <div
-            className="tab-pane fade in show active"
-            id="pills-bio"
-            role="tabpanel"
-            aria-labelledby="pills-bio-tab"
-          >
-            <Markdown text={description} />
-          </div>
-          <div
-            className="tab-pane fade nft"
-            id="pills-nft"
-            role="tabpanel"
-            aria-labelledby="pills-nft-tab"
-          >
-            {state.loadNFT && (
-              <Widget src="mob.near/widget/YourNFTs" props={{ accountId }} />
-            )}
-          </div>
-          <div
-            className="tab-pane fade widget"
-            id="pills-widget"
-            role="tabpanel"
-            aria-labelledby="pills-widget-tab"
-          >
-            {state.loadWidgets && (
-              <Widget src="mob.near/widget/LastWidgets" props={{ accountId }} />
-            )}
-          </div>
-        </div>
+        <Widget
+          src="mob.near/widget/ProfileTabs"
+          props={{ accountId, profile }}
+        />
       </div>
     </div>
   </div>
