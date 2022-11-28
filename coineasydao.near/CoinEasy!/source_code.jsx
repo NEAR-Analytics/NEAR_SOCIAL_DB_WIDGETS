81a0d0ce94924bc5fc74930aa9cad523d1ad91be
coineasydao.near <coineasydao.near>
2022-11-27 22:06:32 -0500

    Update CoinEasy! by coineasydao.near at 2022-11-27 22:06:32.418

---
+++ coineasydao.near/CoinEasy!/source_code.jsx
@@ -0,0 +1,78 @@
+const card = {
+  background: "#FFAE42",
+  border: "1px solid black",
+  borderRadius: "5px",
+  textAlign: "center",
+};
+
+const button = {
+  borderRadius: "5px",
+  margin: "5px 0",
+  padding: "8px",
+  textAlign: "center",
+  background: "#FFAE42",
+  border: "1px solid black",
+};
+
+const accountId = props.accountId ?? "*";
+
+const data = Social.keys(`${accountId}/post/CoinEasy`, "final", {
+  return_type: "History",
+});
+
+if (!data) {
+  return "Loading";
+}
+
+const processData = (data) => {
+  const accounts = Object.entries(data);
+
+  console.log("data", data);
+  const allItems = accounts
+    .map((account) => {
+      const accountId = account[0];
+      const blockHeights = account[1].post.GM;
+      return blockHeights.map((blockHeight) => ({
+        accountId,
+        blockHeight,
+      }));
+    })
+    .flat();
+
+  allItems.sort((a, b) => b.blockHeight - a.blockHeight);
+  console.log("allItems", allItems);
+  return allItems;
+};
+
+if (JSON.stringify(data) !== JSON.stringify(state.data || {})) {
+  State.update({
+    data,
+    allItems: processData(data),
+  });
+}
+
+return (
+  <div>
+    <CommitButton
+      style={button}
+      data={{ post: { CoinEasy: "CoinEasy!" } }}
+      onCommit={() => {
+        State.update({
+          // TODO: Feed needs reload?
+        });
+      }}
+    >
+      CoinEasy!
+    </CommitButton>
+
+    <div>
+      {state.allItems
+        ? state.allItems.map(({ accountId }) => (
+            <div style={card}>
+              {accountId} said <b>CoinEasy!</b>
+            </div>
+          ))
+        : "Loading..."}
+    </div>
+  </div>
+);
