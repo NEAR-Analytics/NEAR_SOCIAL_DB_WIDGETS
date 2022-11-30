5bfc55509f36ae3259e68065baff8dcc2434aa7b
JoeHank9 <JoeHank9>
2022-11-30 02:13:46 -0500

    Update profile by joehank.near at 2022-11-30 02:13:46.148

---
+++ joehank.near/profile/source_code.jsx
@@ -0,0 +1,39 @@
+return (
+  <div>
+    <h1 align="center">Hi 👋, I'm Joe Hank</h1>
+    <h3 align="center">A passionate Blockhchain Developer from México</h3>
+    <h5 align="left">Working at:</h5>
+    <h6>
+      {" "}
+      - 🌱 I'm currently learning and using: Rust, Solidity, Next.js, React,
+      AssamblyScript, JavaScritp, TypeScript
+    </h6>
+    <h6> - 💬 Ask me about: Blockchain, NTFs, SmartContracts</h6>
+    <h6>
+      {" "}
+      - 📫 How to reach me:
+      <a href="mailto:jcanalesn9@gmail.com">jcanalesn9@gmail.com</a>
+    </h6>
+    <h5 align="left">Connect with me:</h5>
+    <p align="left">
+      <a href="https://twitter.com/darkjoehank" target="blank">
+        <img
+          align="center"
+          src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg"
+          alt="JoeHank9"
+          height="30"
+          width="40"
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/joehank/" target="blank">
+        <img
+          align="center"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"
+          alt="JoeHank"
+          height="30"
+          width="30"
+        />
+      </a>
+    </p>
+  </div>
+);
