afe9dd4fe5f55877425d139841fb86e47b97f1cc
evgenykuzyakov <evgenykuzyakov>
2022-11-30 01:31:14 -0500

    Update NftImage2 by mob.near at 2022-11-30 01:31:14.811

--- mob.near/NftImage2/source_code.jsx
+++ mob.near/NftImage2/source_code.jsx
@@ -91,7 +91,13 @@ const replaceIpfs = () => {
 const thumb = (imageUrl) =>
   thumbnail ? `https://i.near.social/${thumbnail}/${imageUrl}` : imageUrl;
 
-console.log(contractId, tokenId, imageUrl, state.imageUrl);
+const match =
+  /^(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})(\/.*)?$/g.exec(
+    imageUrl
+  );
+if (match) {
+  imageUrl = `https://ipfs.near.social/ipfs/${match[1]}${match[2] || ""}`;
+}
 
 return (
   <img
