73d7bb9090e025fbb0362325fcf8fdcfadc24a21
evgenykuzyakov <evgenykuzyakov>
2022-11-30 01:28:16 -0500

    Update NftImage2 by mob.near at 2022-11-30 01:28:16.839

--- mob.near/NftImage2/source_code.jsx
+++ mob.near/NftImage2/source_code.jsx
@@ -37,10 +37,6 @@ if (nftMetadata && nftToken) {
   let tokenMetadata = nftToken.metadata;
   let tokenMedia = tokenMetadata.media || "";
 
-  const ownerId = nftToken.owner_id["Account"]
-    ? nftToken.owner_id["Account"]
-    : nftToken.owner_id;
-
   imageUrl =
     tokenMedia.startsWith("https://") ||
     tokenMedia.startsWith("http://") ||
@@ -67,6 +63,7 @@ if (nftMetadata && nftToken) {
 }
 
 const replaceIpfs = () => {
+  console.log(contractId, tokenId, imageUrl, state.imageUrl);
   if (!state.imageUrl && imageUrl) {
     const match =
       /^(?:https?:\/\/[^\/]+\/ipfs\/)?(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})(\/.*)?$/g.exec(
