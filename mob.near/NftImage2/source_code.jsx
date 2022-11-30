c3af0f0eb7375c7480fda24a6e174967ef3ed1fe
evgenykuzyakov <evgenykuzyakov>
2022-11-30 01:34:04 -0500

    Update NftImage2 by mob.near at 2022-11-30 01:34:04.114

--- mob.near/NftImage2/source_code.jsx
+++ mob.near/NftImage2/source_code.jsx
@@ -62,6 +62,14 @@ if (nftMetadata && nftToken) {
   }
 }
 
+const match =
+  /^(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})(\/.*)?$/g.exec(
+    imageUrl
+  );
+if (match) {
+  imageUrl = `https://ipfs.near.social/ipfs/${match[1]}${match[2] || ""}`;
+}
+
 const replaceIpfs = () => {
   console.log(contractId, tokenId, imageUrl, state.imageUrl);
   if (!state.imageUrl && imageUrl) {
@@ -90,14 +98,6 @@ const replaceIpfs = () => {
 
 const thumb = (imageUrl) =>
   thumbnail ? `https://i.near.social/${thumbnail}/${imageUrl}` : imageUrl;
-
-const match =
-  /^(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})(\/.*)?$/g.exec(
-    imageUrl
-  );
-if (match) {
-  imageUrl = `https://ipfs.near.social/ipfs/${match[1]}${match[2] || ""}`;
-}
 
 return (
   <img
