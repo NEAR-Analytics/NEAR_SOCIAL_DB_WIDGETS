fdc282a23afa4c872d5b03a22467c6e654fd0f6e
evgenykuzyakov <evgenykuzyakov>
2022-11-29 23:11:57 -0500

    Update NotificationFeed by mob.near at 2022-11-29 23:11:57.030

--- mob.near/NotificationFeed/source_code.jsx
+++ mob.near/NotificationFeed/source_code.jsx
@@ -22,4 +22,30 @@ if (notifications.length === 0) {
 
 Storage.set("lastBlockHeight", notifications[0].blockHeight);
 
-return notifications;
+return (
+  <>
+    {notifications.map(({ accountId, blockHeight, value }) => (
+      <div className="d-flex justify-content-between mb-3">
+        <div className="me-4 text-truncate">
+          <div className="text-truncate">
+            <Widget src="mob.near/widget/ProfileLine" props={{ accountId }} />
+          </div>
+          <div
+            className="text-truncate text-muted"
+            style={{ paddingLeft: "1.8em" }}
+          >
+            {value.type === "follow"
+              ? "followed you"
+              : value.type === "unfollow"
+              ? "unfollowed you"
+              : "???"}
+            <Widget src="mob.near/widget/TimeAgo" props={{ blockHeight }} />
+          </div>
+        </div>
+        <div className="text-nowrap">
+          <Widget src="mob.near/widget/FollowButton" props={{ accountId }} />
+        </div>
+      </div>
+    ))}
+  </>
+);
