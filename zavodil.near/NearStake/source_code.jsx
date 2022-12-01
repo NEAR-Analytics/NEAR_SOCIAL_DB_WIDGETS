6075ecd145635d98584bfac4b79c2d1ce22e64cf
zavodil <zavodil>
2022-11-30 19:15:10 -0500

    Update NearStake by zavodil.near at 2022-11-30 19:15:10.282

---
+++ zavodil.near/NearStake/source_code.jsx
@@ -0,0 +1,49 @@
+//NearStake
+if (!context.accountId) {
+  return "Please sign in with NEAR wallet to stake NEAR";
+}
+
+let poolId = props.poolId || "zavodil.poolv1.near";
+let serviceFee = props.serviceFee || "";
+
+initState({ amount: "1", poolId });
+
+const onStakeClick = () => {
+  const gas = 300 * 1000000000000;
+  const deposit = parseFloat(state.amount) * 1000000 + "000000000000000000";
+
+  Near.call(state.poolId, "deposit_and_stake", {}, gas, deposit);
+};
+
+return (
+  <div>
+    <h1>
+      Stake NEAR{" "}
+      <a href="/#/zavodil.near/widget/StakingPools" target="_blank">
+        <span className="badge bg-secondary fs-6 align-middle">All Pools</span>
+      </a>
+    </h1>
+    <p>
+      Pool: <input value={state.poolId} readOnly />
+      {serviceFee ? (
+        <div
+          id="emailHelp"
+          className="form-text"
+        >{`Service Fee: ${serviceFee}%.`}</div>
+      ) : (
+        ""
+      )}
+    </p>
+    <p>
+      Amount (NEAR):
+      <input type="number" value={state.amount} />
+    </p>
+    <CommitButton
+      disabled={context.loading}
+      className={`btn ${context.loading ? "btn-outline-dark" : "btn-primary"}`}
+      onClick={onStakeClick}
+    >
+      Stake
+    </CommitButton>
+  </div>
+);
