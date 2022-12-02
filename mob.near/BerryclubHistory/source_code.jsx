1805de6bfc5fc811176e23d414403b6a8619df35
evgenykuzyakov <evgenykuzyakov>
2022-12-02 02:47:45 -0500

    Update BerryclubHistory by mob.near at 2022-12-02 02:47:45.947

--- mob.near/BerryclubHistory/source_code.jsx
+++ mob.near/BerryclubHistory/source_code.jsx
@@ -8,10 +8,11 @@ const currentBlockHeight = currentBlock.header.height;
 const minBlockHeight = 21793900;
 const numBlocks = currentBlockHeight - minBlockHeight;
 
+const randomBlockHeight = () =>
+  minBlockHeight + Math.floor(Math.random() * numBlocks);
+
 State.init({
-  blockHeight: props.blockHeight
-    ? parseInt(props.blockHeight)
-    : currentBlockHeight,
+  blockHeight: randomBlockHeight(),
 });
 
 return (
@@ -21,7 +22,7 @@ return (
       props={{
         min: minBlockHeight,
         max: currentBlockHeight,
-        defaultValue: currentBlockHeight,
+        defaultValue: state.blockHeight,
         onPointerUp: (blockHeight) => State.update({ blockHeight }),
         title: "Block Height",
       }}
@@ -46,23 +47,27 @@ return (
           className="btn btn-secondary"
           onClick={() => {
             State.update({
-              blockHeight:
-                minBlockHeight + Math.floor(Math.random() * numBlocks),
+              blockHeight: randomBlockHeight(),
             });
           }}
         >
           Random Board
         </button>
       </div>
-      <div style={{ height: "70vh" }}>
+      <div className="mb-2" style={{ height: "70vh" }}>
         <a
-          href={`#/mob.near/widget/BerryclubHistory?blockHeight=${state.blockHeight}`}
+          href={`#/mob.near/widget/BerryclubBoard?blockHeight=${state.blockHeight}`}
+          target="_blank"
         >
           <Widget
             src="mob.near/widget/BerryclubBoard"
             props={{ blockHeight: state.blockHeight }}
           />
         </a>
+      </div>
+      <hr />
+      <div>
+        <Widget src="mob.near/widget/BerryclubFeed" />
       </div>
     </div>
   </div>
