9ab6bba962b4ceb32528cdca285cc64a47524948
evgenykuzyakov <evgenykuzyakov>
2022-12-01 19:21:38 -0500

    Update NftImage by mob.near at 2022-12-01 19:21:38.855

--- mob.near/NftImage/source_code.jsx
+++ mob.near/NftImage/source_code.jsx
@@ -64,7 +64,7 @@ if (nftMetadata && nftToken) {
 }
 
 const rex =
-  /^(?:https?:\/\/)(?:[^\/]+\/ipfs\/)?(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})(?:\.[^\/]+)(\/.*)?$/g;
+  /^(?:https?:\/\/)(?:[^\/]+\/ipfs\/)?(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})(?:\.[^\/]+)?(\/.*)?$/g;
 rex.lastIndex = 0;
 
 const replaceIpfs = (imageUrl) => {
