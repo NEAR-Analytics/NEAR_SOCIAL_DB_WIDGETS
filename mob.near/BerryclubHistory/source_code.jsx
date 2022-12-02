0b4d3d3f637792c49e0bedcb5453587a585e928b
evgenykuzyakov <evgenykuzyakov>
2022-12-01 20:36:13 -0500

    Update BerryclubHistory by mob.near at 2022-12-01 20:36:13.649

---
+++ mob.near/BerryclubHistory/source_code.jsx
@@ -0,0 +1,38 @@
+const currentBlock = Near.block("optimistic");
+
+if (!currentBlock) {
+  return "Loading";
+}
+
+const currentBlockHeight = currentBlock.header.height;
+
+const minBlockHeight = 21793900;
+
+State.init({
+  blockHeight: currentBlockHeight,
+});
+
+return (
+  <div>
+    <Widget
+      src="mob.near/widget/Range"
+      props={{
+        min: minBlockHeight,
+        max: currentBlockHeight,
+        defaultValue: currentBlockHeight,
+        onMouseUp: (blockHeight) => State.update({ blockHeight }),
+        title: "Block Height",
+      }}
+    />
+
+    <div>
+      Board #{state.blockHeight}
+      <div>
+        <Widget
+          src="mob.near/widget/BerryclubBoard"
+          props={{ blockHeight: state.blockHeight }}
+        />
+      </div>
+    </div>
+  </div>
+);
