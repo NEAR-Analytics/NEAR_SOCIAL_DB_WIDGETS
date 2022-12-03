09c9fb1850c1749ee86c8482dc6961a3ecfcdc56
evgenykuzyakov <evgenykuzyakov>
2022-12-02 20:18:22 -0500

    Update BerryclubHistory by mob.near at 2022-12-02 20:18:22.253

--- mob.near/BerryclubHistory/source_code.jsx
+++ mob.near/BerryclubHistory/source_code.jsx
@@ -1,15 +1,18 @@
-const currentBlock = Near.block("optimistic");
+const currentBlock = Near.block("final");
 
 if (!currentBlock) {
   return "Loading";
 }
 
 const currentBlockHeight = currentBlock.header.height;
-
 const minBlockHeight = 21793900;
 
+const numBlocks = currentBlockHeight - minBlockHeight;
+const randomBlockHeight = () =>
+  minBlockHeight + Math.floor(Math.random() * numBlocks);
+
 State.init({
-  blockHeight: currentBlockHeight,
+  blockHeight: randomBlockHeight(),
 });
 
 return (
@@ -24,20 +27,45 @@ return (
         title: "Block Height",
       }}
     />
-
+    <div className="mb-2 d-flex gap-2 flex-wrap">
+      <CommitButton
+        className="btn btn-primary"
+        data={{
+          index: {
+            post: JSON.stringify({
+              key: "berryclub",
+              value: state.blockHeight,
+            }),
+          },
+        }}
+      >
+        Share Board #{state.blockHeight}
+      </CommitButton>
+      <button
+        className="btn btn-secondary"
+        onClick={() => {
+          State.update({
+            blockHeight: randomBlockHeight(),
+          });
+        }}
+      >
+        Random Board
+      </button>
+    </div>
+    <div style={{ height: "min(50vh, 95vw)" }}>
+      <a
+        href={`#/mob.near/widget/BerryclubBoard?blockHeight=${state.blockHeight}`}
+        target="_blank"
+      >
+        <Widget
+          src="mob.near/widget/BerryclubBoard"
+          props={{ blockHeight: state.blockHeight }}
+        />
+      </a>
+    </div>
+    <hr />
     <div>
-      Board #{state.blockHeight}
-      <div style={{ height: "70vh" }}>
-        <a
-          href={`#/mob.near/widget/BerryclubBoard?blockHeight=${state.blockHeight}`}
-          target="_blank"
-        >
-          <Widget
-            src="mob.near/widget/BerryclubBoard"
-            props={{ blockHeight: state.blockHeight }}
-          />
-        </a>
-      </div>
+      <Widget src="mob.near/widget/BerryclubFeed" />
     </div>
   </div>
 );
