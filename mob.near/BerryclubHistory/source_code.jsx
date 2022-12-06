77cc604fa4ce48ae6a48bae115af9d2f4e928c13
evgenykuzyakov <evgenykuzyakov>
2022-12-06 00:01:44 -0500

    Update BerryclubHistory by mob.near at 2022-12-06 00:01:44.287

--- mob.near/BerryclubHistory/source_code.jsx
+++ mob.near/BerryclubHistory/source_code.jsx
@@ -4,19 +4,28 @@ if (!currentBlock) {
   return "Loading";
 }
 
+const Contract = {
+  Berryclub: "berryclub.ek.near",
+  Dacha: "v1.dacha-finance.near",
+};
+
 const finetunes = [
   [-10000, -1000, -100, -10, -1],
   [1, 10, 100, 1000, 10000],
 ];
 
 const currentBlockHeight = currentBlock.header.height;
-const minBlockHeight = 21793900;
+const minBlockHeight = state.contract === Contract.Dacha ? 54570186 : 21793900;
+
+const intoRange = (blockHeight) =>
+  Math.min(currentBlockHeight, Math.max(minBlockHeight, parseInt(blockHeight)));
 
 const numBlocks = currentBlockHeight - minBlockHeight;
 const randomBlockHeight = () =>
   minBlockHeight + Math.floor(Math.random() * numBlocks);
 
 State.init({
+  contract: Contract.Berryclub,
   blockHeight: props.blockHeight
     ? parseInt(props.blockHeight)
     : randomBlockHeight(),
@@ -24,12 +33,46 @@ State.init({
 
 return (
   <div>
+    <div className="btn-group" role="group" aria-label="Contract selector">
+      <input
+        type="radio"
+        className="btn-check"
+        name="contract-radio"
+        id="contract-berryclub"
+        autoComplete="off"
+        checked={state.contract === Contract.Berryclub}
+        onChange={() => State.update({ contract: Contract.Berryclub })}
+        value={Contract.Berryclub}
+        title={"Set contract to Berryclub"}
+      />
+      <label
+        className="btn btn-outline-success fs-2"
+        htmlFor="contract-berryclub"
+      >
+        🥑
+      </label>
+
+      <input
+        type="radio"
+        className="btn-check"
+        name="contract-radio"
+        id="contract-dacha"
+        autoComplete="off"
+        checked={state.contract === Contract.Dacha}
+        onChange={() => State.update({ contract: Contract.Dacha })}
+        value={Contract.Dacha}
+        title={"Set contract to Dacha Finance"}
+      />
+      <label className="btn btn-outline-success fs-2" htmlFor="contract-dacha">
+        🥔
+      </label>
+    </div>
     <Widget
       src="mob.near/widget/Range"
       props={{
         min: minBlockHeight,
         max: currentBlockHeight,
-        value: state.blockHeight,
+        value: intoRange(state.blockHeight),
         onPointerUp: (blockHeight) => State.update({ blockHeight }),
         title: "Block Height",
       }}
@@ -41,7 +84,10 @@ return (
           index: {
             post: JSON.stringify({
               key: "berryclub",
-              value: state.blockHeight,
+              value: {
+                blockHeight: state.blockHeight,
+                contract: state.contract,
+              },
             }),
           },
         }}
@@ -66,13 +112,7 @@ return (
               className="btn btn-outline-secondary"
               onClick={() => {
                 State.update({
-                  blockHeight: Math.min(
-                    currentBlockHeight,
-                    Math.max(
-                      minBlockHeight,
-                      parseInt(state.blockHeight + value)
-                    )
-                  ),
+                  blockHeight: intoRange(parseInt(state.blockHeight) + value),
                 });
               }}
             >
@@ -85,7 +125,7 @@ return (
     <div style={{ height: "min(50vh, 95vw)" }}>
       <Widget
         src="mob.near/widget/BerryclubBoard"
-        props={{ blockHeight: state.blockHeight }}
+        props={{ blockHeight: state.blockHeight, contract: state.contract }}
       />
     </div>
     <hr />
