dec0c01a196e6d58610605aee5783b0533b7e517
evgenykuzyakov <evgenykuzyakov>
2022-12-06 00:23:48 -0500

    Update ItemFeed by mob.near at 2022-12-06 00:23:48.333

--- mob.near/ItemFeed/source_code.jsx
+++ mob.near/ItemFeed/source_code.jsx
@@ -2,44 +2,27 @@ const items = props.items;
 const renderItem = props.renderItem;
 const perPage = props.perPage || 10;
 
-State.init({
-  items,
-  widgets: [],
-});
-
-if (state.items.length != items.length) {
-  if (items.length > state.items.length) {
-    const newItems = items
-      .slice(0, items.length - state.items.length)
-      .map(renderItem);
-    const widgets = [...newItems, ...state.widgets];
-    State.update({
-      widgets,
-      items,
-    });
-  } else {
-    State.update({
-      widgets: [],
-      items,
-    });
-  }
+const jItems = JSON.stringify(items);
+if (state.jItems !== jItems) {
+  State.update({
+    widgets: 0,
+    jItems,
+  });
 }
 
 const makeMoreItems = () => {
-  const newItems = items
-    .slice(state.widgets.length, state.widgets.length + perPage)
-    .map(renderItem);
-  state.widgets.push(...newItems);
-  State.update();
+  State.update({
+    widgets: state.widgets + perPage,
+  });
 };
 
 return (
   <InfiniteScroll
     pageStart={0}
     loadMore={makeMoreItems}
-    hasMore={state.widgets.length < items.length}
+    hasMore={state.widgets < items.length}
     loader={<div className="loader">Loading ...</div>}
   >
-    {state.widgets}
+    {items.slice(0, state.widgets).map(renderItem)}
   </InfiniteScroll>
 );
