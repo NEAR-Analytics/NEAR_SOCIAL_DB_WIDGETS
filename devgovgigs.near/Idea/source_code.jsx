98dae99235803e2ba84a30874a416485abc70c6d
devgovgigs.near <devgovgigs.near>
2022-12-06 23:21:56 -0500

    Update Idea by devgovgigs.near at 2022-12-06 23:21:56.221

---
+++ devgovgigs.near/Idea/source_code.jsx
@@ -0,0 +1,11 @@
+const idea_id = props.idea_id ? parseInt(props.idea_id) : 0;
+const idea =
+  props.idea ?? Near.view("devgovgigs.near", "get_idea", { idea_id });
+
+return (
+  <div>
+    <h3>{idea.name}</h3>
+    <p>Author: {idea.submitter_id}</p>
+    <p>{idea.description}</p>
+  </div>
+);
