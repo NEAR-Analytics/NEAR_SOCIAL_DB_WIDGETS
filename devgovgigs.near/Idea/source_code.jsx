861116f7aa54ca82b52c698aae215a21a03ae8d5
devgovgigs.near <devgovgigs.near>
2022-12-07 00:58:56 -0500

    Update Idea by devgovgigs.near at 2022-12-07 00:58:56.652

--- devgovgigs.near/Idea/source_code.jsx
+++ devgovgigs.near/Idea/source_code.jsx
@@ -2,22 +2,29 @@ const idea_id = props.idea_id ? parseInt(props.idea_id) : 0;
 const idea =
   props.idea ?? Near.view("devgovgigs.near", "get_idea", { idea_id });
 
+function readableDate(timestamp) {
+  var a = new Date(timestamp);
+  return a.toDateString() + " " + a.toLocaleTimeString();
+}
+
+const timestamp = readableDate(
+  idea.timestamp ? idea.timestamp / 1000000 : Date.now()
+);
+
 const submissions = Near.view("devgovgigs.near", "get_submissions", {
   idea_id,
 });
-
-function readableDate(UNIX_timestamp) {
-  var a = new Date(parseInt(UNIX_timestamp) / 1000000);
-  return a.toDateString() + " " + a.toLocaleTimeString();
-}
 
 const submissionHeader = submissions.length > 0 ? <h3>Submissions</h3> : null;
 
 return (
   <div>
+    <Widget
+      src={`mob.near/widget/ProfileLine`}
+      props={{ accountId: idea.submitter_id }}
+    />
     <h3>{idea.name}</h3>
-    <p>Author: {idea.submitter_id}</p>
-    <p>Timestamp: {readableDate(idea.timestamp)}</p>
+    <p>Timestamp: {timestamp}</p>
     <p>{idea.description}</p>
     {submissionHeader}
     {submissions
