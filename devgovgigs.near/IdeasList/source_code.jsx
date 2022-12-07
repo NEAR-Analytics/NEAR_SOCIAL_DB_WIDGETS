4fffef3be0498dfa3758911da27a7e3e511903f4
devgovgigs.near <devgovgigs.near>
2022-12-06 23:33:27 -0500

    Update IdeasList by devgovgigs.near at 2022-12-06 23:33:27.896

--- devgovgigs.near/IdeasList/source_code.jsx
+++ devgovgigs.near/IdeasList/source_code.jsx
@@ -8,6 +8,14 @@ console.log(ideas);
 return (
   <div>
     <h2>Ideas</h2>
+    <p>
+      <a
+        className="btn btn-outline-primary ms-2"
+        href="#/devgovgigs.near/widget/AddIdea"
+      >
+        Add idea
+      </a>
+    </p>
     {ideas
       ? ideas.map((idea) => {
           return <Widget src="devgovgigs.near/widget/Idea" props={{ idea }} />;
