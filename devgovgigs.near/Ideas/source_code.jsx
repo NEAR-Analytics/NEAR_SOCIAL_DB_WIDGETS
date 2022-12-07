f139592f26fc31f2d468ecc454853558c9e9d952
devgovgigs.near <devgovgigs.near>
2022-12-06 23:35:41 -0500

    Update Ideas by devgovgigs.near at 2022-12-06 23:35:41.476

---
+++ devgovgigs.near/Ideas/source_code.jsx
@@ -0,0 +1,25 @@
+const ideas = Near.view("devgovgigs.near", "get_ideas");
+
+// TODO: Sort ideas based on how much in total USD equivalent was pledged through sponsorships.
+// TODO: Sort ideas based on a criteria that includes social activity, like attestations.
+
+console.log(ideas);
+
+return (
+  <div>
+    <h2>Ideas</h2>
+    <p>
+      <a
+        className="btn btn-outline-primary ms-2"
+        href="#/devgovgigs.near/widget/AddIdea"
+      >
+        Add idea
+      </a>
+    </p>
+    {ideas
+      ? ideas.map((idea) => {
+          return <Widget src="devgovgigs.near/widget/Idea" props={{ idea }} />;
+        })
+      : ""}
+  </div>
+);
