1b76156bdbd24cc9709dba7c0995b27b677e14dd
devgovgigs.near <devgovgigs.near>
2022-12-07 01:30:32 -0500

    Update Ideas by devgovgigs.near at 2022-12-07 01:30:32.109

--- devgovgigs.near/Ideas/source_code.jsx
+++ devgovgigs.near/Ideas/source_code.jsx
@@ -3,28 +3,12 @@ const ideas = Near.view(ownerId, "get_ideas");
 
 // TODO: Sort ideas based on how much in total USD equivalent was pledged through sponsorships.
 // TODO: Sort ideas based on a criteria that includes social activity, like attestations.
-
-console.log(ideas);
+// TODO: Make IdeaEditor collapsed by the default.
 
 return (
   <div className="row">
-    <p>
-      <button
-        class="btn btn-primary"
-        type="button"
-        data-toggle="collapse"
-        data-target="#collapseExample"
-        aria-expanded="false"
-        aria-controls="collapseExample"
-      >
-        Button with data-target
-      </button>
-    </p>
-    <div class="collapse" id="collapseExample">
-      <div>
-        <Widget src={`${ownerId}/widget/IdeaEditor`} />
-      </div>
-    </div>
+    <Widget src={`${ownerId}/widget/IdeaEditor`} />
+
     <hr />
     {ideas
       ? ideas.map((idea) => {
