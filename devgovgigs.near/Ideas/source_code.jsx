110751058d00717d9e7070708cc662addcf2fa84
devgovgigs.near <devgovgigs.near>
2022-12-07 23:30:25 -0500

    Update Ideas by devgovgigs.near at 2022-12-07 23:30:25.691

--- devgovgigs.near/Ideas/source_code.jsx
+++ devgovgigs.near/Ideas/source_code.jsx
@@ -3,7 +3,6 @@ const ideas = Near.view(ownerId, "get_ideas");
 
 // TODO: Sort ideas based on how much in total USD equivalent was pledged through sponsorships.
 // TODO: Sort ideas based on a criteria that includes social activity, like attestations.
-// TODO: Make IdeaEditor collapsed by the default.
 
 return (
   <div className="row">
