fd3bc5643541dd3102c58f742c1e95c49b4a91f4
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-08 14:55:17 -0500

    Update newTextAnswerInterface by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-08 14:55:17.100

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newTextAnswerInterface/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newTextAnswerInterface/source_code.jsx
@@ -1,30 +1,8 @@
-let endDate = props.endDate ?? 6516351861321;
-let userMakingQuestion = props.userMakingQuestion ?? DaniNoMeMates;
-
-let profile = Social.getr(`${accountId}/profile`);
-
 return (
-  <div className="d-flex flex-column">
-    <div className="d-flex no-wrap">
-      {/* The next widget need the info of the user making the question */}
-      <Widget
-        src="mob.near/widget/Profile"
-        props={{ userMakingQuestion, profile }}
-      />
-
-      <div className="d-flex">
-        <span className="mx-2">End date: {endDate} </span>
-
-        <span
-          style={{ backgroundColor: endDate > Date.now() ? "blue" : "red" }}
-        >
-          {endDate > Date.now() ? "Active" : "Closed"}
-        </span>
-      </div>
-    </div>
-
+  <>
     <div>
       <h3>Title</h3>
+
       <p>Description</p>
     </div>
 
@@ -32,5 +10,5 @@ return (
       Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer
       Answer Answer Answer Answer Answer Answer Answer Answer Answer
     </p>
-  </div>
+  </>
 );
