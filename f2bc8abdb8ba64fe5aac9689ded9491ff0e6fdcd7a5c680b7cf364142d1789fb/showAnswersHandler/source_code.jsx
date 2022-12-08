97ffc21ace7a7a850a7fb6e0991ca2ac09db5852
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-08 14:54:17 -0500

    Update showAnswersHandler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-08 14:54:17.912

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showAnswersHandler/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showAnswersHandler/source_code.jsx
@@ -1,32 +1,65 @@
 const renderAnswers = () => {
   answers.map((answer) => {
     if (answer.typeOfanswer == "0") {
-      <Widget
-        src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/newTextAnswerInterface"
-        props={
-          {
-            /* setProperties */
+      <>
+        <Widget
+          src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/answersHeader"
+          props={
+            {
+              /* setProperties */
+            }
           }
-        }
-      />;
+        />
+        ;
+        <Widget
+          src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/newTextAnswerInterface"
+          props={
+            {
+              /* setProperties */
+            }
+          }
+        />
+        ;
+      </>;
     } else if (answer.typeOfanswer == "1") {
-      <Widget
-        src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/newYesNoAnswerInterface"
-        props={
-          {
-            /* setProperties */
+      <>
+        <Widget
+          src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/answersHeader"
+          props={
+            {
+              /* setProperties */
+            }
           }
-        }
-      />;
+        />
+        <Widget
+          src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/newYesNoAnswerInterface"
+          props={
+            {
+              /* setProperties */
+            }
+          }
+        />
+      </>;
     } else if (answer.typeOfanswer == "2") {
-      <Widget
-        src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/newMiniMultipleChoiceInterface"
-        props={
-          {
-            /* setProperties */
+      <>
+        <Widget
+          src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/answersHeader"
+          props={
+            {
+              /* setProperties */
+            }
           }
-        }
-      />;
+        />
+        <Widget
+          src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/newMiniMultipleChoiceInterface"
+          props={
+            {
+              /* setProperties */
+            }
+          }
+        />
+        ;
+      </>;
     }
   });
 };
