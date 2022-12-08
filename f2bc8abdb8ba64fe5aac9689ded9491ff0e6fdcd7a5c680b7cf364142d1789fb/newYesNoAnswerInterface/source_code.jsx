f9bc5576f144ef8e90bab31a3caa71dc2babbedd
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-08 17:50:46 -0500

    Update newYesNoAnswerInterface by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-08 17:50:46.713

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newYesNoAnswerInterface/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newYesNoAnswerInterface/source_code.jsx
@@ -1,3 +1,27 @@
+let questionBlockHeight = props.questionBlockHeight;
+let userMakingQuestion = props.accountId;
+
+//TODO you have to use Social.index with the questionBlockHeight call to check all the answers and then reduce them to count positives and negatives votes. You should get someting like the next array
+let countVotes = [2, 1];
+
+const timeAgo = (diffSec) =>
+  diffSec < 60000
+    ? `${(diffSec / 1000) | 0} seconds ago`
+    : diffSec < 3600000
+    ? `${(diffSec / 60000) | 0} minutes ago`
+    : diffSec < 86400000
+    ? `${(diffSec / 3600000) | 0} hours ago`
+    : `${(diffSec / 86400000) | 0} days ago`;
+
+const profileLink = (c) => (
+  <a
+    className="text-decoration-none link-dark"
+    href={`#/mob.near/widget/ProfilePage?accountId=${userMakingQuestion}`}
+  >
+    {c}
+  </a>
+);
+
 return (
   <div className="d-flex align-items-start">
     <i className="bi bi-check-circle-fill" style={{ padding: "0 0.3rem" }}></i>
