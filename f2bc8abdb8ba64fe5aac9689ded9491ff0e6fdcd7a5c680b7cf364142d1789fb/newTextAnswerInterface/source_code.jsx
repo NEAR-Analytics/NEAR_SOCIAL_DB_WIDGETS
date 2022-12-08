870272da2fffbe0c722b8a25a4e14558193e3120
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-08 18:06:36 -0500

    Update newTextAnswerInterface by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-08 18:06:36.003

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newTextAnswerInterface/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newTextAnswerInterface/source_code.jsx
@@ -1,5 +1,3 @@
-let questionTitle = props.question ?? "Title";
-let description = props.description ?? "Description";
 let questionTimestamp = props.questionTimestamp;
 let questionBlockHeight = props.questionBlockHeight ?? 79932883;
 
@@ -35,58 +33,49 @@ const profileLink = (c) => (
 
 return (
   <>
-    <div>
-      <h3>{questionTitle}</h3>
-
-      <p>{description}</p>
-      {answers.map((answer) => {
-        let profile = Social.getr(`${answer.accountId}/profile`);
-
-        return (
-          <div>
-            <div
-              className="d-flex align-items-start"
-              style={{
-                padding: "1.5rem 0",
-                borderBottom: "1px solid #e9e9e9",
-              }}
-            >
-              <div>
+    {answers.map((answer) => {
+      let profile = Social.getr(`${answer.accountId}/profile`);
+      return (
+        <div>
+          <div
+            className="d-flex align-items-start"
+            style={{
+              padding: "1.5rem 0",
+              borderBottom: "1px solid #e9e9e9",
+            }}
+          >
+            <div>
+              {profileLink(
+                <Widget
+                  src="mob.near/widget/ProfileImage"
+                  props={{ accountId: answer.accountId }}
+                />
+              )}
+            </div>
+            <div className="d-flex">
+              <div className="flex-grow-1 me-1 text-truncate">
                 {profileLink(
-                  <Widget
-                    src="mob.near/widget/ProfileImage"
-                    props={{ accountId: answer.accountId }}
-                  />
+                  <>
+                    <span className="fw-bold">{profile.name}</span>
+                    <span className="text-secondary">@{answer.accountId}</span>
+                  </>
                 )}
               </div>
-
-              <div className="d-flex">
-                <div className="flex-grow-1 me-1 text-truncate">
-                  {profileLink(
-                    <>
-                      <span className="fw-bold">{profile.name}</span>
-                      <span className="text-secondary">
-                        @{answer.accountId}
-                      </span>
-                    </>
-                  )}
-                </div>
-                <div>
-                  <small className="ps-1 text-nowrap text-muted ms-auto">
-                    <i className="bi bi-clock me-1"></i>
-                    {timeAgo(Date.now() - questionTimestamp)}
-                  </small>
-                </div>
+              <div>
+                <small className="ps-1 text-nowrap text-muted ms-auto">
+                  <i className="bi bi-clock me-1"></i>
+                  {timeAgo(Date.now() - questionTimestamp)}
+                </small>
               </div>
             </div>
-            <textarea
-              className="w-100"
-              value={answer.value.user_answer}
-              readonly
-            />
           </div>
-        );
-      })}
-    </div>
+          <textarea
+            className="w-100"
+            value={answer.value.user_answer}
+            readonly
+          />
+        </div>
+      );
+    })}
   </>
 );
