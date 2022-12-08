4acf0fadad7e89a5bf0fbe337f569e99935a212f
tonebonekone <tonebonekone>
2022-12-07 19:16:52 -0500

    Update HelloAccountID by djwine_official.near at 2022-12-07 19:16:52.454

--- djwine_official.near/HelloAccountID/source_code.jsx
+++ djwine_official.near/HelloAccountID/source_code.jsx
@@ -1,3 +1,8 @@
 const accountId = context.accountId;
 
+if (context.loading) return "Loading";
+if (!accountId) return "Please sign in with NEAR wallet to use this widget";
+
+console.log();
+
 return `Hello, ${accountId}!`;
