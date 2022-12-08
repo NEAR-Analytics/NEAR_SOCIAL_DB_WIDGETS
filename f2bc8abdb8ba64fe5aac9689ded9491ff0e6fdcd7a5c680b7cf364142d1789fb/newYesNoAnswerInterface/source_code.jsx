f7fca860e455bb0a6c3cd4e9f9d2afa5f403cfcc
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-08 14:53:18 -0500

    Update newYesNoAnswerInterface by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-08 14:53:18.110

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newYesNoAnswerInterface/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newYesNoAnswerInterface/source_code.jsx
@@ -1,16 +1,11 @@
 return (
-  <div>
-    <div className="d-flex align-items-start">
-      <i
-        className="bi bi-check-circle-fill"
-        style={{ padding: "0 0.3rem" }}
-      ></i>
-      <p className="text-secondary">{countVotes[0]}</p>
-      <i
-        className="bi bi-x-octagon-fill"
-        style={{ padding: "0 0.5rem 0 1rem" }}
-      ></i>
-      <p className="text-secondary">{countVotes[1]}</p>
-    </div>
+  <div className="d-flex align-items-start">
+    <i className="bi bi-check-circle-fill" style={{ padding: "0 0.3rem" }}></i>
+    <p className="text-secondary">{countVotes[0]}</p>
+    <i
+      className="bi bi-x-octagon-fill"
+      style={{ padding: "0 0.5rem 0 1rem" }}
+    ></i>
+    <p className="text-secondary">{countVotes[1]}</p>
   </div>
 );
