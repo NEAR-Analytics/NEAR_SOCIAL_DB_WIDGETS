2df1b70a1736d4859991defde7489cc9347b772f
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-08 14:19:17 -0500

    Update showAnswersHandler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-08 14:19:17.294

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showAnswersHandler/source_code.jsx
@@ -0,0 +1,51 @@
+const renderAnswers = () => {
+  answers.map((answer) => {
+    if (answer.typeOfanswer == "0") {
+      <Widget
+        src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/newTextAnswerInterface"
+        props={
+          {
+            /* setProperties */
+          }
+        }
+      />;
+    } else if (answer.typeOfanswer == "1") {
+      <Widget
+        src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/newYesNoAnswerInterface"
+        props={
+          {
+            /* setProperties */
+          }
+        }
+      />;
+    } else if (answer.typeOfanswer == "2") {
+      <Widget
+        src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/newMiniMultipleChoiceInterface"
+        props={
+          {
+            /* setProperties */
+          }
+        }
+      />;
+    }
+  });
+};
+
+return (
+  <div className="d-flex flex-column">
+    <div className="d-flex">
+      {/* Filters */}
+      <button type="button" class="btn btn-outline-primary">
+        ALL STATUS
+      </button>
+      <button type="button" class="btn btn-outline-primary">
+        ALL CATEGORY
+      </button>
+      <button type="button" class="btn btn-outline-primary">
+        CREATED BY ME
+      </button>
+    </div>
+
+    {renderAnswers()}
+  </div>
+);
