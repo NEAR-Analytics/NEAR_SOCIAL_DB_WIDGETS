46fce7dca040c61b2dbbb40aebe2594071da06ef
devgovgigs.near <devgovgigs.near>
2022-12-07 23:20:43 -0500

    Update Submission by devgovgigs.near at 2022-12-07 23:20:43.289

--- devgovgigs.near/Submission/source_code.jsx
+++ devgovgigs.near/Submission/source_code.jsx
@@ -1,10 +1,18 @@
 const submission = props.submission;
 
+function readableDate(UNIX_timestamp) {
+  var a = new Date(parseInt(UNIX_timestamp) / 1000000);
+  return a.toDateString() + " " + a.toLocaleTimeString();
+}
+
 return (
   <div>
-    <h3>{submission.name}</h3>
-    <p>Author: {submission.account_id}</p>
-    <p>Timestamp: {submission.timestamp} </p>
+    <h3>Submission: {submission.name}</h3>
+    <Widget
+      src={`mob.near/widget/ProfileLine`}
+      props={{ accountId: submission.account_id }}
+    />
+    <p>Timestamp: {readableDate(submission.timestamp)} </p>
     <p>{submission.description}</p>
   </div>
 );
