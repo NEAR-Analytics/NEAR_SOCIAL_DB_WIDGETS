cf3d84e6583ccc2b2e8e9bf58f70728e8ede920c
devgovgigs.near <devgovgigs.near>
2022-12-08 19:42:15 -0500

    Update Sponsorship by devgovgigs.near at 2022-12-08 19:42:15.536

--- devgovgigs.near/Sponsorship/source_code.jsx
+++ devgovgigs.near/Sponsorship/source_code.jsx
@@ -10,30 +10,37 @@ const timestamp = readableDate(
 );
 
 return (
-  <div className="row">
-    <div className="mb-2 card">
-      <div className="card-body">
-        <div>
-          <Widget
-            src={`mob.near/widget/ProfileLine`}
-            props={{ accountId: sponsorship.author_id }}
-          />
+  <div className="mb-2 card">
+    <div className="card-header">
+      <small class="text-muted">
+        <div class="row justify-content-between">
+          <div class="col-4">
+            {" "}
+            <Widget
+              src={`mob.near/widget/ProfileLine`}
+              props={{ accountId: sponsorship.author_id }}
+            />
+          </div>
+          <div class="col-4">
+            {" "}
+            <div class="d-flex justify-content-end">{timestamp}</div>
+          </div>
         </div>
-        <div>{timestamp}</div>
-        <hr />
-        <h4>Sponsorship: {sponsorship.name}</h4>
-        <div>
-          Supervisor:
-          <Widget
-            src={`mob.near/widget/ProfileLine`}
-            props={{ accountId: sponsorship.author_id }}
-          />
-        </div>
-        <div>
-          Maximum amount: {sponsorship.amount} {sponsorship.sponsorship_token}
-        </div>
-        <p>{sponsorship.description}</p>
-      </div>
+      </small>
+    </div>
+    <div className="card-body">
+      <h5 class="card-title">Sponsorship: {sponsorship.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Maximum amount: {sponsorship.amount} {sponsorship.sponsorship_token}
+      </h6>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Supervisor:{" "}
+        <Widget
+          src={`mob.near/widget/ProfileLine`}
+          props={{ accountId: sponsorship.supervisor }}
+        />
+      </h6>
+      <p class="card-text">{sponsorship.description}</p>
     </div>
   </div>
 );
