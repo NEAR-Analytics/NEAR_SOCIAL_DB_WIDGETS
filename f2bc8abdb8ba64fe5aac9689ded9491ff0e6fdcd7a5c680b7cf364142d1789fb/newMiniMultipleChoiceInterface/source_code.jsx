573bb2a1e367828d95dbac096c25ae15c56e91a8
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-09 02:41:27 -0500

    Update newMiniMultipleChoiceInterface by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-09 02:41:27.792

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newMiniMultipleChoiceInterface/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newMiniMultipleChoiceInterface/source_code.jsx
@@ -1,18 +1,48 @@
-let questionBlockHeight = props.questionBlockHeight ?? 79932918;
-let choicesOptions = props.choicesOptions ?? ["a", "b", "c"];
-
-//You need to do a Social.index call to get all the answers to this question, then count the amounts of votes for each option and the totals
-let countVotes = [20, 3, 4];
-let amountOfVotes = "30";
+let question = props.question ?? {
+  title: "Multiple choice test",
+  tgLink: "",
+  accountId: "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
+  choicesOptions: ["a", "b", "c"],
+  question: "Testing multiple choice",
+  description: "",
+  questionBlockHeight: 79932918,
+  startDate: Date.now(),
+  endDate: Date.now() + 10000000,
+  storingTimestamp: Date.now(),
+  questionType: "1",
+  answers: [
+    {
+      accountId:
+        "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
+      answer: "0",
+      timeStamp: Date.now(),
+    },
+  ],
+};
 
 function calculatePercentage(votesToThisOption) {
-  return (votesToThisOption / amountOfVotes) * 100;
+  return (votesToThisOption / question.answers.length) * 100;
+}
+
+let countVotes = [];
+for (let i = 0; i < question.choicesOptions.length; i++) {
+  countVotes.push(0);
+}
+
+for (let i = 0; i < question.answers.length; i++) {
+  countVotes[Number(question.answers[i].answer)] += 1;
+}
+
+function displayableOptionName(option) {
+  if (option.length > 12) {
+    return option.slice(0, 12) + "...";
+  }
+  return option;
 }
 
 const renderOption = (option, index) => {
   return (
     <div>
-      <h4>{option}</h4>
       <div className="d-flex">
         <div
           style={{ color: "#000", backgroundColor: "#f1f1f1", width: "90%" }}
@@ -20,6 +50,7 @@ const renderOption = (option, index) => {
           {/* Set the width of the next div to make the bar grow. At the same, use the same value to fill the span tag */}
           <div
             style={{
+              margin: "0.3rem 0px",
               content: "",
               display: "table",
               clear: "both",
@@ -28,13 +59,21 @@ const renderOption = (option, index) => {
               width: `${calculatePercentage(countVotes[index])}%`,
               textAlign: "center",
               backgroundColor: "lightgray",
+              overflow: "visible",
+              whiteSpace: "nowrap",
+              textAlign: "left",
             }}
           >
-            <span>{calculatePercentage(countVotes[index])}</span>
+            <span style={{ overflow: "visible" }}>
+              {displayableOptionName(option)}
+              <span className="text-secondary" style={{ marginLeft: "1rem" }}>
+                ({question.answers.length} votes)
+              </span>
+            </span>
           </div>
         </div>
-        <span style={{ minWidth: "max-content" }}>
-          Of {amountOfVotes} votes
+        <span style={{ minWidth: "max-content", marginLeft: "0.3rem" }}>
+          {calculatePercentage(countVotes[index])}%
         </span>
       </div>
     </div>
@@ -43,7 +82,7 @@ const renderOption = (option, index) => {
 
 return (
   <div className="m-2">
-    {choicesOptions.map((option, index) => {
+    {question.choicesOptions.map((option, index) => {
       return renderOption(option, index);
     })}
   </div>
