ef400a1d17f7dfd4cc3219d80b278c01c00e2e00
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-09 23:17:29 -0500

    Update answersHeader by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-09 23:17:29.738

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answersHeader/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answersHeader/source_code.jsx
@@ -1,38 +1,55 @@
-let title = props.questionTitle ?? "Mock title";
-let description = props.description ?? "Mock description";
-let startDate = props.startDate ?? Date.now() - Date.now() / 1000;
-let endDate = props.endDate ?? Date.now() + Date.now() / 1000;
-let userMakingQuestion = props.userMakingQuestion ?? DaniNoMeMates;
+let question = props.question ?? {
+  title: "Multiple choice test",
+  tgLink: "",
+  accountId: "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
+  choicesOptions: ["a", "b", "c"],
+  question: "Testing multiple choice",
+  description: "",
+  questionBlockHeight: 79932918,
+  startDate: Date.now(),
+  endDate: Date.now() + 10000000,
+  storingTimestamp: Date.now(),
+  questionType: "1",
+  answers: [
+    {
+      accountId:
+        "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
+      answer: "0",
+      timeStamp: Date.now(),
+    },
+  ],
+};
 
 let profile = Social.getr(`${accountId}/profile`);
 
-function dateToTimestamp(date) {
-  //TODO
-  return date;
-}
-
 return (
   <div>
-    <div className="d-flex no-wrap">
+    <div className="d-flex no-wrap justify-content-between">
       <Widget
-        src="mob.near/widget/Profile"
-        props={{ userMakingQuestion, profile }}
+        src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/Profile"
+        props={{ userMakingQuestion: question.accountId, profile }}
       />
 
       <div className="d-flex">
-        <span className="mx-2">End date: {endDate} </span>
+        <span className="mx-2" style={{ fontWeight: "500" }}>
+          End date: {question.endDate}
+        </span>
 
         <span
           style={{
             backgroundColor:
-              dateToTimestamp(startDate) < Date.now() &&
-              dateToTimestamp(endDate) > Date.now()
-                ? "blue"
-                : "red",
+              question.startDate < Date.now() && question.endDate > Date.now()
+                ? "rgb(153, 255, 153)"
+                : "rgb(255, 128, 128)",
+
+            height: "max-content",
+            width: "6rem",
+            border: "1px solid rgb(0, 82, 204)",
+            textAlign: "center",
+            borderRadius: "80px",
           }}
         >
-          {dateToTimestamp(startDate) < Date.now() &&
-          dateToTimestamp(endDate) > Date.now()
+          {question.startDate < Date.now() && question.endDate > Date.now()
             ? "Active"
             : "Closed"}
         </span>
