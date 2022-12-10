95ca8e2b406e92224be4a1f08e6e11f514ccf7a5
tonebonekone <tonebonekone>
2022-12-09 21:40:02 -0500

    Update Table by djwine_official.near at 2022-12-09 21:40:02.450

--- djwine_official.near/Table/source_code.jsx
+++ djwine_official.near/Table/source_code.jsx
@@ -12,19 +12,27 @@ if (!keyLabel) return "Must have keyLabel";
 if (!value) return "Must have value";
 if (!valueLabel) return "Must have value";
 
+const keyStyle = {
+  overflow: "hidden",
+  whiteSpace: "nowrap",
+  maxWidth: "200px",
+};
+
 return (
   <>
     <h2 class="mb-4">{title}</h2>
     <div className="table-responsive">
       <table className="table">
         <thead>
-          <th>{keyLabel}</th>
+          <th style={keyStyle}>{keyLabel}</th>
           <th>{valueLabel}</th>
         </thead>
         <tbody>
           {data.map((i) => (
             <tr className="align-middle">
-              <th scope="row">{i[key]}</th>
+              <th style={keyStyle} scope="row">
+                {i[key]}
+              </th>
               <td>{i[value]}</td>
             </tr>
           ))}
