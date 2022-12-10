7b0728671d07445af0795a2984a1f2b29dd9c8ca
Joe-rlo <Joe-rlo>
2022-12-09 19:04:36 -0500

    Update ShardDog-Create A Free Wallet by orangejoe.near at 2022-12-09 19:04:36.317

---
+++ orangejoe.near/ShardDog-Create A Free Wallet/source_code.jsx
@@ -0,0 +1,10 @@
+return (
+  <>
+    <img src="https://readylayerone.s3.amazonaws.com/SharddogUp_sm.png" />
+    <br />
+    <br />
+    <a href="https://shard.dog/go?url=https://near.social">
+      <button>Create A New Wallet For Free</button>
+    </a>
+  </>
+);
