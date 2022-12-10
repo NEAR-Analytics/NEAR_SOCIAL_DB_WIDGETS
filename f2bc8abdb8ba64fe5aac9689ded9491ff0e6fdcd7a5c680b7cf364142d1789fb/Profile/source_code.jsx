a7497b72e7dbdeb4bf12be706e0272a529d6f7ff
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-09 23:21:59 -0500

    Update Profile by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-09 23:21:59.372

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Profile/source_code.jsx
@@ -0,0 +1,44 @@
+const accountId = props.accountId ?? context.accountId;
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+const name = profile.name;
+const image = profile.image;
+
+function makeAccountIdShorter(accountId) {
+  if (accountId.length > 12) {
+    return accountId.slice(0, 12) + "...";
+  }
+  return accountId;
+}
+
+return (
+  <div className="profile d-inline-block">
+    <a
+      href={`#/mob.near/widget/ProfilePage?accountId=${accountId}`}
+      className="text-decoration-none link-dark"
+    >
+      <Widget
+        src="mob.near/widget/ProfileImage"
+        props={{
+          profile,
+          accountId,
+          className: "float-start d-inline-block me-2",
+        }}
+      />
+      <div className="profile-info d-inline-block" style={{ maxWidth: "16em" }}>
+        <div
+          className="profile-name text-truncate"
+          style={{ fontWeight: "500" }}
+        >
+          {name || "No-name profile"}
+        </div>
+        <div className="profile-links d-flex">
+          <div className="d-inline-block profile-account text-secondary text-truncate">
+            @{makeAccountIdShorter(accountId)}
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+);
