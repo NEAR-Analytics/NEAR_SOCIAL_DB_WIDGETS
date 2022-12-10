cbad57377e46d189930bc081bad6e29cedc6f6c4
tonebonekone <tonebonekone>
2022-12-09 21:36:25 -0500

    Update Leaderboard by djwine_official.near at 2022-12-09 21:36:25.365

--- djwine_official.near/Leaderboard/source_code.jsx
+++ djwine_official.near/Leaderboard/source_code.jsx
@@ -1,57 +1,89 @@
-console.log("context", context);
-
-const accountId = context.accountId;
 const loading = context.loading;
 
 if (loading) return "Loading";
-if (!accountId) return "Please sign in with NEAR wallet to use this widget";
-
-let profile = Social.getr(`${accountId}/profile`);
-// console.log("profile", profile);
 
 let graph = Social.index("graph", "follow");
-// console.log("graph");
+console.log("graph", graph);
 // graph.forEach((i) => console.log(i));
 
-let follows = graph.map((g) => [g.accountId, g.value.accountId, g.blockHeight]);
+let follows = graph.map((g) => ({
+  follower: g.accountId,
+  followed: g.value.accountId,
+  blockHeight: g.blockHeight,
+}));
 
-let followers = graph
-  .map((g) => g.accountId)
+console.log("follows", follows);
+
+let followed = follows
   .reduce((acc, i) => {
-    let result = acc.findIndex((el) => el.accountId === i);
+    let result = acc.findIndex((el) => el.followed === i.followed);
     if (result === -1) {
-      acc.push({ accountId: i, follows: 1 });
+      acc.push({ followed: i.followed, count: 1 });
     } else {
       let el = {
-        accountId: i,
-        follows: acc[result].follows + 1,
+        followed: i.followed,
+        count: acc[result].count + 1,
       };
       acc[result] = el;
     }
-
     return acc;
   }, [])
-  .sort((a, b) => b.follows - a.follows);
+  .sort((a, b) => b.count - a.count);
+
+console.log("followed", followed);
+
+let followers = follows
+  .reduce((acc, i) => {
+    let result = acc.findIndex((el) => el.follower === i.follower);
+    if (result === -1) {
+      acc.push({ follower: i.follower, count: 1 });
+    } else {
+      let el = {
+        follower: i.follower,
+        count: acc[result].count + 1,
+      };
+      acc[result] = el;
+    }
+    return acc;
+  }, [])
+  .sort((a, b) => b.count - a.count);
+
+console.log("followers", followers);
 
 return (
   <>
-    <h1>Leaderboard</h1>
-    <div className="table-responsive">
-      <table className="table table-striped">
-        <thead>
-          <th>Account</th>
-          <th>Other Accounts Followed</th>
-        </thead>
+    <h1 class="mb-4">Leaderboard</h1>
 
-        <tbody>
-          {followers.map((pool) => (
-            <tr className="align-middle">
-              <th scope="row">{pool.accountId}</th>
-              <td>{pool.follows}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div class="container">
+      <div class="row">
+        <div class="col mr-2">
+          <Widget
+            src="djwine_official.near/widget/Table"
+            props={{
+              title: "Followed",
+              data: followed,
+              key: "followed",
+              keyLabel: "Account",
+              value: "count",
+              valueLabel: "Followed by",
+            }}
+          />
+        </div>
+        <div class="col">
+          <Widget
+            src="djwine_official.near/widget/Table"
+            props={{
+              title: "Followers",
+              data: followers,
+              key: "follower",
+              keyLabel: "Account",
+              value: "count",
+              valueLabel: "Following",
+            }}
+          />
+        </div>
+        <div class="col ">One of three columns</div>
+      </div>
     </div>
   </>
 );
