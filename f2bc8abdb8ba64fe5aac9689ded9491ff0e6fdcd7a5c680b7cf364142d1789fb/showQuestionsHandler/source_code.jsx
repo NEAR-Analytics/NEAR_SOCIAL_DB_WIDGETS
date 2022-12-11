ef3c2fe63789ebc8bb5b83d3002dd6e2efbcf033
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-11 02:50:50 -0500

    Update showQuestionsHandler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-11 02:50:50.520

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showQuestionsHandler/source_code.jsx
@@ -0,0 +1,63 @@
+function addAnswersToQuestion(questions, answers) {
+  questions = questions.map((q) => {
+    q.value.answers = [];
+    return q;
+  });
+  for (let i = 0; i < answers.length; i++) {
+    const answer = answers[i];
+    const qIndex = questions.findIndex(
+      (q) => q.blockHeight == answer.value.questionBlockHeight
+    );
+    if (qIndex != -1) questions[qIndex].answers.push(answer);
+    if (qIndex == -1) console.log(`Answer has no related question!`, answer);
+  }
+  return questions;
+}
+
+const displayAnswerWidgetNames = [
+  "newTextAnswerInterface",
+  "newMiniMultipleChoiceInterface",
+];
+
+let questions = Social.index("poll_question", "question-v3.0.1");
+const answers = Social.index("poll_question", "answer-v3.0.1");
+questions = addAnswersToQuestion(questions, answers);
+
+const renderQuestions = () => {
+  return questions.map((question) => {
+    console.log(
+      "W1",
+      `${props.accountId}/widget/${
+        displayAnswerWidgetNames[question.value.questionType]
+      }`
+    );
+    return (
+      <div
+        className="my-5 py-3 px-4"
+        style={{ backgroundColor: "#f2f2f2", borderRadius: "1rem" }}
+      >
+        <Widget
+          src={`${props.accountId}/widget/answersHeader`}
+          props={{ ...question }}
+        />
+        <Widget
+          src={`${props.accountId}/widget/${
+            displayAnswerWidgetNames[question.value.questionType]
+          }`}
+          props={{ ...question }}
+        />
+      </div>
+    );
+  });
+};
+
+return (
+  <div
+    style={{
+      borderRadius: "3px",
+      padding: "8% 5% 1% 5%",
+    }}
+  >
+    {renderQuestions()}
+  </div>
+);
