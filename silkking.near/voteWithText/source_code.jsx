e0fca9aeaa621c4c7f589b140215e570648c2050
silkking.near <silkking.near>
2022-12-11 14:52:21 -0500

    Update voteWithText by silkking.near at 2022-12-11 14:52:21.254

---
+++ silkking.near/voteWithText/source_code.jsx
@@ -0,0 +1,25 @@
+let haveVoted = props.haveVoted;
+
+State.init({ vote: "" });
+
+return (
+  <div>
+    {haveVoted ? (
+      <p
+        className="text-primary"
+        style={{ textAlign: "center", fontWeight: "500" }}
+      >
+        You have already voted
+      </p>
+    ) : (
+      <div>
+        <textarea
+          value={state.vote}
+          onChange={(e) => State.update({ vote: e.target.value })}
+          style={{ width: "100%" }}
+        />
+        {/*TODO replace with commit button*/}
+      </div>
+    )}
+  </div>
+);
