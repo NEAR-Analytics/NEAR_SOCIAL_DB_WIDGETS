9519dfa91b8ffae7387e06862fd0775fcc84ff90
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-11 02:50:38 -0500

    Update answersHeader by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-11 02:50:38.065

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answersHeader/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answersHeader/source_code.jsx
@@ -1,44 +1,39 @@
-let question = props.question ?? {
-  title: "Multiple choice test",
-  tgLink: "",
-  accountId: "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
-  choicesOptions: ["a", "b", "c"],
-  question: "Testing multiple choice",
-  description: "This is a simple test",
-  questionBlockHeight: 79932918,
-  startDate: Date.now(),
-  endDate: Date.now() + 10000000,
-  storingTimestamp: Date.now(),
-  questionType: "1",
-  answers: [
-    {
-      accountId:
-        "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
-      answer: "0",
-      timeStamp: Date.now(),
-    },
-  ],
+let questionParams = props.value ?? {
+  accountId: "mock.near",
+  blockHeight: 80293871,
+  value: {
+    isDraft: false,
+    title: "Mock title",
+    description: "Mock Description",
+    startTimestamp: 1670628600000,
+    endTimestamp: 1671580800000,
+    questionType: "0",
+    question: "Mock question?",
+    choicesOptions: [],
+    timestamp: 1670628584974,
+  },
 };
 
 let profile = Social.getr(`${accountId}/profile`);
 
 return (
-  <div className="my-2 mx-3">
+  <div className="my-2">
     <div className="d-flex no-wrap justify-content-between">
       <Widget
         src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/Profile"
-        props={{ userMakingQuestion: question.accountId, profile }}
+        props={{ userMakingQuestion: questionParams.accountId, profile }}
       />
 
       <div className="d-flex">
         <span className="mx-2" style={{ fontWeight: "500" }}>
-          End date: {question.endDate}
+          End date: {questionParams.endDate}
         </span>
 
         <span
           style={{
             backgroundColor:
-              question.startDate < Date.now() && question.endDate > Date.now()
+              questionParams.startDate < Date.now() &&
+              questionParams.endDate > Date.now()
                 ? "rgb(153, 255, 153)"
                 : "rgb(255, 128, 128)",
 
@@ -49,13 +44,15 @@ return (
             borderRadius: "80px",
           }}
         >
-          {question.startDate < Date.now() && question.endDate > Date.now()
+          {questionParams.startDate < Date.now() &&
+          questionParams.endDate > Date.now()
             ? "Active"
             : "Closed"}
         </span>
       </div>
     </div>
-    <h5 className="mt-3">{question.title}</h5>
-    <p>{question.description}</p>
+    <h5 className="mt-3">{questionParams.title}</h5>
+    <p>{questionParams.description}</p>
+    <p>{questionParams.question}</p>
   </div>
 );
