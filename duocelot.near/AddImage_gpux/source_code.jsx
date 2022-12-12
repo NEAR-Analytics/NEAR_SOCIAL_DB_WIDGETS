62106a25e1c1e71b4f04ab1d5142d0b67db7e982
duocelot.near <duocelot.near>
2022-12-12 04:29:13 -0500

    Update AddImage_gpux by duocelot.near at 2022-12-12 04:29:13.290

--- duocelot.near/AddImage_gpux/source_code.jsx
+++ duocelot.near/AddImage_gpux/source_code.jsx
@@ -75,7 +75,7 @@ return (
             backgroundColor: "black",
             color: "white",
             fontFamily: '"Press Start 2P", sans-serif',
-            margin: "10px, 0px, 10px, 0px",
+            margin: "10px, 40px, 10px, 40px",
           }}
           onChange={(e) => {
             state.prompt = e.target.value;
