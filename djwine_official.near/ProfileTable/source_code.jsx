196127eba9172131b72262b18c0fd944489a6e7d
tonebonekone <tonebonekone>
2022-12-12 16:37:51 -0500

    Update ProfileTable by djwine_official.near at 2022-12-12 16:37:51.198

---
+++ djwine_official.near/ProfileTable/source_code.jsx
@@ -0,0 +1,49 @@
+const title = props.title;
+const data = props.data;
+const key = props.key;
+const value = props.value;
+const link = props.link;
+
+if (!title) return "Must have title";
+if (!data) return "Must have data";
+if (!key) return "Must have key";
+if (!value) return "Must have value";
+
+const style = {
+  overflow: "hidden",
+  whiteSpace: "nowrap",
+  maxWidth: "300px",
+};
+
+// {key.url ? key.url + i[key.id] : i[key.id]}
+// <td>{value.url ? value.url + i[value.id] : i[value.id]}</td>
+
+return (
+  <>
+    <h2 class="mb-4">{title}</h2>
+    <div className="table-responsive">
+      <table className="table">
+        <thead>
+          <th style={style}>{key.label}</th>
+          <th>{value.label}</th>
+        </thead>
+        <tbody>
+          {data.map((i) => (
+            <tr className="align-middle">
+              <td style={{ maxWidth: "300px" }}>
+                <Widget
+                  src="mob.near/widget/ProfileLine"
+                  props={{
+                    accountId: i[key.id],
+                    tooltip: true,
+                  }}
+                />
+              </td>
+              <td>{i[value.id]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </>
+);
