1e15c2bd80dba53cb6a893c60f3345723880779f
silkking.near <silkking.near>
2022-12-13 15:39:13 -0500

    Update answersHeader by silkking.near at 2022-12-13 15:39:13.153

--- silkking.near/answersHeader/source_code.jsx
+++ silkking.near/answersHeader/source_code.jsx
@@ -1,31 +1,40 @@
-// let questionParams = props.value;
-let blockHeight = Number(props.blockHeight);
-let questions = Social.index("poll_question", "question-v3.0.1");
-let questionParams = questions.find((q) => q.blockHeight == blockHeight);
-console.log("Q", questionParams);
+let questionParams = props.value ?? {
+  accountId: "mock.near",
+  blockHeight: 80293871,
+  value: {
+    isDraft: false,
+    title: "Mock title",
+    description: "Mock Description",
+    startTimestamp: 1670628600000,
+    endTimestamp: 1671580800000,
+    questionType: "0",
+    question: "Mock question?",
+    choicesOptions: [],
+    timestamp: 1670628584974,
+  },
+};
 
-let profile = Social.getr(`${questionParams.accountId}/profile`);
+let profile = Social.getr(`${props.accountId}/profile`);
 
 return (
   <div className="my-2">
     <div className="d-flex no-wrap justify-content-between">
       <Widget
-        src={`mob.near/widget/Profile`}
-        props={{ accountId: questionParams.accountId }}
+        src={`f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/Profile`}
+        props={{ userMakingQuestion: props.accountId, profile }}
       />
 
       <div className="d-flex">
         <span className="mx-2" style={{ fontWeight: "500" }}>
-          End date:{" "}
-          {new Date(questionParams.value.endTimestamp).toLocaleDateString()}
+          End date: {new Date(questionParams.endTimestamp).toLocaleDateString()}
         </span>
         <span
           style={{
             backgroundColor:
               questionParams.startDate < Date.now() &&
               questionParams.endTimestamp > Date.now()
-                ? "rgb(255, 128, 128)"
-                : "rgb(153, 255, 153)",
+                ? "rgb(153, 255, 153)"
+                : "rgb(255, 128, 128)",
 
             height: "max-content",
             width: "6rem",
@@ -34,15 +43,15 @@ return (
             borderRadius: "80px",
           }}
         >
-          {questionParams.value.startDate < Date.now() &&
-          questionParams.value.endTimestamp > Date.now()
-            ? "Closed"
-            : "Active"}
+          {questionParams.startDate < Date.now() &&
+          questionParams.endTimestamp > Date.now()
+            ? "Active"
+            : "Closed"}
         </span>
       </div>
     </div>
-    <h5 className="mt-3">{questionParams.value.title}</h5>
-    <p>{questionParams.value.description}</p>
-    <p>{questionParams.value.question}</p>
+    <h5 className="mt-3">{questionParams.title}</h5>
+    <p>{questionParams.description}</p>
+    <p>{questionParams.question}</p>
   </div>
 );
