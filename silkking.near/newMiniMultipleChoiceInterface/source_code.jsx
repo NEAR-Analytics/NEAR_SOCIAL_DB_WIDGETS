163d23f8fcaf7f9c0c0ce794421dbd1d40bfdf32
silkking.near <silkking.near>
2022-12-13 15:11:50 -0500

    Update newMiniMultipleChoiceInterface by silkking.near at 2022-12-13 15:11:50.361

--- silkking.near/newMiniMultipleChoiceInterface/source_code.jsx
+++ silkking.near/newMiniMultipleChoiceInterface/source_code.jsx
@@ -1,22 +1,51 @@
-let blockHeight = props.blockHeight;
-// let question = Social.index("poll_question", "question-v3.0.1");
-let question = props.value;
+if (!props.blockHeight) {
+  return "Property blockHeight not set";
+}
+
+const questionBlockHeight = props.blockHeight;
+const questions = Social.index("poll_question", "question-v3.0.1");
+const questionParams = questions.find(
+  (q) => q.blockHeight == questionBlockHeight
+);
+State.init({ vote: "", showErrorsInForm: false });
+
+const answers = Social.index("poll_question", "answer-v3.0.1");
+const answersToThisQuestion = answers.filter(
+  (a) => a.value.questionBlockHeight == questionBlockHeight
+);
+let usersWithAnswersToThisQuestion = [];
+const validAnswersToThisQuestion = answersToThisQuestion.filter((a) => {
+  const didUserAlreadyAnswered = usersWithAnswersToThisQuestion.includes(
+    a.accountId
+  );
+  if (!didUserAlreadyAnswered) {
+    usersWithAnswersToThisQuestion.push(a.accountId);
+  }
+  return !didUserAlreadyAnswered;
+});
+
+console.log(1, validAnswersToThisQuestion);
 
 function calculatePercentage(votesToThisOption) {
-  if (question.answers.length == 0) return 0;
-  return (votesToThisOption / question.answers.length) * 100;
+  if (validAnswersToThisQuestion.length == 0) return 0;
+  return (votesToThisOption / validAnswersToThisQuestion.length) * 100;
 }
 
-let countVotes = [];
-for (let i = 0; i < question.choicesOptions.length; i++) {
-  countVotes.push(0);
-}
+const countVotes = answersToThisQuestion.reduce((acc, curr) => {
+  const ans = curr.value.answer;
+  const isValidAnswer =
+    !isNaN(ans) &&
+    Number(ans) >= 0 &&
+    Number(ans) < questionParams.value.choicesOptions.length;
+  if (isValidAnswer) {
+    acc[Number(ans)] += 1;
+    return acc;
+  } else {
+    return acc;
+  }
+}, new Array(questionParams.value.choicesOptions.length).fill(0));
 
-for (let i = 0; i < question.answers.length; i++) {
-  // Any user can post any answer they want. They might post "yes" for example
-  const option = Number(question.answers[i].value.answer);
-  if (option >= 0 && option < countVotes.length) countVotes[option] += 1;
-}
+console.log(1, countVotes);
 
 function displayableOptionName(option) {
   if (option.length > 12) {
@@ -27,57 +56,52 @@ function displayableOptionName(option) {
 
 const renderOption = (option, index) => {
   return (
-    <a
-      href={`#${context.accountId}/widget/newVotingInterface?blockHeight=${blockHeight}`}
-      style={{ textDecoration: "none", color: "black" }}
-    >
-      <div className="d-flex">
-        <div style={{ color: "#000", width: "90%" }}>
-          {/* Set the width of the next div to make the bar grow. At the same, use the same value to fill the span tag */}
-          <div
-            style={{
-              margin: "0.3rem 0px",
-              content: "",
-              display: "table",
-              clear: "both",
-              padding: "0.01em 16px",
-              display: "inline-block",
-              width: `${calculatePercentage(countVotes[index])}%`,
-              textAlign: "center",
-              overflow: "visible",
-              whiteSpace: "nowrap",
-              textAlign: "left",
-              backgroundColor: "lightgray",
-            }}
-          >
-            <span style={{ overflow: "visible", fontWeight: "500" }}>
-              {displayableOptionName(option)}
-              <span
-                className="text-secondary"
-                style={{ marginLeft: "1rem", fontWeight: "400" }}
-              >
-                ({countVotes[index]} votes)
-              </span>
-            </span>
-          </div>
-        </div>
-        <span
+    <div className="d-flex">
+      <div style={{ color: "#000", width: "90%" }}>
+        {/* Set the width of the next div to make the bar grow. At the same, use the same value to fill the span tag */}
+        <div
           style={{
-            minWidth: "max-content",
-            marginLeft: "0.3rem",
-            fontWeight: "500",
+            margin: "0.3rem 0px",
+            content: "",
+            display: "table",
+            clear: "both",
+            padding: "0.01em 16px",
+            display: "inline-block",
+            width: `${calculatePercentage(countVotes[index])}%`,
+            textAlign: "center",
+            overflow: "visible",
+            whiteSpace: "nowrap",
+            textAlign: "left",
+            backgroundColor: "lightgray",
           }}
         >
-          {calculatePercentage(countVotes[index])}%
-        </span>
+          <span style={{ overflow: "visible", fontWeight: "500" }}>
+            {displayableOptionName(option)}
+            <span
+              className="text-secondary"
+              style={{ marginLeft: "1rem", fontWeight: "400" }}
+            >
+              ({countVotes[index]} votes)
+            </span>
+          </span>
+        </div>
       </div>
-    </a>
+      <span
+        style={{
+          minWidth: "max-content",
+          marginLeft: "0.3rem",
+          fontWeight: "500",
+        }}
+      >
+        {calculatePercentage(countVotes[index])}%
+      </span>
+    </div>
   );
 };
 
 return (
   <div className="m-2">
-    {question.choicesOptions.map((option, index) => {
+    {questionParams.value.choicesOptions.map((option, index) => {
       return renderOption(option, index);
     })}
   </div>
