4b36f974fdf40deca337bff45415bd85635baea3
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-13 17:41:22 -0500

    Update voteMultipleChoice by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-13 17:41:22.122

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/voteMultipleChoice/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/voteMultipleChoice/source_code.jsx
@@ -1,34 +1,53 @@
-function validateProps(props) {
-  let errors = [];
-  if (!props.question) errors.push("Props doesn't contain question key");
-  if (!props.option) errors.push("Props doesn't contain option key");
-  if (!props.index) errors.push("Props doesn't contain index key");
-  if (!props.haveVoted) errors.push("Props doesn't contain haveVoted key");
-  if (!props.userVote) errors.push("Props doesn't contain userVote key");
-  return errors;
+if (!props.blockHeight) {
+  return "Property blockHeight not set";
+}
+if (isNaN(props.blockHeight)) {
+  return "Property blockHeight should be a number";
 }
 
-const propErrors = validateProps(props);
-if (propErrors.length > 0) {
+const isPreview = props.isPreview;
+const questionBlockHeight = Number(props.blockHeight);
+const questions = Social.index("poll_question", "question-v3.0.1");
+const questionParams = questions.find(
+  (q) => q.blockHeight == questionBlockHeight
+);
+
+const answers = Social.index("poll_question", "answer-v3.0.1");
+const answersToThisQuestion = answers.filter(
+  (a) => a.value.questionBlockHeight == questionBlockHeight
+);
+
+let userVote;
+const currAccountId = context.accountId ?? "";
+function userHasVoted() {
   return (
-    <>
-      {propErrors.map((e) => (
-        <div>{e}</div>
-      ))}
-    </>
+    answersToThisQuestion.find((a) => a.accountId == currAccountId) != undefined
   );
 }
+let hasVoted = userHasVoted();
 
-let question = props.question;
-let option = props.option;
-let index = props.index;
-let haveVoted = props.haveVoted;
-let userVote = props.userVote;
-let isPreview = props.isPreview;
+const countVotes = answersToThisQuestion.reduce((acc, curr) => {
+  const ans = curr.value.answer;
+  const isValidAnswer =
+    !isNaN(ans) &&
+    Number(ans) >= 0 &&
+    Number(ans) < questionParams.value.choicesOptions.length;
+  if (isValidAnswer) {
+    acc[Number(ans)] += 1;
+    return acc;
+  } else {
+    return acc;
+  }
+}, new Array(questionParams.value.choicesOptions.length).fill(0));
 
 State.init({
   vote: userVote ?? "",
 });
+
+const isQuestionOpen =
+  questionParams.value.startTimestamp < Date.now() &&
+  Date.now() < questionParams.value.endTimestamp;
+console.log(1, isQuestionOpen);
 
 const getPublicationParams = () => {
   return {
@@ -38,7 +57,7 @@ const getPublicationParams = () => {
           key: "answer-v3.0.1",
           value: {
             answer: state.vote,
-            questionBlockHeight: props.blockHeight,
+            questionBlockHeight,
           },
         },
         undefined,
@@ -48,14 +67,12 @@ const getPublicationParams = () => {
   };
 };
 
-//TODO get this data
-let countVotes = [1, 0, 0];
-
 function calculatePercentage(votesToThisOption) {
-  return (votesToThisOption / question.value.answers.length) * 100;
+  if (answersToThisQuestion.length == 0) return 0;
+  return (votesToThisOption / answersToThisQuestion.length) * 100;
 }
 
-let styles = haveVoted
+let styles = hasVoted
   ? { color: "#000", width: "90%" }
   : { color: "#000", width: "100%" };
 
@@ -65,73 +82,85 @@ const isValidInput = () => {
 };
 
 return (
-  <div>
-    <div className="d-flex">
-      <div style={styles}>
-        {/* Set the width of the next div to make the bar grow. At the same, use the same value to fill the span tag */}
-        <div
-          style={{
-            margin: "0.3rem 0px",
-            content: "",
-            display: "table",
-            clear: "both",
-            padding: "0.01em 16px",
-            display: "inline-block",
-            width: `${
-              haveVoted ? calculatePercentage(countVotes[index]) : 100
-            }%`,
-            textAlign: "center",
-            overflow: "visible",
-            whiteSpace: "nowrap",
-            textAlign: "left",
-            backgroundColor: `${
-              (haveVoted && state.vote == index) || state.vote == index + ""
-                ? "rgb(153, 255, 153)"
-                : "lightgray"
-            }`,
-          }}
-          onClick={() => !haveVoted && State.update({ vote: index + "" })}
-        >
-          <span style={{ overflow: "visible", fontWeight: "500" }}>
-            {option}
-            {haveVoted && (
-              <span
-                className="text-secondary"
-                style={{ marginLeft: "1rem", fontWeight: "400" }}
+  <>
+    {!isQuestionOpen ? "This question is already closed" : ""}
+    {questionParams.value.choicesOptions.map((option, index) => {
+      return (
+        <div>
+          <div className="d-flex">
+            <div style={styles}>
+              {/* Set the width of the next div to make the bar grow. At the same, use the same value to fill the span tag */}
+              <div
+                style={{
+                  margin: "0.3rem 0px",
+                  content: "",
+                  display: "table",
+                  clear: "both",
+                  padding: "0.01em 16px",
+                  display: "inline-block",
+                  width: `${
+                    hasVoted ? calculatePercentage(countVotes[index]) : 100
+                  }%`,
+                  textAlign: "center",
+                  overflow: "visible",
+                  whiteSpace: "nowrap",
+                  textAlign: "left",
+                  backgroundColor: `${
+                    (hasVoted && state.vote == index) ||
+                    state.vote == index + ""
+                      ? "rgb(153, 255, 153)"
+                      : "lightgray"
+                  }`,
+                }}
+                onClick={() => !hasVoted && State.update({ vote: index + "" })}
               >
-                ({question.answers.length} votes)
+                <span style={{ overflow: "visible", fontWeight: "500" }}>
+                  {option}
+                  {hasVoted && (
+                    <span
+                      className="text-secondary"
+                      style={{ marginLeft: "1rem", fontWeight: "400" }}
+                    >
+                      ({countVotes[index]} votes)
+                    </span>
+                  )}
+                </span>
+              </div>
+            </div>
+            {hasVoted && (
+              <span
+                style={{
+                  minWidth: "max-content",
+                  margin: "0.3rem 0px 0.3rem 0.3rem",
+                  fontWeight: "500",
+                }}
+              >
+                {calculatePercentage(countVotes[index])}%
               </span>
             )}
-          </span>
-        </div>
-      </div>
-      {haveVoted && (
-        <span
-          style={{
-            minWidth: "max-content",
-            margin: "0.3rem 0px 0.3rem 0.3rem",
-            fontWeight: "500",
-          }}
-        >
-          {calculatePercentage(countVotes[index])}%
-        </span>
-      )}
-    </div>
+          </div>
 
-    {haveVoted ? (
-      <p
-        className="text-primary"
-        style={{ textAlign: "center", fontWeight: "500" }}
-      >
-        Voted
-      </p>
-    ) : (
-      <CommitButton
-        className="my-2 btn btn-primary"
-        data={getPublicationParams()}
-      >
-        Vote
-      </CommitButton>
-    )}
-  </div>
+          {isQuestionOpen ? (
+            hasVoted ? (
+              <p
+                className="text-primary"
+                style={{ textAlign: "center", fontWeight: "500" }}
+              >
+                Voted
+              </p>
+            ) : (
+              <CommitButton
+                className="my-2 btn btn-primary"
+                data={getPublicationParams()}
+              >
+                Vote
+              </CommitButton>
+            )
+          ) : (
+            ""
+          )}
+        </div>
+      );
+    })}
+  </>
 );
