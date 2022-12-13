501f61eae23f630eebf9f12fe23105d5dc510b86
silkking.near <silkking.near>
2022-12-12 22:49:04 -0500

    Update voteMultipleChoice by silkking.near at 2022-12-12 22:49:04.040

--- silkking.near/voteMultipleChoice/source_code.jsx
+++ silkking.near/voteMultipleChoice/source_code.jsx
@@ -21,24 +21,24 @@ if (propErrors.length > 0) {
 
 let question = props.value;
 console.log("Q", question);
-let option = props.option;
-let index = props.index;
-let hasVoted = props.haveVoted;
-let userVote = props.userVote;
+// let option = props.option;
+// let index = props.index;
+// let hasVoted = props.haveVoted;
+// let userVote = props.userVote;
 let isPreview = props.isPreview;
 
 State.init({
   vote: userVote ?? "",
 });
 
-const getPublicationParams = () => {
+const getPublicationParams = (index) => {
   return {
     index: {
       poll_question: JSON.stringify(
         {
           key: "answer-v3.0.1",
           value: {
-            answer: state.vote,
+            answer: index + "",
             questionBlockHeight: props.blockHeight,
           },
         },
@@ -53,6 +53,7 @@ const getPublicationParams = () => {
 let countVotes = [1, 0, 0];
 
 function calculatePercentage(votesToThisOption) {
+  if (question.value.answers.length == 0) return 0;
   return (votesToThisOption / question.value.answers.length) * 100;
 }
 
@@ -66,73 +67,80 @@ const isValidInput = () => {
 };
 
 return (
-  <div>
-    <div className="d-flex">
-      <div style={styles}>
-        {/* Set the width of the next div to make the bar grow. At the same, use the same value to fill the span tag */}
-        <div
-          style={{
-            margin: "0.3rem 0px",
-            content: "",
-            display: "table",
-            clear: "both",
-            padding: "0.01em 16px",
-            display: "inline-block",
-            width: `${
-              hasVoted ? calculatePercentage(countVotes[index]) : 100
-            }%`,
-            textAlign: "center",
-            overflow: "visible",
-            whiteSpace: "nowrap",
-            textAlign: "left",
-            backgroundColor: `${
-              (hasVoted && state.vote == index) || state.vote == index + ""
-                ? "rgb(153, 255, 153)"
-                : "lightgray"
-            }`,
-          }}
-          onClick={() => !hasVoted && State.update({ vote: index + "" })}
-        >
-          <span style={{ overflow: "visible", fontWeight: "500" }}>
-            {option}
+  <>
+    {question.choicesOptions.map((option, index) => {
+      return (
+        <div>
+          <div className="d-flex">
+            <div style={styles}>
+              {/* Set the width of the next div to make the bar grow. At the same, use the same value to fill the span tag */}
+              <div
+                style={{
+                  margin: "0.3rem 0px",
+                  content: "",
+                  display: "table",
+                  clear: "both",
+                  padding: "0.01em 16px",
+                  display: "inline-block",
+                  width: `${
+                    hasVoted ? calculatePercentage(countVotes[index]) : 100
+                  }%`,
+                  textAlign: "center",
+                  overflow: "visible",
+                  whiteSpace: "nowrap",
+                  textAlign: "left",
+                  backgroundColor: `${
+                    (hasVoted && state.vote == index) ||
+                    state.vote == index + ""
+                      ? "rgb(153, 255, 153)"
+                      : "lightgray"
+                  }`,
+                }}
+                onClick={() => !hasVoted && State.update({ vote: index + "" })}
+              >
+                <span style={{ overflow: "visible", fontWeight: "500" }}>
+                  {option}
+                  {hasVoted && (
+                    <span
+                      className="text-secondary"
+                      style={{ marginLeft: "1rem", fontWeight: "400" }}
+                    >
+                      ({question.answers.length} votes)
+                    </span>
+                  )}
+                </span>
+              </div>
+            </div>
             {hasVoted && (
               <span
-                className="text-secondary"
-                style={{ marginLeft: "1rem", fontWeight: "400" }}
+                style={{
+                  minWidth: "max-content",
+                  margin: "0.3rem 0px 0.3rem 0.3rem",
+                  fontWeight: "500",
+                }}
               >
-                ({question.answers.length} votes)
+                {calculatePercentage(countVotes[index])}%
               </span>
             )}
-          </span>
-        </div>
-      </div>
-      {hasVoted && (
-        <span
-          style={{
-            minWidth: "max-content",
-            margin: "0.3rem 0px 0.3rem 0.3rem",
-            fontWeight: "500",
-          }}
-        >
-          {calculatePercentage(countVotes[index])}%
-        </span>
-      )}
-    </div>
+          </div>
 
-    {hasVoted ? (
-      <p
-        className="text-primary"
-        style={{ textAlign: "center", fontWeight: "500" }}
-      >
-        Voted
-      </p>
-    ) : (
-      <CommitButton
-        className="my-2 btn btn-primary"
-        data={getPublicationParams()}
-      >
-        Vote
-      </CommitButton>
-    )}
-  </div>
+          {hasVoted ? (
+            <p
+              className="text-primary"
+              style={{ textAlign: "center", fontWeight: "500" }}
+            >
+              Voted
+            </p>
+          ) : (
+            <CommitButton
+              className="my-2 btn btn-primary"
+              data={getPublicationParams(index)}
+            >
+              Vote
+            </CommitButton>
+          )}
+        </div>
+      );
+    })}
+  </>
 );
