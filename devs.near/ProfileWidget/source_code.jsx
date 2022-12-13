1a52b0b56a1ea14d0a051a8d8aeb986ea8a8e47a
near <near>
2022-12-13 01:02:56 -0500

    Update ProfileWidget by devs.near at 2022-12-13 01:02:56.656

---
+++ devs.near/ProfileWidget/source_code.jsx
@@ -0,0 +1,22 @@
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+if (profile === null) {
+  return "Loading";
+}
+
+const description = profile.description;
+
+State.init({
+  loadWidget: false,
+});
+
+return (
+  <div>
+    <ProfileWidget />
+  </div>
+);
