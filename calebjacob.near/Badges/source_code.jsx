84d4efc188c250c5a5ea91e594647499d8e14bf4
calebjacob <calebjacob>
2022-12-14 17:18:31 -0500

    Update Badges by calebjacob.near at 2022-12-14 17:18:31.023

--- calebjacob.near/Badges/source_code.jsx
+++ calebjacob.near/Badges/source_code.jsx
@@ -1,1 +1,47 @@
-return <div>Badges</div>;
+const appName = "Badges";
+const accountId = props.accountId;
+
+if (!accountId) {
+  return "No account ID";
+}
+
+// const badges = Social.getr(`*/badges/*`);
+// const badges = Social.getr(`*/badges`, "final");
+const badges = Social.getr(`${accountId}/*`);
+// const badges = Social.getr(`${accountId}/profile`);
+
+console.log(badges);
+
+if (badges === null) {
+  return "Loading";
+}
+// console.log(badges);
+// initState({ badges });
+
+return (
+  <div className="container row">
+    // <div>{badges}</div>
+    <div className="mb-2">
+      <CommitButton
+        data={{
+          badges: {
+            whale: {
+              info: {
+                name: "Whale",
+                description: "A really whalethy user",
+                image: {
+                  url: "https://upload.wikimedia.org/wikipedia/commons/e/e2/Southern_right_whale.jpg",
+                },
+              },
+              holder: {
+                [accountId]: "",
+              },
+            },
+          },
+        }}
+      >
+        Get that badge
+      </CommitButton>
+    </div>
+  </div>
+);
