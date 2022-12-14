8b8403bce38e438f03deb9ec5a507163a3f670f4
evgenykuzyakov <evgenykuzyakov>
2022-12-13 19:18:29 -0500

    Update Welcome by mob.near at 2022-12-13 19:18:29.665

--- mob.near/Welcome/source_code.jsx
+++ mob.near/Welcome/source_code.jsx
@@ -13,51 +13,10 @@ return (
       )}
     </div>
     <div className="row mb-3">
-      <div>
-        <h4>Applications</h4>
-        <Widget src="mob.near/widget/WidgetIcons" props={{ tag: "app" }} />
-      </div>
+      <Widget src="mob.near/widget/Applications" />
     </div>
     <div className="row mb-3">
-      <div>
-        <h4>People</h4>
-        <div className="mb-2">
-          <Widget
-            src="mob.near/widget/ProfileSearch"
-            props={{
-              limit: 10,
-              onChange: ({ result }) => State.update({ profiles: result }),
-            }}
-          />
-        </div>
-        {state.profiles && state.profiles.length > 0 && (
-          <div className="mb-2">
-            {state.profiles.map((profile, i) => (
-              <div key={i} className="d-flex justify-content-between mb-3">
-                <div className="me-4 text-truncate">
-                  <Widget
-                    src="mob.near/widget/Profile"
-                    props={{ accountId: profile.accountId }}
-                  />
-                </div>
-                <div className="text-nowrap">
-                  <Widget
-                    src="mob.near/widget/FollowButton"
-                    props={{ accountId: profile.accountId }}
-                  />
-                  <Widget
-                    src="mob.near/widget/PokeButton"
-                    props={{ accountId: profile.accountId }}
-                  />
-                </div>
-              </div>
-            ))}
-            <hr />
-          </div>
-        )}
-
-        <Widget src="mob.near/widget/LastProfilesImages" />
-      </div>
+      <Widget src="mob.near/widget/People" />
     </div>
     <div className="row mb-3">
       <div>
