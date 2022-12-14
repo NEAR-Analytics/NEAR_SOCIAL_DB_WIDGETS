a970e1e3974fc2c6b433c3db04d7d19add38538a
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-13 22:10:09 -0500

    Update showQuestionsHandler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-13 22:10:09.231

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showQuestionsHandler/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showQuestionsHandler/source_code.jsx
@@ -1,3 +1,5 @@
+State.init({ showQuestion: false, modalBlockHeight: 0 });
+
 const displayAnswerWidgetNames = [
   "newTextAnswerInterface",
   "newMiniMultipleChoiceInterface",
@@ -7,29 +9,95 @@ let questions = Social.index("poll_question", "question-v3.0.1", {
   accountId: props.accountId,
 });
 
+function closeModalClickingOnTransparent() {
+  console.log("n3");
+  return (e) => {
+    e.target.id == "modal" && State.update({ showQuestion: false });
+  };
+}
+
+const renderModal = () => {
+  return (
+    <div
+      className="modal"
+      id="modal"
+      style={
+        state.showQuestion && { display: "block", backgroundColor: "#7e7e7e70" }
+      }
+      tabindex="-1"
+      role="dialog"
+      onClick={closeModalClickingOnTransparent()}
+    >
+      <div className="modal-dialog" style={{ maxWidth: "95%" }} role="document">
+        <div
+          className="modal-content"
+          style={{ backgroundColor: "rgb(230, 230, 230)" }}
+        >
+          <div className="modal-header flex-row-reverse">
+            <button
+              type="button"
+              className="close"
+              dataDismiss="modal"
+              ariaLabel="Close"
+              onClick={() => State.update({ showQuestion: false })}
+            >
+              <span ariaHidden="true">&times;</span>
+            </button>
+          </div>
+          <div
+            className="modal-body"
+            style={{
+              width: "90%",
+              borderRadius: "1rem",
+              backgroundColor: "white",
+              margin: "0 auto",
+            }}
+          >
+            <Widget
+              src={`${context.accountId}/widget/newVotingInterface`}
+              props={{ blockHeight: state.modalBlockHeight }}
+            />
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-dismiss="modal"
+              onClick={() => State.update({ showQuestion: false })}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const renderQuestions = () => {
   return questions.map((question) => {
     return (
-      <a
-        href={`#${context.accountId}/widget/newVotingInterface?blockHeight=${question.blockHeight}`}
-        style={{ textDecoration: "none", color: "black" }}
+      <div
+        className="my-5 py-3 px-4"
+        style={{ backgroundColor: "white", borderRadius: "1rem" }}
+        onClick={() => {
+          State.update({
+            showQuestion: true,
+            modalBlockHeight: question.blockHeight,
+          });
+        }}
       >
-        <div
-          className="my-5 py-3 px-4"
-          style={{ backgroundColor: "white", borderRadius: "1rem" }}
-        >
-          <Widget
-            src={`${context.accountId}/widget/answersHeader`}
-            props={{ ...question }}
-          />
-          <Widget
-            src={`${context.accountId}/widget/${
-              displayAnswerWidgetNames[question.value.questionType]
-            }`}
-            props={{ ...question }}
-          />
-        </div>
-      </a>
+        <Widget
+          src={`${context.accountId}/widget/answersHeader`}
+          props={{ ...question }}
+        />
+        <Widget
+          src={`${context.accountId}/widget/${
+            displayAnswerWidgetNames[question.value.questionType]
+          }`}
+          props={{ ...question }}
+        />
+      </div>
     );
   });
 };
@@ -42,5 +110,6 @@ return (
     }}
   >
     {renderQuestions()}
+    {state.showQuestion && renderModal()}
   </div>
 );
