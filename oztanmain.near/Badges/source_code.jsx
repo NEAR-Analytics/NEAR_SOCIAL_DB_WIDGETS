122393fef89d33834da87780c9b634805229813c
oztanmain.near <oztanmain.near>
2022-12-14 17:45:49 -0500

    Update Badges by oztanmain.near at 2022-12-14 17:45:49.201

--- oztanmain.near/Badges/source_code.jsx
+++ oztanmain.near/Badges/source_code.jsx
@@ -5,39 +5,19 @@ if (!accountId) {
   return "No account ID";
 }
 
-const badges = Social.getr(`${accountID}/badges/*`);
+const badges = Social.getr(`${accountId}/badges/*`);
 console.log(badges);
 
 if (badges === null) {
   return "Loading";
 }
-// console.log(badges);
-// initState({ badges });
 
 return (
   <div className="container row">
-    <div>{badges}</div>
-    <div className="mb-2">
-      <CommitButton
-        data={{
-          badges: {
-            "6-4": {
-              info: {
-                name: "A Tall Man TM",
-                description: "A really tall man",
-                image: {
-                  url: "https://commons.wikimedia.org/wiki/File:Image-Vertical.jpg#/media/File:Image-Vertical.jpg",
-                },
-              },
-              holder: {
-                [accountId]: "",
-              },
-            },
-          },
-        }}
-      >
-        Get that badge
-      </CommitButton>
+    <div>
+      {Object.values(badges).map((badge) => {
+        return <p>{badge.info.name}</p>;
+      })}
     </div>
   </div>
 );
