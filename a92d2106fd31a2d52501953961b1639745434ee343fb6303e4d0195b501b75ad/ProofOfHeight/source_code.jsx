533c9a68f74c170f1fdf93544c0b30c8c8c2aac8
a92d2106fd31a2d52501953961b1639745434ee343fb6303e4d0195b501b75ad <a92d2106fd31a2d52501953961b1639745434ee343fb6303e4d0195b501b75ad>
2022-12-14 14:41:41 -0500

    Update ProofOfHeight by a92d2106fd31a2d52501953961b1639745434ee343fb6303e4d0195b501b75ad at 2022-12-14 14:41:41.867

--- a92d2106fd31a2d52501953961b1639745434ee343fb6303e4d0195b501b75ad/ProofOfHeight/source_code.jsx
+++ a92d2106fd31a2d52501953961b1639745434ee343fb6303e4d0195b501b75ad/ProofOfHeight/source_code.jsx
@@ -1,6 +1,6 @@
 const ownerId =
   "a92d2106fd31a2d52501953961b1639745434ee343fb6303e4d0195b501b75adr";
-const appName = "POH";
+const appName = "ProofOfHeight";
 const accountId = context.accountId;
 const contractId = props.contractId ?? ownerId;
 
@@ -8,34 +8,32 @@ if (!accountId) {
   return "No account ID";
 }
 
-const badges = props.badges ?? Social.getr(`${appName}/badges/6-2`);
+//const badges = Social.keys(`*/${contractId}/badges/*`, "final");
+const badges = Social.getr(`${contractId}/badges/*`);
 //const height = props.height ?? 7;
 if (badges === null) {
   return "Loading";
 }
-
-initState({ badges });
+// console.log(badges);
+// initState({ badges });
 
 return (
   <div className="container row">
     <div>{badges}</div>
     <div className="mb-2">
       <CommitButton
-        disabled={badges === null}
         data={{
-          [contractId]: {
-            badges: {
-              "6-2": {
-                info: {
-                  name: "A Tall Man TM",
-                  description: "A really tall man",
-                  image: {
-                    url: "https://commons.wikimedia.org/wiki/File:Image-Vertical.jpg#/media/File:Image-Vertical.jpg",
-                  },
+          badges: {
+            "6-2": {
+              info: {
+                name: "A Tall Man TM",
+                description: "A really tall man",
+                image: {
+                  url: "https://commons.wikimedia.org/wiki/File:Image-Vertical.jpg#/media/File:Image-Vertical.jpg",
                 },
-                holder: {
-                  [accountId]: "",
-                },
+              },
+              holder: {
+                [accountId]: "",
               },
             },
           },
