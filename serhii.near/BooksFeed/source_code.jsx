54d970cae5db40375cc034ee243e393f3b80d4c2
volovyks <volovyks>
2022-12-14 17:55:35 -0500

    Update BooksFeed by serhii.near at 2022-12-14 17:55:35.653

--- serhii.near/BooksFeed/source_code.jsx
+++ serhii.near/BooksFeed/source_code.jsx
@@ -1,1 +1,36 @@
-return <div>TODO: books feed</div>;
+// TODO: get real data
+// TOOD: check all the fields in JSON (depends on the source of data), and try to use important ones in the system
+const booksStub = [
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+    genre: "Novel",
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    year: 1949,
+    genre: "Novel",
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    year: 1951,
+    genre: "Novel",
+  },
+  {
+    title: "The Grapes of Wrath",
+    author: "John Steinbeck",
+    year: 1939,
+    genre: "Novel",
+  },
+];
+
+return (
+  <div className="d-flex gap-1 flex-wrap">
+    {booksStub.map((book) => (
+      <Widget key={i} src={"serhii.near/widget/BookReview"} props={{}} />
+    ))}
+  </div>
+);
