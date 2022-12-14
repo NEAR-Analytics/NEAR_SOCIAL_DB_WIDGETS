c27a836a657ac1ccf1746258f3eb5a709e7b2014
silkking.near <silkking.near>
2022-12-14 04:31:36 -0500

    Update voteWithText by silkking.near at 2022-12-14 04:31:36.714

--- silkking.near/voteWithText/source_code.jsx
+++ silkking.near/voteWithText/source_code.jsx
@@ -1,5 +1,34 @@
-if (!props.blockHeight) {
+if (!props.isPreview && !props.blockHeight) {
   return "Property blockHeight not set";
+}
+
+// Utility function
+function getBlockTimestamp(blockHeight) {
+  // It is stored in nanoseconds which is 1e-6 miliseconds
+  return Near.block(blockHeight).header.timestamp / 1e6;
+}
+
+// Discards answers that were posted after question's end date
+function getTimeRelatedValidAnswers(answers) {
+  let low = 0;
+  let high = answers.length - 1;
+  const questionEndTimestamp = questionParams.value.endTimestamp;
+  let endBlockTimestamp = getBlockTimestamp(answers[high].blockHeight);
+  if (endBlockTimestamp < questionEndTimestamp) return answers;
+  // For tries to exceed 50 there should be more than 10e15 answers which will never happen. But if you mess up and make an infinite cycle it will crash. This way it will never be infinite
+  let tries = 10;
+  while (high - low > 1 && tries > 0) {
+    tries--;
+    let curr = Math.floor((high - low) / 2) + low;
+    let currBlockTimestamp = getBlockTimestamp(answers[curr].blockHeight);
+    if (currBlockTimestamp < questionEndTimestamp) {
+      low = curr;
+    } else {
+      high = curr;
+    }
+  }
+  // Slice ignores the index of the last one. Since high - low == 1, high = low + 1
+  return answers.slice(0, high);
 }
 
 let isPreview = props.isPreview;
@@ -10,6 +39,17 @@ const answers = Social.index("poll_question", "answer-v3.0.1");
 const answersToThisQuestion = answers.filter(
   (a) => a.value.questionBlockHeight == questionBlockHeight
 );
+const onTimeAnswersToThisQuestion = getTimeRelatedValidAnswers(
+  answersToThisQuestion
+);
+let usersThatAlreadyReplied = [];
+let validAnswersToThisQuestion = onTimeAnswersToThisQuestion.filter((a) => {
+  const didUserAlreadyVoted = usersThatAlreadyReplied.includes(a.accountId);
+  if (!didUserAlreadyVoted) {
+    usersThatAlreadyReplied.push(a.accountId);
+  }
+  return !didUserAlreadyVoted;
+});
 
 let userVote;
 const loggedAccountId = context.accountId ?? "";
@@ -44,6 +84,18 @@ const isValidInput = () => {
   return result && !isPreview;
 };
 
+const renderAnswers = () => {
+  console.log(2, validAnswersToThisQuestion);
+  return validAnswersToThisQuestion.map((answer) => {
+    return (
+      <Widget
+        src="silkking.near/widget/answer_poll-comment-container"
+        props={{ blockHeight: answer.blockHeight }}
+      />
+    );
+  });
+};
+console.log(1);
 return (
   <div>
     {hasVoted ? (
@@ -60,6 +112,7 @@ return (
           onChange={(e) => State.update({ vote: e.target.value })}
           style={{ width: "100%" }}
         />
+
         {isValidInput() ? (
           <CommitButton
             className="my-2 btn btn-primary"
@@ -77,5 +130,6 @@ return (
         )}
       </div>
     )}
+    {renderAnswers()}
   </div>
 );
