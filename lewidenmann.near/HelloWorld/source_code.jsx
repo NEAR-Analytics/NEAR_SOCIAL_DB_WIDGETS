f5e662945e5e4bef407b6b08a2db8d39eb7e7c45
jon-lewis <jon-lewis>
2022-12-13 19:43:18 -0500

    Update HelloWorld by lewidenmann.near at 2022-12-13 19:43:18.798

--- lewidenmann.near/HelloWorld/source_code.jsx
+++ lewidenmann.near/HelloWorld/source_code.jsx
@@ -1,4 +1,4 @@
 const profileName = Social.get({ keys: [props.accountId + "/profile/name"] });
-const greeting = `Hello ${profileName || "World"}!`;
+const greeting = `Hello ${profileName || "Worlds"}!`;
 
 return <div>{greeting}</div>;
