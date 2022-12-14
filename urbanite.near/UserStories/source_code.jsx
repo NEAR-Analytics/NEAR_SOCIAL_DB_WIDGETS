2aebd0bd578fc924a01bbe9a7d6e9799fbd883f6
urbanite.near <urbanite.near>
2022-12-14 17:09:18 -0500

    Update UserStories by urbanite.near at 2022-12-14 17:09:18.475

--- urbanite.near/UserStories/source_code.jsx
+++ urbanite.near/UserStories/source_code.jsx
@@ -1,1 +1,25 @@
-return <div>Hello World</div>;
+const note = Social.get(`urbanite.near/experimental/stories`);
+
+if (note === null) {
+  return "Loading";
+}
+
+State.init({ note: note || "" });
+
+return (
+  <div>
+    <div className="mb-2">
+      <h4>Notepad</h4>
+      <textarea
+        type="text"
+        rows={10}
+        className="form-control"
+        value={state.note}
+        onChange={(e) => State.update({ note: e.target.value })}
+      />
+    </div>
+    <CommitButton data={{ experimental: { note: state.note } }}>
+      Save note
+    </CommitButton>
+  </div>
+);
