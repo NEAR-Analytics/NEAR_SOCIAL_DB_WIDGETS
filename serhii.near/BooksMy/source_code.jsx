0f71d21676644a77d65cdc34ba05bb872d573b47
volovyks <volovyks>
2022-12-14 18:10:44 -0500

    Update BooksMy by serhii.near at 2022-12-14 18:10:44.922

--- serhii.near/BooksMy/source_code.jsx
+++ serhii.near/BooksMy/source_code.jsx
@@ -1,1 +1,41 @@
-return <div>TODO: My books</div>;
+// TODO: get real data
+// TOOD: check all the fields in JSON (depends on the source of data), and try to use important ones in the system
+const booksStub = [
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+    genre: "Novel",
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    year: 1949,
+    genre: "Novel",
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    year: 1951,
+    genre: "Novel",
+  },
+  {
+    title: "The Grapes of Wrath",
+    author: "John Steinbeck",
+    year: 1939,
+    genre: "Novel",
+  },
+];
+
+return (
+  <div>
+    <div>
+      <button>Add book</button>
+    </div>
+    <div className="d-flex gap-1 flex-wrap">
+      {booksStub.map((book) => (
+        <Widget key={i} src={"serhii.near/widget/BookTile"} props={{ book }} />
+      ))}
+    </div>
+  </div>
+);
