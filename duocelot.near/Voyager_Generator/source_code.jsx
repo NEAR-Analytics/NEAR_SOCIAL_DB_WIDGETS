438a3d40f0c9ae9ba227d45f11fc39af29193242
duocelot.near <duocelot.near>
2022-12-14 06:23:00 -0500

    Update Voyager_Generator by duocelot.near at 2022-12-14 06:23:00.179

--- duocelot.near/Voyager_Generator/source_code.jsx
+++ duocelot.near/Voyager_Generator/source_code.jsx
@@ -1,7 +1,71 @@
 const accountId = context.accountId;
 
 if (!accountId) {
-  return <div>"Please sign in with NEAR wallet to add a new image"</div>;
+  return (
+    <div
+      style={{
+        width: "512px",
+        height: "512px",
+        backgroundSize: "100%",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: "white",
+        position: "flex",
+        fontFamily: '"Press Start 2P", sans-serif',
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        src="https://bafybeiep6yscx365ici64ce2o7ktr3vewuyk7rwj4kxpwrf4ugeloxgfri.ipfs.w3s.link/error-001.jpg"
+        alt="ERROR 001"
+        style={{
+          width: "340px",
+          margin: "50px 40px 10px 80px",
+          position: "flex",
+        }}
+      />
+      <h2
+        style={{
+          fontFamily: "Press Start 2P",
+          fontSize: "14px",
+          color: "black",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        ERROR 001: NO NEAR WALLET CONNECTED.{" "}
+      </h2>{" "}
+      <h3
+        style={{
+          fontFamily: "Press Start 2P",
+          fontSize: "12px",
+          color: "black",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {" "}
+        Please connect with your near wallet or create one for free
+      </h3>
+      <a
+        href="https://shard.dog/go?url=https://app.jumpdefi.xyz"
+        style={{
+          fontFamily: "Press Start 2P",
+          fontSize: "14px",
+          color: "black",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          margin: "40px 1opx 10px 10px",
+        }}
+      >
+        HERE
+      </a>
+    </div>
+  );
 }
 
 initState({
@@ -35,7 +99,7 @@ const shadow = {
 };
 
 const txt1 = {
-  fontFamily: "Arial",
+  fontFamily: "Press Start 2P",
   fontSize: "12px",
   color: "white",
   display: "flex",
@@ -47,7 +111,7 @@ const txt1 = {
 };
 
 const txt2 = {
-  fontFamily: "Arial",
+  fontFamily: "Press Start 2P",
   fontSize: "12px",
   color: "white",
   display: "flex",
