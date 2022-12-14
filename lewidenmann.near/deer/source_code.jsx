c839998be883a9cfd1093d3ec31a2484ec41fa0b
jon-lewis <jon-lewis>
2022-12-14 04:21:05 -0500

    Update deer by lewidenmann.near at 2022-12-14 04:21:05.629

--- lewidenmann.near/deer/source_code.jsx
+++ lewidenmann.near/deer/source_code.jsx
@@ -1,8 +1,6 @@
-let c = 0;
-
 function moreDeer() {
   let deer = [];
-  for (let i = 0; i < 90; i++) {
+  for (let i = 0; i < 1000; i++) {
     deer.push("https://i.giphy.com/media/Qld1cd6a6QlWw/giphy.webp");
   }
   return deer;
@@ -13,24 +11,22 @@ State.init({
 });
 
 function loadMoreDeer() {
-  if (c === 1) {
-    return;
-  }
-  c += 1;
   State.update({
     deer: state.deer.concat(moreDeer()),
   });
 }
 
 return (
-  <InfiniteScroll
-    pageStart={0}
-    loadMore={loadMoreDeer}
-    hasMore={true}
-    loader={<p>deer...</p>}
-  >
-    {state.deer.map((url, index) => (
-      <img src={url} />
-    ))}
-  </InfiniteScroll>
+  <div class="">
+    <InfiniteScroll
+      pageStart={0}
+      loadMore={loadMoreDeer}
+      hasMore={true}
+      loader={<p>deer...</p>}
+    >
+      {state.deer.map((url, index) => (
+        <img src={url} />
+      ))}
+    </InfiniteScroll>
+  </div>
 );
