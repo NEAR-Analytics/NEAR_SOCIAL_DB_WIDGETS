309d0a644a58abc41738cf1aa17e44535baf5046
volovyks <volovyks>
2022-12-14 15:52:37 -0500

    Update BookTile by serhii.near at 2022-12-14 15:52:37.367

--- serhii.near/BookTile/source_code.jsx
+++ serhii.near/BookTile/source_code.jsx
@@ -3,19 +3,25 @@ const book = props.book ?? {};
 const title = book.title ?? "No title";
 const author = book.author ?? "No author";
 const cover = book.cover ?? "";
+const id = book.id ?? 0;
 
 return (
   <div className="profile d-inline-block">
-    <Widget
-      src="serhii.near/widget/BookCover"
-      props={{
-        book,
-        className: "float-start d-inline-block me-2",
-      }}
-    />
-    <div>
-      <p>{title}</p>
-      <p>{author}</p>
-    </div>
+    <a
+      href={`#/serhii.near/widget/BookPage?id=${id}`}
+      className="text-decoration-none link-dark"
+    >
+      <Widget
+        src="serhii.near/widget/BookCover"
+        props={{
+          book,
+          className: "float-start d-inline-block me-2",
+        }}
+      />
+      <div>
+        <p>{title}</p>
+        <p>{author}</p>
+      </div>
+    </a>
   </div>
 );
