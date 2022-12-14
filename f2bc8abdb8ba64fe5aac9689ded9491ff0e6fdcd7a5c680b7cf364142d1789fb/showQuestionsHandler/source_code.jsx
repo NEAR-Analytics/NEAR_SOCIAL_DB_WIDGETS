be2d7a138b12a7fda28468b431db2dd00045db9a
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-13 22:31:41 -0500

    Update showQuestionsHandler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-13 22:31:41.075

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showQuestionsHandler/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showQuestionsHandler/source_code.jsx
@@ -1,106 +1,98 @@
-State.init({ showQuestion: false, modalBlockHeight: 0 });
-
-const displayAnswerWidgetNames = [
-  "newTextAnswerInterface",
-  "newMiniMultipleChoiceInterface",
-];
-
-let questions = Social.index("poll_question", "question-v3.0.1", {
-  accountId: props.accountId,
-});
-
-function closeModalClickingOnTransparent() {
-  console.log("n3");
-  return (e) => {
-    e.target.id == "modal" && State.update({ showQuestion: false });
-  };
+if (!props.isPreview && !props.blockHeight) {
+  return "Prop block height wasn't provided";
 }
 
-const renderModal = () => {
+let isPreview = props.isPreview ?? false;
+
+let questionBlockHeight = Number(props.blockHeight);
+const questions =
+  !props.previewInfo && Social.index("poll_question", "question-v3.0.1");
+const questionParams =
+  props.previewInfo ??
+  questions.find((q) => q.blockHeight == questionBlockHeight);
+console.log(questionParams);
+
+let profile = Social.getr(`${questionParams.accountId}/profile`);
+
+let questionsByThisCreator = Social.index("poll_question", "question-v3.0.1", {
+  accountId: questionParams.accountId,
+});
+
+if (!questions) {
+  return "Loading...";
+}
+
+function sliceString(string, newStringLenght) {
+  if (string.length > newStringLenght) {
+    return string.slice(0, newStringLenght) + "...";
+  }
+  return string;
+}
+
+function transformDateFormat(date) {
+  return new Date(date).toLocaleDateString();
+}
+
+const widgetOwner = "silkking.near";
+const renderVoteMultipleChoice = () => {
+  if (questionParams) {
+    return (
+      <Widget
+        src={`${widgetOwner}/widget/voteMultipleChoice`}
+        props={{
+          ...questionParams,
+          isPreview,
+        }}
+      />
+    );
+  } else {
+    return "Invalid block height provided.";
+  }
+};
+
+const renderVoteText = () => {
   return (
-    <div
-      className="modal"
-      id="modal"
-      style={
-        state.showQuestion && { display: "block", backgroundColor: "#7e7e7e70" }
-      }
-      tabindex="-1"
-      role="dialog"
-      onClick={closeModalClickingOnTransparent()}
-    >
-      <div className="modal-dialog" style={{ maxWidth: "95%" }} role="document">
-        <div
-          className="modal-content"
-          style={{ backgroundColor: "rgb(230, 230, 230)" }}
-        >
-          <div className="modal-header flex-row-reverse">
-            <button
-              type="button"
-              className="close"
-              dataDismiss="modal"
-              ariaLabel="Close"
-              onClick={() => State.update({ showQuestion: false })}
-            >
-              <span ariaHidden="true">&times;</span>
-            </button>
-          </div>
-          <div
-            className="modal-body"
-            style={{
-              width: "90%",
-              borderRadius: "1rem",
-              backgroundColor: "white",
-              margin: "0 auto",
-            }}
-          >
-            <Widget
-              src={`${context.accountId}/widget/newVotingInterface`}
-              props={{ blockHeight: state.modalBlockHeight }}
-            />
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              data-dismiss="modal"
-              onClick={() => State.update({ showQuestion: false })}
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Widget
+      src={`${widgetOwner}/widget/voteWithText`}
+      props={{ ...questionParams, isPreview }}
+    />
   );
 };
 
-const renderQuestions = () => {
-  return questions.map((question) => {
+function getValidAnswersQtyFromQuestion(questionBlockHeight) {
+  // let questionParams = questions.find(q => q.blockHeight == questionBlockHeight)
+
+  const answers = Social.index("poll_question", "answer-v3.0.1");
+  const answersFromThisQuestion = answers.filter(
+    (a) => a.value.questionBlockHeight == questionBlockHeight
+  );
+  const usersWithAnswers = answersFromThisQuestion.map((a) => a.accountId);
+  const usersWithAnswersWithoutDuplicates = usersWithAnswers.filter(
+    (u, index) => usersWithAnswers.indexOf(u) == index
+  );
+  return usersWithAnswersWithoutDuplicates.length;
+}
+
+const renderQuestionsByThisCreator = () => {
+  return questionsByThisCreator.map((questionByCreator, index) => {
+    let divStyle = index == 0 ? {} : { borderTop: "1px solid #ced4da" };
     return (
-      <div
-        className="my-5 py-3 px-4"
-        style={{
-          backgroundColor: "white",
-          borderRadius: "1rem",
-          cursor: "pointer",
-        }}
-        onClick={() => {
-          State.update({
-            showQuestion: true,
-            modalBlockHeight: question.blockHeight,
-          });
-        }}
-      >
-        <Widget
-          src={`${context.accountId}/widget/answersHeader`}
-          props={{ ...question }}
-        />
-        <Widget
-          src={`${context.accountId}/widget/${
-            displayAnswerWidgetNames[question.value.questionType]
-          }`}
-          props={{ ...question }}
-        />
+      <div style={divStyle}>
+        <p style={{ fontWeight: "500" }}>
+          {sliceString(questionByCreator.value.title, 20)}
+        </p>
+        <div className="d-flex justify-content-between flex-nowrap text-secondary">
+          <span>End date</span>
+          <span>
+            {transformDateFormat(questionByCreator.value.endTimestamp)}
+          </span>
+        </div>
+        <div className="d-flex justify-content-between flex-nowrap text-secondary">
+          <span>Votes</span>
+          <span>
+            ({getValidAnswersQtyFromQuestion(questionByCreator.blockHeight)})
+          </span>
+        </div>
       </div>
     );
   });
@@ -108,12 +100,167 @@ const renderQuestions = () => {
 
 return (
   <div
-    style={{
-      borderRadius: "3px",
-      padding: "0px 2%",
-    }}
+    className="d-flex content-align-start justify-content-between"
+    style={{ borderRadius: "3px", padding: "2rem 3rem" }}
   >
-    {renderQuestions()}
-    {state.showQuestion && renderModal()}
+    <div style={{ width: "75%", marginRight: "2rem" }}>
+      <div className="d-flex">
+        <span
+          style={{
+            backgroundColor:
+              questionParams.value.startTimestamp < Date.now() &&
+              questionParams.value.endTimestamp > Date.now()
+                ? "rgb(153, 255, 153)"
+                : "rgb(255, 128, 128)",
+
+            height: "max-content",
+            width: "6rem",
+            border: "1px solid rgb(0, 82, 204)",
+            textAlign: "center",
+            borderRadius: "80px",
+            marginRight: "1rem",
+          }}
+        >
+          {questionParams.value.startTimestamp < Date.now() &&
+          questionParams.value.endTimestamp > Date.now()
+            ? "Active"
+            : "Closed"}
+        </span>
+
+        {Date.now() < questionParams.value.endTimestamp && (
+          <span
+            style={{
+              paddingLeft: "1.5rem",
+              borderLeft: "2px solid #ced4da",
+            }}
+          >
+            Ends in
+            <Widget
+              src={`silkking.near/widget/timeAgo`}
+              props={{ timeInFuture: questionParams.value.endTimestamp }}
+            />
+          </span>
+        )}
+      </div>
+
+      <h2>{questionParams.value.title}</h2>
+
+      <div className="d-flex">
+        <span className="mr-3" style={{ fontWeight: "500" }}>
+          Created by
+        </span>
+
+        <Widget
+          src="mob.near/widget/ProfileImage"
+          props={{
+            profile,
+            question: questionParams.accountId,
+            className: "float-start d-inline-block me-2",
+            style: {
+              width: "1.5rem",
+              marginLeft: "1rem",
+            },
+          }}
+        />
+
+        <span style={{ fontWeigth: "500" }}>
+          {sliceString(questionParams.accountId, 18)}
+        </span>
+      </div>
+
+      <p>{questionParams.value.description}</p>
+
+      {questionParams.value.tgLink != "" &&
+        questionParams.value.tgLink != undefined && (
+          <h4>
+            Discussion link:
+            <a href={questionParams.value.tgLink}>
+              {questionParams.value.tgLink}
+            </a>
+          </h4>
+        )}
+
+      <div
+        style={{ border: "1px solid #ced4da", borderRadius: "0.375rem" }}
+        className="p-3 my-3"
+      >
+        <h4>{questionParams.value.question}</h4>
+
+        {questionParams.value.questionType == "0"
+          ? renderVoteText()
+          : renderVoteMultipleChoice()}
+      </div>
+    </div>
+
+    <div style={{ minWidth: "17rem" }}>
+      <h5>Information</h5>
+      <div
+        className="mb-2"
+        style={{
+          border: "1px solid #ced4da",
+          borderRadius: "0.375rem",
+          fontWeight: "500",
+          padding: "0.5rem 1rem",
+        }}
+      >
+        <div className="d-flex justify-content-between">
+          <span>Status</span>
+          <span>
+            {questionParams.value.startTimestamp < Date.now() &&
+            questionParams.value.endTimestamp > Date.now()
+              ? "Active"
+              : "Closed"}
+          </span>
+        </div>
+
+        <div className="d-flex justify-content-between">
+          <span>Start date</span>
+          <span>
+            {transformDateFormat(questionParams.value.startTimestamp)}
+          </span>
+        </div>
+
+        <div className="d-flex justify-content-between">
+          <span>End date</span>
+          <span>{transformDateFormat(questionParams.value.endTimestamp)}</span>
+        </div>
+
+        <div className="d-flex justify-content-between">
+          <span>Creator</span>
+          <span>{sliceString(questionParams.accountId, 8)}</span>
+        </div>
+      </div>
+
+      {questionsByCreator.length != 1 && (
+        <>
+          <div className="d-flex">
+            <h5>Polls by creator</h5>
+            <h5 style={{ marginLeft: "0.5rem" }}>
+              ({questionsByThisCreator.length})
+            </h5>
+          </div>
+
+          <div
+            style={{
+              border: "1px solid #ced4da",
+              borderRadius: "0.375rem",
+              padding: "0.5rem 1rem",
+            }}
+          >
+            {renderQuestionsByThisCreator()}
+            <div style={{ margin: "1rem 0", textAlign: "center" }}>
+              <a
+                href={`#${widgetOwner}/widget/showQuestionsHandler?accountId=${questionParams.accountId}`}
+                style={{ textDecoration: "none" }}
+              >
+                <button className="btn btn-outline-primary w-75">
+                  View all
+                </button>
+              </a>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   </div>
 );
