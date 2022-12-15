4907d017a1a684921307e62a0008b80a8e9ddb9a
dmitriy_sheleg.near <dmitriy_sheleg.near>
2022-12-15 15:31:47 -0500

    Update GenieQuestionList by dmitriy_sheleg.near at 2022-12-15 15:31:47.583

---
+++ dmitriy_sheleg.near/GenieQuestionList/source_code.jsx
@@ -0,0 +1,29 @@
+const questions = Social.index("genie", "asked");
+questions = questions.reverse();
+if (!questions?.length) {
+  return <p>No Results</p>;
+}
+
+return (
+  <div className="d-flex flex-column gap-3">
+    <div className="d-flex justify-content-end"></div>
+    <div className="d-flex flex-column gap-3">
+      {questions.map((q) => (
+        <div key={q.value} className="d-flex flex-column gap-1 p-2">
+          <Widget
+            src={"michaelpeter.near/widget/GenieQuestionView"}
+            props={{ questionRef: q.value }}
+          />
+          <Widget
+            src={"michaelpeter.near/widget/GenieAnswerList"}
+            props={{ questionRef: q.value }}
+          />
+          <Widget
+            src={"michaelpeter.near/widget/GenieAnswerSubmit"}
+            props={{ questionRef: q.value }}
+          />
+        </div>
+      ))}
+    </div>
+  </div>
+);
