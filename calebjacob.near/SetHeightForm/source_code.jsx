bfb42f098d5f7303dac069a65d256a546d183f2a
calebjacob <calebjacob>
2022-12-14 21:50:43 -0500

    Update SetHeightForm by calebjacob.near at 2022-12-14 21:50:43.579

--- calebjacob.near/SetHeightForm/source_code.jsx
+++ calebjacob.near/SetHeightForm/source_code.jsx
@@ -1,4 +1,8 @@
-State.init({ heightFeet: null, heightInches: null });
+State.init({
+  heightHasBeenSaved: false,
+  heightFeet: null,
+  heightInches: null,
+});
 
 const height = {
   feet: state.inputHeightFeet || 0,
@@ -11,73 +15,84 @@ const formIsValid = heightIsSet && inchesAreValid;
 
 function submitHeight() {
   const totalInches = height.feet * 12 + height.inches;
+  State.update({
+    heightHasBeenSaved: true,
+  });
   console.log(`Your height in total inches: ${totalInches}`);
 }
 
 return (
   <div class="card p-3">
-    <div
-      className=" d-flex flex-column flex-md-row align-items-stretch"
-      style={{ gap: "1rem" }}
-    >
-      <div
-        className="d-flex align-items-center"
-        style={{ marginRight: "auto" }}
-      >
-        <h5 className="m-0">What&apos;s your height?</h5>
+    {state.heightHasBeenSaved ? (
+      <div class="alert alert-success m-0" role="alert">
+        Your height has been saved: {height.feet}′{height.inches}″
       </div>
+    ) : (
+      <div>
+        <div
+          className=" d-flex flex-column flex-md-row align-items-stretch"
+          style={{ gap: "1rem" }}
+        >
+          <div
+            className="d-flex align-items-center"
+            style={{ marginRight: "auto" }}
+          >
+            <h5 className="m-0">What&apos;s your height?</h5>
+          </div>
 
-      <div
-        className="d-flex flex-row"
-        style={{ gap: "1rem", minWidth: "15rem" }}
-      >
-        <div class="form-floating" style={{ width: "50%" }}>
-          <input
-            className="form-control"
-            id="heightFeet"
-            type="number"
-            placeholder="Feet"
-            value={state.giveBadgeAccountId}
-            min="0"
-            onChange={(e) => {
-              const value = Number(e.target.value) || 0;
-              State.update({ inputHeightFeet: e.target.value });
-            }}
-          />
-          <label for="heightFeet">Feet</label>
+          <div
+            className="d-flex flex-row"
+            style={{ gap: "1rem", minWidth: "15rem" }}
+          >
+            <div class="form-floating" style={{ width: "50%" }}>
+              <input
+                className="form-control"
+                id="heightFeet"
+                type="number"
+                placeholder="Feet"
+                value={state.giveBadgeAccountId}
+                min="0"
+                onChange={(e) => {
+                  const value = Number(e.target.value) || 0;
+                  State.update({ inputHeightFeet: e.target.value });
+                }}
+              />
+              <label for="heightFeet">Feet</label>
+            </div>
+
+            <div class="form-floating" style={{ width: "50%" }}>
+              <input
+                className="form-control"
+                id="heightInches"
+                type="number"
+                placeholder="Inches"
+                value={state.giveBadgeAccountId}
+                min="0"
+                max="11"
+                onChange={(e) => {
+                  const value = Number(e.target.value) || 0;
+                  State.update({ inputHeightInches: value });
+                }}
+              />
+              <label for="heightInches">Inches</label>
+            </div>
+          </div>
+
+          <button
+            type="button"
+            className="btn btn-primary"
+            disabled={!formIsValid}
+            onClick={submitHeight}
+          >
+            Save Height: {height.feet}′{height.inches}″
+          </button>
         </div>
 
-        <div class="form-floating" style={{ width: "50%" }}>
-          <input
-            className="form-control"
-            id="heightInches"
-            type="number"
-            placeholder="Inches"
-            value={state.giveBadgeAccountId}
-            min="0"
-            max="11"
-            onChange={(e) => {
-              const value = Number(e.target.value) || 0;
-              State.update({ inputHeightInches: value });
-            }}
-          />
-          <label for="heightInches">Inches</label>
-        </div>
-      </div>
-
-      <button
-        type="button"
-        className="btn btn-primary"
-        disabled={!formIsValid}
-        onClick={submitHeight}
-      >
-        Save Height: {height.feet}′{height.inches}″
-      </button>
-    </div>
-
-    {!inchesAreValid && (
-      <div class="alert alert-danger m-0 mt-3" role="alert">
-        Inches must be between 0 and 11
+        {!inchesAreValid && (
+          <div class="alert alert-danger m-0 mt-3" role="alert">
+            Inches must be between 0 and 11
+          </div>
+        )}
       </div>
     )}
   </div>
