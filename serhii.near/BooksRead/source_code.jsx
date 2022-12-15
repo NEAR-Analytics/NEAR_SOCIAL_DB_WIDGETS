8ea534ab43750e0752a5d29b6608c568cd15ae67
volovyks <volovyks>
2022-12-15 17:32:23 -0500

    Update BooksRead by serhii.near at 2022-12-15 17:32:23.542

--- serhii.near/BooksRead/source_code.jsx
+++ serhii.near/BooksRead/source_code.jsx
@@ -11,12 +11,20 @@ if (!books) {
   return "No books yet";
 }
 
+const BookRows = styled.p`{
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "left",
+  justifyContent: "space-between",
+  width: "100%",
+  height: "100%",
+  padding: "1rem",
+}`;
+
 return (
-  <div>
-    <div className="d-flex gap-1 flex-wrap">
-      {Object.values(books).map((book) => (
-        <Widget key={i} src={"serhii.near/widget/BookTile"} props={{ book }} />
-      ))}
-    </div>
-  </div>
+  <BookRows>
+    {Object.values(books).map((book) => (
+      <Widget key={i} src={"serhii.near/widget/BookTile"} props={{ book }} />
+    ))}
+  </BookRows>
 );
