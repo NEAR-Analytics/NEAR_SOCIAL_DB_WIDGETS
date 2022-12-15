7d0ac0fe7b80fb6677ff9ce270b199deb47cdcbb
calebjacob <calebjacob>
2022-12-15 00:02:42 -0500

    Update UserHeightDisplay by calebjacob.near at 2022-12-15 00:02:42.461

--- calebjacob.near/UserHeightDisplay/source_code.jsx
+++ calebjacob.near/UserHeightDisplay/source_code.jsx
@@ -66,7 +66,7 @@ return (
               {user.accuracyPercentage}% Accurate
             </span>
           ) : (
-            <span className="badge text-bg-secondary">Unverified</span>
+            <span className="badge text-bg-dark">Unverified</span>
           )}
         </div>
       ) : (
