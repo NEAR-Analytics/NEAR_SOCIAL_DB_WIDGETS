8a39b13b69b9b6703c9ffb0c075c82a6a8dd26b9
volovyks <volovyks>
2022-12-15 09:33:32 -0500

    Update BooksMy by serhii.near at 2022-12-15 09:33:32.006

--- serhii.near/BooksMy/source_code.jsx
+++ serhii.near/BooksMy/source_code.jsx
@@ -1,31 +1,15 @@
-// TODO: get real data
-// TOOD: check all the fields in JSON (depends on the source of data), and try to use important ones in the system
-const booksStub = [
-  {
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    year: 1925,
-    genre: "Novel",
-  },
-  {
-    title: "1984",
-    author: "George Orwell",
-    year: 1949,
-    genre: "Novel",
-  },
-  {
-    title: "The Catcher in the Rye",
-    author: "J.D. Salinger",
-    year: 1951,
-    genre: "Novel",
-  },
-  {
-    title: "The Grapes of Wrath",
-    author: "John Steinbeck",
-    year: 1939,
-    genre: "Novel",
-  },
-];
+const accountId = context.accountId;
+
+if (!accountId) {
+  return "";
+}
+
+const books = Social.get(`${accountId}/books/my/**`);
+console.log(books);
+
+if (!books) {
+  return "no books yet";
+}
 
 return (
   <div>
@@ -33,7 +17,7 @@ return (
       <button>Add book</button>
     </div>
     <div className="d-flex gap-1 flex-wrap">
-      {booksStub.map((book) => (
+      {Object.values(books).map((book) => (
         <Widget key={i} src={"serhii.near/widget/BookTile"} props={{ book }} />
       ))}
     </div>
