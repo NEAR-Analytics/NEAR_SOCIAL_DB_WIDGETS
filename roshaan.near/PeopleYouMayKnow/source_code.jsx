3fb2e4768f6f63655ef34cef69751418be79b907
roshaan.near <roshaan.near>
2022-12-15 13:50:58 -0500

    Update PeopleYouMayKnow by roshaan.near at 2022-12-15 13:50:58.105

--- roshaan.near/PeopleYouMayKnow/source_code.jsx
+++ roshaan.near/PeopleYouMayKnow/source_code.jsx
@@ -244,37 +244,17 @@ const switchModeButton = (
       : "Switch to multiselect mode"}
   </button>
 );
-const peopleYouMayKnow = () => {
-  return (
-    <>
-      <h1>People You May Know</h1>
-      <p>
-        Based on your current connections, you might also want to follow the
-        following accounts.
-      </p>
-      <div className="mb-3">{switchModeButton}</div>
-      {state.multiSelectMode && <div className="mb-3">{commitButton}</div>}
-      <ul className="list-group">{followingsRows}</ul>
-      <div className="mt-2 mb-3">{commitButton}</div>
-    </>
-  );
-};
-const searchFriends = () => {
-  return (
-    <div className="row mb-3">
-      <Widget src="mob.near/widget/People" />
-    </div>
-  );
-};
+
 return (
   <>
-    <Widget
-      src="roshaan.near/widget/tabs"
-      props={{
-        tab1: searchFriends,
-        tab2: peopleYouMayKnow,
-        tab3: playGame,
-      }}
-    />
+    <h1>People You May Know</h1>
+    <p>
+      Based on your current connections, you might also want to follow the
+      following accounts.
+    </p>
+    <div className="mb-3">{switchModeButton}</div>
+    {state.multiSelectMode && <div className="mb-3">{commitButton}</div>}
+    <ul className="list-group">{followingsRows}</ul>
+    <div className="mt-2 mb-3">{commitButton}</div>
   </>
 );
