fbf689a6081904a2a1586ac22e64b329c1af63cb
mpeterdev <mpeterdev>
2022-12-14 22:16:00 -0500

    Update GenieQuestionView by michaelpeter.near at 2022-12-14 22:16:00.777

---
+++ michaelpeter.near/GenieQuestionView/source_code.jsx
@@ -0,0 +1,41 @@
+const { questionRef } = props;
+
+State.init({ questionRef, refInput });
+
+if (!state.questionRef) {
+  return (
+    <div className="d-flex flex-column gap-1">
+      <p>No question ref was passed in props, enter one here:</p>
+      <input
+        type="text"
+        className="form-control"
+        value={state.questionRef}
+        onChange={(e) => {
+          State.update({ refInput: e.target.value });
+        }}
+      />
+      <button
+        onClick={() => {
+          State.update({ questionRef: state.refInput });
+        }}
+      >
+        Save
+      </button>
+    </div>
+  );
+}
+
+const asker = state.questionRef.split("--")[0];
+
+const question = Social.get(
+  `${asker}/experimental/genie/questions/${state.questionRef}`
+);
+
+return (
+  <div className="d-flex flex-column gap-1">
+    <p>
+      Asked by: {asker} | Ref: {state.questionRef}
+    </p>
+    <p>{question}</p>
+  </div>
+);
