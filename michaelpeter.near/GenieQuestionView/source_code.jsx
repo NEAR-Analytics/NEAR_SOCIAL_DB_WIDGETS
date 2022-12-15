688d50644a80a9c6cb1ce9b20e739ef4b1de575b
mpeterdev <mpeterdev>
2022-12-15 18:47:48 -0500

    Update GenieQuestionView by michaelpeter.near at 2022-12-15 18:47:48.026

--- michaelpeter.near/GenieQuestionView/source_code.jsx
+++ michaelpeter.near/GenieQuestionView/source_code.jsx
@@ -6,36 +6,63 @@ if (!state.questionRef) {
   return (
     <div className="d-flex flex-column gap-1">
       <p>No question ref was passed in props, enter one here:</p>
-      <input
-        type="text"
-        className="form-control"
-        value={state.questionRef}
-        onChange={(e) => {
-          State.update({ refInput: e.target.value });
-        }}
-      />
-      <button
-        onClick={() => {
-          State.update({ questionRef: state.refInput });
-        }}
-      >
-        Save
-      </button>
+      <div className="d-flex gap-3 align-items-center">
+        <i class="bi bi-search" />
+        <div class="input-group input-group-lg">
+          <input
+            type="text"
+            className="form-control border border-opacity-10"
+            value={state.questionRef}
+            placeholder="Your wish is my command"
+            onChange={(e) => {
+              State.update({ refInput: e.target.value });
+            }}
+          />
+        </div>
+        <button
+          className="btn btn-primary btn-lg"
+          onClick={() => {
+            State.update({ questionRef: state.refInput });
+          }}
+        >
+          Save
+        </button>
+      </div>
     </div>
   );
 }
 
 const asker = state.questionRef.split("--")[0];
 
-const question = Social.get(
+const question = Social.getr(
   `${asker}/experimental/genie/questions/${state.questionRef}`
 );
 
 return (
   <div className="d-flex flex-column gap-1">
-    <h3>{question}</h3>
-    <p>
-      Asked by: {asker} | Ref: {state.questionRef}
-    </p>
+    <div className="d-flex align-items-center">
+      <div
+        style={{ width: "100%" }}
+        className="d-flex align-items-center justify-content-between"
+      >
+        <div className="d-flex align-items-center gap-2">
+          <div
+            style={{
+              backgroundColor: "#000",
+              width: "24px",
+              height: "24px",
+              borderRadius: "50%",
+            }}
+          ></div>
+          <div className="d-flex flex-column">
+            <h6>{asker}</h6>
+            {state.questionRef}
+          </div>
+        </div>
+        <div>timestamp</div>
+      </div>
+    </div>
+    <h3>{question.title}</h3>
+    <h3>{question.content}</h3>
   </div>
 );
