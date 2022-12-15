129e79ba1c09069f52f94d52631fb17b2512566f
mpeterdev <mpeterdev>
2022-12-14 22:40:12 -0500

    Update GenieAnswerSubmit by michaelpeter.near at 2022-12-14 22:40:12.507

---
+++ michaelpeter.near/GenieAnswerSubmit/source_code.jsx
@@ -0,0 +1,40 @@
+State.init({ answerContent: "" });
+
+const { questionRef } = props;
+
+if (!questionRef) {
+  return "No question ref provided";
+}
+
+return (
+  <div className="d-flex flex-row gap-1">
+    <input
+      type="text"
+      className="form-control"
+      value={state.answerContent}
+      onChange={(e) => {
+        State.update({ answerContent: e.target.value });
+      }}
+    />
+    <CommitButton
+      className="btn btn-primary col-2"
+      data={{
+        experimental: {
+          genie: {
+            answers: {
+              [questionRef]: state.answerContent,
+            },
+          },
+        },
+        index: {
+          genie: JSON.stringify({
+            key: "answered1",
+            value: questionRef,
+          }),
+        },
+      }}
+    >
+      Submit answer
+    </CommitButton>
+  </div>
+);
