7240b65a78ad48f7f506c7d8dc4c337c297f23fc
urbanite.near <urbanite.near>
2022-12-15 04:46:29 -0500

    Update AstroBounties by urbanite.near at 2022-12-15 04:46:29.470

--- urbanite.near/AstroBounties/source_code.jsx
+++ urbanite.near/AstroBounties/source_code.jsx
@@ -1,43 +1,3 @@
-const bounties = fetch(
-  "https://search.testnet.app.astrodao.com/bounty/_search?size=20&from=0",
-  {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: {
-        bool: {
-          must: [
-            {
-              simple_query_string: {
-                query: "*",
-                fields: ["accounts"],
-              },
-            },
-          ],
-          must_not: [],
-          should: [],
-        },
-      },
-      sort: [
-        {
-          createTimestamp: {
-            order: "desc",
-          },
-        },
-      ],
-    }),
-  }
-);
-
-console.log(bounties.body.hits);
-
-if (bounties.body.hits.hits.length == 0) {
-  return "Loading...";
-}
-
 return (
   <>
     <h2>Astro Bounties | V1</h2>
@@ -51,35 +11,6 @@ return (
         onChange: ({ result }) => State.update({ apps: result }),
       }}
     />
-    <ol>
-      {bounties
-        ? bounties.body.hits.hits.map((bounty) => {
-            const bountyId = bounty._source.id;
-            return (
-              <li>
-                <div>
-                  <h3>
-                    <b>DAO:</b> {bounty._source.daoId}
-                  </h3>
-                  <p>
-                    <b>Summary:</b> {bounty._source.description}
-                  </p>
-                  <p>
-                    <b>Amount:</b>{" "}
-                    <Widget
-                      src="urbanite.near/widget/YoctoNEARConverter"
-                      props={{ amount: bounty._source.amount }}
-                    />
-                  </p>
-                  <Widget
-                    src="edwardkcyu.near/widget/AstroBountiesCommentEditor"
-                    props={{ bountyId }}
-                  />
-                </div>
-              </li>
-            );
-          })
-        : ""}
-    </ol>
+    <Widget src="urbanite.near/widget/ListAstroBounties" />
   </>
 );
