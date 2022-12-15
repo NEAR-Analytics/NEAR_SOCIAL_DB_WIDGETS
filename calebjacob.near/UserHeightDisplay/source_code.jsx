5cfd32fd3574d7bcb42c545cb1cbddfe759afb3c
calebjacob <calebjacob>
2022-12-14 23:38:39 -0500

    Update UserHeightDisplay by calebjacob.near at 2022-12-14 23:38:39.132

---
+++ calebjacob.near/UserHeightDisplay/source_code.jsx
@@ -0,0 +1,56 @@
+function displayHeight(height) {
+  const feet = Math.floor(height / 12);
+  const inches = height - feet * 12;
+  return `${feet}′${inches}″`;
+}
+
+function accuracyPercentageClassName(accuracyPercentage) {
+  if (accuracyPercentage < 25) {
+    return "text-bg-danger";
+  } else if (accuracyPercentage < 50) {
+    return "text-bg-warning";
+  } else if (accuracyPercentage < 75) {
+    return "text-bg-primary";
+  } else {
+    return "text-bg-success";
+  }
+}
+
+return (
+  <div className="border-top pt-3">
+    <div
+      className="d-flex flex-column flex-sm-row align-items-md-center"
+      style={{ gap: "1rem" }}
+    >
+      <Widget
+        src="mob.near/widget/Profile"
+        props={{ accountId: props.accountId }}
+      />
+
+      {typeof props.heightTotalInches === "number" ? (
+        <div
+          className="d-flex flex-row align-items-center"
+          style={{ gap: "1rem", marginLeft: "auto" }}
+        >
+          <h1 className="m-0">{displayHeight(props.heightTotalInches)}</h1>
+
+          {typeof props.accuracyPercentage === "number" ? (
+            <span
+              className={`badge ${accuracyPercentageClassName(
+                props.accuracyPercentage
+              )}`}
+            >
+              {props.accuracyPercentage}% Accurate
+            </span>
+          ) : (
+            <span className="badge text-bg-secondary">Not Confirmed</span>
+          )}
+        </div>
+      ) : (
+        <div style={{ marginLeft: "auto" }}>
+          <span className="badge text-bg-secondary">Height Not Set</span>
+        </div>
+      )}
+    </div>
+  </div>
+);
