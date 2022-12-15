39852e2f065d7962423c15bc03945bea890c845b
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-15 03:55:51 -0500

    Update newMiniMultipleChoiceInterface by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-15 03:55:51.582

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newMiniMultipleChoiceInterface/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newMiniMultipleChoiceInterface/source_code.jsx
@@ -2,19 +2,54 @@ if (!props.blockHeight) {
   return "Property blockHeight not set";
 }
 
+// Utility function. Consider moving it to an utility widget
+function getBlockTimestamp(blockHeight) {
+  // It is stored in nanoseconds which is 1e-6 miliseconds
+  return Near.block(blockHeight).header.timestamp / 1e6;
+}
+
+// Discards answers that were posted after question's end date. Consider moving to utility widget
+function getTimeRelatedValidAnswers(answers) {
+  let low = 0;
+  let high = answers.length - 1;
+  const questionEndTimestamp = questionParams.value.endTimestamp;
+  let endBlockTimestamp = getBlockTimestamp(answers[high].blockHeight);
+  if (endBlockTimestamp < questionEndTimestamp) return answers;
+  // For tries to exceed 50 there should be more than 10e15 answers which will never happen. But if you mess up and make an infinite cycle it will crash. This way it will never be infinite
+  let tries = 50;
+  while (high - low > 1 && tries > 0) {
+    tries--;
+    let curr = Math.floor((high - low) / 2) + low;
+    let currBlockTimestamp = getBlockTimestamp(answers[curr].blockHeight);
+    if (currBlockTimestamp < questionEndTimestamp) {
+      low = curr;
+    } else {
+      high = curr;
+    }
+  }
+  // Slice ignores the index of the last one. Since high - low == 1, high = low + 1
+  return answers.slice(0, high);
+}
+
 const questionBlockHeight = Number(props.blockHeight);
 const questions = Social.index("poll_question", "question-v3.0.1");
+if (!questions) {
+  return "Loading";
+}
 const questionParams = questions.find(
   (q) => q.blockHeight == questionBlockHeight
 );
 State.init({ vote: "", showErrorsInForm: false });
 
 const answers = Social.index("poll_question", "answer-v3.0.1");
+if (!answers) {
+  return "Loading";
+}
 const answersToThisQuestion = answers.filter(
   (a) => a.value.questionBlockHeight == questionBlockHeight
 );
 let usersWithAnswersToThisQuestion = [];
-const validAnswersToThisQuestion = answersToThisQuestion.filter((a) => {
+let validAnswersToThisQuestion = answersToThisQuestion.filter((a) => {
   const didUserAlreadyAnswered = usersWithAnswersToThisQuestion.includes(
     a.accountId
   );
@@ -23,33 +58,47 @@ const validAnswersToThisQuestion = answersToThisQuestion.filter((a) => {
   }
   return !didUserAlreadyAnswered;
 });
-
-console.log(1, validAnswersToThisQuestion);
+validAnswersToThisQuestion = getTimeRelatedValidAnswers(
+  validAnswersToThisQuestion
+);
 
 function calculatePercentage(votesToThisOption) {
-  if (validAnswersToThisQuestion.length == 0) return 0;
-  return (votesToThisOption / validAnswersToThisQuestion.length) * 100;
+  if (validAnswersToThisQuestion.length == 0) return 100;
+  return (
+    (votesToThisOption / validAnswersToThisQuestion.length) *
+    100
+  ).toFixed(2);
 }
 
-const countVotes = answersToThisQuestion.reduce((acc, curr) => {
-  const ans = curr.value.answer;
-  const isValidAnswer =
-    !isNaN(ans) &&
-    Number(ans) >= 0 &&
-    Number(ans) < questionParams.value.choicesOptions.length;
-  if (isValidAnswer) {
-    acc[Number(ans)] += 1;
-    return acc;
-  } else {
-    return acc;
-  }
-}, new Array(questionParams.value.choicesOptions.length).fill(0));
+const currAccId = context.accountId ?? "";
+const userHasVoted = validAnswersToThisQuestion.find(
+  (a) => a.accountId == currAccId
+);
+const isQuestionOpen =
+  questionParams.value.startTimestamp < Date.now() &&
+  Date.now() < questionParams.value.endTimestamp;
+const displayAnswers = userHasVoted || !isQuestionOpen;
 
-console.log(1, countVotes);
+let countVotes = new Array(questionParams.value.choicesOptions.length).fill(0);
+countVotes = !displayAnswers
+  ? countVotes
+  : validAnswersToThisQuestion.reduce((acc, curr) => {
+      const ans = curr.value.answer;
+      const isValidAnswer =
+        !isNaN(ans) &&
+        Number(ans) >= 0 &&
+        Number(ans) < questionParams.value.choicesOptions.length;
+      if (isValidAnswer) {
+        acc[Number(ans)] += 1;
+        return acc;
+      } else {
+        return acc;
+      }
+    }, countVotes);
 
 function displayableOptionName(option) {
-  if (option.length > 12) {
-    return option.slice(0, 12) + "...";
+  if (option.length > 20) {
+    return option.slice(0, 20) + "...";
   }
   return option;
 }
@@ -67,7 +116,9 @@ const renderOption = (option, index) => {
             clear: "both",
             padding: "0.01em 16px",
             display: "inline-block",
-            width: `${calculatePercentage(countVotes[index])}%`,
+            width: `${
+              displayAnswers ? calculatePercentage(countVotes[index]) : 100
+            }%`,
             textAlign: "center",
             overflow: "visible",
             whiteSpace: "nowrap",
@@ -81,7 +132,7 @@ const renderOption = (option, index) => {
               className="text-secondary"
               style={{ marginLeft: "1rem", fontWeight: "400" }}
             >
-              ({countVotes[index]} votes)
+              {displayAnswers && `(${countVotes[index]} votes)`}
             </span>
           </span>
         </div>
@@ -93,7 +144,7 @@ const renderOption = (option, index) => {
           fontWeight: "500",
         }}
       >
-        {calculatePercentage(countVotes[index])}%
+        {displayAnswers && `${calculatePercentage(countVotes[index])}%`}
       </span>
     </div>
   );
