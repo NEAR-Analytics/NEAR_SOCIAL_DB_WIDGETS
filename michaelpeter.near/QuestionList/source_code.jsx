5f45e7bf3d05b00e0f2035ee3aea98ed090a7816
mpeterdev <mpeterdev>
2022-12-14 20:22:24 -0500

    Update QuestionList by michaelpeter.near at 2022-12-14 20:22:24.481

--- michaelpeter.near/QuestionList/source_code.jsx
+++ michaelpeter.near/QuestionList/source_code.jsx
@@ -20,7 +20,7 @@ if (!questions?.length) {
 // );
 
 return (
-  <div className="d-flex flex-column gap-3">
+  <div className="d-flex flex-column gap-5">
     {/*JSON.stringify(questions)*/}
     {questions.map((q) => (
       <div key={q.value}>
