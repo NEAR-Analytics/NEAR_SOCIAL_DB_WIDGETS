1bb16d2c3b3a940379012e2cfadb292726fa6705
mpeterdev <mpeterdev>
2022-12-14 20:21:04 -0500

    Update QuestionList by michaelpeter.near at 2022-12-14 20:21:04.296

--- michaelpeter.near/QuestionList/source_code.jsx
+++ michaelpeter.near/QuestionList/source_code.jsx
@@ -3,13 +3,31 @@ if (!questions?.length) {
   return <p>No Results</p>;
 }
 
+// return (
+//   <div className="d-flex flex-column gap-3">
+//     {/*JSON.stringify(questions)*/}
+//     {questions.map((q) => (
+//       <div key={q.value}>
+//         {q.accountId} : {q.value} |{" "}
+//         {Social.get(`${q.accountId}/experimental/genie/questions/${q.value}`)}
+//         <Widget
+//           src={"michaelpeter.near/widget/ViewQuestion"}
+//           props={{ questionRef: q.value }}
+//         />
+//       </div>
+//     ))}
+//   </div>
+// );
+
 return (
   <div className="d-flex flex-column gap-3">
     {/*JSON.stringify(questions)*/}
     {questions.map((q) => (
       <div key={q.value}>
-        {q.accountId} : {q.value} |{" "}
-        {Social.get(`${q.accountId}/experimental/genie/questions/${q.value}`)}
+        <Widget
+          src={"michaelpeter.near/widget/ViewQuestion"}
+          props={{ questionRef: q.value }}
+        />
       </div>
     ))}
   </div>
