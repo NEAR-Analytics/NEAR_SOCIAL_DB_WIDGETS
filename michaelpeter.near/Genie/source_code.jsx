2e7cb46cfc89cfadd1cac7410e2ddf1e3366f8dd
mpeterdev <mpeterdev>
2022-12-14 20:48:26 -0500

    Update Genie by michaelpeter.near at 2022-12-14 20:48:26.636

---
+++ michaelpeter.near/Genie/source_code.jsx
@@ -0,0 +1,6 @@
+return (
+  <div className="d-flex flex-column gap-3">
+    <Widget src="michaelpeter.near/widget/SaveQuestion" />
+    <Widget src="michaelpeter.near/widget/QuestionList" />
+  </div>
+);
