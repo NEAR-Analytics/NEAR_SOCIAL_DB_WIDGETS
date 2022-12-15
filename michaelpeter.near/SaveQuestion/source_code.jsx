8f81958267f0bd66382b3e613a5d5388cfed280c
mpeterdev <mpeterdev>
2022-12-14 19:01:08 -0500

    Update SaveQuestion by michaelpeter.near at 2022-12-14 19:01:08.576

--- michaelpeter.near/SaveQuestion/source_code.jsx
+++ michaelpeter.near/SaveQuestion/source_code.jsx
@@ -1,16 +1,16 @@
-State.init({ questionContent: "" });
+State.init({ questionContent: "", timestamp: Date.now() });
 
 if (!context.accountId) {
   return <p>Loading...</p>;
 }
 
-const timestamp = Date.now();
+// const timestamp = Date.now();
 
 // const votes = Social.index('genie', 'vote');
 // const votes = [1, 2, 3];
 // return <div>{votes.map((v) => v).join(", ")}</div>;
 
-const ref = `${context.accountId}/${timestamp}`;
+// const ref = `${context.accountId}/${timestamp}`;
 return (
   <div className="d-flex flex-column">
     Enter your question:
@@ -19,16 +19,22 @@ return (
       className="form-control"
       value={state.accountId}
       onChange={(e) => {
-        // const accountId = e.target.value.toLowerCase().replace(/[^a-z0-9_.-]/g, "");
-        State.update({ questionContent: `${e.target.value} (${timestamp})` });
+        State.update({ questionContent: e.target.value });
       }}
     />
     Q: {state.questionContent}
     <div>
       <CommitButton
+        onClick={() => {
+          State.update({ timestamp: Date.now() });
+        }}
         data={{
           experimental: {
-            genie: { questions: { [ref]: state.questionContent } },
+            genie: {
+              questions: {
+                [`${context.accountId}/${Date.now()}`]: state.questionContent,
+              },
+            },
           },
         }}
       >
