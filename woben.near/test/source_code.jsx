694b0ff5a7754aa0b77bac45263f4a2994dbcda0
woben.near <woben.near>
2022-12-14 19:14:14 -0500

    Update test by woben.near at 2022-12-14 19:14:14.147

---
+++ woben.near/test/source_code.jsx
@@ -0,0 +1,24 @@
+const data = {
+  genie: {
+    question: {
+      "michaelpeter.near/32976423897": {
+        content: "What is a blockchain?",
+      },
+    },
+    answer: {},
+  },
+  index: {
+    genie: {
+      asked: "michaelpeter.near/32976423897",
+    },
+  },
+};
+
+const questions = Social.index("genie", "asked");
+console.log(questions);
+
+return (
+  <CommitButton className="btn-primary" data={data}>
+    "Save Data"
+  </CommitButton>
+);
