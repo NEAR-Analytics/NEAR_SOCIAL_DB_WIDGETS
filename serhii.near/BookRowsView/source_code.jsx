85c1def3bc4eb84e3471e2577024b74383b42b77
volovyks <volovyks>
2022-12-15 17:25:25 -0500

    Update BookRowsView by serhii.near at 2022-12-15 17:25:25.952

---
+++ serhii.near/BookRowsView/source_code.jsx
@@ -0,0 +1,11 @@
+const BookRows = styled.p`{
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "left",
+  justifyContent: "space-between",
+  width: "100%",
+  height: "100%",
+  padding: "1rem",
+}`;
+
+return <BookRows />;
