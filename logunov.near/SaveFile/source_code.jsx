46cb865684b043b04d734b7df11d02cf0f6f68e9
Longarithm <Longarithm>
2022-12-15 13:26:48 -0500

    Update SaveFile by logunov.near at 2022-12-15 13:26:48.043

--- logunov.near/SaveFile/source_code.jsx
+++ logunov.near/SaveFile/source_code.jsx
@@ -30,6 +30,7 @@ State.init({ file: null });
 function readURL(input) {
   //   var reader = new FileReader();
   console.log(globalThis);
+  console.log(FileReader);
   //   reader.onload = function (e) {
   //     $("#blah").attr("src", e.target.result).width(150).height(200);
   //   };
@@ -43,7 +44,7 @@ return (
     <div>{JSON.stringify(state.file)}</div>
     <input
       type="file"
-      onChange={(e) => console.log(e)}
+      onChange={(e) => readURL(e)}
       //   onChange={(e) => State.update({ file: e.target.value })}
     />
     <img id="blah" src="#" alt="your image" />
