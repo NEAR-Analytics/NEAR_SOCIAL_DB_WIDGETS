d38082f00f0eed06086ca6161dda58bf3da3d1b8
mpeterdev <mpeterdev>
2022-12-14 22:23:51 -0500

    Update GenieAnswerList by michaelpeter.near at 2022-12-14 22:23:51.031

--- michaelpeter.near/GenieAnswerList/source_code.jsx
+++ michaelpeter.near/GenieAnswerList/source_code.jsx
@@ -5,14 +5,19 @@ if (!questionRef) {
 }
 
 const answers = Social.index("genie", "answered1");
+const relevantAnswers = answers.filter((a) => a.value === questionRef);
 
 return (
   <div className="d-flex flex-column gap-3">
-    {answers.map((a) => (
-      <Widget
-        src="michaelpeter.near/widget/GenieAnswerView"
-        props={{ questionRef: a.value, answeredBy: a.accountId }}
-      />
-    ))}
+    {relevantAnswers?.length ? (
+      relevantAnswers.map((a) => (
+        <Widget
+          src="michaelpeter.near/widget/GenieAnswerView"
+          props={{ questionRef: a.value, answeredBy: a.accountId }}
+        />
+      ))
+    ) : (
+      <p>No answers</p>
+    )}
   </div>
 );
