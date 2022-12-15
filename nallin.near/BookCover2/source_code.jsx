3a3b8be5a9615df9932fce9f5dbdf67c10dee53e
nallin.near <nallin.near>
2022-12-15 17:31:24 -0500

    Update BookCover2 by nallin.near at 2022-12-15 17:31:24.750

---
+++ nallin.near/BookCover2/source_code.jsx
@@ -0,0 +1,34 @@
+const className = props.className ?? "profile-image d-inline-block";
+const style = props.style ?? { width: "80", height: "80" };
+const imageStyle = props.imageStyle ?? { objectFit: "cover" };
+const imageClassName = props.imageClassName ?? "rounded w-100 h-100";
+const thumbnail = props.thumbnail ?? "thumbnail";
+
+const book = props.book ?? {};
+const title = props.title ?? "Not provided";
+const cover = book.cover;
+
+const inner = (
+  <div className={className} style={style}>
+    <Widget
+      src="mob.near/widget/Image"
+      props={{
+        image: cover,
+        alt: title,
+        className: imageClassName,
+        style: imageStyle,
+        thumbnail,
+        fallbackUrl:
+          "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm",
+      }}
+    />
+  </div>
+);
+
+return tooltip ? (
+  <OverlayTrigger placement="auto" overlay={<Tooltip>{title}</Tooltip>}>
+    {inner}
+  </OverlayTrigger>
+) : (
+  inner
+);
