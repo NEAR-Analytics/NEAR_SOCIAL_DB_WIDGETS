225d3dd57b0c9ca7711e9cb9d8d5e370e04f6f03
saketh.near <saketh.near>
2022-12-15 01:37:03 -0500

    Update AllActivities by saketh.near at 2022-12-15 01:37:03.647

--- saketh.near/AllActivities/source_code.jsx
+++ saketh.near/AllActivities/source_code.jsx
@@ -10,17 +10,17 @@ if (!data) {
 const events = {};
 
 Object.keys(data).forEach((event_creator) => {
-  Object.keys(data[event_creator][appName]).forEach((event_name) => {
-    events[event_name] = event_creator;
+  Object.keys(data[event_creator][appName]).forEach((event_id) => {
+    events[event_id] = event_creator;
   });
 });
 
-const allWidgets = Object.keys(events).map((event_name) => {
-  const event_creator = events[event_name];
+const allWidgets = Object.keys(events).map((event_id) => {
+  const event_creator = events[event_id];
   return (
     <Widget
       src={`${ownerId}/widget/ViewActivity`}
-      props={{ event_creator, event_name }}
+      props={{ event_creator, event_id }}
     />
   );
 });
