7aff5230032866712e8afc8376f46795d199b166
jakmeier <jakmeier>
2022-12-15 17:40:15 -0500

    Update CanvasPartyGame by jakmeier.near at 2022-12-15 17:40:15.076

--- jakmeier.near/CanvasPartyGame/source_code.jsx
+++ jakmeier.near/CanvasPartyGame/source_code.jsx
@@ -15,8 +15,8 @@ const Tile = {
 };
 
 const myTurn = props.session.activePlayer === context.accountId;
-const secondsLeft =
-  (Date.now() / 1000 + MAX_SECONDS - props.session.start / 1000) % 1;
+const msLeft = (Date.now() - props.session.start) / 1000 + MAX_SECONDS;
+const secondsLeft = msLeft - (msLeft % 1);
 
 // Select a view of the map, store it as 2D array of tiles and insert pixels.
 const mapView = (start_x, start_y, width, height) => {
