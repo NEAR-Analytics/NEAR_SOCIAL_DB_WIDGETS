085b413e12feee48ece34313ce34fa73a1129308
jon-lewis <jon-lewis>
2022-12-15 18:51:59 -0500

    Update Proof-of-Height by lewidenmann.near at 2022-12-15 18:51:59.734

--- lewidenmann.near/Proof-of-Height/source_code.jsx
+++ lewidenmann.near/Proof-of-Height/source_code.jsx
@@ -1,4 +1,6 @@
-if (!props.accountId || !context.accountId) {
+const accountId = props.accountId || context.accountId;
+
+if (!accountId) {
   return "Please login to use Proof of Height";
 }
 
@@ -7,7 +9,7 @@ return (
     <h2>Proof of Height</h2>
     <p>Verifying personal details using social proof.</p>
     <div>
-      <Widget src="a92d2106fd31a2d52501953961b1639745434ee343fb6303e4d0195b501b75ad/widget/SetHeightForm" />
+      <Widget src="calebjacob.near/widget/SetHeightForm" />
     </div>
     <div>
       <Widget src="lewidenmann.near/widget/ReviewFollowerHeights" />
