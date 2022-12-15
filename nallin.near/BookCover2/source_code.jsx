ffe8b491f82503d7b699d1a1c70850fd2418891a
nallin.near <nallin.near>
2022-12-15 18:14:31 -0500

    Update BookCover2 by nallin.near at 2022-12-15 18:14:31.407

--- nallin.near/BookCover2/source_code.jsx
+++ nallin.near/BookCover2/source_code.jsx
@@ -1,6 +1,6 @@
 const className = props.className ?? "profile-image d-inline-block";
 const style = props.style ?? { width: "80", height: "80" };
-const imageStyle = props.imageStyle ?? { objectFit: "s" };
+const imageStyle = props.imageStyle ?? { objectFit: "fit" };
 const imageClassName = props.imageClassName ?? "rounded w-100 h-100";
 const thumbnail = props.thumbnail ?? "thumbnail";
 
