cff8b6df9685383a00fd4b4534fa6e8c7d7e408f
volovyks <volovyks>
2022-12-15 10:24:28 -0500

    Update BooksToRead by serhii.near at 2022-12-15 10:24:28.052

--- serhii.near/BooksToRead/source_code.jsx
+++ serhii.near/BooksToRead/source_code.jsx
@@ -1,19 +1,15 @@
-// TODO: get real data
-// TOOD: check all the fields in JSON (depends on the source of data), and try to use important ones in the system
-const booksStub = [
-  {
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    year: 1925,
-    genre: "Novel",
-  },
-  {
-    title: "1984",
-    author: "George Orwell",
-    year: 1949,
-    genre: "Novel",
-  },
-];
+const accountId = context.accountId;
+
+if (!accountId) {
+  return "";
+}
+
+const books = Social.get(`${accountId}/books/wantToRead/**`);
+console.log(books);
+
+if (!books) {
+  return "no books yet";
+}
 
 return (
   <div>
@@ -21,7 +17,7 @@ return (
       <button>Add book</button>
     </div>
     <div className="d-flex gap-1 flex-wrap">
-      {booksStub.map((book) => (
+      {Object.values(books).map((book) => (
         <Widget key={i} src={"serhii.near/widget/BookTile"} props={{ book }} />
       ))}
     </div>
