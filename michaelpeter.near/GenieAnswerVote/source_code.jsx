dfa2c7d984d28a7c970d5c447046a62de87425ec
mpeterdev <mpeterdev>
2022-12-15 00:12:06 -0500

    Update GenieAnswerVote by michaelpeter.near at 2022-12-15 00:12:06.332

--- michaelpeter.near/GenieAnswerVote/source_code.jsx
+++ michaelpeter.near/GenieAnswerVote/source_code.jsx
@@ -1,18 +1,47 @@
-const { questionRef, answer } = props;
+const { questionRef, answeredBy } = props;
+
+const votes = Social.index("genie", "vote");
+const upvotes = votes.filter((v) => v.value > 0);
+const downvotes = votes.filter((v) => v.value < 0);
 
 return (
-  <div className="d-flex gap-1">
-    <CommitButton
-      data={{
-        index: {
-          genie: JSON.stringify({
-            key: "vote",
-            value: questionRef,
-          }),
-        },
-      }}
-    >
-      Save question
-    </CommitButton>
+  <div className="d-flex flex-column gap-1">
+    {JSON.stringify(votes)}
+    <div>Upvotes: {upvotes}</div>
+    <div>Downvotes: {downvotes}</div>
+    <div className="d-flex flex-row gap-1">
+      <CommitButton
+        data={{
+          index: {
+            genie: JSON.stringify({
+              key: "vote",
+              value: {
+                questionRef,
+                answeredBy,
+                value: 1,
+              },
+            }),
+          },
+        }}
+      >
+        Upvote
+      </CommitButton>
+      <CommitButton
+        data={{
+          index: {
+            genie: JSON.stringify({
+              key: "vote",
+              value: {
+                questionRef,
+                answeredBy,
+                value: -1,
+              },
+            }),
+          },
+        }}
+      >
+        Downvote
+      </CommitButton>
+    </div>
   </div>
 );
