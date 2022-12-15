62ba5332de0bece39669b8df91c85b8229b86b05
mpeterdev <mpeterdev>
2022-12-14 22:19:54 -0500

    Update Genie by michaelpeter.near at 2022-12-14 22:19:54.482

--- michaelpeter.near/Genie/source_code.jsx
+++ michaelpeter.near/Genie/source_code.jsx
@@ -1,6 +1,9 @@
-return (
-  <div className="d-flex flex-column gap-3">
-    <Widget src="michaelpeter.near/widget/SaveQuestion" />
-    <Widget src="michaelpeter.near/widget/QuestionList" />
-  </div>
-);
+State.init({ selectedQuestion: "" });
+
+if (!selectedQuestion)
+  return (
+    <div className="d-flex flex-column gap-3">
+      <Widget src="michaelpeter.near/widget/SaveQuestion" />
+      <Widget src="michaelpeter.near/widget/GenieQuestionList" />
+    </div>
+  );
