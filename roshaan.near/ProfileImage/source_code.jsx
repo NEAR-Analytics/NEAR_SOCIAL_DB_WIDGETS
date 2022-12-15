4b3399f6a7abe2ac0e7e023c5385989d5576442c
roshaan.near <roshaan.near>
2022-12-15 08:19:35 -0500

    Update ProfileImage by roshaan.near at 2022-12-15 08:19:35.947

---
+++ roshaan.near/ProfileImage/source_code.jsx
@@ -0,0 +1,56 @@
+const accountId = props.accountId ?? context.accountId;
+const className = props.className ?? "profile-image d-inline-block";
+const style = props.style ?? { width: "3em", height: "3em" };
+const imageStyle = props.imageStyle ?? { objectFit: "cover" };
+const imageClassName = props.imageClassName ?? "rounded w-100 h-100";
+const thumbnail = props.thumbnail ?? "thumbnail";
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+const name = profile.name || "No-name profile";
+const image = profile.image;
+const title = props.title ?? `${name} @${accountId}`;
+const tooltipItems = props.tooltipItems ?? [];
+
+const tooltip =
+  props.tooltip && (props.tooltip === true ? title : props.tooltip);
+
+const inner = (
+  <div className={className} style={style}>
+    <Widget
+      src="mob.near/widget/Image"
+      props={{
+        image,
+        alt: title,
+        className: imageClassName,
+        style: imageStyle,
+        thumbnail,
+        fallbackUrl:
+          "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm",
+      }}
+    />
+  </div>
+);
+
+return tooltip ? (
+  <OverlayTrigger
+    placement="right"
+    overlay={
+      <Tooltip>
+        {
+          <>
+            <li className={`list-group-item`}>{inner}</li>
+            <li className={`list-group-item`}>{title} </li>
+            {tooltipItems.map((item) => {
+              return <li className={`list-group-item`}>{item} </li>;
+            })}
+          </>
+        }
+      </Tooltip>
+    }
+  >
+    {inner}
+  </OverlayTrigger>
+) : (
+  inner
+);
