a1d84f5ad1d85a72faa2a4cf42b2d74541df2bb9
volovyks <volovyks>
2022-12-15 11:29:31 -0500

    Update BooksToRead by serhii.near at 2022-12-15 11:29:31.887

--- serhii.near/BooksToRead/source_code.jsx
+++ serhii.near/BooksToRead/source_code.jsx
@@ -4,18 +4,14 @@ if (!accountId) {
   return "";
 }
 
-const books = Social.get(`${accountId}/books/wantToRead/**`);
-console.log(books);
+const books = Social.get(`${accountId}/books/toRead/**`);
 
 if (!books) {
-  return "no books yet";
+  return "No books yet";
 }
 
 return (
   <div>
-    <div>
-      <button>Add book</button>
-    </div>
     <div className="d-flex gap-1 flex-wrap">
       {Object.values(books).map((book) => (
         <Widget key={i} src={"serhii.near/widget/BookTile"} props={{ book }} />
