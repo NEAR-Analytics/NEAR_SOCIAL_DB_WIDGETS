7eecabf7851359318ec0fe15b92b20f05bef3588
ailisp <ailisp>
2022-12-15 08:07:51 -0500

    Update BooksFeed by bo.near at 2022-12-15 08:07:51.880

---
+++ bo.near/BooksFeed/source_code.jsx
@@ -0,0 +1,19 @@
+const items = Social.index("books", "my");
+
+if (!items) {
+  return "Loading";
+}
+
+console.log(books);
+
+return (
+  <div className="d-flex gap-1 flex-wrap">
+    {items.map((item) => (
+      <Widget
+        key={i}
+        src={"serhii.near/widget/BookReview"}
+        props={{ book: item.value.book }}
+      />
+    ))}
+  </div>
+);
