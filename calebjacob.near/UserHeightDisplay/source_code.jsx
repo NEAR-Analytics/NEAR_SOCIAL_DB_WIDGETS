885201489841919a34e984d6b99fad54de9e1c34
calebjacob <calebjacob>
2022-12-15 00:05:07 -0500

    Update UserHeightDisplay by calebjacob.near at 2022-12-15 00:05:07.497

--- calebjacob.near/UserHeightDisplay/source_code.jsx
+++ calebjacob.near/UserHeightDisplay/source_code.jsx
@@ -66,14 +66,14 @@ return (
               {user.accuracyPercentage}% Accurate
             </span>
           ) : (
-            <span style={{ width: "6.5rem" }} className="badge text-bg-dark">
+            <span style={{ width: "7rem" }} className="badge text-bg-dark">
               Unverified
             </span>
           )}
         </div>
       ) : (
         <div style={{ marginLeft: "auto" }}>
-          <span style={{ width: "6.5rem" }} className="badge text-bg-secondary">
+          <span style={{ width: "7rem" }} className="badge text-bg-secondary">
             Height Not Set
           </span>
         </div>
