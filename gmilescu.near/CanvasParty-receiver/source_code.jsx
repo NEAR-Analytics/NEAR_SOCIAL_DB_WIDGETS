576a05f0322ae49c5c91d58fd65687e93c362ab5
gmilescu <gmilescu>
2022-12-14 20:18:05 -0500

    Update CanvasParty-receiver by gmilescu.near at 2022-12-14 20:18:05.285

---
+++ gmilescu.near/CanvasParty-receiver/source_code.jsx
@@ -0,0 +1,61 @@
+const accountId = context.accountId;
+
+if (context.loading) {
+  return "Loading";
+}
+
+if (!accountId) {
+  return "Sign in with NEAR Wallet";
+}
+
+const notifications = Social.index("notify", accountId, {
+  order: "desc",
+  limit: 100,
+});
+
+if (notifications === null) {
+  return "Loading";
+}
+
+if (notifications.length === 0) {
+  return "No notifications";
+}
+
+Storage.set("lastBlockHeight", notifications[0].blockHeight);
+
+return (
+  <>
+    {notifications.map(({ accountId, blockHeight, value }, i) => (
+      <div key={i} className="d-flex justify-content-between mb-3">
+        <div className="me-4 text-truncate">
+          <div className="text-truncate">
+            <Widget src="mob.near/widget/ProfileLine" props={{ accountId }} />
+          </div>
+          <div
+            className="text-truncate text-muted"
+            style={{ paddingLeft: "1.8em" }}
+          >
+            {value.type === "follow"
+              ? "followed you"
+              : value.type === "unfollow"
+              ? "unfollowed you"
+              : value.type === "poke"
+              ? "poked you"
+              : "???"}
+            <Widget src="mob.near/widget/TimeAgo" props={{ blockHeight }} />
+          </div>
+        </div>
+        <div className="text-nowrap">
+          {value.type === "follow" || value.type === "unfollow" ? (
+            <Widget src="mob.near/widget/FollowButton" props={{ accountId }} />
+          ) : (
+            <Widget
+              src="mob.near/widget/PokeButton"
+              props={{ accountId, back: true }}
+            />
+          )}
+        </div>
+      </div>
+    ))}
+  </>
+);
