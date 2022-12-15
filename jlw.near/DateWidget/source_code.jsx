780e1033ec90fb8a0d0ea45e8fc356667cd7026b
janewang <janewang>
2022-12-14 20:18:25 -0500

    Update DateWidget by jlw.near at 2022-12-14 20:18:25.840

--- jlw.near/DateWidget/source_code.jsx
+++ jlw.near/DateWidget/source_code.jsx
@@ -1,5 +1,3 @@
-const card = props.card;
-
 const DateWidget = styled.div`
     [type="date"] {
     background:#fff url(https://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/calendar_2.png)  97% 50% no-repeat ;
