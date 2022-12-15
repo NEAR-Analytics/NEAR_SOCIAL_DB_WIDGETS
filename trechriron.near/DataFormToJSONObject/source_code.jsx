179367750afbe8c835c1eee36780173f49a4f001
trechiron <trechiron>
2022-12-15 02:25:32 -0500

    Update DataFormToJSONObject by trechriron.near at 2022-12-15 02:25:32.505

--- trechriron.near/DataFormToJSONObject/source_code.jsx
+++ trechriron.near/DataFormToJSONObject/source_code.jsx
@@ -8,19 +8,34 @@ if (!accountId) {
   return "Please sign in with NEAR wallet to use this widget";
 }
 
+function createOptInDataElements(dataElements) {
+  // verify object is formatted JSON
+  // build form based on elements
+  // return form
+}
+
+let bigForm = (
+  <div>
+    <label for="fname">{`First Name: `}</label>
+    <br></br>
+    <input type="text" id="fname" name="fname"></input>
+    <input type="checkbox" id="agecheckb" name="agecheckb" value="true"></input>
+    <br></br>
+    <label for="lname">{`Last Name: `}</label>
+    <br></br>
+    <input type="text" id="lname" name="lname"></input>
+  </div>
+);
+
 let page = (
   <div>
     <h1>{`Hello, ${accountId}!`}</h1>
+    <p>
+      {`Please fill out the following information and check the box if are
+      willing to earn rewards by sharing this data.`}
+    </p>
     <br></br>
-    <form>
-      <label for="fname">{`First name: `}</label>
-      <br></br>
-      <input type="text" id="fname" name="fname"></input>
-      <br></br>
-      <label for="lname">{`Last name: `}</label>
-      <br></br>
-      <input type="text" id="lname" name="lname"></input>
-    </form>
+    {bigForm}
   </div>
 );
 
