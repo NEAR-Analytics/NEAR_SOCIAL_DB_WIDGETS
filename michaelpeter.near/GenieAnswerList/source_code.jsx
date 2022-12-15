06c5bf4a1fd5d46aaa7fb0e9732eb3c38d85d064
mpeterdev <mpeterdev>
2022-12-15 17:30:10 -0500

    Update GenieAnswerList by michaelpeter.near at 2022-12-15 17:30:10.297

--- michaelpeter.near/GenieAnswerList/source_code.jsx
+++ michaelpeter.near/GenieAnswerList/source_code.jsx
@@ -6,14 +6,23 @@ if (!questionRef) {
 
 const answers = Social.index("genie", `answered/${questionRef}`);
 
+const AnswerBox = styled.div`
+  border-top: 0.5px solid #D3D3D3;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+`;
+
 return (
   <div className="d-flex flex-column gap-3">
+    {JSON.stringify(answers)}
     {answers?.length ? (
       answers.map((a) => (
-        <Widget
-          src="michaelpeter.near/widget/GenieAnswerView"
-          props={{ questionRef, answeredBy: a.accountId }}
-        />
+        <AnswerBox>
+          <Widget
+            src="michaelpeter.near/widget/GenieAnswerView"
+            props={{ questionRef, answeredBy: a.accountId }}
+          />
+        </AnswerBox>
       ))
     ) : (
       <div className="alert alert-light">No answers, be the first!</div>
