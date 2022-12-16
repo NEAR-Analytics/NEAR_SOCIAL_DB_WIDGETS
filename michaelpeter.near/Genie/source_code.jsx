6b7d2a689ce30cc235e488f695afea99a1ff0a5c
mpeterdev <mpeterdev>
2022-12-15 23:07:02 -0500

    Update Genie by michaelpeter.near at 2022-12-15 23:07:02.653

--- michaelpeter.near/Genie/source_code.jsx
+++ michaelpeter.near/Genie/source_code.jsx
@@ -27,83 +27,6 @@ const Wrapper = styled.div`
   justify-content:center
   `;
 
-let CurrentContent;
-
-if (state.selectedQuestion) {
-  CurrentContent = (
-    <Wrapper className="d-flex flex-column gap-5">
-      <div>
-        <button className="btn btn-outline-secondary" onClick={clearSelected}>
-          <i class="bi bi-chevron-left" />
-          All Questions
-        </button>
-      </div>
-      <Center>
-        <QuestionWrapper>
-          <Widget
-            src="michaelpeter.near/widget/GenieQuestionAnswerView"
-            props={{ questionRef: state.selectedQuestion }}
-          />
-        </QuestionWrapper>
-      </Center>
-    </Wrapper>
-  );
-} else if (state.showAskForm) {
-  CurrentContent = (
-    <Wrapper>
-      <div className="d-flex flex-row justify-content-end">
-        <button
-          className="btn btn-light"
-          onClick={() => {
-            State.update({ showAskForm: false });
-          }}
-        >
-          <i class="bi bi-x-lg" />
-        </button>
-      </div>
-      <Widget
-        src="michaelpeter.near/widget/GenieSaveQuestion"
-        props={{ searchString: state.searchString, setSearchString }}
-      />
-    </Wrapper>
-  );
-} else {
-  CurrentContent = (
-    <Wrapper className="d-flex flex-column gap-3 pt-1">
-      <div className="d-flex align-items-center gap-3">
-        <i class="bi bi-search" />
-        <div class="input-group input-group-lg">
-          <input
-            type="text"
-            placeholder="Your wish is my command"
-            className="form-control input-group input-group-lg"
-            onChange={(e) => {
-              setSearchString?.(e.target.value);
-            }}
-          />
-        </div>
-        <button
-          className="btn btn-primary text-nowrap"
-          onClick={() => {
-            State.update({ showAskForm: true });
-          }}
-        >
-          <div>
-            <i class="bi bi-chat" />
-            Ask a question
-          </div>
-        </button>
-      </div>
-      <QuestionWrapper>
-        <Widget
-          src="michaelpeter.near/widget/GenieQuestionList"
-          props={{ searchString: state.searchString, setSelectedQuestion }}
-        />
-      </QuestionWrapper>
-    </Wrapper>
-  );
-}
-
 return (
   <div className="d-flex flex-column gap-1">
     <img
@@ -111,6 +34,75 @@ return (
       alt="Genie"
       style={{ width: "10rem", marginBottom: "2rem" }}
     />
-    {CurrentContent}
+    {state.selectedQuestion && (
+      <Wrapper className="d-flex flex-column gap-5">
+        <div>
+          <button className="btn btn-outline-secondary" onClick={clearSelected}>
+            <i class="bi bi-chevron-left" />
+            All Questions
+          </button>
+        </div>
+        <Center>
+          <QuestionWrapper>
+            <Widget
+              src="michaelpeter.near/widget/GenieQuestionAnswerView"
+              props={{ questionRef: state.selectedQuestion }}
+            />
+          </QuestionWrapper>
+        </Center>
+      </Wrapper>
+    )}
+    {state.showAskForm && (
+      <Wrapper>
+        <div className="d-flex flex-row justify-content-end">
+          <button
+            className="btn btn-light"
+            onClick={() => {
+              State.update({ showAskForm: false });
+            }}
+          >
+            <i class="bi bi-x-lg" />
+          </button>
+        </div>
+        <Widget
+          src="michaelpeter.near/widget/GenieSaveQuestion"
+          props={{ searchString: state.searchString, setSearchString }}
+        />
+      </Wrapper>
+    )}
+    {!state.showAskForm && !state.selectedQuestion && (
+      <Wrapper className="d-flex flex-column gap-3 pt-1">
+        <div className="d-flex align-items-center gap-3">
+          <i class="bi bi-search" />
+          <div class="input-group input-group-lg">
+            <input
+              type="text"
+              placeholder="Your wish is my command"
+              className="form-control input-group input-group-lg"
+              onChange={(e) => {
+                setSearchString?.(e.target.value);
+              }}
+            />
+          </div>
+          <button
+            className="btn btn-primary text-nowrap"
+            onClick={() => {
+              State.update({ showAskForm: true });
+            }}
+          >
+            <div>
+              <i class="bi bi-chat" />
+              Ask a question
+            </div>
+          </button>
+        </div>
+        <QuestionWrapper>
+          <Widget
+            src="michaelpeter.near/widget/GenieQuestionList"
+            props={{ searchString: state.searchString, setSelectedQuestion }}
+          />
+        </QuestionWrapper>
+      </Wrapper>
+    )}
   </div>
 );
