04ef6a6d62699205fb37b38be7146c44d7ad7257
ChaoticTempest <ChaoticTempest>
2022-12-15 23:15:31 -0500

    Update BookTile by chaotictempest.near at 2022-12-15 23:15:31.677

--- chaotictempest.near/BookTile/source_code.jsx
+++ chaotictempest.near/BookTile/source_code.jsx
@@ -5,13 +5,31 @@ const author = book.author ?? "No author";
 const cover = book.cover ?? "";
 const rating = book.rating ?? 0;
 const id = book.id ?? 0;
+const desc = book.desc ?? "No description";
 
+const bookdbContractId =
+  props.bookdbContractId ?? "v1bookdb.chaotictempest.near";
 const showAddToRead = props.showAddToRead ?? false;
 const showAddToWantToRead = props.showAddToWantToRead ?? false;
 const onCommitAdd =
   props.onCommitAdd ??
-  ((buttonName, id) => {
-    console.log(`Button ${buttonName} clicked with book-id=${id}`);
+  ((buttonName, bookId) => {
+    console.log(`Committing book_isbn=${bookId} via ${buttonName}`);
+
+    let bookEntry = Near.view(bookdbContractId, "get", {
+      isbn: bookId,
+    });
+    if (bookEntry) {
+      console.log(`${bookId} already indexed in bookdb`);
+      return;
+    }
+
+    Near.call(bookdbContractId, "add_book", {
+      isbn: id,
+      title: title,
+      author: author,
+      desc: desc,
+    });
   });
 
 // TODO: why do we need index here?
