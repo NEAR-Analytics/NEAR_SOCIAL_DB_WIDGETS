093fd1af5ef38e313ecda315863d763dfe7ba273
urbanite.near <urbanite.near>
2022-12-16 04:08:30 -0500

    Update ProfileLine by urbanite.near at 2022-12-16 04:08:30.507

---
+++ urbanite.near/ProfileLine/source_code.jsx
@@ -0,0 +1,54 @@
+const accountId = props.accountId ?? context.accountId;
+const link = props.link ?? true;
+const hideAccountId = props.hideAccountId;
+const hideName = props.hideName;
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+const name = profile.name ?? accountId;
+const title = props.title ?? `${name} @${accountId}`;
+const tooltip =
+  props.tooltip && (props.tooltip === true ? title : props.tooltip);
+
+let inner = (
+  <>
+    <Widget
+      src="mob.near/widget/ProfileImage"
+      props={{
+        style: { width: "1.5em", height: "1.5em" },
+        profile,
+        accountId,
+        className: "d-inline-block",
+        imageClassName: "rounded w-100 h-100 align-top",
+      }}
+    />
+    {!hideName && name}
+    {!hideAccountId && <span className="text-muted">@{accountId}</span>}
+  </>
+);
+
+inner = link ? (
+  <a
+    href={
+      link !== true
+        ? link
+        : `#/mob.near/widget/ProfilePage?accountId=${accountId}`
+    }
+    className="link-dark text-truncate"
+    target="_blank"
+  >
+    {inner}
+  </a>
+) : (
+  <span className="text-truncate">{inner}</span>
+);
+
+if (tooltip) {
+  inner = (
+    <OverlayTrigger placement="auto" overlay={<Tooltip>{tooltip}</Tooltip>}>
+      {inner}
+    </OverlayTrigger>
+  );
+}
+
+return inner;
