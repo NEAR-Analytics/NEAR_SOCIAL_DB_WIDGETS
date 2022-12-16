501942b31f1b589e7b535f2745725628446881ba
minz.near <minz.near>
2022-12-15 22:08:18 -0500

    Update AllActivities by minz.near at 2022-12-15 22:08:18.559

--- minz.near/AllActivities/source_code.jsx
+++ minz.near/AllActivities/source_code.jsx
@@ -8,15 +8,17 @@ if (!events) {
 }
 console.log(events);
 
+const AllWidgets = styled.div`
+display: flex;
+flex-wrap: wrap;
+
+& > * {
+ margin: 6px;
+}
+`;
+
 const allWidgets = events.map((event_id) => {
   console.log(event_id);
   return <Widget src={`minz.near/widget/ViewActivity`} props={{ event_id }} />;
 });
-
-// return (
-//   <Widget
-//     src={`minz.near/widget/ViewActivity`}
-//     props={{ event_id: "event_0" }}
-//   />
-// );
-return <>{allWidgets}</>;
+return <AllWidgets>{allWidgets}</AllWidgets>;
