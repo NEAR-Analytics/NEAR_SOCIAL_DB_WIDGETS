01a568a5120a2d01c8a30b842ef1b268f47486ca
mpeterdev <mpeterdev>
2022-12-15 20:18:34 -0500

    Update GenieQuestionList by michaelpeter.near at 2022-12-15 20:18:34.064

--- michaelpeter.near/GenieQuestionList/source_code.jsx
+++ michaelpeter.near/GenieQuestionList/source_code.jsx
@@ -8,22 +8,41 @@ return (
   <div className="d-flex flex-column gap-1">
     <div className="d-flex justify-content-end"></div>
     <div className="d-flex flex-column gap-3">
-      {questions.map((q) => (
-        <div key={q.value} className="d-flex flex-column gap-1 border p-2">
-          <Widget
-            src={"michaelpeter.near/widget/GenieQuestionView"}
-            props={{ questionRef: q.value, searchString: props.searchString }}
-          />
-          <Widget
-            src={"michaelpeter.near/widget/GenieAnswerList"}
-            props={{ questionRef: q.value }}
-          />
-          <Widget
-            src={"michaelpeter.near/widget/GenieAnswerSubmit"}
-            props={{ questionRef: q.value }}
-          />
-        </div>
-      ))}
+      {questions.map((q) => {
+        const asker = q.value.split("--")[0];
+        const question = Social.getr(
+          `${asker}/experimental/genie/questions/${q.value}`
+        );
+        // return nothing if data does not meet schema
+        if (!question?.title || !question?.content) {
+          return <></>;
+        }
+
+        // return nothing if question does not meet search criteria
+        if (
+          searchString &&
+          !question.title.includes(searchString) &&
+          !question.content.includes(searchString)
+        ) {
+          return <></>;
+        }
+        return (
+          <div key={q.value} className="d-flex flex-column gap-1 border p-2">
+            <Widget
+              src={"michaelpeter.near/widget/GenieQuestionView"}
+              props={{ questionRef: q.value, searchString: props.searchString }}
+            />
+            <Widget
+              src={"michaelpeter.near/widget/GenieAnswerList"}
+              props={{ questionRef: q.value }}
+            />
+            <Widget
+              src={"michaelpeter.near/widget/GenieAnswerSubmit"}
+              props={{ questionRef: q.value }}
+            />
+          </div>
+        );
+      })}
     </div>
   </div>
 );
