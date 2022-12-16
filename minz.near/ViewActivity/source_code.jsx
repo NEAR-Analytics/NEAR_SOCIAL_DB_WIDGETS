1ee0d6406c195cbbe64d6efb1a800a1327d25b49
minz.near <minz.near>
2022-12-15 22:17:34 -0500

    Update ViewActivity by minz.near at 2022-12-15 22:17:34.513

--- minz.near/ViewActivity/source_code.jsx
+++ minz.near/ViewActivity/source_code.jsx
@@ -1,45 +1,116 @@
-let event_id = props.event_id;
+const event_id = props.event_id;
+const accountId = context.accountId;
 const CONTRACT = "event_org.near";
 // let data = Social.getr(`${event_creator}/published_events/${event_id}`);
-let data = Near.view(CONTRACT, "get_event", { event_id: event_id });
-let participants = data.cur_participants;
+const data = Near.view(CONTRACT, "get_event", { event_id: event_id });
+const participants = data.cur_participants;
 
 const participantWidgets = participants.map((accountId) => {
   return (
     <>
-      <Widget src={`zavodil.near/widget/ProfileLine`} props={{ accountId }} />
-      is going!
+      <Widget src={`eugenethedream/widget/ProfileLine`} props={{ accountId }} />
     </>
   );
 });
 
+const EventImage = styled.img`
+  max-width: 400px;
+  max-height: 300px;
+  margin-bottom: 16px;
+  align-self: center;
+`;
+
+const Card = styled.div`
+display: flex;
+flex-direction: column;
+width: 400px;
+padding: 20px;
+border: 1px solid gray;
+`;
+
+const Title = styled.div`
+display: flex;
+align-items: baseline;
+`;
+
+const TitleText = styled.h5`
+flex: 1;
+`;
+
+const NumPersons = styled.div``;
+
+const Organizer = styled.div`
+font-size: 14px;
+`;
+
+const Description = styled.div`
+font-size: 14px;
+color: gray
+`;
+
+const Join = styled.div`
+margin-top: 16px;
+display: flex;
+align-items: center;
+flex-direction: column;
+`;
+
+const JoinBy = styled.div`
+font-style: italic;
+color: gray;
+font-size: 12px;
+`;
+
+const ParticipantCount = styled.div`
+margin-top: 8px;
+`;
+
+function callJoin() {
+  Near.call({
+    contractName: CONTRACT,
+    methodName: "join",
+    args: {
+      account_id: accountId,
+      event_id: event_id,
+    },
+    deposit: data.price,
+  });
+}
+
 return (
-  <div className="mb-2 card">
-    <div className="card-body">
-      <span className="text-truncate">
-        <h5>
-          <b>
-            {data.name || "Untitled"} ({data.min_num}-{data.max_num} people)
-          </b>
-        </h5>
-        Organized by
-        <Widget
-          src={`zavodil.near/widget/ProfileLine`}
-          props={{ accountId: data.owner }}
-        />
-        <br />
-        {data.description || "No description"}
-        <br />
-        Current participants: {data.participants}
-        <br />
-        <CommitButton>
-          Join for <b>{data.price / 1e24} NEAR</b>
-        </CommitButton>{" "}
-        before {Date(data.deadline)}
-        <br />
-        <hr></hr>
-        {participantWidgets}
-      </span>
-    </div>
-  </div>
+  <Card>
+    {data.image_link && (
+      <EventImage src={`https://ipfs.near.social/ipfs/${data.image_link}`} />
+    )}
+    <Title>
+      <TitleText>{data.title || "Untitled"}</TitleText>
+      <NumPersons>
+        {data.min_num} - {data.max_num} people
+      </NumPersons>
+    </Title>
+    <Organizer>
+      Organized by
+      <Widget
+        src={`eugenethedream/widget/ProfileLine`}
+        props={{ accountId: data.owner }}
+      />
+    </Organizer>
+    {data.description && <Description>{data.description}</Description>}
+    <ParticipantCount>
+      {participants.length > 0
+        ? participants.length +
+          (participants.length > 1 ? " participants:" : " participant:")
+        : "Join to be the first participant!"}
+    </ParticipantCount>
+    {participantWidgets}
+    <Join>
+      <button onClick={callJoin}>
+        Join for <b>{data.price / 1e24} NEAR</b>
+      </button>{" "}
+      <JoinBy>
+        Join by{" "}
+        {new Date(data.deadline / 1000000).toISOString().substring(0, 10)}
+      </JoinBy>
+    </Join>
+  </Card>
 );
