003a7b48dacd6469d7716384c870ff5d1b50cbb0
mpeterdev <mpeterdev>
2022-12-15 21:26:52 -0500

    Update GenieAnswerList by michaelpeter.near at 2022-12-15 21:26:52.620

--- michaelpeter.near/GenieAnswerList/source_code.jsx
+++ michaelpeter.near/GenieAnswerList/source_code.jsx
@@ -10,22 +10,28 @@ const AnswerBox = styled.div`
   border-top: 0.5px solid #D3D3D3;
   padding-top: 2rem;
   padding-bottom: 2rem;
-  margin-left: 2rem;
-  margin-right: 2rem;
 `;
 
 return (
   <div className="d-flex flex-row justify-content-center">
-    <div className="d-flex flex-column gap-3" style={{ width: "43rem" }}>
+    <div
+      className="d-flex flex-column gap-3"
+      style={{ width: "43rem", marginLeft: "2rem", marginRight: "2rem" }}
+    >
       {answers?.length ? (
-        answers.map((a) => (
-          <AnswerBox>
-            <Widget
-              src="michaelpeter.near/widget/GenieAnswerView"
-              props={{ questionRef, answeredBy: a.accountId }}
-            />
-          </AnswerBox>
-        ))
+        <div className="d-flex flex-column gap-3">
+          <h3>
+            {answers.length} Answer{answers.length > 1 ? "s" : ""}
+          </h3>
+          {answers.map((a) => (
+            <AnswerBox>
+              <Widget
+                src="michaelpeter.near/widget/GenieAnswerView"
+                props={{ questionRef, answeredBy: a.accountId }}
+              />
+            </AnswerBox>
+          ))}
+        </div>
       ) : (
         <div className="alert alert-light">No answers, be the first!</div>
       )}
