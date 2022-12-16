7121404bebe5ec6f4e3ff3c2b0d29b262bd80478
jakmeier <jakmeier>
2022-12-16 13:10:48 -0500

    Update CanvasParty by jakmeier.near at 2022-12-16 13:10:48.057

--- jakmeier.near/CanvasParty/source_code.jsx
+++ jakmeier.near/CanvasParty/source_code.jsx
@@ -147,7 +147,7 @@ const startGameScreen = (otherPlayer, activePlayer) => {
               otherPlayer,
               activePlayer,
               start: Date.now(),
-              word: words[0], //words[Date.now() % 100],
+              word: words[Date.now() % 100],
             },
           },
         }}
@@ -238,7 +238,6 @@ const playing = (mySession, theirSession) => {
 };
 
 const words = [
-  "Flower",
   "Airplane",
   "Apple",
   "Arrow",
