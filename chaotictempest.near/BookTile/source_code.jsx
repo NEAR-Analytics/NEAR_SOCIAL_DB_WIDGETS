076d3c041d9843184ae41fa41783bd7a4f7a93a7
ChaoticTempest <ChaoticTempest>
2022-12-15 22:36:39 -0500

    Update BookTile by chaotictempest.near at 2022-12-15 22:36:39.945

--- chaotictempest.near/BookTile/source_code.jsx
+++ chaotictempest.near/BookTile/source_code.jsx
@@ -3,18 +3,88 @@ const book = props.book ?? {};
 const title = book.title ?? "No title";
 const author = book.author ?? "No author";
 const cover = book.cover ?? "";
+const rating = book.rating ?? 0;
 const id = book.id ?? 0;
 
-const addName = book.addName ?? "Add Book";
-var onAdd = book.onAdd;
-if (props.debug) {
-  onAdd = (id) => {
-    console.log(`onAdd not set: trying to add book with id=${id}`);
-  };
-}
+const showAddToRead = props.showAddToRead ?? false;
+const showAddToWantToRead = props.showAddToWantToRead ?? false;
+const onClickAdd =
+  props.onClickAdd ??
+  ((buttonName, id) => {
+    console.log(`Button ${buttonName} clicked with book-id=${id}`);
+  });
+
+// TODO: why do we need index here?
+const dataForRead = {
+  books: { read: { [id]: book } },
+  index: {
+    books: JSON.stringify({
+      key: "read",
+      value: {
+        book,
+      },
+    }),
+  },
+};
+
+const datarForToRead = {
+  books: { toRead: { [id]: book } },
+  index: {
+    books: JSON.stringify({
+      key: "toRead",
+      value: {
+        book,
+      },
+    }),
+  },
+};
+
+const BookTyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: left;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  width: "100%";
+  height: "100%";
+`;
+
+const BookTitle = styled.p`
+  margin: 10px;
+  font-size: 15px;
+  font-weight: bold;
+  text-align: left;
+`;
+
+const BookAuthors = styled.p`
+  margin: 10px;
+  font-size: 14px;
+  text-align: left;
+`;
+
+const BookRating = styled.p`
+  margin: 10px;
+  font-size: 14px;
+  font-weight: bold;
+  color: #3c763d;
+`;
+
+const ButtonsBox = styled.p`
+  margin: 10px;  
+`;
+
+const InfoContaier = styled.p`
+  height: '100%';
+  display: 'flex';
+  flexDirection: 'column';
+  alignItems: 'stretch',
+  justifyContent: 'space-between';
+`;
 
 return (
-  <div className="profile d-inline-block">
+  <BookTyle>
     <a
       href={`#/serhii.near/widget/BookPage?id=${id}`}
       className="text-decoration-none link-dark"
@@ -23,29 +93,39 @@ return (
         src="serhii.near/widget/BookCover"
         props={{
           book,
-          className: "float-start d-inline-block me-2",
         }}
       />
-      <div>
-        <p>{title}</p>
-        <p>{author}</p>
-      </div>
     </a>
-
-    {onAdd && (
-      <button
-        className="nav-link active"
-        id="pills-bio-tab"
-        data-bs-toggle="pill"
-        data-bs-target="#pills-bio"
-        type="button"
-        role="tab"
-        aria-controls="pills-bio"
-        aria-selected="true"
-        onClick={() => onAdd(id)}
+    <InfoContaier>
+      <a
+        href={`#/serhii.near/widget/BookPage?id=${id}`}
+        className="text-decoration-none link-dark"
       >
-        {addName}
-      </button>
-    )}
-  </div>
+        <div>
+          <BookTitle className="BookTitle">{title}</BookTitle>
+          <BookAuthors className="BookAuthors">{author}</BookAuthors>
+          <BookRating className="BookRating">{rating}</BookRating>
+        </div>
+      </a>
+
+      <ButtonsBox>
+        {showAddToRead && (
+          <CommitButton
+            data={dataForRead}
+            onClick={() => onClickAdd("AddToRead", id)}
+          >
+            Add to Read
+          </CommitButton>
+        )}
+        {showAddToWantToRead && (
+          <CommitButton
+            data={datarForToRead}
+            onClick={() => onClickAdd("WantToRead", id)}
+          >
+            Want To Read
+          </CommitButton>
+        )}
+      </ButtonsBox>
+    </InfoContaier>
+  </BookTyle>
 );
