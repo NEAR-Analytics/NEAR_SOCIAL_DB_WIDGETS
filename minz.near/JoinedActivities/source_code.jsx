87bf02e221cfeb6a97745e196e34ae9c9a57fb4b
minz.near <minz.near>
2022-12-15 22:50:26 -0500

    Update JoinedActivities by minz.near at 2022-12-15 22:50:26.178

---
+++ minz.near/JoinedActivities/source_code.jsx
@@ -0,0 +1,30 @@
+const accountId = props.accountId;
+if (!accountId) {
+  return "No accountId";
+}
+
+const CONTRACT = "event_org.near";
+
+let events = Near.view(CONTRACT, "get_events_by_participants", {
+  account_id: accountId,
+});
+
+if (!events) {
+  return "Loading";
+}
+
+const AllWidgets = styled.div`
+display: flex;
+flex-wrap: wrap;
+align-items: flex-start;
+
+& > * {
+ margin: 6px;
+}
+`;
+
+const allWidgets = events.map((event_id) => {
+  console.log(event_id);
+  return <Widget src={`minz.near/widget/ViewActivity`} props={{ event_id }} />;
+});
+return <AllWidgets>{allWidgets}</AllWidgets>;
