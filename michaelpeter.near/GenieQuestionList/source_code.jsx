1107e12a9751321ab3557b7d80a8b42cae05a97c
mpeterdev <mpeterdev>
2022-12-15 20:08:02 -0500

    Update GenieQuestionList by michaelpeter.near at 2022-12-15 20:08:02.114

--- michaelpeter.near/GenieQuestionList/source_code.jsx
+++ michaelpeter.near/GenieQuestionList/source_code.jsx
@@ -4,23 +4,15 @@ if (!questions?.length) {
   return <p>No Results</p>;
 }
 
-const { searchString } = props;
-if (searchString) {
-  questions = questions.filter((q) =>
-    q.title?.includes(searchString || q.content?.includes(searchString))
-  );
-}
-
 return (
   <div className="d-flex flex-column gap-1">
     <div className="d-flex justify-content-end"></div>
     <div className="d-flex flex-column gap-3">
-      <div>SearchString:{JSON.stringify(searchString)}</div>
       {questions.map((q) => (
         <div key={q.value} className="d-flex flex-column gap-1 border p-2">
           <Widget
             src={"michaelpeter.near/widget/GenieQuestionView"}
-            props={{ questionRef: q.value }}
+            props={{ questionRef: q.value, searchString: props.searchString }}
           />
           <Widget
             src={"michaelpeter.near/widget/GenieAnswerList"}
