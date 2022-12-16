081f774b2cfc0c10a69ae9503e1ade507f18c3db
minz.near <minz.near>
2022-12-15 19:47:46 -0500

    Update ViewActivity by minz.near at 2022-12-15 19:47:46.489

---
+++ minz.near/ViewActivity/source_code.jsx
@@ -0,0 +1,44 @@
+// let event_id = props.event_id;
+let event_id = "event_2";
+const CONTRACT = "event_org.near";
+// let data = Social.getr(`${event_creator}/published_events/${event_id}`);
+let data = Near.view(CONTRACT, "get_event", { event_id: event_id });
+let participants = data.cur_participants;
+
+const participantWidgets = participants.map((accountId) => {
+  return (
+    <>
+      <Widget src={`zavodil.near/widget/ProfileLine`} props={{ accountId }} />
+      is going!
+    </>
+  );
+});
+
+return (
+  <div className="mb-2 card">
+    <div className="card-body">
+      <span className="text-truncate">
+        <h5>
+          <b>
+            {data.name} ({data.min_num}-{data.max_num} people)
+          </b>
+        </h5>
+        Organized by
+        <Widget
+          src={`zavodil.near/widget/ProfileLine`}
+          props={{ event_creator }}
+        />
+        <br />
+        {data.description}
+        <br />
+        <CommitButton>
+          Join for <b>{data.price / 1e24} NEAR</b>
+        </CommitButton>{" "}
+        before {Date(data.deadline)}
+        <br />
+        <hr></hr>
+        {participantWidgets}
+      </span>
+    </div>
+  </div>
+);
