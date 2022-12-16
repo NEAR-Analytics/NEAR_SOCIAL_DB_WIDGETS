a55f964b399692f038e622922aaef17419dae789
minz.near <minz.near>
2022-12-15 23:14:13 -0500

    Update JoinedActivities by minz.near at 2022-12-15 23:14:13.073

--- minz.near/JoinedActivities/source_code.jsx
+++ minz.near/JoinedActivities/source_code.jsx
@@ -27,4 +27,11 @@ const allWidgets = events.map((event_id) => {
   console.log(event_id);
   return <Widget src={`minz.near/widget/ViewActivity`} props={{ event_id }} />;
 });
-return <AllWidgets>{allWidgets}</AllWidgets>;
+return (
+  <div>
+    <AllWidgets>{allWidgets}</AllWidgets>
+    <div>
+      <Widget src={`minz.near/widget/ActivitiesHomePageButton`} />;
+    </div>
+  </div>
+);
