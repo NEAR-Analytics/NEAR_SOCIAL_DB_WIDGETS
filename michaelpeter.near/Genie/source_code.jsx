52f22f54cbc56e86acb27f7233fb3bda336f99a7
mpeterdev <mpeterdev>
2022-12-15 21:10:17 -0500

    Update Genie by michaelpeter.near at 2022-12-15 21:10:17.074

--- michaelpeter.near/Genie/source_code.jsx
+++ michaelpeter.near/Genie/source_code.jsx
@@ -1,7 +1,7 @@
-State.init({ selectedQuestion: "", searchString: "" });
+State.init({ selectedQuestion: "", searchString: "", showAskForm: false });
 
 const setSearchString = (updatedSearchString) => {
-  State.update({ searchString: updatedSearchString });
+  State.update({ searchString: updatedSearchString.toLowerCase() });
 };
 
 const clearSelected = () => {
@@ -21,24 +21,7 @@ const Center = styled.div`
   justify-content:center;
 `;
 
-if (!state.selectedQuestion) {
-  return (
-    <div className="d-flex flex-column gap-3">
-      <Widget
-        src="michaelpeter.near/widget/GenieSaveQuestion"
-        props={{ searchString: state.searchString, setSearchString }}
-      />
-      <Center>
-        <QuestionWrapper>
-          <Widget
-            src="michaelpeter.near/widget/GenieQuestionList"
-            props={{ searchString: state.searchString, setSelectedQuestion }}
-          />
-        </QuestionWrapper>
-      </Center>
-    </div>
-  );
-} else {
+if (state.selectedQuestion) {
   return (
     <div className="d-flex flex-column gap-1">
       <div>
@@ -49,6 +32,63 @@ if (!state.selectedQuestion) {
           <Widget
             src="michaelpeter.near/widget/GenieQuestionAnswerView"
             props={{ questionRef: state.selectedQuestion }}
+          />
+        </QuestionWrapper>
+      </Center>
+    </div>
+  );
+} else if (state.showAskForm) {
+  return (
+    <>
+      <div className="d-flex flex-row justify-content-end">
+        <button
+          className="btn btn-light"
+          onClick={() => {
+            State.update({ showAskForm: false });
+          }}
+        >
+          <i class="bi bi-x-lg" />
+        </button>
+      </div>
+      <Widget
+        src="michaelpeter.near/widget/GenieSaveQuestion"
+        props={{ searchString: state.searchString, setSearchString }}
+      />
+    </>
+  );
+} else {
+  return (
+    <div className="d-flex flex-column gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <i class="bi bi-search" />
+        <div class="input-group input-group-lg">
+          <input
+            type="text"
+            placeholder="Your wish is my command"
+            className="form-control input-group input-group-lg"
+            value={props.searchString}
+            onChange={(e) => {
+              setSearchString?.(e.target.value);
+            }}
+          />
+        </div>
+        <button
+          className="btn btn-primary text-nowrap"
+          onClick={() => {
+            State.update({ showAskForm: true });
+          }}
+        >
+          <div>
+            <i class="bi bi-chat" />
+            Ask a question
+          </div>
+        </button>
+      </div>
+      <Center>
+        <QuestionWrapper>
+          <Widget
+            src="michaelpeter.near/widget/GenieQuestionList"
+            props={{ searchString: state.searchString, setSelectedQuestion }}
           />
         </QuestionWrapper>
       </Center>
