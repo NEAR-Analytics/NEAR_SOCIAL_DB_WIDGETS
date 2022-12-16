53c3819da1704652e74bcb205ab7a1127732362a
Longarithm <Longarithm>
2022-12-15 21:37:33 -0500

    Update inodes by logunov.near at 2022-12-15 21:37:33.420

--- logunov.near/inodes/source_code.jsx
+++ logunov.near/inodes/source_code.jsx
@@ -4,54 +4,131 @@ if (!accountId) {
   return "Please sign in with NEAR wallet";
 }
 
-const data = Social.get(`${accountId}/scribbles/inode/0`, "final");
+const data = Social.get(`${accountId}/scribbles/inode/0/type`, "final");
+
+// File:
+// {
+//     type: 0,
+//     data: "..."
+// }
+
+// Folder:
+// {
+//     type: 1,
+//     entries: {}
+// }
 
 if (!data) {
-  return "Initialize storage";
-}
-
-const widgets = data ? Object.entries(data) : [];
-const wrappedWidgets = [];
-const maxIndex = -1;
-for (let i = 0; i < widgets.length; ++i) {
-  const index = widgets[i][0];
-  maxIndex = Math.max(maxIndex, index);
-  const src = widgets[i][1];
-  wrappedWidgets.push(
-    <tr border="1">
-      <td>{index}</td>
-      <td>
-        <a href={`#/${src}`}>
-          <i>{src}</i>
-        </a>
-      </td>
-    </tr>
+  // Ask to initialize storage
+  return (
+    <div>
+      <CommitButton
+        data={{
+          scribbles: {
+            inode: {
+              [0]: {
+                type: 1,
+                entries: {},
+              },
+            },
+            next_inode: 1,
+          },
+        }}
+      >
+        Initialize storage
+      </CommitButton>
+    </div>
   );
 }
 
-State.init({ file: null });
+const next_inode = parseInt(
+  Social.get(`${accountId}/scribbles/next_inode`, "final")
+);
 
-function readURL(input) {
-  //   var reader = new FileReader();
-  console.log(globalThis);
-  console.log(input);
-  //   reader.onload = function (e) {
-  //     $("#blah").attr("src", e.target.result).width(150).height(200);
-  //   };
-  console.log("Hey!");
-  //   reader.readAsDataURL(input.files[0]);
-}
+State.init({
+  fs: {
+    root: 0,
+    next_inode: next_inode,
+    new_filename: "",
+    new_data: "",
+  },
+});
 
 return (
   <div>
-    {console.log(readURL)}
-    <div>{JSON.stringify(state.file)}</div>
-    <input
-      type="file"
-      onChange={(e) => readURL(e)}
-      //   onChange={(e) => State.update({ file: e.target.value })}
-    />
-    <img id="blah" src="#" alt="your image" />
-    <input type="submit" value="Submit" />
+    fs = {JSON.stringify(state.fs)}
+    <div className="mb-2">
+      <h4>Set root</h4>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="0"
+        onChange={(e) =>
+          State.update({
+            fs: { ...state.fs, root: parseInt(e.target.value) },
+          })
+        }
+      />
+    </div>
+    <div className="mb-2">
+      <h4>Add file</h4>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="filename"
+        onChange={(e) =>
+          State.update({
+            new_filename: e.target.value,
+          })
+        }
+      />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="data"
+        onChange={(e) =>
+          State.update({
+            new_data: e.target.value,
+          })
+        }
+      />
+    </div>
+    <CommitButton
+      data={{
+        scribbles: {
+          inode: {
+            [state.fs.root]: {
+              entries: {
+                [state.fs.new_filename]: state.fs.next_inode,
+              },
+            },
+            [state.fs.next_inode]: {
+              type: 0,
+              data: state.fs.new_data,
+            },
+          },
+          next_inode: state.fs.next_inode + 1,
+        },
+      }}
+      onClick={(e) =>
+        State.update({
+          fs: { ...state.fs, next_inode: state.fs.next_inode + 1 },
+        })
+      }
+    >
+      Add file
+    </CommitButton>
+    <div className="mb-2">
+      <h4>Remove widget by index</h4>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="0"
+        onChange={(e) => State.update({ remove_index: e.target.value })}
+      />
+    </div>
+    <CommitButton data={{ graph: { widget: { [state.remove_index]: null } } }}>
+      Remove
+    </CommitButton>
   </div>
 );
