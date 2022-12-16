e44eef373158087a7cb0cdcc438b2afc67f3f49b
jeffsatori.near <jeffsatori.near>
2022-12-16 14:41:39 -0500

    Update ThoughtOfTheDay by jeffsatori.near at 2022-12-16 14:41:39.554

---
+++ jeffsatori.near/ThoughtOfTheDay/source_code.jsx
@@ -0,0 +1,82 @@
+State.init({
+  input: "",
+});
+
+const card = {
+  background: "linear-gradient(to right, #72f786, #33ffff)",
+  border: "1px solid black",
+  borderRadius: "5px",
+  textAlign: "center",
+};
+
+const button = {
+  borderRadius: "5px",
+  margin: "5px 0",
+  padding: "8px",
+  textAlign: "center",
+  backgroundColor: "rgb(65, 136, 250)",
+  border: "2px solid black",
+  fontWeight: "bold",
+};
+
+const accountId = props.accountId ?? "*";
+
+const data = Social.index("ThoughtOfTheDay", "answer");
+if (!data) {
+  return "Loading";
+}
+const sortedData = data.sort((d1, d2) => d2.blockHeight - d1.blockHeight);
+
+return (
+  <div>
+    <h2>Share your #ThoughtOfTheDay</h2>
+    <div className="d-flex flex-column w-75 justify-content-around">
+      <textarea
+        style={{
+          backgroundColor: "rgb(235, 255, 238)",
+          border: "1px solid #ced4da",
+          borderRadius: "0.5rem",
+        }}
+        rows="5"
+        value={state.input}
+        onChange={(e) => {
+          State.update({ input: e.target.value });
+        }}
+      />
+    </div>
+    <CommitButton
+      style={button}
+      data={{
+        index: {
+          i_buidl: JSON.stringify(
+            {
+              key: "answer",
+              value: {
+                answer: state.input,
+              },
+            },
+            undefined,
+            0
+          ),
+        },
+      }}
+      onCommit={() => {
+        State.update({
+          reloadData: true,
+        });
+      }}
+    >
+      Post a Thought!
+    </CommitButton>
+
+    <div>
+      {sortedData
+        ? sortedData.map((d) => (
+            <div style={card}>
+              {d.accountId} said <b>{d.value.answer}</b>
+            </div>
+          ))
+        : "Loading..."}
+    </div>
+  </div>
+);
