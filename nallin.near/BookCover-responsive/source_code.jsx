2597a5e2df6931eb08d5855b213c201e65c9b131
nallin.near <nallin.near>
2022-12-16 00:55:27 -0500

    Update BookCover-responsive by nallin.near at 2022-12-16 00:55:27.044

---
+++ nallin.near/BookCover-responsive/source_code.jsx
@@ -0,0 +1,37 @@
+const className =
+  props.className ?? "profile-image d-flex justify-content-center";
+const style = props.style ?? {
+  width: "auto",
+  height: "100%",
+};
+const imageClassName =
+  props.imageClassName ?? "rounded w-auto h-100 object-fit-cover";
+const thumbnail = props.thumbnail ?? "thumbnail";
+
+const book = props.book ?? {};
+const title = props.title ?? "Not provided";
+const cover = book.cover;
+
+const inner = (
+  <div className={className} style={style}>
+    <Widget
+      src="mob.near/widget/Image"
+      props={{
+        image: cover,
+        alt: title,
+        className: imageClassName,
+        thumbnail,
+        fallbackUrl:
+          "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm",
+      }}
+    />
+  </div>
+);
+
+return tooltip ? (
+  <OverlayTrigger placement="auto" overlay={<Tooltip>{title}</Tooltip>}>
+    {inner}
+  </OverlayTrigger>
+) : (
+  inner
+);
