2b2a85d7e99a77486e4b9380094e39df9823731e
mpeterdev <mpeterdev>
2022-12-15 19:53:08 -0500

    Update Genie by michaelpeter.near at 2022-12-15 19:53:08.620

--- michaelpeter.near/Genie/source_code.jsx
+++ michaelpeter.near/Genie/source_code.jsx
@@ -1,9 +1,19 @@
-State.init({ selectedQuestion: "" });
+State.init({ selectedQuestion: "", searchString: "" });
+
+const setSearchString = (updatedSearchString) => {
+  State.update({ searchString: updatedSearchString });
+};
 
 if (!selectedQuestion)
   return (
     <div className="d-flex flex-column gap-3">
-      <Widget src="michaelpeter.near/widget/GenieSaveQuestion" />
-      <Widget src="michaelpeter.near/widget/GenieQuestionList" />
+      <Widget
+        src="michaelpeter.near/widget/GenieSaveQuestion"
+        props={{ searchString, setSearchString }}
+      />
+      <Widget
+        src="michaelpeter.near/widget/GenieQuestionList"
+        props={{ searchString }}
+      />
     </div>
   );
