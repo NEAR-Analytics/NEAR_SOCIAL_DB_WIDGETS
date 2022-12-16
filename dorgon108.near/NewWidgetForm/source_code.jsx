8a5d85dae04ee2c346b7ae49f2358ea6993fede5
dorgon108.near <dorgon108.near>
2022-12-16 05:08:56 -0500

    Update NewWidgetForm by dorgon108.near at 2022-12-16 05:08:56.854

--- dorgon108.near/NewWidgetForm/source_code.jsx
+++ dorgon108.near/NewWidgetForm/source_code.jsx
@@ -1,3 +1,20 @@
+State.init({
+  titleVal: "",
+  urlVal: "",
+  ownerVal: "",
+});
+
+// Set Contract Name
+const cName = "5star.dorgon108.near";
+
+const sendToBlockChain = () => {
+  Near.call(cName, "addNewWidget", {
+    title: state.titleVal,
+    url: state.urlVal,
+    owner: state.ownerVal,
+  });
+};
+
 return (
   <div style={{ backgroundColor: "#A8A8A8", padding: "3vw" }}>
     <div style={{ margin: "10px" }}>
@@ -9,6 +26,9 @@ return (
         type="text"
         name="Comment"
         placeholder="Enter Title Here"
+        onChange={(e) => {
+          State.update({ titleVal: e.target.value });
+        }}
       />
     </div>
     <div style={{ margin: "10px" }}>
@@ -20,6 +40,9 @@ return (
         type="text"
         name="Comment"
         placeholder="Enter Url Here"
+        onChange={(e) => {
+          State.update({ urlVal: e.target.value });
+        }}
       />
     </div>
     <div style={{ margin: "10px" }}>
@@ -32,12 +55,16 @@ return (
         type="text"
         name="Owner"
         placeholder="Enter Owner Here"
+        onChange={(e) => {
+          State.update({ ownerVal: e.target.value });
+        }}
       />
       <div>
         <input
           style={{ borderRadius: "10px", margin: "10px" }}
           type="submit"
           value="Submit"
+          onClick={sendToBlockChain}
         />
       </div>
     </div>
