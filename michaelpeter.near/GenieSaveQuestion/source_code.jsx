fe88f7eb34b564b4de876d3a80a74f570eaa386f
mpeterdev <mpeterdev>
2022-12-15 19:38:34 -0500

    Update GenieSaveQuestion by michaelpeter.near at 2022-12-15 19:38:34.556

--- michaelpeter.near/GenieSaveQuestion/source_code.jsx
+++ michaelpeter.near/GenieSaveQuestion/source_code.jsx
@@ -1,4 +1,9 @@
-State.init({ questionContent: "", timestamp: Date.now() });
+State.init({
+  questionTitle: "",
+  questionContent: "",
+  timestamp: Date.now(),
+  showAskForm: false,
+});
 
 if (!context.accountId) {
   return <p>Please log in to ask a question</p>;
@@ -6,46 +11,76 @@ if (!context.accountId) {
 
 const questionRef = `${context.accountId}--${Date.now()}`;
 return (
-  <div className="d-flex align-items-center gap-3">
-    <i class="bi bi-search" />
-    <div class="input-group input-group-lg">
-      <input
-        type="text"
-        placeholder="Your wish is my command"
-        className="form-control input-group input-group-lg"
-        value={state.questionContent}
-        onChange={(e) => {
-          State.update({ questionContent: e.target.value });
-        }}
-      />
-    </div>
-    <div>
-      <CommitButton
-        className="btn btn-primary"
-        onCommit={() => {
-          State.update({ questionContent: "" });
-        }}
+  <div className="d-flex flex-column">
+    <div className="d-flex align-items-center gap-3">
+      <i class="bi bi-search" />
+      <div class="input-group input-group-lg">
+        <input
+          type="text"
+          placeholder="Your wish is my command"
+          className="form-control input-group input-group-lg"
+          value={state.questionContent}
+        />
+      </div>
+      <button
+        className="btn btn-primary text-nowrap"
         onClick={() => {
-          State.update({ timestamp: Date.now() });
-        }}
-        data={{
-          experimental: {
-            genie: {
-              questions: {
-                [questionRef]: state.questionContent,
-              },
-            },
-          },
-          index: {
-            genie: JSON.stringify({
-              key: "asked",
-              value: questionRef,
-            }),
-          },
+          State.update({ showAskForm: true });
         }}
       >
-        Ask question
-      </CommitButton>
+        <div>
+          <i class="bi bi-chat" />
+          Ask a question
+        </div>
+      </button>
     </div>
+    {state.showAskForm && (
+      <div className="pt-5 d-flex flex-column gap-2">
+        Title of your question:
+        <input
+          type="text"
+          className="form-control"
+          value={state.questionTitle}
+          onChange={(e) => {
+            State.update({ questionTitle: e.target.value });
+          }}
+        />
+        Content of your question:
+        <input
+          type="text"
+          className="form-control"
+          value={state.questionContent}
+          onChange={(e) => {
+            State.update({ questionContent: e.target.value });
+          }}
+        />
+        <CommitButton
+          className="btn btn-primary"
+          onCommit={() => {
+            State.update({ questionContent: "", showAskForm: false });
+          }}
+          onClick={() => {
+            State.update({ timestamp: Date.now() });
+          }}
+          data={{
+            experimental: {
+              genie: {
+                questions: {
+                  [questionRef]: state.questionContent,
+                },
+              },
+            },
+            index: {
+              genie: JSON.stringify({
+                key: "asked",
+                value: questionRef,
+              }),
+            },
+          }}
+        >
+          Ask question
+        </CommitButton>
+      </div>
+    )}
   </div>
 );
