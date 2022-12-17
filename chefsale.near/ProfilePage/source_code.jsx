9598b888034c1b867d6621153e4d62ba62a9459f
chefsale <chefsale>
2022-12-17 00:13:34 -0500

    Update ProfilePage by chefsale.near at 2022-12-17 00:13:34.461

---
+++ chefsale.near/ProfilePage/source_code.jsx
@@ -0,0 +1,35 @@
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+if (profile === null) {
+  return "Loading";
+}
+
+return (
+  <div className="py-1 px-1">
+    <div className="mx-auto">
+      <Widget
+        src="mob.near/widget/ProfileLarge"
+        props={{
+          accountId,
+          profile,
+          link: true,
+          showEditButton: !props.profile,
+        }}
+      />
+
+      <div className="mt-3">
+        <Widget
+          src="mob.near/widget/ProfileTabs"
+          props={{ accountId, profile }}
+        />
+      </div>
+    </div>
+
+    <Widget src="blaze.near/widget/BlogFeed" props={{}} />
+  </div>
+);
