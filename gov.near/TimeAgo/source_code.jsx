7421a1cba7ab228fdf516493f4861b97670695eb
gov.near <gov.near>
2022-12-16 20:05:03 -0500

    Update TimeAgo by gov.near at 2022-12-16 20:05:03.142

---
+++ gov.near/TimeAgo/source_code.jsx
@@ -0,0 +1,45 @@
+if (props.now) {
+  return "now";
+}
+
+const keyPath = props.keyPath;
+let blockHeight = props.blockHeight ? parseInt(props.blockHeight) : undefined;
+
+if (blockHeight === undefined && keyPath) {
+  blockHeight = Social.keys(keyPath, undefined, {
+    return_type: "BlockHeight",
+  });
+  if (blockHeight === null) {
+    return "Loading";
+  }
+  keyPath.split("/").forEach((key) => {
+    blockHeight = blockHeight[key];
+  });
+}
+
+if (!blockHeight) {
+  return "unknown";
+}
+
+const block = Near.block(blockHeight);
+
+if (block === null) {
+  return "Loading";
+}
+
+if (!block) {
+  return "unknown";
+}
+
+const timeMs = parseFloat(block.header.timestamp_nanosec) / 1e6;
+
+const timeAgo = (diffSec) =>
+  diffSec < 60000
+    ? `${(diffSec / 1000) | 0} seconds ago`
+    : diffSec < 3600000
+    ? `${(diffSec / 60000) | 0} minutes ago`
+    : diffSec < 86400000
+    ? `${(diffSec / 3600000) | 0} hours ago`
+    : `${(diffSec / 86400000) | 0} days ago`;
+
+return timeAgo(Date.now() - timeMs);
