b2da167b592f22fce2ce2a2a0a2f47c42e7654ce
gov.near <gov.near>
2022-12-16 20:57:33 -0500

    Update ProjectPage by gov.near at 2022-12-16 20:57:33.108

--- gov.near/ProjectPage/source_code.jsx
+++ gov.near/ProjectPage/source_code.jsx
@@ -3,9 +3,9 @@ if (!accountId) {
   return "No account ID";
 }
 
-const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+const project = props.project ?? Social.getr(`${accountId}/project`);
 
-if (profile === null) {
+if (project === null) {
   return "Loading";
 }
 
@@ -16,16 +16,16 @@ return (
         src="gov.near/widget/ProjectCard"
         props={{
           accountId,
-          profile,
+          project,
           link: true,
-          showEditButton: !props.profile,
+          showEditButton: !props.project,
         }}
       />
 
       <div className="mt-3">
         <Widget
           src="gov.near/widget/ProjectTabs"
-          props={{ accountId, profile }}
+          props={{ accountId, project }}
         />
       </div>
     </div>
