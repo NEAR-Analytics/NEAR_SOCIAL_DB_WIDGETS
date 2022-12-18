16ee3ebe1f10e2333243642f92e77a9b75d4ea1b
zavodil <zavodil>
2022-12-18 00:45:47 -0500

    Update owned-social-avatars by zavodil.near at 2022-12-18 00:45:47.237

---
+++ zavodil.near/owned-social-avatars/source_code.jsx
@@ -0,0 +1,115 @@
+let appName = "avtr";
+let ownerId = "zavodil.near";
+let userId = context.accountId;
+let contractId = "avtr.near";
+
+const owned_tokens = Near.view(contractId, "nft_tokens_for_owner", {
+  account_id: userId,
+});
+
+console.log(owned_tokens);
+
+if (owned_tokens == undefined) {
+  return "";
+}
+
+if (!owned_tokens.length) {
+  return "Loading";
+}
+
+let tokenImg = styled.img`
+width: 100px;
+height: 100px;
+    `;
+
+let previews = [];
+let modals = [];
+
+owned_tokens.map((token) => {
+  let price = 0;
+  try {
+    price = new Big(JSON.parse(token.metadata.extra).mint_price)
+      .div(new Big(10).pow(24))
+      .toFixed(0);
+  } catch (e) {
+    console.log(e);
+  }
+
+  previews.push(
+    <tokenImg
+      src={token.metadata.media}
+      class="float-start"
+      type="button"
+      data-bs-toggle="modal"
+      data-bs-target={`#token-${token.token_id}`}
+    />
+  );
+
+  modals.push(
+    <div
+      class="modal fade"
+      id={`token-${token.token_id}`}
+      tabindex="-1"
+      aria-labelledby={`token-${token.token_id}Label`}
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id={`token-${token.token_id}Label`}>
+              Token {token.token_id}
+              {price > 0 && (
+                <span class="ms-1 badge bg-success">
+                  Mint Price: {price} NEAR
+                </span>
+              )}
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <img src={token.metadata.media} />
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <CommitButton
+              data={{
+                profile: {
+                  image: {
+                    url: null,
+                    nft: {
+                      contractId,
+                      tokenId: token.token_id,
+                    },
+                  },
+                },
+              }}
+            >
+              Set profile picture
+            </CommitButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+return (
+  <>
+    <div class="text-center">
+      <h4>Social Avatars you owned</h4>
+      <div>{previews}</div>
+    </div>
+    {modals}
+  </>
+);
