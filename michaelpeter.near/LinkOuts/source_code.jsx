9e9c6fcdd61b48e2954076cb870574682df2e8b6
mpeterdev <mpeterdev>
2022-12-19 03:17:29 -0500

    Update LinkOuts by michaelpeter.near at 2022-12-19 03:17:29.496

---
+++ michaelpeter.near/LinkOuts/source_code.jsx
@@ -0,0 +1,15 @@
+function LinkOuts() {
+  return (
+    <div className="flex flex-col gap-2">
+      <h2 className="text-2xl">Get Involved</h2>
+      <div className="flex flex-row gap-2">
+        <button className="border rounded p-2 text-cyan-50 hover:bg-neutral-800">
+          What's Near Social?
+        </button>
+        <button className="border rounded p-2 text-cyan-50 hover:bg-neutral-800">
+          Documentation
+        </button>
+      </div>
+    </div>
+  );
+}
