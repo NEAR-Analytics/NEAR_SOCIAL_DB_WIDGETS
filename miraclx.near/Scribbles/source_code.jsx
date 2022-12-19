e7fc27e5a45c0ad7f79e2468720b3059669caada
miraclx <miraclx>
2022-12-19 00:16:30 -0500

    Update Scribbles by miraclx.near at 2022-12-19 00:16:30.861

--- miraclx.near/Scribbles/source_code.jsx
+++ miraclx.near/Scribbles/source_code.jsx
@@ -1,88 +1,155 @@
 const scope = props.scope ?? context.scope;
 
 State.init({
-  query: "**",
-  queryModTime: Date.now(),
+  path: "**",
+  pathModTime: Date.now(),
+  isTreeView: true,
+  newQuery: !true,
   trie: {},
+  rawData: {},
 });
 
-function handleQuery(e) {
-  State.update({ query: e.target.value });
-  setTimeout(() => console.log(10), 1000);
-  return true;
+function switchTab(newView) {
+  if (!state.isTreeView && newView == "tree") {
+    State.update({ isTreeView: true });
+  } else if (state.isTreeView && newView == "raw") {
+    State.update({ isTreeView: false });
+  }
+}
+
+function handleQuery(path) {
+  State.update({ path, newQuery: true });
+  if (state.isTreeView) {
+    Social.keys(state.path, "final");
+  } else {
+    // Social.keys(state.path, "final");
+  }
+  // console.log(state.path);
+  // // while (!(keys && Object.keys(keys).length)) {
+  // //   console.log(keys);
+  // // }
+  // console.log(keys);
+  // State.update({ loading: false });
+  // return true;
+}
+
+function treeView() {
+  console.log("new tree");
+  State.update({ newQuery: false });
+  return (
+    <>
+      {state.path}
+
+      {state.isTreeView}
+    </>
+  );
+}
+
+function rawView() {
+  console.log("new raw");
+  State.update({ newQuery: false });
+  let text = "Hellur World";
+  return (
+    <>
+      <Markdown
+        text={`\`\`\`json
+${text}
+\`\`\``}
+      />
+    </>
+  );
 }
 
 let Styles = styled.span``;
 
+function after(cond, els) {
+  if (cond) return els;
+  return (
+    <div class="d-flex justify-content-center">
+      <div class="spinner-border" role="status"></div>
+    </div>
+  );
+}
+
 return (
   <>
-    <div
-      class="input-group"
-      style={{
-        display: "flex",
-        alignItems: "center",
-      }}
-    >
-      <input type="text" value={state.query} onChange={handleQuery} />
-      <button
-        class="btn btn-outline-secondary"
-        data-toggle="tooltip"
-        data-placement="top"
-        title="Tooltip on top"
-      >
+    <div class="input-group">
+      <input
+        type="text"
+        value={state.path}
+        onChange={(e) => handleQuery(e.target.value)}
+      />
+      <button class="btn btn-outline-secondary" title="Copy Permalink">
         🔗
       </button>
     </div>
 
     <hr />
 
-    <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
-      <li class="nav-item" role="presentation">
-        <button
-          class="nav-link text-dark active"
-          id="home-tab"
-          data-bs-toggle="tab"
-          data-bs-target="#home"
-          type="button"
-          role="tab"
-          aria-controls="home"
-          aria-selected="true"
+    {after(
+      !state.newQuery,
+      <>
+        <ul
+          class="nav nav-tabs justify-content-center"
+          id="myTab"
+          role="tablist"
         >
-          Tree
-        </button>
-      </li>
-      <li class="nav-item" role="presentation">
-        <button
-          class="nav-link text-dark"
-          id="profile-tab"
-          data-bs-toggle="tab"
-          data-bs-target="#profile"
-          type="button"
-          role="tab"
-          aria-controls="profile"
-          aria-selected="false"
-        >
-          Raw
-        </button>
-      </li>
-    </ul>
-    <div class="tab-content" id="myTabContent">
-      <div
-        class="tab-pane fade show active"
-        id="home"
-        role="tabpanel"
-        aria-labelledby="home-tab"
-      >
-        {state.query}
-      </div>
-      <div
-        class="tab-pane fade"
-        id="profile"
-        role="tabpanel"
-        aria-labelledby="profile-tab"
-      >
-        Profile Content
-      </div>
-    </div>
+          <li class="nav-item" role="presentation">
+            <button
+              class="nav-link text-dark active"
+              id="tree-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#tree"
+              type="button"
+              role="tab"
+              aria-controls="tree"
+              aria-selected={state.isTreeView}
+              onClick={() => switchTab("tree")}
+            >
+              Tree
+            </button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button
+              class="nav-link text-dark"
+              id="raw-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#raw"
+              type="button"
+              role="tab"
+              aria-controls="raw"
+              aria-selected={!state.isTreeView}
+              onClick={() => switchTab("raw")}
+            >
+              Raw
+            </button>
+          </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+          <div
+            class="tab-pane fade show active"
+            id="tree"
+            role="tabpanel"
+            aria-labelledby="tree-tab"
+          >
+            {state.isTreeView ? treeView() : <></>}
+          </div>
+          <div
+            class="tab-pane fade"
+            id="raw"
+            role="tabpanel"
+            aria-labelledby="raw-tab"
+          >
+            {!state.isTreeView ? rawView() : <></>}
+          </div>
+        </div>
+      </>
+    )}
   </>
 );
+
+// {state.loading ? (
+//   <div class="d-flex justify-content-center">
+//     <div class="spinner-border" role="status"></div>
+//   </div>
+// ) : }
