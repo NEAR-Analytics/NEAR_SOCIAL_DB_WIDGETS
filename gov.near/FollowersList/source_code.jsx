7696d6fc54bdb4ed5f059aea857e1860fadbfa2b
gov.near <gov.near>
2022-12-19 00:10:31 -0500

    Update FollowersList by gov.near at 2022-12-19 00:10:31.142

--- gov.near/FollowersList/source_code.jsx
+++ gov.near/FollowersList/source_code.jsx
@@ -4,23 +4,23 @@ if (!accountId) {
   return "";
 }
 
-let followers = Social.keys(`*/graph/follow/${accountId}`, "final", {
+let subscribers = Social.keys(`*/graph/follow/${accountId}`, "final", {
   return_type: "BlockHeight",
   values_only: true,
 });
 
-if (followers === null) {
+if (subscribers === null) {
   return "Loading";
 }
 
-followers = Object.entries(followers || {});
-followers.sort(
+subscribers = Object.entries(subscribers || {});
+subscribers.sort(
   (a, b) => b.graph.follow[accountId][1] - a.graph.follow[accountId][1]
 );
 
 return (
   <>
-    {followers.map(([accountId]) => (
+    {subscribers.map(([accountId]) => (
       <div className="d-flex justify-content-between mb-3">
         <div className="me-4">
           <Widget src="gov.near/widget/Project" props={{ accountId }} />
