bc7351fef81ba284e50ad67169bbda09491d3cf4
mpeterdev <mpeterdev>
2022-12-19 03:18:29 -0500

    Update PokeItem by michaelpeter.near at 2022-12-19 03:18:29.498

---
+++ michaelpeter.near/PokeItem/source_code.jsx
@@ -0,0 +1,12 @@
+import DummyImage from "/social-widgets/michaelpeter.near/DummyImage";
+
+function PokeItem() {
+  return (
+    <div className="flex flex-row gap-2 items-center">
+      <DummyImage dimension={10} />
+      <p className="text-2xl">👈</p>
+      <DummyImage dimension={10} />
+      <p className="text-slate-400">2 hours ago</p>
+    </div>
+  );
+}
