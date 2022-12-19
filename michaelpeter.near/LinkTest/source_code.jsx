b63d5ce43a7c238bcb03143264807dac7ec71dd8
mpeterdev <mpeterdev>
2022-12-19 05:41:48 -0500

    Update LinkTest by michaelpeter.near at 2022-12-19 05:41:48.042

--- michaelpeter.near/LinkTest/source_code.jsx
+++ michaelpeter.near/LinkTest/source_code.jsx
@@ -1,14 +1,23 @@
 export default function LinkTest() {
   return (
-    <a
-      onClick={() => {
-        window.top.postMessage(
-          { action: "link", target: "https://www.google.com" },
-          "*"
-        );
-      }}
-    >
-      Google
-    </a>
+    <div className="flex flex-row gap-2">
+      <a
+        onClick={() => {
+          window.top.postMessage(
+            { action: "link", target: "https://www.google.com" },
+            "*"
+          );
+        }}
+      >
+        Google
+      </a>
+      <a
+        onClick={() => {
+          window.open("https://www.google.com");
+        }}
+      >
+        Google New Tab
+      </a>
+    </div>
   );
 }
