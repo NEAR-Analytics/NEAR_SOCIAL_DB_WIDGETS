e1759322e34366026b399eef18e38e762a23ad7a
mpeterdev <mpeterdev>
2022-12-19 05:07:07 -0500

    Update LinkTest by michaelpeter.near at 2022-12-19 05:07:07.106

--- michaelpeter.near/LinkTest/source_code.jsx
+++ michaelpeter.near/LinkTest/source_code.jsx
@@ -3,7 +3,7 @@ export default function LinkTest() {
     <a
       onClick={() => {
         window.top.postMessage(
-          JSON.stringify({ action: "link", target: "www.google.com" }),
+          { action: "link", target: "www.google.com" },
           "*"
         );
       }}
