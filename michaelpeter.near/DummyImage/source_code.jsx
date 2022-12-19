c0cd8e8d643a107edf0eba458692e9a9fc07bb20
mpeterdev <mpeterdev>
2022-12-19 03:46:57 -0500

    Update DummyImage by michaelpeter.near at 2022-12-19 03:46:57.531

--- michaelpeter.near/DummyImage/source_code.jsx
+++ michaelpeter.near/DummyImage/source_code.jsx
@@ -1,4 +1,4 @@
-function DummyImage({ dimension }: { dimension: number }) {
+export default function DummyImage({ dimension }: { dimension: number }) {
   return (
     <div className={`rounded bg-sky-600 w-${dimension} h-${dimension}`}></div>
   );
