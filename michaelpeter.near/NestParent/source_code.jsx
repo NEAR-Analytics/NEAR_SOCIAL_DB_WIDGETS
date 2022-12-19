0ddd0a8ff8e0578f30965dede6e3da9d7faf86f0
mpeterdev <mpeterdev>
2022-12-19 01:11:33 -0500

    Update NestParent by michaelpeter.near at 2022-12-19 01:11:33.555

--- michaelpeter.near/NestParent/source_code.jsx
+++ michaelpeter.near/NestParent/source_code.jsx
@@ -1,5 +1,10 @@
 import SandpackWidget from "/social-widgets/michaelpeter.near/SandpackWidget";
 
 export default function Widget() {
-  return <SandpackWidget />;
+  return (
+    <div style={{ display: flex, flexDirection: "row", rowGap: "1rem" }}>
+      <span>Nested:</span>
+      <SandpackWidget />
+    </div>
+  );
 }
