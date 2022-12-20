5bb85d41280339a1e63690da41910ea9b3960fe2
miraclx <miraclx>
2022-12-20 04:33:38 -0500

    Update Attribution by miraclx.near at 2022-12-20 04:33:38.093

--- miraclx.near/Attribution/source_code.jsx
+++ miraclx.near/Attribution/source_code.jsx
@@ -1,22 +1,27 @@
-let accountId = props.accountId ?? "miraclx.near";
-let nameOnly = props.nameOnly ?? false;
+let authors = props.authors ?? ["Nobody"];
+
+function humanList(list) {
+  return [list.pop(), list.join(", ")].filter(Boolean).reverse().join(" and ");
+}
 
 return (
   <span class="text-muted">
-    {nameOnly ? <></> : <> By</>}
-    <a
-      href={`#/mob.near/widget/ProfilePage?accountId=${accountId}`}
-      class="text-muted text-truncate"
-    >
-      <Widget
-        src="mob.near/widget/ProfileLine"
-        props={{
-          accountId,
-          hideAccountId: true,
-          tooltip: true,
-          link: false,
-        }}
-      />
-    </a>
+    By
+    {authors.map((author) => (
+      <a
+        href={`#/mob.near/widget/ProfilePage?accountId=${author}`}
+        class="text-muted text-truncate"
+      >
+        <Widget
+          src="mob.near/widget/ProfileLine"
+          props={{
+            accountId: author,
+            hideAccountId: true,
+            tooltip: true,
+            link: false,
+          }}
+        />
+      </a>
+    ))}
   </span>
 );
