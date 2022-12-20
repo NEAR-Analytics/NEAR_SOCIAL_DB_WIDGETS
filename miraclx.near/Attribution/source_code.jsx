0a3f6015cb6f5cc8c6ffb86381942723fb38e2f1
miraclx <miraclx>
2022-12-20 14:56:06 -0500

    Update Attribution by miraclx.near at 2022-12-20 14:56:06.975

--- miraclx.near/Attribution/source_code.jsx
+++ miraclx.near/Attribution/source_code.jsx
@@ -14,26 +14,36 @@ function humanList(list) {
   );
 }
 
+if (props.dep)
+  return (
+    <span class="text-muted">
+      By
+      {humanList(
+        authors.map((author) => (
+          <a
+            href={`#/mob.near/widget/ProfilePage?accountId=${author}`}
+            class="text-muted"
+          >
+            <Widget
+              src="mob.near/widget/ProfileLine"
+              props={{
+                accountId: author,
+                hideAccountId: true,
+                tooltip: true,
+                link: false,
+              }}
+            />
+          </a>
+        ))
+      )}
+    </span>
+  );
+
 return (
-  <span class="text-muted">
-    By
-    {humanList(
-      authors.map((author) => (
-        <a
-          href={`#/mob.near/widget/ProfilePage?accountId=${author}`}
-          class="text-muted"
-        >
-          <Widget
-            src="mob.near/widget/ProfileLine"
-            props={{
-              accountId: author,
-              hideAccountId: true,
-              tooltip: true,
-              link: false,
-            }}
-          />
-        </a>
-      ))
-    )}
-  </span>
+  <>
+    <Widget
+      src="miraclx.near/widget/Attribution"
+      props={{ dep: true, authors: ["miraclx.near"] }}
+    />
+  </>
 );
