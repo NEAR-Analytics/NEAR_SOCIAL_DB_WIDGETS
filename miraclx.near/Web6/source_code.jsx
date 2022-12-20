2c7c6385b457c30d3c1307af7bff91913efb63a3
miraclx <miraclx>
2022-12-19 19:38:51 -0500

    Update Web6 by miraclx.near at 2022-12-19 19:38:51.126

---
+++ miraclx.near/Web6/source_code.jsx
@@ -0,0 +1,14 @@
+let iframe = Object.fromEntries(
+  Object.entries(styled.p``).filter(([k]) => k)
+).withComponent("iframe");
+
+return (
+  <>
+    <iframe
+      width="420"
+      height="315"
+      allow="autoplay"
+      src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0&enablejsapi=1"
+    ></iframe>
+  </>
+);
