48f1b69641eb30e0a6f40774354ec90e6fcc3977
miraclx <miraclx>
2022-12-20 04:52:07 -0500

    Update Attribution by miraclx.near at 2022-12-20 04:52:07.713

--- miraclx.near/Attribution/source_code.jsx
+++ miraclx.near/Attribution/source_code.jsx
@@ -1,27 +1,39 @@
 let authors = props.authors ?? ["Nobody"];
 
 function humanList(list) {
-  return [list.pop(), list.join(", ")].filter(Boolean).reverse().join(" and ");
+  let last = list.pop();
+  return (
+    <>
+      {list.length ? (
+        [...list.flatMap((item, idx) => [...(idx ? [","] : []), item]), " and "]
+      ) : (
+        <></>
+      )}
+      {last}
+    </>
+  );
 }
 
 return (
   <span class="text-muted">
     By
-    {authors.map((author) => (
-      <a
-        href={`#/mob.near/widget/ProfilePage?accountId=${author}`}
-        class="text-muted text-truncate"
-      >
-        <Widget
-          src="mob.near/widget/ProfileLine"
-          props={{
-            accountId: author,
-            hideAccountId: true,
-            tooltip: true,
-            link: false,
-          }}
-        />
-      </a>
-    ))}
+    {humanList(
+      authors.map((author) => (
+        <a
+          href={`#/mob.near/widget/ProfilePage?accountId=${author}`}
+          class="text-muted"
+        >
+          <Widget
+            src="mob.near/widget/ProfileLine"
+            props={{
+              accountId: author,
+              hideAccountId: true,
+              tooltip: true,
+              link: false,
+            }}
+          />
+        </a>
+      ))
+    )}
   </span>
 );
