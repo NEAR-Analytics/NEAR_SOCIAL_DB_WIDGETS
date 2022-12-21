1e9f6d59ac1d25c1c6f6d192f34e4ea096ff54f4
evgenykuzyakov <evgenykuzyakov>
2022-12-20 20:01:40 -0500

    Update Profile.ShortInlineBlock by mob.near at 2022-12-20 20:01:40.219

---
+++ mob.near/Profile.ShortInlineBlock/source_code.jsx
@@ -0,0 +1,30 @@
+const accountId = props.accountId;
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+const name = profile.name;
+
+return (
+  <div className="d-flex flex-row">
+    <Widget
+      src="mob.near/widget/ProfileImage"
+      props={{
+        metadata,
+        accountId,
+        widgetName,
+        style: { height: "2.5em", width: "2.5em", minWidth: "2.5em" },
+        className: "me-2",
+      }}
+    />
+    <div className="text-truncate lh-sm">
+      <div className="text-truncate">
+        <span className="fw-bold">{name}</span>
+      </div>
+      <div className="text-truncate text-muted">
+        <small>
+          <span className="font-monospace">@{accountId}</span>
+        </small>
+      </div>
+    </div>
+  </div>
+);
