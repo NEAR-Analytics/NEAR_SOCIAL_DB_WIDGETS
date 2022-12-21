af1830b6415a832ab817630c12d8b0cb06d6a739
evgenykuzyakov <evgenykuzyakov>
2022-12-20 23:09:29 -0500

    Update Welcome2 by mob.near at 2022-12-20 23:09:29.880

--- mob.near/Welcome2/source_code.jsx
+++ mob.near/Welcome2/source_code.jsx
@@ -1,8 +1,8 @@
 return (
   <>
+    <Widget src="mob.near/widget/ProfileOnboarding" />
     {context.accountId && (
       <div className="text-bg-light rounded-4 p-3 mb-3">
-        <Widget src="mob.near/widget/ProfileOnboarding" />
         <a
           href={`#/mob.near/widget/ProfilePage`}
           className="text-decoration-none link-dark"
