a3264cc1a9665776e6ff540397ba18f29c843bb9
evgenykuzyakov <evgenykuzyakov>
2022-12-20 23:31:42 -0500

    Update Welcome by mob.near at 2022-12-20 23:31:42.955

--- mob.near/Welcome/source_code.jsx
+++ mob.near/Welcome/source_code.jsx
@@ -72,85 +72,83 @@ return (
       </div>
     </div>
 
-    <div className="row mb-3">
-      <ul
-        className="d-block d-lg-none nav nav-pills nav-fill mb-3"
-        id="pills-tab"
-        role="tablist"
+    <ul
+      className="d-block d-lg-none nav nav-pills nav-fill mb-3"
+      id="pills-tab"
+      role="tablist"
+    >
+      <li className="nav-item" role="presentation">
+        <button
+          className="nav-link active"
+          id="pills-posts-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#pills-posts"
+          type="button"
+          role="tab"
+          aria-controls="pills-posts"
+          aria-selected="true"
+        >
+          Posts
+        </button>
+      </li>
+      <li className="nav-item" role="presentation">
+        <button
+          className="nav-link"
+          id="pills-graph-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#pills-graph"
+          type="button"
+          role="tab"
+          aria-controls="pills-graph"
+          aria-selected="false"
+        >
+          Graph
+        </button>
+      </li>
+      <li className="nav-item" role="presentation">
+        <button
+          className="nav-link"
+          id="pills-pokes-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#pills-pokes"
+          type="button"
+          role="tab"
+          aria-controls="pills-pokes"
+          aria-selected="false"
+        >
+          Pokes
+        </button>
+      </li>
+    </ul>
+    <div className="tab-content row p-0" id="pills-tabContent">
+      <div
+        className="tab-pane d-lg-block col-lg-3"
+        id="pills-graph"
+        role="tabpanel"
+        aria-labelledby="pills-graph-tab"
       >
-        <li className="nav-item" role="presentation">
-          <button
-            className="nav-link active"
-            id="pills-posts-tab"
-            data-bs-toggle="pill"
-            data-bs-target="#pills-posts"
-            type="button"
-            role="tab"
-            aria-controls="pills-posts"
-            aria-selected="true"
-          >
-            Posts
-          </button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button
-            className="nav-link"
-            id="pills-graph-tab"
-            data-bs-toggle="pill"
-            data-bs-target="#pills-graph"
-            type="button"
-            role="tab"
-            aria-controls="pills-graph"
-            aria-selected="false"
-          >
-            Graph
-          </button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button
-            className="nav-link"
-            id="pills-pokes-tab"
-            data-bs-toggle="pill"
-            data-bs-target="#pills-pokes"
-            type="button"
-            role="tab"
-            aria-controls="pills-pokes"
-            aria-selected="false"
-          >
-            Pokes
-          </button>
-        </li>
-      </ul>
-      <div className="tab-content row" id="pills-tabContent">
-        <div
-          className="tab-pane d-lg-block col-lg-3"
-          id="pills-graph"
-          role="tabpanel"
-          aria-labelledby="pills-graph-tab"
-        >
-          <div className="text-bg-light rounded-4 p-3">
-            <h5>Follow activity</h5>
-            <Widget src="mob.near/widget/FollowFeed" />
-          </div>
+        <div className="text-bg-light rounded-4 p-3">
+          <h5>Follow activity</h5>
+          <Widget src="mob.near/widget/FollowFeed" />
         </div>
-        <div
-          className="tab-pane show active d-lg-block col-lg-6"
-          id="pills-posts"
-          role="tabpanel"
-          aria-labelledby="pills-posts-tab"
-        >
-          <Widget src="mob.near/widget/MainPage.Content" />
-        </div>
-        <div
-          className="tab-pane d-lg-block col-lg-3"
-          id="pills-pokes"
-          role="tabpanel"
-          aria-labelledby="pills-pokes-tab"
-        >
-          <div className="text-bg-light rounded-4 p-3">
-            <h5>Poke activity</h5>
-            <Widget src="mob.near/widget/PokeFeed" />
-          </div>
+      </div>
+      <div
+        className="tab-pane show active d-lg-block col-lg-6"
+        id="pills-posts"
+        role="tabpanel"
+        aria-labelledby="pills-posts-tab"
+      >
+        <Widget src="mob.near/widget/MainPage.Content" />
+      </div>
+      <div
+        className="tab-pane d-lg-block col-lg-3"
+        id="pills-pokes"
+        role="tabpanel"
+        aria-labelledby="pills-pokes-tab"
+      >
+        <div className="text-bg-light rounded-4 p-3">
+          <h5>Poke activity</h5>
+          <Widget src="mob.near/widget/PokeFeed" />
         </div>
       </div>
     </div>
