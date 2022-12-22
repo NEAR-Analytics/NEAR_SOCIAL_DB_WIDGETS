8e4f2e5c46f8c003b956eb67625d7d0fdbe630b2
evgenykuzyakov <evgenykuzyakov>
2022-12-22 17:32:20 -0500

    Update Welcome by mob.near at 2022-12-22 17:32:20.046

--- mob.near/Welcome/source_code.jsx
+++ mob.near/Welcome/source_code.jsx
@@ -3,15 +3,26 @@ return (
     <Widget src="mob.near/widget/ProfileOnboarding" />
     {context.accountId && (
       <div className="text-bg-light rounded-4 p-3 mb-3">
-        <a
-          href={`#/mob.near/widget/ProfilePage`}
-          className="text-decoration-none link-dark"
-        >
-          <Widget
-            src="mob.near/widget/Profile.InlineBlock"
-            props={{ accountId: context.accountId }}
-          />
-        </a>
+        <div className="d-flex justify-content-between align-items-center">
+          <a
+            href={`#/mob.near/widget/ProfilePage`}
+            className="text-decoration-none link-dark me-2 text-truncate"
+          >
+            <Widget
+              src="mob.near/widget/Profile.InlineBlock"
+              props={{ accountId: context.accountId }}
+            />
+          </a>
+
+          <div className="d-none text-nowrap d-md-block">
+            <a
+              href={`#/mob.near/widget/ProfileEditor`}
+              className="btn btn-outline-secondary"
+            >
+              Edit Profile
+            </a>
+          </div>
+        </div>
       </div>
     )}
     <div className="text-bg-light rounded-4 p-3 mb-3">
