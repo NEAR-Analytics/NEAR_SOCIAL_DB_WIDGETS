6222d54505ec2b87303abf4f102111fc461e17e6
evgenykuzyakov <evgenykuzyakov>
2022-12-22 17:26:37 -0500

    Update MainPage.Post by mob.near at 2022-12-22 17:26:37.992

--- mob.near/MainPage.Post/source_code.jsx
+++ mob.near/MainPage.Post/source_code.jsx
@@ -32,18 +32,28 @@ return (
       </small>
     </div>
     <div className="mt-3">
-      {content.text && <Markdown text={content.text} />}
-      {content.image && (
-        <div className="w-100 rounded-3 text-center">
-          <Widget
-            src="mob.near/widget/Image"
-            props={{
-              image: content.image,
-              className: "img-fluid rounded-3",
-              style: { maxHeight: "20em" },
-            }}
-          />
-        </div>
+      {content ? (
+        <>
+          {content.text && <Markdown text={content.text} />}
+          {content.image && (
+            <div className="w-100 rounded-3 text-center">
+              <Widget
+                src="mob.near/widget/Image"
+                props={{
+                  image: content.image,
+                  className: "img-fluid rounded-3",
+                  style: { maxHeight: "20em" },
+                }}
+              />
+            </div>
+          )}
+        </>
+      ) : (
+        <span
+          className="spinner-grow spinner-grow-sm me-1"
+          role="status"
+          aria-hidden="true"
+        />
       )}
     </div>
   </div>
