e2d97a3551656c59246c4712ce365b01beeec0b6
evgenykuzyakov <evgenykuzyakov>
2022-12-22 23:45:50 -0500

    Update LikeButton by mob.near at 2022-12-22 23:45:50.084

--- mob.near/LikeButton/source_code.jsx
+++ mob.near/LikeButton/source_code.jsx
@@ -6,7 +6,7 @@ if (!item) {
 
 const likes = Social.index("like", item);
 
-const loading = likes === null;
+const dataLoading = likes === null;
 
 const likesByUsers = {};
 
@@ -22,48 +22,67 @@ const numLikes = Object.keys(likesByUsers).length;
 const hasLike =
   context.accountId && (state.hasLike || !!likesByUsers[context.accountId]);
 
-const data = {
-  index: {
-    like: JSON.stringify({
-      key: item,
-      value: {
-        type: hasLike ? "unlike" : "like",
-      },
-    }),
-  },
-};
-
-if (!hasLike && props.notifyAccountId) {
-  data.notify = JSON.stringify({
-    key: props.notifyAccountId,
-    value: {
-      type: "like",
-      item,
-    },
-  });
-}
-
-const LikeButton = styled.CommitButton`
+const LikeButton = styled.button`
   &:hover {
     color: red;
-    background: pink;
+    .heart {
+      background: pink;
+    }
   }
 `;
 
+const likeClick = () => {
+  if (state.loading) {
+    return;
+  }
+  State.update({
+    loading: true,
+  });
+  const data = {
+    index: {
+      like: JSON.stringify({
+        key: item,
+        value: {
+          type: hasLike ? "unlike" : "like",
+        },
+      }),
+    },
+  };
+
+  if (!hasLike && props.notifyAccountId) {
+    data.notify = JSON.stringify({
+      key: props.notifyAccountId,
+      value: {
+        type: "like",
+        item,
+      },
+    });
+  }
+  Social.set(data, {
+    onCommit: () => State.update({ loading: false, hasLike: !hasLike }),
+    onCancel: () => State.update({ loading: false }),
+  });
+};
+
 return (
   <LikeButton
-    disabled={loading || !context.accountId}
-    className={`btn border-0 rounded-circle`}
-    data={data}
-    onCommit={() => State.update({ hasLike: !hasLike })}
+    disabled={state.loading || dataLoading || !context.accountId}
+    className={`btn border-0`}
     title={hasLike ? "Unlike" : "Like"}
+    onClick={likeClick}
   >
-    {loading ? (
+    {state.loading ? (
+      <span
+        className="spinner-grow spinner-grow-sm p-2"
+        role="status"
+        aria-hidden="true"
+      />
+    ) : dataLoading ? (
       "Loading"
     ) : hasLike ? (
-      <i className="bi bi-heart-fill"></i>
+      <i className="heart p-2 rounded-circle bi bi-heart-fill"></i>
     ) : (
-      <i className="bi bi-heart"></i>
+      <i className="heart p-2 rounded-circle bi bi-heart"></i>
     )}
     {numLikes > 0 ? numLikes : ""}
   </LikeButton>
