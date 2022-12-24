1d21d71088665e0ad46c82efca1b810a6853d65e
gov.near <gov.near>
2022-12-23 22:03:07 -0500

    Update Comments by gov.near at 2022-12-23 22:03:07.977

--- gov.near/Comments/source_code.jsx
+++ gov.near/Comments/source_code.jsx
@@ -8,40 +8,19 @@ const setSelectedComment = (ref) => {
   State.update({ selectedComment: ref });
 };
 
-const CommentWrapper = styled.div`
-  width: 100%
-`;
-const Center = styled.div`
-  display: flex;
-  flex-direction:row;
-  justify-content:center;
-`;
-const Wrapper = styled.div`
-  width: 50rem
-  display:flex
-  flex-direction:row
-  justify-content:center
-`;
-
 return (
   <div>
     {state.selectedComment && (
-      <Wrapper className="d-flex flex-column gap-5">
-        <div>
-          <button className="btn btn-outline-secondary" onClick={clearSelected}>
-            <i class="bi bi-chevron-left" />
-            All Comments
-          </button>
-        </div>
-        <Center>
-          <CommentWrapper>
-            <Widget
-              src="michaelpeter.near/widget/GenieQuestionAnswerView"
-              props={{ commentRef: state.selectedComment }}
-            />
-          </CommentWrapper>
-        </Center>
-      </Wrapper>
+      <div>
+        <button className="btn btn-outline-secondary" onClick={clearSelected}>
+          <i class="bi bi-chevron-left" />
+          All Comments
+        </button>
+        <Widget
+          src="gov.near/widget/CommentView"
+          props={{ commentRef: state.selectedComment }}
+        />
+      </div>
     )}
     {state.showCommentForm && (
       <Wrapper>
@@ -56,52 +35,39 @@ return (
           </button>
         </div>
         <Widget
-          src="michaelpeter.near/widget/GenieSaveQuestion"
+          src="gov.near/widget/SaveComment"
           props={{ searchString: state.searchString, setSearchString }}
         />
       </Wrapper>
     )}
     {!state.selectedComment && !state.showCommentForm && (
-      <div
-        className="d-flex flex-column gap-3 pt-1"
-        style={{
-          width: "50rem",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
-        <div className="d-flex align-items-center gap-3">
-          <i class="bi bi-search" />
-          <div class="input-group input-group-lg">
-            <input
-              type="text"
-              placeholder="Your wish is my command"
-              className="form-control input-group input-group-lg"
-              value={state.searchString}
-              onChange={(e) => {
-                State.update({ searchString: e.target.value });
-              }}
-            />
-          </div>
-          <button
-            className="btn btn-primary text-nowrap"
-            onClick={() => {
-              State.update({ showCommentForm: true });
+      <div className="d-flex align-items-center gap-3">
+        <div class="input-group input-group-lg">
+          <input
+            type="text"
+            placeholder="Text Goes Here"
+            className="form-control input-group input-group-lg"
+            value={state.searchString}
+            onChange={(e) => {
+              State.update({ searchString: e.target.value });
             }}
-          >
-            <div>
-              <i class="bi bi-chat" />
-              Post a comment!
-            </div>
-          </button>
-        </div>
-        <CommentWrapper>
-          <Widget
-            src="michaelpeter.near/widget/GenieQuestionList"
-            props={{ searchString: state.searchString, setSelectedComment }}
           />
-        </CommentWrapper>
+        </div>
+        <button
+          className="btn btn-primary text-nowrap"
+          onClick={() => {
+            State.update({ showCommentForm: true });
+          }}
+        >
+          <div>
+            <i class="bi bi-chat" />
+            Post a Comment
+          </div>
+        </button>
+        <Widget
+          src="gov.near/widget/CommentList"
+          props={{ searchString: state.searchString, setSelectedComment }}
+        />
       </div>
     )}
   </div>
