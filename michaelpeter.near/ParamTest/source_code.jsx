d966076f2b5941e760286ccb6a3e9a1110006876
mpeterdev <mpeterdev>
2022-12-27 01:08:04 -0500

    Update ParamTest by michaelpeter.near at 2022-12-27 01:08:04.114

--- michaelpeter.near/ParamTest/source_code.jsx
+++ michaelpeter.near/ParamTest/source_code.jsx
@@ -1,7 +1,9 @@
+const { test } = props;
+
 // const params = new URLSearchParams(window.location.search);
 return (
   <div>
-    <h1>Location is... {JSON.stringify(window.location)}</h1>
-    <h1>Params are... {JSON.stringify(window.location.search)}</h1>
+    <h1>Location is... {window.location}</h1>
+    <h1>Params are... {test}</h1>
   </div>
 );
