4ea196f22b14fdbb21c389065c0ff89c3c573d97
roshaan.near <roshaan.near>
2022-12-27 21:32:50 -0500

    Update FollowStats by roshaan.near at 2022-12-27 21:32:50.076

--- roshaan.near/FollowStats/source_code.jsx
+++ roshaan.near/FollowStats/source_code.jsx
@@ -1,4 +1,5 @@
 const accountId = props.accountId;
+const userId = context.accountId;
 
 if (!accountId) {
   return "";
@@ -18,6 +19,36 @@ const numFollowing = following
   ? Object.keys(following[accountId].graph.follow || {}).length
   : null;
 const numFollowers = followers ? Object.keys(followers || {}).length : null;
+
+const accounts = Social.keys(`*/graph/follow/*`, "final", {
+  return_type: "BlockHeight",
+  values_only: true,
+});
+
+let followingsPerAccount = Object.keys(accounts).reduce(
+  (res, id) => ({
+    ...res,
+    [id]: Object.keys(accounts[id].graph.follow).filter((x) => x !== accountId),
+  }),
+  {}
+);
+
+const myTags = Social.getr(`${userId}/profile/tags`, "final") || [];
+const profileVisitedTags =
+  Social.getr(`${accountId}/profile/tags`, "final") || [];
+
+const myFriends = followingsPerAccount[userId] || [];
+
+const findFriendsInCommon = (accountId) => {
+  return myFriends.filter((a) => followingsPerAccount[accountId].includes(a));
+};
+
+const friendsInCommon = findFriendsInCommon(accountId);
+const tagsInCommon = () => {
+  return myTags.filter(
+    (a) => profileVisitedTags.length > 0 && profileVisitedTags.includes(a)
+  );
+};
 
 return (
   <div>
@@ -53,5 +84,52 @@ return (
         </a>
       </div>
     </div>
+    {userId && (
+      <div>
+        {friendsInCommon.length > 0 && (
+          <OverlayTrigger
+            placement="auto"
+            overlay={
+              <Tooltip>
+                <span> You both follow </span>
+                <br />
+                <br />
+                {friendsInCommon.map((friendsInCommon) => {
+                  return (
+                    <li className={`list-group-item`}>{friendsInCommon}</li>
+                  );
+                })}
+              </Tooltip>
+            }
+          >
+            <span
+              className="badge rounded-pill bg-primary"
+              title={`${friendsInCommon.length} followers in common`}
+            >
+              {friendsInCommon.length} friends in common
+            </span>
+          </OverlayTrigger>
+        )}
+        {tagsInCommon.length > 0 && (
+          <OverlayTrigger
+            placement="auto"
+            overlay={
+              <Tooltip>
+                {tagsInCommon.map((tag) => {
+                  return <li className={`list-group-item`}>{tag}</li>;
+                })}
+              </Tooltip>
+            }
+          >
+            <span
+              className="badge rounded-pill bg-primary"
+              title={`${tagsInCommon.length} tags in common`}
+            >
+              {tagsInCommon.length} common tags
+            </span>
+          </OverlayTrigger>
+        )}
+      </div>
+    )}
   </div>
 );
