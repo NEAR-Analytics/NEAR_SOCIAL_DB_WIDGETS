6657e2b9236d17a5c2edd46a708d8f9f9e5bacce
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-27 23:35:34 -0500

    Update voteWithText by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-27 23:35:34.450

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/voteWithText/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/voteWithText/source_code.jsx
@@ -2,10 +2,6 @@ if (!props.isPreview && !props.blockHeight) {
   return "Property blockHeight not set";
 }
 
-const isQuestionOpen =
-  questionParams.value.startTimestamp < Date.now() &&
-  Date.now() < questionParams.value.endTimestamp;
-
 // Utility function
 function getBlockTimestamp(blockHeight) {
   // It is stored in nanoseconds which is 1e-6 miliseconds
@@ -45,25 +41,9 @@ function getTimeRelatedValidAnswers(answers) {
   return answers.slice(0, high);
 }
 
-function closeModalClickingOnTransparent() {
-  return (e) => {
-    e.target.id == "modal" &&
-      State.update({
-        showVeryfyInstructionModal: false,
-        showVeryfyFailedModal: false,
-      });
-  };
-}
-
 let isPreview = props.isPreview;
 const questionBlockHeight = props.blockHeight;
-State.init({
-  vote: "",
-  showErrorsInForm: false,
-  verifiedStatus: "verifying",
-  showVeryfyInstructionModal: false,
-  showVeryfyFailedModal: false,
-});
+State.init({ vote: "", showErrorsInForm: false });
 
 const answers = Social.index("poll_question", "answer-v3.0.1");
 if (!answers) {
@@ -129,87 +109,8 @@ const renderAnswers = () => {
   });
 };
 
-const renderModal = () => {
-  return (
-    <div
-      className="modal"
-      id="modal"
-      style={
-        (state.showVeryfyInstructionModal || state.showVeryfyFailedModal) && {
-          display: "block",
-          backgroundColor: "#7e7e7e70",
-        }
-      }
-      tabindex="-1"
-      role="dialog"
-      onClick={closeModalClickingOnTransparent()}
-    >
-      <div className="modal-dialog" style={{ maxWidth: "90%" }} role="document">
-        <div
-          className="modal-content"
-          style={{ backgroundColor: "rgb(230, 230, 230)" }}
-        >
-          <div className="modal-header flex-row-reverse">
-            <button
-              type="button"
-              className="close"
-              dataDismiss="modal"
-              ariaLabel="Close"
-              onClick={() =>
-                State.update({
-                  showVeryfyInstructionModal: false,
-                  showVeryfyFailedModal: false,
-                })
-              }
-            >
-              <span ariaHidden="true">&times;</span>
-            </button>
-          </div>
-          <div
-            className="modal-body"
-            style={{
-              width: "90%",
-              borderRadius: "1rem",
-              margin: "0 auto",
-              backgroundColor: "white",
-            }}
-          >
-            {state.showVeryfyInstructionModal ? (
-              <p className="text-center">
-                Please complete the Proof of Humanity on the other tab. Once you
-                finish, the process might take a few minutes. Please, reload
-                this tab
-              </p>
-            ) : (
-              <p className="text-center text-danger">
-                The verification has failed. Please verify again.
-              </p>
-            )}
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              data-dismiss="modal"
-              onClick={() =>
-                State.update({
-                  showVeryfyInstructionModal: false,
-                  showVeryfyFailedModal: false,
-                })
-              }
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 return (
   <div>
-    {!isQuestionOpen ? "This question is already closed" : ""}
     {hasVoted ? (
       <p
         className="text-primary"
@@ -218,59 +119,30 @@ return (
         You have already voted
       </p>
     ) : (
-      isQuestionOpen && (
-        <div>
-          <textarea
-            value={state.vote}
-            onChange={(e) => State.update({ vote: e.target.value })}
-            style={{ width: "100%" }}
-          />
+      <div>
+        <textarea
+          value={state.vote}
+          onChange={(e) => State.update({ vote: e.target.value })}
+          style={{ width: "100%" }}
+        />
 
-          {state.verifiedStatus == "verified" ? (
-            isValidInput() ? (
-              <CommitButton
-                className="my-2 btn btn-primary"
-                data={getPublicationParams()}
-              >
-                Done
-              </CommitButton>
-            ) : (
-              <button
-                className="my-2 btn btn-primary"
-                onClick={() => State.update({ showErrorsInForm: true })}
-              >
-                Done
-              </button>
-            )
-          ) : state.verifiedStatus == "verifying" ? (
-            <button type="button" disabled className="my-2 btn btn-primary">
-              <span
-                className="spinner-border spinner-border-sm"
-                role="status"
-                aria-hidden="true"
-              ></span>
-              <span className="sr-only">Verifying...</span>
-            </button>
-          ) : state.verifiedStatus == "failed" ? (
-            <></>
-          ) : (
-            state.verifiedStatus == "notVerified" && (
-              <a
-                href="http://localhost:1234"
-                target="_blank"
-                onClick={() =>
-                  State.update({ showVeryfyInstructionModal: true })
-                }
-              >
-                <button className="my-2 btn btn-primary">Verify</button>
-              </a>
-            )
-          )}
-        </div>
-      )
+        {isValidInput() ? (
+          <CommitButton
+            className="my-2 btn btn-primary"
+            data={getPublicationParams()}
+          >
+            Done
+          </CommitButton>
+        ) : (
+          <button
+            className="my-2 btn btn-primary"
+            onClick={() => State.update({ showErrorsInForm: true })}
+          >
+            Done
+          </button>
+        )}
+      </div>
     )}
     {renderAnswers()}
-    {(state.showVeryfyInstructionModal || state.showVeryfyFailedModal) &&
-      renderModal()}
   </div>
 );
