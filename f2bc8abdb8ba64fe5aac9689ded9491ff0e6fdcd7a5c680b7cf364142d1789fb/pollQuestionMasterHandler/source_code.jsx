b6b3eb6dfa6b523cb7e8e1ee0972a024f8488c59
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-27 23:37:16 -0500

    Update pollQuestionMasterHandler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-27 23:37:16.260

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/pollQuestionMasterHandler/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/pollQuestionMasterHandler/source_code.jsx
@@ -1,87 +1,68 @@
+const tabs = {
+  POLLS: { id: 0, text: "Polls" },
+  NEW_POLL: { id: 1, text: "New Poll" },
+};
+const widgetOwner = "silkking.near";
+
 State.init({
-  displaying: "polls",
+  displaying: tabs.POLLS.id,
   hoveringElement: "",
 });
 
-let selectorStyles = [
-  {
-    cursor: "pointer",
-    fontWeight: "500",
-    color: "black",
-  },
-  {
-    borderTop: "1px solid rgba(0, 0, 0, 0.22)",
-    borderRight: "1px solid rgba(0, 0, 0, 0.22)",
-    borderLeft: "1px solid rgba(0, 0, 0, 0.22)",
-    color: "#1a73e8",
-    cursor: "pointer",
-    fontWeight: "500",
-  },
-];
+const styleSelectedTab = {
+  borderTop: "1px solid rgba(0, 0, 0, 0.22)",
+  borderRight: "1px solid rgba(0, 0, 0, 0.22)",
+  borderLeft: "1px solid rgba(0, 0, 0, 0.22)",
+  color: "#1a73e8",
+  cursor: "pointer",
+  fontWeight: "500",
+};
 
-const widgetOwner = "silkking.near";
+const styleUnselectedTab = {
+  cursor: "pointer",
+  fontWeight: "500",
+  color: "black",
+};
 
 return (
   <div className="px-4 py-3" style={{ backgroundColor: "rgb(230, 230, 230)" }}>
     <div className="d-flex justify-content-between align-items-center">
       <h3 style={{ color: "#1a73e8" }}>LOGO</h3>
       <ul
-        className="nav nav-tabs d-flex justify-content-start"
-        style={{ borderColor: "#00000038", width: "100%", margin: "1rem" }}
+        className="nav nav-tabs w-50 d-flex justify-content-start"
+        style={{ borderColor: "#00000038" }}
       >
-        <li className="nav-item">
-          <p
-            className={
-              state.hoveringElement == "polls" || state.displaying == "polls"
-                ? "nav-link active"
-                : "nav-link"
-            }
-            ariaCurrent="page"
-            onMouseEnter={() => {
-              State.update({ hoveringElement: "polls" });
-            }}
-            onMouseLeave={() => {
-              State.update({ hoveringElement: "" });
-            }}
-            onClick={() => {
-              State.update({ displaying: "polls" });
-            }}
-            style={
-              state.hoveringElement == "polls" || state.displaying == "polls"
-                ? selectorStyles[1]
-                : selectorStyles[0]
-            }
-          >
-            Polls
-          </p>
-        </li>
-        <li className="nav-item">
-          <p
-            className={
-              state.hoveringElement == "newPoll" ||
-              state.displaying == "newPoll"
-                ? "nav-link active"
-                : "nav-link"
-            }
-            onMouseEnter={() => {
-              State.update({ hoveringElement: "newPoll" });
-            }}
-            onMouseLeave={() => {
-              State.update({ hoveringElement: "" });
-            }}
-            onClick={() => {
-              State.update({ displaying: "newPoll" });
-            }}
-            style={
-              state.hoveringElement == "newPoll" ||
-              state.displaying == "newPoll"
-                ? selectorStyles[1]
-                : selectorStyles[0]
-            }
-          >
-            New Poll
-          </p>
-        </li>
+        {Object.keys(tabs).map((tabKey) => {
+          const tab = tabs[tabKey];
+          return (
+            <li className="nav-item">
+              <p
+                className={
+                  state.hoveringElement == tab.id || state.displaying == tab.id
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+                ariaCurrent="page"
+                onMouseEnter={() => {
+                  State.update({ hoveringElement: tab.id });
+                }}
+                onMouseLeave={() => {
+                  State.update({ hoveringElement: "" });
+                }}
+                onClick={() => {
+                  State.update({ displaying: tab.id });
+                }}
+                style={
+                  state.hoveringElement == tab.id || state.displaying == tab.id
+                    ? styleSelectedTab
+                    : styleUnselectedTab
+                }
+              >
+                {tab.text}
+              </p>
+            </li>
+          );
+        })}
       </ul>
       <div
         className="p-2"
@@ -91,10 +72,10 @@ return (
       </div>
     </div>
 
-    {state.displaying == "polls" ? (
+    {state.displaying == tabs.POLLS.id ? (
       <Widget src={`${widgetOwner}/widget/showQuestionsHandler`} />
     ) : (
-      state.displaying == "newPoll" && (
+      state.displaying == tabs.NEW_POLL.id && (
         <Widget src={`${widgetOwner}/widget/newPollQuestionInterface`} />
       )
     )}
