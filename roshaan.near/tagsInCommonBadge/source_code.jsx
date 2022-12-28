aed1fc3bb9bbaf906c92bece36ed222c85e7090f
roshaan.near <roshaan.near>
2022-12-27 21:43:18 -0500

    Update tagsInCommonBadge by roshaan.near at 2022-12-27 21:43:18.902

---
+++ roshaan.near/tagsInCommonBadge/source_code.jsx
@@ -0,0 +1,41 @@
+const accountId = props.accountId;
+const userId = context.accountId;
+
+if (!accountId || !userId) {
+  return "";
+}
+
+const myTags = Social.getr(`${userId}/profile/tags`, "final") || [];
+
+const profileVisitedTags =
+  Social.getr(`${accountId}/profile/tags`, "final") || [];
+
+const tagsInCommon = () => {
+  return myTags.filter(
+    (a) => profileVisitedTags.length > 0 && profileVisitedTags.includes(a)
+  );
+};
+
+return (
+  <div>
+    {tagsInCommon.length > 0 && (
+      <OverlayTrigger
+        placement="auto"
+        overlay={
+          <Tooltip>
+            {tagsInCommon.map((tag) => {
+              return <li className={`list-group-item`}>{tag}</li>;
+            })}
+          </Tooltip>
+        }
+      >
+        <span
+          className="badge rounded-pill bg-primary"
+          title={`${tagsInCommon.length} tags in common`}
+        >
+          {tagsInCommon.length} common tags
+        </span>
+      </OverlayTrigger>
+    )}
+  </div>
+);
