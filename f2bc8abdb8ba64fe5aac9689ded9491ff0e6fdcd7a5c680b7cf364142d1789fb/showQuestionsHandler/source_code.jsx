17ddf8a280477391e5681ed1a1a27b1a03b1272a
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-29 18:47:01 -0500

    Update showQuestionsHandler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-29 18:47:01.158

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showQuestionsHandler/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showQuestionsHandler/source_code.jsx
@@ -111,7 +111,7 @@ const renderQuestions = () => {
           backgroundColor: "white",
           borderRadius: "1rem",
           cursor: "pointer",
-          width: "50%",
+          width: "48%",
         }}
         onClick={() => {
           State.update({
