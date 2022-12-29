6d6c91c860040bb6f5560a7191f67114ca458628
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-28 19:39:13 -0500

    Update answer_poll-comment-container by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-28 19:39:13.532

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll-comment-container/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll-comment-container/source_code.jsx
@@ -1,45 +1,37 @@
 // console.log("props: ", props);
 const comment = props.comment;
-const answerTimeStamp = props.answerTimeStamp;
-const accountId = props.accountId;
+// const answerTimeStamp = props.answerTimeStamp;
+// const accountId = props.accountId;
 
-const profile = Social.getr(`${accountId}/profile`);
+// const profile = Social.getr(`${accountId}/profile`);
 
-const timeAgo = (diffSec) =>
-  diffSec < 60000
-    ? `${(diffSec / 1000) | 0} seconds ago`
-    : diffSec < 3600000
-    ? `${(diffSec / 60000) | 0} minutes ago`
-    : diffSec < 86400000
-    ? `${(diffSec / 3600000) | 0} hours ago`
-    : `${(diffSec / 86400000) | 0} days ago`;
+// const timeAgo = (diffSec) =>
+//   diffSec < 60000
+//     ? `${(diffSec / 1000) | 0} seconds ago`
+//     : diffSec < 3600000
+//     ? `${(diffSec / 60000) | 0} minutes ago`
+//     : diffSec < 86400000
+//     ? `${(diffSec / 3600000) | 0} hours ago`
+//     : `${(diffSec / 86400000) | 0} days ago`;
 
-const profileLink = (c) => (
-  <a
-    className="text-decoration-none link-dark"
-    href={`#/mob.near/widget/ProfilePage?accountId=${userMakingQuestion}`}
-  >
-    {c}
-  </a>
-);
+// const profileLink = (c) => (
+//   <a
+//     className="text-decoration-none link-dark"
+//     href={`#/mob.near/widget/ProfilePage?accountId=${userMakingQuestion}`}
+//   >
+//     {c}
+//   </a>
+// );
 
 return (
-  <div className="flex-column my-2 border border-primary p-2">
-    <small className="ps-1 text-nowrap text-muted ms-auto">
-      <i className="bi bi-clock me-1"></i>
-      {timeAgo(Date.now() - answerTimeStamp)}
-    </small>
-    <div className="flex-row">
-      {profileLink(
-        <div className="d-flex align-items-center">
-          <Widget src="mob.near/widget/ProfileImage" props={{ accountId }} />
-          <div>
-            <span className="fw-bold">{profile.name}</span>
-            <span className="text-secondary">@{accountId}</span>
-          </div>
-        </div>
-      )}
-    </div>
-    <p className="m-3">{comment}</p>
+  <div
+    style={{
+      width: "100%",
+      backgroundColor: "#F0F4FF5C",
+      borderRadius: "8px",
+      maxHeight: "max-content",
+    }}
+  >
+    <p className="m-3 p-1">{comment}</p>
   </div>
 );
