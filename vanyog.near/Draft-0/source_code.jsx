2a7348077605d4ed96a30b2dbc2079e42b4737c7
vanyog.near <vanyog.near>
2022-12-29 15:51:57 -0500

    Update Draft-0 by vanyog.near at 2022-12-29 15:51:57.247

--- vanyog.near/Draft-0/source_code.jsx
+++ vanyog.near/Draft-0/source_code.jsx
@@ -18,19 +18,26 @@ if (note === null) {
 State.init({ note: note || "" });
 
 return (
-  <div>
-    <div className="mb-2">
-      <h4>Notepad</h4>
-      <textarea
-        type="text"
-        rows={10}
-        className="form-control"
-        value={state.note}
-        onChange={(e) => State.update({ note: e.target.value })}
-      />
+  <div className="d-flex ">
+    <div className="ml-2">
+      <div className="mb-2">
+        <h4>Notepad</h4>
+        <textarea
+          type="text"
+          rows={10}
+          className="form-control"
+          value={state.note}
+          onChange={(e) => State.update({ note: e.target.value })}
+        />
+      </div>
+
+      <CommitButton data={{ experimental: { note: state.note } }}>
+        Save note
+      </CommitButton>
     </div>
-    <CommitButton data={{ experimental: { note: state.note } }}>
-      Save note
-    </CommitButton>
+
+    <div>
+      <Markdown text={state.note} />
+    </div>
   </div>
 );
