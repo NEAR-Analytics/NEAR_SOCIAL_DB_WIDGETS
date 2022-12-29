22b6ea368d6c80675825130641c128cf0223376f
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-28 21:34:14 -0500

    Update pollQuestionMasterHandler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-28 21:34:14.627

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/pollQuestionMasterHandler/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/pollQuestionMasterHandler/source_code.jsx
@@ -1,69 +1,161 @@
 const tabs = {
-  POLLS: { id: 0, text: "Polls" },
-  NEW_POLL: { id: 1, text: "New Poll" },
+  MY_POLLS: { id: 0, text: "My Polls" },
+  ALL_EXISTING_POLLS: { id: 1, text: "All existing polls" },
+  NEW_POLL: { id: 2, text: "Create a poll" },
 };
-const widgetOwner = "silkking.near";
+const widgetOwner =
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
 
 State.init({
-  displaying: tabs.POLLS.id,
+  displaying: tabs.MY_POLLS.id,
   hoveringElement: "",
 });
 
-const styleSelectedTab = {
-  borderTop: "1px solid rgba(0, 0, 0, 0.22)",
-  borderRight: "1px solid rgba(0, 0, 0, 0.22)",
-  borderLeft: "1px solid rgba(0, 0, 0, 0.22)",
-  color: "#1a73e8",
-  cursor: "pointer",
-  fontWeight: "500",
-};
-
-const styleUnselectedTab = {
-  cursor: "pointer",
-  fontWeight: "500",
-  color: "black",
-};
-
 return (
-  <div className="px-4 py-3" style={{ backgroundColor: "rgb(230, 230, 230)" }}>
-    <div className="d-flex justify-content-between align-items-center">
-      <h3 style={{ color: "#1a73e8" }}>LOGO</h3>
-      <ul
-        className="nav nav-tabs w-50 d-flex justify-content-start"
-        style={{ borderColor: "#00000038" }}
+  <div
+    style={{
+      backgroundColor: "rgb(230, 230, 230)",
+      fontFamily: "Onest",
+      fontStyle: "normal",
+      borderRadius: "20px",
+    }}
+  >
+    <div
+      className="d-flex justify-content-between align-items-center px-4 py-3"
+      style={{ backgroundColor: "white" }}
+    >
+      <div className="d-flex align-items-center">
+        <div
+          className="d-flex align-items-center justify-content-center"
+          style={{
+            backgroundColor: "#010A2D",
+            color: "white",
+            height: "100%",
+            minWidth: "2.5rem",
+            aspectRatio: "1",
+            borderRadius: "12px",
+          }}
+        >
+          <i className="bi bi-bar-chart-fill"></i>
+        </div>
+        <h3
+          style={{
+            margin: "0 0.5rem",
+            color: "#010A2D",
+            fontWeight: "700",
+            fontSize: "1.3rem",
+            letterSpacing: "0.1px",
+          }}
+        >
+          EasyPoll
+        </h3>
+      </div>
+      <div
+        className="w-100 d-flex justify-content-between"
+        style={{ margin: "0 2rem" }}
       >
-        {Object.keys(tabs).map((tabKey) => {
-          const tab = tabs[tabKey];
-          return (
-            <li className="nav-item">
-              <p
-                className={
-                  state.hoveringElement == tab.id || state.displaying == tab.id
-                    ? "nav-link active"
-                    : "nav-link"
-                }
-                ariaCurrent="page"
-                onMouseEnter={() => {
-                  State.update({ hoveringElement: tab.id });
-                }}
-                onMouseLeave={() => {
-                  State.update({ hoveringElement: "" });
-                }}
-                onClick={() => {
-                  State.update({ displaying: tab.id });
-                }}
-                style={
-                  state.hoveringElement == tab.id || state.displaying == tab.id
-                    ? styleSelectedTab
-                    : styleUnselectedTab
-                }
-              >
-                {tab.text}
-              </p>
-            </li>
-          );
-        })}
-      </ul>
+        <div style={{ marginTop: "0.6rem" }}>
+          <div className="d-flex">
+            {Object.keys(tabs).map((tabKey) => {
+              const tab = tabs[tabKey];
+              if (tabKey != "NEW_POLL") {
+                return (
+                  <div
+                    style={{
+                      marginRight: "1.5rem",
+                      position: "relative",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <p
+                      ariaCurrent="page"
+                      onMouseEnter={() => {
+                        State.update({ hoveringElement: tab.id });
+                      }}
+                      onMouseLeave={() => {
+                        State.update({ hoveringElement: "" });
+                      }}
+                      onClick={() => {
+                        State.update({ displaying: tab.id });
+                      }}
+                      style={{
+                        fontWeight: "500",
+                        fontSize: "1rem",
+                        margin: "0",
+                      }}
+                    >
+                      {tab.text}
+                    </p>
+                    {(state.hoveringElement == tab.id ||
+                      state.displaying == tab.id) && (
+                      <div
+                        style={{
+                          height: "0.2rem",
+                          width: "50%",
+                          position: "absolute",
+                          bottom: "-55%",
+                          left: "25%",
+                          backgroundColor: "#010A2D",
+                          borderRadius: "8px",
+                        }}
+                      >
+                        {/*Decorative Div, do not delete*/}
+                      </div>
+                    )}
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </div>
+
+        <div className="d-flex">
+          <button
+            onMouseEnter={() => {
+              State.update({ hoveringElement: tabs.NEW_POLL.id });
+            }}
+            onMouseLeave={() => {
+              State.update({ hoveringElement: "" });
+            }}
+            onClick={() => {
+              State.update({ displaying: tabs.NEW_POLL.id });
+            }}
+            style={
+              state.hoveringElement == tabs.NEW_POLL.id
+                ? {
+                    border: "2px solid black",
+                    color: "black",
+                    backgroundColor: "white",
+                    fontWeight: "500",
+                    fontSize: "1rem",
+                    margin: "0",
+                    padding: "0.5rem 1.5rem",
+                    borderRadius: "12px",
+                  }
+                : {
+                    border: "2px solid transparent",
+                    fontWeight: "500",
+                    fontSize: "1rem",
+                    margin: "0",
+                    padding: "0.5rem 1.5rem",
+                    backgroundColor: "#010A2D",
+                    borderRadius: "12px",
+                    color: "white",
+                  }
+            }
+          >
+            <i
+              className="bi bi-plus-lg"
+              style={
+                state.hoveringElement == tabs.NEW_POLL.id
+                  ? { color: "black" }
+                  : { color: "white" }
+              }
+            ></i>
+            {tabs.NEW_POLL.text}
+          </button>
+        </div>
+      </div>
       <div
         className="p-2"
         style={{ border: "2px solid #1a73e8", borderRadius: "1rem" }}
@@ -72,11 +164,28 @@ return (
       </div>
     </div>
 
-    {state.displaying == tabs.POLLS.id ? (
-      <Widget src={`${widgetOwner}/widget/showQuestionsHandler`} />
+    {state.displaying == tabs.ALL_EXISTING_POLLS.id ? (
+      <div className="px-4">
+        <h2 style={{ margin: "2rem 0 0.5rem 0", fontWeight: "700" }}>
+          All existing polls
+        </h2>
+        <Widget src={`${widgetOwner}/widget/showQuestionsHandler`} />
+      </div>
+    ) : state.displaying == tabs.MY_POLLS.id ? (
+      <div className="px-4">
+        <h2 style={{ margin: "2rem 0 0.5rem 0", fontWeight: "700" }}>
+          My Polls
+        </h2>
+        <Widget
+          src={`${widgetOwner}/widget/showQuestionsHandler`}
+          props={{ onlyUser: true }}
+        />
+      </div>
     ) : (
       state.displaying == tabs.NEW_POLL.id && (
-        <Widget src={`${widgetOwner}/widget/newPollQuestionInterface`} />
+        <div className="px-4">
+          <Widget src={`${widgetOwner}/widget/newPollQuestionInterface`} />
+        </div>
       )
     )}
   </div>
