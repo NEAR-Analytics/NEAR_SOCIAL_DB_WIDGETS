f31c6c666cdac15a1dace2f3b01a948b9709fb1f
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-28 22:35:42 -0500

    Update minimalistQuestionHeader by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-28 22:35:42.021

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/minimalistQuestionHeader/source_code.jsx
@@ -0,0 +1,28 @@
+let question = props.question;
+
+if (!question) {
+  return "Prop passed wrongly to minimalistQuestionHeader";
+}
+
+return (
+  <div className="d-flex justify-content-start">
+    <div
+      style={{
+        backgroundColor: "#F2F6FA",
+        marginRight: "0.8rem",
+      }}
+    >
+      {question.value.questionType == "0" ? (
+        <i className="bi bi-file-text" style={{ padding: "0.8rem" }}></i>
+      ) : (
+        questionParams.value.questionType == "1" && (
+          <i
+            style={{ transform: "rotate(90deg)", padding: "0.8rem" }}
+            className="bi bi-bar-chart-line"
+          ></i>
+        )
+      )}
+    </div>
+    <h3 style={{ wordWrap: "anywhere" }}>{question.value.title}</h3>
+  </div>
+);
