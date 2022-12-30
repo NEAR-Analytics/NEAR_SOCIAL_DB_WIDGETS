680e91dadae0c541acbb8c46a4bcf3c19571975f
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2022-12-29 22:19:26 -0500

    Update questionsByCreator by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2022-12-29 22:19:26.185

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/questionsByCreator/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/questionsByCreator/source_code.jsx
@@ -31,55 +31,109 @@ function getValidAnswersQtyFromQuestion(questionBlockHeight) {
   return usersWithAnswersWithoutDuplicates.length;
 }
 
-return props.questionsByThisCreator.map((questionByCreator, index) => {
-  let divStyle =
-    index == 0
-      ? {}
-      : { backGroundColor: "white", borderTop: "1px solid #ced4da" };
-  if ((isShort && index < 2) || !isShort) {
-    return (
-      <div style={divStyle}>
-        <p style={{ fontWeight: "500" }}>
-          {sliceString(questionByCreator.value.title, 20)}
-        </p>
-        <div className="d-flex justify-content-between flex-nowrap text-secondary mb-2">
-          <div>
-            <i className="bi bi-people"></i>
-            <span>
-              {getValidAnswersQtyFromQuestion(questionByCreator.blockHeight)}
-            </span>
-          </div>
-          <span>
-            Ends
-            <Widget
-              src={`silkking.near/widget/timeAgo`}
-              props={{ timeInFuture: questionByCreator.value.endTimestamp }}
-            />
-          </span>
-          <span
-            style={{
-              backgroundColor: isQuestionActive(questionByCreator)
-                ? "#D9FCEF"
-                : "#FFE5E5",
-
-              height: "1.5rem",
-              width: "4rem",
-              textAlign: "center",
-              borderRadius: "16px",
-              marginRight: "1rem",
-              lineHeight: "1.5rem",
-              fontSize: "0.8rem",
-              letterSpacing: "-0.025rem",
-              color: isQuestionActive(questionByCreator)
-                ? "#00B37D"
-                : "#FF4747",
-              fontWeight: "500",
-            }}
+return (
+  <>
+    {props.questionsByThisCreator.map((questionByCreator, index) => {
+      if ((isShort && index < 2) || !isShort) {
+        return (
+          <div
+            style={
+              index == 0
+                ? {}
+                : {
+                    backGroundColor: "white",
+                    borderTop: "1px solid #ced4da",
+                    marginTop: "1rem",
+                  }
+            }
           >
-            {isQuestionActive(questionByCreator) ? "Active" : "Closed"}
-          </span>
-        </div>
-      </div>
-    );
-  }
-});
+            <div className="d-flex align-content-center">
+              <div
+                className="d-flex justify-content-center"
+                style={{
+                  maxHeight: "2.8rem",
+                  aspectRatio: "1",
+                  borderRadius: "16px",
+                  backgroundColor: "#F2F6FA",
+                  marginRight: "0.8rem",
+                }}
+              >
+                {questionByCreator.value.questionType == "0" ? (
+                  <i
+                    className="bi bi-file-text"
+                    style={{ padding: "0.6rem 0.8rem" }}
+                  ></i>
+                ) : (
+                  questionByCreator.value.questionType == "1" && (
+                    <i
+                      style={{
+                        transform: "rotate(90deg)",
+                        padding: "0.6rem 0.8rem",
+                      }}
+                      className="bi bi-bar-chart-line"
+                    ></i>
+                  )
+                )}
+              </div>
+              <p style={{ margin: "0.5rem 0 0 0", fontWeight: "500" }}>
+                {sliceString(questionByCreator.value.title, 20)}
+              </p>
+            </div>
+            <div className="d-flex">
+              <div
+                className="d-flex justify-content-center"
+                style={{
+                  width: "2.8rem",
+                  marginRight: "0.8rem",
+                }}
+              >
+                {/*Structural div, do not delete*/}
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-secondary mb-2">
+                <div>
+                  <i className="bi bi-people"></i>
+                  <span>
+                    {getValidAnswersQtyFromQuestion(
+                      questionByCreator.blockHeight
+                    )}
+                  </span>
+                </div>
+                <span>
+                  Ends
+                  <Widget
+                    src={`silkking.near/widget/timeAgo`}
+                    props={{
+                      timeInFuture: questionByCreator.value.endTimestamp,
+                    }}
+                  />
+                </span>
+                <span
+                  style={{
+                    backgroundColor: isQuestionActive(questionByCreator)
+                      ? "#D9FCEF"
+                      : "#FFE5E5",
+
+                    height: "1.5rem",
+                    width: "4rem",
+                    textAlign: "center",
+                    borderRadius: "16px",
+                    marginRight: "1rem",
+                    lineHeight: "1.5rem",
+                    fontSize: "0.8rem",
+                    letterSpacing: "-0.025rem",
+                    color: isQuestionActive(questionByCreator)
+                      ? "#00B37D"
+                      : "#FF4747",
+                    fontWeight: "500",
+                  }}
+                >
+                  {isQuestionActive(questionByCreator) ? "Active" : "Closed"}
+                </span>
+              </div>
+            </div>
+          </div>
+        );
+      }
+    })}
+  </>
+);
