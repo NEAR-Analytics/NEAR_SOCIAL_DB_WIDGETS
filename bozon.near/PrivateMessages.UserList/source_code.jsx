378e2e0544d5525108c55c56f63dabfaaffe0dce
ligebit <ligebit>
2023-01-02 15:59:47 -0500

    Update PrivateMessages.UserList by bozon.near at 2023-01-02 15:59:47.045

---
+++ bozon.near/PrivateMessages.UserList/source_code.jsx
@@ -0,0 +1,84 @@
+const accountId = context.accountId;
+
+if (!accountId) {
+  return "Please sign in with NEAR wallet";
+}
+
+const secretKey = props.secretKey;
+
+if (!secretKey) {
+  return "Send secretKey in props";
+}
+
+const follows = Social.get(`${accountId}/graph/follow/**`);
+
+const allFollowers = follows
+  ? Object.keys(follows).map((f) => {
+      return f;
+    })
+  : [];
+
+State.init({
+  userList: allFollowers,
+});
+
+return (
+  <div>
+    <h1 class="mb-3 text-center">Private Messages</h1>
+
+    <input
+      class="mb-3 form-control"
+      placeholder="Input username"
+      onChange={(e) => {
+        const newFollowersArray = allFollowers.filter(
+          (accountId) => accountId.indexOf(e.target.value) !== -1
+        );
+        const a = Social.get(`${e.target.value}/**`);
+
+        if (a) newFollowersArray.unshift(e.target.value);
+
+        State.update({
+          userList: newFollowersArray,
+        });
+      }}
+    ></input>
+
+    {allFollowers.length === 0 && <p>You don't have any followers yet</p>}
+    {state.userList.map((follower, i) => {
+      const followerRegisteredPublicKey = Social.get(
+        `${follower}/private_messages/public_key`
+      );
+
+      return (
+        <div
+          class="mb-3 d-flex flex-row align-items-center justify-content-between"
+          key={i}
+        >
+          <Widget
+            src="mob.near/widget/Profile.ShortInlineBlock"
+            props={{
+              accountId: follower,
+            }}
+          />
+
+          {!followerRegisteredPublicKey ? (
+            <span
+              style={{ lineHeight: "1.5rem" }}
+              className="badge text-bg-secondary"
+            >
+              User not registered
+            </span>
+          ) : (
+            <button
+              onClick={() => {
+                State.update({ selectedUser: follower });
+              }}
+            >
+              Send message
+            </button>
+          )}
+        </div>
+      );
+    })}
+  </div>
+);
