4bf2de25688b8598028276a3292690423204761d
joannaorlova.near <joannaorlova.near>
2023-01-02 22:35:23 -0500

    Update EventPlanner by joannaorlova.near at 2023-01-02 22:35:23.457

---
+++ joannaorlova.near/EventPlanner/source_code.jsx
@@ -0,0 +1,30 @@
+const accountId = props.accountId ?? context.accountId;
+
+return (
+  <div>
+    <h4>Plan or join group activities on Near Social!</h4>
+    <a
+      href={`#/minz.near/widget/CreateActivity`}
+      className="text-decoration-none link-dark"
+    >
+      <button>Start a new event!</button>
+    </a>
+
+    <a
+      href={`#/minz.near/widget/OwnedActivities?accountId=${accountId}`}
+      className="text-decoration-none link-dark"
+    >
+      <button>My events</button>
+    </a>
+
+    <a
+      href={`#/minz.near/widget/JoinedActivities?accountId=${accountId}`}
+      className="text-decoration-none link-dark"
+    >
+      <button>My joined events</button>
+    </a>
+    <br />
+    <br />
+    <Widget src={`minz.near/widget/AllActivities`} props={{ event_id }} />
+  </div>
+);
