5201ba4e3dc15175f56a9e246babeb175be2db83
devgovgigs.near <devgovgigs.near>
2023-01-04 00:20:22 -0500

    Update PostEditor by devgovgigs.near at 2023-01-04 00:20:22.204

---
+++ devgovgigs.near/PostEditor/source_code.jsx
@@ -0,0 +1,152 @@
+const ownerId = "devgovgigs.near";
+const postType = props.postType ?? "Sponsorship";
+const parentId = props.parentId ?? null;
+
+initState({
+  author_id: context.accountId,
+  labels: [],
+  postType,
+  name: "",
+  description: "",
+  amount: "0",
+  token: "Near",
+  supervisor: "",
+});
+
+let fields = {
+  Comment: ["description"],
+  Idea: ["name", "description"],
+  Submission: ["name", "description"],
+  Attestation: ["name", "description"],
+  Sponsorship: [
+    "name",
+    "description",
+    "amount",
+    "sponsorship_token",
+    "supervisor",
+  ],
+}[postType];
+
+const onClick = () => {
+  let labels = [];
+  let body = {
+    Comment: { description: state.description },
+    Idea: { name: state.name, description: state.description },
+    Submission: { name: state.name, description: state.description },
+    Attestation: { name: state.name, description: state.description },
+    Sponsorship: {
+      name: state.name,
+      description: state.description,
+      amount: state.amount,
+      sponsorship_token: state.token,
+      supervisor: state.supervisor,
+    },
+  }[postType];
+
+  Near.call(ownerId, "add_post", {
+    parent_id: parentId,
+    labels,
+    body,
+  });
+};
+
+const nameDiv = fields.includes("name") ? (
+  <div className="col-lg-6  mb-2">
+    Title:
+    <input
+      type="text"
+      value={state.name}
+      onChange={(event) => State.update({ name: event.target.value })}
+    />
+  </div>
+) : null;
+
+const descriptionDiv = fields.includes("description") ? (
+  <div className="col-lg-12  mb-2">
+    Description:
+    <br />
+    <textarea
+      value={state.description}
+      type="text"
+      rows={6}
+      className="form-control"
+      onChange={(event) => State.update({ description: event.target.value })}
+    />
+  </div>
+) : null;
+
+const amountDiv = fields.includes("amount") ? (
+  <div className="col-lg-6  mb-2">
+    Amount:
+    <input
+      type="text"
+      value={state.amount}
+      onChange={(event) => State.update({ amount: event.target.value })}
+    />
+  </div>
+) : null;
+
+const tokenDiv = fields.includes("sponsorship_token") ? (
+  <div className="col-lg-6  mb-2">
+    Tokens:
+    <input
+      type="text"
+      value={state.token}
+      onChange={(event) => State.update({ token: event.target.value })}
+    />
+  </div>
+) : null;
+
+const supervisorDiv = fields.includes("supervisor") ? (
+  <div className="col-lg-6 mb-2">
+    Supervisor:
+    <input
+      type="text"
+      value={state.supervisor}
+      onChange={(event) => State.update({ supervisor: event.target.value })}
+    />
+  </div>
+) : null;
+
+return (
+  <div className="card">
+    <div className="card-header">{postType} Editor</div>
+
+    <div class="card-body">
+      <div className="row">
+        {nameDiv}
+        {amountDiv}
+        {tokenDiv}
+        {supervisorDiv}
+        {descriptionDiv}
+      </div>
+      <a className="btn btn-outline-primary mb-2" onClick={onClick}>
+        Submit
+      </a>
+    </div>
+    <div class="card-footer">
+      Preview:
+      <Widget
+        src={`${ownerId}/widget/Post`}
+        props={{
+          isPreview: true,
+          id: 0, // irrelevant
+          post: {
+            author_id: state.author_id,
+            likes: [],
+            snapshot: {
+              editor_id: state.editor_id,
+              labels: state.labels,
+              post_type: postType,
+              name: state.name,
+              description: state.description,
+              amount: state.amount,
+              sponsorship_token: state.token,
+              supervisor: state.supervisor,
+            },
+          },
+        }}
+      />
+    </div>
+  </div>
+);
