05486f4e4d34dba2ddaba1b7f3ca8a2aa24ce248
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-01-03 23:45:05 -0500

    Update showQuestionsHandler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-01-03 23:45:05.682

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showQuestionsHandler/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showQuestionsHandler/source_code.jsx
@@ -169,6 +169,7 @@ const renderQuestions = (onlyUsersPolls) => {
     return (
       <>
         {usersMakingQuestions.map((accountId, index) => {
+          console.log("accountId: ", accountId);
           return (
             <div
               className="mx-1 py-3 px-4 my-2"
