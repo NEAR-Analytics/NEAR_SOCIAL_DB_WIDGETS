a177e66386aca8b47f9288ead94b5563dba2519c
ligebit <ligebit>
2023-01-03 20:37:36 -0500

    Update PrivateMessage.Message by bozon.near at 2023-01-03 20:37:36.569

--- bozon.near/PrivateMessage.Message/source_code.jsx
+++ bozon.near/PrivateMessage.Message/source_code.jsx
@@ -42,7 +42,13 @@ const encryptedMessage = messageWithNonceUint8Array.slice(
 const messageTextUint8Array = nacl.box.open(
   encryptedMessage,
   nonce,
-  publicKey,
+  messageObject.receiver_public_key_base64 != publicKeyBase64
+    ? new Uint8Array(
+        new Buffer(messageObject.receiver_public_key_base64, "base64")
+      )
+    : new Uint8Array(
+        new Buffer(messageObject.sender_public_key_base64, "base64")
+      ),
   new Uint8Array(new Buffer(props.secretKeyBase64, "base64"))
 );
 
