5a6f06d2ba6da66ae0f4953fb15967c0823cb01e
eugenewolf507.near <eugenewolf507.near>
2023-01-05 15:32:17 -0500

    Update TestWiki_CreateArticle by eugenewolf507.near at 2023-01-05 15:32:17.610

--- eugenewolf507.near/TestWiki_CreateArticle/source_code.jsx
+++ eugenewolf507.near/TestWiki_CreateArticle/source_code.jsx
@@ -1,20 +1,50 @@
+State.init({});
+
 return (
   <div>
-    <button
-      type="button"
-      onClick={() => {
-        console.log("Save button clicked");
-      }}
-    >
-      Save
-    </button>
-    <button
-      type="button"
-      onClick={() => {
-        console.log("Cancel button clicked");
-      }}
-    >
-      Cancel
-    </button>
+    <div>
+      <button
+        type="button"
+        className="btn btn-success"
+        onClick={() => {
+          console.log("save article");
+          console.log(state);
+        }}
+      >
+        Save Article{" "}
+      </button>
+      <button
+        type="button"
+        className="btn btn-danger"
+        onClick={() => {
+          console.log("cancel");
+          State.update({ note: article.body });
+        }}
+      >
+        Cancel{" "}
+      </button>
+    </div>
+    <div>
+      Input article id (case-sensitive):
+      <input
+        onChange={(e) => {
+          console.log("e", e.data);
+          State.update({ ...state, articleId: e.target.value });
+        }}
+      />
+    </div>
+    <div>
+      Input article body (in makrdown format):
+      <textarea
+        id="textarea1"
+        type="text"
+        rows={10}
+        className="form-control mt-2"
+        onChange={(e) => {
+          console.log("e", e.data);
+          State.update({ ...state, articleBody: e.target.value });
+        }}
+      />
+    </div>
   </div>
 );
