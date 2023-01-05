dc029f8ecb0a1f1ddcc8e01577e18f8ff82e5b40
eugenewolf507.near <eugenewolf507.near>
2023-01-05 14:41:43 -0500

    Update TestWiki_CreateArticle by eugenewolf507.near at 2023-01-05 14:41:43.011

---
+++ eugenewolf507.near/TestWiki_CreateArticle/source_code.jsx
@@ -0,0 +1,20 @@
+return (
+  <div>
+    <button
+      type="button"
+      onClick={() => {
+        console.log("Save button clicked");
+      }}
+    >
+      Save
+    </button>
+    <button
+      type="button"
+      onClick={() => {
+        console.log("Cancel button clicked");
+      }}
+    >
+      Cancel
+    </button>
+  </div>
+);
