29a09efe9e9756e1cc374470007308dc54ea39e0
devgovgigs.near <devgovgigs.near>
2023-01-06 00:05:08 -0500

    Update IdeasList by devgovgigs.near at 2023-01-06 00:05:08.907

--- devgovgigs.near/IdeasList/source_code.jsx
+++ devgovgigs.near/IdeasList/source_code.jsx
@@ -2,6 +2,9 @@ const ownerId = "devgovgigs.near";
 const postIds =
   props.postIds ?? Near.view(ownerId, "get_children_ids").reverse();
 
+console.log("ids");
+console.log(props.postIds);
+
 return (
   <div>
     {postIds
