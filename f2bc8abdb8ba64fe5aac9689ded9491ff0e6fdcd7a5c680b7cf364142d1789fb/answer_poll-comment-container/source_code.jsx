7bcf9e5984244cf110f8bb70c8668b56667a1a12
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-01-06 12:46:42 -0500

    Update answer_poll-comment-container by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-01-06 12:46:42.385

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll-comment-container/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll-comment-container/source_code.jsx
@@ -2,19 +2,19 @@ if (!props.validAnswersToThisPoll) {
   return "Prop validAnswersToThisPoll not set";
 }
 
-const answers = props.validAnswersToThisPoll;
-const questionNumber = props.questionNumber;
+if (!props.questionNumber) {
+  return "Prop questionNumber not set";
+}
+
+let answers = props.validAnswersToThisPoll;
+let questionNumber = props.questionNumber;
 
 return (
   <>
-    {answers.map(answer, (index) => {
-      if (questionNumber == index) {
-        return (
-          <div className="flex-column my-2 border border-primary p-2">
-            <p className="m-3">{answer}</p>
-          </div>
-        );
-      }
+    {answers.map((answer, index) => {
+      <div className="flex-column my-2 border border-primary p-2">
+        if(questionNumber == index) return (<p className="m-3">{answer}</p>);
+      </div>;
     })}
   </>
 );
