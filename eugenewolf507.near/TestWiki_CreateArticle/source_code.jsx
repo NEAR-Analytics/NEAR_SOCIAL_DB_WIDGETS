402bc6c1c48cddd0f4487503ff150b30743ffb4e
eugenewolf507.near <eugenewolf507.near>
2023-01-05 21:39:15 -0500

    Update TestWiki_CreateArticle by eugenewolf507.near at 2023-01-05 21:39:15.915

--- eugenewolf507.near/TestWiki_CreateArticle/source_code.jsx
+++ eugenewolf507.near/TestWiki_CreateArticle/source_code.jsx
@@ -1,32 +1,60 @@
 const initialBody = `# Markdown heading level 1
 
-This is a markdown paragraph. Here are a few examples of markdown syntax and what it looks like.
+This is a markdown paragraph. So, here are a few examples of markdown syntax and what it looks like.
 
 1. markdown
 2. ordered
 3. list`;
 
-State.init({ articleId: "", articleBody: initialBody });
+const errTextNoBody = "ERROR: no article Body",
+  errTextNoId = "ERROR: no article Id",
+  errTextDublicatedId = "ERROR: there is article with such name";
+
+const initialState = {
+  articleId: "",
+  articleBody: initialBody,
+  errorId: "",
+  errorBody: "",
+};
+
+State.init(initialState);
 
 // === SAVE HANDLER ===
 const saveHandler = () => {
+  State.update({ ...state, errorId: "", errorBody: "" });
   if (state.articleId && state.articleBody) {
-    console.log("save article");
-    console.log(state);
+    const articles = Near.view("thewiki.near", "get_article_ids_paged", {
+      from_index: 0,
+      limit: 250,
+    });
+    const isArticleIdDublicated = articles.some(
+      (id) => id.toLowerCase() === state.articleId.toLowerCase()
+    );
+    if (!isArticleIdDublicated) {
+      const idWithoutSpaces = state.articleId.replace(/\s+/g, "");
+      State.update({ ...state, articleId: idWithoutSpaces });
+      console.log("SAVE ARTICLE");
+    } else {
+      State.update({ ...state, errorId: errTextDublicatedId });
+    }
   } else {
     if (!state.articleId) {
-      console.log("ERROR: no article Id");
+      State.update({ ...state, errorId: errTextNoId });
     }
     if (!state.articleBody) {
-      console.log("ERROR: no article Body");
+      State.update({ ...state, errorBody: errTextNoBody });
     }
   }
 };
 
 // === CANCEL HANDLER ===
 const cancelHandler = () => {
-  console.log("cancel");
-  State.update({ ...state, articleId: "", articleBody: "" });
+  State.update({
+    articleId: "",
+    articleBody: "",
+    errorId: null,
+    errorBody: null,
+  });
 };
 
 // === RETURN ===
@@ -48,20 +76,28 @@ return (
         Cancel / Clear
       </button>
     </div>
-    <div>
-      <label for="inputArticleId">Input article id (case-sensitive):</label>
+    <div class="d-flex flex-column pt-3">
+      <label for="inputArticleId">
+        Input article id (case-sensitive, without spaces):
+      </label>
+      <label for="inputArticleId" class="small text-danger">
+        {state.errorId}
+      </label>
       <input
+        className="form-control mt-2"
         id="inputArticleId"
         value={state.articleId}
         onChange={(e) => {
-          console.log("e", e.data);
           State.update({ ...state, articleId: e.target.value });
         }}
       />
     </div>
-    <div>
+    <div class="d-flex flex-column pt-3">
       <label for="textareaArticleBody">
         Input article body (in makrdown format):
+      </label>
+      <label for="textareaArticleBody" class="small text-danger">
+        {state.errorBody}
       </label>
       <textarea
         id="textareaArticleBody"
@@ -70,12 +106,11 @@ return (
         rows={10}
         className="form-control mt-2"
         onChange={(e) => {
-          console.log("e", e.data);
           State.update({ ...state, articleBody: e.target.value });
         }}
       />
     </div>
-    <div>
+    <div class="pt-3">
       Article preview:
       <Markdown text={state.articleBody} />
     </div>
