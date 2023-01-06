c4641d781a47d5924987fb42d644d3e159c269b6
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-01-06 13:15:11 -0500

    Update answer_poll-comment-container by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-01-06 13:15:11.997

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll-comment-container/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/answer_poll-comment-container/source_code.jsx
@@ -12,9 +12,11 @@ let questionNumber = props.questionNumber;
 return (
   <>
     {answers.map((answer) => {
-      <div className="flex-column my-2 border border-primary p-2">
-        <p className="m-3">{answer.value.answer[questionNumber]}</p>
-      </div>;
+      return (
+        <div className="flex-column my-2 border border-primary p-2">
+          <p className="m-3">{answer.value.answer[questionNumber]}</p>
+        </div>
+      );
     })}
   </>
 );
