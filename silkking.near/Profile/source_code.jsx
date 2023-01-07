f33b745a0cda3410c2f56f1927232b4fcd0a8189
silkking.near <silkking.near>
2023-01-06 19:43:13 -0500

    Update Profile by silkking.near at 2023-01-06 19:43:13.415

---
+++ silkking.near/Profile/source_code.jsx
@@ -0,0 +1,56 @@
+const accountId = props.userMakingQuestion ?? context.accountId;
+
+State.init({
+  profile: {},
+});
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+if (JSON.stringify(profile) != JSON.stringify(state.profile)) {
+  State.update({ profile: profile });
+}
+
+if (!profile) {
+  return "Loading";
+}
+
+const name = profile.name;
+const image = profile.image;
+
+function makeAccountIdShorter(accountId) {
+  if (accountId.length > 12) {
+    return accountId.slice(0, 12) + "...";
+  }
+  return accountId;
+}
+
+return (
+  <div className="profile d-inline-block">
+    <a
+      href={`#/mob.near/widget/ProfilePage?accountId=${accountId}`}
+      className="text-decoration-none link-dark"
+    >
+      <Widget
+        src="mob.near/widget/ProfileImage"
+        props={{
+          profile,
+          accountId,
+          className: "float-start d-inline-block me-2",
+        }}
+      />
+      <div className="profile-info d-inline-block" style={{ maxWidth: "16em" }}>
+        <div
+          className="profile-name text-truncate"
+          style={{ fontWeight: "500" }}
+        >
+          {name || "No-name profile"}
+        </div>
+        <div className="profile-links d-flex">
+          <div className="d-inline-block profile-account text-secondary text-truncate">
+            @{makeAccountIdShorter(accountId)}
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+);
