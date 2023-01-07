d99d5684c0743e1b6606e9f6efd129554930926a
devgovgigs.near <devgovgigs.near>
2023-01-07 06:04:15 -0500

    Update CompactPost by devgovgigs.near at 2023-01-07 06:04:15.729

--- devgovgigs.near/CompactPost/source_code.jsx
+++ devgovgigs.near/CompactPost/source_code.jsx
@@ -1,6 +1,9 @@
 const ownerId = "devgovgigs.near";
 const postId = props.post.id ?? (props.id ? parseInt(props.id) : 0);
 const post = props.post ?? Near.view(ownerId, "get_post", { post_id: postId });
+if (!post) {
+  return <div>Loading ...</div>;
+}
 const snapshot = post.snapshot;
 
 const shareButton = (
