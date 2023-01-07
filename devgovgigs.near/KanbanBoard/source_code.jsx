068acfded27867a07131f752c1807960c75d8149
devgovgigs.near <devgovgigs.near>
2023-01-07 01:33:14 -0500

    Update KanbanBoard by devgovgigs.near at 2023-01-07 01:33:14.554

--- devgovgigs.near/KanbanBoard/source_code.jsx
+++ devgovgigs.near/KanbanBoard/source_code.jsx
@@ -15,5 +15,31 @@ return (
         </div>
       </div>
     </div>
+    <div class="col">
+      <div class="card">
+        <div class="card-header">
+          <span class="badge text-bg-primary">wg-protocol-discussion</span>
+        </div>
+        <div class="card-body border-secondary">
+          <Widget
+            src={`${ownerId}/widget/Post`}
+            props={{ id: 2, isCompact: true }}
+          />
+        </div>
+      </div>
+    </div>
+    <div class="col-sm">
+      <div class="card">
+        <div class="card-header">
+          <span class="badge text-bg-primary">wg-protocol-discussion</span>
+        </div>
+        <div class="card-body border-secondary">
+          <Widget
+            src={`${ownerId}/widget/Post`}
+            props={{ id: 0, isCompact: true }}
+          />
+        </div>
+      </div>
+    </div>
   </div>
 );
