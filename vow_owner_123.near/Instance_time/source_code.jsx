8aa8ddabdaa894bab03d325918b6b4ad193c463f
vow_owner_123.near <vow_owner_123.near>
2023-01-08 12:45:30 -0500

    Update Instance_time by vow_owner_123.near at 2023-01-08 12:45:30.433

--- vow_owner_123.near/Instance_time/source_code.jsx
+++ vow_owner_123.near/Instance_time/source_code.jsx
@@ -21,53 +21,18 @@ const button = {
   borderRadius: "5px",
   margin: "5px 0",
   padding: "8px",
+  marginTop: "10px",
   textAlign: "center",
   background: "linear-gradient(to right, #4deeea, #f000ff)",
   border: "2px solid black",
   fontWeight: "bold",
 };
-const imgWH = {
-  width: "25px",
-  height: "25px",
+const table = {
+  display: "flex",
+  flex: "1",
+  marginTop: "1rem",
 };
-const time_zones = [
-  "(UTC-11:00) Samoa",
-  "(UTC-10:00) Hawaii",
-  "(UTC-09:00) Alaska",
-  "(UTC-08:00) Pacific Time (US; Canada)",
-  "(UTC-07:00) Arizona",
-  "(UTC-06:00) Central America",
-  "(UTC-06:00) Saskatchewan",
-  "(UTC-05:00) Eastern Time (US; Canada)",
-  "(UTC-04:00) Atlantic Time (Canada)",
-  "(UTC-04:30) Caracas",
-  "(UTC-04:00) Santiago",
-  "(UTC-03:30) Newfoundland",
-  "(UTC-03:00) Brasilia",
-  "(UTC-02:00) Mid-Atlantic",
-  "(UTC-01:00) Azores",
-  "(UTC+00:00) UTC",
-  "(UTC+01:00) Amsterdam",
-  "(UTC+02:00) Athens",
-  "(UTC+03:00) Baghdad",
-  "(UTC+04:00) Abu Dhabi",
-  "(UTC+04:30) Kabul",
-  "(UTC+05:00) Islamabad",
-  "(UTC+05:30) Chennai",
-  "(UTC+05:45) Kathmandu",
-  "(UTC+06:00) Almaty",
-  "(UTC+06:30) Rangoon",
-  "(UTC+07:00) Bangkok",
-  "(UTC+08:00) Beijing",
-  "(UTC+09:00) Irkutsk",
-  "(UTC+09:00) Seoul",
-  "(UTC+09:00) Tokyo",
-  "(UTC+09:30) Adelaide",
-  "(UTC+10:00) Guam",
-  "(UTC+11:00) Vladivostok",
-  "(UTC+12:00) Auckland",
-  "(UTC+13:00) Nuku'alofa",
-];
+const time_zones = [];
 const hours = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
   22, 23, 24,
@@ -126,6 +91,45 @@ const sortAndRemoveRepeated = (flag, data) => {
     if (!repeated) final.push(sortedTimeData[k]);
   }
   return final;
+};
+const getData = () => {
+  var zone = state._time_zone.split(" ")[0].split("UTC")[1].split(":");
+  var hours = parseInt(zone[0]);
+  var offset = hours + (parseInt(zone[1]) / 60) * ((hours > 0) * 2 - 1);
+  var temp = [];
+  var flag = false;
+  for (var i = 0; i < 7; i++) {
+    if (state._is_on[i] == "on") {
+      for (var j = 0; j < 2; j++) {
+        const time =
+          j == 0
+            ? parseInt(state._from[i]) + 24 * i - offset
+            : parseInt(state._to[i]) + 24 * i - offset;
+        if (time > 168) {
+          temp.push(time - 168);
+          flag = true;
+        } else if (time < 0) {
+          temp.push(time + 168);
+          flag = true;
+        } else temp.push(time);
+      }
+    }
+  }
+  const final = sortAndRemoveRepeated(flag, temp);
+  return {
+    index: {
+      Instance_time: JSON.stringify(
+        {
+          key: "data",
+          value: {
+            _data: final,
+          },
+        },
+        undefined,
+        0
+      ),
+    },
+  };
 };
 for (let i = 0; i < sortedData.length; i++) {
   if (accountIds.indexOf(sortedData[i].accountId) < 0) {
@@ -186,28 +190,45 @@ return (
   <div>
     <br />
     <br />
-    <h3>Weekly Schedule</h3>
-    <p>current_user: {context.accountId}</p>
-    <div className="d-flex">
-      <p>Select Time zone: </p>
-      <select
-        name="zones"
-        id="zones"
-        value={state._time_zone}
-        onChange={(e) => {
-          State.update({ _time_zone: e.target.value });
-        }}
-      >
-        {time_zones.map((zone) => (
-          <option value={zone}>{zone}</option>
-        ))}
-      </select>
+    <div className="d-flex flex-column align-self-center">
+      <h3>Weekly Schedule</h3>
+      <div className="d-flex flex-row align-items-center">
+        <div className="p-2" style={table}>
+          current_user:
+        </div>
+        <div style={table}>{context.accountId}</div>
+      </div>
+      <div className="d-flex flex-row align-items-center">
+        <div className="p-2 flex-1" style={table}>
+          Select Time zone:
+        </div>
+        <div className="d-flex flex-1" style={table}>
+          <select
+            name="zones"
+            id="zones"
+            value={state._time_zone}
+            onChange={(e) => {
+              State.update({ _time_zone: e.target.value });
+            }}
+          >
+            {time_zones.map((zone) => (
+              <option value={zone}>{zone}</option>
+            ))}
+          </select>
+        </div>
+      </div>
     </div>
-    <div className="d-flex flex-column w-75 justify-content-around">
+    <div className="d-flex flex-column mt-3">
+      <div className="d-flex flex-row">
+        <div style={table}>Day</div>
+        <div style={table}>On</div>
+        <div style={table}>From</div>
+        <div style={table}>To</div>
+      </div>
       {days.map((day, index) => (
-        <div>
-          <p>{day}</p>
-          <div className="d-flex justify-content-around">
+        <div className="d-flex flex-row">
+          <div style={table}>{day}</div>
+          <div style={table}>
             <select
               value={state._is_on[index]}
               onChange={(e) => {
@@ -228,87 +249,49 @@ return (
               <option value="on">on</option>
               <option value="off">off</option>
             </select>
-            {state._is_on[index] == "on" && (
-              <>
-                <p>From</p>
-                <select
-                  value={state._from[index]}
-                  onChange={(e) => {
-                    onTimeChanged(e, index, true);
-                  }}
-                >
-                  {hours.map((hour) => (
-                    <option value={hour}>{hour}</option>
-                  ))}
-                </select>
-                <p>To</p>
-                <select
-                  value={state._to[index]}
-                  onChange={(e) => {
-                    onTimeChanged(e, index, false);
-                  }}
-                >
-                  {hours.map((hour) => (
-                    <option value={hour}>{hour}</option>
-                  ))}
-                </select>
-              </>
-            )}
+          </div>
+          <div style={table}>
+            <select
+              value={state._from[index]}
+              disabled={state._is_on[index] == "off"}
+              onChange={(e) => {
+                onTimeChanged(e, index, true);
+              }}
+            >
+              {hours.map((hour) => (
+                <option value={hour}>{hour}</option>
+              ))}
+            </select>
+          </div>
+          <div style={table}>
+            <select
+              value={state._to[index]}
+              disabled={state._is_on[index] == "off"}
+              onChange={(e) => {
+                onTimeChanged(e, index, false);
+              }}
+            >
+              {hours.map((hour) => (
+                <option value={hour}>{hour}</option>
+              ))}
+            </select>
           </div>
         </div>
       ))}
     </div>
-    <p>
+    <CommitButton
+      style={button}
+      disabled={!state._validate_result}
+      data={getData()}
+    >
+      Send It!
+    </CommitButton>
+    <div className="pt=4">
       {days.map((day, index) => {
         return !state._validate_error[index] && `${day} `;
       })}
       {!state._validate_result && "time set wrong"}
-    </p>
-    <CommitButton
-      style={button}
-      disabled={!state._validate_result}
-      data={() => {
-        var zone = state._time_zone.split(" ")[0].split("UTC")[1].split(":");
-        var hours = parseInt(zone[0]);
-        var offset = hours + (parseInt(zone[1]) / 60) * ((hours > 0) * 2 - 1);
-        var temp = [];
-        var flag = false;
-        for (var i = 0; i < 7; i++) {
-          if (state._is_on[i] == "on") {
-            for (var j = 0; j < 2; j++) {
-              const time =
-                j == 0
-                  ? parseInt(state._from[i]) + 24 * i - offset
-                  : parseInt(state._to[i]) + 24 * i - offset;
-              if (time > 168) {
-                temp.push(time - 168);
-                flag = true;
-              } else if (time < 0) {
-                temp.push(time + 168);
-                flag = true;
-              } else temp.push(time);
-            }
-          }
-        }
-        const final = sortAndRemoveRepeated(flag, temp);
-        return {
-          index: {
-            Instance_time: JSON.stringify(
-              {
-                key: "data",
-                value: {
-                  _data: final,
-                },
-              },
-              undefined,
-              0
-            ),
-          },
-        };
-      }}
-    >
-      Send It!
-    </CommitButton>
+    </div>
     <br />
     <br />
     <div>
