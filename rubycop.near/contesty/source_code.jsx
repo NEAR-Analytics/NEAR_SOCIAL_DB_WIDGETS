45e2e827f451d2b98074aae904aae838ec6744a8
rubycop.near <rubycop.near>
2023-01-08 22:30:50 -0500

    Update contesty by rubycop.near at 2023-01-08 22:30:50.468

--- rubycop.near/contesty/source_code.jsx
+++ rubycop.near/contesty/source_code.jsx
@@ -131,36 +131,9 @@ return (
       <div className="btn-block my-2">
         <button onClick={getPair}>Find Random NFT Pair</button>
       </div>
-      <h3>Leaderboard</h3>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">NFT contract rating</th>
-            <th scope="col">NFT token id</th>
-          </tr>
-        </thead>
-        <tbody>
-          {stats
-            .map((k) => k.value.nft_stats)
-            .sort((a, b) => b.rating - a.rating)
-            .map(({ contract_id, token_id, rating }, i) => (
-              <tr key={i}>
-                <td>{i + 1}</td>
-                <td>
-                  <b>{rating}</b>
-                </td>
-                <td>
-                  <a
-                    href={`#mob.near/widget/NftImage?tokenId=${token_id}&contractId=${contract_id}`}
-                  >
-                    {token_id}
-                  </a>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+
+      <Widget src="rubycop.near/widget/contesty_leaderboard" />
+
       <div className="mt-2">
         <small>
           Follow <a href="https://twitter.com/ContestyNFT">Contesty.app</a>
