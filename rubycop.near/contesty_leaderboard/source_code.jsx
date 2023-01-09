e752a877790316ad0090980df324d888b84974e3
rubycop.near <rubycop.near>
2023-01-08 22:47:31 -0500

    Update contesty_leaderboard by rubycop.near at 2023-01-08 22:47:31.343

---
+++ rubycop.near/contesty_leaderboard/source_code.jsx
@@ -0,0 +1,36 @@
+const stats = Social.index("graph", "nft_likes", { order: "desc" });
+console.log(stats);
+return (
+  <>
+    <h3>NFT Leaderboard</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">NFT contract rating</th>
+          <th scope="col">NFT token id</th>
+        </tr>
+      </thead>
+      <tbody>
+        {stats
+          .map((k) => k.value.nft_likes)
+          .sort((a, b) => b.rating - a.rating)
+          .map(({ contract_id, token_id, rating }, i) => (
+            <tr key={i}>
+              <td>{i + 1}</td>
+              <td>
+                <b>{rating}</b>
+              </td>
+              <td>
+                <a
+                  href={`#mob.near/widget/NftImage?tokenId=${token_id}&contractId=${contract_id}`}
+                >
+                  {token_id}
+                </a>
+              </td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  </>
+);
