2714cfa1af6280f6f0efc2005cf227e4d053569a
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-01-11 04:32:21 -0500

    Update newVotingInterface by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-01-11 04:32:21.209

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newVotingInterface/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/newVotingInterface/source_code.jsx
@@ -6,8 +6,6 @@ State.init({
   profile: {},
   pollsByThisCreator: [{}],
   answers: [{}],
-  showShareOptions: false,
-  linkCopied: false,
 });
 
 if (!props.isPreview && !props.blockHeight) {
@@ -484,104 +482,14 @@ return (
         <div
           style={{
             position: "relative",
-            maxWidth: "max-content",
+            width: "max-content",
             margin: "1rem",
           }}
         >
-          <i
-            style={{ cursor: "pointer" }}
-            className="bi bi-share"
-            onClick={() => {
-              State.update({ showShareOptions: !state.showShareOptions });
-            }}
-          ></i>
-          {state.showShareOptions && (
-            <div
-              style={{
-                position: "absolute",
-                left: "1rem",
-                backgroundColor: "#FFFFFF",
-                border: "1.5px solid #F0F4F7",
-                boxShadow: "0px 8px 28px rgba(43, 68, 106, 0.05)",
-                borderRadius: "28px",
-                zIndex: "1",
-                width: "40vw",
-                padding: "1rem",
-                border: "1.5px solid #F0F4F7",
-              }}
-            >
-              <div className="d-flex flex-row-reverse">
-                <i
-                  className="bi bi-x"
-                  style={{ cursor: "pointer" }}
-                  onClick={() =>
-                    State.update({ showShareOptions: false, linkCopied: false })
-                  }
-                ></i>
-              </div>
-              <h3>Share</h3>
-              <p
-                style={{
-                  color: "#474D55",
-                  letterSpacing: "-0.01em",
-                }}
-              >
-                Use this link to share the poll with your participants
-              </p>
-              <div
-                className="d-flex justify-content-between align-items-center"
-                style={{
-                  backgroundColor: "#F2F6FA",
-                  padding: "1rem 2rem",
-                  borderRadius: "17px",
-                }}
-              >
-                <span
-                  style={{ color: "#0065FF", wordWrap: "anywhere" }}
-                >{`https://near.social/#/${widgetOwner}/widget/pollQuestionMasterHandler?sharedBlockHeight=${questionBlockHeight}`}</span>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    marginLeft: "0.5rem",
-                    minWidth: "2.5rem",
-                  }}
-                >
-                  <i
-                    className="bi-clipboard"
-                    style={
-                      state.linkCopied
-                        ? {
-                            color: "#0065FF",
-                            transition: "color 0.3s linear",
-                            cursor: "pointer",
-                          }
-                        : {
-                            transition: "color 0.3s linear",
-                            cursor: "pointer",
-                            color: "black",
-                          }
-                    }
-                    onClick={() => {
-                      clipboard.writeText(
-                        `https://near.social/#/${widgetOwner}/widget/pollQuestionMasterHandler?sharedBlockHeight=${questionBlockHeight}`
-                      );
-                      State.update({ linkCopied: true });
-                    }}
-                  ></i>
-                  {state.linkCopied && (
-                    <span
-                      className="text-secondary"
-                      style={{ fontSize: "0.7rem" }}
-                    >
-                      Copied!
-                    </span>
-                  )}
-                </div>
-              </div>
-            </div>
-          )}
+          <Widget
+            src={`${widgetOwner}/widget/shareWidget`}
+            props={{ questionBlockHeight }}
+          />
         </div>
         <div
           className="p-3"
