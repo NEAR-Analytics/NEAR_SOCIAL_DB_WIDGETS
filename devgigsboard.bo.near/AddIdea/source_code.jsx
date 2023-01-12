fe79687b79382751230c9cc52cf54dc269ce568a
devgigsboard.bo.near <devgigsboard.bo.near>
2023-01-12 08:48:36 -0500

    Update AddIdea by devgigsboard.bo.near at 2023-01-12 08:48:36.279

---
+++ devgigsboard.bo.near/AddIdea/source_code.jsx
@@ -0,0 +1,29 @@
+initState({ name: "", description: "" });
+const ownerId = "devgigsboard.bo.near";
+
+const onClick = () => {
+  Near.call(ownerId, "add_idea", {
+    name: state.name,
+    description: state.description,
+  });
+};
+
+const handleChange = (event) => {
+  State.update({ description: event.target.value });
+};
+
+return (
+  <div>
+    <h2>Add Idea</h2>
+    <p>
+      Title: <input type="text" value={state.name} />
+    </p>
+    <p>Description:</p>
+    <textarea value={state.description} onChange={handleChange} />
+    <p>
+      <a className="btn btn-outline-primary ms-2" onClick={onClick}>
+        Submit
+      </a>
+    </p>
+  </div>
+);
