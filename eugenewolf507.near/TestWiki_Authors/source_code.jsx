3a53604f5db76592139b6194f9d26ccfb54df15d
eugenewolf507.near <eugenewolf507.near>
2023-01-13 12:09:31 -0500

    Update TestWiki_Authors by eugenewolf507.near at 2023-01-13 12:09:31.968

--- eugenewolf507.near/TestWiki_Authors/source_code.jsx
+++ eugenewolf507.near/TestWiki_Authors/source_code.jsx
@@ -1,8 +1,33 @@
 const accountsNum = Near.view("testwiki.near", "get_num_accounts");
 
+const accounts = Near.view("testwiki.near", "get_accounts_paged");
+
+const getAuthorArticles = (authorId) => {
+  if (authorId) {
+    return Near.view("testwiki.near", "get_account", {
+      account_id: authorId,
+    });
+  }
+  return [];
+};
+
+console.log(getAuthorArticles("motzart.near"));
+
 return (
   <div>
     <h1>Authors</h1>
-    <div>Total {accountsNum} authors</div>
+    <div>Total: {accountsNum} authors</div>
+    {accounts.map(([accountId, account]) => (
+      <li key={accountId}>
+        <span>{accountId} - </span>
+        {account.articles.length > 0 && (
+          <span className="muted">
+            {" "}
+            {account.articles.length} article
+            {account.articles.length > 1 ? "s" : ""}
+          </span>
+        )}
+      </li>
+    ))}
   </div>
 );
