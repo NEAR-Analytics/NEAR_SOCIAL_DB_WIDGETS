748c0d58cef93c2fb8cc5df6f11285ec46a4a133
eugenewolf507.near <eugenewolf507.near>
2023-01-13 14:03:18 -0500

    Update TestWiki_Authors by eugenewolf507.near at 2023-01-13 14:03:18.066

--- eugenewolf507.near/TestWiki_Authors/source_code.jsx
+++ eugenewolf507.near/TestWiki_Authors/source_code.jsx
@@ -1,3 +1,5 @@
+State.init({});
+
 const accountsNum = Near.view("testwiki.near", "get_num_accounts");
 
 const accounts = Near.view("testwiki.near", "get_accounts_paged");
@@ -11,23 +13,41 @@ const getAuthorArticles = (authorId) => {
   return [];
 };
 
-console.log(getAuthorArticles("motzart.near"));
+const handleArticle = (e, articleId) => {
+  State.update({ ...state, articleId: articleId, authorId: undefined });
+};
+
+const handleAuthor = (e, authorId) => {
+  State.update({ ...state, articleId: undefined, authorId });
+  console.log("handleAuthor");
+};
+// use link for author to get author's articles
+// <a
+//   href=""
+//   style={{ textDecoration: "underline" }}
+//   onClick={(e) => handleAuthor(e, accountId)}
+// >
+// </a>
 
 return (
   <div>
     <h1>Authors</h1>
     <div>Total: {accountsNum} authors</div>
-    {accounts.map(([accountId, account]) => (
-      <li key={accountId}>
-        <span>{accountId} - </span>
-        {account.articles.length > 0 && (
-          <span className="muted">
-            {" "}
-            {account.articles.length} article
-            {account.articles.length > 1 ? "s" : ""}
+    <ul>
+      {accounts.map(([accountId, account]) => (
+        <li key={accountId}>
+          <span>
+            {accountId} -
+            {account.articles.length > 0 && (
+              <span>
+                {" "}
+                {account.articles.length} article
+                {account.articles.length > 1 ? "s" : ""}
+              </span>
+            )}
           </span>
-        )}
-      </li>
-    ))}
+        </li>
+      ))}
+    </ul>
   </div>
 );
