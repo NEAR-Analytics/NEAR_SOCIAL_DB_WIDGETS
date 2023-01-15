6eb92047b2bd225ae68607a2cbaf76da488b2c8a
vow_owner_123.near <vow_owner_123.near>
2023-01-14 19:09:50 -0500

    Update Instance_time by vow_owner_123.near at 2023-01-14 19:09:50.285

--- vow_owner_123.near/Instance_time/source_code.jsx
+++ vow_owner_123.near/Instance_time/source_code.jsx
@@ -1,22 +1,11 @@
 State.init({
-  _is_on: ["off", "off", "off", "off", "off", "off", "off"],
-  _from: ["0", "0", "0", "0", "0", "0", "0"],
-  _to: ["1", "1", "1", "1", "1", "1", "1"],
+  _is_on: ["on", "on", "on", "on", "on", "off", "off"],
+  _from: ["10", "10", "10", "10", "10", "10", "10"],
+  _to: ["18", "18", "18", "18", "18", "18", "18"],
   _time_zone: "(UTC+00:00) UTC",
   _validate_result: true,
   _validate_error: [true, true, true, true, true, true, true],
 });
-const widgetName = "Instance_time";
-const widgetPath = `vow_owner_123.near/widget/${widgetName}`;
-const metadata = props.metadata ?? Social.getr(`${widgetPath}/metadata`);
-const card = {
-  background: "linear-gradient(to right, #4deeea, #f000ff)",
-  border: "1px solid black",
-  borderRadius: "5px",
-  textAlign: "center",
-  color: "white",
-  padding: "10px",
-};
 const button = {
   borderRadius: "5px",
   margin: "5px 0",
@@ -31,6 +20,14 @@ const table = {
   display: "flex",
   flex: "1",
   marginTop: "1rem",
+};
+const flex_row = {
+  display: "flex",
+  flexDirection: "row",
+};
+const flex_column = {
+  display: "flex",
+  flexDirection: "column",
 };
 const time_zones = [
   "(UTC-11:00) Samoa",
@@ -83,13 +80,8 @@ const days = [
   "Saturday",
   "Sunday",
 ];
-const data = Social.index("Instance_time", "data");
-if (!data) {
-  return "Loading datas";
-}
-var sortedData = data.sort((d1, d2) => d2.blockHeight - d1.blockHeight);
-var finalData = [];
-var accountIds = [];
+const tbl_headers = ["Day", "On", "From", "To"];
+
 const validate = () => {
   var result = true;
   for (var i = 0; i < 7; i++) {
@@ -97,9 +89,9 @@ const validate = () => {
   }
   State.update({ _validate_result: result });
 };
-const onTimeChanged = (e, index, is_from_to) => {
+const onTimeChanged = (value, index, is_from_to) => {
   let temp = is_from_to ? state._from : state._to;
-  temp[index] = e.target.value;
+  temp[index] = value;
   is_from_to ? State.update({ _from: temp }) : State.update({ _to: temp });
   let error_temp = state._validate_error;
   if (parseInt(state._from[index]) >= parseInt(state._to[index])) {
@@ -168,78 +160,24 @@ const getData = () => {
     },
   };
 };
-for (let i = 0; i < sortedData.length; i++) {
-  if (accountIds.indexOf(sortedData[i].accountId) < 0) {
-    accountIds.push(sortedData[i].accountId);
-    var date = new Date();
-    var utc_offset = -date.getTimezoneOffset() / 60;
-    var times = sortedData[i].value._data;
-    var temp = [];
-    var flag = false;
-    for (var j = 0; j < times.length; j++) {
-      const time = times[j] + utc_offset;
-      if (time > 168) {
-        temp.push(time - 168);
-        flag = true;
-      } else if (time < 0) {
-        temp.push(time + 168);
-        flag = true;
-      } else temp.push(time);
-    }
-    const final = sortAndRemoveRepeated(flag, temp);
-    for (var m = 0; m < final.length - 1; m += 2) {
-      const _from = final[m];
-      const _to = final[m + 1];
-      for (var o = 1; o < 7; o++) {
-        if (o * 24 > _from && o * 24 < _to) {
-          final.push(o * 24, o * 24);
-        }
-      }
-    }
-    var sortedTimeDataNew = final.sort((d2, d1) => d2 - d1);
-    var weeklyData = [];
-    for (var t = 0; t < 7; t++) {
-      var dailyData = [];
-      var exist = false;
-      for (var p = 0; p < sortedTimeDataNew.length - 1; p += 2) {
-        var _from = sortedTimeDataNew[p];
-        var _to = sortedTimeDataNew[p + 1];
-        if (_to > t * 24 && _to <= (t + 1) * 24) {
-          dailyData.push({
-            _from: _from - t * 24,
-            _to: _to - t * 24,
-          });
-          exist = true;
-        }
-      }
-      if (!exist) weeklyData.push({ on_off: "off", data: [] });
-      else weeklyData.push({ on_off: "on", data: dailyData });
-    }
-    finalData.push({
-      accountId: sortedData[i].accountId,
-      value: {
-        _data: weeklyData,
-      },
-    });
-  }
-}
+
 return (
   <div>
     <br />
     <br />
-    <div className="d-flex flex-column align-self-center">
+    <div style={flex_column} className="align-self-center">
       <h3>Weekly Schedule</h3>
-      <div className="d-flex flex-row align-items-center">
+      <div style={flex_row} className="align-items-center">
         <div className="p-2" style={table}>
           current_user:
         </div>
         <div style={table}>{context.accountId}</div>
       </div>
-      <div className="d-flex flex-row align-items-center">
+      <div style={flex_row}>
         <div className="p-2 flex-1" style={table}>
           Select Time zone:
         </div>
-        <div className="d-flex flex-1" style={table}>
+        <div style={table}>
           <select
             name="zones"
             id="zones"
@@ -255,15 +193,14 @@ return (
         </div>
       </div>
     </div>
-    <div className="d-flex flex-column mt-3">
-      <div className="d-flex flex-row">
-        <div style={table}>Day</div>
-        <div style={table}>On</div>
-        <div style={table}>From</div>
-        <div style={table}>To</div>
+    <div style={flex_column} className="mt-3">
+      <div style={flex_row}>
+        {tbl_headers.map((header) => (
+          <div style={table}>{header}</div>
+        ))}
       </div>
       {days.map((day, index) => (
-        <div className="d-flex flex-row">
+        <div style={flex_row}>
           <div style={table}>{day}</div>
           <div style={table}>
             <select
@@ -288,30 +225,66 @@ return (
             </select>
           </div>
           <div style={table}>
-            <select
-              value={state._from[index]}
-              disabled={state._is_on[index] == "off"}
-              onChange={(e) => {
-                onTimeChanged(e, index, true);
-              }}
-            >
-              {hours.map((hour) => (
-                <option value={hour}>{hour}</option>
-              ))}
-            </select>
+            <div className="d-flex">
+              <select
+                value={state._from[index]}
+                disabled={state._is_on[index] == "off"}
+                onChange={(e) => {
+                  onTimeChanged(e.target.value, index, true);
+                }}
+              >
+                {hours.map((hour) => (
+                  <option value={hour}>{hour}</option>
+                ))}
+              </select>
+            </div>
+            <div style={flex_column}>
+              <div
+                onClick={() => {
+                  onTimeChanged(parseInt(state._from[index]) + 1, index, true);
+                }}
+              >
+                <i class="bi-caret-up"></i>
+              </div>
+              <div
+                onClick={() => {
+                  onTimeChanged(parseInt(state._from[index]) - 1, index, true);
+                }}
+              >
+                <i class="bi-caret-down"></i>
+              </div>
+            </div>
           </div>
           <div style={table}>
-            <select
-              value={state._to[index]}
-              disabled={state._is_on[index] == "off"}
-              onChange={(e) => {
-                onTimeChanged(e, index, false);
-              }}
-            >
-              {hours.map((hour) => (
-                <option value={hour}>{hour}</option>
-              ))}
-            </select>
+            <div style={flex_row}>
+              <select
+                value={state._to[index]}
+                disabled={state._is_on[index] == "off"}
+                onChange={(e) => {
+                  onTimeChanged(e.target.value, index, false);
+                }}
+              >
+                {hours.map((hour) => (
+                  <option value={hour}>{hour}</option>
+                ))}
+              </select>
+            </div>
+            <div style={flex_column}>
+              <div
+                onClick={() => {
+                  onTimeChanged(parseInt(state._to[index]) + 1, index, false);
+                }}
+              >
+                <i class="bi-caret-up"></i>
+              </div>
+              <div
+                onClick={() => {
+                  onTimeChanged(parseInt(state._to[index]) - 1, index, false);
+                }}
+              >
+                <i class="bi-caret-down"></i>
+              </div>
+            </div>
           </div>
         </div>
       ))}
@@ -332,43 +305,14 @@ return (
     <br />
     <br />
     <div>
-      {finalData
-        ? finalData.map((d) => (
-            <div style={card}>
-              <Widget
-                src="mob.near/widget/ProfileImage"
-                props={{
-                  accountId: d.accountId,
-                  className: "d-inline-block",
-                  style: { width: "1.5em", height: "1.5em" },
-                }}
-              />
-              <a
-                href={`#/mob.near/widget/ProfilePage?accountId=${d.accountId}`}
-              >
-                {d.accountId}
-              </a>
-              <div>
-                <div>
-                  <b>
-                    {d.value._data.map((week, index) => {
-                      return (
-                        <>
-                          <div>{`${days[index]} : ${week.on_off}`}</div>
-                          <div>
-                            {week.data.map((y) => (
-                              <li>{`${y._from} - ${y._to}`}</li>
-                            ))}
-                          </div>
-                        </>
-                      );
-                    })}
-                  </b>
-                </div>
-              </div>
-            </div>
-          ))
-        : "Loading..."}
+      <Widget
+        src="vow_owner_123.near/widget/Instance_time_review"
+        props={{
+          accountId: d.accountId,
+          className: "d-inline-block",
+          style: { width: "1.5em", height: "1.5em" },
+        }}
+      />
     </div>
   </div>
 );
