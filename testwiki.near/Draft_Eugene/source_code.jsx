e12e6ec5b020fb60d2c7ea646e2136de6e9c0db3
testwiki.near <testwiki.near>
2023-01-17 12:08:09 -0500

    Update Draft_Eugene by testwiki.near at 2023-01-17 12:08:09.401

--- testwiki.near/Draft_Eugene/source_code.jsx
+++ testwiki.near/Draft_Eugene/source_code.jsx
@@ -1,13 +1,16 @@
 const accountId = props.accountId ?? context.accountId;
 
-let data = Social.keys(`${accountId}/widget/*`, "final", {
+let data1 = Social.keys(`${accountId}/widget/*`, "final", {
   return_type: "BlockHeight",
   values_only: true,
 });
 
-const profile = Social.getr(`${accountId}/profile`);
+const data2 = Social.getr(`${accountId}/widget/*`);
 
-console.log("data is ", data);
-console.log("accountId is ", accountId);
-console.log("profile is ", profile);
+const data3 = Social.get(`${accountId}/widget/*`);
+
+console.log("data1 is ", data1);
+console.log("data2 is ", data2);
+console.log("data3 is ", data3);
+
 return <div>Hello World</div>;
