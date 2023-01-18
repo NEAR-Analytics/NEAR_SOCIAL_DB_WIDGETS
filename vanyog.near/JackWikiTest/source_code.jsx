6ea74fa3590fa7cb1ec750714dd365a620b7f39e
vanyog.near <vanyog.near>
2023-01-17 19:19:59 -0500

    Update JackWikiTest by vanyog.near at 2023-01-17 19:19:59.581

--- vanyog.near/JackWikiTest/source_code.jsx
+++ vanyog.near/JackWikiTest/source_code.jsx
@@ -78,6 +78,10 @@ const saveHandler = (e) => {
       };
 
       Social.set({ articles: JSON.stringify([...state.articles, newArticle]) });
+
+      Social.set({
+        articlesNavigation: JSON.stringify([...state.articles, nameOfArticle]),
+      });
     } else {
       State.update({ ...state, errorId: errTextDublicatedId });
     }
