1ae4361b6772bd8153b126c80808bcff662e7cc8
ciencia.near <ciencia.near>
2023-01-18 15:25:23 -0500

    Update Profile by ciencia.near at 2023-01-18 15:25:23.746

---
+++ ciencia.near/Profile/source_code.jsx
@@ -0,0 +1,36 @@
+if (!props.answers) {
+  return "Prop validAnswersToThisPoll not set";
+}
+
+if (!props.questionNumber) {
+  return "Prop questionNumber not set";
+}
+let answers = props.answers;
+let questionNumber = props.questionNumber;
+
+return (
+  <>
+    {answers.map((answer, i) => {
+      return (
+        <div
+          style={
+            i % 2 == 0 ? { marginRight: "0.5rem" } : { marginLeft: "0.5rem" }
+          }
+        >
+          <p
+            className="py-2 px-3"
+            style={{
+              backgroundColor: "#F2F6FA",
+              color: "#59606A",
+              border: "1px solid #F2F6FA",
+              borderRadius: "8px",
+              minHeight: "3rem",
+            }}
+          >
+            {answer.value.answer[questionNumber]}
+          </p>
+        </div>
+      );
+    })}
+  </>
+);
