598d2950b1c70ed7bf32692df8c7f3d553365cdd
eugenewolf507.near <eugenewolf507.near>
2023-01-19 12:36:07 -0500

    Update Draft_Eugene2 by eugenewolf507.near at 2023-01-19 12:36:07.892

--- eugenewolf507.near/Draft_Eugene2/source_code.jsx
+++ eugenewolf507.near/Draft_Eugene2/source_code.jsx
@@ -3,105 +3,82 @@ const accountId = props.accountId ?? context.accountId;
 
 // ========= TRY OBJECT STRUCTURE =========
 const dataStructure = {
-  articlesAll: {
-    testwiki: {
-      article1: {
-        author: "testwiki",
-        body: "body for article 1 by testwiki",
-      },
-      article2: {
-        author: "testwiki",
-        body: "body for article 2 by testwiki",
-      },
-    },
-    eugenewolf507: {
-      articleA: {
-        author: "testwiki",
-        body: "body for article A by eugenewolf507",
-      },
-      articleB: {
-        author: "testwiki",
-        body: "body for article B by eugenewolf507",
-      },
-    },
+  articlG: {
+    author: "EV",
+    body: "body for article G",
   },
-  articlesNav: ["articleA", "articleB", "article1", "article2"],
+  articleH: {
+    author: "EV",
+    body: "body for article H",
+  },
+  article7: {
+    author: "EV",
+    body: "body for article 7",
+  },
+  article8: {
+    author: "EV",
+    body: "body for article 8",
+  },
 };
 
-// ========= HANDLERS =========
+// ========= HANDLER SET =========
 const clickHandlerSetStructure = () => {
-  Social.set(JSON.stringify(dataStructure));
+  // Social.set(JSON.stringify(dataStructure));
+  Social.set({ articles: JSON.stringify(dataStructure) });
 };
+
+// ========= HANDLER GET =========
 const clickHandlerGetStructure = () => {
+  // === ALL ARTICLES
   // path that works
   // const path = "/*";
   // const path = "/widget/*";
   // const path = "/widget/Draft_Eugene2";
-  const path = "/articles/**";
-
-  // path that doesn't works
-  // const path = "/articles/articlesAll";
-
+  const path = "/articlesPersonal2/**";
   const data1 = Social.get(`${accountId}${path}`);
   console.log(data1);
 
+  // === ONE ARTICLE
   // const data2 = Social.keys("*/widget/*", "final");
   // const data2 = Social.keys("*/articles/*", "final");
   // const data2 = Social.getr("bearner.near/nametag/bearner.near/tags", "final");
-  const data2 = Social.getr(
-    "eugenewolf507.near/articles/articlesAll/eugenewolf507/articleA",
+  const data2 = Social.get(
+    `${accountId}/articlesPersonal2/articleF/**`,
     "final"
   );
   console.log(data2);
+
+  // === ALL IDS
+  const data3 = Social.keys(`${accountId}/articlesPersonal2/*`, "final");
+  console.log(Object.keys(data3[accountId].articlesPersonal2));
 };
 
 // ========= RETURN =========
 return (
   <div>
     <p>Press button and check log in browser</p>
-    <div>
-      <button onClick={clickHandlerSetStructure}>Set Structure</button>
-      <CommitButton
-        data={{
-          articles: {
-            articlesAll: {
-              testwiki: {
-                article1: {
-                  author: "testwiki",
-                  body: "body for article 1 by testwiki",
-                },
-                article2: {
-                  author: "testwiki",
-                  body: "body for article 2 by testwiki",
-                },
-              },
-              eugenewolf507: {
-                articleA: {
-                  author: "testwiki",
-                  body: "body for article A by eugenewolf507",
-                },
-                articleB: {
-                  author: "testwiki",
-                  body: "body for article B by eugenewolf507",
-                },
-              },
-            },
-            articlesNav: [
-              "articleA",
-              "articleB",
-              "articleC",
-              "article1",
-              "article2",
-            ],
-          },
-        }}
-        // data={{
-        //   articles: { "eugenewolf507.near": { homepage: state.homepage } },
-        // }}
-      >
-        SetStructure with CommitButton
-      </CommitButton>
-    </div>
+    {/* <button onClick={clickHandlerSetStructure}>Set Structure</button> */}
+    <CommitButton
+      data={{
+        articlesPersonal2: dataStructure,
+      }}
+      // option how to write to anather account and syntax why we use {{}}
+      // but I don't check it
+      // data={{
+      //   articles: { "eugenewolf507.near": { homepage: state.homepage } },
+      // }}
+    >
+      SET All with CommitButton
+    </CommitButton>
+    <CommitButton
+      data={{
+        articlesPersonal2: {
+          articleF: { body: "body 4 article F: FFFFFFF" },
+        },
+      }}
+    >
+      CHANGE One with CommitButton
+    </CommitButton>
     <button onClick={clickHandlerGetStructure}>Get Structure</button>
   </div>
 );
