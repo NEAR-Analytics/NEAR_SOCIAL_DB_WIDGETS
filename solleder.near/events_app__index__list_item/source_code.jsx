7d541f0126c83af3198da9a1f27f540ceeb366b7
solleder.near <solleder.near>
2023-01-19 16:35:50 -0500

    Update events_app__index__list_item by solleder.near at 2023-01-19 16:35:50.252

--- solleder.near/events_app__index__list_item/source_code.jsx
+++ solleder.near/events_app__index__list_item/source_code.jsx
@@ -1,1 +1,89 @@
-constCONTRACT_OWNER=solleder.near;letevent=props.event||null;//returndata;if(!event){return;}constBG_CARD=#ffffff;constEventCard=styled.div`display:flex;flex-direction:column;align-items:center;justify-content:center;width:100%;max-width:400px;margin:2remauto;padding:1rem0;background-color:${BG_CARD};border-radius:5px;box-shadow:005px0rgba(0,0,0,0.2);border:1pxsolid#ccc;`;constEventImage=styled.img`width:100%;height:auto;`;constEventHeader=styled.div`display:flex;flex-direction:column;align-items:center;justify-content:center;width:100%;border-bottom:1pxsolid#ccc;`;constEventTitle=styled.h1`font-size:1.5rem;font-weight:500;margin:0;padding:1rem;`;constEventDescription=styled.p`font-size:1rem;font-weight:400;margin:0;padding:1rem;`;constEventDate=styled.p`font-size:1rem;font-weight:400;margin:0;padding:1rem;`;constEventBody=styled.div`display:flex;flex-direction:column;align-items:center;justify-content:center;width:100%;padding:1rem;`;return(<ahref={`#/${CONTRACT_OWNER}/widget/show?event_id=${event.event_id}`}className="text-decoration-none"><EventCard><EventHeader><EventImagesrc={event.image}/><EventTitle>{event.name}</EventTitle></EventHeader><EventBody><EventDescription>{event.description}</EventDescription><EventDate>{event.start_date}</EventDate><EventDate>{event.end_date}</EventDate></EventBody></EventCard></a>);
+const CONTRACT_OWNER = solleder.near;
+
+let event = props.event || null;
+
+// return data;
+if (!event) {
+return ;
+}
+
+const BG_CARD = #ffffff;
+
+const EventCard = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+width: 100%;
+max-width: 400px;
+margin: 2rem auto;
+padding: 1rem 0;
+background-color: ${BG_CARD};
+border-radius: 5px;
+box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.2);
+border: 1px solid #ccc;
+`;
+
+const EventImage = styled.img`
+width: 100%;
+height: auto;
+`;
+
+const EventHeader = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+width: 100%;
+border-bottom: 1px solid #ccc;
+`;
+
+const EventTitle = styled.h1`
+font-size: 1.5rem;
+font-weight: 500;
+margin: 0;
+padding: 1rem;
+`;
+
+const EventDescription = styled.p`
+font-size: 1rem;
+font-weight: 400;
+margin: 0;
+padding: 1rem;
+`;
+
+const EventDate = styled.p`
+font-size: 1rem;
+font-weight: 400;
+margin: 0;
+padding: 1rem;
+`;
+
+const EventBody = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+width: 100%;
+padding: 1rem;
+`;
+
+return (
+<a
+href={`#/${CONTRACT_OWNER}/widget/show?event_id=${event.event_id}`}
+className="text-decoration-none"
+>
+<EventCard>
+<EventHeader>
+<EventImage src={event.image} />
+<EventTitle>{event.name}</EventTitle>
+</EventHeader>
+
+<EventBody>
+<EventDescription>{event.description}</EventDescription>
+<EventDate>{event.start_date}</EventDate>
+<EventDate>{event.end_date}</EventDate>
+</EventBody>
+</EventCard>
+</a>
+);
