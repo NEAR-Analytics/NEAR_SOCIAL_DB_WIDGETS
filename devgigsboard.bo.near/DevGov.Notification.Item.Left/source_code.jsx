c10c9abbaf230a075017f2ea7f46d41fc097e7a6
devgigsboard.bo.near <devgigsboard.bo.near>
2023-01-19 03:44:59 -0500

    Update DevGov.Notification.Item.Left by devgigsboard.bo.near at 2023-01-19 03:44:59.157

--- devgigsboard.bo.near/DevGov.Notification.Item.Left/source_code.jsx
+++ devgigsboard.bo.near/DevGov.Notification.Item.Left/source_code.jsx
@@ -1,1 +1,25 @@
-return <div>Hello World left</div>;
+if (!props.type) {
+  return "Loading ...";
+}
+
+const type = props.type.split("/")[1];
+return props.type ? (
+  <>
+    {type == "like"
+      ? "liked"
+      : type == "reply"
+      ? "relied"
+      : type == "edit"
+      ? "edited"
+      : "???"}{" "}
+    your
+    <a
+      className="fw-bold text-muted"
+      href={`#/devgigsboard.bo.near/widget/Ideas?postId=${props.post}`}
+    >
+      Developer Governance Post
+    </a>
+  </>
+) : (
+  "Loading ..."
+);
