c651cdd906ca4d28faefb4020b7c5b3b30dc8b4d
evgenykuzyakov <evgenykuzyakov>
2023-01-19 18:10:31 -0500

    Update CopyButton by mob.near at 2023-01-19 18:10:31.156

---
+++ mob.near/CopyButton/source_code.jsx
@@ -0,0 +1,27 @@
+return (
+  <OverlayTrigger
+    placement="auto"
+    overlay={
+      <Tooltip>{state.copied ? "Copied!" : "Copy to clipboard"}</Tooltip>
+    }
+  >
+    <button
+      className={props.className ?? "btn btn-outline-primary border-0"}
+      onClick={() => {
+        clipboard.writeText(props.text).then(() => {
+          State.update({ copied: true });
+          if (props.onCopy) {
+            props.onCopy(props.text);
+          }
+        });
+      }}
+    >
+      {state.copied ? (
+        <i className="bi bi-check-lg" />
+      ) : (
+        <i className="bi bi-clipboard" />
+      )}
+      {props.label}
+    </button>
+  </OverlayTrigger>
+);
