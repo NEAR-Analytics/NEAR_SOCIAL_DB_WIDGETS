0eaa1fffd8d731bcd59da74a06a8e56d50b202ba
devgigsboard.bo.near <devgigsboard.bo.near>
2023-01-19 03:38:52 -0500

    Update DevGov.Notification.Item.Right by devgigsboard.bo.near at 2023-01-19 03:38:52.176

--- devgigsboard.bo.near/DevGov.Notification.Item.Right/source_code.jsx
+++ devgigsboard.bo.near/DevGov.Notification.Item.Right/source_code.jsx
@@ -1,1 +1,10 @@
-return <div>Hello World right</div>;
+return (
+  <>
+    <a
+      className="btn btn-outline-dark"
+      href={`#/devgovgigs.near/widget/Ideas?postId=${props.post}`}
+    >
+      View Developer Governance Post
+    </a>
+  </>
+);
