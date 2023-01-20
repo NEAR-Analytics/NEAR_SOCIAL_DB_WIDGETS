1ae8752faf7db53d23db20804d8577772b24ac2d
eugenewolf507.near <eugenewolf507.near>
2023-01-20 11:26:20 -0500

    Update TestWiki_Authors by eugenewolf507.near at 2023-01-20 11:26:20.914

--- eugenewolf507.near/TestWiki_Authors/source_code.jsx
+++ eugenewolf507.near/TestWiki_Authors/source_code.jsx
@@ -1,26 +1,31 @@
 State.init({});
 
+const wikiTestData = Social.keys("*/wikiTest/articles/*", "final");
+console.log(wikiTestData);
+const accountsSocialDB = Object.keys(wikiTestData);
+console.log(accountsSocialDB);
+
 const accountsNum = Near.view("testwiki.near", "get_num_accounts");
 
 const accounts = Near.view("testwiki.near", "get_accounts_paged");
 
-const getAuthorArticles = (authorId) => {
-  if (authorId) {
-    return Near.view("testwiki.near", "get_account", {
-      account_id: authorId,
-    });
-  }
-  return [];
-};
+// const getAuthorArticles = (authorId) => {
+//   if (authorId) {
+//     return Near.view("testwiki.near", "get_account", {
+//       account_id: authorId,
+//     });
+//   }
+//   return [];
+// };
 
-const handleArticle = (e, articleId) => {
-  State.update({ ...state, articleId: articleId, authorId: undefined });
-};
+// const handleArticle = (e, articleId) => {
+//   State.update({ ...state, articleId: articleId, authorId: undefined });
+// };
 
-const handleAuthor = (e, authorId) => {
-  State.update({ ...state, articleId: undefined, authorId });
-  console.log("handleAuthor");
-};
+// const handleAuthor = (e, authorId) => {
+//   State.update({ ...state, articleId: undefined, authorId });
+//   console.log("handleAuthor");
+// };
 // use link for author to get author's articles
 // <a
 //   href=""
@@ -33,6 +38,14 @@ return (
   <div>
     <h1>Authors</h1>
     <div>Total: {accountsNum} authors</div>
+    <ul>
+      {accountsSocialDB &&
+        accountsSocialDB.map((accountId) => (
+          <li key={accountId}>
+            <span>{accountId} -</span>
+          </li>
+        ))}
+    </ul>
     <ul>
       {accounts &&
         accounts.map(([accountId, account]) => (
