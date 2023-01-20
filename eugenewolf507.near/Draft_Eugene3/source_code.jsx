00623a1282f48515252d9d734df88fc6bad7a4ea
eugenewolf507.near <eugenewolf507.near>
2023-01-20 15:17:21 -0500

    Update Draft_Eugene3 by eugenewolf507.near at 2023-01-20 15:17:21.055

--- eugenewolf507.near/Draft_Eugene3/source_code.jsx
+++ eugenewolf507.near/Draft_Eugene3/source_code.jsx
@@ -1,33 +1,17 @@
-// =========  =========
 const accountId = props.accountId ?? context.accountId;
-
-// ========= HANDLER GET =========
 const clickHandlerGetStructureThis = () => {
   console.log("fgdsfsd");
-  // === ALL ARTICLES
-  // path that works
-  // const path = "/*";
-  // const path = "/widget/*";
-  // const path = "/widget/Draft_Eugene2";
   const path = "/articlesPersonal2/**";
   const data1 = Social.get(`${accountId}${path}`);
   console.log(data1);
-
-  // === ONE ARTICLE
-  // const data2 = Social.keys("*/widget/*", "final");
-  // const data2 = Social.keys("*/articles/*", "final");
-  // const data2 = Social.getr("bearner.near/nametag/bearner.near/tags", "final");
   const data2 = Social.get(
     `${accountId}/articlesPersonal2/articleF/**`,
     "final"
   );
   console.log(data2);
-
-  // === ALL IDS
   const data3 = Social.keys(`${accountId}/articlesPersonal2/*`, "final");
   console.log(Object.keys(data3[accountId].articlesPersonal2));
 };
-
 return (
   <div>
     <div>
