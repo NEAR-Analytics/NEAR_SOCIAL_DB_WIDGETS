6fb382542c438fe7e5b22893cd9602ff064ab52f
eugenewolf507.near <eugenewolf507.near>
2023-01-20 15:07:25 -0500

    Update JackWikiTest by eugenewolf507.near at 2023-01-20 15:07:25.582

--- eugenewolf507.near/JackWikiTest/source_code.jsx
+++ eugenewolf507.near/JackWikiTest/source_code.jsx
@@ -216,38 +216,4 @@ const getAuthors = () => {
     </>
   );
 };
-return (
-  <>
-    <ul className="nav nav-pills nav-fill mb-4" id="pills-tab" role="tablist">
-      {pills.map(({ id, title }, i) => (
-        <li className="nav-item" role="presentation" key={i}>
-          <button
-            className={`nav-link ${i === 0 ? "active" : ""}`}
-            id={`pills-${id}-tab`}
-            data-bs-toggle="pill"
-            data-bs-target={`#pills-${id}`}
-            type="button"
-            role="tab"
-            aria-controls={`pills-${id}`}
-            aria-selected={i === 0}
-            onClick={() => {
-              const key = `load${id}`;
-
-              State.update({
-                ...state,
-                article: undefined,
-                authorId: undefined,
-                note: undefined,
-                editArticle: false,
-                currentTab: key,
-              });
-              console.log("state", state);
-            }}
-          >
-            {title}
-          </button>
-        </li>
-      ))}
-    </ul>
-  </>
-);
+return <></>;
