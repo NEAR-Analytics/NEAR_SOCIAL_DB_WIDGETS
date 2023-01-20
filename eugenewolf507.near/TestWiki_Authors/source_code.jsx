396a132f3a5afd25710f2b7e4cbbf90ecab8bd50
eugenewolf507.near <eugenewolf507.near>
2023-01-20 15:16:27 -0500

    Update TestWiki_Authors by eugenewolf507.near at 2023-01-20 15:16:27.429

--- eugenewolf507.near/TestWiki_Authors/source_code.jsx
+++ eugenewolf507.near/TestWiki_Authors/source_code.jsx
@@ -1,39 +1,10 @@
 State.init({});
-
 const wikiTestData = Social.keys("*/wikiTest/articles/*", "final");
 console.log(wikiTestData);
 const accountsSocialDB = Object.keys(wikiTestData);
 console.log(accountsSocialDB);
-
 const accountsNum = Near.view("testwiki.near", "get_num_accounts");
-
 const accounts = Near.view("testwiki.near", "get_accounts_paged");
-
-// const getAuthorArticles = (authorId) => {
-//   if (authorId) {
-//     return Near.view("testwiki.near", "get_account", {
-//       account_id: authorId,
-//     });
-//   }
-//   return [];
-// };
-
-// const handleArticle = (e, articleId) => {
-//   State.update({ ...state, articleId: articleId, authorId: undefined });
-// };
-
-// const handleAuthor = (e, authorId) => {
-//   State.update({ ...state, articleId: undefined, authorId });
-//   console.log("handleAuthor");
-// };
-// use link for author to get author's articles
-// <a
-//   href=""
-//   style={{ textDecoration: "underline" }}
-//   onClick={(e) => handleAuthor(e, accountId)}
-// >
-// </a>
-
 return (
   <div>
     <h1>Authors</h1>
