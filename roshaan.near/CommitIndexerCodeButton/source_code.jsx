02a72062b478ab71018e3b22ecb9f7ac08347f26
roshaan.near <roshaan.near>
2023-01-19 19:30:14 -0500

    Update CommitIndexerCodeButton by roshaan.near at 2023-01-19 19:30:14.584

---
+++ roshaan.near/CommitIndexerCodeButton/source_code.jsx
@@ -0,0 +1,29 @@
+const MIN_DEPOSIT = "300000000000000000000000";
+let contractId = "near-indexer.near";
+let indexer_name = props.indexer_name;
+let indexer_code = props.indexer_code;
+
+let UpdateIndexerCode = () => {
+  const gas = 200000000000000;
+  const deposit = new Big(depositYocto).plus(MIN_DEPOSIT).toFixed(0);
+
+  Near.call(
+    contractId,
+    "register_indexer_function",
+    {
+      name: indexer_name,
+      code: indexer_code,
+    },
+    gas,
+    deposit
+  );
+};
+
+return (
+  <button
+    disabled={!indexer_code || !indexer_name || !context.accountId}
+    onClick={UpdateIndexerCode}
+  >
+    Save Indexer Code
+  </button>
+);
