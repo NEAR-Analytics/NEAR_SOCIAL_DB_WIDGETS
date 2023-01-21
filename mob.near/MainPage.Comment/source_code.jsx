e3c527755c12e02654f933e6a4142664a22ca5b4
evgenykuzyakov <evgenykuzyakov>
2023-01-20 20:03:41 -0500

    Update MainPage.Comment by mob.near at 2023-01-20 20:03:41.268

--- mob.near/MainPage.Comment/source_code.jsx
+++ mob.near/MainPage.Comment/source_code.jsx
@@ -14,29 +14,7 @@ return (
       props={{ accountId, blockHeight, link, postType: "comment" }}
     />
     <div className="mt-2 text-break">
-      {content ? (
-        <>
-          {content.text && <Markdown text={content.text} />}
-          {content.image && (
-            <div className="w-100 rounded-3 text-center">
-              <Widget
-                src="mob.near/widget/Image"
-                props={{
-                  image: content.image,
-                  className: "img-fluid rounded-3",
-                  style: { maxHeight: "20em" },
-                }}
-              />
-            </div>
-          )}
-        </>
-      ) : (
-        <span
-          className="spinner-grow spinner-grow-sm me-1"
-          role="status"
-          aria-hidden="true"
-        />
-      )}
+      <Widget src="mob.near/widget/MainPage.Post.Content" props={{ content }} />
     </div>
     {blockHeight !== "now" && (
       <div className="mt-1">
