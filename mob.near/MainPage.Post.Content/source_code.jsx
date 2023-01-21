b33d7b66894f0c36f267345066b04481938e98cf
evgenykuzyakov <evgenykuzyakov>
2023-01-20 20:00:36 -0500

    Update MainPage.Post.Content by mob.near at 2023-01-20 20:00:36.220

---
+++ mob.near/MainPage.Post.Content/source_code.jsx
@@ -0,0 +1,26 @@
+const content = props.content;
+return content ? (
+  <>
+    {content.text && (
+      <Widget src="mob.near/widget/SocialMarkdown" text={content.text} />
+    )}
+    {content.image && (
+      <div className="w-100 rounded-3 text-center">
+        <Widget
+          src="mob.near/widget/Image"
+          props={{
+            image: content.image,
+            className: "img-fluid rounded-3",
+            style: { maxHeight: "20em" },
+          }}
+        />
+      </div>
+    )}
+  </>
+) : (
+  <span
+    className="spinner-grow spinner-grow-sm me-1"
+    role="status"
+    aria-hidden="true"
+  />
+);
