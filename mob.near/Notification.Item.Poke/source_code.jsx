35f0067d4aff42ff429d5db6aaa0e210442534ee
evgenykuzyakov <evgenykuzyakov>
2023-01-22 19:04:01 -0500

    Update Notification.Item.Poke by mob.near at 2023-01-22 19:04:01.067

--- mob.near/Notification.Item.Poke/source_code.jsx
+++ mob.near/Notification.Item.Poke/source_code.jsx
@@ -1,24 +1,17 @@
-const { accountId, blockHeight, value } = props;
+const { accountId } = props;
 
 return (
-  <div className="d-flex justify-content-between">
-    <div className="me-2 text-truncate">
-      <div className="text-truncate">
-        <Widget src="mob.near/widget/ProfileLine" props={{ accountId }} />
-      </div>
-      <div
-        className="text-truncate text-muted"
-        style={{ paddingLeft: "1.8em" }}
-      >
-        poked you
-        <Widget src="mob.near/widget/TimeAgo" props={{ blockHeight }} />
-      </div>
-    </div>
-    <div className="text-nowrap">
-      <Widget
-        src="mob.near/widget/PokeButton"
-        props={{ accountId, back: true }}
-      />
-    </div>
-  </div>
+  <Widget
+    src="mob.near/widget/Notification.Item.LR"
+    props={{
+      L: "poked you",
+      R: (
+        <Widget
+          src="mob.near/widget/PokeButton"
+          props={{ accountId, back: true }}
+        />
+      ),
+      ...props,
+    }}
+  />
 );
