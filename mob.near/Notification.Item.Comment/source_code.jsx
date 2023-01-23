01063f4546e290f6457b7d4b8c01d5b0110f4768
evgenykuzyakov <evgenykuzyakov>
2023-01-22 19:12:27 -0500

    Update Notification.Item.Comment by mob.near at 2023-01-22 19:12:27.050

---
+++ mob.near/Notification.Item.Comment/source_code.jsx
@@ -0,0 +1,49 @@
+const { accountId, value } = props;
+
+return (
+  <Widget
+    src="mob.near/widget/Notification.Item.LR"
+    props={{
+      L: (
+        <>
+          <a
+            className="fw-bold text-muted"
+            href={`#/mob.near/widget/MainPage.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+          >
+            replied
+          </a>{" "}
+          to your
+          {value.item.path === `${context.accountId}/post/main` ? (
+            <a
+              className="fw-bold text-muted"
+              href={`#/mob.near/widget/MainPage.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+            >
+              post
+            </a>
+          ) : (
+            "item???"
+          )}
+        </>
+      ),
+      R: (
+        <>
+          <a
+            className="btn btn-outline-dark"
+            href={`#/mob.near/widget/MainPage.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+          >
+            View comment
+          </a>
+          {value.item.path === `${context.accountId}/post/main` && (
+            <a
+              className="btn btn-outline-dark d-none d-md-inline-block"
+              href={`#/mob.near/widget/MainPage.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+            >
+              View post
+            </a>
+          )}
+        </>
+      ),
+      ...props,
+    }}
+  />
+);
