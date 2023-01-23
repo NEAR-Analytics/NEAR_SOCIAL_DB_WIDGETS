ef74661a059204592c1f3272a3467f111c65f557
evgenykuzyakov <evgenykuzyakov>
2023-01-22 19:01:23 -0500

    Update Notification.Item.LR by mob.near at 2023-01-22 19:01:23.576

---
+++ mob.near/Notification.Item.LR/source_code.jsx
@@ -0,0 +1,19 @@
+const { accountId, blockHeight, value } = props;
+
+return (
+  <div className="d-flex justify-content-between">
+    <div className="me-2 text-truncate">
+      <div className="text-truncate">
+        <Widget src="mob.near/widget/ProfileLine" props={{ accountId }} />
+      </div>
+      <div
+        className="text-truncate text-muted"
+        style={{ paddingLeft: "1.8em" }}
+      >
+        {props.L}
+        <Widget src="mob.near/widget/TimeAgo" props={{ blockHeight }} />
+      </div>
+    </div>
+    <div className="text-nowrap">{props.R}</div>
+  </div>
+);
