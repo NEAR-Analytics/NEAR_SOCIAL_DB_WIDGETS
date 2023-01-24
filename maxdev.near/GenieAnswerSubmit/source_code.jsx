45e8239adb091c64c084dc9eca6220a2f1997fbe
maxhr <maxhr>
2023-01-24 06:53:38 -0500

    Update GenieAnswerSubmit by maxdev.near at 2023-01-24 06:53:38.429

---
+++ maxdev.near/GenieAnswerSubmit/source_code.jsx
@@ -0,0 +1,43 @@
+const ownerId = "maxhr.near";
+State.init({ answerContent: "" });
+
+const { questionRef } = props;
+
+if (!questionRef) {
+  return "No question ref provided";
+}
+
+return (
+  <div className="d-flex flex-row gap-1">
+    <textarea
+      className="form-control"
+      value={state.answerContent}
+      onChange={(e) => {
+        State.update({ answerContent: e.target.value });
+      }}
+    />
+    <CommitButton
+      className="btn btn-primary col-2"
+      onCommit={() => {
+        State.update({ answerContent: "" });
+      }}
+      data={{
+        experimental: {
+          genie: {
+            answers: {
+              [questionRef]: state.answerContent,
+            },
+          },
+        },
+        index: {
+          genie: JSON.stringify({
+            key: `answered/${questionRef}`,
+            value: 1,
+          }),
+        },
+      }}
+    >
+      Submit answer
+    </CommitButton>
+  </div>
+);
