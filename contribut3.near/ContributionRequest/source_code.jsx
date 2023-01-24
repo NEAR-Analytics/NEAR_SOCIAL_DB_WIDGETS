6955b83268967dbae6dbe9907c6843eec88aac93
contribut3.near <contribut3.near>
2023-01-24 11:21:36 -0500

    Update ContributionRequest by contribut3.near at 2023-01-24 11:21:36.120

--- contribut3.near/ContributionRequest/source_code.jsx
+++ contribut3.near/ContributionRequest/source_code.jsx
@@ -1,6 +1,12 @@
 const ownerId = "contribut3.near";
+const accountId = context.accountId;
 const entityId = props.entityId;
 const contributorId = props.contributorId;
+
+initState({
+  description: "",
+  startDate: "",
+});
 
 if (!entityId || !contributorId) {
   return (
@@ -9,6 +15,15 @@ if (!entityId || !contributorId) {
     </div>
   );
 }
+
+const contributor = Near.view(
+  ownerId,
+  "get_contribution",
+  { entity_id: entityId, contributor_id: accountId },
+  "final"
+);
+
+const isAuthorized = !!contributor && contributor.permissions.includes("Admin");
 
 const contributionRequest = isPreview
   ? props.contributionRequest
@@ -84,6 +99,62 @@ const descriptionArea = isUnderPost ? (
   <Markdown class="card-text" text={description}></Markdown>
 );
 
+const startDateInput = (
+  <div className="col-lg-6 mb-2">
+    Start date of contribution (optional):
+    <input
+      type="date"
+      value={state.startDate}
+      onChange={(e) => State.update({ startDate: e.target.value })}
+    />
+  </div>
+);
+
+const descriptionDiv = (
+  <div className="col-lg-12  mb-2">
+    Updated description (optional):
+    <br />
+    <textarea
+      value={state.description}
+      type="text"
+      rows={6}
+      className="form-control"
+      onChange={(event) => State.update({ description: event.target.value })}
+    />
+  </div>
+);
+
+const approveButton =
+  isAuthorized && !isPreview ? (
+    <div className="card-footer">
+      <div className="">
+        {startDateInput}
+        {descriptionDiv}
+        <a
+          className="btn btn-outline-primary mb-2"
+          onClick={() => {
+            const args = {
+              entity_id: entityId,
+              contributor_id: contributorId,
+            };
+
+            if (state.description) {
+              args.description = state.description;
+            }
+
+            if (state.startDate) {
+              args.start_date = state.startDate;
+            }
+
+            Near.call(ownerId, "approve_contribution", args);
+          }}
+        >
+          Approve
+        </a>{" "}
+      </div>
+    </div>
+  ) : null;
+
 return (
   <div className={`card my-2`}>
     {header}
@@ -91,5 +162,6 @@ return (
       {postTitle}
       {descriptionArea}
     </div>
+    {approveButton}
   </div>
 );
