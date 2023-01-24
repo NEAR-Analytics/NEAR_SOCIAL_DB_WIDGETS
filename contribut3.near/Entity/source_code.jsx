f12980d9ca124ed99462f295dd674f945e31379e
contribut3.near <contribut3.near>
2023-01-23 20:13:33 -0500

    Update Entity by contribut3.near at 2023-01-23 20:13:33.220

---
+++ contribut3.near/Entity/source_code.jsx
@@ -0,0 +1,34 @@
+const ownerId = "contribut3.near";
+const accountId = props.accountId;
+
+if (!accountId) {
+  return <div>Cannot show entity without account ID!</div>;
+}
+
+const entity = Near.view(
+  ownerId,
+  "get_entity",
+  { account_id: accountId },
+  "final"
+);
+
+if (!entity) {
+  return <div>Loading...</div>;
+}
+
+return (
+  <div className="card">
+    <div className="card-header">
+      <Widget
+        src="mob.near/widget/ProfileLine"
+        props={{
+          accountId,
+        }}
+      />
+    </div>
+    <div className="card-body">
+      <div>Type: {entity.kind}</div>
+      <div>Status: {entity.status}</div>
+    </div>
+  </div>
+);
