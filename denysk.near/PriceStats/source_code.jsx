3599c55249a209ccd62c05f1c824aea03f2ef724
AngelBlock <AngelBlock>
2023-01-24 17:49:59 -0500

    Update PriceStats by denysk.near at 2023-01-24 17:49:59.210

--- denysk.near/PriceStats/source_code.jsx
+++ denysk.near/PriceStats/source_code.jsx
@@ -6,13 +6,13 @@ const data = fetch("https://api.coingecko.com/api/v3/coins/near", {
 });
 
 return (
-  <div className="text-bg-light rounded-4 p-1 mb-1">
+  <div className="text-bg-light rounded-4 p-3 mb-3">
     {data !== null ? (
-      <div>
-        <div class="d-flex flex-sm-row flex-column">
-          <div class="mr-auto p-2">
+      <p>
+        <div class="d-flex clearfix flex-wrap flex-column flex-sm-row">
+          <div class="p-2">
             <div>NEAR/USD</div>
-            <h2>
+            <h1>
               ${data.body.market_data.current_price.usd}
               <small
                 class={
@@ -28,35 +28,37 @@ return (
                 )}
                 %
               </small>
-            </h2>
+            </h1>
           </div>
-          <div class="mr-auto p-2">
+          <div class="p-2">
             ATH
             <p class="text-success">
-              <b>${data.body.market_data.ath.usd.toFixed(2)}</b>
+              ${data.body.market_data.ath.usd.toFixed(2)}
             </p>
           </div>
-          <div class="mr-auto p-2">
+          <div class="p-2">
             24h high
             <p>
-              <b>${data.body.market_data.high_24h.usd.toFixed(2)}</b>
+              {" "}
+              <b>${data.body.market_data.high_24h.usd.toFixed(2)} </b>
             </p>
           </div>
           <div class="p-2">
             24h low
             <p>
-              <b>${data.body.market_data.low_24h.usd.toFixed(2)}</b>
+              {" "}
+              <b>${data.body.market_data.low_24h.usd.toFixed(2)} </b>
             </p>
           </div>
           <div class="p-2">
             Market Cap
             <p>
               <b>
-                $
-                {data.body.market_data.market_cap.usd
-                  .toFixed(0)
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                {"$" +
+                  data.body.market_data.market_cap.usd
+                    .toFixed(0)
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               </b>
             </p>
           </div>
@@ -64,11 +66,11 @@ return (
             Volume
             <p>
               <b>
-                $
-                {data.body.market_data.total_volume.usd
-                  .toFixed(0)
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                {"$" +
+                  data.body.market_data.total_volume.usd
+                    .toFixed(0)
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               </b>
             </p>
           </div>
@@ -76,47 +78,43 @@ return (
             Circulating Supply
             <p>
               <b>
-                $
-                {data.body.market_data.circulating_supply
-                  .toFixed(0)
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                {"$" +
+                  data.body.market_data.circulating_supply
+                    .toFixed(0)
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
               </b>
             </p>
           </div>
         </div>
         <div>
-          <div class="blockquote p-1">
-            <p class="blockquote-footer">
-              <small>
-                pricing information provided by{" "}
-                <a
-                  target="_blank"
-                  style={{ color: "inherit" }}
-                  variant="caption"
-                  rel="nofollow"
-                  href="https://www.coingecko.com/en/api_terms"
-                >
-                  CoinGecko
-                </a>
-              </small>
-              <small>|</small>
-              <small>
-                widget sponsored by{" "}
-                <a
-                  target="_blank"
-                  style={{ color: "inherit" }}
-                  variant="caption"
-                  rel="nofollow"
-                  href="https://www.nearweek.com"
-                >
-                  NEARWEEK
-                </a>
-              </small>
-            </p>
-          </div>
+          <small>
+            pricing information provided by{" "}
+            <a
+              target="_blank"
+              style={{ color: "inherit" }}
+              variant="caption"
+              rel="nofollow"
+              href="https://www.coingecko.com/en/api_terms"
+            >
+              CoinGecko
+            </a>
+          </small>
+          <small>|</small>
+          <small>
+            widget sponsored by{" "}
+            <a
+              target="_blank"
+              style={{ color: "inherit" }}
+              variant="caption"
+              rel="nofollow"
+              href="https://www.nearweek.com"
+            >
+              NEARWEEK
+            </a>
+          </small>
         </div>
-      </div>
+      </p>
     ) : (
       <div>Loading ...</div>
     )}
