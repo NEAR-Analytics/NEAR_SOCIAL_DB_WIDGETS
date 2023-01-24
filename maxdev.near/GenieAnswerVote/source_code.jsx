b251ce750b11e649780d389357c06b4a41068b9a
maxhr <maxhr>
2023-01-24 06:54:24 -0500

    Update GenieAnswerVote by maxdev.near at 2023-01-24 06:54:24.160

---
+++ maxdev.near/GenieAnswerVote/source_code.jsx
@@ -0,0 +1,74 @@
+const ownerId = "maxhr.near";
+const { questionRef, answeredBy } = props;
+
+const votes = Social.index("genie", `vote/${questionRef}/${answeredBy}`) || [];
+const upvotes = votes.filter((v) => v.value.dir === 1).length;
+const downvotes = votes.filter((v) => v.value.dir === -1).length;
+
+return (
+  <div className="d-flex flex-column gap-1">
+    {/*JSON.stringify(votes)*/}
+    {/*<div>Upvotes: {upvotes}</div>*/}
+    {/*<div>Downvotes: {downvotes}</div>*/}
+    <div
+      className="d-flex flex-row"
+      style={{
+        width: "8rem",
+        borderStyle: "solid",
+        borderWidth: "2px",
+        borderRadius: ".5rem",
+      }}
+    >
+      <CommitButton
+        className="btn btn-outline-dark col-8"
+        style={{
+          borderRadius: 0,
+          borderRight: "2px solid black",
+          borderLeft: "0",
+          borderTop: "0",
+          borderBottom: "0",
+        }}
+        data={{
+          index: {
+            genie: JSON.stringify([
+              {
+                key: `vote/${questionRef}/${answeredBy}`,
+                value: {
+                  questionRef,
+                  answeredBy,
+                  dir: 1,
+                },
+              },
+              {
+                key: `vote/${answeredBy}`,
+                value: 1,
+              },
+            ]),
+          },
+        }}
+      >
+        <i className="bi bi-arrow-up-short" />
+        {upvotes}
+      </CommitButton>
+      <CommitButton
+        className="btn btn-outline-dark border-none border-0 col"
+        style={{ borderRadius: 0 }}
+        data={{
+          index: {
+            genie: JSON.stringify({
+              key: `vote/${questionRef}/${answeredBy}`,
+              value: {
+                questionRef,
+                answeredBy,
+                dir: -1,
+              },
+            }),
+          },
+        }}
+      >
+        <i className="bi bi-arrow-down-short" />
+        {/*downvotes*/}
+      </CommitButton>
+    </div>
+  </div>
+);
