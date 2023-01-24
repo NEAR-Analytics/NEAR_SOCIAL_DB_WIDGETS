7f6349360f4ee292f574063caf2f6578b23c4191
maxhr <maxhr>
2023-01-24 06:52:17 -0500

    Update GenieQuestionAnswerView by maxdev.near at 2023-01-24 06:52:17.005

---
+++ maxdev.near/GenieQuestionAnswerView/source_code.jsx
@@ -0,0 +1,38 @@
+const ownerId = "maxhr.near";
+const { questionRef, searchString } = props;
+
+const asker = questionRef.split("--")[0];
+
+const question = Social.getr(
+  `${asker}/experimental/genie/questions/${questionRef}`
+);
+
+const BodyText = styled.p`
+  color: #68717A
+`;
+
+return (
+  <div className="d-flex flex-column gap-1">
+    <div className="d-flex flex-column gap-1">
+      <div className="d-flex align-items-center">
+        <div
+          style={{ width: "100%" }}
+          className="d-flex align-items-center justify-content-between"
+        >
+          <Widget
+            src="tiffany.near/widget/Profile"
+            props={{ accountId: asker }}
+          />
+          <div>
+            {new Date(
+              parseInt(questionRef.split("--")[1])
+            ).toLocaleDateString()}
+          </div>
+        </div>
+      </div>
+      <h3>{question.title}</h3>
+      <BodyText>{question.content}</BodyText>
+    </div>
+    <Widget src={`${ownerId}/widget/GenieAnswerList`} props={{ questionRef }} />
+  </div>
+);
