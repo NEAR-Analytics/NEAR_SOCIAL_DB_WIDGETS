d8d2af722d96b5fd01f620dc53455779c6977e73
events_v1.near <events_v1.near>
2023-01-25 17:28:05 -0500

    Update app__frame by events_v1.near at 2023-01-25 17:28:05.530

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -1,7 +1,7 @@
 const APP_OWNER = 'events_v1.near';
 const APP_NAME = 'events_app';
 const EVENTS_CONTRACT = 'events_v1.near';
-const ENTRY_ROUTE = '{{ env.ENTRY_ROUTE }}';
+const ENTRY_ROUTE = 'index';
 
 const accountId = context.accountId;
 if (!accountId) {
