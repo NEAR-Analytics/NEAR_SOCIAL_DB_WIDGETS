6c7024d901516dbb8f2dfcb266d513b3d780b60b
contribut3.near <contribut3.near>
2023-01-25 11:25:29 -0500

    Update ContributorList by contribut3.near at 2023-01-25 11:25:29.525

--- contribut3.near/ContributorList/source_code.jsx
+++ contribut3.near/ContributorList/source_code.jsx
@@ -3,21 +3,23 @@ const search = props.search ?? "";
 
 const allContributors = (
   Near.view(ownerId, "get_contributors", {}, "final", true) ?? []
-).filter((accountId) => (search ? accountId.includes(search) : true));
+)
+  .filter((accountId) => (search ? accountId.includes(search) : true))
+  .sort((a, b) => a.localeCompare(b));
 
-allContributors.sort((a, b) => a.localeCompare(b));
+if (!allContributors || allContributors.length === 0) {
+  return "No contributors found!";
+}
 
 return (
   <div>
-    {allContributors
-      ? allContributors.map((accountId) => (
-          <div key={accountId} className="mb-2">
-            <Widget
-              src={`${ownerId}/widget/Contributor`}
-              props={{ accountId, notStandalone: true }}
-            />
-          </div>
-        ))
-      : ""}
+    {allContributors.map((accountId) => (
+      <div key={accountId} className="mb-2">
+        <Widget
+          src={`${ownerId}/widget/Contributor`}
+          props={{ accountId, notStandalone: true }}
+        />
+      </div>
+    ))}
   </div>
 );
