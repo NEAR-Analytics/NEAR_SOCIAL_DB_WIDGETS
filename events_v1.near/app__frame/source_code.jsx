f8062324f035eb1e0e3d71de3071c1f85f021538
events_v1.near <events_v1.near>
2023-01-25 17:44:47 -0500

    Update app__frame by events_v1.near at 2023-01-25 17:44:47.562

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -99,9 +99,13 @@ return (
       style={{
         width: '100%',
         height: '100%',
-        position: 'relative',
-        overflow: 'hidden',
+        position: 'fixed',
+        overflow: 'scroll',
         backgroundColor: 'red',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
       }}
     >
       {renderComponent(state.route.name, state.route.props)}
