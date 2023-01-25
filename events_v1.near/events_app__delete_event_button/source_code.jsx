a02cc0166478d0ac81fcd87f235ef5fdf5e7e3ae
events_v1.near <events_v1.near>
2023-01-25 16:06:34 -0500

    Update events_app__delete_event_button by events_v1.near at 2023-01-25 16:06:34.550

--- events_v1.near/events_app__delete_event_button/source_code.jsx
+++ events_v1.near/events_app__delete_event_button/source_code.jsx
@@ -36,7 +36,7 @@ const DeleteEventButton = styled.button`
   font-size: 14px;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
   cursor: pointer;
-  transition: 0.3s;
+  transition: all 0.3s ease-in-out;
 
   &:hover {
     background-color: #e74c3c;
