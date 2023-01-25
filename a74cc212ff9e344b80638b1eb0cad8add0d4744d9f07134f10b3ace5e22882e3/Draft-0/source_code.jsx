010e6ac61bc3629a45b59d24bc3391d926b56fdb
a74cc212ff9e344b80638b1eb0cad8add0d4744d9f07134f10b3ace5e22882e3 <a74cc212ff9e344b80638b1eb0cad8add0d4744d9f07134f10b3ace5e22882e3>
2023-01-25 08:10:18 -0500

    Update Draft-0 by a74cc212ff9e344b80638b1eb0cad8add0d4744d9f07134f10b3ace5e22882e3 at 2023-01-25 08:10:18.560

---
+++ a74cc212ff9e344b80638b1eb0cad8add0d4744d9f07134f10b3ace5e22882e3/Draft-0/source_code.jsx
@@ -0,0 +1,47 @@
+const accountId = props.debugAccountId ?? context.accountId;
+
+const project = Social.getr(`${accountId}/project`);
+
+const name = project.name;
+const image = project.image;
+
+const editProjectButton = (
+  <div>
+    <a className="btn btn-success" href="#/gov.near/widget/ProjectEditor">
+      Edit Project
+    </a>
+  </div>
+);
+
+if (!project) {
+  return (
+    <div>
+      <p>Create your project!</p>
+      {editProjectButton}
+    </div>
+  );
+}
+
+if (!name) {
+  return (
+    <div className="alert alert-warning">
+      <p>Your project is missing a name.</p>
+      {editProjectButton}
+    </div>
+  );
+}
+
+if (
+  !image.ipfs_cid &&
+  (!image.nft.contractId || !image.nft.tokenId) &&
+  !image.url
+) {
+  return (
+    <div className="alert alert-warning">
+      <p>Your project is missing an image.</p>
+      {editProjectButton}
+    </div>
+  );
+}
+
+return <></>;
