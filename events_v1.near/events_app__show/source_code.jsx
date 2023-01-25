b3f11b0e2e3d12729f5333f20d6b123a443a63ae
events_v1.near <events_v1.near>
2023-01-25 15:52:55 -0500

    Update events_app__show by events_v1.near at 2023-01-25 15:52:55.257

---
+++ events_v1.near/events_app__show/source_code.jsx
@@ -0,0 +1,39 @@
+const EVENTS_CONTRACT = 'events_v1.near';
+const APP_OWNER = 'events_v1.near';
+const APP_NAME = 'events_app';
+
+const eventId = props.event_id;
+if (!eventId) {
+  return 'props.eventId is required';
+}
+
+const event = Near.view(EVENTS_CONTRACT, 'get_event', {
+  event_id: props.event_id,
+});
+if (!event) {
+  return 'Loading';
+}
+
+const images = event.images || [];
+
+const EventImage = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+return (
+  <>
+    {images.map((image, idx) => {
+      return <EventImage src={image} key={`image_idx_${idx}`} />;
+    })}
+
+    <h1>{event.name}</h1>
+
+    <a
+      href={`#/${APP_OWNER}/widget/${APP_NAME}__edit?event_id=${event.id}`}
+      className="text-decoration-none"
+    >
+      Edit
+    </a>
+  </>
+);
