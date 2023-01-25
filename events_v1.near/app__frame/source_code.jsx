d11bcbc9519c18834cd931b987d5b2e9b3028855
events_v1.near <events_v1.near>
2023-01-25 17:59:08 -0500

    Update app__frame by events_v1.near at 2023-01-25 17:59:08.259

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -109,6 +109,7 @@ function renderComponent(name, props) {
         ...props,
         routing,
         ...buildRenderingInfo(),
+        Components,
         accountId,
         env,
         VERSION,
