379bf219553f7a1bf236cefc27004ccd621484ac
contribut3.near <contribut3.near>
2023-01-25 08:15:35 -0500

    Update AdminList by contribut3.near at 2023-01-25 08:15:35.658

---
+++ contribut3.near/AdminList/source_code.jsx
@@ -0,0 +1,28 @@
+const ownerId = "contribut3.near";
+
+const allEntities =
+  Near.view(
+    ownerId,
+    "get_contributor_admin_entities",
+    { account_id: props.accountId },
+    "final"
+  ) ?? [];
+
+allEntities.sort((a, b) => a.localeCompare(b));
+
+console.log(allEntities);
+
+return (
+  <div>
+    {allEntities && allEntities.length > 0
+      ? allEntities.map(([accountId]) => (
+          <div key={accountId} className="mb-2">
+            <Widget
+              src={`${ownerId}/widget/Entity`}
+              props={{ accountId, notStandalone: true }}
+            />
+          </div>
+        ))
+      : "No entities with Admin access for your account!"}
+  </div>
+);
