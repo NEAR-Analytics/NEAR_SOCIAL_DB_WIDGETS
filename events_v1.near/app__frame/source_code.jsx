2b171b57fba9e017beef88b6814e8fcc35341b63
events_v1.near <events_v1.near>
2023-01-25 17:20:34 -0500

    Update app__frame by events_v1.near at 2023-01-25 17:20:34.596

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -1,3 +1,11 @@
 const APP_OWNER = '{{ env.APP_OWNER }}';
 const APP_NAME = '{{ env.APP_NAME }}';
 const EVENTS_CONTRACT = '{{ env.EVENTS_CONTRACT }}';
+
+const env = {
+  APP_OWNER,
+  APP_NAME,
+  EVENTS_CONTRACT,
+};
+
+return <>{JSON.stringify(env, null, 4)}</>;
