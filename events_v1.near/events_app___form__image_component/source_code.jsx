2a8a56652886afc3b83a8584e1c4fde06567cec2
events_v1.near <events_v1.near>
2023-01-25 16:19:28 -0500

    Update events_app___form__image_component by events_v1.near at 2023-01-25 16:19:28.722

---
+++ events_v1.near/events_app___form__image_component/source_code.jsx
@@ -0,0 +1,71 @@
+const image = props.image;
+
+if (!image) {
+  return 'loading';
+}
+
+const ErrorMessage = styled.div`
+  color: red;
+`;
+
+const onChange = props.onChange;
+if (!onChange) {
+  return <ErrorMessage>onChange is required</ErrorMessage>;
+}
+
+const onRemove = props.onRemove;
+if (!onRemove) {
+  return <ErrorMessage>onRemove is required</ErrorMessage>;
+}
+
+const ImageTypes = [
+  { value: 'tile', label: 'Tile' },
+  { value: 'banner', label: 'Banner' },
+];
+
+const Select = styled.select`
+  width: 100%;
+  padding: 0.5rem;
+  margin: 0;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+`;
+
+return (
+  <>
+    <Select
+      style={{ width: '100px' }}
+      value={image.type}
+      onChange={(event) => {
+        image.type = event.target.value;
+        onChange(image);
+      }}
+    >
+      {ImageTypes.map((type) => (
+        <option key={type.value} value={type.value}>
+          {type.label}
+        </option>
+      ))}
+    </Select>
+
+    <div className="ms-2">
+      <IpfsImageUpload
+        image={image.url}
+        onChange={(event) => {
+          image.url = event.target.value;
+          onChange(image);
+        }}
+      />
+    </div>
+
+    <button
+      className="ms-2 btn btn-danger"
+      onClick={() => {
+        onRemove(image);
+      }}
+    >
+      Remove
+    </button>
+  </>
+);
