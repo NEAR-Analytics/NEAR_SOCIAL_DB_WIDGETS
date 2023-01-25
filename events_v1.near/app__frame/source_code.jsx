9695559dbf73b08741d2fdd00ae7ad5b46df43d6
events_v1.near <events_v1.near>
2023-01-25 17:44:55 -0500

    Update app__frame by events_v1.near at 2023-01-25 17:44:55.284

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -100,7 +100,6 @@ return (
         width: '100%',
         height: '100%',
         position: 'fixed',
-        overflow: 'scroll',
         backgroundColor: 'red',
         top: 0,
         left: 0,
