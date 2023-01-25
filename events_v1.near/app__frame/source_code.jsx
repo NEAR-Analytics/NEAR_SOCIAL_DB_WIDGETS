d93a041300c33fde9c315500c9031635eb520b94
events_v1.near <events_v1.near>
2023-01-25 17:35:40 -0500

    Update app__frame by events_v1.near at 2023-01-25 17:35:40.912

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -48,6 +48,22 @@ const Select = styled.select`
   cursor: pointer;
 `;
 
+const Button = styled.button`
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  transition: all 0.5s ease;
+
+  &:hover {
+    background-color: #3e8e41;
+  }
+`;
+
 const rendering = {
   renderComponent,
   components: {
