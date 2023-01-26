364c9055a7ea3a3bd53be7306dd2d66de2894ee0
events_v1.near <events_v1.near>
2023-01-26 17:33:37 -0500

    Update app__layouts__modal by events_v1.near at 2023-01-26 17:33:37.568

--- events_v1.near/app__layouts__modal/source_code.jsx
+++ events_v1.near/app__layouts__modal/source_code.jsx
@@ -100,6 +100,11 @@ return (
               width: '100%',
               maxWidth: '100%',
               overflow: 'auto',
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
               paddingTop: title ? PADDING_TITLE_WITH : PADDING_TITLE_WITHOUT,
             }}
           >
