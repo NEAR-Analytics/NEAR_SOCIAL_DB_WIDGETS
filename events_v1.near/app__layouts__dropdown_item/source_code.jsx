2928047ea3f01474e5a8e30dfc1f0ac5fd6a1243
events_v1.near <events_v1.near>
2023-01-25 20:31:16 -0500

    Update app__layouts__dropdown_item by events_v1.near at 2023-01-25 20:31:16.147

--- events_v1.near/app__layouts__dropdown_item/source_code.jsx
+++ events_v1.near/app__layouts__dropdown_item/source_code.jsx
@@ -1,5 +1,7 @@
+const key = props.key || props.component.src;
+
 return (
-  <li className="nav-item">
+  <li className="nav-item" key={idx}>
     <Widget src={props.component.src} props={props.component.props} />
   </li>
 );
