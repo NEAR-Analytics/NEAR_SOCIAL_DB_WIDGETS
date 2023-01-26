fef62e4f751fb413a5d2d44a2d6e4d344cf2d497
evgenykuzyakov <evgenykuzyakov>
2023-01-26 01:37:39 -0500

    Update Notification.Item.Mention by mob.near at 2023-01-26 01:37:39.186

---
+++ mob.near/Notification.Item.Mention/source_code.jsx
@@ -0,0 +1,50 @@
+const { accountId, value, blockHeight } = props;
+
+return (
+  <Widget
+    src="mob.near/widget/Notification.Item.LR"
+    props={{
+      L: (
+        <>
+          mention you in their
+          {value.item.path === `${accountId}/post/main` ? (
+            <a
+              className="fw-bold text-muted"
+              href={`#/mob.near/widget/MainPage.Post.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+            >
+              post
+            </a>
+          ) : value.item.path === `${accountId}/post/comment` ? (
+            <a
+              className="fw-bold text-muted"
+              href={`#/mob.near/widget/MainPage.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+            >
+              comment
+            </a>
+          ) : (
+            "item???"
+          )}
+        </>
+      ),
+      R:
+        value.item.path === `${accountId}/post/main` ? (
+          <a
+            className="btn btn-outline-dark"
+            href={`#/mob.near/widget/MainPage.Post.Page?accountId=${accountId}&blockHeight=${value.item.blockHeight}`}
+          >
+            View post
+          </a>
+        ) : value.item.path === `${accountId}/post/comment` ? (
+          <a
+            className="btn btn-outline-dark"
+            href={`#/mob.near/widget/MainPage.Comment.Page?accountId=${accountId}&blockHeight=${value.item.blockHeight}`}
+          >
+            View comment
+          </a>
+        ) : (
+          ""
+        ),
+      ...props,
+    }}
+  />
+);
