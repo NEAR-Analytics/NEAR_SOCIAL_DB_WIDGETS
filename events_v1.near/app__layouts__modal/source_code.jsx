7318c8383a1882c732a2d0d20a3f577285d0d08a
events_v1.near <events_v1.near>
2023-01-26 17:45:43 -0500

    Update app__layouts__modal by events_v1.near at 2023-01-26 17:45:43.116

--- events_v1.near/app__layouts__modal/source_code.jsx
+++ events_v1.near/app__layouts__modal/source_code.jsx
@@ -16,7 +16,7 @@ return (
         backgroundColor: 'rgba(255,255,255,0.85)',
         overflow: 'hidden',
 
-        backdropFilter: 'blur(10px) saturate(180%)',
+        backdropFilter: 'blur(20px) saturate(180%)',
       }}
     >
       <div
@@ -30,8 +30,7 @@ return (
           height: '80%',
           maxWidth: 600,
           maxHeight: 600,
-
-          boxShadow: '0 0 10px 10px rgba(0,0,0,0.5)',
+          boxShadow: '0 0 40px -10px black',
           borderRadius: BORDER_RADIUS,
         }}
       >
