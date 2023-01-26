38eeebb806b4171b4f6e998163fcc53454e5d875
v3rify.near <v3rify.near>
2023-01-26 18:27:46 -0500

    Update Index by v3rify.near at 2023-01-26 18:27:46.155

--- v3rify.near/Index/source_code.jsx
+++ v3rify.near/Index/source_code.jsx
@@ -13,8 +13,8 @@ const result = state.show ? (
       alignItems: "center",
       left: 0,
       top: 0,
-      width: "100vw",
-      height: "100vh",
+      width: "100%",
+      height: "100%",
       opacity: 0.8,
       backgroundColor: "grey",
     }}
