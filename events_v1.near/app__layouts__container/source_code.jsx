ff97eb46421c7c1bac05244412f14d24cecb24a8
events_v1.near <events_v1.near>
2023-01-25 19:07:34 -0500

    Update app__layouts__container by events_v1.near at 2023-01-25 19:07:34.835

--- events_v1.near/app__layouts__container/source_code.jsx
+++ events_v1.near/app__layouts__container/source_code.jsx
@@ -5,7 +5,7 @@ const items = props.layoutProps.items ?? [];
 
 const Dropdown = items && items.length > 0 ? (
   <>
-            <button
+     <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
@@ -24,56 +24,6 @@ const Dropdown = items && items.length > 0 ? (
                   Home
                 </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <div className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
             </div>
           </div>  
         </>
