6bb304713d56ef38c1d08181009f597169c19bb2
events_v1.near <events_v1.near>
2023-01-26 17:12:49 -0500

    Update app__layouts__container by events_v1.near at 2023-01-26 17:12:49.842

--- events_v1.near/app__layouts__container/source_code.jsx
+++ events_v1.near/app__layouts__container/source_code.jsx
@@ -96,12 +96,12 @@ return (
         }}
       >
         <div className="col-12">
-          {props.engine.renderComponent(
+          {/* {props.engine.renderComponent(
             props.component.name,
             props.component.props,
             props.component.layout,
             props.component.layoutProps
-          )}
+          )} */}
         </div>
       </div>
     </div>
