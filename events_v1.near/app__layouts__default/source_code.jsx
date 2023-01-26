370398121faa35cbaf0625962b4f883ebfa0c2af
events_v1.near <events_v1.near>
2023-01-26 17:16:24 -0500

    Update app__layouts__default by events_v1.near at 2023-01-26 17:16:24.821

--- events_v1.near/app__layouts__default/source_code.jsx
+++ events_v1.near/app__layouts__default/source_code.jsx
@@ -1,1 +1,6 @@
-return <Widget src={props.component.src} props={props.component.props} />;
+return props.engine.renderComponent(
+  props.component.name,
+  props.component.props,
+  props.component.layout,
+  props.component.layoutProps
+);
