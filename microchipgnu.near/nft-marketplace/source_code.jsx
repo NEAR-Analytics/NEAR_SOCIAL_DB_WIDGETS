7ee9eeeca4507575ee4409500b78a7f78ead8327
microchipgnu <microchipgnu>
2023-01-26 15:55:38 -0500

    Update nft-marketplace by microchipgnu.near at 2023-01-26 15:55:38.867

--- microchipgnu.near/nft-marketplace/source_code.jsx
+++ microchipgnu.near/nft-marketplace/source_code.jsx
@@ -12,7 +12,7 @@ const data = fetch("https://graph.mintbase.xyz", {
   body: JSON.stringify({
     query: `
       query MyQuery {
-        mb_views_active_listings(limit: 10, order_by: {created_at: desc}, where: {market_id: {_eq: "simple.market.mintbase1.near"}}) {
+        mb_views_active_listings(limit: 100, order_by: {created_at: desc}, where: {market_id: {_eq: "simple.market.mintbase1.near"}}) {
             listed_by
             created_at
             price
