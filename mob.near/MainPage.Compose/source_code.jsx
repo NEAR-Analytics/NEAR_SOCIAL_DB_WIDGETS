a7ef36555f3ea5704c53867b20a33653cee724e0
evgenykuzyakov <evgenykuzyakov>
2023-01-25 23:20:49 -0500

    Update MainPage.Compose by mob.near at 2023-01-25 23:20:49.112

--- mob.near/MainPage.Compose/source_code.jsx
+++ mob.near/MainPage.Compose/source_code.jsx
@@ -3,77 +3,43 @@ if (!context.accountId) {
 }
 
 State.init({
-  image: {},
-  text: "",
+  composeData: ({ content }) => {
+    return {
+      post: {
+        main: JSON.stringify(content),
+      },
+      index: {
+        post: JSON.stringify({
+          key: "main",
+          value: {
+            type: "md",
+          },
+        }),
+      },
+    };
+  },
+  onChange: ({ content }) => {
+    State.update({ content });
+  },
 });
-
-const content = (state.text || state.image.cid) && {
-  type: "md",
-  text: state.text,
-  image: state.image.cid ? { ipfs_cid: state.image.cid } : undefined,
-};
-
-const onChange = (text) => {
-  State.update({
-    text,
-  });
-};
 
 return (
   <>
-    <div className="text-bg-light rounded-4">
-      <div className="p-2">
-        <textarea
-          className="form-control border-0 text-bg-light w-100"
-          value={state.text || ""}
-          onChange={(e) => onChange(e.target.value)}
-          placeholder="What's happening?"
-        />
-      </div>
-      <div className="d-flex flex-row p-2 border-top">
-        <div className="flex-grow-1">
-          <IpfsImageUpload
-            image={state.image}
-            className="btn btn-outline-secondary border-0 rounded-3"
-          />
-        </div>
-        <div>
-          <CommitButton
-            disabled={!content}
-            force
-            className="btn btn-dark rounded-3"
-            data={{
-              post: {
-                main: JSON.stringify(content),
-              },
-              index: {
-                post: JSON.stringify({
-                  key: "main",
-                  value: {
-                    type: "md",
-                  },
-                }),
-              },
-            }}
-            onCommit={() =>
-              State.update({
-                image: {},
-                text: "",
-              })
-            }
-          >
-            Post
-          </CommitButton>
-        </div>
-      </div>
-    </div>
-    {content && (
+    <Widget
+      src="mob.near/widget/Common.Compose"
+      props={{
+        composeData: state.composeData,
+        composeText: "Post",
+        onChange: state.onChange,
+      }}
+    />
+    {state.content && (
       <div className="mt-3">
         <Widget
           src="mob.near/widget/MainPage.Post"
           props={{
             accountId: context.accountId,
-            content,
+            content: state.content,
             blockHeight: "now",
           }}
         />
