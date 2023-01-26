7f127fcc813d066bfaf2289c383ad0a1d2884b0a
events_v1.near <events_v1.near>
2023-01-25 20:19:43 -0500

    Update app__frame by events_v1.near at 2023-01-25 20:19:43.315

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -88,16 +88,15 @@ const currentRoute = state.layers[state.layers.length - 1];
 
 function push(name, props, layout, layoutProps) {
   console.log('push', name, props, layout, layoutProps);
+  const layer = {
+    name,
+    props: props || {},
+    layout: layout || 'default',
+    layoutProps: layoutProps || {},
+  };
+
   State.update({
-    layers: [
-      ...state.layers,
-      {
-        name,
-        props: props || {},
-        layout: layout || 'default',
-        layoutProps: layoutProps || {},
-      },
-    ],
+    layers: [...state.layers, layer],
   });
   console.log('state', state.layers);
 }
