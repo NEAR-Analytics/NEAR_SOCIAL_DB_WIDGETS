ff40a9570aff71e6919a8d0c84dbf784a7a13953
microchipgnu <microchipgnu>
2023-01-26 16:26:02 -0500

    Update PriceOracle.NearPrices by microchipgnu.near at 2023-01-26 16:26:02.808

---
+++ microchipgnu.near/PriceOracle.NearPrices/source_code.jsx
@@ -0,0 +1,33 @@
+let keys = props.keys ?? ["wrap.near"];
+
+let price_data = Near.view("priceoracle.near", "get_price_data", {});
+let assets = Near.view("oracle-prices.near", "get_config", { keys });
+
+let price_data_prepared = [];
+price_data.prices.map(
+  (data) => (price_data_prepared[data.asset_id] = data.price)
+);
+
+let prices = assets.map((asset) => {
+  let asset_account_id = asset[0];
+  let asset_name = asset[1].token_name;
+  let asset_decimals = asset[1].decimals;
+
+  let asset_price = price_data_prepared[asset_account_id];
+  let price = new Big(asset_price.multiplier).div(
+    new Big(10).pow(asset_price.decimals - asset_decimals)
+  );
+
+  return (
+    <div class="col">
+      {asset_name}: ${price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")}
+    </div>
+  );
+});
+
+return (
+  <div class="container">
+    <h5>Priceoracle.near prices</h5>
+    <div class="row">{prices}</div>
+  </div>
+);
