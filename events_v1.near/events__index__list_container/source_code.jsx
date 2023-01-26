12c0c4b7743f7b7ae625b81b5f147e8e151d9d70
events_v1.near <events_v1.near>
2023-01-26 13:04:11 -0500

    Update events__index__list_container by events_v1.near at 2023-01-26 13:04:11.245

---
+++ events_v1.near/events__index__list_container/source_code.jsx
@@ -0,0 +1,21 @@
+const EVENTS_CONTRACT = 'events_v1.near';
+
+// accountID is used to determine for whom the events are displayed
+// if no accountID is provided, all events are displayed
+const accountId = props.forAccountId;
+
+let events = [];
+if (accountId === undefined) {
+  events = Near.view(EVENTS_CONTRACT, 'get_all_events');
+} else {
+  events = Near.view(EVENTS_CONTRACT, 'get_all_events_by_account', {
+    account_id: accountId,
+  });
+}
+
+const Loading = props.Components.Loading;
+if (!events) {
+  return <Loading>Loading events</Loading>;
+}
+
+return props.engine.renderComponent('index.list', { events });
