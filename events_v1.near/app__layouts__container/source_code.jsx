4e8877f791ddde59f630df9f5c442d2dacde3a17
events_v1.near <events_v1.near>
2023-01-25 19:04:39 -0500

    Update app__layouts__container by events_v1.near at 2023-01-25 19:04:39.941

--- events_v1.near/app__layouts__container/source_code.jsx
+++ events_v1.near/app__layouts__container/source_code.jsx
@@ -10,7 +10,7 @@ return (
     >
       <div className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="">
             {title}
           </a>
 
@@ -25,6 +25,7 @@ return (
           >
             <span className="navbar-toggler-icon"></span>
           </button>
+
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
