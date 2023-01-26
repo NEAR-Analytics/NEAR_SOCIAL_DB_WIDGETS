0aeaedec29b0b0747e725c470bc719e193108d2d
contribut3.near <contribut3.near>
2023-01-26 13:35:31 -0500

    Update ModeratorAccountIdInput by contribut3.near at 2023-01-26 13:35:31.417

--- contribut3.near/ModeratorAccountIdInput/source_code.jsx
+++ contribut3.near/ModeratorAccountIdInput/source_code.jsx
@@ -13,6 +13,7 @@ return (
       id="account-id-input"
       labelKey="name"
       onChange={(accountId) => {
+        console.log(accountId);
         const existing = getEntity(accountId[0].name);
         const updatedState = {
           accountId,
