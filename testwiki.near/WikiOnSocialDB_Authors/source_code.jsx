a5e568b0dd688c0b1537bcca92189cd17841df18
testwiki.near <testwiki.near>
2023-01-27 08:17:36 -0500

    Update WikiOnSocialDB_Authors by testwiki.near at 2023-01-27 08:17:36.497

--- testwiki.near/WikiOnSocialDB_Authors/source_code.jsx
+++ testwiki.near/WikiOnSocialDB_Authors/source_code.jsx
@@ -1,1 +1,24 @@
+console.log(props.text);
+// const authors = Array.from(props.resultArticles, ({ author }) => author);
+// const uniqAuthors = Array.from(new Set(authors));
+return (
+  <>
+    <h6>Total authors: {uniqAuthors.length}</h6>
+    <ul>
+      <li>{props.text}</li>
+      {/*
+      {uniqAuthors.map((author) => (
+        <li>
+          <a
+            href={`https://near.social/#/mob.near/widget/ProfilePage?accountId=${author}`}
+          >
+            {author}
+          </a>
+        </li>
+      ))}
+      */}
+    </ul>
+  </>
+);
+
 return <div>Author Widget {props.text}</div>;
