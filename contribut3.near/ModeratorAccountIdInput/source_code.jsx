c6a2d5b3369757672ce4e314060bd118e8f9f8d0
contribut3.near <contribut3.near>
2023-01-27 15:09:39 -0500

    Update ModeratorAccountIdInput by contribut3.near at 2023-01-27 15:09:39.016

--- contribut3.near/ModeratorAccountIdInput/source_code.jsx
+++ contribut3.near/ModeratorAccountIdInput/source_code.jsx
@@ -4,6 +4,63 @@ const accountId = props.accountId;
 const fixed = props.fixed;
 const update = props.update;
 
+initState({
+  valid: props.valid ?? true,
+  errorMessage: "",
+});
+
+const validate = (accountId) => {
+  const accountIdRegex =
+    /^(([a-z\d]+[\-_])*[a-z\d]+\.)*([a-z\d]+[\-_])*[a-z\d]+$/;
+
+  if (typeof accountId !== "string") {
+    State.update({
+      valid: false,
+      errorMessage: "Account ID must be a text value!",
+    });
+    update(accountId, false);
+    return;
+  }
+
+  if (accountId.length < 2) {
+    State.update({
+      valid: false,
+      errorMessage: "Account ID must be at least 2 characters long!",
+    });
+    update(accountId, false);
+    return;
+  }
+
+  if (accountId.length > 64) {
+    State.update({
+      valid: false,
+      errorMessage: "Account ID must be at most 64 characters long!",
+    });
+    update(accountId, false);
+    return;
+  }
+
+  if (!accountIdRegex.test(accountId)) {
+    State.update({
+      valid: false,
+      errorMessage: (
+        <>
+          Account ID must follow the rules specified{" "}
+          <a href="https://nomicon.io/DataStructures/Account#account-id-rules">
+            here
+          </a>
+          !
+        </>
+      ),
+    });
+    update(accountId, false);
+    return;
+  }
+
+  State.update({ valid: true, errorMessage: "" });
+  update(accountId, true);
+};
+
 const formatDate = (timestampString) => {
   const date = new Date(Number(timestampString));
   return `${date.getFullYear()}-${(date.getMonth() + 1)
@@ -14,24 +71,14 @@ const formatDate = (timestampString) => {
 return (
   <div className="col-lg-12 mb-2">
     Account ID of entity:
+    <small style={{ color: "red" }}>
+      {state.valid ? null : state.errorMessage}
+    </small>
     <Typeahead
       id="account-id-input"
       labelKey="name"
       onChange={(accountId) => {
         const args = { account_id: accountId[0].name };
-        const [{ name }] = accountId;
-        if (
-          !(
-            typeof name === "string" &&
-            name.length >= 2 &&
-            name.length <= 64 &&
-            /^(([a-z\d]+[\-_])*[a-z\d]+\.)*([a-z\d]+[\-_])*[a-z\d]+$/.test(name)
-          )
-        ) {
-          accountId = [];
-          return;
-        }
-
         Near.asyncView(ownerId, "get_entity", args, "final").then(
           (existing) => {
             const updatedState = {
@@ -53,6 +100,7 @@ return (
           }
         );
       }}
+      onBlur={() => validate(accountId[0].name)}
       options={allAccountIds}
       placeholder="contribut3.near, social.near..."
       selected={state.accountId}
