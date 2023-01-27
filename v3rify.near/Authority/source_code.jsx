d1335ea6554634d1ff94f25955762972bb8f3b98
v3rify.near <v3rify.near>
2023-01-26 21:18:30 -0500

    Update Authority by v3rify.near at 2023-01-26 21:18:30.511

--- v3rify.near/Authority/source_code.jsx
+++ v3rify.near/Authority/source_code.jsx
@@ -1,78 +1,43 @@
 initState({
-  query: props.query ?? "",
-  result: 0,
-  show: false,
+  authorities: (props.authorities.split(",") ?? []).map((accountId) => ({
+    accountId,
+  })),
 });
-
-if (!props.show) {
-  Near.asyncView(
-    "v3rify.near",
-    "get_verification",
-    { entry_id: state.query },
-    "final"
-  ).then((result) => State.update({ result, show: true }));
-}
-
-const result = state.show ? (
-  <div
-    style={{
-      position: "absolute",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      left: 0,
-      top: 0,
-      width: "100%",
-      height: "100%",
-      opacity: 0.8,
-      backgroundColor: "grey",
-    }}
-  >
-    <div
-      className="card"
-      style={{ opacity: 1 }}
-      onBlur={() => State.update({ show: false })}
-    >
-      <div className="card-header">Verification score for {state.query}</div>
-      <div className="card-body">
-        {state.result === null ? (
-          <>
-            <div>There is no entry for this query</div>
-            <a
-              role="button"
-              className="btn btn-primary"
-              href={`https://near.social/#/v3rify.near/widget/Submit?entryId=${state.query}`}
-            >
-              Submit a verification
-            </a>
-          </>
-        ) : (
-          `The verification score is: ${state.result}`
-        )}
-      </div>
-    </div>
-  </div>
-) : null;
 
 return (
   <div className="card">
     <div className="card-body">
-      <label htmlFor="search">Check:</label>
-      <input
-        type="text"
-        role="search"
-        id="search"
-        placeholder="near.org"
-        value={state.query}
-        onChange={(e) => State.update({ query: e.target.value })}
+      <label htmlFor="search">
+        Authorities to add (a comma separated list of account IDs):
+      </label>
+      <Typeadead
+        id="authorities"
+        labelKey="accountId"
+        placeholder="v3rify.near"
+        selected={state.authorities}
+        onChange={(authorities) => State.update({ authorities })}
+        optionis={[]}
+        multiple
+        allowNew
+        positionFixed
       />
 
       <a
         role="button"
         className="btn btn-primary mt-2"
-        href={`https://near.social/#/v3rify.near/widget/Index?query=${state.query}`}
+        onClick={() =>
+          Near.call(
+            "v3rify.near",
+            "add_authorities",
+            {
+              authorities: state.authorities.map(({ accountId }) => accountId),
+            },
+            `${30 * (10 ^ 12)}`,
+            "1"
+          )
+        }
       >
-        Check
+        Add
       </a>
     </div>
   </div>
