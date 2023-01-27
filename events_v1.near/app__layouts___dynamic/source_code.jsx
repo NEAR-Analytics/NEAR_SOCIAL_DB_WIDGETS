b36014c55c4ea8a5a88702d66df75006a45d8ca1
events_v1.near <events_v1.near>
2023-01-26 19:37:32 -0500

    Update app__layouts___dynamic by events_v1.near at 2023-01-26 19:37:32.732

--- events_v1.near/app__layouts___dynamic/source_code.jsx
+++ events_v1.near/app__layouts___dynamic/source_code.jsx
@@ -3,3 +3,36 @@ props.componentProps.controller = {
     props.componentProps.layout = layout;
   },
 };
+
+// guard to allow 'default' layout exit infinite render loop
+if (
+  layout === 'default' ||
+  layout === null ||
+  layout === '' ||
+  layout === undefined
+) {
+  return (
+    <Widget
+      src={`${appOwner}/widget/${appName}__${slugFromName(name)}`}
+      key={key}
+      props={widgetProps}
+    />
+  );
+}
+
+return (
+  <Widget
+    src={layoutFromName(layout)}
+    key={key}
+    props={{
+      ...componentProps,
+      ...(layoutProps || {}),
+      component: {
+        name: name,
+        props: props,
+        layout: innerLayout,
+        layoutProps: innerLayoutProps,
+      },
+    }}
+  />
+);
