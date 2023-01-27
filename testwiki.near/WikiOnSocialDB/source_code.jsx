38db42433222e5cff37f70440da6aa071867d9a8
testwiki.near <testwiki.near>
2023-01-27 08:20:04 -0500

    Update WikiOnSocialDB by testwiki.near at 2023-01-27 08:20:04.870

--- testwiki.near/WikiOnSocialDB/source_code.jsx
+++ testwiki.near/WikiOnSocialDB/source_code.jsx
@@ -352,6 +352,7 @@ return (
             <Widget
               src="testwiki.near/widget/WikiOnSocialDB_Authors"
               props={{
+                resultArticles,
                 text: "Hello",
               }}
             />
