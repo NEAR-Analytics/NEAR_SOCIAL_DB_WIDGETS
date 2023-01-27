279d06d838cd1219d45efbe5b922dfb59c3006e7
evgenykuzyakov <evgenykuzyakov>
2023-01-27 18:26:08 -0500

    Update Profile.Popover by mob.near at 2023-01-27 18:26:08.015

--- mob.near/Profile.Popover/source_code.jsx
+++ mob.near/Profile.Popover/source_code.jsx
@@ -3,12 +3,18 @@ if (!accountId) {
   return "Requires accountID prop";
 }
 
-State.init({
-  date: new Date().toString(),
-});
-
 return (
-  <div>
-    Hello @{accountId} {state.date}
+  <div className="d-flex flex-column gap-1">
+    <Widget src="mob.near/widget/Profile.InlineBlock" props={{ accountId }} />
+    <div className="d-flex">
+      <div className="me-3">
+        <Widget src="mob.near/widget/FollowStats" props={{ accountId }} />
+      </div>
+      <Widget src="mob.near/widget/FollowsYouBadge" props={{ accountId }} />
+    </div>
+    <div className="d-flex gap-2">
+      <Widget src="mob.near/widget/FollowButton" props={{ accountId }} />
+      <Widget src="mob.near/widget/PokeButton" props={{ accountId }} />
+    </div>
   </div>
 );
