db78c91b89325f41c156dbfdee4ef6b35990262d
testwiki.near <testwiki.near>
2023-01-27 08:23:03 -0500

    Update WikiOnSocialDB_Authors by testwiki.near at 2023-01-27 08:23:03.287

--- testwiki.near/WikiOnSocialDB_Authors/source_code.jsx
+++ testwiki.near/WikiOnSocialDB_Authors/source_code.jsx
@@ -1,12 +1,12 @@
 console.log(props);
-// const authors = Array.from(props.resultArticles, ({ author }) => author);
-// const uniqAuthors = Array.from(new Set(authors));
+const authors = Array.from(props.resultArticles, ({ author }) => author);
+const uniqAuthors = Array.from(new Set(authors));
 return (
   <>
     <h6>Total authors: {uniqAuthors.length}</h6>
     <ul>
       <li>{props.text}</li>
-      {/*
+
       {uniqAuthors.map((author) => (
         <li>
           <a
@@ -16,7 +16,6 @@ return (
           </a>
         </li>
       ))}
-      */}
     </ul>
   </>
 );
