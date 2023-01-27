556005a09fa9fe6ed1915e765de19852e7c71e6d
v3rify.near <v3rify.near>
2023-01-26 21:22:27 -0500

    Update Authority by v3rify.near at 2023-01-26 21:22:27.899

--- v3rify.near/Authority/source_code.jsx
+++ v3rify.near/Authority/source_code.jsx
@@ -34,7 +34,7 @@ return (
             {
               authorities: state.authorities.map(({ accountId }) => accountId),
             },
-            `${30 * (10 ^ 12)}`,
+            `${30 * 10 ** 12}`,
             "1"
           )
         }
