d7b5ce663c1aaba8bf9c8e822cb8256d0afc7388
testwiki.near <testwiki.near>
2023-01-27 13:12:06 -0500

    Update WikiOnSocialDB_AllArticlesList by testwiki.near at 2023-01-27 13:12:06.126

--- testwiki.near/WikiOnSocialDB_AllArticlesList/source_code.jsx
+++ testwiki.near/WikiOnSocialDB_AllArticlesList/source_code.jsx
@@ -1,1 +1,17 @@
-return <div>Hello World</div>;
+const { filteredArticles } = props;
+return (
+  <ol>
+    {filteredArticles &&
+      filteredArticles.map((article, index) => (
+        <li key={article.articleId}>
+          <a href="" onClick={(e) => handleArticle(e, article)}>
+            {article.articleId}{" "}
+            <small>
+              (author: {article.author}
+              {getDateLastEdit(article.timeLastEdit)})
+            </small>
+          </a>
+        </li>
+      ))}
+  </ol>
+);
