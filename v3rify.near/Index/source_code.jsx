70acb427bc4eacca25fb36d889e39b17f72d125c
v3rify.near <v3rify.near>
2023-01-26 19:27:08 -0500

    Update Index by v3rify.near at 2023-01-26 19:27:08.875

--- v3rify.near/Index/source_code.jsx
+++ v3rify.near/Index/source_code.jsx
@@ -27,6 +27,7 @@ const result = state.show ? (
             <div>There is no entry for this query</div>
             <a
               role="button"
+              className="btn btn-primary"
               href={`https://near.social/#/v3rify.near/widget/Submit?${state.query}`}
             >
               Submit a verification
