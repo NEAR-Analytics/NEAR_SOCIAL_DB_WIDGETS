3315fe44d8e5f78eeb2299675899e2e1ba8f81bd
events_v1.near <events_v1.near>
2023-01-28 16:49:01 -0500

    Update events__components__dropdown_item by events_v1.near at 2023-01-28 16:49:01.294

--- events_v1.near/events__components__dropdown_item/source_code.jsx
+++ events_v1.near/events__components__dropdown_item/source_code.jsx
@@ -1,3 +1,86 @@
+console.log('COMPONENT', { props });
+const DropdownButton = styled.button`
+  background-color: transparent;
+  border: none;
+  outline: none;
+  color: black;
+  font-size: 1.5rem;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+  margin-right: 1rem;
+
+  &:hover {
+    color: #007bff;
+  }
+
+  &:focus {
+    color: #007bff;
+
+    &::after {
+      content: '';
+      display: block;
+      width: 100%;
+      height: 2px;
+      background-color: #007bff;
+    }
+  }
+
+  &::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 2px;
+    background-color: transparent;
+  }
+
+  &.active {
+    color: #007bff;
+
+    &::after {
+      content: '';
+      display: block;
+      width: 100%;
+      height: 2px;
+      background-color: #007bff;
+
+      transition: all 0.2s ease-in-out;
+    }
+  }
+
+  &.active:hover {
+    color: #007bff;
+  }
+
+  &.active:focus {
+    color: #007bff;
+  }
+
+  &.active:focus::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 2px;
+    background-color: #007bff;
+  }
+
+  &.active:focus:hover {
+    color: #007bff;
+  }
+
+  &.active:focus:hover::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 2px;
+    background-color: #007bff;
+  }
+
+  &.active:focus:hover:focus {
+    color: #007bff;
+  }
+`;
+
 return (
   <button
     className="nav-link"
