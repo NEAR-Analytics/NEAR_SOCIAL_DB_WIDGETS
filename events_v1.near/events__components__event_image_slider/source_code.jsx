d27935cece8d5f1a2288954e2a23e1fd14b79019
events_v1.near <events_v1.near>
2023-01-28 16:19:21 -0500

    Update events__components__event_image_slider by events_v1.near at 2023-01-28 16:19:21.516

---
+++ events_v1.near/events__components__event_image_slider/source_code.jsx
@@ -0,0 +1,24 @@
+let event = props.event || null;
+
+// return data;
+if (!event) {
+  // TODO: return default image
+  return (<img src="" />);
+}
+
+
+
+return (
+  <div
+        {event.images &&
+          event.images.length > 0 &&
+          event.images.map((image) => {
+            return (
+              <EventImage
+                src={`https://ipfs.near.social/ipfs/${image.url.cid}`}
+                key={image.cid}
+              />
+            );
+          })}
+  </div>
+);
