3e4285cf1a3bfafa6d84d15fb8e283f727715b51
events_v1.near <events_v1.near>
2023-01-28 16:26:56 -0500

    Update app__layouts__container by events_v1.near at 2023-01-28 16:26:56.567

--- events_v1.near/app__layouts__container/source_code.jsx
+++ events_v1.near/app__layouts__container/source_code.jsx
@@ -22,7 +22,7 @@ const dropdownElement =
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           {dropdownItems.map((item, idx) => {
-            <>
+            return (<>
             {
               idx;
             }
@@ -35,7 +35,7 @@ const dropdownElement =
               item.layout,
               item.layoutProps
             );
-              </>
+              </>)
           })}
         </ul>
       </div>
