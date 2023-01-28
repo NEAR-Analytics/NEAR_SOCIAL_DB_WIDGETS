a3c03f2b7086cc8ef40aabab98ddf04398911764
events_v1.near <events_v1.near>
2023-01-28 16:45:51 -0500

    Update app__layouts__dropdown_item by events_v1.near at 2023-01-28 16:45:51.737

--- events_v1.near/app__layouts__dropdown_item/source_code.jsx
+++ events_v1.near/app__layouts__dropdown_item/source_code.jsx
@@ -1,8 +1,12 @@
-{
-  props.__.engine.renderComponent(
-    props.component.name,
-    props.component.props,
-    props.component.layout,
-    props.component.layoutProps
-  );
-}
+const key = props.key || props.component.src;
+
+return (
+  <li className="nav-item" key={key}>
+    {props.__.engine.renderComponent(
+      props.component.name,
+      props.component.props,
+      props.component.layout,
+      props.component.layoutProps
+    )}
+  </li>
+);
