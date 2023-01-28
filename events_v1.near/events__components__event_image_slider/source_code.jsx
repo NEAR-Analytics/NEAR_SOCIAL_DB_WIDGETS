a923c0b8e548fa1e56a0366b9d3d6251bf3f6534
events_v1.near <events_v1.near>
2023-01-28 16:20:39 -0500

    Update events__components__event_image_slider by events_v1.near at 2023-01-28 16:20:39.871

--- events_v1.near/events__components__event_image_slider/source_code.jsx
+++ events_v1.near/events__components__event_image_slider/source_code.jsx
@@ -9,16 +9,15 @@ if (!event) {
 
 
 return (
-  <div
-        {event.images &&
-          event.images.length > 0 &&
-          event.images.map((image) => {
-            return (
-              <EventImage
-                src={`https://ipfs.near.social/ipfs/${image.url.cid}`}
-                key={image.cid}
-              />
-            );
-          })}
-  </div>
+  <>
+    {event.images &&
+      event.images.length > 0 &&
+      event.images.map((image) => {
+        return (
+          <EventImage
+            src={`https://ipfs.near.social/ipfs/${image.url.cid}`}
+            key={image.cid}
+          />
+        );
+  </>
 );
