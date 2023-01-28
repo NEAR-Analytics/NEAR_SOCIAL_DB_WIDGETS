b149031119e53bb2b05c23edbd00a20d017e0737
events_v1.near <events_v1.near>
2023-01-28 16:53:20 -0500

    Update events__show by events_v1.near at 2023-01-28 16:53:20.173

--- events_v1.near/events__show/source_code.jsx
+++ events_v1.near/events__show/source_code.jsx
@@ -12,6 +12,25 @@ if (!event) {
   return 'Loading';
 }
 
+props.__.engine.layoutProps({
+  dropdownItems: [
+    {
+      name: 'components.dropdown_item',
+      props: {
+        label: 'Edit',
+        onClick: () => {
+          props.__.engine.push('edit', { event_id: event.id }, 'container', {
+            title: 'Edit Event',
+            back: true,
+          });
+        },
+      },
+      layout: 'dropdown_item',
+      layoutProps: {},
+    },
+  ],
+});
+
 return (
   <>
     {props.__.engine.renderComponent('components.event_image_slider', {
