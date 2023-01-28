42f173452a86718c12867bfe814885d80ae07e67
events_v1.near <events_v1.near>
2023-01-28 16:57:23 -0500

    Update app__frame by events_v1.near at 2023-01-28 16:57:23.148

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -307,8 +307,8 @@ function renderComponent(name, props, layout, layoutProps) {
         rerender,
         storageGet,
         storageSet,
-        registerLayout(la, lap) {
-          // registerLayout(componentProps.__.ref, la, lap);
+        registerLayout: function (la, lap) {
+          registerLayout(componentProps.__.ref, la, lap);
         },
       },
       Components: {
