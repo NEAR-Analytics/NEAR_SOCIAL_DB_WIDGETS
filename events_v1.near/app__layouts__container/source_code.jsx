dbcdc55efef0ff1b0e1ca6c362bebdb6308e338d
events_v1.near <events_v1.near>
2023-01-29 14:28:21 -0500

    Update app__layouts__container by events_v1.near at 2023-01-29 14:28:21.958

--- events_v1.near/app__layouts__container/source_code.jsx
+++ events_v1.near/app__layouts__container/source_code.jsx
@@ -94,9 +94,7 @@ return (
         <div className="col-12">
           {props.__engine.renderComponent(
             props.component.name,
-            props.component.props,
-            props.component.layout,
-            props.component.layoutProps
+            props.component.props
           )}
         </div>
       </div>
