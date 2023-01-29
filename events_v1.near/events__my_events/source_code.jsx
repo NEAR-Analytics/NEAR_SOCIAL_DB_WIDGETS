e9f46d68b55304fc4a94f56da7ea80630fec89ce
events_v1.near <events_v1.near>
2023-01-29 14:44:39 -0500

    Update events__my_events by events_v1.near at 2023-01-29 14:44:39.804

--- events_v1.near/events__my_events/source_code.jsx
+++ events_v1.near/events__my_events/source_code.jsx
@@ -2,4 +2,6 @@ props.__layout.change('container', {
   title: 'My events',
 });
 
-return props.__engine.renderComponent('index.list_container', {});
+return props.__engine.renderComponent('index.list_container', {
+  forAccountId: props.__engine.accountId,
+});
