c93783284c437e3f4799a8fdd14f6b9e25d38b10
events_v1.near <events_v1.near>
2023-01-29 18:59:57 -0500

    Update events__components__event_image_slider by events_v1.near at 2023-01-29 18:59:57.510

--- events_v1.near/events__components__event_image_slider/source_code.jsx
+++ events_v1.near/events__components__event_image_slider/source_code.jsx
@@ -48,7 +48,7 @@ return (
             height: '100%',
             maxWidth: '100%',
             boxSizing: 'border-box',
-            objectFit: 'contain',
+            objectFit: 'cover',
           }}
         />
       );
