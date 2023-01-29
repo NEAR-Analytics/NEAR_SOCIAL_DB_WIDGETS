d9fbe9633f6ebd22bda2a3c27233293759f1bdaa
events_v1.near <events_v1.near>
2023-01-29 18:01:04 -0500

    Update events__show by events_v1.near at 2023-01-29 18:01:04.612

--- events_v1.near/events__show/source_code.jsx
+++ events_v1.near/events__show/source_code.jsx
@@ -1,3 +1,10 @@
+const EVENTS_CONTRACT = 'events_v1.near';
+
+const eventId = props.event_id;
+if (!eventId) {
+  return 'props.eventId is required';
+}
+
 function onClick(e) {
   console.log('Edit Event1 ', e);
   props.__engine.push('edit', { event_id: props.event_id });
@@ -14,13 +21,6 @@ props.controller.setLayout('container', {
     },
   ],
 });
-
-const EVENTS_CONTRACT = 'events_v1.near';
-
-const eventId = props.event_id;
-if (!eventId) {
-  return 'props.eventId is required';
-}
 
 const event = Near.view(EVENTS_CONTRACT, 'get_event', {
   event_id: props.event_id,
