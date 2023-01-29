8724aba76d4b594bc58f80dfafb0dd8a0eb6d11a
events_v1.near <events_v1.near>
2023-01-29 18:54:13 -0500

    Update events__layouts__modal by events_v1.near at 2023-01-29 18:54:13.425

--- events_v1.near/events__layouts__modal/source_code.jsx
+++ events_v1.near/events__layouts__modal/source_code.jsx
@@ -106,6 +106,7 @@ return (
               maxWidth: '100%',
               overflow: 'auto',
               position: 'absolute',
+              // offset by border-radius, so that scrollbars don't show up
               top: BORDER_RADIUS,
               left: 0,
               right: 0,
