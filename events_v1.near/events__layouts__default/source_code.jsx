2abb1bead00cbcfdd8a677be40b4046a06724384
events_v1.near <events_v1.near>
2023-01-29 16:50:13 -0500

    Update events__layouts__default by events_v1.near at 2023-01-29 16:50:13.958

--- events_v1.near/events__layouts__default/source_code.jsx
+++ events_v1.near/events__layouts__default/source_code.jsx
@@ -1,6 +1,1 @@
-// return props.__engine.renderComponent(
-// props.__component.props
-//   props.__component.name,
-// );
-
 return props.__engine.renderRaw(props.component.name, props.component.props);
