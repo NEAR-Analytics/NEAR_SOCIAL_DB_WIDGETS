9dc526d64b6f2f049103be77a53061e7d8fdd69f
events_v1.near <events_v1.near>
2023-01-29 18:55:36 -0500

    Update events___form__image_component by events_v1.near at 2023-01-29 18:55:36.550

--- events_v1.near/events___form__image_component/source_code.jsx
+++ events_v1.near/events___form__image_component/source_code.jsx
@@ -28,8 +28,6 @@ if (!state) {
   return 'Loading...';
 }
 
-console.log('ImageComponent', state.localImg, image.url.cid);
-
 const ImageTypes = [
   { value: 'tile', label: 'Tile' },
   { value: 'banner', label: 'Banner' },
