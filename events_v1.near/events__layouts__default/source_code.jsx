c5f2d7c3d74575c396ddcf450412185c1855b586
events_v1.near <events_v1.near>
2023-01-29 17:06:36 -0500

    Update events__layouts__default by events_v1.near at 2023-01-29 17:06:36.987

--- events_v1.near/events__layouts__default/source_code.jsx
+++ events_v1.near/events__layouts__default/source_code.jsx
@@ -3,7 +3,9 @@
 //   props.component.props
 // );
 
-return <Widget src={props.__engine.widgetPathFromName(props.component.name)} props={
-  props.component.props,
-
-} />;
+return (
+  <Widget
+    src={props.__engine.widgetPathFromName(props.component.name)}
+    props={props.component.props}
+  />
+);
