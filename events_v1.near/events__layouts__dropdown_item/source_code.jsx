b91e43bbe3dfd6bc5a96ca214caf8d7391047360
events_v1.near <events_v1.near>
2023-01-29 16:42:34 -0500

    Update events__layouts__dropdown_item by events_v1.near at 2023-01-29 16:42:34.610

--- events_v1.near/events__layouts__dropdown_item/source_code.jsx
+++ events_v1.near/events__layouts__dropdown_item/source_code.jsx
@@ -3,8 +3,8 @@ const key = props.key || props.component.src;
 return (
   <li className="nav-item" key={key}>
     {props.__engine.renderComponent(
-      props.__component.name,
-      props.__component.props
+      props.component.name,
+      props.component.props
     )}
   </li>
 );
