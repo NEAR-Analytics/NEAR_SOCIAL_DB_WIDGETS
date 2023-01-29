46191acc692569ab25a26b14cf752a5ce4deceb3
events_v1.near <events_v1.near>
2023-01-29 16:12:30 -0500

    Update app__frame by events_v1.near at 2023-01-29 16:12:30.683

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -393,7 +393,7 @@ return (
             backgroundColor: 'transparent',
             zIndex: index,
             position: 'fixed',
-            top: 72,
+            top: 0,
             left: 0,
             right: 0,
             bottom: 0,
