cb476c29023225de40fedab8c36b2090b8e7b694
events_v1.near <events_v1.near>
2023-01-29 15:39:21 -0500

    Update app__frame by events_v1.near at 2023-01-29 15:39:21.786

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -296,15 +296,15 @@ function pop() {
   rerender();
 }
 
-let counter = 0;
-function _renderComponent(owner, name, props, controller) {
-  console.log('renderComponent', owner, name, props);
-  counter = counter + 1;
-  // need another const ref to prevent vm to re-render
-  const ref = counter + 1;
+// let counter = 0;
+function _renderComponent(owner, name, props) {
+  // console.log('renderComponent', owner, name, props);
+  // counter = counter + 1;
+  // // need another const ref to prevent vm to re-render
+  // const ref = counter + 1;
 
-  function renderComponent(_name, _props, _controller) {
-    return _renderComponent(ref, _name, _props, _controller);
+  function renderComponent(_name, _props) {
+    return _renderComponent(ref, _name, _props);
   }
 
   const engine = {
@@ -334,21 +334,6 @@ function _renderComponent(owner, name, props, controller) {
       propIsRequiredMessage,
     },
   };
-
-  // if controlling component is provided, render component raw with controller provided
-  if (controller) {
-    return (
-      <Widget
-        src={widgetPathFromName(name)}
-        key={props && props.key ? props.key : name}
-        props={{
-          __engine: engine,
-          ...controller,
-          ...props,
-        }}
-      />
-    );
-  }
 
   return (
     <Widget
