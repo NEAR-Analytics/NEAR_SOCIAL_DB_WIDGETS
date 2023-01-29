5bfbd55af963aae2665acf8b7923bf7c770a7d19
events_v1.near <events_v1.near>
2023-01-29 16:14:01 -0500

    Update events__my_events by events_v1.near at 2023-01-29 16:14:01.000

--- events_v1.near/events__my_events/source_code.jsx
+++ events_v1.near/events__my_events/source_code.jsx
@@ -1,7 +1,7 @@
-if (!props.__layout) {
-  console.log('no layout');
-  return '';
-}
+// if (!props.__layout) {
+//   console.log('no layout');
+//   return '';
+// }
 props.__layout.setLayout('container', {
   title: 'My events',
 });
