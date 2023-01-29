c6550da4bda87f7570688b223c9752e44c0d052f
events_v1.near <events_v1.near>
2023-01-29 13:34:43 -0500

    Update app__layout_controller by events_v1.near at 2023-01-29 13:34:43.379

--- events_v1.near/app__layout_controller/source_code.jsx
+++ events_v1.near/app__layout_controller/source_code.jsx
@@ -15,35 +15,31 @@ if (!state) {
 const layout = state.layout;
 const layoutProps = state.layoutProps;
 
-function renderControl() {
-  // guard to allow 'default' layout exit infinite render loop
-  if (
-    layout === 'default' ||
-    layout === null ||
-    layout === '' ||
-    layout === undefined
-  ) {
-    return (
-      <Widget src={props.__engine.widgetFromName(name)} props={widgetProps} />
-    );
-  }
-
+// guard to allow 'default' layout exit infinite render loop
+if (
+  layout === 'default' ||
+  layout === null ||
+  layout === '' ||
+  layout === undefined
+) {
   return (
-    <Widget
-      src={layoutFromName(layout)}
-      key={key}
-      props={{
-        ...componentProps,
-        ...(layoutProps || {}),
-        component: {
-          name: name,
-          props: props,
-          layout: innerLayout,
-          layoutProps: innerLayoutProps,
-        },
-      }}
-    />
+    <Widget src={props.__engine.widgetFromName(name)} props={widgetProps} />
   );
 }
 
-return renderControl();
+return (
+  <Widget
+    src={layoutFromName(layout)}
+    key={key}
+    props={{
+      ...componentProps,
+      ...(layoutProps || {}),
+      component: {
+        name: name,
+        props: props,
+        layout: innerLayout,
+        layoutProps: innerLayoutProps,
+      },
+    }}
+  />
+);
