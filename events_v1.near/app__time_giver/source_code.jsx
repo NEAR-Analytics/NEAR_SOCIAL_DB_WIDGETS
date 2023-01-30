7a4eded161366b4ee6d3d112886058353c6b22f7
events_v1.near <events_v1.near>
2023-01-30 09:29:52 -0500

    Update app__time_giver by events_v1.near at 2023-01-30 09:29:52.417

--- events_v1.near/app__time_giver/source_code.jsx
+++ events_v1.near/app__time_giver/source_code.jsx
@@ -1,6 +1,6 @@
 const res = fetch('https://rpc.mainnet.near.org/status', { subscribe: true });
-console
-  .log(res)(props.callbacks || [])
-  .map((callback) => {
-    callback();
-  });
+console.log(res);
+
+(props.callbacks || []).forEach((callback) => {
+  callback();
+});
