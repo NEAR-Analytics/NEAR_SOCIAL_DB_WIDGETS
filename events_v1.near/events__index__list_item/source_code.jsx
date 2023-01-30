d9ffa4c443af9b9bb271e32c2bfbff964a03dd14
events_v1.near <events_v1.near>
2023-01-30 17:42:21 -0500

    Update events__index__list_item by events_v1.near at 2023-01-30 17:42:21.473

--- events_v1.near/events__index__list_item/source_code.jsx
+++ events_v1.near/events__index__list_item/source_code.jsx
@@ -39,14 +39,12 @@ const EventHeaderImage = styled.div`
 `;
 
 const EventTitle = styled.h1`
-  font-size: 1.25vw;
+  font-size: calc(max(1.25rem, 1.25vw));
   font-weight: 500;
   margin: 0;
   padding: 1vw calc(max(0.5rem, 0.5vw));
   width: 100%;
   border-bottom: 0.1vw solid #cccccc;
-  flex-shrink: 0;
-  flex-grow: 1;
 `;
 
 const EventDate = styled.div`
