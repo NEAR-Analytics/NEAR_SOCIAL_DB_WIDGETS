204f951ab1b65c34c76daab95f357fe966cc2982
events_v1.near <events_v1.near>
2023-01-30 15:03:04 -0500

    Update events__index__list_item by events_v1.near at 2023-01-30 15:03:04.982

--- events_v1.near/events__index__list_item/source_code.jsx
+++ events_v1.near/events__index__list_item/source_code.jsx
@@ -22,7 +22,7 @@ const EventCard = styled.div`
   transition: all 0.5s cubic-bezier(0.25, 0.8, 0.25, 1);
 
   &:hover {
-    box-shadow: 5px 0 20px 2px rgba(0, 0, 0, 0.2);
+    box-shadow: 5px 0 15px -2px rgba(0, 0, 0, 0.2);
   }
 `;
 
