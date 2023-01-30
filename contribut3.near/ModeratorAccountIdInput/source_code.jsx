e58ece3af8b3250a3a2785c3cfe04f2caf1c2398
contribut3.near <contribut3.near>
2023-01-30 12:05:01 -0500

    Update ModeratorAccountIdInput by contribut3.near at 2023-01-30 12:05:01.204

--- contribut3.near/ModeratorAccountIdInput/source_code.jsx
+++ contribut3.near/ModeratorAccountIdInput/source_code.jsx
@@ -63,9 +63,11 @@ const validate = (accountId) => {
 
 const formatDate = (timestampString) => {
   const date = new Date(Number(timestampString));
-  return `${date.getFullYear()}-${(date.getMonth() + 1)
-    .toString()
-    .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
 };
 
 return (
