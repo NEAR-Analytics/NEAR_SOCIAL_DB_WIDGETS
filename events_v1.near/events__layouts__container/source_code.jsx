55216f4d3f932d40abcf7ca117c24435d0b4ad2e
events_v1.near <events_v1.near>
2023-01-30 13:30:55 -0500

    Update events__layouts__container by events_v1.near at 2023-01-30 13:30:55.768

--- events_v1.near/events__layouts__container/source_code.jsx
+++ events_v1.near/events__layouts__container/source_code.jsx
@@ -90,11 +90,6 @@ const navbar = (
   </div>
 );
 
-const blurs = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-];
-
 return (
   <>
     <div
@@ -107,27 +102,6 @@ return (
     >
       {navbar}
 
-      {blurs.map((i) => {
-        const elHeight = 8;
-        const minBlur = 30;
-        const maxBlur = 80;
-        const blurDelta = (maxBlur - minBlur) / blurs.length;
-
-        return (
-          <div
-            key={i}
-            style={{
-              position: 'fixed',
-              top: NAVBAR_OFFSET_TOP + NAVBAR_HEIGHT + i * elHeight,
-              height: elHeight,
-              backgroundColor: 'rgba(255,255,255, 0.5)',
-              backdropFilter: `blur(${maxBlur - i * blurDelta}px)`,
-              width: '100%',
-              zIndex: 99999999,
-            }}
-          />
-        );
-      })}
       <div
         className="row"
         style={{
