d570a6bf36aecc2c84c2ab7bc99324801fc2601f
events_v1.near <events_v1.near>
2023-01-30 12:26:56 -0500

    Update events__components__event_image_slider by events_v1.near at 2023-01-30 12:26:56.192

--- events_v1.near/events__components__event_image_slider/source_code.jsx
+++ events_v1.near/events__components__event_image_slider/source_code.jsx
@@ -40,12 +40,11 @@ fetch('https://api.coingecko.com/api/v3/coins/near', {
 });
 
 const index = Storage.get('index') || 0;
-if(Storage.get('index') < 2){
+if(Storage.get('index') < 3){
   console.log("index", index)
   Storage.set('index', Storage.get('index') + 1);
   return
 }
-
 
 props.onUpdate()
 Storage.set('index', 0)
