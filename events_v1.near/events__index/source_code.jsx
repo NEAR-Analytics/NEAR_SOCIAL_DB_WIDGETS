c75f8f8dde71ffba1de03a2f62d482aa73d4287d
events_v1.near <events_v1.near>
2023-01-30 16:32:26 -0500

    Update events__index by events_v1.near at 2023-01-30 16:32:26.228

--- events_v1.near/events__index/source_code.jsx
+++ events_v1.near/events__index/source_code.jsx
@@ -4,7 +4,7 @@ props.controller.setLayout('container', {
 
   primaryAction: {
     label: 'Create new Event',
-    onClick: "props.__engine.push('new', {});",
+    onClick: 'props.__engine.push("new", {});',
   },
 });
 
