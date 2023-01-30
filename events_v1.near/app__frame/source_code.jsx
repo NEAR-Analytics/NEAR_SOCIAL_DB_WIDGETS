eed0c389f5b0ac001c70edde1c024c87c506be54
events_v1.near <events_v1.near>
2023-01-30 13:12:10 -0500

    Update app__frame by events_v1.near at 2023-01-30 13:12:10.941

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -64,7 +64,7 @@ const VERSION = '0.2.0';
  *  @param {String} name - the name of the widget to render
  *  @param {Object} props - the props to pass to the widget
  *  available in: props.engine
- *  renderComponent(name, props, layout, layoutProps)
+ *  renderComponent(name, props)
  *    renders a widget with the given name and props within the given layout,
  *    use this instead of <Widget src="" />
  *
@@ -72,7 +72,7 @@ const VERSION = '0.2.0';
  *  @param {String} name - the name of the widget to render
  *  @param {Object} props - the props to pass to the widget
  *  available in: props.routing
- *  push(name, props, layout, layoutProps)
+ *  push(name, props)
  *    pushes a new layer onto the stack of layers to render
  *    this will cause the app to render a new layer on top of the current layer
  *
