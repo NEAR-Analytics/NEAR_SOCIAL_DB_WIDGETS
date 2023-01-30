9c78f127d5108a64e4ab9fd6bd36fdcaa3ea0a98
events_v1.near <events_v1.near>
2023-01-30 17:50:48 -0500

    Update events__index by events_v1.near at 2023-01-30 17:50:48.768

--- events_v1.near/events__index/source_code.jsx
+++ events_v1.near/events__index/source_code.jsx
@@ -15,14 +15,6 @@ return (
   <Container>
     <Button
       onClick={() => {
-        props.__engine.push('new', {});
-      }}
-    >
-      Create new Event
-    </Button>
-
-    <Button
-      onClick={() => {
         return props.__engine.push('my_events', {});
       }}
     >
