49ce8b87babdc11b63a9eff122e558542e8bffe8
microchipgnu <microchipgnu>
2023-01-29 23:24:46 -0500

    Update camera by microchipgnu.near at 2023-01-29 23:24:46.603

--- microchipgnu.near/camera/source_code.jsx
+++ microchipgnu.near/camera/source_code.jsx
@@ -1,1 +1,24 @@
-return <input type="file" accept="image/*;capture=camera" />;
+//return <input type="file" accept="image/*;capture=camera" />;
+
+const posts = Array.from(Array(100).keys());
+
+return (
+  <div class="bg-black text-white p-2 rounded container-fluid">
+    <div class="w-full d-flex justify-content-center py-4">
+      <div class="text-center">
+        <p>Minsta</p>
+        <input type="file" accept="image/*;capture=camera" class="" />
+      </div>
+    </div>
+    <div class="container-fluid d-flex flex-wrap justify-content-center gap-2">
+      {posts.map((post) => {
+        return (
+          <div
+            class="col-md-4 col-sm-6 px-1 bg-danger"
+            style={{ width: "150px", height: "150px" }}
+          ></div>
+        );
+      })}
+    </div>
+  </div>
+);
