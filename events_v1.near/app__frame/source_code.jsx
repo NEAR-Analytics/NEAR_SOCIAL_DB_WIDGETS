036bcd6c68c4268c78305937ac0043db877751c5
events_v1.near <events_v1.near>
2023-01-30 17:10:38 -0500

    Update app__frame by events_v1.near at 2023-01-30 17:10:38.952

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -348,8 +348,11 @@ function pop() {
 }
 
 function dirtyEval(args) {
+  const method = args[0];
   const key = args[1];
   const mArgs = args.slice(2);
+
+  console.log('dirtyEval', method, key, mArgs);
 
   switch (method) {
     case 'push':
