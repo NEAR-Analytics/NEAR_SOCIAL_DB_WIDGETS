e3cb1f98fc7e5041c98cd3853cc6787007eb3bf6
events_v1.near <events_v1.near>
2023-01-30 12:41:24 -0500

    Update app__frame by events_v1.near at 2023-01-30 12:41:24.946

--- events_v1.near/app__frame/source_code.jsx
+++ events_v1.near/app__frame/source_code.jsx
@@ -420,7 +420,7 @@ return (
       props={{
         onUpdate: () => {
           const v = appStateGet('renderCycles') || 0;
-          appStateSet('renderCycles', (v + 1) % 100);
+          appStateSet('renderCycles', (v + 1) % 10);
 
           const tickCallbacks = appStateGet('tickCallbacks', []);
           tickCallbacks.forEach((tickCallback) => {
