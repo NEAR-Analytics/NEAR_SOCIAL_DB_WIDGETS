b1c8610a438efa4adcb0939d7133c947feef828b
eugenewolf507.near <eugenewolf507.near>
2023-01-31 12:44:35 -0500

    Update Draft_SaveArticlesPerUser by eugenewolf507.near at 2023-01-31 12:44:35.991

--- eugenewolf507.near/Draft_SaveArticlesPerUser/source_code.jsx
+++ eugenewolf507.near/Draft_SaveArticlesPerUser/source_code.jsx
@@ -3,45 +3,37 @@ const accountId = props.accountId ?? context.accountId;
 
 // ========= TRY OBJECT STRUCTURE =========
 const dataStructure = {
-  articlG: {
+  article3G: {
     author: "EV",
-    body: "body for article G",
+    body: "body for article 3G",
   },
-  articleH: {
-    author: "EV",
-    body: "body for article H",
-  },
-  article7: {
-    author: "EV",
-    body: "body for article 7",
-  },
-  article8: {
-    author: "EV",
-    body: "body for article 8",
-  },
+  // article3H: {
+  //   author: "EV",
+  //   body: "body for article 3H",
+  // },
+  // article17: {
+  //   author: "EV",
+  //   body: "body for article 17",
+  // },
+  // article18: {
+  //   author: "EV",
+  //   body: "body for article 18",
+  // },
 };
 
 // ========= HANDLER SET =========
-const clickHandlerSetStructure = () => {
-  // Social.set(JSON.stringify(dataStructure));
-  Social.set({ articles: JSON.stringify(dataStructure) });
+const getPublicationParamsAllArticles = () => {
+  return { articlesPersonal2: dataStructure };
 };
 
 // ========= HANDLER GET =========
 const clickHandlerGetStructure = () => {
   // === ALL ARTICLES
-  // path that works
-  // const path = "/*";
-  // const path = "/widget/*";
-  // const path = "/widget/Draft_Eugene2";
   const path = "/articlesPersonal2/**";
   const data1 = Social.get(`${accountId}${path}`);
   console.log(data1);
 
   // === ONE ARTICLE
-  // const data2 = Social.keys("*/widget/*", "final");
-  // const data2 = Social.keys("*/articles/*", "final");
-  // const data2 = Social.getr("bearner.near/nametag/bearner.near/tags", "final");
   const data2 = Social.get(
     `${accountId}/articlesPersonal2/articleF/**`,
     "final"
@@ -57,28 +49,9 @@ const clickHandlerGetStructure = () => {
 return (
   <div>
     <p>Press button and check log in browser</p>
-    {/* <button onClick={clickHandlerSetStructure}>Set Structure</button> */}
-    <CommitButton
-      data={{
-        articlesPersonal2: dataStructure,
-      }}
-      // option how to write to anather account and syntax why we use {{}}
-      // but I don't check it
-      // data={{
-      //   articles: { "eugenewolf507.near": { homepage: state.homepage } },
-      // }}
-    >
-      SET All with CommitButton
+    <CommitButton data={getPublicationParamsAllArticles()}>
+      SET Data
     </CommitButton>
-    <CommitButton
-      data={{
-        articlesPersonal2: {
-          articleF: { body: "body 4 article F: FFFFFFF" },
-        },
-      }}
-    >
-      CHANGE One with CommitButton
-    </CommitButton>
-    <button onClick={clickHandlerGetStructure}>Get Structure</button>
+    <button onClick={clickHandlerGetStructure}>GET Data</button>
   </div>
 );
