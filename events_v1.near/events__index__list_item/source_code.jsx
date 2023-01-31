43b335d365cd3e0f4110b331024a140920d9810c
events_v1.near <events_v1.near>
2023-01-31 15:20:37 -0500

    Update events__index__list_item by events_v1.near at 2023-01-31 15:20:37.246

--- events_v1.near/events__index__list_item/source_code.jsx
+++ events_v1.near/events__index__list_item/source_code.jsx
@@ -72,6 +72,8 @@ function showEvent() {
 const startDate = new Date(event.start_date);
 const endDate = new Date(event.end_date);
 const datesAreEqual = startDate.toDateString() === endDate.toDateString();
+const endDateIsNull =
+  endDate === null || endDate.toDateString() === new Date(0).toDateString();
 
 return (
   <EventCard
