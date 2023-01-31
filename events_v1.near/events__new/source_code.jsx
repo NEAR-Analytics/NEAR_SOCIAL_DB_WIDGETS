3ecf3ccf7f21375cb2030c6daabe5438c1ed1a4f
events_v1.near <events_v1.near>
2023-01-31 15:10:15 -0500

    Update events__new by events_v1.near at 2023-01-31 15:10:15.808

--- events_v1.near/events__new/source_code.jsx
+++ events_v1.near/events__new/source_code.jsx
@@ -7,6 +7,23 @@ const EVENTS_CONTRACT = 'events_v1.near';
 const APP_OWNER = 'events_v1.near';
 const APP_NAME = 'events';
 
+const latestEvent = Near.view(EVENTS_CONTRACT, 'get_latest_event', {
+  account_id: props.__engine.accountId,
+});
+if (!latestEvent) {
+  return 'Loading';
+}
+
+const SECONDS_10 = 10000;
+// if event was just created, pop the stack and return
+if (
+  latestEvent &&
+  new Date().getTime() - new Date(latestEvent.created_at).getTime() < SECONDS_10
+) {
+  props.__engine.pop();
+  return 'Event updated';
+}
+
 function createEvent(data) {
   const {
     name,
