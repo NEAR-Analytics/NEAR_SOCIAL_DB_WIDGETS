f066e84db6fbc5302c1d285eeb7a7b5f0aa87d25
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-01-31 15:15:37 -0500

    Update DevSupport.Feed by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-01-31 15:15:37.302

---
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Feed/source_code.jsx
@@ -0,0 +1,25 @@
+const index = {
+  action: "question",
+  key: "main",
+  options: {
+    limit: 10,
+    order: "desc",
+    accountId: props.accounts,
+  },
+};
+
+const renderItem = (a) =>
+  a.value.type === "md" && (
+    <div key={JSON.stringify(a)} className="mb-3">
+      <Widget
+        src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Preview"
+        props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
+      />
+    </div>
+  );
+
+return (
+  <div>
+    <Widget src="mob.near/widget/IndexFeed" props={{ index, renderItem }} />
+  </div>
+);
