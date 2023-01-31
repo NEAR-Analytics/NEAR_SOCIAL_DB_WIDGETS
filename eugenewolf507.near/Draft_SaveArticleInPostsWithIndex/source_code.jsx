6fe3e3757283997fdb5db4e69ca5dd0b0cf6a369
eugenewolf507.near <eugenewolf507.near>
2023-01-31 14:12:25 -0500

    Update Draft_SaveArticleInPostsWithIndex by eugenewolf507.near at 2023-01-31 14:12:25.336

--- eugenewolf507.near/Draft_SaveArticleInPostsWithIndex/source_code.jsx
+++ eugenewolf507.near/Draft_SaveArticleInPostsWithIndex/source_code.jsx
@@ -1,26 +1,32 @@
 const accountId = props.accountId ?? context.accountId;
-const clickHandlerGetStructureThis = () => {
-  console.log("fgdsfsd");
+// ========= TRY OBJECT STRUCTURE =========
+const dataStructure = {
+  article3G: {
+    author: "EV",
+    body: "body for article 3G",
+  },
+};
+
+// ========= HANDLER SET =========
+const getPublicationParamsAllArticles = () => {
+  return { articlesPersonal2: dataStructure };
+};
+
+// ========= HANDLER GET =========
+const clickHandlerGetStructure = () => {
+  // === ALL ARTICLES
   const path = "/articlesPersonal2/**";
   const data1 = Social.get(`${accountId}${path}`);
   console.log(data1);
-  const data2 = Social.get(
-    `${accountId}/articlesPersonal2/articleF/**`,
-    "final"
-  );
-  console.log(data2);
-  const data3 = Social.keys(`${accountId}/articlesPersonal2/*`, "final");
-  console.log(Object.keys(data3[accountId].articlesPersonal2));
 };
+
+// ========= RETURN =========
 return (
   <div>
-    <div>
-      <h2>draft 2 widget</h2>
-      <Widget src="eugenewolf507.near/widget/Draft_Eugene2" />
-    </div>
-    <div>
-      <h2>this widget</h2>
-      <button onClick={clickHandlerGetStructureThis}>Get Structure</button>
-    </div>
+    <p>Press button and check log in browser</p>
+    <CommitButton data={getPublicationParamsAllArticles()}>
+      SET Data
+    </CommitButton>
+    <button onClick={clickHandlerGetStructure}>GET Data</button>
   </div>
 );
