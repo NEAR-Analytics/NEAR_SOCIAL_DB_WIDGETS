1db699045d9b0d805e14dc1673b292b13191da23
vow_owner_123.near <vow_owner_123.near>
2023-01-31 10:54:50 -0500

    Update Instance_time_setting by vow_owner_123.near at 2023-01-31 10:54:50.035

--- vow_owner_123.near/Instance_time_setting/source_code.jsx
+++ vow_owner_123.near/Instance_time_setting/source_code.jsx
@@ -170,7 +170,7 @@ const getData = () => {
           value: {
             _data: final,
             _time_zone: time_zone,
-            is_on: props.data.is_on,
+            _is_on: props.data.is_on,
           },
         },
         undefined,
