1638b40cfd623f6cf4eace36bd3f34ea4c6c98d9
vow_owner_123.near <vow_owner_123.near>
2023-01-31 13:17:34 -0500

    Update Instance_Time_share by vow_owner_123.near at 2023-01-31 13:17:34.059

--- vow_owner_123.near/Instance_Time_share/source_code.jsx
+++ vow_owner_123.near/Instance_Time_share/source_code.jsx
@@ -1,11 +1,11 @@
-let accountId = props.accountId;
+let blockHeightToShare = props.blockHeight;
 
 State.init({
   showShareOptions: false,
   linkCopied: false,
 });
 
-let widgetOwner = "vow_owner_123.near";
+let widgetOwner = "easypoll.near";
 
 return (
   <>
@@ -47,7 +47,7 @@ return (
             letterSpacing: "-0.01em",
           }}
         >
-          Use this link to share the instance time with your participants
+          Use this link to share the poll with your participants
         </p>
         <div
           className="d-flex justify-content-between align-items-center"
@@ -59,7 +59,7 @@ return (
         >
           <span
             style={{ color: "#0065FF", wordWrap: "anywhere" }}
-          >{`https://near.social/#/${widgetOwner}/widget/Instance_time_card?accountId=${accountId}`}</span>
+          >{`https://near.social/#/${widgetOwner}/widget/EasyPoll?sharedBlockHeight=${blockHeightToShare}`}</span>
           <div
             style={{
               display: "flex",
@@ -86,7 +86,7 @@ return (
               }
               onClick={() => {
                 clipboard.writeText(
-                  `https://near.social/#/${widgetOwner}/widget/Instance_time_card?accountId=${accountId}`
+                  `https://near.social/#/${widgetOwner}/widget/EasyPoll?sharedBlockHeight=${blockHeightToShare}`
                 );
                 State.update({ linkCopied: true });
               }}
