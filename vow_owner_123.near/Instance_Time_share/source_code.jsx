c5d0890833b71cd5490c7edbb308134cc54eb5dd
vow_owner_123.near <vow_owner_123.near>
2023-01-31 14:30:03 -0500

    Update Instance_time_share by vow_owner_123.near at 2023-01-31 14:30:03.173

--- vow_owner_123.near/Instance_Time_share/source_code.jsx
+++ vow_owner_123.near/Instance_Time_share/source_code.jsx
@@ -86,7 +86,7 @@ return (
               }
               onClick={() => {
                 clipboard.writeText(
-                  `https://near.social/#/${widgetOwner}/widget/EasyPoll?sharedBlockHeight=${blockHeightToShare}`
+                  `https://near.social/#/${widgetOwner}/widget/Instance_time_card?accountId=${accountId}`
                 );
                 State.update({ linkCopied: true });
               }}
