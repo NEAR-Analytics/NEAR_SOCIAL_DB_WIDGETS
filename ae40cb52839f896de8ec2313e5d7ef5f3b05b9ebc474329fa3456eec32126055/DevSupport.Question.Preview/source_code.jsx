ba0397ce38c0c74bb605f571f732e64589e69252
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-01-31 14:42:38 -0500

    Update DevSupport.Question.Preview by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-01-31 14:42:38.308

---
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Question.Preview/source_code.jsx
@@ -0,0 +1,82 @@
+const accountId = props.accountId;
+const blockHeight =
+  props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
+
+const question = JSON.parse(
+  Social.get(`${accountId}/question/main`, blockHeight) ?? "null"
+);
+
+console.log("A", question);
+
+const notifyAccountId = accountId;
+const item = {
+  type: "social",
+  path: `${accountId}/question/main`,
+  blockHeight,
+};
+
+const link = `#/mob.near/widget/MainPage.Post.Page?accountId=${accountId}&blockHeight=${blockHeight}`;
+
+function readableDate(timestamp) {
+  var a = new Date(timestamp);
+  return a.toDateString() + " " + a.toLocaleTimeString();
+}
+
+const timestamp = readableDate(
+  snapshot.timestamp ? snapshot.timestamp / 1000000 : Date.now()
+);
+
+const postLables = question.labels ? (
+  <div class="card-title">
+    {question.labels.map((label) => {
+      return (
+        <a
+          href={`https://near.social/#/devgovgigs.near/widget/Ideas?label=${label}`}
+        >
+          <span class="badge p-0 pe-2 text-primary">{label}</span>
+        </a>
+      );
+    })}
+  </div>
+) : null;
+
+const likeBtnClass = "bi-arrow-up-square";
+
+const footer = (
+  <div className="card-header">
+    <small class="text-muted">
+      <div class="row justify-content-between">
+        <div class="col-6">
+          <a href="#" onClick={onLike}>
+            <i class={`bi ${likeBtnClass} me-1`}> </i>
+            Upvote ({post.likes.length ?? 0})
+          </a>
+        </div>
+
+        <div class="col-6">
+          <div class="d-flex justify-content-end">
+            {timestamp}
+            <i class="bi bi-clock-history px-2"></i>
+            {shareButton}
+          </div>
+        </div>
+      </div>
+    </small>
+  </div>
+);
+
+return (
+  <div className="border p-3">
+    <h1>{question.title}</h1>
+    {postLables}
+
+    <div className="pt-2 text-break">
+      <Widget
+        src="mob.near/widget/MainPage.Post.Content"
+        props={{ content: { text: question.content.text } }}
+      />
+    </div>
+
+    {footer}
+  </div>
+);
