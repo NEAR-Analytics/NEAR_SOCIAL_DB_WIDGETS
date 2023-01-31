96b7048c577fb424eb2ebea61737b99ccca489a4
evgenykuzyakov <evgenykuzyakov>
2023-01-31 17:16:38 -0500

    Update MainPage.Post by mob.near at 2023-01-31 17:16:38.054

--- mob.near/MainPage.Post/source_code.jsx
+++ mob.near/MainPage.Post/source_code.jsx
@@ -58,6 +58,7 @@ return (
         src="mob.near/widget/MainPage.Comment.Feed"
         props={{
           item,
+          highlightComment: props.highlightComment,
         }}
       />
     </div>
