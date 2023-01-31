d90a3875ae26ff6b5425f323dbf1e4dd445bd801
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-01-31 16:10:45 -0500

    Update DevSuport.Main by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-01-31 16:10:45.487

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSuport.Main/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSuport.Main/source_code.jsx
@@ -1,29 +1,31 @@
-initState({ showReply: false });
+// By default the form to ask a question is hidden
+initState({ askQuestion: false });
 
 return (
   <div class="container">
     <div class="row">
       <div class="col-8">
-        <h2>Questions</h2>
+        <h1>Questions</h1>
       </div>
       <div class="col-4 justify-content-end">
         <button
           class="btn btn-primary float-end"
           disabled={!context.accountId}
           onClick={() => {
-            State.update({ showReply: !state.showReply });
+            State.update({ askQuestion: !state.askQuestion });
           }}
         >
           Ask a Question
         </button>
       </div>
     </div>
-
+    {/* Element to create the question */}
     <div class="my-3">
-      {state.showReply && (
+      {state.askQuestion && (
         <Widget src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Edit" />
       )}
     </div>
+    {/* Feed of Questions */}
     <Widget src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Feed" />
   </div>
 );
