931cfe6b126352fcedbfd9b9f4bce3f5785aa6e6
events_v1.near <events_v1.near>
2023-01-31 15:10:29 -0500

    Update events__new by events_v1.near at 2023-01-31 15:10:29.116

--- events_v1.near/events__new/source_code.jsx
+++ events_v1.near/events__new/source_code.jsx
@@ -14,14 +14,14 @@ if (!latestEvent) {
   return 'Loading';
 }
 
-const SECONDS_10 = 10000;
+const SECONDS_8 = 8000;
 // if event was just created, pop the stack and return
 if (
   latestEvent &&
-  new Date().getTime() - new Date(latestEvent.created_at).getTime() < SECONDS_10
+  new Date().getTime() - new Date(latestEvent.created_at).getTime() < SECONDS_8
 ) {
   props.__engine.pop();
-  return 'Event updated';
+  return 'Event created';
 }
 
 function createEvent(data) {
