5e647bd4964ee8f10898a3b0852958e567571232
events_v1.near <events_v1.near>
2023-01-31 14:53:08 -0500

    Update events__not_found by events_v1.near at 2023-01-31 14:53:08.812

--- events_v1.near/events__not_found/source_code.jsx
+++ events_v1.near/events__not_found/source_code.jsx
@@ -1,5 +1,5 @@
-// props.controller.setLayout('modal', {
-//   title: 'Alert',
-// });
+props.controller.setLayout('modal', {
+  title: 'Alert',
+});
 
 return <div>{props.message || 'unknown Error'}</div>;
