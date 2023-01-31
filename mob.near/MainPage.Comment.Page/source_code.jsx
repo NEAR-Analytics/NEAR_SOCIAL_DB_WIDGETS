e68e95a9c4d51fd277d6cb78b6da493f9a3b98a3
evgenykuzyakov <evgenykuzyakov>
2023-01-31 17:23:41 -0500

    Update MainPage.Comment.Page by mob.near at 2023-01-31 17:23:41.148

--- mob.near/MainPage.Comment.Page/source_code.jsx
+++ mob.near/MainPage.Comment.Page/source_code.jsx
@@ -1,1 +1,29 @@
-return <Widget src="mob.near/widget/MainPage.Comment" props={props} />;
+const { accountId, blockHeight } = props;
+
+const content = JSON.parse(
+  Social.get(`${accountId}/post/comment`, blockHeight) ?? "null"
+);
+if (content === null) {
+  return "Loading";
+}
+const item = content.item;
+
+const extractParentPost = (item) => {
+  if (!item || item.type !== "social" || !item.path || !item.blockHeight) {
+    return undefined;
+  }
+  const accountId = item.path.split("/")[0];
+  return `${accountId}/post/main` === item.path
+    ? { accountId, blockHeight: item.blockHeight }
+    : undefined;
+};
+
+const parentPost = extractParentPost(item);
+return parentPost ? (
+  <Widget
+    src="mob.near/widget/MainPage.Post"
+    props={{ ...parentPost, highlightComment: props, commentsLimit: 30 }}
+  />
+) : (
+  <Widget src="mob.near/widget/MainPage.Comment" props={props} />
+);
