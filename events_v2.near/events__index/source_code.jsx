f8c49cdae814276fc1f4e05e86f6f900a13c2df2
events_v2.near <events_v2.near>
2023-02-01 11:20:40 -0500

    Update events__index by events_v2.near at 2023-02-01 11:20:40.715

--- events_v2.near/events__index/source_code.jsx
+++ events_v2.near/events__index/source_code.jsx
@@ -13,13 +13,19 @@ const Container = props.__engine.Components.Container;
 
 return (
   <Container>
-    Important: open in /embed/:
-    <br />
-    <a href="https://near.social/#/embed/events_v2.near/widget/events__app">
-      Events Fullscreen:
-      https://near.social/#/embed/events_v2.near/widget/events__app
-    </a>
-    <br />
+    <div
+      style={{
+        marginBottom: '20px',
+      }}
+    >
+      Important: open in /embed/:
+      <br />
+      <a href="https://near.social/#/embed/events_v2.near/widget/events__app">
+        Events Fullscreen:
+        https://near.social/#/embed/events_v2.near/widget/events__app
+      </a>
+    </div>
+
     <Button
       onClick={() => {
         return props.__engine.push('my_events', {});
