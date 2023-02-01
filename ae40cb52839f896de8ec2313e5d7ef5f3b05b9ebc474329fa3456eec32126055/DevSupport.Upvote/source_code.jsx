2c13d36e7d40d07237dd9cf4ccd09f5a848dba58
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-02-01 16:27:48 -0500

    Update DevSupport.Upvote by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-02-01 16:27:48.150

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Upvote/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Upvote/source_code.jsx
@@ -1,5 +1,80 @@
+const accountId = props.accountId;
+const blockHeight = props.blockHeight;
+
+const item = props.item;
+
+if (!item) {
+  return "";
+}
+
+const likes = Social.index("upvote", item);
+
+const dataLoading = likes === null;
+
+let upvotes = 0;
+let downvotes = 0;
+
+(likes || []).forEach((like) => {
+  if (like.value.type === "upvote") {
+    upvotes += 1;
+  }
+  if (like.value.type === "downvote") {
+    downvotes += 1;
+  }
+});
+
+// state.hasLike === true
+
+// const hasLike = context.accountId && !!likesByUsers[context.accountId];
+
+const likeClick = () => {
+  if (state.loading) {
+    return;
+  }
+  State.update({
+    loading: true,
+  });
+  const data = {
+    index: {
+      upvote: JSON.stringify({
+        key: item,
+        value: {
+          type: hasLike ? "downvote" : "upvote",
+        },
+      }),
+    },
+  };
+
+  if (!hasLike && props.notifyAccountId) {
+    data.index.notify = JSON.stringify({
+      key: props.notifyAccountId,
+      value: {
+        type: "upvote",
+        item,
+      },
+    });
+  }
+  Social.set(data, {
+    onCommit: () => State.update({ loading: false, hasLike: !hasLike }),
+    onCancel: () => State.update({ loading: false }),
+  });
+};
+
 return (
-  <a href="#">
-    <i class={`bi bi-arrow-up-square`}> </i> Upvotes [0]
-  </a>
+  <div className="d-inline-flex align-items-center">
+    <button
+      disabled={state.loading || dataLoading || !context.accountId}
+      className="btn me-1 text-primary"
+      onClick={likeClick}
+    >
+      {state.loading || dataLoading ? (
+        <span
+          className="spinner-grow spinner-grow-sm p-2"
+          role="status"
+          aria-hidden="true"
+        />
+      ) : null}
+      <i class={`bi bi-arrow-up-square`}> </i> Upvotes [{upvotes}]
+    </button>
+  </div>
 );
