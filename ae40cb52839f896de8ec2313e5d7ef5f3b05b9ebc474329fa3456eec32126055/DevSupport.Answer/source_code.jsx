d3d7ac1051805dcc46d1c0c8be28b99ea9ed1a63
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-01-31 19:02:28 -0500

    Update DevSupport.Answer by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-01-31 19:02:28.181

---
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Answer/source_code.jsx
@@ -0,0 +1,8 @@
+const accountId = props.accountId;
+const blockHeight = parseInt(props.blockHeight);
+
+const answer = JSON.parse(
+  Social.get(`${accountId}/question/answer`, blockHeight) ?? "null"
+);
+
+return answer;
