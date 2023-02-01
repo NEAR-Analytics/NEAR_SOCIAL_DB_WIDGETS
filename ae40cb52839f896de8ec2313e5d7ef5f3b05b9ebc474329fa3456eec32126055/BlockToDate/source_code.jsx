b74524f7eabfb8333115a28d6097faba361255c5
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-01-31 20:25:26 -0500

    Update BlockToDate by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-01-31 20:25:26.980

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/BlockToDate/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/BlockToDate/source_code.jsx
@@ -13,4 +13,9 @@ if (!block) {
 const timeMs = parseFloat(block.header.timestamp_nanosec) / 1e6;
 const date = new Date(timeMs);
 
-return date.toDateString();
+return (
+  <>
+    <i class="bi bi-clock-history px-2"></i>
+    {date.toDateString()}
+  </>
+);
