7727ff26f4c37232ebbbfb918019355e9bda9372
events_v2.near <events_v2.near>
2023-02-01 10:54:50 -0500

    Update events__new by events_v2.near at 2023-02-01 10:54:50.091

---
+++ events_v2.near/events__new/source_code.jsx
@@ -0,0 +1,70 @@
+props.controller.setLayout('modal', {
+  title: 'Create Event',
+  back: true,
+});
+
+const EVENTS_CONTRACT = 'events_v2.near';
+const APP_OWNER = 'events_v2.near';
+const APP_NAME = 'events';
+
+const latestEvent = Near.view(EVENTS_CONTRACT, 'get_latest_event', {
+  account_id: props.__engine.accountId,
+});
+if (!latestEvent) {
+  return 'Loading';
+}
+
+const SECONDS_8 = 8000;
+// if event was just created, pop the stack and return
+if (
+  latestEvent &&
+  new Date().getTime() - new Date(latestEvent.created_at).getTime() < SECONDS_8
+) {
+  props.__engine.pop();
+  return 'Event created';
+}
+
+function createEvent(data) {
+  const {
+    name,
+    type,
+    category,
+    status,
+    start_date,
+    end_date,
+    location,
+    images,
+    links,
+    description,
+  } = data;
+
+  props.__engine.contract.call(EVENTS_CONTRACT, 'create_event', {
+    account_id: props.__engine.accountId,
+    name,
+    type,
+    category,
+    status,
+    start_date,
+    end_date,
+    location,
+    images,
+    links,
+    description,
+  });
+}
+
+function onSave(data) {
+  createEvent(data);
+}
+
+return (
+  <div>
+    <Widget
+      src={`${APP_OWNER}/widget/${APP_NAME}___form`}
+      props={{
+        onSave,
+        buttonText: 'Create event',
+      }}
+    />
+  </div>
+);
