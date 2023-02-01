87d198a593cb8f414425dd061e2b76277d542cae
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-01-31 20:35:38 -0500

    Update DevSupport.Question.LabelsDisplay by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-01-31 20:35:38.009

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Question.LabelsDisplay/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Question.LabelsDisplay/source_code.jsx
@@ -2,9 +2,7 @@ return props.labels ? (
   <div class="card-title">
     {props.labels.map((label) => {
       return (
-        <a
-          href={`https://near.social/#/devgovgigs.near/widget/Ideas?label=${label}`}
-        >
+        <a href="#">
           <span class="badge p-0 pe-2 text-primary">{label}</span>
         </a>
       );
