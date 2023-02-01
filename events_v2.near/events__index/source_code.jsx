6588fb3af67dcc8b131d356b63f91b585b07a5d0
events_v2.near <events_v2.near>
2023-02-01 10:54:24 -0500

    Update events__index by events_v2.near at 2023-02-01 10:54:24.083

---
+++ events_v2.near/events__index/source_code.jsx
@@ -0,0 +1,28 @@
+props.controller.setLayout('container', {
+  title: 'ND Events',
+  back: false,
+
+  primaryAction: {
+    label: 'Create new Event',
+    onClick: ['push', 'new', {}],
+  },
+});
+
+const Button = props.__engine.Components.Button;
+const Container = props.__engine.Components.Container;
+
+return (
+  <Container>
+    <Button
+      onClick={() => {
+        return props.__engine.push('my_events', {});
+      }}
+    >
+      My Events
+    </Button>
+
+    <br />
+
+    {props.__engine.renderComponent('index.list_container', {})}
+  </Container>
+);
