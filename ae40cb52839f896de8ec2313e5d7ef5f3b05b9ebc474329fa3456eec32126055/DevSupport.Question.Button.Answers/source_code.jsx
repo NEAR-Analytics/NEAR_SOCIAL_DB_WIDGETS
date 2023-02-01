642f544cdfa607b120f292b91cdd929cee792f74
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-02-01 18:33:04 -0500

    Update DevSupport.Question.Button.Answers by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-02-01 18:33:04.604

---
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Question.Button.Answers/source_code.jsx
@@ -0,0 +1,18 @@
+const accountId = props.accountId;
+const blockHeight = props.blockHeight;
+
+const item = props.item || {
+  type: "social",
+  path: `${accountId}/question/main`,
+  blockHeight,
+};
+
+const link = `#/ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Page?accountId=${accountId}&blockHeight=${blockHeight}`;
+
+const likes = Social.index("answer", item);
+
+return (
+  <a href={link} class="ms-3">
+    <i class={`bi bi-chat`}> </i> Answers [{likes.length}]
+  </a>
+);
