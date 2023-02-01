bcf3ad45d5e0b184026a39f1d8bc39cf43db8cb9
events_v2.near <events_v2.near>
2023-02-01 10:53:21 -0500

    Update app__layout_controller by events_v2.near at 2023-02-01 10:53:21.933

---
+++ events_v2.near/app__layout_controller/source_code.jsx
@@ -0,0 +1,53 @@
+if (state === undefined || state === null) {
+  State.init({
+    layout: null,
+    layoutProps: null,
+  });
+  return null;
+}
+
+function setLayout(name, props) {
+  if (
+    state &&
+    state.layout === name &&
+    JSON.stringify(state.layoutProps) === JSON.stringify(props)
+  ) {
+    return;
+  }
+
+  State.update({
+    layout: name,
+    layoutProps: props,
+  });
+}
+
+const layout = state.layout;
+const layoutProps = state.layoutProps || {};
+
+let layoutName = layout;
+if (
+  layout === '' ||
+  layout === 'default' ||
+  layout === null ||
+  layout === undefined
+) {
+  layoutName = 'default';
+}
+
+// get existing controller from component props or create a new one
+const controller = props.component.props.controller || {
+  setLayout,
+};
+
+const layProps = {
+  ...layoutProps,
+  __engine: props.__engine,
+  controller,
+  component: {
+    name: props.component.name,
+    props: { ...props.component.props, controller, __engine: props.__engine },
+  },
+};
+
+const path = props.__engine.layoutPathFromName(layoutName);
+return <Widget src={path} props={layProps} />;
