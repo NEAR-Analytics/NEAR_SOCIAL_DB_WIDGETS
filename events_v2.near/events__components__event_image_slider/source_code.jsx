66ec52c33b07133843f89210af7cb167e7f7fcd8
events_v2.near <events_v2.near>
2023-02-01 10:53:58 -0500

    Update events__components__event_image_slider by events_v2.near at 2023-02-01 10:53:58.285

---
+++ events_v2.near/events__components__event_image_slider/source_code.jsx
@@ -0,0 +1,60 @@
+let event = props.event || null;
+
+if (!state) {
+  State.init({ index: 0 });
+}
+
+if (!event) {
+  // TODO: return default image
+  return <img src="" alt="Event!" />;
+}
+
+const mode = props.mode || 'banner';
+
+const imagesWithCid = (event.images || [])
+  .filter((image) => {
+    return image.url && image.url.cid;
+  })
+  .filter((image) => {
+    return image.url.cid.length > 0;
+  });
+
+const displayImages = imagesWithCid.filter((image) => {
+  return image.type === mode;
+});
+
+return (
+  <div
+    style={{
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      maxWidth: '100%',
+      overflow: 'hidden',
+      userSelect: 'none',
+    }}
+  >
+    {displayImages.map((image) => {
+      return (
+        <img
+          src={`https://ipfs.near.social/ipfs/${image.url.cid}`}
+          key={image.cid}
+          alt={image.url.cid}
+          style={{
+            width: '100%',
+            height: '100%',
+            maxWidth: '100%',
+            boxSizing: 'border-box',
+            objectFit: 'cover',
+            transform: `translateX(-${
+              (state.index * 100) / displayImages.length
+            }%)`,
+            transition: 'transform 0.5s',
+          }}
+        />
+      );
+    })}
+  </div>
+);
