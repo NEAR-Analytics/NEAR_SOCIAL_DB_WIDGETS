ef1868c025bfcddbb45dcd8078d9c596029ef04d
events_v2.near <events_v2.near>
2023-02-01 10:53:37 -0500

    Update events__app by events_v2.near at 2023-02-01 10:53:37.738

---
+++ events_v2.near/events__app/source_code.jsx
@@ -0,0 +1,15 @@
+const APP_OWNER = 'events_v2.near';
+const APP_NAME = 'events';
+
+return (
+  <Widget
+    src={`${APP_OWNER}/widget/app__frame`}
+    props={{
+      ...props,
+      appOwner: APP_OWNER,
+      appName: APP_NAME,
+      entryRoute: 'index',
+      entryProps: {},
+    }}
+  />
+);
