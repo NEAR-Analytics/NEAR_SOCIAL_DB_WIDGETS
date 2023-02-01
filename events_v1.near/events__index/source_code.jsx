093954060e6e152080688188b09d4afcb5396b4a
events_v1.near <events_v1.near>
2023-02-01 11:26:55 -0500

    Update events__index by events_v1.near at 2023-02-01 11:26:55.546

--- events_v1.near/events__index/source_code.jsx
+++ events_v1.near/events__index/source_code.jsx
@@ -13,6 +13,19 @@ const Container = props.__engine.Components.Container;
 
 return (
   <Container>
+    <div
+      style={{
+        marginBottom: '20px',
+      }}
+    >
+      Important: open in /embed/:
+      <br />
+      <a href="https://near.social/#/embed/events_v2.near/widget/events__app">
+        Events Fullscreen:
+        https://near.social/#/embed/events_v2.near/widget/events__app
+      </a>
+    </div>
+
     <Button
       onClick={() => {
         return props.__engine.push('my_events', {});
@@ -20,9 +33,7 @@ return (
     >
       My Events
     </Button>
-
     <br />
-
     {props.__engine.renderComponent('index.list_container', {})}
   </Container>
 );
