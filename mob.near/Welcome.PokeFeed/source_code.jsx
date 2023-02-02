00db482875af7eae48ddac3b50afde4d35b27a3a
evgenykuzyakov <evgenykuzyakov>
2023-02-02 16:47:40 -0500

    Update Welcome.PokeFeed by mob.near at 2023-02-02 16:47:40.451

--- mob.near/Welcome.PokeFeed/source_code.jsx
+++ mob.near/Welcome.PokeFeed/source_code.jsx
@@ -7,39 +7,40 @@ const index = {
   },
 };
 
-const renderItem = (a) => (
-  <div key={JSON.stringify(a)} className="mb-2">
-    <span className="fs-4">
-      <Widget
-        src="mob.near/widget/ProfileLine"
-        props={{
-          accountId: a.value.accountId,
-          hideName: true,
-          hideAccountId: true,
-          tooltip: true,
-        }}
-      />
-      <span role="img" aria-label="poked" title="poked">
-        👈
+const renderItem = (a) =>
+  a.value.accountId && (
+    <div key={JSON.stringify(a)} className="mb-2">
+      <span className="fs-4">
+        <Widget
+          src="mob.near/widget/ProfileLine"
+          props={{
+            accountId: a.value.accountId,
+            hideName: true,
+            hideAccountId: true,
+            tooltip: true,
+          }}
+        />
+        <span role="img" aria-label="poked" title="poked">
+          👈
+        </span>
+        <Widget
+          src="mob.near/widget/ProfileLine"
+          props={{
+            accountId: a.accountId,
+            hideName: true,
+            hideAccountId: true,
+            tooltip: true,
+          }}
+        />
       </span>
-      <Widget
-        src="mob.near/widget/ProfileLine"
-        props={{
-          accountId: a.accountId,
-          hideName: true,
-          hideAccountId: true,
-          tooltip: true,
-        }}
-      />
-    </span>
-    <span className="text-muted">
-      <Widget
-        src="mob.near/widget/TimeAgo"
-        props={{ blockHeight: a.blockHeight }}
-      />
-    </span>
-  </div>
-);
+      <span className="text-muted">
+        <Widget
+          src="mob.near/widget/TimeAgo"
+          props={{ blockHeight: a.blockHeight }}
+        />
+      </span>
+    </div>
+  );
 
 return (
   <>
