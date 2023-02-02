97c33b1a7f63e8c37f02c2fdb16060c2b0a6262a
events_v2.near <events_v2.near>
2023-02-02 16:13:47 -0500

    Update events__layouts__modal by events_v2.near at 2023-02-02 16:13:47.224

--- events_v2.near/events__layouts__modal/source_code.jsx
+++ events_v2.near/events__layouts__modal/source_code.jsx
@@ -18,7 +18,7 @@ const ModalContent = styled.div`
   top: 50%;
   left: 50%;
   position: absolute;
-  transform-origin: bottom left;
+  transform-origin: bottom;
   background-color: white;
   width: 80%;
   height: 80%;
