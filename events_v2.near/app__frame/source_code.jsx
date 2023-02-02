10833a35cdd404573d938a6f7592df7e46360d81
events_v2.near <events_v2.near>
2023-02-02 17:07:49 -0500

    Update app__frame by events_v2.near at 2023-02-02 17:07:49.117

--- events_v2.near/app__frame/source_code.jsx
+++ events_v2.near/app__frame/source_code.jsx
@@ -553,7 +553,7 @@ const AppLayer = styled.div`
 `;
 
 const TimerRef = styled.div`
-  animation: ${FadeIn} ${({ duration }) => duration}ms ease-out;
+  animation: ${FadeIn} 10ms ease-out;
   width: 0;
   height: 0;
 `;
