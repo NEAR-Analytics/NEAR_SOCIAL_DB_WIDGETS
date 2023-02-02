c1177ae2313456bbf1e81d641f7f54107e292735
evgenykuzyakov <evgenykuzyakov>
2023-02-02 01:25:44 -0500

    Update MainPage.Comment by mob.near at 2023-02-02 01:25:44.377

--- mob.near/MainPage.Comment/source_code.jsx
+++ mob.near/MainPage.Comment/source_code.jsx
@@ -18,42 +18,47 @@ const extractNotifyAccountId = (item) => {
 const link = `#/mob.near/widget/MainPage.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`;
 
 return (
-  <div
-    className={`pt-3 border-top pb-2 ${
-      highlight ? "bg-warning bg-opacity-10" : ""
-    }`}
-  >
-    <Widget
-      src="mob.near/widget/MainPage.Post.Header"
-      props={{ accountId, blockHeight, link, postType: "comment" }}
-    />
-    <div className="mt-2 text-break">
-      <Widget src="mob.near/widget/MainPage.Post.Content" props={{ content }} />
-    </div>
-    {blockHeight !== "now" && (
-      <div className="mt-1 d-flex justify-content-between">
+  <>
+    <div
+      className={`pt-3 border-top pb-2 ${
+        highlight ? "bg-warning bg-opacity-10" : ""
+      }`}
+    >
+      <Widget
+        src="mob.near/widget/MainPage.Post.Header"
+        props={{ accountId, blockHeight, link, postType: "comment" }}
+      />
+      <div className="mt-2 text-break">
         <Widget
-          src="mob.near/widget/LikeButton"
-          props={{
-            notifyAccountId: accountId,
-            item: {
-              type: "social",
-              path: `${accountId}/post/comment`,
-              blockHeight,
-            },
-          }}
+          src="mob.near/widget/MainPage.Post.Content"
+          props={{ content }}
         />
-        {parentItem && (
+      </div>
+      {blockHeight !== "now" && (
+        <div className="mt-1 d-flex justify-content-between">
           <Widget
-            src="mob.near/widget/CommentButton"
+            src="mob.near/widget/LikeButton"
             props={{
-              onClick: () =>
-                !state.showReply && State.update({ showReply: true }),
+              notifyAccountId: accountId,
+              item: {
+                type: "social",
+                path: `${accountId}/post/comment`,
+                blockHeight,
+              },
             }}
           />
-        )}
-      </div>
-    )}
+          {parentItem && (
+            <Widget
+              src="mob.near/widget/CommentButton"
+              props={{
+                onClick: () =>
+                  !state.showReply && State.update({ showReply: true }),
+              }}
+            />
+          )}
+        </div>
+      )}
+    </div>
     {state.showReply && (
       <div className="mb-2" key="reply">
         <Widget
@@ -67,5 +72,5 @@ return (
         />
       </div>
     )}
-  </div>
+  </>
 );
