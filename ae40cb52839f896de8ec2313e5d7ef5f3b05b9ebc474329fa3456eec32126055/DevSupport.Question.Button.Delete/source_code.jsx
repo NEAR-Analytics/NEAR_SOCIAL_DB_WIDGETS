43cda46ddb5bed45fe32617398bf0ef8b72c4a5a
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-02-03 17:51:12 -0500

    Update DevSupport.Question.Button.Delete by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-02-03 17:51:12.329

---
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Question.Button.Delete/source_code.jsx
@@ -0,0 +1,23 @@
+const accountId = props.accountId;
+const blockHeight = props.blockHeight;
+const adminContract = props.adminContract;
+const admins = props.admins;
+
+if (!admins.includes(context.accountId)) {
+  return "";
+}
+
+const onClick = () => {
+  NEAR.call(adminContract, "hide_element", { id: { accountId, blockHeight } });
+};
+
+return (
+  <button
+    disabled={state.loading || dataLoading || !context.accountId}
+    className="text-black border-0 bg-white"
+    onClick={onClick}
+  >
+    <i class={`bi bi-trash`}> </i>
+    Delete
+  </button>
+);
