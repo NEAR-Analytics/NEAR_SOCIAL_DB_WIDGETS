0ea8a5b97b73ad4d885596460cddcf91f3bb1992
testwiki.near <testwiki.near>
2023-02-03 20:56:22 -0500

    Update WikiOnSocialDB_Authors by testwiki.near at 2023-02-03 20:56:22.310

--- testwiki.near/WikiOnSocialDB_Authors/source_code.jsx
+++ testwiki.near/WikiOnSocialDB_Authors/source_code.jsx
@@ -1,42 +1,30 @@
 const authors = Array.from(props.resultArticles, ({ author }) => author);
-const uniqAuthors = Array.from(new Set(authors));
+// const uniqAuthors = Array.from(new Set(authors));
 
-const tweets = [
-  { id: "000", likes: 5, tags: ["js", "nodejs"] },
-  { id: "001", likes: 2, tags: ["html", "css"] },
-  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-  { id: "003", likes: 8, tags: ["css", "react"] },
-  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-];
-// const getTags = (arr) =>
-//   arr.reduce((acc, x) => (x.tags ? acc.concat(x.tags) : acc), []);
-// const tags = getTags(tweets);
-// console.log(tags);
-
-const getTagStats = (acc, tag) => {
-  if (!acc.hasOwnProperty(tag)) {
-    acc[tag] = 0;
+const getAuthorsStats = (acc, author) => {
+  if (!acc.hasOwnProperty(author)) {
+    acc[author] = 0;
   }
-  acc[tag] += 1;
+  acc[author] += 1;
   return acc;
 };
-const countTags = (arr) => arr.reduce(getTagStats, {});
-// const tagCount = countTags(tags);
-
-const authorsCount = countTags(authors);
-console.log(authorsCount);
+const countAuthors = (arr) => arr.reduce(getAuthorsStats, {});
+const authorsCountObject = countAuthors(authors);
+const authorsCountArray = Object.entries(authorsCountObject);
+console.log(authorsCountArray);
 
 return (
   <>
-    <h6>Total authors: {uniqAuthors.length}</h6>
+    <h6>Total authors: {authorsCountArray.length}</h6>
     <ul>
-      {uniqAuthors.map((author) => (
+      {authorsCountArray.map(([author, quantity]) => (
         <li>
           <a
             href={`https://near.social/#/mob.near/widget/ProfilePage?accountId=${author}`}
           >
             {author}
-          </a>
+          </a>{" "}
+          - {quantity}
         </li>
       ))}
     </ul>
