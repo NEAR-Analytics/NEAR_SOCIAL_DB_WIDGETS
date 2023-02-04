bff51140d5c6f90c2908b0d6d2c569b14f5cf8d3
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-02-03 23:14:10 -0500

    Update DevSupport.Answer.Button.Valid by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-02-03 23:14:10.973

---
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Answer.Button.Valid/source_code.jsx
@@ -0,0 +1,25 @@
+const accountId = props.accountId;
+const blockHeight = props.blockHeight;
+const adminContract = props.adminContract;
+const admins = props.admins;
+
+if (!admins.includes(context.accountId)) {
+  return "";
+}
+
+const onClick = () => {
+  Near.call(adminContract, "mark_useful", {
+    id: { account_id: accountId, block_height: blockHeight },
+  });
+};
+
+return (
+  <button
+    disabled={state.loading || dataLoading || !context.accountId}
+    className="text-success border-0 bg-white ms-3"
+    onClick={onClick}
+  >
+    <i class={`bi bi-check-square`}> </i>
+    Mark as Valid
+  </button>
+);
