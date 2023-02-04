9e8d80a79242f2c60a2b512184c4629264d4c7f7
zavodil <zavodil>
2023-02-04 18:26:18 -0500

    Update PriceOracle.NearPrices by zavodil.near at 2023-02-04 18:26:18.788

--- zavodil.near/PriceOracle.NearPrices/source_code.jsx
+++ zavodil.near/PriceOracle.NearPrices/source_code.jsx
@@ -31,13 +31,13 @@ let prices = assets.map((asset) => {
   );
 
   return (
-    <div class="col">
+    <small class="col">
       {asset_name}:{" "}
       {`$${price
         .toFixed(price.cmp(new Big(100)) === 1 ? 1 : 2)
         .replace(/\d(?=(\d{3})+\.)/g, "$&,")}
       `}
-    </div>
+    </small>
   );
 });
 
