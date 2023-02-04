aa7a48bf5c04fcd45585693a476df4671df66064
evgenykuzyakov <evgenykuzyakov>
2023-02-03 20:39:17 -0500

    Update MainPage.Post.Content by mob.near at 2023-02-03 20:39:17.670

--- mob.near/MainPage.Post.Content/source_code.jsx
+++ mob.near/MainPage.Post.Content/source_code.jsx
@@ -1,24 +1,32 @@
 const content = props.content;
+const raw = !!props.raw;
+
 return content ? (
   <>
-    {content.text && (
-      <Widget
-        src="mob.near/widget/SocialMarkdown"
-        props={{ text: content.text }}
-      />
-    )}
-    {content.image && (
-      <div className="w-100 rounded-3 text-center">
+    {content.text &&
+      (raw ? (
+        <pre>{content.text}</pre>
+      ) : (
         <Widget
-          src="mob.near/widget/Image"
-          props={{
-            image: content.image,
-            className: "img-fluid rounded-3",
-            style: { maxHeight: "20em" },
-          }}
+          src="mob.near/widget/SocialMarkdown"
+          props={{ text: content.text }}
         />
-      </div>
-    )}
+      ))}
+    {content.image &&
+      (raw ? (
+        <pre>{content.image}</pre>
+      ) : (
+        <div className="w-100 rounded-3 text-center">
+          <Widget
+            src="mob.near/widget/Image"
+            props={{
+              image: content.image,
+              className: "img-fluid rounded-3",
+              style: { maxHeight: "20em" },
+            }}
+          />
+        </div>
+      ))}
   </>
 ) : (
   <span
