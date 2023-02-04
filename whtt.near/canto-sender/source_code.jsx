62e229e9e955ce323c323be1ebcd428634890377
whtt.near <whtt.near>
2023-02-04 01:20:43 -0500

    Update canto-sender by whtt.near at 2023-02-04 01:20:43.284

---
+++ whtt.near/canto-sender/source_code.jsx
@@ -0,0 +1,163 @@
+const sender = Ethers.send("eth_requestAccounts", [])[0];
+
+if (!sender) return "Please login first";
+
+const erc20Abi = fetch(
+  "https://gist.githubusercontent.com/veox/8800debbf56e24718f9f483e1e40c35c/raw/f853187315486225002ba56e5283c1dba0556e6f/erc20.abi.json"
+);
+if (!erc20Abi.ok) {
+  return "scam";
+}
+
+const iface = new ethers.utils.Interface(erc20Abi.body);
+
+initState({
+  token: "",
+  tokenDecimals: "",
+  sendTo: "",
+  sender,
+  senderBalance: "0",
+  receiverBalance: "0",
+  receiver: "",
+  amount: "1",
+});
+
+const tokens = {
+  "Select Token": "",
+  NOTE: "0x4e71a2e537b7f9d9413d3991d37958c0b5e1e503",
+};
+
+const tokensMenuItems = Object.keys(tokens).map((token) => (
+  <option value={tokens[token]}>{token}</option>
+));
+
+const setSendTo = (sendTo) => {
+  const receiver = Ethers.resolveName(sendTo);
+  State.update({ sendTo, receiver: receiver ?? "" });
+  refreshBalances();
+};
+
+const setToken = (token) => {
+  State.update({ token });
+  getTokenDecimals();
+};
+
+const getTokenBalance = (receiver) => {
+  const encodedData = iface.encodeFunctionData("balanceOf", [receiver]);
+
+  return Ethers.provider()
+    .call({
+      to: state.token,
+      data: encodedData,
+    })
+    .then((rawBalance) => {
+      const receiverBalanceHex = iface.decodeFunctionResult(
+        "balanceOf",
+        rawBalance
+      );
+
+      return Big(receiverBalanceHex.toString())
+        .div(Big(10).pow(state.tokenDecimals))
+        .toFixed(2)
+        .replace(/\d(?=(\d{3})+\.)/g, "$&,");
+    });
+};
+
+const getTokenDecimals = () => {
+  const encodedData = iface.encodeFunctionData("decimals", []);
+
+  return Ethers.provider()
+    .call({
+      to: state.token,
+      data: encodedData,
+    })
+    .then((tokenDecimals) => {
+      State.update({ tokenDecimals: parseInt(Number(tokenDecimals)) });
+      refreshBalances();
+    });
+};
+
+const refreshBalances = () => {
+  getTokenBalance(state.sender).then((value) => {
+    State.update({ senderBalance: value });
+  });
+
+  getTokenBalance(state.receiver).then((value) => {
+    State.update({ receiverBalance: value });
+  });
+};
+
+const sendTokens = () => {
+  const erc20 = new ethers.Contract(
+    state.token,
+    erc20Abi.body,
+    Ethers.provider().getSigner()
+  );
+
+  let amount = ethers.utils.parseUnits(state.amount, state.tokenDecimals);
+
+  erc20.transfer(state.receiver, amount);
+
+  console.log("transactionHash is " + transactionHash);
+};
+
+return (
+  <>
+    <h3>Send ERC-20 tokens</h3>
+    <div class="mb-3">
+      <label for="selectToken">Select token</label>
+      <select
+        class="form-select"
+        id="selectToken"
+        onChange={(e) => {
+          setToken(e.target.value);
+        }}
+      >
+        {tokensMenuItems}
+      </select>
+    </div>
+
+    <div class="mb-3">
+      <label for="send-to" class="form-label">
+        Recepient address
+      </label>
+      <input
+        value={state.sendTo}
+        class="form-control"
+        id="send-to"
+        placeholder="vitalik.eth"
+        onChange={(e) => setSendTo(e.target.value)}
+      />
+      {state.receiver && (
+        <div class="text-secondary mt-3">Resolved to {state.receiver}</div>
+      )}
+      {state.receiverBalance != "0" && (
+        <div class="text-secondary mt-3">
+          Receiver's balance: {state.receiverBalance}
+        </div>
+      )}
+
+      {state.senderBalance != "0" && (
+        <div class="text-secondary mt-3">
+          Sender's balance: {state.senderBalance}
+        </div>
+      )}
+    </div>
+
+    <div class="mb-3">
+      <label for="amount" class="form-label">
+        Enter the amount
+      </label>
+      <input
+        value={state.amount}
+        class="form-control"
+        id="amount"
+        placeholder=""
+        onChange={(e) => State.update({ amount: e.target.value })}
+      />
+    </div>
+    <div class="mb-3">
+      <button onClick={sendTokens}>Send</button>
+    </div>
+  </>
+);
