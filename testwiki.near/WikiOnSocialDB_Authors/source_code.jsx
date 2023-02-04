9848c79fcd74d6c2393f90b723c3628fcc63cff3
testwiki.near <testwiki.near>
2023-02-03 20:05:46 -0500

    Update WikiOnSocialDB_Authors by testwiki.near at 2023-02-03 20:05:46.452

--- testwiki.near/WikiOnSocialDB_Authors/source_code.jsx
+++ testwiki.near/WikiOnSocialDB_Authors/source_code.jsx
@@ -1,5 +1,6 @@
 const authors = Array.from(props.resultArticles, ({ author }) => author);
 const uniqAuthors = Array.from(new Set(authors));
+console.log(uniqAuthors);
 return (
   <>
     <h6>Total authors: {uniqAuthors.length}</h6>
