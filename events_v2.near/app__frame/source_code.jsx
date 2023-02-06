ab0d1dad518ac4a35257e961ab3ea3668d0ebd6e
events_v2.near <events_v2.near>
2023-02-06 14:09:08 -0500

    Update app__frame by events_v2.near at 2023-02-06 14:09:08.707

--- events_v2.near/app__frame/source_code.jsx
+++ events_v2.near/app__frame/source_code.jsx
@@ -84,11 +84,9 @@ const Animations = {
   FadeIn: styled.keyframes`
     0% {
       opacity: 0;
-      transform: translateY(10px);
     }
     100% {
       opacity: 1;
-      transform: translateY(0px);
     }
   `,
 };
