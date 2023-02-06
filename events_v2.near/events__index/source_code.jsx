739ef010aedd89da8ff787520529e94f11ed4723
events_v2.near <events_v2.near>
2023-02-06 16:17:52 -0500

    Update events__index by events_v2.near at 2023-02-06 16:17:52.938

--- events_v2.near/events__index/source_code.jsx
+++ events_v2.near/events__index/source_code.jsx
@@ -1,4 +1,4 @@
-props.controller.setLayout('container', {
+props.controller.setLayout('layouts:container', {
   title: 'ND Events',
   back: false,
 
@@ -13,19 +13,6 @@ const Container = props.__engine.Components.Container;
 
 return (
   <Container>
-    {/* <div
-      style={{
-        marginBottom: '20px',
-      }}
-    >
-      Important: open in /embed/:
-      <br />
-      <a href="https://near.social/#/embed/events_v1.near/widget/events__app">
-        Events Fullscreen:
-        https://near.social/#/embed/events_v1.near/widget/events__app
-      </a>
-    </div> */}
-
     <Button
       onClick={() => {
         return props.__engine.push('my_events', {});
@@ -33,7 +20,9 @@ return (
     >
       My Events
     </Button>
+
     <br />
+
     {props.__engine.renderComponent('index.list_container', {})}
   </Container>
 );
