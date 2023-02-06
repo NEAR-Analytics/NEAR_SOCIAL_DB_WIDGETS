fc40075bac7b959e862d4a35af57112ab48248f3
events_v2.near <events_v2.near>
2023-02-06 15:21:22 -0500

    Update events__layouts__container by events_v2.near at 2023-02-06 15:21:22.941

--- events_v2.near/events__layouts__container/source_code.jsx
+++ events_v2.near/events__layouts__container/source_code.jsx
@@ -34,13 +34,13 @@ const dropdownElement =
 
 const Pulse = styled.keyframes`
   0% {
-    transform: scale(1);
+    transform: scale(0.95);
   }
   50% {
     transform: scale(1.1);
   }
   100% {
-    transform: scale(1);
+    transform: scale(0.95);
   }
 `;
 
