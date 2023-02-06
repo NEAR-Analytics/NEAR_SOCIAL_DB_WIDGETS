73c58471cf4640a4a11a4edfa23a95997e55ca0e
eugenewolf507.near <eugenewolf507.near>
2023-02-06 12:38:10 -0500

    Update WikiOnSocialDB_AllArticlesList by eugenewolf507.near at 2023-02-06 12:38:10.537

---
+++ eugenewolf507.near/WikiOnSocialDB_AllArticlesList/source_code.jsx
@@ -0,0 +1,17 @@
+const { filteredArticles, getDateLastEdit, handleArticle, state } = props;
+return (
+  <ol>
+    {filteredArticles &&
+      filteredArticles.map((article, index) => (
+        <li key={article.articleId}>
+          <a href="" onClick={(e) => handleArticle(e, article)}>
+            {article.articleId}{" "}
+            <small>
+              (author: {article.author}
+              {getDateLastEdit(article.timeLastEdit)})
+            </small>
+          </a>
+        </li>
+      ))}
+  </ol>
+);
