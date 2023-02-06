ea57d2f4fa5961600596076bf5092afe81d632c1
eugenewolf507.near <eugenewolf507.near>
2023-02-06 18:34:39 -0500

    Update WikiOnSocialDB_ArticlesByUser by eugenewolf507.near at 2023-02-06 18:34:39.308

--- eugenewolf507.near/WikiOnSocialDB_ArticlesByUser/source_code.jsx
+++ eugenewolf507.near/WikiOnSocialDB_ArticlesByUser/source_code.jsx
@@ -1,3 +1,39 @@
 const addressForArticles = "wikiTest";
 const authorForWidget = "eugenewolf507.near";
+const mOKEDAYTHOR = "vanyog.near";
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+if (profile === null) {
+  return "Loading";
+}
+
+const wikiTestData = Social.get(`*/${addressForArticles}/articles/**`, "final");
+const wikiTestArr = wikiTestData && Object.values(wikiTestData);
+const resultArticles =
+  wikiTestArr &&
+  wikiTestArr.reduce(
+    (acc, account) =>
+      acc.concat(Object.values(account[addressForArticles].articles)),
+    []
+  );
+
+resultArticles.length &&
+  resultArticles.sort((a, b) => {
+    return Number(b.timeLastEdit) - Number(a.timeLastEdit);
+  });
+
+const filteredArticles =
+  resultArticles.length &&
+  resultArticles.reduce((acc, article) => {
+    if (acc.some(({ articleId }) => articleId === article.articleId)) {
+      return acc;
+    } else {
+      return [...acc, article];
+    }
+  }, []);
+
+console.log(filteredArticles);
 return <div>Hello World</div>;
