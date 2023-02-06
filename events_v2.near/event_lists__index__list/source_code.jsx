dd49c18d392a892ed034e9a9731cef000e8ddb35
events_v2.near <events_v2.near>
2023-02-06 16:35:38 -0500

    Update event_lists__index__list by events_v2.near at 2023-02-06 16:35:38.852

---
+++ events_v2.near/event_lists__index__list/source_code.jsx
@@ -0,0 +1,58 @@
+let event_lists = props.event_lists || [];
+if (!event_lists) {
+  return '';
+}
+
+// if event_lists are empty we want to show an empty list message
+if (event_lists.length === 0) {
+  return 'No event_lists found';
+}
+
+const SlideInLeft = styled.keyframes`
+  0% {
+    opacity: 0;
+    transform: translateX(-20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const IndexList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: stretch;
+  justify-content: flex-start;
+
+  width: auto;
+  margin-left: -20px;
+  margin-right: -20px;
+
+  & > * {
+    margin: 20px 20px;
+    max-width: 520px;
+    min-width: 320px;
+    width: 240px;
+    flex-grow: 3;
+    flex-shrink: 3;
+
+    animation: ${SlideInLeft} 0.3s ease-in-out;
+  }
+`;
+
+return (
+  <IndexList>
+    {event_lists.map((event) => {
+      return (
+        <div key={event.event_id}>
+          {props.__engine.renderComponent('index.list_item', { event })}
+        </div>
+      );
+    })}
+
+    <div>{/* spacer */}</div>
+    <div>{/* spacer */}</div>
+    <div>{/* spacer */}</div>
+  </IndexList>
+);
