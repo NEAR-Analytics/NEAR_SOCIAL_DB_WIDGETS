3b403b67c62c98d731f0f49f040fcb4b7d8177df
events_v2.near <events_v2.near>
2023-02-06 14:36:04 -0500

    Update events__layouts__container by events_v2.near at 2023-02-06 14:36:04.245

--- events_v2.near/events__layouts__container/source_code.jsx
+++ events_v2.near/events__layouts__container/source_code.jsx
@@ -32,6 +32,18 @@ const dropdownElement =
     </>
   ) : null;
 
+const Pulse = styled.keyframes`
+  0% {
+    transform: scale(0.95);
+  }
+  50% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0.95);
+  }
+`;
+
 const NavPrimaryButton = styled.button`
   background-color: transparent;
   user-select: none;
@@ -53,6 +65,8 @@ const NavPrimaryButton = styled.button`
   &:hover {
     /* darker and transparent */
     background-color: rgba(44, 44, 84, 0.85);
+
+    animation: ${Pulse} 4s infinite;
   }
 `;
 
