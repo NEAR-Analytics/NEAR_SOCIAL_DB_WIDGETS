52c3d0a67a4ba9d443f89b3c021b67ffff985dbb
events_v2.near <events_v2.near>
2023-02-06 17:02:39 -0500

    Update event_lists___form by events_v2.near at 2023-02-06 17:02:39.235

--- events_v2.near/event_lists___form/source_code.jsx
+++ events_v2.near/event_lists___form/source_code.jsx
@@ -31,13 +31,7 @@ if (!state) {
 
 const ValidationError = props.__engine.Components.ValidationError;
 const FullActionButton = props.__engine.Components.FullActionButton;
-const FormLabel = props.__engine.Components.FormLabel;
-
-const LinkTypes = [
-  { value: 'register', label: 'Register' },
-  { value: 'tickets', label: 'Tickets' },
-  { value: 'join_stream', label: 'Stream URL' },
-];
+const FormFormLabel = props.__engine.Components.FormFormLabel;
 
 function addError(key, message) {
   const oldErrors = { ...state.errors };
@@ -86,28 +80,9 @@ function assertCondition(valid, condition, key, message) {
 }
 
 function sanitize(data) {
-  const {
-    name,
-    type,
-    category,
-    status,
-    start_date,
-    end_date,
-    location,
-    images,
-    links,
-    description,
-  } = data;
+  const { name, description } = data;
   return {
     name,
-    type,
-    category,
-    status,
-    start_date: new Date(start_date).getTime(),
-    end_date: new Date(end_date).getTime(),
-    location,
-    images,
-    links,
     description,
   };
 }
@@ -115,17 +90,7 @@ function sanitize(data) {
 function validate(data) {
   let valid = true;
 
-  const {
-    name,
-    description,
-    end_date,
-    start_date,
-    images,
-    category,
-    type,
-    status,
-    location,
-  } = data;
+  const { name, description } = data;
 
   clearErrors();
 
@@ -142,91 +107,6 @@ function validate(data) {
       description.length < MAX_LENGTH_DESCRIPTION,
     'description',
     `Description must be between ${MIN_LENGTH_DESCRIPTION} and ${MAX_LENGTH_DESCRIPTION} characters long. Currently: ${description.length} characters.`
-  );
-
-  valid = assertCondition(
-    valid,
-    location !== null &&
-      location.length >= MIN_LENGTH_LOCATION &&
-      location.length < MAX_LENGTH_LOCATION,
-    'location',
-    `Location must be between ${MIN_LENGTH_LOCATION} and ${MAX_LENGTH_LOCATION} characters long. Currently: ${location.length} characters.`
-  );
-
-  valid = assertCondition(
-    valid,
-    category !== null &&
-      category.length >= MIN_LENGTH_CATEGORY &&
-      category.length < MAX_LENGTH_CATEGORY,
-    'category',
-    `Category must be between ${MIN_LENGTH_CATEGORY} and ${MAX_LENGTH_CATEGORY} characters long. Currently: ${category.length} characters.`
-  );
-
-  valid = assertCondition(
-    valid,
-    !end_date || new Date(end_date).getTime() > new Date(start_date).getTime(),
-    'end_date',
-    'End date must be after start date, or empty'
-  );
-
-  valid = assertCondition(
-    valid,
-    start_date !== null && start_date !== undefined && start_date !== '',
-    'start_date',
-    'Event must have a start date'
-  );
-
-  valid = assertCondition(
-    valid,
-    category !== null && category !== undefined && category !== '',
-    'category',
-    'Event must have a category'
-  );
-
-  valid = assertCondition(
-    valid,
-    location !== null && location !== undefined && location !== '',
-    'location',
-    'Event must have a location'
-  );
-
-  valid = assertCondition(
-    valid,
-    images !== null &&
-      images !== undefined &&
-      images.length >= 2 &&
-      images.reduce(
-        (acc, image) =>
-          acc &&
-          image.url !== null &&
-          image.url !== undefined &&
-          image.url !== '',
-        true
-      ) &&
-      images.reduce(
-        (acc, image) =>
-          acc &&
-          image.type !== null &&
-          image.type !== undefined &&
-          image.type !== '',
-        true
-      ),
-    'images',
-    'Event must have at least 2 images with a url and type'
-  );
-
-  valid = assertCondition(
-    valid,
-    type !== null && type !== undefined && type !== '',
-    'type',
-    'Event must have a type'
-  );
-
-  valid = assertCondition(
-    valid,
-    status !== null && status !== undefined && status !== '',
-    'status',
-    'Event must have a status'
   );
 
   return valid;
@@ -258,7 +138,7 @@ return (
     }}
   >
     <div className="mt-3">
-      <Label>Name</Label>
+      <FormLabel>Name</FormLabel>
       <input
         type="text"
         placeholder="Event Name"
@@ -268,10 +148,10 @@ return (
         }}
       />
     </div>
-    <ErrorMessage>{getErrors('name')}</ErrorMessage>
+    <ValidationError>{getErrors('name')}</ValidationError>
 
     <div className="mt-3">
-      <Label>Description</Label>
+      <FormLabel>Description</FormLabel>
       <textarea
         className="w-100"
         placeholder="Event Description"
@@ -282,10 +162,10 @@ return (
         rows={3}
       />
     </div>
-    <ErrorMessage>{getErrors('description')}</ErrorMessage>
+    <ValidationError>{getErrors('description')}</ValidationError>
 
     <div className="mt-3">
-      <Label>Type</Label>
+      <FormLabel>Type</FormLabel>
       <Select
         value={state.type}
         onChange={(event) => {
@@ -299,10 +179,10 @@ return (
         ))}
       </Select>
     </div>
-    <ErrorMessage>{getErrors('type')}</ErrorMessage>
+    <ValidationError>{getErrors('type')}</ValidationError>
 
     <div className="mt-3">
-      <Label>Category</Label>
+      <FormLabel>Category</FormLabel>
       <input
         type="text"
         placeholder="Event Category"
@@ -312,10 +192,10 @@ return (
         }}
       />
     </div>
-    <ErrorMessage>{getErrors('category')}</ErrorMessage>
+    <ValidationError>{getErrors('category')}</ValidationError>
 
     <div className="mt-3">
-      <Label>Status</Label>
+      <FormLabel>Status</FormLabel>
       <Select
         value={state.status}
         onChange={(event) => {
@@ -329,10 +209,10 @@ return (
         ))}
       </Select>
     </div>
-    <ErrorMessage>{getErrors('status')}</ErrorMessage>
+    <ValidationError>{getErrors('status')}</ValidationError>
 
     <div className="mt-3">
-      <Label>Start Date</Label>
+      <FormLabel>Start Date</FormLabel>
       <input
         type="date"
         value={state.start_date}
@@ -341,10 +221,10 @@ return (
         }}
       />
     </div>
-    <ErrorMessage>{getErrors('start_date')}</ErrorMessage>
+    <ValidationError>{getErrors('start_date')}</ValidationError>
 
     <div className="mt-3">
-      <Label>End Date</Label>
+      <FormLabel>End Date</FormLabel>
       <input
         type="date"
         value={state.end_date}
@@ -353,10 +233,10 @@ return (
         }}
       />
     </div>
-    <ErrorMessage>{getErrors('end_date')}</ErrorMessage>
+    <ValidationError>{getErrors('end_date')}</ValidationError>
 
     <div className="mt-3">
-      <Label>Location</Label>
+      <FormLabel>Location</FormLabel>
       <textarea
         className="w-100"
         placeholder="Event Location"
@@ -367,10 +247,10 @@ return (
         rows={3}
       />
     </div>
-    <ErrorMessage>{getErrors('location')}</ErrorMessage>
+    <ValidationError>{getErrors('location')}</ValidationError>
 
     <div className="mt-3">
-      <Label>Images</Label>
+      <FormLabel>Images</FormLabel>
 
       {state.images.map((image, index) => (
         <div key={index} className="mb-4 d-flex">
@@ -402,10 +282,10 @@ return (
         Add Image
       </button>
     </div>
-    <ErrorMessage>{getErrors('images')}</ErrorMessage>
+    <ValidationError>{getErrors('images')}</ValidationError>
 
     <div className="mt-3">
-      <Label>Links</Label>
+      <FormLabel>Links</FormLabel>
       {state.links.map((link, index) => (
         <div key={index} className="mb-4">
           <input
@@ -490,16 +370,16 @@ return (
         Add Link
       </button>
     </div>
-    <ErrorMessage>{getErrors('links')}</ErrorMessage>
+    <ValidationError>{getErrors('links')}</ValidationError>
 
     <br />
-    <Button
+    <FullActionButton
       className="mt-3"
       onClick={() => {
         sanitizeValidateAndCall(state);
       }}
     >
       {buttonText}
-    </Button>
+    </FullActionButton>
   </div>
 );
