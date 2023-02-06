b0c592ed1f3edfa2798a050d59c084625176bce0
events_v2.near <events_v2.near>
2023-02-06 15:21:31 -0500

    Update events__layouts__container by events_v2.near at 2023-02-06 15:21:31.039

--- events_v2.near/events__layouts__container/source_code.jsx
+++ events_v2.near/events__layouts__container/source_code.jsx
@@ -59,8 +59,8 @@ const NavPrimaryButton = styled.button`
 
   border-radius: 8px;
   border: 1px solid white;
-  box-shadow: box-shadow: 0 0 2px 1px rgb(0, 0, 0, 0.3),
-      0 0 49px 2px rgb(255, 255, 255, 0.4)
+  box-shadow: 0 0 2px 1px rgb(0, 0, 0, 0.3),
+      0 0 89px 2px rgb(255, 255, 255, 0.4)
   
 
   &:hover {
