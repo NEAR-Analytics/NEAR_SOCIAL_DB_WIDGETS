f2cea7f299ff5a99cbcbe65b313da13c7386aee8
Tekser17 <Tekser17>
2023-02-07 00:14:38 -0500

    Update ProfilePage by tekser15.near at 2023-02-07 00:14:38.885

---
+++ tekser15.near/ProfilePage/source_code.jsx
@@ -0,0 +1,37 @@
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+if (profile === null) {
+  return "Loading";
+}
+
+return (
+  <div className="py-1 px-1">
+    <div className="mx-auto">
+      <Widget
+        src="mob.near/widget/ProfileLarge"
+        props={{
+          accountId,
+          profile,
+          link: true,
+          showEditButton: !props.profile,
+        }}
+      />
+      <h1> Hello Guys</h1>
+      <img
+        src={"https://i.ibb.co/tK5Dvsn/Screenshot-605.png"}
+        alt="Bulbasaur"
+      />
+      <div className="mt-3">
+        <Widget
+          src="mob.near/widget/ProfileTabs"
+          props={{ accountId, profile }}
+        />
+      </div>
+    </div>
+  </div>
+);
