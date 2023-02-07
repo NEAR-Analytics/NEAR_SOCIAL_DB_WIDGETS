552e6f8fd008977659e82f8f0db8d314faa58942
events_v2.near <events_v2.near>
2023-02-07 11:09:21 -0500

    Update event_lists__index__list by events_v2.near at 2023-02-07 11:09:21.582

--- events_v2.near/event_lists__index__list/source_code.jsx
+++ events_v2.near/event_lists__index__list/source_code.jsx
@@ -8,41 +8,10 @@ if (event_lists.length === 0) {
   return 'No Results';
 }
 
-const SlideInLeft = styled.keyframes`
-  0% {
-    opacity: 0;
-    transform: translateX(-20px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateX(0);
-  }
-`;
-
-const IndexList = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: stretch;
-  justify-content: flex-start;
-
-  width: auto;
-  margin-left: -20px;
-  margin-right: -20px;
-
-  & > * {
-    margin: 20px 20px;
-    max-width: 520px;
-    min-width: 320px;
-    width: 45vw;
-    flex-grow: 3;
-    flex-shrink: 3;
-
-    animation: ${SlideInLeft} 0.3s ease-in-out;
-  }
-`;
+const GridContainer = props.__engine.Components.GridContainer;
 
 return (
-  <IndexList>
+  <GridContainer>
     {event_lists.map((event_list) => {
       return (
         <div key={event_list.event_list_id}>
@@ -53,5 +22,5 @@ return (
 
     <div>{/* spacer */}</div>
     <div>{/* spacer */}</div>
-  </IndexList>
+  </GridContainer>
 );
