e6ce8759032a695e554d0318e0d2b0f448099819
eugenewolf507.near <eugenewolf507.near>
2023-02-07 10:32:11 -0500

    Update WikiOnSocialDB_MainNavigation by eugenewolf507.near at 2023-02-07 10:32:11.800

--- eugenewolf507.near/WikiOnSocialDB_MainNavigation/source_code.jsx
+++ eugenewolf507.near/WikiOnSocialDB_MainNavigation/source_code.jsx
@@ -1,1 +1,50 @@
-return <div>Hello World</div>;
+const addressForArticles = "wikiTest";
+const authorForWidget = "eugenewolf507.near";
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+if (profile === null) {
+  return "Loading";
+}
+
+const pills = [
+  { id: "articles", title: "Articles" },
+  { id: "authors", title: "Authors" },
+  { id: "create", title: "Create Article" },
+];
+return (
+  <div>
+    <ul className="nav nav-pills nav-fill mb-4" id="pills-tab" role="tablist">
+      {pills.map(({ id, title }, i) => (
+        <li className="nav-item" role="presentation" key={i}>
+          <button
+            className={`nav-link ${i === 0 ? "active" : ""}`}
+            id={`pills-${id}-tab`}
+            data-bs-toggle="pill"
+            data-bs-target={`#pills-${id}`}
+            type="button"
+            role="tab"
+            aria-controls={`pills-${id}`}
+            aria-selected={i === 0}
+            onClick={() => {
+              const key = `load${id}`;
+
+              State.update({
+                ...state,
+                article: undefined,
+                authorId: undefined,
+                note: undefined,
+                editArticle: false,
+                currentTab: key,
+              });
+            }}
+          >
+            {title}
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
