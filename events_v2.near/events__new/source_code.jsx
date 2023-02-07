c3df74302a682cf7dfd8182478d6ce10a40c8b4c
events_v2.near <events_v2.near>
2023-02-07 12:31:07 -0500

    Update events__new by events_v2.near at 2023-02-07 12:31:07.116

--- events_v2.near/events__new/source_code.jsx
+++ events_v2.near/events__new/source_code.jsx
@@ -5,9 +5,13 @@ props.controller.setLayout('layouts:modal', {
 
 const EVENTS_CONTRACT = 'events_v2.near';
 
-const latestEvent = Near.view(EVENTS_CONTRACT, 'get_latest_event', {
-  account_id: props.__engine.accountId,
-});
+const latestEvent = props.__engine.contract.view(
+  EVENTS_CONTRACT,
+  'get_latest_event',
+  {
+    account_id: props.__engine.accountId,
+  }
+);
 if (!latestEvent) {
   // return 'Loading';
 }
