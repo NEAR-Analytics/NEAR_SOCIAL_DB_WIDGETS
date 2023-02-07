b0a27da3cbc0f20ab2b87c07eb67b20d5c26f061
microchipgnu <microchipgnu>
2023-02-07 15:39:16 -0500

    Update ListFromCollection by microchipgnu.near at 2023-02-07 15:39:16.890

--- microchipgnu.near/ListFromCollection/source_code.jsx
+++ microchipgnu.near/ListFromCollection/source_code.jsx
@@ -1,5 +1,11 @@
 const accountId = context?.accountId;
 
+const size = "5em";
+
+State.init({
+  tokens: {},
+});
+
 const _data = fetch("https://graph.mintbase.xyz", {
   method: "POST",
   headers: {
@@ -20,10 +26,6 @@ const _data = fetch("https://graph.mintbase.xyz", {
       accountId: accountId,
     },
   }),
-});
-
-State.init({
-  tokens: {},
 });
 
 const data = _data.body.data.mb_views_nft_owned_tokens;
@@ -54,16 +56,23 @@ const removeFromCart = (token_id, nft_contract_id) => {
   });
 };
 
+const listMarket = () => {};
+
+const numTokensSelected = Object.keys(state.tokens).length;
+
 return (
   <div class="d-flex flex-column gap-2">
-    <div>{JSON.stringify(Object.keys(state.tokens).length)}</div>
+    <div>{numTokensSelected}</div>
+    <button>List for 1N</button>
     {data.map(({ token_id, nft_contract_id }) => {
       const key = `${token_id}::${nft_contract_id}`;
       const addedToken = !!state.tokens[key];
 
       return (
         <button
-          class={`p-2 ${addedToken ? "bg-primary" : "bg-black"} text-white`}
+          class={`p-2 ${
+            addedToken ? "bg-primary" : "bg-black"
+          } text-white d-flex gap-2 rounded`}
           key={key}
           onClick={() => {
             if (!addedToken) {
@@ -73,7 +82,33 @@ return (
             }
           }}
         >
-          {token_id} | {nft_contract_id}
+          <Widget
+            src="mob.near/widget/NftImage"
+            props={{
+              nft: {
+                tokenId: token_id,
+                contractId: nft_contract_id,
+              },
+              style: {
+                width: size,
+                height: size,
+                objectFit: "cover",
+                minWidth: size,
+                minHeight: size,
+                maxWidth: size,
+                maxHeight: size,
+                overflowWrap: "break-word",
+              },
+              thumbnail: "thumbnail",
+              className: "",
+              fallbackUrl:
+                "https://ipfs.near.social/ipfs/bafkreihdiy3ec4epkkx7wc4wevssruen6b7f3oep5ylicnpnyyqzayvcry",
+            }}
+          />
+          <div>
+            <p>{token_id}</p>
+            <p>{nft_contract_id}</p>
+          </div>
         </button>
       );
     })}
