797e46fac527b6c48c5358abc31e7c299ff01a40
rubycop.near <rubycop.near>
2023-02-07 00:33:15 -0500

    Update NftVotingLeaderboard by rubycop.near at 2023-02-07 00:33:15.249

--- rubycop.near/NftVotingLeaderboard/source_code.jsx
+++ rubycop.near/NftVotingLeaderboard/source_code.jsx
@@ -5,22 +5,37 @@ stats = [...new Map(stats.map((item) => [item.token_id, item])).values()];
 return (
   <>
     <h3>NFT Leaderboard</h3>
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Action</th>
-          <th scope="col">Preview</th>
-          <th scope="col">NFT token id</th>
-        </tr>
-      </thead>
-      <tbody>
-        {stats
-          .sort((a, b) => b.rating - a.rating)
-          .map(({ account_id, contract_id, token_id, rating }, i) => (
-            <tr className="align-middle" key={i}>
-              <td>{i + 1}</td>
-              <td className="text-center">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+      {stats
+        .sort((a, b) => b.rating - a.rating)
+        .map(({ account_id, contract_id, token_id, rating }, i) => (
+          <div class="col">
+            <div class="card h-100">
+              <Widget
+                key={i}
+                src={"mob.near/widget/NftImage"}
+                props={{
+                  tokenId: token_id,
+                  contractId: contract_id,
+                  style: {
+                    height: "15em",
+                    objectFit: "cover",
+                    maxHeight: "15em",
+                    overflowWrap: "break-word",
+                  },
+                  className: "card-img-top",
+                }}
+              />
+
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  <a
+                    href={`#mob.near/widget/NftImage?tokenId=${token_id}&contractId=${contract_id}`}
+                  >
+                    {contract_id}:{token_id}
+                  </a>
+                </p>
                 <Widget
                   src="rubycop.near/widget/NftVotingButton"
                   props={{
@@ -30,38 +45,10 @@ return (
                     rating,
                   }}
                 />
-              </td>
-              <td>
-                <Widget
-                  key={i}
-                  src={"mob.near/widget/NftImage"}
-                  props={{
-                    tokenId: token_id,
-                    contractId: contract_id,
-                    style: {
-                      width: "5em",
-                      height: "5em",
-                      objectFit: "cover",
-                      minWidth: "5em",
-                      minHeight: "5em",
-                      maxWidth: "5em",
-                      maxHeight: "5em",
-                      overflowWrap: "break-word",
-                    },
-                    className: "img-thumbnail",
-                  }}
-                />
-              </td>
-              <td>
-                <a
-                  href={`#mob.near/widget/NftImage?tokenId=${token_id}&contractId=${contract_id}`}
-                >
-                  {contract_id}:{token_id}
-                </a>
-              </td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
   </>
 );
