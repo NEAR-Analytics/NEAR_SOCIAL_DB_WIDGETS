ce4841fba4ec699c341519761c42adf033eaa6c3
leomanza <leomanza>
2023-02-07 15:45:39 -0500

    Update MetaStakingDashComponent by manzanal.near at 2023-02-07 15:45:39.251

--- manzanal.near/MetaStakingDashComponent/source_code.jsx
+++ manzanal.near/MetaStakingDashComponent/source_code.jsx
@@ -1,21 +1,22 @@
 const near_staked = props.near_staked;
 const near_assigned = props.near_assigned;
 
-const YoctoToNear = (amountYocto) =>
-  new Big(amountYocto).div(new Big(10).pow(24)).toFixed(2);
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
 
 return (
-  <div className="text-bg-light rounded-2 p-1">
+  <div className="text-bg-light rounded-4 p-2">
     {data !== null ? (
       <p>
         <div class="d-flex justify-content-around clearfix">
-          <div class="p-1">
-            <div>NEAR STAKED AT METAPOOL</div>
-            <h2>{near_staked.toFixed(2)} Ⓝ</h2>
+          <div class="p-3">
+            <div>STAKED AT METAPOOL</div>
+            <h2>{numberWithCommas(near_staked.toFixed(0))} Ⓝ</h2>
           </div>
-          <div class="p-1">
-            <div>NEAR ASSIGNED BY VOTES</div>
-            <h2>{near_assigned.toFixed(2)} Ⓝ</h2>
+          <div class="p-3">
+            <div>ASSIGNED BY VOTES</div>
+            <h2>{numberWithCommas(near_assigned.toFixed(0))} Ⓝ</h2>
           </div>
         </div>
       </p>
