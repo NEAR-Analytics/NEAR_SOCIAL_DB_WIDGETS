b6482bfe562da68f30669d1e0d563616e3efaaaa
microchipgnu <microchipgnu>
2023-02-06 20:07:38 -0500

    Update PostShortcut by microchipgnu.near at 2023-02-06 20:07:38.686

--- microchipgnu.near/PostShortcut/source_code.jsx
+++ microchipgnu.near/PostShortcut/source_code.jsx
@@ -1,1 +1,37 @@
+const text = props?.text || "";
+
+const data = {
+  post: {
+    main: JSON.stringify({
+      type: "md",
+      text: text,
+    }),
+  },
+  index: {
+    post: JSON.stringify({
+      key: "main",
+      value: {
+        type: "md",
+      },
+    }),
+  },
+};
+
+if (text) {
+  const gas = 200000000000000;
+  const deposit = 0;
+
+  Near.call([
+    {
+      contractName: "social.near",
+      methodName: "set",
+      args: {
+        data,
+      },
+      gas: gas,
+      deposit: deposit,
+    },
+  ]);
+}
+
 return <div>Hello World</div>;
