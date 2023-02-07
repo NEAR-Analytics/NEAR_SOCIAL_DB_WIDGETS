8643a3271bcbdbfa369d36e8885cecc2bbdfb37c
events_v2.near <events_v2.near>
2023-02-07 12:07:35 -0500

    Update layouts__loading by events_v2.near at 2023-02-07 12:07:35.504

--- events_v2.near/layouts__loading/source_code.jsx
+++ events_v2.near/layouts__loading/source_code.jsx
@@ -44,9 +44,9 @@ return (
     <AlertContent>
       <div>
         <div>
-          <Dot delay={'0s'} />
-          <Dot delay={'0.2s'} />
-          <Dot delay={'0.4s'} />
+          <Dot delay={'0s'}>1</Dot>
+          <Dot delay={'0.2s'}>3</Dot>
+          <Dot delay={'0.4s'}>5</Dot>
         </div>
         {props.__engine.renderComponent(
           props.component.name,
