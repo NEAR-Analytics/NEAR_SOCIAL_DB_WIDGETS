90c726b598677d8043b8617bde6874093ed81424
events_v2.near <events_v2.near>
2023-02-08 18:24:45 -0500

    Update app__frame by events_v2.near at 2023-02-08 18:24:45.816

--- events_v2.near/app__frame/source_code.jsx
+++ events_v2.near/app__frame/source_code.jsx
@@ -313,7 +313,7 @@ const Components = {
     font-weight: 400;
     margin: 0;
     padding: ${({ small }) => (small ? '0.5vw' : '1vw')};
-    height: 42px;
+    height: 44px;
     flex-grow: 0;
     flex-shrink: 0;
     width: 100%;
