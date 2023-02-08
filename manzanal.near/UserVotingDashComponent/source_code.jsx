8c0460ee7f0b22541a6b3ebc6c3eaf338fe361ce
leomanza <leomanza>
2023-02-08 15:43:59 -0500

    Update UserVotingDashComponent by manzanal.near at 2023-02-08 15:43:59.572

--- manzanal.near/UserVotingDashComponent/source_code.jsx
+++ manzanal.near/UserVotingDashComponent/source_code.jsx
@@ -28,15 +28,15 @@ let data = {
 };
 
 return (
-  <div class="card w-50">
+  <div class="card">
     <div class="card-body p-3">
       <h5 class="card-title">Your Voting Power Balance</h5>
       <div class="row">
-        <div class="col-sm-4 p-3">
+        <div class="col-md-6 p-3">
           <h6 class="card-subtitle mb-2 text-muted">Available</h6>
           <h6 class="text-success">{numberWithCommas(data.available)} VP</h6>
         </div>
-        <div class="col-sm-4 p-3">
+        <div class="col-md-6 p-3">
           <h6 class="card-subtitle mb-2 text-muted">In Use</h6>
           <h6 class="text-warning">{numberWithCommas(data.inUse)} VP</h6>
         </div>
