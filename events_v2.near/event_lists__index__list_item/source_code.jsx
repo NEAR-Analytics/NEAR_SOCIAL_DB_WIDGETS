29d01c9ae17ab2de782ed1baf17933d0ae4905b7
events_v2.near <events_v2.near>
2023-02-08 17:54:00 -0500

    Update event_lists__index__list_item by events_v2.near at 2023-02-08 17:54:00.493

--- events_v2.near/event_lists__index__list_item/source_code.jsx
+++ events_v2.near/event_lists__index__list_item/source_code.jsx
@@ -63,7 +63,7 @@ const CardBody = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  padding: 0;
+  padding: 0 20px;
   width: 66%;
   height: 100%;
   overflow-y: hidden;
