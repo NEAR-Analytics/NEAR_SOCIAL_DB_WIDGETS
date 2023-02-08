285e4cf4c9505281e6be292b000c1d9948be3d0d
leomanza <leomanza>
2023-02-08 14:49:31 -0500

    Update ValidatorStakingVoteAction by manzanal.near at 2023-02-08 14:49:31.809

--- manzanal.near/ValidatorStakingVoteAction/source_code.jsx
+++ manzanal.near/ValidatorStakingVoteAction/source_code.jsx
@@ -8,12 +8,15 @@ const GET_VP_METHOD = "get_available_voting_power";
 const VOTE_METHOD = "vote";
 const VOTE_CONTRACT_ADDRESS_ARG = "metastaking.app";
 
+const YoctoToNear = (amountYocto) =>
+  new Big(amountYocto).div(new Big(10).pow(24)).toFixed(2);
+
 const getVotingPowerBalance = () => {
   const balanceYocto = Near.view(CONTRACT_ID, GET_VP_METHOD, {
     voter_id: context.accountId,
   });
-  const _balance = parseInt(balanceYocto) / 1000000000000000000000000;
-  return _balance.toFixed(0);
+  const _balance = YoctoToNear(balanceYocto);
+  return _balance;
 };
 
 initState({
@@ -55,53 +58,75 @@ const handleBlur = (e) => {
 
 return (
   <div>
-    <h1>Vote&nbsp;for&nbsp;{state.poolId}</h1>
-    <p>Validator: {state.poolId}</p>
-    <p>Balance: {state.balance} Voting Power</p>
-    <p>
-      Amount (Voting Power):
-      <div class="input-group">
-        <input
-          type="number"
-          value={state.amount}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          class="form-control"
-          placeholder="Amount Voting Power"
-          aria-label="amount-voting-power"
-          aria-describedby="btnGroupAddon"
-        />
-        <div class="input-group-prepend">
-          <button
-            class="input-group-text"
-            id="btnGroupAddon"
-            className={`btn ${
-              context.loading ? "btn-outline-dark" : "btn-secondary"
-            }`}
-            onClick={onUseMaxClick}
-          >
-            Use MAX
-          </button>
+    <div class="d-flex flex-row justify-content-between clearfix">
+      <div class="col-sm-6">
+        <div class="bs-calltoaction bs-calltoaction-default">
+          <div class="col-md-9 cta-contents text-primary">
+            <h5>{state.poolId}</h5>
+          </div>
+        </div>
+        <a
+          disabled={context.loading}
+          className={`btn ${
+            context.loading ? "btn-outline-dark" : "btn-secondary"
+          }`}
+          href={`/#/manzanal.near/widget/ValidatorsStakingVote`}
+          role="button"
+        >
+          Back&nbsp;to&nbsp;validators
+        </a>
+      </div>
+    </div>
+    <div class="d-flex flex-row mt-3">
+      <div class="col-sm-6">
+        <div class="d-flex justify-content-start">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Vote</h5>
+              <p>How much Voting Power do you want to asign to this node?</p>
+              <div class="input-group input-group-md my-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">VP</span>
+                </div>
+                <input
+                  type="number"
+                  value={state.amount}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  class="form-control"
+                  placeholder="Amount Voting Power"
+                  aria-label="amount-voting-power"
+                  aria-describedby="btnGroupAddon"
+                />
+                <div class="input-group-append ">
+                  <button
+                    class="btn btn-outline-secondary"
+                    id="btnGroupAddon"
+                    className={`btn ${
+                      context.loading ? "btn-outline-dark" : "btn-secondary"
+                    }`}
+                    onClick={onUseMaxClick}
+                  >
+                    Use MAX
+                  </button>
+                </div>
+              </div>
+              <button
+                disabled={context.loading || !state.valid}
+                className={`btn ${
+                  context.loading ? "btn-outline-dark" : "btn-primary"
+                }`}
+                onClick={onVoteClick}
+              >
+                Vote this validator
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </p>
-
-    <button
-      disabled={context.loading || !state.valid}
-      className={`btn ${context.loading ? "btn-outline-dark" : "btn-primary"}`}
-      onClick={onVoteClick}
-    >
-      Vote
-    </button>
-    <a
-      disabled={context.loading}
-      className={`btn ${
-        context.loading ? "btn-outline-dark" : "btn-secondary"
-      }`}
-      href={`/#/manzanal.near/widget/ValidatorsStakingVote`}
-      role="button"
-    >
-      Back&nbsp;to&nbsp;Validators
-    </a>
+      <div class="col-md-6 w-100">
+        <Widget src="manzanal.near/widget/UserVotingDashComponent" props={{}} />
+      </div>
+    </div>
   </div>
 );
