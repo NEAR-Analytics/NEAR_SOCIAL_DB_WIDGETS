a22a6a463d24461c5c7c77434c1b629f732b9587
calebjacob <calebjacob>
2023-02-08 17:48:51 -0500

    Update LatestApplications by calebjacob.near at 2023-02-08 17:48:51.084

---
+++ calebjacob.near/LatestApplications/source_code.jsx
@@ -0,0 +1,13 @@
+const H2 = styled.h2`
+  font-size: 19px;
+  line-height: 22px;
+  color: #11181C;
+  margin: 0 0 25px;
+`;
+
+return (
+  <>
+    <H2>Latest Applications</H2>
+    <Widget src="calebjacob.near/widget/ApplicationCard" />
+  </>
+);
