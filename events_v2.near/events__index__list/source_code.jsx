18672e14f220003a5c741020b7f4a8d2c55eb16d
events_v2.near <events_v2.near>
2023-02-08 21:59:14 -0500

    Update events__index__list by events_v2.near at 2023-02-08 21:59:14.476

--- events_v2.near/events__index__list/source_code.jsx
+++ events_v2.near/events__index__list/source_code.jsx
@@ -1,3 +1,5 @@
+const ANIMATION_DELAY = 300;
+
 let events = props.events || [];
 if (!events) {
   return '';
@@ -12,10 +14,13 @@ const GridContainer = props.__engine.Components.GridContainer;
 
 return (
   <GridContainer itemWidth={'380px'}>
-    {events.map((event) => {
+    {events.map((event, idx) => {
       return (
         <div key={event.event_id}>
-          {props.__engine.renderComponent('index.list_item', { event })}
+          {props.__engine.renderComponent('index.list_item', {
+            event,
+            delay: `${(idx + 1) * ANIMATION_DELAY}ms`,
+          })}
         </div>
       );
     })}
