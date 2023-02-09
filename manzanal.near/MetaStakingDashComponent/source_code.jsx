2e416b6ff2b8f0e9a755a43f02a8887f12a3c340
leomanza <leomanza>
2023-02-09 12:47:13 -0500

    Update MetaStakingDashComponent by manzanal.near at 2023-02-09 12:47:13.140

--- manzanal.near/MetaStakingDashComponent/source_code.jsx
+++ manzanal.near/MetaStakingDashComponent/source_code.jsx
@@ -10,13 +10,13 @@ return (
     <div class="card-body p-3">
       <h5 class="card-title">Meta Pool Staking</h5>
       <div class="row">
-        <div class="col-sm-6 p-3">
+        <div class="col-md-6 p-3">
           <h6 class="card-subtitle mb-2 text-muted">Staked at Meta Pool</h6>
           <h6 class="text-primary fs-5">
             {numberWithCommas(near_staked.toFixed(0))} Ⓝ
           </h6>
         </div>
-        <div class="col-sm-6 p-3">
+        <div class="col-md-6 p-3">
           <h6 class="card-subtitle mb-2 text-muted">Assigned by Votes</h6>
           <h6 class="text-primary fs-5">
             {numberWithCommas(near_assigned.toFixed(0))} Ⓝ
