094c9bf1d89d5c26311370230c1f036bc532c408
eugenewolf507.near <eugenewolf507.near>
2023-02-09 17:56:51 -0500

    Update WikiOnSocialDB_OneArticle by eugenewolf507.near at 2023-02-09 17:56:51.632

--- eugenewolf507.near/WikiOnSocialDB_OneArticle/source_code.jsx
+++ eugenewolf507.near/WikiOnSocialDB_OneArticle/source_code.jsx
@@ -100,7 +100,7 @@ return (
               <Widget
                 src="mob.near/widget/MarkdownEditorIframe"
                 props={{
-                  initialText: state.article,
+                  initialText: "state.article",
                   onChange: (note) => State.update({ note }),
                 }}
               />
