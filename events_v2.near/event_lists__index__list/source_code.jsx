70eee133b9740ae6f8e8dd363e761e1d4517691f
events_v2.near <events_v2.near>
2023-02-09 17:07:04 -0500

    Update event_lists__index__list by events_v2.near at 2023-02-09 17:07:04.084

--- events_v2.near/event_lists__index__list/source_code.jsx
+++ events_v2.near/event_lists__index__list/source_code.jsx
@@ -11,14 +11,4 @@ props.controller.setLayout(`index.list.${props.layout}`, {
   items: event_lists,
 });
 
-return (
-  <>
-    {event_lists.map((event_list, idx) => {
-      return (
-        <ListWrapper key={`${idx}-${event_list.event_list_id}`}>
-          {props.__engine.renderComponent('index.list_item', { event_list })}
-        </ListWrapper>
-      );
-    })}
-  </>
-);
+return <></>;
