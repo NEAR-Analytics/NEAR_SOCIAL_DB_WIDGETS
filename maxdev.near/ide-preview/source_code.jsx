6010c8167f89c8931d1d5744311fe29a548fd637
maxhr <maxhr>
2023-02-09 11:02:24 -0500

    Update ide-preview by maxdev.near at 2023-02-09 11:02:24.798

--- maxdev.near/ide-preview/source_code.jsx
+++ maxdev.near/ide-preview/source_code.jsx
@@ -1,12 +1,19 @@
 const { idePreview } = props;
-const { code, config, props: p } = idePreview;
+const {
+  code,
+  config,
+  props: p,
+} = typeof idePreview === "string" ? JSON.parse(idePreview) : idePreview;
+console.log("preview props", code, config, p);
 if (typeof code !== "string") {
   throw {
     message: "No code supplied to perview. Props: " + JSON.stringify(props),
   };
 }
 if (code.trim() === "") {
-  throw { message: "Preview code is empty. Props: " + JSON.stringify(props) };
+  throw {
+    message: "Preview code is empty. Props: " + JSON.stringify(props),
+  };
 }
 
 return <Widget code={code} props={p || {}} config={config || {}} />;
