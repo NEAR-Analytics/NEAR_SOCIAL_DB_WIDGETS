6d771310a8e557823606b7364dbf5a6a049ebf4f
events_v2.near <events_v2.near>
2023-02-09 15:31:54 -0500

    Update event_lists__index__list_item by events_v2.near at 2023-02-09 15:31:54.837

--- events_v2.near/event_lists__index__list_item/source_code.jsx
+++ events_v2.near/event_lists__index__list_item/source_code.jsx
@@ -85,6 +85,10 @@ const EventTileWrapper = styled.div`
   flex-grow & > * {
     height: auto;
   }
+
+  @media (max-width: 768px) {
+    max-width: 25%;
+  }
 `;
 
 const TextButton = styled.button`
