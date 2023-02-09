a6d0b93149a0d99bb1f17f284deddc03ced842ce
events_v2.near <events_v2.near>
2023-02-09 17:24:48 -0500

    Update event_lists__index__list__item by events_v2.near at 2023-02-09 17:24:48.398

--- events_v2.near/event_lists__index__list__item/source_code.jsx
+++ events_v2.near/event_lists__index__list__item/source_code.jsx
@@ -1,9 +1,7 @@
-const { event_list } = props;
+const { event_list, include_events } = props;
 if (!event_list) {
   return props.__engine.helpers.propIsRequiredMessage('event_list');
 }
-
-const { include_events } = props;
 
 if (include_events) {
   return props.__engine.renderComponent('index.list.item.with-events', {
