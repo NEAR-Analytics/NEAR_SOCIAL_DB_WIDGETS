08214a2270a7f7923b45190d52e4ac44b7716575
events_v2.near <events_v2.near>
2023-02-08 21:21:20 -0500

    Update event_lists__show by events_v2.near at 2023-02-08 21:21:20.178

--- events_v2.near/event_lists__show/source_code.jsx
+++ events_v2.near/event_lists__show/source_code.jsx
@@ -136,10 +136,14 @@ return (
     <Container>
       <GridContainer itemWidth={'300px'}>
         {state.event_list.events.map(({ event }) => {
-          return props.__engine.renderComponent(
-            'index.list_item',
-            { event: event },
-            { appName: 'events' }
+          return (
+            <div>
+              {props.__engine.renderComponent(
+                'index.list_item',
+                { event: event },
+                { appName: 'events' }
+              )}
+            </div>
           );
         })}
       </GridContainer>
