183e534b775ce50802662d0d267e93014ab627ad
nearmax <nearmax>
2023-02-08 23:04:23 -0500

    Update TeamsList by nearmax.near at 2023-02-08 23:04:23.140

--- nearmax.near/TeamsList/source_code.jsx
+++ nearmax.near/TeamsList/source_code.jsx
@@ -1,7 +1,12 @@
 const ownerId = "nearmax.near";
 
-const access_info = Near.view("devgovgigs.near", "get_access_control_info");
-const root_members = Near.view("devgovgigs.near", "get_root_members");
+const access_info =
+  Near.view("devgovgigs.near", "get_access_control_info") ?? null;
+const root_members = Near.view("devgovgigs.near", "get_root_members") ?? null;
+
+if (!access_info || !root_members) {
+  return <div>Loading...</div>;
+}
 
 return (
   <div>
