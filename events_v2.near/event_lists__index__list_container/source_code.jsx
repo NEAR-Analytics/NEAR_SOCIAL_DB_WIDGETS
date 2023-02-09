98c2ee93feada49e52b31603ccfafa8edd00e7de
events_v2.near <events_v2.near>
2023-02-09 16:58:56 -0500

    Update event_lists__index__list_container by events_v2.near at 2023-02-09 16:58:56.297

--- events_v2.near/event_lists__index__list_container/source_code.jsx
+++ events_v2.near/event_lists__index__list_container/source_code.jsx
@@ -4,8 +4,11 @@ const EVENTS_CONTRACT = 'events_v2.near';
 // if no accountID is provided, all event_lists are displayed
 const forAccountId = props.forAccountId;
 
+const showAllEvents = forAccountId === undefined;
+const listLayout = showAllEvents ? 'big' : 'small';
+
 let event_lists = [];
-if (forAccountId === undefined) {
+if (showAllEvents) {
   event_lists = props.__engine.contract.view(
     EVENTS_CONTRACT,
     'get_all_event_lists'
@@ -29,6 +32,6 @@ const header = props.header;
 return (
   <>
     {header ? <ContainerHeader>{header}</ContainerHeader> : null}
-    {props.__engine.renderComponent('index.list', { event_lists })}
+    {props.__engine.renderComponent('index.list', { event_lists, listLayout })}
   </>
 );
