5f1765ba4f69666254777466e5a8d9cd5d3ce9e0
events_v2.near <events_v2.near>
2023-02-09 17:37:31 -0500

    Update event_lists__index__list__item__default by events_v2.near at 2023-02-09 17:37:31.537

--- events_v2.near/event_lists__index__list__item__default/source_code.jsx
+++ events_v2.near/event_lists__index__list__item__default/source_code.jsx
@@ -3,4 +3,23 @@ if (!event_list) {
   return props.__engine.helpers.propIsRequiredMessage('event_list');
 }
 
-return <>{event_list.name}</>;
+const TextButton = props.__engine.Components.TextButton;
+
+return (
+  <>
+    <TextButton
+      onClick={() => {
+        props.__engine.navigate(`/event_lists/${event_list.event_list_id}`);
+      }}
+      role="link"
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          props.__engine.navigate(`/event_lists/${event_list.event_list_id}`);
+        }
+      }}
+      tabIndex={0}
+    >
+      {event_list.name}
+    </TextButton>
+  </>
+);
