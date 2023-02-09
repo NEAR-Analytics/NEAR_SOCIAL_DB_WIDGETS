6f7c23d3ad23e727dd41754df009d831a6814a0a
calebjacob <calebjacob>
2023-02-09 17:49:12 -0500

    Update Feed by calebjacob.near at 2023-02-09 17:49:12.486

--- calebjacob.near/Feed/source_code.jsx
+++ calebjacob.near/Feed/source_code.jsx
@@ -12,7 +12,7 @@ const renderItem = (a) =>
   a.value.type === "md" && (
     <div key={JSON.stringify(a)}>
       <Widget
-        src="calebjacob.near/widget/FeedPost"
+        src="calebjacob.near/widget/Feed.Post"
         props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
       />
     </div>
