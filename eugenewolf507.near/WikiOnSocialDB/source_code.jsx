76fd6bfec761472a1149f492f145a4456f110f69
eugenewolf507.near <eugenewolf507.near>
2023-02-09 18:23:16 -0500

    Update WikiOnSocialDB by eugenewolf507.near at 2023-02-09 18:23:16.008

--- eugenewolf507.near/WikiOnSocialDB/source_code.jsx
+++ eugenewolf507.near/WikiOnSocialDB/source_code.jsx
@@ -9,52 +9,6 @@ if (profile === null) {
   return "Loading";
 }
 
-const wikiTestData = Social.get(`*/${addressForArticles}/articles/**`, "final");
-const wikiTestArr = wikiTestData && Object.values(wikiTestData);
-const resultArticles =
-  wikiTestArr &&
-  wikiTestArr.reduce(
-    (acc, account) =>
-      acc.concat(Object.values(account[addressForArticles].articles)),
-    []
-  );
-
-resultArticles.length &&
-  resultArticles.sort((a, b) => {
-    return Number(b.timeLastEdit) - Number(a.timeLastEdit);
-  });
-
-const filteredArticles =
-  resultArticles.length &&
-  resultArticles.reduce((acc, article) => {
-    if (!acc.some(({ articleId }) => articleId === article.articleId)) {
-      return [...acc, article];
-    } else {
-      return acc;
-    }
-  }, []);
-
-State.init({
-  currentTab: "loadarticles",
-});
-
-const description = profile.description;
-
-const handleAuthor = (e, authorId) => {
-  State.update({ ...state, article: undefined, authorId });
-};
-
-const getDate = (timestamp) => {
-  const date = new Date(Number(timestamp));
-  return date.toDateString();
-};
-
-const getDateLastEdit = (timestamp) => {
-  const date = new Date(Number(timestamp));
-  const dateString = `${date.toLocaleDateString()} / ${date.toLocaleTimeString()}`;
-  return dateString;
-};
-
 return (
   <>
     <Widget
@@ -67,8 +21,6 @@ return (
         props={{
           filteredArticles,
           getDateLastEdit,
-          handleArticle,
-          state,
         }}
       />
     </div>
