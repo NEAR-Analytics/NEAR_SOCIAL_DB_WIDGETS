bc712046579a0a48763188b90a8784ab455d2251
calebjacob <calebjacob>
2023-02-08 22:54:14 -0500

    Update Activity by calebjacob.near at 2023-02-08 22:54:14.581

--- calebjacob.near/Activity/source_code.jsx
+++ calebjacob.near/Activity/source_code.jsx
@@ -1,6 +1,7 @@
 const Wrapper = styled.div`
   border-left: 1px solid #ECEEF0;
   border-right: 1px solid #ECEEF0;
+  padding-top: 24px;
 `;
 
 const H2 = styled.h2`
@@ -8,12 +9,20 @@ const H2 = styled.h2`
   line-height: 22px;
   color: #11181C;
   margin: 0 0 25px;
+  padding: 0 24px;
+`;
+
+const CreatePostWrapper = styled.div`
+  border-top: 1px solid #ECEEF0;
+  border-bottom: 1px solid #ECEEF0;
 `;
 
 return (
   <Wrapper>
     <H2>Activity</H2>
 
-    <Widget src="calebjacob.near/widget/CreatePost" />
+    <CreatePostWrapper>
+      <Widget src="calebjacob.near/widget/CreatePost" />
+    </CreatePostWrapper>
   </Wrapper>
 );
