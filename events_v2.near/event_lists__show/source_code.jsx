64597e5d230ba1a56b130f881e64517937b33acf
events_v2.near <events_v2.near>
2023-02-09 16:39:12 -0500

    Update event_lists__show by events_v2.near at 2023-02-09 16:39:12.255

--- events_v2.near/event_lists__show/source_code.jsx
+++ events_v2.near/event_lists__show/source_code.jsx
@@ -62,6 +62,7 @@ const InfoBar = props.__engine.Components.InfoBar;
 const InfoBarItem = props.__engine.Components.InfoBarItem;
 const InfoBarLink = props.__engine.Components.InfoBarLink;
 const GridContainer = props.__engine.Components.GridContainer;
+const GridItemWrapper = props.__engine.Components.GridItemWrapper;
 
 function removeEventList() {
   const contract = EVENTS_CONTRACT;
@@ -139,13 +140,13 @@ return (
       <GridContainer itemWidth={'300px'} itemMaxWidth={'300px'}>
         {state.event_list.events.map(({ event }, idx) => {
           return (
-            <div key={`${idx}-${event.id}`}>
+            <GridItemWrapper key={`${idx}-${event.id}`}>
               {props.__engine.renderComponent(
                 'index.list_item',
                 { event: event },
                 { appName: 'events' }
               )}
-            </div>
+            </GridItemWrapper>
           );
         })}
       </GridContainer>
