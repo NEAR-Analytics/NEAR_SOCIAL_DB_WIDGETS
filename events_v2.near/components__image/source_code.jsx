a5eaeb0a94c9f05b357dcc48e09cc1eaa5bbe034
events_v2.near <events_v2.near>
2023-02-08 19:43:27 -0500

    Update components__image by events_v2.near at 2023-02-08 19:43:27.427

--- events_v2.near/components__image/source_code.jsx
+++ events_v2.near/components__image/source_code.jsx
@@ -1,52 +1,36 @@
 let event = props.event || null;
-const url = props.url
+const url = props.url;
 
-if(!url){
+if (!url) {
   return props.__engine.helpers.propIsRequiredMessage('url');
 }
 
 if (!state) {
   State.init({ loaded: false, src: null });
-  return
+  return;
 }
-
-
-
-
-      const img = new Image();
-      img.src = `https://ipfs.near.social/ipfs/${image.url.cid}`;
-      img.onload = () => {
-        src = img.src;
-      };
-        <
-          src={src}
-          key={image.cid}
-          alt={image.url.cid}
-          style={{
-            width: '100%',
-            height: '100%',
-            maxWidth: '100%',
-            boxSizing: 'border-box',
-            objectFit: 'cover',
-            transform: `translateX(-${
-              (state.index * 100) / displayImages.length
-            }%)`,
-            transition: 'transform 0.5s',
-          }}
-        />
 
 return (
   <>
     {!state.loaded && (
-      <img src={url} onLoad={()=>{
-        State.update({loaded: true, src: url})
-      }} style={{display: hidden}} />
+      <img
+        src={url}
+        onLoad={() => {
+          State.update({ loaded: true, src: url });
+        }}
+        style={{ display: hidden }}
+      />
     )}
 
     {state.loaded && (
-      <img 
+      <img
         src={state.src}
-        style={{display: 'block', width: '100%', height: '100%', objectFit: 'cover'}}
+        style={{
+          display: 'block',
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+        }}
       />
     )}
   </>
