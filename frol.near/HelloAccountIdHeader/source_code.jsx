41435662e2960a58b8a7e37853b2412b38fe3677
frol <frol>
2023-02-09 17:00:31 -0500

    Update HelloAccountIdHeader by frol.near at 2023-02-09 17:00:31.758

---
+++ frol.near/HelloAccountIdHeader/source_code.jsx
@@ -0,0 +1,12 @@
+const accountId = context.accountId;
+const widgetSrc = context.widgetSrc;
+
+if (context.loading) {
+  return "Loading";
+}
+
+if (!accountId) {
+  return "Please sign in with NEAR wallet to use this widget";
+}
+
+return <h1>{`Hello, ${accountId}! From ${widgetSrc}`}</h1>;
