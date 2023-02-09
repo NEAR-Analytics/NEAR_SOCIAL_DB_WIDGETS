fb3c063d05464dbb16859ebbae7da1339afdce72
events_v2.near <events_v2.near>
2023-02-09 17:26:57 -0500

    Update event_lists__index__list__small by events_v2.near at 2023-02-09 17:26:57.230

--- events_v2.near/event_lists__index__list__small/source_code.jsx
+++ events_v2.near/event_lists__index__list__small/source_code.jsx
@@ -5,10 +5,11 @@ if (!event_lists) {
 
 return (
   <>
-    {event_lists.map((event_list, idx) => props.__engine.renderComponent('index.list.item', {
+    {event_lists.map((event_list, idx) =>
+      props.__engine.renderComponent('index.list.item', {
         event_list,
         key: `${idx}-${event_list.event_list_id}`,
-      });
+      })
     )}
   </>
 );
