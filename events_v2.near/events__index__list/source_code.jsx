7c7a12de87b69a100e0c4b8140c575b7fd19a33a
events_v2.near <events_v2.near>
2023-02-09 14:25:26 -0500

    Update events__index__list by events_v2.near at 2023-02-09 14:25:26.397

--- events_v2.near/events__index__list/source_code.jsx
+++ events_v2.near/events__index__list/source_code.jsx
@@ -1,5 +1,3 @@
-const ANIMATION_DELAY = 50;
-
 let events = props.events || [];
 if (!events) {
   return '';
@@ -10,22 +8,36 @@ if (events.length === 0) {
   return 'No events found';
 }
 
-const FadeIn = props.__engine.Components.FadeIn;
-const GridContainer = props.__engine.Components.GridContainer;
+const IndexList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: stretch;
+  justify-content: flex-start;
+
+  width: auto;
+  margin-left: -20px;
+  margin-right: -20px;
+
+  & > * {
+    margin: 20px 20px;
+    max-width: 520px;
+    min-width: 320px;
+    width: 240px;
+    flex-grow: 3;
+    flex-shrink: 3;
+  }
+`;
 
 return (
-  <GridContainer itemWidth={'380px'}>
-    {events.map((event, idx) => {
-      const delay = `${(idx + 2) * ANIMATION_DELAY}ms`;
-      return (
-        <FadeIn key={event.event_id} delay={delay}>
-          {props.__engine.renderComponent('index.list_item', { event, delay })}
-        </FadeIn>
-      );
+  <IndexList>
+    {events.map((event) => {
+      return props.__engine.renderComponent('index.list_item', {
+        event,
+        key: event.event_id,
+      });
     })}
-
     <div>{/* spacer */}</div>
     <div>{/* spacer */}</div>
     <div>{/* spacer */}</div>
-  </GridContainer>
+  </IndexList>
 );
