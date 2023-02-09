41edafe89b8be8f11b4a66295a3b1f1c0e07cf78
leomanza <leomanza>
2023-02-08 20:54:10 -0500

    Update ValidatorsCount by manzanal.near at 2023-02-08 20:54:10.340

--- manzanal.near/ValidatorsCount/source_code.jsx
+++ manzanal.near/ValidatorsCount/source_code.jsx
@@ -3,7 +3,7 @@ return (
   <div class="card w-25">
     <div class="card-body p-3">
       <h5 class="card-title">Validators</h5>
-      <p class="text-primary">
+      <p class="text-primary fs-5">
         <i class="bi-union" />
         {count}
       </p>
