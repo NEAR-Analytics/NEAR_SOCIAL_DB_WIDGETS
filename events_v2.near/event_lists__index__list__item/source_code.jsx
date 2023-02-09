8e543ca5394bb9d0e331741413b7c59b466b12ea
events_v2.near <events_v2.near>
2023-02-09 17:25:25 -0500

    Update event_lists__index__list__item by events_v2.near at 2023-02-09 17:25:25.205

--- events_v2.near/event_lists__index__list__item/source_code.jsx
+++ events_v2.near/event_lists__index__list__item/source_code.jsx
@@ -3,12 +3,10 @@ if (!event_list) {
   return props.__engine.helpers.propIsRequiredMessage('event_list');
 }
 
-if (include_events) {
-  return props.__engine.renderComponent('index.list.item.with-events', {
-    event_list,
-  });
-}
+const widget = include_events
+  ? 'index.list.item.with-events'
+  : 'index.list.item.default';
 
-return props.__engine.renderComponent('index.list.item.default', {
+return props.__engine.renderComponent(widget, {
   event_list,
 });
