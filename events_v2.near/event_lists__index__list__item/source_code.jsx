c812f892d6ce0b91927949a95fc8ef7a47ce13d2
events_v2.near <events_v2.near>
2023-02-09 17:21:48 -0500

    Update event_lists__index__list__item by events_v2.near at 2023-02-09 17:21:48.660

--- events_v2.near/event_lists__index__list__item/source_code.jsx
+++ events_v2.near/event_lists__index__list__item/source_code.jsx
@@ -1,13 +1,6 @@
-const EVENTS_CONTRACT = 'events_v2.near';
-const EVENTS_LIMIT = 5;
-const DESCRIPTION_MAX_LENGTH = 200;
-const ANIMATION_DELAY = 300;
-
-const include_events = props.include_events || false;
-
 const event_list = props.event_list || null;
 if (!event_list) {
   return props.__engine.helpers.propIsRequiredMessage('event_list');
 }
 
-return <>asdfjha s;dkf</>;
+return <>{event_list.name}</>;
