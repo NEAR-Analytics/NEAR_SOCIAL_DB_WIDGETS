fa8a4728de3b891205d0c83320152fc9fa1a689c
roshaan.near <roshaan.near>
2023-02-09 23:08:39 -0500

    Update queryapi__IndexerFunctionEditor by roshaan.near at 2023-02-09 23:08:39.216

--- roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
+++ roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
@@ -20,7 +20,7 @@ const code = `
 <script>
 function TestReact(props) {
   const [value, setValue] = React.useState(props.initialText || "");
-  return React.createElement(useMonaco.default, {}
+  return React.createElement(useMonaco, {})
 }
 
 const domContainer = document.querySelector('#react-root');
