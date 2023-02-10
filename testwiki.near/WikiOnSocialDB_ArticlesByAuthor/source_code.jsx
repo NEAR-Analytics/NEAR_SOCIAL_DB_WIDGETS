96b45b9fe26faa3ae8582241bc0507a9d7f0d72f
testwiki.near <testwiki.near>
2023-02-09 19:54:05 -0500

    Update WikiOnSocialDB_ArticlesByAuthor by testwiki.near at 2023-02-09 19:54:05.486

---
+++ testwiki.near/WikiOnSocialDB_ArticlesByAuthor/source_code.jsx
@@ -0,0 +1,86 @@
+const addressForArticles = "wikiTest";
+const authorForWidget = "testwiki.near";
+const authorId = props.author;
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+if (profile === null) {
+  return "Loading";
+}
+
+const getDateLastEdit = (timestamp) => {
+  const date = new Date(Number(timestamp));
+  const dateString = `${date.toLocaleDateString()} / ${date.toLocaleTimeString()}`;
+  return dateString;
+};
+
+const wikiTestData = Social.get(`*/${addressForArticles}/articles/**`, "final");
+const wikiTestArr = wikiTestData && Object.values(wikiTestData);
+const resultArticles =
+  wikiTestArr &&
+  wikiTestArr.reduce(
+    (acc, account) =>
+      acc.concat(Object.values(account[addressForArticles].articles)),
+    []
+  );
+
+resultArticles.length &&
+  resultArticles.sort((a, b) => {
+    return Number(b.timeLastEdit) - Number(a.timeLastEdit);
+  });
+
+const filteredArticles =
+  resultArticles.length &&
+  resultArticles.reduce((acc, article) => {
+    if (acc.some(({ articleId }) => articleId === article.articleId)) {
+      return acc;
+    } else {
+      return [...acc, article];
+    }
+  }, []);
+
+const filteredArticlesByUser =
+  filteredArticles.length &&
+  filteredArticles.reduce((acc, article) => {
+    if (article.author === authorId) {
+      return [...acc, article];
+    } else {
+      return acc;
+    }
+  }, []);
+
+return (
+  <>
+    <Widget
+      src={`${authorForWidget}/widget/WikiOnSocialDB_MainNavigation`}
+      props={{ currentNavPill: "authors" }}
+    />
+    <div>
+      Author:
+      <a
+        href={`https://near.social/#/mob.near/widget/ProfilePage?accountId=${authorId}`}
+        target="_blank"
+      >
+        {authorId}
+      </a>
+      <ol>
+        {filteredArticlesByUser &&
+          filteredArticlesByUser.map((article, index) => (
+            <li key={article.articleId}>
+              <a
+                href={`#/${authorForWidget}/widget/WikiOnSocialDB_OneArticle?articleId=${article.articleId}`}
+              >
+                {article.articleId}{" "}
+                <small>
+                  (last edited:
+                  {getDateLastEdit(article.timeLastEdit)})
+                </small>
+              </a>
+            </li>
+          ))}
+      </ol>
+    </div>
+  </>
+);
