94a1733d0aae2aecea1b521451064b4c9c08b1d2
roshaan.near <roshaan.near>
2023-02-09 22:25:11 -0500

    Update queryapi__IndexerFunctionEditor by roshaan.near at 2023-02-09 22:25:11.171

--- roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
+++ roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
@@ -10,7 +10,7 @@ const code = `
 <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
 <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
 <script src="https://unpkg.com/react-markdown-editor-lite@1.3.4/lib/index.js" crossorigin></script>
-<
+<script src=
 <link rel="stylesheet" href="https://unpkg.com/react-markdown-editor-lite@1.3.4/lib/index.css" />
 
 
