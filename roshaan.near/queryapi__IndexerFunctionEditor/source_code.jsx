27c1d29f69a31a39dae73078aee331c9ff72ab90
roshaan.near <roshaan.near>
2023-02-09 22:06:22 -0500

    Update queryapi__IndexerFunctionEditor by roshaan.near at 2023-02-09 22:06:22.855

--- roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
+++ roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
@@ -7,22 +7,37 @@ State.init({
 });
 
 const code = `
-<link rel="stylesheet" data-name="vs/editor/editor.main" href="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs/editor/editor.main.min.css">
-<div id="container" style="height:400px;border:1px solid black;"></div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs/loader.min.js"></script>
-<script>
-// require is provided by loader.min.js.
-require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs' }});
-require(["vs/editor/editor.main"], () => {
-  monaco.editor.create(document.getElementById('container'), {
-    value: \`function x() {
-  console.log("Hello world!");
-}\`,
-    language: 'javascript',
-    theme: 'vs-dark',
-  });
+<script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-markdown-editor-lite@1.3.4/lib/index.js" crossorigin></script>
+<link rel="stylesheet" href="https://unpkg.com/react-markdown-editor-lite@1.3.4/lib/index.css" />
 
-window.top.postMessage(monaco.editor.getModels()[0], "*"); 
+
+<div id="react-root"></div>
+
+<script>
+function TestReact(props) {
+  const [value, setValue] = React.useState(props.initialText || "");
+  return React.createElement(ReactMarkdownEditorLite.default, {
+      value,
+      view: { menu: true, md: true, html: false },
+      canView: { menu: true, md: false, html: false, fullScreen: false, hideMenu: true },
+      onChange: ({ text }) => {
+        setValue(text);
+        window.top.postMessage(text, "*");
+      },
+      renderHTML: () => {},
+      className: "full",
+    }); 
+}
+
+const domContainer = document.querySelector('#react-root');
+const root = ReactDOM.createRoot(domContainer);
+
+window.addEventListener("message", (event) => {
+  root.render(React.createElement(TestReact, {
+    initialText: event.data,
+  }));
 });
 
 </script>
@@ -32,7 +47,6 @@ return (
   <>
     <div>
       <iframe
-      t
         className="w-100"
         style={{ height: '300px' }}
         srcDoc={code}
