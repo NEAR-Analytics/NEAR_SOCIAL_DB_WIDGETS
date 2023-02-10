07508c8b714def4d4fe900c17b477e5f150ffe64
testwiki.near <testwiki.near>
2023-02-09 20:04:42 -0500

    Update WikiOnSocialDB_MainNavigation by testwiki.near at 2023-02-09 20:04:42.257

---
+++ testwiki.near/WikiOnSocialDB_MainNavigation/source_code.jsx
@@ -0,0 +1,38 @@
+const currentPill = props.currentNavPill ?? "";
+const authorForWidget = "testwiki.near";
+const pills = [
+  {
+    id: "articles",
+    title: "Articles",
+    widgetName: "WikiOnSocialDB",
+  },
+  {
+    id: "authors",
+    title: "Authors",
+    widgetName: "WikiOnSocialDB_Authors",
+  },
+  {
+    id: "create",
+    title: "Create Article",
+    widgetName: "WikiOnSocialDB_CreateArticle",
+  },
+];
+
+return (
+  <div>
+    <ul className="nav nav-pills nav-fill mb-4">
+      {pills.map(({ id, title, widgetName }, i) => (
+        <li className="nav-item">
+          <a
+            href={`#/${authorForWidget}/widget/${widgetName}`}
+            class={`nav-link ${
+              id === currentPill ? "active" : ""
+            } text-decoration-none`}
+          >
+            {title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
