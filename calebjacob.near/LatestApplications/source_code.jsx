5c89d47773235a55dfc8e369d11c4840f04bf14c
calebjacob <calebjacob>
2023-02-09 19:21:28 -0500

    Update LatestApplications by calebjacob.near at 2023-02-09 19:21:28.395

--- calebjacob.near/LatestApplications/source_code.jsx
+++ calebjacob.near/LatestApplications/source_code.jsx
@@ -1,3 +1,42 @@
+const limit = 5;
+
+const widgetMetadata = Social.get(`*/widget/*/metadata/**`, "final");
+const widgetBlockheightData = Social.keys("*/widget/*/metadata", "final", {
+  return_type: "BlockHeight",
+});
+let apps = [];
+
+if (widgetMetadata && widgetBlockheightData) {
+  console.log(widgetMetadata);
+  console.log(widgetBlockheightData);
+
+  const widgets = [];
+
+  Object.keys(widgetBlockheightData).forEach((accountId) => {
+    return Object.keys(widgetBlockheightData[accountId].widget).forEach(
+      (widgetName) => {
+        widgets.push({
+          accountId,
+          widgetName,
+          blockHeight: widgetBlockheightData[accountId].widget[widgetName],
+        });
+      }
+    );
+  });
+
+  widgets.sort((a, b) => b.blockHeight - a.blockHeight);
+
+  apps = widgets.slice(0, limit).map((widget) => {
+    return {
+      ...widget,
+      metadata:
+        widgetMetadata[widget.accountId].widget[widget.widgetName].metadata,
+    };
+  });
+
+  console.log(applications);
+}
+
 const H2 = styled.h2`
   font-size: 19px;
   line-height: 22px;
@@ -13,16 +52,10 @@ return (
   <>
     <H2>Latest Applications</H2>
 
-    <CardWrapper>
-      <Widget src="calebjacob.near/widget/ApplicationCard" />
-    </CardWrapper>
-
-    <CardWrapper>
-      <Widget src="calebjacob.near/widget/ApplicationCard" />
-    </CardWrapper>
-
-    <CardWrapper>
-      <Widget src="calebjacob.near/widget/ApplicationCard" />
-    </CardWrapper>
+    {apps.map((app, i) => (
+      <CardWrapper key={i}>
+        <Widget src="calebjacob.near/widget/ApplicationCard" props={{ app }} />
+      </CardWrapper>
+    ))}
   </>
 );
