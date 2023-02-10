9f6b5ee1b22d8cd2246eaef421ba1281799d2f83
0bserver07 <0bserver07>
2023-02-10 15:29:31 -0500

    Update toolipse-last-try by y3k.near at 2023-02-10 15:29:31.446

---
+++ y3k.near/toolipse-last-try/source_code.jsx
@@ -0,0 +1,124 @@
+const accountId = props.accountId || context.accountId;
+const marketId = "simple.market.mintbase1.near";
+const AFFILIATE_ACCOUNT = props.affiliateAccount || "toolipse.near";
+
+const data = fetch("https://graph.mintbase.xyz", {
+  method: "POST",
+  headers: {
+    "mb-api-key": "anon",
+    "Content-Type": "application/json",
+    "x-hasura-role": "anonymous",
+  },
+  body: JSON.stringify({
+    query: `
+      query MyQuery {
+        mb_views_active_listings(limit: 700, order_by: {price: desc},where: {minter: {_eq: "toolipse.near"}}) {
+            listed_by
+            created_at
+            price
+            nft_contract_id
+            token_id
+            metadata_id
+        }   
+      }
+`,
+  }),
+});
+
+let Style = styled.div`
+
+/* Gray Scale */
+.hover08 a img {
+	-webkit-filter: grayscale(100%);
+	filter: grayscale(100%);
+	-webkit-transition: .3s ease-in-out;
+	transition: .3s ease-in-out;
+}
+.hover08 a:hover img {
+	-webkit-filter: grayscale(0);
+	filter: grayscale(0);
+}
+        `;
+
+const YoctoToNear = (amountYocto) =>
+  new Big(amountYocto).div(new Big(10).pow(24)).toString();
+
+let buy = (price, token_id, nft_contract_id) => {
+  const gas = 200000000000000;
+  const deposit = new Big(price).toFixed(0);
+
+  Near.call(
+    marketId,
+    "buy",
+    {
+      nft_contract_id: nft_contract_id,
+      token_id: token_id,
+      referrer_id: AFFILIATE_ACCOUNT,
+    },
+    gas,
+    deposit
+  );
+};
+
+if (!data.ok) {
+  return "Loading";
+}
+
+const size = "10em";
+
+return data !== null ? (
+  <Style>
+    <h1>Artworks available on @mintbase</h1>
+    <p>Nice to see that my artworks can be buyable from here.</p>
+    <div className="d-flex gap-4 flex-wrap">
+      {data.body.data?.mb_views_active_listings.map((listing, i) => {
+        const priceYocto = listing.price.toLocaleString().replace(/,/g, "");
+        const priceNear = YoctoToNear(priceYocto);
+
+        return (
+          <div className="d-flex flex-column gap-1 hover08">
+            <a
+              href={`https://mintbase.xyz/meta/${listing.metadata_id}/`}
+              target="_blank"
+            >
+              <Widget
+                src="mob.near/widget/NftImage"
+                props={{
+                  nft: {
+                    tokenId: listing.token_id,
+                    contractId: listing.nft_contract_id,
+                  },
+                  style: {
+                    width: size,
+                    height: size,
+                    objectFit: "cover",
+                    minWidth: size,
+                    minHeight: size,
+                    maxWidth: size,
+                    maxHeight: size,
+                    overflowWrap: "break-word",
+                  },
+                  thumbnail: "thumbnail",
+                  className: "rounded img-thumbnail",
+                  fallbackUrl:
+                    "https://ipfs.near.social/ipfs/bafkreihdiy3ec4epkkx7wc4wevssruen6b7f3oep5ylicnpnyyqzayvcry",
+                }}
+              />
+            </a>
+            <button
+              disabled={!accountId}
+              onClick={() => {
+                if (!accountId) return;
+                buy(priceYocto, listing.token_id, listing.nft_contract_id);
+              }}
+            >
+              Buy for {priceNear} N
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  </Style>
+) : (
+  <p>loading...</p>
+);
