9300dff2cfe72d3377925706b339d8103581d81b
roshaan.near <roshaan.near>
2023-02-09 23:03:38 -0500

    Update queryapi__IndexerFunctionEditor by roshaan.near at 2023-02-09 23:03:38.082

--- roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
+++ roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
@@ -20,7 +20,7 @@ const code = `
 <script>
 function TestReact(props) {
   const [value, setValue] = React.useState(props.initialText || "");
-  return React.createElement(@monaco-editor/react.Editor, {})
+  return React.createElement(Editor, {})
 
 }
 
