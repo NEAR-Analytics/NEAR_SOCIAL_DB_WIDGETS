41cd63f7024926107f95a4624ea3fdbb24b04cee
testwiki.near <testwiki.near>
2023-02-09 20:00:43 -0500

    Update WikiOnSocialDB_AllArticlesList by testwiki.near at 2023-02-09 20:00:43.938

--- testwiki.near/WikiOnSocialDB_AllArticlesList/source_code.jsx
+++ testwiki.near/WikiOnSocialDB_AllArticlesList/source_code.jsx
@@ -1,10 +1,45 @@
-const { filteredArticles, getDateLastEdit, handleArticle, state } = props;
+const addressForArticles = "wikiTest";
+const authorForWidget = "testwiki.near";
+
+const wikiTestData = Social.get(`*/${addressForArticles}/articles/**`, "final");
+const wikiTestArr = wikiTestData && Object.values(wikiTestData);
+const resultArticles =
+  wikiTestArr &&
+  wikiTestArr.reduce(
+    (acc, account) =>
+      acc.concat(Object.values(account[addressForArticles].articles)),
+    []
+  );
+
+resultArticles.length &&
+  resultArticles.sort((a, b) => {
+    return Number(b.timeLastEdit) - Number(a.timeLastEdit);
+  });
+
+const filteredArticles =
+  resultArticles.length &&
+  resultArticles.reduce((acc, article) => {
+    if (!acc.some(({ articleId }) => articleId === article.articleId)) {
+      return [...acc, article];
+    } else {
+      return acc;
+    }
+  }, []);
+
+const getDateLastEdit = (timestamp) => {
+  const date = new Date(Number(timestamp));
+  const dateString = `${date.toLocaleDateString()} / ${date.toLocaleTimeString()}`;
+  return dateString;
+};
+
 return (
   <ol>
     {filteredArticles &&
       filteredArticles.map((article, index) => (
         <li key={article.articleId}>
-          <a href="" onClick={(e) => handleArticle(e, article)}>
+          <a
+            href={`#/${authorForWidget}/widget/WikiOnSocialDB_OneArticle?articleId=${article.articleId}`}
+          >
             {article.articleId}{" "}
             <small>
               (author: {article.author}
