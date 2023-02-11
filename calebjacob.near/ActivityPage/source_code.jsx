b212314a877a7a1ddedd4b29e79690666f66dd1b
calebjacob <calebjacob>
2023-02-10 23:06:00 -0500

    Update ActivityPage by calebjacob.near at 2023-02-10 23:06:00.793

---
+++ calebjacob.near/ActivityPage/source_code.jsx
@@ -0,0 +1,29 @@
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 284px minmax(1px, max-content) 284px;
+  grid-gap: 16px;
+`;
+
+const Sidebar = styled.div`
+  padding-top: 24px;
+`;
+
+const Main = styled.div`
+  padding-top: 24px;
+  border-left: 1px solid #ECEEF0;
+  border-right: 1px solid #ECEEF0;
+`;
+
+return (
+  <Wrapper>
+    <Sidebar>
+      <Widget src="calebjacob.near/widget/LatestApps" />
+    </Sidebar>
+    <Main>
+      <Widget src="calebjacob.near/widget/Activity" />
+    </Main>
+    <Sidebar>
+      <Widget src="calebjacob.near/widget/CustomWidgetDisplay" />
+    </Sidebar>
+  </Wrapper>
+);
