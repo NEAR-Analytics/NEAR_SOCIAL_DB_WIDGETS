8abf09e71aec0cc0f68262c025d08d55bb20a9ec
calebjacob <calebjacob>
2023-02-10 21:56:01 -0500

    Update LatestApps by calebjacob.near at 2023-02-10 21:56:01.101

--- calebjacob.near/LatestApps/source_code.jsx
+++ calebjacob.near/LatestApps/source_code.jsx
@@ -1,36 +1,28 @@
-const limit = 5;
-
-const widgetMetadata = Social.get(`*/widget/*/metadata/**`, "final");
-const widgetBlockheightData = Social.keys("*/widget/*/metadata", "final", {
+const data = Social.keys("*/widget/*/metadata", "final", {
   return_type: "BlockHeight",
 });
+const limit = 5;
 let apps = [];
-const totalApps = Object.keys(widgetMetadata || {}).length;
+let totalApps = 0;
 
-if (widgetMetadata && widgetBlockheightData) {
-  const widgets = [];
+if (data) {
+  const result = [];
 
-  Object.keys(widgetBlockheightData).forEach((accountId) => {
-    return Object.keys(widgetBlockheightData[accountId].widget).forEach(
-      (widgetName) => {
-        widgets.push({
-          accountId,
-          widgetName,
-          blockHeight: widgetBlockheightData[accountId].widget[widgetName],
-        });
-      }
-    );
+  Object.keys(data).forEach((accountId) => {
+    return Object.keys(data[accountId].widget).forEach((widgetName) => {
+      totalApps++;
+
+      result.push({
+        accountId,
+        widgetName,
+        blockHeight: data[accountId].widget[widgetName],
+      });
+    });
   });
 
-  widgets.sort((a, b) => b.blockHeight - a.blockHeight);
+  result.sort((a, b) => b.blockHeight - a.blockHeight);
 
-  apps = widgets.slice(0, limit).map((widget) => {
-    return {
-      ...widget,
-      metadata:
-        widgetMetadata[widget.accountId].widget[widget.widgetName].metadata,
-    };
-  });
+  apps = result.slice(0, limit);
 }
 
 const H2 = styled.h2`
@@ -78,7 +70,10 @@ return (
 
     {apps.map((app, i) => (
       <CardWrapper key={i}>
-        <Widget src="calebjacob.near/widget/AppCard" props={{ app }} />
+        <Widget
+          src="calebjacob.near/widget/AppCard"
+          props={{ src: `${app.accountId}/widget/${app.widgetName}` }}
+        />
       </CardWrapper>
     ))}
 
