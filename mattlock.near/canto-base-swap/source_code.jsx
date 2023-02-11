e1329b6fa8521092baeb6c6273628bc53a57f4c7
mattlockyer <mattlockyer>
2023-02-11 15:35:19 -0500

    Update canto-base-swap by mattlock.near at 2023-02-11 15:35:19.067

---
+++ mattlock.near/canto-base-swap/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <Widget
+    src="mattlock.near/widget/cantoswap-component"
+    props={{
+      contractName: "Base Router",
+      contractAddress: "0xa252eEE9BDe830Ca4793F054B506587027825a8e",
+      abiUrl:
+        "https://gist.githubusercontent.com/mattlockyer/a9de9620116c546f85003010f6a3bec5/raw/e2e00d35baa20d67aaae1278182f44773f434b44/cantoBaseV1RouterAbi",
+    }}
+  />
+);
