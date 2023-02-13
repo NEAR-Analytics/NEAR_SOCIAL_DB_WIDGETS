626dd9fd61d204c6b16a56553283a05a59213f77
events_v2.near <events_v2.near>
2023-02-13 16:22:59 -0500

    Update components__search-bar by events_v2.near at 2023-02-13 16:22:59.414

---
+++ events_v2.near/components__search-bar/source_code.jsx
@@ -0,0 +1,36 @@
+const Searchbar = styled.input`
+  width: auto;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 0;
+  outline: none;
+  font-size: 1rem;
+  margin: 8px;
+  border-radius: 8px;
+  position: fixed;
+  top: 64px;
+  left: 0;
+  right: 0;
+  z-index: 1;
+
+  &:focus {
+    outline: none;
+    border-color: #333;
+  }
+
+  &::placeholder {
+    color: #999;
+  }
+`;
+
+return (
+  <div style={{ position: 'relative' }}>
+    <Searchbar
+      onChange={(e) => {
+        const term = e.target.value;
+        State.update({ term });
+      }}
+      placeholder="Search for events"
+    />
+  </div>
+);
