2d6c93ccaf4f946a0852c8ef1ef10f1d8d3940b0
events_v2.near <events_v2.near>
2023-02-13 12:44:00 -0500

    Update event_lists__index by events_v2.near at 2023-02-13 12:44:00.693

--- events_v2.near/event_lists__index/source_code.jsx
+++ events_v2.near/event_lists__index/source_code.jsx
@@ -9,12 +9,12 @@ props.controller.setLayout('layouts:container', {
   items: [
     {
       label: 'Lists',
-      onClick: [{ appName: 'event_lists', call: 'push' }, 'index', {}],
+      onClick: [{ appName: 'event_lists', method: 'push' }, 'index', {}],
       active: true,
     },
     {
       label: 'Events',
-      onClick: [{ appName: 'events', call: 'push' }, 'index', {}],
+      onClick: [{ appName: 'events', method: 'push' }, 'index', {}],
     },
   ],
 });
