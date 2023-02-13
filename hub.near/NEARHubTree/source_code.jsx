524ad300761dabb35e61e2f246de111cf542bc33
NEARHub-online <NEARHub-online>
2023-02-13 15:35:41 -0500

    Update NEARHubTree by hub.near at 2023-02-13 15:35:41.505

---
+++ hub.near/NEARHubTree/source_code.jsx
@@ -0,0 +1,140 @@
+return (
+  <div className="container">
+    <div className="row mb-3"></div>
+
+    <div className="row mb-3">
+      <div>
+        <h4>NEAR Hub your portal to everything NEAR</h4>
+        <div className="mb-3"></div>
+        <a
+          className="btn btn-outline-primary"
+          onClick={(e) => rollImage()}
+          style={{
+            width: "200px",
+            backgroundColor: "black",
+            color: "white",
+            fontFamily: '"Press Start 2P", sans-serif',
+            margin: "20px 20px 20px 20px",
+            border: "1px solid #3a0201",
+          }}
+          href="https://nearhub.club"
+        >
+          NEAR Hub Online
+        </a>
+      </div>
+      <div className="mb-3"></div>
+      <br></br>
+      <br></br>
+      <div>
+        <h4>NEARHub GeNFT ai to text image minter</h4>
+        <div className="mb-3"></div>
+        <a
+          className="btn btn-outline-primary"
+          onClick={(e) => rollImage()}
+          style={{
+            width: "200px",
+            backgroundColor: "black",
+            color: "white",
+            fontFamily: '"Press Start 2P", sans-serif',
+            margin: "20px 20px 20px 20px",
+            border: "1px solid #3a0201",
+          }}
+          href="https://genft.nearhub.club"
+        >
+          GeNFT Minter
+        </a>
+      </div>
+      <div className="mb-3"></div>
+      <br></br>
+      <br></br>
+      <div>
+        <h4>
+          NEAR Hub Talk voice and video calling. Connect via NEAR Hub Phone,
+          browser or jitsi app
+        </h4>
+        <p>(jitsi server: "https://talk.nearhub.online")</p>
+        <a
+          className="btn btn-outline-primary"
+          onClick={(e) => rollImage()}
+          style={{
+            width: "200px",
+            backgroundColor: "black",
+            color: "white",
+            fontFamily: '"Press Start 2P", sans-serif',
+            margin: "20px 20px 20px 20px",
+            border: "1px solid #3a0201",
+          }}
+          href="https://talk.nearhub.online"
+        >
+          NEAR Hub Talk
+        </a>
+      </div>
+      <div className="mb-3"></div>
+      <br></br>
+      <br></br>
+      <div>
+        <h4>NEAR Hub Whiteboard</h4>
+        <div className="mb-3"></div>
+        <a
+          className="btn btn-outline-primary"
+          onClick={(e) => rollImage()}
+          style={{
+            width: "200px",
+            backgroundColor: "black",
+            color: "white",
+            fontFamily: '"Press Start 2P", sans-serif',
+            margin: "20px 20px 20px 20px",
+            border: "1px solid #3a0201",
+          }}
+          href="https://draw.nearhub.club"
+        >
+          NEAR Hub Whiteboard
+        </a>
+      </div>
+      <div className="mb-3"></div>
+      <br></br>
+      <br></br>
+      <div>
+        <h4>NEAR Hub Comics</h4>
+        <div className="mb-3"></div>
+        <a
+          className="btn btn-outline-primary"
+          onClick={(e) => rollImage()}
+          style={{
+            width: "200px",
+            backgroundColor: "black",
+            color: "white",
+            fontFamily: '"Press Start 2P", sans-serif',
+            margin: "20px 20px 20px 20px",
+            border: "1px solid #3a0201",
+          }}
+          href="https://comics.nearhub.club"
+        >
+          NEAR Hub Comics
+        </a>
+      </div>
+      <div className="mb-3"></div>
+      <br></br>
+      <br></br>
+      <div>
+        <h4>NEAR Hub Tutorial Videos</h4>
+        <div className="mb-3"></div>
+        <a
+          className="btn btn-outline-primary"
+          onClick={(e) => rollImage()}
+          style={{
+            width: "200px",
+            backgroundColor: "black",
+            color: "white",
+            fontFamily: '"Press Start 2P", sans-serif',
+            margin: "20px 20px 20px 20px",
+            border: "1px solid #3a0201",
+          }}
+          href="https://www.youtube.com/playlist?list=PLpS5XZ5rOII2yKmlHjbXp6iiTKCNCiL9R"
+        >
+          NEAR Hub Video Tutorials
+        </a>
+      </div>
+    </div>
+  </div>
+);
