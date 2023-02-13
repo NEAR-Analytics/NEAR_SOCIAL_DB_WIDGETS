0b78a12c2f1c21cd4f70286c590410dc1553d303
events_v2.near <events_v2.near>
2023-02-13 17:24:57 -0500

    Update components__search_bar by events_v2.near at 2023-02-13 17:24:57.984

--- events_v2.near/components__search_bar/source_code.jsx
+++ events_v2.near/components__search_bar/source_code.jsx
@@ -148,6 +148,10 @@ return (
         padding: GRID_PAD,
         marginBottom: GRID_PAD,
         zIndex: 1,
+
+        '&:focus': {
+          outline: 'none',
+        },
       }}
     />
   </div>
