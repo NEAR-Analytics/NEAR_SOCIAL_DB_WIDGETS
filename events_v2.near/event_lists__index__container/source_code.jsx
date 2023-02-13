0e7673fb44e3af18f75d70aefd56352e98d1a046
events_v2.near <events_v2.near>
2023-02-13 10:18:54 -0500

    Update event_lists__index__container by events_v2.near at 2023-02-13 10:18:54.572

--- events_v2.near/event_lists__index__container/source_code.jsx
+++ events_v2.near/event_lists__index__container/source_code.jsx
@@ -35,6 +35,6 @@ return (
       layout: listLayout,
       search: props.search || false,
     })}
-    <div style={{ marginBottom: 20 }}></div>
+    <div style={{ marginBottom: 1000 }}></div>
   </>
 );
