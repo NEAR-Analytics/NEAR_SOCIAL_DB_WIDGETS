55d93cb2d530f2799ab67e69595373a708ab123f
events_v2.near <events_v2.near>
2023-02-13 10:19:37 -0500

    Update event_lists__index by events_v2.near at 2023-02-13 10:19:37.006

--- events_v2.near/event_lists__index/source_code.jsx
+++ events_v2.near/event_lists__index/source_code.jsx
@@ -20,7 +20,7 @@ return (
       })}
     </Container>
     <Hr />
-    <div style={{ marginBottom: 100 }}></div>
+    <div style={{ marginBottom: 40 }}></div>
 
     <Container>
       {props.__engine.renderComponent('index.container', {
