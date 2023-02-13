ea1560d93eb5325c396acbfa569baa0a813b8e03
events_v2.near <events_v2.near>
2023-02-13 16:21:25 -0500

    Update event_lists__index__list by events_v2.near at 2023-02-13 16:21:25.349

--- events_v2.near/event_lists__index__list/source_code.jsx
+++ events_v2.near/event_lists__index__list/source_code.jsx
@@ -1,24 +1,34 @@
-const event_lists = props.event_lists || [];
-if (!event_lists) {
+if (!state) {
+  const event_lists = props.event_lists || [];
+  if (!event_lists) {
+    return props.__engine.loading();
+  }
+  if (!event_lists) {
+    return props.__engine.loading();
+  }
+
+  State.init({ allItems: event_lists, items: [] });
   return props.__engine.loading();
 }
+
 const widgetName = `index.list.${props.layout}`;
 
-// TODO: Search bar
-
-const SearchBar = props.__engine.renderComponent('index.search_bar', {
-  items: event_lists,
+const SearchBar = props.__engine.renderComponent('components:search_bar', {
+  items: state.allItems,
   fields: {
     name: 1,
     description: 0.8,
     location: 1.2,
     category: 1.1,
   },
+  onSearch: (items) => {
+    State.update({ items: items });
+  },
 });
 
 return (
   <>
     {SearchBar}
-    {props.__engine.renderComponent(widgetName, { event_lists })}
+    {props.__engine.renderComponent(widgetName, { event_lists: state.items })}
   </>
 );
