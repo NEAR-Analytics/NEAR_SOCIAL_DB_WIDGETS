f9ea1643d7834def9d8cd0df06aa1f18d76e35f7
roshaan.near <roshaan.near>
2023-02-13 18:49:27 -0500

    Update queryapi__IndexerFunctionEditor by roshaan.near at 2023-02-13 18:49:27.100

--- roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
+++ roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
@@ -1,6 +1,3 @@
-//props indexer_name
-let indexer_name = props.indexer_name;
-
 const initialText = '# Hello World\n\n';
 State.init({
   m: initialText,
@@ -9,33 +6,32 @@ State.init({
 const code = `
 <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
 <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
-<script src="https://unpkg.com/react-markdown-editor-lite@1.3.4/lib/index.js" crossorigin></script>
-<link rel="stylesheet" href="https://unpkg.com/react-monaco-editor@0.51.0/lib/index.js" />
 
-<style>
-#text-editor {
-  border: 1px solid red;
-}
-#react-root {
-  border: 1px solid blue;
-}
-</style>
 <div id="root">
-<h1> code editor </h1>
-<div id="code-editor">
-code editor </div>
-<h1> text editor </h1>
-
-<div id="text-editor">
-text editor
 </div>
-</div>
-<script>
+<script >
 function TestReact(props) {
   const [value, setValue] = React.useState(props.initialText || "");
-  return React.createElement(MonacoEditor, {
-    height="400"
-    language="javascript"
+  React.useEffect(() => {
+    // Add a listener for messages from other sources
+    window.addEventListener("message", handleMessage);
+
+    return () => {
+      // Clean up the listener when the component unmounts
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
+
+  const handleMessage = (event) => {
+    // Handle incoming messages
+    console.log("Received message:", event.data);
+    // Forward the message to the parent window
+    window.parent.postMessage(event.data, "*");
+  };
+  return React.createElement("iframe", {
+    src:"http://localhost:3000/query-api-editor",
+    height: "400px",
+    width: "400px",
     }); 
 }
 
@@ -51,20 +47,16 @@ window.addEventListener("message", (event) => {
 
 </script>
 `;
+
 return (
-  <>
-    <div>
-      <iframe
-        className="w-100"
-        style={{ height: '300px' }}
-        srcDoc={code}
-        message={initialText}
-        onMessage={(m) => console.log(m, 'yo')}
-      />
-    </div>
-    <Markdown text={state.m} />
-    <Widget
-      src={'roshaan.near/widget/queryapi__RegisterIndexerFunctionButton'}
+  <div>
+    <iframe
+      className="w-100"
+      style={{ height: '300px' }}
+      srcDoc={code}
+      message={initialText}
+      onMessage={(m) => State.update({ m })}
     />
-  </>
+    <Markdown text={state.m} />
+  </div>
 );
