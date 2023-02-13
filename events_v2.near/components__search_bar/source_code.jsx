579b6bbccb390692b8c124e6a179d4114466b0bb
events_v2.near <events_v2.near>
2023-02-13 16:38:54 -0500

    Update components__search_bar by events_v2.near at 2023-02-13 16:38:54.438

--- events_v2.near/components__search_bar/source_code.jsx
+++ events_v2.near/components__search_bar/source_code.jsx
@@ -97,6 +97,7 @@ return (
     <Searchbar
       onChange={(e) => {
         const term = e.target.value;
+        console.log('searching', { term });
         State.update({ term });
       }}
       placeholder="Search for events"
