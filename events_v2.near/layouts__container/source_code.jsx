b2457089546913395df697b9181069cfad0c167e
events_v2.near <events_v2.near>
2023-02-13 12:06:56 -0500

    Update layouts__container by events_v2.near at 2023-02-13 12:06:56.062

--- events_v2.near/layouts__container/source_code.jsx
+++ events_v2.near/layouts__container/source_code.jsx
@@ -187,7 +187,7 @@ const navbar = (
                   props.__engine.hacks.dirtyEval(item.onClick);
                 }
               }}
-              active={item.active}
+              active={item.active ? true : false}
             >
               {item.label}
             </NavbarItem>
