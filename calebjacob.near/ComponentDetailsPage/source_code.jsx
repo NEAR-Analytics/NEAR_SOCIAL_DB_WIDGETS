09e402e050522f2610107e9bd6b13a7cf9ac28f2
calebjacob <calebjacob>
2023-02-14 17:22:56 -0500

    Update ComponentDetailsPage by calebjacob.near at 2023-02-14 17:22:56.896

--- calebjacob.near/ComponentDetailsPage/source_code.jsx
+++ calebjacob.near/ComponentDetailsPage/source_code.jsx
@@ -76,46 +76,6 @@ const Thumbnail = styled.div`
   }
 `;
 
-const TagsWrapper = styled.div`
-  position: relative;
-
-  &::after {
-    content: '';
-    display: block;
-    height: 100%;
-    width: 16px;
-    background: linear-gradient(to right, rgba(255,255,255,0), rgba(255,255,255,1));
-    position: absolute;
-    top: 0;
-    right: 0;
-  }
-`;
-
-const Tags = styled.ul`
-  display: flex;
-  list-style: none;
-  gap: 6px;
-  overflow: auto;
-  margin: 0;
-  padding: 6px 16px 0 0;
-
-  scrollbar-width: none;
-  -ms-overflow-style: none;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-const Tag = styled.li`
-  border: 1px solid #E6E8EB;
-  padding: 3px 8px;
-  border-radius: 6px;
-  font-size: 12px;
-  line-height: 18px;
-  color: #687076;
-  font-weight: 500;
-`;
-
 const ButtonLink = styled.a`
   display: inline-flex;
   align-items: center;
@@ -385,13 +345,12 @@ return (
           {tags.length > 0 && (
             <div>
               <SmallTitle>Tags</SmallTitle>
-              <TagsWrapper>
-                <Tags>
-                  {tags.map((tag, i) => (
-                    <Tag key={i}>{tag}</Tag>
-                  ))}
-                </Tags>
-              </TagsWrapper>
+              <Widget
+                src="calebjacob.near/widget/ComponentTags"
+                props={{
+                  tags,
+                }}
+              />
             </div>
           )}
 
