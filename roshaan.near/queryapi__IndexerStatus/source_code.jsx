b68fe96a9358d043aa5f4da23ea2ed3c1a2096ad
roshaan.near <roshaan.near>
2023-02-14 17:22:42 -0500

    Update queryapi__IndexerStatus by roshaan.near at 2023-02-14 17:22:42.449

--- roshaan.near/queryapi__IndexerStatus/source_code.jsx
+++ roshaan.near/queryapi__IndexerStatus/source_code.jsx
@@ -1,8 +1,36 @@
 //props indexer_name
 let indexer_name = props.indexer_name;
+let query = fetch(
+  'https://query-api-hasura-vcqilefdcq-uc.a.run.app/v1/graphql',
+  {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query: `
+  query IndexerStatus {
+  indexer_state(
+    where: {function_name: {_eq: ${indexer_name}}}
+    order_by: {current_block_height: desc}
+  ) {
+    current_block_height
+  }
+  log_entries(
+    where: {function_name: {_eq: ${indexer_name}}}
+    order_by: {timestamp: desc}
+  ) {
+    id
+    message
+    timestamp
+  }
+}
+    `,
+    }),
+  }
+);
 
 return (
   <>
     <h1>Indexer Status</h1>
+    {JSON.stringify(query)}
   </>
 );
