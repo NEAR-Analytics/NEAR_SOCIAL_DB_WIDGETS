ae4bfb1daa0fdc7f87ccd6886d9a0d5d9ceb8e1b
eugenewolf507.near <eugenewolf507.near>
2023-02-14 10:35:39 -0500

    Update Draft_CheckStructureCommentsIndex by eugenewolf507.near at 2023-02-14 10:35:39.870

--- eugenewolf507.near/Draft_CheckStructureCommentsIndex/source_code.jsx
+++ eugenewolf507.near/Draft_CheckStructureCommentsIndex/source_code.jsx
@@ -13,12 +13,28 @@ const getPublicationParamsAllArticles = () => ({
 });
 // ========= HANDLER GET =========
 const clickHandlerGetStructure = () => {
-  const questionsIndex = Social.index("poll_question", "question-v3.1.0");
-  console.log("questionsIndex", questionsIndex);
-  const getData = Social.get(`mob.near/**`, "final");
-  console.log("getData", getData);
-  const getData = Social.get(`mob.near/**`, "final");
-  console.log("getData", getData);
+  const accountId = "mob.near";
+
+  const posts = Social.get(`${accountId}/post/main`, 84445357);
+  console.log("postsGet", posts);
+  const postsIndex = Social.index("post", "main", {
+    limit: 10,
+    order: "desc",
+    accountId,
+  });
+  console.log(postsIndex);
+  console.log("==================");
+
+  postsIndex.forEach(({ accountId, blockHeight }) => {
+    const postData = Social.get(`${accountId}/post/main`, blockHeight);
+    console.log(postData);
+    console.log(accountId, blockHeight);
+  });
+
+  //   const questionsIndex = Social.index("poll_question", "question-v3.1.0");
+  //   console.log("questionsIndex", questionsIndex);
+  //   const questionsGet = Social.get(`*/index/poll_question/`, "final");
+  //   console.log("questionsGet", questionsGet);
 };
 
 // ========= RETURN =========
