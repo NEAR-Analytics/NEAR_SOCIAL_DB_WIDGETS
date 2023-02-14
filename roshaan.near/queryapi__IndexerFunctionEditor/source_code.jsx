9258dd7494396d7bff95232c5a5bb661e74bd424
roshaan.near <roshaan.near>
2023-02-14 15:30:10 -0500

    Update queryapi__IndexerFunctionEditor by roshaan.near at 2023-02-14 15:30:10.972

--- roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
+++ roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
@@ -4,9 +4,7 @@ State.init({
 });
 
 const code = `
-<div id="root"></div>
-<iframe id="react-app-iframe" onload="test()" src="http://localhost:3000/query-api-editor" width="750px" height="750px"></iframe>
-
+<iframe id="react-app-iframe" onload="test()" src="http://localhost:3000/query-api-editor" width="1250px" height="500px"></iframe>
 <script>
 function test() {
 let receiverWindow = document.getElementById("react-app-iframe").contentWindow
@@ -23,12 +21,11 @@ return (
   <div>
     <iframe
       className="w-100"
-      style={{ height: '300px' }}
+      style={{ height: '500px' }}
       srcDoc={code}
       message={initialText}
       onMessage={(m) => State.update({ m })}
     />
-    <Markdown text={state.m} />
     <Widget
       src={'roshaan.near/widget/queryapi__RegisterIndexerFunctionButton'}
       props={{
