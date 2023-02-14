08e2176128620af245b6554cf7be0bd12ee7dbf6
NearSocial <NearSocial>
2023-02-14 13:59:39 -0500

    Update Docs by hack.near at 2023-02-14 13:59:39.233

---
+++ hack.near/Docs/source_code.jsx
@@ -0,0 +1,30 @@
+return (
+  <div className="container">
+    <div className="row mb-3">
+      <div>
+        <h1>Widget Factory</h1>
+        <h3>
+          <b>Guide:</b> Building Open Web Apps
+        </h3>
+        <p>
+          Learn about this uniquely flexible and reliable social data protocol!
+        </p>
+        <p>
+          Below are useful resources for integrating your app with Near Social.
+        </p>
+      </div>
+    </div>
+    <div className="row mb-3">
+      <div>
+        <h2>Virtual Machine APIs</h2>
+        <ul>
+          <li>Social</li>
+          <li>Near</li>
+          <li>State</li>
+          <li>Storage</li>
+          <li>Cache</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
