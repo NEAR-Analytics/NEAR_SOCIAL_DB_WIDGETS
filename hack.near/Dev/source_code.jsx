32dbbb2aa8951cc9d9eb3610f68ac8307804aebc
NearSocial <NearSocial>
2023-02-14 13:39:12 -0500

    Update Dev by hack.near at 2023-02-14 13:39:12.370

---
+++ hack.near/Dev/source_code.jsx
@@ -0,0 +1,53 @@
+return (
+  <div className="container">
+    <div className="row mb-3"></div>
+
+    <div className="row mb-3">
+      <div>
+        <h4>Near Social Development</h4>
+        <p>Create a project page with a featured widget!</p>
+        <div className="mb-3"></div> <div className="mb-3"></div>
+        <a
+          className="btn btn-outline-primary"
+          href="https://near.social/#/create.near/widget/Page?accountId=create.near"
+        >
+          Example
+        </a>
+        <a
+          className="btn btn-outline-primary"
+          href="https://devgovgigs.near.social"
+        >
+          Open Discussion
+        </a>
+        <a className="btn btn-outline-primary" href="https://nearbuilders.com">
+          Learn More
+        </a>
+      </div>
+    </div>
+    <div className="row mb-3">
+      <div>
+        <h1>Widget Factory</h1>
+        <h3>
+          <b>Guide:</b> Using Open Web Components
+        </h3>
+        <p>
+          The <i>composability</i> of blockchain-based frontend solutions
+          changes the game for web developers! Below are useful resources for
+          anyone building on Near Social.
+        </p>
+      </div>
+    </div>
+    <div className="row mb-3">
+      <div>
+        <h2>Available Components</h2>
+        <ul>
+          <li>react-bootstrap</li>
+          <li>Widget src=""</li>
+          <li>Commit Button</li>
+          <li>Markdown Editor</li>
+          <li>Styled Components</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
