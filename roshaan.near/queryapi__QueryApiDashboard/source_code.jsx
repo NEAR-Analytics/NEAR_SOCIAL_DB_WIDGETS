94fa06bcfeebd7bada32a1c359b291e8e1666489
roshaan.near <roshaan.near>
2023-02-14 19:59:56 -0500

    Update queryapi__QueryApiDashboard by roshaan.near at 2023-02-14 19:59:56.527

--- roshaan.near/queryapi__QueryApiDashboard/source_code.jsx
+++ roshaan.near/queryapi__QueryApiDashboard/source_code.jsx
@@ -63,7 +63,10 @@ return (
 
     {state.activeTab === 'editor-window' && (
       <TabContent>
-        <Widget src={'roshaan.near/widget/queryapi__IndexerFunctionEditor' props={indexer_function_name}} />
+        <Widget src={'roshaan.near/widget/queryapi__IndexerFunctionEditor'       props={{
+        indexer_name: 'registry.queryapi.testnet',
+        indexer_code: state.m,
+      }}} />
         This is the Dashboard tab content. You can add any components or
         information related to the dashboard here.
       </TabContent>
