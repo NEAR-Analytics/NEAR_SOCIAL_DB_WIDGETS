44e4d59e5aff62b3e376d028253e53d4d2a73bc9
calebjacob <calebjacob>
2023-02-14 19:03:51 -0500

    Update ComponentDetailsPage by calebjacob.near at 2023-02-14 19:03:51.978

--- calebjacob.near/ComponentDetailsPage/source_code.jsx
+++ calebjacob.near/ComponentDetailsPage/source_code.jsx
@@ -3,14 +3,15 @@ State.init({
   selectedTab: props.tab ?? "about",
 });
 
-const [accountId, widget, widgetName] = props.src.split("/");
+const src = props.src;
+const [accountId, widget, widgetName] = src.split("/");
 const existsData = Social.keys(`${accountId}/widget/${widgetName}`);
 const exists = !existsData || Object.keys(existsData).length > 0;
 const data = Social.get(`${accountId}/widget/${widgetName}/**`);
 const code = data[""];
 const metadata = data.metadata;
 const tags = Object.keys(metadata.tags || {});
-const shareUrl = `https://near.social/#/calebjacob.near/widget/ComponentDetailsPage?src=${props.src}`;
+const shareUrl = `https://near.social/#/calebjacob.near/widget/ComponentDetailsPage?src=${src}`;
 
 const dependencyMatch = code && code.matchAll(/<Widget.+src="(.+)".+\/>/g);
 const dependencySources = [...(dependencyMatch || [])]
@@ -23,57 +24,8 @@ ${code}
 \`\`\`
 `;
 
-const Header = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 32px;
+const SummaryWrapper = styled.div`
   margin-bottom: 32px;
-
-  @media (max-width: 770px) {
-    gap: 16px;
-  }
-`;
-
-const Actions = styled.div`
-  display: flex;
-  gap: 12px;
-  flex-wrap: wrap;
-  margin-bottom: 16px;
-`;
-
-const Title = styled.h1`
-  font-size: 32px;
-  line-height: 39px;
-  color: #11181C;
-  margin: 0 0 8px;
-  font-weight: 600;
-
-  @media (max-width: 770px) {
-    font-size: 20px;
-    line-height: 24px;
-    margin: 0;
-  }
-`;
-
-const Thumbnail = styled.div`
-  width: 100px;
-  height: 100px;
-  flex-shrink: 0;
-  border: 1px solid #ECEEF0;
-  border-radius: 12px;
-  overflow: hidden;
-  box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06);
-
-  img {
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-  }
-
-  @media (max-width: 770px) {
-    width: 50px;
-    height: 50px;
-  }
 `;
 
 const ButtonLink = styled.a`
@@ -219,81 +171,23 @@ if (!exists) {
   return (
     <>
       <Title>Error</Title>
-      <Text>Could not find: {props.src}</Text>
+      <Text>Could not find: {src}</Text>
     </>
   );
 }
 
 return (
   <>
-    <Header>
-      <Thumbnail>
-        <Widget
-          src="mob.near/widget/Image"
-          props={{
-            image: metadata.image,
-            fallbackUrl:
-              "https://ipfs.near.social/ipfs/bafkreido7gsk4dlb63z3s5yirkkgrjs2nmyar5bxyet66chakt2h5jve6e",
-            alt: metadata.name,
-          }}
-        />
-      </Thumbnail>
-
-      <div>
-        <Title>{metadata.name || widgetName}</Title>
-        <Text>{props.src}</Text>
-      </div>
-    </Header>
-
-    <Actions>
-      <ButtonLink primary href={`/#/${props.src}`}>
-        Open
-      </ButtonLink>
-
-      <ButtonLink href={`/#/edit/${props.src}`}>
-        {context.accountId === accountId ? (
-          <>
-            <i class="bi bi-pencil-fill"></i> Edit
-          </>
-        ) : (
-          <>
-            <i class="bi bi-git"></i> Fork
-          </>
-        )}
-      </ButtonLink>
-
-      <ButtonLink
-        href={`/#/calebjacob.near/widget/ComponentDetailsPage?src=${props.src}&tab=source`}
-      >
-        <i class="bi bi-code-square"></i>
-        View Source
-      </ButtonLink>
-
-      <OverlayTrigger
-        placement="top"
-        overlay={<Tooltip>Copy to clipboard</Tooltip>}
-      >
-        <ButtonLink
-          as="button"
-          type="button"
-          onMouseLeave={() => {
-            State.update({ copiedShareUrl: false });
-          }}
-          onClick={() => {
-            clipboard.writeText(shareUrl).then(() => {
-              State.update({ copiedShareUrl: true });
-            });
-          }}
-        >
-          {state.copiedShareUrl ? (
-            <i class="bi bi-check-circle"></i>
-          ) : (
-            <i class="bi bi-box-arrow-up-right"></i>
-          )}
-          Share URL
-        </ButtonLink>
-      </OverlayTrigger>
-    </Actions>
+    <SummaryWrapper>
+      <Widget
+        src="calebjacob.near/widget/ComponentSummary"
+        props={{
+          size: "large",
+          showTags: false,
+          src,
+        }}
+      />
+    </SummaryWrapper>
 
     <Tabs>
       <TabsButton
@@ -411,7 +305,7 @@ return (
       <HistoryContainer>
         <Widget
           src="bozon.near/widget/WidgetHistory"
-          props={{ widgetPath: props.src }}
+          props={{ widgetPath: src }}
         />
       </HistoryContainer>
     )}
