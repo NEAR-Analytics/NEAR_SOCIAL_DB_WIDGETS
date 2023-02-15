f289832b1a11111093a32c272693959d18f2e3fc
silkking.near <silkking.near>
2023-02-15 15:03:24 -0500

    Update test2 by silkking.near at 2023-02-15 15:03:24.425

--- silkking.near/test2/source_code.jsx
+++ silkking.near/test2/source_code.jsx
@@ -1,3 +1,42 @@
-console.log(callback);
+State.init({
+  questionNumber: -1,
+  somethingOnTop: false,
+});
 
-return <div style={props.styleModal}>Hello World</div>;
+const questions = ["Question 1", "Question 2"];
+
+function renderWidget() {
+  State.update({ somethingOnTop: true });
+}
+
+function renderWithoutSomethingOnTop() {
+  return (
+    <>
+      <p>{state.questionNumber}</p>
+      <button onClick={() => State.update({ questionNumber: 0 })}>
+        Show 1
+      </button>
+      <button onClick={() => State.update({ questionNumber: 1 })}>
+        Show 2
+      </button>
+      {state.questionNumber != -1 && (
+        <div>
+          <p>{questions[state.questionNumber]}</p>
+          <button onClick={renderWidget}>View all</button>
+        </div>
+      )}
+    </>
+  );
+}
+
+function renderWithSomethingOnTop(widgetSrc) {
+  return <Widget src={widgetSrc} props={{}} />;
+}
+
+return (
+  <>
+    {state.somethingOnTop
+      ? renderWithSomethingOnTop("silkking.near/widget/test2")
+      : renderWithoutSomethingOnTop()}
+  </>
+);
