fded8cb077df01957612d70b23ddb8189cbffb39
ethden.near <ethden.near>
2023-02-15 00:22:26 -0500

    Update BosHomepage by ethden.near at 2023-02-15 00:22:26.562

--- ethden.near/BosHomepage/source_code.jsx
+++ ethden.near/BosHomepage/source_code.jsx
@@ -1,33 +1,35 @@
-return (
-  <div className="position-relative">
-    <div style={{ position: "relative", zIndex: 1 }}>
-      <h1>What is bOS?</h1>
-      <h2>
-        {"<"}Blockchain Operating System{">"}
-      </h2>
-    </div>
-    <svg
-      width="674"
-      height="422"
-      viewBox="0 0 674 422"
-      className="position-absolute top-0"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M0 0H674L578 422H0V0Z" fill="url(#paint0_linear_87_1036)" />
-      <defs>
-        <linearGradient
-          id="paint0_linear_87_1036"
-          x1="634.27"
-          y1="119.482"
-          x2="78.6826"
-          y2="356.622"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stop-color="#4D1CB6" />
-          <stop offset="1" stop-color="#AC3A7F" />
-        </linearGradient>
-      </defs>
-    </svg>
-  </div>
-);
+// return (
+//   <div className="position-relative">
+//     <div style={{ position: "relative", zIndex: 1 }}>
+//       <h1>What is bOS?</h1>
+//       <h2>
+//         {"<"}Blockchain Operating System{">"}
+//       </h2>
+//     </div>
+//     <svg
+//       width="674"
+//       height="422"
+//       viewBox="0 0 674 422"
+//       className="position-absolute top-0"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg"
+//     >
+//       <path d="M0 0H674L578 422H0V0Z" fill="url(#paint0_linear_87_1036)" />
+//       <defs>
+//         <linearGradient
+//           id="paint0_linear_87_1036"
+//           x1="634.27"
+//           y1="119.482"
+//           x2="78.6826"
+//           y2="356.622"
+//           gradientUnits="userSpaceOnUse"
+//         >
+//           <stop stop-color="#4D1CB6" />
+//           <stop offset="1" stop-color="#AC3A7F" />
+//         </linearGradient>
+//       </defs>
+//     </svg>
+//   </div>
+// );
+
+return <Widget src="mattlock.near/widget/bos-landing-page" props={{}} />;
