cbef3e2516c69cdbba53e992ce6a5cb2efe832b3
eugenewolf507.near <eugenewolf507.near>
2023-02-14 21:26:22 -0500

    Update Draft_CheckStructureCommentsIndex by eugenewolf507.near at 2023-02-14 21:26:22.405

--- eugenewolf507.near/Draft_CheckStructureCommentsIndex/source_code.jsx
+++ eugenewolf507.near/Draft_CheckStructureCommentsIndex/source_code.jsx
@@ -15,26 +15,50 @@ const getPublicationParamsAllArticles = () => ({
 const clickHandlerGetStructure = () => {
   const accountId = "mob.near";
 
-  const posts = Social.get(`${accountId}/post/main`, 84445357);
-  console.log("postsGet", posts);
+  console.log(" ========== GET ONE POST ========== ");
+  const post = Social.get(`${accountId}/post/main`, 84722911);
+  //84445357 - blockHeight put into post inside social get
+  console.log("postGet", post);
+  //   const postData = Social.get(`${accountId}/post/**`, "final");
+  //   console.log("postData", postData);
+  //   console.log(" ========== GET INDEX ARRAY FOR POSTS ========== ");
   const postsIndex = Social.index("post", "main", {
     limit: 10,
     order: "desc",
     accountId,
   });
   console.log(postsIndex);
-  console.log("==================");
+  //   console.log(" ========== GET 10 POSTS ========== ");
+  //   postsIndex.forEach(({ accountId, blockHeight }) => {
+  //     const postData = Social.get(`${accountId}/post/main`, blockHeight);
+  //     console.log(postData);
+  //   });
 
-  postsIndex.forEach(({ accountId, blockHeight }) => {
-    const postData = Social.get(`${accountId}/post/main`, blockHeight);
-    console.log(postData);
-    console.log(accountId, blockHeight);
-  });
+  console.log(" ========== GET SOME COMMENTS ========== ");
+  const item = {
+    type: "social",
+    path: `${accountId}/post/main`,
+    blockHeight: 84722911,
+  };
+  const index = {
+    action: "comment",
+    key: item,
+    options: {
+      limit: 3,
+      order: "desc",
+      accountId: "bosco.near",
+      subscribe: false,
+    },
+  };
+  const indexComments = Social.index(index.action, index.key, index.options);
+  console.log("indexComments", indexComments);
+  const comment1 = Social.get(`bosco.near/post/comment`, "final");
+  console.log(comment1);
 
-  //   const questionsIndex = Social.index("poll_question", "question-v3.1.0");
-  //   console.log("questionsIndex", questionsIndex);
-  //   const questionsGet = Social.get(`*/index/poll_question/`, "final");
-  //   console.log("questionsGet", questionsGet);
+  const comment2 = Social.get(`bosco.near/post/comment`, 84723425);
+  console.log(comment2);
+  const comment3 = Social.get(`bosco.near/post/comment`, 84723113);
+  console.log(comment3);
 };
 
 // ========= RETURN =========
