5389ce5d240d7e18d7fb881c58c7a92cef786502
roshaan.near <roshaan.near>
2023-02-16 17:40:46 -0500

    Update queryapi__IndexerFunctionEditor by roshaan.near at 2023-02-16 17:40:46.763

--- roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
+++ roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
@@ -1,33 +1,32 @@
-let initialText = '';
+let initialText = "";
 const indexer_function_name = props.indexer_name;
 const registry_contract_id =
-  props.registry_contract_id || 'registry.queryapi.near';
+  props.registry_contract_id || "registry.queryapi.near";
 let accountId = context.accountId;
 State.init({
   m: initialText,
 });
+
 if (!accountId) {
-  return 'Please sign in to use this widget.';
+  return "Please sign in to use this widget.";
 }
 
-let QueryIndexerCode = () => {
-  Near.view(registry_contract_id, 'register_indexer_function', {
+let queryIndexerCode = () => {
+  return Near.asyncView(registry_contract_id, "read_indexer_function", {
     name: indexer_function_name,
   });
 };
-let loadIndexerCode = QueryIndexerCode();
-
-if (loadIndexerCode !== undefined) {
-  console.log(loadIndexerCode);
-  state.update({ m: loadIndexerCode });
-}
+queryIndexerCode().then((data) => {
+  if (!data) return;
+  console.log(data, "data");
+  State.update({ m: data });
+});
 
 const code = `
-<iframe id="react-app-iframe" onload="test()" src="https://query-api-react.vercel.app/query-api-editor" width="1250px" height="500px"></iframe>
+<iframe id="react-app-iframe" onload="test()" src="http://localhost:3001/query-api-editor" width="1250px" height="500px"></iframe>
 <script>
 function test() {
 let receiverWindow = document.getElementById("react-app-iframe").contentWindow
-
 window.addEventListener("message", function(event){
      window.top.postMessage(event.data, "*");
 });
@@ -39,15 +38,15 @@ return (
   <div>
     <iframe
       className="w-100"
-      style={{ height: '500px' }}
+      style={{ height: "500px" }}
       srcDoc={code}
-      message={state.m}
+      message={stae.m}
       onMessage={(m) => State.update({ m })}
     />
     <Widget
-      src={'roshaan.near/widget/queryapi__RegisterIndexerFunctionButton'}
+      src={"roshaan.near/widget/queryapi__RegisterIndexerFunctionButton"}
       props={{
-        indexer_name: 'Roshaans_indexer',
+        indexer_name: "",
         indexer_code: state.m,
       }}
     />
