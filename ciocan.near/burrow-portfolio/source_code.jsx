8acb26c11336aebe5eb3d7a8fc81b011766e6055
ciocan <ciocan>
2023-02-16 15:41:16 -0500

    Update burrow-portfolio by ciocan.near at 2023-02-16 15:41:16.222

--- ciocan.near/burrow-portfolio/source_code.jsx
+++ ciocan.near/burrow-portfolio/source_code.jsx
@@ -10,22 +10,6 @@ const shrinkToken = (value, decimals, fixed) => {
   return new Big(value).div(new Big(10).pow(decimals)).toFixed(fixed);
 };
 
-const nFormat = (num, digits) => {
-  const lookup = [
-    { value: 1, symbol: "" },
-    { value: 1e3, symbol: "k" },
-    { value: 1e6, symbol: "M" },
-  ];
-  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-  var item = lookup
-    .slice()
-    .reverse()
-    .find((item) => num >= item.value);
-  return item
-    ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol
-    : "0";
-};
-
 const { assets, rewards, account } = state;
 
 const hasData = assets.length > 0 && rewards.length > 0;
@@ -37,19 +21,39 @@ const onLoad = (data) => {
 const suppliedAssets = hasData
   ? account.supplied.map((suppliedAsset) => {
       const asset = assets.find((a) => a.token_id === suppliedAsset.token_id);
-      console.log(asset, suppliedAsset);
       const r = rewards.find((a) => a.token_id === asset.token_id);
       const totalApy = r.apyBase + r.apyRewardTvl + r.apyReward;
 
       const decimals = asset.metadata.decimals + asset.config.extra_decimals;
       const deposited = Number(shrinkToken(suppliedAsset.balance, decimals));
-      const usd = (deposited * asset.price.usd).toFixed(4);
+      const usd = (deposited * asset.price.usd).toFixed(2);
 
       return (
         <tr>
           <td>{asset.metadata.symbol}</td>
           <td class="text-end">{toAPY(totalApy)}%</td>
           <td class="text-end">{deposited.toFixed(6)}</td>
+          <td class="text-end">${usd}</td>
+        </tr>
+      );
+    })
+  : undefined;
+
+const borrowedAssets = hasData
+  ? account.borrowed.map((borrowedAsset) => {
+      const asset = assets.find((a) => a.token_id === borrowedAsset.token_id);
+      const r = rewards.find((a) => a.token_id === asset.token_id);
+      const totalApy = r.apyBase + r.apyRewardTvl + r.apyReward;
+
+      const decimals = asset.metadata.decimals + asset.config.extra_decimals;
+      const borrowed = Number(shrinkToken(borrowedAsset.balance, decimals));
+      const usd = (borrowed * asset.price.usd).toFixed(2);
+
+      return (
+        <tr>
+          <td>{asset.metadata.symbol}</td>
+          <td class="text-end">{toAPY(totalApy)}%</td>
+          <td class="text-end">{borrowed.toFixed(6)}</td>
           <td class="text-end">${usd}</td>
         </tr>
       );
@@ -81,6 +85,28 @@ return (
         </tr>
       </thead>
       <tbody>{suppliedAssets}</tbody>
+    </table>
+
+    <table class="table">
+      <thead>
+        <tr
+          style={{
+            color: "rgba(0, 0, 0, 0.4)",
+          }}
+        >
+          <th scope="col">Borrowed Assets</th>
+          <th scope="col" class="text-end">
+            APY
+          </th>
+          <th scope="col" class="text-end">
+            Deposited
+          </th>
+          <th scope="col" class="text-end">
+            $
+          </th>
+        </tr>
+      </thead>
+      <tbody>{borrowedAssets}</tbody>
     </table>
   </div>
 );
