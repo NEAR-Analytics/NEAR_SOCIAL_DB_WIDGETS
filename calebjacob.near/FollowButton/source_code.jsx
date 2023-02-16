073927f7b839e74bac17647f8fff36d812547fc4
calebjacob <calebjacob>
2023-02-15 21:30:28 -0500

    Update FollowButton by calebjacob.near at 2023-02-15 21:30:28.582

---
+++ calebjacob.near/FollowButton/source_code.jsx
@@ -0,0 +1,84 @@
+if (
+  !props.accountId ||
+  !context.accountId ||
+  context.accountId === props.accountId
+) {
+  return "";
+}
+
+const followEdge = Social.keys(
+  `${context.accountId}/graph/follow/${props.accountId}`,
+  undefined,
+  {
+    values_only: true,
+  }
+);
+
+const inverseEdge = Social.keys(
+  `${props.accountId}/graph/follow/${context.accountId}`,
+  undefined,
+  {
+    values_only: true,
+  }
+);
+
+const loading = followEdge === null || inverseEdge === null;
+const follow = followEdge && Object.keys(followEdge).length;
+const inverse = inverseEdge && Object.keys(inverseEdge).length;
+
+const type = follow ? "unfollow" : "follow";
+
+const data = {
+  graph: { follow: { [props.accountId]: follow ? null : "" } },
+  index: {
+    graph: JSON.stringify({
+      key: "follow",
+      value: {
+        type,
+        accountId: props.accountId,
+      },
+    }),
+    notify: JSON.stringify({
+      key: props.accountId,
+      value: {
+        type,
+      },
+    }),
+  },
+};
+
+const Wrapper = styled.div`
+  .follow-button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    padding: 8px 16px;
+    height: 32px;
+    border-radius: 100px;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 15px;
+    text-align: center;
+    cursor: pointer;
+    background: #FBFCFD;
+    color: #006ADC;
+    border: 1px solid #D7DBDF;
+    width: 100%;
+
+    &:hover,
+    &:focus {
+      background: #ECEDEE;
+      text-decoration: none;
+      outline: none;
+    }
+  }
+`;
+
+return (
+  <Wrapper>
+    <CommitButton disabled={loading} className="follow-button" data={data}>
+      {follow ? "Following" : inverse ? "Follow Back" : "Follow"}
+    </CommitButton>
+  </Wrapper>
+);
