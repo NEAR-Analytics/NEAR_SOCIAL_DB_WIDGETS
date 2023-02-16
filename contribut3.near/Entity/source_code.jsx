c6fb399df099325731640db2a10237b687c172e6
contribut3.near <contribut3.near>
2023-02-16 13:32:19 -0500

    Update Entity by contribut3.near at 2023-02-16 13:32:19.585

--- contribut3.near/Entity/source_code.jsx
+++ contribut3.near/Entity/source_code.jsx
@@ -211,7 +211,10 @@ const footer = (
       <span className="ms-1">{founderProfile.name}</span>
     </div>
     <div className="ms-3">
-      <i className="bi-play" style={{ width: "1.5em", height: "1.5em" }} />
+      <i
+        className="bi-play d-inline-block"
+        style={{ width: "1.5em", height: "1.5em" }}
+      />
       <span className="ms-1">{entity.status}</span>
     </div>
   </div>
