2944987c45f26f00336bb7bd41de2aa0582a586c
contribut3.near <contribut3.near>
2023-02-16 12:33:37 -0500

    Update Entity by contribut3.near at 2023-02-16 12:33:37.667

--- contribut3.near/Entity/source_code.jsx
+++ contribut3.near/Entity/source_code.jsx
@@ -155,7 +155,10 @@ const body = (
     <div className="d-flex flex-row justify-content-start">
       <div>{circle}</div>
       <div className="d-flex flex-column justify-content-between align-items-start">
-        <div></div>
+        <div>
+          <b>{name}</b>
+          <span className="text-muted">@{accountId}</span>
+        </div>
       </div>
     </div>
     <div>Name: {entity.name}</div>
