65e3f4b7fcddaf57391d4c487068144aede615b6
contribut3.near <contribut3.near>
2023-02-16 11:32:37 -0500

    Update Dashboard by contribut3.near at 2023-02-16 11:32:37.225

--- contribut3.near/Dashboard/source_code.jsx
+++ contribut3.near/Dashboard/source_code.jsx
@@ -89,7 +89,7 @@ return (
             <span>Requests</span>
           </label>
         </div>
-        <div className="w-25 col-12 col-md-10 col-lg-8">
+        <div className="w-25 col-12 col-md-10 col-lg-8 mt-3">
           <div className="card card-sm">
             <div className="card-body row p-0 ps-2 align-items-center">
               <div className="col-auto pe-0 me-0">
