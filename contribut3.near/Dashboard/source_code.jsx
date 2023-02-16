b8cab689f2c1b3baa406f421db55516b0b195056
contribut3.near <contribut3.near>
2023-02-16 11:05:57 -0500

    Update Dashboard by contribut3.near at 2023-02-16 11:05:57.680

--- contribut3.near/Dashboard/source_code.jsx
+++ contribut3.near/Dashboard/source_code.jsx
@@ -24,13 +24,14 @@ return (
               <span>Contribution request</span>
             </a>
           </li>
-          <li><hr className="dropdown-divider"</li>
+          <li><hr className="dropdown-divider" /></li>
           <li>
             <a className="dropdown-item">
               <i className="bi-boxes" />
               <span>Project</span>
             </a>
           </li>
+          <li><hr className="dropdown-divider"</li>
           <li>
             <a className="dropdown-item">
               <i className="bi-diagram-2" />
