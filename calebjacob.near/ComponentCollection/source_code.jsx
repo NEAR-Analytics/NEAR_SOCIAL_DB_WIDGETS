cbf24fb0e03e0b3b35a0bccf5559635d997e8133
calebjacob <calebjacob>
2023-02-16 18:39:42 -0500

    Update ComponentCollection by calebjacob.near at 2023-02-16 18:39:42.679

---
+++ calebjacob.near/ComponentCollection/source_code.jsx
@@ -0,0 +1,49 @@
+const accountId = props.accountId || context.accountId;
+
+if (!accountId) return "";
+
+let components = [];
+
+const data = Social.keys(`${accountId}/widget/*`, "final", {
+  return_type: "BlockHeight",
+});
+
+if (data) {
+  components = [];
+
+  Object.keys(data).forEach((accountId) => {
+    return Object.keys(data[accountId].widget).forEach((widgetName) => {
+      components.push({
+        accountId,
+        widgetName,
+        blockHeight: data[accountId].widget[widgetName],
+      });
+    });
+  });
+
+  components.sort((a, b) => b.blockHeight - a.blockHeight);
+}
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 24px;
+`;
+
+const CardWrapper = styled.div``;
+
+return (
+  <Wrapper>
+    {components.map((component, i) => (
+      <CardWrapper key={i}>
+        <Widget
+          src="calebjacob.near/widget/ComponentCard"
+          props={{
+            src: `${component.accountId}/widget/${component.widgetName}`,
+            tagsAsSubtitle: true,
+          }}
+        />
+      </CardWrapper>
+    ))}
+  </Wrapper>
+);
