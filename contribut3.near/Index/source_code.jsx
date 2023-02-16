b3f89991965c207d772b66fa33b87f42e37c566e
contribut3.near <contribut3.near>
2023-02-16 12:00:39 -0500

    Update Index by contribut3.near at 2023-02-16 12:00:39.314

--- contribut3.near/Index/source_code.jsx
+++ contribut3.near/Index/source_code.jsx
@@ -218,7 +218,7 @@ return (
       />
     </div>
     <div className="vr mx-2" />
-    <div className="w-full">
+    <div className="w-100">
       {tabContent}
       {/* {controls} */}
       {/* {navbar} */}
