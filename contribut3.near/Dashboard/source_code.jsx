fa7022f5de8537e1cae0f8ea324242e1b29e7c28
contribut3.near <contribut3.near>
2023-02-16 11:06:59 -0500

    Update Dashboard by contribut3.near at 2023-02-16 11:06:59.153

--- contribut3.near/Dashboard/source_code.jsx
+++ contribut3.near/Dashboard/source_code.jsx
@@ -2,47 +2,49 @@ const ownerId = "contribut3.near";
 
 return (
   <div>
-    <div className="d-flex flex-row justify-content-between">
-      <div>
-        <h1 className="fs-2">Dashboard</h1>
-        <p className="fw-semibold fs-5 text-muted">
-          Find projects, contributors or requests
-        </p>
-      </div>
-      <div className="dropdown">
-        <a
-          className="btn btn-info dropdown-toggle"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Create new...
-        </a>
-        <ul className="dropdown-menu">
-          <li>
-            <a className="dropdown-item">
-              <i className="bi-ui-checks-grid" />
-              <span>Contribution request</span>
-            </a>
-          </li>
-          <li>
-            <hr className="dropdown-divider" />
-          </li>
-          <li>
-            <a className="dropdown-item">
-              <i className="bi-boxes" />
-              <span>Project</span>
-            </a>
-          </li>
-          <li>
-            <hr className="dropdown-divider" />
-          </li>
-          <li>
-            <a className="dropdown-item">
-              <i className="bi-diagram-2" />
-              <span>Organization</span>
-            </a>
-          </li>
-        </ul>
+    <div>
+      <div className="d-flex flex-row justify-content-between">
+        <div>
+          <h1 className="fs-2">Dashboard</h1>
+          <p className="fw-semibold fs-5 text-muted">
+            Find projects, contributors or requests
+          </p>
+        </div>
+        <div className="dropdown">
+          <a
+            className="btn btn-info dropdown-toggle"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Create new...
+          </a>
+          <ul className="dropdown-menu">
+            <li>
+              <a className="dropdown-item">
+                <i className="bi-ui-checks-grid" />
+                <span>Contribution request</span>
+              </a>
+            </li>
+            <li>
+              <hr className="dropdown-divider" />
+            </li>
+            <li>
+              <a className="dropdown-item">
+                <i className="bi-boxes" />
+                <span>Project</span>
+              </a>
+            </li>
+            <li>
+              <hr className="dropdown-divider" />
+            </li>
+            <li>
+              <a className="dropdown-item">
+                <i className="bi-diagram-2" />
+                <span>Organization</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
