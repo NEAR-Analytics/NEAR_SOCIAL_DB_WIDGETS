53d18d023831604dfeb57ed49f5ba6e6e8f0a4d6
contribut3.near <contribut3.near>
2023-02-16 09:29:55 -0500

    Update Sidebar by contribut3.near at 2023-02-16 09:29:55.571

--- contribut3.near/Sidebar/source_code.jsx
+++ contribut3.near/Sidebar/source_code.jsx
@@ -3,26 +3,32 @@ const ownerId = "contribut3.near";
 return (
   <div className="d-flex flex-column">
     <a>
-      <i className="bi-" />
+      <i className="bi-house" />
       <span>Dashboard</span>
     </a>
     <a>
+      <i className="bi-" />
       <span>Profile</span>
     </a>
     <a>
+      <i className="bi-" />
       <span>My projects</span>
     </a>
     <a>
+      <i className="bi-" />
       <span>My organizations</span>
     </a>
     <a>
+      <i className="bi-" />
       <span>My invites</span>
     </a>
     <a>
+      <i className="bi-" />
       <span>My contributions</span>
     </a>
     <hr className="border-2" />
     <a>
+      <i className="bi-" />
       <span>About this app</span>
     </a>
   </div>
