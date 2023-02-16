eec57bb84b089f750e5f86efb0e3dd5f10717f42
contribut3.near <contribut3.near>
2023-02-16 10:38:08 -0500

    Update Sidebar by contribut3.near at 2023-02-16 10:38:08.153

--- contribut3.near/Sidebar/source_code.jsx
+++ contribut3.near/Sidebar/source_code.jsx
@@ -23,17 +23,43 @@ return (
         Web3 Combinator
       </h4>
     </a>
-    {navItem({ text: "Dashboard", link: "#", icon: "bi-house" })}
-    {navItem({ text: "Profile", link: "#", icon: "bi-person" })}
-    {navItem({ text: "My projects", link: "#", icon: "bi-boxes" })}
-    {navItem({ text: "My organizations", link: "#", icon: "bi-diagram-2" })}
-    {navItem({ text: "My invites", link: "#", icon: "bi-envelope" })}
+    {navItem({
+      text: "Dashboard",
+      link: "#",
+      icon: "bi-house",
+      id: "dashboard",
+    })}
+    {navItem({ text: "Profile", link: "#", icon: "bi-person", id: "profile" })}
+    {navItem({
+      text: "My projects",
+      link: "#",
+      icon: "bi-boxes",
+      id: "projects",
+    })}
+    {navItem({
+      text: "My organizations",
+      link: "#",
+      icon: "bi-diagram-2",
+      id: "orgs",
+    })}
+    {navItem({
+      text: "My invites",
+      link: "#",
+      icon: "bi-envelope",
+      id: "invites",
+    })}
     {navItem({
       text: "My contributions",
       link: "#",
       icon: "bi-ui-checks-grid",
+      id: "contributions",
     })}
     <hr className="border-2" />
-    {navItem({ text: "About this app", link: "#", icon: "bi-info-square" })}
+    {navItem({
+      text: "About this app",
+      link: "#",
+      icon: "bi-info-square",
+      id: "about",
+    })}
   </div>
 );
