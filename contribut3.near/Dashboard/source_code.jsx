305685fdca6b75215aa7463350712a1c3a889b05
contribut3.near <contribut3.near>
2023-02-16 10:59:37 -0500

    Update Dashboard by contribut3.near at 2023-02-16 10:59:37.514

--- contribut3.near/Dashboard/source_code.jsx
+++ contribut3.near/Dashboard/source_code.jsx
@@ -2,7 +2,7 @@ const ownerId = "contribut3.near";
 
 return (
   <div>
-    <div>
+    <div className="d-flex flex-row justify-content-between">
       <div>
         <h1 className="fs-2">Dashboard</h1>
         <p className="fw-semibold fs-5 text-muted">
