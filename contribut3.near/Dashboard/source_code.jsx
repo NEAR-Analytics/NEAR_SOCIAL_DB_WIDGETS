6b34d2af49ddf58c79f1f782af08f0e0fca5ffab
contribut3.near <contribut3.near>
2023-02-16 10:58:42 -0500

    Update Dashboard by contribut3.near at 2023-02-16 10:58:42.323

--- contribut3.near/Dashboard/source_code.jsx
+++ contribut3.near/Dashboard/source_code.jsx
@@ -2,9 +2,11 @@ const ownerId = "contribut3.near";
 
 return (
   <div>
-    <h1 className="fs-2">Dashboard</h1>
-    <p className="fw-semibold fs-5 text-muted">
-      Find projects, contributors or requests
-    </p>
+    <div>
+      <h1 className="fs-2">Dashboard</h1>
+      <p className="fw-semibold fs-5 text-muted">
+        Find projects, contributors or requests
+      </p>
+    </div>
   </div>
 );
