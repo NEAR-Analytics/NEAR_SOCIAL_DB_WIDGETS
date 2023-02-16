955583e66f91c92323a33be0a9378c88233a36de
contribut3.near <contribut3.near>
2023-02-16 10:45:19 -0500

    Update Index by contribut3.near at 2023-02-16 10:45:19.704

--- contribut3.near/Index/source_code.jsx
+++ contribut3.near/Index/source_code.jsx
@@ -206,7 +206,7 @@ const content = {
 
 return (
   <div className="d-flex flex-row">
-    <div>
+    <div className="px-1">
       <Widget src={`${ownerId}/widget/Sidebar`} props={{ tab: props.tab }} />
     </div>
     <div className="vr mx-2" />
