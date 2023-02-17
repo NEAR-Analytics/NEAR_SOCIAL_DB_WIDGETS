75f2e8b760e0b8faf6d6f0dcc52fc28e450778f6
eugenewolf507.near <eugenewolf507.near>
2023-02-17 14:42:45 -0500

    Update WikiOnSocialDB_OneArticle by eugenewolf507.near at 2023-02-17 14:42:45.705

--- eugenewolf507.near/WikiOnSocialDB_OneArticle/source_code.jsx
+++ eugenewolf507.near/WikiOnSocialDB_OneArticle/source_code.jsx
@@ -1,29 +1,54 @@
-const addressForArticles = "wikiTest";
+const addressForArticles = "wikiTest2Article";
 const authorForWidget = "eugenewolf507.near";
-const { articleId } = props;
-State.init({});
+const { articleId, blockHeight, lastEditor } = props;
 
-const allArticlesWithOneID = Social.get(
-  `*/${addressForArticles}/articles/${articleId}/*`,
-  "final"
-);
-const articlesArr = allArticlesWithOneID && Object.values(allArticlesWithOneID);
-const resultArticlesWithOneId =
-  articlesArr &&
-  articlesArr.reduce(
-    (acc, account) =>
-      acc.concat(Object.values(account[addressForArticles].articles)),
-    []
+// === *** NEW DATA BASE *** ===
+const getNewDBHandler = (e) => {
+  const lastEditor = "eugenewolf507.near",
+    blockHeight = 85380376;
+  // blockHeight = 85347668;
+
+  const article = Social.get(
+    `${lastEditor}/${addressForArticles}/main`,
+    blockHeight
   );
 
-resultArticlesWithOneId.length &&
-  resultArticlesWithOneId.sort((a, b) => {
-    return Number(b.timeLastEdit) - Number(a.timeLastEdit);
-  });
+  console.log(JSON.parse(article));
+};
 
-const article = resultArticlesWithOneId[0];
+const article = Social.get(
+  `${lastEditor}/${addressForArticles}/main`,
+  blockHeight
+);
 
-State.update({ article });
+State.init({});
+
+// const allArticlesWithOneID = Social.get(
+//   `*/${addressForArticles}/articles/${articleId}/*`,
+//   "final"
+// );
+// const articlesArr = allArticlesWithOneID && Object.values(allArticlesWithOneID);
+// const resultArticlesWithOneId =
+//   articlesArr &&
+//   articlesArr.reduce(
+//     (acc, account) =>
+//       acc.concat(Object.values(account[addressForArticles].articles)),
+//     []
+//   );
+
+// resultArticlesWithOneId.length &&
+//   resultArticlesWithOneId.sort((a, b) => {
+//     return Number(b.timeLastEdit) - Number(a.timeLastEdit);
+//   });
+
+// const article = resultArticlesWithOneId[0];
+
+const article = Social.get(
+  `${lastEditor}/${addressForArticles}/main`,
+  blockHeight
+);
+const jArticle = JSON.parse(article);
+State.update({ jArticle });
 
 const getDate = (timestamp) => {
   const date = new Date(Number(timestamp));
@@ -47,6 +72,9 @@ const saveArticle = (args) => {
 
 return (
   <>
+    {/* === *** NEW DATA BASE *** === */}
+    <button onClick={getNewDBHandler}>GET Data - delete this button</button>
+    {/* === *** NEW DATA BASE *** === */}
     <Widget
       src={`${authorForWidget}/widget/WikiOnSocialDB_MainNavigation`}
       props={{ currentNavPill: "articles" }}
