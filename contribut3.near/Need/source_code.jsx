cf25507ac7c4253005060b0e06486be0dd44850e
contribut3.near <contribut3.near>
2023-02-17 14:09:35 -0500

    Update Need by contribut3.near at 2023-02-17 14:09:35.682

--- contribut3.near/Need/source_code.jsx
+++ contribut3.near/Need/source_code.jsx
@@ -2,6 +2,7 @@ const ownerId = "contribut3.near";
 const accountId = props.accountId;
 const notStandalone = props.notStandalone ?? false;
 const isPreview = props.isPreview ?? false;
+const cid = props.cid;
 
 if (!accountId || !cid) {
   return "Cannot render contribution need widget without account ID or CID!";
