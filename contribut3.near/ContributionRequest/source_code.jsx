379b1b84bea27f8c8b8a51d0df89877825407189
contribut3.near <contribut3.near>
2023-02-17 15:21:59 -0500

    Update ContributionRequest by contribut3.near at 2023-02-17 15:21:59.850

--- contribut3.near/ContributionRequest/source_code.jsx
+++ contribut3.near/ContributionRequest/source_code.jsx
@@ -40,71 +40,8 @@ const description = isPreview
 
 const descriptionArea = <Markdown text={description} />;
 
-const startDateInput = (
-  <div className="col-lg-6 mb-2">
-    Start date of contribution (optional):
-    <input
-      type="date"
-      value={state.startDate}
-      onChange={(e) => State.update({ startDate: e.target.value })}
-    />
-  </div>
-);
-
-const descriptionDiv = (
-  <div className="col-lg-12  mb-2">
-    Updated description (optional):
-    <br />
-    <textarea
-      value={state.description}
-      type="text"
-      rows={6}
-      className="form-control"
-      onChange={(event) => State.update({ description: event.target.value })}
-    />
-  </div>
-);
-
-const body = (
-  <div className="card-body">
-    {postTitle}
-    {descriptionArea}
-  </div>
-);
-
-const footer =
-  !isAuthorized || isPreview ? null : (
-    <div className="card-footer">
-      <div>
-        {startDateInput}
-        {descriptionDiv}
-        <a
-          className="btn btn-outline-primary mb-2"
-          onClick={() => {
-            const args = {
-              entity_id: entityId,
-              contributor_id: contributorId,
-            };
-
-            if (state.description) {
-              args.description = state.description;
-            }
-
-            if (state.startDate) {
-              args.start_date = state.startDate;
-            }
-
-            Near.call(ownerId, "approve_contribution", args);
-          }}
-        >
-          Approve
-        </a>
-      </div>
-    </div>
-  );
-
 return (
-  <div className={`card my-2`}>
+  <div className="card">
     {header}
     {body}
     {footer}
