807e8bcf512298dc57a577cb160397b2ec791a68
contribut3.near <contribut3.near>
2023-02-17 14:35:27 -0500

    Update Inbox by contribut3.near at 2023-02-17 14:35:27.679

--- contribut3.near/Inbox/source_code.jsx
+++ contribut3.near/Inbox/source_code.jsx
@@ -14,11 +14,64 @@ const header = (
   </div>
 );
 
+const contentSelectButton = ({ id, text, icon }) => (
+  <a
+    className={`btn ${state.content === id ? "btn-secondary" : "btn-outline-secondary"
+      }`}
+    href={`https://near.social/#/${ownerId}/widget/Index?tab=dashboard&content=${id}${props.search ? "&search=" + props.search : ""
+      }`}
+    onClick={() => State.update({ content: id })}
+  >
+    <i className={icon} />
+    <span>{text}</span>
+  </a>
+);
+
+const contentSelector = (
+  <div className="btn-group" role="group" aria-label="Content Tab Selector">
+    {contentSelectButton({
+      id: "proposals",
+      text: "Proposals",
+      icon: "bi-person-plus",
+    })}
+    {contentSelectButton({
+      id: "invitations",
+      text: "Invitations",
+      icon: "bi-person-up",
+    })}
+  </div>
+);
+
+const searchBar = (
+  <div className="w-25 col-12 col-md-10 col-lg-8">
+    <div className="card card-sm">
+      <div className="card-body row p-0 ps-2 align-items-center">
+        <div className="col-auto pe-0 me-0">
+          <i className="bi-search" />
+        </div>
+        <div className="col ms-0">
+          <input
+            className="form-control border-0"
+            type="search"
+            value={state.search}
+            placeholder="Search"
+            onChange={(e) => State.update({ search: e.target.value })}
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
 return (
   <div>
     <div className="mb-3 px-3">
       <div className="d-flex flex-row justify-content-between mb-3">
         {header}
+      </div>
+      <div className="d-flex flex-row justify-content-between">
+        {contentSelector}
+        {searchBar}
       </div>
     </div>
     <div className="px-3 pt-3"></div>
