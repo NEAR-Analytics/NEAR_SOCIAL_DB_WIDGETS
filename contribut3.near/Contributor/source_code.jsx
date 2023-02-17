946ecd5e77b4102c65863e07393f97818da172cd
contribut3.near <contribut3.near>
2023-02-17 13:31:34 -0500

    Update Contributor by contribut3.near at 2023-02-17 13:31:34.948

--- contribut3.near/Contributor/source_code.jsx
+++ contribut3.near/Contributor/source_code.jsx
@@ -7,120 +7,138 @@ if (!accountId) {
   return "Cannot show contributor without account ID!";
 }
 
-const shareButton = props.isPreview ? null : (
-  <a
-    className="card-link"
-    href={`https://near.social/#/${ownerId}/widget/Contributor?accountId=${accountId}`}
-    role="button"
-    target="_blank"
-    title="Open in new tab"
-  >
-    <div className="bi bi-share" />
-  </a>
-);
-
-if (notStandalone) {
-  return (
-    <div className="card">
-      <div className="card-body">
-        <div className="row justify-content-between">
-          <div className="col-4">
-            <Widget src={`mob.near/widget/ProfileLine`} props={{ accountId }} />
-          </div>
-          <div className="col-5">
-            <div className="d-flex justify-content-end">{shareButton}</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-const contributor = isPreview
+const conrtibutor = isPreview
   ? props.contributor
-  : Near.view(
-      ownerId,
-      "get_contributor",
-      { account_id: accountId },
-      "final",
-      true
-    );
+  : Near.view(ownerId, "get_contributor", { account_id: accountId }, "final");
 
-if (!contributor) {
+if (!entity) {
   return isPreview
-    ? "You must provide a contributor object in preview mode!"
+    ? "You must provide a contributor object in preview mode"
     : "Loading...";
 }
 
-const contributions = Near.view(
-  ownerId,
-  "get_contributor_contributions",
-  { account_id: accountId },
-  "final",
-  true
-);
+const profile = Social.getr(`${accountId}/profile`);
 
-const contributionsList = notStandalone ? null : (
-  <div className="mb-2">
-    Contributions:
-    <br />
-    {!contributions ? (
-      <div>Loading...</div>
-    ) : (
-      Object.keys(contributions).map((entityId) => (
-        <Widget
-          src={`${ownerId}/widget/Contribution`}
-          props={{ entityId, contributorId: accountId, id: entityId }}
-        />
-      ))
-    )}
+const tags = Object.keys(profile.tags ?? {});
+const image = profile.image;
+const url =
+  (image.ipfs_cid
+    ? `https://ipfs.near.social/ipfs/${image.ipfs_cid}`
+    : image.url) || "https://thewiki.io/static/media/sasha_anon.6ba19561.png";
+
+const circle = (
+  <div
+    className="profile-circle d-inline-block"
+    title={`${profile.name} @${accountId}`}
+    style={{ width: "4em", height: "4em" }}
+  >
+    <img
+      className="rounded-circle w-100 h-100"
+      style={{ objectFit: "cover" }}
+      src={`https://i.near.social/thumbnail/${url}`}
+      alt="profile image"
+    />
   </div>
 );
 
-const header = (
-  <div className="card-header">
-    <small className="text-muted">
-      <div className="row justify-content-between">
-        <div className="col-4">
-          <Widget src={`mob.near/widget/ProfileLine`} props={{ accountId }} />
-        </div>
-        <div className="col-5">
-          <div className="d-flex justify-content-end">{shareButton}</div>
-        </div>
-      </div>
-    </small>
+const [[founder]] = (contributions ?? []).filter((contribution) => {
+  const [_, details] = contribution;
+  const all = [...details.history, details.current];
+  return all.some((detail) => detail.description === "");
+});
+
+const founderProfile = Social.getr(`${founder}/profile`);
+const founderImage = profile.image;
+const founderImageUrl =
+  (founderImage.ipfs_cid
+    ? `https://ipfs.near.social/ipfs/${founderImage.ipfs_cid}`
+    : founderImage.url) ||
+  "https://thewiki.io/static/media/sasha_anon.6ba19561.png";
+
+const founderCircle = (
+  <div
+    className="profile-circle d-inline-block"
+    title={`${founderProfile.name} @${founder}`}
+    style={{ width: "1.5em", height: "1.5em" }}
+  >
+    <img
+      className="rounded-circle w-100 h-100"
+      style={{ objectFit: "cover" }}
+      src={`https://i.near.social/thumbnail/${founderImageUrl}`}
+      alt="profile image"
+    />
   </div>
 );
 
 const body = (
-  <div className="card-body">
-    <div className="mb-2">
-      Contribution types:
-      <ul>
-        {contributor.contribution_types.map((ct) => (
-          <li>{typeof ct !== "string" ? `Other: ${ct.Other}` : ct}</li>
-        ))}
-      </ul>
+  <div
+    className="d-flex flex-row justify-content-start"
+    id={accountId}
+    style={{ minHeight: "10em" }}
+  >
+    <div className="flex-grow-1 py-3">
+      <div className="d-flex flex-row justify-content-start">
+        <div className="m-2">{circle}</div>
+        <div className="d-flex flex-column justify-content-between align-items-start w-100">
+          <div className="w-100 d-flex flex-row justify-content-between align-items-start">
+            <div>
+              <b>{name}</b>
+              <span className="text-muted">@{accountId}</span>
+            </div>
+            <div className="text-success">
+              <i className="bi-play" />
+              <span className="ms-1">{entity.status}</span>
+            </div>
+          </div>
+          <div className="d-flex flex-row justify-content-start align-items-center my-1">
+            {founderCircle}
+            <span className="mx-1">{founderProfile.name}</span>
+            <span className="text-muted">@{founder}</span>
+          </div>
+          <div className="text-truncate text-muted">
+            {tags.length > 0 ? (
+              <>
+                {tags.map((tag) => (
+                  <span
+                    className="d-inline-block mx-1 py-1 px-2 badge border border-secondary text-secondary text-muted text-center"
+                    key={tag}
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
+          <div className="text-truncate my-2">{profile.description}</div>
+        </div>
+      </div>
     </div>
-    <div className="mb-2">
-      Skills:
-      <ul>
-        {contributor.skills.map((s) => (
-          <li>{s}</li>
-        ))}
-      </ul>
+    <div className="vr mx-3" />
+    <div className="d-flex flex-row justify-content-end align-items-start py-3">
+      <a
+        className="btn btn-outline-secondary me-2"
+        href={`https://near.social/#/${ownerId}/widget/Entity?accountId=${accountId}`}
+      >
+        View details
+      </a>
+      <a className="btn btn-outline-secondary">
+        <i className="bi-box-arrow-up-right" />
+      </a>
     </div>
-    <div className="mb-2">
-      Resume:
-      <p>{contributor.resume}</p>
-    </div>
-    {contributionsList}
   </div>
 );
 
 return (
   <div className="card">
-    {header}
-    {body}
+    <div className="card-body px-3 py-0">
+      {body}
+      {details}
+      {needForm}
+      {contributionsList}
+      {requestsList}
+      {inviteList}
+    </div>
   </div>
 );
