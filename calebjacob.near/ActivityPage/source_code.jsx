c66d78bdd1745c9944ae26527a0ff3a231997838
calebjacob <calebjacob>
2023-02-16 23:08:17 -0500

    Update ActivityPage by calebjacob.near at 2023-02-16 23:08:17.430

--- calebjacob.near/ActivityPage/source_code.jsx
+++ calebjacob.near/ActivityPage/source_code.jsx
@@ -17,7 +17,7 @@ const Wrapper = styled.div`
 
 const Main = styled.div`
   display: grid;
-  grid-template-columns: 300px minmax(0, 1fr) 300px;
+  grid-template-columns: 290px minmax(0, 1fr) 290px;
   grid-gap: 16px;
 
   @media (max-width: 1200px) {
