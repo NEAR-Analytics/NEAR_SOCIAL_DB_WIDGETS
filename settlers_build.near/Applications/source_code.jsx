974f27183cec2aaa91e886654555eb35a871abc7
settlers_build.near <settlers_build.near>
2023-02-17 01:00:07 -0500

    Update Applications by settlers_build.near at 2023-02-17 01:00:07.807

---
+++ settlers_build.near/Applications/source_code.jsx
@@ -0,0 +1,49 @@
+return (
+  <div>
+    <h5>Applications</h5>
+    <div className="mb-2">
+      <Widget
+        src="mob.near/widget/ComponentSearch"
+        props={{
+          boostedTag: "app",
+          placeholder: "🔍 Search Applications",
+          limit: 10,
+          onChange: ({ result }) => {
+            State.update({ apps: result });
+          },
+        }}
+      />
+    </div>
+    {state.apps && (
+      <div className="mb-2">
+        {state.apps.map((app, i) => (
+          <div key={i}>
+            <Widget
+              src="mob.near/widget/ComponentSearch.Item"
+              props={{
+                link: `#/${app.widgetSrc}`,
+                accountId: app.accountId,
+                widgetName: app.widgetName,
+                onHide: () => State.update({ apps: null }),
+                extraButtons: ({ widgetPath }) => (
+                  <a
+                    target="_blank"
+                    className="btn btn-outline-secondary"
+                    href={`#/mob.near/widget/WidgetSource?src=${widgetPath}`}
+                  >
+                    Source
+                  </a>
+                ),
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    )}
+
+    <Widget
+      src="mob.near/widget/WidgetIcons"
+      props={{ tag: "dev", limit: 24 }}
+    />
+  </div>
+);
