c2bcf25857c11103acafdaf0770c1d6e76b4bcc8
contribut3.near <contribut3.near>
2023-02-17 15:19:42 -0500

    Update ContributionRequest by contribut3.near at 2023-02-17 15:19:42.377

--- contribut3.near/ContributionRequest/source_code.jsx
+++ contribut3.near/ContributionRequest/source_code.jsx
@@ -24,9 +24,9 @@ const isAuthorized = !!contributor && contributor.permissions.includes("Admin");
 const contributionRequest = props.isPreview
   ? props.contributionRequest
   : Near.view(ownerId, "get_contribution_request", {
-      entity_id: entityId,
-      contributor_id: contributorId,
-    });
+    entity_id: entityId,
+    contributor_id: contributorId,
+  });
 
 if (!contributionRequest) {
   return props.isPreview
@@ -34,53 +34,11 @@ if (!contributionRequest) {
     : "Loading...";
 }
 
-const shareButton = props.isPreview ? null : (
-  <a
-    className="card-link"
-    href={`https://near.social/#/${ownerId}/widget/ContributionRequest?entityId=${entityId}&contributorId=${contributorId}`}
-    role="button"
-    target="_blank"
-    title="Open in new tab"
-  >
-    <div className="bi bi-share" />
-  </a>
-);
-
-const header = (
-  <div className="card-header">
-    <small className="text-muted">
-      <div className="row justify-content-between">
-        <div className="col-4">
-          <Widget
-            src={`mob.near/widget/ProfileLine`}
-            props={{ accountId: contributorId }}
-          />
-        </div>
-        <div className="col-5">
-          <div className="d-flex justify-content-end">{shareButton}</div>
-        </div>
-      </div>
-    </small>
-  </div>
-);
-
-const entityLink = (
-  <Widget src={`mob.near/widget/ProfileLine`} props={{ accountId: entityId }} />
-);
-
-const postTitle = (
-  <h5 className="card-title">
-    <div className="row justify-content-between">
-      <div className="col-9">Contribution Request for {entityLink}</div>
-    </div>
-  </h5>
-);
-
 const description = isPreview
   ? props.contributionRequest.description
   : contributionRequest.description;
 
-const descriptionArea = <Markdown className="card-text" text={description} />;
+const descriptionArea = <Markdown text={description} />;
 
 const startDateInput = (
   <div className="col-lg-6 mb-2">
