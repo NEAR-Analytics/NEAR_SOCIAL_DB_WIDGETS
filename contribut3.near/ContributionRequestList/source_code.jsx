70c6a080a57cdbf406397ba10eff7c3ea7be5c12
contribut3.near <contribut3.near>
2023-02-17 15:39:13 -0500

    Update ContributionRequestList by contribut3.near at 2023-02-17 15:39:13.494

--- contribut3.near/ContributionRequestList/source_code.jsx
+++ contribut3.near/ContributionRequestList/source_code.jsx
@@ -10,17 +10,17 @@ const requests = Near.view(
   true
 );
 
-const allNeeds = requests.filter(([contributorId]) =>
+const allRequests = requests.filter(([contributorId]) =>
   contributorId.includes(search)
 );
 
-if (!allNeeds || allNeeds.length === 0) {
+if (!allRequests || allRequests.length === 0) {
   return "No contribution needs found!";
 }
 
 return (
   <>
-    {allNeeds.map(([accountId, cid]) => (
+    {allRequests.map(([accountId, cid]) => (
       <div key={cid} className="mb-2">
         <Widget src={`${ownerId}/widget/Need`} props={{ accountId, cid }} />
       </div>
