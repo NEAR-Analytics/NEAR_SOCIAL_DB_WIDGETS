71d4e33376269e112f11586fdec1bea31e0549f1
roshaan.near <roshaan.near>
2023-02-17 17:31:12 -0500

    Update queryapi__IndexerFunctionEditor by roshaan.near at 2023-02-17 17:31:12.313

--- roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
+++ roshaan.near/queryapi__IndexerFunctionEditor/source_code.jsx
@@ -4,50 +4,129 @@ const registry_contract_id =
   props.registry_contract_id || "registry.queryapi.near";
 let accountId = props.accountId || context.accountId;
 State.init({
-  m: initialText,
+  code: initialText,
 });
 
 if (!accountId) {
   return "Please sign in to use this widget.";
 }
 
-let queryIndexerCode = () => {
-  return Near.asyncView(registry_contract_id, "read_indexer_function", {
-    name: indexer_function_name,
-  });
-};
-queryIndexerCode().then((data) => {
+Near.asyncView(registry_contract_id, "read_indexer_function", {
+  name: `${accountId}/${indexer_function_name}`,
+}).then((data) => {
   if (!data) return;
-  console.log(data, "data");
-  State.update({ m: data });
+  console.log(data, "data loaded", `${accountId}/${indexer_function_name}`);
+  State.update({ code: data });
 });
 
+let updateIndexerCode = (code) => {
+  const gas = 200000000000000;
+
+  Near.call(
+    contractId,
+    "register_indexer_function",
+    {
+      name: indexer_function_name,
+      code: code,
+    },
+    gas
+  );
+};
+
+const reducer = (message) => {
+  switch (message.action) {
+    case "register_function":
+      updateIndexerCode(message.value);
+      break;
+
+    case "initial_load":
+      State.update({ code: message.value });
+      break;
+
+    case "default":
+      console.log("default case");
+  }
+};
 const code = `
-<iframe id="react-app-iframe" onload="test()" src="http://localhost:3001/query-api-editor" width="1250px" height="500px"></iframe>
 <script>
-function test() {
-let receiverWindow = document.getElementById("react-app-iframe").contentWindow
-window.addEventListener("message", function(event){
-     window.top.postMessage(event.data, "*");
-});
+function test(data) {
+  let receiverWindow = document.getElementById("react-app-iframe").contentWindow
+  if (!receiverWindow) {
+      console.warn("receiver window not found!!!!")
+  }
+  console.log(receiverWindow, "receiverrrr")
+  try {
+      if (event.data.action === "send_indexer_details") {
+        console.log("trying to send the data to react app")
+          receiverWindow.postMessage({
+              action: "subscription_request",
+              data: data
+          }, "*")
+      }
+  } catch (error) {
+      console.log(error, "errored out")
+  }
+window.addEventListener("message", function(event) {
+  console.log("message came from :", event.source.name)
+  if (event.action === "register_function") {
+      window.top.postMessage(event.data, "*");
+  }
+  if (event.action === "request_indexer_details") {
+      event.source.postMessage({
+          action: "subscription_request",
+          data: data
+      }, "*")
+  }
+})
 }
+</script>
+<iframe name="react-app" id="react-app-iframe" src="http://localhost:3000/query-api-editor" width="1250px" height="500px"></iframe>
+
+<script>
+window.addEventListener("message", function(event){
+    console.log(event, "event detaisl")
+    try {
+      if(event.data.action ==="send_indexer_details") {
+        console.log("sender-indexer details")
+        test(event.data)
+      }
+  
+    // let receiverWindow = document.getElementById("react-app-iframe").contentWindow
+    // console.log(receiverWindow, "receiverWindow")
+    // receiverWindow.postMessage({action: "subscription", data: event.data}, "*");
+    }
+    catch(error){
+        console.log(error, "we errored out")
+    }
+
+})
 </script>
 `;
 
 return (
   <div>
     <iframe
+      name="widget-iframe"
       className="w-100"
       style={{ height: "500px" }}
       srcDoc={code}
-      message={stae.m}
-      onMessage={(m) => State.update({ m })}
+      message={{
+        action: "send_indexer_details",
+        value: {
+          indexer_name: indexer_function_name,
+          indexer_code: state.code,
+        },
+      }}
+      onMessage={(message) => reducer(message)}
     />
     <Widget
       src={"roshaan.near/widget/queryapi__RegisterIndexerFunctionButton"}
       props={{
-        indexer_name: indexer_function_name,
-        indexer_code: state.m,
+        action: "send_indexer_details",
+        value: {
+          indexer_name: indexer_function_name,
+          indexer_code: state.code,
+        },
       }}
     />
   </div>
