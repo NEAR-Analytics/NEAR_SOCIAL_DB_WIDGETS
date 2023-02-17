6938ad4056bdd9ce0c296a567158730777865984
contribut3.near <contribut3.near>
2023-02-17 13:32:38 -0500

    Update Contributor by contribut3.near at 2023-02-17 13:32:38.860

--- contribut3.near/Contributor/source_code.jsx
+++ contribut3.near/Contributor/source_code.jsx
@@ -41,32 +41,10 @@ const circle = (
   </div>
 );
 
-const [[founder]] = (contributions ?? []).filter((contribution) => {
-  const [_, details] = contribution;
-  const all = [...details.history, details.current];
-  return all.some((detail) => detail.description === "");
-});
-
-const founderProfile = Social.getr(`${founder}/profile`);
-const founderImage = profile.image;
-const founderImageUrl =
-  (founderImage.ipfs_cid
-    ? `https://ipfs.near.social/ipfs/${founderImage.ipfs_cid}`
-    : founderImage.url) ||
-  "https://thewiki.io/static/media/sasha_anon.6ba19561.png";
-
-const founderCircle = (
-  <div
-    className="profile-circle d-inline-block"
-    title={`${founderProfile.name} @${founder}`}
-    style={{ width: "1.5em", height: "1.5em" }}
-  >
-    <img
-      className="rounded-circle w-100 h-100"
-      style={{ objectFit: "cover" }}
-      src={`https://i.near.social/thumbnail/${founderImageUrl}`}
-      alt="profile image"
-    />
+const contributorType = (
+  <div>
+    <i className="bi-person" />
+    <span>Individual contributor</span>
   </div>
 );
 
