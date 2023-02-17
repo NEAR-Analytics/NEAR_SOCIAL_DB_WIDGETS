12d5c8a942699f3c90d7c1eb8692dc76c89ed2b2
contribut3.near <contribut3.near>
2023-02-17 14:32:48 -0500

    Update Inbox by contribut3.near at 2023-02-17 14:32:48.698

---
+++ contribut3.near/Inbox/source_code.jsx
@@ -0,0 +1,21 @@
+const ownerId = "contribut3.near";
+
+const header = (
+  <div>
+    <h1 className="fs-2">Inbox</h1>
+    <p className="fw-semibold fs-5 text-muted">
+      Manage invitations and proposals
+    </p>
+  </div>
+);
+
+return (
+  <div>
+    <div className="mb-3 px-3">
+      <div className="d-flex flex-row justify-content-between mb-3">
+        {header}
+      </div>
+    </div>
+    <div className="px-3 pt-3"></div>
+  </div>
+);
