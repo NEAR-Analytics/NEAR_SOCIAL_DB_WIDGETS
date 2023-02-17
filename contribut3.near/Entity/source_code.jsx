ed101d173012f9fe809ab602e22a82cdad817490
contribut3.near <contribut3.near>
2023-02-17 13:17:15 -0500

    Update Entity by contribut3.near at 2023-02-17 13:17:15.309

--- contribut3.near/Entity/source_code.jsx
+++ contribut3.near/Entity/source_code.jsx
@@ -199,7 +199,7 @@ const body = (
     </div>
     <div className="d-flex flex-row justify-content-end align-items-start col-3">
       <div className="vr h-100 mx-2" />
-      <div>
+      <div className="d-flex flex-row justify-content-end align-items-start">
         <a
           className="btn btn-outline-secondary me-2"
           href={`https://near.social/#/${ownerId}/widget/Entity?accountId=${accountId}`}
