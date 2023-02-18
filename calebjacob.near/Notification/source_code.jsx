ceb8f7cd3b696c3096a49a682ccfb0046e0aafdd
calebjacob <calebjacob>
2023-02-17 22:24:42 -0500

    Update Notification by calebjacob.near at 2023-02-17 22:24:42.516

--- calebjacob.near/Notification/source_code.jsx
+++ calebjacob.near/Notification/source_code.jsx
@@ -42,6 +42,48 @@ const Text = styled.p`
   }
 `;
 
+const Button = styled.a`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  padding: 8px 16px;
+  height: 32px;
+  border-radius: 100px;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 15px;
+  text-align: center;
+  cursor: pointer;
+  background: #FBFCFD;
+  border: 1px solid #D7DBDF;
+  color: #11181C !important;
+
+  &.button--primary {
+    width: 100%;
+    color: #006ADC !important;
+
+    @media (max-width: 1200px) {
+      width: auto;
+    }
+  }
+
+  &:hover,
+  &:focus {
+    background: #ECEDEE;
+    text-decoration: none;
+    outline: none;
+  }
+
+  i {
+    color: #7E868C;
+  }
+
+  .bi-16 {
+    font-size: 16px;
+  }
+`;
+
 console.log(props);
 
 return (
@@ -52,8 +94,10 @@ return (
     />
 
     <Text>
-      {type === "follow" && "Followed you"}
-      {type === "unfollow" && "Unfollowed you"}
+      {type === "follow" && <>Followed you</>}
+      {type === "unfollow" && <>Unfollowed you</>}
+      {type === "poke" && <>Poked you</>}
+      {type === "like" && <>Liked your post</>}
       <Widget
         src="mob.near/widget/TimeAgo"
         props={{ blockHeight: props.blockHeight }}
@@ -66,6 +110,21 @@ return (
         src="calebjacob.near/widget/FollowButton"
         props={{ accountId: props.accountId }}
       />
+    )}
+
+    {type === "poke" && (
+      <Widget
+        src="calebjacob.near/widget/PokeButton"
+        props={{ accountId: props.accountId, back: true }}
+      />
+    )}
+
+    {type === "like" && (
+      <Button
+        href={`/#/calebjacob.near/widget/PostPage?accountId=${props.acc}&blockHeight=85455129`}
+      >
+        View Post
+      </Button>
     )}
   </Wrapper>
 );
