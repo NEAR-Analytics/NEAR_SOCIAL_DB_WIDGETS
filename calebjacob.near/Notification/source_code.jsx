9dce53f2396a4295edc6a6681490c824e4d82de1
calebjacob <calebjacob>
2023-02-17 22:47:17 -0500

    Update Notification by calebjacob.near at 2023-02-17 22:47:17.850

--- calebjacob.near/Notification/source_code.jsx
+++ calebjacob.near/Notification/source_code.jsx
@@ -150,26 +150,3 @@ return (
     )}
   </Wrapper>
 );
-
-// return (
-//   <div className="mb-3">
-//     {value.type === "follow" || value.type === "unfollow" ? (
-//       <Widget src="mob.near/widget/Notification.Item.Follow" props={props} />
-//     ) : value.type === "poke" ? (
-//       <Widget src="mob.near/widget/Notification.Item.Poke" props={props} />
-//     ) : value.type === "like" ? (
-//       <Widget src="mob.near/widget/Notification.Item.Like" props={props} />
-//     ) : value.type === "comment" ? (
-//       <Widget src="mob.near/widget/Notification.Item.Comment" props={props} />
-//     ) : value.type && value.type?.startsWith("devgovgigs/") ? (
-//       <Widget src="mob.near/widget/Notification.Item.DevGov" props={props} />
-//     ) : value.type === "mention" ? (
-//       <Widget src="mob.near/widget/Notification.Item.Mention" props={props} />
-//     ) : (
-//       <div>
-//         Unknown notification:{" "}
-//         <span className="font-monospace">{JSON.stringify(value)}</span>
-//       </div>
-//     )}
-//   </div>
-// );
