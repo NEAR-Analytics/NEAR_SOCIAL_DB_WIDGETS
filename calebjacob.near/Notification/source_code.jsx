d27563caf004502e8bd4c87efa621afc3edbcc1f
calebjacob <calebjacob>
2023-02-17 22:17:51 -0500

    Update Notification by calebjacob.near at 2023-02-17 22:17:51.999

--- calebjacob.near/Notification/source_code.jsx
+++ calebjacob.near/Notification/source_code.jsx
@@ -1,14 +1,74 @@
-const { value } = props;
+const { type } = props.value;
 
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
-  gap: 12px;
+  gap: 24px;
+
+  > *:first-child {
+    border-right: 1px solid #ECEEF0;
+    padding-right: 24px;
+  }
+
+  > *:last-child {
+      margin-left: auto;
+  }
+`;
+
+const Text = styled.p`
+  margin: 0;
+  line-height: 1.5rem;
+  color: ${(p) => (p.bold ? "#11181C" : "#687076")} !important;
+  font-weight: ${(p) => (p.bold ? "600" : "400")};
+  font-size: ${(p) => (p.small ? "12px" : "14px")};
+  overflow: ${(p) => (p.ellipsis ? "hidden" : "")};
+  text-overflow: ${(p) => (p.ellipsis ? "ellipsis" : "")};
+  white-space: ${(p) => (p.ellipsis ? "nowrap" : "")};
+  overflow-wrap: anywhere;
+
+  b {
+    font-weight: 600;
+    color: #11181C;
+  }
+  
+  &[href] {
+    display: inline-flex;
+    gap: 0.25rem;
+    
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+  }
 `;
 
 console.log(props);
 
-return <Wrapper>hello</Wrapper>;
+return (
+  <Wrapper>
+    <Widget
+      src="calebjacob.near/widget/AccountProfile"
+      props={{ accountId: props.accountId }}
+    />
+
+    <Text>
+      {type === "follow" && "Followed you"}
+      {type === "unfollow" && "Unfollowed you"}
+      <Widget
+        src="mob.near/widget/TimeAgo"
+        props={{ blockHeight: props.blockHeight }}
+      />{" "}
+      ago
+    </Text>
+
+    {(type === "follow" || type === "unfollow") && (
+      <Widget
+        src="calebjacob.near/widget/FollowButton"
+        props={{ accountId: props.accountId }}
+      />
+    )}
+  </Wrapper>
+);
 
 // return (
 //   <div className="mb-3">
