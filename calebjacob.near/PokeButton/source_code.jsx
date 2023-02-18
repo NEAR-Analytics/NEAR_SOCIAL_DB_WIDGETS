ce0264a3c2dbca26bfd949ea440b0b6eccabc68c
calebjacob <calebjacob>
2023-02-17 22:14:42 -0500

    Update PokeButton by calebjacob.near at 2023-02-17 22:14:42.421

--- calebjacob.near/PokeButton/source_code.jsx
+++ calebjacob.near/PokeButton/source_code.jsx
@@ -23,9 +23,44 @@ const data = {
   },
 };
 
+const Wrapper = styled.div`
+  .poke-button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    padding: 8px 16px;
+    height: 32px;
+    border-radius: 100px;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 15px;
+    text-align: center;
+    cursor: pointer;
+    background: #FBFCFD;
+    border: 1px solid #D7DBDF;
+    color: #11181C !important;
+
+    &:hover,
+    &:focus {
+      background: #ECEDEE;
+      text-decoration: none;
+      outline: none;
+    }
+
+    i {
+      display: inline-block;
+      transform: rotate(90deg);
+      color: #7E868C;
+    }
+  }
+`;
+
 return (
-  <CommitButton force className={props.className} data={data}>
-    <i className="bi bi-hand-index-thumb"></i>
-    {props.back ? "Poke Back" : "Poke"}
-  </CommitButton>
+  <Wrapper className={props.className}>
+    <CommitButton className="poke-button" force data={data}>
+      <i className="bi bi-hand-index-thumb"></i>
+      {props.back ? "Poke Back" : "Poke"}
+    </CommitButton>
+  </Wrapper>
 );
