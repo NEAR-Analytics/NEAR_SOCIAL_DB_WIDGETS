aa7273c738418683cdb05cda0f2e1b5532e3b063
ligebit <ligebit>
2023-02-18 22:19:59 -0500

    Update Sudoku.Leaderboard by bozon.near at 2023-02-18 22:19:59.931

--- bozon.near/Sudoku.Leaderboard/source_code.jsx
+++ bozon.near/Sudoku.Leaderboard/source_code.jsx
@@ -1,3 +1,17 @@
-console.log(props.leaderboard);
+if (!props.top_by_count) return "";
 
-return <div>{props.leaderboard}</div>;
+State.update({
+  top_by_count: Object.keys(props.leaderboard.top_by_count)
+    .map((el) => [el, props.leaderboard.top_by_count[el]])
+    .sort((a, b) => a[1] - b[1]),
+});
+
+return (
+  <div>
+    {state.top_by_count.map((el) => (
+      <div>
+        {el[0]} {el[1]}
+      </div>
+    ))}
+  </div>
+);
