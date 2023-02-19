87bfadf2256140a9d2c5371fad94540812e3cd05
ligebit <ligebit>
2023-02-18 20:18:39 -0500

    Update Sudoku.Board by bozon.near at 2023-02-18 20:18:39.808

--- bozon.near/Sudoku.Board/source_code.jsx
+++ bozon.near/Sudoku.Board/source_code.jsx
@@ -2,7 +2,7 @@
 //props.init_board
 //props.update(x, y, value)
 
-if (!props.current_board || !props.init_board || props.update) return "";
+if (!props.current_board || !props.init_board || !props.update) return "";
 
 const Board = styled.div`
   display: grid;
