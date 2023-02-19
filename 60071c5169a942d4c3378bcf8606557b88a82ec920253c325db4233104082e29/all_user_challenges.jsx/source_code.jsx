02aa8b9cf54c40592b79abfa0035a1e35de88908
60071c5169a942d4c3378bcf8606557b88a82ec920253c325db4233104082e29 <60071c5169a942d4c3378bcf8606557b88a82ec920253c325db4233104082e29>
2023-02-19 16:55:51 -0500

    Update all_user_challenges.jsx by 60071c5169a942d4c3378bcf8606557b88a82ec920253c325db4233104082e29 at 2023-02-19 16:55:51.225

---
+++ 60071c5169a942d4c3378bcf8606557b88a82ec920253c325db4233104082e29/all_user_challenges.jsx/source_code.jsx
@@ -0,0 +1,35 @@
+const accountId = props.accountId ?? context.accountId;
+
+const testnetWidgets = {
+  languageChallenge: "betteryou.near/widget/language_challenge",
+  wakeupChallenge: "betteryou.near/widget/wakeup_challenge",
+};
+
+let logo = (
+  <img src="https://user-images.githubusercontent.com/91919554/207791330-aa5bc143-b51d-49b0-a0be-eae26365f1f2.png" />
+);
+
+if (props.hideLogo) {
+  logo = "";
+}
+
+const widgets = testnetWidgets;
+return (
+  <div>
+    {logo}
+    <div>
+      <div style={{ display: "inline-block" }}>
+        <Widget
+          src={widgets["wakeupChallenge"]}
+          props={{ accountId, emptyIfNoChallenge: true }}
+        />
+      </div>
+      <div style={{ display: "inline-block" }}>
+        <Widget
+          src={widgets["languageChallenge"]}
+          props={{ accountId, emptyIfNoChallenge: true }}
+        />
+      </div>
+    </div>
+  </div>
+);
