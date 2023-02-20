1903ae16e6bfec614328e0af73affa58ce966dab
mattlockyer <mattlockyer>
2023-02-20 18:13:35 -0500

    Update bos-landing-page by mattlock.near at 2023-02-20 18:13:35.563

--- mattlock.near/bos-landing-page/source_code.jsx
+++ mattlock.near/bos-landing-page/source_code.jsx
@@ -15,23 +15,33 @@ const css = `
     src: url("${fontUrl}");
 }
 
-.apps {
-  margin-top: 32px;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 16px;
-  grid-auto-rows: minmax(100px, auto);
+a {
+  padding: 8px;
+  border-radius: 0;
 }
 
+a: hover {
+  border-radius: 0;
+}
+
+.apps {
+  margin-top: 32px;
+  align-items: center;
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+}
 
 .flex {
   display: flex;
   align-items: center;
+  .flex-right {
+    padding-left: 8px;
+  }
 }
 
 .app-image {
   width: 100px;
-  margin-right: 16px;
 }
 
 .header {
@@ -43,16 +53,19 @@ const css = `
     > div {
         padding: 32px;
         h1 {
-        font-size: 3rem;
+          font-size: 3rem;
         }
         h2 {
             color: #ddd;
         }
+        h3 {
+          margin-bottom: -16px;
+        }
         > div {
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            height: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          height: 100%;
         }
     }
 }
@@ -60,7 +73,7 @@ const css = `
 .header-left {
     background: linear-gradient(253.16deg, #4D1CB6 1.53%, #AC3A7F 86.25%);
     width: 55%;
-    height: 440px;
+    height: 260px;
   transform-origin: 100% 0;
   transform: skew(-10deg);
   border-right: 4px solid black;
@@ -70,7 +83,7 @@ const css = `
     margin-left: -100px;
     background: linear-gradient(255.45deg, #7D14AF 12.67%, #00D1FF 87.81%);
     width: calc(45% + 100px);
-    height: 440px;
+    height: 260px;
     text-align: right;
 }
 
@@ -80,14 +93,14 @@ const css = `
     z-index: 1;
     opacity: 0.5;
   width: 100vw;
-  height: 440px;
+    height: 260px;
   background: url('https://s3-alpha-sig.figma.com/img/aa66/ca0f/536910691a243c017d29d4fe29b4da0a?Expires=1677456000&Signature=SCOYHroTrc--MSSaDWtzcsPvcoHQs9~lF2cwUv-4liY-MTa01xehz4dcVFgvyyqrN8oohEeIZ28SjtcVRFVquNupbyqMUe06cB84IN8T-tvZ7pJqsT96NB2rwpfZvTgcBioY0lRVU45~LGxBF6Mq-Cow-WjIwjU3UZUADEdhVQx85g1hK3OIpn4xP2QJmKor1raV4EdvS7dI0PzyzZg1b9h3EUgEACu15Bl~7d6HD6~a1mQVz6WLJOa4GlOpAnOs7fpG6d8vsWjA1BV6jkXHkra4F82EvKGOhXJp-Ti04S37Km5OQBxc6KOTB8MbOS-FOgeVENObE~7l-fuMZvLW1A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4');
  }
 
  .header-content-left {
      position: absolute;
      z-index: 2;
-     height: 440px;
+    height: 260px;
  }
 
  .main {
@@ -110,7 +123,7 @@ const css = `
     border: none;
   }
   .noise {
-    height: 880px;
+    height: 520px;
   }
 }
 `;
@@ -170,8 +183,8 @@ return (
             src="mob.near/widget/ComponentSearch"
             props={{
               limit: 10,
-              term: "",
-              filterTag: "",
+              term: "#ethdenver2023",
+              boostedTag: "ethdenver2023",
               onChange: ({ result: components, term }) => {
                 const componentsWithMeta = components.map((c) => ({
                   ...c,
@@ -188,25 +201,40 @@ return (
             {state.components.map((component, i) => (
               <div key={i} class="widget">
                 <div class="flex">
-                  <div>
-                    <Widget
-                      src="mob.near/widget/Image"
-                      props={{
-                        image: component.image,
-                        alt: component.name,
-                        className: imageClassName,
-                        style: imageStyle,
-                        thumbnail: component.thumbnail,
-                        fallbackUrl:
-                          "https://ipfs.near.social/ipfs/bafkreido7gsk4dlb63z3s5yirkkgrjs2nmyar5bxyet66chakt2h5jve6e",
-                      }}
-                    />
-                  </div>
-                  <div>
+                  <a
+                    href={`#/${component.widgetSrc}`}
+                    target="_blank"
+                    className="btn btn-outline-secondary border-0"
+                  >
+                    <div>
+                      <Widget
+                        src="mob.near/widget/Image"
+                        props={{
+                          image: component.image,
+                          alt: component.name,
+                          className: imageClassName,
+                          style: imageStyle,
+                          thumbnail: component.thumbnail,
+                          fallbackUrl:
+                            "https://ipfs.near.social/ipfs/bafkreido7gsk4dlb63z3s5yirkkgrjs2nmyar5bxyet66chakt2h5jve6e",
+                        }}
+                      />
+                    </div>
+                  </a>
+
+                  <div class="flex-right">
                     <p>{component.widgetName}</p>
                   </div>
                 </div>
                 <p>{component.description}</p>
+
+                <a
+                  href={`#/mob.near/widget/WidgetSource?src=${component.widgetSrc}`}
+                  target="_blank"
+                  className="btn btn-outline-secondary border-0"
+                >
+                  <i className="bi bi-file-earmark-code me-1"></i>Source
+                </a>
 
                 {
                   console.log(component)
