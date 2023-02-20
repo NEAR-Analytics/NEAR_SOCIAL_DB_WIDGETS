273c37b1c9d5857d68373ba935fff5d5df6b0925
roshaan.near <roshaan.near>
2023-02-20 15:28:48 -0500

    Update queryapi__IndexerView by roshaan.near at 2023-02-20 15:28:48.773

--- roshaan.near/queryapi__IndexerView/source_code.jsx
+++ roshaan.near/queryapi__IndexerView/source_code.jsx
@@ -1,3 +1,6 @@
+const indexerName = props.indexerName;
+const accountId = props.accountId;
+if (!indexerName || accountId) return "";
 const Card = styled.div`
   border-radius: 12px;
   background: #fff;
