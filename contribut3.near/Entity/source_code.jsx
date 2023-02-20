25cdd0b4fa82562cf2e0a5c09faec0e55d512e6c
contribut3.near <contribut3.near>
2023-02-20 13:11:28 -0500

    Update Entity by contribut3.near at 2023-02-20 13:11:28.768

--- contribut3.near/Entity/source_code.jsx
+++ contribut3.near/Entity/source_code.jsx
@@ -105,6 +105,15 @@ const ctas = inboxView ? (
   </>
 );
 
+const buttonAction = ({ text, icon, id }) => (
+  <li>
+    <a className="dropdown-item" id={id}>
+      <i className={icon} />
+      <span>{text}</span>
+    </a>
+  </li>
+);
+
 const body = (
   <div
     className="d-flex flex-row justify-content-start"
@@ -123,9 +132,52 @@ const body = (
             {inboxView ? (
               <></>
             ) : (
-              <div className="text-success">
-                <i className="bi-play" />
-                <span className="ms-1">{entity.status}</span>
+              <div className="d-flex flex-column justify-content-between align-items-center">
+                <div className="text-success">
+                  <i className="bi-play" />
+                  <span className="ms-1">{entity.status}</span>
+                </div>
+                <div className="btn-group dropstart">
+                  <a
+                    className="btn btn-outline-secondary dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <i className="bi-three-dots-vertical" />
+                  </a>
+
+                  <ul className="dropdown-menu">
+                    {buttonAction({
+                      text: "Propose contribution",
+                      icon: "bi-person-up",
+                      id: "contribute",
+                    })}
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    {buttonAction({
+                      text: "Invite to contribute",
+                      icon: "bi-person-plus",
+                      id: "invite",
+                    })}
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    {buttonAction({
+                      text: "View details",
+                      icon: "bi-info-circle",
+                      id: "info",
+                    })}
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    {buttonAction({
+                      text: "Share",
+                      icon: "bi-arrow-up-right",
+                      id: "share",
+                    })}
+                  </ul>
+                </div>
               </div>
             )}
           </div>
