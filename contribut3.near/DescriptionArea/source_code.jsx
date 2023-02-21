55978a3dcbbb6ab497d24c4eaa073fa94d419d42
contribut3.near <contribut3.near>
2023-02-21 11:26:45 -0500

    Update DescriptionArea by contribut3.near at 2023-02-21 11:26:45.252

--- contribut3.near/DescriptionArea/source_code.jsx
+++ contribut3.near/DescriptionArea/source_code.jsx
@@ -7,5 +7,10 @@ if (description.length > 100) {
 return (
   <div className="text-truncate my-2">
     {description.length > 100 ? description.substring(0, 90) : description}
+    {description.length > 100 ? (
+      <b className="text-primary">Read more</b>
+    ) : (
+      <></>
+    )}
   </div>
 );
