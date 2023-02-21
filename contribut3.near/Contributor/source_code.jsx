78930bdc5591a109260b7ca16198d99fa23d1fff
contribut3.near <contribut3.near>
2023-02-21 12:25:36 -0500

    Update Contributor by contribut3.near at 2023-02-21 12:25:36.451

--- contribut3.near/Contributor/source_code.jsx
+++ contribut3.near/Contributor/source_code.jsx
@@ -75,7 +75,7 @@ const body = (
               <div
                 className={`text-${active ? "success" : "muted"
                   } me-3 d-flex flex-row justify-content-end align-items-center`}
-                style={{ minWidth: "6em", fontSize: "xx-small" }}
+                style={{ minWidth: "6em", fontSize: "x-small" }}
               >
                 {active ? (
                   <i
