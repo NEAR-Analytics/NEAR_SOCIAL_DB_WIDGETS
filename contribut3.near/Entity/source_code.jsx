66719b41f7586ec5945722d2b883ea1d289314df
contribut3.near <contribut3.near>
2023-02-21 11:41:52 -0500

    Update Entity by contribut3.near at 2023-02-21 11:41:52.654

--- contribut3.near/Entity/source_code.jsx
+++ contribut3.near/Entity/source_code.jsx
@@ -46,74 +46,72 @@ const body = (
     id={accountId}
     style={{ minHeight: "8em" }}
   >
-    <div className="flex-grow-1 py-3">
-      <Widget
-        src={`${ownerId}/widget/ProfileLine`}
-        props={{
-          accountId,
-          isEntity: true,
-          imageSize: "3em",
-          additionalColumn: inboxView ? (
-            <></>
-          ) : (
-            <div className="d-flex flex-row justify-content-between align-items-center">
-              <div
-                className={`text-${entity.status ? "success" : "muted"
-                  } me-3 d-flex flex-row`}
-              >
-                <i className={entity.status ? "bi-play" : "bi-x"} />
-                <span className="ms-1">{entity.status}</span>
-              </div>
-              <Widget
-                src={`${ownerId}/widget/CardMenu`}
-                props={{
-                  items: [
-                    {
-                      text: "Propose contribution",
-                      icon: "bi-person-up",
-                      id: "contribute",
-                    },
-                    {
-                      text: "Invite to contribute",
-                      icon: "bi-person-plus",
-                      id: "invite",
-                    },
-                    {
-                      text: "View details",
-                      icon: "bi-info-circle",
-                      id: "info",
-                    },
-                    {
-                      text: "Share",
-                      icon: "bi-arrow-up-right",
-                      id: "share",
-                    },
-                  ],
-                }}
-              />
+    <Widget
+      src={`${ownerId}/widget/ProfileLine`}
+      props={{
+        accountId,
+        isEntity: true,
+        imageSize: "3em",
+        additionalColumn: inboxView ? (
+          <></>
+        ) : (
+          <div className="d-flex flex-row justify-content-between align-items-center">
+            <div
+              className={`text-${entity.status ? "success" : "muted"
+                } me-3 d-flex flex-row`}
+            >
+              <i className={entity.status ? "bi-play" : "bi-x"} />
+              <span className="ms-1">{entity.status}</span>
             </div>
-          ),
-          additionalRow: (
-            <>
-              <Widget
-                src={`${ownerId}/widget/ProfileLine`}
-                props={{ accountId: founder }}
-              />
-              <Widget
-                src={`${ownerId}/widget/Tags`}
-                props={{ tags: profile.tags }}
-              />
-              <Widget
-                src={`${ownerId}/widget/DescriptionArea`}
-                props={{
-                  description: entity.description || profile.description,
-                }}
-              />
-            </>
-          ),
-        }}
-      />
-    </div>
+            <Widget
+              src={`${ownerId}/widget/CardMenu`}
+              props={{
+                items: [
+                  {
+                    text: "Propose contribution",
+                    icon: "bi-person-up",
+                    id: "contribute",
+                  },
+                  {
+                    text: "Invite to contribute",
+                    icon: "bi-person-plus",
+                    id: "invite",
+                  },
+                  {
+                    text: "View details",
+                    icon: "bi-info-circle",
+                    id: "info",
+                  },
+                  {
+                    text: "Share",
+                    icon: "bi-arrow-up-right",
+                    id: "share",
+                  },
+                ],
+              }}
+            />
+          </div>
+        ),
+        additionalRow: (
+          <>
+            <Widget
+              src={`${ownerId}/widget/ProfileLine`}
+              props={{ accountId: founder }}
+            />
+            <Widget
+              src={`${ownerId}/widget/Tags`}
+              props={{ tags: profile.tags }}
+            />
+            <Widget
+              src={`${ownerId}/widget/DescriptionArea`}
+              props={{
+                description: entity.description || profile.description,
+              }}
+            />
+          </>
+        ),
+      }}
+    />
   </div>
 );
 
