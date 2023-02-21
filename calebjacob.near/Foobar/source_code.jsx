c22e227a8761dba88a08301f356f1d207489068f
calebjacob <calebjacob>
2023-02-20 20:52:01 -0500

    Update Foobar by calebjacob.near at 2023-02-20 20:52:01.717

---
+++ calebjacob.near/Foobar/source_code.jsx
@@ -0,0 +1,18 @@
+return (
+  <div>
+    <Widget
+      src="calebjacob.near/widget/AccountProfile"
+      props={{ accountId: context.accountId }}
+    />
+
+    <Widget
+      src="calebjacob.near/widget/AccountProfile"
+      props={{ accountId: context.accountId }}
+    />
+
+    <Widget
+      src="calebjacob.near/widget/AccountProfileInline"
+      props={{ accountId: context.accountId }}
+    />
+  </div>
+);
