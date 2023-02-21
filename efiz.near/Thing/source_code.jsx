a1954b0004eab464206f9985ac2d308925b0f2a8
elliotBraem <elliotBraem>
2023-02-21 01:57:58 -0500

    Update Thing by efiz.near at 2023-02-21 01:57:58.710

--- efiz.near/Thing/source_code.jsx
+++ efiz.near/Thing/source_code.jsx
@@ -1,26 +1,7 @@
 const accountId = context.accountId;
-const data = Social.keys(`${accountId}/thing`, "final", {
-  return_type: "History",
-});
 
 if (!accountId) {
   return "Please sign in with NEAR wallet to view this thing";
 }
 
-initState({
-  thing: props.thing,
-});
-
-const entry = {
-  thing: state.thing,
-};
-
-return (
-  <div>
-    {" "}
-    <div>
-      <h4>Thing</h4>
-    </div>
-    <div className="mb-2">{entry.thing}</div>
-  </div>
-);
+return <p>{props.thing.id}</p>;
