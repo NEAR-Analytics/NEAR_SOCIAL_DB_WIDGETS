a007ad07bc03a7b3788e173a03aff8ff9ef0280c
calebjacob <calebjacob>
2023-02-21 18:26:09 -0500

    Update Posts.Feed by calebjacob.near at 2023-02-21 18:26:09.704

--- calebjacob.near/Posts.Feed/source_code.jsx
+++ calebjacob.near/Posts.Feed/source_code.jsx
@@ -11,6 +11,10 @@ const index = {
 const Post = styled.div`
   border-bottom: 1px solid #ECEEF0;
   padding: 24px 24px 12px;
+
+  @media (max-width: 1200px) {
+    padding: 12px 12px 0px;
+  }
 `;
 
 const renderItem = (a) =>
