2300d8ccfe836a83fa7ae0b73759082479df5acd
contribut3.near <contribut3.near>
2023-02-21 11:15:30 -0500

    Update Tags by contribut3.near at 2023-02-21 11:15:30.253

--- contribut3.near/Tags/source_code.jsx
+++ contribut3.near/Tags/source_code.jsx
@@ -1,1 +1,20 @@
-const tags = props.tags || [];
+const tags = Object.keys(props.tags || {});
+
+return (
+  <div className="text-truncate text-muted">
+    {tags.length > 0 ? (
+      <>
+        {tags.map((tag) => (
+          <span
+            className="d-inline-block mx-1 py-1 px-2 badge border border-secondary text-secondary text-muted text-center"
+            key={tag}
+          >
+            {tag}
+          </span>
+        ))}
+      </>
+    ) : (
+      <></>
+    )}
+  </div>
+);
