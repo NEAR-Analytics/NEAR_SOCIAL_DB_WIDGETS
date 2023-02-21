8ebe35b052f6efae30b624fed3fe1911ebfb869b
contribut3.near <contribut3.near>
2023-02-21 11:28:02 -0500

    Update DescriptionArea by contribut3.near at 2023-02-21 11:28:02.029

--- contribut3.near/DescriptionArea/source_code.jsx
+++ contribut3.near/DescriptionArea/source_code.jsx
@@ -1,9 +1,5 @@
 let description = props.description || "";
 
-if (description.length > 100) {
-  description = description.substring(0, 90) + "Read more";
-}
-
 return (
   <div className="text-truncate my-2">
     {description.length > 100 ? description.substring(0, 90) : description}
