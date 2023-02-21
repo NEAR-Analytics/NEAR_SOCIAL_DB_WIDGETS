e477c4d1bbe85381a0503ad4f0b48a2fa7c87bee
calebjacob <calebjacob>
2023-02-21 18:31:59 -0500

    Update Posts.Post by calebjacob.near at 2023-02-21 18:31:59.226

--- calebjacob.near/Posts.Post/source_code.jsx
+++ calebjacob.near/Posts.Post/source_code.jsx
@@ -3,7 +3,7 @@ const blockHeight =
   props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
 const subscribe = !!props.subscribe;
 const notifyAccountId = accountId;
-const postUrl = `/#/calebjacob.near/widget/PostPage?accountId=${accountId}&blockHeight=${blockHeight}`;
+const postUrl = `https://alpha.near.org/#/calebjacob.near/widget/PostPage?accountId=${accountId}&blockHeight=${blockHeight}`;
 
 const content =
   props.content ??
