30fde135ec0199d271b58dd52436d8d03e37acdf
let45fc.near <let45fc.near>
2023-02-22 00:34:43 -0500

    Update LaserChess3D by let45fc.near at 2023-02-22 00:34:43.591

---
+++ let45fc.near/LaserChess3D/source_code.jsx
@@ -0,0 +1,37 @@
+State.init({
+  roomData: null,
+  roomId: null,
+});
+
+const callback = (roomData, roomId, created) => {
+  State.update({
+    roomData,
+    roomId,
+  });
+};
+
+if (state.roomData) {
+  return (
+    <>
+      <h1>Got game data!</h1>
+      <p>{JSON.stringify(state.roomData)}</p>
+    </>
+  );
+}
+
+return (
+  <div>
+    <Widget
+      src="let45fc.near/widget/UsersMatcher"
+      props={{
+        widgetKey: "laserchess3d",
+        widgeName: "Laser chess",
+        loadRoomCallback: callback,
+        initialValue: {
+          boardNotation:
+            "l++3d++kd++b++2/2b7/3B+6/b++1B1ss+1b+++1B+/b+++1B+1S+S1b++1B/6b+++3/7B++2/2B+DKD3L",
+        },
+      }}
+    />
+  </div>
+);
