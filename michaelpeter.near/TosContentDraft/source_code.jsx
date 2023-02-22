bf7c9eabbc0aa41b2183b7cdd39ec2e1fa4160f7
mpeterdev <mpeterdev>
2023-02-21 19:50:39 -0500

    Update TosContentDraft by michaelpeter.near at 2023-02-21 19:50:39.701

--- michaelpeter.near/TosContentDraft/source_code.jsx
+++ michaelpeter.near/TosContentDraft/source_code.jsx
@@ -1,6 +1,55 @@
 const tos = `
-## Terms of Service
-You agree not to post illicit content.
+# alpha.near.org terms of service
+
+## Clarum docto extra det sub nova
+
+Lorem markdownum expendite luminibus tenent quoque, sustinet iuxta, et revocare
+ne potes, Theseus nulla volutant erunt plus. Loqui regna et deum Iuventae quas,
+consulit adsiduis invenit in origine referat inhaesi Typhoea desine, Herculis
+Enipeus. Pentheus ipse officium clamat saturatos concessa, in fretum eo
+coniunctaque si femina et Iapetionides fugit furit nebulas. Se et nunc paterer
+paelicis et totum reflectitur facta protinus salignis fortibus prius Lacon
+mortisque, variarum? Montibus inmoritur meruere laquei, generis Athamanas
+nympham tantaque, amoris caeruleum prohibete inhospita curruque legit; *tactus*
+una primis.
+
+## Fuerat sit et fatetur innixamque hoc
+
+Accepisse lascivaque ausa tempora urna non corpore facta aequora nostrae. Par
+lecto solutus geminis tacta: est annum, pudorem nititur teste caput Baccho;
+vellet inscripta probat ultima. Caicus et tempora aliter quondam lacertis fato
+exit sequentur **illo**; Iovi Phoebus quae Sabaea verba summo suus. Regia
+invadunt circuiere sequenti remos in geri aliamve, quies quae dixi manu Musa
+inpune, huc temptamenta vir. Adhuc in gramen parari columnae et esse, sed tamen
+prima Apolline, ad acuta tantum.
+
+    var pageGrepAdd = cmykCompatible;
+    if (enterpriseServerSprite(cms)) {
+        key_error = pcmcia;
+        led_vaporware.filePathWeb(utf, shareware);
+    }
+    piracyDual(recordPointLink, blu_console, 18 * partition);
+
+## Adhuc ergo sub rettulit mihi lacrimisque refugam
+
+Tamen [in retentus cum](http://www.quem-cognoscere.org/) faciem Aeneae parabat
+numero metuit **priorum quotiensque** canos? Serpentis mox esse sua leporem
+Phoebi culpa utroque, me supra tantorum o?
+
+Duratos dextroque, in Haec deprendere vixque? Omne signum humi iactata; signatum
+corneus credens!
+
+## Praecluserat ossa consulat iussa
+
+Othryn ruit nervis utile. Se tam totumque, Lelegas et vulnus **vetustas**
+matresque deae lambit me. Flores inrumpere optes silvisque sumptoque modo totam;
+et sparso proxima poenaeque et amor *tantum occupet caelatus* honores.
+Circumstant Iuppiter fera domitae corpore viderat suco aperire primum occubuisse
+tectus. Qua solent litus, nec vina ut plagas illis honoris, ante querenda non
+Siculique.
+
+Maiora esse puppes pendere, hoc decoro eripiunt hostisque perire movet, quanto.
+Volucris monstris reponit et ante ora, fuit, et!
 `;
 
 return <Markdown text={tos} />;
