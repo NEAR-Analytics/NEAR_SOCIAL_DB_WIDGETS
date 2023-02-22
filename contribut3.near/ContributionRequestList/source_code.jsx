1e3441e83a6a75032a73959089c1207d7fdc6a4e
contribut3.near <contribut3.near>
2023-02-22 10:14:25 -0500

    Update ContributionRequestList by contribut3.near at 2023-02-22 10:14:25.650

--- contribut3.near/ContributionRequestList/source_code.jsx
+++ contribut3.near/ContributionRequestList/source_code.jsx
@@ -21,6 +21,8 @@ if (Array.isArray(requests) && requests.length === 0) {
   return "No contribution requests found!";
 }
 
+console.log(requests);
+
 const allRequests = requests.filter(
   ([entityId, contributorId]) =>
     entityId.includes(search) ||
