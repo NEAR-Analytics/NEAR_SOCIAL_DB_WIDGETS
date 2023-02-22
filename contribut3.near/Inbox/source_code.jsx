447321f32e8ed7b3e51fbf60840275623dac4328
contribut3.near <contribut3.near>
2023-02-22 11:26:54 -0500

    Update Inbox by contribut3.near at 2023-02-22 11:26:54.533

--- contribut3.near/Inbox/source_code.jsx
+++ contribut3.near/Inbox/source_code.jsx
@@ -34,29 +34,6 @@ const header = (
   </div>
 );
 
-const contentSelectButton = ({ id, text, icon, count }) => (
-  <a
-    className={`btn ${state.content === id ? "btn-secondary" : "btn-outline-secondary"
-      }`}
-    href={`https://near.social/#/${ownerId}/widget/Index?tab=inbox&content=${id}${props.search ? "&search=" + props.search : ""
-      }`}
-    onClick={() => State.update({ content: id })}
-  >
-    <i className={icon} />
-    <span>{text}</span>
-    {count > 0 ? (
-      <div
-        className="d-inline-block rounded-circle bg-danger text-center"
-        style={{ minWidth: "1.5em", height: "1.5em", color: "#FFF" }}
-      >
-        {count}
-      </div>
-    ) : (
-      <></>
-    )}
-  </a>
-);
-
 const contentSelector = (
   <Widget
     src={`${ownerId}/widget/TabSelector`}
