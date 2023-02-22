eb408b8594f67a89fed11a012218142c2766300a
contribut3.near <contribut3.near>
2023-02-22 10:39:34 -0500

    Update ContributorList by contribut3.near at 2023-02-22 10:39:34.384

--- contribut3.near/ContributorList/source_code.jsx
+++ contribut3.near/ContributorList/source_code.jsx
@@ -1,8 +1,15 @@
 const ownerId = "contribut3.near";
 const search = props.search ?? "";
+const accountId = props.accountId;
 
 const allContributors = (
-  Near.view(ownerId, "get_contributors", {}, "final", true) ?? []
+  Near.view(
+    ownerId,
+    accountId ? "get_entity_contributions" : "get_contributors",
+    accountId ? { entity_id: accountId } : {},
+    "final",
+    true
+  ) ?? []
 )
   .filter((accountId) => (search ? accountId.includes(search) : true))
   .sort((a, b) => a.localeCompare(b));
