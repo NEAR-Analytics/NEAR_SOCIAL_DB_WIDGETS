c70b50ec838aa435a3651d02efa759b5184985ba
contribut3.near <contribut3.near>
2023-02-22 09:55:40 -0500

    Update SocialLinks by contribut3.near at 2023-02-22 09:55:40.790

--- contribut3.near/SocialLinks/source_code.jsx
+++ contribut3.near/SocialLinks/source_code.jsx
@@ -19,56 +19,6 @@ const linksList = supportedLinks
     </li>
   ));
 
-if ("github" in links) {
-  linksList.push(
-    <li>
-      <a href={`https://github.com/${links["github"]}`} target="_blank">
-        <i className="bi-github" />
-      </a>
-    </li>
-  );
-}
-
-// if ("discord" in links) {
-//   linksList.push(
-//     <li>
-//       <a href={`https://discord.com/u/${links["discord"]}`} target="_blank">
-//         <i className="bi-discord" />
-//       </a>
-//     </li>
-//   );
-// }
-
-if ("reddit" in links) {
-  linksList.push(
-    <li>
-      <a href={`https://reddit.com/u/${links["reddit"]}`} target="_blank">
-        <i className="bi-reddit" />
-      </a>
-    </li>
-  );
-}
-
-if ("twitter" in links) {
-  linksList.push(
-    <li>
-      <a href={`https://twitter.com/${links["twitter"]}`} target="_blank">
-        <i className="bi-twitter" />
-      </a>
-    </li>
-  );
-}
-
-if ("youtube" in links) {
-  linksList.push(
-    <li>
-      <a href={`https://youtube.com/${links["youtube"]}`} target="_blank">
-        <i className="bi-youtube" />
-      </a>
-    </li>
-  );
-}
-
 return (
   <ul className="d-flex flex-row justify-content-between align-items-center">
     {linksList}
