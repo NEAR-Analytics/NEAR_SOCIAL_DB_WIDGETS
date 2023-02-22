017279e0400d5d7736cd892c98f08740460f049e
contribut3.near <contribut3.near>
2023-02-22 11:55:49 -0500

    Update EntityPage by contribut3.near at 2023-02-22 11:55:49.429

--- contribut3.near/EntityPage/source_code.jsx
+++ contribut3.near/EntityPage/source_code.jsx
@@ -42,7 +42,7 @@ const profile = Social.getr(`${entityId}/profile`);
 
 const controls = isAuthorized ? (
   <div className="d-flex flex-row justify-content-between align-items-center">
-    <a className="btn btn-outline-secondary me-2">
+    <a className="btn btn-outline-secondary me-2" style={{ width: "8em" }}>
       <i className="bi-pencil-square" />
       <span>Edit project</span>
     </a>
