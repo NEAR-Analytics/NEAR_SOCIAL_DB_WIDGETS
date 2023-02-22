859dbcc3f1010a83db1b0aa3c7ae9b4a719c8aa5
contribut3.near <contribut3.near>
2023-02-22 09:49:49 -0500

    Update SocialLinks by contribut3.near at 2023-02-22 09:49:49.180

--- contribut3.near/SocialLinks/source_code.jsx
+++ contribut3.near/SocialLinks/source_code.jsx
@@ -11,3 +11,43 @@ if ("github" in links) {
     </li>
   );
 }
+
+// if ("discord" in links) {
+//   linksList.push(
+//     <li>
+//       <a href={`https://discord.com/u/${links["discord"]}`} target="_blank">
+//         <i className="bi-discord" />
+//       </a>
+//     </li>
+//   );
+// }
+
+if ("reddit" in links) {
+  linksList.push(
+    <li>
+      <a href={`https://reddit.com/u/${links["reddit"]}`} target="_blank">
+        <i className="bi-reddit" />
+      </a>
+    </li>
+  );
+}
+
+if ("twitter" in links) {
+  linksList.push(
+    <li>
+      <a href={`https://twitter.com/${links["twitter"]}`} target="_blank">
+        <i className="bi-twitter" />
+      </a>
+    </li>
+  );
+}
+
+if ("youtube" in links) {
+  linksList.push(
+    <li>
+      <a href={`https://youtube.com/${links["youtube"]}`} target="_blank">
+        <i className="bi-youtube" />
+      </a>
+    </li>
+  );
+}
