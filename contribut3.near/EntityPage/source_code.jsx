640577fa56d150af01ce2ed0a87fd08e6f497211
contribut3.near <contribut3.near>
2023-02-22 08:38:26 -0500

    Update EntityPage by contribut3.near at 2023-02-22 08:38:26.858

--- contribut3.near/EntityPage/source_code.jsx
+++ contribut3.near/EntityPage/source_code.jsx
@@ -50,32 +50,10 @@ const body = (
           props={{
             accountId,
             isEntity: true,
-            imageSize: "3em",
+            imageSize: "4em",
             update: props.update,
           }}
         />
-        <div>
-          <div className="d-flex flex-row justify-content-start">
-            <div className="m-2">{circle}</div>
-            <div className="d-flex flex-column justify-content-between align-items-start w-100">
-              <div className="w-100 d-flex flex-row justify-content-between align-items-start">
-                <div>
-                  <b>{profile.name}</b>
-                  <span className="text-muted">@{accountId}</span>
-                </div>
-                <div className="text-success">
-                  <i className="bi-play" />
-                  <span className="ms-1">{entity.status}</span>
-                </div>
-              </div>
-              <div className="d-flex flex-row justify-content-start align-items-center my-1">
-                {founderCircle}
-                <span className="mx-1">{founderProfile.name}</span>
-                <span className="text-muted">@{founder}</span>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
       <div className="d-flex flex-row justify-content-end align-items-start ps-4 py-3">
         <a
