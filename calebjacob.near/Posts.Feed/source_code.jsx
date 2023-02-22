06cbe743f1a55751a3b0728cf6879aa00ce8fb76
calebjacob <calebjacob>
2023-02-21 22:45:20 -0500

    Update Posts.Feed by calebjacob.near at 2023-02-21 22:45:20.379

--- calebjacob.near/Posts.Feed/source_code.jsx
+++ calebjacob.near/Posts.Feed/source_code.jsx
@@ -10,16 +10,16 @@ const index = {
 
 const Post = styled.div`
   border-bottom: 1px solid #ECEEF0;
-  padding: 24px 24px 12px;
+  padding: 24px 0 12px;
 
   @media (max-width: 1200px) {
-    padding: 12px 12px 0px;
+    padding: 12px 0 0;
   }
 `;
 
 const renderItem = (a) =>
   a.value.type === "md" && (
-    <Post key={JSON.stringify(a)}>
+    <Post className="post" key={JSON.stringify(a)}>
       <Widget
         src="calebjacob.near/widget/Posts.Post"
         props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
@@ -27,8 +27,4 @@ const renderItem = (a) =>
     </Post>
   );
 
-return (
-  <div>
-    <Widget src="mob.near/widget/IndexFeed" props={{ index, renderItem }} />
-  </div>
-);
+return <Widget src="mob.near/widget/IndexFeed" props={{ index, renderItem }} />;
