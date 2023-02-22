171b6b9fd06a8a09af9e12c2f8ab156d1266e106
contribut3.near <contribut3.near>
2023-02-22 09:47:23 -0500

    Update SocialLinks by contribut3.near at 2023-02-22 09:47:23.221

--- contribut3.near/SocialLinks/source_code.jsx
+++ contribut3.near/SocialLinks/source_code.jsx
@@ -5,7 +5,7 @@ const linksList = [];
 if ("github" in links) {
   linksList.push(
     <li>
-      <a href={`https://github.com/${links["github"]}`}>
+      <a href={`https://github.com/${links["github"]}`} target="_blank">
         <i className="bi-github" />
       </a>
     </li>
