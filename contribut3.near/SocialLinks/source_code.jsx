cecbcbba50de4f9d3a978edfe3daceb722004363
contribut3.near <contribut3.near>
2023-02-22 09:55:34 -0500

    Update SocialLinks by contribut3.near at 2023-02-22 09:55:34.061

--- contribut3.near/SocialLinks/source_code.jsx
+++ contribut3.near/SocialLinks/source_code.jsx
@@ -1,12 +1,23 @@
 const links = props.links;
 
-const linksList = [];
-
 const supportedLinks = [
   { name: "github", url: "https://github.com/", icon: "bi-github" },
   { name: "discord", url: "https://discord.com/", icon: "bi-discord" },
-  {},
+  { name: "reddit", url: "https://reddit.com/u/", icon: "bi-reddit" },
+  { name: "twitter", url: "https://twitter.com/", icon: "bi-twitter" },
+  { name: "youtube", url: "https://youtube.com/", icon: "bi-youtube" },
+  { name: "website", url: "https://", icon: "bi-globe2" },
 ];
+
+const linksList = supportedLinks
+  .filter(({ name }) => name in links)
+  .map(({ name, url, icon }) => (
+    <li>
+      <a href={`${url}${links[name]}`} target="_blank">
+        <i className={icon} />
+      </a>
+    </li>
+  ));
 
 if ("github" in links) {
   linksList.push(
