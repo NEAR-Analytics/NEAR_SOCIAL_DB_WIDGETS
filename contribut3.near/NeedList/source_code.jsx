65fdb65ac283f7ae8c15506c5f75b00e623cdaf5
contribut3.near <contribut3.near>
2023-02-23 15:41:15 -0500

    Update NeedList by contribut3.near at 2023-02-23 15:41:15.996

--- contribut3.near/NeedList/source_code.jsx
+++ contribut3.near/NeedList/source_code.jsx
@@ -20,8 +20,6 @@ if (Object.keys(needs).length === 0) {
   return "Couldn't find any contribution needs!";
 }
 
-console.log(needs);
-
 const allNeeds = Object.keys(needs)
   .reduce((list, accountIdOrCid) => {
     if (props.accountId) {
