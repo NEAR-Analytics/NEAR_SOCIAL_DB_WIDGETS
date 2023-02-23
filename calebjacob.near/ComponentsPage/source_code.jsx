f942fb554cf10a867ed68bed43e71207e66d784c
calebjacob <calebjacob>
2023-02-23 17:41:38 -0500

    Update ComponentsPage by calebjacob.near at 2023-02-23 17:41:38.435

---
+++ calebjacob.near/ComponentsPage/source_code.jsx
@@ -0,0 +1,152 @@
+// const limit = 5;
+let components = [];
+let totalComponents = 0;
+
+const data = Social.keys("*/widget/*", "final", {
+  return_type: "BlockHeight",
+});
+
+if (data) {
+  const result = [];
+
+  Object.keys(data).forEach((accountId) => {
+    return Object.keys(data[accountId].widget).forEach((widgetName) => {
+      totalComponents++;
+
+      result.push({
+        accountId,
+        widgetName,
+        blockHeight: data[accountId].widget[widgetName],
+      });
+    });
+  });
+
+  result.sort((a, b) => b.blockHeight - a.blockHeight);
+  components = result.slice(0, 21);
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const H1 = styled.h1`
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 39px;
+  color: #11181C;
+  margin: 0;
+`;
+
+const H2 = styled.h2`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+  color: #687076;
+  margin: 0;
+`;
+
+const Text = styled.p`
+  margin: 0;
+  line-height: 1.5rem;
+  color: ${(p) => (p.bold ? "#11181C" : "#687076")} !important;
+  font-weight: ${(p) => (p.bold ? "600" : "400")};
+  font-size: ${(p) => (p.small ? "12px" : "14px")};
+  overflow: ${(p) => (p.ellipsis ? "hidden" : "")};
+  text-overflow: ${(p) => (p.ellipsis ? "ellipsis" : "")};
+  white-space: ${(p) => (p.ellipsis ? "nowrap" : "")};
+  overflow-wrap: anywhere;
+
+  b {
+    font-weight: 600;
+    color: #11181C;
+  }
+  
+  &[href] {
+    display: inline-flex;
+    gap: 0.25rem;
+    
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const Items = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 12px;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+
+  @media (max-width: 900px) {
+    grid-template-columns: minmax(0, 1fr);
+  }
+`;
+
+const Item = styled.div``;
+
+const Button = styled.button`
+  display: block;
+  width: 100%;
+  padding: 8px;
+  height: 32px;
+  background: #FBFCFD;
+  border: 1px solid #D7DBDF;
+  border-radius: 6px;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 15px;
+  text-align: center;
+  cursor: pointer;
+  color: #11181C !important;
+  margin: 0;
+
+  &:hover,
+  &:focus {
+    background: #ECEDEE;
+    text-decoration: none;
+    outline: none;
+  }
+
+  span {
+    color: #687076 !important;
+  }
+`;
+
+return (
+  <Wrapper>
+    <Header>
+      <H1>Components</H1>
+      <H2>Discover the latest components from the NEAR community.</H2>
+    </Header>
+
+    <Text>{totalComponents} components</Text>
+
+    <Items>
+      {components.map((component, i) => (
+        <Item key={i}>
+          <Widget
+            src="calebjacob.near/widget/ComponentCard"
+            props={{
+              src: `${component.accountId}/widget/${component.widgetName}`,
+              blockHeight: component.blockHeight,
+            }}
+          />
+        </Item>
+      ))}
+    </Items>
+
+    <Button type="button">Load More</Button>
+  </Wrapper>
+);
