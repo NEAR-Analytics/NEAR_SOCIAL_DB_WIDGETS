bf7cbddd07996ae6ef562fd2c12b2d418008c886
contribut3.near <contribut3.near>
2023-02-23 13:19:10 -0500

    Update ContributionList by contribut3.near at 2023-02-23 13:19:10.269

--- contribut3.near/ContributionList/source_code.jsx
+++ contribut3.near/ContributionList/source_code.jsx
@@ -1,1 +1,33 @@
+const ownerId = "contribut3.near";
+const accountId = props.accountId;
+const isEntity = props.isEntity ?? false;
+
+if (!accountId) {
+  return "Cannot render contribution list without account ID!";
+}
+
+const contributions = Object.keys(
+  Near.view(
+    ownerId,
+    isEntity ? "get_entity_contributions" : "get_contributor_contributions",
+    { contributor_id: accountId },
+    "final",
+    true
+  ) ?? {}
+);
+
+if (!contributions) {
+  return "Loading...";
+}
+
+if (Array.isArray(contributions) && contributions.length === 0) {
+  return "No contribution requests found!";
+}
+
+const allContributions = contributions.filter((id) => id.includes(search));
+
+if (!allContributions || allContributions.length === 0) {
+  return "No requests match search criteria!";
+}
+
 return "";
