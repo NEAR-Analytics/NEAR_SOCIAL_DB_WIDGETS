3402118e11d257b8897537f76f29f923dd913614
gov.near <gov.near>
2023-02-23 14:30:15 -0500

    Update Global by gov.near at 2023-02-23 14:30:15.022

--- gov.near/Global/source_code.jsx
+++ gov.near/Global/source_code.jsx
@@ -41,30 +41,9 @@ return (
         and follow us here on Near Social.
       </p>
     </div>
-    <div className="bg-light px-4 pb-4">
-      <div className="d-md-flex justify-content-between pt-3 mb-2">
-        <div style={{ paddingTop: "3rem" }}>
-          <div className="me-2 d-sm-flex gap-1 flex-row align-items-center">
-            <div className="me-2 position-relative">
-              <b>{name}</b>
-              <div className="small text-truncate">
-                <i className="bi bi-person-fill text-secondary"></i>
-                {ownerId}
-                <Widget
-                  src="mob.near/widget/FollowsYouBadge"
-                  props={{ ownerId }}
-                />
-              </div>
-            </div>
-
-            <div>
-              <Widget src="mob.near/widget/FollowButton" props={{ ownerId }} />
-            </div>
-          </div>
-          <div>
-            <Widget src="gov.near/widget/JoinStats" props={{ ownerId }} />
-          </div>
-        </div>
+    <div>
+      <div>
+        <Widget src="gov.near/widget/Join" props={{ ownerId }} />
       </div>
     </div>
     <h2>Connect and Collaborate</h2>
