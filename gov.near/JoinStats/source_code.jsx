7e4fcf30ecfc879b63aaa8f146a88969b4e2af6b
gov.near <gov.near>
2023-02-23 14:23:02 -0500

    Update JoinStats by gov.near at 2023-02-23 14:23:02.609

--- gov.near/JoinStats/source_code.jsx
+++ gov.near/JoinStats/source_code.jsx
@@ -18,7 +18,7 @@ const numFollowers = followers ? Object.keys(followers || {}).length : null;
 return (
   <div>
     <div className="d-flex flex-row">
-      <div>
+      <div className="m-2">
         <a
           href={`#/mob.near/widget/FollowPage?ownerId=${ownerId}&tab=followers`}
           className="text-dark"
@@ -29,6 +29,19 @@ return (
             "?"
           )}{" "}
           <span className="text-muted">Member{numFollowers !== 1 && "s"}</span>
+        </a>
+      </div>
+      <div className="m-2">
+        <a
+          href={`#/gov.near/widget/JoinPage?ownerId=${ownerId}&tab=following`}
+          className="text-dark"
+        >
+          {numFollowing !== null ? (
+            <span className="fw-bolder">{numFollowing}</span>
+          ) : (
+            "?"
+          )}{" "}
+          <span className="text-muted">Leader{numFollowing !== 1 && "s"}</span>
         </a>
       </div>
     </div>
