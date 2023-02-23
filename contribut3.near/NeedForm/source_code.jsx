eab9505c9dfe0dd17813a9bbd1ba351dcc90a765
contribut3.near <contribut3.near>
2023-02-23 11:09:10 -0500

    Update NeedForm by contribut3.near at 2023-02-23 11:09:10.489

--- contribut3.near/NeedForm/source_code.jsx
+++ contribut3.near/NeedForm/source_code.jsx
@@ -87,7 +87,7 @@ const onSubmit = () => {
 
 return (
   <div className="px-3" style={{ maxWidth: "45em" }}>
-    <h1 className="fs-2">Create new contribution request</h1>
+    <h1 className="fs-2 mb-3">Create new contribution request</h1>
     <div className="bg-light m-2 p-4 rounded-2">
       <div className="row">
         {entityIdInput}
