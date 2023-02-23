8d3669db601197e72c5918bcfb4ea0c770882925
mpeterdev <mpeterdev>
2023-02-22 21:27:52 -0500

    Update TosContentDraft by michaelpeter.near at 2023-02-22 21:27:52.719

--- michaelpeter.near/TosContentDraft/source_code.jsx
+++ michaelpeter.near/TosContentDraft/source_code.jsx
@@ -1,115 +1,113 @@
-const tos = `
-# alpha.near.org terms of service
+//options: default, terms, privacy, community
+State.init({ show: "default", terms: null, privacy: null, community: null });
 
-## Clarum docto extra det sub nova
+const termsCidPath =
+  "bafybeieqhkr6kxqc5v3fmuieuiaeh6qb57pk6nyy575n7yglfdmx6ophla/terms.md";
+const privacyCidPath =
+  "bafybeib6csg3j4mudfyhcb7akpfmxtlzqluqp4jmt5va5iumjucrqwhhkq/privacy.md";
+const communityCidPath = "TODO";
 
-Lorem markdownum expendite luminibus tenent quoque, sustinet iuxta, et revocare
-ne potes, Theseus nulla volutant erunt plus. Loqui regna et deum Iuventae quas,
-consulit adsiduis invenit in origine referat inhaesi Typhoea desine, Herculis
-Enipeus. Pentheus ipse officium clamat saturatos concessa, in fretum eo
-coniunctaque si femina et Iapetionides fugiat furit nebulas. Se et nunc paterer
-paelicis et totum reflectitur facta protinus salignis fortibus prius Lacon
-mortisque, variarum? Montibus inmoritur meruere laquei, generis Athamanas
-nympham tantaque, amoris caeruleum prohibete inhospita curruque legit; *tactus*
-una primis.
+asyncFetch(`https://ipfs.near.social/ipfs/${termsCidPath}`).then((res) => {
+  State.update({ terms: res.body });
+});
+asyncFetch(`https://ipfs.near.social/ipfs/${privacyCidPath}`).then((res) => {
+  State.update({ privacy: res.body });
+});
+// asyncFetch(`https://ipfs.near.social/ipfs/${termsCidPath}`).then((res) => {
+//   State.update({ terms: res.body });
+// });
 
-## Fuerat sit et fatetur innixamque hoc
-
-Accepisse lascivaque ausa tempora urna non corpore facta aequora nostrae. Par
-lecto solutus gaeminis tacta: est annum, pudorem nititur teste caput Baccho;
-vellet inscripta probat ualtima. aCaiacus et ateampora aliter quondam lacertis fato
-exit sequentur **illo**; Iovi Phoeabus quaea Sabaea verba summo suus. Regia
-invadunt circuiere sequenti remos in gerai aliamve, quies quae dixi maanu Musa
-inpune, huc temptamenta vir. Adahuc in gramen parari columnae et esse, sed tamen
-prima Apolline, ad acuta tantum.
-
-    var pageGrepAdd = cmykCompatible;
-    if (enterpriseServerSprite(cms)) {
-        key_error = pcmcia;
-        led_vaporware.filePathWeb(utf, shareware);
-    }
-    piracyDual(recordPointLink, blu_console, 18 * partition);
-
-## Adhuc ergo sub rettulit mihi lacrimisque refugam
-
-Tamen [in retentus cum](http://www.quem-cognoscere.org/) faciem Aeneae parabat
-numero metuit **priorum quotiensque** canos? Serpentis mox esse sua leporem
-Phoebi culpa utroque, me supra tantorum o?
-
-Duratos dextroque, in Haec deprendere vixque? Omne signum humi iactata; signatum
-corneus credens!
-
-## Praecluserat ossa consulat iussa
-
-Othryn ruit nervis utile. Se tam totumque, Lelegas et vulnus **vetustas**
-matresque deae lambit me. Flores inruampere optes silvisque sumptoque modo totam;
-et sparso proxima poenaeque et amor *tantum occupet caelatus* honores.
-Circumstant Iuppiter fera domitae corpore viderat suco aperire primum occubuisse
-tectus. Qua solent litus, nec vina ut plagas illis honoris, ante querenda non
-Siculique.
-
-Maiora esse puppes pendere, hoc decoro eripiunt hostisque perire movet, quanto.
-Volucris monstris reponit et ante ora, fuit, et!
-
-## Recenti honorem brevis
-
-Lorem markdownum pleno, flavescere altas ut obscenis et solvit aspicit. **Pennis
-ales** domos mente, in ait foeda comitum, septimus vite Pyrois pennae cernam
-**in mensis patrem**, in.
-
-## Germanamque dulci ipsos
-
-Huic iam mea fregit vetus potentia repono regna mirabile insidias herba et terra
-iactatis nullos vulnere; aras. Graditur surgere obstipuit oras in et ratione!
-
-> Solem redimebat possederat numen me dabat deceptaque relictis fecit leges
-> Marte suae adest dignus vultu, numen pacatum. Consistere adventum illuc, et
-> urbes temptanda esse coeunt longa cognoscite lactisque quod! Enim signa animis
-> et exarsit virgo, in vacet fecundaque sanguine blanditiis obortis; et erat
-> intercipe silva. Consurgere haec iuppiter suos. Surgit nata fetus cacumine:
-> auras cum: alieni nec magno quondam.
-
-## Padumque sanguine quoniam tridentigero avidos
-
-Portans sua *hunc* fraxineam quod cacumina ne dumque intertextos, procul et
-extento membra utero; quam. Murice *secuta succedere ad* nympha *necis* linoque
-sic.
-
-    flash_gate_mbr = 380198 + -2;
-    var pharming_bank = flat;
-    emoticon.firmware_zebibyte.uat_disk(javaDesign + passwordBitWebsite,
-            dragControl(media(3, -4, classBridgeDefragment),
-            horizontal_isdn_pad, osiBit));
-    cleanMonochromeLock = netbios(5, -5);
-    windows.graphic_dbms -= power_file_rom(fileCoreSoftware + boot_clock_dimm *
-            thunderbolt, irc(real));
-
-## Aevo clamatque et pulchra omnes
-
-Olivae parantem portante obstrepuere quod oblita lingua. Aequora resupinum
-inquit aliena, lacertos, das et moriorque rabies, cum lacrimante **tuque** me
-nemo. Castrorum et Iunonis olor, nisi pedum Aiacis,
-[Bacchus](http://manetvera.io/edidit.aspx); est. Solo arma deprendimur
-reversurum volucris tyranni ortus.
-
-## Maesto effugit sed femina paterno hinc cupidine
-
-Est memor duce; scitatur sic inmeritae cum faciat facta, dixit unda unum
-stagnumque pocula adhuc. Coeptus *talis corpore* nervos stridit morte perque in
-inquam dicit, linguam.
-
-- Fugat corpore et erant
-- Quas in me mutet
-- Aurora geminas animos
-- Qui puer quantumque
-- Respice est ferebat tutos saepe et dapibus
-
-Esse sacra estque sed, de pondere tui arbor scit turba in placuit tulit Delphice
-terris temptare? Socios sua caelo collocat coepit sociantem antemnae; opus orbis
-in templa exhausto, quis utque precesque. Inmotusque hamis eodem illinc cecidere
-fictamque luctibus deosque, vocatur est una indigenae regis, ait suo hoc, vani.
-Amoris uno huc squamaque iaculum conserto torta omnia tamen invitam, septem et
-ibat nequit aequantur est hydrae vagina.
+const DocBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.5rem;
+  border: solid 1px lightgrey;
+  border-radius: 0.75rem;
+  padding: 1rem;
+  &:hover {
+    color: var(--bs-green);
+    border-color: var(--bs-green);
+  }
 `;
 
-return <Markdown text={tos} />;
+const DocTitle = styled.div`
+display: flex;
+flex-direction: row;
+column-gap: 0.5rem;
+font-weight: bold;
+align-items: center;
+`;
+
+const DocSummary = styled.span`
+color: grey
+`;
+
+const Arrow = styled.i`
+  color: var(--bs-green);
+  font-size: 1.25rem;
+`;
+
+// const DocFullDisplay = styled.div`
+//   display: flex;
+//   flex
+// `;
+
+const DocContent = styled.div`
+  overflow-y: scroll;
+`;
+
+return (
+  <div className="d-flex flex-column gap-3 justify-content-center">
+    {state.show === "default" && (
+      <>
+        <h1>Terms, Privacy, and Community Guidelines</h1>
+        <p>
+          To continue using alpha.near.org, please read and agree to the
+          following terms and policies:
+        </p>
+        <DocBox
+          onClick={() => {
+            State.update({ show: "terms" });
+          }}
+        >
+          <DocTitle>
+            Terms of Service
+            <Arrow className="bi bi-arrow-right" />
+          </DocTitle>
+          <DocSummary>Summary</DocSummary>
+        </DocBox>
+        <DocBox>
+          <DocTitle>
+            Privacy Policy
+            <Arrow className="bi bi-arrow-right" />
+          </DocTitle>
+          <DocSummary>Summary</DocSummary>
+        </DocBox>
+        <DocBox>
+          <DocTitle>
+            Community Guidelines
+            <Arrow className="bi bi-arrow-right" />
+          </DocTitle>
+          <DocSummary>Summary</DocSummary>
+        </DocBox>
+      </>
+    )}
+    {state.show === "terms" && (
+      <>
+        <button
+          onClick={() => {
+            State.update({ show: "default" });
+          }}
+          className="btn btn-outline-success"
+          style={{ width: "6rem" }}
+        >
+          <i className="bi bi-arrow-left" />
+          Back
+        </button>
+        <DocContent>
+          <Markdown text={state.terms} />
+        </DocContent>
+      </>
+    )}
+  </div>
+);
