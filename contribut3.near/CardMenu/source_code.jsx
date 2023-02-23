f42c14c26d307d06f257f17ded113ae70d5d44db
contribut3.near <contribut3.near>
2023-02-23 10:01:10 -0500

    Update CardMenu by contribut3.near at 2023-02-23 10:01:10.071

--- contribut3.near/CardMenu/source_code.jsx
+++ contribut3.near/CardMenu/source_code.jsx
@@ -16,7 +16,7 @@ const menuItems = items.reduce(
         href={href}
         onClick={onClick}
         role="button"
-        data-bs-toggle={toggle ? "modal" : ""}
+        {...(toggle ? { "data-bs-toggle": "modal" })}
         data-bs-target={target}
       >
         <i className={icon} />
