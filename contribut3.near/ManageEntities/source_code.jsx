fa9939b915bc4c9b75a895932e1d039f92b23346
contribut3.near <contribut3.near>
2023-02-23 16:08:23 -0500

    Update ManageEntities by contribut3.near at 2023-02-23 16:08:23.102

--- contribut3.near/ManageEntities/source_code.jsx
+++ contribut3.near/ManageEntities/source_code.jsx
@@ -19,47 +19,6 @@ const header = (
   </div>
 );
 
-const createNewButton = ({ id, text, icon }) => (
-  <li>
-    <a className="dropdown-item" id={id}>
-      <i className={icon} />
-      <span>{text}</span>
-    </a>
-  </li>
-);
-
-const createNewDropdown = (
-  <div className="dropdown">
-    <a
-      className="btn btn-info dropdown-toggle"
-      style={{ backgroundColor: "#7f56d9", borderColor: "#7f56d9" }}
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      Create new...
-    </a>
-    <ul className="dropdown-menu">
-      {createNewButton({
-        id: "request",
-        text: "Contribution request",
-        icon: "bi-ui-checks-grid",
-      })}
-      <li>
-        <hr className="dropdown-divider" />
-      </li>
-      {createNewButton({ id: "project", text: "Project", icon: "bi-boxes" })}
-      <li>
-        <hr className="dropdown-divider" />
-      </li>
-      {createNewButton({
-        id: "organization",
-        text: "Organization",
-        icon: "bi-diagram-2",
-      })}
-    </ul>
-  </div>
-);
-
 const contentSelector = (
   <Widget
     src={`${ownerId}/widget/TabSelector`}
@@ -87,27 +46,6 @@ const contentSelector = (
       ],
     }}
   />
-);
-
-const searchBar = (
-  <div className="w-25 col-12 col-md-10 col-lg-8">
-    <div className="card card-sm">
-      <div className="card-body row p-0 ps-2 align-items-center">
-        <div className="col-auto pe-0 me-0">
-          <i className="bi-search" />
-        </div>
-        <div className="col ms-0">
-          <input
-            className="form-control border-0"
-            type="search"
-            value={props.search}
-            placeholder="Search"
-            onChange={(e) => props.update({ search: e.target.value })}
-          />
-        </div>
-      </div>
-    </div>
-  </div>
 );
 
 const content = {
