2d83a0c06373fa75bf2ca6c991c7d5aec170bfd7
calebjacob <calebjacob>
2023-02-22 23:20:06 -0500

    Update AccountAutocomplete by calebjacob.near at 2023-02-22 23:20:06.468

--- calebjacob.near/AccountAutocomplete/source_code.jsx
+++ calebjacob.near/AccountAutocomplete/source_code.jsx
@@ -53,8 +53,8 @@ function onResultClick(id) {
 const Wrapper = styled.div`
   background: #ECEEF0;
   display: flex;
-  gap: 6px;
-  padding: 6px;
+  gap: 8px;
+  padding: 8px;
   overflow: auto;
   scroll-behavior: smooth;
   align-items: center;
