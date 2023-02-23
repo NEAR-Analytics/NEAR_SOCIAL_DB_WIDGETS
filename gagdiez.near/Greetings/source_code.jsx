16c97cf14c2f0a7370b9041646f5d407e0458ea5
gagdiez.near <gagdiez.near>
2023-02-23 14:23:22 -0500

    Update Greetings by gagdiez.near at 2023-02-23 14:23:22.932

---
+++ gagdiez.near/Greetings/source_code.jsx
@@ -0,0 +1,13 @@
+let greeting = "Have a great day";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <h1>Hello {props.name}</h1>
+
+      <p> {greeting} </p>
+
+      <button class="btn btn-primary"> Bootstrap Button </button>
+    </div>
+  </>
+);
