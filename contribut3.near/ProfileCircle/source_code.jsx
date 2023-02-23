61f035f755813b48870d14d3fd9cd434eb77ecaf
contribut3.near <contribut3.near>
2023-02-23 12:09:01 -0500

    Update ProfileCircle by contribut3.near at 2023-02-23 12:09:01.730

--- contribut3.near/ProfileCircle/source_code.jsx
+++ contribut3.near/ProfileCircle/source_code.jsx
@@ -11,28 +11,10 @@ Near.asyncView(
   ownerId,
   isEntity ? "get_entity" : "get_contributor",
   { account_id: accountId },
-  "final",
-  true
+  "final"
 ).then((data) => State.update({ data }));
 
 const profile = Social.getr(`${accountId}/profile`);
-
-if (!state.data || !profile) {
-  return (
-    <div
-      className="profile-circle d-inline-block"
-      title={`loading...`}
-      style={{ width: size, height: size }}
-    >
-      <img
-        className="rounded-circle w-100 h-100"
-        style={{ objectFit: "cover" }}
-        src={`https://i.near.social/thumbnail/https://thewiki.io/static/media/sasha_anon.6ba19561.png`}
-        alt="profile image"
-      />
-    </div>
-  );
-}
 
 const fullName = profile.name || state.data.name || accountId;
 const image = profile.image;
@@ -41,17 +23,19 @@ const url =
     ? `https://ipfs.near.social/ipfs/${image.ipfs_cid}`
     : image.url) || "https://thewiki.io/static/media/sasha_anon.6ba19561.png";
 
-return (
-  <div
-    className="profile-circle d-inline-block"
-    title={`${fullName} @${accountId}`}
-    style={{ width: size, height: size }}
-  >
-    <img
-      className="rounded-circle w-100 h-100"
-      style={{ objectFit: "cover" }}
-      src={`https://i.near.social/thumbnail/${url}`}
-      alt="profile image"
-    />
-  </div>
-);
+if (state.data && profile) {
+  return (
+    <div
+      className="profile-circle d-inline-block"
+      title={`${fullName} @${accountId}`}
+      style={{ width: size, height: size }}
+    >
+      <img
+        className="rounded-circle w-100 h-100"
+        style={{ objectFit: "cover" }}
+        src={`https://i.near.social/thumbnail/${url}`}
+        alt="profile image"
+      />
+    </div>
+  );
+}
