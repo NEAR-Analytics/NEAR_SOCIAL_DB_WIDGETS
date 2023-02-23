d485bb088db0007219399c7be5829d12ff6ebfae
contribut3.near <contribut3.near>
2023-02-23 16:03:14 -0500

    Update Dashboard by contribut3.near at 2023-02-23 16:03:14.264

--- contribut3.near/Dashboard/source_code.jsx
+++ contribut3.near/Dashboard/source_code.jsx
@@ -19,58 +19,6 @@ const header = (
   </div>
 );
 
-const createNewButton = ({ id, text, icon, kind }) => (
-  <li>
-    <a
-      className="dropdown-item"
-      href={`https://near.social/#/${ownerId}/widget/Index?tab=create&content=${id}${kind ? "&kind=" + kind : ""
-        }`}
-      onClick={() => props.update("create")}
-    >
-      <i className={icon} />
-      <span>{text}</span>
-    </a>
-  </li>
-);
-
-const createNewDropdown = (
-  <div className="dropdown">
-    <a
-      className="btn btn-info dropdown-toggle"
-      style={{ backgroundColor: "#7f56d9", borderColor: "#7f56d9" }}
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      Create new...
-    </a>
-    <ul className="dropdown-menu">
-      {createNewButton({
-        id: "request",
-        text: "Contribution request",
-        icon: "bi-ui-checks-grid",
-      })}
-      <li>
-        <hr className="dropdown-divider" />
-      </li>
-      {createNewButton({
-        id: "entity",
-        text: "Project",
-        icon: "bi-boxes",
-        kind: "Project",
-      })}
-      <li>
-        <hr className="dropdown-divider" />
-      </li>
-      {createNewButton({
-        id: "entity",
-        text: "Organization",
-        icon: "bi-diagram-2",
-        kind: "Organization",
-      })}
-    </ul>
-  </div>
-);
-
 const contentSelector = (
   <Widget
     src={`${ownerId}/widget/TabSelector`}
@@ -98,27 +46,6 @@ const contentSelector = (
       ],
     }}
   />
-);
-
-const searchBar = (
-  <div className="w-25 col-12 col-md-10 col-lg-8">
-    <div className="card card-sm">
-      <div className="card-body row p-0 ps-2 align-items-center">
-        <div className="col-auto pe-0 me-0">
-          <i className="bi-search" />
-        </div>
-        <div className="col ms-0">
-          <input
-            className="form-control border-0"
-            type="search"
-            value={props.search}
-            placeholder="Search"
-            onChange={(e) => props.update({ search: e.target.value })}
-          />
-        </div>
-      </div>
-    </div>
-  </div>
 );
 
 const content = {
