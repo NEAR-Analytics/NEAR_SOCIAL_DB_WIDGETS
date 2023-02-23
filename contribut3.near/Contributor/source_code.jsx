4455de949aafbfa7619a745997afdc59c77c2779
contribut3.near <contribut3.near>
2023-02-23 14:31:54 -0500

    Update Contributor by contribut3.near at 2023-02-23 14:31:54.476

--- contribut3.near/Contributor/source_code.jsx
+++ contribut3.near/Contributor/source_code.jsx
@@ -101,7 +101,6 @@ const body = (
                       icon: "bi-info-circle",
                       href: `https://near.social/#/${ownerId}/widget/Index?tab=contributor&accountId=${accountId}`,
                       onClick: () =>
-                        props.update &&
                         props.update({
                           tab: "contributor",
                           content: "",
