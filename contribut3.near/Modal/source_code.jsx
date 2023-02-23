f13e8bfd6d15bfb6469239d1fef5db221c5936c2
contribut3.near <contribut3.near>
2023-02-23 09:25:07 -0500

    Update Modal by contribut3.near at 2023-02-23 09:25:07.846

---
+++ contribut3.near/Modal/source_code.jsx
@@ -0,0 +1,26 @@
+const title = props.title;
+const body = props.body;
+const confirmText = props.confirmText;
+const onConfirm = props.onConfirm;
+
+return (
+  <div className="modal" tabIndex="-1">
+    <div className="modal-dialog">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title">{title}</h5>
+          <a className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+        </div>
+        <div className="modal-body">{body}</div>
+        <div className="modal-footer">
+          <a className="btn btn-secondary" data-bs-dismiss="modal">
+            Close
+          </a>
+          <a className="btn btn-primary" onClick={onConfirm}>
+            {confirmText}
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+);
