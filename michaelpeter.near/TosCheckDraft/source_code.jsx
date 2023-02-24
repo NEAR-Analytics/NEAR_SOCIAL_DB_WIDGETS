b55939278ee987dd5289dfa76928bee05dd43654
mpeterdev <mpeterdev>
2023-02-23 21:02:27 -0500

    Update TosCheckDraft by michaelpeter.near at 2023-02-23 21:02:27.683

--- michaelpeter.near/TosCheckDraft/source_code.jsx
+++ michaelpeter.near/TosCheckDraft/source_code.jsx
@@ -139,7 +139,7 @@ return (
               }}
               onClick={logOut}
             >
-              Sign Out (WIP)
+              Decline
             </button>
             <CommitButton
               style={{
