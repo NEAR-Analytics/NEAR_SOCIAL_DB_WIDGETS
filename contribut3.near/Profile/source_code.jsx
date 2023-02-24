74fb8b63c2bfb57a0ba73c575ba652a08637c7c8
contribut3.near <contribut3.near>
2023-02-24 10:43:28 -0500

    Update Profile by contribut3.near at 2023-02-24 10:43:28.645

--- contribut3.near/Profile/source_code.jsx
+++ contribut3.near/Profile/source_code.jsx
@@ -148,7 +148,7 @@ return (
   <div>
     <div className="mb-5">{body}</div>
     <div className="d-flex flex-row justify-content-between ps-3">
-      {contentSelector}
+      <span className="fw-bold">Conrtibutes to</span>
       <Widget
         src={`${ownerId}/widget/SearchInput`}
         props={{ search: props.search, update: props.update }}
