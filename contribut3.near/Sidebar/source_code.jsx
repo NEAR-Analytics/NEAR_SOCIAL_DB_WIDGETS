8a6da28862fc732be7c0d73a9dbdbedfe39a474b
contribut3.near <contribut3.near>
2023-02-24 10:06:21 -0500

    Update Sidebar by contribut3.near at 2023-02-24 10:06:21.078

--- contribut3.near/Sidebar/source_code.jsx
+++ contribut3.near/Sidebar/source_code.jsx
@@ -60,7 +60,7 @@ return (
       href={`https://near.social/#/${ownerId}/widget/Index`}
       onClick={() => props.update({ tab: "home", content: "", search: "" })}
     >
-      <h4 className="fs-5 text-nowrap d-flex flex-row align-items-center">
+      <h4 className="fs-4 text-nowrap d-flex flex-row align-items-center">
         <Widget src={`${ownerId}/widget/Logo`} props={{ size: 32 }} />
         <span className="ms-2">Web3 Combinator</span>
       </h4>
