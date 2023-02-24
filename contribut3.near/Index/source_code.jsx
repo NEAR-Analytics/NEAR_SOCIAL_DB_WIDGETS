0f5ca759b4ecfccfb2cff1c542f1b4f4d1f5eb62
contribut3.near <contribut3.near>
2023-02-24 09:46:26 -0500

    Update Index by contribut3.near at 2023-02-24 09:46:26.845

--- contribut3.near/Index/source_code.jsx
+++ contribut3.near/Index/source_code.jsx
@@ -102,13 +102,15 @@ const tabContent = {
 
 return (
   <div className="d-flex flex-row">
-    <div className="px-1">
-      <Widget
-        src={`${ownerId}/widget/Sidebar`}
-        props={{ tab: state.tab, update }}
-      />
+    <div className="d-flex flex-row position-sticky">
+      <div className="px-1">
+        <Widget
+          src={`${ownerId}/widget/Sidebar`}
+          props={{ tab: state.tab, update }}
+        />
+      </div>
+      <div className="vr mx-2" style={{ height: "90vh" }} />
     </div>
-    <div className="vr mx-2" style={{ height: "90vh" }} />
     <div className="w-100">{tabContent}</div>
   </div>
 );
