c4e996bf1f5f5aac7545b3ce27c113c51874e823
calebjacob <calebjacob>
2023-02-24 17:37:33 -0500

    Update AccountProfileCard by calebjacob.near at 2023-02-24 17:37:33.093

--- calebjacob.near/AccountProfileCard/source_code.jsx
+++ calebjacob.near/AccountProfileCard/source_code.jsx
@@ -11,7 +11,7 @@ const Card = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  gap: 12px;
+  gap: 16px;
   width: 100%;
   border-radius: 12px;
   z-index: 1070;
@@ -19,12 +19,12 @@ const Card = styled.div`
   border: 1px solid #ECEEF0;
   box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06);
   overflow: hidden;
-  padding: 12px;
+  padding: 16px;
 `;
 
 const CardLeft = styled.div`
   display: flex;
-  gap: 12px;
+  gap: 16px;
   align-items: center;
   width: 100%;
   min-width: 0;
