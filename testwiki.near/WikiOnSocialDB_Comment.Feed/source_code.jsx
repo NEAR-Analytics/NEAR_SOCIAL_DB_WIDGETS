c4ced5b884c63a35fb8817fd9631e33e61e8a848
testwiki.near <testwiki.near>
2023-02-23 20:57:01 -0500

    Update WikiOnSocialDB_Comment.Feed by testwiki.near at 2023-02-23 20:57:01.441

---
+++ testwiki.near/WikiOnSocialDB_Comment.Feed/source_code.jsx
@@ -0,0 +1,47 @@
+const addressForArticles = "wikiTest2Article";
+const addressForComments = "wikiTest2Comment";
+const authorForWidget = "testwiki.near";
+const index = {
+  action: addressForComments,
+  key: props.item,
+  options: {
+    limit: props.limit ?? 3,
+    order: "desc",
+    accountId: props.accounts,
+    subscribe: props.subscribe,
+  },
+};
+
+const raw = !!props.raw;
+
+const renderItem = (a) =>
+  a.value.type === "md" && (
+    <div key={JSON.stringify(a)}>
+      <Widget
+        src={`${authorForWidget}/widget/WikiOnSocialDB_Comment`}
+        props={{
+          accountId: a.accountId,
+          blockHeight: a.blockHeight,
+          highlight:
+            a.accountId === props.highlightComment?.accountId &&
+            a.blockHeight === props.highlightComment?.blockHeight,
+          raw,
+        }}
+      />
+    </div>
+  );
+
+return (
+  <div>
+    <Widget
+      src={`${authorForWidget}/widget/WikiOnSocialDB_ManualIndexFeed`}
+      props={{
+        index,
+        reverse: true,
+        renderItem,
+        nextLimit: 10,
+        loadMoreText: "Show earlier comments...",
+      }}
+    />
+  </div>
+);
