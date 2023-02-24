8f1f7954fb31d4fbfa53a72fd454fd6ae92c361b
contribut3.near <contribut3.near>
2023-02-24 12:12:45 -0500

    Update EntityForm by contribut3.near at 2023-02-24 12:12:45.958

--- contribut3.near/EntityForm/source_code.jsx
+++ contribut3.near/EntityForm/source_code.jsx
@@ -1,4 +1,5 @@
-const createDate = (date = new Date()) => {
+const createDate = (input) => {
+  const date = input ?? new Date();
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
