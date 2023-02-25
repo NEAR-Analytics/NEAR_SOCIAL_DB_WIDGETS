5e1b64be4e5f17a12d80067190a58cf141f25f0d
thebos.near <thebos.near>
2023-02-24 20:05:31 -0500

    Update Privacy by thebos.near at 2023-02-24 20:05:31.556

---
+++ thebos.near/Privacy/source_code.jsx
@@ -0,0 +1,113 @@
+const content = `
+Your Privacy Rights
+---
+
+Effective Date: February 24, 2023
+
+This Privacy Policy describes the types of information gathered by Proximity Labs Limited (BVI) (" **Proximity**", "us" or "we") in the process of providing http://bos.gg/ (the "Site"), how we use the information, with whom the information may be shared, what choices are available to you, and our efforts to protect the information you provide to us through the Site.
+
+By using the Site, you agree to allow us to process information in accordance with this Privacy Policy.
+
+This policy is subject to change. If our information retention or usage practices change, we will let you know by posting the Privacy Policy changes on the Site. Your continued use of the Service following our notice of changes to this Privacy Policy, or other method of legal acceptance, means you accept such changes. Please refer to the "Effective Date" above to see when this Policy was last updated.
+
+**Territoriality**
+
+This Privacy Policy may be subject to the provisions of the EU General Data Protection Regulation ("EU GDPR"), the UK General Data Protection Regulation ("UK-GDPR", and together with the EU GDPR, the "GDPR"), and other applicable privacy laws. Under the GDPR, Proximity is a data Controller. If you are an individual located in the United Kingdom, Northern Ireland, the European Economic Area, or Switzerland (collectively, and for the purposes of this Privacy Policy, the "EEA"), you are a Data Subject with certain protected privacy rights concerning your Personal Data. We will take commercially reasonable steps to maintain compliance with GDPR. Your Personal Data may identify you as a person, and thus may be referred to in this Privacy Policy as Personally Identifiable Information ("PII").
+
+1. **Who Collects Your Information On Our Service?**
+
+Under the GDPR, Proximity is what is known as a "Controller" of the Personal Data that you provide us with. We collect information from you on the Site, and we are responsible for protection of your information.
+
+1. **What Information Does Proximity Collect?**
+
+Like most websites delivered via the Internet, we may collect non-personally identifiable or anonymous information about you that is stored in technical log files. This information may include, but is not limited to, IP addresses, traffic routing data, system configuration information, and other information about traffic to our website.
+
+Automatically tracking Internet Protocol (IP) addresses is one method of automatically collecting information about your activities online and information volunteered by you. An IP address is a number that is automatically assigned to your device whenever you surf the internet.
+
+1. **Why Is My Information Being Collected?**
+
+We may accept and gather information to provide the Site to you. If you contact us, we collect your personal information so that we can respond to your requests for information. We may collect log information to help us to diagnose problems with our servers, administer the Site, calculate usage levels, and otherwise provide services to you.
+
+1. **How Do We Use the Information We Collect?**
+
+We may use anonymous information that we collect to enable you to access and use the Site. We may use any anonymous, aggregate information without restriction.
+
+1. **Do We Share Your Personal Information?**
+
+We may make available personal data or aggregate information to our affiliates and related entities, agents, representatives, service providers and contractors for the purposes outlined in this Privacy Policy. Any information shared with such service providers is subject to the terms of this Privacy Policy. All service providers that we engage with are restricted to only utilizing the information according to our instructions. We work with persons or organizations with whom we contract to carry out internal operations or business activities, which includes exchanging information with other companies and organizations for fraud protection, and spam and malware prevention.
+
+1. **What Are Your Rights Regarding Your Personal Information?**
+
+You have the right to obtain confirmation as to whether personal data concerning you is being processed, and, where that is the case, access the personal data and all the relevant information. For any questions about the process, you may send an email to legal@proximity.dev.
+
+1. **How Do We Retain and Protect Your Information?**
+
+We will hold your personal data only for as long as it is necessary for us to do so, having regard to the purposes described in this Policy and our own legal and regulatory requirements. We usually do not retain any data concerning users. From time to time, in order to perform analysis concerning the use of our Site and ameliorate its settings we will retain personal data for a maximum period of 90 days. The retained data will be immediately erased at the end of our analysis.
+
+Please be aware that public blockchains provide transparency into transactions and Proximity is not responsible for preventing or managing information broadcasted on a blockchain.
+
+1. ' **EEA' Privacy Rights.**
+
+If you currently reside in the EEA, the GDPR applies to your PII and you are a Data Subject. The GDPR requires that we, in our capacity as a Controller, have a legal basis to process your PII.
+
+1. We process your PII under one or more of the following legal bases:
+
+- Processing is necessary for our legitimate interests (or those of a third party) and your interests, and fundamental rights do not override those interests.
+- To perform the contract that we are about to enter with you (e.g., our Terms of Service).
+- To comply with a legal obligation.
+- We have your consent to do so.
+
+1. Under the GDPR, as a Data Subject you have certain rights. They are:
+
+- The right to be informed. This is your right to be informed about what we are processing, why, and who else the data may be passed to.
+- The right of access. This is your right to see what data about you is held by us.
+- The right to rectification. This is the right to have your data corrected or amended if what is held is incorrect in some way.
+- The right to be forgotten. This is the right to have your personal data to be deleted in the event that such data is no longer required for the purposes it was collected for, your consent for the processing of the data is withdrawn, or the data is being unlawfully processed.
+- The right to restrict processing. This is the right to ask for a temporary halt to processing of your personal data, such as in the case where a dispute or legal case has to be concluded, or the data is being corrected.
+- The right to data portability. This is the right to ask for your personal data to be provided to you in a structured, commonly used, and machine-readable format.
+- The right to object. This is the right to object to further processing your personal data if such processing is inconsistent with the primary purposes for which it was collected.
+- Rights in relation to automated decision making and profiling. This is the right to not be subject to a decision based solely on automated processing. The Site does not engage in automated decision making and profiling.
+
+You can find instructions for enforcing some of these rights elsewhere in this Privacy Policy. Otherwise, if you wish to find out more about these rights, please contact us at legal@proximity.dev.
+
+1. **Children and Young People's Information.**
+
+We do not knowingly collect any information from any minors, and we comply with all applicable privacy laws including the Children's Online Privacy Protection Act ("COPPA") and associated Federal Trade Commission ("FTC") rules for collecting personal information from minors. If you believe that your child under 13 has gained access to our Site without your permission and provided PII to us, please contact us at legal@proximity.dev and we will make efforts to delete the child's information in accordance with COPPA.
+
+1. **California Residents**
+
+This California Privacy Notice ("Notice") is for California residents, and describes how we collect, use, and share your personal information. This Notice supplements our Global Privacy Policy. Any terms defined in the California Consumer Privacy Act (as amended by the California Privacy Rights Act of 2020) ("CCPA") have the same meaning when used in this Notice.
+
+The CCPA, does not apply to certain information, such as information subject to the Gramm-Leach-Bliley Act. The specific personal information that we collect, use, and disclose relating to a California resident covered by the CCPA will vary based on our relationship or interaction with you.
+
+Sources of Personal Information. We may gather various types of personal information from individuals who access or use our Site, from a range of sources, such as:
+
+- information we receive from our Affiliates and/or third parties; and
+- information we collect automatically through cookies and similar technologies.
+
+Selling or Sharing of Personal Information. We do not have actual knowledge that we sell or share personal information of individuals under 16 years of age.
+
+Rights. As a California resident, you may have the rights listed below in relation to personal information that we have collected about you. However, these rights are not absolute, and in certain cases, we may decline your request as permitted by law.
+
+Right to Know. You have a right to request the following information about our collection, use and disclosure of your personal information over the prior 12 months, and ask that we provide you with a copy of the following:
+
+  1. categories of and specific pieces of personal information we have collected, sold, or shared about you;
+  2. categories of sources from which we collect personal information;
+  3. the business of commercial purposes for collecting personal information;
+  4. categories of third parties to whom the personal information was disclosed for a business purpose; and
+  5. categories of personal information disclosed about you for a business purpose.
+
+Right to Correct. You have a right to request that we correct inaccurate personal information maintained about you.
+
+Right to Delete. You have a right to request that we delete personal information, subject to certain exceptions.
+
+Right to Opt out of Selling or Sharing. You have the right to direct a business that sells or shares personal information about you to third parties to stop doing so.
+
+Right to Non-Discrimination. We will not discriminate against you for exercising any of these rights.
+
+**Authorized Agent.** Generally, if you are a California resident, you may designate an authorized agent to make a request to access or a request to delete on your behalf. To do so, you must: (1) provide that authorized agent written and signed permission to submit such a request; and (2) verify your own identity directly with us. Please note, we may deny a request from an authorized agent that does not submit proof that they have been authorized by you to act on your behalf. We will respond to your authorized agent's request if they submit proof that they are registered with the California Secretary of State to be able to act on your behalf, or submit evidence you have provided them with power of attorney pursuant to California Probate Code section 4121 to 4130. We may deny requests from authorized agents who do not submit proof that they have been authorized by you to act on their behalf, or are unable to verify their identity.
+
+If you have any questions or suggestions regarding our Privacy Policy, please contact us via e-mail at legal@proximity.dev.
+`;
+
+return <Markdown text={content} />;
