e9748d4281e64ad9a23b50c7cf40d14df125e19a
wecare.near <wecare.near>
2023-02-24 19:46:02 -0500

    Update BosTerms by wecare.near at 2023-02-24 19:46:02.598

---
+++ wecare.near/BosTerms/source_code.jsx
@@ -0,0 +1,24 @@
+const content = `
+Updated: 2/24/2023
+
+Terms of Use
+
+Welcome to bos.gg, a website ("Site") provided by Proximity Labs Limited (BVI) ("Proximity", "we", "our", or "us").
+
+THE SITE DOES NOT FACILITATE ANY ASPECT OF BLOCKCHAIN BASED DIGITAL ASSET TRANSACTIONS. THE SITE MERELY PROVIDES A SET OF TOOLS FOR USERS TO VIEW AND LOCALLY INTERACT WITH CODE THAT MAY BE RELEVANT TO BLOCKCHAIN BASED NETWORKS. THE ACCURACY, COMPLETENESS, OR USEFULNESS OF INFORMATION PRESENTED ON THE SITE IN CONNECTION WITH THESE TOOLS AND ANY CODE PROVIDED BY USERS IS NOT GUARANTEED BY US. THE OWNER/OPERATOR OF THE SITE DOES NOT HAVE CUSTODY OVER YOUR ASSETS OR ACCESS TO YOUR PRIVATE KEY AND CANNOT INITIATE A TRANSFER OF DIGITAL ASSETS OR OTHERWISE ACCESS YOUR DIGITAL ASSETS.
+
+THE SITE IS NOT A BROKER OR INTERMEDIARY AND IS IN NO WAY YOUR AGENT, ADVISOR, OR CUSTODIAN, AND WE DO NOT HAVE A FIDUCIARY RELATIONSHIP OR OBLIGATION TO YOU REGARDING ANY OTHER DECISIONS OR ACTIVITIES THAT YOU EFFECT WHEN USING YOUR WALLET TO INTERACT WITH ANY BLOCKCHAIN PROTOCOL.
+
+WE ARE NOT RESPONSIBLE FOR ANY ACTIVITIES THAT YOU ENGAGE IN WHEN USING BLOCKCHAIN TECHNOLOGIES, AND YOU SHOULD UNDERSTAND THE RISKS ASSOCIATED WITH PARTICIPATING IN DECENTRALIZED PROTOCOLS ON NEAR, ETHEREUM, OR OTHER BLOCKCHAINS.
+
+This Terms of Use Agreement (the "Terms" or "Agreement") explains the terms and conditions by which you may access and use the Site. Please read this Agreement carefully. This Agreement applies to you ("You") as a user of the Site. _ **This Agreement contains a mandatory individual arbitration and class action/jury trial waiver provision that requires the use of arbitration on an individual basis to resolve disputes, rather than jury trials or class actions.** _
+
+To use the Site, you must be able to form a legally binding contract online either as an individual or on behalf of a legal entity. To that end, you represent that, if you are agreeing to this Agreement on behalf of a legal entity, you have the legal authority to bind the company or other legal entity to this Agreement and you are at least 18 years old or the age of majority where you reside, whichever is older, can form a legally binding contract online, and have the full, right, power, and authority to enter into and to comply with the obligations under this Agreement.
+
+You are advised to check this Agreement periodically to familiarize yourself with any changes to the terms. We, in our sole discretion, reserve the right to make changes to our terms of use. Changes are binding on users of the Site and will take effect immediately upon posting. As a user, you agree to be bound by any changes, variations, or modifications to our terms of use and your continued use of the Site shall constitute acceptance of any such changes, variations, or modifications.
+
+By accessing or using the Site, you signify that you have read, understand, and agree to be bound by this Agreement in its entirety. _If you do not agree, you are not authorized to access or use the Site._
+
+`;
+
+return <Markdown text={content} />;
