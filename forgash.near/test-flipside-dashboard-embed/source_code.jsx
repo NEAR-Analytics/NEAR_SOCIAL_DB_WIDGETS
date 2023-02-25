de5c75fd1f9c698749bacff4ae748051037baaa8
forgxyz <forgxyz>
2023-02-24 23:32:14 -0500

    Update test-flipside-dashboard-embed by forgash.near at 2023-02-24 23:32:14.076

---
+++ forgash.near/test-flipside-dashboard-embed/source_code.jsx
@@ -0,0 +1,8 @@
+return (
+  <iframe
+    src="https://flipsidecrypto.xyz/embeds/dashboards/78833ac9-120f-4ea6-be4b-dc693e08865b"
+    width="1000"
+    height="1000"
+    title="`social.near` Contract Authorizations"
+  />
+);
