f23e07769f877c5ec663b8b1b4d662ff0ac7983f
NearSocial <NearSocial>
2023-02-25 19:22:14 -0500

    Update Builders by hack.near at 2023-02-25 19:22:14.912

--- hack.near/Builders/source_code.jsx
+++ hack.near/Builders/source_code.jsx
@@ -35,7 +35,6 @@ State.init({
 
 return (
   <div className="container">
-    <div className="row mb-3"></div>
     <Widget src="mob.near/widget/ProfileOnboarding" />
     <div className="row mb-3">
       <div className="mb-3"></div>
@@ -66,104 +65,9 @@ return (
           Data
         </a>
       </div>
-      <div className="mb-3"></div>
-      <div className="alert alert-primary">
-        <h2>Near Social Hackathon</h2>
-        <h3>
-          <b>February 8-24</b>
-        </h3>
-        <h5>
-          <b>Goal:</b> Onboarding Devs Quickly and Easily
-        </h5>
-        <p>
-          To get started, check out ideas on the{" "}
-          <a href="https://devgovgigs.near.social">Gigs Board</a>.
-        </p>
-        <div className="mb-3"></div>
-        <a
-          className="btn btn-outline-primary"
-          href="https://devgovgigs.near.social"
-        >
-          Explore Ideas
-        </a>
-        <a className="btn btn-outline-primary" href="https://t.me/NearSocial">
-          Group Chat
-        </a>
-        <div className="mb-3"></div>
-        <div>
-          <h3>Prize Pools</h3>
-          <ul>
-            <li>
-              Onboarding Solutions: <b>$5000</b>
-            </li>
-            <li>
-              Gaming Use Cases: <b>1000 NEAR</b>
-            </li>
-          </ul>
-        </div>
-        <div className="mb-3"></div>
-        <div>
-          <h4>Submit Your Project</h4>
-          <h5>Create a Summary Post</h5>
-          <p>
-            These project submissions will appear in the{" "}
-            <a href="https://near.social/#/mob.near/widget/MainPage.Content">
-              main page
-            </a>{" "}
-            content.
-          </p>
-          <p>Be sure to include any relevant links by using Markdown format:</p>
-          <p>
-            <i>[link text](URL)</i>
-          </p>
-          <p>
-            Remember to notify the jury by mentioning @hack.near in your post.
-            We are looking for any useful contributions and helpful
-            documentation.
-          </p>
-          <p>
-            Follow <a href="https://devs.near.social">@devs.near</a> to learn
-            more!
-          </p>
-        </div>
-        <div className="mb-3"></div>
-        <Widget
-          src="mob.near/widget/Common.Compose"
-          props={{
-            placeholder: "Hackathon Project Submission @hack.near",
-            onChange: state.onChange,
-            onHelper: ({ extractTagNotifications }) => {
-              State.update({ extractTagNotifications });
-            },
-            composeButton: (onCompose) => (
-              <CommitButton
-                disabled={!state.content}
-                force
-                className="btn btn-dark rounded-3"
-                data={composeData}
-                onCommit={() => {
-                  onCompose();
-                }}
-              >
-                Submit
-              </CommitButton>
-            ),
-          }}
-        />
-        {state.content && (
-          <div className="mt-3">
-            <Widget
-              src="mob.near/widget/MainPage.Post"
-              props={{
-                accountId: context.accountId,
-                content: state.content,
-                blockHeight: "now",
-              }}
-            />
-          </div>
-        )}
+      <div className="row mt-3">
+        <Widget src="manzanal.near/widget/CommonComponentsLibrary" />
       </div>
-      <div className="mb-3"></div>
     </div>
   </div>
 );
