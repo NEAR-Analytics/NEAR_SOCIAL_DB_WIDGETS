54f188eb683b33a4e96e29e7da4ada4260e0f694
NearSocial <NearSocial>
2023-02-26 16:04:23 -0500

    Update TransferProposal by hack.near at 2023-02-26 16:04:23.264

--- hack.near/TransferProposal/source_code.jsx
+++ hack.near/TransferProposal/source_code.jsx
@@ -15,7 +15,7 @@ const handleProposal = () => {
   const deposit = 100000000000000000000000;
   Near.call([
     {
-      contractName: "global.sputnik-dao.near",
+      contractName: "multi.sputnik-dao.near",
       methodName: "add_proposal",
       args: {
         proposal: {
