fceb830b2fa8f166e1a74f9d04f2e3a1d4a91284
NearSocial <NearSocial>
2023-02-26 17:07:52 -0500

    Update MultiDAO by hack.near at 2023-02-26 17:07:52.598

--- hack.near/MultiDAO/source_code.jsx
+++ hack.near/MultiDAO/source_code.jsx
@@ -7,6 +7,7 @@ if (!accountId) {
 return (
   <div>
     <div className="mb-3">
+      <h3>Membership</h3>
       <Widget src="hack.near/widget/AddMemberToRole" props={{ accountId }} />
       <Widget
         src="hack.near/widget/RemoveMemberFromRole"
@@ -14,12 +15,15 @@ return (
       />
     </div>
     <div className="mb-3">
+      <h3>Transfer NEAR</h3>
       <Widget src="hack.near/widget/TransferProposal" props={{ accountId }} />
     </div>
     <div className="mb-3">
+      <h3>Polling</h3>
       <Widget src="hack.near/widget/CreatePoll" props={{ accountId }} />
     </div>
     <div className="mb-3">
+      <h3>Function Calls</h3>
       <Widget
         src="hack.near/widget/FunctionCallProposal"
         props={{ accountId }}
