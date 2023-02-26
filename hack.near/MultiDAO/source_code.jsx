793f1aa02514b43a6f4b8cbc762772f97dc927f2
NearSocial <NearSocial>
2023-02-26 17:16:48 -0500

    Update MultiDAO by hack.near at 2023-02-26 17:16:48.605

--- hack.near/MultiDAO/source_code.jsx
+++ hack.near/MultiDAO/source_code.jsx
@@ -1,4 +1,5 @@
 const accountId = props.accountId ?? context.accountId;
+const daoId = props.daoId ?? "multi.sputnik-dao.near";
 
 if (!accountId) {
   return "Please connect your NEAR wallet :)";
@@ -8,25 +9,31 @@ return (
   <div>
     <div className="mb-3">
       <h3>Membership</h3>
-      <Widget src="hack.near/widget/AddMemberToRole" props={{ accountId }} />
+      <Widget
+        src="hack.near/widget/AddMemberToRole"
+        props={{ accountId, daoId }}
+      />
       <Widget
         src="hack.near/widget/RemoveMemberFromRole"
-        props={{ accountId }}
+        props={{ accountId, daoId }}
       />
     </div>
     <div className="mb-3">
       <h3>Transfer NEAR</h3>
-      <Widget src="hack.near/widget/TransferProposal" props={{ accountId }} />
+      <Widget
+        src="hack.near/widget/TransferProposal"
+        props={{ accountId, daoId }}
+      />
     </div>
     <div className="mb-3">
       <h3>Polling</h3>
-      <Widget src="hack.near/widget/CreatePoll" props={{ accountId }} />
+      <Widget src="hack.near/widget/CreatePoll" props={{ accountId, daoId }} />
     </div>
     <div className="mb-3">
       <h3>Function Calls</h3>
       <Widget
         src="hack.near/widget/FunctionCallProposal"
-        props={{ accountId }}
+        props={{ accountId, daoId }}
       />
     </div>
   </div>
