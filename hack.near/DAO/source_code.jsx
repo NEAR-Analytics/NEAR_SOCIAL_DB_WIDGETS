46a17896f2d127aaad88da36036d597c122b27ee
NearSocial <NearSocial>
2023-02-26 15:47:18 -0500

    Update DAO by hack.near at 2023-02-26 15:47:18.401

--- hack.near/DAO/source_code.jsx
+++ hack.near/DAO/source_code.jsx
@@ -7,19 +7,25 @@ if (!accountId) {
 return (
   <div>
     <div className="mb-3">
-      <Widget src="hack.near/widget/AddMemberToRole" props={{ accountId }} />
+      <Widget src="hack.near/widget/AddMemberProposal" props={{ accountId }} />
     </div>
     <div className="mb-3">
-      <Widget src="hack.near/widget/AddMemberToRole" props={{ accountId }} />
+      <Widget
+        src="hack.near/widget/RemoveMemberProposal"
+        props={{ accountId }}
+      />
     </div>
     <div className="mb-3">
-      <Widget src="hack.near/widget/AddMemberToRole" props={{ accountId }} />
+      <Widget src="hack.near/widget/TransferProposal" props={{ accountId }} />
     </div>
     <div className="mb-3">
-      <Widget src="hack.near/widget/AddMemberToRole" props={{ accountId }} />
+      <Widget src="hack.near/widget/CreatePoll" props={{ accountId }} />
     </div>
     <div className="mb-3">
-      <Widget src="hack.near/widget/AddMemberToRole" props={{ accountId }} />
+      <Widget
+        src="hack.near/widget/FunctionCallProposal"
+        props={{ accountId }}
+      />
     </div>
   </div>
 );
