b95f774884fcde393c3ff1fd2d13f4622680e69f
NearSocial <NearSocial>
2023-02-26 16:26:53 -0500

    Update CreatePoll by hack.near at 2023-02-26 16:26:53.219

--- hack.near/CreatePoll/source_code.jsx
+++ hack.near/CreatePoll/source_code.jsx
@@ -5,19 +5,22 @@ if (!accountId) {
 }
 
 State.init({
-  member_id: accountId,
+  description: state.description,
 });
 
 const handleProposal = () => {
+  if (!state.description) {
+    return;
+  }
   const gas = 200000000000000;
   const deposit = 100000000000000000000000;
   Near.call([
     {
-      contractName: "global.sputnik-dao.near",
+      contractName: "multi.sputnik-dao.near",
       methodName: "add_proposal",
       args: {
         proposal: {
-          description: "yes / no",
+          description: state.description,
           kind: "Vote",
         },
       },
@@ -27,9 +30,17 @@ const handleProposal = () => {
   ]);
 };
 
+const onChangeDescription = (description) => {
+  State.update({
+    description,
+  });
+};
+
 return (
   <div className="mb-3">
-    <button className="btn btn-success" onClick={handleProposal}>
+    Poll Question (yes / no):
+    <input type="text" onChange={(e) => onChangeDescription(e.target.value)} />
+    <button className="btn btn-success mt-3" onClick={handleProposal}>
       Create Poll
     </button>
   </div>
