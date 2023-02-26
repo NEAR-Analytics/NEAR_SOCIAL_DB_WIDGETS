edfaf81548abdf46afc00e7e5d4df88a2183f340
NearSocial <NearSocial>
2023-02-26 15:36:26 -0500

    Update CreatePoll by hack.near at 2023-02-26 15:36:26.648

---
+++ hack.near/CreatePoll/source_code.jsx
@@ -0,0 +1,36 @@
+const accountId = props.accountId ?? context.accountId;
+
+if (!accountId) {
+  return "Please connect your NEAR wallet :)";
+}
+
+State.init({
+  member_id: accountId,
+});
+
+const handleProposal = () => {
+  const gas = 200000000000000;
+  const deposit = 100000000000000000000000;
+  Near.call([
+    {
+      contractName: "global.sputnik-dao.near",
+      methodName: "add_proposal",
+      args: {
+        proposal: {
+          description: "yes / no",
+          kind: Vote,
+        },
+      },
+      gas: gas,
+      deposit: deposit,
+    },
+  ]);
+};
+
+return (
+  <div className="mb-3">
+    <button className="btn btn-success" onClick={handleProposal}>
+      Create Poll
+    </button>
+  </div>
+);
