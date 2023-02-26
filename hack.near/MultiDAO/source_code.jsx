7bfbd40a4426aec098259dd2c36cdbdf6c695257
NearSocial <NearSocial>
2023-02-26 16:57:56 -0500

    Update MultiDAO by hack.near at 2023-02-26 16:57:56.307

---
+++ hack.near/MultiDAO/source_code.jsx
@@ -0,0 +1,31 @@
+const accountId = props.accountId ?? context.accountId;
+
+if (!accountId) {
+  return "Please connect your NEAR wallet :)";
+}
+
+return (
+  <div>
+    <div className="mb-3">
+      <Widget src="hack.near/widget/AddMemberToRole" props={{ accountId }} />
+    </div>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/RemoveMemberFromRole"
+        props={{ accountId }}
+      />
+    </div>
+    <div className="mb-3">
+      <Widget src="hack.near/widget/TransferProposal" props={{ accountId }} />
+    </div>
+    <div className="mb-3">
+      <Widget src="hack.near/widget/CreatePoll" props={{ accountId }} />
+    </div>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/FunctionCallProposal"
+        props={{ accountId }}
+      />
+    </div>
+  </div>
+);
