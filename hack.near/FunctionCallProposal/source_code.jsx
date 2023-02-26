2055d885f1275cefd4742b5dc9fe7fcd3fc65c55
NearSocial <NearSocial>
2023-02-26 16:48:08 -0500

    Update FunctionCallProposal by hack.near at 2023-02-26 16:48:08.471

--- hack.near/FunctionCallProposal/source_code.jsx
+++ hack.near/FunctionCallProposal/source_code.jsx
@@ -5,10 +5,15 @@ if (!accountId) {
 }
 
 State.init({
-  member_id: accountId,
+  receiver_id: state.receiver_id,
+  method_name: state.method_name,
+  args: state.args,
 });
 
 const handleProposal = () => {
+  if (!(state.receiver_id && state.method_name && state.args)) {
+    return;
+  }
   const gas = 200000000000000;
   const deposit = 100000000000000000000000;
   Near.call([
@@ -17,25 +22,62 @@ const handleProposal = () => {
       methodName: "add_proposal",
       args: {
         proposal: {
-          description: "potential member",
+          description: "custom function call",
           kind: {
-            AddMemberToRole: {
-              member_id: accountId,
-              role: "community",
+            FunctionCall: {
+              receiver_id: state.receiver_id,
+              actions: [
+                {
+                  method_name: state.method_name,
+                  args: state.args,
+                  deposit: "0",
+                  gas: "150000000000000",
+                },
+              ],
             },
           },
         },
       },
-      gas: gas,
       deposit: deposit,
+      gas: gas,
     },
   ]);
 };
 
+const onChangeContract = (receiver_id) => {
+  State.update({
+    receiver_id,
+  });
+};
+
+const onChangeMethod = (method_name) => {
+  State.update({
+    method_name,
+  });
+};
+
+const onChangeArgs = (args) => {
+  State.update({
+    args,
+  });
+};
+
 return (
   <div className="mb-3">
-    <button className="btn btn-success" onClick={handleProposal}>
-      Propose Action
-    </button>
+    <div className="mb-3">
+      Contract:
+      <input type="text" onChange={(e) => onChangeContract(e.target.value)} />
+    </div>
+    <div className="mb-3">
+      Method:
+      <input type="text" onChange={(e) => onChangeTitle(e.target.value)} />
+    </div>
+    <div className="mb-3">
+      Arguments (JSON):
+      <input type="json" onChange={(e) => onChangeArgs(e.target.value)} />
+      <button className="btn btn-success mt-3" onClick={handleProposal}>
+        Propose Action
+      </button>
+    </div>
   </div>
 );
