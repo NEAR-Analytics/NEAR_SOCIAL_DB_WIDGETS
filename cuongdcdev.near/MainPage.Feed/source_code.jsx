5c8893ea102833f66f744d746a24be2de2a9855d
cuongdcdev <cuongdcdev>
2023-02-27 16:53:02 -0500

    Update MainPage.Feed by cuongdcdev.near at 2023-02-27 16:53:02.381

---
+++ cuongdcdev.near/MainPage.Feed/source_code.jsx
@@ -0,0 +1,31 @@
+const index = {
+  action: "post",
+  key: "main",
+  options: {
+    limit: 10,
+    order: "desc",
+    accountId: ["cuongdcdev.near"],
+  },
+};
+
+let blockedarr = props.blockedListArr ? props.blockedListArr : [];
+
+const renderItem = (a) =>
+  a.value.type === "md" && (
+    <div key={JSON.stringify(a)} className="mb-3">
+      <Widget
+        src="cuongdcdev.near/widget/MainPage.PostPlus"
+        props={{
+          accountId: a.accountId,
+          blockHeight: a.blockHeight,
+          blockedListArr: blockedarr,
+        }}
+      />
+    </div>
+  );
+
+return (
+  <div>
+    <Widget src="mob.near/widget/IndexFeed" props={{ index, renderItem }} />
+  </div>
+);
