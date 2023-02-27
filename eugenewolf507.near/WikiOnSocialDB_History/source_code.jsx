b5356b497a98e27bc6fdfd9805a4caa83e63a393
eugenewolf507.near <eugenewolf507.near>
2023-02-27 09:50:47 -0500

    Update WikiOnSocialDB_History by eugenewolf507.near at 2023-02-27 09:50:47.949

--- eugenewolf507.near/WikiOnSocialDB_History/source_code.jsx
+++ eugenewolf507.near/WikiOnSocialDB_History/source_code.jsx
@@ -2,71 +2,14 @@
 ---props---
 props.widgetPath?: string,
 */
-const authorForWidget = "eugenewolf507.near";
-const addressForArticles = "wikiTest2Article";
 const initWidgetPath = props.widgetPath || "devgovgigs.near/widget/Ideas";
 
 State.init({
   widgetPath: initWidgetPath,
 });
 
-// === START ===
-const handler = () => {
-  // ============ WIDGETS
-  const widgetPath = "devgovgigs.near/widget/Ideas";
-
-  const historyBlocksRequestKeysWidgets = Social.keys(
-    `${widgetPath}`,
-    "final",
-    {
-      return_type: "History",
-    }
-  );
-  //   const historyBlocksRequestGetWidget = Social.get(`${widgetPath}`, "final");
-
-  console.log(
-    "historyBlocksRequestKeysWidgets",
-    historyBlocksRequestKeysWidgets
-  );
-  //   console.log("historyBlocksRequestGetWidget", historyBlocksRequestGetWidget);
-
-  // ============ ARTICLES
-  // ========== GET INDEX ARRAY FOR ARTICLES ==========
-  const postsIndex = Social.index(addressForArticles, "main", {
-    order: "desc",
-    accountId: undefined,
-  });
-  // ========== GET ALL ARTICLES ==========
-  const resultArticles =
-    postsIndex &&
-    postsIndex.reduce((acc, { accountId, blockHeight }) => {
-      const postData = Social.get(
-        `${accountId}/${addressForArticles}/main`,
-        blockHeight
-      );
-      const postDataWithBlockHeight = { ...JSON.parse(postData), blockHeight };
-      return [...acc, postDataWithBlockHeight];
-    }, []);
-  // ========== FIND ALL VERSIONS OF ONE ARTICLE ==========
-  const filteredArticles =
-    resultArticles.length &&
-    resultArticles.reduce((acc, article) => {
-      if (article.articleId === "FirstNewDBTest") {
-        return [...acc, article];
-      } else {
-        return acc;
-      }
-    }, []);
-  console.log("filteredArticles", filteredArticles);
-};
-
-// === END ===
-
 return (
   <div>
-    {/* === START === */}
-    <button onClick={handler}>GET DATA (delete thos button)</button>
-    {/* === END === */}
     <h1 class="text-center">Widget History</h1>
 
     <div class="input-group mb-3">
