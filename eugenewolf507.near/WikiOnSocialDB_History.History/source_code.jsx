f7c3580b8655ddc3790e5c75750d9b2999b1ea15
eugenewolf507.near <eugenewolf507.near>
2023-02-27 19:51:56 -0500

    Update WikiOnSocialDB_History.History by eugenewolf507.near at 2023-02-27 19:51:56.328

--- eugenewolf507.near/WikiOnSocialDB_History.History/source_code.jsx
+++ eugenewolf507.near/WikiOnSocialDB_History.History/source_code.jsx
@@ -15,20 +15,6 @@ State.init({
   selectedBlockHeight: null,
 });
 
-// --- OLD START
-// const historyBlocksRequest = Social.keys(`${props.widgetPath}`, "final", {
-//   return_type: "History",
-// });
-
-// if (historyBlocksRequest === null) return "loading...";
-
-const [widgetAccountId, _, widgetName] = props.widgetPath.split("/");
-
-// let blocksChanges =
-//   historyBlocksRequest[widgetAccountId]?.["widget"]?.[widgetName];
-// -- OLD END
-
-// === START ===
 // ========== GET INDEX ARRAY FOR ARTICLES ==========
 const postsIndex = Social.index(addressForArticles, "main", {
   order: "desc",
@@ -58,14 +44,17 @@ const filteredArticles =
 console.log("filteredArticles", filteredArticles);
 if (filteredArticles === null) return "loading...";
 
-let blocksChanges = filteredArticles.map((item) => item.blockHeight);
+let blocksChanges = filteredArticles.map((item) => ({
+  blockHeight: item.blockHeight,
+  lastEditor: item.lastEditor,
+}));
 console.log("blocksChanges", blocksChanges);
 
 // === END ===
 
 if (props.count) props.count(blocksChanges.length);
 
-if (blocksChanges) blocksChanges = blocksChanges?.sort((a, b) => b - a);
+// if (blocksChanges) blocksChanges = blocksChanges?.sort((a, b) => b - a);
 
 if (!state.selectedBlockHeight) state.selectedBlockHeight = blocksChanges[0];
 
@@ -93,7 +82,7 @@ const renderBlockChangesLink = (blockHeight) => {
 };
 
 function blockHeightToWidgetCode(blockHeight) {
-  const index = blocksChanges.findIndex((el) => el == blockHeight);
+  const index = blocksChanges.findIndex((el) => el.blockHeight == blockHeight);
   return (
     <Widget
       style={{ minHeight: "200px" }}
@@ -102,14 +91,14 @@ function blockHeightToWidgetCode(blockHeight) {
       props={{
         pathToWidget: props.widgetPath,
         currentBlockHeight: blockHeight,
-        prevBlockHeight: blocksChanges[index + 1],
+        prevBlockHeight: blocksChanges[index + 1].blockHeight,
       }}
     />
   );
 }
 
 function blockHeightToWidgetRender(blockHeight) {
-  const index = blocksChanges.findIndex((el) => el == blockHeight);
+  const index = blocksChanges.findIndex((el) => el.blockHeight == blockHeight);
   return (
     <Widget
       style={{ minHeight: "200px" }}
@@ -118,7 +107,7 @@ function blockHeightToWidgetRender(blockHeight) {
       props={{
         pathToWidget: props.widgetPath,
         currentBlockHeight: blockHeight,
-        prevBlockHeight: blocksChanges[index + 1],
+        prevBlockHeight: blocksChanges[index + 1].blockHeight,
       }}
     />
   );
@@ -171,18 +160,15 @@ return (
       <div>
         <div div class="card mb-3">
           <h3 class="card-header">{blocksChanges.length} Commits</h3>
-
           <div class="list-group">
             {blocksChanges
               .slice(0, 5)
-              .map((height) => renderBlockChangesLink(height))}
-
+              .map((height) => renderBlockChangesLink(height.blockHeight))}
             <div class="collapse" id="collapseExample">
               {blocksChanges
                 .slice(5)
-                .map((height) => renderBlockChangesLink(height))}
+                .map((height) => renderBlockChangesLink(height.blockHeight))}
             </div>
-
             {blocksChanges.length > 5 && (
               <button
                 class="list-group-item active"
@@ -225,11 +211,15 @@ return (
         </Tabs>
 
         {state.selectedTab == "code" && (
-          <div>{blockHeightToWidgetCode(state.selectedBlockHeight)}</div>
+          <div>
+            {blockHeightToWidgetCode(state.selectedBlockHeight.blockHeight)}
+          </div>
         )}
 
         {state.selectedTab == "render" && (
-          <div>{blockHeightToWidgetRender(state.selectedBlockHeight)}</div>
+          <div>
+            {blockHeightToWidgetRender(state.selectedBlockHeight.blockHeight)}
+          </div>
         )}
       </div>
     )}
