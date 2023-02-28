d6bb4f8bfec88a0fbcdcef72d50e5e0f41ce816b
evgenykuzyakov <evgenykuzyakov>
2023-02-28 01:13:36 -0500

    Update MainPage.Compose by mob.near at 2023-02-28 01:13:36.933

--- mob.near/MainPage.Compose/source_code.jsx
+++ mob.near/MainPage.Compose/source_code.jsx
@@ -17,14 +17,30 @@ const composeData = () => {
     },
   };
 
-  const notifications = state.extractTagNotifications(state.content.text, {
+  const item = {
     type: "social",
     path: `${context.accountId}/post/main`,
-  });
+  };
+
+  const notifications = state.extractMentionNotifications(
+    state.content.text,
+    item
+  );
 
   if (notifications.length) {
     data.index.notify = JSON.stringify(
       notifications.length > 1 ? notifications : notifications[0]
+    );
+  }
+
+  const hashtags = state.extractHashtags(state.content.text);
+
+  if (hashtags.length) {
+    data.index.hashtag = JSON.stringify(
+      hashtags.map((hashtag) => ({
+        key: hashtag,
+        value: item,
+      }))
     );
   }
 
@@ -44,8 +60,8 @@ return (
       props={{
         placeholder: "What's happening?",
         onChange: state.onChange,
-        onHelper: ({ extractTagNotifications }) => {
-          State.update({ extractTagNotifications });
+        onHelper: ({ extractMentionNotifications, extractHashtags }) => {
+          State.update({ extractMentionNotifications, extractHashtags });
         },
         composeButton: (onCompose) => (
           <CommitButton
