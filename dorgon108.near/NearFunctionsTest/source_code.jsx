73b2d654379470db25404b9495d931b198908c36
dorgon108.near <dorgon108.near>
2023-02-28 02:40:53 -0500

    Update NearFunctionsTest by dorgon108.near at 2023-02-28 02:40:53.391

--- dorgon108.near/NearFunctionsTest/source_code.jsx
+++ dorgon108.near/NearFunctionsTest/source_code.jsx
@@ -1,13 +1,26 @@
+State.init({
+  value: "value to update",
+});
+
+console.log(Near.view("nearsocialexamples.near", "get_greeting"));
+
 const testCall = () => {
-  return Near.call(
-    "nearsocialexamples.near",
-    "set_greeting",
-    `{"message":"Hi Near Social"}`
-  );
+  return Near.call("nearsocialexamples.near", "set_greeting", {
+    message: "Hi Near Social",
+  });
+};
+
+const testView = () => {
+  State.update({
+    value: Near.view("nearsocialexamples.near", "get_greeting"),
+  });
 };
 
 return (
   <div>
-    <button onClick={testCall}>test</button>
+    <button onClick={testCall}>test call</button>
+    <button onClick={testView}>test view</button>
+
+    <div>{state.value}</div>
   </div>
 );
