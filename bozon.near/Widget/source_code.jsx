9a1b2c7ef4781771b5f03046419939092a567eb1
ligebit <ligebit>
2023-02-28 11:10:09 -0500

    Update Widget by bozon.near at 2023-02-28 11:10:09.646

--- bozon.near/Widget/source_code.jsx
+++ bozon.near/Widget/source_code.jsx
@@ -1,10 +1,11 @@
 State.init({
-  widgetPath: "",
+  widgetPath: "bozon.near/widget/WidgetHistory.CodeHistory",
 });
 
 return (
   <div>
     <input
+      value={state.widgetPath}
       onChange={(e) => State.update({ widgetPath: e.target.value })}
       class="form-control mb-3"
     ></input>
