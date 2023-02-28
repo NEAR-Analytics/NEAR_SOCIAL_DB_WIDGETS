42cc11f1e80a52bdd9573df203d6e7e07b35dda4
calebjacob <calebjacob>
2023-02-27 22:29:39 -0500

    Update Homepage by calebjacob.near at 2023-02-27 22:29:39.606

--- calebjacob.near/HomePage/source_code.jsx
+++ calebjacob.near/HomePage/source_code.jsx
@@ -1,11 +1,1 @@
-const accountId = props.accountId ?? context.accountId;
-
-const homepage = accountId
-  ? Social.get(`${accountId}/settings/near.social/homepage`)
-  : undefined;
-
-if (homepage === null) {
-  return "Loading";
-}
-
-return <Widget src={homepage ?? "mob.near/widget/Welcome"} />;
+return "deprecated";
