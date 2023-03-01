468e77b77beb0b3b874854a2bf21c19c186e2161
NearSocial <NearSocial>
2023-03-01 06:11:10 -0500

    Update GroupMembers by hack.near at 2023-03-01 06:11:10.926

--- hack.near/GroupMembers/source_code.jsx
+++ hack.near/GroupMembers/source_code.jsx
@@ -1,8 +1,27 @@
 const accountId = props.accountId ?? context.accountId;
-const daoId = props.daoId ?? "multi.sputnik-dao.near";
+const daoId = props.daoId ?? "global.sputnik-dao.near";
 
-const group = props.groupId ?? "council";
+const group = props.groupId ?? "community";
 const policy = Near.view(daoId, "get_policy");
+
+const data = Social.keys("*/profile", "final");
+
+if (!data) {
+  return "Loading...";
+}
+
+const accounts = Object.entries(data);
+const allMembers = [];
+
+for (let i = 0; i < accounts.length; ++i) {
+  const memberId = accounts[i][0];
+
+  allMembers.push(
+    <div className="mb-2">
+      <Widget src="mob.near/widget/Profile" props={{ memberId }} />
+    </div>
+  );
+}
 
 const members = policy.roles
   .filter((role) => role.name === group)
@@ -12,8 +31,16 @@ const members = policy.roles
     return members;
   });
 
+const memberId = policy.roles
+  .filter((role) => role.name === group)
+  .map((role) => {
+    const memberId = role.kind.Group;
+
+    return memberId;
+  });
+
 return (
-  <div className="m-2">
+  <div>
     <h3>Groups:</h3>
     <h4>{group}</h4>
     <Widget
@@ -23,5 +50,25 @@ return (
         accountId: members,
       }}
     />
+    <div className="mb-2">
+      {members.map(({ accountId }, i) => (
+        <div
+          key={i}
+          className="d-flex justify-content-between align-items-center mb-3"
+        >
+          <div className="mt-2 text-truncate">
+            <a
+              href={`#/mob.near/widget/ProfilePage?accountId=${accountId}`}
+              className="text-decoration-none link-dark text-truncate"
+            >
+              <Widget
+                src="mob.near/widget/Profile.InlineBlock"
+                props={{ accountId }}
+              />
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 );
