7c1d1d983a3a03019ddea34a8e90407146373ee4
NearSocial <NearSocial>
2023-03-01 04:29:14 -0500

    Update GroupCheck by hack.near at 2023-03-01 04:29:14.692

--- hack.near/GroupCheck/source_code.jsx
+++ hack.near/GroupCheck/source_code.jsx
@@ -1,7 +1,12 @@
-const policy = Near.view("multi.sputnik-dao.near", "get_policy");
-console.log(policy.roles);
+const accountId = props.accountId ?? context.accountId;
+const daoId = props.daoId ?? "multi.sputnik-dao.near";
 
-const accountId = context.accountId;
+if (!accountId) {
+  return "Please connect your NEAR wallet :)";
+}
+
+const policy = Near.view(daoId, "get_policy");
+console.log(policy.roles);
 
 const groups = policy.roles
   .filter((role) => role.name === "council")
@@ -17,4 +22,69 @@ const check = groups.map((group) => {
     : group.filter((address) => address === accountId).length > 0;
 });
 
-return <> {JSON.stringify(check)}</>;
+State.init({
+  receiver_id: accountId,
+  amount: state.amount,
+});
+
+const handleProposal = () => {
+  if (!(state.receiver_id && state.amount)) {
+    return;
+  }
+  const gas = 200000000000000;
+  const deposit = 100000000000000000000000;
+  Near.call([
+    {
+      contractName: daoId,
+      methodName: "add_proposal",
+      args: {
+        proposal: {
+          description: "transfer proposal",
+          kind: {
+            Transfer: {
+              token_id: "",
+              receiver_id: state.receiver_id,
+              amount: state.amount,
+            },
+          },
+        },
+      },
+      gas: gas,
+      deposit: deposit,
+    },
+  ]);
+};
+
+const onChangeRecipient = (receiver_id) => {
+  State.update({
+    receiver_id,
+  });
+};
+
+const onChangeAmount = (amount) => {
+  State.update({
+    amount,
+  });
+};
+
+return (
+  <div className="mb-3">
+    {check && (
+      <div className="mb-2">
+        Recipient:
+        <input
+          type="text"
+          onChange={(e) => onChangeRecipient(e.target.value)}
+        />
+      </div>
+    )}
+    <div className="mb-3">
+      Amount:
+      <input type="number" onChange={(e) => onChangeAmount(e.target.value)} />
+    </div>
+
+    <button className="btn btn-success" onClick={handleProposal}>
+      Propose Transfer
+    </button>
+  </div>
+);
