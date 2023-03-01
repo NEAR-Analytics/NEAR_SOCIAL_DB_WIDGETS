528b97be91d02bb092e0bcb511833024ecff0563
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-01 13:43:44 -0500

    Update HelloNear by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-01 13:43:44.948

---
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/HelloNear/source_code.jsx
@@ -0,0 +1,38 @@
+const contract = "hello.near-examples.near";
+const greeting = Near.view(contract, "get_greeting", {});
+
+State.init({ greeting: greeting });
+
+const onInputChange = ({ target }) => {
+  State.update({ greeting: target.value });
+};
+
+const onBtnClick = () => {
+  Near.call(contract, "set_greeting", {
+    greeting: state.greeting,
+  });
+};
+
+return (
+  <>
+    <div class="container border border-info p-3">
+      <h3 class="text-center">
+        The contract says:
+        <span class="text-decoration-underline">{state.greeting} </span>
+      </h3>
+
+      <p class="text-center py-2">
+        Look at that! A Hello World app! With its greeting stored on the NEAR
+        blockchain.{" "}
+      </p>
+
+      <div class="border border-black p-3">
+        <label>Update greeting</label>
+        <input placeholder="Howdy" onChange={onInputChange} />
+        <button class="btn btn-primary mt-2" onClick={onBtnClick}>
+          Save
+        </button>
+      </div>
+    </div>
+  </>
+);
