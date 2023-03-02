5eec5399668c87964079905879807206831b257d
ligebit <ligebit>
2023-03-01 19:56:15 -0500

    Update Widget.CreatePullRequest by bozon.near at 2023-03-01 19:56:15.014

--- bozon.near/Widget.CreatePullRequest/source_code.jsx
+++ bozon.near/Widget.CreatePullRequest/source_code.jsx
@@ -26,69 +26,86 @@ function getDatastringFromBlockHeight(blockHeight) {
 
 return (
   <div>
+    <div class="d-flex flex-row align-items-center mb-3">
+      <div class="col"></div>
+      <h4 class="col">Create Pull Request</h4>
+      <div class="col d-flex justify-content-end">
+        <button onClick={() => props.onClose()}>X</button>
+      </div>
+    </div>
+
+    <div className="col-lg-12  mb-2">
+      Widget path:
+      <input
+        class="form-control mb-3"
+        placeholder="path to widget"
+        defaultValue={state.widgetPath || initWidgetPath}
+        onBlur={(e) => {
+          State.update({
+            widgetPath: e.target.value,
+          });
+        }}
+      />
+      Code:
+      <textarea
+        type="text"
+        rows={6}
+        class="form-control"
+        placeholder="code"
+        onBlur={(e) => {
+          State.update({
+            code: e.target.value,
+          });
+        }}
+      />
+      Memo:
+      <textarea
+        type="text"
+        rows={2}
+        class="form-control"
+        placeholder="code"
+        onBlur={(e) => {
+          State.update({
+            memo: e.target.value,
+          });
+        }}
+      />
+    </div>
+
+    <button
+      onClick={onClick}
+      class={`btn btn-primary`}
+      disabled={widgetCode === undefined}
+    >
+      {widgetCode ? "Submit" : "Widget not found"}
+    </button>
+
+    <hr />
+
     <div className="card">
       <div className="card-header d-flex align-items-center justify-content-between">
-        Create Pull Request<button onClick={() => props.onClose()}>X</button>
+        <div>
+          <Widget
+            src="mob.near/widget/ProfileLine"
+            props={{ accountId: context.accountId }}
+          />
+        </div>
+        <div>{getDatastringFromBlockHeight()}</div>
       </div>
 
       <div class="card-body">
         <div className="row">
           <div className="col-lg-12  mb-2">
-            Widget path:
-            <input
-              class="form-control mb-3"
-              placeholder="path to widget"
-              defaultValue={state.widgetPath || initWidgetPath}
-              onBlur={(e) => {
-                State.update({
-                  widgetPath: e.target.value,
-                });
-              }}
-            />
-            Code:
-            <textarea
-              type="text"
-              rows={6}
-              class="form-control"
-              placeholder="code"
-              onBlur={(e) => {
-                State.update({
-                  code: e.target.value,
-                });
-              }}
-            />
-            Memo:
-            <textarea
-              type="text"
-              rows={2}
-              class="form-control"
-              placeholder="code"
-              onBlur={(e) => {
-                State.update({
-                  memo,
-                });
+            {state.memo}
+            <Widget
+              src={`bozon.near/widget/CodeDiff`}
+              props={{
+                currentCode: state.code,
+                prevCode: widgetCode,
               }}
             />
           </div>
         </div>
-
-        <button
-          onClick={onClick}
-          class={`btn btn-primary`}
-          disabled={widgetCode === undefined}
-        >
-          {widgetCode ? "Submit" : "Widget not found"}
-        </button>
-      </div>
-      <div class="card-footer">
-        Preview:
-        <Widget
-          src={`bozon.near/widget/CodeDiff`}
-          props={{
-            currentCode: state.code,
-            prevCode: widgetCode,
-          }}
-        />
       </div>
     </div>
   </div>
