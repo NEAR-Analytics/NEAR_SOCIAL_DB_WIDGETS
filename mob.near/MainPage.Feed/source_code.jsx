a5baa92562279673cc88c72622290e68dfc24d40
evgenykuzyakov <evgenykuzyakov>
2023-03-01 22:06:36 -0500

    Update MainPage.Feed by mob.near at 2023-03-01 22:06:36.372

--- mob.near/MainPage.Feed/source_code.jsx
+++ mob.near/MainPage.Feed/source_code.jsx
@@ -20,6 +20,9 @@ const renderItem = (a) =>
 
 return (
   <div>
-    <Widget src="mob.near/widget/IndexFeed" props={{ index, renderItem }} />
+    <Widget
+      src="mob.near/widget/FilteredIndexFeed"
+      props={{ index, renderItem }}
+    />
   </div>
 );
