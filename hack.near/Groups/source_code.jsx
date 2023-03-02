3b4bce900476a830f1db3def47666ab896a4658c
NearSocial <NearSocial>
2023-03-02 10:13:53 -0500

    Update Groups by hack.near at 2023-03-02 10:13:53.690

--- hack.near/Groups/source_code.jsx
+++ hack.near/Groups/source_code.jsx
@@ -3,27 +3,6 @@ const daoId = props.daoId ?? "multi.sputnik-dao.near";
 const groupId = props.groupId ?? "council";
 const policy = Near.view(daoId, "get_policy");
 
-const allMembers = [];
-const allGroups = [];
-
-for (let i = 0; i < accounts.length; ++i) {
-  const memberId = accounts[i][0];
-
-  allMembers.push(
-    <div className="mb-2">
-      <Widget src="mob.near/widget/Profile" props={{ memberId }} />
-    </div>
-  );
-}
-
-const members = policy.roles
-  .filter((role) => role.name === groupId)
-  .map((role) => {
-    const members = role.kind.Group;
-
-    return members;
-  });
-
 const groups = policy.roles
   .filter((role) => role.name !== "all")
   .map((role) => {
