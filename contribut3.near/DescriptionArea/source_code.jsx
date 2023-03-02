6ec080ef39394d5fbb94bd4fa8e6c55cd0861d45
contribut3.near <contribut3.near>
2023-03-02 10:20:55 -0500

    Update DescriptionArea by contribut3.near at 2023-03-02 10:20:55.132

--- contribut3.near/DescriptionArea/source_code.jsx
+++ contribut3.near/DescriptionArea/source_code.jsx
@@ -31,12 +31,3 @@ return (
     )}
   </div>
 );
-//     {/* {description.length > lengthCutoff */ }
-// {/*   ? description.substring(0, lengthCutoff - 10) */ }
-// {/*   : description} */ }
-// {/* {description.length > lengthCutoff ? ( */ }
-// {/*   <b className="text-primary">Read more</b> */ }
-// {/* ) : ( */ }
-// {/*   <></> */ }
-// {/* )} */ }
-// );
