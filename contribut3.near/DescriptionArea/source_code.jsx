044f14cfed4f74d38a3e7a9e76ae4a6317230bbe
contribut3.near <contribut3.near>
2023-03-02 10:05:27 -0500

    Update DescriptionArea by contribut3.near at 2023-03-02 10:05:27.754

--- contribut3.near/DescriptionArea/source_code.jsx
+++ contribut3.near/DescriptionArea/source_code.jsx
@@ -2,18 +2,17 @@ const description = props.description || "";
 const lengthCutoff = 90;
 
 return (
-  <div
+  <Markdown
     className="text-truncate my-2 w-100"
-  // style={{ textOverflow: "ellipsis" }}
-  >
-    {description}
-    {/* {description.length > lengthCutoff */}
-    {/*   ? description.substring(0, lengthCutoff - 10) */}
-    {/*   : description} */}
-    {/* {description.length > lengthCutoff ? ( */}
-    {/*   <b className="text-primary">Read more</b> */}
-    {/* ) : ( */}
-    {/*   <></> */}
-    {/* )} */}
-  </div>
+    // style={{ textOverflow: "ellipsis" }}
+    props={{ text: description }}
+  />
+    {/* {description.length > lengthCutoff */ }
+{/*   ? description.substring(0, lengthCutoff - 10) */ }
+{/*   : description} */ }
+{/* {description.length > lengthCutoff ? ( */ }
+{/*   <b className="text-primary">Read more</b> */ }
+{/* ) : ( */ }
+{/*   <></> */ }
+{/* )} */ }
 );
