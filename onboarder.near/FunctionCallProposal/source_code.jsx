d1f1ed88f7cff9c0af945458a9be20e7f9dad9a4
onboarder.near <onboarder.near>
2023-03-02 01:36:07 -0500

    Update FunctionCallProposal by onboarder.near at 2023-03-02 01:36:07.828

---
+++ onboarder.near/FunctionCallProposal/source_code.jsx
@@ -0,0 +1,84 @@
+const accountId = props.accountId ?? context.accountId;
+const daoId = props.daoId ?? "onboarddao.sputnik-dao.near";
+
+if (!accountId) {
+  return "Please connect your NEAR wallet :)";
+}
+
+State.init({
+  receiver_id: state.receiver_id,
+  method_name: state.method_name,
+  args: state.args,
+});
+
+const handleProposal = () => {
+  if (!(state.receiver_id && state.method_name && state.args)) {
+    return;
+  }
+  const gas = 200000000000000;
+  const deposit = 100000000000000000000000;
+  Near.call([
+    {
+      contractName: daoId,
+      methodName: "add_proposal",
+      args: {
+        proposal: {
+          description: "custom function call",
+          kind: {
+            FunctionCall: {
+              receiver_id: state.receiver_id,
+              actions: [
+                {
+                  method_name: state.method_name,
+                  args: state.args,
+                  deposit: "1",
+                  gas: "150000000000000",
+                },
+              ],
+            },
+          },
+        },
+      },
+      deposit: deposit,
+      gas: gas,
+    },
+  ]);
+};
+
+const onChangeContract = (receiver_id) => {
+  State.update({
+    receiver_id,
+  });
+};
+
+const onChangeMethod = (method_name) => {
+  State.update({
+    method_name,
+  });
+};
+
+const onChangeArgs = (args) => {
+  State.update({
+    args,
+  });
+};
+
+return (
+  <div className="mb-3">
+    <div className="mb-3">
+      DAO Contract:
+      <input type="text" onChange={(e) => onChangeContract(e.target.value)} />
+    </div>
+    <div className="mb-3">
+      Method:
+      <input type="text" onChange={(e) => onChangeMethod(e.target.value)} />
+    </div>
+    <div className="mb-3">
+      Arguments (JSON):
+      <input type="json" onChange={(e) => onChangeArgs(e.target.value)} />
+      <button className="btn btn-outline-danger mt-3" onClick={handleProposal}>
+        Propose Action
+      </button>
+    </div>
+  </div>
+);
