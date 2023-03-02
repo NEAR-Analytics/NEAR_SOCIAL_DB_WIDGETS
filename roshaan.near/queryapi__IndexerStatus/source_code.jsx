3de07938f820872d5ca8db86d7e5f9edb6a362e3
roshaan.near <roshaan.near>
2023-03-02 15:09:00 -0500

    Update queryapi__IndexerStatus by roshaan.near at 2023-03-02 15:09:00.098

--- roshaan.near/queryapi__IndexerStatus/source_code.jsx
+++ roshaan.near/queryapi__IndexerStatus/source_code.jsx
@@ -42,9 +42,9 @@ const Subheading = styled.h2`
 const Card = styled.div`
   border-radius: 12px;
   background: #fff;
-  border: ${(div) => (div.selected ? "1px solid black" : "1px solid #eceef0")};
   box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
     0px 1px 2px rgba(16, 24, 40, 0.06);
+  height: 80vh;
 `;
 
 const CardBody = styled.div`
@@ -53,9 +53,6 @@ const CardBody = styled.div`
   gap: 16px;
   align-items: center;
   flex-direction: column;
-  > * {
-    min-width: 0;
-  }
 `;
 
 const CardFooter = styled.div`
@@ -89,7 +86,7 @@ if (!indexer_name) return "missing indexer_name";
 const state_table = "| Function Name | Current Block Height |\n| --- | --- |\n";
 
 const logs_table =
-  "| Block Height | Function Name | Id | Message | Timestamp |\n| --- | --- | --- | --- | --- |\n";
+  "| Block Height | Function Name | Message | Timestamp |\n| --- | --- | --- | --- |\n";
 const indexer_values_table =
   "| Function Name | Key Name | Value |\n| --- | --- | --- |\n";
 
@@ -104,23 +101,21 @@ State.init({
   logsOffset: 0,
   stateOffset: 0,
 });
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-  try {
-    const result = await asyncFetch(
-      "https://query-api-hasura-vcqilefdcq-uc.a.run.app/v1/graphql",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          query: operationsDoc,
-          variables: variables,
-          operationName: operationName,
-        }),
-      }
-    );
-  } catch (e) {
-    console.log("error:", e);
-  }
-  return result.body.data;
+function fetchGraphQL(operationsDoc, operationName, variables) {
+  return asyncFetch(
+    "https://query-api-hasura-vcqilefdcq-uc.a.run.app/v1/graphql",
+    {
+      method: "POST",
+      headers: {
+        "x-hasura-admin-secret": "FgG6BmTJqgbVMRnL63zv",
+      },
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName,
+      }),
+    }
+  );
 }
 
 const createGraphQLLink = () => {
@@ -176,42 +171,55 @@ const indexerStateDoc = `
   }
 `;
 
-// fetchGraphQL(IndexerStorageDoc, "IndexerStorage", {
-//   offset: state.indexer_resOffset * LIMIT,
-// }).then((result) => {
-//   console.log("result", result);
-//   if (result.status === 200) {
-//     State.update({
-//       indexer_res: result.body.data.indexer_storage,
-//       indexer_resCount:
-//         result.body.data.indexer_storage_aggregate.aggregate.count,
-//     });
-//   }
-// });
+fetchGraphQL(IndexerStorageDoc, "IndexerStorage", {
+  offset: state.indexer_resOffset * LIMIT,
+}).then((result) => {
+  console.log("result", result);
+  if (result.status === 200) {
+    try {
+      State.update({
+        indexer_res: result.body.data.indexer_storage,
+        indexer_resCount:
+          result.body.data.indexer_storage_aggregate.aggregate.count,
+      });
+    } catch (e) {
+      console.log("error", e);
+    }
+  }
+});
 
-// fetchGraphQL(logsDoc, "QueryLogs", {
-//   offset: state.logsOffset * LIMIT,
-// }).then((result) => {
-//   console.log("resultquerylogs", result);
-//   if (result.status === 200) {
-//     State.update({
-//       logs: result.body.data.indexer_log_entries,
-//       logsCount: result.body.data.indexer_log_entries_aggregate.aggregate.count,
-//     });
-//   }
-// });
+fetchGraphQL(logsDoc, "QueryLogs", {
+  offset: state.logsOffset * LIMIT,
+}).then((result) => {
+  console.log("resultquerylogs", result);
+  if (result.status === 200) {
+    try {
+      State.update({
+        logs: result.body.data.indexer_log_entries,
+        logsCount:
+          result.body.data.indexer_log_entries_aggregate.aggregate.count,
+      });
+    } catch (e) {
+      console.log("error", e);
+    }
+  }
+});
 
-// fetchGraphQL(indexerStateDoc, "IndexerState", {
-//   offset: state.stateOffset * LIMIT,
-// }).then((result) => {
-//   console.log("indexerstatelogs", result);
-//   if (result.status === 200) {
-//     State.update({
-//       state: result.body.data.indexer_state,
-//       stateCount: result.body.data.indexer_state_aggregate.aggregate.count,
-//     });
-//   }
-// });
+fetchGraphQL(indexerStateDoc, "IndexerState", {
+  offset: state.stateOffset * LIMIT,
+}).then((result) => {
+  console.log("indexerstatelogs", result);
+  if (result.status === 200) {
+    try {
+      State.update({
+        state: result.body.data.indexer_state,
+        stateCount: result.body.data.indexer_state_aggregate.aggregate.count,
+      });
+    } catch (e) {
+      console.log("error", e);
+    }
+  }
+});
 
 const create_table = () => {
   console.log(state.indexer_res, "RESSS");
@@ -219,7 +227,7 @@ const create_table = () => {
     indexer_values_table += `| ${row.function_name} | ${row.key_name} | ${row.value} |\n`;
   });
   state.logs.forEach((row) => {
-    logs_table += `| ${row.block_height} | ${row.function_name} | ${row.id} | ${row.message} | ${row.timestamp} |\n`;
+    logs_table += `| ${row.block_height} | ${row.function_name} | ${row.message} | ${row.timestamp} |\n`;
   });
   state.state.forEach((row) => {
     state_table += `| ${row.function_name} | ${row.current_block_height} |\n`;
@@ -232,9 +240,13 @@ const onLogsPageChange = (page) => {
     fetchGraphQL(logsDoc, "QueryLogs", { offset: page * LIMIT }).then(
       (result) => {
         if (result.status === 200) {
-          State.update({
-            logs: result.body.data.indexer_log_entries,
-          });
+          try {
+            State.update({
+              logs: result.body.data.indexer_log_entries,
+            });
+          } catch (e) {
+            console.log("error", e);
+          }
         }
       }
     );
@@ -250,17 +262,21 @@ const onIndexerResPageChange = (page) => {
       offset: page * LIMIT,
     }).then((result) => {
       if (result.status === 200) {
-        State.update({
-          indexer_res: result.body.data.indexer_storage,
-        });
+        try {
+          State.update({
+            indexer_res: result.body.data.indexer_storage,
+          });
+        } catch (e) {
+          console.log("error", e);
+        }
       }
     });
   } catch (e) {
     console.log("error:", e);
   }
+
+  console.log("page clicked indexer res", page);
 };
-onLogsPageChange(0);
-onIndexerResPageChange(0);
 create_table();
 return (
   <>
