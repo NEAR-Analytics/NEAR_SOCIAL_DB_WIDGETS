aa9988ec7f8c5fae10ab73c009877ccffc02af34
contribut3.near <contribut3.near>
2023-03-02 09:54:37 -0500

    Update DescriptionArea by contribut3.near at 2023-03-02 09:54:37.056

--- contribut3.near/DescriptionArea/source_code.jsx
+++ contribut3.near/DescriptionArea/source_code.jsx
@@ -2,7 +2,10 @@ const description = props.description || "";
 const lengthCutoff = 90;
 
 return (
-  <div className="text-truncate my-2 w-75" style={{ textOverflow: "ellipsis" }}>
+  <div
+    className="text-truncate my-2 w-100"
+    style={{ textOverflow: "ellipsis" }}
+  >
     {description}
     {/* {description.length > lengthCutoff */}
     {/*   ? description.substring(0, lengthCutoff - 10) */}
