693eab7572e0c982b2e67b467a6a4149ef355870
contribut3.near <contribut3.near>
2023-03-02 10:12:50 -0500

    Update DescriptionArea by contribut3.near at 2023-03-02 10:12:50.534

--- contribut3.near/DescriptionArea/source_code.jsx
+++ contribut3.near/DescriptionArea/source_code.jsx
@@ -2,11 +2,13 @@ const description = props.description || "";
 const lengthCutoff = 90;
 
 return (
-  <Markdown
-    // className="text-truncate my-2 w-100"
-    // style={{ textOverflow: "ellipsis" }}
-    text={description}
-  />
+  <div className="d-flex flex-row justify-content-start align-items-center">
+    <Markdown
+      // className="text-truncate my-2 w-100"
+      // style={{ textOverflow: "ellipsis" }}
+      text={description}
+    />
+  </div>
 );
 //     {/* {description.length > lengthCutoff */ }
 // {/*   ? description.substring(0, lengthCutoff - 10) */ }
