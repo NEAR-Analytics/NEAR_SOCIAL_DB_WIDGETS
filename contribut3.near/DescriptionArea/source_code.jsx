6d4f32b9bd7e588e4141c946a73503b80995bda6
contribut3.near <contribut3.near>
2023-03-02 09:39:28 -0500

    Update DescriptionArea by contribut3.near at 2023-03-02 09:39:28.373

--- contribut3.near/DescriptionArea/source_code.jsx
+++ contribut3.near/DescriptionArea/source_code.jsx
@@ -4,7 +4,7 @@ const lengthCutoff = 90;
 return (
   <div
     className="text-truncate my-2 w-100"
-    style={{ textOverflow: "Read more" }}
+    style={{ textOverflow: 'string("Read more")' }}
   >
     {description}
     {/* {description.length > lengthCutoff */}
