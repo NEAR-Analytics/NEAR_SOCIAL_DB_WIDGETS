1c4bbac178c33a8c666f58f77b6f3afb015ed1b8
NearSocial <NearSocial>
2023-03-02 10:27:00 -0500

    Update GroupMembers by hack.near at 2023-03-02 10:27:00.534

--- hack.near/GroupMembers/source_code.jsx
+++ hack.near/GroupMembers/source_code.jsx
@@ -1,27 +1,8 @@
 const accountId = props.accountId ?? context.accountId;
-const daoId = props.daoId ?? "global.sputnik-dao.near";
+const daoId = props.daoId ?? "multi.sputnik-dao.near";
 
 const groupId = props.groupId ?? "community";
 const policy = Near.view(daoId, "get_policy");
-
-const data = Social.keys("*/profile", "final");
-
-if (!data) {
-  return "Loading...";
-}
-
-const accounts = Object.entries(data);
-const allMembers = [];
-
-for (let i = 0; i < accounts.length; ++i) {
-  const memberId = accounts[i][0];
-
-  allMembers.push(
-    <div className="mb-2">
-      <Widget src="mob.near/widget/Profile" props={{ memberId }} />
-    </div>
-  );
-}
 
 const members = policy.roles
   .filter((role) => role.name === groupId)
@@ -34,13 +15,22 @@ const members = policy.roles
 return (
   <>
     <div>
-      <h3>Group Members:</h3>
+      <h3>{groupId}</h3>
+      <h5>members:</h5>
       <div>
-        {members.map((memberId, i) => (
-          <h4>{memberId}</h4>
+        {members.map((member, i) => (
+          <a
+            key={i}
+            className="text-decoration-none"
+            href={`#mob.near/widget/ProfilePage?accountId=${memberId}`}
+          >
+            <h4>{member}</h4>
+          </a>
         ))}
       </div>
-      <div>{allMembers}</div>
+      <a href="https://near.social/#/hack.near/widget/Groups">
+        <button className="btn btn-primary mt-2">See All Groups</button>
+      </a>
     </div>
   </>
 );
