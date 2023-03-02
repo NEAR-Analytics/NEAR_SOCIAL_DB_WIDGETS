9ae25c287e3e0fe997adf1934f4496f6f9f59a5a
contribut3.near <contribut3.near>
2023-03-02 10:06:30 -0500

    Update DescriptionArea by contribut3.near at 2023-03-02 10:06:30.161

--- contribut3.near/DescriptionArea/source_code.jsx
+++ contribut3.near/DescriptionArea/source_code.jsx
@@ -3,10 +3,11 @@ const lengthCutoff = 90;
 
 return (
   <Markdown
-    className="text-truncate my-2 w-100"
+    // className="text-truncate my-2 w-100"
     // style={{ textOverflow: "ellipsis" }}
     props={{ text: description }}
-  />;
+  />
+);
 //     {/* {description.length > lengthCutoff */ }
 // {/*   ? description.substring(0, lengthCutoff - 10) */ }
 // {/*   : description} */ }
