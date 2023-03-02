1200ff1ef5987955401a22a485ff04d119109a97
contribut3.near <contribut3.near>
2023-03-02 09:34:05 -0500

    Update Tags by contribut3.near at 2023-03-02 09:34:05.376

--- contribut3.near/Tags/source_code.jsx
+++ contribut3.near/Tags/source_code.jsx
@@ -1,7 +1,7 @@
 const tags = Object.keys(props.tags || {});
 
 return (
-  <div className="text-truncate text-muted" style={{ maxWidth: "200px" }}>
+  <div className="text-truncate text-muted" style={{ maxWidth: "300px" }}>
     {tags.length > 0 ? (
       <>
         {tags.map((tag) => (
