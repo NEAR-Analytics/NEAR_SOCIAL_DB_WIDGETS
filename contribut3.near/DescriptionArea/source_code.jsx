5f50466288f73bb35cbb2d6b537823ac749ce040
contribut3.near <contribut3.near>
2023-03-02 10:14:27 -0500

    Update DescriptionArea by contribut3.near at 2023-03-02 10:14:27.570

--- contribut3.near/DescriptionArea/source_code.jsx
+++ contribut3.near/DescriptionArea/source_code.jsx
@@ -1,13 +1,24 @@
 const description = props.description || "";
-const lengthCutoff = 90;
+const lengthCutoff = 100;
+
+State.init({
+  showAll: description <= lengthCutoff,
+});
 
 return (
   <div className="d-flex flex-row justify-content-start align-items-center">
     <Markdown
       // className="text-truncate my-2 w-100"
       // style={{ textOverflow: "ellipsis" }}
-      text={description}
+      text={
+        state.showAll ? description : description.substring(0, lengthCutoff)
+      }
     />
+    {state.showAll ? (
+      <></>
+    ) : (
+      <a onClick={() => State.update({ showAll: true })}>Read more</a>
+    )}
   </div>
 );
 //     {/* {description.length > lengthCutoff */ }
