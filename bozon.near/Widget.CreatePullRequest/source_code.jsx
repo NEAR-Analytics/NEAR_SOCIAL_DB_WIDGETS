437278951b809d918a6aa6b390563ec5c379fd29
ligebit <ligebit>
2023-03-03 15:39:06 -0500

    Update Widget.CreatePullRequest by bozon.near at 2023-03-03 15:39:06.818

--- bozon.near/Widget.CreatePullRequest/source_code.jsx
+++ bozon.near/Widget.CreatePullRequest/source_code.jsx
@@ -1,28 +1,62 @@
 /*
 
-onClose(): function
+widgetPath: string,
+onClose(): function,
 
 */
 
-const initWidgetPath = "devgovgigs.near/widget/Ideas";
-
-State.init({
-  widgetPath: initWidgetPath,
-  code: "",
-  memo: "",
-});
-
-const widgetCode = Social.get(state.widgetPath);
+const widgetCode = Social.get(props.widgetPath);
 
 if (widgetCode === null) return "loading...";
 
-const [widgetAccountId, _, widgetName] = state.widgetPath.split("/");
+console.log(Storage.get("props"));
+
+State.init({
+  code: widgetCode,
+  memo: "",
+  tab: "code",
+  props: Storage.get("props") || {},
+});
+
+const [widgetAccountId, _, widgetName] = props.widgetPath.split("/");
 
 function getDatastringFromBlockHeight(blockHeight) {
   const block = Near.block(blockHeight);
   const date = new Date(block.header.timestamp_nanosec / 1e6);
   return date.toDateString() + " " + date.toLocaleTimeString();
 }
+
+const Tabs = styled.div`
+  display: flex;
+  padding: 0 12px;
+  height: 48px;
+  border-bottom: 1px solid #ECEEF0;
+`;
+
+const TabsButton = styled.button`
+  font-weight: 400;
+  font-size: 14px;
+  padding: 0 12px;
+  position: relative;
+  color: ${(p) => (p.selected ? "#11181C" : "#687076")};
+  background: none;
+  border: none;
+
+  &:hover {
+    color: #11181C;
+  }
+
+  &::after {
+    content: '';
+    display: ${(p) => (p.selected ? "block" : "none")};
+    position: absolute;
+    bottom: 0;
+    left: 12px;
+    right: 12px;
+    height: 3px;
+    background: #0091FF;
+  }
+`;
 
 return (
   <div>
@@ -35,26 +69,16 @@ return (
     </div>
 
     <div className="col-lg-12  mb-2">
-      Widget path:
-      <input
-        class="form-control mb-3"
-        placeholder="path to widget"
-        defaultValue={state.widgetPath || initWidgetPath}
-        onBlur={(e) => {
-          State.update({
-            widgetPath: e.target.value,
-          });
-        }}
-      />
       Code:
       <textarea
         type="text"
         rows={6}
         class="form-control"
         placeholder="code"
+        defaultValue={widgetCode}
         onBlur={(e) => {
           State.update({
-            code: e.target.value,
+            widgetCode: e.target.value,
           });
         }}
       />
@@ -63,7 +87,7 @@ return (
         type="text"
         rows={2}
         class="form-control"
-        placeholder="code"
+        placeholder="memo"
         onBlur={(e) => {
           State.update({
             memo: e.target.value,
@@ -82,29 +106,79 @@ return (
 
     <hr />
 
-    <div className="card">
-      <div className="card-header d-flex align-items-center justify-content-between">
-        <div>
+    <div>
+      <div className="border rounded-4 p-3 pb-1">
+        <div className="mb-3 d-flex flex-row justify-content-between">
+          <div className="flex-grow-1 text-truncate">
+            <a
+              className="text-dark text-decoration-none text-truncate"
+              href={`#/mob.near/widget/ProfilePage?accountId=${accountId}`}
+            >
+              <Widget
+                src="mob.near/widget/Profile.ShortInlineBlock"
+                props={{ accountId: context.accountId, tooltip: true }}
+              />
+            </a>
+          </div>
+          <div className="text-muted">{getDatastringFromBlockHeight()}</div>
+        </div>
+
+        <div className="mb-3">{state.memo}</div>
+
+        <Tabs>
+          <TabsButton
+            onClick={() => State.update({ tab: "code" })}
+            selected={state.tab === "code"}
+          >
+            Code
+          </TabsButton>
+          <TabsButton
+            onClick={() => State.update({ tab: "render" })}
+            selected={state.tab === "render"}
+          >
+            Render
+          </TabsButton>
+        </Tabs>
+
+        <div
+          style={{
+            display: state.tab == "code" ? "block" : "none",
+          }}
+        >
           <Widget
-            src="mob.near/widget/ProfileLine"
-            props={{ accountId: context.accountId }}
+            src={`bozon.near/widget/CodeDiff`}
+            props={{
+              currentCode: state.code,
+              prevCode: widgetCode,
+            }}
           />
         </div>
-        <div>{getDatastringFromBlockHeight()}</div>
-      </div>
 
-      <div class="card-body">
-        <div className="row">
-          <div className="col-lg-12  mb-2">
-            {state.memo}
-            <Widget
-              src={`bozon.near/widget/CodeDiff`}
-              props={{
-                currentCode: state.code,
-                prevCode: widgetCode,
-              }}
-            />
-          </div>
+        <div
+          style={{
+            display: state.tab == "render" ? "block" : "none",
+          }}
+          className="p-3"
+        >
+          props:
+          <textarea
+            type="text"
+            rows={2}
+            class="form-control"
+            defaultValue={JSON.stringify(state.props)}
+            onBlur={(e) => {
+              try {
+                const provProps = JSON.parse(e.target.value);
+
+                Storage.set("props", provProps);
+
+                State.update({
+                  props: provProps,
+                });
+              } catch {}
+            }}
+          />
+          <Widget code={state.code} props={state.props} />
         </div>
       </div>
     </div>
