6c5a797c5b99112cdba9f9ce4a9c95b18915f1af
https://github.com/Zahidul-Islam <https://github.com/Zahidul-Islam>
2023-03-03 23:49:16 -0500

    Update Profile by zahidulislam.near at 2023-03-03 23:49:16.535

--- zahidulislam.near/Profile/source_code.jsx
+++ zahidulislam.near/Profile/source_code.jsx
@@ -1,1 +1,34 @@
-return "hello";
+const profile = props.profile;
+const cssFont = fetch(
+  "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+).body;
+
+const css = fetch(
+  "https://raw.githubusercontent.com/cryptosynk/near-social-profile/main/css/mainLight.css"
+).body;
+
+const theme = "light";
+
+const Theme = styled.div`
+  font-family: "Open Sans", sans-serif;
+  ${cssFont}
+  ${css}
+`;
+
+return (
+  <Theme>
+    <div className="container">
+      <div className="content">
+        <Widget
+          src="storyboard.testnet/widget/Profile.LeftSection"
+          props={{ accountId, profile, theme }}
+        />
+
+        <Widget
+          src="storyboard.testnet/widget/Profile.RightSection"
+          props={{ accountId, profile, theme }}
+        />
+      </div>
+    </div>
+  </Theme>
+);
