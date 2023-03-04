59dbdc93c1d6f4a5e19a4aba596568fe8a731f2c
markeljan <markeljan>
2023-03-04 04:11:03 -0500

    Update MarkdownDiff by markeljan.near at 2023-03-04 04:11:03.438

--- markeljan.near/MarkdownDiff/source_code.jsx
+++ markeljan.near/MarkdownDiff/source_code.jsx
@@ -292,7 +292,7 @@ const linePropsMobile = (lineNumber) => {
     display: "block",
     width: "auto",
     background: "#fff",
-    marginRight: `-${parseInt(longestLineLength * 0.39)}em`,
+    marginRight: `-${parseInt(longestLineLength * 0.41)}em`,
     marginLeft: "-1em",
     marginTop: conditionalTopMargin,
     marginBottom: conditionalBottomMargin,
