2e2df59184b57385813e99344a291e5e5092c3c6
thalburner.near <thalburner.near>
2023-03-03 23:29:06 -0500

    Update dev-gigs.components.Hottest by thalburner.near at 2023-03-03 23:29:06.332

---
+++ thalburner.near/dev-gigs.components.Hottest/source_code.jsx
@@ -0,0 +1,69 @@
+// This component implementation was forked from [IndexFeed], but it does not fully implement lazy loading.
+// While this component uses InfiniteScroll, it still loads the whole list of Post IDs in one view call.
+// The contract will need to be extended with pagination support, yet, even in the current state the page loads much faster.
+// [IndexFeed]: https://near.social/#/mob.near/widget/WidgetSource?src=mob.near/widget/IndexFeed
+
+/* INCLUDE: "common.jsx" */
+const nearDevGovGigsContractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "thalburner.near").split("/", 1)[0];
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
+  (context.widgetSrc ?? "thalburner.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
+    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
+  };
+  return (
+    <Widget
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+  if (props.nearDevGovGigsContractAccountId) {
+    linkProps.nearDevGovGigsContractAccountId =
+      props.nearDevGovGigsContractAccountId;
+  }
+  if (props.nearDevGovGigsWidgetsAccountId) {
+    linkProps.nearDevGovGigsWidgetsAccountId =
+      props.nearDevGovGigsWidgetsAccountId;
+  }
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+  const linkPropsQuery = Object.entries(linkProps)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+  return `#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+/* END_INCLUDE: "common.jsx" */
+
+const posts = Near.view("devgovgigs.near", "get_posts");
+
+const post_id_likes = [];
+
+posts.forEach((post) => {
+  post_id_likes.push({ id: post.id, likes: post.likes.length });
+});
+
+post_id_likes.sort((a, b) => b.likes - a.likes);
+
+const post_ids_sorted = post_id_likes.map((post) => post.id);
+console.log(post_ids_sorted);
+return (
+  <div>
+    <Widget src="thalburner.near/widget/dev-gigs.components.Post" />;
+    <Widget src="thalburner.near/widget/dev-gigs.components.Post" />;
+  </div>
+);
