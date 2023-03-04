0813fa4d82a11e59772f2dd91223cac2937dd713
samoykute.near <samoykute.near>
2023-03-04 04:29:07 -0500

    Update HelloPoodle by samoykute.near at 2023-03-04 04:29:07.491

--- samoykute.near/HelloPoodle/source_code.jsx
+++ samoykute.near/HelloPoodle/source_code.jsx
@@ -6,15 +6,10 @@ if (profile === null) {
   return "Loading...";
 }
 
-const name = profile.name || "";
-const tags = Object.keys(profile.tags ?? {});
-
 return (
   <>
     <div class="container border border-info p-3 text-center">
-      <h1>
-        Hello Poodle {name}, {tags}
-      </h1>
+      <h1>Hello Poodle {profile.name}</h1>
       <h1>Hello Poodle {props.name2}</h1>
 
       <p> {greeting} </p>
