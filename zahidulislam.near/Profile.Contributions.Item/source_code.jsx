f19198de1f0123931bc91c3a6e638e2b9c6ea87a
https://github.com/Zahidul-Islam <https://github.com/Zahidul-Islam>
2023-03-03 23:57:53 -0500

    Update Profile.Contributions.Item by zahidulislam.near at 2023-03-03 23:57:53.312

---
+++ zahidulislam.near/Profile.Contributions.Item/source_code.jsx
@@ -0,0 +1,63 @@
+State.init({
+  show: false,
+});
+
+const handleOnMouseEnter = () => {
+  State.update({ show: true });
+};
+const handleOnMouseLeave = () => {
+  State.update({ show: false });
+};
+
+const overlay = (
+  <div
+    style={{
+      maxWidth: "24em",
+      zIndex: 1070,
+
+      width: 150,
+      backgroundColor: "rgba(0, 0, 0, 0.8)",
+      color: "#c3cbd4",
+      textAlign: "left",
+      borderRadius: 4,
+      padding: "5px 10px",
+
+      fontSize: "0.9rem",
+    }}
+    onMouseEnter={handleOnMouseEnter}
+    onMouseLeave={handleOnMouseLeave}
+  >
+    {!item.empty && (
+      <span>
+        <span>Date:</span> {props.date}
+        <br />
+        <span>Value:</span> {props.item.value}
+      </span>
+    )}
+  </div>
+);
+
+return (
+  <OverlayTrigger
+    show={state.show}
+    trigger={["hover", "focus"]}
+    delay={{ show: 250, hide: 300 }}
+    placement="auto"
+    overlay={overlay}
+  >
+    <div
+      className="contributionItem"
+      style={{
+        borderRadius: 2,
+        backgroundColor: props.item?.empty
+          ? "rgba(256,256,256,.05)"
+          : props.color,
+        aspectRatio: 1 / 1,
+        maxWidth: 20,
+        maxHeight: 20,
+      }}
+      onMouseEnter={handleOnMouseEnter}
+      onMouseLeave={handleOnMouseLeave}
+    />
+  </OverlayTrigger>
+);
