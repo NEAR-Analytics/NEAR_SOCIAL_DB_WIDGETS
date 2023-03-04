7fbbf8b2280c4054a13c00fea4df042d9c8c74a2
samoykute.near <samoykute.near>
2023-03-04 04:24:42 -0500

    Update HelloPoodle by samoykute.near at 2023-03-04 04:24:42.631

--- samoykute.near/HelloPoodle/source_code.jsx
+++ samoykute.near/HelloPoodle/source_code.jsx
@@ -1,9 +1,20 @@
 let greeting = "Have a great day";
 
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+if (profile === null) {
+  return "Loading...";
+}
+
+const name = profile.name || "";
+const tags = Object.keys(profile.tags ?? {});
+
 return (
   <>
     <div class="container border border-info p-3 text-center">
-      <h1>Hello Poodle {props.username}</h1>
+      <h1>
+        Hello Poodle {name}, {tags}
+      </h1>
       <h1>Hello Poodle {props.name2}</h1>
 
       <p> {greeting} </p>
