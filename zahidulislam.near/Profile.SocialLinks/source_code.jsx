20c59ee382536c3f9668249acdbcd3324dbb4400
https://github.com/Zahidul-Islam <https://github.com/Zahidul-Islam>
2023-03-04 00:06:27 -0500

    Update Profile.SocialLinks by zahidulislam.near at 2023-03-04 00:06:27.730

--- zahidulislam.near/Profile.SocialLinks/source_code.jsx
+++ zahidulislam.near/Profile.SocialLinks/source_code.jsx
@@ -6,61 +6,62 @@ const linktree = profile.linktree;
 
 return (
   <div className="socialLinks">
-    {Object.entries(linktree)?.map(([name, value], index) => (
-      <>
-        {(name === "twitter" && (
-          <a
-            className="socialIcon"
-            href={`https://twitter.com/${value}`}
-            target="_blank"
-          >
-            <img
-              className="invertColor"
-              style={{ height: 20, opacity: 0.5 }}
-              src="https://cdn-icons-png.flaticon.com/512/733/733635.png"
-              alt="twitter"
-            />
-          </a>
-        )) ||
-          (name === "github" && (
+    {linktree &&
+      Object.entries(linktree)?.map(([name, value], index) => (
+        <>
+          {(name === "twitter" && (
             <a
               className="socialIcon"
-              href={`https://github.com/${value}`}
+              href={`https://twitter.com/${value}`}
               target="_blank"
             >
               <img
                 className="invertColor"
                 style={{ height: 20, opacity: 0.5 }}
-                src="https://cdn-icons-png.flaticon.com/512/2111/2111425.png"
-                alt="github"
+                src="https://cdn-icons-png.flaticon.com/512/733/733635.png"
+                alt="twitter"
               />
             </a>
           )) ||
-          (name === "telegram" && (
-            <a
-              className="socialIcon"
-              href={`https://telegram.me/${value}`}
-              target="_blank"
-            >
-              <img
-                className="invertColor"
-                style={{ height: 20, opacity: 0.5 }}
-                src="https://cdn-icons-png.flaticon.com/512/2111/2111708.png"
-                alt="telegram"
-              />
-            </a>
-          )) ||
-          (name === "website" && (
-            <a className="socialIcon" href={value} target="_blank">
-              <img
-                className="invertColor"
-                style={{ height: 20, opacity: 0.5 }}
-                src="https://cdn-icons-png.flaticon.com/512/1006/1006771.png"
-                alt="website"
-              />
-            </a>
-          ))}
-      </>
-    ))}
+            (name === "github" && (
+              <a
+                className="socialIcon"
+                href={`https://github.com/${value}`}
+                target="_blank"
+              >
+                <img
+                  className="invertColor"
+                  style={{ height: 20, opacity: 0.5 }}
+                  src="https://cdn-icons-png.flaticon.com/512/2111/2111425.png"
+                  alt="github"
+                />
+              </a>
+            )) ||
+            (name === "telegram" && (
+              <a
+                className="socialIcon"
+                href={`https://telegram.me/${value}`}
+                target="_blank"
+              >
+                <img
+                  className="invertColor"
+                  style={{ height: 20, opacity: 0.5 }}
+                  src="https://cdn-icons-png.flaticon.com/512/2111/2111708.png"
+                  alt="telegram"
+                />
+              </a>
+            )) ||
+            (name === "website" && (
+              <a className="socialIcon" href={value} target="_blank">
+                <img
+                  className="invertColor"
+                  style={{ height: 20, opacity: 0.5 }}
+                  src="https://cdn-icons-png.flaticon.com/512/1006/1006771.png"
+                  alt="website"
+                />
+              </a>
+            ))}
+        </>
+      ))}
   </div>
 );
