9f8e52632267bcfa36aaac0f946f5570cfd3f3f8
samoykute.near <samoykute.near>
2023-03-04 04:04:03 -0500

    Update HelloPoodle by samoykute.near at 2023-03-04 04:04:03.107

---
+++ samoykute.near/HelloPoodle/source_code.jsx
@@ -0,0 +1,11 @@
+let greeting = "Have a great day";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <h1>Hello Poodle {props.name}</h1>
+
+      <p> {greeting} </p>
+    </div>
+  </>
+);
