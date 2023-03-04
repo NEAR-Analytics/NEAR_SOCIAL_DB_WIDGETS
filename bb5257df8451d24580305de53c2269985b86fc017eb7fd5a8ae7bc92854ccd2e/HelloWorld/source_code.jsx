3e12ae13bcdce4f47171ce9a3fcafd260a06cdc2
bb5257df8451d24580305de53c2269985b86fc017eb7fd5a8ae7bc92854ccd2e <bb5257df8451d24580305de53c2269985b86fc017eb7fd5a8ae7bc92854ccd2e>
2023-03-04 18:57:21 -0500

    Update HelloWorld by bb5257df8451d24580305de53c2269985b86fc017eb7fd5a8ae7bc92854ccd2e at 2023-03-04 18:57:21.524

--- bb5257df8451d24580305de53c2269985b86fc017eb7fd5a8ae7bc92854ccd2e/HelloWorld/source_code.jsx
+++ bb5257df8451d24580305de53c2269985b86fc017eb7fd5a8ae7bc92854ccd2e/HelloWorld/source_code.jsx
@@ -1,15 +1,1 @@
-State.init({ accountId: "" });
-
-return (
-  <input
-    type="text"
-    className="form-control"
-    value={state.accountId}
-    onChange={(e) => {
-      const accountId = e.target.value
-        .toLowerCase()
-        .replace(/[^a-z0-9_.-]/g, "");
-      State.update({ accountId });
-    }}
-  />
-);
+return "BB";
