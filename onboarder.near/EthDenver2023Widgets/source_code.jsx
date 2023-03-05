78f20a0320ba615b6ecda681b68dc42b0b052176
onboarder.near <onboarder.near>
2023-03-05 13:11:22 -0500

    Update EthDenver2023Widgets by onboarder.near at 2023-03-05 13:11:22.568

---
+++ onboarder.near/EthDenver2023Widgets/source_code.jsx
@@ -0,0 +1,78 @@
+return (
+  <div>
+    <h1>👋 Howdy, our team Hacked on alot of different Widgets</h1>
+    <h2>👥 NEAR widgets</h2>
+    <ul>
+      <li>
+        <a
+          href="https://near.social/#/onboarder.near/widget/DAOSocialSearch"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DAO NEAR Social Search
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://near.social/#/onboarder.near/widget/AddMemberToRole"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DAO Add Member to Proposal
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://near.social/#/onboarder.near/widget/DonateNEARtoAddress"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Donate NEAR to Address
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://near.social/#/hack.near/widget/DAOs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          All DAOs
+        </a>
+      </li>
+    </ul>
+    <h2>🌈 EVM widgets</h2>
+    <ul>
+      <li>
+        <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+          Polygon Token Sender
+        </a>
+      </li>
+      <li>
+        <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+          Polygon Token List
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://near.social/#/onboarder.near/widget/Lido-Testnet"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lido on Testnet
+        </a>
+      </li>
+    </ul>
+    <h2>🚀 Improvements to the Blockchain Operating System</h2>
+    <ul>
+      <li>
+        <a
+          href="https://near.social/#/onboarder.near/widget/BOSDirectory"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          BOS Directory
+        </a>
+      </li>
+    </ul>
+  </div>
+);
