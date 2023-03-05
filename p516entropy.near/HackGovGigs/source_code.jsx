d79cfcd0dbcb050e1dae1d4694710e04d2f78bd0
p516entropy.near <p516entropy.near>
2023-03-05 02:08:48 -0500

    Update HackGovGigs by p516entropy.near at 2023-03-05 02:08:48.120

--- p516entropy.near/HackGovGigs/source_code.jsx
+++ p516entropy.near/HackGovGigs/source_code.jsx
@@ -1,9 +1,9 @@
 return (
   <div class="row">
-    <div class="col-12  col-sm-7">
+    <div class="col-12 col-sm-7">
       <Widget src={`p516entropy.near/widget/PostsFuzzySearch`} />
     </div>
-    <div class="col-12  col-sm-5">
+    <div class="col-12 col-sm-5">
       <Widget src={`p516entropy.near/widget/HottestDevPosts`} />
     </div>
   </div>
