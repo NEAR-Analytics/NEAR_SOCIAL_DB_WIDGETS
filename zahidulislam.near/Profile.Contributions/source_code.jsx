874fd9c99905d926cf2cb0075c14340f056925a5
https://github.com/Zahidul-Islam <https://github.com/Zahidul-Islam>
2023-03-05 09:07:06 -0500

    Update Profile.Contributions by zahidulislam.near at 2023-03-05 09:07:06.543

--- zahidulislam.near/Profile.Contributions/source_code.jsx
+++ zahidulislam.near/Profile.Contributions/source_code.jsx
@@ -1,3 +1,42 @@
+const accountId = props.accountId || context.accountId;
+
+if (!accountId) return "Login or send accountId in the props";
+
+const profile = Social.getr(`${accountId}/profile`);
+
+const allWidgetsHistoryChangesBlocks = Social.keys(
+  `${accountId}/widget/*`,
+  "final",
+  {
+    return_type: "History",
+  }
+);
+
+if (allWidgetsHistoryChangesBlocks === null) return "Loading...";
+
+const widget = allWidgetsHistoryChangesBlocks[accountId].widget;
+
+const totalCommits = Object.keys(widget)
+  .map((key) => widget[key])
+  .flat()
+  .map((blockHeight) => {
+    const xs = Near.block(blockHeight);
+
+    const timeMs = parseFloat(xs.header.timestamp_nanosec) / 1e6;
+
+    return new Date(timeMs).toLocaleDateString();
+  });
+
+const checkOccurrence = (array, element) => {
+  let counter = 0;
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] == element) {
+      counter++;
+    }
+  }
+  return counter;
+};
+
 const generate = () => {
   // Create an empty array to hold the objects
   const dataArray = [] || [{ empty: true }];
@@ -7,15 +46,11 @@ const generate = () => {
     // Create a new date object for the current day
     const date = new Date(2023, 0, i + 1);
 
-    // Generate a random number between 0-6 for the value
-    const value = Math.floor(Math.random() * 7);
+    const ys = checkOccurrence(totalCommits, date.toLocaleDateString());
 
     // Create a new object with the date and value and add it to the array
-    dataArray.push({ date: date.toISOString(), value });
+    dataArray.push({ date: date.toLocaleDateString(), ys });
   }
-
-  // Print the array to the console
-  // console.log(dataArray);
 
   return dataArray;
 };
