153970f412815393af43123f0cb19c2e20500c39
antonyip <antonyip>
2023-03-05 03:00:44 -0500

    Update Flipside-API-Getter by 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422 at 2023-03-05 03:00:44.687

--- 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422/Flipside-API-Getter/source_code.jsx
+++ 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422/Flipside-API-Getter/source_code.jsx
@@ -2,28 +2,30 @@ State.init({
   results: "Awaiting Query...",
 });
 
-function sendQueryToBackend() {
-  const options = {
-    method: "POST",
-    body: `{ "query": "${props.query}" }`,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+const options = {
+  method: "POST",
+  body: `{ "query": "${props.query}" }`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
-  State.update({
-    results: "Query Triggered...",
-  });
+State.update({
+  results: "Query Triggered...",
+});
 
-  const res = asyncFetch(
-    "https://flipside-api.antonyip.com/getCachedQuery",
-    options
-  ).then((res) => {
+asyncFetch("https://flipside-api.antonyip.com/getCachedQuery", options).then(
+  (res) => {
+    State.update({
+      results: "What is results?",
+    });
     if (!res.ok) {
-      props.onComplete(
-        false,
-        `near.social issue with fetch: ${JSON.stringify(res)}`
-      );
+      if (props.onComplete) {
+        props.onComplete(
+          false,
+          `near.social issue with fetch: ${JSON.stringify(res)}`
+        );
+      }
       State.update({
         results: `near.social issue with fetch: ${JSON.stringify(res)}`,
       });
@@ -31,7 +33,12 @@ function sendQueryToBackend() {
     }
     // select date_trunc('day', block_timestamp), count(1) from ethereum.core.blocks where block_timestamp > '2023-03-01' group by 1
     if (res.body.error) {
-      props.onComplete(false, `anton's api issue: ${JSON.stringify(res.body)}`);
+      if (props.onComplete) {
+        props.onComplete(
+          false,
+          `anton's api issue: ${JSON.stringify(res.body)}`
+        );
+      }
       State.update({
         results: `anton's api issue: ${JSON.stringify(res.body)}`,
       });
@@ -41,9 +48,11 @@ function sendQueryToBackend() {
     State.update({
       results: `${JSON.stringify(res.body)}`,
     });
-    props.onComplete(true, res.body);
-  });
-}
+    if (props.onComplete) {
+      props.onComplete(true, res.body);
+    }
+  }
+);
 
 if (props.debug === "true") {
   return (
