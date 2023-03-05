080f45ef14944ca29d013202f53ee597a5613bfe
antonyip <antonyip>
2023-03-05 09:40:55 -0500

    Update QuickChart.io by 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422 at 2023-03-05 09:40:55.115

---
+++ 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422/QuickChart.io/source_code.jsx
@@ -0,0 +1,5 @@
+return (
+  <div>
+    <img src="https://quickchart.io/chart?width=300&height=200&chart={type:'bar',data:{labels:['January','February', 'March','April', 'May'], datasets:[{label:'Dogs',data:[50,60,70,180,190]},{label:'Cats',data:[100,200,300,400,500]}]}}" />
+  </div>
+);
