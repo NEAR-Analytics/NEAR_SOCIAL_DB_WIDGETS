dadea52409564a3fb1d46a378d85856021037813
ligebit <ligebit>
2023-03-05 13:31:29 -0500

    Update uiw-ReactTextareaCodeEditor by bozon.near at 2023-03-05 13:31:29.221

--- bozon.near/uiw-ReactTextareaCodeEditor/source_code.jsx
+++ bozon.near/uiw-ReactTextareaCodeEditor/source_code.jsx
@@ -1,3 +1,13 @@
+// props = {
+//   editorProps: {},
+//   defaultValue: "{}",
+//   onChange: (text) => {
+//     console.log(text);
+//   },
+// };
+
+State.init({ defaultValue });
+
 const code = `
 <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
 <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
@@ -11,7 +21,14 @@ const code = `
 const CodeEditor = window["@uiw/react-textarea-code-editor"].default;
 
 function App(props) {
+  const [value, setValue] = React.useState(props.defaultValue || "");
+
   return React.createElement(CodeEditor, {
+    value,
+    onChange: (e) => {
+      setValue(e.target.value);
+      window.top.postMessage(e.target.value, "*");
+    },
     "padding": 15,
     "data-color-mode": "light", 
     "language": "js",
@@ -21,7 +38,7 @@ function App(props) {
       "fontFamily":
         "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace"
     },
-    ...props,
+    ...props.editorProps,
   }); 
 }
 
@@ -29,7 +46,7 @@ const domContainer = document.querySelector('#react-root');
 const root = ReactDOM.createRoot(domContainer);
 
 window.addEventListener("message", (event) => {
-  root.render(React.createElement(App, event.props));
+  root.render(React.createElement(App, event.data));
 });
 
 </script>
@@ -37,6 +54,14 @@ window.addEventListener("message", (event) => {
 
 return (
   <div>
-    <iframe className="w-100" srcDoc={code} message={props} />
+    <iframe
+      className="w-100"
+      srcDoc={code}
+      message={{
+        editorProps: props.editorProps,
+        defaultValue: props.defaultValue,
+      }}
+      onMessage={(m) => props.onChange(m)}
+    />
   </div>
 );
