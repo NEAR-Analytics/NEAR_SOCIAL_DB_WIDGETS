a316664bbd08315ccfb1c740460889737896af9c
bb5257df8451d24580305de53c2269985b86fc017eb7fd5a8ae7bc92854ccd2e <bb5257df8451d24580305de53c2269985b86fc017eb7fd5a8ae7bc92854ccd2e>
2023-03-05 03:16:25 -0500

    Update HelloWorld by bb5257df8451d24580305de53c2269985b86fc017eb7fd5a8ae7bc92854ccd2e at 2023-03-05 03:16:25.829

--- bb5257df8451d24580305de53c2269985b86fc017eb7fd5a8ae7bc92854ccd2e/HelloWorld/source_code.jsx
+++ bb5257df8451d24580305de53c2269985b86fc017eb7fd5a8ae7bc92854ccd2e/HelloWorld/source_code.jsx
@@ -1,25 +1,95 @@
-async function connect() {
-  /** MetaMask injects a global API into websites visited by its users at `window.ethereum`. This API allows websites to request users' Ethereum accounts, read data from blockchains the user is connected to, and suggest that the user sign messages and transactions. The presence of the provider object indicates an Ethereum user. Read more: https://ethereum.stackexchange.com/a/68294/85979**/
 
-  // Check if MetaMask is installed, if it is, try connecting to an account
-  if (typeof window.ethereum !== "undefined") {
-    try {
-      console.log("connecting");
-      // Requests that the user provides an Ethereum address to be identified by. The request causes a MetaMask popup to appear. Read more: https://docs.metamask.io/guide/rpc-api.html#eth-requestaccounts
-      await ethereum.request({ method: "eth_requestAccounts" });
-    } catch (error) {
-      console.log(error);
-    }
-    // If connected, change button to "Connected"
-    document.getElementById("login_button").innerHTML = "Connected";
-    // If connected, enable "Swap" button
-    document.getElementById("swap_button").disabled = false;
-  }
-  // Ask user to install MetaMask if it's not detected
-  else {
-    document.getElementById("login_button").innerHTML =
-      "Please install MetaMask";
+let currentTrade = {};
+let currentSelectSide;
+let tokens;
+
+async function init(){
+  await listAvailableTokens();
+}
+
+async function listAvailableTokens(){
+  console.log("initializing");
+  let response = await fetch('https://tokens.coingecko.com/uniswap/all.json');
+  let tokenListJSON = await response.json();
+  console.log("listing available tokens");
+  console.log(tokenListJSON);
+  tokens = tokenListJSON.tokens
+  console.log("tokens:", tokens);
+
+  // create token list for modal
+  let parent = document.getElementById("token_list");
+  for (const i in tokens){
+    // token row in the modal token list
+    let div = document.createElement("div");
+    div.className = "token_row";
+    let html = `
+    <img class="token_list_img" src="${tokens[i].logoURI}">
+      <span class="token_list_text">${tokens[i].symbol}</span>
+      `;
+    div.innerHTML = html;
+    div.onclick = () => {
+      selectToken(tokens[i]);
+    };
+    parent.appendChild(div);
   }
 }
-// Call the connect function when the login_button is clicked
+
+function selectToken(token) {
+  closeModal();
+  currentTrade[currentSelectSide] = token;
+  console.log("currentTrade:" , currentTrade);
+  renderInterface();
+}
+
+function renderInterface(){
+  if (currentTrade.from) {
+    console.log(currentTrade.from)
+    document.getElementById("from_token_img").src = currentTrade.from.logoURI;
+    document.getElementById("from_token_text").innerHTML = currentTrade.from.symbol;
+  }
+  if (currentTrade.to) {
+    document.getElementById("to_token_img").src = currentTrade.to.logoURI;
+    document.getElementById("to_token_text").innerHTML = currentTrade.to.symbol;
+  }
+
+}
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        try {
+          console.log("connecting");
+          await ethereum.request({ method: "eth_requestAccounts" });
+        } catch (error) {
+          console.log(error);
+        }
+        document.getElementById("login_button").innerHTML = "Connected";
+        // const accounts = await ethereum.request({ method: "eth_accounts" });
+        document.getElementById("swap_button").disabled = false;
+      } else {
+        document.getElementById("login_button").innerHTML =
+          "Please install MetaMask";
+      }
+    }
+
+function openModal(side){
+    currentSelectSide = side;
+    document.getElementById("token_modal").style.display = "block";
+}
+
+function closeModal(){
+  document.getElementById("token_modal").style.display = "none";
+}
+
+init();
+
 document.getElementById("login_button").onclick = connect;
+document.getElementById("from_token_select").onclick = () => {
+  openModal("from");
+};
+document.getElementById("to_token_select").onclick = () => {
+  openModal("to");
+};
+document.getElementById("modal_close").onclick = closeModal;
+
+
+return ()
