c48de13ebef0b5322c9e385ba0857aaabf8fe27e
evgenykuzyakov <evgenykuzyakov>
2023-03-04 21:25:39 -0500

    Update Insta.Main by mob.near at 2023-03-04 21:25:39.206

---
+++ mob.near/Insta.Main/source_code.jsx
@@ -0,0 +1,6 @@
+return (
+  <div className="d-flex flex-wrap">
+    <Widget src="mob.near/widget/Insta.Compose" props={{}} />
+    <Widget src="mob.near/widget/Insta.Feed" props={{}} />
+  </div>
+);
