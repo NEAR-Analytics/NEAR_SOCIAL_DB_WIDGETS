ab8fe40e9e6fcae6158b8e39c2b38ffe28e0f2e8
onboarder.near <onboarder.near>
2023-03-05 12:29:44 -0500

    Update erc20-factory by onboarder.near at 2023-03-05 12:29:44.314

--- onboarder.near/erc20-factory/source_code.jsx
+++ onboarder.near/erc20-factory/source_code.jsx
@@ -16,128 +16,39 @@ if (!erc20FactoryAbi.ok) {
 }
 
 const erc20Factory = "0x5ad70c187e448b9ca6104cc55619bf9a3a271efd"; // polygon
-const iface = new ethers.utils.Interface(erc20Abi.body);
 const interface = new ethers.utils.Interface(erc20FactoryAbi.body); // added this
-
-initState({
-  // this was included in the reference example
-  token: "",
-  tokenDecimals: "",
-  sendTo: "",
-  sender,
-  senderBalance: "0",
-  receiverBalance: "0",
-  receiver: "",
-  amount: "1",
-});
 
 /**TO DO INIT STATE FOR ECR20 = FACTORY */
 initState({
-  name: "",
-  symbol: "",
-  decimals: "",
-  sendTo: "",
+  name: "TOKEN",
+  symbol: "TKN",
+  decimals: 18,
+  sendTo: "0xd0decff60b18c8c17d1d616d5b81bee67aa5c327", // sending to our address
   sender, // msg.sender
   address, // after deployed this is conrect
   // senderBalance: "0",
   // receiverBalance: "0",
-  initialSupply: "",
+  initialSupply: 10000,
 });
+console.log(
+  "LOG1: Name - " +
+    state.name +
+    "Decimals - " +
+    state.decimals +
+    " Supply - " +
+    state.initialSupply +
+    " Symbol - " +
+    state.symbol
+);
 
-const tokens = {
-  "Select Token": "",
-  USDT: "0xdac17f958d2ee523a2206206994597c13d831ec7",
-  DAI: "0x6b175474e89094c44da98b954eedeac495271d0f",
-  USDC: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-  MKR: "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
-};
-
-const tokensMenuItems = Object.keys(tokens).map((token) => (
-  <option value={tokens[token]}>{token}</option>
-));
-
-const setSendTo = (sendTo) => {
-  const receiver = Ethers.resolveName(sendTo);
-  State.update({ sendTo, receiver: receiver ?? "" });
-  refreshBalances();
-};
-
-const setToken = (token) => {
-  State.update({ token });
-  getTokenDecimals();
-};
-
-const getTokenBalance = (receiver) => {
-  const encodedData = iface.encodeFunctionData("balanceOf", [receiver]);
-
-  return Ethers.provider()
-    .call({
-      to: state.token,
-      data: encodedData,
-    })
-    .then((rawBalance) => {
-      const receiverBalanceHex = iface.decodeFunctionResult(
-        "balanceOf",
-        rawBalance
-      );
-
-      return Big(receiverBalanceHex.toString())
-        .div(Big(10).pow(state.tokenDecimals))
-        .toFixed(2)
-        .replace(/\d(?=(\d{3})+\.)/g, "$&,");
-    });
-};
-
-const getTokenDecimals = () => {
-  const encodedData = iface.encodeFunctionData("decimals", []);
-
-  return Ethers.provider()
-    .call({
-      to: state.token,
-      data: encodedData,
-    })
-    .then((tokenDecimals) => {
-      State.update({ tokenDecimals: parseInt(Number(tokenDecimals)) });
-      refreshBalances();
-    });
-};
-
-const refreshBalances = () => {
-  getTokenBalance(state.sender).then((value) => {
-    State.update({ senderBalance: value });
-  });
-
-  getTokenBalance(state.receiver).then((value) => {
-    State.update({ receiverBalance: value });
-  });
-};
 /**TO DO: MUST CHANGE COPIED FROM SEND TOKEN */
 const deployToken = () => {
   const erc20 = new ethers.Contract(
     erc20Factory,
-    erc20FactoryAbi // trying without body
+    erc20FactoryAbi, // trying without body
     // erc20FactoryAbi.body, // changed this to abi
-    // Ethers.provider().getSigner()
-  );
-
-  let amount = ethers.utils.parseUnits(state.amount, state.tokenDecimals);
-
-  erc20.transfer(state.receiver, amount);
-
-  console.log("transactionHash is " + transactionHash);
-};
-//
-
-const sendTokens = () => {
-  const erc20 = new ethers.Contract(
-    state.token,
-    erc20Abi.body,
     Ethers.provider().getSigner()
   );
-
-  let amount = ethers.utils.parseUnits(state.amount, state.tokenDecimals);
-
-  erc20.transfer(state.receiver, amount);
 
   console.log("transactionHash is " + transactionHash);
 };
@@ -196,61 +107,6 @@ return (
 
     <div class="mb-3">
       <button onClick={deployToken}>Deploy Token (WIP)</button>
-    </div>
-    <h3>📩 Send ERC-20 tokens (old reference widget)</h3>
-
-    <div class="mb-3">
-      <label for="selectToken">Select token</label>
-      <select
-        class="form-select"
-        id="selectToken"
-        onChange={(e) => {
-          setToken(e.target.value);
-        }}
-      >
-        {tokensMenuItems}
-      </select>
-    </div>
-    <div class="mb-3">
-      <label for="send-to" class="form-label">
-        Recepient address
-      </label>
-      <input
-        value={state.sendTo}
-        class="form-control"
-        id="send-to"
-        placeholder="vitalik.eth"
-        onChange={(e) => setSendTo(e.target.value)}
-      />
-      {state.receiver && (
-        <div class="text-secondary mt-3">Resolved to {state.receiver}</div>
-      )}
-      {state.receiverBalance != "0" && (
-        <div class="text-secondary mt-3">
-          Receiver's balance: {state.receiverBalance}
-        </div>
-      )}
-
-      {state.senderBalance != "0" && (
-        <div class="text-secondary mt-3">
-          Sender's balance: {state.senderBalance}
-        </div>
-      )}
-    </div>
-    <div class="mb-3">
-      <label for="amount" class="form-label">
-        Enter the amount
-      </label>
-      <input
-        value={state.amount}
-        class="form-control"
-        id="amount"
-        placeholder=""
-        onChange={(e) => State.update({ amount: e.target.value })}
-      />
-    </div>
-    <div class="mb-3">
-      <button onClick={sendTokens}>Send</button>
     </div>
   </>
 );
