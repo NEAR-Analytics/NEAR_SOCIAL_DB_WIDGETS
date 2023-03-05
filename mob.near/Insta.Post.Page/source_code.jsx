8b38e82a5c45defd4b308890b73d861f3d4e5c09
evgenykuzyakov <evgenykuzyakov>
2023-03-05 00:49:50 -0500

    Update Insta.Post.Page by mob.near at 2023-03-05 00:49:50.512

--- mob.near/Insta.Post.Page/source_code.jsx
+++ mob.near/Insta.Post.Page/source_code.jsx
@@ -1,6 +1,45 @@
+const accountId = props.accountId;
+const blockHeight = parseInt(props.blockHeight);
+const content =
+  props.content ??
+  JSON.parse(Social.get(`${accountId}/post/insta`, blockHeight) ?? "null");
+
+const subscribe = !!props.subscribe;
+const raw = !!props.raw;
+const hideLink = !!props.hideLink;
+
+const imageStyle = props.imageStyle ?? {
+  objectFit: "cover",
+};
+const imageClassName = props.imageClassName ?? "w-100 h-100";
+
 return (
-  <Widget
-    src="mob.near/widget/Insta.Post"
-    props={{ subscribe: true, hideLink: true, ...props }}
-  />
+  <div>
+    <div className="mb-1">
+      <Widget src="mob.near/widget/Profile.InlineBlock" props={{ accountId }} />
+    </div>
+    <div className="ratio ratio-1x1 mb-1">
+      <Widget
+        src="mob.near/widget/Image"
+        props={{
+          image: content.image,
+          style: imageStyle,
+          className: imageClassName,
+        }}
+      />
+    </div>
+    <div className="mb-1">
+      <Widget
+        src="mob.near/widget/LikeButton"
+        props={{
+          item: {
+            type: "social",
+            path: `${accountId}/post/insta`,
+            blockHeight,
+          },
+          notifyAccountId: accountId,
+        }}
+      />
+    </div>
+  </div>
 );
