5bb3af3fcbad39e9e347a4d7056bb00636e06131
evgenykuzyakov <evgenykuzyakov>
2023-03-05 00:58:17 -0500

    Update Insta.Post by mob.near at 2023-03-05 00:58:17.748

--- mob.near/Insta.Post/source_code.jsx
+++ mob.near/Insta.Post/source_code.jsx
@@ -3,7 +3,11 @@ const blockHeight =
   props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
 const content =
   props.content ??
-  JSON.parse(Social.get(`${accountId}/post/insta`, blockHeight) ?? "null");
+  JSON.parse(
+    Social.get(`${accountId}/post/main`, blockHeight) ??
+      Social.get(`${accountId}/post/insta`, blockHeight) ??
+      "null"
+  );
 const subscribe = !!props.subscribe;
 const raw = !!props.raw;
 const hideLink = !!props.hideLink;
@@ -64,7 +68,7 @@ return (
             props={{
               item: {
                 type: "social",
-                path: `${accountId}/post/insta`,
+                path: `${accountId}/post/main`,
                 blockHeight,
               },
               notifyAccountId: accountId,
