147d1f0b68c1767cf353a7035ab53e31af253198
antonyip <antonyip>
2023-03-05 03:10:17 -0500

    Update Flipside-API-Getter by 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422 at 2023-03-05 03:10:17.975

--- 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422/Flipside-API-Getter/source_code.jsx
+++ 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422/Flipside-API-Getter/source_code.jsx
@@ -10,49 +10,42 @@ const options = {
   },
 };
 
-State.update({
-  results: "Query Triggered...",
-});
-
-asyncFetch("https://flipside-api.antonyip.com/getCachedQuery", options).then(
-  (res) => {
-    State.update({
-      results: "What is results?",
-    });
-    if (!res.ok) {
-      if (props.onComplete) {
-        props.onComplete(
-          false,
-          `near.social issue with fetch: ${JSON.stringify(res)}`
-        );
-      }
-      State.update({
-        results: `near.social issue with fetch: ${JSON.stringify(res)}`,
-      });
-      return;
-    }
-    // select date_trunc('day', block_timestamp), count(1) from ethereum.core.blocks where block_timestamp > '2023-03-01' group by 1
-    if (res.body.error) {
-      if (props.onComplete) {
-        props.onComplete(
-          false,
-          `anton's api issue: ${JSON.stringify(res.body)}`
-        );
-      }
-      State.update({
-        results: `anton's api issue: ${JSON.stringify(res.body)}`,
-      });
-      return;
-    }
-
-    State.update({
-      results: `${JSON.stringify(res.body)}`,
-    });
+const res = fetch("https://flipside-api.antonyip.com/getCachedQuery", options); //.then((res) =>
+{
+  State.update({
+    results: "What is results?",
+  });
+  if (!res.ok) {
     if (props.onComplete) {
-      props.onComplete(true, res.body);
+      props.onComplete(
+        false,
+        `near.social issue with fetch: ${JSON.stringify(res)}`
+      );
     }
+    State.update({
+      results: `near.social issue with fetch: ${JSON.stringify(res)}`,
+    });
+    return;
   }
-);
+  // select date_trunc('day', block_timestamp), count(1) from ethereum.core.blocks where block_timestamp > '2023-03-01' group by 1
+  if (res.body.error) {
+    if (props.onComplete) {
+      props.onComplete(false, `anton's api issue: ${JSON.stringify(res.body)}`);
+    }
+    State.update({
+      results: `anton's api issue: ${JSON.stringify(res.body)}`,
+    });
+    return;
+  }
+
+  State.update({
+    results: `${JSON.stringify(res.body)}`,
+  });
+  if (props.onComplete) {
+    props.onComplete(true, res.body);
+  }
+}
+//);
 
 if (props.debug === "true") {
   return (
