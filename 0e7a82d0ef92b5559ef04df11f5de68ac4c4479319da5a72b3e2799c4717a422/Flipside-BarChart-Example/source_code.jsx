a4ed5a236ee8a549441c8ad5b0d0a03ac5644a70
antonyip <antonyip>
2023-03-05 02:51:04 -0500

    Update Flipside-BarChart-Example by 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422 at 2023-03-05 02:51:04.996

--- 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422/Flipside-BarChart-Example/source_code.jsx
+++ 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422/Flipside-BarChart-Example/source_code.jsx
@@ -81,91 +81,93 @@ const height = 500;
 const maxValue = Math.max(...data.body.map((d) => d.ACTIVE_WALLETS)) * 1.1;
 
 return (
-  <Style>
-    <div className="text-bg-light rounded-4 p-3 mb-4">
-      {data !== null ? (
-        <p>
-          <div class="d-flex clearfix flex-wrap flex-column flex-sm-row">
-            <div class="p-2">
-              <div>
-                <h2>Metric: {METRIC_NAME}</h2>
+  <>
+    <Style>
+      <div className="text-bg-light rounded-4 p-3 mb-4">
+        {data !== null ? (
+          <p>
+            <div class="d-flex clearfix flex-wrap flex-column flex-sm-row">
+              <div class="p-2">
+                <div>
+                  <h2>Metric: {METRIC_NAME}</h2>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div>
-            <hr />
-            <div className="bar-chart">
-              <svg
-                width={width}
-                height={height}
-                viewBox={`0 0 ${width} ${height + 200} `}
-                preserveAspectRatio="xMidYMid meet"
-              >
-                {data.body.map((d, i) => (
-                  <g key={i} className="barTextH">
-                    <rect
-                      className="bar"
-                      x={i * (width / data.body.length)}
-                      y={height - (d.ACTIVE_WALLETS / maxValue) * height}
-                      width={width / data.body.length - 2}
-                      height={(d.ACTIVE_WALLETS / maxValue) * height}
-                      fill="#61dafb"
-                    />
-                    <text
-                      className="text-primary-emphasis"
-                      x={
-                        i * (width / data.body.length) +
-                        width / data.body.length / 2
-                      }
-                      y={height + 40}
-                      transform={`rotate(-75 ${
-                        i * (width / data.body.length) +
-                        width / data.body.length / 2
-                      } ${height + 40})`}
-                      textAnchor="middle"
-                    >
-                      / {formatDate(d.MONTH)}
-                    </text>
-                    <text
-                      className="text-primary-emphasis"
-                      x={
-                        i * (width / data.body.length) +
-                        width / data.body.length / 2
-                      }
-                      y={height + 90}
-                      transform={`rotate(-75 ${
-                        i * (width / data.body.length) +
-                        width / data.body.length / 2
-                      } ${height + 110})`}
-                      textAnchor="middle"
-                    >
-                      {formatNumberWithCommas(d.ACTIVE_WALLETS)}
-                    </text>
-                  </g>
-                ))}
-              </svg>
+            <div>
+              <hr />
+              <div className="bar-chart">
+                <svg
+                  width={width}
+                  height={height}
+                  viewBox={`0 0 ${width} ${height + 200} `}
+                  preserveAspectRatio="xMidYMid meet"
+                >
+                  {data.body.map((d, i) => (
+                    <g key={i} className="barTextH">
+                      <rect
+                        className="bar"
+                        x={i * (width / data.body.length)}
+                        y={height - (d.ACTIVE_WALLETS / maxValue) * height}
+                        width={width / data.body.length - 2}
+                        height={(d.ACTIVE_WALLETS / maxValue) * height}
+                        fill="#61dafb"
+                      />
+                      <text
+                        className="text-primary-emphasis"
+                        x={
+                          i * (width / data.body.length) +
+                          width / data.body.length / 2
+                        }
+                        y={height + 40}
+                        transform={`rotate(-75 ${
+                          i * (width / data.body.length) +
+                          width / data.body.length / 2
+                        } ${height + 40})`}
+                        textAnchor="middle"
+                      >
+                        / {formatDate(d.MONTH)}
+                      </text>
+                      <text
+                        className="text-primary-emphasis"
+                        x={
+                          i * (width / data.body.length) +
+                          width / data.body.length / 2
+                        }
+                        y={height + 90}
+                        transform={`rotate(-75 ${
+                          i * (width / data.body.length) +
+                          width / data.body.length / 2
+                        } ${height + 110})`}
+                        textAnchor="middle"
+                      >
+                        {formatNumberWithCommas(d.ACTIVE_WALLETS)}
+                      </text>
+                    </g>
+                  ))}
+                </svg>
+              </div>
             </div>
-          </div>
-          <div>
-            <hr />
-            <small className="fw-bold">
-              Data is provided by{" "}
-              <a
-                target="_blank"
-                style={{ color: "inherit" }}
-                variant="caption"
-                rel="nofollow"
-                href="https://www.flipsidecrypto.com/"
-              >
-                Flipside Crypto
-              </a>
-            </small>
-          </div>
-        </p>
-      ) : (
-        <div>Loading ...</div>
-      )}
-    </div>
-  </Style>
+            <div>
+              <hr />
+              <small className="fw-bold">
+                Data is provided by{" "}
+                <a
+                  target="_blank"
+                  style={{ color: "inherit" }}
+                  variant="caption"
+                  rel="nofollow"
+                  href="https://www.flipsidecrypto.com/"
+                >
+                  Flipside Crypto
+                </a>
+              </small>
+            </div>
+          </p>
+        ) : (
+          <div>Loading ...</div>
+        )}
+      </div>
+    </Style>
+  </>
 );
