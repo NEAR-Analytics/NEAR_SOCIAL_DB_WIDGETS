979fc75d75ae3a5d2b39feff250c46d14550dc27
evgenykuzyakov <evgenykuzyakov>
2023-03-04 19:45:53 -0500

    Update Insta.Feed by mob.near at 2023-03-04 19:45:53.144

---
+++ mob.near/Insta.Feed/source_code.jsx
@@ -0,0 +1,27 @@
+const index = {
+  action: "post",
+  key: "insta",
+  options: {
+    limit: 30,
+    order: "desc",
+    accountId: props.accounts,
+  },
+};
+
+const renderItem = (a) =>
+  a.value.type === "insta" && (
+    <Widget
+      key={JSON.stringify(a)}
+      src="mob.near/widget/Insta.Post"
+      props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
+    />
+  );
+
+return (
+  <div>
+    <Widget
+      src="mob.near/widget/FilteredIndexFeed"
+      props={{ index, renderItem }}
+    />
+  </div>
+);
