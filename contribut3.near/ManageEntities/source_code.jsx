08dff1741d2e4268f1e8fb2f4273a7ae76dda1f4
contribut3.near <contribut3.near>
2023-03-06 16:28:26 -0500

    Update ManageEntities by contribut3.near at 2023-03-06 16:28:26.755

--- contribut3.near/ManageEntities/source_code.jsx
+++ contribut3.near/ManageEntities/source_code.jsx
@@ -33,8 +33,8 @@ const contentSelector = (
           text: "My Proposals",
           icon: (
             <svg
-              width="15"
-              height="15"
+              width="20"
+              height="20"
               viewBox="0 0 20 20"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -74,8 +74,8 @@ const contentSelector = (
           text: "Requests",
           icon: (
             <svg
-              width="15"
-              height="15"
+              width="20"
+              height="20"
               viewBox="0 0 20 20"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
