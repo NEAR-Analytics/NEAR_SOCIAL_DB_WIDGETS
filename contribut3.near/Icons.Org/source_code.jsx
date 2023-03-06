81f990d87ec34c22d130c4fbf2e95b526b41685c
contribut3.near <contribut3.near>
2023-03-06 16:18:26 -0500

    Update Icons.Org by contribut3.near at 2023-03-06 16:18:26.225

--- contribut3.near/Icons.Org/source_code.jsx
+++ contribut3.near/Icons.Org/source_code.jsx
@@ -6,12 +6,6 @@ return (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <rect
-      width="24"
-      height="24"
-      transform="translate(0 0.161133)"
-      fill="white"
-    />
     <path
       fill-rule="evenodd"
       clip-rule="evenodd"
