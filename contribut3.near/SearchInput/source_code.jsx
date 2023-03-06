137aff660de3d3dd38409db692b1d9ebecf6456b
contribut3.near <contribut3.near>
2023-03-06 16:38:03 -0500

    Update SearchInput by contribut3.near at 2023-03-06 16:38:03.567

--- contribut3.near/SearchInput/source_code.jsx
+++ contribut3.near/SearchInput/source_code.jsx
@@ -20,9 +20,7 @@ return (
   <div className="w-25 col-12 col-md-10 col-lg-8">
     <div className="card card-sm">
       <div className="card-body row p-0 ps-2 align-items-center">
-        <div className="col-auto pe-0 me-0">
-          <i className="bi-search" />
-        </div>
+        <div className="col-auto pe-0 me-0">{icon}</div>
         <div className="col ms-0">
           <input
             className="form-control border-0"
