73e30e1f7c60598de7736521183b62373919b77a
contribut3.near <contribut3.near>
2023-03-06 11:36:38 -0500

    Update CardMenu by contribut3.near at 2023-03-06 11:36:38.893

--- contribut3.near/CardMenu/source_code.jsx
+++ contribut3.near/CardMenu/source_code.jsx
@@ -68,9 +68,9 @@ const DropdownList = styled.ul`
   transition: transform 0.2s ease-in-out;
   transform-origin: top right;
 
-  &.show {
-    transform: translate(-44px, 0px) scale(1);
-  }
+  /* &.show { */
+  /*   transform: translate(-44px, 0px) scale(1); */
+  /* } */
 `;
 
 const Dot = styled.i`
@@ -101,13 +101,20 @@ const MenuIcon = styled.a`
 
 const DropdownContainer = styled.div`
   position: relative;
+
+  &:focus-within ul + && {
+    transform: translate(-44px, 0px) scale(1);
+  }
 `;
 
 return (
   <DropdownContainer>
     <MenuIcon
       onClick={() => State.update({ show: !state.show })}
-      onBlur={() => { console.log("blur!")State.update({ show: false }) }}
+      onBlur={() => {
+        console.log("blur!");
+        State.update({ show: false });
+      }}
     >
       <Dot />
       <Dot />
