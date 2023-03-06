69c02447c794592e2dc80dc9938f33cbd0b5b930
luacan.near <luacan.near>
2023-03-05 19:27:06 -0500

    Update FlyingRhinoWeb3 by luacan.near at 2023-03-05 19:27:06.749

---
+++ luacan.near/FlyingRhinoWeb3/source_code.jsx
@@ -0,0 +1,13 @@
+let greeting = "Marketing Web3 Agency in NEAR ";
+let greeting2 = "Scaling up Web3 projects";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center min-vw-100">
+      <h1>Flying Rhino Web3</h1>
+      <p> {greeting} </p>
+      <p> {greeting2} </p>
+      <h2>Hello {props.name} !</h2>
+    </div>
+  </>
+);
