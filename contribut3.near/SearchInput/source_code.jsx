30069f271147fb96cc84f6026a1d2c8d7b7e73d7
contribut3.near <contribut3.near>
2023-03-06 16:37:46 -0500

    Update SearchInput by contribut3.near at 2023-03-06 16:37:46.904

--- contribut3.near/SearchInput/source_code.jsx
+++ contribut3.near/SearchInput/source_code.jsx
@@ -1,3 +1,21 @@
+const icon = (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M17.5 17.5L14.5834 14.5833M16.6667 9.58333C16.6667 13.4954 13.4954 16.6667 9.58333 16.6667C5.67132 16.6667 2.5 13.4954 2.5 9.58333C2.5 5.67132 5.67132 2.5 9.58333 2.5C13.4954 2.5 16.6667 5.67132 16.6667 9.58333Z"
+      stroke="#667085"
+      stroke-width="1.66667"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+
 return (
   <div className="w-25 col-12 col-md-10 col-lg-8">
     <div className="card card-sm">
@@ -18,6 +36,3 @@ return (
     </div>
   </div>
 );
-<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M17.5 17.5L14.5834 14.5833M16.6667 9.58333C16.6667 13.4954 13.4954 16.6667 9.58333 16.6667C5.67132 16.6667 2.5 13.4954 2.5 9.58333C2.5 5.67132 5.67132 2.5 9.58333 2.5C13.4954 2.5 16.6667 5.67132 16.6667 9.58333Z" stroke="#667085" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
