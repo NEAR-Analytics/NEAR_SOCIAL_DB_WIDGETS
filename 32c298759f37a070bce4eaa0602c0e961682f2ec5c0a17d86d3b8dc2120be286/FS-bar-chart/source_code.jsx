9d92ee20f483f2a58cf40976d76c467dfe6f46cd
saleh20032004 <saleh20032004>
2023-03-07 09:55:44 -0500

    Update FS-bar-chart by 32c298759f37a070bce4eaa0602c0e961682f2ec5c0a17d86d3b8dc2120be286 at 2023-03-07 09:55:44.731

--- 32c298759f37a070bce4eaa0602c0e961682f2ec5c0a17d86d3b8dc2120be286/FS-bar-chart/source_code.jsx
+++ 32c298759f37a070bce4eaa0602c0e961682f2ec5c0a17d86d3b8dc2120be286/FS-bar-chart/source_code.jsx
@@ -24,7 +24,7 @@ asyncFetch("https://flipside-api.antonyip.com/getCachedQuery", options).then(
     let queryResults = res.body;
 
     console.log("queryResults", queryResults);
-    const chartType = `"bar"`;
+    const chartType = `"pie"`;
     let chartBottomAxisLabels = "[";
     let chartValues = "[";
     // parsing of data from flipside
