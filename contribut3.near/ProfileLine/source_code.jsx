71ede774ada190d483eb4cba9bd6f9df4f17cdae
contribut3.near <contribut3.near>
2023-03-07 08:10:12 -0500

    Update ProfileLine by contribut3.near at 2023-03-07 08:10:12.581

--- contribut3.near/ProfileLine/source_code.jsx
+++ contribut3.near/ProfileLine/source_code.jsx
@@ -6,6 +6,13 @@ const additionalRow = props.additionalRow;
 const alignment = additionalRow ? "start" : "center";
 const additionalColumn = props.additionalColumn;
 const imageSize = props.imageSize;
+const linkNavigate = () =>
+  props.update({
+    tab: isEntity ? "entity" : "contributor",
+    accountId,
+    content: "",
+    search: "",
+  });
 
 State.init({
   data: null,
@@ -28,48 +35,35 @@ const ImageContainer = styled.div`
   margin: 0.5em;
 `;
 
+const ImageLink = styled.a`
+  color: #667085;
+
+  span {
+    color: #687076;
+    margin: 0 0.25em;
+  }
+`;
+
 if (state.data || profile) {
   return (
     <div
       className={`d-flex flex-row justify-content-start align-items-${alignment}`}
     >
-      <a
-        className="text-dark"
-        href={href}
-        onClick={() =>
-          props.update({
-            tab: isEntity ? "entity" : "contributor",
-            content: "",
-            search: "",
-            accountId,
-          })
-        }
-      >
+      <ImageLink href={href} onClick={linkNavigate}>
         <ImageContainer>
           <Widget
             src={`${ownerId}/widget/ProfileCircle`}
             props={{ accountId, size: imageSize, isEntity }}
           />
         </ImageContainer>
-      </a>
+      </ImageLink>
       <div className="d-flex flex-column justify-content-between align-items-start flex-grow-1">
         <div className="w-100 d-flex flex-row justify-content-between align-items-start">
           <div>
-            <a
-              className="text-dark"
-              href={href}
-              onClick={() =>
-                props.update({
-                  tab: isEntity ? "entity" : "contributor",
-                  content: "",
-                  search: "",
-                  accountId,
-                })
-              }
-            >
+            <ImageLink href={href} onClick={linkNavigate}>
               <b>{fullName}</b>
               <span className="text-muted mx-1">@{accountId}</span>
-            </a>
+            </ImageLink>
             {additionalText}
           </div>
         </div>
