e41ad3b1d485dc1e08c2a25a73dd3a862081a0e1
contribut3.near <contribut3.near>
2023-03-07 12:19:31 -0500

    Update ContributionList by contribut3.near at 2023-03-07 12:19:31.237

--- contribut3.near/ContributionList/source_code.jsx
+++ contribut3.near/ContributionList/source_code.jsx
@@ -53,63 +53,7 @@ return (
               update: props.update,
             }}
           />
-          <Widget
-            src={`${ownerId}/widget/Need`}
-            props={{
-              accountId,
-              cid,
-              update: props.update,
-            }}
-          />
         </WidgetContainer>
       ))}
   </InfiniteScroll>
-);
-
-if (!accountId) {
-  return "Cannot render contribution list without account ID!";
-}
-
-const contributions = Object.keys(
-  Near.view(
-    ownerId,
-    isEntity ? "get_entity_contributions" : "get_contributor_contributions",
-    { account_id: accountId },
-    "final",
-    true
-  ) ?? {}
-);
-
-if (!contributions) {
-  return "Loading...";
-}
-
-if (Array.isArray(contributions) && contributions.length === 0) {
-  return "No contributions found!";
-}
-
-const allContributions = contributions.filter((id) =>
-  id.includes(props.search)
-);
-
-if (!allContributions || allContributions.length === 0) {
-  return "No contributions match search criteria!";
-}
-
-return (
-  <>
-    {allContributions.map((id) => (
-      <div key={id} className="mt-3">
-        <Widget
-          src={`${ownerId}/widget/Contribution`}
-          props={{
-            entityId: isEntity ? accountId : id,
-            contributorId: isEntity ? id : accountId,
-            isEntity,
-            update: props.update,
-          }}
-        />
-      </div>
-    ))}
-  </>
 );
