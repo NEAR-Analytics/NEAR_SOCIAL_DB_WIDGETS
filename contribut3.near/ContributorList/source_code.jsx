ee1d720eb8816cd41ed6d3d97c8a5d7fa378d1d0
contribut3.near <contribut3.near>
2023-03-07 11:45:09 -0500

    Update ContributorList by contribut3.near at 2023-03-07 11:45:09.292

--- contribut3.near/ContributorList/source_code.jsx
+++ contribut3.near/ContributorList/source_code.jsx
@@ -2,6 +2,14 @@ const ownerId = "contribut3.near";
 const search = props.search ?? "";
 const accountId = props.accountId;
 const cid = props.cid;
+const limit = 10;
+
+State.init({
+  contributors: [],
+  shown: [],
+  from: 0,
+  hasMore: true,
+});
 
 const allContributors = (
   accountId
@@ -16,6 +24,30 @@ const allContributors = (
     )
     : Near.view(ownerId, "get_contributors", {}, "final", false) ?? []
 ).filter((id) => id.includes(search));
+
+if (state.contributors.length === 0) {
+  Near.asyncView(
+    ownerId,
+    accountId
+      ? cid
+        ? "get_need_contributions"
+        : "get_entity_contributions"
+      : "get_contributors",
+    {
+      ...(accountId ? { account_id: accountId } : {}),
+      ...(cid ? { cid } : {}),
+    },
+    "final",
+    false
+  ).then((contributors) => {
+    State.update({
+      contributors: contributors.sort(),
+      shown: contributors.slice(0, limit),
+      from: limit,
+      hasMore: contributors.length > limit,
+    });
+  });
+}
 
 if (!allContributors || allContributors.length === 0) {
   return "No contributors found!";
