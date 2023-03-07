f7c63cd5197f41bebd1e66cf042791b9ecebf9bd
contribut3.near <contribut3.near>
2023-03-07 08:58:21 -0500

    Update EntityList by contribut3.near at 2023-03-07 08:58:21.587

--- contribut3.near/EntityList/source_code.jsx
+++ contribut3.near/EntityList/source_code.jsx
@@ -1,23 +1,34 @@
 const ownerId = "contribut3.near";
 const search = props.search ?? "";
 
-const allEntities = Object.keys(
-  Near.view(ownerId, "get_entities", {}, "final", false) ?? {}
-).filter((accountId) => (search ? accountId.includes(search) : true));
+State.init({
+  entities: [],
+  from: 0,
+  hasMore: true,
+});
 
-if (!allEntities || allEntities.length === 0) {
-  return "No entities found!";
-}
+const loadMore = () => {
+  Near.asyncView(ownerId, "get_entities", { from: state.from, limit: 10 }).then(
+    (entities) =>
+      State.update({
+        from: state.from + 10,
+        entities: [...state.entities, ...Object.keys(entities)],
+        hasMore: Objet.keys(entities).length > 0,
+      })
+  );
+};
 
 return (
-  <>
-    {allEntities.map((accountId) => (
-      <div key={accountId} className="mb-4">
-        <Widget
-          src={`${ownerId}/widget/Entity`}
-          props={{ accountId, update: props.update }}
-        />
-      </div>
-    ))}
-  </>
+  <InfiniteScroll loadMore={loadMore} hasMore={state.hasMore}>
+    {state.entities
+      .filter((accountId) => (search ? accountId.includes(search) : true))
+      .map((accountId) => (
+        <div key={accountId} className="mb-4">
+          <Widget
+            src={`${ownerId}/widget/Entity`}
+            props={{ accountId, update: props.update }}
+          />
+        </div>
+      ))}
+  </InfiniteScroll>
 );
