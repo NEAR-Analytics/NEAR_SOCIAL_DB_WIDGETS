d4f999b32cebd573875e2ee8594e3970b2e34f86
contribut3.near <contribut3.near>
2023-03-07 12:19:20 -0500

    Update ContributionList by contribut3.near at 2023-03-07 12:19:20.923

--- contribut3.near/ContributionList/source_code.jsx
+++ contribut3.near/ContributionList/source_code.jsx
@@ -1,5 +1,4 @@
 const ownerId = "contribut3.near";
-const accountId = props.accountId;
 const isEntity = props.isEntity ?? false;
 const limit = 10;
 
@@ -14,7 +13,7 @@ if (state.items.length === 0) {
   Near.asyncView(
     ownerId,
     isEntity ? "get_entity_contributions" : "get_contributor_contributions",
-    { account_id: accountId },
+    { account_id: props.accountId },
     "final",
     false
   ).then((accountIds) =>
@@ -26,6 +25,46 @@ if (state.items.length === 0) {
     })
   );
 }
+
+const loadMore = () => {
+  State.update({
+    shown: state.items.slice(0, state.from + limit),
+    from: state.from + limit,
+    hasMore: state.from + limit < state.items.length,
+  });
+};
+
+const WidgetContainer = styled.div`
+  margin: 0.5em 0;
+`;
+
+return (
+  <InfiniteScroll loadMore={loadMore} hasMore={state.hasMore}>
+    {state.shown
+      .filter((accountId) => accountId.includes(search))
+      .map((accountId) => (
+        <WidgetContainer key={accountId}>
+          <Widget
+            src={`${ownerId}/widget/Contribution`}
+            props={{
+              entityId: isEntity ? props.accountId : accountId,
+              contributorId: !isEntity ? props.accountId : accountId,
+              isEntity,
+              update: props.update,
+            }}
+          />
+          <Widget
+            src={`${ownerId}/widget/Need`}
+            props={{
+              accountId,
+              cid,
+              update: props.update,
+            }}
+          />
+        </WidgetContainer>
+      ))}
+  </InfiniteScroll>
+);
 
 if (!accountId) {
   return "Cannot render contribution list without account ID!";
