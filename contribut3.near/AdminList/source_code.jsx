f5c095b9a8a961424a0f1cd08feb1ea4aa3eabb8
contribut3.near <contribut3.near>
2023-03-07 10:49:10 -0500

    Update AdminList by contribut3.near at 2023-03-07 10:49:10.262

--- contribut3.near/AdminList/source_code.jsx
+++ contribut3.near/AdminList/source_code.jsx
@@ -1,15 +1,41 @@
 const ownerId = "contribut3.near";
+const limit = 10;
 const search = props.search ?? "";
 
-const allEntities = Object.keys(
+State.init({
+  entities: [],
+  shown: [],
+  from: 0,
+  hasMore: true,
+});
+
+Near.asyncView(
+  ownerId,
+  "get_admin_entities",
+  { account_id: context.accountId },
+  "final",
+  false
+).then((entities) => State.update({ entities: entities.sort() }));
+
+const allEntities = (
   Near.view(
     ownerId,
     "get_admin_entities",
     { account_id: context.accountId },
     "final",
-    true
-  ) ?? {}
+    false
+  ) ?? []
 ).filter((accountId) => accountId.includes(search));
+
+const loadMore = () =>
+  State.update({
+    shown: [
+      ...state.shown,
+      state.entities.slice(state.from, state.from + limit),
+    ],
+    from: state.from + limit,
+    hasMore: state.from + limit < state.entities.length,
+  });
 
 if (!allEntities || allEntities.length === 0) {
   return "No entities with Admin access for your account!";
