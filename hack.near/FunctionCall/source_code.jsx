0b27ec0eb6a885e7d657d94f40324e8e0f1e7d0d
NearSocial <NearSocial>
2023-03-07 17:08:16 -0500

    Update FunctionCall by hack.near at 2023-03-07 17:08:16.014

--- hack.near/FunctionCall/source_code.jsx
+++ hack.near/FunctionCall/source_code.jsx
@@ -13,12 +13,14 @@ State.init({
   gas: "",
 });
 
+const fc_args = Buffer.from(state.args, "utf-8").toString("base64");
+
 const handleFunctionCall = () => {
   Near.call([
     {
       contractName: contractId,
       methodName: state.method_name,
-      args: state.args,
+      args: fc_args,
       deposit: state.deposit ?? "1",
       gas: state.gas ?? "200000000000000",
     },
