44c7fe2a27f5f7ac07c3e4c8337df0b151b328f5
contribut3.near <contribut3.near>
2023-03-07 12:07:19 -0500

    Update InviteList by contribut3.near at 2023-03-07 12:07:19.553

--- contribut3.near/InviteList/source_code.jsx
+++ contribut3.near/InviteList/source_code.jsx
@@ -4,7 +4,7 @@ const accountId = props.accountId;
 const limit = 10;
 
 State.init({
-  invites: [],
+  items: [],
   shown: [],
   from: 0,
   hasMore: true,
@@ -19,7 +19,7 @@ if (state.invites.length === 0) {
     false
   ).then((invites) =>
     State.update({
-      invites: invites.sort(),
+      items: invites.sort(),
       shown: invites.slice(0, limit),
       from: limit,
       hasMore: invites.length > limit,
@@ -29,9 +29,9 @@ if (state.invites.length === 0) {
 
 const loadMore = () =>
   State.update({
-    shown: state.invites.slice(0, state.from + limit),
+    shown: state.items.slice(0, state.from + limit),
     from: state.from + limit,
-    hasMore: state.from + limit < state.invites.length,
+    hasMore: state.from + limit < state.items.length,
   });
 
 const WidgetContainer = styled.div`
