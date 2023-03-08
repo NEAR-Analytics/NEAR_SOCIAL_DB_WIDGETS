1e7f8f2c528fba3755d3106047c2e568aae981f0
contribut3.near <contribut3.near>
2023-03-08 09:11:55 -0500

    Update ContributionRequest by contribut3.near at 2023-03-08 09:11:55.889

--- contribut3.near/ContributionRequest/source_code.jsx
+++ contribut3.near/ContributionRequest/source_code.jsx
@@ -102,9 +102,9 @@ const controls = (
     >
       <i>
         <svg
-          width="21"
+          width="20"
           height="20"
-          viewBox="0 0 21 20"
+          viewBox="0 0 20 20"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
@@ -130,9 +130,9 @@ const controls = (
     >
       <i>
         <svg
-          width="21"
+          width="20"
           height="20"
-          viewBox="0 0 21 20"
+          viewBox="0 0 20 20"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
