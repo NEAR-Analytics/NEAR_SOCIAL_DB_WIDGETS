8a6bda433f7345bd633f4cb3bbd686c6f9506d5f
contribut3.near <contribut3.near>
2023-03-08 10:57:11 -0500

    Update ContributionRequestForm by contribut3.near at 2023-03-08 10:57:11.749

--- contribut3.near/ContributionRequestForm/source_code.jsx
+++ contribut3.near/ContributionRequestForm/source_code.jsx
@@ -58,33 +58,40 @@ const EntityInput = styled.div`
   margin-bottom: 0.5em;
 `;
 
-const entityEditor = props.entity ? (
-  <div>
-    <Label htmlFor="account-id">Contribute to:</Label>
-    <div
-      className="rounded-3 bg-light"
-      style={{ height: "5em" }}
-      id="account-id"
-    >
-      <Widget
-        src={`${ownerId}/widget/ProfileLine`}
-        props={{ accountId: props.entity, imageSize: "4em", isEntity: true }}
-      />
-    </div>
-  </div>
-) : (
-  <div className="col-lg-12  mb-2">
+const entityEditor = (
+  <EntityInput>
     <Label htmlFor="enity-id">Contribute to:</Label>
-    <Typeahead
-      id="entity-id"
-      labelKey="name"
-      onChange={(entity) => State.update({ entity })}
-      options={state.existingEntities}
-      placeholder="social.near, contribut3.near"
-      selected={state.entity}
-      positionFixed
-    />
-  </div>
+    {props.entity ? (
+      <div>
+        <div
+          className="rounded-3 bg-light"
+          style={{ height: "5em" }}
+          id="account-id"
+        >
+          <Widget
+            src={`${ownerId}/widget/ProfileLine`}
+            props={{
+              accountId: props.entity,
+              imageSize: "4em",
+              isEntity: true,
+            }}
+          />
+        </div>
+      </div>
+    ) : (
+      <div className="col-lg-12  mb-2">
+        <Typeahead
+          id="entity-id"
+          labelKey="name"
+          onChange={(entity) => State.update({ entity })}
+          options={state.existingEntities}
+          placeholder="social.near, contribut3.near"
+          selected={state.entity}
+          positionFixed
+        />
+      </div>
+    )}
+  </EntityInput>
 );
 
 const descriptionDiv = (
