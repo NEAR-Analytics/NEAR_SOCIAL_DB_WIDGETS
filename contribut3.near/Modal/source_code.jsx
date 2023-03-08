ceb4c131ea03c78b27aa69144f8a89bbcb2580b1
contribut3.near <contribut3.near>
2023-03-08 09:56:28 -0500

    Update Modal by contribut3.near at 2023-03-08 09:56:28.235

--- contribut3.near/Modal/source_code.jsx
+++ contribut3.near/Modal/source_code.jsx
@@ -13,7 +13,7 @@ const Modal = styled.div`
   align-items: center;
 `;
 
-const ModalBackgrop = styled.div`
+const ModalBackdrop = styled.div`
   position: absolute;
   inset: 0;
   background-color: rgba(0, 0, 0, 0.5);
@@ -33,38 +33,41 @@ const ModalHeader = styled.div`
   align-items: flex-start;
 `;
 
+const ModalBody = styled.div``;
+
+const ModalFooter = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
 return (
-  <div
-    className={`modal fade ${hidden ? "" : "show d-block"}`}
-    data-bs-backdrop="static"
-    tabIndex="-1"
-    aria-hidden={hidden}
-  >
-    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h5 className="modal-title">{title}</h5>
-          <a
-            className="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-            onClick={onClose}
-          />
-        </div>
-        <div className="modal-body">{body}</div>
-        <div className="modal-footer justify-content-between">
-          <a
-            className="btn btn-secondary"
-            data-bs-dismiss="modal"
-            onClick={onClose}
-          >
-            Close
-          </a>
-          <a className="btn btn-primary" onClick={onConfirm}>
-            {confirmText}
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Modal hidden={hidden}>
+    <ModalBackdrop />
+    <ModalDialog>
+      <ModalHeader>
+        <h5 className="modal-title">{title}</h5>
+        <a
+          className="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+          onClick={onClose}
+        />
+      </ModalHeader>
+      <ModalBody>{body}</ModalBody>
+      <ModalFooter>
+        <a
+          className="btn btn-secondary"
+          data-bs-dismiss="modal"
+          onClick={onClose}
+        >
+          Close
+        </a>
+        <a className="btn btn-primary" onClick={onConfirm}>
+          {confirmText}
+        </a>
+      </ModalFooter>
+    </ModalDialog>
+  </Modal>
 );
