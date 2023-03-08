2e91ff3e3f3b98dc3c3e0e6565d19ad0a7e5555f
contribut3.near <contribut3.near>
2023-03-08 09:00:59 -0500

    Update ContributionRequest by contribut3.near at 2023-03-08 09:00:59.457

--- contribut3.near/ContributionRequest/source_code.jsx
+++ contribut3.near/ContributionRequest/source_code.jsx
@@ -103,7 +103,23 @@ const controls = (
         })
       }
     >
-      <i className="bi-x" />
+      <i>
+        <svg
+          width="21"
+          height="20"
+          viewBox="0 0 21 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M15.5 5L5.5 15M5.5 5L15.5 15"
+            stroke="#F04438"
+            stroke-width="1.66667"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </i>
       <span>Reject</span>
     </a>
   </Controls>
