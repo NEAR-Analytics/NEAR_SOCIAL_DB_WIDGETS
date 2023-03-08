ff61511276ba1a823f9c5bac82075f68edd173b8
NearSocial <NearSocial>
2023-03-08 01:46:38 -0500

    Update DAO by hack.near at 2023-03-08 01:46:38.840

--- hack.near/DAO/source_code.jsx
+++ hack.near/DAO/source_code.jsx
@@ -3,33 +3,8 @@ const daoId = props.daoId ?? "multi.sputnik-dao.near";
 const groupId = props.groupId ?? "council";
 const policy = Near.view(daoId, "get_policy");
 
-State.init({
-  dao_id: "",
-  group_id: "",
-});
-
-const onChangeDAO = (daoId) => {
-  State.update({
-    daoId,
-  });
-};
-
-const onChangeGroup = (groupId) => {
-  State.update({
-    groupId,
-  });
-};
-
 return (
   <div className="mb-3">
-    <div className="mb-3">
-      DAO:
-      <input type="text" onChange={(e) => onChangeDAO(e.target.value)} />
-    </div>
-    <div className="mb-3">
-      Group:
-      <input type="text" onChange={(e) => onChangeGroup(e.target.value)} />
-    </div>
     <h2 className="mt-3">{daoId}</h2>
     <div className="mt-3">
       <Widget
