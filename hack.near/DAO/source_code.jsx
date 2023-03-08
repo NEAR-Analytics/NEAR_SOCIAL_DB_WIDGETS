91018ab96cfe0ecd9571316f478f5ba1524158f0
NearSocial <NearSocial>
2023-03-08 00:14:15 -0500

    Update DAO by hack.near at 2023-03-08 00:14:15.871

--- hack.near/DAO/source_code.jsx
+++ hack.near/DAO/source_code.jsx
@@ -1,30 +1,46 @@
 const accountId = props.accountId ?? context.accountId;
+const daoId = props.daoId ?? "multi.sputnik-dao.near";
+const groupId = props.groupId ?? "council";
+const policy = Near.view(daoId, "get_policy");
 
-if (!accountId) {
-  return "Please connect your NEAR wallet :)";
-}
+State.init({
+  dao_id: daoId,
+  group_id: groupId,
+});
+
+const onChangeDAO = (dao_id) => {
+  State.update({
+    dao_id,
+  });
+};
+
+const onChangeGroup = (group_id) => {
+  State.update({
+    group_id,
+  });
+};
 
 return (
-  <div>
+  <div className="mb-3">
     <div className="mb-3">
-      <Widget src="hack.near/widget/AddMemberToRole" props={{ accountId }} />
+      DAO:
+      <input type="text" onChange={(e) => onChangeDAO(e.target.value)} />
     </div>
     <div className="mb-3">
+      Group:
+      <input type="text" onChange={(e) => onChangeGroup(e.target.value)} />
+    </div>
+    <h2 className="mt-3">{daoId}</h2>
+    <div className="mt-3">
       <Widget
-        src="hack.near/widget/RemoveMemberFromRole"
-        props={{ accountId }}
+        src="hack.near/widget/Groups"
+        props={{ accountId, daoId, groupId }}
       />
     </div>
-    <div className="mb-3">
-      <Widget src="hack.near/widget/TransferProposal" props={{ accountId }} />
-    </div>
-    <div className="mb-3">
-      <Widget src="hack.near/widget/CreatePoll" props={{ accountId }} />
-    </div>
-    <div className="mb-3">
+    <div>
       <Widget
-        src="hack.near/widget/FunctionCallProposal"
-        props={{ accountId }}
+        src="hack.near/widget/GroupMembers"
+        props={{ accountId, daoId, groupId }}
       />
     </div>
   </div>
