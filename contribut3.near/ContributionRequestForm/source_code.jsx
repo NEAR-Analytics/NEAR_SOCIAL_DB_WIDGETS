c5c00070542a165292dd88ce0d53e24acf161988
contribut3.near <contribut3.near>
2023-03-08 14:10:26 -0500

    Update ContributionRequestForm by contribut3.near at 2023-03-08 14:10:26.158

--- contribut3.near/ContributionRequestForm/source_code.jsx
+++ contribut3.near/ContributionRequestForm/source_code.jsx
@@ -152,18 +152,112 @@ const onConfirm = () => {
 
   Near.call(ownerId, "request_contribution", args);
 };
+//
+// return (
+//   <Widget
+//     src={`${ownerId}/widget/Modal`}
+//     props={{
+//       title: "Propose contribution",
+//       confirmText,
+//       onConfirm,
+//       hidden: props.hidden,
+//       onClose: props.onClose,
+//       body,
+//       id,
+//     }}
+//   />
+// );
+const Modal = styled.div`
+  display: ${({ hidden }) => (hidden ? "none" : "flex")};
+  position: fixed;
+  inset: 0;
+  justify-content: center;
+  align-items: center;
+  opacity: 1;
+  z-index: 1;
+`;
+
+const ModalBackdrop = styled.div`
+  position: absolute;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0.4;
+`;
+
+const ModalDialog = styled.div`
+  padding: 2em;
+  z-index: 3;
+  background-color: white;
+  border-radius: 4px;
+  width: 40%;
+`;
+
+const ModalHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+const ModalFooter = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+const CloseButton = styled.button`
+  background-color: white;
+  padding: 0.7em;
+  border-radius: 4px;
+  border: 0;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  color: #344054;
+`;
+
+const ConfirmButton = styled.button`
+  padding: 0.7em;
+  border-radius: 4px;
+  border: 0;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  background-color: #12b76a;
+  color: white;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #0e9f5d;
+  }
+`;
 
 return (
-  <Widget
-    src={`${ownerId}/widget/Modal`}
-    props={{
-      title: "Propose contribution",
-      confirmText,
-      onConfirm,
-      hidden: props.hidden,
-      onClose: props.onClose,
-      body,
-      id,
-    }}
-  />
+  <Modal hidden={props.hidden}>
+    <ModalBackdrop />
+    <ModalDialog>
+      <ModalHeader>
+        <h5>Propose contribution</h5>
+        <CloseButton onClick={props.onClose}>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15.5 5L5.5 15M5.5 5L15.5 15"
+              stroke="currentColor"
+              stroke-width="1.66667"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </CloseButton>
+      </ModalHeader>
+      <div>{body}</div>
+      <ModalFooter>
+        <CloseButton onClick={props.onClose}>Cancel</CloseButton>
+        <ConfirmButton onClick={props.onConfirm}>{confirmText}</ConfirmButton>
+      </ModalFooter>
+    </ModalDialog>
+  </Modal>
 );
