ba9c20d09644e327cec2cd6e3b215a4030ea0a8d
dev368.near <dev368.near>
2023-03-08 06:23:21 -0500

    Update hello by dev368.near at 2023-03-08 06:23:21.412

---
+++ dev368.near/hello/source_code.jsx
@@ -0,0 +1,18 @@
+return (
+  <div>
+    <h1> Hello! </h1>
+    <div>
+      <img
+        src="https://ipfs.near.social/ipfs/bafkreih6pjlzjpx4j23gz3l2dbi5eogk5r6ndfbbakkgwoi4hzf3zqpwo4"
+        width="200"
+        height="200"
+      ></img>
+    </div>
+
+    <a href="/#/mob.near/widget/ProfilePage?accountId=dev368.near">
+      I am Dev368
+    </a>
+    <div></div>
+    <a href="https://twitter.com/zens68">My Twitter</a>
+  </div>
+);
