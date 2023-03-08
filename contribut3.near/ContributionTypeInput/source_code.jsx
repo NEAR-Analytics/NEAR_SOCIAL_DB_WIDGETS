ce14c3a33bb4c5729e89b3a273eeb5c0dfb0d584
contribut3.near <contribut3.near>
2023-03-08 11:42:07 -0500

    Update ContributionTypeInput by contribut3.near at 2023-03-08 11:42:07.905

--- contribut3.near/ContributionTypeInput/source_code.jsx
+++ contribut3.near/ContributionTypeInput/source_code.jsx
@@ -4,13 +4,27 @@ const multiple = props.multiple ?? false;
 const update = props.update;
 const allContributionTypes = props.allContributionTypes ?? [];
 
+State.init({
+  options: [],
+  fetched: false,
+});
+
+if (!state.fetched) {
+  Near.asyncView(ownerId, "get_contribution_types", {}, "final", false).then(
+    (types) =>
+      State.update({
+        fetched: true,
+        options: types.map((name) => ({ name })),
+      })
+  );
+}
+
 if (!update) {
   return "Cannot render contribution type input without update function!";
 }
 
 const Label = styled.label`
   font-weight: 600;
-  font-size: 0.9em;
   color: #344054;
 `;
 
@@ -21,7 +35,7 @@ return (
       id="contribution-type"
       labelKey="name"
       onChange={update}
-      options={allContributionTypes}
+      options={state.options}
       placeholder="Development, Investment, Legal..."
       selected={contributionType}
       positionFixed
