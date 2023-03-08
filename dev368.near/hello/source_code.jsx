1379cb587e22f860171bfc586f55657b5bfaa14c
dev368.near <dev368.near>
2023-03-08 07:25:01 -0500

    Update hello by dev368.near at 2023-03-08 07:25:01.669

--- dev368.near/hello/source_code.jsx
+++ dev368.near/hello/source_code.jsx
@@ -1,18 +1,18 @@
+let linkAvt =
+  "https://ipfs.near.social/ipfs/bafkreih6pjlzjpx4j23gz3l2dbi5eogk5r6ndfbbakkgwoi4hzf3zqpwo4";
+
+let linkProfile = "/#/mob.near/widget/ProfilePage?accountId=dev368.near";
+let linkTwitter = "https://twitter.com/zens68";
+
 return (
   <div>
     <h1> Hello! </h1>
     <div>
-      <img
-        src="https://ipfs.near.social/ipfs/bafkreih6pjlzjpx4j23gz3l2dbi5eogk5r6ndfbbakkgwoi4hzf3zqpwo4"
-        width="200"
-        height="200"
-      ></img>
+      <img src={linkAvt} width="200" height="200"></img>
     </div>
 
-    <a href="/#/mob.near/widget/ProfilePage?accountId=dev368.near">
-      I am Dev368
-    </a>
+    <a href={linkProfile}>I am Dev368</a>
     <div></div>
-    <a href="https://twitter.com/zens68">My Twitter</a>
+    <a href={linkTwitter}>My Twitter</a>
   </div>
 );
