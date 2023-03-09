ad29bfb66e40d9f675c6a677eabf2d09190aa0f3
mohammadhs.near <mohammadhs.near>
2023-03-09 04:42:14 -0500

    Update NearGithubActivities by mohammadhs.near at 2023-03-09 04:42:14.004

---
+++ mohammadhs.near/NearGithubActivities/source_code.jsx
@@ -0,0 +1,20 @@
+let Style = styled.div``;
+
+return (
+  <Style>
+    <div class="row">
+      <div class="col-lg-12 mb-3">
+        <Widget
+          src="mohammadhs.near/widget/NearGithubActivityTotalOverall"
+          props={{}}
+        />{" "}
+      </div>
+      <div class="col-lg-12 mb-3">
+        <Widget
+          src="mohammadhs.near/widget/NearGithubActivityPullReqs"
+          props={{}}
+        />
+      </div>
+    </div>
+  </Style>
+);
