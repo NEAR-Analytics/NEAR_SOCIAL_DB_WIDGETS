8db02c8f744468cca9f6861f5e9455d988f53831
585ecc66a2926b3cce3c2bda2f6d78aef769afc50e954146bb1319b6dd705171 <585ecc66a2926b3cce3c2bda2f6d78aef769afc50e954146bb1319b6dd705171>
2023-03-09 00:50:16 -0500

    Update Flipsidecrypto.xyz by 585ecc66a2926b3cce3c2bda2f6d78aef769afc50e954146bb1319b6dd705171 at 2023-03-09 00:50:16.514

---
+++ 585ecc66a2926b3cce3c2bda2f6d78aef769afc50e954146bb1319b6dd705171/Flipsidecrypto.xyz/source_code.jsx
@@ -0,0 +1,21 @@
+return (
+  <div>
+    what is near? NEAR Protocol is a decentralized blockchain platform designed
+    to enable the creation and execution of decentralized applications (dApps).
+    It aims to provide a scalable and secure infrastructure for developers to
+    build and deploy decentralized applications on the blockchain. NEAR Protocol
+    uses a Proof-of-Stake (PoS) consensus mechanism, which allows for faster and
+    more efficient transaction processing compared to other blockchain
+    platforms. This is achieved through a unique sharding design, where
+    transactions are processed in parallel across multiple shards. One of the
+    key features of NEAR Protocol is its focus on developer experience, with a
+    user-friendly development environment, built-in smart contract languages,
+    and support for popular programming languages such as Rust and
+    AssemblyScript. NEAR also offers a range of developer tools, including its
+    own decentralized storage solution, which can be used to store and retrieve
+    data in a secure and decentralized manner. Overall, NEAR Protocol aims to
+    provide a blockchain platform that is accessible and user-friendly for
+    developers, while also offering a highly scalable and secure infrastructure
+    for the creation and execution of decentralized applications.
+  </div>
+);
