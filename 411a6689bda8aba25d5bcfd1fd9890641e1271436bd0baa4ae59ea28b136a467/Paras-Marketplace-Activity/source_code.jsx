32c4f9f97f0035cb51c742a91807de565c8d601b
kaiblade <kaiblade>
2023-03-09 01:39:35 -0500

    Update Paras-Marketplace-Activity by 411a6689bda8aba25d5bcfd1fd9890641e1271436bd0baa4ae59ea28b136a467 at 2023-03-09 01:39:35.477

--- 411a6689bda8aba25d5bcfd1fd9890641e1271436bd0baa4ae59ea28b136a467/Paras-Marketplace-Activity/source_code.jsx
+++ 411a6689bda8aba25d5bcfd1fd9890641e1271436bd0baa4ae59ea28b136a467/Paras-Marketplace-Activity/source_code.jsx
@@ -51,33 +51,173 @@ const linkStyle = {
   fontWeight: 500,
 };
 
+State.init({
+  imgSrc1: "",
+});
+
+State.init({
+  imgSrc2: "",
+});
+
+State.init({
+  imgSrc3: "",
+});
+
+const options = {
+  subscribe: true,
+  method: "GET",
+  headers: {
+    Accept: "*/*",
+  },
+};
+
 const apiUrl1 =
   "https://api.flipsidecrypto.com/api/v2/queries/fe213752-a832-4f0b-a08a-52e85ae53798/data/latest";
 
 const apiUrl2 =
   "https://api.flipsidecrypto.com/api/v2/queries/a371f03d-ccdf-4e58-81cb-1c1aaff87b27/data/latest";
 
-let parasActivity = fetch(apiUrl1, {
-  subscribe: true,
-  method: "GET",
-  headers: {
-    Accept: "*/*",
-  },
-});
+let parasActivity = fetch(apiUrl1, options);
 
-let parasMetrics = fetch(apiUrl2, {
-  subscribe: true,
-  method: "GET",
-  headers: {
-    Accept: "*/*",
-  },
-});
+let parasMetrics = fetch(apiUrl2, options);
 
 const pM = parasMetrics.body[0];
+
+let res1 = fetch(
+  "https://api.flipsidecrypto.com/api/v2/queries/10ee9d9f-489f-40d6-a2c4-2c474b659d03/data/latest",
+  options
+);
+
+let res2 = fetch(
+  "https://api.flipsidecrypto.com/api/v2/queries/89568d53-1fe3-441a-b3e0-4ffd6f53afa9/data/latest",
+  options
+);
+
+let res3 = fetch(
+  "https://api.flipsidecrypto.com/api/v2/queries/5fc6a473-47ff-4d61-8ff9-ad57aa3bb1d9/data/latest",
+  options
+);
+
+let res4 = fetch(
+  "https://api.flipsidecrypto.com/api/v2/queries/c0fcca8f-f9bb-4eeb-b888-44363de1fc4e/data/latest",
+  options
+);
 
 function numberWithCommas(x) {
   return JSON.stringify(x).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+function chartMaker(response, imgsrc, type) {
+  let respKeys = Object.keys(response.body[0]);
+  let queryResults = Object.entries(response.body);
+  const chartType = `${JSON.stringify(type)}`;
+  const chartTitle = `"Total Gross Sales in US$"`;
+
+  let chartBottomAxisLabels = "[";
+  let chartValues = "[";
+
+  if (queryResults !== "") {
+    queryResults.map(([k, v], i) => {
+      if (i !== 0) {
+        chartBottomAxisLabels += ",";
+        chartValues += ",";
+      } else if (type === "donut") {
+        chartValues += `{label: '${chartTitle}', data:[`;
+      } else if (type === "bar") {
+        chartValues += `{label: '${chartTitle}',backgroundColor:'rgba(54, 162, 235, 0.5)',borderColor:'rgb(54, 162, 235)',borderWidth:1,data:[`;
+      } else if (type === "line") {
+        chartValues += `{label: '${chartTitle}', pointRadius: 0, fill: false, backgroundColor:'rgba(54, 162, 235, 0.5)',borderColor:'rgb(54, 162, 235)',borderWidth:1,data:[`;
+      }
+
+      chartBottomAxisLabels += "'";
+      chartBottomAxisLabels += v[respKeys[0]];
+      chartBottomAxisLabels += "'";
+
+      chartValues += "'";
+      chartValues += v[respKeys[1]];
+      chartValues += "'";
+    });
+
+    chartValues += "]}]";
+    chartBottomAxisLabels += "]";
+
+    const imgTemplate = `https://quickchart.io/chart?width=265&height=180&chart=
+    {type:${chartType},data:{labels:${chartBottomAxisLabels}, datasets:${chartValues}},
+    options:
+    {plugins:
+    {datalabels:{
+    display:true,
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    font:{size:8,style:'bold',},
+    borderRadius:10,
+      },
+      },
+      legend:{labels:{fontSize:5,fontStyle:'bold'},position:'right'}
+      }
+      }`;
+
+    const imgTemplate1 = `https://quickchart.io/chart?width=270&height=220&chart=
+    {type:${chartType},data:{labels:${chartBottomAxisLabels}, datasets:${chartValues}},
+    options:{
+    scales:{
+    xAxes:[{gridLines:{display: false,},scaleLabel:{fontFamily:'roboto', display:true,fontSize:7,fontStyle:'bold',labelString:'Days'},ticks: {fontFamily: 'roboto',fontStyle: 'bold',fontSize:6}}],
+    yAxes:[{gridLines:{display: false,}, scaleLabel:{fontFamily:'roboto',display:true,fontSize:7,fontStyle:'bold',labelString: 'Total Gross Sales'},ticks: {fontFamily: 'roboto',fontStyle: 'bold',fontSize:6}}]
+    },plugins:
+    { tickFormat: {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }, "roundedBars": true, datalabels:{
+    display:false,
+    font:{size:2,style:'bold',},
+    borderRadius:10,
+      },
+      },
+      legend:{labels:{fontSize:5,fontStyle:'bold'},position:'right',display:false,}
+      }
+      }`;
+
+    const imgTemplate2 = `https://quickchart.io/chart?width=270&height=220&chart=
+    {type:${chartType},data:{labels:${chartBottomAxisLabels}, datasets:${chartValues}},
+    options:{
+    scales:{
+    xAxes:[{gridLines:{display: false,},scaleLabel:{fontFamily:'roboto', display:true,fontSize:7,fontStyle:'bold',labelString:'Days'},ticks: {fontFamily: 'roboto',fontStyle: 'bold',fontSize:6}}],
+    yAxes:[{gridLines:{display: false,}, scaleLabel:{fontFamily:'roboto',display:true,fontSize:7,fontStyle:'bold',labelString: 'Cumulative Number of Sales'},ticks: {fontFamily: 'roboto',fontStyle: 'bold',fontSize:6}}]
+    },plugins:
+    { datalabels:{
+    display:false,
+    font:{size:2,style:'bold',},
+    borderRadius:10,
+      },
+      },
+      legend:{labels:{fontSize:5,fontStyle:'bold'},position:'right',display:false,}
+      }
+      }`;
+
+    if (imgsrc === "imgSrc1") {
+      State.update({
+        imgSrc1: imgTemplate,
+      });
+    } else if (imgsrc === "imgSrc2") {
+      State.update({
+        imgSrc2: imgTemplate,
+      });
+    } else if (imgsrc === "imgSrc3") {
+      State.update({
+        imgSrc3: imgTemplate1,
+      });
+    } else if (imgsrc === "imgSrc4") {
+      State.update({
+        imgSrc4: imgTemplate2,
+      });
+    }
+  }
+}
+
+chartMaker(res1, "imgSrc1", "donut");
+chartMaker(res2, "imgSrc2", "donut");
+chartMaker(res3, "imgSrc3", "bar");
+chartMaker(res4, "imgSrc4", "line");
 
 let allRows = [];
 for (let i = 0; i < parasActivity.body.length; i++) {
@@ -157,7 +297,7 @@ return (
       </div>
     </div>
 
-    <div class="row" style={{ marginTop: "50px" }}>
+    <div class="row" style={{ marginTop: "20px" }}>
       <div
         class="card text-white bg-success mb-3"
         style={{ maxWidth: "14rem", marginRight: "10px" }}
@@ -259,6 +399,34 @@ return (
             %
           </p>
         </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <a href="https://flipsidecrypto.xyz/edit/queries/10ee9d9f-489f-40d6-a2c4-2c474b659d03">
+          <h6>Top 10 Highest Selling Collections (Last 7 Days)</h6>
+        </a>
+        <img src={state.imgSrc1} />
+      </div>
+      <div class="col">
+        <a href="https://flipsidecrypto.xyz/edit/queries/89568d53-1fe3-441a-b3e0-4ffd6f53afa9">
+          <h6>Top 10 Sellers Based on Gross Sales in US$ (Last 7 Days)</h6>
+        </a>
+        <img src={state.imgSrc2} />
+      </div>
+    </div>
+    <div class="row" style={{ marginTop: "20px" }}>
+      <div class="col">
+        <a href="https://flipsidecrypto.xyz/edit/queries/5fc6a473-47ff-4d61-8ff9-ad57aa3bb1d9">
+          <h6>Total Gross Sales in US$ Per Day (Last 30 Days)</h6>
+        </a>
+        <img src={state.imgSrc3} />
+      </div>
+      <div class="col">
+        <a href="https://flipsidecrypto.xyz/edit/queries/c0fcca8f-f9bb-4eeb-b888-44363de1fc4e">
+          <h6>Cumulative Number of Sales (Last 30 Days)</h6>
+        </a>
+        <img src={state.imgSrc4} />
       </div>
     </div>
   </div>
