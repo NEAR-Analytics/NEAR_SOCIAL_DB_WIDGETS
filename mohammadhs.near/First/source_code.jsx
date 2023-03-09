c7890758be869c6a6a45a75e5e957db109358ad1
mohammadhs.near <mohammadhs.near>
2023-03-08 22:00:35 -0500

    Update First by mohammadhs.near at 2023-03-08 22:00:35.809

---
+++ mohammadhs.near/First/source_code.jsx
@@ -0,0 +1,17 @@
+let Style = styled.div``;
+
+return (
+  <Style>
+    <div class="row">
+      <div class="col">
+        <Widget src="y3k.near/widget/MonthlyActiveAccounts" props={{}} />
+      </div>
+      <div class="col">
+        <Widget
+          src="davidweinstein.near/widget/MonthlyActiveAccounts"
+          props={{}}
+        />{" "}
+      </div>
+    </div>
+  </Style>
+);
