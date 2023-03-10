f5e0731f3e13321b0f9501ee2f1d1a8d23e1521d
Jikugodwill <Jikugodwill>
2023-03-10 09:13:17 -0500

    Update gdocs by e0b0efdc641a50e91e54b55d669362bf93e02b6ed5024da0e305c6afd9ea03ed at 2023-03-10 09:13:17.810

--- e0b0efdc641a50e91e54b55d669362bf93e02b6ed5024da0e305c6afd9ea03ed/gdocs/source_code.jsx
+++ e0b0efdc641a50e91e54b55d669362bf93e02b6ed5024da0e305c6afd9ea03ed/gdocs/source_code.jsx
@@ -6,14 +6,8 @@ const url = `https://docs.google.com/document/d/e/${props.docId}/pub?embedded=tr
 return (
   <>
     <iframe
-      className="container-fluid border border-primary rounded"
+      className="container-fluid vh-100 border border-primary rounded"
       src={url}
-      // width="100%"
-      height="100vh"
-      // style={{ position: "absolute", top: 0, left: 0 }}
-      //   frameBorder="0"
-      //   marginHeight="0"
-      //   marginWidth="0"
     />
   </>
 );
