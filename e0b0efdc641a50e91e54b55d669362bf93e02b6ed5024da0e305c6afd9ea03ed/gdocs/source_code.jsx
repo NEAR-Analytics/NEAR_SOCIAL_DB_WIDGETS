b2cfe90638016c5a3ccbec02fe329a2553f6bcae
Jikugodwill <Jikugodwill>
2023-03-10 05:38:38 -0500

    Update gdocs by e0b0efdc641a50e91e54b55d669362bf93e02b6ed5024da0e305c6afd9ea03ed at 2023-03-10 05:38:38.688

--- e0b0efdc641a50e91e54b55d669362bf93e02b6ed5024da0e305c6afd9ea03ed/gdocs/source_code.jsx
+++ e0b0efdc641a50e91e54b55d669362bf93e02b6ed5024da0e305c6afd9ea03ed/gdocs/source_code.jsx
@@ -2,17 +2,19 @@ if (!props.docId) {
   return <div className="">No document ID provided.</div>;
 }
 
-const url = `https://docs.google.com/document/d/${props.docId}/preview`;
+const url = `https://docs.google.com/document/d/e/${props.docId}/pub?embedded=true"`;
 return (
-  <div className="container min-vw-100">
-    <iframe
-      src={url}
-      width="100%"
-      height="100%"
-      style={{ position: "absolute", top: 0, left: 0 }}
-      frameBorder="0"
-      marginHeight="0"
-      marginWidth="0"
-    />
-  </div>
+  <>
+    <div className="container min-vw-100">
+      <iframe
+        src={url}
+        width="100%"
+        height="100%"
+        style={{ position: "absolute", top: 0, left: 0 }}
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+      />
+    </div>
+  </>
 );
