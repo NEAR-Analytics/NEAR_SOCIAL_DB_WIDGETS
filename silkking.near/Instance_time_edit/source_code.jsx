9f628bf19d201ed4038180514a9b3fb6625baf90
silkking.near <silkking.near>
2023-03-10 14:05:51 -0500

    Update Instance_time_edit by silkking.near at 2023-03-10 14:05:51.251

--- silkking.near/Instance_time_edit/source_code.jsx
+++ silkking.near/Instance_time_edit/source_code.jsx
@@ -6,8 +6,7 @@ const tabs = props.tabs;
 var sortedData = data.sort((d1, d2) => d1.blockHeight - d2.blockHeight);
 var finalData = {};
 
-const widgetOwner =
-  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+const widgetOwner = "silkking.near";
 
 const sortAndRemoveRepeated = (flag, data) => {
   var temp = data;
