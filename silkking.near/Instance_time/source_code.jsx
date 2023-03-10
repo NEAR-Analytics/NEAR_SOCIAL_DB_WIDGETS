3f5b90195dcf4495a337426e9de89888e7ebf051
silkking.near <silkking.near>
2023-03-10 14:05:41 -0500

    Update Instance_time by silkking.near at 2023-03-10 14:05:41.987

--- silkking.near/Instance_time/source_code.jsx
+++ silkking.near/Instance_time/source_code.jsx
@@ -8,8 +8,7 @@ if (!profile) {
   return "Loading profile";
 }
 
-const widgetOwner =
-  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+const widgetOwner = "silkking.near";
 
 var sortedData = data.sort((d1, d2) => d2.blockHeight - d1.blockHeight);
 var accountIds = ["All"];
