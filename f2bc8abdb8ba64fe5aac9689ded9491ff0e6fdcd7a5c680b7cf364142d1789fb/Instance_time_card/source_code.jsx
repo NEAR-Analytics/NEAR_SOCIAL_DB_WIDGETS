87ec34225150eb48829f47f2658c03ca719fdc8f
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-03-11 12:59:56 -0500

    Update Instance_time_card by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-03-11 12:59:56.987

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Instance_time_card/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Instance_time_card/source_code.jsx
@@ -60,7 +60,6 @@ const getFormatedTime = (time) => {
 
 var date = new Date();
 var utc_offset = -date.getTimezoneOffset() / 60;
-console.log("1");
 for (let i = 0; i < sortedData.length; i++) {
   if (sortedData[i].accountId == accountId) {
     var times = sortedData[i].value._data;
@@ -115,7 +114,6 @@ for (let i = 0; i < sortedData.length; i++) {
     };
   }
 }
-console.log("2");
 
 function sliceString(string, newStringLength) {
   if (string.length > newStringLength) {
