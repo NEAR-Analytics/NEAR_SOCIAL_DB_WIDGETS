f39515b44399e27f52de2d3711d90a6ae7de68cb
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-03-11 13:42:18 -0500

    Update Instance_time_setting by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-03-11 13:42:18.888

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Instance_time_setting/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Instance_time_setting/source_code.jsx
@@ -4,7 +4,8 @@ const tabs = props.tabs;
 const sortAndRemoveRepeated = (flag, data) => {
   var temp = data;
   if (flag) temp.push(0, 168);
-  var sortedTimeData = temp.sort((d2, d1) => d2 - d1);
+  var sortedTimeData =
+    temp && temp.length ? temp.sort((d2, d1) => d2 - d1) : [];
 
   var final = [];
   for (var k = 0; k < sortedTimeData.length; k++) {
