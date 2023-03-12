76f6faf1885a85a542bc9e0fcf00f22b95a7aa9a
ChaoticTempest <ChaoticTempest>
2023-03-11 20:20:31 -0500

    Update Test2 by chaotictempest.near at 2023-03-11 20:20:31.181

---
+++ chaotictempest.near/Test2/source_code.jsx
@@ -0,0 +1,43 @@
+const accountId = props.accountId;
+const profile = props.profile || Social.get(`${accountId}/profile/**`, "final");
+const profileUrl = `/#/calebjacob.near/widget/ProfilePage?accountId=${accountId}`;
+
+const Avatar = styled.a`
+  width: 60px;
+  height: 60px;
+  flex-shrink: 0;
+  border: 1px solid #ECEEF0;
+  overflow: hidden;
+  border-radius: 56px;
+  transition: border-color 200ms;
+
+  img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover,
+  &:focus {
+    border-color: #D0D5DD;
+  }
+`;
+
+return (
+  <Avatar
+    href={profileUrl}
+    onClick={(event) => {
+      console.log("event", event);
+    }}
+  >
+    <Widget
+      src="mob.near/widget/Image"
+      props={{
+        image: profile.image,
+        alt: profile.name,
+        fallbackUrl:
+          "https://ipfs.near.social/ipfs/bafkreibiyqabm3kl24gcb2oegb7pmwdi6wwrpui62iwb44l7uomnn3lhbi",
+      }}
+    />
+  </Avatar>
+);
