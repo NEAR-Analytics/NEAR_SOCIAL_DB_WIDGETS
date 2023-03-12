4f27be3a5bc543fa3f784da794b68e212bfdb857
elliotBraem <elliotBraem>
2023-03-11 21:41:01 -0500

    Update Thing by efiz.near at 2023-03-11 21:41:01.145

--- efiz.near/Thing/source_code.jsx
+++ efiz.near/Thing/source_code.jsx
@@ -1,36 +1,10 @@
-const accountId = props.accountId || context.accountId;
-
-const index = {
-  action: "thing",
-  key: "main",
-  options: {
-    limit: 10,
-    order: "desc",
-    accountId: accountId,
-  },
-};
-
-const renderItem = (a) => {
-  const blockHeight = parseInt(a.blockHeight);
-  const content = JSON.parse(
-    Social.get(`${accountId}/thing/main`, blockHeight) ?? "null"
-  );
-
-  return (
-    // We can check the commit to make sure this is what we want
-    a.value.type === "thing" && (
-      <div key={JSON.stringify(a)} className="mb-3">
-        {content.color}
-      </div>
-    )
-  );
-};
+const accountId = props.accountId;
+const blockHeight =
+  props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
+const content =
+  props.content ??
+  JSON.parse(Social.get(`${accountId}/thing/main`, blockHeight) ?? "null");
 
 return (
-  <div>
-    <Widget
-      src="mob.near/widget/FilteredIndexFeed"
-      props={{ index, renderItem }}
-    />
-  </div>
+  <div className="border rounded-4 p-3 pb-1">{JSON.stringify(content)}</div>
 );
