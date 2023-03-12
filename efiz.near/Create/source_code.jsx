7483e9c2f839561942b924724d8a51aebf5ce57d
elliotBraem <elliotBraem>
2023-03-11 19:01:08 -0500

    Update Create by efiz.near at 2023-03-11 19:01:08.227

--- efiz.near/Create/source_code.jsx
+++ efiz.near/Create/source_code.jsx
@@ -1,36 +1,109 @@
-const accountId = context.accountId;
+/*
+---props---
 
-if (!accountId) {
-  return "Please sign in with NEAR wallet to add a new thing";
-}
+props.widgetPath?: string,
 
-initState({
-  thing: "",
+*/
+
+// How can I access the widget path dynamically?
+const initWidgetPath = "efiz.near/widget/Create";
+
+State.init({
+  widgetPath: initWidgetPath,
+  color: "",
+  brand: "",
+  size: "",
+  material: "",
 });
 
-const entry = {
-  thing: state.thing,
-};
+// START GET THE WIDGET COMMIT
+const historyBlocksRequest = Social.keys(`${initWidgetPath}`, "final", {
+  return_type: "History",
+});
 
-const handleChange = (event) => {
-  State.update({ content: event.target.value });
+if (historyBlocksRequest === null) return "loading...";
+
+const [widgetAccountId, _, widgetName] = initWidgetPath.split("/");
+
+let blocksChanges =
+  historyBlocksRequest[widgetAccountId]?.["widget"]?.[widgetName];
+
+// Latest commit sits at blocksChanges[0]
+// How do we know this is the one they are using though?
+// END GET THE WIDGET COMMIT
+
+const composeData = () => {
+  const data = {
+    thing: {
+      main: JSON.stringify({
+        color: state.color,
+        brand: state.brand,
+        size: state.size,
+        material: state.material,
+        commit: blocksChanges[0]
+      }),
+    },
+    index: {
+      thing: JSON.stringify({
+        key: "main",
+        value: {
+          type: "thing",
+        },
+      }),
+    },
+  };
+
+  return data;
 };
 
 return (
-  <div className="row mb-3">
-    <div>
-      <h4>Thing</h4>
-    </div>
-    <input
-      className="mb-2"
-      value={state.thing}
-      onChange={({ target }) => State.update({ thing: target.value })}
-    />
+  <div>
+    <h1 class="text-center">Thing</h1>
 
+    <div class="input-group mb-3">
+      <input
+        class="form-control"
+        placeholder={"color"}
+        onChange={({ target }) => State.update({ color: target.value })}
+      />
+      <input
+        class="form-control"
+        placeholder={"brand"}
+        onChange={({ target }) => State.update({ brand: target.value })}
+      />
+      <input
+        class="form-control"
+        placeholder={"size"}
+        onChange={({ target }) => State.update({ size: target.value })}
+      />
+      <input
+        class="form-control"
+        placeholder={"material"}
+        onChange={({ target }) => State.update({ material: target.value })}
+      />
+    </div>
+    <div>
+      <div className={"list-group-item list-group-item-action "}>
+        #{blocksChanges[0]}
+      </div>
+      <div className={"list-group-item list-group-item-action "}>
+        {state.color}
+      </div>
+      <div className={"list-group-item list-group-item-action "}>
+        {state.brand}
+      </div>
+      <div className={"list-group-item list-group-item-action "}>
+        {state.size}
+      </div>
+      <div className={"list-group-item list-group-item-action "}>
+        {state.material}
+      </div>
+    </div>
     <CommitButton
-      data={{
-        thing: entry.thing,
-      }}
+      disabled={false}
+      force
+      className="btn btn-dark rounded-3"
+      data={composeData}
     >
       Create
     </CommitButton>
