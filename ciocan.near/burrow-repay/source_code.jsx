2d518698aa8da0f6ed4ed164e5265d7df5f0239e
ciocan <ciocan>
2023-03-13 15:46:19 -0400

    Update burrow-repay by ciocan.near at 2023-03-13 15:46:19.284

---
+++ ciocan.near/burrow-repay/source_code.jsx
@@ -0,0 +1,138 @@
+let BURROW_CONTRACT = "contract.main.burrow.near";
+let accountId = context.accountId;
+
+const { selectedTokenId, amount, hasError, assets, rewards, account } = state;
+
+const hasData = assets.length > 0 && rewards.length > 0;
+
+const onLoad = (data) => {
+  State.update(data);
+};
+
+const shrinkToken = (value, decimals) => {
+  return new Big(value).div(new Big(10).pow(decimals));
+};
+
+const expandToken = (value, decimals) => {
+  return new Big(value).mul(new Big(10).pow(decimals));
+};
+
+const formatToken = (v) => Math.floor(v * 10_000) / 10_000;
+const toAPY = (v) => Math.round(v * 100) / 100;
+
+if (!accountId) {
+  return <div>Please sign in with NEAR wallet</div>;
+}
+
+const listAssets =
+  assets &&
+  assets
+    ?.filter((a) => a.accountBalance > 0)
+    ?.map((asset) => {
+      const { token_id, metadata } = asset;
+      if (account.borrowed.map((a) => a.token_id).includes(token_id)) {
+        return (
+          <option key={token_id} value={token_id}>
+            {metadata.symbol}
+          </option>
+        );
+      }
+    });
+
+let availableBalance = 0;
+let apy = 0;
+
+const getBalance = (asset) => {
+  if (!asset) return 0;
+  const { token_id, accountBalance, metadata } = asset;
+  return formatToken(
+    shrinkToken(accountBalance, metadata.decimals).toFixed()
+  ).toString();
+};
+
+const getApy = (asset) => {
+  if (!asset) return 0;
+  const r = rewards.find((a) => a.token_id === asset.token_id);
+  return toAPY(r.apyBaseBorrow);
+};
+
+if (selectedTokenId) {
+  const token = selectedTokenId === "NEAR" ? "wrap.near" : selectedTokenId;
+  const asset = assets.find((a) => a.token_id === token);
+  availableBalance =
+    selectedTokenId === "NEAR" ? nearBalance : getBalance(asset);
+  apy = getApy(asset);
+}
+
+const storageBurrow = Near.view(BURROW_CONTRACT, "storage_balance_of", {
+  account_id: accountId,
+});
+
+const storageToken = selectedTokenId
+  ? Near.view(selectedTokenId, "storage_balance_of", {
+      account_id: accountId,
+    })
+  : null;
+
+const handleSelect = (e) => {
+  State.update({
+    selectedTokenId: e.target.value,
+    amount: "",
+    hasError: false,
+  });
+};
+
+const handleAmount = (e) => {
+  State.update({
+    amount: Number(e.target.value),
+    selectedTokenId,
+    hasError: false,
+  });
+};
+
+const handleRepay = () => {
+  console.log("handleRepay", account);
+  if (!selectedTokenId || !amount || hasError) return;
+};
+
+return (
+  <div style={{ maxWidth: "300px" }}>
+    {!hasData && (
+      <Widget src="ciocan.near/widget/burrow-data" props={{ onLoad }} />
+    )}
+    <div class="card-body d-grid gap-3">
+      <div>
+        <div class="mb-2 text-muted">From</div>
+        <select
+          onChange={handleSelect}
+          class="p-2 mb-1"
+          style={{ width: "100%" }}
+        >
+          <option value="">Repay an asset</option>
+          {listAssets}
+        </select>
+        <div>
+          <span class="badge bg-light text-dark">
+            {availableBalance} available
+          </span>
+          <span class="badge bg-light text-dark">{apy}% Borrow APY</span>
+        </div>
+      </div>
+      <div>
+        <div class="mb-2 text-muted">Amount</div>
+        <input type="number" value={amount} onChange={handleAmount} />
+      </div>
+      {hasError && (
+        <p class="alert alert-danger" role="alert">
+          Amount greater than balance
+        </p>
+      )}
+      <button
+        onClick={handleRepay}
+        style={{ background: "#4ED58A", borderColor: "white" }}
+      >
+        Repay
+      </button>
+    </div>
+  </div>
+);
