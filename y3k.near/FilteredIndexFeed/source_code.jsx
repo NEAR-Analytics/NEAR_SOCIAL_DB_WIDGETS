ed3016a738c3e82e2fd79a0e11b39132909ad845
0bserver07 <0bserver07>
2023-03-14 15:29:56 -0400

    Update FilteredIndexFeed by y3k.near at 2023-03-14 15:29:56.954

--- y3k.near/FilteredIndexFeed/source_code.jsx
+++ y3k.near/FilteredIndexFeed/source_code.jsx
@@ -2,4 +2,4 @@ const filter = context.accountId && {
   ignore: Social.getr(`${context.accountId}/graph/hide`),
 };
 
-return <Widget src="mob.near/widget/IndexFeed" props={{ filter, ...props }} />;
+return <Widget src="y3k.near/widget/IndexFeed" props={{ filter, ...props }} />;
