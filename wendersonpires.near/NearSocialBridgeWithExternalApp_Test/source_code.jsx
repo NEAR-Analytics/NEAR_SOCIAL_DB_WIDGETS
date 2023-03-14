cc827424534c1bc15e2e6423c7a53f6400913516
wpdas <wpdas>
2023-03-13 23:12:20 -0400

    Update NearSocialBridgeWithExternalApp_Test by wendersonpires.near at 2023-03-13 23:12:20.587

--- wendersonpires.near/NearSocialBridgeWithExternalApp_Test/source_code.jsx
+++ wendersonpires.near/NearSocialBridgeWithExternalApp_Test/source_code.jsx
@@ -5,13 +5,58 @@ State.init({
 const accountId = props.accountId ?? context.accountId ?? "*";
 const profileInfo = props.profile ?? Social.getr(`${accountId}/profile`);
 
+const code = `
+<div>Expression: <pre id="exp" /></div>
+<div>Results: <pre id="res" /></div>
+
+<script>
+    window.top.postMessage({type: "history-push-state", payload: { path: "?r=/home" }}, "*");
+    // window.history.pushState({}, "", "?r=/home");
+    // window.top.postMessage("loaded", "*");
+    // window.addEventListener("message", (event) => {
+    //     const data = event.data
+    //     document.getElementById("exp").innerHTML = JSON.stringify(data);
+    //     try {
+    //         const result = eval(data.exp);
+    //         document.getElementById("res").innerHTML = result;
+    //         event.source.postMessage(result, "*");
+    //     } catch (e) {
+    //         // ignore
+    //     }
+    // }, false);
+</script>
+`;
+
+console.log(props);
+
+const historyPushState = (newUrlState) => {
+  console.log(window.parent);
+};
+
+const processMessage = (message) => {
+  if (message.type === "history-push-state") {
+    historyPushState(message.payload.path);
+  }
+};
+
+return (
+  <div>
+    <iframe
+      className="w-50 border"
+      srcDoc={code}
+      message={{ exp: state.text || "" }}
+      onMessage={(res1) => processMessage(res1)}
+    />
+  </div>
+);
+
 return (
   <div>
     <iframe
       className="w-100"
       style={{ height: "600px" }}
       // My external app
-      src="https://satori-hq.github.io/near-social-bridge-temp"
+      src="https://satori-hq.github.io/near-social-bridge-temp/"
       // Data the Near Social View is going to send to my External App
       message={{
         accountId,
