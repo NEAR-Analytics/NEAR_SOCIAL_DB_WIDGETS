01675105c7dd14b4aa3eea01a0bf37e5623debc3
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-14 13:59:36 -0400

    Update Learning.Menu by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-14 13:59:36.109

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/Learning.Menu/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/Learning.Menu/source_code.jsx
@@ -1,10 +1,15 @@
-const { labels, widgets, mainApp } = props;
+const { labels, widgets, mainApp, disabled } = props;
+
+const disabledClass = disabled ? "disabled" : "";
 
 let elems = [];
 for (const i = 0; i < labels.length; i++) {
   elems.push(
     <>
-      <a href={`https://near.social/#/${mainApp}?currentWidget=${widgets[i]}`}>
+      <a
+        class={`btn ${disabledClass} border-0`}
+        href={`https://near.social/#/${mainApp}?currentWidget=${widgets[i]}`}
+      >
         <li
           class="list-group-item"
           onClick={() => {
