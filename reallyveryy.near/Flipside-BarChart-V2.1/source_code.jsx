7400890d2f452f94006bcd39105f28275e646134
reallyveryy.near <reallyveryy.near>
2023-03-15 13:01:17 -0400

    Update Flipside-BarChart-V2.1 by reallyveryy.near at 2023-03-15 13:01:17.320

--- reallyveryy.near/Flipside-BarChart-V2.1/source_code.jsx
+++ reallyveryy.near/Flipside-BarChart-V2.1/source_code.jsx
@@ -18,50 +18,52 @@ const options = {
   body: formBody,
 };
 
-asyncFetch("https://flipside.leslug.com/execute", options).then((res) => {
-  let queryResults = res.body;
+if (state.currentQuery != props.query) {
+  asyncFetch("https://flipside.leslug.com/execute", options).then((res) => {
+    let queryResults = res.body;
 
-  console.log("queryResults", queryResults);
-  const chartType = `"bar"`;
-  let chartBottomAxisLabels = "[";
-  let chartValues = "[";
+    console.log("queryResults", queryResults);
+    const chartType = `"bar"`;
+    let chartBottomAxisLabels = "[";
+    let chartValues = "[";
 
-  // parsing of data from flipside
-  if (queryResults !== 0) {
-    // parsing of labels
-    let currentRows = [];
-    queryResults.map((d, i) => {
-      if (currentRows.includes(d[rowValue])) {
-        return; // no duplicates
-      }
+    // parsing of data from flipside
+    if (queryResults !== 0) {
+      // parsing of labels
+      let currentRows = [];
+      queryResults.map((d, i) => {
+        if (currentRows.includes(d[rowValue])) {
+          return; // no duplicates
+        }
 
-      if (i !== 0) {
-        chartBottomAxisLabels += ",";
-        chartValues += ",";
-      } else {
-        chartValues += `{label: '${props.title}', data:[`;
-      }
+        if (i !== 0) {
+          chartBottomAxisLabels += ",";
+          chartValues += ",";
+        } else {
+          chartValues += `{label: '${props.title}', data:[`;
+        }
 
-      currentRows.push(d[rowValue]);
-      chartBottomAxisLabels += "'";
-      chartBottomAxisLabels += d[rowValue];
-      chartBottomAxisLabels += "'";
+        currentRows.push(d[rowValue]);
+        chartBottomAxisLabels += "'";
+        chartBottomAxisLabels += d[rowValue];
+        chartBottomAxisLabels += "'";
 
-      chartValues += "'";
-      chartValues += d[columnValue];
-      chartValues += "'";
-    });
+        chartValues += "'";
+        chartValues += d[columnValue];
+        chartValues += "'";
+      });
 
-    chartValues += "]}]";
-    chartBottomAxisLabels += "]";
-    const imgTemplate = `https://quickchart.io/chart?width=${props.chartWidth}&height=${props.chartHeight}&chart={type:${chartType},data:{labels:${chartBottomAxisLabels}, datasets:${chartValues}}}`;
-    State.update({
-      imgSrc: imgTemplate,
-      query: props.query,
-    });
-  }
-});
-
+      chartValues += "]}]";
+      chartBottomAxisLabels += "]";
+      const imgTemplate = `https://quickchart.io/chart?width=${props.chartWidth}&height=${props.chartHeight}&chart={type:${chartType},data:{labels:${chartBottomAxisLabels}, datasets:${chartValues}}}`;
+      State.update({
+        imgSrc: imgTemplate,
+        query: props.query,
+      });
+    }
+  });
+}
+console.log(state.currentQuery, props.query);
 return (
   <>
     {state.currentQuery != props.query ? (
