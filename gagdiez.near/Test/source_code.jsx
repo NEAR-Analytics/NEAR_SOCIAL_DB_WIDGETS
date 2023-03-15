d91bbfac8ffc0fc30f4c60925f7f9d33cc4e17a4
gagdiez.near <gagdiez.near>
2023-03-15 11:13:18 -0400

    Update Test by gagdiez.near at 2023-03-15 11:13:18.531

---
+++ gagdiez.near/Test/source_code.jsx
@@ -0,0 +1,14 @@
+// const redirectMap = {
+//   "mob.near/widget/ProfileCircle": {code: "return <h2>🤖🤖🤖🤖🤖 OVERRIDE</h2>"},
+//   "mob.near/widget/ProfileImage": {code: "return <h2>🤖🤖🤖🤖🤖 OVERRIDE</h2>"},
+// };
+
+return (
+  <>
+    <h2> Profile Circle </h2>
+    <Widget src="mob.near/widget/ProfileCircle" />
+
+    <h2> Profile Image </h2>
+    <Widget src="mob.near/widget/ProfileImage" />
+  </>
+);
