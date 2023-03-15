3d78e57094a5cc2eebae7e0e5250723ca3b9a8bd
AZbang <AZbang>
2023-03-15 08:07:57 -0400

    Update StakeHERE by azbang.near at 2023-03-15 08:07:57.438

---
+++ azbang.near/StakeHERE/source_code.jsx
@@ -0,0 +1,93 @@
+State.init({ link: null, parent: "" });
+
+const Page = styled.div`
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0px;
+    left: 0;
+    bottom: 0px;
+    background: rgba(244, 235, 234, 0.56);
+    justify-content: center;
+    align-items: center;
+    display: flex;
+`;
+
+const BlurScreen = styled.div`
+    position: absolute;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.3);
+    backdrop-filter: blur(10px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+if (context.accountId == null) {
+  return (
+    <Page>
+      <h2 style={{ width: "100%", textAlign: "center" }}>Sign in please</h2>
+    </Page>
+  );
+}
+
+console.log(state);
+return (
+  <Page
+    style={{
+      paddingTop: 68,
+      paddingBottom: state.parent.includes("alpha.near.org") ? 68 : 0,
+    }}
+  >
+    <iframe
+      style={{ width: "100%", maxWidth: 460, height: "100%" }}
+      src="https://my.herewallet.app?stake"
+      message={{
+        accountId: context.accountId,
+        localStorage: Storage.get("localStorage"),
+      }}
+      onMessage={(e) => {
+        switch (e.action) {
+          case "saveLocalStorage":
+            Storage.set("localStorage", e.data);
+            return;
+
+          case "openLink":
+            State.update({ link: e.data.link });
+            return;
+
+          case "parentDomain":
+            State.update({ parent: e.data });
+            return;
+
+          case "signAndSendTransaction":
+            const call = e.data.actions[0].params;
+            Near.call(
+              e.data.receiverId,
+              call.methodName,
+              call.args,
+              call.gas,
+              call.deposit
+            );
+            return;
+        }
+      }}
+    />
+
+    {state.link && (
+      <BlurScreen>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={state.link}
+          style={{ fontSize: 24 }}
+          onClick={() => State.update({ link: null })}
+        >
+          Open link
+        </a>
+      </BlurScreen>
+    )}
+  </Page>
+);
