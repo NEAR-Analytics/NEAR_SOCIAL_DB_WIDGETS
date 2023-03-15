6158d5e77ebe70048aa779659fe0a206bd6fc2c5
antonyip <antonyip>
2023-03-15 11:07:22 -0400

    Update NEAR-Segmentation-Tool-For-MDAO by 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422 at 2023-03-15 11:07:22.704

--- 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422/NEAR-Segmentation-Tool-For-MDAO/source_code.jsx
+++ 0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422/NEAR-Segmentation-Tool-For-MDAO/source_code.jsx
@@ -55,20 +55,147 @@ select
 from
   final
 `,
-  title: "Monthly fact_profile_changes",
+  title: "Near Social - New Authorizers Over Time",
+  chartWidth: 640,
+  chartHeight: 200,
+};
+
+let ageChartProps = {
+  query: `with
+  social_inits as (
+    select
+      *
+    from
+      near.core.fact_actions_events_addkey
+    where
+      receiver_id = 'social.near'
+  ),
+  add_signer as (
+    select
+      k.tx_hash,
+      k.block_id,
+      k.block_timestamp,
+      t.tx_signer,
+      k.allowance,
+      t.tx_signer = t.tx_receiver
+    from
+      social_inits k
+      left join near.core.fact_transactions t using (tx_hash)
+  ),
+  de_dupe_resigners as (
+    select
+      block_timestamp,
+      tx_signer
+    from
+      add_signer
+    qualify
+      ROW_NUMBER() over (
+        PARTITION BY
+          tx_signer
+        ORDER BY
+          block_timestamp ASC
+      ) = 1
+  ),
+  interested_addresses as (
+    select
+      tx_signer
+    from
+      de_dupe_resigners
+    WHERE
+      block_timestamp BETWEEN '2022-01-01' and '2022-06-01'
+  ),
+  first_tx_table as (
+    select
+      min(t.block_timestamp) as first_tx,
+      ia.tx_signer
+    from
+      interested_addresses ia
+      left join near.core.fact_transactions t using (tx_signer)
+    group by
+      2
+  ),
+  date_diff_table as (
+    select
+      DATEDIFF('day', first_tx, current_date()) as wallet_age_day,
+      tx_signer
+    from
+      first_tx_table
+  ),
+  final as (
+    select
+      COUNT_IF(wallet_age_day > 7) as week_old_wallets,
+      COUNT_IF(wallet_age_day > 30) as month_old_wallets,
+      COUNT_IF(wallet_age_day > 365) as year_old_wallets,
+      COUNT_IF(1 = 1) as total_wallets
+    from
+      date_diff_table
+  ),
+  flip as (
+    select
+      *
+    from
+      final unpivot (
+        my_count for wallet_age in (
+          week_old_wallets,
+          month_old_wallets,
+          year_old_wallets,
+          total_wallets
+        )
+      )
+  )
+select
+  *
+from
+  flip`,
+  title: "Age of Wallets",
   chartWidth: 640,
   chartHeight: 200,
 };
 
 // bypass because server cannot handle newline..
 chart4Props.query = chart4Props.query.replaceAll("\n", " ");
+ageChartProps.query = ageChartProps.query.replaceAll("\n", " ");
+
+console.log(ageChartProps.query);
+/*
+- age of wallets
+- Staking history, average amount staked, average number of times staking
+- DEX swaps
+- What % of users bridged assets into NEAR vs using a centralized exchange, which bridges & centralized exchanges are most popular.
+- NEAR Social Activity
+*/
 
 return (
   <div>
-    <h2>Widgets Created by Anton using Flipside's Database.</h2>
+    <h2>NEAR - Segmentation Tool</h2>
+    <div>
+      Bounty Question: Create a segmentation tool to help us understand the NEAR
+      profiles of new users on Near.social. The intention is to allow users to
+      select a new group of Near users based on when they joined then look at
+      that group of users wallet behavior
+    </div>
     <Widget
       src="0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422/widget/Flipside-BarChart-V2"
       props={chart4Props}
     />
+    <div>Date Start</div>
+    <div>Date End</div>
+    <div>Go</div>
+    <div>
+      <h4>Age of Wallets</h4>
+      <Widget
+        src="0e7a82d0ef92b5559ef04df11f5de68ac4c4479319da5a72b3e2799c4717a422/widget/Flipside-BarChart-V2"
+        props={ageChartProps}
+      />
+    </div>
+    <div>Staking History</div>
+    <div>Average Amount Staked</div>
+    <div>Average Number of Times Staked</div>
+    <div>
+      What % of users bridged assets into NEAR vs using a centralized exchange,
+      which bridges & centralized exchanges are most popular.
+    </div>
+    <div>NEAR Social Activity</div>
+    <div>DEX Swaps</div>
   </div>
 );
