58ed411cf907cfaa084e0a80b53407972ef113e9
reallyveryy.near <reallyveryy.near>
2023-03-15 11:04:03 -0400

    Update Flipside-BarChart-V2.1 by reallyveryy.near at 2023-03-15 11:04:03.109

--- reallyveryy.near/Flipside-BarChart-V2.1/source_code.jsx
+++ reallyveryy.near/Flipside-BarChart-V2.1/source_code.jsx
@@ -22,6 +22,36 @@ asyncFetch("https://flipside.leslug.com/execute", options).then((res) => {
   let queryResults = res.body;
 
   console.log("queryResults", queryResults);
+  const chartType = `"bar"`;
+  let chartBottomAxisLabels = "[";
+  let chartValues = "[";
+
+  // parsing of data from flipside
+  if (queryResults !== "") {
+    // parsing of labels
+    queryResults.rows.map((d, i) => {
+      if (i !== 0) {
+        chartBottomAxisLabels += ",";
+        chartValues += ",";
+      } else {
+        chartValues += `{label: '${props.title}', data:[`;
+      }
+      chartBottomAxisLabels += "'";
+      chartBottomAxisLabels += d[rowValue];
+      chartBottomAxisLabels += "'";
+
+      chartValues += "'";
+      chartValues += d[columnValue];
+      chartValues += "'";
+    });
+
+    chartValues += "]}]";
+    chartBottomAxisLabels += "]";
+    const imgTemplate = `https://quickchart.io/chart?width=${props.chartWidth}&height=${props.chartHeight}&chart={type:${chartType},data:{labels:${chartBottomAxisLabels}, datasets:${chartValues}}}`;
+    State.update({
+      imgSrc: imgTemplate,
+    });
+  }
 });
 
 return (
