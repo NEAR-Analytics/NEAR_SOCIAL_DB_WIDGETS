71af6f8f86796c4eb3b9cb52c90e1894a03d2caf
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-16 16:26:19 -0400

    Update DevSuport.Main by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-16 16:26:19.120

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSuport.Main/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSuport.Main/source_code.jsx
@@ -12,7 +12,7 @@ const adminContract = "admin.dev-support.near";
 const admins = Near.view(adminContract, "get_admins", {});
 
 return (
-  <div class="container">
+  <div class="container py-3">
     <div class="row">
       <div class="col-8">
         <h1>Developer Support</h1>
