44311e0ad84178844d6b2a276fd616bf888b6942
ciocan <ciocan>
2023-03-16 12:06:16 -0400

    Update account-signin by ciocan.near at 2023-03-16 12:06:16.614

--- ciocan.near/account-signin/source_code.jsx
+++ ciocan.near/account-signin/source_code.jsx
@@ -3,7 +3,9 @@ let accountId = context.accountId;
 if (accountId) return <div />;
 
 return (
-  <a href="https://wallet.near.org/" target="blank">
-    Please sign in with NEAR wallet
-  </a>
+  <div>
+    <a href="https://wallet.near.org/" target="blank">
+      Please sign in with NEAR wallet
+    </a>
+  </div>
 );
