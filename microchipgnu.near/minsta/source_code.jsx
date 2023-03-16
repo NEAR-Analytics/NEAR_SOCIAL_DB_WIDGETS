019fa60254a5293b757c54ca263edce5ed08498e
microchipgnu <microchipgnu>
2023-03-16 11:58:35 -0400

    Update minsta by microchipgnu.near at 2023-03-16 11:58:35.695

--- microchipgnu.near/minsta/source_code.jsx
+++ microchipgnu.near/minsta/source_code.jsx
@@ -1,7 +1,7 @@
 const accountId = context?.accountId;
 
 const proxyMinter = "proxy2.minsta.near";
-const nftContractId = props?.contractId || "minsta.mintbase1.near";
+const nftContractId = "minsta.mintbase1.near";
 const mbGraphEndpoint = "https://graph.mintbase.xyz";
 
 const ipfsUrl = (cid) => `https://ipfs.near.social/ipfs/${cid}`;
@@ -18,7 +18,7 @@ const data = fetch(mbGraphEndpoint, {
   body: JSON.stringify({
     query: `
   query FetchFeedMintedThings {
-  nft_activities(where: {kind: {_eq: "mint"}, nft_contract_id: {_eq: "minsta.mintbase1.near"}}, limit: 5, order_by: {timestamp: desc}) {
+  nft_activities(where: {kind: {_eq: "mint"}, nft_contract_id: {_eq: "minsta.mintbase1.near"}}, limit: 10, order_by: {timestamp: desc}) {
       nft_contract_id
       action_receiver
       token_id
@@ -96,16 +96,36 @@ if (data?.body?.data?.nft_activities) {
   posts = data?.body?.data?.nft_activities;
 }
 
-if (posts.length === 0) {
-  return "Loading...";
-}
-
-const size = "3em";
+const size = "8em";
 
 return (
   <div class="text-black p-2 container-fluid d-flex flex-column w-100 text-center justify-content-center align-items-center">
-    <h4>Minsta</h4>
-    <p style={{ fontSize: 12 }}>Capture, mint and share moments with others.</p>
+    <div class="flex justify-content-between">
+      <div>
+        <a
+          href={`https://shard.dog/go?url=https://near.social/#${context.widgetSrc}`}
+        >
+          Create account
+        </a>
+      </div>
+      <div class="flex gap-8">
+        <a
+          href="https://blog.mintbase.xyz/ethdenver-photo-book-mints-on-near-social-minsta-and-mintbase-baec3f49bd4c"
+          target="_blank"
+        >
+          Learn more
+        </a>
+        |
+        <a href="https://ethdenver2023.minsta.me/" target="_blank">
+          Minsta
+        </a>
+        |
+        <a href="https://ethdenver2023.minsta.me/leaderboard" target="_blank">
+          Leaderboard
+        </a>
+      </div>
+    </div>
+
     <div class="container-fluid text-center d-flex flex-column justify-content-center align-items-center">
       <Files
         multiple={false}
@@ -118,7 +138,7 @@ return (
         }}
         class="text-center d-flex justify-content-center align-items-center"
       >
-        <div class="d-flex m-4 px-2 py-1 rounded bg-black text-white justify-content-center align-items-center">
+        <div class="d-flex m-4 px-4 py-3 rounded bg-black text-white justify-content-center align-items-center">
           {state.img?.uploading ? (
             <>...</>
           ) : state.img?.cid ? (
@@ -148,29 +168,33 @@ return (
               <span role="img" aria-label="poked" title="poked">
                 📸
               </span>
-              <Widget
-                src="mob.near/widget/NftImage"
-                props={{
-                  nft: {
-                    tokenId: post.token_id,
-                    contractId: post.nft_contract_id,
-                  },
-                  style: {
-                    width: size,
-                    height: size,
-                    objectFit: "cover",
-                    minWidth: size,
-                    minHeight: size,
-                    maxWidth: size,
-                    maxHeight: size,
-                    overflowWrap: "break-word",
-                  },
-                  thumbnail: "thumbnail",
-                  className: "",
-                  fallbackUrl:
-                    "https://ipfs.near.social/ipfs/bafkreihdiy3ec4epkkx7wc4wevssruen6b7f3oep5ylicnpnyyqzayvcry",
-                }}
-              />
+              <a
+                href={`https://mintbase.xyz/contract/${post.nft_contract_id}/token/${post.token_id}`}
+              >
+                <Widget
+                  src="mob.near/widget/NftImage"
+                  props={{
+                    nft: {
+                      tokenId: post.token_id,
+                      contractId: post.nft_contract_id,
+                    },
+                    style: {
+                      width: size,
+                      height: size,
+                      objectFit: "cover",
+                      minWidth: size,
+                      minHeight: size,
+                      maxWidth: size,
+                      maxHeight: size,
+                      overflowWrap: "break-word",
+                    },
+                    thumbnail: "thumbnail",
+                    className: "",
+                    fallbackUrl:
+                      "https://ipfs.near.social/ipfs/bafkreihdiy3ec4epkkx7wc4wevssruen6b7f3oep5ylicnpnyyqzayvcry",
+                  }}
+                />
+              </a>
               <span role="img" aria-label="poked" title="poked">
                 ➡️
               </span>
@@ -188,13 +212,5 @@ return (
         })}
       </div>
     </div>
-
-    <a
-      href="https://twitter.com/microchipgnu/status/1542215896063672320"
-      target="_blank"
-      class="my-4"
-    >
-      What is this?{" "}
-    </a>
   </div>
 );
