b9a5651fbdc42aacf430c121b66850b645f2b453
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-16 13:36:12 -0400

    Update DevSupport.Question.Button.Flag by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-16 13:36:12.981

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Question.Button.Flag/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Question.Button.Flag/source_code.jsx
@@ -1,6 +1,10 @@
 const admin = "gagdiez.near";
 const question = { accountId: props.accountId, blockHeight: props.blockHeight };
 
+if (question.accountId === undefined || question.blockHeight === undefined) {
+  return;
+}
+
 const data = {
   index: {
     graph: JSON.stringify({
