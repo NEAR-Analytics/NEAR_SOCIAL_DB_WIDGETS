767713f64e33434900b9c04d6936576f8c12f5bf
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-16 18:07:00 -0400

    Update DevSuport.Main by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-16 18:07:00.384

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSuport.Main/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSuport.Main/source_code.jsx
@@ -36,7 +36,7 @@ return (
     {/* Widget to display Feed of Questions */}
     <Widget
       src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Feed"
-      props={{ admins, adminContract }}
+      props={{ admins, adminContract: adminContract }}
     />
   </div>
 );
