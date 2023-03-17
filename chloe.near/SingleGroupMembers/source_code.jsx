81f4fed337b1013fddd0fe69045e1053c91e2143
doulos819 <doulos819>
2023-03-17 18:49:51 -0400

    Update SingleGroupMembers by chloe.near at 2023-03-17 18:49:51.004

--- chloe.near/SingleGroupMembers/source_code.jsx
+++ chloe.near/SingleGroupMembers/source_code.jsx
@@ -1,22 +1,15 @@
-const accountId = props.accountId ?? context.accountId;
-const daoId = props.daoId ?? "marmaj.sputnik-dao.near";
-const policy = Near.view(daoId, "get_policy");
+// SingleGroupMembers.js
+function SingleGroupMembers(props) {
+  const { accountId, daoId, groupId } = props;
 
-const groups = policy.roles
-  .filter((role) => role.name !== "all")
-  .map((role) => role.name);
-
-return (
-  <div>
-    <h2 className="mt-3">{daoId}</h2>
-    {groups.map((groupId) => (
-      <div className="mb-3" key={groupId}>
-        <h3>{groupId}</h3>
-        <Widget
-          src="hack.near/widget/GroupMembers"
-          props={{ accountId, daoId, groupId }}
-        />
-      </div>
-    ))}
-  </div>
-);
+  return (
+    <div>
+      <h3>{groupId}</h3>
+      <Widget
+        src="hack.near/widget/GroupMembers"
+        props={{ accountId, daoId, groupId }}
+        displayGroupName={false}
+      />
+    </div>
+  );
+}
