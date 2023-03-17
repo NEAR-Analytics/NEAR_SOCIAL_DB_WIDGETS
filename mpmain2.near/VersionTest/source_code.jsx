a14363cd7a96c56e7df57df011f9124f17c59b19
mpmain2.near <mpmain2.near>
2023-03-17 15:40:15 -0400

    Update VersionTest by mpmain2.near at 2023-03-17 15:40:15.975

--- mpmain2.near/VersionTest/source_code.jsx
+++ mpmain2.near/VersionTest/source_code.jsx
@@ -1,6 +1,11 @@
+const versions = Social.keys("mpmain2.near/widget/VersionTestInner", "final", {
+  return_type: "History",
+  // subscribe: true,
+});
 return (
   <div>
-    Outer
+    Versions:{JSON.stringify(versions)}
+    <Widget src="mpmain2.near/widget/VersionTestInner@86775507" />
     <Widget src="mpmain2.near/widget/VersionTestInner" />
   </div>
 );
