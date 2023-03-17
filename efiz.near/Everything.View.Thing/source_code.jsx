79aa94391f3286379f7c40f9492b27f1a339a939
elliotBraem <elliotBraem>
2023-03-16 23:01:32 -0400

    Update Everything.View.Thing by efiz.near at 2023-03-16 23:01:32.204

---
+++ efiz.near/Everything.View.Thing/source_code.jsx
@@ -0,0 +1,66 @@
+const ERROR_WIDGET = "evrything.near/widget/Everything.Error";
+
+const ThingContainer = styled.div`
+  padding: 2px;
+`;
+
+const thingId = props.thingId;
+
+if (thingId === null) {
+  return (
+    <Widget
+      src={ERROR_WIDGET}
+      props={{
+        message: "thing id was not provided.",
+      }}
+    />
+  );
+}
+
+const type = props.type;
+
+if (type === null) {
+  return (
+    <Widget
+      src={ERROR_WIDGET}
+      props={{
+        message: `provided type: "${props.type}" is not valid.`,
+      }}
+    />
+  );
+}
+
+const data = fetch(type.queries?.getById.url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    query: type.queries?.getById.query,
+    variables: {
+      thingId: thingId,
+    },
+  }),
+});
+
+if (data.body.errors) {
+  return (
+    <Widget
+      src={ERROR_WIDGET}
+      props={{
+        message: JSON.stringify(data.body.errors[0].message),
+      }}
+    />
+  );
+}
+
+return (
+  <ThingContainer>
+    <Widget
+      src={props.widget}
+      props={{
+        data: data.body.data,
+      }}
+    />
+  </ThingContainer>
+);
