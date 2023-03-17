9e2d49a2b6e5c9a330fd0dcf0b3d0ae03d94dc83
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-17 14:12:56 -0400

    Update DevSupport.Answer by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-17 14:12:56.203

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Answer/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DevSupport.Answer/source_code.jsx
@@ -69,7 +69,7 @@ return (
 
       <Widget
         src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Answer.Feed"
-        props={{ accountId, blockHeight, admins, adminContract }}
+        props={{ item, admins, adminContract }}
       />
     </div>
   </>
