82d5bb4f52e25b131a63a674e4acffadf78075a2
evrything.near <evrything.near>
2023-03-16 21:12:54 -0400

    Update Everything by evrything.near at 2023-03-16 21:12:54.919

---
+++ evrything.near/Everything/source_code.jsx
@@ -0,0 +1,43 @@
+const accountId = props.accountId || "evrything.near";
+
+const types = Social.keys(`${accountId}/type/*`, "final", {
+  return_type: "BlockHeight",
+  values_only: true,
+});
+
+// All types from every account
+// const data = Social.keys("*/type/*");
+
+types = Object.entries(types[accountId].type ?? {});
+
+State.init({
+  selected: `${accountId}/type/Everything`,
+  type: Type.get(`${accountId}/type/Everything`),
+});
+
+const setSelected = (selection) => {
+  State.update({
+    selected: selection,
+    type: Type.get(props.type),
+  });
+};
+
+return (
+  <>
+    <Widget
+      src={"evrything.near/widget/Everything.Search"}
+      props={{
+        accountId: accountId,
+        selected: state.selected,
+        setSelected: setSelected,
+        options: types,
+      }}
+    />
+    <Widget
+      src={state.type?.widgets.all}
+      props={{
+        type: state.type,
+      }}
+    />
+  </>
+);
