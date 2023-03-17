e90f9697ef99381f3dd5363883a801aafafc80c2
humanman <humanman>
2023-03-17 19:19:20 -0400

    Update wikiMarkdownFetcher by humanman.near at 2023-03-17 19:19:20.226

---
+++ humanman.near/wikiMarkdownFetcher/source_code.jsx
@@ -0,0 +1,31 @@
+const accountId = context.accountId;
+const markdownContent = props.path
+  ? `https://raw.githubusercontent.com/near/wiki/master/wiki/support/understanding-web3/${props.path}.md`
+  : `# No Data`;
+
+if (!accountId) {
+  return "Please sign in with NEAR wallet to use this widget";
+}
+
+const res = props.path ? fetch(markdownContent) : "";
+// const delimiter = "\n";
+// const start = 4;
+// const body = res.body.split(delimiter).slice(start).join("\n");
+const body = res.length ? res.body : markdownContent;
+
+if (context.loading) {
+  return "Loading";
+}
+
+return (
+  <div>
+    <Markdown
+      text={body}
+      transformImageUri={(uri) =>
+        uri.startsWith("http")
+          ? uri
+          : `https://cryptobootcampassets.s3.amazonaws.com/${uri.slice(26)}`
+      }
+    />
+  </div>
+);
