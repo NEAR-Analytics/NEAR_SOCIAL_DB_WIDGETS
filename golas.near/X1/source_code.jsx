7c10e1281a53a0c6ba5a6b6cdc960dfb108086c3
golas.near <golas.near>
2023-03-17 17:19:40 -0400

    Update X1 by golas.near at 2023-03-17 17:19:40.020

--- golas.near/X1/source_code.jsx
+++ golas.near/X1/source_code.jsx
@@ -1,8 +1,3 @@
 const { calc } = require("golas.near/widget/Module-1");
-const { calc2 } = require("golas.near/widget/Module-2");
 
-return (
-  <div>
-    {calc(100, 11)}-{calc2(100, 11)}
-  </div>
-);
+return <div>{calc(100, 11)}</div>;
