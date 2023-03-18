8f07b91e62500f857343c32801122cd93f273da3
humanman <humanman>
2023-03-17 20:59:34 -0400

    Update CryptoBootcamp by humanman.near at 2023-03-17 20:59:34.665

--- humanman.near/CryptoBootcamp/source_code.jsx
+++ humanman.near/CryptoBootcamp/source_code.jsx
@@ -1,26 +1,19 @@
 const accountId = context.accountId;
-State.init({
-  text: initialText,
-  path: "01_history/1.1_history-of-the-internet",
-  content: "",
-});
+const initalPath = `01_history/1.1_history-of-the-internet`;
+const initialUrl = `https://raw.githubusercontent.com/near/wiki/master/wiki/support/understanding-web3/01_history/1.1_history-of-the-internet.md`;
 
-const markdownContent = `https://raw.githubusercontent.com/near/wiki/master/wiki/support/understanding-web3/${state.path}.md`;
-console.log(markdownContent);
 if (!accountId) {
   return "Please sign in with NEAR wallet to use this widget";
 }
 
-const res = fetch(state.path);
+const res = fetch(initialUrl);
 const delimiter = "\n";
 const start = 4;
 const body = res.body.split(delimiter).slice(start).join("\n");
 
-State.update({ content: res.body });
-
-if (context.loading) {
-  return "Loading";
-}
+State.init({
+  content: body,
+});
 
 const moduleArr = [
   {
@@ -33,14 +26,14 @@ const moduleArr = [
   },
   {
     label: "1.3 The History of Crypto",
-    path: "01_history/1.2_history-of-crypto",
+    path: "01_history/1.3_history-of-crypto",
   },
 ];
 
 const handleModuleSelect = (val) => {
-  const m = fetch(
-    `https://raw.githubusercontent.com/near/wiki/master/wiki/support/understanding-web3/${val}.md`
-  );
+  const url = `https://raw.githubusercontent.com/near/wiki/master/wiki/support/understanding-web3/${val}.md`;
+  const fetched = fetch(url);
+  const m = fetched.body.split(delimiter).slice(start).join("\n");
   State.update({ path: val, content: m });
 };
 
@@ -49,28 +42,23 @@ if (context.loading) {
 }
 
 return (
-  <Markdown
-    text={body}
-    transformImageUri={(uri) =>
-      uri.startsWith("http")
-        ? uri
-        : `https://cryptobootcampassets.s3.amazonaws.com/${uri.slice(26)}`
-    }
-  />
-  //   <div>
-  // {moduleArr.map((obj, index) => (
-  //   <div style={{ marginBottom: "5px" }}>
-  //     <button onClick={() => handleModuleSelect(obj.path)} key={index}>
-  //       {obj.label}
-  //     </button>
-  //   </div>
-  // ))}
-
-  //   </div>
+  <div>
+    {moduleArr.map((obj, index) => (
+      <div style={{ marginBottom: "5px" }}>
+        <button onClick={() => handleModuleSelect(obj.path)} key={index}>
+          {obj.label}
+        </button>
+      </div>
+    ))}
+    <br />
+    <br />
+    <Markdown
+      text={state.content}
+      transformImageUri={(uri) =>
+        uri.startsWith("http")
+          ? uri
+          : `https://cryptobootcampassets.s3.amazonaws.com/${uri.slice(26)}`
+      }
+    />
+  </div>
 );
-// {moduleArr.map((obj) => {
-//   <div>
-//     hello //{" "}
-//     <button onClick={handleModuleSelect(obj.path)}>{obj.label}</button>
-//   </div>;
-// })}
