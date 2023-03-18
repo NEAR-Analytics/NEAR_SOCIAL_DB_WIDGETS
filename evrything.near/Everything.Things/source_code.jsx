af7d7c2933cc8c20cd4d07a241a8e4f66fda39f3
evrything.near <evrything.near>
2023-03-18 19:27:39 -0400

    Update Everything.Things by evrything.near at 2023-03-18 19:27:39.024

---
+++ evrything.near/Everything.Things/source_code.jsx
@@ -0,0 +1,35 @@
+const type = props.type;
+
+if (!type) {
+  return "props.type is not defined";
+}
+
+const index = {
+  action: "thing",
+  key: "main",
+  options: {
+    limit: 10,
+    order: "desc",
+    accountId: props.accounts,
+  },
+};
+
+const renderThing = (a) => {
+  return <div>{JSON.stringify(a)}</div>;
+};
+
+// <div key={JSON.stringify(a)} className="mb-3">
+//   <Widget
+//     src={type.widgets?.view}
+//     props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
+//   />
+// </div>
+
+return (
+  <div>
+    <Widget
+      src="evrything.near/widget/FilteredIndexMasonry"
+      props={{ index, renderThing }}
+    />
+  </div>
+);
