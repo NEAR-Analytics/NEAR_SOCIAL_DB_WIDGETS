abfaf486997643a8f5d67d1638255038990b2851
MinorityProgrammers <MinorityProgrammers>
2023-03-18 05:27:45 -0400

    Update RainbowBridgeWNEAR by minorityprogrammers.near at 2023-03-18 05:27:45.777

--- minorityprogrammers.near/RainbowBridgeWNEAR/source_code.jsx
+++ minorityprogrammers.near/RainbowBridgeWNEAR/source_code.jsx
@@ -7,12 +7,45 @@ const receiver_id = "aurora";
 const contract_name = "wrap.near";
 const auroraAddress = "0x97B882530830a10f07A9f9A733cB7d0491F808Dc"; // minority programmersEth address
 
+function isEthereumAddress(address) {
+  // Check if the address is a hexadecimal string of 40 characters
+  if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+    return false;
+  }
+
+  // Check if the address has a valid checksum
+  const checksumAddress = toChecksumAddress(address);
+  return address === checksumAddress;
+}
+
+function toChecksumAddress(address) {
+  // Remove "0x" prefix if it exists
+  const addressWithoutPrefix = address.toLowerCase().replace(/^0x/, "");
+
+  // Compute the hash of the lowercase address
+  const hash = web3.utils.sha3(addressWithoutPrefix);
+
+  // Convert the hash to a hex string and compare characters of the address and the hash
+  let checksumAddress = "0x";
+  for (let i = 0; i < addressWithoutPrefix.length; i++) {
+    if (parseInt(hash[i], 16) >= 8) {
+      checksumAddress += addressWithoutPrefix[i].toUpperCase();
+    } else {
+      checksumAddress += addressWithoutPrefix[i];
+    }
+  }
+
+  return checksumAddress;
+}
 initState({
   amount: amount,
   ethereum_receiver: auroraAddress,
+  valid_address: true,
 });
 
 const onChangeAddress = (ethereum_receiver) => {
+  state.valid_address = isEthereumAddress(ethereum_receiver);
+  console.log(state.valid_address); // checking if i updated correctly
   State.update({
     ethereum_receiver,
   });
@@ -59,13 +92,22 @@ return (
   <div>
     <h1>🌈 Send NEAR to Aurora Address </h1>
     <div className=" mb-2">
-      Aurora Address Your Want to Bridge
+      Aurora Address to Bridge To
       <input
         type="text"
         placeholder={state.ethereum_receiver}
         onChange={(e) => onChangeAddress(e.target.value)}
       />
-    </div>
+    </div>{" "}
+    {(state.valid_address && (
+      <div className="alert alert-success">
+        <i className="bi bi-x"></i> Aurora (ETH) Address is valid
+      </div> // this conditional isnt working to check valid adress on the front end
+    )) || (
+      <div className="alert alert-danger">
+        <i className="bi bi-x"></i> Not an Aurora (ETH) Address
+      </div>
+    )}
     <div className=" mb-2">
       Enter Amount of NEAR to Bridge
       <input
