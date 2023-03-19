bc5e72a3fb8112b0f4270af60a0719c3c910be9f
evrything.near <evrything.near>
2023-03-18 21:29:49 -0400

    Update IndexMasonry by evrything.near at 2023-03-18 21:29:49.388

--- evrything.near/IndexMasonry/source_code.jsx
+++ evrything.near/IndexMasonry/source_code.jsx
@@ -113,11 +113,6 @@ if (filter) {
       (item) => !(item.accountId in filter.ignore)
     );
   }
-  if (filter.matchesType) {
-    filteredItems = filteredItems.filter(
-      (item) => item.type === filter.matchesType
-    );
-  }
 }
 
 const maybeFetchMore = () => {
