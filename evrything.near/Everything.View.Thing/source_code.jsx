195d415ecdfde46b9876d2063fc04462e8646ad6
evrything.near <evrything.near>
2023-03-18 20:46:52 -0400

    Update Everything.View.Thing by evrything.near at 2023-03-18 20:46:52.961

--- evrything.near/Everything.View.Thing/source_code.jsx
+++ evrything.near/Everything.View.Thing/source_code.jsx
@@ -4,38 +4,31 @@ const ThingContainer = styled.div`
   padding: 2px;
 `;
 
-const thingId = props.thingId;
+const accountId = props.accountId;
+const blockHeight = parseInt(props.blockHeight);
+const content = JSON.parse(
+  Social.get(`${accountId}/thing/main`, blockHeight) ?? "null"
+);
 
-if (thingId === null) {
+const type = Type.get(props.type);
+
+if (type === null) {
   return (
     <Widget
       src={ERROR_WIDGET}
       props={{
-        message: "thing id was not provided.",
+        message: `type: "${a.value.type}" is not valid.`,
       }}
     />
   );
 }
-
-const type = Type.get(props.type);
-
-// if (type === null) {
-//   return (
-//     <Widget
-//       src={ERROR_WIDGET}
-//       props={{
-//         message: `provided type: "${props.type}" is not valid.`,
-//       }}
-//     />
-//   );
-// }
 
 return (
   <ThingContainer>
     <Widget
       src={type.widgets?.view}
       props={{
-        thingId: thingId,
+        data: JSON.parse(content),
       }}
     />
   </ThingContainer>
