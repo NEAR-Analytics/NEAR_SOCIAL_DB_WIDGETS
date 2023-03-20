ef4c4089d78a0687afa593265f5313c461ad7648
NearSocial <NearSocial>
2023-03-20 04:26:28 -0400

    Update Common.Component.Library by hack.near at 2023-03-20 04:26:28.475

--- hack.near/Common.Component.Library/source_code.jsx
+++ hack.near/Common.Component.Library/source_code.jsx
@@ -1,4 +1,224 @@
 const ownerId = "manzanal.near";
+
+const limitPerPage = 21;
+let components = [];
+let totalComponents = 0;
+const componentsUrl = "/#/adminalpha.near/widget/ComponentsPage";
+
+State.init({
+  tab: "home",
+  id: "",
+  currentPage: 0,
+  selectedTab: props.tab || "all",
+});
+
+if (props.tab && props.tab !== state.selectedTab) {
+  State.update({
+    selectedTab: props.tab,
+  });
+}
+
+const tagsData = Social.get("*/widget/*/metadata/tags/*", "final");
+
+const data = Social.keys("*/widget/*", "final", {
+  return_type: "BlockHeight",
+});
+
+if (data) {
+  const result = [];
+
+  Object.keys(data).forEach((accountId) => {
+    return Object.keys(data[accountId].widget).forEach((widgetName) => {
+      totalComponents++;
+
+      if (state.selectedTab === "apps") {
+        const hasAppTag =
+          tagsData[accountId].widget[widgetName]?.metadata?.tags["app"] === "";
+        if (!hasAppTag) return;
+      }
+
+      result.push({
+        accountId,
+        widgetName,
+        blockHeight: data[accountId].widget[widgetName],
+      });
+    });
+  });
+
+  result.sort((a, b) => b.blockHeight - a.blockHeight);
+  components = result.slice(0, state.currentPage * limitPerPage + limitPerPage);
+}
+
+function onSearchChange({ result, term }) {
+  if (term.trim()) {
+    State.update({ searchResults: result || [] });
+  } else {
+    State.update({ searchResults: null });
+  }
+}
+
+const items = state.searchResults || components;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+  padding-bottom: 48px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const Search = styled.div`
+  width: 246px;
+
+  @media (max-width: 500px) {
+    width: 100%;
+  }
+`;
+
+const H1 = styled.h1`
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 39px;
+  color: #11181c;
+  margin: 0;
+`;
+
+const H2 = styled.h2`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+  color: #687076;
+  margin: 0;
+`;
+
+const Text = styled.p`
+  margin: 0;
+  line-height: 1.5rem;
+  color: ${(p) => (p.bold ? "#11181C" : "#687076")} !important;
+  font-weight: ${(p) => (p.bold ? "600" : "400")};
+  font-size: ${(p) => (p.small ? "12px" : "14px")};
+  overflow: ${(p) => (p.ellipsis ? "hidden" : "")};
+  text-overflow: ${(p) => (p.ellipsis ? "ellipsis" : "")};
+  white-space: ${(p) => (p.ellipsis ? "nowrap" : "")};
+  overflow-wrap: anywhere;
+
+  b {
+    font-weight: 600;
+    color: #11181c;
+  }
+
+  &[href] {
+    display: inline-flex;
+    gap: 0.25rem;
+
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const Items = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 24px;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+
+  @media (max-width: 800px) {
+    grid-template-columns: minmax(0, 1fr);
+  }
+`;
+
+const Item = styled.div``;
+
+const Button = styled.button`
+  display: block;
+  width: 100%;
+  padding: 8px;
+  height: 32px;
+  background: #fbfcfd;
+  border: 1px solid #d7dbdf;
+  border-radius: 50px;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 15px;
+  text-align: center;
+  cursor: pointer;
+  color: #11181c !important;
+  margin: 0;
+
+  &:hover,
+  &:focus {
+    background: #ecedee;
+    text-decoration: none;
+    outline: none;
+  }
+
+  span {
+    color: #687076 !important;
+  }
+`;
+
+const Tabs = styled.div`
+  display: flex;
+  height: 48px;
+  border-bottom: 1px solid #eceef0;
+  margin-bottom: -24px;
+  overflow: auto;
+  scroll-behavior: smooth;
+
+  @media (max-width: 1200px) {
+    background: #f8f9fa;
+    border-top: 1px solid #eceef0;
+    margin-left: -12px;
+    margin-right: -12px;
+
+    > * {
+      flex: 1;
+    }
+  }
+`;
+
+const TabsButton = styled.a`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  font-weight: 600;
+  font-size: 12px;
+  padding: 0 12px;
+  position: relative;
+  color: ${(p) => (p.selected ? "#11181C" : "#687076")};
+  background: none;
+  border: none;
+  outline: none;
+  text-align: center;
+  text-decoration: none !important;
+
+  &:hover {
+    color: #11181c;
+  }
+
+  &::after {
+    content: "";
+    display: ${(p) => (p.selected ? "block" : "none")};
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 3px;
+    background: #59e692;
+  }
+`;
+
 const curatedComps = [
   {
     category: "Buttons",
@@ -112,38 +332,63 @@ const debug = props.debug ?? false;
 
 const searchComponents = () => {
   return (
-    <div class="mb-4">
-      <div className="mb-2">
+    <Wrapper>
+      <Search>
         <Widget
-          src="mob.near/widget/ComponentSearch"
+          src="adminalpha.near/widget/ComponentSearch"
           props={{
-            debug: debug,
-            filterTag: filterTag,
-            placeholder: "🔍 Search for common components",
-            limit: 24,
-            onChange: ({ result }) => {
-              State.update({ components: result });
-            },
+            limit: 21,
+            onChange: onSearchChange,
           }}
         />
-      </div>
-      {state.components && (
-        <div className="mb-2">
-          {state.components.map((comp, i) => (
-            <div class="mb-2" key={i}>
+      </Search>
+      {!state.searchResults && (
+        <Tabs>
+          <TabsButton
+            href={`${componentsUrl}?tab=all`}
+            selected={state.selectedTab === "all"}
+          >
+            All
+          </TabsButton>
+
+          <TabsButton
+            href={`${componentsUrl}?tab=apps`}
+            selected={state.selectedTab === "apps"}
+          >
+            Apps
+          </TabsButton>
+        </Tabs>
+      )}
+
+      {state.searchResults?.length === 0 && (
+        <Text>No components matched your search.</Text>
+      )}
+
+      {items.length > 0 && (
+        <Items>
+          {items.map((component, i) => (
+            <Item key={component.accountId + component.widgetName}>
               <Widget
-                src="mob.near/widget/WidgetMetadata"
+                src="adminalpha.near/widget/ComponentCard"
                 props={{
-                  accountId: comp.accountId,
-                  widgetName: comp.widgetName,
-                  expanded: false,
+                  src: `${component.accountId}/widget/${component.widgetName}`,
+                  blockHeight: component.blockHeight,
                 }}
               />
-            </div>
+            </Item>
           ))}
-        </div>
+        </Items>
       )}
-    </div>
+
+      {!state.searchResults && (
+        <Button
+          type="button"
+          onClick={() => State.update({ currentPage: state.currentPage + 1 })}
+        >
+          Load More
+        </Button>
+      )}
+    </Wrapper>
   );
 };
 
@@ -176,13 +421,9 @@ const renderCategory = (categoryId) => {
     </div>
   );
 };
-State.init({
-  tab: "home",
-  id: "",
-});
 
 const renderHome = () => {
-  return <Widget src="hack.near/widget/Components" />;
+  return <Widget src="adminalpha.near/widget/ComponentsPage" />;
 };
 
 const onSelect = (selection) => {
@@ -217,14 +458,7 @@ return (
           props={{ authors: [ownerId], dep: true }}
         />
       </div>
-      <div class="col-md-9">
-        {" "}
-        <h2>Components Library</h2>
-        <p class="text text-muted">
-          Building blocks for Near Social applications.
-        </p>
-        {renderContent}
-      </div>
+      <div class="col-md-9">{renderContent}</div>
     </div>
   </>
 );
