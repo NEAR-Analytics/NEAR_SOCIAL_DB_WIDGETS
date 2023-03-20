d386e9f3395c234c4b82d0aec5b16ef8431926dd
evrything.near <evrything.near>
2023-03-20 04:03:33 -0400

    Update Everything.Create.Test by evrything.near at 2023-03-20 04:03:33.338

--- evrything.near/Everything.Create.Test/source_code.jsx
+++ evrything.near/Everything.Create.Test/source_code.jsx
@@ -1,44 +1,73 @@
-const Header = styled.div` width: 100%; display: flex; flex-direction: row; justify-content: space-between; align-items: center; `;
-const Title = styled.div` font-size: 24px; line-height: 33.6px; `;
-const Form = styled.div` display: flex; flex-direction: column; gap: 4px; `;
-const Input = styled.input` width: 100%; `;
-const TextArea = styled.textarea` width: 100%; `;
-const ButtonRow = styled.div` display: flex; flex-direction: row; gap: 4px; `;
+const Header = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Title = styled.div`
+  font-size: 24px;
+  line-height: 33.6px;
+`;
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+const Input = styled.input`
+  width: 100%;
+`;
+const TextArea = styled.textarea`
+  width: 100%;
+`;
+const ButtonRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+`;
+const Button = styled.button`
+  padding: 8px 20px;
+  max-width: 90px;
+`;
+
 const typeStr = "evrything.near/type/Test";
-const type = Type.get(typeStr);
+
 State.init({ title: "", description: "" });
-function composeData() {
-  const data = {
-    thing: { main: JSON.stringify(state) },
-    index: {
-      everythingv0: JSON.stringify({ key: "main", value: { type: typeStr } }),
-    },
-  };
-  return data;
+
+const resetThing = () => {
+  State.update({
+    title: "",
+    description: "",
+  });
+};
+
+function createThing() {
+  const resp = Evrything.create(state, typeStr);
+  State.update({
+    publish: resp,
+  });
 }
+
 return (
   <>
-    {" "}
     <Header>
-      {" "}
-      <Title>{typeStr.split("/")[2]}</Title>{" "}
-    </Header>{" "}
+      <Title>{typeStr.split("/")[2]}</Title>
+    </Header>
     <Form>
       <Input
         placeholder="title"
         onChange={({ target }) => State.update({ title: target.value })}
-      />{" "}
+      />
       <TextArea
         placeholder="description"
         onInput={({ target }) => State.update({ description: target.value })}
       />
       <ButtonRow>
-        {" "}
-        <CommitButton force data={composeData} onCommit={resetThing}>
-          {" "}
-          create{" "}
-        </CommitButton>{" "}
-      </ButtonRow>{" "}
-    </Form>{" "}
+        <Button onClick={createThing}>create</Button>
+        <CommitButton force data={state.publish} onCommit={resetThing}>
+          publish
+        </CommitButton>
+      </ButtonRow>
+    </Form>
   </>
 );
