e2d9520c964f2a9b46a39a826f6ee3766180117e
humanman <humanman>
2023-03-20 13:06:45 -0400

    Update CryptoBootcamp by humanman.near at 2023-03-20 13:06:45.211

--- humanman.near/CryptoBootcamp/source_code.jsx
+++ humanman.near/CryptoBootcamp/source_code.jsx
@@ -6,15 +6,6 @@ if (!accountId) {
   return "Please sign in with NEAR wallet to use this widget";
 }
 
-const res = fetch(initialUrl);
-const delimiter = "\n";
-const start = 4;
-const body = res.body.split(delimiter).slice(start).join("\n");
-
-State.init({
-  content: body,
-});
-
 const lessons = [
   "1.1 The History of the Internet",
   "1.2 The History of Money",
@@ -52,51 +43,60 @@ const lessonPaths = {
   "1.1 The History of the Internet": "01_history/1.1_history-of-the-internet",
   "1.2 The History of Money": "01_history/1.2_history-of-money",
   "1.3 The History of Crypto": "01_history/1.3_history-of-crypto",
-  "2.1 The Evolution of Blockchain": "02_landscape/2.1_evolution-blockchain.md",
-  "2.2 Ethereum 1.0 / Ethereum 2.0": "02_landscape/2.2_eth1.0-eth2.0.md",
-  "2.3 NEAR Protocol": "02_landscape/2.3_NEAR-Protocol.md",
-  "2.4 The Product Stack": "02_landscape/2.4_product-stack.md",
-  "2.5 Composability": "02_landscape/2.5_composability.md",
+  "2.1 The Evolution of Blockchain": "02_landscape/2.1_evolution-blockchain",
+  "2.2 Ethereum 1.0 / Ethereum 2.0": "02_landscape/2.2_eth1.0-eth2.0",
+  "2.3 NEAR Protocol": "02_landscape/2.3_NEAR-Protocol",
+  "2.4 The Product Stack": "02_landscape/2.4_product-stack",
+  "2.5 Composability": "02_landscape/2.5_composability",
   "2.6 Crypto Ecosystems and Emerging Technologies":
-    "02_landscape/2.6_emerging-technologies.md",
+    "02_landscape/2.6_emerging-technologies",
   "2.7 Measuring Success Against other Ecosystems":
-    "02_landscape/2.7_measuring-success.md",
-  "2.8 Legal Pathways and Project Design":
-    "02_landscape/2.8_legal-and-product.md",
-  "3.1 Understanding DeFi": "03_defi/3.1_understanding-defi.md",
-  "3.2 DEX / AMM / Aggregators / Perps": "03_defi/3.2_dex-amm-agg-perps.md",
-  "3.3 Stablecoins": "03_defi/3.3_stablecoins.md",
-  "3.4 DeFi Lending": "03_defi/3.4_lending.md",
-  "3.5 Synthetic Assets": "03_defi/3.5_synthetics.md",
-  "3.6 Staking": "03_defi/3.6_staking.md",
-  "3.7 Prediction Market Design": "03_defi/3.7_prediction-market-design.md",
-  "4.1 What is a Non-Fungible Token (NFT)?": "04_creator-economy/4.1_nfts.md",
+    "02_landscape/2.7_measuring-success",
+  "2.8 Legal Pathways and Project Design": "02_landscape/2.8_legal-and-product",
+  "3.1 Understanding DeFi": "03_defi/3.1_understanding-defi",
+  "3.2 DEX / AMM / Aggregators / Perps": "03_defi/3.2_dex-amm-agg-perps",
+  "3.3 Stablecoins": "03_defi/3.3_stablecoins",
+  "3.4 DeFi Lending": "03_defi/3.4_lending",
+  "3.5 Synthetic Assets": "03_defi/3.5_synthetics",
+  "3.6 Staking": "03_defi/3.6_staking",
+  "3.7 Prediction Market Design": "03_defi/3.7_prediction-market-design",
+  "4.1 What is a Non-Fungible Token (NFT)?": "04_creator-economy/4.1_nfts",
   "4.2 NFT Lending, fractionalization and time-scarcity":
-    "04_creator-economy/4.2_nft-lending.md",
-  "4.3 Social Tokens": "04_creator-economy/4.3_social-tokens.md",
+    "04_creator-economy/4.2_nft-lending",
+  "4.3 Social Tokens": "04_creator-economy/4.3_social-tokens",
   "4.4 The Making of the Creator Economy":
-    "04_creator-economy/4.4_creator-economy.md",
+    "04_creator-economy/4.4_creator-economy",
   "4.5 Future Creator Economy Developments":
-    "04_creator-economy/4.5_future-economy.md",
+    "04_creator-economy/4.5_future-economy",
   "5.1 Decentralized Autonomous Organizations (DAOs)":
-    "05_DAOs/5.1_dao-landscape.md",
-  "5.2 Sputnik V2 and Astro DAO on NEAR": "05_DAOs/5.2_sputnik-vs-astro-dao.md",
-  "5.3 Protocol Politicians and Governance Design": "05_DAOs/5.3_governance.md",
+    "05_DAOs/5.1_dao-landscape",
+  "5.2 Sputnik V2 and Astro DAO on NEAR": "05_DAOs/5.2_sputnik-vs-astro-dao",
+  "5.3 Protocol Politicians and Governance Design": "05_DAOs/5.3_governance",
   "5.4 Network States and the Future of Governance":
-    "05_DAOs/5.4_network-states.md",
+    "05_DAOs/5.4_network-states",
   "6.1 Theory and Evolution of the Metaverse":
-    "06_metaverse/6.1_theory-and-evolution.md",
-  "6.2 Digital Real Estate Economies":
-    "06_metaverse/6.2_realstate-economies.md",
-  "6.3 Metaverse on NEAR": "06_metaverse/6.3_metaverse-on-near.md",
+    "06_metaverse/6.1_theory-and-evolution",
+  "6.2 Digital Real Estate Economies": "06_metaverse/6.2_realstate-economies",
+  "6.3 Metaverse on NEAR": "06_metaverse/6.3_metaverse-on-near",
 };
+
+const res = fetch(initialUrl);
+const delimiter = "\n";
+const start = 4;
+const body = res.body.split(delimiter).slice(start).join("\n");
+
+State.init({
+  content: body,
+  selected: lessons[0],
+  path: lessonPaths[0][lessons[0]],
+});
 
 const handleModuleSelect = (val) => {
   const newPath = lessonPaths[val];
   const url = `https://raw.githubusercontent.com/near/wiki/master/wiki/support/understanding-web3/${newPath}.md`;
-  const fetched = fetch(url);
-  const m = fetched.body.split(delimiter).slice(start).join("\n");
-  State.update({ path: val, content: m });
+  const fecthed = fetch(url);
+  const m = fecthed.body.split(delimiter).slice(start).join("\n");
+  State.update({ selected: val, path: lessonPaths[val], content: m });
 };
 
 if (context.loading) {
@@ -107,7 +107,10 @@ return (
   <div>
     <Typeahead
       options={lessons}
-      onChange={(val) => handleModuleSelect(val)}
+      onChange={(val) => {
+        handleModuleSelect(val);
+      }}
+      selected={state.selected}
       placeholder="Select a lecture..."
     />
     <br />
