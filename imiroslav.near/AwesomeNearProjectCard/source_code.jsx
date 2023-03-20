8fe9fee63c65a09a2534e227dbe5697e286358cf
iMiroslav <iMiroslav>
2023-03-20 12:44:51 -0400

    Update AwesomeNearProjectCard by imiroslav.near at 2023-03-20 12:44:51.442

--- imiroslav.near/AwesomeNearProjectCard/source_code.jsx
+++ imiroslav.near/AwesomeNearProjectCard/source_code.jsx
@@ -1,3 +1,7 @@
+const widgetAuthor = "imiroslav.near";
+const API_KEY = "tESt_keY"; // TODO get from Social db?
+const symbol = props.symbol ?? "NEAR";
+
 const css = fetch(
   "https://gateway.pinata.cloud/ipfs/QmTQKDx6jGLkigj9yAWZow1d1dEYrqUeSvKgHtcmnBEVYK?_gl=1*1uz9sye*_ga*MjRlZDQxNzQtOGRlNy00ZGZjLTlhNWEtMWEyNjU5OGUzMGMw*_ga_5RMPXG14TE*MTY3OTI5NjkzNy4xLjEuMTY3OTI5NzQ3MC41NC4wLjA."
 ).body;
@@ -13,24 +17,86 @@ if (!state.theme) {
 
 const Theme = state.theme;
 
-const isBuilding = props.status === "BUILDING";
+const queryContent = {
+  query: `{
+    projects(filter: {contractAddress: {}, symbol: "${symbol}"}) {
+        edges {
+            node {
+                logo
+                link
+                linkedIn
+                investors
+                grants
+                title
+                telegram
+                status
+                oneliner
+                medium
+                twitter
+                website
+                whitepaper
+                github
+                facebook
+                discord
+                description
+                dapp
+                categories
+                }   
+            }
+        }
+    }`,
+};
+
+const queryBody = JSON.stringify(queryContent);
+const projects = fetch("https://awesomenear.com/api/graphql/public", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+  body: queryBody,
+});
+
+if (!projects) return "Loading";
+console.log(projects);
+
+if (projects?.body?.errors) {
+  return `Erorr: ${projects.body.errors.map((error) => error.message)}`;
+}
+
+const data = projects?.body?.data?.projects;
+if (!data) {
+  return "NO DATA";
+}
+
+if (data.edges.length === 0) return `Failed to get data for symbol ${symbol}`;
+
+const project = {
+  ...data.edges[0].node,
+  series: [], // TODO not exposed by API yet
+};
+
+console.log(project);
+
+const isBuilding = project.status === "BUILDING";
 
 return (
   <Theme>
+    <Widget src={`${widgetAuthor}/widget/AwesomeNearIcons`} />
     <div className="near-item">
       <div className="near-item-test">
         <div className="tile">
           <div className="tile-icon">
-            <img src={props.logo} alt={props.title} loading="lazy" />
+            <img src={project.logo} alt={project.title} loading="lazy" />
           </div>
           <div className="tile-content">
-            <h2 className="tile-title">{props.title}</h2>
+            <h2 className="tile-title">{project.title}</h2>
             <div className="tile-tags">
               {isBuilding ? (
                 <span>"COMMING SOON"</span>
               ) : (
                 <>
-                  {props.categories.map((category, i) => (
+                  {project.categories.map((category, i) => (
                     <span key={i} title={category}>
                       {category}
                     </span>
@@ -41,59 +107,59 @@ return (
           </div>
         </div>
         <div className="tile">
-          <h3 className="tile-subtitle">{props.oneliner}</h3>
+          <h3 className="tile-subtitle">{project.oneliner}</h3>
         </div>
       </div>
       <div className="near-item-footer">
         <div className="tile-social">
-          {props.website && (
+          {project.website && (
             <svg className="icon" height="20" width="20">
               <use xlinkHref="#icon-website"></use>
             </svg>
           )}
-          {props.twitter && (
+          {project.twitter && (
             <svg className="icon" height="20" width="20">
               <use xlinkHref="#icon-twitter"></use>
             </svg>
           )}
-          {props.facebook && (
+          {project.facebook && (
             <svg className="icon" height="20" width="20">
               <use xlinkHref="#icon-facebook"></use>
             </svg>
           )}
-          {props.medium && (
+          {project.medium && (
             <svg className="icon" height="20" width="20">
               <use xlinkHref="#icon-medium"></use>
             </svg>
           )}
-          {props.telegram && (
+          {project.telegram && (
             <svg className="icon" height="20" width="20">
               <use xlinkHref="#icon-telegram"></use>
             </svg>
           )}
-          {props.github && (
+          {project.github && (
             <svg className="icon" height="20" width="20">
               <use xlinkHref="#icon-github"></use>
             </svg>
           )}
-          {props.linkedIn && (
+          {project.linkedIn && (
             <svg className="icon" height="20" width="20">
               <use xlinkHref="#icon-linkedin"></use>
             </svg>
           )}
-          {props.astroDAO && (
+          {project.astroDAO && (
             <svg className="icon" height="20" width="20">
               <use xlinkHref="#icon-astrodao"></use>
             </svg>
           )}
-          {props.discord && (
+          {project.discord && (
             <svg className="icon" height="20" width="20">
               <use xlinkHref="#icon-discord"></use>
             </svg>
           )}
         </div>
         <div className="tile-series">
-          {props.series.map((serie, i) => (
+          {project.series.map((serie, i) => (
             <div key={i} className={`label-series ${serie.toLowerCase()}`}>
               <svg className="icon icon-series" height="20" width="20">
                 <use xlinkHref={`#icon-${serie.toLowerCase()}`}></use>
