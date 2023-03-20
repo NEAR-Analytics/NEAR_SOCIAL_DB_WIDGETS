caac06ec73b55883018353cd065a06b94c9fc989
wpdas <wpdas>
2023-03-20 01:41:26 -0400

    Update NearSocialBridgeWithExternalApp_Test by wendersonpires.near at 2023-03-20 01:41:26.540

--- wendersonpires.near/NearSocialBridgeWithExternalApp_Test/source_code.jsx
+++ wendersonpires.near/NearSocialBridgeWithExternalApp_Test/source_code.jsx
@@ -1,154 +1,83 @@
-// INITIAL PROPS [EDIT]:
-const externalAppUrl = "https://6fa4294326de.ngrok.app/";
-const env = "development"; // Possible values: 'development' | 'production'
+// Load React, React Dom and the Core Bridge library
+const code = `
+<script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
+<div id="bridge-root"></div>
+<script src="https://unpkg.com/near-social-bridge@1.0.0-beta/bridge.min.js" crossorigin></script>
+`;
 
-// SETUP: [Navigation] Get the initial route / path (optional)
-// path: e.g "https://near.social/#/wendersonpires.near/widget/MyWidget?path=/"
+// External App Url
+const externalAppUrl = "https://6fa4294326de.ngrok.app/";
+
+// Initial Path
 const initialPath = props.path;
 
-let sessionStorageClone = {};
+// Initial State
+State.init({
+  iframeHeight: 800,
+  currentMessage: { type: "connect-view", externalAppUrl, initialPath },
+});
 
-// SETUP:  message sender (it will re-send the message to the iframe source)
+// Message sender
 const sendMessage = (message) => {
   State.update({
     currentMessage: message,
   });
 };
 
-// SETUP: Answer Factory
+// Answer Factory
 const buildAnswer = (requestType, payload) => {
   return {
+    from: "view",
     type: "answer",
     requestType,
     payload,
-  };
-};
-
-// SETUP: Set initial state
-State.init({
-  // SETUP:  message sender (it will re-send the message to the iframe source)
-  currentMessage: {},
-  connectMessageSent: false,
-  // Possible values: 'development' | 'production'
-  env,
-  renderIframe: true,
-  // SETUP: Iframe height
-  iframeHeight: 720,
-  // SETUP: [Session Storage] It'll always have the same data provided inside the external app
-  sessionStorageClone: {},
-});
-
-// SETUP: Connect Payload data sent once the connection is established
-const accountId = props.accountId ?? context.accountId ?? "*";
-const profileInfo = props.profile ?? Social.getr(`${accountId}/profile`);
-const createConnectionPayload = () => {
-  // Return the connect payload
-  return {
-    type: "connect",
     created_at: Date.now(),
-    payload: {
-      // SETUP: initial path (tell the external app witch route should be rendered first)
-      initialPath,
-      // additional data below (optional)
-      accountId,
-      ipfsCidAvatar: profileInfo.image?.ipfs_cid,
-    },
   };
 };
-const welcomePayload = createConnectionPayload();
 
-// Wait a bit to send the "connect" payload only after all the scripts are loaded
-setTimeout(() => {
-  State.update({ connectMessageSent: true });
-  if (!state.connectMessageSent) {
-    sendMessage(welcomePayload);
-
-    // If in Development mode, check the connection every 2 seconds.
-    // This is because the external app can be reloaded when the developer
-    // makes changes to it and the connection is lost
-    if (state.env === "development") {
-      setInterval(() => {
-        sendMessage(createConnectionPayload());
-      }, 2000);
-    }
-  }
-}, 0);
-
-// SETUP: On message handler. On get message handler (from the External App)
 const onMessageHandler = (message) => {
-  // Internal Request Handler
-  internalRequestHandler(message);
-
-  // Custom Request Handler
-  customRequestHandler(message);
+  requestsHandler(message);
 };
 
-const internalRequestHandler = (message) => {
+// REQUEST HANDLERS BELOW
+const requestsHandler = (message) => {
   switch (message.type) {
-    case "nsb:session-storage:hydrate-viewer":
-      sessionStorageHydrateViewer(message.type, message.payload);
-      break;
-    case "nsb:session-storage:hydrate-app":
-      sessionStorageHydrateApp(message.type, message.payload);
-      break;
     case "nsb:navigation:sync-content-height":
-      syncContentHeight(message.type, message.payload);
+      setIframeHeight(message.type, message.payload);
+      break;
+    case "get-user-info":
+      sendUserInfo(message.type, message.payload);
       break;
   }
 };
 
-const sessionStorageHydrateViewer = (requestType, payload) => {
-  if (payload) {
-    State.update({ sessionStorageClone: payload });
-  }
-
-  const responseBody = buildAnswer(requestType);
-  sendMessage(responseBody);
+// [DON'T REMOVE]: Set thew new iFrame height based on the new screen/route
+const setIframeHeight = (requestType, payload) => {
+  State.update({ iframeHeight: payload.height + 20 });
 };
 
-const sessionStorageHydrateApp = (requestType, payload) => {
-  const responseBody = buildAnswer(requestType, state.sessionStorageClone);
-  sendMessage(responseBody);
-};
-
-const syncContentHeight = (requestType, payload) => {
-  if (payload.height) {
-    State.update({ iframeHeight: payload.height });
-  }
-
-  const responseBody = buildAnswer(requestType);
-  sendMessage(responseBody);
-};
-
-// SETUP: Custom Request Handlers Below
-const customRequestHandler = (message) => {
-  switch (message.type) {
-    case "get-updated-user-info":
-      sendUpdatedUserInfo(message.type, message.payload);
-      break;
-  }
-};
-
-const sendUpdatedUserInfo = (requestType, payload) => {
-  const updatedUserInfo = buildAnswer(requestType, {
+// [E.g]Get user info
+const sendUserInfo = (requestType, payload) => {
+  const accountId = context.accountId ?? "*";
+  const profileInfo = Social.getr(`${accountId}/profile`);
+  const responseBody = buildAnswer(requestType, {
     accountId,
     profileInfo,
   });
-  sendMessage(updatedUserInfo);
+  sendMessage(responseBody);
 };
+// REQUEST HANDLERS ABOVE
 
 return (
-  <iframe
-    id="main-iframe"
-    className="w-100"
-    style={{
-      height: `${state.iframeHeight}px`,
-    }}
-    // Load external app
-    src={externalAppUrl}
-    // Data the Near Social View is going to send to the External App
-    message={state.currentMessage}
-    // When the external app send a message back to the NS View
-    onMessage={onMessageHandler}
-  />
+  <div>
+    <iframe
+      className="w-100"
+      style={{ height: `${state.iframeHeight}px` }}
+      srcDoc={code}
+      message={state.currentMessage}
+      onMessage={onMessageHandler}
+    />
+    <Markdown text={state.m} />
+  </div>
 );
