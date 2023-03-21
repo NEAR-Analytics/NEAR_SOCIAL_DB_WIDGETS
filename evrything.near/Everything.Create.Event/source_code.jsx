1e7d21c550552ab648dfa7e7a377ff3587e2b7c7
evrything.near <evrything.near>
2023-03-20 21:32:09 -0400

    Update Everything.Create.Event by evrything.near at 2023-03-20 21:32:09.751

--- evrything.near/Everything.Create.Event/source_code.jsx
+++ evrything.near/Everything.Create.Event/source_code.jsx
@@ -1,1 +1,48 @@
-const Header = styled.div` width: 100%; display: flex; flex-direction: row; justify-content: space-between; align-items: center; `; const Title = styled.div` font-size: 24px; line-height: 33.6px; `; const Form = styled.div` display: flex; flex-direction: column; gap: 4px; `; const Input = styled.input` width: 100%; `; const TextArea = styled.textarea` width: 100%; `; const ButtonRow = styled.div` display: flex; flex-direction: row; gap: 4px; `; const typeStr = "evrything.near/type/Event"; const type = Type.get(typeStr); State.init({"title":"","description":"","date":""}); function composeData() { const data = { thing: { main: JSON.stringify(state), }, index: { everythingv0: JSON.stringify({ key: "main", value: { type: typeStr, }, }), }, }; return data; } return ( <> <Header> <Title>{typeStr.split("/")[2]}</Title> </Header> <Form><Input placeholder="title" onChange={({ target }) => State.update({ "title": target.value })} /> <TextArea placeholder="description" onInput={({ target }) => State.update({ "description": target.value })} /> <Input placeholder="date" onChange={({ target }) => State.update({ "date": target.value })} /><ButtonRow> <CommitButton force data={composeData} onCommit={resetThing} > create </CommitButton> </ButtonRow> </Form> </> );
+const Header = styled.div` width: 100%; display: flex; flex-direction: row; justify-content: space-between; align-items: center; `;
+const Title = styled.div` font-size: 24px; line-height: 33.6px; `;
+const Form = styled.div` display: flex; flex-direction: column; gap: 4px; `;
+const Input = styled.input` width: 100%; `;
+const TextArea = styled.textarea` width: 100%; `;
+const ButtonRow = styled.div` display: flex; flex-direction: row; gap: 4px; `;
+const typeStr = "evrything.near/type/Event";
+const type = Type.get(typeStr);
+State.init({ title: "", description: "", date: "" });
+function composeData() {
+  const data = {
+    thing: { main: JSON.stringify(state) },
+    index: {
+      tempeverything: JSON.stringify({ key: "main", value: { type: typeStr } }),
+    },
+  };
+  return data;
+}
+return (
+  <>
+    {" "}
+    <Header>
+      {" "}
+      <Title>{typeStr.split("/")[2]}</Title>{" "}
+    </Header>{" "}
+    <Form>
+      <Input
+        placeholder="title"
+        onChange={({ target }) => State.update({ title: target.value })}
+      />{" "}
+      <TextArea
+        placeholder="description"
+        onInput={({ target }) => State.update({ description: target.value })}
+      />{" "}
+      <Input
+        placeholder="date"
+        onChange={({ target }) => State.update({ date: target.value })}
+      />
+      <ButtonRow>
+        {" "}
+        <CommitButton force data={composeData} onCommit={resetThing}>
+          {" "}
+          create{" "}
+        </CommitButton>{" "}
+      </ButtonRow>{" "}
+    </Form>{" "}
+  </>
+);
