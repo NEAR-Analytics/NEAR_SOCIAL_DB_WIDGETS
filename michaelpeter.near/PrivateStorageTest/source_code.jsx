8c3eb2e29bf5d35d4bb91e484972672bd7c21d46
mpeterdev <mpeterdev>
2023-03-20 20:35:23 -0400

    Update PrivateStorageTest by michaelpeter.near at 2023-03-20 20:35:23.856

---
+++ michaelpeter.near/PrivateStorageTest/source_code.jsx
@@ -0,0 +1,17 @@
+State.init({ message: "" });
+const storedMessage = Storage.privateGet("message");
+return (
+  <div className="d-flex flex-column gap-2">
+    <span>
+      Message: <span>{storedMessage}</span>
+    </span>
+    <input type="text" placeholder="message" value={state.message} />
+    <button
+      onClick={() => {
+        Storage.privateSet("message", state.message);
+      }}
+    >
+      Save
+    </button>
+  </div>
+);
