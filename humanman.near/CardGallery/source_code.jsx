00f89b3651ee9be7fe49eb57f3e22290188750b9
humanman <humanman>
2023-03-21 16:21:11 -0400

    Update CardGallery by humanman.near at 2023-03-21 16:21:11.213

--- humanman.near/CardGallery/source_code.jsx
+++ humanman.near/CardGallery/source_code.jsx
@@ -1,24 +1,6 @@
 // propagates props to child widget SimpleCard to create card gallery
 const user = "humanman.near";
 
-// const Card = styled.button`
-//   display: flex;
-//   flex-direction: column;
-//   background-color: white;
-//   justify-content: center;
-//   align-items: center;
-//   width: 300px;
-//   height: 300px;
-//   border: 1px solid rgba(0, 0, 0, 0.15);
-//   border-radius: 8px;
-//   color: black;
-//   cursor: pointer;
-
-//   &:hover {
-//     background-color: #e9ecef;
-//   }
-// `;
-
 const Gallery = styled.div`
     display: flex;
     justify-content: center;
@@ -35,24 +17,6 @@ return (
           props={{ img, link, title, description }}
         />
       );
-      //   return (
-      //     <Card>
-      //       <img style={{ maxWidth: "80%", margin: "auto" }} src={img} />
-      //       <a href={link}>
-      //         <h4>{title}</h4>
-      //         <hr />
-      //         <p style={{ textAlign: "center" }}>{description}</p>
-      //       </a>
-      //     </Card>
-      //   );
     })}
   </Gallery>
 );
-
-// return (
-//   <div>
-//     {props.cardData.map((data) => {
-//       <Widget src={`${user}/widget/SimpleCard`} props={data} />;
-//     })}
-//   </div>
-// );
