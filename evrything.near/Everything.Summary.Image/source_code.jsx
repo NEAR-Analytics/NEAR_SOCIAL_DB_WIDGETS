466c5a7f43667e3a8027baf683f3c8ca67239877
evrything.near <evrything.near>
2023-03-20 22:02:33 -0400

    Update Everything.Summary.Image by evrything.near at 2023-03-20 22:02:33.223

--- evrything.near/Everything.Summary.Image/source_code.jsx
+++ evrything.near/Everything.Summary.Image/source_code.jsx
@@ -1,5 +1,5 @@
 const data = props.data;
-const Card = styled.img`height: 300px; width: 300px; background-color: white; padding: 12px; margin: 8px; border-radius: 22px; box-shadow: 5px 5px 5px gray; border: solid gray; `;
+const Card = styled.img`height: 300px; width: 300px; background-color: white; margin: 8px; border-radius: 22px; box-shadow: 5px 5px 5px gray; `;
 return (
   <Card src={`https://ipfs.near.social/ipfs/${data.img.cid}`} alt={"image"} />
 );
