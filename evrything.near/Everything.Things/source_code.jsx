5f26df716de3ac5991bc368cd9c1641af76784a9
evrything.near <evrything.near>
2023-03-20 20:54:05 -0400

    Update Everything.Things by evrything.near at 2023-03-20 20:54:05.526

--- evrything.near/Everything.Things/source_code.jsx
+++ evrything.near/Everything.Things/source_code.jsx
@@ -5,7 +5,7 @@ if (!type) {
 }
 
 const index = {
-  action: "everythingv0", // this could work as a sort of "domain"...
+  action: "tempeverything", // this could work as a sort of "domain"...
   key: "main",
   options: {
     limit: 10,
