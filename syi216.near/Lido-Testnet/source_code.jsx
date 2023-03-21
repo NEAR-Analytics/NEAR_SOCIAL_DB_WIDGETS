9a08b02525781dd0b4c4cd5b15bdb23192d82c01
syi216.near <syi216.near>
2023-03-21 19:46:59 -0400

    Update Lido-Testnet by syi216.near at 2023-03-21 19:46:59.849

--- syi216.near/Lido-Testnet/source_code.jsx
+++ syi216.near/Lido-Testnet/source_code.jsx
@@ -5,6 +5,8 @@ const lidoContract = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F";
 const mainnetLidoContract = "0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f";
 const gorliLidoContract = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
 const tokenDecimals = 18;
+const contract = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2";
+
 //const sushiTokenContract = "0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f";
 
 //const network = "gorli"; // "gorli" // "rinkeby" // "mainnet"
@@ -24,6 +26,21 @@ switch (network) {
     lidoContract = mainnetLidoContract;
     break;
 }
+
+const handleSelect = (data) => {
+  console.log(data.target.value);
+  let info = data.target.value.split("-");
+  State.update({ tokenTo: info[1] });
+  if (info[0] == "sushi") {
+    State.update({ tokenSelected: 0 });
+  } else if (info[0] == "usdt") {
+    State.update({ tokenSelected: 1 });
+  } else if (info[0] == "near") {
+    State.update({ tokenSelected: 2 });
+  }
+  console.log(state.tokenSelected);
+  contract = data.target.value;
+};
 
 const lidoAbi = fetch(
   "https://raw.githubusercontent.com/cloudmex/sushiswap-bos/main/abi-sushi.json"
@@ -72,6 +89,7 @@ const iface = new ethers.utils.Interface(lidoAbi.body);
 
 const submitEthers = (strEther, _referral) => {
   if (!strEther) {
+    console.log("contrato: ", state.tokenTo);
     return console.log("Amount is missing");
   }
 
@@ -89,10 +107,9 @@ const submitEthers = (strEther, _referral) => {
   //const usdtContract = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
   //const sushiMainContract = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2"
 
-  let ARR = [
-    "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2",
-  ];
+  let ARR = ["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", state.tokenTo];
+
+  console.log("contract: ", contract);
 
   console.log("amountlol", amount);
   console.log("ARRlol", ARR);
@@ -102,7 +119,7 @@ const submitEthers = (strEther, _referral) => {
   );
   console.log("Sender:", state.sender);
   console.log("block.timestamp + 60 1678849571");
-
+  console.log("token to swap: ", state.tokenTo);
   erc20
     .swapExactETHForTokens(
       amount,
@@ -132,6 +149,8 @@ if (state.sender === undefined) {
 // FETCH SENDER BALANCE
 
 if (state.balance === undefined && state.sender) {
+  State.update({ tokenTo: "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2" });
+  State.update({ tokenSelected: 0 });
   Ethers.provider()
     .getBalance(state.sender)
     .then((balance) => {
@@ -186,9 +205,381 @@ if (state.stakedBalance === undefined && state.sender) {
 const cssFont = fetch(
   "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800"
 ).body;
-const css = fetch(
-  "https://pluminite.mypinata.cloud/ipfs/Qmboz8aoSvVXLeP5pZbRtNKtDD3kX5D9DEnfMn2ZGSJWtP"
-).body;
+// const css = fetch(
+//   "https://pluminite.mypinata.cloud/ipfs/Qmboz8aoSvVXLeP5pZbRtNKtDD3kX5D9DEnfMn2ZGSJWtP"
+// ).body;
+const css = `
+.LidoContainer{
+    box-sizing: border-box;
+    margin: 0px auto;
+    min-width: 320px;
+    width: 100%;
+    padding: 0px 32px;
+    max-width: 560px;
+    position: relative;
+    margin-top: 8px;
+    margin-bottom: 8px;
+}
+
+.Header{
+    font-weight: 800;
+    font-size: 26px;
+    margin-bottom: 0.2em;
+    line-height: 1.2em;
+    text-align: center;
+}
+
+.SubHeader{
+    font-weight: 500;
+    color: #FF79D8;
+    margin-bottom: 16px;
+    font-size: 12px;
+    line-height: 1.5em;
+    text-align: center;
+};
+
+.LidoForm{
+    background: linear-gradient(65.21deg, rgb(223, 39, 270) 19.1%, rgb(255, 135, 220) 100%);
+    margin-bottom: -20px;
+    border-bottom-left-radius: 0px;
+    border-bottom-right-radius: 0px;
+    padding-bottom: 52px;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 1.6em;
+    border-radius: 20px;
+    margin: 0px;
+    padding: 32px;
+    box-shadow: none;
+    color: #fff;    
+}
+
+.LidoFormTopContainer{
+    margin-top: 0px;
+    display: flex;
+    margin: 20px 0px;
+}
+
+.LidoFormTopContainerLeft{
+    margin-right: 18px;
+    flex-basis: 50%;
+    -webkit-box-flex: 1;
+    flex-grow: 1;
+    font-size: 12px;
+    line-height: 1.6em;
+}
+
+.LidoFormTopContainerLeftContent1{
+    display: flex;
+    flex-direction: row;
+    -webkit-box-pack: start;
+    justify-content: flex-start;
+    -webkit-box-align: center;
+    align-items: center;
+}
+
+.LidoFormTopContainerLeftContent1Container{
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+}
+
+.LidoFormTopContainerLeftContent1Circle{
+    background-color: #53BA95;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    margin-left: 8px;
+}
+
+.LidoFormTopContainerLeftContent2{
+    margin-top: 2px;
+    font-size: 18px;
+    line-height: 1.4em;
+    font-weight: 800;
+    white-space: nowrap;
+    display: block;
+}
+
+.LidoFormTopContainerRight{
+    align-self: stretch;
+    display: flex;
+    flex: 1 1 50%;
+    -webkit-box-flex: 1;
+    overflow: hidden;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: end;
+    justify-content: flex-end;
+    margin-left: auto;
+}
+
+.LidoFormTopContainerRightContent1{
+    max-width: 100%;
+    box-sizing: border-box;
+    overflow: hidden;
+    margin: 0px;
+    border-radius: 1000px;
+    padding: 4px;
+    display: inline-flex;
+    -webkit-box-align: center;
+    align-items: center;
+    background: rgba(0, 0, 0, .2);
+    color: #fff;    
+}
+
+.LidoFormTopContainerRightContent1Text{
+    padding: 0px 6px;
+    font-weight: 400;
+}
+
+.LidoSplitter{
+    box-sizing: border-box;
+    list-style: none;
+    opacity: 0.1;
+    padding: 0px;
+    flex-shrink: 0;
+    -webkit-box-flex: 0;
+    flex-grow: 0;
+    border-top: 1px solid currentcolor;
+    width: 100%;
+    height: 0px;
+    margin: 0px;
+}
+
+.LidoFormBottomContainer{
+    margin-bottom: 0px;
+    display: flex;
+    margin: 20px 0px;
+}
+
+.LidoAprContainer{
+    margin-right: 0px;
+    flex-basis: 50%;
+    -webkit-box-flex: 1;
+    flex-grow: 1;
+    font-size: 12px;
+    line-height: 1.6em;
+}
+
+.LidoAprTitle{
+    display: flex;
+    flex-direction: row;
+    -webkit-box-pack: start;
+    justify-content: flex-start;
+    -webkit-box-align: center;
+    align-items: center;
+}
+
+.LidoAprValue{
+    margin-top: 2px;
+    font-size: 16px;
+    line-height: 1.4em;
+    font-weight: 800;
+    white-space: nowrap;
+    color: rgb(97, 183, 95);
+    font-size: 16px;
+    line-height: 1.4em;
+    font-weight: 800;
+    white-space: nowrap;
+}        
+}
+
+.LidoStakeForm{
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 1.6em;
+    border-radius: 0px 0px 20px 20px;
+    margin: 0px;
+    padding: 32px;
+    box-shadow: none;
+    background: #eee;
+    color: #7a8aa0;
+    margin-top: -30px;
+}
+
+.LidoStakeFormInputContainer{
+    margin-bottom: 16px;
+    z-index: 2;
+    position: relative;
+    display: inline-flex;
+    border-width: 1px;
+    border-style: solid;
+    border-image: initial;
+    border-radius: 10px;
+    -webkit-box-align: stretch;
+    align-items: stretch;
+    box-sizing: border-box;
+    padding: 0px 15px;
+    cursor: text;
+    transition: border-color 100ms ease 0s;
+    width: 100%;
+    background: #fff;
+    border-color: rgba(0,10,61,.12);
+    color: #273852;
+}
+
+.LidoStakeFormInputContainerSpan1{
+    -webkit-box-flex: 0;
+    flex-grow: 0;
+    flex-shrink: 0;
+    cursor: inherit;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    padding-right: 16px;
+}
+
+.selectCSS{
+    font-weight: 400;
+    font-size: 14px;
+    display: flex;
+    border: none;
+    -webkit-box-flex: 1;
+    flex-grow: 1;
+    position: relative;
+    padding: 17px 0px;
+    color: #273852;
+}
+
+.LidoStakeFormInputContainerSpan2{
+    font-weight: 400;
+    font-size: 14px;
+    display: flex;
+    -webkit-box-flex: 1;
+    flex-grow: 1;
+    position: relative;
+    padding: 17px 0px;
+}
+
+.LidoStakeFormInputContainerSpan2Input{
+    width: 100%;
+    font-family: inherit;
+    font-weight: 400;
+    font-size: 1em;
+    line-height: 1.43em;
+    padding: 0px;
+    border-radius: 0px;
+    background: transparent;
+    box-shadow: none;
+    border: none;
+    outline: none;
+    position: relative;
+    top: 0px;
+    color: #273852;
+}
+
+.LidoStakeFormInputContainerSpan3{
+    -webkit-box-flex: 0;
+    flex-grow: 0;
+    flex-shrink: 0;
+    cursor: inherit;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    padding-left: 16px;
+}
+
+.LidoStakeFormInputContainerSpan3Content{
+    cursor: pointer;
+    letter-spacing: 0.4px;
+    box-sizing: border-box;
+    margin: 0px;
+    border: none;
+    outline: none;
+    white-space: nowrap;
+    overflow: hidden;
+    position: relative;
+    background: transparent;
+    font-family: inherit;
+    font-weight: 700;
+    width: auto;
+    line-height: 1em;
+    font-size: 10px;
+    border-radius: 6px;
+    padding: 11px 16px;
+    min-width: 50px;
+    color: #fa7aad;
+    &::before {
+            display: block;
+            background-color: #f72277;
+            transition: opacity 100ms ease 0s;
+            opacity: 0.1;
+            content: "";
+            position: absolute;
+            inset: 0px;
+            pointer-events: none;
+            border-radius: inherit;
+        }    
+}
+
+.LidoStakeFormInputContainerSpan3Max{
+    position: relative;
+    pointer-events: none;
+    visibility: visible;
+}
+
+.LidoStakeFormSubmitContainer{
+    cursor: pointer;
+    box-sizing: border-box;
+    margin: 0px;
+    border: none;
+    outline: none;
+    white-space: nowrap;
+    overflow: hidden;
+    position: relative;
+    background-image: initial;
+    background-position: initial;
+    background-size: initial;
+    background-repeat: initial;
+    background-attachment: initial;
+    background-origin: initial;
+    background-clip: initial;
+    font-family: inherit;
+    font-weight: 700;
+    width: 100%;
+    line-height: 1em;
+    font-size: 14px;
+    border-radius: 10px;
+    padding: 21px 44px;
+    min-width: 120px;
+    color: #fff;
+    background-color: #fa7aad;
+    transition: background-color 100ms ease 0s;
+    &:hover {
+        background-color: ##f72277;
+    }
+}
+
+.LidoFooterContainer{
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 1.6em;
+}
+
+.LidoFooterRaw{
+    margin-top: 0px;
+    display: flex;
+    margin: 16px 0px;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 1.6em;
+}
+
+.LidoFooterRawLeft{
+    color: #7a8aa0;
+    -webkit-box-flex: 1;
+    flex-grow: 1;
+}
+
+.LidoFooterRawRight{
+    color: #273852;
+    text-align: right;
+    margin-left: 32px;
+    -webkit-box-flex: 1;
+    flex-grow: 1;
+}
+`;
 
 if (!cssFont || !css) return "";
 
@@ -216,7 +607,7 @@ const getSender = () => {
 return (
   <Theme>
     <div class="LidoContainer">
-      <div class="Header">Swap $ETH &lt;&gt; $SUSHI</div>
+      <div class="Header">Swap $ETH &lt;&gt; $SUSHI/$USDT/$NEAR</div>
       <div class="SubHeader">Swap ETH and receive SUSHI</div>
 
       <div class="LidoForm">
@@ -239,7 +630,9 @@ return (
               <div class="LidoFormTopContainerRight">
                 <div class="LidoFormTopContainerRightContent1">
                   <div class="LidoFormTopContainerRightContent1Text">
-                    <span>{getSender()}</span>
+                    <span>
+                      <b>Account:</b> {getSender()}
+                    </span>
                   </div>
                 </div>
               </div>
@@ -249,6 +642,28 @@ return (
         )}
       </div>
       <div class="LidoStakeForm">
+        <div class="mb-2 LidoStakeFormInputContainer">
+          <select
+            name="select"
+            id="token"
+            class="selectCSS"
+            onChange={handleSelect}
+          >
+            <option
+              value="sushi-0x6b3595068778dd592e39a122f4f5a5cf09c90fe2"
+              data-image="https://www.spectre.ai/assets/images/assets/SUSHI-logo.png?v=2.13"
+            >
+              SUSHI
+            </option>
+            <option value="usdt-0xdAC17F958D2ee523a2206206994597C13D831ec7">
+              USDT
+            </option>
+            <option value="near-0x85F17Cf997934a597031b2E18a9aB6ebD4B9f6a4">
+              NEAR
+            </option>
+          </select>
+        </div>
+
         <div class="LidoStakeFormInputContainer">
           <span class="LidoStakeFormInputContainerSpan1">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
@@ -311,27 +726,6 @@ return (
             connectLabel="Connect with Web3"
           />
         )}
-
-        <div class="LidoFooterContainer">
-          {state.sender && (
-            <div class="LidoFooterRaw">
-              <div class="LidoFooterRawLeft">You will receive</div>
-              <div class="LidoFooterRawRight">${state.strEther ?? 0} stETH</div>
-            </div>
-          )}
-          <div class="LidoFooterRaw">
-            <div class="LidoFooterRawLeft">Exchange rate</div>
-            <div class="LidoFooterRawRight">1 ETH = 1 stETH</div>
-          </div>
-          <div class="LidoFooterRaw">
-            <div class="LidoFooterRawLeft">Transaction cost</div>
-            <div class="LidoFooterRawRight">{state.txCost}</div>
-          </div>
-          <div class="LidoFooterRaw">
-            <div class="LidoFooterRawLeft">Reward fee</div>
-            <div class="LidoFooterRawRight">10%</div>
-          </div>
-        </div>
       </div>
     </div>
   </Theme>
