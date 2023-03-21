cf704143e1ca1f3496dd7423326cbe26e35339b3
humanman <humanman>
2023-03-21 18:11:36 -0400

    Update FoundersResources by humanman.near at 2023-03-21 18:11:36.383

--- humanman.near/FoundersResources/source_code.jsx
+++ humanman.near/FoundersResources/source_code.jsx
@@ -1,44 +1,381 @@
 const user = "humanman.near";
 
 const props = {
-  growth: [
+  bdBasics: [
     {
-      title: "Hello",
-      description: "crypsom lorem ethereum bitcoin accelerator",
-      link: "https://google.com",
-      img: "https://cryptobootcampassets.s3.amazonaws.com/mod-em-4.3.3.png",
+      title: "How to Achieve Product Market Fit",
+      description:
+        "No-nonsense guide to the product-market fit meaning, why it’s critical, and how to get it.",
+      link: "https://forwardpartners.com/growth-playbooks/how-to-achieve-product-market-fit",
+      img: "",
+      tags: ["business fundamentals", "essentials"],
     },
-  ],
-  technical: [
     {
-      title: "Hello",
-      description: "crypsom lorem ethereum bitcoin accelerator",
-      link: "https://google.com",
-      img: "https://cryptobootcampassets.s3.amazonaws.com/mod-em-4.3.3.png",
+      title: "Essential Startup Advice",
+      description:
+        "Y Combinator's collection of tips they consider the most important, most transformative advice for startups",
+      link: "https://www.ycombinator.com/library/4D-yc-s-essential-startup-advice",
+      img: "",
+      tags: ["business fundamentals", "essentials"],
     },
-  ],
-  hr: [
     {
-      title: "Hello",
-      description: "crypsom lorem ethereum bitcoin accelerator",
-      link: "https://google.com",
-      img: "https://cryptobootcampassets.s3.amazonaws.com/mod-em-4.3.3.png",
+      title: "Perfecting Your One Sentence Pitch",
+      description:
+        "Founder Institute's simple one-sentence pitch format to grab the listener's attention",
+      link: "https://fi.co/one-sentence-pitch",
+      img: "",
+      tags: ["business fundamentals", "essentials"],
+    },
+    {
+      title: "Benchmarks Guide",
+      description:
+        "Metrics providing a solid baseline for entrepreneurs planning out their fundraising and growth strategy.",
+      link: "https://fi.co/benchmarks",
+      img: "",
+      tags: ["business fundamentals", "essentials"],
+    },
+    {
+      title: "66 Biggest Mistakes First Time Founders Make",
+      description:
+        "Y Combinator Managing Director and Group Partner Michael Seibel on the biggest mistakes first-time founders make.",
+      link: "https://www.ycombinator.com/library/66-biggest-mistakes-first-time-founders-make",
+      img: "",
+      tags: ["business fundamentals", "general advice"],
+    },
+    {
+      title: "How to Generate and Validate Startup Ideas",
+      description:
+        "Get the tools and resources that will help you develop great startup ideas.",
+      link: "https://fi.co/startup-idea-validation",
+      img: "",
+      tags: ["business fundamentals", "general advice"],
+    },
+    {
+      title: "Entrepreneur DNA Assessment",
+      description:
+        "Founder Institute's self-assessment for becoming a successful founder of a fast-growth company",
+      link: "https://fi.co/dna",
+      img: "",
+      tags: ["business fundamentals", "general advice"],
+    },
+    {
+      title: "How to Plan an MVP",
+      description:
+        "Y Combinator Managing Director Michael Seibel shares his approach to building an MVP and getting your first users",
+      link: "https://www.ycombinator.com/library/6f-how-to-plan-an-mvp",
+      img: "",
+      tags: ["business fundamentals", "mvp"],
+    },
+    {
+      title: "Startup Ecosystem Canvas",
+      description:
+        "Template for plotting our your local ecosystem to help newcomers",
+      link: "https://fi.co/canvas_template",
+      img: "",
+      tags: ["business fundamentals", "mvp"],
+    },
+    {
+      title: "How to Design a Better Pitch Deck",
+      description: "Here’s how to make a solid Demo Day slide deck.",
+      link: "https://www.ycombinator.com/library/4T-how-to-design-a-better-pitch-deck",
+      img: "",
+      tags: ["business fundamentals", "pitching"],
+    },
+    {
+      title: "SAFE Template",
+      description:
+        "SAFE (simple agreement for future equity) for early-stage fundraising",
+      link: "https://www.ycombinator.com/library/6z-fundraising-templates-safe-financing-documents",
+      img: "",
+      tags: ["business fundamentals", "fundraising"],
     },
   ],
   legal: [
     {
-      title: "Hello",
-      description: "crypsom lorem ethereum bitcoin accelerator",
-      link: "https://google.com",
-      img: "https://cryptobootcampassets.s3.amazonaws.com/mod-em-4.3.3.png",
+      title: "CavenwellDAO",
+      description: "The bridge between Web 3.0 and traditional legal services",
+      link: "https://cavenwell.io/",
+      img: "",
+      tags: ["legal", "services", "DAO"],
+    },
+    {
+      title: "Legal Nodes",
+      description:
+        "Connecting founders with multiple legal professionals, all in one place.",
+      link: "https://legalnodes.com/article/legal-structure-decentralized-apps",
+      img: "",
+      tags: ["legal", "services"],
+    },
+    {
+      title: "DAObox",
+      description:
+        "Provides a legal framework for DAOs including establishment, accounting, and payments",
+      link: "https://www.google.com/url?q=https%3A%2F%2Fdaobox.org%2F&sa=D&sntz=1&usg=AOvVaw30NfKF0u-wJIce2Kn_SvB3",
+      img: "",
+      tags: ["legal", "DAO", "services"],
     },
   ],
-  bdBasics: [
+  hr: [
     {
-      title: "Hello",
-      description: "crypsom lorem ethereum bitcoin accelerator",
-      link: "https://google.com",
-      img: "https://cryptobootcampassets.s3.amazonaws.com/mod-em-4.3.3.png",
+      title: "NEAR Careers Portal",
+      description:
+        "Discover open roles available within the NEAR Ecosystem, and you can also post open roles within your team as well",
+      link: "https://careers.near.org/jobs",
+      img: "",
+      tags: ["hr", "recruitment"],
+    },
+    {
+      title: "BEExperience",
+      description:
+        "The HUB of education and recruitment within Web3 providing services to hiring entities, as well as individuals",
+      link: "https://beexperience.io/",
+      img: "",
+      tags: ["hr", "recruitment"],
+    },
+    {
+      title: "Black Freelancer",
+      description:
+        "BlackFreelancer is a hub for black creatives across the globe to showcase their work and get hired",
+      link: "https://www.blackfreelancer.com/",
+      img: "",
+      tags: ["hr", "recruitment"],
+    },
+  ],
+  growth: [
+    {
+      title: "Project Growth & Marketing Toolkit",
+      description:
+        "In-depth overview of best practices and proven strategies for growth & marketing your project in the web3/crypto space",
+      link: "https://docs.google.com/presentation/d/1Abjnbw6qNSC7hu3vAqWqo9hn5pOArakIfU9ZRR0SQTI/edit?usp=sharing",
+      img: "",
+      tags: ["growth&marketing", "ecosystem"],
+    },
+    {
+      title: "NEAR Ecosystem Marketing Activation Workflow",
+      description:
+        "Enables new projects entering the NEAR Ecosystem to request marketing support",
+      link: "https://drive.google.com/file/d/1NsMDzKxFHoKopgEOpf2JGVXI8NzvDrsc/view",
+      img: "",
+      tags: ["growth&marketing", "ecosystem"],
+    },
+    {
+      title: "NEAR Medium publication Guidelines",
+      description: "Top stories & project launches",
+      link: "https://docs.google.com/document/d/1MNy9PikQWHLiXQyDs-E7ijeA79rsBZ2TY3wzh6GGG08/edit?usp=sharing",
+      img: "",
+      tags: ["growth&marketing", "ecosystem"],
+    },
+    {
+      title: "NEAR Week",
+      description:
+        "The largest marketing & news platform in the NEAR ecosystem",
+      link: "https://nearweek.com/",
+      img: "",
+      tags: ["growth&marketing", "ecosystem"],
+    },
+    {
+      title: "NearStarter",
+      description:
+        "Incubating, accelerating, and funding state-of-the-art projects deploying on the NEAR Protocol",
+      link: "https://nearstarter.fi/",
+      img: "",
+      tags: ["growth&marketing", "ecosystem", "accelerator"],
+    },
+    {
+      title: "NEAR Insider",
+      description:
+        "NEAR ecosystem focused digital marketing and social media agency",
+      link: "https://drive.google.com/file/d/1MboTFLzp-8ydMgyVcww0H_BgjUpPnWOR/view",
+      img: "",
+      tags: ["growth&marketing", "ecosystem"],
+    },
+    {
+      title: "NEAR Stars",
+      description: "Promotion of NEAR ecosystem projects",
+      link: "https://t.me/nearstarsguild",
+      img: "",
+      tags: ["growth&marketing", "ecosystem", "guild", "promotion"],
+    },
+    {
+      title: "Flying Rhino",
+      link: "https://flyingrhinoweb3.com/",
+      description:
+        "Collaborate with all the developed and ongoing developing NFT dapps on NEAR. Join the discord to interact",
+      img: "",
+      tags: ["growth&marketing", "ecosystem", "nft"],
+    },
+    {
+      title: "NEAR News Twitter Accounts",
+      link: "https://twitter.com/i/lists/1590678464478208001",
+      description:
+        "Curated Twitter list of the many accounts focused on pushing out NEAR ecosystem news",
+      img: "",
+      tags: ["growth&marketing", "ecosystem"],
+    },
+    {
+      title: "NEAR Builders",
+      link: "https://docs.nearbuilders.com/community-groups/",
+      description:
+        "Community groups focused on building and growing the NEAR ecosystem",
+      img: "",
+      tags: ["growth&marketing", "community"],
+    },
+    {
+      title: "Ready Layer One",
+      link: "https://near.social/#/mob.near/widget/MyPage?accountId=readylayerone.near",
+      description:
+        "Web3 podcast focused on the NEAR ecosystem. Educating users and bringing together the community",
+      img: "",
+      tags: ["growth&marketing", "community"],
+    },
+    {
+      title: "NEAR Workshops",
+      link: "https://nearworkshops.com/watch",
+      description:
+        "Free live educational content series tailored to teaching developers the latest tooling, SDKs, and APIs across NEAR",
+      img: "",
+      tags: ["growth&marketing", "community"],
+    },
+    {
+      title: "Launchpad Guide",
+      link: "https://medium.com/nearweek/a-guide-to-launchpads-on-near-ido-edition-b3afe6659cb9",
+      description: "Guide to IDO Platforma on NEAR",
+      img: "",
+      tags: ["growth&marketing", "ido"],
+    },
+    {
+      title: "Ecosystem Calendar",
+      link: "https://nearweek.com/calendar",
+      description:
+        "Wider visibility into upcoming events, launches, Twitter Spaces, campaigns, etc... Make sure to submit yours!",
+      img: "",
+      tags: ["growth&marketing", "events"],
+    },
+    {
+      title: "MarketingDAO",
+      link: "https://docs.google.com/document/d/1i1PbFQKlwyWzjGZMoeUIM3gy3ghWKH3Yo4iOi-D8N_U/view",
+      description:
+        "Facilitates the allocation of Community funds for marketing activities under $10,000",
+      img: "",
+      tags: ["growth&marketing", "DAO", "fundraising"],
+    },
+  ],
+  technical: [
+    {
+      title: "Blockchain Operating System (BOS)",
+      description:
+        "The gateway to Web3. Build and use Web3 components for any chain or protocol",
+      link: "https://alpha.near.org",
+      img: "",
+      tags: ["technical", "dev-tools", "bos"],
+    },
+    {
+      title: "NEAR Docs",
+      link: "https://docs.google.com/document/d/1i1PbFQKlwyWzjGZMoeUIM3gy3ghWKH3Yo4iOi-D8N_U/view",
+      description: "Your entry point to using NEAR's tech stack",
+      img: "",
+      tags: ["technical", "dev-tools"],
+    },
+    {
+      title: "Pagoda",
+      link: "https://www.pagoda.co/",
+      description:
+        "The first-ever Web3 startup platform, and main core protocol contributor of NEAR Protocol",
+      img: "",
+      tags: ["technical", "dev-tools"],
+    },
+    {
+      title: "Croncat",
+      link: "https://cron.cat/",
+      description: "Decentralized Scheduling for Blockchain Transactions",
+      img: "",
+      tags: ["technical", "dev-tools"],
+    },
+    {
+      title: "Kurtosis",
+      link: "https://www.kurtosistech.com/",
+      description:
+        "Local development and testing environments for developers in the NEAR ecosystem",
+      img: "",
+      tags: ["technical", "dev-tools"],
+    },
+    {
+      title: "Calimero",
+      link: "https://www.calimero.network/",
+      description:
+        "Allowing deployment of a customisable, easy to use private shard with built-in privacy features",
+      img: "",
+      tags: ["technical", "dev-tools"],
+    },
+    {
+      title: "IPFS",
+      link: "https://ipfs.tech/",
+      description:
+        "A peer-to-peer hypermedia protocol to make the web faster, safer, and more open",
+      img: "",
+      tags: ["technical", "storage"],
+    },
+    {
+      title: "The Graph",
+      link: "https://thegraph.com/",
+      description:
+        "Developer tools to process blockchain events and make the resulting data easily available",
+      img: "",
+      tags: ["technical", "explorers&indexers"],
+    },
+    {
+      title: "NEAR Wallet Selector",
+      link: "https://github.com/near/wallet-selector",
+      description:
+        "Provides an abstraction over various wallets within the NEAR ecosystem",
+      img: "",
+      tags: ["technical", "wallets"],
+    },
+    {
+      title: "Seda (formerly Flux)",
+      link: "https://www.seda.xyz/",
+      description: "Serving as the trust-less data layer for Web3",
+      img: "",
+      tags: ["technical", "oracles"],
+    },
+    {
+      title: "BlockSec",
+      link: "https://blocksec.com/",
+      description:
+        "Building BlockChain Security Infrastructure | Smart Contract Audits",
+      img: "",
+      tags: ["technical", "security"],
+    },
+    {
+      title: "MetaBUILD",
+      link: "https://near.org/metabuild/",
+      description:
+        "The NEAR MetaBUILD hackathons will feature exciting themes focused on solving real-world problems.",
+      img: "",
+      tags: ["technical", "hackathons"],
+    },
+    {
+      title: "Stats Gallery",
+      link: "https://stats.gallery/",
+      description:
+        "Guides and tutorials from around the network! Quests. Get and complete quests to pump your account up!",
+      img: "",
+      tags: ["technical", "analytics"],
+    },
+    {
+      title: "Aurora EVM",
+      link: "https://aurora.dev/",
+      description: "Aurora is an EVM solution built on the NEAR Protocol",
+      img: "",
+      tags: ["technical", "interoperability"],
+    },
+    {
+      title: "Octopus Network",
+      link: "https://oct.network/",
+      description:
+        "A NEAR-based multichain interoperable crypto-network for launching and running Web3 substrate-based, EVM compatible Appchains",
+      img: "",
+      tags: ["technical", "interoperability"],
     },
   ],
 };
