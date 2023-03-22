e8872953ea8020ce06f884155c8ed347530a0c00
NearSocial <NearSocial>
2023-03-22 16:18:09 -0400

    Update Common.DAO.Library.Navbar by hack.near at 2023-03-22 16:18:09.329

--- hack.near/Common.DAO.Library.Navbar/source_code.jsx
+++ hack.near/Common.DAO.Library.Navbar/source_code.jsx
@@ -31,7 +31,7 @@ return (
           className={`nav-link mt-2 rounded-3${
             item.type === props.tab ? "general" : ""
           }`}
-          href={`https://near.social/#/${ownerId}/widget/Common.DAO.Library?tab=${item.id}`}
+          href={`https://near.social/#/${ownerId}/widget/Common.DAO.Library?tab=dao&id=${item.id}`}
           onClick={() => props.onSelect({ tab: "dao", id: item.id })}
         >
           {" "}
