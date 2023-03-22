f3f93ab47bc0e3ca1f97115556a9b2d9797e4fc7
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-22 11:06:59 -0400

    Update ComponentDetailsPage.Discussion.Feed by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-22 11:06:59.531

---
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/ComponentDetailsPage.Discussion.Feed/source_code.jsx
@@ -0,0 +1,34 @@
+const index = {
+  action: "c_discussion",
+  key: props.componentSrc,
+  options: {
+    limit: 10,
+    order: "desc",
+  },
+};
+
+const Post = styled.div`
+  border-bottom: 1px solid #ECEEF0;
+  padding: 24px 0 12px;
+
+  @media (max-width: 1200px) {
+    padding: 12px 0 0;
+  }
+`;
+
+const renderItem = (a) =>
+  a.value.type === "md" && (
+    <Post className="post" key={JSON.stringify(a)}>
+      <Widget
+        src="calebjacob.near/widget/Posts.Post"
+        props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
+      />
+    </Post>
+  );
+
+return (
+  <Widget
+    src="adminalpha.near/widget/IndexFeed"
+    props={{ index, renderItem, moderatorAccount: "adminalpha.near" }}
+  />
+);
