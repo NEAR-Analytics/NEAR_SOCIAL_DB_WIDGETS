7d694b292da845a0b9e8c0df5748496b3ad19454
elliotBraem <elliotBraem>
2023-03-22 16:02:27 -0400

    Update Everything.Summary.Pants by efiz.near at 2023-03-22 16:02:27.654

--- efiz.near/Everything.Summary.Pants/source_code.jsx
+++ efiz.near/Everything.Summary.Pants/source_code.jsx
@@ -1,65 +1,31 @@
 const data = props.data;
 const Card = styled.div` height: 300px; background-color: white; padding: 12px; margin: 8px; box-shadow: 5px 5px 5px blue; `;
 const Icon = styled.div` height: 24px; width: 24px; `;
-const Body = styled.div` margin-left: 12px; display: flex; flex-direction: column; justify-content: space-between; `;
+const Body = styled.div` margin-left: 12px; display: flex; flex-direction: row; gap: 8px; justify-content: space-between; `;
 const Content = styled.div` display: flex; flex-direction: column; gap: 4px; `;
 return (
   <Card>
     {" "}
     <div className="d-flex flex-row h-100">
-      {" "}
-      <Icon>
-        {" "}
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-            stroke="#292D32"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M17 12C17 14.76 14.76 17 12 17V7C14.76 7 17 9.24 17 12Z"
-            stroke="#292D32"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M12 7V17C9.24 17 7 14.76 7 12C7 9.24 9.24 7 12 7Z"
-            stroke="#292D32"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M12 22V17"
-            stroke="#292D32"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M12 7V2"
-            stroke="#292D32"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>{" "}
-      </Icon>{" "}
       <Body>
-        {" "}
+        <img
+          src={`https://ipfs.near.social/ipfs/bafybeia5jestpcujijyscwe55r2bfg2rgidbpaztrsxuyxekyb2smp3che`}
+          alt={"image"}
+          style={{ width: "200px", height: "200px" }}
+        />
         <Content>
-          <h1>{data["brand"]}</h1>
-          <p>{data["color"]}</p> <p>{data["material"]}</p>{" "}
-          <p>{data["width"]}</p> <p>{data["length"]}</p>
+          <span>
+            <strong>Brand:</strong>
+            <h1>{data["brand"]}</h1>
+          </span>
+          <span>
+            <strong>Color:</strong>
+            <p>{data["color"]}</p>
+          </span>
+          <p>{data["material"]}</p>
+          <span>
+            {data["width"]} x {data["length"]}
+          </span>
         </Content>{" "}
       </Body>{" "}
     </div>{" "}
