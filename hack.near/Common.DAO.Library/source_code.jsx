ac6db174c529cd38a63effff8866bdf35e964cf5
NearSocial <NearSocial>
2023-03-22 15:44:25 -0400

    Update Common.DAO.Library by hack.near at 2023-03-22 15:44:25.317

--- hack.near/Common.DAO.Library/source_code.jsx
+++ hack.near/Common.DAO.Library/source_code.jsx
@@ -197,7 +197,7 @@ return (
     <div class="row">
       <div class="col-md-3">
         <Widget
-          src={`${ownerId}/widget/Common.Component.Library.Navbar`}
+          src={`${ownerId}/widget/Common.DAO.Library.Navbar`}
           props={{
             tab: state.tab,
             onSelect,
