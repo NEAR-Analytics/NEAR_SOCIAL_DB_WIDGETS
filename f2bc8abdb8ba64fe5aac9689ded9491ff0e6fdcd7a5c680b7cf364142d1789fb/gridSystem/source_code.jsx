a7898ac4fd6e94606260bc3aa0dc80be3f0f4f6d
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-03-22 01:46:02 -0400

    Update gridSystem by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-03-22 01:46:02.282

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/gridSystem/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/gridSystem/source_code.jsx
@@ -1,34 +1,20 @@
-const colCount = props.colCount;
-const brakePoint = props.brakePoint;
-const children = props.children;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
 
-let rowCount = Math.floor(children.length / colCount) + 1;
-let index = 0;
-
-const buildGrid = () => {
-  return renderRows();
-};
-
-const renderRows = () => {
-  let rows = [];
-
-  for (let row = 0; row < rowCount; row++) {
-    rows.push(<Row className="Row">{renderCols()}</Row>);
+  @media (hover: none) {
+    grid-template-columns: repeat(1, 1fr);
   }
-  return rows;
-};
+  
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
-const renderCols = () => {
-  let cols = [];
-
-  for (let col = 0; col < colCount; col++) {
-    if (index < children.length) {
-      cols.push(<Col className="Col">{children[index]}</Col>);
-      index++;
-    }
-
-    return cols;
-  }
-};
-
-return <Container className="Container">{buildGrid()}</Container>;
+return (
+  <Grid>
+    <p>Hello World!</p>
+    <p>Hello World2!</p>
+    <p>Hello World3!</p>
+  </Grid>
+);
