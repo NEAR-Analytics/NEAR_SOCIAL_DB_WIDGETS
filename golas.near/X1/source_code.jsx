befc6a97648c9aa283277edea8183010560d7861
golas.near <golas.near>
2023-03-22 17:44:22 -0400

    Update X1 by golas.near at 2023-03-22 17:44:22.566

--- golas.near/X1/source_code.jsx
+++ golas.near/X1/source_code.jsx
@@ -1,3 +1,20 @@
 const { calc } = require("golas.near/widget/Module-1");
 
-return <div>{calc(100, 11)}</div>;
+function calc222(a, b) {
+  return a * b;
+}
+
+const kkk = "KKK:";
+
+return (
+  <div>
+    <div>
+      <p>{kkk}</p>
+    </div>
+    <div>{calc(100, 11)}</div>
+    <div>{calc(100, 11)}</div>
+    <div>{calc(100, 11)}</div>
+    <div>{calc(100, 11)}</div>
+    <div>{calc(100, 11)}</div>
+  </div>
+);
