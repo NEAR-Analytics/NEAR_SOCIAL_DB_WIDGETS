d1f313ef937b056ef9664ed3d1d3b02b048a6af0
microchipgnu <microchipgnu>
2023-03-22 12:05:54 -0400

    Update DeployGateway by microchipgnu.near at 2023-03-22 12:05:54.557

---
+++ microchipgnu.near/DeployGateway/source_code.jsx
@@ -0,0 +1,7 @@
+return (
+  <div>
+    <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fnear%2Fnear-discovery-alpha&build-command=npm%20run%20build&install-command=npm%20install&output-directory=dist">
+      <img src="https://vercel.com/button" alt="Deploy with Vercel" />
+    </a>
+  </div>
+);
