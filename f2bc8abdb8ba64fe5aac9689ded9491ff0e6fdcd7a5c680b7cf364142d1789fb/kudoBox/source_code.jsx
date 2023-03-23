a763a451d9f99b4a9b1c3fe0666f832663cc720b
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-03-23 18:28:24 -0400

    Update kudoBox by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-03-23 18:28:24.407

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/kudoBox/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/kudoBox/source_code.jsx
@@ -1,8 +1,22 @@
 const d = props.d;
 const thisWidgetInlineStyles = props.allWidgetsInlineStyles.renderKudoBox;
 const thisWidgetClassNames = props.allWidgetsClassNames.renderKudoBox;
+const updateGeneralState = props.updateGeneralState;
+const upvotes = props.upvotes;
 
-State.init({});
+State.init({
+  hoveringElement: "",
+});
+
+function getShowCommentsButtonContainerStyles() {
+    const standardStyles = thisWidgetInlineStyles.showCommentsButtonContainer
+    const hoveringStyles = standardStyles
+
+    hoveringStyles["color"] = "rgba(0,191,255,255)";
+    hoveringStyles["background-color"] = "rgba(229, 248, 255, 255)";
+
+    return state.hoveringElement == "showCommentsButtonContainer" ? hoveringStyles : standardStyles
+}
 
 return (
   <div style={thisWidgetInlineStyles.cardContainer}>
@@ -20,9 +34,15 @@ return (
     <div
       className={thisWidgetClassNames.showCommentsButtonContainer}
       style={thisWidgetInlineStyles.showCommentsButtonContainer}
+      onMouseEnter={() => {
+        State.update({ hoveringElement: "showCommentsButtonContainer" });
+      }}
+      onMouseLeave={() => {
+        State.update({ hoveringElement: "" });
+      }}
     >
       <p style={thisWidgetInlineStyles.textShowComment}>Show comments</p>
-      <i className="bi bi-caret-down"></i>
+      <i className="bi bi-caret-down" styles={state.hoveringElement == "showCommentsButtonContainer" ? thisWidgetInlineStyles.}></i>
     </div>
 
     {RenderAllCommentAnswerBox(d)}
