5e7551670d58d65fc316eb7c42f9065d8267d27e
sainthiago <sainthiago>
2023-03-23 11:21:24 -0400

    Update Content-Posts.Post by sainthiago.near at 2023-03-23 11:21:24.727

---
+++ sainthiago.near/Content-Posts.Post/source_code.jsx
@@ -0,0 +1,112 @@
+const accountId = props.accountId;
+const blockHeight =
+  props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
+const subscribe = !!props.subscribe;
+const notifyAccountId = accountId;
+const postUrl = `https://alpha.near.org/#/calebjacob.near/widget/PostPage?accountId=${accountId}&blockHeight=${blockHeight}`;
+
+const content =
+  props.content ??
+  JSON.parse(Social.get(`${accountId}/post/main`, blockHeight) ?? "null");
+
+const item = {
+  type: "social",
+  path: `${accountId}/post/main`,
+  blockHeight,
+};
+
+const Post = styled.div`
+  position: relative;
+
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    left: 19px;
+    top: 52px;
+    bottom: 12px;
+    width: 2px;
+    background: #ECEEF0;
+  }
+`;
+
+const Header = styled.div`
+  margin-bottom: 0;
+  display: inline-flex;
+`;
+
+const Body = styled.div`
+  padding-left: 52px;
+  padding-bottom: 1px;
+`;
+
+const Content = styled.div`
+  img {
+    display: block;
+    max-width: 100%;
+    max-height: 80vh;
+    margin: 0 0 12px;
+  }
+`;
+
+const Text = styled.p`
+  display: block;
+  margin: 0;
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 400;
+  color: #687076;
+  white-space: nowrap;
+`;
+
+return (
+  <Post>
+    <Header>
+      <Widget
+        src="calebjacob.near/widget/AccountProfile"
+        props={{
+          accountId,
+          hideAccountId: true,
+          inlineContent: (
+            <>
+              <Text as="span">･</Text>
+              <Text>
+                {blockHeight === "now" ? (
+                  "now"
+                ) : (
+                  <>
+                    <Widget
+                      src="mob.near/widget/TimeAgo"
+                      props={{ blockHeight }}
+                    />{" "}
+                    ago
+                  </>
+                )}
+              </Text>
+            </>
+          ),
+        }}
+      />
+    </Header>
+
+    <Body>
+      <Content>
+        {content.text && (
+          <Widget
+            src="calebjacob.near/widget/SocialMarkdown"
+            props={{ text: content.text }}
+          />
+        )}
+
+        {content.image && (
+          <Widget
+            src="mob.near/widget/Image"
+            props={{
+              image: content.image,
+            }}
+          />
+        )}
+      </Content>
+    </Body>
+  </Post>
+);
