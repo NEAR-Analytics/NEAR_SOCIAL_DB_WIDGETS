51abad524e7a2b5eb39e08e1b8b5067d578190b5
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-23 09:38:09 -0400

    Update ComponentDetailsPage.Discussion.Feed by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-23 09:38:09.995

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/ComponentDetailsPage.Discussion.Feed/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/ComponentDetailsPage.Discussion.Feed/source_code.jsx
@@ -20,7 +20,7 @@ const renderItem = (a) =>
   a.value.type === "md" && (
     <Post className="post" key={JSON.stringify(a)}>
       <Widget
-        src="calebjacob.near/widget/Posts.Post"
+        src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/ComponentDetailsPage.Discussion"
         props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
       />
     </Post>
