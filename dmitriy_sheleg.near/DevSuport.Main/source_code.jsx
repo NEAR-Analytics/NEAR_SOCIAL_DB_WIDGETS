19c0d40fd9e122861c359cb814ff09ff038e5106
dmitriy_sheleg.near <dmitriy_sheleg.near>
2023-03-23 13:17:43 -0400

    Update DevSuport.Main by dmitriy_sheleg.near at 2023-03-23 13:17:43.996

---
+++ dmitriy_sheleg.near/DevSuport.Main/source_code.jsx
@@ -0,0 +1,42 @@
+if (context.loading) {
+  return "Loading";
+}
+
+// By default the form to ask a question is hidden
+initState({ askQuestion: false });
+const toggleQuestion = () => {
+  State.update({ askQuestion: !state.askQuestion });
+};
+
+const adminContract = "admin.dev-support.near";
+const admins = Near.view(adminContract, "get_admins", {});
+
+return (
+  <div class="container py-3 mb-5">
+    <div class="row">
+      <div class="col-8">
+        <h1>Developer Support</h1>
+      </div>
+      <div class="col-4 justify-content-end mt-2">
+        <button
+          class="btn btn-primary float-end"
+          disabled={!context.accountId}
+          onClick={toggleQuestion}
+        >
+          <i class="bi bi-chat-dots"></i> Ask a Question
+        </button>
+      </div>
+    </div>
+    {/* Widget to create the question */}
+    <div class="my-3">
+      {state.askQuestion && (
+        <Widget src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Edit" />
+      )}
+    </div>
+    {/* Widget to display Feed of Questions */}
+    <Widget
+      src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Feed"
+      props={{ admins, adminContract: adminContract }}
+    />
+  </div>
+);
