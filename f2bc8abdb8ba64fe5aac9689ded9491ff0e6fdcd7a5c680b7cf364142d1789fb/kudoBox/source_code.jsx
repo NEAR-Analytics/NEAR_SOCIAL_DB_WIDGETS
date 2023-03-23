35413d39127ee4caf063d3845a33118f77eac700
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-03-23 19:29:05 -0400

    Update kudoBox by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-03-23 19:29:05.075

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/kudoBox/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/kudoBox/source_code.jsx
@@ -1,6 +1,7 @@
 const d = props.d;
 const thisWidgetInlineStyles = props.thisWidgetInlineStyles;
 const thisWidgetClassNames = props.thisWidgetClassNames;
+console.log(thisWidgetInlineStyles);
 const updateGeneralState = props.updateGeneralState;
 const upvotes = props.upvotes;
 
