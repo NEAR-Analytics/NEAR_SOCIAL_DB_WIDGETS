5e64e93be5e6374b19d90e37d4ab74232b8833a1
syi216.near <syi216.near>
2023-03-23 22:32:44 -0400

    Update GNS-Staking by syi216.near at 2023-03-23 22:32:44.261

--- syi216.near/GNS-Staking/source_code.jsx
+++ syi216.near/GNS-Staking/source_code.jsx
@@ -131,7 +131,7 @@ if (state.stakedBalance === undefined && state.sender) {
         stakedBalance: Big(res[0]).div(Big(10).pow(18)).toFixed(2),
       });
       State.update({
-        daiBalance: Big(res[1]).div(Big(10).pow(18)).toFixed(4),
+        daiBalance: Big(res[1]).div(Big(10).pow(18)).toFixed(10),
       });
     });
 }
@@ -225,9 +225,9 @@ return (
           </div>
           <div class="LidoFormTopContainerRight">
             <div class="LidoAprContainer">
-              <div class="LidoAprTitle">Your APR</div>
+              <div class="LidoAprTitle">Pending DAI rewards</div>
               <div class="LidoAprValue">
-                {(state.daiBalance * 100).toFixed(2) ?? "..."}%
+                {state.daiBalance ?? "..."}&nbsp;DAI
               </div>
             </div>
           </div>
