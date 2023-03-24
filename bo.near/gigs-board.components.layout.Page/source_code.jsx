abe170b3cc9782c7e129e75560e68c90167c49bb
ailisp <ailisp>
2023-03-24 03:51:41 -0400

    Update gigs-board.components.layout.Page by bo.near at 2023-03-24 03:51:41.986

--- bo.near/gigs-board.components.layout.Page/source_code.jsx
+++ bo.near/gigs-board.components.layout.Page/source_code.jsx
@@ -75,11 +75,27 @@ const Scroll = styled.div`
   margin-top: calc(-24px + 100px + 35px);
 }`;
 
+const Gradient = styled.div`
+{
+  position: absolute;
+  width: 1057.31px;
+  height: 2620.61px;
+  left: 1986.1px;
+  top: -332.99px;
+
+  background: linear-gradient(180deg, #00CCFF 0.57%, #3252A6 100.57%);
+  opacity: 0.22;
+  filter: blur(250px);
+  transform: rotate(99.78deg);
+}
+`;
+
 return (
   <>
     <Banner>
       <div class="row"></div>
     </Banner>
+    <Gradient></Gradient>
     <Logo>
       <img src="https://ipfs.near.social/ipfs/bafkreifm5y7r6bqcjtef6wytrp7ysdxocmgmjffjziorqya4p7kbtamntu"></img>
     </Logo>
