1e86cf65d11edc28d17d735eba9de1fa96e9ca7c
ailisp <ailisp>
2023-03-24 06:23:27 -0400

    Update gigs-board.pages.Feed by bo.near at 2023-03-24 06:23:27.634

---
+++ bo.near/gigs-board.pages.Feed/source_code.jsx
@@ -0,0 +1,73 @@
+/* INCLUDE: "common.jsx" */
+const nearDevGovGigsContractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
+  (context.widgetSrc ?? "bo.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
+    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
+  };
+  return (
+    <Widget
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+  if (props.nearDevGovGigsContractAccountId) {
+    linkProps.nearDevGovGigsContractAccountId =
+      props.nearDevGovGigsContractAccountId;
+  }
+  if (props.nearDevGovGigsWidgetsAccountId) {
+    linkProps.nearDevGovGigsWidgetsAccountId =
+      props.nearDevGovGigsWidgetsAccountId;
+  }
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+  const linkPropsQuery = Object.entries(linkProps)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+  return `#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+/* END_INCLUDE: "common.jsx" */
+
+State.init({
+  propsLabel: props.label,
+  label: props.label,
+});
+
+// When rerendered with different props, State will be preserved, so we need to update the state when we detect that the props have changed.
+if (props.label !== state.propsLabel) {
+  State.update({
+    propsLabel: props.label,
+    label: props.label,
+  });
+}
+
+const onSearch = (label) => {
+  State.update({ label });
+};
+
+return widget("components.layout.Page", {
+  navbarChildren: widget("components.layout.Search", {
+    searchQuery: { label: state.label },
+    onSearch,
+  }),
+  children: widget("components.posts.List", {
+    recency: props.recency,
+    label: state.label,
+  }),
+});
