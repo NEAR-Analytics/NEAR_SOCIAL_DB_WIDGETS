cf4c9f19ff7a38eaa302090fcb20d66451501650
joannaorlova.near <joannaorlova.near>
2023-03-23 23:51:06 -0400

    Update ayooo by joannaorlova.near at 2023-03-23 23:51:06.091

--- joannaorlova.near/ayooo/source_code.jsx
+++ joannaorlova.near/ayooo/source_code.jsx
@@ -1,9 +1,10 @@
+let name = props.name || "Default";
 let greeting = "Have a great day";
 
 return (
   <>
     <div>
-      <h1>Hello</h1>
+      <h1>Hello {name}</h1>
       <p> {greeting} </p>
     </div>
   </>
