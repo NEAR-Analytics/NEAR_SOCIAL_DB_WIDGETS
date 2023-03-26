4c13f4fe922a85803e8c818bf4e51cf7f598e5a2
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-03-25 23:42:40 -0400

    Update showCommentsButton by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-03-25 23:42:40.164

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/showCommentsButton/source_code.jsx
@@ -0,0 +1,46 @@
+const thisWidgetInlineStyles = props.thisWidgetInlineStyles;
+const thisWidgetClassNames = props.thisWidgetClassNames;
+const fatherStateUpdate = props.fatherStateUpdate;
+const showComments = props.showComments;
+const d = props.d;
+
+State.init({
+  hoveringElement: "",
+});
+
+function getCaretDirection() {
+  return state.showComments
+    ? thisWidgetInlineStyles.renderKudoBox.flipButton
+    : { transition: "transform 1s" };
+}
+
+return (
+  <div
+    className={thisWidgetClassNames.showCommentsButtonContainer}
+    style={getShowCommentsButtonContainerStyles()}
+    onMouseEnter={() => {
+      d.value.comments.length > 0 &&
+        State.update({ hoveringElement: "showCommentsButtonContainer" });
+    }}
+    onMouseLeave={() => {
+      d.value.comments.length > 0 && State.update({ hoveringElement: "" });
+    }}
+    onClick={() => {
+      d.value.comments.length > 0 &&
+        fatherStateUpdate({ showComments: !showComments });
+    }}
+  >
+    <p style={thisWidgetInlineStyles.renderKudoBox.textShowComment}>
+      {showComments
+        ? "Hide comments"
+        : d.value.comments.length > 0
+        ? "Show comments"
+        : "No comments"}
+    </p>
+    {d.value.comments.length > 0 && (
+      <div style={getCaretDirection()}>
+        <i className="bi bi-caret-down"></i>
+      </div>
+    )}
+  </div>
+);
