e8111780f1e154a83ea8cd96d94d79cefb40a956
Jikugodwill <Jikugodwill>
2023-03-27 19:06:08 -0400

    Update nearprojectcard by jgodwill.near at 2023-03-27 19:06:08.334

--- jgodwill.near/nearprojectcard/source_code.jsx
+++ jgodwill.near/nearprojectcard/source_code.jsx
@@ -1,3 +1,61 @@
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 33%;
+  box-shadow: 0 0.05rem 0.05rem rgb(34 34 34 / 5%), 0 0.2rem 0.8rem rgb(34 34 34 / 8%);
+  border-radius: 10px;
+  border: 0.05rem solid #eee;
+  background: #fff;
+  cursor: pointer;
+  transition: all ease 0.3s;
+  padding: 1em;
+  margin: 0.5rem;
+  max-width: 220px;
+  max-height: 200px;
+  overflow: hidden;
+  justify-content: space-between;
+
+  &:hover{
+     transform: translateY(-0.1rem);
+  }
+`;
+
+const Title = styled.div`
+  display: flex;
+  margin-bottom: 5px;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 0.8rem;
+`;
+
+const IconWrapper = styled.div`
+  width: 40px;
+  min-width: 40px;
+  height: 40px;
+  min-height: 40px;
+`;
+
+const ProjectName = styled.span`
+  font-weight: bold;
+  font-size: 0.9rem;
+`;
+
+const projectCategory = styled.span`
+  font-weight: lighter;
+  color: #888;
+  font-size: 0.7rem;
+`;
+
+const projectSubtitle = sytled.span`
+  color: #222;
+  font-size: 0.7rem;
+  margin: 1em 0;
+  height: 30px;
+`;
+
 // truncate text and add an ellipsis if needed
 const truncateText = (text, characters) => {
   if (!text) return "";
@@ -7,29 +65,30 @@ const truncateText = (text, characters) => {
 };
 
 return (
-  <div className={styles.container}>
-    <div className={styles.projectTitle}>
-      <div className={styles.projectIconWrapper}>
+  <Card>
+    <Title>
+      <IconWrapper>
         <img
           src={
             props.projectData.Icon ||
-            "https://images.pexels.com/photos/10175905/pexels-photo-10175905.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            "https://images.pexels.com/photos/10175905/pexels-photo-10175905.jpeg?auto=compress&cs=tinysrgb&lazy=load"
           }
+          className="w-100  h-100 shadow rounded-circle img-fluid"
           alt="Project Icon"
         />
-      </div>
-      <div className={styles.projectInfo}>
-        <span className={styles.projectName}>
+      </IconWrapper>
+      <Info>
+        <ProjectName>
           {props.projectData.ProjectName || "Project Name"}
-        </span>
-        <span className={styles.projectCategory}>
+        </ProjectName>
+        <projectCategory>
           {truncateText(props.projectData.Category, 20) || "Category"}
-        </span>
-      </div>
-    </div>
-    <span className={styles.projectSubtitle}>
+        </projectCategory>
+      </Info>
+    </Title>
+    <projectSubtitle>
       {truncateText(props.projectData.Subtitle, 60)}
-    </span>
-    <div className={styles.projectLinks}>Icons</div>
-  </div>
+    </projectSubtitle>
+    <div className="d-flex">Icons</div>
+  </Card>
 );
