7110541e58c0e2cfc2e9f60a2840194da20b44b1
dmitriy_sheleg.near <dmitriy_sheleg.near>
2023-03-27 11:14:42 -0400

    Update DevSupport.Question.Preview by dmitriy_sheleg.near at 2023-03-27 11:14:42.930

--- dmitriy_sheleg.near/DevSupport.Question.Preview/source_code.jsx
+++ dmitriy_sheleg.near/DevSupport.Question.Preview/source_code.jsx
@@ -85,7 +85,8 @@ return (
         <div class="row">
           <div class="col">
             <Widget
-              src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Button.Answers"
+              // src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Button.Answers"
+              src="dmitriy_sheleg.near/widget/DevSupport.Question.Button.Answers"
               props={{ accountId, blockHeight }}
             />
           </div>
