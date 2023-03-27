8a1f17d661b9842874c2ebac06b6cb80e601b825
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-27 14:56:04 -0400

    Update NestedDiscussions.SinglePageView by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-27 14:56:04.982

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.SinglePageView/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.SinglePageView/source_code.jsx
@@ -5,22 +5,9 @@ const previewWidget =
   props.previewWidget ||
   "ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Preview";
 
-const item = {
-  type: "social",
-  path: `${accountId}/${dbAction}/main`,
-  blockHeight,
-};
-
-const content = JSON.parse(
-  Social.get(`${accountId}/${dbAction}/main`, blockHeight) ??
-    '{"content": null}'
-).content;
-
-return content;
-
 return (
   <Widget
     src={previewWidget}
-    props={{ identifier: item, commentsLimit: 30, subscribe: true }}
+    props={{ identifier, accountId, blockHeight, dbAction }}
   />
 );
