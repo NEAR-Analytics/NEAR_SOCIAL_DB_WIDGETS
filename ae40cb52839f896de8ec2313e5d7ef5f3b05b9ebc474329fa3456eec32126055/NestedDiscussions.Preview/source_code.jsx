a78347dddf6023755047181d024392bda1199a83
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-27 13:48:11 -0400

    Update NestedDiscussions.Preview by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-27 13:48:11.966

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.Preview/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.Preview/source_code.jsx
@@ -163,7 +163,7 @@ return (
         <Widget
           src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Feed"
           props={{
-            item,
+            identifier: item,
             highlightComment: props.highlightComment,
             limit: props.commentsLimit,
             subscribe,
