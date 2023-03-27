a97f6d32cba0b76beea74e82fae25407f3f4d1fe
dmitriy_sheleg.near <dmitriy_sheleg.near>
2023-03-27 19:48:30 -0400

    Update DevSuport.Main by dmitriy_sheleg.near at 2023-03-27 19:48:30.231

--- dmitriy_sheleg.near/DevSuport.Main/source_code.jsx
+++ dmitriy_sheleg.near/DevSuport.Main/source_code.jsx
@@ -11,23 +11,47 @@ const toggleQuestion = () => {
 const adminContract = "admin.dev-support.near";
 const admins = Near.view(adminContract, "get_admins", {});
 
+const sortByCategories = ["Newest", "Popular"];
+const filterByTopic = ["one", "two", "three"];
+
+const Divider = styled.div`
+    width: 1px;
+    height: 100%;
+    background-color: #ECEEF0;
+`;
+
 return (
   <div class="container py-3 mb-5">
-    {/* Title */}
-    <Widget
-      src="dmitriy_sheleg.near/widget/DevSupport.Discussion.Title"
-      props={{ disabled: !context.accountId, onClick: toggleQuestion }}
-    />
-    {/* Widget to create the question */}
-    <div class="my-3">
-      {state.askQuestion && (
-        <Widget src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Edit" />
-      )}
+    <div class="row">
+      <div class="col-3">
+        <Widget
+          src="dmitriy_sheleg.near/widget/DevSupport.Main.Sidebar"
+          props={{ categories: sortByCategories, topics: filterByTopic }}
+        />
+      </div>
+
+      <div class="col-1">
+        <Divider />
+      </div>
+
+      <div class="col-8">
+        {/* Title */}
+        <Widget
+          src="dmitriy_sheleg.near/widget/DevSupport.Discussion.Title"
+          props={{ disabled: !context.accountId, onClick: toggleQuestion }}
+        />
+        {/* Widget to create the question */}
+        <div class="my-3">
+          {state.askQuestion && (
+            <Widget src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Edit" />
+          )}
+        </div>
+        {/* Widget to display Feed of Questions */}
+        <Widget
+          src="dmitriy_sheleg.near/widget/DevSupport.Feed"
+          props={{ admins, adminContract: adminContract }}
+        />
+      </div>
     </div>
-    {/* Widget to display Feed of Questions */}
-    <Widget
-      src="dmitriy_sheleg.near/widget/DevSupport.Feed"
-      props={{ admins, adminContract: adminContract }}
-    />
   </div>
 );
