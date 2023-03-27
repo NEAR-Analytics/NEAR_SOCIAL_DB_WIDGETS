a71b403557269094aa60e0bb20b73619dcc59be4
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-27 13:24:55 -0400

    Update NestedDiscussions by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-27 13:24:55.998

---
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions/source_code.jsx
@@ -0,0 +1,61 @@
+const composeWidget =
+  props.composeWidget ||
+  "ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Compose";
+
+const previewWidget =
+  props.previewWidget ||
+  "ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Preview";
+
+const dbAction = props.dbAction || "discuss";
+
+const identifier = props.identifier;
+
+if (!identifier) {
+  return "Please select an identifier for this discussion";
+}
+
+const DiscussionContainer = styled.div`
+  @media (max-width: 1200px) {
+    > div:first-child {
+      border-top: none;
+    }
+  }
+`;
+
+const ComposeWrapper = styled.div`
+  border-top: 1px solid #ECEEF0;
+  border-bottom: 1px solid #ECEEF0;
+`;
+
+const FeedWrapper = styled.div`
+  .post {
+    padding-left: 24px;
+    padding-right: 24px;
+
+    @media (max-width: 1200px) {
+      padding-left: 12px;
+      padding-right: 12px;
+    }
+  }
+`;
+
+const notLoggedMessage =
+  props.notLoggedMessage || "Please login to join the discussion";
+
+return (
+  <DiscussionContainer>
+    {context.accountId ? (
+      <ComposeWrapper>
+        <Widget src={composeWidget} props={{ dbAction, identifier }} />
+      </ComposeWrapper>
+    ) : (
+      <p> ${notLoggedMessage} </p>
+    )}
+    <FeedWrapper>
+      <Widget
+        src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Feed"
+        props={{ previewWidget, identifier }}
+      />
+    </FeedWrapper>
+  </DiscussionContainer>
+);
