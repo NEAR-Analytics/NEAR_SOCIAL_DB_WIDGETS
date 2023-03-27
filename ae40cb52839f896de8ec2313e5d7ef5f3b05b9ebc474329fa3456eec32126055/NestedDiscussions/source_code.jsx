fe1a27f04bd1dfd105f6941ffaaa1e6bdaf0a57f
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-03-27 13:40:15 -0400

    Update NestedDiscussions by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-03-27 13:40:15.394

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions/source_code.jsx
@@ -54,7 +54,7 @@ return (
     <FeedWrapper>
       <Widget
         src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Feed"
-        props={{ previewWidget, identifier }}
+        props={{ dbAction, previewWidget, identifier }}
       />
     </FeedWrapper>
   </DiscussionContainer>
