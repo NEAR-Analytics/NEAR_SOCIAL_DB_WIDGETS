6f8ad98c5feedfab1eba7784750474f937e59a6a
microchipgnu <microchipgnu>
2023-03-27 16:35:29 -0400

    Update ListFromCollection by microchipgnu.near at 2023-03-27 16:35:29.001

--- microchipgnu.near/ListFromCollection/source_code.jsx
+++ microchipgnu.near/ListFromCollection/source_code.jsx
@@ -2,7 +2,7 @@ const accountId = context?.accountId;
 const marketAddress = "simple.market.mintbase1.near";
 
 const LISTING_DEPOSIT_IN_NEAR_PER_TOKEN = 10000000000000000000000;
-const size = "5em";
+const size = "3em";
 
 State.init({
   tokens: {},
@@ -59,6 +59,13 @@ const removeFromCart = (token_id, nft_contract_id) => {
   });
 };
 
+const clear = () => {
+  State.update({
+    tokens: {},
+    price: 0,
+  });
+};
+
 const numTokensSelected = Object.keys(state.tokens).length;
 
 const YoctoToNear = (amountYocto) =>
@@ -109,17 +116,47 @@ if (!accountId) return <div>Sign in first</div>;
 
 return (
   <div class="d-flex flex-column gap-4">
-    <h4>Multi-listing tool for NFTs</h4>
-
+    <div class="d-flex gap-2 items-center">
+      <h4>Multi-listing tool for NFTs</h4>
+    </div>
+    <div class="d-flex gap-2">
+      <p class="text-black p-1">
+        <span class="fw-bold">{numTokensSelected}</span> tokens
+      </p>
+      <p class="text-black p-1">
+        <span class="fw-bold">{state.price}</span>N
+      </p>
+    </div>
+    <div class="d-flex gap-1 flex-wrap">
+      {Object.values(state.tokens).map((token) => {
+        return (
+          <Widget
+            src="mob.near/widget/NftImage"
+            props={{
+              nft: {
+                tokenId: token.tokenId,
+                contractId: token.nftContractId,
+              },
+              style: {
+                width: size,
+                height: size,
+                objectFit: "cover",
+                minWidth: size,
+                minHeight: size,
+                maxWidth: size,
+                maxHeight: size,
+                overflowWrap: "break-word",
+              },
+              thumbnail: "thumbnail",
+              className: "",
+              fallbackUrl:
+                "https://ipfs.near.social/ipfs/bafkreihdiy3ec4epkkx7wc4wevssruen6b7f3oep5ylicnpnyyqzayvcry",
+            }}
+          />
+        );
+      })}
+    </div>
     <div class="w-100">
-      <div class="d-flex gap-2">
-        <p class="text-white p-2 bg-black rounded">
-          Tokens selected <span class="fw-bold">{numTokensSelected}</span>
-        </p>
-        <p class="text-white p-2 bg-black rounded">
-          Price: <span class="fw-bold">{state.price}N</span>
-        </p>
-      </div>
       <div class="d-flex gap-2">
         <input type="text" value={state.price}></input>
         <button
@@ -129,57 +166,27 @@ return (
         >
           List
         </button>
+        <button
+          class="text-white p-1 bg-black rounded"
+          onClick={() => {
+            clear();
+          }}
+        >
+          Clear
+        </button>
       </div>
     </div>
     <div>
-      <h4>Tokens from your collection</h4>
-      <div class="d-flex flex-wrap gap-4">
-        {data.map(({ token_id, nft_contract_id }) => {
-          const key = `${token_id}::${nft_contract_id}`;
-          const addedToken = !!state.tokens[key];
-
-          return (
-            <button
-              class={`p-2 ${
-                addedToken ? "bg-primary" : "bg-black"
-              } text-white rounded`}
-              key={key}
-              onClick={() => {
-                if (!addedToken) {
-                  addToListCart(token_id, nft_contract_id);
-                } else {
-                  removeFromCart(token_id, nft_contract_id);
-                }
-              }}
-            >
-              <Widget
-                src="mob.near/widget/NftImage"
-                props={{
-                  nft: {
-                    tokenId: token_id,
-                    contractId: nft_contract_id,
-                  },
-                  style: {
-                    width: size,
-                    height: size,
-                    objectFit: "cover",
-                    minWidth: size,
-                    minHeight: size,
-                    maxWidth: size,
-                    maxHeight: size,
-                    overflowWrap: "break-word",
-                  },
-                  thumbnail: "thumbnail",
-                  className: "",
-                  fallbackUrl:
-                    "https://ipfs.near.social/ipfs/bafkreihdiy3ec4epkkx7wc4wevssruen6b7f3oep5ylicnpnyyqzayvcry",
-                }}
-              />
-              <div></div>
-            </button>
-          );
-        })}
-      </div>
+      <p>Your collection</p>
+      <Widget
+        src="microchipgnu.near/widget/NFT.Selector"
+        props={{
+          compressImages: true,
+          onChange: ({ contractId, tokenId }) => {
+            addToListCart(tokenId, contractId);
+          },
+        }}
+      />
     </div>
   </div>
 );
