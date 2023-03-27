fdd19ac51dce41b5d37dc9df7ee099c0f470391f
652a8c3d2171a43404b88aa01678f4d59693492fe8852d801f1504efd4dc7214 <652a8c3d2171a43404b88aa01678f4d59693492fe8852d801f1504efd4dc7214>
2023-03-27 11:55:07 -0400

    Update TP_Intro by 652a8c3d2171a43404b88aa01678f4d59693492fe8852d801f1504efd4dc7214 at 2023-03-27 11:55:07.864

---
+++ 652a8c3d2171a43404b88aa01678f4d59693492fe8852d801f1504efd4dc7214/TP_Intro/source_code.jsx
@@ -0,0 +1,73 @@
+return (
+  <div>
+    <p>
+      <strong>Introducing the Ultimate Poker Experience: Total Poker</strong>
+      <br />
+      Get ready to immerse yourself in the world of poker like never before with
+      Total Poker! We&#39;ve taken your favorite game and revolutionized it
+      using the cutting-edge NEAR protocol. Our mission? To deliver the most
+      user-friendly and rewarding poker app on the market.
+    </p>
+
+    <p>
+      <strong>Elevate Your Game</strong>
+      <br />
+      Whether you&#39;re a poker pro or a first-time player, Total Poker is
+      designed to help you master the game and reap the rewards. With our
+      groundbreaking technology, we&#39;re ensuring a fair and balanced game
+      environment, so you can focus on honing your skills and raking in those
+      wins.
+    </p>
+
+    <p>
+      <strong>Fairness Guaranteed</strong>
+      <br />
+      We&#39;re not just leveling the playing field; we&#39;re making it even
+      safer than a real table! With our innovative device-controlled fairness
+      system, you can trust that every hand is dealt justly, making Texas Holdem
+      Poker more secure than ever before.
+    </p>
+
+    <p>
+      <strong>Don&#39;t Miss Out - Join the Fun Today</strong>
+      <br />
+      Don&#39;t let this incredible poker experience pass you by! Download Total
+      Poker now, and discover why countless players are raving about the future
+      of poker. Trust us - you won&#39;t regret it!
+    </p>
+
+    <p>
+      <strong>Instructions:</strong>
+      <br />
+      <strong>iOS</strong> -{" "}
+      <a href="https://testflight.apple.com/join/vx8zQYrx">
+        https://testflight.apple.com/join/vx8zQYrx
+      </a>
+    </p>
+
+    <p>
+      <strong>Android </strong>-&nbsp;
+      <a href="https://drive.google.com/file/d/1c5xpr2YNhaUjjARcWwI86CoJa9tEXY9o">
+        https://drive.google.com/file/d/1c5xpr2YNhaUjjARcWwI86CoJa9tEXY9o
+      </a>
+    </p>
+
+    <ol>
+      <li>Install the APK or TestFlight</li>
+      <li>Register in the game using your NEAR account.</li>
+      <li>Make a transfer from the specified account to totalpoker.near.</li>
+      <li>Wait a bit (a couple of minutes).</li>
+      <li>
+        See Diamonds on your game account, in the amount of the transfer
+        multiplied by 1000.
+      </li>
+      <li>Invite friends and play.</li>
+      <li>Click the &quot;cash out&quot; button in the menu.</li>
+      <li>
+        The game account balance will be reset to zero, and all Diamonds at a
+        rate of 1000 for 1 NEAR will be sent to the specified NEAR account
+        (commission of 0.001 NEAR).
+      </li>
+    </ol>
+  </div>
+);
