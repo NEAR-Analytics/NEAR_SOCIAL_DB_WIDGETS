286ec06fea17caf52875c5294a3b2bd8021c6577
near-ndc <near-ndc>
2023-03-27 21:32:21 -0400

    Update Common.Compose by neardigitalcollective.near at 2023-03-27 21:32:21.551

--- neardigitalcollective.near/Common.Compose/source_code.jsx
+++ neardigitalcollective.near/Common.Compose/source_code.jsx
@@ -141,7 +141,7 @@ return (
             State.update({ showAccountAutocomplete: false });
           }
         }}
-        placeholder={props.placeholder ?? "What's happening?"}
+        placeholder={props.placeholder ?? "Tell em how they did!"}
       />
       {autocompleteEnabled && state.showAccountAutocomplete && (
         <div className="pt-1 w-100 overflow-hidden">
