42480a57ff93ea06f057e4db1aac7c45ce682af7
ailisp <ailisp>
2023-03-28 09:27:00 -0400

    Update gigs-board.components.layout.FeaturedCommunity by bo.near at 2023-03-28 09:27:00.632

--- bo.near/gigs-board.components.layout.FeaturedCommunity/source_code.jsx
+++ bo.near/gigs-board.components.layout.FeaturedCommunity/source_code.jsx
@@ -45,14 +45,14 @@ function href(widgetName, linkProps) {
 /* END_INCLUDE: "common.jsx" */
 
 const Hover = styled.div`
-   card:hover {
+   hover:hover {
     box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
       0px 1px 2px rgba(16, 24, 40, 0.06);
   }
 `;
 
 return <a href={href('Community', {tag: props.tag})}><Hover>
-    <div class="card">
+    <div class="card hover">
       <img src={props.cover} class="card-img-top"></img>
       <div class="h5 pt-3 ps-3">{props.title}</div>
       <div class="ps-3 pb-2 text-secondary">{props.desc}
