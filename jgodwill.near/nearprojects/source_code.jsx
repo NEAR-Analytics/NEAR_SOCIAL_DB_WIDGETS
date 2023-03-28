1e6f210f23f9c189cf0c63d0fd59d409c688f897
Jikugodwill <Jikugodwill>
2023-03-28 10:28:39 -0400

    Update nearprojects by jgodwill.near at 2023-03-28 10:28:39.635

--- jgodwill.near/nearprojects/source_code.jsx
+++ jgodwill.near/nearprojects/source_code.jsx
@@ -2,24 +2,6 @@ const user = "jgodwill.near";
 
 const data = props?.projectData;
 
-// Define initial state
-
-State.init({
-  lastProject: 100,
-});
-
-// Define function to handle scrolling
-const handleScroll = () => {
-  if (
-    window.innerHeight + document.scrollingElement.scrollTop >
-    document.body.offsetHeight
-  ) {
-    State.update({
-      lastProject: Math.min(lastProject + 100, data.length),
-    });
-  }
-};
-
 const Cards = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -30,25 +12,22 @@ const Cards = styled.div`
   padding-top: 3em;
   box-shadow: 0 0.05rem 0.05rem rgb(34 34 34 / 5%), 0 0.2rem 0.8rem rgb(34 34 34 / 8%);
 `;
-const componentList = data.slice(0, lastProject).map((projectData, i) => {
-  if (projectData.ProjectName)
-    return (
-      <Widget
-        src={`${user}/widget/nearprojectcard`}
-        key={i}
-        props={projectData}
-      />
-    );
-});
 
 return (
-  <>
-    <div class="container-fluid">
-      <h3 class="text-center">Sample list of near projects </h3>
-      <hr />
-      <Cards onScroll={handleScroll}>
-        {componentList || "Components Here"}
-      </Cards>
-    </div>
-  </>
+  <div class="container-fluid">
+    <h3 class="text-center">Sample list of near projects </h3>
+    <hr />
+    <Cards onScroll={handleScroll}>
+      {data.map((projectData, i) => {
+        if (projectData.ProjectName)
+          return (
+            <Widget
+              src={`${user}/widget/nearprojectcard`}
+              key={i}
+              props={projectData}
+            />
+          );
+      }) || "Components Here"}
+    </Cards>
+  </div>
 );
