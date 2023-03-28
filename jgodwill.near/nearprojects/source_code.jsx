06998933ef8ffa607fa283129144605ec4802641
Jikugodwill <Jikugodwill>
2023-03-28 10:53:11 -0400

    Update nearprojects by jgodwill.near at 2023-03-28 10:53:11.201

--- jgodwill.near/nearprojects/source_code.jsx
+++ jgodwill.near/nearprojects/source_code.jsx
@@ -8,7 +8,7 @@ const Cards = styled.div`
   width: 100%;
   background: rgba(214, 214, 214, 0.2);
   border-radius: 10px;
-  justify-content: space-evenly;
+  justify-content: center;
   padding-top: 3em;
   box-shadow: 0 0.05rem 0.05rem rgb(34 34 34 / 5%), 0 0.2rem 0.8rem rgb(34 34 34 / 8%);
 `;
@@ -17,7 +17,7 @@ return (
   <div class="container-fluid">
     <h3 class="text-center">Sample list of near projects </h3>
     <hr />
-    <Cards onScroll={handleScroll}>
+    <Cards>
       {data.map((projectData, i) => {
         if (projectData.ProjectName)
           return (
