81fb10cb5227a4ef30040653c016149462149ca0
ailisp <ailisp>
2023-03-28 09:02:39 -0400

    Update gigs-board.components.layout.FeaturedCommunity by bo.near at 2023-03-28 09:02:39.736

--- bo.near/gigs-board.components.layout.FeaturedCommunity/source_code.jsx
+++ bo.near/gigs-board.components.layout.FeaturedCommunity/source_code.jsx
@@ -44,17 +44,18 @@ function href(widgetName, linkProps) {
 }
 /* END_INCLUDE: "common.jsx" */
 
-const Card = styled.div`
+const Hover = styled.div`
    &:hover {
     box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
       0px 1px 2px rgba(16, 24, 40, 0.06);
   }
 `;
 
-return <Card class="card">
-      <img src="https://ipfs.near.social/ipfs/bafkreifuflol4fgihxcgpxkl56lygpgdisdfbizrjpt4jhir7mvx3ddh4a" class="card-img-top"></img>
-      <div class="h5 pt-3 ps-3">Zero Knowledge</div>
-      <div class="ps-3 pb-2 text-secondary">
-Building a zero knowledge ecosystem on NEAR.
+return <a href={href('pages/Community')}><Hover>
+    <div class="card">
+      <img src={props.cover} class="card-img-top"></img>
+      <div class="h5 pt-3 ps-3">{props.title}</div>
+      <div class="ps-3 pb-2 text-secondary">{props.desc}
 </div>
-</Card>;
+</div>
+</Hover></a>;
