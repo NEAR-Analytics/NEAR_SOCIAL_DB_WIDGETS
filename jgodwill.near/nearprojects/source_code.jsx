14c887cb623a9b14b3353440172fddab9d819baf
Jikugodwill <Jikugodwill>
2023-03-28 13:41:55 -0400

    Update nearprojects by jgodwill.near at 2023-03-28 13:41:55.059

--- jgodwill.near/nearprojects/source_code.jsx
+++ jgodwill.near/nearprojects/source_code.jsx
@@ -1,6 +1,125 @@
 const user = "jgodwill.near";
 
-const data = props?.projectData;
+const gist_id = "09f2b3bc64829b975ce847f90bb2af28";
+
+const DUMMYDATA = [
+  {
+    ProjectName: "ABCD",
+    Category: null,
+    Subtitle: null,
+    AwesomeNearLink: null,
+    Series: null,
+    ABBV: null,
+    Icon: "ABCD",
+    "Website Link": "ABCD",
+    "Buy Link": null,
+    "Stake Link": null,
+    "DApp Link": "ABCD",
+    Facebook: "ABCD",
+    Twitter: "ABCD",
+    Github: "ABCD",
+    Telegram: "ABCD",
+    Discord: "ABCD",
+    Linkedin: null,
+    Medium: "ABCD",
+    "Other Links": null,
+    "Near Token": null,
+    "Aurora Token": null,
+    "Ethereum Token": null,
+    "Other Tokens": "ABCD",
+    Description: "ABCD",
+    Grants: null,
+    "News Articles": null,
+    WalletSelector_Comments1: null,
+    WalletSelector_Comments2: null,
+    Notes:
+      "PrevFundraising:xyza|CurrentFundraising:ABCD|TobeContacted:ABCD|Integrated_WS:ABCD|Email:ABCD|ContactEmail:ABCD",
+    Date_Created: "ABCD",
+  },
+  {
+    ProjectName: "ABCD",
+    Category: null,
+    Subtitle: null,
+    AwesomeNearLink: null,
+    Series: null,
+    ABBV: null,
+    Icon: "ABCD",
+    "Website Link": "ABCD",
+    "Buy Link": null,
+    "Stake Link": null,
+    "DApp Link": "ABCD",
+    Facebook: "ABCD",
+    Twitter: "ABCD",
+    Github: "ABCD",
+    Telegram: "ABCD",
+    Discord: "ABCD",
+    Linkedin: null,
+    Medium: "ABCD",
+    "Other Links": null,
+    "Near Token": null,
+    "Aurora Token": null,
+    "Ethereum Token": null,
+    "Other Tokens": "ABCD",
+    Description: "ABCD",
+    Grants: null,
+    "News Articles": null,
+    WalletSelector_Comments1: null,
+    WalletSelector_Comments2: null,
+    Notes:
+      "PrevFundraising:xyza|CurrentFundraising:ABCD|TobeContacted:ABCD|Integrated_WS:ABCD|Email:ABCD|ContactEmail:ABCD",
+    Date_Created: "ABCD",
+  },
+  {
+    ProjectName: "ABCD",
+    Category: null,
+    Subtitle: null,
+    AwesomeNearLink: null,
+    Series: null,
+    ABBV: null,
+    Icon: "ABCD",
+    "Website Link": "ABCD",
+    "Buy Link": null,
+    "Stake Link": null,
+    "DApp Link": "ABCD",
+    Facebook: "ABCD",
+    Twitter: "ABCD",
+    Github: "ABCD",
+    Telegram: "ABCD",
+    Discord: "ABCD",
+    Linkedin: null,
+    Medium: "ABCD",
+    "Other Links": null,
+    "Near Token": null,
+    "Aurora Token": null,
+    "Ethereum Token": null,
+    "Other Tokens": "ABCD",
+    Description: "ABCD",
+    Grants: null,
+    "News Articles": null,
+    WalletSelector_Comments1: null,
+    WalletSelector_Comments2: null,
+    Notes:
+      "PrevFundraising:xyza|CurrentFundraising:ABCD|TobeContacted:ABCD|Integrated_WS:ABCD|Email:ABCD|ContactEmail:ABCD",
+    Date_Created: "ABCD",
+  },
+];
+State.init({
+  jsonData: props.projectData,
+});
+
+async () => {
+  let response = await fetch(`https://api.github.com/gists/${gist_id}`, {
+    method: "GET",
+  });
+
+  let data = await response.text();
+  State.update({ jsonData: data });
+};
+if (!data) {
+  State.update({
+    jsonData: DUMMYDATA,
+  });
+}
 
 const Cards = styled.div`
   display: flex;
@@ -18,7 +137,7 @@ return (
     <h3 class="text-center">Sample list of near projects </h3>
     <hr />
     <Cards>
-      {data.map((projectData, i) => {
+      {state.jsonData?.map((projectData, i) => {
         if (projectData.ProjectName)
           return (
             <Widget
