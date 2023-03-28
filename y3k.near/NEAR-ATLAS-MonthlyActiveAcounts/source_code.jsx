f830d72390a89db2ec2fbbda28c9b4b30f7423a3
0bserver07 <0bserver07>
2023-03-27 23:48:27 -0400

    Update NEAR-ATLAS-MonthlyActiveAcounts by y3k.near at 2023-03-27 23:48:27.039

--- y3k.near/NEAR-ATLAS-MonthlyActiveAcounts/source_code.jsx
+++ y3k.near/NEAR-ATLAS-MonthlyActiveAcounts/source_code.jsx
@@ -1,8 +1,8 @@
 let static_file_param = "NEAR-ATLAS-DATA-MAAs.csv";
 // Monthly Active Accounts Example
-let rawData = fetch(
-  "https://github-near-data-api.vercel.app/api/static_file_param?filename=" +
-    static_file_param,
+// Monthly Active Accounts Example
+let raw_data = fetch(
+  "https://api.flipsidecrypto.com/api/v2/queries/c493c7b1-cfcc-4aee-ad79-869b4ed8ca90/data/latest",
   {
     subscribe: true,
     method: "GET",
@@ -12,65 +12,10 @@ let rawData = fetch(
   }
 );
 
-const METRIC_NAME = "NEAR Monthly Active Accounts";
+const data = raw_data.body;
 
 let Style = styled.div`
-        
-        
-              .bar {
-                transition: fill 0.2s;
-              }
-        
-              .bar:hover {
-                fill: #ffa726;
-              }
-        
-              .bar-chart {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-              }
-        
-                svg {
-                  width: 80%;
-                }
-        
-                rect {
-                  shape-rendering: crispEdges;
-                  fill: #61dafb;
-                  stroke: #333;
-                  stroke-width: 1;
-                }
-        
-        
                 `;
-
-const colorGenerator = () => {
-  const colors = [
-    "rgb(255, 99, 132)",
-    "rgb(75, 192, 192)",
-    "rgb(153, 102, 255)",
-    "rgb(255, 159, 64)",
-    "rgb(54, 162, 235)",
-    "rgb(201, 203, 207)",
-    "rgb(255, 205, 86)",
-    "rgb(255, 99, 71)",
-    "rgb(147, 112, 219)",
-    "rgb(0, 128, 128)",
-    "rgb(100, 149, 237)",
-    "rgb(127, 255, 0)",
-  ];
-
-  let index = 0;
-
-  return () => {
-    if (index >= colors.length) {
-      index = 0;
-    }
-
-    return colors[index++];
-  };
-};
 
 const months = [
   "January",
@@ -89,70 +34,65 @@ const months = [
 
 // logic start
 
-function parseUTCDate(dateString) {
-  const [month, day, year] = dateString
-    .split("/")
-    .map((str) => parseInt(str, 10));
-  // Subtract 1 from the month, as JavaScript months are zero-based
-  const utcTimestamp = Date.UTC(year, month - 1, day);
-  return new Date(utcTimestamp);
-}
+const NEW_MAAS = {};
+const RETURNING_MAAS = {};
 
-const getBackgroundColor = colorGenerator();
+data.map((entry) => {
+  NEW_MAAS[entry["DAY"]] = entry["NEW_MAAS"];
+  RETURNING_MAAS[entry["DAY"]] = entry["RETURNING_MAAS"];
+});
 
-let processedData = [];
-let labels_of_weeks = [];
-let weeklyData = [];
+// console.log(data);
+// console.log(RETURNING_MAAS);
 
-try {
-  rawData.body.forEach((datum) => {
-    if (!datum["Weekly_Date"]) {
-      return;
-    }
-
-    const activity_date = parseUTCDate(datum["Weekly_Date"]);
-
-    const weeklyString = datum.Weekly.replace(",", "").replace(",", "") || "0"; // default to "0" if Weekly is empty or not defined
-    const weeklyValue = parseFloat(weeklyString);
-
-    // append to labels_of_weeks
-    if (!labels_of_weeks.includes(datum["Weekly_Date"])) {
-      labels_of_weeks.push(datum["Weekly_Date"]);
-      weeklyData.push(weeklyValue);
-    }
-  });
-} catch (err) {
-  console.log(err);
-}
+const dates = data.map((entry) => entry["DAY"]);
 
 // console.log(processedData);
 
 // logic part-2
 
-const data = {
-  labels: labels_of_weeks,
-  datasets: [
-    {
-      label: "MAAs - Trailing 30 Day",
-      data: weeklyData,
-      backgroundColor: ["rgba(54, 162, 235, 0.2)"],
-      borderColor: ["rgba(54, 162, 235, 1)"],
-      borderWidth: 1,
-    },
-  ],
-};
-
-// config
-const config = {
-  type: "bar",
-  data,
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
+const stacked_options = {
+  plugins: {
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart - Stacked",
     },
   },
+  responsive: true,
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+const stacked_bar_labels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+];
+
+const stacked_bar_data = {
+  dates,
+  datasets: [
+    {
+      label: "Returning MAAs",
+      data: RETURNING_MAAS,
+      backgroundColor: "rgb(255, 99, 132)",
+    },
+    {
+      label: "New MAAs",
+      data: NEW_MAAS,
+      backgroundColor: "rgb(75, 192, 192)",
+    },
+  ],
 };
 
 return (
@@ -163,8 +103,7 @@ return (
           <div class="">
             <div class="">
               <div>
-                <h3>{METRIC_NAME}</h3>
-                <BarEl options={config} data={data} />
+                <BarEl options={stacked_options} data={stacked_bar_data} />
               </div>
             </div>
           </div>
