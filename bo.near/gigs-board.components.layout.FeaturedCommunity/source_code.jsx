eab133a320d36ea07ebf63379ad027e39718effb
ailisp <ailisp>
2023-03-28 10:51:23 -0400

    Update gigs-board.components.layout.FeaturedCommunity by bo.near at 2023-03-28 10:51:23.406

--- bo.near/gigs-board.components.layout.FeaturedCommunity/source_code.jsx
+++ bo.near/gigs-board.components.layout.FeaturedCommunity/source_code.jsx
@@ -44,19 +44,20 @@ function href(widgetName, linkProps) {
 }
 /* END_INCLUDE: "common.jsx" */
 
+
 const Hover = styled.div`
-   &:hover {
+  &:hover {
     box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
       0px 1px 2px rgba(16, 24, 40, 0.06);
   }
 `;
 
-return <Hover>
-    <div class="card">
-        <a href={href('Community', {tag: props.tag, title: props.title})}>
-      <img src={props.cover} class="card-img-top"></img>
+return (
+    <Hover className="card">
+      <a href={href("Community", { tag: props.tag, title: props.title, desc: props.desc })}>
+        <img src={props.cover} class="card-img-top"></img>
       </a>
       <div class="h5 pt-3 ps-3">{props.title}</div>
       <div class="ps-3 pb-2 text-secondary">{props.desc}</div>
-    </div>
-</Hover>;
+  </Hover>
+);
