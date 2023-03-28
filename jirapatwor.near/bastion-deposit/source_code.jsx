a145edcffff6eb93c4489ff16acdfa67ded7d5cc
jirapatwor.near <jirapatwor.near>
2023-03-28 10:28:59 -0400

    Update bastion-deposit by jirapatwor.near at 2023-03-28 10:28:59.687

---
+++ jirapatwor.near/bastion-deposit/source_code.jsx
@@ -0,0 +1,27 @@
+return (
+  <div style={{ maxWidth: "300px" }}>
+    <div class="card-body d-grid gap-3">
+      <div>
+        <div class="mb-2 text-muted">Token</div>
+        <select onChange={[]} class="p-2 mb-1" style={{ width: "100%" }}>
+          <option value="">Choose your token</option>
+        </select>
+      </div>
+      <div>
+        <div class="mb-2 text-muted">Amount</div>
+        <input type="number" value={amount} onChange={handleAmount} />
+      </div>
+      {hasError && (
+        <p class="alert alert-danger" role="alert">
+          Amount greater than balance
+        </p>
+      )}
+      <button
+        onClick={[]}
+        style={{ background: "#4ED58A", borderColor: "white" }}
+      >
+        Deposit
+      </button>
+    </div>
+  </div>
+);
