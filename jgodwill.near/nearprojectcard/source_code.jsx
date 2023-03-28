0ca59d37069496c37f852f32cf7896ef54c138c0
Jikugodwill <Jikugodwill>
2023-03-28 04:49:01 -0400

    Update nearprojectcard by jgodwill.near at 2023-03-28 04:49:01.629

--- jgodwill.near/nearprojectcard/source_code.jsx
+++ jgodwill.near/nearprojectcard/source_code.jsx
@@ -74,7 +74,7 @@ return (
             "https://images.pexels.com/photos/10175905/pexels-photo-10175905.jpeg?auto=compress&cs=tinysrgb&lazy=load"
           }
           className="w-100  h-100 shadow rounded-circle img-fluid oveflow-hidden"
-          alt="Project Icon"
+          alt="Thumbnail"
         />
       </IconWrapper>
       <Info>
