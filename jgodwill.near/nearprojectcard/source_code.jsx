f39a558671010000fafafa20c5ae3ff154117ec3
Jikugodwill <Jikugodwill>
2023-03-28 04:50:24 -0400

    Update nearprojectcard by jgodwill.near at 2023-03-28 04:50:24.379

--- jgodwill.near/nearprojectcard/source_code.jsx
+++ jgodwill.near/nearprojectcard/source_code.jsx
@@ -74,7 +74,6 @@ return (
             "https://images.pexels.com/photos/10175905/pexels-photo-10175905.jpeg?auto=compress&cs=tinysrgb&lazy=load"
           }
           className="w-100  h-100 shadow rounded-circle img-fluid oveflow-hidden"
-          alt="Thumbnail"
         />
       </IconWrapper>
       <Info>
