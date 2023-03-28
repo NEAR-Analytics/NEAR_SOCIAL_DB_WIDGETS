0fee7bfc99de6a6716028ebe47b511f16c175826
jirapatwor.near <jirapatwor.near>
2023-03-28 15:53:21 -0400

    Update bastion-deposit by jirapatwor.near at 2023-03-28 15:53:21.255

--- jirapatwor.near/bastion-deposit/source_code.jsx
+++ jirapatwor.near/bastion-deposit/source_code.jsx
@@ -4,6 +4,7 @@ const TokensDetail = {
     name: "Near",
     symbol: "NEAR",
     address: "0xC42C30aC6Cc15faC9bD938618BcaA1a1FaE8501d",
+    cAddress: "0x8C14ea853321028a7bb5E4FB0d0147F183d3B677",
     icon: "near.svg",
     decimals: 24,
   },
@@ -11,6 +12,7 @@ const TokensDetail = {
     name: "USD Coin",
     symbol: "USDC",
     address: "0xB12BFcA5A55806AaF64E99521918A4bf0fC40802",
+    cAddress: "0xe5308dc623101508952948b141fD9eaBd3337D99",
     icon: "usdc.svg",
     decimals: 6,
   },
@@ -18,6 +20,7 @@ const TokensDetail = {
     name: "Tether USD",
     symbol: "USDT.e",
     address: "0x4988a896b1227218e4A686fdE5EabdcAbd91571f",
+    cAddress: "0x845E15A441CFC1871B7AC610b0E922019BaD9826",
     icon: "usdt.svg",
     decimals: 6,
   },
@@ -25,6 +28,7 @@ const TokensDetail = {
     name: "Wrapped Bitcoin",
     symbol: "WBTC",
     address: "0xf4eb217ba2454613b15dbdea6e5f22276410e89e",
+    cAddress: "0xfa786baC375D8806185555149235AcDb182C033b",
     icon: "btc.png",
     decimals: 8,
   },
@@ -32,6 +36,7 @@ const TokensDetail = {
     name: "Ether",
     symbol: "ETH",
     address: ethers.constants.AddressZero,
+    cAddress: "0x4E8fE8fd314cFC09BDb0942c5adCC37431abDCD0",
     icon: "eth.svg",
     decimals: 18,
   },
@@ -39,6 +44,12 @@ const TokensDetail = {
 const Comptroller = "0x6De54724e128274520606f038591A00C5E94a1F6";
 const EIP20InterfaceABI = fetch(
   "https://raw.githubusercontent.com/JirapatWov/bos/main/EIP20.json"
+).body;
+const CEthABI = fetch(
+  "https://raw.githubusercontent.com/JirapatWov/bos/main/CEther.json"
+).body;
+const CErc20ABI = fetch(
+  "https://raw.githubusercontent.com/JirapatWov/bos/main/CErc20.json"
 ).body;
 const sender = Ethers.send("eth_requestAccounts", [])[0];
 if (!sender) return "Please login first";
@@ -82,74 +93,57 @@ const handleApprove = () => {
     TokensDetail[selectedTokenId].decimals
   ).toFixed();
 
-  //   let amountUse = ethers.utils
-  //     .parseUnits(amount, TokensDetail[selectedTokenId].decimals)
-  //     .toHexString();
+  State.update({
+    status: expandedAmount,
+  });
+
+  erc20
+    .approve(TokensDetail[selectedTokenId].cAddress, expandedAmount)
+    .then((transactionHash) => {
+      console.log("transactionHash is " + transactionHash);
+    });
+};
+
+const handleDeposit = () => {
+  if (!selectedTokenId || !amount || hasError) return;
+  State.update({
+    status: "123",
+  });
+
+  let contractABI;
+  if (selectedTokenId == "ETH") {
+    contractABI = CEthABI;
+  } else {
+    contractABI = CErc20ABI;
+  }
+
+  const connection = new ethers.Contract(
+    selectedTokenId,
+    contractABI,
+    Ethers.provider().getSigner()
+  );
+  State.update({
+    status: "345",
+  });
+  const expandedAmount = expandToken(
+    amount,
+    TokensDetail[selectedTokenId].decimals
+  ).toFixed();
+
+  const toBigNumber = ethers.BigNumber.from(expandedAmount);
+  console.log("test");
+  console.log(toBigNumber);
 
   State.update({
     status: expandedAmount,
   });
 
-  erc20.approve(selectedTokenId, expandedAmount).then((transactionHash) => {
+  connection.mint(toBigNumber).then((transactionHash) => {
     console.log("transactionHash is " + transactionHash);
-  });
-
-  //   let transactions = [];
-
-  //   const expandedAmount = expandToken(
-  //     amount,
-  //     TokensDetail[selectedTokenId].decimals
-  //   ).toFixed();
-
-  //   State.update({
-  //     status: expandedAmount,
-  //   });
-
-  //   const depositTransaction = {
-  //     contractName: selectedTokenId,
-  //     methodName: "approve",
-  //     deposit: new Big("1").toFixed(),
-  //     args: {
-  //       spender: sender,
-  //       amount: expandedAmount,
-  //     },
-  //   };
-
-  //   transactions.push(depositTransaction);
-
-  //   Near.call(transactions);
-
-  //   const eip20 = new ethers.Contract(
-  //     Comptroller,
-  //     EIP20InterfaceABI,
-  //     Ethers.provider()
-  //   );
-
-  //   eip20.callStatic.approve(selectedTokenId, expandedAmount).then((data) => {
-  //     State.update({ data });
-  //   });
-};
-
-const handleDeposit = () => {
-  if (!selectedTokenId || !amount || hasError) {
     State.update({
-      status: "falseee",
+      status: transactionHash,
     });
-    return;
-  }
-
-  const eip20 = new ethers.Contract(Comptroller, EIP20InterfaceABI, sender);
-
-  const expandedAmount = expandToken(
-    amount,
-    TokensDetail[selectedTokenId].address
-  ).toFixed();
-
-  eip20.callStatic
-    .cTokenSupply(selectedTokenId, expandedAmount, sender)
-    .then((data) => {
-      State.update({ data });
-    });
+  });
 };
 
 return (
