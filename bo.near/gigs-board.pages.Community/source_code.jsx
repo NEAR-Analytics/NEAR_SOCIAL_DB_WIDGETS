07df43803e960bfe4d60ffdcf3fa18d6fe17eb6a
ailisp <ailisp>
2023-03-28 14:13:06 -0400

    Update gigs-board.pages.Community by bo.near at 2023-03-28 14:13:06.281

--- bo.near/gigs-board.pages.Community/source_code.jsx
+++ bo.near/gigs-board.pages.Community/source_code.jsx
@@ -44,62 +44,6 @@ function href(widgetName, linkProps) {
 }
 /* END_INCLUDE: "common.jsx" */
 
-const Grey = styled.div`
-{
-  background-color: #F3F3F3;
-}`
-
-// TODO nav-underline is available in bootstrap: https://getbootstrap.com/docs/5.3/components/navs-tabs/#underline, 
-// but it's not there in near social, need write such style here
-return (
-  <Grey>
-    <div aria-label="breadcrumb">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item">
-          <a href={href("Feed")}>Developer Governance</a>
-        </li>
-        <li class="breadcrumb-item">
-          <a href={href("Feed")}>Communities</a>
-        </li>
-        <li class="breadcrumb-item active" aria-current="page">
-          {props.title}
-        </li>
-      </ol>
-    </div>
-    <div class="d-flex flex-row align-items-center">
-      <img src={props.icon}></img>
-      <div>
-        <div class="h5 pt-3 ps-3">{props.title}</div>
-        <div class="ps-3 pb-2 text-secondary">{props.desc}</div>
-      </div>
-    </div>
-    <div>
-      <ul class="nav nav-underline">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">
-            <i class="bi-house-door"> </i>
-            Overview
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <i class="bi-chat-square-text"> </i>
-            Discussions
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <i class="bi-kanban"> </i>
-            Sponsorship
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link">
-            <i class="bi-calendar"> </i>
-            Events
-          </a>
-        </li>
-      </ul>
-    </div>
-  </Grey>
-);
+return widget("components.layout.Page", {
+  header: widget("components.layout.CommunityHeader", {title: props.title, icon: props.icon, desc: props.desc}),
+});
