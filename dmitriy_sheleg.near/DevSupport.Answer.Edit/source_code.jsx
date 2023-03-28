0512c80311ac6a00140e5885984cbcc45d504c22
dmitriy_sheleg.near <dmitriy_sheleg.near>
2023-03-27 21:39:32 -0400

    Update DevSupport.Answer.Edit by dmitriy_sheleg.near at 2023-03-27 21:39:32.632

---
+++ dmitriy_sheleg.near/DevSupport.Answer.Edit/source_code.jsx
@@ -0,0 +1,86 @@
+if (!context.accountId) {
+  return "";
+}
+
+const item = props.item;
+
+if (!context.accountId) {
+  return "";
+}
+
+const composeData = () => {
+  const data = {
+    question: {
+      answer: JSON.stringify(Object.assign({ item }, state.content)),
+    },
+    index: {
+      answer: JSON.stringify({
+        key: item,
+        value: {
+          type: "md",
+        },
+      }),
+    },
+  };
+
+  const notifications = state.extractTagNotifications(state.content.text, {
+    type: "social",
+    path: `${context.accountId}/question/answer`,
+  });
+
+  if (props.notifyAccountId) {
+    notifications.push({
+      key: props.notifyAccountId,
+      value: {
+        type: "answer",
+        item,
+      },
+    });
+  }
+
+  console.log("reply to: ", props.notifyAccountId);
+
+  if (notifications.length) {
+    data.index.notify = JSON.stringify(
+      notifications.length > 1 ? notifications : notifications[0]
+    );
+  }
+
+  return data;
+};
+
+State.init({
+  onChange: ({ content }) => {
+    State.update({ content });
+  },
+});
+
+return (
+  <>
+    <Widget
+      src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Compose"
+      props={{
+        placeholder: "Reply",
+        initialText: props.initialText,
+        onChange: state.onChange,
+        onHelper: ({ extractTagNotifications }) => {
+          State.update({ extractTagNotifications });
+        },
+        composeButton: (onCompose) => (
+          <CommitButton
+            disabled={!state.content}
+            force
+            className="btn btn-dark rounded-3"
+            data={composeData}
+            onCommit={() => {
+              onCompose();
+              props.onComment && props.onComment(state.content);
+            }}
+          >
+            Answer
+          </CommitButton>
+        ),
+      }}
+    />
+  </>
+);
