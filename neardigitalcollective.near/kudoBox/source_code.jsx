827bd380d8a4c43351d2073ba2e7104fa6807bc0
near-ndc <near-ndc>
2023-03-27 21:10:26 -0400

    Update kudoBox by neardigitalcollective.near at 2023-03-27 21:10:26.431

--- neardigitalcollective.near/kudoBox/source_code.jsx
+++ neardigitalcollective.near/kudoBox/source_code.jsx
@@ -74,7 +74,7 @@ const RenderAllCommentAnswerBox = (d) => {
                       }
                     >
                       <Widget
-                        src={`mob.near/widget/FollowButton`}
+                        src={`${widgetOwner}/widget/FollowButton`}
                         props={{ accountId: c.accountId }}
                       />
                     </div>
