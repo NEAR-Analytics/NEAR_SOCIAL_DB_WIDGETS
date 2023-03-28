949f8738fe0dcd666eb0eca59ccd7c6ed53b87e0
ailisp <ailisp>
2023-03-28 08:52:07 -0400

    Update gigs-board.components.layout.FeaturedCommunity by bo.near at 2023-03-28 08:52:07.183

--- bo.near/gigs-board.components.layout.FeaturedCommunity/source_code.jsx
+++ bo.near/gigs-board.components.layout.FeaturedCommunity/source_code.jsx
@@ -45,30 +45,16 @@ function href(widgetName, linkProps) {
 /* END_INCLUDE: "common.jsx" */
 
 const Card = styled.div`
-   {
-    border: 1px solid #eceef0;
-
-    /* Shadow/sm */
+   &:hover {
     box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
       0px 1px 2px rgba(16, 24, 40, 0.06);
-    border-radius: 16px;
   }
 `;
 
-return <div class="card">
+return <Card class="card">
       <img src="https://ipfs.near.social/ipfs/bafkreifuflol4fgihxcgpxkl56lygpgdisdfbizrjpt4jhir7mvx3ddh4a" class="card-img-top"></img>
-      <div class="nav navbar-brand h1 p-2">Zero Knowledge</div>
-<div class="mt-2 text-secondary">
+      <div class="h5 pt-3 ps-3">Zero Knowledge</div>
+      <div class="ps-3 pb-2 text-secondary">
 Building a zero knowledge ecosystem on NEAR.
 </div>
-</div>;
-// return <Card>
-//         <div class="p-3">
-//             <div class="d-flex flex-row align-items-center">
-//             <img src="https://ipfs.near.social/ipfs/bafkreihbjm67uavkjkvfqomzx5v63t6kossqwfuptdxfb4vbcpbw3gezdm"></img>
-
-//         </div>
-//         <div class="border-top p-3">
-//             <a class="btn btn-light rounded-5 border w-100">View Details</a>
-//         </div>
-//     </Card>;
+</Card>;
