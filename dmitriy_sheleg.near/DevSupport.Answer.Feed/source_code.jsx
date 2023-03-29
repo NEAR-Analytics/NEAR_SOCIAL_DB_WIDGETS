e7bd7f0213f3f284a0bd349fbb6d1b61589a574b
dmitriy_sheleg.near <dmitriy_sheleg.near>
2023-03-28 20:55:28 -0400

    Update DevSupport.Answer.Feed by dmitriy_sheleg.near at 2023-03-28 20:55:28.765

---
+++ dmitriy_sheleg.near/DevSupport.Answer.Feed/source_code.jsx
@@ -0,0 +1,44 @@
+const index = {
+  action: "answer",
+  key: props.item,
+  options: {
+    limit: props.limit ?? 3,
+    order: "desc",
+    accountId: props.accounts,
+  },
+};
+
+const nested = props.nested || false;
+const admins = props.admins;
+const adminContract = props.adminContract;
+
+const renderItem = (a) =>
+  a.value.type === "md" && (
+    <div key={JSON.stringify(a)}>
+      <Widget
+        src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Answer"
+        props={{
+          accountId: a.accountId,
+          blockHeight: a.blockHeight,
+          admins,
+          adminContract,
+          nested,
+        }}
+      />
+    </div>
+  );
+
+return (
+  <div>
+    <Widget
+      src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Answer.ManualIndexFeed"
+      props={{
+        index,
+        renderItem,
+        nextLimit: 10,
+        loadMoreText: "See more answers",
+        nested,
+      }}
+    />
+  </div>
+);
