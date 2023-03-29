32323f06aeee57a6bba6414b71538d46999828de
near-analytics.near <near-analytics.near>
2023-03-29 15:36:42 -0400

    Update WidgetSource by near-analytics.near at 2023-03-29 15:36:42.761

---
+++ near-analytics.near/WidgetSource/source_code.jsx
@@ -0,0 +1,22 @@
+const src = props.src ?? "mob.near/widget/WidgetSource";
+const [accountId, widget, widgetName] = src.split("/");
+
+const code = Social.get(src);
+
+const text = `
+\`\`\`jsx
+${code}
+\`\`\`
+`;
+
+return (
+  <>
+    <Widget
+      src="mob.near/widget/WidgetMetadata"
+      props={{ accountId, widgetName, expanded: true }}
+    />
+    <Markdown text={text} />
+    <h3>Dependencies</h3>
+    <Widget src="mob.near/widget/WidgetDependencies" props={{ src, code }} />
+  </>
+);
