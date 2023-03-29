722c3b763269b072f5027392fac994ee9949079c
doulos819 <doulos819>
2023-03-29 04:15:14 -0400

    Update LZUTF8Decompressor by chloe.near at 2023-03-29 04:15:14.678

--- chloe.near/LZUTF8Decompressor/source_code.jsx
+++ chloe.near/LZUTF8Decompressor/source_code.jsx
@@ -1,24 +1,29 @@
 const LZUTF8Decompressor = (props) => {
-  const { input } = props;
+  const { frames, onDecompress } = props;
 
   const [output, setOutput] = React.useState("");
 
   const accountId = "chloe.near";
 
   const handleDecompress = async () => {
-    const inputBytes = new TextEncoder().encode(input);
-    const outputBytes = await window.nearApi.connection.provider.query({
-      request_type: "call_function",
-      finality: "final",
-      account_id: accountId,
-      method_name: "decompressBlockToString",
-      args_base64: Buffer.from(inputBytes).toString("base64"),
-      args: {
-        input: inputBytes,
-      },
-    });
+    const decompressedFrames = [];
+    for (const input of frames) {
+      const inputBytes = new TextEncoder().encode(input);
+      const outputBytes = await window.nearApi.connection.provider.query({
+        request_type: "call_function",
+        finality: "final",
+        account_id: accountId,
+        method_name: "decompressBlockToString",
+        args_base64: Buffer.from(inputBytes).toString("base64"),
+        args: {
+          input: inputBytes,
+        },
+      });
 
-    setOutput(new TextDecoder().decode(outputBytes));
+      decompressedFrames.push(new TextDecoder().decode(outputBytes));
+    }
+    setOutput(decompressedFrames);
+    onDecompress(decompressedFrames);
   };
 
   return (
