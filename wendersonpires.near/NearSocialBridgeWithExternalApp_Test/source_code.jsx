85eb0e7763aff4727c95671fbd4f87c45d1b00eb
wpdas <wpdas>
2023-03-30 18:55:05 -0400

    Update NearSocialBridgeWithExternalApp_Test by wendersonpires.near at 2023-03-30 18:55:05.143

--- wendersonpires.near/NearSocialBridgeWithExternalApp_Test/source_code.jsx
+++ wendersonpires.near/NearSocialBridgeWithExternalApp_Test/source_code.jsx
@@ -17,154 +17,7 @@ const code = `
 <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
 <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
 <div id="bridge-root"></div>
-<script>
-// Viewer port
-let viewerPort
-
-// Outside of the component state controller
-let state = {
-  externalAppUrl: '',
-  initialPath: null,
-  iframeHeight: 480,
-  userInfo: null,
-  initialPayload: {},
-  connectMessageSent: false,
-}
-
-// Core Component
-function NearSocialBridgeCore() {
-  const [externalAppUrl, setExternalAppUrl] = React.useState(state.externalAppUrl)
-  const [connectMessageSent, setConnectMessageSent] = React.useState(state.connectMessageSent)
-  const [iframeHeight, setIframeHeight] = React.useState(state.iframeHeight)
-  const [, setUserInfo] = React.useState(state.userInfo)
-
-  React.useEffect(() => {
-    const handler = (e) => {
-      // Set the Viewer port
-      if (!viewerPort && e.data.type === 'connect-view') {
-        viewerPort = e.source
-        setExternalAppUrl(e.data.externalAppUrl)
-        setIframeHeight(e.data.initialIframeHeight || 480)
-        setUserInfo(e.data.userInfo)
-        state.externalAppUrl = e.data.externalAppUrl
-        state.initialPath = e.data.initialPath
-        state.userInfo = e.data.userInfo
-        state.initialPayload = e.data.initialPayload
-        state.iframeHeight = e.data.initialIframeHeight || 480
-      }
-
-      // When get a message from the View
-      if (viewerPort && e.data.from === 'view') {
-        // Is it message to the core?
-        if (e.data.type.includes('core:')) {
-          // process eventual message to core here
-          return
-        }
-
-        // Send to external app
-        sendMessage(e.data)
-      }
-    }
-
-    window.addEventListener('message', handler)
-
-    return () => {
-      window.removeEventListener('message', handler)
-    }
-  }, [])
-
-  const sendMessage = (message) => {
-    var iframe = document.getElementById('myIframe')
-    iframe.contentWindow.postMessage(message, '*')
-  }
-
-  const sendMessageToView = (message) => {
-    viewerPort.postMessage(message, '*')
-  }
-
-  // Answer Factory
-  const buildAnswer = (requestType, payload) => {
-    return {
-      from: 'core',
-      type: 'answer',
-      requestType,
-      payload,
-    }
-  }
-
-  // Build connection payload
-  const buildConnectionPayload = () => ({
-    type: 'connect',
-    payload: {
-      userInfo: state.userInfo,
-      initialPath: state.initialPath,
-      initialPayload: state.initialPayload,
-    },
-    created_at: Date.now(),
-  })
-
-  const onMessageHandler = (message) => {
-    // Internal Request Handler
-    if (message.data.from === 'external-app') {
-      // Send to View
-      sendMessageToView(message.data)
-
-      // core.js - internal handlers
-      switch (message.data.type) {
-        case 'nsb:navigation:sync-content-height':
-          syncContentHeight(message.data.type, message.data.payload)
-          break
-      }
-    }
-  }
-
-  const syncContentHeight = (requestType, payload) => {
-    if (payload.height) {
-      setIframeHeight(payload.height)
-      state.iframeHeight = payload.height
-    }
-
-    const responseBody = buildAnswer(requestType)
-    sendMessage(responseBody)
-  }
-
-  function onLoadHandler() {
-    // On load iframe
-    if (!connectMessageSent) {
-      setConnectMessageSent(true)
-      state.connectMessageSent = true
-
-      // On get msg from External App
-      window.addEventListener('message', onMessageHandler, false)
-    }
-
-    // Send the welcome message (connects with the external app)
-    const welcomePayload = buildConnectionPayload()
-    sendMessage(welcomePayload)
-
-    // Wait a bit and send the message again to ensure the app and scripts are loaded and ready
-    setTimeout(() => {
-      sendMessage(buildConnectionPayload())
-    }, 2000)
-  }
-
-  // Wait for the external app url to render the iframe
-  if (!state.externalAppUrl) return null
-
-  return React.createElement('iframe', {
-    sandbox: 'allow-scripts allow-popups-to-escape-sandbox allow-popups',
-    id: 'myIframe',
-    src: externalAppUrl,
-    style: { border: 'none', width: '100%', height: iframeHeight + 'px', margin: 0, padding: 0 },
-    onLoad: onLoadHandler,
-  })
-}
-
-const domContainer = document.querySelector('#bridge-root')
-const root = ReactDOM.createRoot(domContainer)
-root.render(React.createElement(NearSocialBridgeCore, {}))
-
-</script>
+<script src="https://unpkg.com/near-social-bridge@1.0.0-rc3/widget/core.min.js" crossorigin></script>
 `;
 
 // (i) Discovery API uses cached data structure
@@ -313,7 +166,6 @@ const CORE_STORAGE_KEY = "app:storage";
 const sessionStorageHydrateViewer = (requestType, payload) => {
   if (payload) {
     // store data
-    console.log("Hydrate Viewer", payload);
     Storage.privateSet(CORE_STORAGE_KEY, payload);
 
     const responseBody = buildAnswer(requestType, payload);
@@ -325,7 +177,6 @@ const sessionStorageHydrateViewer = (requestType, payload) => {
 const sessionStorageHydrateApp = (requestType) => {
   // get stored data
   const storageData = Storage.privateGet(CORE_STORAGE_KEY);
-  console.log("Stored data:", storageData);
   const responseBody = buildAnswer(requestType, storageData);
   Utils.sendMessage(responseBody);
 };
