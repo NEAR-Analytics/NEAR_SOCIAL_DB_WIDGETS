a8bfafeb40283c74db1418f81f98988033dd6bc7
golas.near <golas.near>
2023-03-30 20:57:07 -0400

    Update Module-1 by golas.near at 2023-03-30 20:57:07.854

--- golas.near/Module-1/source_code.jsx
+++ golas.near/Module-1/source_code.jsx
@@ -1,8 +1,8 @@
-const { Module2 } = require("golas.near/widget/Module-2");
+// const { Module2 } = require("golas.near/widget/Module-2");
 
 function calc(a, b) {
   const mmm = Module2(a, b);
   return a + b + mmm;
 }
 
-return calc;
+// return calc;
