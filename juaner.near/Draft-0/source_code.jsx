d1fabc4ea863eac8f5a702c4fa01549c6dd1de23
juaner.near <juaner.near>
2023-03-31 01:11:21 -0400

    Update Draft-0 by juaner.near at 2023-03-31 01:11:21.525

--- juaner.near/Draft-0/source_code.jsx
+++ juaner.near/Draft-0/source_code.jsx
@@ -1,1 +1,11 @@
-return <div> good morning</div>;
+let greeting = "Have a great day";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <h1>Hello {props.name}</h1>
+
+      <p> {greeting} </p>
+    </div>
+  </>
+);
