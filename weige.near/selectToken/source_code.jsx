3d3165940b355f616fce70528d73de0acc570053
max-mainnet <max-mainnet>
2023-04-01 10:15:13 -0400

    Update selectToken by weige.near at 2023-04-01 10:15:13.737

--- weige.near/selectToken/source_code.jsx
+++ weige.near/selectToken/source_code.jsx
@@ -284,6 +284,8 @@ return (
               <TokenLine
                 onClick={(e) => {
                   // e.preventDefault();
+
+                  console.log(tokensMeta[token_id]);
                   handleSelect(tokensMeta[token_id]);
                   handleClose();
                 }}
