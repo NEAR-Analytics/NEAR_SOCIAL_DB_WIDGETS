2d92c56481dfebefd154d2a4a6ca0b34fb3d3e6f
golas.near <golas.near>
2023-03-31 21:32:24 -0400

    Update X2 by golas.near at 2023-03-31 21:32:24.801

--- golas.near/X2/source_code.jsx
+++ golas.near/X2/source_code.jsx
@@ -1,3 +1,39 @@
+const aaa = () => {
+  return "aaa";
+};
+
+// function require(src) {
+//   var module = { exports: {} };
+
+//   function func(module) {
+//     const calc = (a, b) => {
+//       return a + b;
+//     };
+//     module.exports = calc;
+//   }
+//   func(module);
+//   return module.exports;
+// }
+
+// require("golas.near/module/MM-1");
+
+// const calcX = () => {
+//   var module = { exports: undefined };
+//   function func() {
+//     const calc = (a, b) => {
+//       return a + b;
+//     };
+//     module.exports = calc;
+//   }
+//   func();
+//   return module.exports;
+// };
+
+// const calc = calcX();
+
 const calc = require("golas.near/module/MM-1");
 
-return <>{calc(10, 20)}</>;
+console.log("calc(1, 22)", calc(1, 23222));
+console.log("aaa", aaa());
+
+return <>12345-{calc(1, 242)}</>;
