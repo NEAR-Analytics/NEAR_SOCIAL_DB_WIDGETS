67bbd53e148f097ae8129836cd32beb5182b7dd2
evrything.near <evrything.near>
2023-04-02 02:31:26 -0400

    Update Everything by evrything.near at 2023-04-02 02:31:26.348

--- evrything.near/Everything/source_code.jsx
+++ evrything.near/Everything/source_code.jsx
@@ -6,8 +6,68 @@ return (
     <Widget
       src={"evrything.near/widget/Everything.Things"}
       props={{
-        type: state.selected,
+        type: "evrything.near/type/Everything",
       }}
     />
   </>
 );
+
+// heavily influenced by saidulbadhon.near's SearchPage
+
+// function onSearchChange({ result, term }) {
+//   console.log(result, term);
+//   if (term.trim()) {
+//     State.update({ searchResults: result || [] });
+//   } else {
+//     State.update({ searchResults: null });
+//   }
+// }
+// const items = state.searchResults || components;
+
+// console.log(items);
+// return (
+//   <div
+//     style={{
+//       padding: 16,
+//       display: "flex",
+//       flexDirection: "column",
+//       alignItems: "center",
+//       gap: 16,
+//       width: "100%",
+//     }}
+//   >
+//     <div
+//       style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//         gap: 4,
+//       }}
+//     >
+//       <Widget src="evrything.near/widget/H1" />
+//     </div>
+
+//     <div
+//       style={{
+//         width: "100%",
+//         display: "grid",
+//         gap: 16,
+//         gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+//       }}
+//     >
+//       {items.length > 0 &&
+//         items?.map((component, index) => (
+//           <div key={index}>
+//             <Widget
+//               src="saidulbadhon.near/widget/SearchPage.ComponentItem"
+//               props={{
+//                 src: `${component.accountId}/widget/${component.widgetName}`,
+//                 blockHeight: component.blockHeight,
+//                 theme: props.theme,
+//               }}
+//             />
+//           </div>
+//         ))}
+//     </div>
+//   </div>
+// );
