74a7f5730081db063a7188e3a659f3ced2e4d1a4
evrything.near <evrything.near>
2023-04-02 02:33:21 -0400

    Update Everything.MainPage by evrything.near at 2023-04-02 02:33:21.932

--- evrything.near/Everything.MainPage/source_code.jsx
+++ evrything.near/Everything.MainPage/source_code.jsx
@@ -3,13 +3,6 @@
 
 return (
   <>
-    <div style={{ paddingLeft: "5em" }}>
-      This application may not work in your viewer. Access it{" "}
-      <a href="https://temp.everything.dev" target="_blank">
-        here
-      </a>
-      .
-    </div>
     <Widget src={"evrything.near/widget/Everything"} />
   </>
 );
