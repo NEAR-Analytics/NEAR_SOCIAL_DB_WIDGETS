b4c7886cfc314bd27d68dfa115f7991a8c7a773b
juaner.near <juaner.near>
2023-04-03 08:24:39 -0400

    Update ref-market-burrow-repay by juaner.near at 2023-04-03 08:24:39.662

--- juaner.near/ref-market-burrow-repay/source_code.jsx
+++ juaner.near/ref-market-burrow-repay/source_code.jsx
@@ -160,7 +160,7 @@ const handleRepay = () => {
     gas: expandToken(300, 12),
     args: {
       receiver_id: BURROW_CONTRACT,
-      amount: expandedAmount.toFixed(0),
+      amount: new Big(amount).toFixed(),
       msg: JSON.stringify(repayTemplate),
     },
   };
