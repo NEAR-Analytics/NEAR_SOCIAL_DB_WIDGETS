0c501d3df6e3f2150397705cda96d99c8aee15d3
contribut3.near <contribut3.near>
2023-04-03 10:22:58 -0400

    Update Vendor.Card by contribut3.near at 2023-04-03 10:22:58.609

--- contribut3.near/Vendor.Card/source_code.jsx
+++ contribut3.near/Vendor.Card/source_code.jsx
@@ -36,7 +36,6 @@ const body = (
       src={`${ownerId}/widget/ProfileLine`}
       props={{
         accountId,
-        isEntity: true,
         imageSize: "3em",
         update: props.update,
       }}
