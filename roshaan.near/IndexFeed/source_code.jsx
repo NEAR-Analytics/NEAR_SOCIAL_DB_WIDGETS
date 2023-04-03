10accafd60662b0ac3bd999c251aac5dee475d4a
roshaan.near <roshaan.near>
2023-04-03 15:03:03 -0400

    Update IndexFeed by roshaan.near at 2023-04-03 15:03:03.381

--- roshaan.near/IndexFeed/source_code.jsx
+++ roshaan.near/IndexFeed/source_code.jsx
@@ -13,88 +13,13 @@ if (!index) {
 
 const filter = props.filter;
 
-const postsQuery = `
-  query IndexerQuery($offset: Int)  {
-    indexer_storage(
-      offset: $offset,
-      where: {function_name: {_eq: "roshaan.near/near-social-posts"}}
-      order_by: {key_name: desc}
-      limit: ${index.options.limit}
-    ) {
-      key_name
-      value
-    }
-  }
-`;
-
-function fetchGraphQL(operationsDoc, operationName, variables) {
-  return asyncFetch(
-    "https://query-api-hasura-vcqilefdcq-uc.a.run.app/v1/graphql",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        query: operationsDoc,
-        variables: variables,
-        operationName: operationName,
-      }),
-    }
-  );
-}
-
-fetchGraphQL(postsQuery, "IndexerQuery").then((result) => {
-  if (result.status === 200) {
-    if (result.body.data) {
-      const initial_items = [];
-      result.body.data.indexer_storage.forEach((posts) => {
-        if (posts.value !== `\"[]\"`) {
-          const parsedOuterString = JSON.parse(posts.value);
-          const parsedInnerString = JSON.parse(parsedOuterString);
-          const post_parsed = JSON.parse(parsedInnerString[0].post);
-          parsedInnerString[0].post = post_parsed;
-          parsedInnerString[0].blockHeight = parsedInnerString[0].block_height;
-          delete parsedInnerString[0].block_height;
-          parsedInnerString[0].accountId = parsedInnerString[0].account_id;
-          delete parsedInnerString[0].account_id;
-          if (parsedInnerString[0].post !== null) {
-            initial_items.push(parsedInnerString[0]);
-          }
-        }
-      });
-      State.update({ initialItems: initial_items });
-    }
-  }
-});
-
-const Post = styled.div`
-  border-bottom: 1px solid #ECEEF0;
-  padding: 24px 0 12px;
-
-  @media (max-width: 1200px) {
-    padding: 12px 0 0;
-  }
-`;
-
 const renderItem =
   props.renderItem ??
-  ((item, i) => {
-    console.log(item, "ITEM");
-    return (
-      <Post className="post" key={JSON.stringify(a)}>
-        <Widget
-          src="roshaan.near/widget/Posts.Post"
-          props={{
-            accountId: item.accountId,
-            blockHeight: item.blockHeight,
-            content: item.post,
-          }}
-        />
-      </Post>
-    );
-  });
-
-// <div key={JSON.stringify(item)}>
-//   #{item.block_height}: {JSON.stringify(item)}
-// </div>
+  ((item, i) => (
+    <div key={JSON.stringify(item)}>
+      #{item.blockHeight}: {JSON.stringify(item)}
+    </div>
+  ));
 const cachedRenderItem = (item, i) => {
   const key = JSON.stringify(item);
 
@@ -116,8 +41,8 @@ index.options.limit = Math.min(
 );
 const reverse = !!props.reverse;
 
-// const initialItems = Social.index(index.action, index.key, index.options);
-if (state.initialItems === null) {
+const initialItems = Social.index(index.action, index.key, index.options);
+if (initialItems === null) {
   return "";
 }
 
@@ -140,64 +65,47 @@ const mergeItems = (newItems) => {
   return items;
 };
 
-const jInitialItems = JSON.stringify(state.initialItems);
+const jInitialItems = JSON.stringify(initialItems);
 if (state.jInitialItems !== jInitialItems) {
   const jIndex = JSON.stringify(index);
-  if (jIndex !== state.jIndex) {
+  const nextFetchFrom = computeFetchFrom(initialItems, index.options.limit);
+  if (jIndex !== state.jIndex || nextFetchFrom !== state.initialNextFetchFrom) {
     State.update({
       jIndex,
       jInitialItems,
-      items: state.initialItems,
+      items: initialItems,
       fetchFrom: false,
-      nextFetchFrom: computeFetchFrom(state.initialItems, index.options.limit),
+      initialNextFetchFrom: nextFetchFrom,
+      nextFetchFrom,
       displayCount: initialRenderLimit,
       cachedItems: {},
     });
   } else {
     State.update({
       jInitialItems,
-      items: mergeItems(state.initialItems),
+      items: mergeItems(initialItems),
     });
   }
 }
 
 if (state.fetchFrom) {
   const limit = addDisplayCount;
-  console.log(state.fetchFrom, "fetch from");
-  fetchGraphQL(postsQuery, "IndexerQuery", {
-    offset: state.fetchFrom,
-  }).then((result) => {
-    if (result.status === 200) {
-      if (result.body.data) {
-        const posts = [];
-        result.body.data.indexer_storage.forEach((posts) => {
-          if (posts.value !== `\"[]\"`) {
-            const parsedOuterString = JSON.parse(posts.value);
-            const parsedInnerString = JSON.parse(parsedOuterString);
-            const post_parsed = JSON.parse(parsedInnerString[0].post);
-            parsedInnerString[0].post = post_parsed;
-            parsedInnerString[0].blockHeight =
-              parsedInnerString[0].block_height;
-            delete parsedInnerString[0].block_height;
-            parsedInnerString[0].accountId = parsedInnerString[0].account_id;
-            delete parsedInnerString[0].account_id;
-
-            if (parsedInnerString[0].post !== null) {
-              posts.push(parsedInnerString[0]);
-            }
-          }
-        });
-        if (posts.length > 0) {
-          const newFoundItems = !!posts.length;
-          State.update({
-            items: mergeItems(posts),
-            fetchFrom: false,
-            nextFetchFrom: computeFetchFrom(posts, limit, newFoundItems),
-          });
-        }
-      }
-    }
-  });
+  const newItems = Social.index(
+    index.action,
+    index.key,
+    Object.assign({}, index.options, {
+      from: state.fetchFrom,
+      subscribe: undefined,
+      limit,
+    })
+  );
+  if (newItems !== null) {
+    State.update({
+      items: mergeItems(newItems),
+      fetchFrom: false,
+      nextFetchFrom: computeFetchFrom(newItems, limit),
+    });
+  }
 }
 
 const filteredItems = state.items;
