13e2c33c9c155fc598a95fb36b120cd60cee6e23
frichard5.near <frichard5.near>
2023-04-03 11:38:22 -0400

    Update NDC-Dashboard by frichard5.near at 2023-04-03 11:38:22.215

--- frichard5.near/NDC-Dashboard/source_code.jsx
+++ frichard5.near/NDC-Dashboard/source_code.jsx
@@ -1,5 +1,27 @@
+const user = "frichard5.near";
+State.init({
+  selectedDao: "marketing.sputnik-dao.near",
+});
+
+const Balances = (
+  <Widget
+    src={`${user}/widget/account_balance`}
+    props={{ account: state.selectedDao }}
+  />
+);
+
+const selectDao = ({ target: { value } }) => {
+  State.update({ selectedDao: value });
+};
+
 return (
   <>
     <h1>NDC Dashboard</h1>
+    <select onChange={selectDao}>
+      <option value="marketing.sputnik-dao.near">Marketing</option>
+      <option value="creativesdao.sputnik-dao.near">Creative</option>
+      <option value="neardevgov.sputnik-dao.near">Gov</option>
+    </select>
+    {Balances}
   </>
 );
