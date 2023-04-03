ea7cd6dca09410c3bcdb4a72ac255f4a4116786f
contribut3.near <contribut3.near>
2023-04-03 08:23:51 -0400

    Update ProjectCard by contribut3.near at 2023-04-03 08:23:51.712

--- contribut3.near/ProjectCard/source_code.jsx
+++ contribut3.near/ProjectCard/source_code.jsx
@@ -1,6 +1,13 @@
 const ownerId = "contribut3.near";
 const projectId = props.projectId;
 
-State.init({});
+State.init({
+  project: null,
+  projectIsFetched: false,
+});
+
+if (!state.projectIsFetched) {
+  Near.asyncView(ownerId, "get_project", { account_id: projectId }, "final", false).then((project) => State.update({ project, projectIsFetched: true }));
+}
 
 return <Widget src={`${ownerId}/widget/Card`} props={{ header: "", body: "", footer: "" }} />
