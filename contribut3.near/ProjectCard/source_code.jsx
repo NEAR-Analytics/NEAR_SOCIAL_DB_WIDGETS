e7c8fde1ab3479da5b99ab1a3e5f4ac26cce0bbc
contribut3.near <contribut3.near>
2023-04-03 08:21:59 -0400

    Update ProjectCard by contribut3.near at 2023-04-03 08:21:59.992

---
+++ contribut3.near/ProjectCard/source_code.jsx
@@ -0,0 +1,6 @@
+const ownerId = "contribut3.near";
+const projectId = props.projectId;
+
+State.init({});
+
+return <Widget src={`${ownerId}/widget/Card`} props={{ header: "", body: "", footer: "" }} />
