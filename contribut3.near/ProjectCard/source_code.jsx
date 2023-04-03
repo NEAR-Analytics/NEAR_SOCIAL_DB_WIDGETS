91291c64d46e5d0bbba4b00e52b747c83b40a751
contribut3.near <contribut3.near>
2023-04-03 09:00:00 -0400

    Update ProjectCard by contribut3.near at 2023-04-03 09:00:00.535

--- contribut3.near/ProjectCard/source_code.jsx
+++ contribut3.near/ProjectCard/source_code.jsx
@@ -66,28 +66,38 @@ const Footer = styled.a`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  padding: .5em 1em;
-  gap: .5em;
+  padding: 0.5em 1em;
+  gap: 0.5em;
   width: 100%;
   height: 2.5em;
-  background: #FAFAFA;
-  border: 1px solid #ECEEF0;
+  background: #fafafa;
+  border: 1px solid #eceef0;
   border-radius: 50px;
   flex: none;
   order: 0;
-  flex-grow: 1;font-style: normal;
+  flex-grow: 1;
+  font-style: normal;
   font-weight: 600;
-  font-size: .9em;
+  font-size: 0.9em;
   line-height: 1em;
   text-align: center;
   color: #101828;
 `;
 
-const footer = <Footer href={`/#/${ownerId}/widget/Index?tab=entity&accountId=${accountId}`} onClick={() => props.update({
-  tab: "entity",
-  content: "",
-  search: "",
-  accountId,
-})}>View details</Footer>;
+const footer = (
+  <Footer
+    href={`/#/${ownerId}/widget/Index?tab=entity&accountId=${accountId}`}
+    onClick={() =>
+      props.update({
+        tab: "entity",
+        content: "",
+        search: "",
+        accountId,
+      })
+    }
+  >
+    View details
+  </Footer>
+);
 
 return <Widget src={`${ownerId}/widget/Card`} props={{ body, footer: "" }} />;
