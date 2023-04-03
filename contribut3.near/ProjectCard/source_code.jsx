3afbca98a866bf636b8c0918caf4759e04906f95
contribut3.near <contribut3.near>
2023-04-03 08:40:29 -0400

    Update ProjectCard by contribut3.near at 2023-04-03 08:40:29.700

--- contribut3.near/ProjectCard/source_code.jsx
+++ contribut3.near/ProjectCard/source_code.jsx
@@ -4,16 +4,28 @@ const projectId = props.projectId;
 State.init({
   founders: null,
   foundersIsFetched: false,
+  requests: null,
+  requestsIsFetched: false,
 });
 
 if (!state.foundersIsFetched) {
   Near.asyncView(
     ownerId,
-    "get_founders", 
+    "get_founders",
     { account_id: projectId },
     "final",
     false
   ).then((founders) => State.update({ founders, foundersIsFetched: true }));
+}
+
+if (!state.requestsIsFetched) {
+  Near.asyncView(
+    ownerId,
+    "get_project_requests",
+    { account_id: projectId },
+    "final",
+    false
+  ).then((requests) => State.update({ requests, requestsIsFetched: true }));
 }
 
 return (
