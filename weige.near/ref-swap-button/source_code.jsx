20a1c17bc1141a621440d324662275d6a8c2f7a0
max-mainnet <max-mainnet>
2023-04-02 23:39:11 -0400

    Update ref-swap-button by weige.near at 2023-04-02 23:39:11.450

--- weige.near/ref-swap-button/source_code.jsx
+++ weige.near/ref-swap-button/source_code.jsx
@@ -3,9 +3,10 @@ const { accountId, notEnough, canSwap, callTx } = props;
 const ButtonWrapper = styled.button`
   
   border-radius: 12px;
-  opacity: ${(props) => (props.notEnough || props.disabled ? "0.5" : "1")};
-  background: ${(props) => (props.notEnough ? "#FF88B3" : "#00FFD1")};
-  font-weight: 700ž
+
+  background-color: ${(props) =>
+    props.notEnough ? "rgba(255,136,179, 0.5)" : "rgba(0,255,209,1)"};
+  font-weight: 700;
   font-size: 18px;
   cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")}
   color:black;
