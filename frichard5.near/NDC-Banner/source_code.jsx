101cf3314e4bf25495b4edb771efcea84c636f1c
frichard5.near <frichard5.near>
2023-04-04 16:04:07 -0400

    Update NDC-Banner by frichard5.near at 2023-04-04 16:04:07.635

--- frichard5.near/NDC-Banner/source_code.jsx
+++ frichard5.near/NDC-Banner/source_code.jsx
@@ -6,13 +6,20 @@ const Logo = styled.img`
     width: 200px;
 `;
 
-const Lines = styled.img`
+const Lines = styled.div`
+    position: absolute;
+    top:0;
+    left:0;
     width: 100%;
+    height: 200px;
+    background: url("https://ipfs.near.social/ipfs/bafybeibrijoowwlrlhxn54skisw2uitt3bex54yirua74nlbo2gkee5d2a");
+    background-size: contain;
+    background-repeat: no-repeat;
 `;
 
 return (
   <Banner role="banner">
     <Logo src="https://ipfs.near.social/ipfs/bafkreihbisodby5rjawdzxqokdbqbrcskp2kayrqcos73smdba2svzciym" />
-    <Lines src="https://ipfs.near.social/ipfs/bafkreihbisodby5rjawdzxqokdbqbrcskp2kayrqcos73smdba2svzciym" />
+    <Lines src="https://ipfs.near.social/ipfs/bafybeifjsr4vz2266xw27t4uvlhelnixnbxozfkmr7c7zhpsnkttucrkli" />
   </Banner>
 );
