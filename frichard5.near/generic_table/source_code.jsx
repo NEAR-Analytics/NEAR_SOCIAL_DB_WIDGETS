3398fb5acb618e91478dda91ade73406b066d1a4
frichard5.near <frichard5.near>
2023-04-04 06:06:49 -0400

    Update generic_table by frichard5.near at 2023-04-04 06:06:49.442

--- frichard5.near/generic_table/source_code.jsx
+++ frichard5.near/generic_table/source_code.jsx
@@ -1,7 +1,23 @@
-const columns = props.columns || [{ label: "columns missing" }];
+const columns = props.columns || [
+  { label: "props columns missing", id: "data" },
+];
+const data = props.data || [{ data: "no data props" }];
 const test = props.test;
 
 test();
+let rows = [];
+
+props.data &&
+  props.forEach.map((d) => {
+    rows.push(
+      <tr>
+        {columns.map((c) => (
+          <td>{d[c.id]}</td>
+        ))}
+      </tr>
+    );
+  });
+
 return (
   <table>
     <tr>
