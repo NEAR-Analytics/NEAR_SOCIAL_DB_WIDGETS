b2d57e5f464e16de97792f79f847935eeb5aeefe
contribut3.near <contribut3.near>
2023-04-04 12:38:23 -0400

    Update Dashboard by contribut3.near at 2023-04-04 12:38:23.780

--- contribut3.near/Dashboard/source_code.jsx
+++ contribut3.near/Dashboard/source_code.jsx
@@ -99,11 +99,11 @@ return (
       </Heading>
       <div className="d-flex flex-row justify-content-between">
         {contentSelector}
-        <Widget
-          src={`${ownerId}/widget/SearchInput`}
-          props={{ search: props.search, update: props.update }}
-        />
       </div>
+      <Widget
+        src={`${ownerId}/widget/SearchInput`}
+        props={{ search: props.search, update: props.update }}
+      />
     </div>
     <div className="px-3 pt-3">{content}</div>
   </div>
