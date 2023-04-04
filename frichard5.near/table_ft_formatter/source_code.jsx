58558833338a85652035c47d9f059c47066ab128
frichard5.near <frichard5.near>
2023-04-04 12:19:09 -0400

    Update table_ft_formatter by frichard5.near at 2023-04-04 12:19:09.616

--- frichard5.near/table_ft_formatter/source_code.jsx
+++ frichard5.near/table_ft_formatter/source_code.jsx
@@ -1,5 +1,13 @@
+const ftList = props.ftList || [];
+
 const numberWithCommas = (x) => {
   return JSON.stringify(x).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+const defaultFt = { icon: "", symbol: "" };
+const findFt = (ftAddress) => {
+  if (!ftList.length) return defaultFt;
+  const ft = ftList.body.find((f) => ftAddress === f.token_account_id);
+  return ft ? ft : defaultFt;
 };
 
 return <div>Hello World</div>;
