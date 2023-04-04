b95e5de905d97b38028125877dcdb404a8ae2d67
contribut3.near <contribut3.near>
2023-04-04 08:08:23 -0400

    Update NavbarControl by contribut3.near at 2023-04-04 08:08:23.580

--- contribut3.near/NavbarControl/source_code.jsx
+++ contribut3.near/NavbarControl/source_code.jsx
@@ -2,9 +2,7 @@ const formName = props.formName;
 const icon = props.icon;
 const text = props.text;
 
-if (!formName || !icon || !text) {
-  return "Cannot render navbar control without form name, icon or text!";
-}
+if (!formName || !icon || !text) { return "Cannot render navbar control without form name, icon or text!"; }
 
 return (
   <li className="nav-item">
@@ -18,8 +16,6 @@ return (
       aria-expanded="false"
       aria-controls={`collapse${formName}Form`}
     >
-
-
       <i className={icon} />
       {text}
     </a>
