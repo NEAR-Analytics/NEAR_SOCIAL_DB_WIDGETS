b6843c108339c3bde079484efefeee17c111db47
frichard5.near <frichard5.near>
2023-04-04 13:51:29 -0400

    Update table_ft_formatter by frichard5.near at 2023-04-04 13:51:29.323

--- frichard5.near/table_ft_formatter/source_code.jsx
+++ frichard5.near/table_ft_formatter/source_code.jsx
@@ -1,13 +1,14 @@
 const ftList = props.ftList || [];
 const { amount, ft } = props;
 console.log("FTLIST", ftList);
+console.log(amount, ft);
 const numberWithCommas = (x) => {
   return JSON.stringify(x).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 const defaultFt = { icon: "", symbol: "" };
 const findFt = (ftAddress) => {
   if (!ftList.length) return defaultFt;
-  const ft = ftList.body.find((f) => ftAddress === f.token_account_id);
+  const ft = ftList.find((f) => ftAddress === f.token_account_id);
   return ft ? ft : defaultFt;
 };
 
