8fd0c61d3c1158cc26eb75bc04f9497861ffe69f
juaner.near <juaner.near>
2023-04-04 08:03:04 -0400

    Update ref-market-burrow-burrow by juaner.near at 2023-04-04 08:03:04.151

--- juaner.near/ref-market-burrow-burrow/source_code.jsx
+++ juaner.near/ref-market-burrow-burrow/source_code.jsx
@@ -27,6 +27,7 @@ const Container = styled.div`
       align-items:center;
       justify-content:space-between;
       margin-left:6px;
+      color:red;
     }
     .template .title{
       font-size:14px;
