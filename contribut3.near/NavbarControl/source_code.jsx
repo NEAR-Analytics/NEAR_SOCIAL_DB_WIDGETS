1114837b49093cc297bba2c18358b59cc49cc013
contribut3.near <contribut3.near>
2023-04-04 09:51:41 -0400

    Update NavbarControl by contribut3.near at 2023-04-04 09:51:41.790

--- contribut3.near/NavbarControl/source_code.jsx
+++ contribut3.near/NavbarControl/source_code.jsx
@@ -3,13 +3,13 @@ const icon = props.icon;
 const text = props.text;
 
 if (!formName || !icon || !text) {
-  return "Cannot render navbar control without form name, icon or text!";
+  // return "Cannot render navbar control without form name, icon or text!";
 }
 
-return (
-  <li className="nav-item">
-    <a> <i className={icon} />
-      {text}
-    </a>
-  </li>
+// return (
+<li className="nav-item">
+  <a> <i className={icon} />
+    {text}
+  </a>
+</li>
 );
