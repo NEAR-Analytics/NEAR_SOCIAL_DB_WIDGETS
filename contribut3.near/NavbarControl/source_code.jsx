a2f2737c1076e2209a95bd830147b2b0f852615b
contribut3.near <contribut3.near>
2023-04-04 08:46:02 -0400

    Update NavbarControl by contribut3.near at 2023-04-04 08:46:02.445

--- contribut3.near/NavbarControl/source_code.jsx
+++ contribut3.near/NavbarControl/source_code.jsx
@@ -8,15 +8,7 @@ if (!formName || !icon || !text) {
 
 return (
   <li className="nav-item">
-    <a
-      className="nav-link active"
-      aria-current="page"
-      data-bs-toggle="collapse"
-      href={`#collapse${formName}Form`}
-      role="button"
-      aria-expanded="false"
-      aria-controls={`collapse${formName}Form`}
-    >
+    <a>
       <i className={icon} />
       {text}
     </a>
