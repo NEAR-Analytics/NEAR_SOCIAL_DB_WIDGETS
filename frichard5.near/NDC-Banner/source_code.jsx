046ed19966c8c9fdc29ff6b546dd670a7bc7195b
frichard5.near <frichard5.near>
2023-04-04 15:45:11 -0400

    Update NDC-Banner by frichard5.near at 2023-04-04 15:45:11.597

--- frichard5.near/NDC-Banner/source_code.jsx
+++ frichard5.near/NDC-Banner/source_code.jsx
@@ -1,1 +1,18 @@
-return <div>Hello World</div>;
+const Banner = styled.div`
+    width: 100%;
+    height: 200px;
+`;
+const Logo = styled.img`
+    width: 200px;
+`;
+
+const Lines = styled.img`
+    width: 100%;
+`;
+
+return (
+  <Banner role="banner">
+    <Logo src="https://ipfs.near.social/ipfs/bafkreihbisodby5rjawdzxqokdbqbrcskp2kayrqcos73smdba2svzciym" />
+    <Lines src="https://ipfs.near.social/ipfs/bafybeics4dmd4pxcz2sheslisxwosj43vt3emdxelhbptv73om6p6wlhxi" />
+  </Banner>
+);
