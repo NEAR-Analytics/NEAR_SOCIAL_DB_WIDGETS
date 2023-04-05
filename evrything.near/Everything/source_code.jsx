cd34583546fcfd339295dad216bc6daf2169e84e
evrything.near <evrything.near>
2023-04-04 23:39:26 -0400

    Update Everything by evrything.near at 2023-04-04 23:39:26.538

--- evrything.near/Everything/source_code.jsx
+++ evrything.near/Everything/source_code.jsx
@@ -1,5 +1,15 @@
 const accountId = props.accountId || "evrything.near";
 
+const types = Social.keys(`${accountId}/type/*`, "final", {
+  return_type: "BlockHeight",
+  values_only: true,
+});
+
+// All types from every account
+// const data = Social.keys("*/type/*");
+
+types = Object.entries(types[accountId].type ?? {});
+
 return (
   <>
     <div
@@ -20,15 +30,18 @@ return (
         }}
       >
         <Widget src="evrything.near/widget/H1" />
-        <p>
-          The elements below are each a representation of that data Type's
-          default view Widget.
-        </p>
-        <p>
-          Head over to the{" "}
-          <a href="https://temp.everything.dev/#/editor">editor</a> to create
-          your own Type.
-        </p>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 4,
+          }}
+        >
+          {types.map((it) => (
+            <button className="text-lowercase">{it[0] + "s"}</button>
+          ))}
+          <button className="text-lowercase">+</button>
+        </div>
       </div>
       <Widget
         src={"evrything.near/widget/Everything.Things"}
@@ -39,63 +52,3 @@ return (
     </div>
   </>
 );
-
-// heavily influenced by saidulbadhon.near's SearchPage
-
-// function onSearchChange({ result, term }) {
-//   console.log(result, term);
-//   if (term.trim()) {
-//     State.update({ searchResults: result || [] });
-//   } else {
-//     State.update({ searchResults: null });
-//   }
-// }
-// const items = state.searchResults || components;
-
-// console.log(items);
-// return (
-//   <div
-//     style={{
-//       padding: 16,
-//       display: "flex",
-//       flexDirection: "column",
-//       alignItems: "center",
-//       gap: 16,
-//       width: "100%",
-//     }}
-//   >
-//     <div
-//       style={{
-//         display: "flex",
-//         flexDirection: "column",
-//         alignItems: "center",
-//         gap: 4,
-//       }}
-//     >
-//       <Widget src="evrything.near/widget/H1" />
-//     </div>
-
-//     <div
-//       style={{
-//         width: "100%",
-//         display: "grid",
-//         gap: 16,
-//         gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
-//       }}
-//     >
-//       {items.length > 0 &&
-//         items?.map((component, index) => (
-//           <div key={index}>
-//             <Widget
-//               src="saidulbadhon.near/widget/SearchPage.ComponentItem"
-//               props={{
-//                 src: `${component.accountId}/widget/${component.widgetName}`,
-//                 blockHeight: component.blockHeight,
-//                 theme: props.theme,
-//               }}
-//             />
-//           </div>
-//         ))}
-//     </div>
-//   </div>
-// );
