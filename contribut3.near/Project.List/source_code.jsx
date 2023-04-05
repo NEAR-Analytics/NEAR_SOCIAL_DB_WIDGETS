da3a55c6d3c0fc675dc025a1e55b77476a502182
contribut3.near <contribut3.near>
2023-04-05 10:57:09 -0400

    Update Project.List by contribut3.near at 2023-04-05 10:57:09.789

--- contribut3.near/Project.List/source_code.jsx
+++ contribut3.near/Project.List/source_code.jsx
@@ -3,6 +3,29 @@ const search = props.search ?? "";
 
 State.init({
   items: [],
+  itemsIsFetched: false,
 });
 
-return <Widget src={`${ownerId}/widget/List`} props={{ items: state.items, createItem: (accountId) => <Widget src={`${ownerId}/widget/Project.Card`} props={{ accountId }} /> }} />
+if (!state.itemsIsFetched) {
+  Near.asyncView(
+    ownerId,
+    "get_projects",
+    {},
+    "final",
+    false
+  ).then((items) => State.update({ items, itemsIsFetched: true }));
+
+  return <>"Loading..."</>;
+}
+
+return (
+  <Widget
+    src={`${ownerId}/widget/List`}
+    props={{
+      items: state.items,
+      createItem: (accountId) => (
+        <Widget src={`${ownerId}/widget/Project.Card`} props={{ accountId }} />
+      ),
+    }}
+  />
+);
