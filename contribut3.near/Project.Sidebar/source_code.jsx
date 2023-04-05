6a5e782792bf7094c120e6c416456eddec0b4684
contribut3.near <contribut3.near>
2023-04-05 12:39:41 -0400

    Update Project.Sidebar by contribut3.near at 2023-04-05 12:39:41.014

--- contribut3.near/Project.Sidebar/source_code.jsx
+++ contribut3.near/Project.Sidebar/source_code.jsx
@@ -1,8 +1,10 @@
 const ownerId = "contribut3.near";
+const accountId = props.accountId;
+const gas = "300000000000000";
 
 return (
   <>
-    <Widget src={`${ownerId}/widget/Project.Details`} />
+    <Widget src={`${ownerId}/widget/Project.Details`} props={{ onSave: (project) => { State.update({ project: { ...state.project, ...project } }); Near.call(ownerId, "edit_project", { account_id: accountId, project: state.project }, gas, "0") } }} />
     <Widget src={`${ownerId}/widget/Project.Achievements`} />
   </>
 );
