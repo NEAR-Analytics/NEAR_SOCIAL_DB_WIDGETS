4dbb289f47bee0eec60361cb0d9e414e0cfe9353
contribut3.near <contribut3.near>
2023-04-05 11:43:11 -0400

    Update Project.Sidebar by contribut3.near at 2023-04-05 11:43:11.985

---
+++ contribut3.near/Project.Sidebar/source_code.jsx
@@ -0,0 +1,8 @@
+const ownerId = "contribut3.near";
+
+return (
+  <>
+    <Widget src={`${ownerId}/widget/Project.Details`} />
+    <Widget src={`${ownerId}/widget/Project.Achievements`} />
+  </>
+);
