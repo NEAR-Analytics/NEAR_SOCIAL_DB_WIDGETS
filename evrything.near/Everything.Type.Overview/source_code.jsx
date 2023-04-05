225f2fe66d7b8dcbe5e11d65f17fd55d7211bdd5
evrything.near <evrything.near>
2023-04-04 23:46:14 -0400

    Update Everything.Type.Overview by evrything.near at 2023-04-04 23:46:14.796

--- evrything.near/Everything.Type.Overview/source_code.jsx
+++ evrything.near/Everything.Type.Overview/source_code.jsx
@@ -1,1 +1,43 @@
-return <div>{props.type}</div>;
+const accountId = props.accountId || "evrything.near";
+const type = props.type || "evrything.near/type/Everything";
+
+return (
+  <>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 16,
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 4,
+        }}
+      >
+        <Widget src="evrything.near/widget/H1" text={type.split("/")[2]} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 4,
+          }}
+        >
+          <button className="text-lowercase">view type details</button>
+          <button className="text-lowercase">create new</button>
+        </div>
+      </div>
+      <Widget
+        src={"evrything.near/widget/Everything.Things"}
+        props={{
+          type,
+        }}
+      />
+    </div>
+  </>
+);
