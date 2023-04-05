bf37e943589fe848abcd0874ba46ad6da7fbd4fd
evrything.near <evrything.near>
2023-04-04 21:59:52 -0400

    Update IndexMasonry by evrything.near at 2023-04-04 21:59:52.518

--- evrything.near/IndexMasonry/source_code.jsx
+++ evrything.near/IndexMasonry/source_code.jsx
@@ -199,9 +199,16 @@ return props.manual ? (
     }
   >
     {props.headerElement}
-    <Masonry breakpointCols={{ default: NUM_COLUMNS_LG, 800: NUM_COLUMNS_SM }}>
+    <div
+      style={{
+        width: "100%",
+        display: "grid",
+        gap: 16,
+        gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+      }}
+    >
       {renderedItems}
-    </Masonry>
+    </div>
     {props.footerElement}
   </InfiniteScroll>
 );
