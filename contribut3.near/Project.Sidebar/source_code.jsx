e01181065a6077675c53ab5c3418e33671eafde4
contribut3.near <contribut3.near>
2023-04-05 13:50:28 -0400

    Update Project.Sidebar by contribut3.near at 2023-04-05 13:50:28.665

--- contribut3.near/Project.Sidebar/source_code.jsx
+++ contribut3.near/Project.Sidebar/source_code.jsx
@@ -2,13 +2,20 @@ const ownerId = "contribut3.near";
 const accountId = props.accountId;
 const gas = "300000000000000";
 
+const companySizeTiers = [
+  [1, 9], // Micro-businesses
+  [10, 49], // Small businesses
+  [50, 249], // Medium-sized businesses
+  [250, 999], // Large businesses
+  [1000, Infinity], // Enterprise businesses
+];
+
 return (
   <>
     <Widget
       src={`${ownerId}/widget/Project.Details`}
       props={{
         onSave: (project) => {
-          console.log({ project });
           State.update({ project: { ...state.project, ...project } });
           Near.call(
             ownerId,
