1f07b8792531c0e13527a2f352788cf7c714caa3
frichard5.near <frichard5.near>
2023-04-05 08:42:39 -0400

    Update NDC-Tabs by frichard5.near at 2023-04-05 08:42:39.488

--- frichard5.near/NDC-Tabs/source_code.jsx
+++ frichard5.near/NDC-Tabs/source_code.jsx
@@ -1,1 +1,27 @@
-return <div>Hello World</div>;
+const tabs = props.tabs || [
+  { value: "tab1", label: "data" },
+  { value: "tab2", label: "time" },
+];
+const tabSelect =
+  props.tabSelect || ((tab) => console.log(`selected tab: ${tab}`));
+
+const tabSelectCb = (tabValue) => {
+  return () => tabSelect(tabValue);
+};
+
+const Tabs = styled.div`
+    display:flex;
+`;
+const TabButton = styled.button`
+    background: ${(props) => props.theme.main};
+`;
+
+let tabList = [];
+
+tabs.forEach((tab) => {
+  tabList.push(
+    <TabButton onClick={tabSelectCb(tab.value)}>{tab.label}</TabButton>
+  );
+});
+
+return <Tabs>{tabList}</Tabs>;
