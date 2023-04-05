502cd9d24140ac89eaf61f090cf0431e3143175d
contribut3.near <contribut3.near>
2023-04-05 09:25:46 -0400

    Update Filter by contribut3.near at 2023-04-05 09:25:46.531

--- contribut3.near/Filter/source_code.jsx
+++ contribut3.near/Filter/source_code.jsx
@@ -1,6 +1,10 @@
 const ownerId = "contribut3.near";
 const name = props.name ?? "Type";
-const options = props.options ?? [{ id: "option-1", text: "Option 1", href: "#" }, { id: "option-2", text: "Option 2", href: "#" }, { id: "option-2", text: "Option 2", href: "#" }];
+const options = props.options ?? [
+  { id: "option-1", text: "Option 1", href: "#" },
+  { id: "option-2", text: "Option 2", href: "#" },
+  { id: "option-2", text: "Option 2", href: "#" },
+];
 const selected = props.selected ?? options[0].id;
 
 const DropdownContainer = styled.div`
@@ -15,14 +19,14 @@ const DropdownItem = styled.li`
     width: 100%;
     clear
     padding: 1em;
-    font-while: ${({ selected }) => selected ? "600" : "400"};
+    font-while: ${({ selected }) => (selected ? "600" : "400")};
     white-space: nowrap;
     text-decoration: none;
     color: #000;
     border: 0;
     text-align: left;
     transition: background-color 0.2s ease-in-out;
-    background-color: ${({ selected }) => selected ? "#e9ecef" : "#fff"};
+    background-color: ${({ selected }) => (selected ? "#e9ecef" : "#fff")};
 
     &:hover {
       color: #000;
@@ -67,7 +71,7 @@ const arrowIcon = (
 const Label = styled.label`
   font-style: normal;
   font-weight: 600;
-  font-size: .95em;
+  font-size: 0.95em;
   line-height: 1.25em;
   color: #344054;
 `;
@@ -77,16 +81,29 @@ const DropdownButton = styled.button`
   flex-direction: row;
   align-items: center;
   padding: 0px;
-  gap: .25em;
+  gap: 0.25em;
   font-style: normal;
   font-weight: 400;
-  font-size: .95em;
+  font-size: 0.95em;
   line-height: 1.25em;
   color: #101828;
 `;
 
 const createOption = ({ id, text, href }) => (
   <DropdownItem key={id} selected={selected === id}>
-    <a href={href} onClick={() => props.update(id)}>{text}</a>
+    <a href={href} onClick={() => props.update(id)}>
+      {text}
+    </a>
   </DropdownItem>
 );
+
+return (
+  <>
+    <Label htmlFor={name}>{name}</Label>
+    <DropdownContainer>
+      <DropdownButton>
+        {options.find((option) => option.id === selected).text}
+      </DropdownButton>
+    </DropdownContainer>
+  </>
+)
