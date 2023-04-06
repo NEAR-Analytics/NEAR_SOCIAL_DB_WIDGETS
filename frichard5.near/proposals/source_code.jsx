f046d9ea7fc7b1cb36e1968862ecd0d39d7bab26
frichard5.near <frichard5.near>
2023-04-06 07:54:07 -0400

    Update proposals by frichard5.near at 2023-04-06 07:54:07.783

--- frichard5.near/proposals/source_code.jsx
+++ frichard5.near/proposals/source_code.jsx
@@ -1,3 +1,8 @@
+const account = props.account || "foundation.near";
+const ftList = props.ftList;
+const apiUrl = `https://api.pikespeak.ai/daos/proposals/${account}`;
+const publicApiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
+
 const forgeUrl = (apiUrl, params) =>
   apiUrl +
   Object.keys(params).reduce(
@@ -5,4 +10,21 @@ const forgeUrl = (apiUrl, params) =>
     "?"
   );
 
+State.init({
+  params: {
+    offset: 0,
+  },
+});
+
+const fetchTransfers = (params) => {
+  const proposals = fetch(forgeUrl(apiUrl, params), {
+    mode: "cors",
+    headers: {
+      "x-api-key": publicApiKey,
+    },
+  });
+  proposals.body && State.update({ proposals: proposals.body });
+};
+fetchTransfers(tate.params);
+console.log("PROPOSALS", state.proposals);
 return <div>Hello World</div>;
