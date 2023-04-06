82afd6ac6b50ed11caa95ba1e693da6440d193a9
contribut3.near <contribut3.near>
2023-04-06 12:03:16 -0400

    Update NavbarControl by contribut3.near at 2023-04-06 12:03:16.911

--- contribut3.near/NavbarControl/source_code.jsx
+++ contribut3.near/NavbarControl/source_code.jsx
@@ -67,7 +67,7 @@ const logo = (
     href={`/#/${ownerId}/widget/Index`}
     onClick={() => props.update({ tab: "home", content: "", search: "" })}
   >
-    <></>NEAR Horizon
+    <Widget src={`${ownerId}/widget/Logo`} />NEAR Horizon
   </LogoArea>
 );
 
