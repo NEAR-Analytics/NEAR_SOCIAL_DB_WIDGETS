51756f626c7a6cf47c530f02d90724d2d5e636bf
frichard5.near <frichard5.near>
2023-04-06 15:42:59 -0400

    Update NDC-select by frichard5.near at 2023-04-06 15:42:59.361

--- frichard5.near/NDC-select/source_code.jsx
+++ frichard5.near/NDC-select/source_code.jsx
@@ -3,7 +3,11 @@ const { options, selectedTab, onChange } = props;
 const Options = [];
 
 options.forEach((o) => {
-  Options.push(<option value={o.value}>{o.label}</option>);
+  Options.push(
+    <option value={o.value} selected={o.value === selectedTab}>
+      {o.label}
+    </option>
+  );
 });
 
 return <select onChange={onChange}>{Options}</select>;
