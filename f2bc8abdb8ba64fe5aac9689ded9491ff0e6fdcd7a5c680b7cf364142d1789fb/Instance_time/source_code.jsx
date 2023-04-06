14f40634835ad09d17cec897cf788fbfcbe262b3
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-04-06 09:04:15 -0400

    Update Instance_time by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-04-06 09:04:15.640

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Instance_time/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Instance_time/source_code.jsx
@@ -321,37 +321,37 @@ return (
         </div>
       </div>
       <div className={thisWidgetClassNames.decorativeDivInHeader}></div>
-    </div>
 
-    <button
-      className={thisWidgetClassNames.newShceduleButtonInMobile}
-      onMouseEnter={() => {
-        State.update({ hoveringElement: tabs.NEW_SCHEDULE.id });
-      }}
-      onMouseLeave={() => {
-        State.update({ hoveringElement: "" });
-      }}
-      onClick={() => {
-        State.update({ tab: tabs.NEW_SCHEDULE.id });
-      }}
-      style={
-        state.hoveringElement == tabs.NEW_SCHEDULE.id ||
-        state.tab == tabs.NEW_SCHEDULE.id
-          ? thisWidgetInlineStyles.newScheduleButtonHovering
-          : thisWidgetInlineStyles.newScheduleButton
-      }
-    >
-      <i
-        className={thisWidgetClassNames.newScheduleButtonInnerIcon}
+      <button
+        className={thisWidgetClassNames.newShceduleButtonInMobile}
+        onMouseEnter={() => {
+          State.update({ hoveringElement: tabs.NEW_SCHEDULE.id });
+        }}
+        onMouseLeave={() => {
+          State.update({ hoveringElement: "" });
+        }}
+        onClick={() => {
+          State.update({ tab: tabs.NEW_SCHEDULE.id });
+        }}
         style={
           state.hoveringElement == tabs.NEW_SCHEDULE.id ||
           state.tab == tabs.NEW_SCHEDULE.id
-            ? thisWidgetInlineStyles.newScheduleButtonInnerIconHovered
-            : thisWidgetInlineStyles.newScheduleButtonInnerIcon
+            ? thisWidgetInlineStyles.newScheduleButtonHovering
+            : thisWidgetInlineStyles.newScheduleButton
         }
-      ></i>
-      {tabs.NEW_SCHEDULE.text}
-    </button>
+      >
+        <i
+          className={thisWidgetClassNames.newScheduleButtonInnerIcon}
+          style={
+            state.hoveringElement == tabs.NEW_SCHEDULE.id ||
+            state.tab == tabs.NEW_SCHEDULE.id
+              ? thisWidgetInlineStyles.newScheduleButtonInnerIconHovered
+              : thisWidgetInlineStyles.newScheduleButtonInnerIcon
+          }
+        ></i>
+        {tabs.NEW_SCHEDULE.text}
+      </button>
+    </div>
 
     <div className={thisWidgetClassNames.bodyContainer}>
       {state.tab == tabs.OPEN_SCHEDULE.id ? (
