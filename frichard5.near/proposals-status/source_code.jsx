4c75912b610e575df8bf1fe31e0799f05ab3fa55
frichard5.near <frichard5.near>
2023-04-06 08:56:11 -0400

    Update proposals-status by frichard5.near at 2023-04-06 08:56:11.014

--- frichard5.near/proposals-status/source_code.jsx
+++ frichard5.near/proposals-status/source_code.jsx
@@ -1,1 +1,52 @@
-return <div>Hello World</div>;
+const widgetProvider = props.widgetProvider;
+const account = props.account || "marketing.sputnik-dao.near";
+const apiUrl = `https://api.pikespeak.ai/daos/proposals/status/${account}`;
+const publicApiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
+
+const forgeUrl = (apiUrl, params) =>
+  apiUrl +
+  Object.keys(params).reduce(
+    (paramString, p) => paramString + `${p}=${params[p]}&`,
+    "?"
+  );
+
+const resPerPage = 10;
+
+State.init({
+  proposalsStatus: [],
+});
+
+const columns = [
+  {
+    id: "status",
+    label: "Status",
+  },
+  {
+    id: "count",
+    label: "Count",
+  },
+];
+
+const GenericTable = (
+  <Widget
+    src={`${widgetProvider}/widget/generic_table`}
+    props={{
+      title: `${account} proposals`,
+      columns,
+      data: state.proposalsStatus,
+    }}
+  />
+);
+
+const fetchTransfers = (params) => {
+  const proposalsStatus = fetch(forgeUrl(apiUrl, params), {
+    mode: "cors",
+    headers: {
+      "x-api-key": publicApiKey,
+    },
+  });
+  proposals.body && State.update({ proposalsStatus: proposalsStatus.body });
+};
+fetchTransfers(state.params);
+console.log("PROPOSALS", state.proposals);
+return <>{GenericTable}</>;
