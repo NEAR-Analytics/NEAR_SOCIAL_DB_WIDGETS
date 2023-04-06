329d26eb784e35fecaf0e25eca23a00092db8d09
frichard5.near <frichard5.near>
2023-04-06 09:15:52 -0400

    Update proposals-status by frichard5.near at 2023-04-06 09:15:52.088

--- frichard5.near/proposals-status/source_code.jsx
+++ frichard5.near/proposals-status/source_code.jsx
@@ -1,3 +1,4 @@
+const widgetProvider = props.widgetProvider;
 const account = props.account || "marketing.sputnik-dao.near";
 const apiUrl = `https://api.pikespeak.ai/daos/proposals/status/${account}`;
 const publicApiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
