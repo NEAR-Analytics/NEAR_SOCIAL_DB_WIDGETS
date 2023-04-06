1b362053450707783a187a422e68f8e8d27f8c10
frichard5.near <frichard5.near>
2023-04-06 13:43:48 -0400

    Update proposals-status by frichard5.near at 2023-04-06 13:43:48.387

--- frichard5.near/proposals-status/source_code.jsx
+++ frichard5.near/proposals-status/source_code.jsx
@@ -1,44 +1,55 @@
-const columns = props.columns || [
-  { label: "props columns missing", id: "data" },
-];
-const data = props.data || [{ data: "no data props" }];
-const { title, nextPage, previousPage, offset, resPerPage } = props;
+const widgetProvider = props.widgetProvider;
+const account = props.account || "marketing.sputnik-dao.near";
+const apiUrl = `https://api.pikespeak.ai/daos/proposals/status/${account}`;
+const publicApiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
 
-const formatRow = (data, column) => {
-  return column.formatter ? column.formatter(data) : <td>{data[column.id]}</td>;
+State.init({
+  proposalsStatus: [],
+});
+
+const order = {
+  proposal_type: 0,
+  InProgress: 1,
+  Approved: 2,
+  Rejected: 3,
+  Expired: 4,
+  Total: 5,
+  ApprovalRate: 6,
 };
-
-let rows = [];
-
-data &&
-  data.forEach((d) => {
-    rows.push(<tr>{columns.map((c) => formatRow(d, c))}</tr>);
+const columns = state.proposalsStatus.reduce((acc, pS) => {
+  Object.keys(pS).forEach((k) => {
+    if (!acc.find((c) => c.id === k)) {
+      acc.push({
+        id: k,
+        label: k === "proposal_type" ? "Type" : k,
+      });
+    }
   });
+  return acc;
+}, []);
+const orderedColumns = columns.sort((a, b) => order[a.id] - order[b.id]);
 
-return (
-  <div>
-    {title && <h2>{title}</h2>}
-    <table>
-      <tr>
-        {columns.map((c) => (
-          <th>{c.label}</th>
-        ))}
-      </tr>
-      {rows}
-    </table>
-    {nextPage && previousPage && resPerPage && (
-      <div>
-        {offset ? (
-          <button onClick={previousPage}>previous {resPerPage}</button>
-        ) : (
-          ""
-        )}
-        {data.length === resPerPage ? (
-          <button onClick={nextPage}>next {resPerPage}</button>
-        ) : (
-          ""
-        )}
-      </div>
-    )}
-  </div>
+const GenericTable = (
+  <Widget
+    src={`${widgetProvider}/widget/generic_table`}
+    props={{
+      title: `Status stats`,
+      columns: orderedColumns,
+      data: state.proposalsStatus,
+    }}
+  />
 );
+
+const fetchTransfers = () => {
+  const proposalsStatus = fetch(apiUrl, {
+    mode: "cors",
+    headers: {
+      "x-api-key": publicApiKey,
+    },
+  });
+  proposalsStatus.body &&
+    State.update({ proposalsStatus: proposalsStatus.body });
+};
+fetchTransfers();
+
+return <>{GenericTable}</>;
