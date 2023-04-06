daaf8133b7aee376159d0cad77863349bc07e3db
contribut3.near <contribut3.near>
2023-04-06 10:54:49 -0400

    Update Inputs.Viewable.Select by contribut3.near at 2023-04-06 10:54:49.810

--- contribut3.near/Inputs.Viewable.Select/source_code.jsx
+++ contribut3.near/Inputs.Viewable.Select/source_code.jsx
@@ -44,43 +44,46 @@ const SaveButton = styled.button`
 `;
 
 const edit = (update, v) => (
-  <Select.Root value={v} onValueChange={update}>
-    <Select.Trigger>
-      <Select.Value />
-      <Select.Icon>
-        <svg
-          width="12"
-          height="8"
-          viewBox="0 0 12 8"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1 1.5L6 6.5L11 1.5"
-            stroke="white"
-            stroke-width="1.66667"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </Select.Icon>
-    </Select.Trigger>
+  <LabelArea>
+    <Select.Root value={v} onValueChange={update}>
+      <Select.Trigger>
+        <Select.Value />
+        <Select.Icon>
+          <svg
+            width="12"
+            height="8"
+            viewBox="0 0 12 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 1.5L6 6.5L11 1.5"
+              stroke="white"
+              stroke-width="1.66667"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </Select.Icon>
+      </Select.Trigger>
 
-    <Select.Portal>
-      <Select.Content>
-        <Select.Viewport>
-          {options.map((option) =>
-            <Select.Item key={option}>
-              <Select.ItemText>{option}</Select.ItemText>
-              <Select.ItemIndicator>
-                "-"
-              </Select.ItemIndicator>
-            </Select.Item>
-          )}
-        </Select.Viewport>
-      </Select.Content>
-    </Select.Portal>
-  </Select.Root>
+      <Select.Portal>
+        <Select.Content>
+          <Select.Viewport>
+            {options.map((option) =>
+              <Select.Item key={option}>
+                <Select.ItemText>{option}</Select.ItemText>
+                <Select.ItemIndicator>
+                  "-"
+                </Select.ItemIndicator>
+              </Select.Item>
+            )}
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+    <SaveButton onClick={() => onSave(v)}>Save</SaveButton>
+  </LabelArea>
 );
 
 return (
