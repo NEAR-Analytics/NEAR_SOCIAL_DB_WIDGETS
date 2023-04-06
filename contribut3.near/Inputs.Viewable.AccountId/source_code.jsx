9e1d36f23837af7c4a2dfa061e34b9dd40e641fb
contribut3.near <contribut3.near>
2023-04-06 11:50:58 -0400

    Update Inputs.Viewable.AccountId by contribut3.near at 2023-04-06 11:50:58.884

--- contribut3.near/Inputs.Viewable.AccountId/source_code.jsx
+++ contribut3.near/Inputs.Viewable.AccountId/source_code.jsx
@@ -9,7 +9,62 @@ const onSave = props.onSave ?? (() => { });
 State.init({
   valid: true,
   errorMessage: <></>,
-})
+});
+
+const validate = (accountId) => {
+  const accountIdRegex =
+    /^(([a-z\d]+[\-_])*[a-z\d]+\.)*([a-z\d]+[\-_])*[a-z\d]+$/;
+
+  if (typeof accountId !== "string") {
+    State.update({
+      valid: false,
+      errorMessage: "Account ID must be a text value!",
+    });
+    return;
+  }
+
+  if (accountId.length < 2) {
+    State.update({
+      valid: false,
+      errorMessage: "Account ID must be at least 2 characters long!",
+    });
+    return;
+  }
+
+  if (accountId.length > 64) {
+    State.update({
+      valid: false,
+      errorMessage: "Account ID must be at most 64 characters long!",
+    });
+    return;
+  }
+
+  if (!accountIdRegex.test(accountId)) {
+    State.update({
+      valid: false,
+      errorMessage: (
+        <>
+          Account ID must follow the rules specified{" "}
+          <a href="https://nomicon.io/DataStructures/Account#account-id-rules">
+            here
+          </a>
+          !
+        </>
+      ),
+    });
+    return;
+  }
+
+  // if (forbiddenIds.has(accountId)) {
+  //   State.update({
+  //     valid: false,
+  //     errorMessage: "This account ID has already been used!",
+  //   });
+  //   return;
+  // }
+  //
+  State.update({ valid: true, errorMessage: "" });
+};
 
 const LabelArea = styled.div`
   display: flex;
