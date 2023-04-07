82474fb8712fa0c8df259c12de936ff6004e7b82
contribut3.near <contribut3.near>
2023-04-07 11:05:56 -0400

    Update Project.HeaderDetails by contribut3.near at 2023-04-07 11:05:56.179

--- contribut3.near/Project.HeaderDetails/source_code.jsx
+++ contribut3.near/Project.HeaderDetails/source_code.jsx
@@ -14,21 +14,34 @@ const Details = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
-  gap: .5em;
+  gap: 0.5em;
 `;
 
 return (
   <Container>
-    <Widget src={`${ownerId}/widget/Project.Icon`} props={{ accountId: ownerId, size: "8em" }} />
+    <Widget
+      src={`${ownerId}/widget/Project.Icon`}
+      props={{ accountId: ownerId, size: "8em" }}
+    />
     <Details>
-      <Widget src={`${ownerId}/widget/NameAndAccount`} props={{ accountId: ownerId, name: "NEAR Horizon" }} />
+      <Widget
+        src={`${ownerId}/widget/NameAndAccount`}
+        props={{ accountId: ownerId, name: "NEAR Horizon" }}
+      />
       <Widget
         src={`${ownerId}/widget/Inputs.Viewable.OneLiner`}
         props={{
           value: "Simple solutions for complex tasks",
           id: "one_liner",
-          onSave: (one_liner) => Near.call("social.near", "set", { data: { [accountId]: { profile: { one_liner } } } })
+          onSave: (one_liner) =>
+            Near.call("social.near", "set", {
+              data: { [accountId]: { profile: { one_liner } } },
+            }),
         }}
+      />
+      <Widget
+        src={`${ownerId}/widget/BadgeList`}
+        props={{ badges: [{ value: "Verified" }, { value: "Fundraiser", color: "#62ebe4" }] }}
       />
     </Details>
   </Container>
