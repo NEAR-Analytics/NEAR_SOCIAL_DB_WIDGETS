b804c0b894e091dae8cf876553b2d6a038942f44
contribut3.near <contribut3.near>
2023-04-07 10:05:31 -0400

    Update Project.HeaderDetails by contribut3.near at 2023-04-07 10:05:31.050

--- contribut3.near/Project.HeaderDetails/source_code.jsx
+++ contribut3.near/Project.HeaderDetails/source_code.jsx
@@ -1,5 +1,5 @@
 const ownerId = "contribut3.near";
 
 return (
-  <Widget src={`${ownerId}/widget/Project.Icon`} props={{ accountId: ownerId }} />
+  <Widget src={`${ownerId}/widget/Project.Icon`} props={{ accountId: ownerId, size: 8em }} />
 );
