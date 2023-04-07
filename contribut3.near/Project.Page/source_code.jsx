4353149efc26faae5b7fe24211c563381c60bcc3
contribut3.near <contribut3.near>
2023-04-07 09:52:19 -0400

    Update Project.Page by contribut3.near at 2023-04-07 09:52:19.411

--- contribut3.near/Project.Page/source_code.jsx
+++ contribut3.near/Project.Page/source_code.jsx
@@ -10,6 +10,14 @@ const getContent = (content) => {
   return content;
 };
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 1em;
+`;
+
 const Header = styled.div`
   display: flex;
   flex-direction: row;
@@ -56,62 +64,73 @@ const Sidebar = styled.div`
 `;
 
 return (
-  <ContentContainer>
-    <MainContent>
-      <Widget
-        src={`${ownerId}/widget/TabSelector`}
-        props={{
-          tab: "project",
-          content: getContent(props.content),
-          search: props.search,
-          update: props.update,
-          buttons: [
-            {
-              id: "overview",
-              text: "Overview",
+  <Container>
+    <Header>
+      <HeaderDetails />
+      <HeaderProgress>
+        <Widget
+          src={`${ownerId}/widget/Project.Progress`}
+          props={{}}
+        />
+      </HeaderProgress>
+    </Header>
+    <ContentContainer>
+      <MainContent>
+        <Widget
+          src={`${ownerId}/widget/TabSelector`}
+          props={{
+            tab: "project",
+            content: getContent(props.content),
+            search: props.search,
+            update: props.update,
+            buttons: [
+              {
+                id: "overview",
+                text: "Overview",
+              },
+              {
+                id: "requests",
+                text: "Requests",
+              },
+              {
+                id: "people",
+                text: "People",
+              },
+              {
+                id: "funding",
+                text: "Funding",
+              },
+              {
+                id: "history",
+                text: "Work history",
+              },
+              {
+                id: "graduation",
+                text: "Graduation",
+              },
+            ],
+          }}
+        />
+        <Widget
+          src={`${ownerId}/widget/Project.About`}
+          props={{
+            onSave: (s) => {
+              console.log(s);
             },
-            {
-              id: "requests",
-              text: "Requests",
+          }}
+        />
+      </MainContent>
+      <Sidebar>
+        <Widget
+          src={`${ownerId}/widget/Project.Sidebar`}
+          props={{
+            accountId: "contribut3.near",
+            onSave: (s) => {
+              console.log(s);
             },
-            {
-              id: "people",
-              text: "People",
-            },
-            {
-              id: "funding",
-              text: "Funding",
-            },
-            {
-              id: "history",
-              text: "Work history",
-            },
-            {
-              id: "graduation",
-              text: "Graduation",
-            },
-          ],
-        }}
-      />
-      <Widget
-        src={`${ownerId}/widget/Project.About`}
-        props={{
-          onSave: (s) => {
-            console.log(s);
-          },
-        }}
-      />
-    </MainContent>
-    <Sidebar>
-      <Widget
-        src={`${ownerId}/widget/Project.Sidebar`}
-        props={{
-          accountId: "contribut3.near",
-          onSave: (s) => {
-            console.log(s);
-          },
-        }}
-      />
-    </Sidebar>
-  </ContentContainer>
+          }}
+        />
+      </Sidebar>
+    </ContentContainer>
+  </Container>
 );
