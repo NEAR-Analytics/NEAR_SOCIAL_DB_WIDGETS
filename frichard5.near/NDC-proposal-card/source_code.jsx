f4cf62ca2fa5d48533e2fca3de0094c3dae57652
frichard5.near <frichard5.near>
2023-04-07 12:27:32 -0400

    Update NDC-proposal-card by frichard5.near at 2023-04-07 12:27:32.123

--- frichard5.near/NDC-proposal-card/source_code.jsx
+++ frichard5.near/NDC-proposal-card/source_code.jsx
@@ -2,7 +2,9 @@ const proposal = props.proposal;
 
 const ProposalCard = styled.div`
   height: 300px;
-  box-shadow: 3px 2px 24px rgba(68, 152, 224, 0.2);
+  width: 80%;
+  margin: 50px 0px;
+  box-shadow: 3px 2px 24px rgba(68, 152, 224, 0.3);
 `;
 
 return <ProposalCard>{proposal.proposal_id}</ProposalCard>;
