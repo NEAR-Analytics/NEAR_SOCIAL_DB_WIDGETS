6df85d5402d04ea1d58f91a27699e5195fcf900e
frichard5.near <frichard5.near>
2023-04-06 22:15:10 -0400

    Update NDC-Page by frichard5.near at 2023-04-06 22:15:10.699

--- frichard5.near/NDC-Page/source_code.jsx
+++ frichard5.near/NDC-Page/source_code.jsx
@@ -12,54 +12,47 @@ const theme = {
 //    background: ${(props) => props.theme.secondaryBlue};
 
 const DashboardContainer = styled.div`
-  * {
-    font-family: 'avenir lt std';    
-  }
-  h2 {
-    font-weight: 750;
-  }
-  padding: 28px;
-  background-size: contain;
-  background-repeat: no-repeat;
-  ${avenirFontFamily}
-  button {
-    margin-left: 10px;
-    color: black;
-    background:rgba(255, 213, 13, 0.5);
-    border: none;
-    &:hover {
-      background:rgba(255, 213, 13, 1);
-      color: black;
+    * {
+        font-family: 'avenir lt std';    
     }
-    &:active {
-        background-color:rgba(255, 213, 13, 0.8);
-        color: black !important;
+    h2 {
+        font-weight: 750;
     }
-  }
-  ::-webkit-scrollbar {
-  width: 8px;
-  height: 8px;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
-  background: transparent;
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: rgba(255, 255, 255, 0.12);
-  border-radius: 3px;
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: rgba(255, 255, 255, 0.08);
-}
-
-::-webkit-scrollbar-corner {
-  background-color: transparent;
-}
+    padding: 28px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    ${avenirFontFamily}
+    button {
+        margin-left: 10px;
+        color: black;
+        background:rgba(255, 213, 13, 0.5);
+        border: none;
+        &:hover {
+        background:rgba(255, 213, 13, 1);
+        color: black;
+        }
+        &:active {
+            background-color:rgba(255, 213, 13, 0.8);
+            color: black !important;
+        }
+    }
+    &::-webkit-scrollbar {
+        width: 8px;
+        height: 8px;
+    }
+    &::-webkit-scrollbar-track {
+        background: transparent;
+    }
+    &::-webkit-scrollbar-thumb {
+        background: rgba(255, 255, 255, 0.12);
+        border-radius: 3px;
+    }
+    &::-webkit-scrollbar-thumb:hover {
+        background: rgba(255, 255, 255, 0.08);
+    }
+    &::-webkit-scrollbar-corner {
+        background-color: transparent;
+    }
 `;
 
 const Banner = <Widget src={`${widgetProvider}/widget/NDC-Banner`} />;
