760edcccad734373a7b09e5b67b39ea71b24d1be
contribut3.near <contribut3.near>
2023-04-07 09:53:28 -0400

    Update Project.Page by contribut3.near at 2023-04-07 09:53:28.575

--- contribut3.near/Project.Page/source_code.jsx
+++ contribut3.near/Project.Page/source_code.jsx
@@ -28,11 +28,18 @@ const Header = styled.div`
 `;
 
 const HeaderDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
   width: 80%;
 `;
 
 const HeaderProgress = styled.div`
-display: flex;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
   width: 20%;
 `;
 
