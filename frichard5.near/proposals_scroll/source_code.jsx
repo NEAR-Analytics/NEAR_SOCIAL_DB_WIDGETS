78f5fc3be871b8f5f3612fbe2c291cd218631b19
frichard5.near <frichard5.near>
2023-04-07 10:44:24 -0400

    Update proposals_scroll by frichard5.near at 2023-04-07 10:44:24.297

--- frichard5.near/proposals_scroll/source_code.jsx
+++ frichard5.near/proposals_scroll/source_code.jsx
@@ -1,11 +1,3 @@
 const { hasMore, fetchMore, cards } = props;
 console.log("CARDS", cards);
-return (
-  <>
-    {cards.length && (
-      <InfiniteScroll loadMore={fetchMore} hasMore={hasMore} pageStart={0}>
-        {cards}
-      </InfiniteScroll>
-    )}
-  </>
-);
+return <div>test</div>;
