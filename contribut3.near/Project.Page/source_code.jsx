249430ff9024ea459e2120eb7ebe16b3fc7e19ec
contribut3.near <contribut3.near>
2023-04-07 09:15:46 -0400

    Update Project.Page by contribut3.near at 2023-04-07 09:15:46.789

--- contribut3.near/Project.Page/source_code.jsx
+++ contribut3.near/Project.Page/source_code.jsx
@@ -1,5 +1,38 @@
 const ownerId = "contribut3.near";
 
-return <>
-  <Widget src={`${ownerId}/widget/Project.About`} props={{ onSave: (s) => { console.log(s) } }} />
-</>
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 1em;
+  width: 100%;
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 1em;
+  width: 80%;
+`;
+
+const Sidebar = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 1em;
+  width: 20%;
+`;
+
+return <ContentContainer>
+  <MainContent>
+
+    <Widget src={`${ownerId}/widget/Project.About`} props={{ onSave: (s) => { console.log(s) } }} />
+  </MainContent>
+  <Sidebar>
+    <Widget src={`${ownerId}/widget/Project.Sidebar`} props={{ onSave: (s) => { console.log(s) } }} />
+  </Sidebar>
+</ContentContainer>
