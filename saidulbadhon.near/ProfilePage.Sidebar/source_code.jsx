36933582cd2b6ea5bd2852a9f3b8b8f767e6dca3
saidulbadhon <saidulbadhon>
2023-04-07 10:07:17 -0400

    Update ProfilePage.Sidebar by saidulbadhon.near at 2023-04-07 10:07:17.441

--- saidulbadhon.near/ProfilePage.Sidebar/source_code.jsx
+++ saidulbadhon.near/ProfilePage.Sidebar/source_code.jsx
@@ -11,22 +11,6 @@ const url = image.ipfs_cid
   ? `https://ipfs.near.social/ipfs/${image?.ipfs_cid}`
   : "https://thewiki.io/static/media/sasha_anon.6ba19561.png";
 
-// const cssFont = fetch(
-//   "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-// ).body;
-
-// const css = fetch(
-//   "https://raw.githubusercontent.com/cryptosynk/near-social-profile/main/css/mainLight.css"
-// ).body;
-
-// const Theme = styled.div`
-//   font-family: "Open Sans", sans-serif;
-//   ${cssFont}
-//   ${css}
-// `;
-
-// if (!cssFont || !css) return "Loading fonts & css";
-
 State.init({
   showEditProfile: false,
 });
