4a0a73f51d1f605dbc7941e25306508da59cec62
contribut3.near <contribut3.near>
2023-04-07 09:48:07 -0400

    Update Project.Progress by contribut3.near at 2023-04-07 09:48:07.633

---
+++ contribut3.near/Project.Progress/source_code.jsx
@@ -0,0 +1,63 @@
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: .5em 1em;
+  gap: .5em;
+  background: #d9f4ff;
+  border-radius: 8px;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0px;
+  gap: .5em;
+`;
+
+const Label = styled.span`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: .5em;
+  font-style: normal;
+  font-weight: 600;
+  font-size: .75em;
+  line-height: 1em;
+  color: #11181c;
+`;
+
+const Value = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  gap: .5em;
+  font-style: normal;
+  font-weight: 700;
+  font-size: .75em;
+  line-height: 14em;
+  color: #11181c;
+`;
+
+return (
+  <Container>
+    <Row>
+      <Label>Credits:</Label>
+      <Value>0 HHZN</Value>
+    </Row>
+    <Row>
+      <Label>Graduation:</Label>
+      <Value>10%</Value>
+    </Row>
+    <Row>
+      <Label>Profile:</Label>
+      <Value>60%</Value>
+    </Row>
+  </Container>
+)
