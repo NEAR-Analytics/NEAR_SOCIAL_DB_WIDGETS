23fa258518fefd2cbed242906171efc444ca37bc
frichard5.near <frichard5.near>
2023-04-06 21:23:01 -0400

    Update NDC-select by frichard5.near at 2023-04-06 21:23:01.209

--- frichard5.near/NDC-select/source_code.jsx
+++ frichard5.near/NDC-select/source_code.jsx
@@ -1,5 +1,15 @@
 const { options, selectedOption, onChange } = props;
 
+const Select = styled.select`
+  border: none;
+  padding: 10px;
+  font-size: 16px; 
+  cursor: pointer; 
+  option:hover {
+    background:red;
+  }
+`;
+
 const Options = [];
 
 options.forEach((o) => {
