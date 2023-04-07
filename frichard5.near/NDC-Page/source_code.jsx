43f55f11067dc6c40c118d197db1e33a80fe11b8
frichard5.near <frichard5.near>
2023-04-06 20:13:50 -0400

    Update NDC-Page by frichard5.near at 2023-04-06 20:13:50.108

---
+++ frichard5.near/NDC-Page/source_code.jsx
@@ -0,0 +1,31 @@
+const widgetContainer = "frichard5.near";
+// Style
+const avenirFontFamily = fetch(
+  "https://fonts.cdnfonts.com/css/avenir-lt-std"
+).body;
+const theme = {
+  main: "#FFD50D",
+  secondaryPink: "#F29BC0",
+  secondaryBlue: "#4498E0",
+};
+
+//    background: ${(props) => props.theme.secondaryBlue};
+
+const DashboardContainer = styled.div`
+  * {
+    font-family: 'avenir lt std';    
+  }
+  h2 {
+    font-weight: 750;
+  }
+  padding: 28px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  ${avenirFontFamily}
+`;
+
+const Banner = <Widget src={`${widgetProvider}/widget/NDC-Banner`} />;
+
+const DashBoard = <Widget src={`${widgetProvider}/widget/NDC-Dashboard`} />;
+
+return <DashboardContainer theme={theme}>{Banner}</DashboardContainer>;
