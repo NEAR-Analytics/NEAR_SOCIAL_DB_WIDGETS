3e3191f33f861c8e2f458fa28d36d13b3955ad85
contribut3.near <contribut3.near>
2023-04-07 10:09:57 -0400

    Update Project.HeaderDetails by contribut3.near at 2023-04-07 10:09:57.290

--- contribut3.near/Project.HeaderDetails/source_code.jsx
+++ contribut3.near/Project.HeaderDetails/source_code.jsx
@@ -1,5 +1,25 @@
 const ownerId = "contribut3.near";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 1em;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: .5em;
+`;
+
 return (
-  <Widget src={`${ownerId}/widget/Project.Icon`} props={{ accountId: ownerId, size: "8em" }} />
+  <Container>
+    <Widget src={`${ownerId}/widget/Project.Icon`} props={{ accountId: ownerId, size: "8em" }} />
+    <Details>
+    </Details>
+  </Container>
 );
