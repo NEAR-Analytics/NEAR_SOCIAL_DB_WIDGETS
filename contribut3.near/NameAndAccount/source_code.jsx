083f0eb22db4df0b0cfc251c4901f871ce3c1f63
contribut3.near <contribut3.near>
2023-04-07 10:21:58 -0400

    Update NameAndAccount by contribut3.near at 2023-04-07 10:21:58.575

---
+++ contribut3.near/NameAndAccount/source_code.jsx
@@ -0,0 +1,23 @@
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: .5em;
+`;
+
+const Name = styled.span`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.625em;
+  line-height: 2.5em;
+  color: #101828;
+`;
+
+const Account = styled.span`
+  font-style: normal;
+  font-weight: 400;
+  font-size: .95em;
+  line-height: 1.4em;
+  color: #687076;
+`;
