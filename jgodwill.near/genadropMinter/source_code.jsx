438d2ac1992c7fc05d4681905bde5973ff9b2430
Jikugodwill <Jikugodwill>
2023-04-08 15:08:06 -0400

    Update genadropMinter by jgodwill.near at 2023-04-08 15:08:06.014

--- jgodwill.near/genadropMinter/source_code.jsx
+++ jgodwill.near/genadropMinter/source_code.jsx
@@ -12,9 +12,9 @@ if (profile === null) {
 }
 
 const handleMint = () => {
-  // if (!(state.title && state.description && state.image.cid)) {
-  //   return;
-  // }
+  if (!state.image.cid) {
+    return;
+  }
   if (!accountId) {
     console.log("Please login");
     State.update({
@@ -153,13 +153,19 @@ const Heading = styled.p`
 
 const Toast = styled.div`
   position: absolute;
-  top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-conten: center;
+  top: 40px;
   right: 20px;
   background-color: red;
   color: #fff;
   padding: 16px;
   border-radius: 8px;
   z-index: 100;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  line-height:1;
 `;
 
 const Elipse = styled.div`
@@ -227,7 +233,7 @@ return (
   <Main className="container-fluid">
     {!accountId && <p>Please sign in with NEAR wallet</p>}
     <Heading className="text-center fs-2 fw-bold">Mint NFT on genadrop</Heading>
-    {!state.image.cid ? (
+    {state.image.cid ? (
       <div>
         <Card className="d-flex flex-column align-items-center">
           <ImageCard>
