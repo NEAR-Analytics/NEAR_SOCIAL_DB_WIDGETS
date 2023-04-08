6415dede4c0786f09760a43e7a2fcfa1aaebf3a8
saidulbadhon <saidulbadhon>
2023-04-08 15:58:17 -0400

    Update ProfilePage.Main by saidulbadhon.near at 2023-04-08 15:58:17.668

--- saidulbadhon.near/ProfilePage.Main/source_code.jsx
+++ saidulbadhon.near/ProfilePage.Main/source_code.jsx
@@ -26,8 +26,6 @@ const cssFont = fetch(
   "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
 ).body;
 
-// https://raw.githubusercontent.com/SaidulBadhon/Bet-BD-Website/master/main.css
-
 const css = fetch(
   props.theme.name === "dark"
     ? "https://raw.githubusercontent.com/SaidulBadhon/Bet-BD-Website/master/main.css"
