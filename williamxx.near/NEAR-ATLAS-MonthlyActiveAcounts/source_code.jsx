f0cf36dc7e7eca1e0e8cc5ae3740bfe3aa8ec067
williamxx.near <williamxx.near>
2023-04-09 18:08:39 -0400

    Update NEAR-ATLAS-MonthlyActiveAcounts by williamxx.near at 2023-04-09 18:08:39.468

---
+++ williamxx.near/NEAR-ATLAS-MonthlyActiveAcounts/source_code.jsx
@@ -0,0 +1,84 @@
+let static_file_param = "NEAR-ATLAS-DATA-MAAs.csv";
+// Monthly Active Accounts Example
+// Monthly Active Accounts Example
+let raw_data = fetch(
+  "https://api.flipsidecrypto.com/api/v2/queries/c493c7b1-cfcc-4aee-ad79-869b4ed8ca90/data/latest",
+  {
+    subscribe: true,
+    method: "GET",
+    headers: {
+      Accept: "*/*",
+    },
+  }
+);
+
+const data = raw_data.body || [];
+
+let Style = styled.div`
+                `;
+
+// logic start
+
+const sortedData = data.sort((a, b) => {
+  return new Date(a["DAY"]) - new Date(b["DAY"]);
+});
+
+const NEW_MAAS = {};
+const RETURNING_MAAS = {};
+
+data.map((entry) => {
+  NEW_MAAS[entry["DAY"]] = entry["NEW_MAAS"];
+  RETURNING_MAAS[entry["DAY"]] = entry["RETURNING_MAAS"];
+});
+
+// console.log(data);
+// console.log(RETURNING_MAAS);
+
+const dates = data.map((entry) => entry["DAY"]);
+
+// console.log(processedData);
+
+// logic part-2
+
+const stacked_options = {
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+const stacked_bar_data = {
+  dates,
+  datasets: [
+    {
+      label: "New MAAs",
+      data: NEW_MAAS,
+      backgroundColor: "rgb(75, 192, 192)",
+    },
+    {
+      label: "Returning MAAs",
+      data: RETURNING_MAAS,
+      backgroundColor: "rgb(255, 99, 132)",
+    },
+  ],
+};
+
+return (
+  <Style>
+    <div className="text-bg-dark container">
+      {data !== null ? (
+        <div className="rounded-4 p-3 mb-4">
+          <div className="">
+            <BarEl options={stacked_options} data={stacked_bar_data} />
+          </div>
+        </div>
+      ) : (
+        <div>Loading ...</div>
+      )}
+    </div>
+  </Style>
+);
