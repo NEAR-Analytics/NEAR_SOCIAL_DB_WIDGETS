7806806d4e6ae8864e5f9f0e4de3555230be8e07
evrything.near <evrything.near>
2023-04-09 18:29:18 -0400

    Update Everything.View.Thing by evrything.near at 2023-04-09 18:29:18.358

--- evrything.near/Everything.View.Thing/source_code.jsx
+++ evrything.near/Everything.View.Thing/source_code.jsx
@@ -4,6 +4,16 @@ const ThingContainer = styled.div`
   padding: 2px;
 `;
 
+const Toolbar = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const Icon = styled.div`
+  cursor: pointer;
+`;
+
 const src = props.src;
 
 const accountId = props.accountId;
@@ -12,8 +22,29 @@ const content = JSON.parse(
   Social.get(`${accountId}/thing/main`, blockHeight) ?? "null"
 );
 
+const deleteThing = () => {
+  Social.set(
+    {
+      modification: {
+        [blockHeight]: {
+          "": JSON.stringify({ action: "DELETE" }),
+        },
+      },
+    },
+    {
+      force: true,
+    }
+  );
+};
+
 return (
   <ThingContainer>
+    {context.accountId === accountId ? (
+      <Toolbar>
+        <Icon onClick={deleteThing}>Delete</Icon>
+      </Toolbar>
+    ) : null}
+
     <Widget
       src={src}
       props={{
