0433b4fd376a74c67496d79581a6d38fd262c25c
contribut3.near <contribut3.near>
2023-04-10 06:52:52 -0400

    Update Logo by contribut3.near at 2023-04-10 06:52:52.620

--- contribut3.near/Logo/source_code.jsx
+++ contribut3.near/Logo/source_code.jsx
@@ -1,7 +1,7 @@
 const size = props.size ?? 128;
 const ratio = 128 / 111;
 const height = size * ratio;
-const full = props.full ?? false;
+const full = props.full ?? true;
 
 const fullLogo = (
   <svg width="209" height="30" viewBox="0 0 209 30" fill="none" xmlns="http://www.w3.org/2000/svg">
