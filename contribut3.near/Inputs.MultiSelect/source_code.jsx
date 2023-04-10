e3244b386051f1e02f23594715df49ccef1cf50b
contribut3.near <contribut3.near>
2023-04-10 12:29:02 -0400

    Update Inputs.MultiSelect by contribut3.near at 2023-04-10 12:29:02.463

--- contribut3.near/Inputs.MultiSelect/source_code.jsx
+++ contribut3.near/Inputs.MultiSelect/source_code.jsx
@@ -1,6 +1,7 @@
 const label = props.label ?? "Label";
 const placeholder = props.placeholder ?? "Placeholder";
 const value = props.value ?? "";
+const options = props.options ?? [];
 const onChange = props.onChange ?? (() => { });
 const validate = props.validate ?? (() => { });
 const error = props.error ?? "";
