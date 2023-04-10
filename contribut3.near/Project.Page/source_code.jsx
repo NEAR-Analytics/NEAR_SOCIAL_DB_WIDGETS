393566f103c8b3217084d35ef2cabe5b34f10aee
contribut3.near <contribut3.near>
2023-04-10 06:36:31 -0400

    Update Project.Page by contribut3.near at 2023-04-10 06:36:31.049

--- contribut3.near/Project.Page/source_code.jsx
+++ contribut3.near/Project.Page/source_code.jsx
@@ -1,6 +1,13 @@
 const ownerId = "contribut3.near";
 
-const availableContent = ["overview", "requests", "people", "funding", "history", "graduation"];
+const availableContent = [
+  "overview",
+  "requests",
+  "people",
+  "funding",
+  "history",
+  "graduation",
+];
 
 const getContent = (content) => {
   if (!content || !availableContent.includes(content)) {
@@ -59,7 +66,7 @@ const MainContent = styled.div`
   justify-content: flex-start;
   gap: 1em;
   width: 80%;
-  padding-top: .25em;
+  padding-top: 0.25em;
 `;
 
 const Sidebar = styled.div`
@@ -70,6 +77,19 @@ const Sidebar = styled.div`
   gap: 1em;
   width: 20%;
 `;
+const plusIcon = (
+  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_1624_38049)">
+      <path d="M9 6V12M6 9H12M16.5 9C16.5 13.1421 13.1421 16.5 9 16.5C4.85786 16.5 1.5 13.1421 1.5 9C1.5 4.85786 4.85786 1.5 9 1.5C13.1421 1.5 16.5 4.85786 16.5 9Z" stroke="#11181C" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
+    </g>
+    <defs>
+      <clipPath id="clip0_1624_38049">
+        <rect width="18" height="18" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+
+);
 
 return (
   <Container>
@@ -81,10 +101,7 @@ return (
         />
       </HeaderDetails>
       <HeaderProgress>
-        <Widget
-          src={`${ownerId}/widget/Project.Progress`}
-          props={{}}
-        />
+        <Widget src={`${ownerId}/widget/Project.Progress`} props={{}} />
       </HeaderProgress>
     </Header>
     <ContentContainer>
