af612f7c6d0d38c465a46d00ced84f5546a6a31c
elliotbraem.near <elliotbraem.near>
2023-04-10 17:49:59 -0400

    Update Everything by elliotbraem.near at 2023-04-10 17:49:59.905

---
+++ elliotbraem.near/Everything/source_code.jsx
@@ -0,0 +1,23 @@
+return (
+  <div className="d-flex flex-column">
+    <div className="mt-auto py-3">
+      <div className="container">
+        <div className="d-flex justify-content-end">
+          <a href={"/#/evrything-docs.near/widget/Everything.Documentation"}>
+            documentation
+          </a>
+        </div>
+      </div>
+    </div>
+    <Widget
+      src="evrything.near/widget/Everything.Template"
+      props={{
+        accountId: "elliotbraem.near",
+        font: "Verdana",
+        type: "everything",
+        text: "elliot",
+        domain: "elliotbraem",
+      }}
+    />
+  </div>
+);
