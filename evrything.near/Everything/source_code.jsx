81db52b5f62c442590bfdd4632ea4b839b3517e5
evrything.near <evrything.near>
2023-04-10 16:03:05 -0400

    Update Everything by evrything.near at 2023-04-10 16:03:05.563

--- evrything.near/Everything/source_code.jsx
+++ evrything.near/Everything/source_code.jsx
@@ -1,12 +1,23 @@
 return (
-  <Widget
-    src="evrything.near/widget/Everything.Template"
-    props={{
-      accountId: "evrything.near",
-      font: "Times New Roman",
-      type: "everything",
-      text: "everything",
-      domain: "everything",
-    }}
-  />
+  <div className="d-flex flex-column min-vh-100">
+    <Widget
+      src="evrything.near/widget/Everything.Template"
+      props={{
+        accountId: "evrything.near",
+        font: "Times New Roman",
+        type: "everything",
+        text: "everything",
+        domain: "everything",
+      }}
+    />
+    <div className="mt-auto py-3 bg-light">
+      <div className="container">
+        <div className="d-flex justify-content-end">
+          <a href={"/#/evrything-docs.near/widget/Everything.Documentation"}>
+            documentation
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 );
