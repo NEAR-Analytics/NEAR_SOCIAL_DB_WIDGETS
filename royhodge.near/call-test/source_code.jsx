3118130ffd21c9c1f7d43f6a85de91bb742f8a1e
modelomaker812 <modelomaker812>
2023-04-10 03:47:41 -0400

    Update call-test by royhodge.near at 2023-04-10 03:47:41.545

---
+++ royhodge.near/call-test/source_code.jsx
@@ -0,0 +1,26 @@
+State.init({
+  value: "value to update",
+});
+
+console.log(Near.view("dev-1680714992236-65898712788505", "get_user"));
+
+const testCall = () => {
+  return Near.call("dev-1680714992236-65898712788505", "set_user", {
+    message: "Near User #1",
+  });
+};
+
+const testView = () => {
+  State.update({
+    value: Near.view("dev-1680714992236-65898712788505", "get_user"),
+  });
+};
+
+return (
+  <div>
+    <button onClick={testCall}>test call</button>
+    <button onClick={testView}>test view</button>
+
+    <div>{state.value}</div>
+  </div>
+);
