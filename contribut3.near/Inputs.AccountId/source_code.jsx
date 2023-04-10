834a289babf18d8e589bf12bb117844536b0b4d0
contribut3.near <contribut3.near>
2023-04-10 08:12:12 -0400

    Update Inputs.AccountId by contribut3.near at 2023-04-10 08:12:12.921

--- contribut3.near/Inputs.AccountId/source_code.jsx
+++ contribut3.near/Inputs.AccountId/source_code.jsx
@@ -1,49 +1,79 @@
-const label = props.label ?? "Label";
-const placeholder = props.placeholder ?? "Placeholder";
+const ownerId = "contribut3.near";
+const label = props.label ?? "Account ID";
+const placeholder = props.placeholder ?? "Enter your account ID";
 const value = props.value ?? "";
 const onChange = props.onChange ?? (() => { });
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
-  padding: 0px;
-  gap: 0.45em;
-  width: 100%;
-`;
+State.init({
+  valid: true,
+  errorMessage: <></>,
+});
 
-const Label = styled.label`
-  font-style: normal;
-  font-weight: 600;
-  font-size: 0.95em;
-  line-height: 1.25em;
-  color: #344054;
-`;
+const validate = (accountId) => {
+  const accountIdRegex =
+    /^(([a-z\d]+[\-_])*[a-z\d]+\.)*([a-z\d]+[\-_])*[a-z\d]+$/;
 
-const Input = styled.input`
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 0.5em 0.75em;
-  gap: 0.5em;
-  background: #ffffff;
-  border: 1px solid #d0d5dd;
-  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
-  border-radius: 4px;
-  color: #101828;
-  width: 100%;
-`;
+  if (typeof accountId !== "string") {
+    State.update({
+      valid: false,
+      errorMessage: "Account ID must be a text value!",
+    });
+    return;
+  }
+
+  if (accountId.length < 2) {
+    State.update({
+      valid: false,
+      errorMessage: "Account ID must be at least 2 characters long!",
+    });
+    return;
+  }
+
+  if (accountId.length > 64) {
+    State.update({
+      valid: false,
+      errorMessage: "Account ID must be at most 64 characters long!",
+    });
+    return;
+  }
+
+  if (!accountIdRegex.test(accountId)) {
+    State.update({
+      valid: false,
+      errorMessage: (
+        <>
+          Account ID must follow the rules specified{" "}
+          <a href="https://nomicon.io/DataStructures/Account#account-id-rules">
+            here
+          </a>
+          !
+        </>
+      ),
+    });
+    return;
+  }
+
+  // if (forbiddenIds.has(accountId)) {
+  //   State.update({
+  //     valid: false,
+  //     errorMessage: "This account ID has already been used!",
+  //   });
+  //   return;
+  // }
+  //
+  State.update({ valid: true, errorMessage: "" });
+};
 
 return (
-  <Container>
-    <Label>{label}</Label>
-    <Input
-      type="text"
-      placeholder={placeholder}
-      value={value}
-      onChange={({ target: { value } }) => onChange(value)}
-    />
-  </Container>
+  <Widget
+    src={`${ownerId}/widget/Inputs.Text`}
+    props={{
+      label,
+      placeholder,
+      value,
+      onChange,
+      validate,
+      error: state.error,
+    }}
+  />
 );
