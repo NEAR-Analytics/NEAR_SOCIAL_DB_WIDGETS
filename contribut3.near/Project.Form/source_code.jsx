d411ca9cea8dad976dc9799271cbc447406c013b
contribut3.near <contribut3.near>
2023-04-10 07:47:33 -0400

    Update Project.Form by contribut3.near at 2023-04-10 07:47:33.400

--- contribut3.near/Project.Form/source_code.jsx
+++ contribut3.near/Project.Form/source_code.jsx
@@ -1,7 +1,67 @@
 const ownerId = "contribut3.near";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  gap: 3em;
+`;
+
+const Header = styled.h1`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 2em;
+  line-height: 1.4em;
+  text-align: center;
+  color: #000000;
+`;
+
+const SubHeader = styled.h2`
+  font-style: normal;
+  font-weight: 400;
+  font-size: .95em;
+  line-height: 1.25em;
+  text-align: center;
+  color: #101828;
+`;
+
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 80%;
+  gap: 1em;
+`;
+
+const FormHeader = styled.h3`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px 0px .5em;
+  border-bottom: 1px solid #eceef0;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.125em;
+  line-height: 1.25em;
+  color: #000000;
+`;
+
 State.init({
   name: "",
 });
 
-return <Widget src={`${ownerId}/widget/Inputs.Text`} props={{ label: "Project name *", placeholder: "Layers", value: state.name, onChange: (name) => State.update({ name }) }} />;
+return (
+  <Widget
+    src={`${ownerId}/widget/Inputs.Text`}
+    props={{
+      label: "Project name *",
+      placeholder: "Layers",
+      value: state.name,
+      onChange: (name) => State.update({ name }),
+    }}
+  />
+);
