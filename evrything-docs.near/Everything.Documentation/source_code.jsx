93cbbc34e5a69037ea3b11df6b80d5595db0dab7
evrything-docs.near <evrything-docs.near>
2023-04-10 13:34:27 -0400

    Update Everything.Documentation by evrything-docs.near at 2023-04-10 13:34:27.409

---
+++ evrything-docs.near/Everything.Documentation/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <Widget
+    src="evrything.near/widget/Everything.Template"
+    props={{
+      accountId: "evrything-docs.near", // which account's Types to use (your near account)
+      font: "Times New Roman", // select a web safe font
+      text: "documentation", // main title
+      domain: "everything-docs", // domain data should be saved to
+    }}
+  />
+);
