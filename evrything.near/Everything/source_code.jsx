4f0909f83cd84669264289ed4331a287436226bd
evrything.near <evrything.near>
2023-04-10 16:03:39 -0400

    Update Everything by evrything.near at 2023-04-10 16:03:39.412

--- evrything.near/Everything/source_code.jsx
+++ evrything.near/Everything/source_code.jsx
@@ -1,5 +1,14 @@
 return (
-  <div className="d-flex flex-column min-vh-100">
+  <div className="d-flex flex-column">
+    <div className="mt-auto py-3">
+      <div className="container">
+        <div className="d-flex justify-content-end">
+          <a href={"/#/evrything-docs.near/widget/Everything.Documentation"}>
+            documentation
+          </a>
+        </div>
+      </div>
+    </div>
     <Widget
       src="evrything.near/widget/Everything.Template"
       props={{
@@ -10,14 +19,5 @@ return (
         domain: "everything",
       }}
     />
-    <div className="mt-auto py-3 bg-light">
-      <div className="container">
-        <div className="d-flex justify-content-end">
-          <a href={"/#/evrything-docs.near/widget/Everything.Documentation"}>
-            documentation
-          </a>
-        </div>
-      </div>
-    </div>
   </div>
 );
