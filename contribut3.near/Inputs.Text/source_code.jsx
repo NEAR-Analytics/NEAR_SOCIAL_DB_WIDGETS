d042f97679da2f526a88d7de7e07a1a1437a1422
contribut3.near <contribut3.near>
2023-04-10 07:36:49 -0400

    Update Inputs.Text by contribut3.near at 2023-04-10 07:36:49.138

---
+++ contribut3.near/Inputs.Text/source_code.jsx
@@ -0,0 +1,30 @@
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: .45em;
+  width: 100%;
+`;
+
+const Label = styled.label`
+  font-style: normal;
+  font-weight: 600;
+  font-size: .95em;
+  line-height: 1.25em;
+  color: #344054;
+`;
+
+const Input = styled.input`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: .5em .75em;
+  gap: .5em;
+  background: #ffffff;
+  border: 1px solid #d0d5dd;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  border-radius: 4px;
+  color: #101828;
+`;
