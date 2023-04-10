2c096864f6646eb8a2d019382f945181dbe0f918
contribut3.near <contribut3.near>
2023-04-10 06:30:59 -0400

    Update Buttons.Green by contribut3.near at 2023-04-10 06:30:59.739

--- contribut3.near/Buttons.Green/source_code.jsx
+++ contribut3.near/Buttons.Green/source_code.jsx
@@ -1,4 +1,18 @@
 const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: .5em 1em;
+  gap: .5em;
+  border-radius: 50px;
   background: #00ec97;
   color: #11181c;
+  font-style: normal;
+  font-weight: 600;
+  font-size: .95em;
+  line-height: 1em;
+  text-align: center;
 `;
+
+return <Button onClick={props.onClick}>{props.text}</Button>;
