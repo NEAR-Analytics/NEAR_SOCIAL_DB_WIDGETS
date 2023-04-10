fb3018165cd33da62ab8a389c435a241e226234e
contribut3.near <contribut3.near>
2023-04-10 06:37:01 -0400

    Update Project.Page by contribut3.near at 2023-04-10 06:37:01.383

--- contribut3.near/Project.Page/source_code.jsx
+++ contribut3.near/Project.Page/source_code.jsx
@@ -80,7 +80,7 @@ const Sidebar = styled.div`
 const plusIcon = (
   <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g clip-path="url(#clip0_1624_38049)">
-      <path d="M9 6V12M6 9H12M16.5 9C16.5 13.1421 13.1421 16.5 9 16.5C4.85786 16.5 1.5 13.1421 1.5 9C1.5 4.85786 4.85786 1.5 9 1.5C13.1421 1.5 16.5 4.85786 16.5 9Z" stroke="#11181C" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M9 6V12M6 9H12M16.5 9C16.5 13.1421 13.1421 16.5 9 16.5C4.85786 16.5 1.5 13.1421 1.5 9C1.5 4.85786 4.85786 1.5 9 1.5C13.1421 1.5 16.5 4.85786 16.5 9Z" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
     </g>
     <defs>
       <clipPath id="clip0_1624_38049">
@@ -88,7 +88,6 @@ const plusIcon = (
       </clipPath>
     </defs>
   </svg>
-
 );
 
 return (
