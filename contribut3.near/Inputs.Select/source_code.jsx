36ef07925a476df65c2a1b96950fc975f527472c
contribut3.near <contribut3.near>
2023-04-10 09:13:47 -0400

    Update Inputs.Select by contribut3.near at 2023-04-10 09:13:47.164

--- contribut3.near/Inputs.Select/source_code.jsx
+++ contribut3.near/Inputs.Select/source_code.jsx
@@ -60,24 +60,26 @@ return (
     <Label>{label}</Label>
     <Select.Root onValueChange={(value) => onChange(options.find((option) => option.value === value))}>
       <Select.Trigger>
-        <Select.Value aria-label={value} />
-        <Select.Icon>
-          <svg
-            width="12"
-            height="8"
-            viewBox="0 0 12 8"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1 1.5L6 6.5L11 1.5"
-              stroke="currentColor"
-              stroke-width="1.66667"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </Select.Icon>
+        <Input>
+          <Select.Value aria-label={value} />
+          <Select.Icon>
+            <svg
+              width="12"
+              height="8"
+              viewBox="0 0 12 8"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1 1.5L6 6.5L11 1.5"
+                stroke="currentColor"
+                stroke-width="1.66667"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </Select.Icon>
+        </Input>
       </Select.Trigger>
 
       <Select.Content position="popper" sideOffset={5}>
