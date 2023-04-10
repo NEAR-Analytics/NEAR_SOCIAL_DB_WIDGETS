d898af5584f9eb1f1e28241c585fea9a743b51cf
contribut3.near <contribut3.near>
2023-04-10 06:46:23 -0400

    Update Buttons.Grey by contribut3.near at 2023-04-10 06:46:23.327

---
+++ contribut3.near/Buttons.Grey/source_code.jsx
@@ -0,0 +1,17 @@
+const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: .75em 1em;
+  gap: .5em;
+  background: #fafafa;
+  border: 1px solid #eceef0;
+  border-radius: 50px;
+  color: #006adc;
+  font-style: normal;
+  font-weight: 600;
+  font-size: .95em;
+  line-height: 1em;
+  text-align: center;
+`;
