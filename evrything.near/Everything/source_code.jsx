0cf4923762042d56f7ae1a105ed47a686f54249f
evrything.near <evrything.near>
2023-04-09 21:09:18 -0400

    Update Everything by evrything.near at 2023-04-09 21:09:18.794

--- evrything.near/Everything/source_code.jsx
+++ evrything.near/Everything/source_code.jsx
@@ -1,13 +1,41 @@
-const accountId = props.accountId || "evrything.near";
-const font = props.font || "Times New Roman";
-const type = props.type || "Everything";
-const text = props.text || type.toLowerCase();
+const accountId = props.accountId || "evrything.near"; // which account's Types to use
+const font = props.font || "Times New Roman"; // custom font for H1
+const type = props.type || "everything"; // selected type
+const text = props.text || type.toLowerCase(); // text for H1
 
 const H1 = styled.h1`
   font-family: ${font}, Times, serif;
   font-size: 4em;
   line-height: 1.25;
   font-weight: 400;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  width: 100%;
+`;
+
+const Controller = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+  margin-top: 160px;
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 4px;
+  margin: 0 4px;
+`;
+
+const Button = styled.button`
+    text-transform: lowercase !important;
 `;
 
 const types = Social.keys(`${accountId}/type/*`, "final", {
@@ -23,98 +51,68 @@ State.init({
 });
 
 const handleSelectType = (typeName) => {
-  if (typeName === "Everything") {
+  if (typeName === "everything") {
     State.update({
-      title: typeName.toLowerCase(),
-      type: typeName,
+      title: text,
+      type: "everything",
     });
   } else {
     State.update({
       title: typeName.toLowerCase() + "s",
-      type: typeName,
+      type: `${accountId}/type/${typeName}`,
     });
   }
 };
 
 return (
   <>
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        gap: 16,
-        width: "100%",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          gap: 4,
-          marginTop: 160,
-        }}
-      >
+    <Container>
+      <Controller>
         <H1>{state.title}</H1>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            gap: 4,
-            margin: "0 4px",
-          }}
-        >
-          {state.type === "Everything" ? (
+        <ButtonRow>
+          {state.type === "everything" ? (
             <>
               {types.map((it) => (
-                <button
-                  className="text-lowercase"
-                  onClick={() => handleSelectType(it[0])}
-                >
+                <Button onClick={() => handleSelectType(it[0])}>
                   {it[0] + "s"}
-                </button>
+                </Button>
               ))}
               {context.accountId === accountId ? ( // currently thinking the button should only show if you are able to create types in domain
                 <a
                   href={`/#/evrything.near/widget/Everything.Create.Type`} // this could get way more intense
                   style={{ textDecoration: "none", color: "inherit" }}
                 >
-                  <button className="text-lowercase">+</button>
+                  <Button>+</Button>
                 </a>
               ) : null}
             </>
           ) : (
             <>
-              <button
-                className="text-lowercase"
-                onClick={() => handleSelectType("Everything")}
-              >
+              <Button onClick={() => handleSelectType("everything")}>
                 back
-              </button>
+              </Button>
               <a
-                href={`/#/evrything.near/widget/Everything.Type.Details?type=${type}`}
+                href={`/#/evrything.near/widget/Everything.Type.Details?type=${accountId}/type/${state.type}`}
                 style={{ textDecoration: "none", color: "inherit" }}
               >
-                <button className="text-lowercase">view type details</button>
+                <Button>view type details</Button>
               </a>
               <a
-                href={`/#/evrything.near/widget/Everything.Type.Create?type=${type}`}
+                href={`/#/evrything.near/widget/Everything.Type.Create?type=${accountId}/type/${state.type}`}
                 style={{ textDecoration: "none", color: "inherit" }}
               >
-                <button className="text-lowercase">create new</button>
+                <Button>create new</Button>
               </a>
             </>
           )}
-        </div>
-      </div>
+        </ButtonRow>
+      </Controller>
       <Widget
         src={"evrything.near/widget/Everything.Things"}
         props={{
           type: `${accountId}/type/${state.type}`,
         }}
       />
-    </div>
+    </Container>
   </>
 );
