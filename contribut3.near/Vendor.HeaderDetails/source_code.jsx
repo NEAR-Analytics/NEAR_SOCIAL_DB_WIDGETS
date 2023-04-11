24db74dbee6386c5e074780a96bd19c84f5c2b0a
contribut3.near <contribut3.near>
2023-04-11 12:28:42 -0400

    Update Vendor.HeaderDetails by contribut3.near at 2023-04-11 12:28:42.495

--- contribut3.near/Vendor.HeaderDetails/source_code.jsx
+++ contribut3.near/Vendor.HeaderDetails/source_code.jsx
@@ -52,7 +52,7 @@ return (
             } else {
               args.data[accountId] = { profile: { name } };
             }
-            Near.call({ contractName: "social.near", methodName: "set", args, gas: "1", deposit: "1" });
+            Near.call({ contractName: "social.near", methodName: "set", args, deposit: "1000000000000000" });
           }
         }}
       />
@@ -68,7 +68,7 @@ return (
             } else {
               args.data[accountId] = { profile: { tagline } };
             }
-            Near.call({ contractName: "social.near", methodName: "set", args, gas: "1", deposit: "1" });
+            Near.call({ contractName: "social.near", methodName: "set", args, deposit: "1000000000000000" });
           }
         }}
       />
