21c4e61ca031360669765847ef8a805541c77b6c
xoaohack.near <xoaohack.near>
2023-04-10 22:19:04 -0400

    Update Everything.Create.Connection by xoaohack.near at 2023-04-10 22:19:04.900

--- xoaohack.near/Everything.Create.Connection/source_code.jsx
+++ xoaohack.near/Everything.Create.Connection/source_code.jsx
@@ -1,1 +1,85 @@
-return (<p>Go configure your <a href="/#/edit/xoaohack.near/widget/Everything.Create.Connection">create widget</a>!</p>);
+// Repository: https://github.com/near-everything/type-creator
+const externalAppUrl = "https://connection-creator.vercel.app/";
+const accountId = context.accountId;
+
+/**
+ * Initial Path (optional but recommended)
+ */
+const path = props.path;
+/**
+ * Initial view height (optional but recommended)
+ */
+const initialViewHeight = 500;
+const initialPayload = {};
+
+/**
+ * Request Handlers - Backend.
+ *
+ * - request: payload sent by External App
+ *
+ * - response: method to send the answer back to the External App
+ *
+ * - utils: Utils features like
+ *      - promisify: (caller, resolve, reject)
+ *      There's no Promisse for some features yet, So this is util for when you need to get cached data using DiscoveryAPI, e.g:
+ *      utils.promisify(() => Social.getr(`${context.accountId}/profile`), (res) => console.log(res), (err) => console.log(err))
+ *
+ * @param {{type: string, payload: {}}} request request with payload sent by External App
+ * @param {(request) => {send: () => void}} response send the answer back to the External App
+ * @param {{promisify:(caller: () => void, resolve: (data) => void, reject: (error) => void)}} utils Utils features like
+ */
+const requestHandler = (request, response, Utils) => {
+  switch (request.type) {
+    case "create-type":
+      handleCreateType(request, response);
+      break;
+  }
+};
+
+const handleCreateType = (request, response) => {
+  const { payload } = request;
+  if (payload) {
+    Social.set(
+      {
+        thing: {
+          main: JSON.stringify(payload),
+        },
+        index: {
+          xoaohack: JSON.stringify({
+            key: "main",
+            value: {
+              type: "xoaohack.near/type/Connection",
+            },
+          }),
+        },
+      },
+      {
+        force: true,
+        onCommit: () => {
+          response(request).send({ success: true });
+        },
+        onCancel: () => {
+          response(request).send({ error: "the action was canceled" });
+        },
+      }
+    );
+    return;
+  }
+  // Error
+  response(request).send({
+    error: "type must be provided",
+  });
+};
+
+return (
+  <Widget
+    src={"wendersonpires.near/widget/NearSocialBridgeCore"}
+    props={{
+      externalAppUrl,
+      path,
+      initialViewHeight,
+      initialPayload,
+      requestHandler,
+    }}
+  />
+);
