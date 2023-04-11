9882f727ccfc27483167be0f69712e9107c15c02
contribut3.near <contribut3.near>
2023-04-11 11:04:35 -0400

    Update Inputs.Viewable by contribut3.near at 2023-04-11 11:04:35.278

--- contribut3.near/Inputs.Viewable/source_code.jsx
+++ contribut3.near/Inputs.Viewable/source_code.jsx
@@ -17,7 +17,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
-  // gap: ${({ big }) => (big ? ".625em" : ".25em")};
+  gap: ${({ big }) => (big ? ".625em" : ".25em")};
   width: 100%;
 `;
 
