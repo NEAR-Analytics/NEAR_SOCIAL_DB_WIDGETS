83c680a0d65cfeac2e99d04b1b7516dd206b8e74
dorgon108.near <dorgon108.near>
2023-04-10 23:50:15 -0400

    Update SearchPopupFunctions by dorgon108.near at 2023-04-10 23:50:15.829

--- dorgon108.near/SearchPopupFunctions/source_code.jsx
+++ dorgon108.near/SearchPopupFunctions/source_code.jsx
@@ -367,25 +367,6 @@ const onSearchResultClick = ({ searchPosition, objectID, eventName }) => {
 
 return (
   <Wrapper>
-    {showHeader && (
-      <Header>
-        <H1>Search</H1>
-        <H2>Explore and find everything on the Blockchain Operating System</H2>
-      </Header>
-    )}
-
-    {showSearchBar && (
-      <Search>
-        <Widget
-          src="chaotictempest.near/widget/SearchPill"
-          props={{
-            onChange: onSearchChange,
-            term: props.term,
-          }}
-        />
-      </Search>
-    )}
-
     {state.search && (
       <Facets>
         <Widget
