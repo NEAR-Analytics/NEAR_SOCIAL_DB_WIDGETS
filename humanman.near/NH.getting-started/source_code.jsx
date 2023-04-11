546a8250a7b2a85ec47a92cb70d738ab7414368e
humanman <humanman>
2023-04-11 19:17:58 -0400

    Update NH.getting-started by humanman.near at 2023-04-11 19:17:58.913

---
+++ humanman.near/NH.getting-started/source_code.jsx
@@ -0,0 +1,62 @@
+return (
+  <div>
+    <h1>NEAR Horizon Getting Started Guide</h1>
+    <br />
+    <p>
+      Welcome to the NEAR Horizon Getting Started Guide. This guide will help
+      you get started with NEAR Horizon, a Web3 platform for connecting startups
+      to contributors, services, resources, and financing.{" "}
+    </p>
+    <br />
+    <h2>Landing Page</h2>
+    <br />
+    <p>
+      Whether you've navigated to the NEAR Horizon landing page from a shared
+      link or found it through a BOS Gateway, you'll be greeted with a
+      personalized dashboard.{" "}
+    </p>
+    <br />
+    <img
+      alt="near horizon landing page"
+      src="https://nearhorizonassets.s3.amazonaws.com/landing_basic_features_optimized.gif"
+    />
+    <br />
+    <p>
+      Here you can create a new project, manage your existing projects, update
+      your profile, check your inbox, and more. These options are available in
+      the sidebar on the left.
+    </p>
+    <br />
+    <h2>Creating a Project</h2>
+    <br />
+    <p>
+      To create a new project, click the "Create new" button on the top right of
+      the page. A drop-down will appear. Select "Project". A form will appear.
+      Fill out the form with the details of your project.
+    </p>
+    <br />
+    <img
+      alt="creating a project on near horizon"
+      src="https://nearhorizonassets.s3.amazonaws.com/create_project_optimized.gif"
+    />
+    <br />
+    <br />
+    <p>
+      When you are ready, click the "Create project" button at the bottom of the
+      form. Or you can save the form and complete later by clicking the "Save
+      for later" button (also at the bottom of the form). Once created, you will
+      be navigated to the project page.{" "}
+    </p>
+    <br />
+    <p>
+      Take a moment to review your project details which are displayed in the
+      sidebar on the right. You can edit any of these details by clicking the
+      "Edit" button next to the detail you would like to change.
+    </p>
+    <br />
+    <p>
+      That's it! You've created your first project on NEAR Horizon.
+      Congratulations!
+    </p>
+  </div>
+);
