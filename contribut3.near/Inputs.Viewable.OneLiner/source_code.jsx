f07cf704430024f4a4379dc1521571d12c26820d
contribut3.near <contribut3.near>
2023-04-11 10:47:23 -0400

    Update Inputs.Viewable.OneLiner by contribut3.near at 2023-04-11 10:47:23.735

--- contribut3.near/Inputs.Viewable.OneLiner/source_code.jsx
+++ contribut3.near/Inputs.Viewable.OneLiner/source_code.jsx
@@ -41,25 +41,109 @@ const SaveButton = styled.button`
   color: #11181c;
 `;
 
+const edit = (update, v) => (
+  <LabelArea>
+    <Input
+      id
+      type={isLink ? "url" : "text"}
+      value={v}
+      onChange={(e) => update(e.target.value)}
+    />
+    <SaveButton onClick={() => onSave(v)}>Save</SaveButton>
+  </LabelArea>
+);
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: ${({ big }) => (big ? ".625em" : ".25em")};
+  width: 100%;
+`;
+
+const Label = styled.label`
+  font-style: normal;
+  font-weight: 600;
+  font-size: ${({ big }) => (big ? "1em" : ".95em")};
+  line-height: ${({ big }) => (big ? "1.4em" : "1em")};
+  color: #11181c;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 0.25em;
+`;
+
+const EditButton = styled.button`
+  font-weight: 400;
+  font-size: 0.9em;
+  line-height: 1em;
+  color: #006adc;
+  background: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  overflow: hidden;
+  position: absolute;
+  inset: auto auto auto 0;
+
+  &.hidden {
+    transform: scaleX(0);
+  }
+
+  &.left {
+    transform-origin: left;
+  }
+
+  &.right {
+    transform-origin: right;
+  }
+`;
+
+const EditButtonContainer = styled.div`
+  position: relative;
+  width: min-content;
+  height: 1em;
+`;
+
+const noLabel = true;
+
 return (
-  <Widget
-    src={`${ownerId}/widget/Inputs.Viewable`}
-    props={{
-      id,
-      label: value,
-      value,
-      edit: (update, v) => (
-        <LabelArea>
-          <Input
-            id
-            type={isLink ? "url" : "text"}
-            value={v}
-            onChange={(e) => update(e.target.value)}
-          />
-          <SaveButton onClick={() => onSave(v)}>Save</SaveButton>
-        </LabelArea>
-      ),
-      view: <></>,
-    }}
-  />
+  <Container big={big}>
+    <Row>
+      {noLabel ? (
+        view
+      ) : (
+        <Label htmlFor={id} big={big}>
+          {label}
+        </Label>
+      )}
+      <EditButtonContainer>
+        <EditButton
+          onClick={() => State.update({ edit: false })}
+          className={`right ${state.edit ? "" : "hidden"}`}
+        >
+          Cancel
+        </EditButton>
+        <EditButton
+          onClick={() => State.update({ edit: true })}
+          className={`left ${state.edit ? "hidden" : ""}`}
+        >
+          Edit
+        </EditButton>
+      </EditButtonContainer>
+    </Row>
+
+    {state.edit ? (
+      edit((value) => State.update({ value }), state.value)
+    ) : noLabel ? (
+      <></>
+    ) : (
+      value
+    )}
+  </Container>
 );
