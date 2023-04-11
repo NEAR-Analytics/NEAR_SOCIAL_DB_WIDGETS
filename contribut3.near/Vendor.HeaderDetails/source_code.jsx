88b69de0355e00de857e1580dd8f5465c12d0d8f
contribut3.near <contribut3.near>
2023-04-11 11:56:18 -0400

    Update Vendor.HeaderDetails by contribut3.near at 2023-04-11 11:56:18.051

---
+++ contribut3.near/Vendor.HeaderDetails/source_code.jsx
@@ -0,0 +1,75 @@
+const ownerId = "contribut3.near";
+const accountId = props.accountId ?? context.accountId;
+
+State.init({
+  profile: null,
+  profileIsFetched: false,
+});
+
+if (!state.profileIsFetched) {
+  Near.asyncView(
+    "social.near",
+    "get",
+    { keys: [`${accountId}/profile/**`] },
+    "final",
+    false
+  ).then((profile) => State.update({ profile: profile[accountId].profile, profileIsFetched: true }));
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 1em;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 0.5em;
+`;
+
+return (
+  <Container>
+    <Widget
+      src={`${ownerId}/widget/Vendor.Icon`}
+      props={{ accountId, size: "8em" }}
+    />
+    <Details>
+      <Widget
+        src={`${ownerId}/widget/Inputs.Viewable.NameAndAccount`}
+        props={{
+          value: state.profile.name,
+          id: "name",
+          accountId,
+          onSave: (name) =>
+            Near.call("social.near", "set", {
+              data: { [accountId]: { profile: { name } } },
+            }),
+        }}
+      />
+      <Widget
+        src={`${ownerId}/widget/Inputs.Viewable.OneLiner`}
+        props={{
+          value: "Simple solutions for complex tasks",
+          id: "tagline",
+          onSave: (tagline) =>
+            Near.call("social.near", "set", {
+              data: { [accountId]: { profile: { tagline } } },
+            }),
+        }}
+      />
+      <Widget
+        src={`${ownerId}/widget/BadgeList`}
+        props={{
+          badges: [
+            { value: "Verified" },
+          ],
+        }}
+      />
+    </Details>
+  </Container>
+);
