19dfdc8f86bf7b34afac4fce9b416ebe7af44aff
contribut3.near <contribut3.near>
2023-04-11 13:57:41 -0400

    Update Vendor.Sidebar by contribut3.near at 2023-04-11 13:57:41.946

--- contribut3.near/Vendor.Sidebar/source_code.jsx
+++ contribut3.near/Vendor.Sidebar/source_code.jsx
@@ -1,47 +1,16 @@
 const ownerId = "contribut3.near";
 const accountId = props.accountId;
-const gas = "300000000000000";
-
-const companySizeTiers = [
-  "1-10 employees",
-  "11-50 employees",
-  "51-250 employees",
-  "251-1000 employees",
-  "1001+ employees",
-];
-
-State.init({
-  project: null,
-  projectIsFetched: false,
-});
-
-if (!state.projectIsFetched) {
-  Near.asyncView(
-    ownerId,
-    "get_vendor",
-    { account_id: accountId },
-    "final",
-    false
-  ).then((project) => State.update({ project, projectIsFetched: true }));
-  return <>Loading...</>;
-}
 
 return (
   <Widget
     src={`${ownerId}/widget/Vendor.Details`}
     props={{
-      onSave: (project) => {
-        State.update({
-          project: {
-            application: { ...state.project.application, ...project },
-          },
+      onSave: (profile) => {
+        Near.call({
+          contractName: "social.near",
+          methodName: "edit_project",
+          args: { data: { [accountId]: { profile } } },
         });
-        Near.call(
-          ownerId,
-          "edit_project",
-          { account_id: accountId, project: state.project },
-          gas,
-        );
       },
     }}
   />
