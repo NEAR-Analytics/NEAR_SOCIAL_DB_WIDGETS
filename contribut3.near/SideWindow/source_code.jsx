581a775c29a18783bb133e20fc4e9f7df58bb06d
contribut3.near <contribut3.near>
2023-04-12 10:38:52 -0400

    Update SideWindow by contribut3.near at 2023-04-12 10:38:52.819

--- contribut3.near/SideWindow/source_code.jsx
+++ contribut3.near/SideWindow/source_code.jsx
@@ -50,10 +50,30 @@ const Content = styled.div`
   overflow-y: scroll;
 `;
 
+const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0.75em 1em;
+  gap: 0.5em;
+  background: #fafafa;
+  border: 1px solid #eceef0;
+  border-radius: 50px;
+  color: #006adc;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 0.95em;
+  line-height: 1em;
+  text-align: center;
+`;
+
 return (
   <Dialog.Root>
-    <Dialog.Trigger>
-      {trigger}
+    <Dialog.Trigger asChild>
+      <Button>
+        {trigger}
+      </Button>
     </Dialog.Trigger>
 
     <Dialog.Overlay asChild>
