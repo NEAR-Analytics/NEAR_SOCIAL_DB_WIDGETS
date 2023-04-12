3bd886a50d28bceb949175daac65de8930313d8b
contribut3.near <contribut3.near>
2023-04-12 12:09:27 -0400

    Update Vendor.RequestForm by contribut3.near at 2023-04-12 12:09:27.188

--- contribut3.near/Vendor.RequestForm/source_code.jsx
+++ contribut3.near/Vendor.RequestForm/source_code.jsx
@@ -20,7 +20,8 @@ if (!state.projectsIsFetched) {
   ).then((projects) => {
     State.update({
       projects: projects.map((accountId) => ({
-        text: <Widget src={`${ownerId}/widget/Project.Line`} props={{ accountId, size: "1em" }} />,
+        // text: <Widget src={`${ownerId}/widget/Project.Line`} props={{ accountId, size: "1em" }} />,
+        text: accountId,
         value: accountId,
       })), projectsIsFetched: true
     })
