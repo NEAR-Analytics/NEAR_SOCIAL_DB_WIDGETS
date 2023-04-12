1576cd3c709accc1d29bcfde40580eb5e5dd8b66
contribut3.near <contribut3.near>
2023-04-12 10:54:15 -0400

    Update Vendor.RequestSideWindow by contribut3.near at 2023-04-12 10:54:15.350

---
+++ contribut3.near/Vendor.RequestSideWindow/source_code.jsx
@@ -0,0 +1,9 @@
+
+return <Widget
+  src={`${ownerId}/widget/SideWindow`}
+  props={{
+    title: "Request a contribution",
+    description: "Request a contribution from this vendor",
+    trigger: <>{personPlus}Request contribution</>,
+  }}
+/>
