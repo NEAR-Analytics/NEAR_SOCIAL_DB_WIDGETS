32b8b32016e24fe1110951893a8edb689def71a5
contribut3.near <contribut3.near>
2023-04-12 11:04:35 -0400

    Update Vendor.RequestSideWindow by contribut3.near at 2023-04-12 11:04:35.131

--- contribut3.near/Vendor.RequestSideWindow/source_code.jsx
+++ contribut3.near/Vendor.RequestSideWindow/source_code.jsx
@@ -1,49 +1,11 @@
 const ownerId = "contribut3.near";
 
-const body = (
-  <>
-    <Widget
-      src={`${ownerId}/widget/Inputs.Select`}
-      props={{
-        label: "Contribution to *",
-        options: [
-          {
-            value: "1",
-            text: "1",
-          },
-          {
-            value: "2",
-            text: "2",
-          },
-          {
-            value: "3",
-            text: "3",
-          },
-        ],
-        value: "1",
-        onChange: (e) => {
-          console.log(e);
-        }
-      }}
-    />
-    <Widget
-      src={`${ownerId}/widget/Inputs.TextArea`}
-      props={{
-        label: "Message",
-        placeholder: "Describe the contribution you would like to request",
-        value: state.message,
-        onChange: (message) => State.update({ message }),
-      }}
-    />
-  </>
-);
-
 return <Widget
   src={`${ownerId}/widget/SideWindow`}
   props={{
     title: "Request a contribution",
     description: "Request a contribution from this vendor",
     trigger: <>{personPlus}Request contribution</>,
-    children: body,
+    children: <Widget src={`${ownerId}/widget/Vendor.RequestForm`} />,
   }}
 />
