e64b56c63cb02900f220e4868b19dae4ae6cc189
vlodkow.near <vlodkow.near>
2023-04-12 10:15:11 -0400

    Update Test1 by vlodkow.near at 2023-04-12 10:15:11.530

--- vlodkow.near/Test1/source_code.jsx
+++ vlodkow.near/Test1/source_code.jsx
@@ -1,12 +1,21 @@
-let greeting = "and have a great day";
 let user_account = context.accountId;
+
+State.init({ greeting: "have a great day!!!" });
+const onChange = ({ target }) => {
+  State.update({ greeting: target.value });
+};
 
 return (
   <>
     <div class="container border border-info p-3 text-center min-vw-100">
-      <h1>Hello World</h1>
-      <p>{greeting}</p>
-      {user_account}
+      <h1>Hello World! {user_account}</h1>
+      <hr />
+      {user_account && (
+        <>
+          <p>{state.greeting}</p>
+          <input onChange={onChange} />
+        </>
+      )}
     </div>
   </>
 );
