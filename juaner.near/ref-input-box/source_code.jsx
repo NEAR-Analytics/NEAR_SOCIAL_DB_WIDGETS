e525ed05274e0058e58e523b3ee60506b570e11c
juaner.near <juaner.near>
2023-04-12 13:41:09 -0400

    Update ref-input-box by juaner.near at 2023-04-12 13:41:09.836

---
+++ juaner.near/ref-input-box/source_code.jsx
@@ -0,0 +1,55 @@
+const Container = styled.div`
+    input{
+      background: #152528;
+      border-radius: 12px;
+      height: 55px;
+      font-size:20px;
+      color: #7E8A93;
+      padding:0 15px 0 15px;
+      border:none;
+    }
+    input:focus{
+      outline:none;
+      background: #152528; 
+      color: #7E8A93;
+    }
+ 
+    .balance {
+      font-size:12px;
+      color:#4B6778;
+      margin-left:6px;
+      cursor:pointer;
+      text-decoration:underline;
+    }
+
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+    -webkit-appearance: none !important;
+    }
+`;
+const { handleAmount, balance, balance$ } = props;
+const { amount } = state;
+State.init({
+  amount: 0,
+});
+function changeAmount(e) {
+  const value = Number(e.target.value);
+  console.log("88888888888-amount", value);
+  if (Big(value || 0).gt(balance || 0)) return;
+  console.log("校验过了哦");
+  State.update({
+    amount: value,
+  });
+  handleAmount(value);
+}
+function changeToMax() {
+  handleAmount(balance || 0);
+}
+return (
+  <Container>
+    <input type="number" value={amount} onChange={changeAmount} />
+    <span class="balance" onClick={changeToMax}>
+      Balance: {balance || "0"}(${balance$ || "0"})
+    </span>
+  </Container>
+);
