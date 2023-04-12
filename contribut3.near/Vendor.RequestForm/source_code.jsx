1c62dc67488a429cd896601831fc5df9c61ab847
contribut3.near <contribut3.near>
2023-04-12 11:37:15 -0400

    Update Vendor.RequestForm by contribut3.near at 2023-04-12 11:37:15.446

--- contribut3.near/Vendor.RequestForm/source_code.jsx
+++ contribut3.near/Vendor.RequestForm/source_code.jsx
@@ -1,32 +1,30 @@
 const ownerId = "contribut3.near";
 
 State.init({
+  requestId: [],
   message: "",
-})
+  requests: [],
+  requestsIsFetched: false,
+});
+
+if (!state.requestsIsFetched) {
+  Near.asyncView(
+    ownerId,
+    "get_admin_requests",
+    { account_id: context.accountId },
+    "final",
+    false,
+  ).then((requests) => State.update({ requests, requestsIsFetched: true }));
+}
 
 return <>
   <Widget
     src={`${ownerId}/widget/Inputs.Select`}
     props={{
       label: "Contribution to *",
-      options: [
-        {
-          value: "1",
-          text: "1",
-        },
-        {
-          value: "2",
-          text: "2",
-        },
-        {
-          value: "3",
-          text: "3",
-        },
-      ],
-      value: "1",
-      onChange: (e) => {
-        console.log(e);
-      }
+      options: requests,
+      value: state.requestId,
+      onChange: (requestId) => State.update({ requestId }),
     }}
   />
   <Widget
