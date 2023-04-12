bee558091399e30d999bc7139bd9897bf9b54f9b
contribut3.near <contribut3.near>
2023-04-12 10:55:42 -0400

    Update Vendor.RequestSideWindow by contribut3.near at 2023-04-12 10:55:42.114

--- contribut3.near/Vendor.RequestSideWindow/source_code.jsx
+++ contribut3.near/Vendor.RequestSideWindow/source_code.jsx
@@ -1,3 +1,30 @@
+const ownerId = "contribut3.near";
+
+const body = (
+  <>
+    <Widget
+      src={`${ownerId}/widget/Inputs.Select`}
+      props={{
+        label: "Contribution to *",
+        options: [
+          {
+            value: "1",
+            text: "1",
+          },
+          {
+            value: "2",
+            text: "2",
+          },
+          {
+            value: "3",
+            text: "3",
+          },
+        ],
+        value: "1",
+      }}
+    />
+  </>
+);
 
 return <Widget
   src={`${ownerId}/widget/SideWindow`}
