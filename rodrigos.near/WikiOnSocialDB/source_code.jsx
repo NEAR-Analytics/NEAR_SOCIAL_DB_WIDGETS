f1742524bf94df69038fce105ffd20f35cbda978
rodrigos.near <rodrigos.near>
2023-04-12 15:42:32 -0400

    Update WikiOnSocialDB by rodrigos.near at 2023-04-12 15:42:32.613

---
+++ rodrigos.near/WikiOnSocialDB/source_code.jsx
@@ -0,0 +1,26 @@
+const addressForArticles = "wikiTest";
+const writersWhiteList = ["testwiki.near", "eugenewolf507.near"];
+const authorForWidget = "testwiki.near";
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+if (profile === null) {
+  return "Loading";
+}
+
+return (
+  <>
+    <Widget
+      src={`${authorForWidget}/widget/WikiOnSocialDB_MainNavigation`}
+      props={{ currentNavPill: "articles", writersWhiteList }}
+    />
+    <div>
+      <Widget
+        src={`${authorForWidget}/widget/WikiOnSocialDB_AllArticlesList`}
+        props={{ writersWhiteList }}
+      />
+    </div>
+  </>
+);
