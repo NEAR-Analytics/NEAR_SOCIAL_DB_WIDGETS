9364242de0e478767acc4809364c8b98de2dc0d3
contribut3.near <contribut3.near>
2023-04-12 11:44:25 -0400

    Update Vendor.RequestForm by contribut3.near at 2023-04-12 11:44:25.745

--- contribut3.near/Vendor.RequestForm/source_code.jsx
+++ contribut3.near/Vendor.RequestForm/source_code.jsx
@@ -3,9 +3,28 @@ const ownerId = "contribut3.near";
 State.init({
   requestId: [],
   message: "",
+  projectId: [],
+  projects: [],
+  projectsIsFetched: false,
   requests: [],
   requestsIsFetched: false,
 });
+
+if (!state.projectIsFetched) {
+  Near.asyncView(
+    ownerId,
+    "get_admin_projects",
+    { account_id: context.accountId },
+    "final",
+    false,
+  ).then((projects) => State.update({
+    projects: projects.map((accountId) => ({
+      name: <Widget src={`${ownerId}/widget/Project.Line`} props={{ accountId, size: "1em" }} />,
+      value: accountId,
+    })), projectsIsFetched: true
+  }));
+  return <>Loading...</>;
+}
 
 if (!state.requestsIsFetched) {
   Near.asyncView(
@@ -26,23 +45,27 @@ const Form = styled.div`
   gap: 1em;
 `;
 
-return (<Form>
-  <Widget
-    src={`${ownerId}/widget/Inputs.Select`}
-    props={{
-      label: "Contribution to *",
-      options: requests,
-      value: state.requestId,
-      onChange: (requestId) => State.update({ requestId }),
-    }}
-  />
-  <Widget
-    src={`${ownerId}/widget/Inputs.TextArea`}
-    props={{
-      label: "Message",
-      placeholder: "Describe the contribution you would like to request",
-      value: state.message,
-      onChange: (message) => State.update({ message }),
-    }}
-  />
-</Form>);
+const
+
+return (
+  <Form>
+    <Widget
+      src={`${ownerId}/widget/Inputs.Select`}
+      props={{
+        label: "Contribution to *",
+        options: requests,
+        value: state.requestId,
+        onChange: (requestId) => State.update({ requestId }),
+      }}
+    />
+    <Widget
+      src={`${ownerId}/widget/Inputs.TextArea`}
+      props={{
+        label: "Message",
+        placeholder: "Describe the contribution you would like to request",
+        value: state.message,
+        onChange: (message) => State.update({ message }),
+      }}
+    />
+  </Form>
+);
