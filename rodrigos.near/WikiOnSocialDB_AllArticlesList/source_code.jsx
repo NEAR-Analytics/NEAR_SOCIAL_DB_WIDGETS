436ef82e08ac2b00b782a13504ca0dcdfd38e0c8
rodrigos.near <rodrigos.near>
2023-04-12 15:42:46 -0400

    Update WikiOnSocialDB_AllArticlesList by rodrigos.near at 2023-04-12 15:42:46.624

---
+++ rodrigos.near/WikiOnSocialDB_AllArticlesList/source_code.jsx
@@ -0,0 +1,60 @@
+const addressForArticles = "wikiTest2Article";
+const writersWhiteList = props.writersWhiteList ?? "";
+const authorForWidget = "testwiki.near";
+// ========== GET INDEX ARRAY FOR ARTICLES ==========
+const postsIndex = Social.index(addressForArticles, "main", {
+  order: "desc",
+  accountId: undefined,
+});
+// ========== GET ALL ARTICLES ==========
+const resultArticles =
+  postsIndex &&
+  postsIndex
+    .reduce((acc, { accountId, blockHeight }) => {
+      const postData = Social.get(
+        `${accountId}/${addressForArticles}/main`,
+        blockHeight
+      );
+      const postDataWithBlockHeight = { ...JSON.parse(postData), blockHeight };
+      return [...acc, postDataWithBlockHeight];
+    }, [])
+    .filter((article) =>
+      writersWhiteList.some((addr) => addr === article.author)
+    );
+
+// ========== FILTER DUPLICATES ==========
+const filteredArticles =
+  resultArticles.length &&
+  resultArticles.reduce((acc, article) => {
+    if (!acc.some(({ articleId }) => articleId === article.articleId)) {
+      return [...acc, article];
+    } else {
+      return acc;
+    }
+  }, []);
+
+const getDateLastEdit = (timestamp) => {
+  const date = new Date(Number(timestamp));
+  const dateString = `${date.toLocaleDateString()} / ${date.toLocaleTimeString()}`;
+  return dateString;
+};
+
+return (
+  <ol>
+    {filteredArticles &&
+      filteredArticles.map((article) => (
+        <li key={article.articleId}>
+          <a
+            href={`#/${authorForWidget}/widget/WikiOnSocialDB_OneArticle?articleId=${article.articleId}&blockHeight=${article.blockHeight}&lastEditor=${article.lastEditor}
+            `}
+          >
+            {article.articleId}{" "}
+            <small>
+              (author: {article.author}
+              {getDateLastEdit(article.timeLastEdit)})
+            </small>
+          </a>
+        </li>
+      ))}
+  </ol>
+);
