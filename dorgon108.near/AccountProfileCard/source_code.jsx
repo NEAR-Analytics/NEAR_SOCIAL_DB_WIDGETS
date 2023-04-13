6e6edc005400f7355e532e16f0ee9e992fd4e79a
dorgon108.near <dorgon108.near>
2023-04-13 18:23:36 -0400

    Update AccountProfileCard by dorgon108.near at 2023-04-13 18:23:36.407

--- dorgon108.near/AccountProfileCard/source_code.jsx
+++ dorgon108.near/AccountProfileCard/source_code.jsx
@@ -1,6 +1,10 @@
-const accountId = props.accountId;
+// const accountId = props.accountId;
+const accountId = props.accountId ?? "dorgon108.near";
+
 const profile = props.profile || Social.get(`${accountId}/profile/**`, "final");
-const tags = Object.keys(profile.tags || {});
+// const tags = Object.keys(profile.tags || {});
+const tags = Object.keys(profile.tags || { users: "dog", animals: "zoo" });
+
 console.log(tags);
 const profileUrl = `/#/calebjacob.near/widget/ProfilePage?accountId=${accountId}`;
 const onPointerUp =
@@ -16,18 +20,16 @@ State.init({
 });
 
 const Card = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 16px;
-  width: 100%;
-  border-radius: 12px;
-  z-index: 1070;
-  background: #fff;
-  border: 1px solid #ECEEF0;
-  box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06);
-  overflow: hidden;
-  padding: 16px;
+
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: flex-start;
+padding: 0px;
+gap: 10px;
+
+width: 445.85px;
+height: 24.1px;
 `;
 
 const CardLeft = styled.div`
@@ -39,25 +41,28 @@ const CardLeft = styled.div`
 
   > div {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     width: 100%;
     min-width: 0;
+    
   }
 `;
 
 const Avatar = styled.a`
-  width: 60px;
-  height: 60px;
+  width: 50px;
+  height: 50px;
   flex-shrink: 0;
   border: 1px solid #ECEEF0;
   overflow: hidden;
   border-radius: 56px;
   transition: border-color 200ms;
 
+
   img {
     object-fit: cover;
-    width: 100%;
-    height: 100%;
+ width: 24px;
+height: 24px;
+    border-radius: 50%;
   }
 
   &:hover,
@@ -67,13 +72,18 @@ const Avatar = styled.a`
 `;
 
 const TextLink = styled.a`
+
+width: 107px;
+height: 22px;
   display: block;
   margin: 0;
+  margin-right:42.22;
   font-size: 14px;
   line-height: 18px;
-  color: ${(p) => (p.bold ? "#11181C !important" : "#687076 !important")};
+  color: ${(p) => (p.bold ? "#FFFFFF !important" : "#606D7A !important")};
   font-weight: ${(p) => (p.bold ? "600" : "400")};
   font-size: ${(p) => (p.small ? "12px" : "14px")};
+  font-style:normal;
   overflow: ${(p) => (p.ellipsis ? "hidden" : "visible")};
   text-overflow: ${(p) => (p.ellipsis ? "ellipsis" : "unset")};
   white-space: nowrap;
@@ -85,6 +95,43 @@ const TextLink = styled.a`
   }
 `;
 
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 -10px;
+  align-items: center;
+`;
+
+const Col = styled.div`
+  flex: 1;
+  padding: 0 10px;
+`;
+
+const col1 = {
+  width: "66.32px",
+};
+
+const col2 = {
+  width: "107px",
+  marginRight: 42.22,
+};
+
+const col3 = {
+  width: "172px",
+};
+
+const col4 = {
+  right: 0,
+
+  padding: 0,
+  textAlign: "right",
+};
+
 const TagsWrapper = styled.div`
   padding-top: 4px;
 `;
@@ -92,42 +139,51 @@ const TagsWrapper = styled.div`
 return (
   <Card>
     <CardLeft>
-      <Avatar href={profileUrl} onPointerUp={onPointerUp}>
-        <Widget
-          src="mob.near/widget/Image"
-          props={{
-            image: profile.image,
-            alt: profile.name,
-            fallbackUrl:
-              "https://ipfs.near.social/ipfs/bafkreibiyqabm3kl24gcb2oegb7pmwdi6wwrpui62iwb44l7uomnn3lhbi",
-          }}
-        />
-      </Avatar>
-
-      <div>
-        <TextLink href={profileUrl} onPointerUp={onPointerUp} ellipsis bold>
-          {profile.name || accountId.split(".near")[0]}
-        </TextLink>
-        <TextLink href={profileUrl} onPointerUp={onPointerUp} ellipsis>
-          @{accountId}
-        </TextLink>
-
-        {tags.length > 0 && (
-          <TagsWrapper>
+      <Row>
+        <Col style={col1}>
+          <Avatar href={profileUrl} onPointerUp={onPointerUp}>
             <Widget
-              src="calebjacob.near/widget/Tags"
-              props={{ tags, scroll: true }}
+              src="mob.near/widget/Image"
+              props={{
+                image: profile.image,
+                alt: profile.name,
+                fallbackUrl:
+                  "https://ipfs.near.social/ipfs/bafkreibiyqabm3kl24gcb2oegb7pmwdi6wwrpui62iwb44l7uomnn3lhbi",
+              }}
             />
-          </TagsWrapper>
-        )}
-      </div>
-    </CardLeft>
+          </Avatar>
+        </Col>
 
-    {!!context.accountId && context.accountId !== props.accountId && (
-      <Widget
-        src="calebjacob.near/widget/FollowButton"
-        props={{ accountId: props.accountId }}
-      />
-    )}
+        <Col style={col2}>
+          <TextLink href={profileUrl} onPointerUp={onPointerUp} ellipsis bold>
+            {profile.name || accountId.split(".near")[0]}
+          </TextLink>
+        </Col>
+        <Col style={col3}>
+          <TextLink href={profileUrl} onPointerUp={onPointerUp} ellipsis>
+            @{accountId}
+          </TextLink>
+        </Col>
+
+        <Col style={col4}>
+          {!!context.accountId && context.accountId !== props.accountId && (
+            <button
+              style={{
+                cursor: "pointer",
+                backgroundColor: "rgba(255, 193, 7, 0)",
+                border: "none",
+                margin: 0,
+              }}
+            >
+              <img
+                src="https://i.imgur.com/hOp3Ivj.png"
+                alt="Follow icon"
+                style={{ height: "20px", marginRight: "5px" }}
+              />
+            </button>
+          )}
+        </Col>
+      </Row>
+    </CardLeft>
   </Card>
 );
