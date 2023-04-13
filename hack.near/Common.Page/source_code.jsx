c15e21b40d57141b204342ce26ff6406e0048f2b
NearSocial <NearSocial>
2023-04-13 16:33:54 -0400

    Update Common.Page by hack.near at 2023-04-13 16:33:54.028

--- hack.near/Common.Page/source_code.jsx
+++ hack.near/Common.Page/source_code.jsx
@@ -42,7 +42,7 @@ return (
         role="tabpanel"
         aria-labelledby="pills-feed-tab"
       >
-        <Widget src="mob.near/widget/MainPage.Content" props={props} />
+        <Widget src="hack.near/widget/Custom.Page.Content" props={props} />
       </div>
       <div
         className="tab-pane d-lg-block col-lg-4"
