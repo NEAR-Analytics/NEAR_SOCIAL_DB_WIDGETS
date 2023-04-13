c6f2a8969ddfb2c39c67bc1baffd5fcccd8fc37c
contribut3.near <contribut3.near>
2023-04-13 10:45:56 -0400

    Update Project.Page by contribut3.near at 2023-04-13 10:45:56.509

--- contribut3.near/Project.Page/source_code.jsx
+++ contribut3.near/Project.Page/source_code.jsx
@@ -234,6 +234,7 @@ return (
             content: getContent(props.content),
             search: props.search,
             update: props.update,
+            accountId: props.accountId,
             buttons: [
               {
                 id: "overview",
