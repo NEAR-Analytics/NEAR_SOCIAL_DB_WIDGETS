285d73f38c9315cc95e65996a56e6cbc2871d4ac
ailisp <ailisp>
2023-04-13 11:23:49 -0400

    Update gigs-board.components.layout.SearchByAuthor by bo.near at 2023-04-13 11:23:49.044

---
+++ bo.near/gigs-board.components.layout.SearchByAuthor/source_code.jsx
@@ -0,0 +1,74 @@
+/* INCLUDE: "common.jsx" */
+const nearDevGovGigsContractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
+    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
+  };
+  return (
+    <Widget
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+  if (props.nearDevGovGigsContractAccountId) {
+    linkProps.nearDevGovGigsContractAccountId =
+      props.nearDevGovGigsContractAccountId;
+  }
+  if (props.nearDevGovGigsWidgetsAccountId) {
+    linkProps.nearDevGovGigsWidgetsAccountId =
+      props.nearDevGovGigsWidgetsAccountId;
+  }
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+  const linkPropsQuery = Object.entries(linkProps)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+  return `#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+/* END_INCLUDE: "common.jsx" */
+
+const onSearchAuthor = props.onSearchAuthor;
+const selectedAuthors = props.searchQuery?.author
+  ? [{ name: props.searchQuery.author }]
+  : [];
+
+const authors = Near.view(nearDevGovGigsContractAccountId, "get_all_authors");
+if (!authors) {
+  return <div>Loading ...</div>;
+}
+const wrappedAuthors = authors.map((author) => ({ name: author }));
+
+const onChangeAuthor = (selectedAuthors) => {
+  onSearchAuthor(selectedAuthors[0]?.name);
+};
+
+return (
+  <>
+    <Typeahead
+      clearButton
+      id="basic-typeahead-single"
+      labelKey="name"
+      onChange={onChangeAuthor}
+      options={wrappedAuthors}
+      placeholder="Search by author"
+      selected={selectedAuthors}
+    />
+  </>
+);
