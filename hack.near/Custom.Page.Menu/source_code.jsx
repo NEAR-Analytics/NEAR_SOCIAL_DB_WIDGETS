f7564b7d1d041b782bfed8f6988661f42437d671
NearSocial <NearSocial>
2023-04-13 18:12:08 -0400

    Update Custom.Page.Menu by hack.near at 2023-04-13 18:12:08.703

--- hack.near/Custom.Page.Menu/source_code.jsx
+++ hack.near/Custom.Page.Menu/source_code.jsx
@@ -8,7 +8,10 @@ if (side === null) {
 
 const defaultWidgets = [
   {
-    src: "hack.near/widget/ABC.Page.Builders",
+    src: "mob.near/widget/Applications",
+  },
+  {
+    src: "mob.near/widget/People",
   },
 ];
 
