c363ffcd60e3142b5f6fcf6f356654a55c06e6c4
dima_sheleg.near <dima_sheleg.near>
2023-04-13 20:22:49 -0400

    Update DevSupport.Answer.Edit by dima_sheleg.near at 2023-04-13 20:22:49.519

---
+++ dima_sheleg.near/DevSupport.Answer.Edit/source_code.jsx
@@ -0,0 +1,83 @@
+if (!context.accountId) {
+  return "";
+}
+
+const item = props.item;
+
+const composeData = () => {
+  const data = {
+    question: {
+      answer: JSON.stringify(Object.assign({ item }, state.content)),
+    },
+    index: {
+      answer: JSON.stringify({
+        key: item,
+        value: {
+          type: "md",
+        },
+      }),
+    },
+  };
+
+  const notifications = state.extractTagNotifications(state.content.text, {
+    type: "social",
+    path: `${context.accountId}/question/answer`,
+  });
+
+  if (props.notifyAccountId) {
+    notifications.push({
+      key: props.notifyAccountId,
+      value: {
+        type: "answer",
+        item,
+      },
+    });
+  }
+
+  if (notifications.length) {
+    data.index.notify = JSON.stringify(
+      notifications.length > 1 ? notifications : notifications[0]
+    );
+  }
+
+  return data;
+};
+
+State.init({
+  onChange: ({ content }) => {
+    State.update({ content });
+  },
+});
+
+return (
+  <>
+    <Widget
+      src="dima_sheleg.near/widget/DevSupport.Compose"
+      props={{
+        placeholder: `Reply${
+          props.notifyAccountId ? ` to ${props.notifyAccountId}` : ""
+        }`,
+        initialText: props.initialText,
+        onChange: state.onChange,
+        onHelper: ({ extractTagNotifications }) => {
+          State.update({ extractTagNotifications });
+        },
+        withProfileImage: context.accountId,
+        composeButton: (onCompose) => (
+          <CommitButton
+            disabled={!state.content}
+            force
+            className="commit-post-button"
+            data={composeData}
+            onCommit={() => {
+              onCompose();
+              props.onComment && props.onComment(state.content);
+            }}
+          >
+            Reply
+          </CommitButton>
+        ),
+      }}
+    />
+  </>
+);
