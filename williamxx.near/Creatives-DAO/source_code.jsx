22a104a3adcf5ef3ee3bbb8735f149016714e0fc
williamxx.near <williamxx.near>
2023-04-14 06:56:22 -0400

    Update Creatives-DAO by williamxx.near at 2023-04-14 06:56:22.891

--- williamxx.near/Creatives-DAO/source_code.jsx
+++ williamxx.near/Creatives-DAO/source_code.jsx
@@ -2,44 +2,25 @@ const accountId = props.accountId ?? context.accountId;
 const daoId = props.daoId ?? "creativesdao.sputnik-dao.near";
 
 if (!accountId) {
-  return "Please connect your NEAR wallet :)";
+  return (
+    <div>
+      <p>Please connect your NEAR wallet or create a new one:</p>
+      <a href="https://shard.dog/go" target="_blank" rel="noreferrer">
+        <button>Create NEAR Wallet</button>
+      </a>
+    </div>
+  );
 }
 
 return (
   <div>
     <div className="mb-3">
-      <h2>DAO</h2>
+      <h2>WELCOME TO CREATIVES DAO</h2>
       <Widget src="hack.near/widget/DAO" props={{ daoId }} />
     </div>
     <div className="mb-3">
-      <h2>Proposals</h2>
-      <h3>Membership</h3>
-      <Widget
-        src="hack.near/widget/AddMemberToRole"
-        props={{ accountId, daoId }}
-      />
-      <Widget
-        src="hack.near/widget/RemoveMemberFromRole"
-        props={{ accountId, daoId }}
-      />
-    </div>
-    <div className="mb-3">
-      <h3>Transfer NEAR</h3>
-      <Widget
-        src="hack.near/widget/TransferProposal"
-        props={{ accountId, daoId }}
-      />
-    </div>
-    <div className="mb-3">
-      <h3>Polling</h3>
+      <h5>Congratulations on getting your proposals accepted</h5>
       <Widget src="hack.near/widget/CreatePoll" props={{ accountId, daoId }} />
-    </div>
-    <div className="mb-3">
-      <h3>Function Calls</h3>
-      <Widget
-        src="hack.near/widget/FunctionCallProposal"
-        props={{ accountId, daoId }}
-      />
     </div>
   </div>
 );
