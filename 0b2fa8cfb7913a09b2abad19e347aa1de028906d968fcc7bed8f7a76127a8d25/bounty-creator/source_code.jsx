7122b2431f1c3ae7fe009d0a9cd52e142f444c38
0b2fa8cfb7913a09b2abad19e347aa1de028906d968fcc7bed8f7a76127a8d25 <0b2fa8cfb7913a09b2abad19e347aa1de028906d968fcc7bed8f7a76127a8d25>
2023-04-14 14:41:24 -0400

    Update bounty-creator by 0b2fa8cfb7913a09b2abad19e347aa1de028906d968fcc7bed8f7a76127a8d25 at 2023-04-14 14:41:24.604

--- 0b2fa8cfb7913a09b2abad19e347aa1de028906d968fcc7bed8f7a76127a8d25/bounty-creator/source_code.jsx
+++ 0b2fa8cfb7913a09b2abad19e347aa1de028906d968fcc7bed8f7a76127a8d25/bounty-creator/source_code.jsx
@@ -18,8 +18,78 @@ if (state.balance === undefined && state.sender) {
 
 // return <p>Account: {sender}</p>;
 
-// FETCH CSS
+//CONTRACT INTERACTIONS
 
+//fetch the ABI
+const bountyContractAbi = fetch(
+  "https://gist.githubusercontent.com/birchmd/f52cc8244be64eca036d9156486307d4/raw/66da5745b22c140bdf4eff848ab4e3aa5f0b666e/BountyProgram.abi.json"
+);
+if (!bountyContractAbi.ok) {
+  return "scam";
+}
+
+//testnet
+const bountyContractAddress = "0xb405a96238ca46E9d3268271F87dbBf90E4903Bf";
+const bountyObject = new ethers.Contract(
+  bountyContractAddress,
+  bountyContractAbi.body,
+  Ethers.provider().getSigner()
+);
+
+initState({
+  prURL: "https://github.com/aurora-is-near/aurora-engine/pull/739",
+  rewardAsset: "0x7ACAf6167a39BE1dfFBb542Ac848030dcDF141CF", //dummy USDC
+  rewardAmount: 1000,
+  lockPeriod: 5000000,
+  debuglogs: "",
+});
+
+const createBounty = () => {
+  // State.update({ debuglogs: "starting" });
+
+  const output = bountyObject.createBounty(
+    state.prURL,
+    state.rewardAsset,
+    state.rewardAmount,
+    state.lockPeriod
+  );
+
+  State.update({ debuglogs: JSON.stringify(output) });
+};
+
+//initiate functions with etherjs ? only needed if we do queries
+const iface = new ethers.utils.Interface(bountyContractAbi.body);
+
+initState({
+  token: "",
+  tokenDecimals: "",
+  sendTo: "",
+  sender,
+  senderBalance: "0",
+  receiverBalance: "0",
+  receiver: "",
+  amount: "1",
+});
+
+const tokens = {
+  "Select Token": "",
+  USDT: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+  DAI: "0x6b175474e89094c44da98b954eedeac495271d0f",
+  USDC: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  MKR: "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
+};
+
+//functions for UI
+const tokensMenuItems = Object.keys(tokens).map((token) => (
+  <option value={tokens[token]}>{token}</option>
+));
+
+const setToken = (token) => {
+  State.update({ token });
+  getTokenDecimals();
+};
+
+// FETCH CSS
 const cssFont = fetch(
   "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800"
 ).body;
@@ -78,13 +148,20 @@ return (
           placeholder="0.0"
           required
         />
+        <label for="selectToken">Select token</label>
+        <select
+          class="form-select"
+          id="selectToken"
+          onChange={(e) => {
+            setToken(e.target.value);
+          }}
+        >
+          {tokensMenuItems}
+        </select>
         <br />
 
         {!!state.sender ? (
-          <button
-            class="LidoStakeFormSubmitContainer"
-            onClick={() => submitEthers(state.strEther, state.sender)}
-          >
+          <button class="LidoStakeFormSubmitContainer" onClick={createBounty}>
             <span>Submit</span>
           </button>
         ) : (
@@ -94,6 +171,8 @@ return (
           />
         )}
       </div>
+
+      <div> {state.debuglogs ?? "the logs will appear here"} </div>
     </div>
   </Theme>
 );
