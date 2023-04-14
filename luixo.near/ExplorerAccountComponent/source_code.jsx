5d0df1eaa8c4d64898ee0e196b4fa16870dcb57a
luixo.near <luixo.near>
2023-04-13 22:47:06 -0400

    Update ExplorerAccountComponent by luixo.near at 2023-04-13 22:47:06.012

--- luixo.near/ExplorerAccountComponent/source_code.jsx
+++ luixo.near/ExplorerAccountComponent/source_code.jsx
@@ -2,18 +2,13 @@ const accountId = props.accountId;
 if (!accountId) {
   return "No account ID";
 }
-const params = { language: props.language, embedded: true };
-const query = Object.entries(params)
-  .filter(([_key, value]) => value !== undefined)
-  .map(([key, value]) => `${key}=${value}`)
-  .join("&");
 const isBeta = props.beta === undefined ? true : props.beta;
 return (
   <Widget
     src="luixo.near/widget/ExplorerIframeComponent"
     props={{
-      url: `${isBeta ? "beta/" : ""}accounts/${accountId}?${query}`,
-      style: { height: props.height },
+      url: `${isBeta ? "beta/" : ""}accounts/${accountId}`,
+      query: { language: props.language, embedded: true },
       network: props.network,
     }}
   />
