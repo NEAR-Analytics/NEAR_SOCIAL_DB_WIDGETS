514d536c848c4e46fab7c3beac5aeb8d52124e84
akshatg6.near <akshatg6.near>
2023-04-15 04:59:40 -0400

    Update Document by akshatg6.near at 2023-04-15 04:59:40.570

---
+++ akshatg6.near/Document/source_code.jsx
@@ -0,0 +1,9 @@
+{
+  "evrything-docs.near": {
+    "type": {
+      "Document": {
+        "": "{\"properties\":[{\"name\":\"title\",\"type\":\"string\"},{\"name\":\"body\",\"type\":\"markdown\"},{\"name\":\"author\",\"type\":\"string\"}],\"widgets\":{\"summary\":\"evrything-docs.near/widget/Everything.Summary.Document\",\"view\":\"evrything-docs.near/widget/Everything.View.Document\",\"create\":\"evrything-docs.near/widget/Everything.Create.Document\"}}"
+      }
+    }
+  }
+}
