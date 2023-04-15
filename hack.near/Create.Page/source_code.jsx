0b3e37418ad6bb13e8d6469b6338561379dcb776
NearSocial <NearSocial>
2023-04-14 22:52:43 -0400

    Update Create.Page by hack.near at 2023-04-14 22:52:43.901

--- hack.near/Create.Page/source_code.jsx
+++ hack.near/Create.Page/source_code.jsx
@@ -11,7 +11,7 @@ const createThing = () => {
     {
       widget: {
         [`${state.hashtag}.Page`]: {
-          "": `const hashtag = props.hashtag; return (<Widget src="${template}" props={{hashtag: props.hashtag}} />);`,
+          "": `const hashtag = "${state.hashtag}"; return (<Widget src="${template}" props={{hashtag: props.hashtag}} />);`,
           metadata: {
             tags: {
               page: "",
