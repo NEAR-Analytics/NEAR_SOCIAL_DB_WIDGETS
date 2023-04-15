e362df430e4ff90f64022c6d7da9d4a6d0a8d124
NearSocial <NearSocial>
2023-04-14 22:57:52 -0400

    Update aurora.Page by hack.near at 2023-04-14 22:57:52.975

--- hack.near/aurora.Page/source_code.jsx
+++ hack.near/aurora.Page/source_code.jsx
@@ -1,1 +1,2 @@
-const hashtag = props.hashtag ?? "aurora"; return (<Widget src="hack.near/widget/Test.Page" props={{hashtag: props.hashtag}} />);
+const hashtag = props.hashtag ?? "aurora";
+return <Widget src="hack.near/widget/Test.Page" props={{ hashtag: hashtag }} />;
