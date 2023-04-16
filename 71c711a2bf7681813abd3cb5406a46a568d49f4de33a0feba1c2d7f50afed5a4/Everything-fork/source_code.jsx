b79f982283ddd33db2caae7526ad31c248cae0e1
71c711a2bf7681813abd3cb5406a46a568d49f4de33a0feba1c2d7f50afed5a4 <71c711a2bf7681813abd3cb5406a46a568d49f4de33a0feba1c2d7f50afed5a4>
2023-04-15 23:12:55 -0400

    Update Everything-fork by 71c711a2bf7681813abd3cb5406a46a568d49f4de33a0feba1c2d7f50afed5a4 at 2023-04-15 23:12:55.751

--- 71c711a2bf7681813abd3cb5406a46a568d49f4de33a0feba1c2d7f50afed5a4/Everything-fork/source_code.jsx
+++ 71c711a2bf7681813abd3cb5406a46a568d49f4de33a0feba1c2d7f50afed5a4/Everything-fork/source_code.jsx
@@ -6,6 +6,7 @@ return (
         "71c711a2bf7681813abd3cb5406a46a568d49f4de33a0feba1c2d7f50afed5a4", // which account's Types to use (your near account)
       font: "Times New Roman", // select a web safe font
       text: "dhawal", // main title
+      type: "everything",
       domain: "dhawal-docs", // domain data should be saved to
     }}
   />
