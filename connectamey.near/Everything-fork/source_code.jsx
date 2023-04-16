cfff96fc7b120e225faefa4b071f5957f3fab529
connectamey <connectamey>
2023-04-15 20:28:27 -0400

    Update Everything-fork by connectamey.near at 2023-04-15 20:28:27.380

--- connectamey.near/Everything-fork/source_code.jsx
+++ connectamey.near/Everything-fork/source_code.jsx
@@ -12,11 +12,10 @@ return (
     <Widget
       src="evrything.near/widget/Everything.Template"
       props={{
-        accountId: "connectamey.near",
-        font: "Times New Roman",
-        type: "everything",
-        text: "everything",
-        domain: "lionhack23",
+        accountId: "connectamey.near", // which account's Types to use (your near account)
+        font: "Times New Roman", // select a web safe font
+        text: "documentation", // main title
+        domain: "lionhack23", // domain data should be saved to
       }}
     />
   </div>
