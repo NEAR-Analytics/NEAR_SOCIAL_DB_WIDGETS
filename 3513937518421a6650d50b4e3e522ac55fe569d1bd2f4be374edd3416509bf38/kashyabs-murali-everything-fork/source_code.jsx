7ebf4408005fb6167261c7600ee8396564801ffe
3513937518421a6650d50b4e3e522ac55fe569d1bd2f4be374edd3416509bf38 <3513937518421a6650d50b4e3e522ac55fe569d1bd2f4be374edd3416509bf38>
2023-04-16 00:18:07 -0400

    Update kashyabs-murali-everything-fork by 3513937518421a6650d50b4e3e522ac55fe569d1bd2f4be374edd3416509bf38 at 2023-04-16 00:18:07.582

--- 3513937518421a6650d50b4e3e522ac55fe569d1bd2f4be374edd3416509bf38/kashyabs-murali-everything-fork/source_code.jsx
+++ 3513937518421a6650d50b4e3e522ac55fe569d1bd2f4be374edd3416509bf38/kashyabs-murali-everything-fork/source_code.jsx
@@ -12,7 +12,8 @@ return (
     <Widget
       src="evrything.near/widget/Everything.Template"
       props={{
-        accountId: "evrything.near",
+        accountId:
+          "3513937518421a6650d50b4e3e522ac55fe569d1bd2f4be374edd3416509bf38",
         font: "Times New Roman",
         type: "everything",
         text: "kashyab",
