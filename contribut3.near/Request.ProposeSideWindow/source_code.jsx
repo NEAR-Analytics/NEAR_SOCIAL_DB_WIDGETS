f616d99dc8b6205e0266c91be977cc18030cbad6
contribut3.near <contribut3.near>
2023-04-17 07:45:22 -0400

    Update Request.ProposeSideWindow by contribut3.near at 2023-04-17 07:45:22.164

---
+++ contribut3.near/Request.ProposeSideWindow/source_code.jsx
@@ -0,0 +1,25 @@
+const ownerId = "contribut3.near";
+const accountId = props.accountId;
+
+return (
+  <Widget
+    src={`${ownerId}/widget/SideWindow`}
+    props={{
+      title: "Propose contribution",
+      description: (
+        <Widget
+          src={`${ownerId}/widget/Request.line`}
+          props={{ accountId }}
+        />
+      ),
+      trigger: <>Propose contribution</>,
+      children: (
+        <Widget
+          src={`${ownerId}/widget/Request.ContributionForm`}
+          props={{ accountId }}
+        />
+      ),
+      minWidth: "600px",
+    }}
+  />
+);
