bb55c0dc8400daadca9824a276d761748f2f0e53
williamxx.near <williamxx.near>
2023-04-17 10:30:55 -0400

    Update CreativesDAO-Banner by williamxx.near at 2023-04-17 10:30:55.692

---
+++ williamxx.near/CreativesDAO-Banner/source_code.jsx
@@ -0,0 +1,34 @@
+const Banner = styled.div`
+    width: 100%;
+    height: 200px;
+    margin-bottom: 200px;
+`;
+
+const Logo = styled.img`
+  width: 150px;
+  position: absolute;
+  top: 150px;
+  left: 60px;
+  z-index: 1;
+  border-radius: 50%;
+  background: rgba(68, 152, 224, 0.75);
+  border: 3px solid white;
+`;
+
+const Lines = styled.div`
+    position: absolute;
+    top:24px;
+    left:0;
+    width: 100%;
+    height: 200px;
+    background: url("https://ipfs.near.social/ipfs/bafkreicplzojcvh2u2qqrfv5uevguxmvq6gccp4b3munmd434saqjilopq");
+    background-size: contain;
+    background-repeat: no-repeat;
+`;
+
+return (
+  <Banner role="banner">
+    <Logo src="https://ipfs.near.social/ipfs/bafkreibb4m4rszlhf46wgob25lhpuxgfbjpc23swd4qne5e6y7eccinkge" />
+    <Lines src="https://ipfs.near.social/ipfs/bafkreicplzojcvh2u2qqrfv5uevguxmvq6gccp4b3munmd434saqjilopq" />
+  </Banner>
+);
