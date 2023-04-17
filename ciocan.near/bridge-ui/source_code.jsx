8f28ba10d2edc7f54de36c1f8e0a5e0f90da3214
ciocan <ciocan>
2023-04-17 19:03:23 -0400

    Update bridge-ui by ciocan.near at 2023-04-17 19:03:23.257

--- ciocan.near/bridge-ui/source_code.jsx
+++ ciocan.near/bridge-ui/source_code.jsx
@@ -1,5 +1,39 @@
-const { network, assets, balance } = props;
-const { action } = state;
+const { from, to, assets, onTabChange, onAction, title } = props;
+const { action, amount, selectedAssetId } = state;
+
+const handleAction = () => {
+  const selectedAssetIdFromProps = assets.find((a) => a.selected)?.id;
+  const assetId = selectedAssetId || selectedAssetIdFromProps || assets?.[0].id;
+  if (onAction) onAction({ networkId: from.network.id, amount, assetId });
+};
+
+const handleMax = () => {
+  State.update({ amount: from.balance });
+};
+
+const handleAmountChange = (e) => {
+  State.update({ amount: e.target.value });
+};
+
+const handleAssetChange = (e) => {
+  State.update({ selectedAssetId: e.target.value });
+};
+
+const handleTabChange = (tab) => {
+  State.update({ action: tab });
+  const data =
+    tab === "deposit"
+      ? {
+          from,
+          to,
+        }
+      : {
+          from: to,
+          to: from,
+        };
+  if (onTabChange) onTabChange(data);
+  State.update({ amount: 0 });
+};
 
 const Container = styled.div`
     max-width: 400px;
@@ -13,10 +47,13 @@ const Container = styled.div`
     border-radius: 0.5rem;
 `;
 
+const isDeposit = !action || action === "deposit";
+const actionTitle = isDeposit ? "Deposit" : "Withdraw";
+
 return (
   <Container>
     <div className="d-flex gap-4 align-items-center mb-3 justify-content-center">
-      <h5>Bridge</h5>
+      <h5>{title || "Bridge"}</h5>
       <div class="action btn-group" role="group" aria-label="Deposit">
         <input
           id="deposit"
@@ -24,8 +61,8 @@ return (
           class="btn-check"
           name="btnradioaction"
           autocomplete="off"
-          checked={!action || action === "deposit"}
-          onClick={() => State.update({ action: "deposit" })}
+          checked={isDeposit}
+          onClick={() => handleTabChange("deposit")}
         />
         <label class="btn btn-outline-primary" for="deposit">
           Deposit
@@ -36,8 +73,8 @@ return (
           class="btn-check"
           name="btnradioaction"
           autocomplete="off"
-          checked={action === "withdraw"}
-          onClick={() => State.update({ action: "withdraw" })}
+          checked={!isDeposit}
+          onClick={() => handleTabChange("withdraw")}
         />
         <label class="btn btn-outline-primary" for="withdraw">
           Withdraw
@@ -48,8 +85,12 @@ return (
     <div className="p-4">
       <div className="d-flex justify-content-between">
         <div className="d-flex flex-column gap-2">
-          <span>{network.from}</span>
-          <select class="form-select" aria-label="select asset">
+          <span>{from.network.value}</span>
+          <select
+            class="form-select"
+            aria-label="select asset"
+            onChange={handleAssetChange}
+          >
             {assets.map((asset) => (
               <option value={asset.id} selected={asset.selected}>
                 {asset.value}
@@ -59,24 +100,36 @@ return (
         </div>
         <div className="d-flex flex-column gap-2">
           <div className="d-flex justify-content-between">
-            <span>Balance: 0.00</span>
+            <span>Balance: {from.balance}</span>
           </div>
           <div className="input-group">
             <input
               style={{ maxWidth: "120px" }}
               type="number"
-              defaultValue="0.00"
+              min="0"
+              step="0.1"
+              defaultValue={props.amount}
+              value={amount}
               placeholder="0.00"
+              onChange={handleAmountChange}
             />
-            <button className="btn btn-light btn-sm">max</button>
+            <button className="btn btn-light btn-sm" onClick={handleMax}>
+              max
+            </button>
           </div>
         </div>
       </div>
     </div>
     <div className="border border-secondary border-bottom-0 border-light" />
     <div className="p-4 d-flex justify-content-between">
-      <div>{network.to}</div>
-      <div>Balance: 0.00</div>
+      <div>{to.network.value}</div>
+      <div>Balance: {to.balance}</div>
+    </div>
+    <div className="border border-secondary border-bottom-0 border-light" />
+    <div className="p-4 d-grid">
+      <button className="btn btn-primary" onClick={handleAction}>
+        {actionTitle}
+      </button>
     </div>
   </Container>
 );
