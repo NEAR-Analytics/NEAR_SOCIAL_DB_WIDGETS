ae5099e207dc54f1ce1972eb534a71c96e11097f
frichard5.near <frichard5.near>
2023-04-18 08:35:32 -0400

    Update proposal-status-by-month by frichard5.near at 2023-04-18 08:35:32.564

--- frichard5.near/proposal-status-by-month/source_code.jsx
+++ frichard5.near/proposal-status-by-month/source_code.jsx
@@ -3,4 +3,22 @@ const account = props.account || "marketing.sputnik-dao.near";
 const apiUrl = `api.pikespeak.ai/daos/proposals/status-by-day/${account}`;
 const publicApiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
 
+State.init({
+  proposalsByMonth: [],
+});
+
+const fetchTransfers = () => {
+  const proposalsByMonth = fetch(apiUrl, {
+    mode: "cors",
+    headers: {
+      "x-api-key": publicApiKey,
+    },
+  });
+  proposalsStatus.body &&
+    State.update({ proposalsStatus: proposalsByMonth.body });
+};
+fetchTransfers();
+
+console.log(state.proposalsByMonth);
+
 return <div>Hello World</div>;
