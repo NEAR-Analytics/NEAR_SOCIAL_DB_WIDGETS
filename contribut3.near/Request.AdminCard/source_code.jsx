85315dffc16471a1967655b4b0f7893e7e043446
contribut3.near <contribut3.near>
2023-04-19 11:05:16 -0400

    Update Request.AdminCard by contribut3.near at 2023-04-19 11:05:16.163

--- contribut3.near/Request.AdminCard/source_code.jsx
+++ contribut3.near/Request.AdminCard/source_code.jsx
@@ -38,7 +38,9 @@ if (!state.nameIsFetched) {
     { keys: [`${accountId}/profile/name`] },
     "final",
     false
-  ).then((data) => State.update({ name: data[accountId].profile.name, nameIsFetched: true }));
+  ).then((data) =>
+    State.update({ name: data[accountId].profile.name, nameIsFetched: true })
+  );
 }
 
 const Owner = styled.a`
@@ -46,14 +48,14 @@ const Owner = styled.a`
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
-  gap: .25em;
+  gap: 0.25em;
   width: 25%;
 `;
 
 const Title = styled.a`
   font-style: normal;
   font-weight: 600;
-  font-size: .95em;
+  font-size: 0.95em;
   line-height: 1em;
   color: #101828;
   width: 45%;
@@ -63,18 +65,10 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 1em .95em;
+  padding: 1em 0.95em;
   gap: 1em;
-  border-bottom: 1px solid #EAECF0;
+  border-bottom: 1px solid #eaecf0;
   width: 100%;
-`;
-
-const Name = styled.a`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  width: 35%;
 `;
 
 const Other = styled.div`
@@ -87,23 +81,34 @@ const Other = styled.div`
 
 return (
   <Container>
-    <Owner href={`/${ownerId}/widget/Index?tab=project&accountId=${props.accountId}`}>
+    <Owner
+      href={`/${ownerId}/widget/Index?tab=project&accountId=${props.accountId}`}
+    >
       <Widget
         src={`${ownerId}/widget/Project.Icon`}
         props={{ accountId: props.accountId, size: "2.5em" }}
       />
       <Widget
         src={`${ownerId}/widget/NameAndAccount`}
-        props={{ accountId: props.accountId, name: state.name, nameSize: "1.125em" }}
+        props={{
+          accountId: props.accountId,
+          name: state.name,
+          nameSize: "1.125em",
+        }}
       />
     </Owner>
-    <Title href={`/${ownerId}/widget/Index?tab=request&accountId=${accountId}&cid=${cid}`}>
+    <Title
+      href={`/${ownerId}/widget/Index?tab=request&accountId=${accountId}&cid=${cid}`}
+    >
       {state.request.title}
     </Title>
     <Other>{new Date().toLocaleDateString()}</Other>
     <Other>{state.proposals.length}</Other>
     <Other>
-      <Widget src={`${ownerId}/widget/ActiveIndicator`} props={{ active: state.request.open }} />
+      <Widget
+        src={`${ownerId}/widget/ActiveIndicator`}
+        props={{ active: state.request.open }}
+      />
     </Other>
   </Container>
 );
