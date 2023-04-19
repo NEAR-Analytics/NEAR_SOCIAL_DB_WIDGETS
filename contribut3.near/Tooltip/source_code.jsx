59d7152fe6cadc442f8f63877741bd4273f03dde
contribut3.near <contribut3.near>
2023-04-19 18:41:24 -0400

    Update Tooltip by contribut3.near at 2023-04-19 18:41:24.177

--- contribut3.near/Tooltip/source_code.jsx
+++ contribut3.near/Tooltip/source_code.jsx
@@ -43,33 +43,38 @@ const slideLeftAndFade = styled.keyframes`
 `;
 
 const StyledArrow = styled.div`
-  fill: #fff;
 `;
 
-const StyledContent = styled(RadixTooltip.Content)`
-  border-radius: 4px;
-  padding: 10px 15px;
-  font-size: 15px;
-  line-height: 1;
-  color: var(--violet11);
-  background-color: white;
-  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
-  user-select: none;
-  animation-duration: 400ms;
-  animation-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
-  will-change: transform, opacity;
+const StyledContent = styled.div`
+  .arrow {
+    fill: #fff;
+  }
 
-  &[data-state='delayed-open'][data-side='top'] {
-    animation-name: ${slideDownAndFade};
-  }
-  &[data-state='delayed-open'][data-side='right'] {
-    animation-name: ${slideLeftAndFade};
-  }
-  &[data-state='delayed-open'][data-side='bottom'] {
-    animation-name: ${slideUpAndFade};
-  }
-  &[data-state='delayed-open'][data-side='left'] {
-    animation-name: ${slideRightAndFade};
+  .content {
+    border-radius: 4px;
+    padding: 10px 15px;
+    font-size: 15px;
+    line-height: 1;
+    color: var(--violet11);
+    background-color: white;
+    box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+    user-select: none;
+    animation-duration: 400ms;
+    animation-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
+    will-change: transform, opacity;
+
+    &[data-state='delayed-open'][data-side='top'] {
+      animation-name: ${slideDownAndFade};
+    }
+    &[data-state='delayed-open'][data-side='right'] {
+      animation-name: ${slideLeftAndFade};
+    }
+    &[data-state='delayed-open'][data-side='bottom'] {
+      animation-name: ${slideUpAndFade};
+    }
+    &[data-state='delayed-open'][data-side='left'] {
+      animation-name: ${slideRightAndFade};
+    }
   }
 `;
 
@@ -82,9 +87,9 @@ return (
     </RadixTooltip.Trigger>
     <StyledContent sideOffset={5} side="top" align="center">
       {props.content}
-      <StyledArrow >
+      <StyledArrow>
         <Radix.Arrow />
-      </StyledArrow >
+      </StyledArrow>
     </StyledContent>
   </RadixTooltip.Root>
 );
