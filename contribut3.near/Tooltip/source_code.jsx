0a4192ebbf951ebb9cf369742fce4ae734a3c531
contribut3.near <contribut3.near>
2023-04-19 18:42:20 -0400

    Update Tooltip by contribut3.near at 2023-04-19 18:42:20.613

--- contribut3.near/Tooltip/source_code.jsx
+++ contribut3.near/Tooltip/source_code.jsx
@@ -42,10 +42,7 @@ const slideLeftAndFade = styled.keyframes`
   }
 `;
 
-const StyledArrow = styled.div`
-`;
-
-const StyledContent = styled.div`
+const StyledTooltip = styled.div`
   .arrow {
     fill: #fff;
   }
@@ -85,11 +82,11 @@ return (
         <path d="M5.30258 5.24935C5.43973 4.85949 5.71042 4.53074 6.06672 4.32134C6.42302 4.11194 6.84194 4.0354 7.24927 4.10526C7.6566 4.17513 8.02606 4.3869 8.29221 4.70307C8.55836 5.01924 8.70403 5.4194 8.70341 5.83268C8.70341 6.99935 6.95341 7.58268 6.95341 7.58268M7.00008 9.91602H7.00591M12.8334 6.99935C12.8334 10.221 10.2217 12.8327 7.00008 12.8327C3.77842 12.8327 1.16675 10.221 1.16675 6.99935C1.16675 3.77769 3.77842 1.16602 7.00008 1.16602C10.2217 1.16602 12.8334 3.77769 12.8334 6.99935Z" stroke="#7E868C" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
       </svg>
     </RadixTooltip.Trigger>
-    <StyledContent sideOffset={5} side="top" align="center">
-      {props.content}
-      <StyledArrow>
-        <Radix.Arrow />
-      </StyledArrow>
-    </StyledContent>
+    <StyledTooltip>
+      <Radix.Content className="content" sideOffset={5} side="top" align="center">
+        {props.content}
+        <Radix.Arrow className="arrow" />
+      </Radix.Content>
+    </StyledTooltip>
   </RadixTooltip.Root>
 );
