d4d4ad6d810e9cab360407202a0fd2937b23c4b5
nycdao.near <nycdao.near>
2023-04-18 21:38:59 -0400

    Update NYC.Summary by nycdao.near at 2023-04-18 21:38:59.912

---
+++ nycdao.near/NYC.Summary/source_code.jsx
@@ -0,0 +1,48 @@
+return (
+  <div>
+    <h5>How to Get Involved</h5>
+    <div className="mb-2 mt-3 d-flex gap-3 flex-wrap">
+      <a
+        className="btn btn-outline-success"
+        href="#/hack.near/widget/Page.Builder"
+      >
+        Create a Page
+      </a>
+      <a className="btn btn-outline-primary" href="https://hack.near.social">
+        Resources
+      </a>
+    </div>
+    <div className="mb-2 mt-2 d-flex gap-2 flex-wrap">
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="#/mob.near/widget/ProfilePage?accountId=hackerhouse.near"
+      >
+        <i className="bi bi-person-circle"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://t.me/nearnyc"
+      >
+        <i className="bi bi-telegram"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://github.com/NearBuilders"
+      >
+        <i className="bi bi-github"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://twitter.com/NEAR__nyc"
+      >
+        <i className="bi bi-twitter"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://nearnyc.org"
+      >
+        <i className="bi bi-calendar"></i>
+      </a>
+    </div>
+  </div>
+);
