37b331541a896397788d8f7f2f2e5113aafc8839
contribut3.near <contribut3.near>
2023-04-19 12:24:35 -0400

    Update Inputs.SetUpPermissions by contribut3.near at 2023-04-19 12:24:35.795

--- contribut3.near/Inputs.SetUpPermissions/source_code.jsx
+++ contribut3.near/Inputs.SetUpPermissions/source_code.jsx
@@ -1,8 +1,8 @@
 const ownerId = "contribut3.near";
 
-console.log(Social.keys(`${context.accountId}/graph/follow/*`, "final", {
-  return_type: "BlockHeight",
-  values_only: true,
+console.log(Social.get(`${context.accountId}/graph/follow/*`, "final", {
+  // return_type: "BlockHeight",
+  // values_only: true,
 }));
 
 return (<Widget
