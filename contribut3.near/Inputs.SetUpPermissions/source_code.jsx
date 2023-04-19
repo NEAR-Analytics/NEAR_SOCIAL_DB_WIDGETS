8be9e7882567f50ab597929de095e737588b4b9f
contribut3.near <contribut3.near>
2023-04-19 12:28:35 -0400

    Update Inputs.SetUpPermissions by contribut3.near at 2023-04-19 12:28:35.488

--- contribut3.near/Inputs.SetUpPermissions/source_code.jsx
+++ contribut3.near/Inputs.SetUpPermissions/source_code.jsx
@@ -1,17 +1,37 @@
 const ownerId = "contribut3.near";
 
-console.log(Social.get(`${context.accountId}/graph/follow/*`, "final", {
-  // return_type: "BlockHeight",
-  // values_only: true,
-}));
+State.init({
+  following: [],
+  followingIsFetched: false,
+  value: [],
+})
+
+if (!state.followingIsFetched) {
+  Near.asyncView(
+    "social.near",
+    "get",
+    { keys: [`${context.accountId}/graph/follow/*`] },
+    "final",
+    false,
+  ).then((data) => State.update({
+    following: Object.keys(data[context.accountId].graph.follow).map((name) => ({ name })),
+    followingIsFetched: true,
+  }));
+  return <>Loading...</>;
+}
+
+// console.log(Social.get(`${context.accountId}/graph/follow/*`, "final", {
+//   // return_type: "BlockHeight",
+//   // values_only: true,
+// }));
 
 return (<Widget
   src={`${ownerId}/widget/Inputs.MultiSelect`}
   props={{
     label: "Accounts to add permissions to",
     placeholder: "Add accounts",
-    options: [{ name: "wallets" }, { name: "games" }],
-    value: state.tags,
-    onChange: (tags) => State.update({ tags }),
+    options: state.following,
+    value: state.value,
+    onChange: (value) => State.update({ value }),
   }}
 />);
