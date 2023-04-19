48b5abba746cd8a4c85e7bd62b0e2f167d50ed1d
contribut3.near <contribut3.near>
2023-04-19 12:42:48 -0400

    Update Inputs.SetUpPermissions by contribut3.near at 2023-04-19 12:42:48.070

--- contribut3.near/Inputs.SetUpPermissions/source_code.jsx
+++ contribut3.near/Inputs.SetUpPermissions/source_code.jsx
@@ -41,14 +41,40 @@ if (!state.followingIsFetched || !state.accountsWithPermissionsIsFetched) {
   return <>Loading...</>;
 }
 
+const Accounts = styled.ul`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.125em;
+  line-height: 1em;
+  color: #344054;
+`;
 
-return (<Widget
-  src={`${ownerId}/widget/Inputs.MultiSelect`}
-  props={{
-    label: "Accounts to add permissions to",
-    placeholder: "Add accounts",
-    options: state.following,
-    value: state.value,
-    onChange: (value) => State.update({ value }),
-  }}
-/>);
+const Existing = styled.div`
+  h3 {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.5em;
+    line-height: 1.5em;
+    color: #344054;
+  }
+`;
+
+return (
+  <>
+    <Widget
+      src={`${ownerId}/widget/Inputs.MultiSelect`}
+      props={{
+        label: "Accounts to add permissions to",
+        placeholder: "Add accounts",
+        options: state.following,
+        value: state.value,
+        onChange: (value) => State.update({ value }),
+      }}
+    />
+    <Existing>
+      <h3>Existing permissions</h3>
+      <Accounts>
+        {state.accountsWithPermissions.map((accountId) => (<li key={accountId}>{accountId}</li>))}
+      </Accounts>
+    </Existing>
+  </>);
