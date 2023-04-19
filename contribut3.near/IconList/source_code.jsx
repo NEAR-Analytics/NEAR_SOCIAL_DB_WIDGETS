5b1b981bdf540836a5d768942998a2d72bca3dcf
contribut3.near <contribut3.near>
2023-04-19 09:25:48 -0400

    Update IconList by contribut3.near at 2023-04-19 09:25:48.660

---
+++ contribut3.near/IconList/source_code.jsx
@@ -0,0 +1,50 @@
+const ownerId = "contribut3.near";
+const ids = props.ids ?? [];
+const onlyOne = ids.length === 1;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: .25em;
+  width: 100%;
+`;
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: .125em;
+  width: 100%;
+`;
+
+State.init({
+  names: [],
+  namesIsFetched: false,
+});
+
+if (!state.namesIsFetched) {
+  Near.asyncView(
+    "social.near",
+    "get",
+    { keys: ids.map((id) => `${id}/profile/name`) },
+    "final",
+    false
+  ).then((data) =>
+    State.update({
+      names: ids.reduce((acc, id) => ({ ...acc, [id]: data[id].profile.name }), {}),
+      namesIsFetched: true,
+    })
+  );
+}
+
+const createItem = (accountId) => (
+  <Item key={accountId}>
+    <Widget src={`${ownerId}/widget/Vendor.Icon`} props={{ accountId }} />
+    {onlyOne ? (
+      <Widget src={`${ownerId}/widget/Vendor.NameAndAccount`} props={{ accountId, name: state }} />
+    ) : <></>}
+  </Item>
+);
