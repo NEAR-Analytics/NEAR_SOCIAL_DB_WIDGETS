7221d8eb09c30bcee46955cc297116c0f7baae0c
nycdao.near <nycdao.near>
2023-04-18 20:38:20 -0400

    Update NYC.Page by nycdao.near at 2023-04-18 20:38:20.094

--- nycdao.near/NYC.Page/source_code.jsx
+++ nycdao.near/NYC.Page/source_code.jsx
@@ -2,7 +2,7 @@ const hashtag = props.hashtag ?? "nyc";
 
 return (
   <>
-    <Widget src="mob.near/widget/ProfileOnboarding" />
+    <Widget src="nycdao.near/widget/CommunityOnboarding" />
     <ul
       className="nav nav-pills nav-fill mb-3 d-lg-none"
       id="pills-tab"
@@ -40,11 +40,11 @@ return (
     <div className="tab-content row p-0" id="pills-tabContent">
       <div
         className="tab-pane show active d-lg-block col-lg-8"
-        id="pills-feed"
+        id="pills-content"
         role="tabpanel"
-        aria-labelledby="pills-feed-tab"
+        aria-labelledby="pills-content-tab"
       >
-        <Widget src="hack.near/widget/Custom.Page.Content" props={props} />
+        <Widget src="nycdao.near/widget/NYC.Page.Content" props={props} />
       </div>
       <div
         className="tab-pane d-lg-block col-lg-4"
@@ -52,7 +52,7 @@ return (
         role="tabpanel"
         aria-labelledby="pills-explore-tab"
       >
-        <Widget src="hack.near/widget/Custom.Page.Menu" props={props} />
+        <Widget src="nycdao.near/widget/NYC.Page.Menu" props={props} />
       </div>
     </div>
   </>
