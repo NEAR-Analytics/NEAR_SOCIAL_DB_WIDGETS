80e7222d7400cfdb16a894752c93c1efc784657f
contribut3.near <contribut3.near>
2023-04-19 11:29:31 -0400

    Update Application.AdminCard by contribut3.near at 2023-04-19 11:29:31.597

--- contribut3.near/Application.AdminCard/source_code.jsx
+++ contribut3.near/Application.AdminCard/source_code.jsx
@@ -74,6 +74,9 @@ const Badge = styled.div`
   text-align: center;
 `;
 
+if (!state.projectIsFetched || !state.profileIsFetched) {
+  return <div>Loading...</div>;
+}
 console.log(state.project)
 const text = typeof state.project.application_status === "string" ? state.project.application_status : Object.keys(state.project.application_status)[0];
 
