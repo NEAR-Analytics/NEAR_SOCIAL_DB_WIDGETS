77db756ee2313efdfdbd77104bccc3131c6d6653
contribut3.near <contribut3.near>
2023-04-19 09:55:51 -0400

    Update Project.AdminList by contribut3.near at 2023-04-19 09:55:51.892

--- contribut3.near/Project.AdminList/source_code.jsx
+++ contribut3.near/Project.AdminList/source_code.jsx
@@ -14,15 +14,45 @@ if (!state.itemsIsFetched) {
   return <>Loading...</>;
 }
 
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: .75em .95em;
+  gap: .75em;
+  width: 100%;
+  background: #f9fafb;
+  border-bottom: 1px solid #eaecf0;
+`;
+
+const Name = styled.div`
+  width: 35%;
+`;
+
+const Other = styled.div`
+  width: 13%;
+`;
+
 return (
-  <Widget
-    src={`${ownerId}/widget/List`}
-    props={{
-      search,
-      items: state.items,
-      createItem: (accountId) => (
-        <Widget src={`${ownerId}/widget/Project.Card`} props={{ accountId }} />
-      ),
-    }}
-  />
+  <div>
+    <Header>
+      <Name>Name</Name>
+      <Other>Admins</Other>
+      <Other>Accelerator</Other>
+      <Other>Graduation</Other>
+      <Other>Created</Other>
+      <Other>Status</Other>
+    </Header>
+    <Widget
+      src={`${ownerId}/widget/List`}
+      props={{
+        search,
+        items: state.items,
+        createItem: (accountId) => (
+          <Widget src={`${ownerId}/widget/Project.Card`} props={{ accountId }} />
+        ),
+      }}
+    />
+  </div>
 );
