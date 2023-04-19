59204afac3b06582c96f7fad57f5a2694e2e2ac5
frichard5.near <frichard5.near>
2023-04-19 14:35:02 -0400

    Update proposal-by-month by frichard5.near at 2023-04-19 14:35:02.651

--- frichard5.near/proposal-by-month/source_code.jsx
+++ frichard5.near/proposal-by-month/source_code.jsx
@@ -1,7 +1,7 @@
 const widgetProvider = props.widgetProvider;
 const account = props.account || "marketing.sputnik-dao.near";
 const widgetUrl = `https://api.pikespeak.ai/widgets/dao/${account}`;
-
+console.log(account);
 return (
   <>
     <iframe
