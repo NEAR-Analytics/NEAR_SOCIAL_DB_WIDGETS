5f941290166d96d31b1beab910b9d6a3ed901adf
contribut3.near <contribut3.near>
2023-04-19 10:43:06 -0400

    Update Request.AdminCard by contribut3.near at 2023-04-19 10:43:06.122

--- contribut3.near/Request.AdminCard/source_code.jsx
+++ contribut3.near/Request.AdminCard/source_code.jsx
@@ -1,70 +1,67 @@
 const ownerId = "contribut3.near";
 const accountId = props.accountId;
+const cid = props.cid;
 
 State.init({
-  project: null,
-  projectIsFetched: false,
-  founders: null,
-  foundersIsFetched: false,
-  requests: null,
-  requestsIsFetched: false,
-  profile: null,
-  profileIsFetched: false,
+  request: null,
+  requestIsFetched: false,
+  proposals: null,
+  proposalsIsFetched: false,
+  name: null,
+  nameIsFetched: false,
 });
 
-if (!state.projectIsFetched) {
+if (!state.requestIsFetched) {
   Near.asyncView(
     ownerId,
-    "get_project",
-    { account_id: accountId },
+    "get_request",
+    { account_id: accountId, cid },
     "final",
     false
-  ).then((project) => State.update({ project, projectIsFetched: true }));
+  ).then((request) => State.update({ request, requestIsFetched: true }));
 }
 
-if (!state.foundersIsFetched) {
+if (!state.proposalsIsFetched) {
   Near.asyncView(
     ownerId,
-    "get_founders",
-    { account_id: accountId },
+    "get_request_proposals",
+    { account_id: accountId, cid },
     "final",
     false
-  ).then((founders) => State.update({ founders, foundersIsFetched: true }));
+  ).then((proposals) => State.update({ proposals, proposalsIsFetched: true }));
 }
 
-if (!state.requestsIsFetched) {
-  Near.asyncView(
-    ownerId,
-    "get_project_requests",
-    { account_id: accountId },
-    "final",
-    false
-  ).then((requests) => State.update({ requests, requestsIsFetched: true }));
-}
-
-if (!state.descriptionIsFetched) {
+if (!state.nameIsFetched) {
   Near.asyncView(
     "social.near",
     "get",
-    { keys: [`${accountId}/profile/*`] },
+    { keys: [`${accountId}/profile/name`] },
     "final",
     false
-  ).then((data) =>
-    State.update({
-      profile: data[accountId].profile,
-      profileIsFetched: true,
-    })
-  );
+  ).then((data) => State.update({ name: data[accountId].profile.name, nameIsFetched: true }));
 }
 
-const Container = styled.div`
+const Owner = styled.a`
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 1em .95em;
-  gap: 1em;
-  border-bottom: 1px solid #EAECF0;
-  width: 100%;
+  justify-content: flex-start;
+  width: 25%;
+`;
+
+const Title = styled.a`
+  font-style: normal;
+  font-weight: 600;
+  font-size: .95em;
+  line-height: 1em;
+  color: #101828;
+  width: 45%;
+`;
+
+const Container = styled.div`
+  .cont {
+    width: 100% !important;
+  }
 `;
 
 const Name = styled.a`
@@ -80,44 +77,28 @@ const Other = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  width: 13%;
-`;
-
-const Badge = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: .5em .95em;
-  background: #f2f4f7;
-  mix-blend-mode: multiply;
-  border-radius: 16px;
-  font-style: normal;
-  font-weight: 500;
-  font-size: .75em;
-  line-height: 1.125em;
-  text-align: center;
+  width: 10%;
 `;
 
 return (
   <Container>
-    <Name href={`/${ownerId}/widget/Index?tab=project&accountId=${props.accountId}`}>
+    <Owner href={`/${ownerId}/widget/Index?tab=project&accountId=${props.accountId}`}>
       <Widget
         src={`${ownerId}/widget/Project.Icon`}
         props={{ accountId: props.accountId, size: "2.5em" }}
       />
       <Widget
         src={`${ownerId}/widget/NameAndAccount`}
-        props={{ accountId: props.accountId, name: state.profile.name, nameSize: "1.125em" }}
+        props={{ accountId: props.accountId, name: state.name, nameSize: "1.125em" }}
       />
-    </Name>
-    <Other>
-      <Widget src={`${ownerId}/widget/IconList`} props={{ ids: state.founders, iconOnly: true, justify: "center" }} />
-    </Other>
-    <Other><Badge>{state.project.application_status}</Badge></Other>
-    <Other><Badge>{state.project.graduation_status}</Badge></Other>
+    </Owner>
+    <Title href={`/${ownerId}/widget/Index?tab=request&accountId=${accountId}&cid=${cid}`}>
+      {state.request.title}
+    </Title>
     <Other>{new Date().toLocaleDateString()}</Other>
+    <Other>{state.proposals.length}</Other>
     <Other>
-      <Widget src={`${ownerId}/widget/ActiveIndicator`} props={{ active: true }} />
+      <Widget src={`${ownerId}/widget/ActiveIndicator`} props={{ active: state.request.open }} />
     </Other>
   </Container>
 );
