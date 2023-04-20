6f60ade45de90cb727b025c3224de7b14479f56d
contribut3.near <contribut3.near>
2023-04-20 12:45:26 -0400

    Update HomePage by contribut3.near at 2023-04-20 12:45:26.071

--- contribut3.near/HomePage/source_code.jsx
+++ contribut3.near/HomePage/source_code.jsx
@@ -255,7 +255,7 @@ const mapImage = (src) => `https://ipfs.near.social/ipfs/${src}`;
 return (
   <Container>
     <Section style={{ padding: "4em 0" }}>
-      <Row style={{ padding: "0 0 2em 0" }}>
+      <Row style={{ padding: "0 0 4em 0" }}>
         <img src={mapImage(mainImage)} />
         <MainSubSection>
           <img src={mapImage(logo)} />
