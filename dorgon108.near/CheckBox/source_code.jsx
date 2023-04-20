807c1446b33962a96befd7b8aa2cf8d0974fb147
dorgon108.near <dorgon108.near>
2023-04-20 02:54:21 -0400

    Update CheckBox by dorgon108.near at 2023-04-20 02:54:21.004

---
+++ dorgon108.near/CheckBox/source_code.jsx
@@ -0,0 +1,122 @@
+let filters = props.filters ?? {};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+
+`;
+const CustomCheckbox = styled.span`
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  border: 1px solid black;
+  margin-right: 8px;
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    display: none;
+    left: 5px;
+    top: 2px;
+    width: 4px;
+    height: 8px;
+    border: solid white;
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg);
+  }
+`;
+
+const CheckboxGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+  color:#878A8E;
+  gap: 16px;
+
+  
+`;
+
+const CheckboxLabel = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const CheckboxInput = styled.div`
+  display: inline-block;
+  margin-right: 8px;
+
+  & input[type="checkbox"] {
+    background-color: #your_desired_color;
+    width: 40px;
+height: 40px;
+
+  }
+  & input[type="checkbox"]:checked {
+    background-color: black;
+  }
+`;
+
+const SubCheckboxGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+  gap: 16px;
+
+`;
+
+State.init({});
+
+const handleChange = (primary, sub) => {
+  if (sub) {
+    State.update((prevState) => ({
+      ...prevState,
+      [primary]: { ...prevState[primary], [sub]: !prevState[primary]?.[sub] },
+    }));
+  } else {
+    State.update((prevState) => ({
+      ...prevState,
+      [primary]: prevState[primary]
+        ? { ...prevState[primary], [primary]: !prevState[primary][primary] }
+        : { [primary]: true },
+    }));
+  }
+};
+
+console.log(state);
+
+// ... JSX structure goes here ...
+return (
+  <Container>
+    {Object.entries(filters).map(([primary, subFilters]) => (
+      <CheckboxGroup key={primary}>
+        <CheckboxLabel checked={state[primary]?.[primary] || false}>
+          <CheckboxInput>
+            <input
+              type="checkbox"
+              checked={state[primary]?.[primary] || false}
+              onChange={() => handleChange(primary)}
+            />
+          </CheckboxInput>
+          {primary}
+        </CheckboxLabel>
+        {subFilters && (
+          <SubCheckboxGroup>
+            {Object.entries(subFilters).map(([sub, _]) => (
+              <CheckboxLabel key={sub} checked={state[primary]?.[sub] || false}>
+                <CheckboxInput>
+                  <input
+                    type="checkbox"
+                    checked={state[primary]?.[sub] || false}
+                    onChange={() => handleChange(primary, sub)}
+                  />
+                </CheckboxInput>
+                {sub}
+              </CheckboxLabel>
+            ))}
+          </SubCheckboxGroup>
+        )}
+      </CheckboxGroup>
+    ))}
+  </Container>
+);
