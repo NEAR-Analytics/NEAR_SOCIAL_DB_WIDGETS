58250362ce6834d58487c8077f5b8271ea3f0db9
contribut3.near <contribut3.near>
2023-04-20 16:07:22 -0400

    Update Proposal.AdminList by contribut3.near at 2023-04-20 16:07:22.726

--- contribut3.near/Proposal.AdminList/source_code.jsx
+++ contribut3.near/Proposal.AdminList/source_code.jsx
@@ -68,10 +68,10 @@ return (
       props={{
         search,
         items: state.items,
-        createItem: ([[projectId, cid], vendor_id]) => (
+        createItem: ([[projectId, cid], vendorId]) => (
           <Widget
             src={`${ownerId}/widget/Proposal.AdminCard`}
-            props={{ projectId, cid, vendor_id }}
+            props={{ projectId, cid, vendorId }}
           />
         ),
       }}
