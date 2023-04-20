9d4d30134b914ee4f01fd3c7683b62fac4a6787c
elliotBraem <elliotBraem>
2023-04-20 17:00:50 -0400

    Update NYC.Social by efiz.near at 2023-04-20 17:00:50.322

--- efiz.near/NYC.Social/source_code.jsx
+++ efiz.near/NYC.Social/source_code.jsx
@@ -1,3 +1,23 @@
-const hashtag = props.hashtag ?? "nyc";
+/**
+ * Configure your community feed.
+ */
+const hashtags = ["nyc"]; // any dedicated hashtags?
+const daoId = "liberty.sputnik-dao.near"; // restrict posting to members of a DAO (Optional)
+const groupId = "community"; // which group can post?
 
-return <Widget src="efiz.near/widget/Posts" props={{ hashtags: [hashtag] }} />;
+const policy = Near.view(daoId, "get_policy");
+const group = policy.roles
+  .filter((role) => role.name === groupId)
+  .map((role) => {
+    const group = role.kind.Group;
+
+    return group;
+  });
+
+const community = {
+  daoId,
+  members: group,
+  domain: "nycdao.near",
+};
+
+return <Widget src="efiz.near/widget/Posts" props={{ hashtags, community }} />;
