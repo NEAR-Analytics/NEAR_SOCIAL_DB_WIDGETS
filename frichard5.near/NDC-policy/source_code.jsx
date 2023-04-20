ee5e71e2f5b49e81a2a747d5512e33c8acad4f56
frichard5.near <frichard5.near>
2023-04-20 09:12:43 -0400

    Update NDC-policy by frichard5.near at 2023-04-20 09:12:43.470

---
+++ frichard5.near/NDC-policy/source_code.jsx
@@ -0,0 +1,22 @@
+const widgetProvider = props.widgetProvider;
+const account = props.account || "marketing.sputnik-dao.near";
+const apiUrl = `https://api.pikespeak.ai/dao/policy/${account}`;
+const publicApiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
+
+const fetchPolicy = () => {
+  const policy = fetch(apiUrl, {
+    mode: "cors",
+    headers: {
+      "x-api-key": publicApiKey,
+    },
+  });
+  policy.body &&
+    State.update({
+      policy: policy.body,
+    });
+};
+!state.policy && fetchPolicy();
+
+console.log("POLICY", state.policy);
+
+return <div>Hello policy</div>;
