55fc10541dd6c0804a3a0344153b7a1bee099d9b
contribut3.near <contribut3.near>
2023-04-20 11:10:44 -0400

    Update HomePage by contribut3.near at 2023-04-20 11:10:44.637

--- contribut3.near/HomePage/source_code.jsx
+++ contribut3.near/HomePage/source_code.jsx
@@ -188,6 +188,14 @@ const List = styled.div`
   }
 `;
 
+const SubHeading = styled.h2`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 48px;
+  line-height: 120%;
+  color: #FFFFFF;
+`;
+
 const foundersImages = ["bafkreiaypep5xqg5vhbw7gralttufuzbqk5oggts2uyfbnjgpshtk5mggu", "bafkreiajakictce47mjdcpro45s3hasoozmuik4n3dkcstizpqv4igbx74", "bafkreihefba6pk23b3jcq2dugs7mw5ktkqtvcuqj5c2owrpkhspthuahyu"]
 const contributorsImage = "bafkreicce3m4vow4eisshn4zdbfrqfda62uavb7t5s2n2bjaawck2i7e64";
 const investorsImage = "bafkreiaypep5xqg5vhbw7gralttufuzbqk5oggts2uyfbnjgpshtk5mggu";
