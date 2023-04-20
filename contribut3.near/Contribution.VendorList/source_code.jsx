83315db1207ab8db1507dc8d4f0a508e8368f659
contribut3.near <contribut3.near>
2023-04-20 07:23:44 -0400

    Update Contribution.VendorList by contribut3.near at 2023-04-20 07:23:44.763

--- contribut3.near/Contribution.VendorList/source_code.jsx
+++ contribut3.near/Contribution.VendorList/source_code.jsx
@@ -17,37 +17,6 @@ if (!state.itemsIsFetched) {
 
   return <>Loading...</>;
 }
-
-const Header = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0.75em 0.95em;
-  gap: 0.75em;
-  width: 100%;
-  background: #f9fafb;
-  border-bottom: 1px solid #eaecf0;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 0.75em;
-  line-height: 1em;
-  color: #475467;
-`;
-
-const Owner = styled.div`
-  width: 20%;
-`;
-
-const Title = styled.div`
-  width: 40%;
-`;
-
-const Other = styled.div`
-  text-align: center;
-  width: 10%;
-`;
-
 const Container = styled.div`
   .cont {
     width: 100% !important;
@@ -56,13 +25,6 @@ const Container = styled.div`
 
 return (
   <Container>
-    <Header>
-      <Owner>Contractor</Owner>
-      <Owner>Owner</Owner>
-      <Title>Contract</Title>
-      <Other>Created</Other>
-      <Other>Status</Other>
-    </Header>
     <Widget
       src={`${ownerId}/widget/List`}
       props={{
@@ -70,8 +32,8 @@ return (
         items: state.items,
         createItem: ([[project_id, cid], vendor_id]) => (
           <Widget
-            src={`${ownerId}/widget/Contribution.AdminCard`}
-            props={{ project_id, cid, vendor_id }}
+            src={`${ownerId}/widget/Contribution.Card`}
+            props={{ project_id, cid, vendor_id, isVendorView: true }}
           />
         ),
       }}
