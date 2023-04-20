d48e4ca1b7b88d47d99c25a4f6305b2d0342dacb
contribut3.near <contribut3.near>
2023-04-20 07:29:06 -0400

    Update Contribution.VendorList by contribut3.near at 2023-04-20 07:29:06.807

--- contribut3.near/Contribution.VendorList/source_code.jsx
+++ contribut3.near/Contribution.VendorList/source_code.jsx
@@ -9,7 +9,7 @@ State.init({
 if (!state.itemsIsFetched) {
   Near.asyncView(
     ownerId,
-    "get_vendor_completed_contributions",
+    "get_vendor_contributions",
     { account_id: context.accountId },
     "final",
     false
