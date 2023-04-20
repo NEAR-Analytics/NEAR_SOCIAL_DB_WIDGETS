1ac7601464fe023b428e2eec5b2b274514f11d8e
frichard5.near <frichard5.near>
2023-04-20 14:33:51 -0400

    Update NDC-members-voters by frichard5.near at 2023-04-20 14:33:51.056

--- frichard5.near/NDC-members-voters/source_code.jsx
+++ frichard5.near/NDC-members-voters/source_code.jsx
@@ -3,6 +3,38 @@ const account = props.account || "marketing.sputnik-dao.near";
 const apiUrl = `https://api.pikespeak.ai/daos/votes/${account}`;
 const publicApiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
 
+const columns = [
+  {
+    id: "account",
+    label: "Account",
+    formatter: (d) => {
+      return (
+        <a
+          href={`https://explorer.near.org/accounts/${d.account}`}
+          target="_blank"
+        >
+          {d.account}
+        </a>
+      );
+    },
+  },
+  {
+    id: "isCouncil",
+    label: "Council member",
+    formatter: (d) => (d.isCouncil ? "yes" : "no"),
+  },
+  {
+    id: "approve",
+    label: "Approved",
+    formatter: (d) => <span className="approved">{d.approve}</span>,
+  },
+  {
+    id: "rejected",
+    label: "Rejected",
+    formatter: (d) => <span className="rejected">{d.approve}</span>,
+  },
+];
+
 const fetchVotersByProposal = () => {
   const voters = fetch(apiUrl, {
     mode: "cors",
@@ -17,6 +49,20 @@ const fetchVotersByProposal = () => {
 };
 !state.voters && fetchVotersByProposal();
 
-console.log("VOTERS", state.voters);
-
-return <div>Hello voters</div>;
+return (
+  <div>
+    {state.voters &&
+      state.voters.map((v) => {
+        return (
+          <Widget
+            src={`${widgetProvider}/widget/generic_table`}
+            props={{
+              title: `${v.type}`,
+              columns,
+              data: v.voters,
+            }}
+          />
+        );
+      })}
+  </div>
+);
