5f68b085b72871390d0a75b35d2ec95d963aee5a
contribut3.near <contribut3.near>
2023-04-21 08:24:36 -0400

    Update Request.Page by contribut3.near at 2023-04-21 08:24:36.521

--- contribut3.near/Request.Page/source_code.jsx
+++ contribut3.near/Request.Page/source_code.jsx
@@ -205,12 +205,7 @@ return (
     <ContentContainer>
       <MainContent>{content}</MainContent>
       <Sidebar>
-        <Widget
-          src={`${ownerId}/widget/Request.Sidebar`}
-          props={{
-            accountId,
-          }}
-        />
+        <Widget src={`${ownerId}/widget/Request.Sidebar`} props={{ accountId }} />
       </Sidebar>
     </ContentContainer>
   </Container>
