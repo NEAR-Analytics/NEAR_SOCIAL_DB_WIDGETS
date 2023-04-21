d8ba1595b7746ecf72e2827ed6ce573d7d49fc40
contribut3.near <contribut3.near>
2023-04-21 08:56:03 -0400

    Update Request.Page by contribut3.near at 2023-04-21 08:56:03.816

--- contribut3.near/Request.Page/source_code.jsx
+++ contribut3.near/Request.Page/source_code.jsx
@@ -241,8 +241,8 @@ return (
                 },
               ],
             }}
-          />
-        : <></>
+          />)
+          : (<></>)
         }
         {content}</MainContent>
       <Sidebar>
