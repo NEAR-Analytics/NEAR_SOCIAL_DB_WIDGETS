491916e5d973c849e731cfd9adc9b280ecdecd0d
oztanmain.near <oztanmain.near>
2023-04-21 17:08:24 -0400

    Update OnboardingStarterComponent by oztanmain.near at 2023-04-21 17:08:24.840

--- oztanmain.near/OnboardingStarterComponent/source_code.jsx
+++ oztanmain.near/OnboardingStarterComponent/source_code.jsx
@@ -1,9 +1,17 @@
 return (
-  <>
+  <div
+    style={{
+      padding: "25px",
+      borderRadius: "6px",
+      border: "1px dashed #6D62D4",
+      background: "#F3F2FD",
+      color: "#2A297A",
+    }}
+  >
     //src="near/widget/Onboarding.ComponentCard" to be pasted below
     <Widget />
     <br />
     //src="near/widget/Onboarding.ComponentCollection" to be pasted below
     <Widget />
-  </>
+  </div>
 );
