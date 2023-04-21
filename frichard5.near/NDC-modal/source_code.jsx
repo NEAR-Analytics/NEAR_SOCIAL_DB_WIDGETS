25d9b92a636e9a4323088855e498fa29e6185993
frichard5.near <frichard5.near>
2023-04-21 13:17:36 -0400

    Update NDC-modal by frichard5.near at 2023-04-21 13:17:36.943

--- frichard5.near/NDC-modal/source_code.jsx
+++ frichard5.near/NDC-modal/source_code.jsx
@@ -32,10 +32,19 @@ const closeModal = () => {
 return (
   <Modal id="modal">
     <button
-      style={{ position: "absolute", right: "30px" }}
+      style={{ position: "absolute", right: "100px", top: "29%" }}
       onClick={closeModal}
     >
-      Close
+      <svg
+        height="20px"
+        class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-1om0hkc"
+        focusable="false"
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+        data-testid="CloseIcon"
+      >
+        <path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+      </svg>
     </button>
     <ComponentWrapper id="modal-comp" className="component-wrapper">
       {component}
