e43e94b79e89b1ea6f1f0f7d9772cfbc9bc06fa3
evrything.near <evrything.near>
2023-04-21 18:22:57 -0400

    Update Everything.ViewAll.Events by evrything.near at 2023-04-21 18:22:57.509

---
+++ evrything.near/Everything.ViewAll.Events/source_code.jsx
@@ -0,0 +1,24 @@
+let events = [];
+
+const data = fetch("https://monkfish-app-ginhc.ondigitalocean.app/graphql", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "X-Everything": "simple",
+  },
+  body: JSON.stringify({
+    query:
+      "query events($source: String) { findEvents(source: {is: $source}) { id, title, start, end, url } }",
+  }),
+  variables: {
+    source: "hello",
+  },
+});
+
+// we can do a few things here...
+
+if (!data.body.errors) {
+  events = data.body.data.findEvents;
+}
+
+return <Widget src={"evrything.near/widget/Calendar"} props={{ events }} />;
