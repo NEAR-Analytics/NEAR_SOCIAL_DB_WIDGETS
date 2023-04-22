daa7a963ec73155d5daf1ae701a7c4eeafdee232
zavodil <zavodil>
2023-04-22 12:35:13 -0400

    Update testParent by zavodil.near at 2023-04-22 12:35:13.626

---
+++ zavodil.near/testParent/source_code.jsx
@@ -0,0 +1,30 @@
+const onLoad = (obj) => {
+  console.log(obj.func(1));
+  State.update({ funcRemote: obj.func });
+  console.log(state.funcRemote(2));
+};
+
+if (typeof state.funcRemote === "function") {
+  console.log("Run funcRemote", state.funcRemote(3));
+}
+
+const onClick = () => {
+  if (typeof state.funcRemote === "function") {
+    console.log("onClick funcRemote", state.funcRemote(4));
+  }
+};
+
+return (
+  <div>
+    {typeof state.funcRemote !== "function" && (
+      <Widget
+        src="zavodil.near/widget/test"
+        props={{
+          onLoad,
+          value: "test",
+        }}
+      />
+    )}
+    <button onClick={() => onClick()}>Button</button>
+  </div>
+);
