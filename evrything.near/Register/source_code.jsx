baba1d44a8d215b43fd50880a5a580139ef752b7
evrything.near <evrything.near>
2023-04-21 22:06:07 -0400

    Update Register by evrything.near at 2023-04-21 22:06:07.454

--- evrything.near/Register/source_code.jsx
+++ evrything.near/Register/source_code.jsx
@@ -1,61 +1,136 @@
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-bottom: 16px;
-`;
+// const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   padding-bottom: 16px;
+// `;
 
-const Text = styled.p`
-  font-size: 1.2rem;
-  margin-bottom: 1rem;
-  font-family: ${font}, Times, serif;
-`;
+// const Text = styled.p`
+//   font-size: 1.2rem;
+//   margin-bottom: 1rem;
+//   font-family: ${font}, Times, serif;
+// `;
 
 const Link = styled.a`
   text-decoration: none;
 `;
 
 const Button = styled.button`
-  background-color: #2ecc71;
-  color: #fff;
-  border: none;
-  padding: 1rem 2rem;
-  border-radius: 5px;
-  font-size: 1.2rem;
-  cursor: pointer;
-  transition: background-color 0.2s ease-in-out;
+  text-transform: lowercase !important;
+`;
 
-  &:hover {
-    background-color: #27ae60;
+const Post = styled.div`
+  position: relative;
+
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    left: 19px;
+    top: 52px;
+    bottom: 12px;
+    width: 2px;
+    background: #ECEEF0;
+  }
+`;
+
+const Header = styled.div`
+  margin-bottom: 0;
+  display: inline-flex;
+`;
+
+const Body = styled.div`
+  padding-left: 52px;
+  padding-bottom: 1px;
+`;
+
+const Content = styled.div`
+  img {
+    display: block;
+    max-width: 100%;
+    max-height: 80vh;
+    margin: 0 0 12px;
+  }
+  padding-top: 4px;
+`;
+
+const Text = styled.p`
+  display: block;
+  margin: 0;
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 400;
+  color: #687076;
+  white-space: nowrap;
+`;
+
+const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  margin: -6px -6px 6px;
+`;
+
+const Comments = styled.div`
+  > div > div:first-child {
+    padding-top: 12px;
+  }
+`;
+const CommentWrapper = styled.div`
+  > div:first-child {
+    > a:first-child {
+      display: inline-flex;
+      margin-bottom: 24px;
+      font-size: 14px;
+      line-height: 20px;
+      color: #687076;
+      outline: none;
+      font-weight: 600;
+
+      &:hover,
+      &:focus {
+        color: #687076;
+        text-decoration: underline;
+      }
+    }
   }
 `;
 
 return (
-  <Container>
-    <Text>
-      In order to post, you'll need to create an account with
-      <a href="https://near.org/">NEAR</a>.<br />
-      <br />
-      <strong>everything</strong> was built using fully decentralized
-      technologies, it's completely open source, and will always be free and
-      open to the public.
-      <br />
-      <br />
-      We take your privacy seriously, and we don't collect any of your
-      information.
-      <br />
-      <br />
-      It is a platform built for the people, by the people. And we mean that
-      literally-- tell us what you want us to build and help us build it!
-      <br />
-      <br />
-      Because with <strong>everything</strong> we can do <i>anything</i>
-      .
-      <br />
-      <br />
-      It's about time we had a platform like this.
-    </Text>
-    <Link href="https://shard.dog/go?url=https://near.social">
-      <Button>register</Button>
-    </Link>
-  </Container>
+  <Post>
+    <Header>
+      <Widget
+        src="calebjacob.near/widget/AccountProfile"
+        props={{
+          accountId: "evrything.near",
+          hideAccountId: false,
+        }}
+      />
+    </Header>
+    <Body>
+      <Content>
+        Welcome! In order to post, you'll need to create an account with
+        <a href="https://near.org/">NEAR</a>.
+        <br />
+        <br />
+        <Link href="https://shard.dog/go?url=https://near.social">
+          <Button>register</Button>
+        </Link>
+        <br />
+        <br />
+        <strong>everything</strong> was built using fully decentralized
+        technologies, it's completely open source, it will always be free and
+        open to the public, and we don't collect any of your information.
+        <br />
+        <br />
+        It is a platform built for the people, by the people. And we mean that--
+        tell us what you want us to build and help us build it!
+        <br />
+        <br />
+        Because with <strong>everything</strong> we can do <i>anything</i>.
+        <br />
+        It's about time we had a platform like this.
+        <br />
+      </Content>
+    </Body>
+  </Post>
 );
