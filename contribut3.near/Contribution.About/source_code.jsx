8c980e4f3a19f8c6a1c87e34f7367777bf3632c3
contribut3.near <contribut3.near>
2023-04-24 09:17:52 -0400

    Update Contribution.About by contribut3.near at 2023-04-24 09:17:52.949

--- contribut3.near/Contribution.About/source_code.jsx
+++ contribut3.near/Contribution.About/source_code.jsx
@@ -1,1 +1,51 @@
-return <>Details</>;
+const ownerId = "contribut3.near";
+const projectId = props.projectId;
+const cid = props.cid;
+const vendorId = props.vendorId;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 1em;
+  padding: 0.5em 0.2em;
+  width: 100%;
+`;
+
+State.init({
+  proposal: null,
+  proposalIsFetched: false,
+});
+
+if (!state.requestIsFetched) {
+  Near.asyncView(
+    ownerId,
+    "get_proposal",
+    { project_id: projectId, cid, vendor_id: vendorId },
+    "final",
+    false
+  ).then((proposal) => State.update({ proposal, proposalIsFetched: true }));
+}
+
+if (!state.requestIsFetched) {
+  return <>Loading...</>;
+}
+
+return (
+  <Container>
+    <Widget
+      src={`${ownerId}/widget/Inputs.Viewable.TextArea`}
+      props={{
+        label: "Description",
+        id: "description",
+        value: state.request.description,
+        onSave: (description) =>
+          Near.call(ownerId, "edit_request", {
+            request: { ...state.request, description },
+          }),
+        canEdit: props.isAdmin,
+      }}
+    />
+  </Container>
+);
