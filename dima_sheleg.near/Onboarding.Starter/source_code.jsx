2e6ad2d876b8ef3b96804f06f4345ddb6ab368d4
dima_sheleg.near <dima_sheleg.near>
2023-04-24 16:11:39 -0400

    Update Onboarding.Starter by dima_sheleg.near at 2023-04-24 16:11:39.249

--- dima_sheleg.near/Onboarding.Starter/source_code.jsx
+++
@@ -1,9 +0,0 @@
-return (
-  <>
-    {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
-    <Widget />
-    <br />
-    {/* src="near/widget/Onboarding.ComponentCollection" to be pasted below */}
-    <Widget />
-  </>
-);
