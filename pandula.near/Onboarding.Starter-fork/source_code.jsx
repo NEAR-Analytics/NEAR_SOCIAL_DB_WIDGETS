fe52f10e41fd957240165a5051c59f5d14d80451
pandula.near <pandula.near>
2023-04-24 13:32:54 -0400

    Update Onboarding.Starter-fork by pandula.near at 2023-04-24 13:32:54.135

---
+++ pandula.near/Onboarding.Starter-fork/source_code.jsx
@@ -0,0 +1,9 @@
+return (
+  <>
+    {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
+    <Widget src="near/widget/Onboarding.ComponentCard" props={{ accountId }} />
+    <br />
+    {/* src="near/widget/Onboarding.ComponentCollection" to be pasted below */}
+    <Widget src="near/widget/Onboarding.ComponentCollection" />
+  </>
+);
