d7d347eafe3672c3bc8887e9edea8d94da68cb64
contribut3.near <contribut3.near>
2023-04-24 08:50:17 -0400

    Update Contribution.Activity by contribut3.near at 2023-04-24 08:50:17.560

--- contribut3.near/Contribution.Activity/source_code.jsx
+++ contribut3.near/Contribution.Activity/source_code.jsx
@@ -1,1 +1,30 @@
+const contribution = props.contribution;
+const request = props.request;
+const projectName = props.projectName;
+const vendorName = props.vendorName;
+
+const activity = [
+  {
+    id: "created",
+    text: <><b>{projectName}</b> created a contract <b>"{request.title}"</b> with <b>{vendorName}</b></>
+  }, {
+    id: "awaiting",
+    text: <>Awaiting approval by <b>{vendorName}</b></>
+  },
+];
+
+if ((!("Rejected" in contribution.status) && !("Created" in contribution.status))) {
+  activity.push({
+    id: "accepted",
+    text: <>{vendorName} accepted contract</>
+  });
+}
+
+if ("Rejected" in contribution.status) {
+  activity.push({
+    id: "rejected",
+    text: <>{vendorName} rejected contract</>
+  });
+}
+
 return <>Here</>;
