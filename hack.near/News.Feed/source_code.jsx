aaa6f333e68280397a219d1bdc469dbfb916fc6a
NearSocial <NearSocial>
2023-04-24 13:41:52 -0400

    Update News.Feed by hack.near at 2023-04-24 13:41:52.643

---
+++ hack.near/News.Feed/source_code.jsx
@@ -0,0 +1,26 @@
+const daoId = "nearweek-news-contribution.sputnik-dao.near"; // restrict posting to members of a DAO (optional)
+const groupId = "Whitelisted Members"; // which group can post?
+
+const policy = Near.view(daoId, "get_policy");
+const group = policy.roles
+  .filter((role) => role.name === groupId)
+  .map((role) => {
+    const group = role.kind.Group;
+
+    return group;
+  });
+
+const hashtags = [{ name: "news", required: true }];
+
+return (
+  <Widget
+    src="efiz.near/widget/Community.Posts"
+    props={{
+      communityHashtags: hashtags,
+      communityDomain: "nearweekapp.near",
+      communityMembers: group[0],
+      exclusive: false,
+      allowPublicPosting: true,
+    }}
+  />
+);
