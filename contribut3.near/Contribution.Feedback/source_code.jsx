f3c1f4c8cb3cfa9c11c46832c315b19db56b84b3
contribut3.near <contribut3.near>
2023-04-24 09:22:00 -0400

    Update Contribution.Feedback by contribut3.near at 2023-04-24 09:22:00.064

--- contribut3.near/Contribution.Feedback/source_code.jsx
+++ contribut3.near/Contribution.Feedback/source_code.jsx
@@ -1,1 +1,9 @@
+const status = props.status;
+const vendorFeedback = props.vendorFeedback;
+const projectFeedback = props.projectFeedback;
+
+if (!("Completed" in status)) {
+  return <>No feedback yet. Contract still in progress...</>;
+}
+
 return <>Feedback</>;
