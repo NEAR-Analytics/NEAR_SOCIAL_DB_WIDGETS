922658759c7ac8c614573c9cc3af437c8ce08b16
ref-admin.near <ref-admin.near>
2023-04-25 14:56:14 -0400

    Update ref-component-tab by ref-admin.near at 2023-04-25 14:56:14.418

---
+++ ref-admin.near/ref-component-tab/source_code.jsx
@@ -0,0 +1,58 @@
+const { tab, changeTab } = props;
+
+const TagWrapper = styled.div`
+  display: flex;
+  align-items:center;
+  padding-bottom: 16px;
+  color: #FFFFFF;
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+const Box = styled.div`
+    opacity: ${(p) => (p.chosed ? "1" : "0.6")};
+    margin-right: 70px;
+    cursor: pointer;
+
+
+
+`;
+
+const SeperatorLine = styled.div`
+    width: 100%;
+    height: 2px;
+    background:#002C35;
+`;
+
+const BoxLine = styled.div`
+    height: 4px;
+    width: 100%;
+    background: ${(p) => (p.chosed ? "#00FFD1" : "transparent")} ;
+    position:relative;
+    top:19px
+`;
+
+return (
+  <div>
+    <TagWrapper>
+      {["Components", "Templates", "My collections"].map((tabNew) => {
+        return (
+          <div>
+            <Box
+              chosed={tab === tabNew}
+              onClick={() => {
+                changeTab(tabNew);
+              }}
+            >
+              {tabNew}
+
+              {<BoxLine chosed={tab === tabNew}></BoxLine>}
+            </Box>
+          </div>
+        );
+      })}
+    </TagWrapper>
+
+    <SeperatorLine></SeperatorLine>
+  </div>
+);
