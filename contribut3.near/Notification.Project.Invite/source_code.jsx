00202343cb97487a9edc2f4e334737c542ef17d5
contribut3.near <contribut3.near>
2023-04-25 16:22:52 -0400

    Update Notification.Project.Invite by contribut3.near at 2023-04-25 16:22:52.367

--- contribut3.near/Notification.Project.Invite/source_code.jsx
+++ contribut3.near/Notification.Project.Invite/source_code.jsx
@@ -107,7 +107,7 @@ return (
       invited
       <Widget
         src="near/widget/AccountProfileInline"
-        props={{ accountId: props.value.vendorId }}
+        props={{ accountId: props.value.vendorId || props.value.accountId }}
       />
       to contribute to request
       <Text bold>{state.request.title}.</Text>
