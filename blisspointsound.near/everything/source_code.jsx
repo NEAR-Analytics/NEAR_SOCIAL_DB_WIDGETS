346cbbeeb13b2d0915d44abd9fa7997f44e19737
blisspointsound.near <blisspointsound.near>
2023-04-25 18:41:07 -0400

    Update everything by blisspointsound.near at 2023-04-25 18:41:07.524

---
+++ blisspointsound.near/everything/source_code.jsx
@@ -0,0 +1,1 @@
+return (<Widget src="evrything.near/widget/Everything.Template" props={{ accountId: "blisspointsound.near", font: "Times New Roman", type: "everything", text: "blisspointsound.near", domain: "everything" }} />);
