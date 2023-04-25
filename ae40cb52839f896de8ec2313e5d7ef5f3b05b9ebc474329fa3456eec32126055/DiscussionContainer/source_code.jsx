dc706c7a59c661c9e2d29c808ecb8f0d5cfe56a9
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-04-25 14:16:23 -0400

    Update DiscussionContainer by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-04-25 14:16:23.449

---
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DiscussionContainer/source_code.jsx
@@ -0,0 +1,13 @@
+if (props.tab !== "my_tab") return <></>;
+
+return (
+  <Widget
+    src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions"
+    props={{
+      identifier: "uuid-4",
+      notificationWidget:
+        "ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DiscussionContainer",
+      notificationParams: { tab: "my_tab" },
+    }}
+  />
+);
