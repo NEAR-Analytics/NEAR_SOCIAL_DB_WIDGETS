3d83b797dbee98ad1cac7c4193b627f661448134
contribut3.near <contribut3.near>
2023-04-25 09:03:39 -0400

    Update Vendor.InviteForm by contribut3.near at 2023-04-25 09:03:39.658

--- contribut3.near/Vendor.InviteForm/source_code.jsx
+++ contribut3.near/Vendor.InviteForm/source_code.jsx
@@ -233,54 +233,56 @@ return (
             </>
           ),
           onClick: () => {
-            // Near.view(ownerId, "get_vendor", { account_id: props.accountId }).then(({ permisssions }) => {
-            // const data = Object.keys(permissions).filter((account) => permissions[account].includes("Admin")).map((account) => ({
-            //   index: {
-            //     graph: JSON.stringify({
-            //       key: "project/invite",
-            //       value: { accountId: state.projectId.value },
-            //     }),
-            //     inbox: JSON.stringify({
-            //       key: account,
-            //       value: {
-            //         type: "project/invite",
-            //         requestId: [state.projectId.value, state.requestId.value],
-            //         message: state.message,
-            //         vendorId: props.accountId,
-            //       },
-            //     }),
-            //   }
-            // }));
-            // Near.call(data.map((index) => ({
-            //   contractName: "social.near",
-            //   methodName: "set",
-            //   args: index,
-            // })));
-            Near.call({
-              contractName: "social.near", methodName: "set", args: {
-                data: {
-                  [props.accountId]: {
-                    index: {
-                      graph: JSON.stringify({
-                        key: "project/invite",
-                        value: { accountId: state.projectId.value },
-                      }),
-                      inbox: JSON.stringify({
-                        key: props.accountId,
-                        value: {
-                          type: "project/invite",
-                          requestId: [state.projectId.value, state.requestId.value],
-                          vendorId: props.accountId,
-                          message: state.message,
-                        },
-                      }),
-                    }
+            Near.view(ownerId, "get_vendor", { account_id: props.accountId }).then(({ permisssions }) => {
+              const data = Object.keys(permissions).filter((account) => permissions[account].includes("Admin")).map((account) => ({
+                [context.accountId]: {
+                  index: {
+                    graph: JSON.stringify({
+                      key: "project/invite",
+                      value: { accountId: state.projectId.value },
+                    }),
+                    inbox: JSON.stringify({
+                      key: account,
+                      value: {
+                        type: "project/invite",
+                        requestId: [state.projectId.value, state.requestId.value],
+                        message: state.message,
+                        vendorId: props.accountId,
+                      },
+                    }),
                   }
-                },
-              },
-            }
-            )
-            // })
+                }
+              }));
+              Near.call(data.map((index) => ({
+                contractName: "social.near",
+                methodName: "set",
+                args: index,
+              })));
+              // Near.call({
+              //   contractName: "social.near", methodName: "set", args: {
+              //     data: {
+              //       [props.accountId]: {
+              //         index: {
+              //           graph: JSON.stringify({
+              //             key: "project/invite",
+              //             value: { accountId: state.projectId.value },
+              //           }),
+              //           inbox: JSON.stringify({
+              //             key: props.accountId,
+              //             value: {
+              //               type: "project/invite",
+              //               requestId: [state.projectId.value, state.requestId.value],
+              //               vendorId: props.accountId,
+              //               message: state.message,
+              //             },
+              //           }),
+              //         }
+              //       }
+              //     },
+              //   },
+              // }
+              // )
+            })
           },
         }}
       />
