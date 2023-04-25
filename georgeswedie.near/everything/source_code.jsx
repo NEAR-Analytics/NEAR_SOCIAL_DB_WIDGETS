e7a0029bd7a23c381c7b58e4c9d7d60b40c6b71f
georgeswedie.near <georgeswedie.near>
2023-04-25 03:54:14 -0400

    Update everything by georgeswedie.near at 2023-04-25 03:54:14.190

---
+++ georgeswedie.near/everything/source_code.jsx
@@ -0,0 +1,1 @@
+return (<Widget src="evrything.near/widget/Everything.Template" props={{ accountId: "georgeswedie.near", font: "Times New Roman", type: "everything", text: "georgeswedie.near", domain: "everything" }} />);
