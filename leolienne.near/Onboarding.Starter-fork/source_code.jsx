06689bfbef42f226175e43275c0b89554c2d34a0
leolienne.near <leolienne.near>
2023-04-25 18:52:03 -0400

    Update Onboarding.Starter-fork by leolienne.near at 2023-04-25 18:52:03.410

---
+++ leolienne.near/Onboarding.Starter-fork/source_code.jsx
@@ -0,0 +1,10 @@
+return (
+  <>
+    {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
+    <Widget src="near/widget/Onboarding.ComponentCard" props={{ accountId }} />
+    <br />
+    {/* src="near/widget/Onboarding.ComponentCollection" to be pasted below */}
+    <Widget src="contribut3.near/widget/IpfsFilesUpload" />
+    <Widget src="near/widget/Onboarding.ComponentCollection" />
+  </>
+);
