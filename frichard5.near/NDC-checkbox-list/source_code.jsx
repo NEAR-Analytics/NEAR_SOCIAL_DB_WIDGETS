320464846db8a80e8850d5be08027017f89487ec
frichard5.near <frichard5.near>
2023-04-25 08:49:49 -0400

    Update NDC-checkbox-list by frichard5.near at 2023-04-25 08:49:49.792

--- frichard5.near/NDC-checkbox-list/source_code.jsx
+++ frichard5.near/NDC-checkbox-list/source_code.jsx
@@ -1,1 +1,20 @@
-return <div>Hello World</div>;
+const { checkboxes, title } = props;
+
+const Input = styled.input``;
+const Fieldset = styled.fieldset`
+    display: flex;
+    flex-direction: column;
+`;
+
+return (
+  <Fieldset>
+    <p>{title}</p>
+    {checkboxes.map((c) => {
+      return (
+        <label>
+          <Input type="checkbox" value={c.value} /> {c.label}
+        </label>
+      );
+    })}
+  </Fieldset>
+);
