225df092a9c0bc18b5d959352122d4f131134020
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-04-25 17:51:48 -0400

    Update DiscussionContainer by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-04-25 17:51:48.790

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DiscussionContainer/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/DiscussionContainer/source_code.jsx
@@ -5,9 +5,9 @@ return (
     src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions"
     props={{
       identifier: "uuid-4",
-      notificationWidget:
+      parentComponent:
         "ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DiscussionContainer",
-      notificationWidgetParams: { tab: "discussion" },
+      parentParams: { tab: "discussion" },
       notifyAccountId: "gagdiez.near",
       highlightComment: props.highlightComment,
     }}
