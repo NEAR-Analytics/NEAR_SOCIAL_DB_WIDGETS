4edfa329d66476b69a18f4ffe7ac3eb8b219e0cc
nearhorizon.near <nearhorizon.near>
2023-04-26 09:49:20 -0400

    Update Request.About by nearhorizon.near at 2023-04-26 09:49:20.342

---
+++ nearhorizon.near/Request.About/source_code.jsx
@@ -0,0 +1,51 @@
+const ownerId = "nearhorizon.near";
+const accountId = props.accountId;
+const cid = props.cid;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 1em;
+  padding: 0.5em 0.2em;
+  width: 100%;
+`;
+
+State.init({
+  request: null,
+  requestIsFetched: false,
+});
+
+if (!state.requestIsFetched) {
+  Near.asyncView(
+    ownerId,
+    "get_request",
+    { account_id: accountId, cid },
+    "final",
+    false
+  ).then((request) => State.update({ request, requestIsFetched: true }));
+}
+
+if (!state.requestIsFetched) {
+  return <>Loading...</>;
+}
+
+return (
+  <Container>
+    <Widget
+      src={`${ownerId}/widget/Inputs.Viewable.TextArea`}
+      props={{
+        label: "Description",
+        id: "description",
+        value: state.request.description,
+        onSave: (description) =>
+          Near.call(ownerId, "edit_request", {
+            cid,
+            request: { ...state.request, description },
+          }),
+        canEdit: props.isAdmin,
+      }}
+    />
+  </Container>
+);
