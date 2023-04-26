db17646f7e85bc96f49340bf04717016e2972570
contribut3.near <contribut3.near>
2023-04-26 07:36:19 -0400

    Update Project.Documents by contribut3.near at 2023-04-26 07:36:19.602

--- contribut3.near/Project.Documents/source_code.jsx
+++ contribut3.near/Project.Documents/source_code.jsx
@@ -1,12 +1,65 @@
 const ownerId = "contribut3.near";
+const canEdit = props.isAdmin ?? false;
+const accountId = props.accountId;
 
-return <>
-  <Widget src={`${ownerId}/widget/Inputs.File`} props={{
-    label: "Pitch deck",
-    id: "pitch-deck",
-    fileAccept: [".pdf"],
-    value: "",
-    onSave: (v) => { console.log(v); },
-    canEdit: true,
-  }} />
-</>;
+State.init({
+  project: null,
+  projectIsFetched: false,
+});
+
+if (!state.projectIsFetched) {
+  Near.asyncView(
+    ownerId,
+    "get_project",
+    { account_id: accountId },
+    "final",
+    false
+  ).then((project) => State.update({ project, projectIsFetched: true }));
+  return <>Loading...</>;
+}
+
+const onSave = (project) => {
+  Near.call(
+    ownerId,
+    "edit_project",
+    {
+      account_id: accountId,
+      project: {
+        ...state.project,
+        ...project,
+        application: {
+          ...state.project.application,
+          ...project.application,
+          graduation: {
+            ...state.project.application.graduation,
+            ...project.application.graduation,
+          },
+          private: {
+            ...state.project.application.private,
+            ...project.application.private,
+            graduation: {
+              ...state.project.application.private.graduation,
+              ...project.application.private.graduation,
+            }
+          },
+        },
+      },
+    }
+  )
+}
+
+return (
+  <>
+    <Widget
+      src={`${ownerId}/widget/Inputs.File`}
+      props={{
+        label: "Pitch deck",
+        id: "pitch-deck",
+        fileAccept: [".pdf"],
+        value: "",
+        onSave: (pitch_deck) => onSave({ application: { graduation: { pitch_deck } } }),
+        canEdit,
+      }}
+    />
+  </>
+);
