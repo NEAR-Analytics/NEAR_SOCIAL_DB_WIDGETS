94b5173632c35b0503fe8509476ab73d767a8352
NearSocial <NearSocial>
2023-04-26 00:15:27 -0400

    Update DAO.JoinButton by hack.near at 2023-04-26 00:15:27.884

--- hack.near/DAO.JoinButton/source_code.jsx
+++ hack.near/DAO.JoinButton/source_code.jsx
@@ -1,6 +1,6 @@
 const accountId = context.accountId;
-const daoId = props.daoId ?? "onboarddao.sputnik-dao.near";
-const role = props.role ?? "council";
+const daoId = props.daoId ?? "multi.sputnik-dao.near";
+const role = props.role ?? "community";
 
 const handleProposal = () => {
   const gas = 200000000000000;
