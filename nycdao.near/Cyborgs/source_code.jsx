61b9b49bc5010c4815a8e7ed910571e6924d265f
nycdao.near <nycdao.near>
2023-04-25 21:45:13 -0400

    Update Cyborgs by nycdao.near at 2023-04-25 21:45:13.111

---
+++ nycdao.near/Cyborgs/source_code.jsx
@@ -0,0 +1,64 @@
+return (
+  <div>
+    <h1>🌐 Cybernetic Organizations</h1>
+
+    <div className="mb-2">
+      <Widget
+        src="onboarder.near/widget/SputnikDAOProfileSearchStringMatch"
+        props={{
+          limit: 20,
+          onChange: ({ result }) => State.update({ profiles: result }),
+        }}
+      />
+    </div>
+    <br />
+
+    <a
+      className="btn btn-outline-primary"
+      href="https://near.social/#/onboarder.near/widget/FunctionCallProposal"
+    >
+      Submit Proposal to <b>Create a Profile</b> for a Decentralized Autonomous
+      Organization
+    </a>
+    <br />
+
+    <hr />
+
+    {state.profiles && state.profiles.length > 0 && (
+      <div className="mb-2">
+        {state.profiles.map(({ accountId }, i) => (
+          <div
+            key={i}
+            className="d-flex justify-content-between align-items-center mb-3"
+          >
+            <div className="me-2 text-truncate">
+              <a
+                href={`#/mob.near/widget/ProfilePage?accountId=${accountId}`}
+                className="text-decoration-none link-dark text-truncate"
+              >
+                <Widget
+                  src="mob.near/widget/Profile.InlineBlock"
+                  props={{ accountId }}
+                />
+              </a>
+            </div>
+            <div className="d-none text-nowrap d-md-block">
+              <div className="m-1">
+                <Widget
+                  src="mob.near/widget/FollowButton"
+                  props={{ accountId }}
+                />
+              </div>
+              <div className="m-1">
+                <Widget
+                  src="hack.near/widget/DAO.JoinButton"
+                  props={{ daoId: accountId }}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )}
+  </div>
+);
