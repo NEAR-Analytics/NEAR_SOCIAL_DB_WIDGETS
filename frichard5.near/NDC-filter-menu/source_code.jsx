a1adf8d224d0d0f56e7cd3889af7f10f9b085583
frichard5.near <frichard5.near>
2023-04-26 13:04:35 -0400

    Update NDC-filter-menu by frichard5.near at 2023-04-26 13:04:35.621

--- frichard5.near/NDC-filter-menu/source_code.jsx
+++ frichard5.near/NDC-filter-menu/source_code.jsx
@@ -1,7 +1,22 @@
 const { widgetProvider, comps } = props;
 
+State.init({
+  isOpen: false,
+});
+
 const FilterMenu = styled.div`
-  display: flex;
+    display: flex;
+    position: absolute;
+    z-index: 1;
+    background: white;
+    width: 100%;
+    padding: 20px;
+    box-shadow: 3px 2px 24px rgba(68, 152, 224, 0.3);
 `;
 
-return <FilterMenu>{comps.map((c) => c)}</FilterMenu>;
+return (
+  <>
+    <button onClick={State.update({ isOpen: !state.isOpen })}>open</button>
+    {state.isOpen && <FilterMenu>{comps.map((c) => c)}</FilterMenu>}
+  </>
+);
