a60ec3071bd8a6dec3c236416ee3d79c4d526f19
billblystone.near <billblystone.near>
2023-04-26 14:35:56 -0400

    Update everything by billblystone.near at 2023-04-26 14:35:56.244

---
+++ billblystone.near/everything/source_code.jsx
@@ -0,0 +1,1 @@
+return (<Widget src="evrything.near/widget/Everything.Template" props={{ accountId: "billblystone.near", font: "Times New Roman", type: "everything", text: "billblystone.near", domain: "everything" }} />);
