cf00ec7cf5a10963dd3c0eed553804f79c082acf
nearhorizon.near <nearhorizon.near>
2023-04-27 09:37:52 -0400

    Update TNCPage by nearhorizon.near at 2023-04-27 09:37:52.086

--- nearhorizon.near/TNCPage/source_code.jsx
+++ nearhorizon.near/TNCPage/source_code.jsx
@@ -7,7 +7,7 @@ if (!state.codeIsFetched) {
   asyncFetch(
     "https://ipfs.io/ipfs/QmccEhWD248HDqi6yZ4Ay8a8VHvAnCBdXPN1TxbNmj4cKZ?filename=NEARHorizonTCsv1.1Final.html"
   ).then((res) => {
-    console.log(res);
+    State.set({ code: res.body, codeIsFetched: true });
   });
   return <>Loading...</>;
 }
