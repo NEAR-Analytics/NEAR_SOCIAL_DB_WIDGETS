94328279da7b1d8c7b3eee43fb95091691f783c6
frichard5.near <frichard5.near>
2023-04-27 17:26:47 -0400

    Update NDC-transfers-overview by frichard5.near at 2023-04-27 17:26:47.347

---
+++ frichard5.near/NDC-transfers-overview/source_code.jsx
@@ -0,0 +1,12 @@
+const widgetProvider = props.widgetProvider;
+const account = props.account || "marketing.sputnik-dao.near";
+const widgetUrl = `https://api.pikespeak.ai/widgets/transfers/${account}`;
+
+return (
+  <>
+    <iframe
+      style={{ width: "100%", height: "440px", marginTop: "40px" }}
+      src={widgetUrl}
+    ></iframe>
+  </>
+);
