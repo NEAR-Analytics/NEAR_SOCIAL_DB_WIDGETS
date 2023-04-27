999cf04c8ba79ce2427f896ab9d2c263440eb09f
juaner.near <juaner.near>
2023-04-27 03:47:42 -0400

    Update homePage-config by juaner.near at 2023-04-27 03:47:42.895

--- juaner.near/homePage-config/source_code.jsx
+++ juaner.near/homePage-config/source_code.jsx
@@ -1,3 +1,1 @@
-const storageCustomHomePage = Storage.privateGet("myHomePagePath");
-console.log("其它组件使用尝试-storageCustomHomePage", storageCustomHomePage);
 return <Widget src="juaner.near/widget/ref-home" />;
