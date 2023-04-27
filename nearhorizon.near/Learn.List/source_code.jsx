0df2640a5691d01fa11db1d7785b479f99bd4f7d
nearhorizon.near <nearhorizon.near>
2023-04-27 08:05:53 -0400

    Update Learn.List by nearhorizon.near at 2023-04-27 08:05:53.518

---
+++ nearhorizon.near/Learn.List/source_code.jsx
@@ -0,0 +1,35 @@
+const ownerId = "nearhorizon.near";
+const cardData = props.cardData ?? [];
+
+const List = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: stretch;
+  flex-wrap: wrap;
+  gap: 1em;
+
+  > div {
+    width: 100%;
+
+    @media (min-width: 768px) {
+      width: 48%;
+    }
+
+    @media (min-width: 1024px) {
+      width: 30%;
+    }
+  }
+`;
+
+return (
+  <List>
+    {cardData.map((cardProps) => (
+      <div>
+        <Widget src={`${ownerId}/widget/Learn.Card`} props={cardProps} />
+      </div>
+    )}
+  </List>
+);
+
+
