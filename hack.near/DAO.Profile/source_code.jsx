d591022eb30e085a74f1fff652d9479b1cbe19c4
NearSocial <NearSocial>
2023-04-26 20:03:06 -0400

    Update DAO.Profile by hack.near at 2023-04-26 20:03:06.520

--- hack.near/DAO.Profile/source_code.jsx
+++ hack.near/DAO.Profile/source_code.jsx
@@ -1,6 +1,4 @@
-State.init({
-  daoId: props.daoId,
-});
+const daoId = props.daoId;
 
 const onChangeContract = (daoId) => {
   State.update({
@@ -9,33 +7,7 @@ const onChangeContract = (daoId) => {
 };
 
 if (!daoId) {
-  return (
-    <>
-      <div className="mt-3">
-        <Widget src="hack.near/widget/Cyborgs" />
-      </div>
-      <div className="mt-3">
-        <input
-          type="text"
-          placeholder="example.sputnik-dao.near"
-          onChange={(e) => onChangeContract(e.target.value)}
-        />
-      </div>
-      <div className="d-flex flex-row bd-highlight mb-3">
-        <div className="m-2">
-          <a
-            className="btn btn-primary mt-3"
-            href={`/#/hack.near/widget/DAO.Profile?daoId=${state.daoId}`}
-          >
-            View Any Profile
-          </a>
-        </div>
-        <div className="m-2">
-          <Widget src="hack.near/widget/CreateDAO" />
-        </div>
-      </div>
-    </>
-  );
+  return <Widget src="hack.near/widget/Onboard" />;
 }
 
 const profile = props.profile ?? Social.getr(`${daoId}/profile`);
@@ -44,24 +16,22 @@ if (profile === null) {
   return "Loading...";
 }
 
-if (daoId !== null) {
-  return (
-    <div className="py-1 px-1">
-      <div className="mx-auto">
-        <Widget
-          src="hack.near/widget/DAO.ProfileLarge"
-          props={{
-            daoId,
-            profile,
-            link: true,
-            showEditButton: !props.profile,
-          }}
-        />
+return (
+  <div className="py-1 px-1">
+    <div className="mx-auto">
+      <Widget
+        src="hack.near/widget/DAO.ProfileLarge"
+        props={{
+          daoId,
+          profile,
+          link: true,
+          showEditButton: !props.profile,
+        }}
+      />
 
-        <div className="mt-3">
-          <Widget src="hack.near/widget/DAO.Tabs" props={{ daoId, profile }} />
-        </div>
+      <div className="mt-3">
+        <Widget src="hack.near/widget/DAO.Tabs" props={{ daoId, profile }} />
       </div>
     </div>
-  );
-}
+  </div>
+);
