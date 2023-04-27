6fd5d784bff710577768457c47ee887d56c778e4
frichard5.near <frichard5.near>
2023-04-26 20:47:05 -0400

    Update NDC-filter-menu by frichard5.near at 2023-04-26 20:47:05.964

--- frichard5.near/NDC-filter-menu/source_code.jsx
+++ frichard5.near/NDC-filter-menu/source_code.jsx
@@ -31,7 +31,7 @@ const FilterMenu = styled.div`
     position: absolute;
     z-index: 1;
     background: white;
-    width: 100%;
+    width: fit-content;
     padding: 20px;
     box-shadow: 3px 2px 24px rgba(68, 152, 224, 0.3);
     border-radius: 4px;
