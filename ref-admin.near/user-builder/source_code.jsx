6c80d3ac2985cd9bc1bd740b734f6cb97638e320
ref-admin.near <ref-admin.near>
2023-04-28 14:40:42 -0400

    Update user-builder by ref-admin.near at 2023-04-28 14:40:42.894

---
+++ ref-admin.near/user-builder/source_code.jsx
@@ -0,0 +1,51 @@
+const Container = styled.div`
+    display:inline-flex;
+    align-items:center;
+    background: #0D1C1F;
+    border-radius: 10px;
+    padding:4px;
+    .default{
+        display:flex;
+        align-items:center;
+        border-radius: 6px;
+        font-weight: 500;
+        font-size: 14px;
+        color:#fff;
+        height:28px;
+        padding:0 25px;
+        cursor:pointer;
+    }
+    .active{
+        background: #0092FF;
+    }
+`;
+const { buttonStatus } = state;
+State.init({
+  buttonStatus: "user",
+});
+function switchButton(type) {
+  Storage.set("ref-mode", type);
+  State.update({
+    buttonStatus: type,
+  });
+}
+return (
+  <Container>
+    <span
+      class={`default ${buttonStatus == "user" ? "active" : ""}`}
+      onClick={() => {
+        switchButton("user");
+      }}
+    >
+      User
+    </span>
+    <span
+      class={`default ${buttonStatus == "builder" ? "active" : ""}`}
+      onClick={() => {
+        switchButton("builder");
+      }}
+    >
+      Builder
+    </span>
+  </Container>
+);
