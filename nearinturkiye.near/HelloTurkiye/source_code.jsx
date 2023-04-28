509846d8c8f49e314f66b86f034a961e4774f315
nearinturkiye.near <nearinturkiye.near>
2023-04-28 14:31:35 -0400

    Update HelloTurkiye by nearinturkiye.near at 2023-04-28 14:31:35.304

--- nearinturkiye.near/HelloTurkiye/source_code.jsx
+++ nearinturkiye.near/HelloTurkiye/source_code.jsx
@@ -1,7 +1,8 @@
+let greeting = "Bizi Near Social'da takip etmeyi unutma :)";
 return (
   <>
     <div class="container border border-info p-3 text-center">
-      <h1>Merhaba Türkiye{props.name}</h1>
+      <h1>NEAR Türkiye'ye Hoşgeldin {props.name}</h1>
 
       <p> {greeting} </p>
     </div>
