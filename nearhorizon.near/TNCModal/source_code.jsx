4392e41d22e1c2da2a9bd5b6565e4f233404e38b
nearhorizon.near <nearhorizon.near>
2023-04-28 12:41:37 -0400

    Update TNCModal by nearhorizon.near at 2023-04-28 12:41:37.128

---
+++ nearhorizon.near/TNCModal/source_code.jsx
@@ -0,0 +1,10 @@
+return (
+  <Dialog.Root>
+    <Dialog.Overlay />
+    <Dialog.Content>
+      <Dialog.Title></Dialog.Title>
+      <Dialog.Description></Dialog.Description>
+      <Dialog.Close />
+    </Dialog.Content>
+  </Dialog.Root>
+);
