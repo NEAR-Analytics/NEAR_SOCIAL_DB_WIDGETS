52362dfc17b94f444f118c4d7690b342502fbaed
frichard5.near <frichard5.near>
2023-04-28 15:32:17 -0400

    Update NDC-transfers-by-account by frichard5.near at 2023-04-28 15:32:17.634

--- frichard5.near/NDC-transfers-by-account/source_code.jsx
+++ frichard5.near/NDC-transfers-by-account/source_code.jsx
@@ -165,6 +165,7 @@ const ProposalCard = (
   <Widget
     src={`${widgetProvider}/widget/NDC-proposal-card`}
     props={{
+      widgetProvider,
       proposal: state.proposal,
       council: state.council,
     }}
