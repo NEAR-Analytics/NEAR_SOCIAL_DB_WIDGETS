05a525389cddff0bac262b016596fd2e84a36c01
nearhorizon.near <nearhorizon.near>
2023-04-28 13:35:56 -0400

    Update TNCModal by nearhorizon.near at 2023-04-28 13:35:56.895

--- nearhorizon.near/TNCModal/source_code.jsx
+++ nearhorizon.near/TNCModal/source_code.jsx
@@ -25,7 +25,7 @@ State.init({
 });
 
 return (
-  <Dialog.Root>
+  <Dialog.Root open={props.open} onOpenChange={(open) => props.update(open)}>
     <Dialog.Overlay />
     <Dialog.Content>
       <Dialog.Title>Terms and Conditions</Dialog.Title>
