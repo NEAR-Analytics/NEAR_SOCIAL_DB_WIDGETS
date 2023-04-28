bf0ebba57552c908c42ff071e0bd5b72fcfc6a06
nearhorizon.near <nearhorizon.near>
2023-04-28 12:44:11 -0400

    Update TNCModal by nearhorizon.near at 2023-04-28 12:44:11.091

--- nearhorizon.near/TNCModal/source_code.jsx
+++ nearhorizon.near/TNCModal/source_code.jsx
@@ -1,9 +1,31 @@
+State.init({
+  agree: false,
+});
+
 return (
   <Dialog.Root>
     <Dialog.Overlay />
     <Dialog.Content>
       <Dialog.Title>Terms and Conditions</Dialog.Title>
-      <Dialog.Description></Dialog.Description>
+      <Dialog.Description>
+        <Widget
+          src={`${ownerId}/widget/Inputs.Checkbox`}
+          props={{
+            label: (
+              <>
+                By checking this box you acknowledge that you understand and
+                agree with{" "}
+                <a href={`${ownerId}/widget/TNCPage`}>
+                  NEAR Horizon Terms and Conditions
+                </a>
+              </>
+            ),
+            value: state.agree,
+            id: "agree",
+            onChange: (agree) => State.update({ agree }),
+          }}
+        />
+      </Dialog.Description>
       <Dialog.Close />
     </Dialog.Content>
   </Dialog.Root>
