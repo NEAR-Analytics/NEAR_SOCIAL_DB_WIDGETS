e4628f486362588ae53a7e60d48969c43140164b
ref-admin.near <ref-admin.near>
2023-04-28 16:47:38 +0000

    Update wrap-code by ref-admin.near at 2023-04-28T16:47:38.336Z

--- ref-admin.near/wrap-code/source_code.jsx
+++ ref-admin.near/wrap-code/source_code.jsx
@@ -1,2 +1,14 @@
 const { code } = props;
-return <div>{code}</div>;
+const current_mode = Storage.get(
+  "ref-mode",
+  "ref-admin.near/widget/user-builder"
+);
+return (
+  <div
+    style={{
+      display: current_mode == "builder" ? "block" : "none",
+    }}
+  >
+    {code}
+  </div>
+);
