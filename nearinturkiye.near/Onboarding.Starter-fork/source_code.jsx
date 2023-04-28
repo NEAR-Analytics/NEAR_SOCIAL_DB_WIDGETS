cac04714efc918352e70900c351d35f58503cb96
nearinturkiye.near <nearinturkiye.near>
2023-04-28 14:10:46 -0400

    Update Onboarding.Starter-fork by nearinturkiye.near at 2023-04-28 14:10:46.134

---
+++ nearinturkiye.near/Onboarding.Starter-fork/source_code.jsx
@@ -0,0 +1,7 @@
+return (
+  <>
+    {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
+    <Widget src="near/widget/Onboarding.ComponentCard" props={{ accountId }} />
+  </>
+);
+<Widget src="near/widget/Onboarding.ComponentCollection" props={{}} />;
