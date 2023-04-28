e04161ff8926ce29d9d3e946e6250aec11ba3f6f
nearinturkiye.near <nearinturkiye.near>
2023-04-28 14:31:25 -0400

    Update HelloComposition by nearinturkiye.near at 2023-04-28 14:31:25.426

---
+++ nearinturkiye.near/HelloComposition/source_code.jsx
@@ -0,0 +1,11 @@
+const user = "nearinturkiye.near";
+
+return (
+  <>
+    <h3> Composition </h3>
+    <p> Components can be composed </p>
+    <hr />
+
+    <Widget src={`${user}/widget/HelloTurkiye`} props={props} />
+  </>
+);
