94139eac12233ec9d3c33b758ea5feab9ee18dd5
frichard5.near <frichard5.near>
2023-04-28 15:39:31 -0400

    Update NDC-modal by frichard5.near at 2023-04-28 15:39:31.862

--- frichard5.near/NDC-modal/source_code.jsx
+++ frichard5.near/NDC-modal/source_code.jsx
@@ -7,8 +7,9 @@ State.init({
 if (state.isOpen != isOpen) State.update({ isOpen });
 
 const Modal = styled.div`
-    position: absolute;
+    position: fixed;
     z-index:101;
+    top: 0px;
     width: 100%;
     height: 100vh;
     display: ${state.isOpen ? "block" : "none"};
