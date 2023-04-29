6b3164b09e5e5cf97dc7276dc3449d56a79d46bb
ref-admin.near <ref-admin.near>
2023-04-29 17:37:17 -0400

    Update xBox-detail-page by ref-admin.near at 2023-04-29 17:37:17.315

---
+++ ref-admin.near/xBox-detail-page/source_code.jsx
@@ -0,0 +1,20 @@
+const Wrapper = styled.div`
+`;
+const SummaryWrapper = styled.div`
+  margin-bottom: 32px;
+`;
+return (
+  <Wrapper>
+    <SummaryWrapper>
+      <Widget
+        src="ref-admin.near/widget/ComponentSummary"
+        props={{
+          primaryAction: "open",
+          size: "large",
+          showTags: false,
+          src: "ref-admin.near/widget/xBox",
+        }}
+      />
+    </SummaryWrapper>
+  </Wrapper>
+);
