f9f32c19b4d9a0695ce847002a3af970acd82628
ref-admin.near <ref-admin.near>
2023-04-30 06:55:23 -0400

    Update xBox by ref-admin.near at 2023-04-30 06:55:23.534

--- ref-admin.near/xBox/source_code.jsx
+++ ref-admin.near/xBox/source_code.jsx
@@ -1,7 +1,12 @@
 const Wrap = styled.div`
+  display:flex;
+  justify-content:center;
+  gap:24px;
+  margin-top:20px;
 `;
 return (
   <Wrap>
     <Widget src="ref-admin.near/widget/LiNEAR"></Widget>
+    <Widget src="ref-admin.near/widget/XREF"></Widget>
   </Wrap>
 );
