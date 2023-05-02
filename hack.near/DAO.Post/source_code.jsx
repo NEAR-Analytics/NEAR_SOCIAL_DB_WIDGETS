32fe8cf0ceffe56a05c20c8d6f4efa73c51d41fb
NearSocial <NearSocial>
2023-05-02 16:05:23 -0400

    Update DAO.Post by hack.near at 2023-05-02 16:05:23.489

--- hack.near/DAO.Post/source_code.jsx
+++ hack.near/DAO.Post/source_code.jsx
@@ -1,140 +1,20 @@
 const accountId = props.accountId;
-const contract_name =
-  props.daoId ?? "nearweek-news-contribution.sputnik-dao.near";
-
 const blockHeight =
   props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
 const content =
   props.content ??
-  JSON.parse(Social.get(`${accountId}/post/main`, blockHeight) ?? "null");
+  JSON.parse(Social.get(`${accountId}/post/dao`, blockHeight) ?? "null");
 const subscribe = !!props.subscribe;
 const raw = !!props.raw;
 
 const notifyAccountId = accountId;
-
-const link = `#/mob.near/widget/MainPage.Post.Page?accountId=${accountId}&blockHeight=${blockHeight}`;
-
-const convertAmount = (amount, decimals) => {
-  const [whole, fractional] = amount.toString().split(".");
-  const wholePart = new BN(whole).mul(new BN("10").pow(new BN(decimals)));
-  if (fractional === undefined) {
-    return wholePart.toString();
-  }
-  const fractionalPart = new BN(fractional).mul(
-    new BN("10").pow(new BN(decimals - fractional.length))
-  );
-  return wholePart.add(fractionalPart).toString();
-};
-
-State.init({
-  recipient: accountId,
-  amount: 0.5,
-  deposit: 0.1,
-});
-
-const onChangeAmount = (amount) => {
-  State.update({
-    amount,
-  });
-};
-
-const onChangeRecipient = (recipient) => {
-  State.update({
-    recipient,
-  });
-};
-
-const onChangeDescription = (description) => {
-  State.update({
-    description,
-  });
-};
-
-const handleProposal = () => {
-  Near.call([
-    {
-      contractName: contract_name,
-      methodName: "add_proposal",
-      args: {
-        proposal: {
-          description: content.text,
-          kind: {
-            Transfer: {
-              token_id: "",
-              receiver_id: state.recipient.trim().toLowerCase(),
-              amount: convertAmount(state.amount.toString(), 24) ?? 0,
-            },
-          },
-        },
-      },
-      gas: state.gas ?? 200000000000000,
-      deposit: 100000000000000000000000,
-    },
-  ]);
-};
-
 const item = {
   type: "social",
-  path: `${accountId}/post/main`,
+  path: `${accountId}/post/dao`,
   blockHeight,
 };
 
-const reposts = [
-  {
-    key: "main",
-    value: {
-      type: "repost",
-      item,
-    },
-  },
-  {
-    key: item,
-    value: {
-      type: "repost",
-    },
-  },
-];
-
-const social_args = JSON.stringify({
-  data: {
-    [contract_name]: {
-      index: {
-        repost: JSON.stringify(reposts),
-      },
-    },
-  },
-});
-
-const proposal_args = Buffer.from(social_args, "utf-8").toString("base64");
-
-const handleBoostProposal = () => {
-  Near.call([
-    {
-      contractName: contract_name,
-      methodName: "add_proposal",
-      args: {
-        proposal: {
-          description: content.text,
-          kind: {
-            FunctionCall: {
-              receiver_id: "social.near",
-              actions: [
-                {
-                  method_name: "set",
-                  args: proposal_args,
-                  deposit: "100000000000000000000000",
-                  gas: "200000000000000",
-                },
-              ],
-            },
-          },
-        },
-      },
-      deposit: "100000000000000000000000",
-      gas: "200000000000000",
-    },
-  ]);
-};
+const link = `#/hack.near/widget/DAO.Post?accountId=${accountId}&blockHeight=${blockHeight}`;
 
 return (
   <div className="border-bottom pt-3 pb-1">
@@ -159,7 +39,7 @@ return (
             }}
           />
         </div>
-        <div className="me-3">
+        <div className="me-4">
           <Widget
             src="mob.near/widget/RepostButton"
             props={{
@@ -168,7 +48,7 @@ return (
             }}
           />
         </div>
-        <div className="me-3">
+        <div className="me-4">
           <Widget
             src="mob.near/widget/LikeButton"
             props={{
@@ -185,14 +65,6 @@ return (
         </div>
       </div>
     )}
-    <div className="me-3">
-      <button className="btn btn-primary m-2" onClick={handleProposal}>
-        Submit to DAO
-      </button>
-      <button className="btn btn-success m-2" onClick={handleBoostProposal}>
-        Request a Boost
-      </button>
-    </div>
     <div className="mt-3 ps-5">
       {state.showReply && (
         <div className="mb-2">
