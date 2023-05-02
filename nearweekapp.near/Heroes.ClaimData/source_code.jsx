7dad6f62de2feb8e399f21173d798a59c735c426
nearweek <nearweek>
2023-05-02 09:56:20 -0400

    Update Heroes.ClaimData by nearweekapp.near at 2023-05-02 09:56:20.238

--- nearweekapp.near/Heroes.ClaimData/source_code.jsx
+++ nearweekapp.near/Heroes.ClaimData/source_code.jsx
@@ -1,10 +1,10 @@
 const tokenMap = {
   "a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near": {
-    symbol: "USDT.e",
+    symbol: "USDC.e",
     decimals: 6,
   },
   "dac17f958d2ee523a2206206994597c13d831ec7.factory.bridge.near": {
-    symbol: "USDC.e",
+    symbol: "USDT.e",
     decimals: 6,
   },
   "6b175474e89094c44da98b954eedeac495271d0f.factory.bridge.near": {
