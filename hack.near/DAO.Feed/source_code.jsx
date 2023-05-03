e1a1bbba0b9bbfedb3ad7cf69820899fc8951dec
NearSocial <NearSocial>
2023-05-02 23:47:32 -0400

    Update DAO.Feed by hack.near at 2023-05-02 23:47:32.801

--- hack.near/DAO.Feed/source_code.jsx
+++ hack.near/DAO.Feed/source_code.jsx
@@ -1,4 +1,4 @@
-const daoId = props.daoId ?? props.accountId ?? context.accountId;
+const accountId = props.daoId ?? props.accountId ?? context.accountId;
 
 const feed = accountId
   ? Social.get(`${accountId}/settings/dao/feed`)
@@ -8,4 +8,4 @@ if (feed === null) {
   return "Loading...";
 }
 
-return <Widget src={feed ?? "hack.near/widget/DAO.Social"} props={daoId} />;
+return <Widget src={feed ?? "hack.near/widget/DAO.Social"} props={props} />;
