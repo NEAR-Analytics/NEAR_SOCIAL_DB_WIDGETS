39e8169b35323e775d3b681dc9f582e7e691bbeb
nearhorizon.near <nearhorizon.near>
2023-05-03 15:05:30 -0400

    Update Inputs.SetUpPermissions by nearhorizon.near at 2023-05-03 15:05:30.679

--- nearhorizon.near/Inputs.SetUpPermissions/source_code.jsx
+++ nearhorizon.near/Inputs.SetUpPermissions/source_code.jsx
@@ -22,9 +22,10 @@ if (!state.followingIsFetched) {
   ).then((data) => {
     console.log(data);
     State.update({
-      following: Object.keys(data[context.accountId]?.graph?.follow ?? {}).map(
-        (name) => ({ name })
-      ),
+      following: (Objec.keys(data).length > 0
+        ? Object.keys(data[context.accountId]?.graph?.follow ?? {})
+        : []
+      ).map((name) => ({ name })),
       followingIsFetched: true,
     });
   });
