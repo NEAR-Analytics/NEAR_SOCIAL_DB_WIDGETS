77d9cf297310a183820cf250cd8c0f757cc016f5
NearSocial <NearSocial>
2023-05-03 04:03:13 -0400

    Update DAO.View by hack.near at 2023-05-03 04:03:13.565

--- hack.near/DAO.View/source_code.jsx
+++ hack.near/DAO.View/source_code.jsx
@@ -1,7 +1,19 @@
 const daoId = props.daoId ?? "multi.sputnik-dao.near";
 
+const defaultId = 1;
+
+State.init({
+  proposal_id: state.proposal_id ?? defaultId,
+});
+
+const onChangeProposal = (proposal_id) => {
+  State.update({
+    proposal_id,
+  });
+};
+
 const proposal = Near.view(daoId, "get_proposal", {
-  id: 15,
+  id: state.proposal_id,
 });
 
 const handleApprove = () => {
@@ -10,7 +22,7 @@ const handleApprove = () => {
       contractName: daoId,
       methodName: "act_proposal",
       args: {
-        id: 1,
+        id: state.proposal_id,
         action: "VoteApprove",
       },
       gas: 200000000000000,
@@ -18,9 +30,34 @@ const handleApprove = () => {
   ]);
 };
 
+const handleReject = () => {
+  Near.call([
+    {
+      contractName: daoId,
+      methodName: "act_proposal",
+      args: {
+        id: state.proposal_id,
+        action: "VoteReject",
+      },
+      gas: 200000000000000,
+    },
+  ]);
+};
+
+console.log(proposal_id);
+console.log(state.proposal_id);
+
 return (
   <>
-    <h3>Proposal #{proposal.id}</h3>
+    <input
+      type="number"
+      placeholder="Input Proposal ID Here"
+      value={state.proposal_id}
+      onChange={(e) => onChangeProposal(e.target.value)}
+    />
+    <hr />
+    <p>Proposal #{state.proposal_id}</p>
+
     <h5>by {proposal.proposer}</h5>
     <p>{proposal.description}</p>
     <div>
