9f63d26d4046f9f6f3cb1d24804eb93c7f93b943
frichard5.near <frichard5.near>
2023-05-03 10:20:15 -0400

    Update NDC-search by frichard5.near at 2023-05-03 10:20:15.127

--- frichard5.near/NDC-search/source_code.jsx
+++ frichard5.near/NDC-search/source_code.jsx
@@ -1,1 +1,33 @@
+const widgetProvider = props.widgetProvider;
+
+State.init({
+  input: "",
+});
+
+const Input = (
+  <Widget
+    src={`${widgetProvider}/widget/NDC-input`}
+    props={{
+      buttonLabel: "Search",
+      placeholder: "Enter an account address or a proposal id",
+    }}
+  />
+);
+
+const fetchProposal = (id) => {
+  const proposal = fetch(apiProposalUrl + `?id=${id}`, {
+    mode: "cors",
+    headers: {
+      "x-api-key": publicApiKey,
+    },
+  });
+  proposal.body &&
+    State.update({
+      proposal: proposal.body.length ? proposal.body[0] : [],
+      fetchingProposal: false,
+    });
+};
+
+fetchProposal(state.proposalId);
+
 return <div>Hello World</div>;
