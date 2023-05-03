88d7a64f75602426a94a42b453c7977ba9d5a135
NearSocial <NearSocial>
2023-05-03 02:58:49 -0400

    Update DAO.View by hack.near at 2023-05-03 02:58:49.180

--- hack.near/DAO.View/source_code.jsx
+++ hack.near/DAO.View/source_code.jsx
@@ -1,18 +1,9 @@
 const daoId = props.daoId ?? "multi.sputnik-dao.near";
 
-const proposals = Near.view(daoId, "get_proposals", {
-  from_index: 0,
-  limit: 5,
+const proposal = Near.view(daoId, "get_proposal", {
+  id: 0,
 });
-
-console.log(proposals);
-
-const proposal = proposals.id[0];
 
 console.log(proposal);
 
-return (
-  <div>
-    <h3>{proposal}</h3>
-  </div>
-);
+return <h3>{proposal}</h3>;
