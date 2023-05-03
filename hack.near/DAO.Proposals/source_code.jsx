21cb0616e6557f2988b7efc4eb52d453e8bd92c5
NearSocial <NearSocial>
2023-05-03 05:16:02 -0400

    Update DAO.Proposals by hack.near at 2023-05-03 05:16:02.713

--- hack.near/DAO.Proposals/source_code.jsx
+++ hack.near/DAO.Proposals/source_code.jsx
@@ -1,219 +1,36 @@
-const widgetProvider = "frichard5.near";
-const refUrl = "https://api.stats.ref.finance/api/ft";
+const daoId = props.daoId ?? "multi.sputnik-dao.near";
 
-// Initial state
 State.init({
-  selectedDao: "marketing.sputnik-dao.near",
-  selectedTab: "money",
+  daoId,
 });
 
-const WidgetsContainer = styled.div`
-    
-`;
+const proposals = Near.view(daoId, "get_proposals", {
+  from_index: 0,
+  limit: 10,
+});
 
-// Fetch
-const ftList = fetch(refUrl);
+console.log(proposals);
 
-// Components
-
-const Balances = (
-  <Widget
-    src={`${widgetProvider}/widget/account_balance`}
-    props={{
-      account: state.selectedDao,
-      ftList: ftList.body && ftList.body,
-      widgetProvider,
-    }}
-  />
-);
-
-const NearTransfers = (
-  <Widget
-    src={`${widgetProvider}/widget/near_transfers`}
-    props={{
-      account: state.selectedDao,
-      widgetProvider,
-    }}
-  />
-);
-
-const FTransfers = (
-  <Widget
-    src={`${widgetProvider}/widget/ft_transfers`}
-    props={{
-      account: state.selectedDao,
-      ftList: ftList.body && ftList.body,
-      widgetProvider,
-    }}
-  />
-);
-
-const ContractMetrics = (
-  <Widget
-    src={`${widgetProvider}/widget/contract_metrics`}
-    props={{
-      account: state.selectedDao,
-      widgetProvider,
-    }}
-  />
-);
-
-const ProposalStatus = (
-  <Widget
-    src={`${widgetProvider}/widget/proposals-status`}
-    props={{
-      account: state.selectedDao,
-      widgetProvider,
-    }}
-  />
-);
-
-const ProposalsByMonth = (
-  <Widget
-    src={`${widgetProvider}/widget/proposal-by-month`}
-    props={{
-      account: state.selectedDao,
-      widgetProvider,
-    }}
-  />
-);
-
-const Proposals = (
-  <Widget
-    src={`${widgetProvider}/widget/proposals`}
-    props={{
-      account: state.selectedDao,
-      widgetProvider,
-    }}
-  />
-);
-
-const Policy = (
-  <Widget
-    src={`${widgetProvider}/widget/NDC-policy`}
-    props={{
-      account: state.selectedDao,
-      widgetProvider,
-    }}
-  />
-);
-
-const VotersByProposal = (
-  <Widget
-    src={`${widgetProvider}/widget/NDC-members-voters`}
-    props={{
-      account: state.selectedDao,
-      widgetProvider,
-    }}
-  />
-);
-
-const VoteHistory = (
-  <Widget
-    src={`${widgetProvider}/widget/NDC-vote-history`}
-    props={{
-      account: state.selectedDao,
-      widgetProvider,
-    }}
-  />
-);
-
-const Tabs = (
-  <Widget
-    src={`${widgetProvider}/widget/NDC-Tabs`}
-    props={{
-      tabs: [
-        {
-          value: "overview",
-          label: "Overview",
-          selected: true,
-          //selected: state.selectedTab === "metrics",
-          components: (
-            <WidgetsContainer>
-              {ProposalsByMonth}
-              {ProposalStatus}
-            </WidgetsContainer>
-          ),
-        },
-        {
-          value: "proposals",
-          label: "Proposals",
-          components: <WidgetsContainer>{Proposals}</WidgetsContainer>,
-        },
-        {
-          value: "treasury",
-          label: "Treasury",
-          components: (
-            <WidgetsContainer>
-              {Balances}
-              {NearTransfers}
-              {FTransfers}
-            </WidgetsContainer>
-          ),
-        },
-        {
-          value: "members",
-          label: "Members",
-          //selected: state.selectedTab === "members",
-          components: (
-            <WidgetsContainer>
-              {VoteHistory}
-              {VotersByProposal}
-            </WidgetsContainer>
-          ),
-        },
-        {
-          value: "policy",
-          label: "Policy",
-          components: <WidgetsContainer>{Policy}</WidgetsContainer>,
-        },
-      ],
-      widgetProvider,
-    }}
-  />
-);
-
-const selectDao = ({ target: { value } }) => {
-  State.update({ selectedDao: value });
+const onChangeDAO = (daoId) => {
+  State.update({
+    daoId,
+  });
 };
-
-const Select = (
-  <Widget
-    src={`${widgetProvider}/widget/NDC-select`}
-    props={{
-      options: [
-        { value: "marketing.sputnik-dao.near", label: "Marketing" },
-
-        { value: "rc-dao.sputnik-dao.near", label: "Global" },
-        { value: "near-china-community.sputnik-dao.near", label: "China" },
-        { value: "nearafrica.sputnik-dao.near", label: "Africa" },
-        { value: "near-german.sputnik-dao.near", label: "Germany" },
-        { value: "near-indonesia.sputnik-dao.near", label: "Indonesia" },
-        { value: "near-italia.sputnik-dao.near", label: "Italy" },
-        { value: "nearprotocolkoreadao.sputnik-dao.near", label: "Korea" },
-        {
-          value: "open-web-academy.sputnik-dao.near",
-          label: "OWA",
-        },
-        { value: "near-native.sputnik-dao.near", label: "Russia" },
-        { value: "sankore-2.sputnik-dao.near", label: "Sankore" },
-        {
-          value: "nearprotocol-turkiye.sputnik-dao.near",
-          label: "Turkey",
-        },
-        { value: "nearuaguild.sputnik-dao.near", label: "Ukraine" },
-        { value: "near-venezuela.sputnik-dao.near", label: "Venezuela" },
-      ],
-      selectedOption: state.selectedDao,
-      onChange: selectDao,
-      top: 29,
-    }}
-  />
-);
 
 return (
   <>
-    {Select}
-    {Tabs}
+    <div>
+      <h3>DAO Proposals</h3>
+
+      <div>
+        {proposals.map((proposal, i) => (
+          <Widget
+            key={i}
+            src="hack.near/widget/DAO.View"
+            props={{ daoId, id: i }}
+          />
+        ))}
+      </div>
+    </div>
   </>
 );
