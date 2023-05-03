32ea986c0074c47581a27ae22201794398d6a239
NearSocial <NearSocial>
2023-05-03 03:19:44 -0400

    Update DAO.View by hack.near at 2023-05-03 03:19:44.467

--- hack.near/DAO.View/source_code.jsx
+++ hack.near/DAO.View/source_code.jsx
@@ -1,9 +1,36 @@
 const daoId = props.daoId ?? "multi.sputnik-dao.near";
 
 const proposal = Near.view(daoId, "get_proposal", {
-  id: 0,
+  id: 1,
 });
 
-console.log(proposal);
+const handleApprove = () => {
+  Near.call([
+    {
+      contractName: daoId,
+      methodName: "act_proposal",
+      args: {
+        id: 1,
+        action: "VoteApprove",
+      },
+      gas: 200000000000000,
+    },
+  ]);
+};
 
-return <h3>{proposal}</h3>;
+return (
+  <>
+    <h3>Proposal #{proposal.id}</h3>
+    <h5>by {proposal.proposer}</h5>
+    <p>{proposal.description}</p>
+    <div>
+      <h4>Vote Below</h4>
+      <a className="btn btn-outline-success" onClick={handleApprove}>
+        Yes
+      </a>
+      <a className="btn btn-outline-danger" onClick={handleReject}>
+        No
+      </a>
+    </div>
+  </>
+);
