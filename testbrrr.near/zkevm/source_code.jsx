b0f73a29eb213ee8a9af28195c6417dcb523efd4
testbrrr.near <testbrrr.near>
2023-05-02 21:26:27 -0400

    Update zkevm by testbrrr.near at 2023-05-02 21:26:27.346

--- testbrrr.near/zkevm/source_code.jsx
+++ testbrrr.near/zkevm/source_code.jsx
@@ -39,7 +39,7 @@ const sender = Ethers.send("eth_requestAccounts", [])[0];
 const swapProps = { forceNetwork: "Polygon zkEVM" };
 
 return (
-  <Theme style={{ background: "black" }}>
+  <Theme style={{ background: "black", color: "white" }}>
     <div style={{ textAlign: "center", marginTop: 100 }}>
       <img
         src="https://ecosystem.polygon.technology/assets/images/polygon-zkevm-logo.svg"
