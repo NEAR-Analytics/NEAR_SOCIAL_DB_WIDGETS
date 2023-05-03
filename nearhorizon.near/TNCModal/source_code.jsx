b311bc6967743912c886533955002c2ad3c28a84
nearhorizon.near <nearhorizon.near>
2023-05-03 08:19:06 -0400

    Update TNCModal by nearhorizon.near at 2023-05-03 08:19:06.982

--- nearhorizon.near/TNCModal/source_code.jsx
+++ nearhorizon.near/TNCModal/source_code.jsx
@@ -76,44 +76,7 @@ return (
       <Dialog.Content asChild>
         <Content>
           <Dialog.Title>Terms and Conditions</Dialog.Title>
-          <Dialog.Description>
-            <Widget
-              src={`${ownerId}/widget/Inputs.Checkbox`}
-              props={{
-                label: (
-                  <>
-                    By checking this box you acknowledge that you understand and
-                    agree with{" "}
-                    <a href={`${ownerId}/widget/TNCPage`}>
-                      NEAR Horizon Terms and Conditions
-                    </a>
-                  </>
-                ),
-                value: state.agree,
-                id: "agree",
-                onChange: (agree) => {
-                  console.log(agree);
-                  State.update({ agree: !state.agree });
-                },
-              }}
-            />
-          </Dialog.Description>
-          <Footer>
-            <Dialog.Close asChild>
-              <CloseButton href="/">Close</CloseButton>
-            </Dialog.Close>
-            <Widget
-              src={`${ownerId}/widget/Buttons.Green`}
-              props={{
-                text: <>Accept</>,
-                disabled: !state.agree,
-                onClick: () => {
-                  if (!state.agree) return;
-                  props.accept();
-                },
-              }}
-            />
-          </Footer>
+          <Widget src={`${ownerId}/widget/TNCForm`} />
         </Content>
       </Dialog.Content>
     </Dialog.Root>
