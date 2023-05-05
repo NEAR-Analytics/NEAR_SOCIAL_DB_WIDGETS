3a576038858f981f576a341d635d20b699d203d4
frichard5.near <frichard5.near>
2023-05-05 15:04:19 -0400

    Update NDC-proposer-detail by frichard5.near at 2023-05-05 15:04:19.673

--- frichard5.near/NDC-proposer-detail/source_code.jsx
+++ frichard5.near/NDC-proposer-detail/source_code.jsx
@@ -5,8 +5,103 @@ const apiUrl = `https://api.pikespeak.ai/daos/account-proposal-history/${account
 const publicApiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
 const resPerPage = 10;
 
+const columns = [
+  {
+    id: "submission_time",
+    label: "Date",
+    formatter: (data) => {
+      const milliTimestamp = Math.trunc(
+        Number(data["timestamp"]) / Math.pow(10, 6)
+      );
+      return new Date(Number(milliTimestamp)).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+      });
+    },
+  },
+  {
+    id: "proposal_type",
+    label: "Type",
+  },
+  {
+    id: "proposal_id",
+    label: "Proposal id",
+    formatter: (d) => {
+      return (
+        <a
+          href={`https://near.social/edit#/frichard5.near/widget/NDC-Page?tab=proposal&proposal_id=${d.proposal_id}`}
+          target="_blank"
+        >
+          {d.proposal_id}
+        </a>
+      );
+    },
+  },
+  {
+    id: "total",
+    label: "Details",
+    formatter: (d) => {
+      const setModal = (d) => {
+        return () => {
+          State.update({
+            isModalOpen: true,
+            currentProposerDetail: d.account,
+          });
+        };
+      };
+      return <button onClick={setModal(d)}>Show</button>;
+    },
+  },
+];
+
+State.init({
+  displayedHistory: [],
+  history: [],
+  offset: 0,
+});
+
+const nextPage = () => {
+  const currentOffset = state.offset + resPerPage;
+  State.update({
+    offset: currentOffset,
+    displayedHistory: state.history.slice(
+      currentOffset,
+      resPerPage + currentOffset
+    ),
+  });
+};
+
+const previousPage = () => {
+  const currentOffset = state.offset - resPerPage;
+  State.update({
+    offset: currentOffset,
+    displayedHistory: state.history.slice(
+      currentOffset,
+      resPerPage + currentOffset
+    ),
+  });
+};
+
+const GenericTable = (
+  <Widget
+    src={`${widgetProvider}/widget/generic_table`}
+    props={{
+      title: "Proposers",
+      columns,
+      data: state.displayedProposers,
+      nextPage,
+      previousPage,
+      offset: state.offset,
+      resPerPage,
+    }}
+  />
+);
+
 const fetchProposerHistory = () => {
-  const history = fetch(apiUrl, {
+  const history = fetch(apiUrl + `?proposer=${proposer}`, {
     mode: "cors",
     headers: {
       "x-api-key": publicApiKey,
@@ -20,5 +115,17 @@ const fetchProposerHistory = () => {
 };
 
 fetchProposerHistory();
-console.log("HISTORY", state);
-return <div>Hello World</div>;
+
+return (
+  <>
+    <h2>
+      <a
+        href={`https://explorer.near.org/accounts/${proposer}`}
+        target="_blank"
+      >
+        {proposer} proposal history
+      </a>
+    </h2>
+    {GenericTable}
+  </>
+);
