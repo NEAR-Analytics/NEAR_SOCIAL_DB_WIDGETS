bd819103ae884534a35f752d3720e760bdeb9b48
frichard5.near <frichard5.near>
2023-05-05 13:25:01 -0400

    Update NDC-proposer-detail by frichard5.near at 2023-05-05 13:25:01.886

--- frichard5.near/NDC-proposer-detail/source_code.jsx
+++ frichard5.near/NDC-proposer-detail/source_code.jsx
@@ -1,1 +1,24 @@
+const widgetProvider = props.widgetProvider;
+const account = props.account || "marketing.sputnik-dao.near";
+const proposer = props.proposer;
+const apiUrl = `https://api.pikespeak.ai/daos/account-proposal-history/${account}`;
+const publicApiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
+const resPerPage = 10;
+
+const fetchProposerHistory = () => {
+  const history = fetch(apiUrl, {
+    mode: "cors",
+    headers: {
+      "x-api-key": publicApiKey,
+    },
+  });
+  history.body &&
+    State.update({
+      displayedHistory: history.body.slice(0, resPerPage),
+      history: history.body,
+    });
+};
+
+fetchProposerHistory();
+console.log("HISTORY", state);
 return <div>Hello World</div>;
