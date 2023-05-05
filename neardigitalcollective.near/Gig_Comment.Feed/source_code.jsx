46a97d7797aa402694bea316817e956b720ecb90
near-ndc <near-ndc>
2023-05-05 18:22:51 +0000

    Update Gig_Comment.Feed by neardigitalcollective.near at 2023-05-05T18:22:51.703Z

---
+++ neardigitalcollective.near/Gig_Comment.Feed/source_code.jsx
@@ -0,0 +1,47 @@
+const addressForComments = "gig-comments";
+const addressForArticles = "ndcGigArticle";
+const authorForWidget = "neardigitalcollective.near";
+const index = {
+  action: addressForComments,
+  key: props.item,
+  options: {
+    limit: props.limit ?? 3,
+    order: "desc",
+    accountId: props.accounts,
+    subscribe: props.subscribe,
+  },
+};
+
+const raw = !!props.raw;
+
+const renderItem = (a) =>
+  a.value.type === "md" && (
+    <div key={JSON.stringify(a)}>
+      <Widget
+        src={`${authorForWidget}/widget/Gigs_Comment`}
+        props={{
+          accountId: a.accountId,
+          blockHeight: a.blockHeight,
+          highlight:
+            a.accountId === props.highlightComment?.accountId &&
+            a.blockHeight === props.highlightComment?.blockHeight,
+          raw,
+        }}
+      />
+    </div>
+  );
+
+return (
+  <div>
+    <Widget
+      src={`${authorForWidget}/widget/Gigs_ManualIndexFeed`}
+      props={{
+        index,
+        reverse: true,
+        renderItem,
+        nextLimit: 10,
+        loadMoreText: "Show earlier comments...",
+      }}
+    />
+  </div>
+);
