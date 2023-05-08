27cb5a8becfab2a637d00b9d9f3f1f0f94996734
frichard5.near <frichard5.near>
2023-05-08 12:21:52 +0000

    Update NDC-vote-on-proposal by frichard5.near at 2023-05-08T12:21:52.648Z

---
+++ frichard5.near/NDC-vote-on-proposal/source_code.jsx
@@ -0,0 +1,49 @@
+const { daoId, proposal_id } = props;
+
+const handleApprove = () => {
+  Near.call([
+    {
+      contractName: daoId,
+      methodName: "act_proposal",
+      args: {
+        id: JSON.parse(proposal_id),
+        action: "VoteApprove",
+      },
+      gas: 300000000000000,
+    },
+  ]);
+};
+
+const handleReject = () => {
+  Near.call([
+    {
+      contractName: daoId,
+      methodName: "act_proposal",
+      args: {
+        id: JSON.parse(proposal_id),
+        action: "VoteReject",
+      },
+      gas: 300000000000000,
+    },
+  ]);
+};
+
+const Label = styled.span`
+  color:#8c8c8c;
+  font-size: 11px;
+`;
+
+return (
+  <>
+    <Label>Approve or reject the proposal {proposal_id}</Label>
+    <div>
+      <a className="btn btn-outline-success" onClick={handleApprove}>
+        Approve
+      </a>
+      <a className="btn btn-outline-danger" onClick={handleReject}>
+        Reject
+      </a>
+      <hr />
+    </div>
+  </>
+);
