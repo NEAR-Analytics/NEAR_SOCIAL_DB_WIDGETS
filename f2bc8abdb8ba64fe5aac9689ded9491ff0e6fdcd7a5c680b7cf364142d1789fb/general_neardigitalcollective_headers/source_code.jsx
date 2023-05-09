827a1f55f7e587f6be53994fc000444d2f7dfcf0
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-05-09 21:36:15 +0000

    Update general_neardigitalcollective_headers by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-05-09T21:36:15.417Z

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/general_neardigitalcollective_headers/source_code.jsx
@@ -0,0 +1,83 @@
+const articleId = props.articleId ?? "Test";
+const author =
+  props.author ??
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+const timeCreate = props.timeCreate ?? {
+  date: 2000,
+  time: 2000,
+};
+const headerOptionsItems = props.headerOptionsItems;
+
+const widgetOwner =
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+
+const propsPassedWrongFeedback = () => {
+  let feedbackArray = [];
+
+  if (!timeCreate.date) {
+    feedbackArray.push("Time creation date passed wrongly");
+  }
+  if (!timeCreate.time) {
+    feedbackArray.push("Time creation time passed wrongly");
+  }
+  if (!author) {
+    feedbackArray.push("Author passed wrongly");
+  }
+
+  return (
+    <>
+      <h5 className="text-danger">Error:</h5>
+      <ul>
+        {feedbackArray.map((element) => {
+          return <li>{element}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
+
+if (!timeCreate.time || !timeCreate.date || !author) {
+  return <>{propsPassedWrongFeedback()}</>;
+}
+
+const getDateLastEdit = (timestamp) => {
+  const date = new Date(Number(timestamp));
+  const dateString = {
+    date: date.toLocaleDateString(),
+    time: date.toLocaleTimeString(),
+  };
+  return dateString;
+};
+
+return (
+  <div className="row d-flex justify-content-center">
+    {articleId && (
+      <h5 className="card-title text-center pb-2 mb-2 border-bottom">
+        {articleId}
+      </h5>
+    )}
+
+    <div className="col flex-grow-1">
+      <Widget
+        src={`${widgetOwner}/widget/Profile.ShortInlineBlock`}
+        props={{ accountId: author, tooltip: true, maxWidth: "100%" }}
+      />
+    </div>
+
+    <div className="d-none d-lg-block col flex-grow-0">
+      <p className="card-subtitle text-muted text-end">
+        {getDateLastEdit(article.timeCreate).date}
+      </p>
+      <p className="card-subtitle text-muted text-end">
+        {getDateLastEdit(article.timeCreate).time}
+      </p>
+    </div>
+
+    <Widget
+      src={`${widgetOwner}/widget/general_neardigitalcollective_headers.options`}
+      props={{
+        items: headerOptionsItems,
+      }}
+    />
+  </div>
+);
