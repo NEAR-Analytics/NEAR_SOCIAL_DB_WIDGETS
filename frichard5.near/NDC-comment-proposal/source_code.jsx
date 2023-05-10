dbb349ff929137b64e8fc7b77e4d89a9f3ffa10b
frichard5.near <frichard5.near>
2023-05-10 13:21:58 +0000

    Update NDC-comment-proposal by frichard5.near at 2023-05-10T13:21:58.858Z

--- frichard5.near/NDC-comment-proposal/source_code.jsx
+++ frichard5.near/NDC-comment-proposal/source_code.jsx
@@ -1,1 +1,44 @@
-return <div>Hello World</div>;
+const comment = () => {
+  const dataa = Social.get("frichard5.near/**");
+  console.log("uhm", dataa);
+  const index = {
+    action: "post",
+    key: "main",
+    options: {
+      limit: 10,
+      order: "desc",
+      accountId: props.accounts,
+    },
+  };
+  const initialItems = Social.index(index.action, index.key, index.options);
+  console.log("initialItems", initialItems);
+  //const setPost = Social.set();
+  const data = {
+    post: {
+      main: JSON.stringify({ type: "md", test: "test wrong zkey" }),
+    },
+    index: {
+      post: JSON.stringify({
+        key: "main",
+        value: {
+          type: "mid",
+        },
+      }),
+    },
+  };
+  Social.set(data, { force: true });
+  //Social.set({ ndcTest: "hello" });
+  /*const item = {
+    type: "social",
+    path: `satoshidev.near/post/main`,
+    blockheight: 91360190,
+  };
+  const uh = Social.index("like", item);
+  console.log("azdaz", uh);*/
+};
+
+return (
+  <div>
+    <button onClick={comment}>Comment</button>
+  </div>
+);
