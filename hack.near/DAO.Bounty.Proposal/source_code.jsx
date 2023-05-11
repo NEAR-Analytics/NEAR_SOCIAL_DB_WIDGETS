c9b5c47c9c10f68eac22a6f3958675f551adba91
NearSocial <NearSocial>
2023-05-11 00:34:54 +0000

    Update DAO.Bounty.Proposal by hack.near at 2023-05-11T00:34:54.415Z

--- hack.near/DAO.Bounty.Proposal/source_code.jsx
+++ hack.near/DAO.Bounty.Proposal/source_code.jsx
@@ -11,6 +11,8 @@ State.init({
   amount: state.amount,
   times: state.times,
   max_deadline: state.max_deadline,
+  deposit: state.deposit,
+  gas: state.gas,
 });
 
 const handleProposal = () => {
@@ -21,8 +23,8 @@ const handleProposal = () => {
     times: JSON.parse(state.times),
     max_deadline: JSON.stringify(state.max_deadline * 3600000000000),
   };
-  const gas = 200000000000000;
-  const deposit = 100000000000000000000000;
+  const gas = state.gas ?? 200000000000000;
+  const deposit = state.deposit ?? 100000000000000000000000;
   Near.call([
     {
       contractName: daoId,
@@ -73,6 +75,18 @@ const onChangeDeadline = (max_deadline) => {
   });
 };
 
+const onChangeDeposit = (deposit) => {
+  State.update({
+    deposit,
+  });
+};
+
+const onChangeGas = (gas) => {
+  State.update({
+    gas,
+  });
+};
+
 return (
   <div className="mb-3">
     <h2>Propose New Bounty</h2>
@@ -84,17 +98,35 @@ return (
       Reward Token ID:
       <input type="text" onChange={(e) => onChangeToken(e.target.value)} />
     </div>
-    <div className="mb-3">
-      Amount per Claim:
-      <input type="number" onChange={(e) => onChangeAmount(e.target.value)} />
+    <div className="row mb-3">
+      <div className="col-sm mb-3">
+        Token reward amount per bounty claim:
+        <input type="number" onChange={(e) => onChangeAmount(e.target.value)} />
+      </div>
+      <div className="col-sm mb-3">
+        Number of times the bounty can be claimed:
+        <input type="number" onChange={(e) => onChangeTimes(e.target.value)} />
+      </div>
+      <div className="col-sm mb-3">
+        Number of hours (after claim) until the deadline:
+        <input
+          type="number"
+          onChange={(e) => onChangeDeadline(e.target.value)}
+        />
+      </div>
     </div>
-    <div className="mb-3">
-      Number of times the bounty can be claimed:
-      <input type="number" onChange={(e) => onChangeTimes(e.target.value)} />
-    </div>
-    <div className="mb-3">
-      Number of hours (after claim) until the deadline:
-      <input type="number" onChange={(e) => onChangeDeadline(e.target.value)} />
+    <div className="row mb-3">
+      <div className="col-sm mb-3">
+        Deposit{" "}
+        <input
+          type="number"
+          onChange={(e) => onChangeDeposit(e.target.value)}
+        />
+      </div>
+      <div className="col-sm mb-3">
+        Gas
+        <input type="number" onChange={(e) => onChangeGas(e.target.value)} />
+      </div>
     </div>
     <button className="btn btn-success" onClick={handleProposal}>
       Propose Bounty
