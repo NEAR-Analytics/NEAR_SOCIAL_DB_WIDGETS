40011e644dc172d5bbb5ef66ff43bf39750f073b
NearSocial <NearSocial>
2023-05-11 03:16:23 +0000

    Update DAO.Rewards by hack.near at 2023-05-11T03:16:23.835Z

--- hack.near/DAO.Rewards/source_code.jsx
+++ hack.near/DAO.Rewards/source_code.jsx
@@ -1,7 +1,10 @@
+const accountId = props.accountId ?? context.accountId;
+const daoId = props.daoId ?? "multi.sputnik-dao.near";
+
 return (
   <>
-    <Widget src="hack.near/widget/DAO.Reward.Proposal" />
+    <Widget src="hack.near/widget/DAO.Reward.Proposal" props={props} />
     <hr />
-    <Widget src="hack.near/widget/DAO.Reward.Claim" />
+    <Widget src="hack.near/widget/DAO.Reward.Claim" props={props} />
   </>
 );
