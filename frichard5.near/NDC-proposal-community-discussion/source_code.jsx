f75238d4f8c9ad3ae670d01061bd24c43d9fca29
frichard5.near <frichard5.near>
2023-05-11 05:07:09 +0000

    Update NDC-proposal-community-discussion by frichard5.near at 2023-05-11T05:07:09.679Z

--- frichard5.near/NDC-proposal-community-discussion/source_code.jsx
+++ frichard5.near/NDC-proposal-community-discussion/source_code.jsx
@@ -1,1 +1,72 @@
-return <div></div>;
+const { widgetProvider, proposal_id } = props;
+
+State.init({
+  showReply: false,
+  displayComments: props.displayComments,
+});
+
+const item = {
+  type: "sputnik_proposal_comment",
+  path: `${context.accountId}/proposal/main`,
+  proposal_id,
+};
+
+const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  margin: -6px -6px 6px;
+`;
+
+const Comments = styled.div`
+  > div > div:first-child {
+    padding-top: 12px;
+  }
+`;
+
+return (
+  <>
+    <button
+      onClick={() => State.update({ displayComments: !state.displayComments })}
+    >
+      {" "}
+      Comments{" "}
+    </button>
+    {state.displayComments ? (
+      <div>
+        <Actions>
+          <Widget
+            src="near/widget/CommentButton"
+            props={{
+              item,
+              onClick: () => State.update({ showReply: !state.showReply }),
+            }}
+          />
+        </Actions>
+        {state.showReply && (
+          <div className="mb-2">
+            <Widget
+              src="near/widget/Comments.Compose"
+              props={{
+                //notifyAccountId,
+                item,
+                onComment: () => State.update({ showReply: false }),
+              }}
+            />
+          </div>
+        )}
+        <Comments>
+          <Widget
+            src="near/widget/Comments.Feed"
+            props={{
+              item,
+              subscribe: true,
+            }}
+          />
+        </Comments>
+      </div>
+    ) : (
+      ""
+    )}
+  </>
+);
