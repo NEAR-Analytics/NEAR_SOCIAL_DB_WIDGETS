2704523a833263523c6f0230a85557ea0dfb68b3
elliotBraem <elliotBraem>
2023-05-11 01:57:54 +0000

    Update Tree by efiz.near at 2023-05-11T01:57:54.354Z

--- efiz.near/Tree/source_code.jsx
+++ efiz.near/Tree/source_code.jsx
@@ -2,16 +2,55 @@ const path = props.path || `${context.accountId}/**`;
 
 const rootNode = Social.get(path, "final");
 
-function traverse(data, callback) {
-  for (const key in data) {
-    const value = data[key];
+function traverse(newPath, data, callback) {
+  const parts = newPath.split("/");
 
-    if (typeof value === "object") {
-      traverse(value, callback);
-    } else {
-      callback(key, value);
+  if (parts.length === 1) {
+    parts.push("**");
+
+    value = Social.get(parts.join("/"), "final");
+    edges = Object.keys(value);
+    value = JSON.stringify(value, undefined, 2);
+  } else if (parts.length === 2) {
+    parts.push("**");
+
+    value = Social.get(parts.join("/"), "final");
+    edges = Object.keys(value);
+    value = JSON.stringify(value, undefined, 2);
+  } else if (parts.length > 2) {
+    value = Social.get(parts.join("/"), "final");
+    const standard = parts[1];
+    switch (standard) {
+      case "index":
+        value = JSON.parse(value);
+        edges = ["hi"];
+        value = JSON.stringify(value, undefined, 2);
+        // if you go to an index, then it shows all the data
+        break;
+      case "post":
+        //   value = Social.get(parts.slice(0, 3).join("/"), "final");
+        const value = Social.index("post", parts[2], {
+          limit: 10,
+          order: "desc",
+          accountId: parts[0],
+        });
+        value = JSON.stringify(value, undefined, 2);
+        console.log(value);
+        // we want a special view for this
+        break;
     }
   }
+
+  console.log(data);
+  //   for (const key in data) {
+  //     const value = data[key];
+
+  //     if (typeof value === "object") {
+  //       traverse(value, callback);
+  //     } else {
+  //       callback(key, value);
+  //     }
+  //   }
 }
 
 function handleTraverse(node) {
@@ -22,6 +61,6 @@ function handleTraverse(node) {
 return (
   <Widget
     src="efiz.near/widget/Node"
-    props={{ node: rootNode, onTraverse: handleTraverse }}
+    props={{ path, node: rootNode, onTraverse: handleTraverse }}
   />
 );
