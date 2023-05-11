d14743b7f87febbb0b9fbe5034afc81df76c4228
contribut3.near <contribut3.near>
2023-05-11 13:50:58 +0000

    Update Investor.Form by contribut3.near at 2023-05-11T13:50:58.047Z

--- contribut3.near/Investor.Form/source_code.jsx
+++ contribut3.near/Investor.Form/source_code.jsx
@@ -277,7 +277,7 @@ return (
           error: state.descriptionError,
         }}
       />
-      <Widget
+      {/*<Widget
         src={`${ownerId}/widget/Inputs.MultiSelect`}
         props={{
           label: "Tags",
@@ -291,7 +291,7 @@ return (
               })),
             }),
         }}
-      />
+      />*/}
       <Widget
         src={`${ownerId}/widget/Inputs.Text`}
         props={{
@@ -348,26 +348,26 @@ return (
                       : {}),
                     ...(state.tags.length
                       ? {
-                          tags: state.tags.reduce(
-                            (acc, { name }) =>
-                              Object.assign(acc, { [name]: "" }),
-                            {}
-                          ),
-                        }
+                        tags: state.tags.reduce(
+                          (acc, { name }) =>
+                            Object.assign(acc, { [name]: "" }),
+                          {}
+                        ),
+                      }
                       : {}),
                     ...(state.website || state.socials
                       ? {
-                          ...state.socials,
-                          ...(state.website
-                            ? {
-                                website: state.website.startsWith("http://")
-                                  ? state.website.substring(7)
-                                  : state.website.startsWith("https://")
-                                  ? state.website.substring(8)
-                                  : state.website,
-                              }
-                            : {}),
-                        }
+                        ...state.socials,
+                        ...(state.website
+                          ? {
+                            website: state.website.startsWith("http://")
+                              ? state.website.substring(7)
+                              : state.website.startsWith("https://")
+                                ? state.website.substring(8)
+                                : state.website,
+                          }
+                          : {}),
+                      }
                       : {}),
                   },
                 },
