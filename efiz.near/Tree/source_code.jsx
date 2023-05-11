ced8fba142f1752df42ee10e62436828bf422fa2
elliotBraem <elliotBraem>
2023-05-11 03:10:13 +0000

    Update Tree by efiz.near at 2023-05-11T03:10:13.497Z

--- efiz.near/Tree/source_code.jsx
+++ efiz.near/Tree/source_code.jsx
@@ -1,47 +1,64 @@
-const path = props.path || `${context.accountId}/**`;
-
+const key = props.key || context.accountId;
+const path = `${context.accountId}/**`;
 const rootNode = Social.get(path, "final");
 
-function traverse(newPath, data, callback) {
-  const parts = newPath.split("/");
+State.init({
+  node: rootNode,
+  prevNode: rootNode,
+  path,
+  key,
+});
 
-  if (parts.length === 1) {
-    parts.push("**");
+// newPath, data, callback
+function traverse(path, node) {
+  console.log(JSON.stringify(node));
+  State.update({
+    path,
+    node,
+    prevPath: state.path,
+  });
+  //   const parts = newPath.split("/");
 
-    value = Social.get(parts.join("/"), "final");
-    edges = Object.keys(value);
-    value = JSON.stringify(value, undefined, 2);
-  } else if (parts.length === 2) {
-    parts.push("**");
+  //   State.update({ path: newPath, node: data });
+  //   callback(newPath, data);
 
-    value = Social.get(parts.join("/"), "final");
-    edges = Object.keys(value);
-    value = JSON.stringify(value, undefined, 2);
-  } else if (parts.length > 2) {
-    value = Social.get(parts.join("/"), "final");
-    const standard = parts[1];
-    switch (standard) {
-      case "index":
-        value = JSON.parse(value);
-        edges = ["hi"];
-        value = JSON.stringify(value, undefined, 2);
-        // if you go to an index, then it shows all the data
-        break;
-      case "post":
-        //   value = Social.get(parts.slice(0, 3).join("/"), "final");
-        const value = Social.index("post", parts[2], {
-          limit: 10,
-          order: "desc",
-          accountId: parts[0],
-        });
-        value = JSON.stringify(value, undefined, 2);
-        console.log(value);
-        // we want a special view for this
-        break;
-    }
-  }
+  //   if (parts.length === 1) {
+  //     parts.push("**");
 
-  console.log(data);
+  //     value = Social.get(parts.join("/"), "final");
+  //     edges = Object.keys(value);
+  //     value = JSON.stringify(value, undefined, 2);
+  //   } else if (parts.length === 2) {
+  //     parts.push("**");
+
+  //     value = Social.get(parts.join("/"), "final");
+  //     edges = Object.keys(value);
+  //     value = JSON.stringify(value, undefined, 2);
+  //   } else if (parts.length > 2) {
+  //     value = Social.get(parts.join("/"), "final");
+  //     const standard = parts[1];
+  //     switch (standard) {
+  //       case "index":
+  //         value = JSON.parse(value);
+  //         edges = ["hi"];
+  //         value = JSON.stringify(value, undefined, 2);
+  //         // if you go to an index, then it shows all the data
+  //         break;
+  //       case "post":
+  //         //   value = Social.get(parts.slice(0, 3).join("/"), "final");
+  //         const value = Social.index("post", parts[2], {
+  //           limit: 10,
+  //           order: "desc",
+  //           accountId: parts[0],
+  //         });
+  //         value = JSON.stringify(value, undefined, 2);
+  //         console.log(value);
+  //         // we want a special view for this
+  //         break;
+  //     }
+  //   }
+
+  //   console.log(data);
   //   for (const key in data) {
   //     const value = data[key];
 
@@ -53,14 +70,20 @@ function traverse(newPath, data, callback) {
   //   }
 }
 
-function handleTraverse(node) {
-  traverse(node, (node) => console.log(node.value));
+function handleTraverse(path, node) {
+  traverse(path, node, (node) => console.log(node.value));
   // could check the type, return the correct data.
 }
 
 return (
   <Widget
     src="efiz.near/widget/Node"
-    props={{ path, node: rootNode, onTraverse: handleTraverse }}
+    props={{
+      key: "evrything.near",
+      path: state.path,
+      node: state.node,
+      prevPath: state.prevPath,
+      onTraverse: handleTraverse,
+    }}
   />
 );
