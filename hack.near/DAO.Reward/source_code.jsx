020257419b8afeb5e401e8d9e8c31ceb56329d27
NearSocial <NearSocial>
2023-05-11 02:01:43 +0000

    Update DAO.Reward by hack.near at 2023-05-11T02:01:43.958Z

--- hack.near/DAO.Reward/source_code.jsx
+++ hack.near/DAO.Reward/source_code.jsx
@@ -164,7 +164,7 @@ return (
             <div className="col-sm">
               <h5>Amount</h5>
               <p>
-                {Math.round(bounty.amount / 1000000000000000000000000)}
+                {bounty.amount / 1000000000000000000000000}
                 {bounty.token === "" ? "NEAR" : ""}
               </p>
             </div>
