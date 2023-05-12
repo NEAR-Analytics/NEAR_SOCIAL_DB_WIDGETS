bc59050ea5a732eb41a3b0dc7289b8c661f8088b
elliotBraem <elliotBraem>
2023-05-12 13:53:54 +0000

    Update Node by efiz.near at 2023-05-12T13:53:54.088Z

--- efiz.near/Node/source_code.jsx
+++ efiz.near/Node/source_code.jsx
@@ -7,7 +7,6 @@ const setPath = props.setPath;
 const history = props.history;
 const setHistory = props.setHistory;
 const isRoot = props.isRoot;
-const renderView = props.renderView;
 
 State.init({
   expanded: false,
@@ -32,11 +31,44 @@ const Button = styled.button`
   text-transform: lowercase !important;
 `;
 
+const ChildNode = styled.div`
+  margin-left: ${path.split("/").length * 4}px
+`;
+
+const renderView = () => {
+  return <Widget src="efiz.near/widget/View" props={{ path, type }} />;
+};
+
 return (
   <div>
     {history.length > 1 && isRoot && <Button onClick={handleBack}>back</Button>}
     {isRoot ? <p>{label}</p> : <Button onClick={handleInto}>{label}</Button>}
     <Button onClick={handleExpand}>{state.expanded ? "-" : "+"}</Button>
-    {state.expanded && <div>{renderView(path)}</div>}
+    {state.expanded && (
+      <div>
+        {node && typeof node === "object" ? (
+          Object.entries(node).map(([key, val]) => (
+            <ChildNode>
+              <Widget
+                src="efiz.near/widget/Node"
+                props={{
+                  key,
+                  label: key,
+                  node: val,
+                  type: key,
+                  path: `${path}/${key}`,
+                  setPath: setPath,
+                  history,
+                  setHistory: setHistory,
+                  isRoot: false,
+                }}
+              />
+            </ChildNode>
+          ))
+        ) : (
+          <div>{renderView()}</div>
+        )}
+      </div>
+    )}
   </div>
 );
