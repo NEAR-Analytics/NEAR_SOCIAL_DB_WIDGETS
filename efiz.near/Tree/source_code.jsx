e7c87928be61ecfd995e251a76b01729a2138890
elliotBraem <elliotBraem>
2023-05-12 14:19:18 +0000

    Update Tree by efiz.near at 2023-05-12T14:19:18.976Z

--- efiz.near/Tree/source_code.jsx
+++ efiz.near/Tree/source_code.jsx
@@ -15,6 +15,7 @@ function setHistory(history) {
   State.update({ history });
 }
 
+// HOW TO GET DATA AT ANY GIVEN NODE //
 function getNode(path) {
   const parts = path.split("/");
   let value = {};
@@ -34,10 +35,12 @@ function getNode(path) {
   } else if (parts.length > 2) {
     // EVERYTHING ELSE //
     const standard = parts[1];
+    // GRAPH //
     if (standard === "graph") {
       if (parts.length > 3) {
-        // BACK TO ACCOUNT
+        // FOLLOW
         if (parts[2] === "follow") {
+          // BACK TO ACCOUNT
           setPath(parts[3]);
           setHistory([...history, parts[3]]);
         }
@@ -45,9 +48,11 @@ function getNode(path) {
         parts.push("**");
         value = Social.get(parts.join("/"), "final");
       }
+      // PROFILE //
     } else if (standard === "profile") {
       console.log(parts.join("/"));
       value = Social.get(parts.join("/"), "final");
+      // POST //
     } else if (standard === "post") {
       value = path;
     } else {
@@ -59,38 +64,6 @@ function getNode(path) {
 }
 
 const node = getNode(state.path);
-
-function renderView(nodePath) {
-  const ChildNode = styled.div`
-  margin-left: ${nodePath.split("/").length * 4}px
-`;
-  return (
-    <>
-      {node && typeof node === "object" ? (
-        Object.entries(node).map(([key, val]) => (
-          <ChildNode>
-            <Widget
-              src="efiz.near/widget/Node"
-              props={{
-                key,
-                label: key,
-                node: val,
-                type: key,
-                path: `${nodePath}/${key}`,
-                setPath: setPath,
-                history,
-                setHistory: setHistory,
-                isRoot: false,
-              }}
-            />
-          </ChildNode>
-        ))
-      ) : (
-        <div>{renderView()}</div>
-      )}
-    </>
-  );
-}
 
 return (
   <Widget
@@ -104,7 +77,6 @@ return (
       history: state.history,
       setHistory: setHistory,
       isRoot: true,
-      renderView: renderView,
     }}
   />
 );
