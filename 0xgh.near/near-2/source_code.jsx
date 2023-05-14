97a61c600a4451c6705aa6ea8b3619e5850396d9
0xgh.near <0xgh.near>
2023-05-14 09:07:15 +0000

    Update near-2 by 0xgh.near at 2023-05-14T09:07:15.085Z

--- 0xgh.near/near-2/source_code.jsx
+++ 0xgh.near/near-2/source_code.jsx
@@ -1,5 +1,6 @@
 const Near2Wrapper = styled.div`
   background-color: yellow;
+  padding: 1rem;
 `;
 
 let name = props.name || "User";
@@ -9,5 +10,6 @@ return (
     <p>Component-2 : near-2.jsx</p>
     <p>props:name = {name}</p>
     <p>계정 = {context.accountId}</p>
+    <Widget src={`0xgh.near/widget/near-3`} />
   </Near2Wrapper>
 );
