278f22680302264e7248f94c5b0b416a083fc042
NearSocial <NearSocial>
2023-05-14 03:34:10 +0000

    Update dev.Page by hack.near at 2023-05-14T03:34:10.920Z

--- hack.near/dev.Page/source_code.jsx
+++ hack.near/dev.Page/source_code.jsx
@@ -44,7 +44,7 @@ return (
         role="tabpanel"
         aria-labelledby="pills-feed-tab"
       >
-        <Widget src="hack.near/widget/Custom.Page.Content" props={props} />
+        <Widget src="hack.near/widget/dev.Content" props={props} />
       </div>
       <div
         className="tab-pane d-lg-block col-lg-4"
