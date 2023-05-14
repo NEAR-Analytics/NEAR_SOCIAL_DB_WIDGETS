e8c139cae394c157174275fb9f3a6f305631f3db
0xgh.near <0xgh.near>
2023-05-14 09:00:21 +0000

    Update near-2 by 0xgh.near at 2023-05-14T09:00:21.508Z

--- 0xgh.near/near-2/source_code.jsx
+++ 0xgh.near/near-2/source_code.jsx
@@ -6,6 +6,7 @@ let name = props.name || "User";
 
 return (
   <Near2Wrapper>
+    <p>Component-2 : near-2.jsx</p>
     <p>props:name = {name}</p>
     <p>계정 = {context.accountId}</p>
   </Near2Wrapper>
