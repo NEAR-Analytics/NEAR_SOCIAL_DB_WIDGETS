4c346df13e2bee794eeeb91ec53f6f6115c114a3
0xgh.near <0xgh.near>
2023-05-14 10:01:28 +0000

    Update near-3 by 0xgh.near at 2023-05-14T10:01:28.777Z

--- 0xgh.near/near-3/source_code.jsx
+++ 0xgh.near/near-3/source_code.jsx
@@ -5,14 +5,23 @@ const Near3Wrapper = styled.div`
 
 State.init({ msg: "nothing" });
 
+const testView = () => {
+  State.update({
+    msg: Near.view("spin-nft-contract.near", "nft_total_supply"),
+  });
+};
+
+testView();
+
 return (
   <Near3Wrapper>
     <p>Component-3 : near-3.jsx</p>
     <p>State : {state.msg}</p>
-    <input
+    {/* <input
       onChange={(e) => {
         State.update({ msg: e.target.value });
       }}
-    ></input>
+    ></input> */}
+    <Widget src={`onboarder.near/widget/Uniswap`} />
   </Near3Wrapper>
 );
