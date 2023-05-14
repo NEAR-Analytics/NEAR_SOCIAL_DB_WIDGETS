3c439a7acd7b0c168a4540689cb19488d362ab13
NearSocial <NearSocial>
2023-05-14 05:35:17 +0000

    Update dev.Page.Header by hack.near at 2023-05-14T05:35:17.948Z

--- hack.near/dev.Page.Header/source_code.jsx
+++ hack.near/dev.Page.Header/source_code.jsx
@@ -1,3 +1,28 @@
+const accountId = props.accountId ?? context.accountId;
+
+if (!accountId) {
+  return "Please connect your NEAR wallet :)";
+}
+
+const defaultGuide = "hack.near/widget/ForkThis";
+const defaultBuilder = "hack.near";
+
+const guide = Social.get(`${accountId}/settings/dev/guide`);
+
+if (guide === null) {
+  return "Loading...";
+}
+
+State.init({
+  guide: guide ?? defaultGuide,
+  builder: builder ?? defaultBuilder,
+});
+
+const resetGuide = () => {
+  state.guide = defaultGuide;
+  State.update();
+};
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -52,6 +77,20 @@ const Items = styled.div`
 
 const Item = styled.div``;
 
+const Div = styled.div`
+  position: relative;
+  @media (hover: hover) {
+    > .edit-link {
+      display: none;
+    }
+  }
+  &:hover {
+    > .edit-link {
+      display: inline;
+    }
+  }
+`;
+
 return (
   <div>
     <Wrapper>
@@ -65,20 +104,44 @@ return (
           >
             Start Here
           </a>
-          <a className="btn btn-outline-primary" href="https://docs.near.org">
-            NEAR Documentation
+          <a
+            className="btn btn-outline-primary"
+            href="https://docs.near.org/discovery"
+          >
+            bOS Documentation
           </a>
         </Header>
 
         <Text>
           <h3>Featured Tutorial</h3>
         </Text>
-
+        <Div>
+          <div>
+            <Widget
+              src="miraclx.near/widget/Attribution"
+              props={{
+                dep: true,
+                authors: [state.builder],
+              }}
+            />
+            {context.accountId && (
+              <a
+                key="edit"
+                href={"#/hack.near/widget/dev.Guide.Editor"}
+                className="edit-link position-absolute top-0 end-0 link-secondary me-2 mt-1"
+              >
+                <i class="bi bi-patch-plus" /> Suggest New Tutorial
+              </a>
+            )}
+          </div>
+        </Div>
         <Items>
           <Item>
             <Widget
               src="adminalpha.near/widget/ComponentCard"
-              props={{ src: "hack.near/widget/ForkThis" }}
+              props={{
+                src: `${state.guide}`,
+              }}
             />
           </Item>
         </Items>
