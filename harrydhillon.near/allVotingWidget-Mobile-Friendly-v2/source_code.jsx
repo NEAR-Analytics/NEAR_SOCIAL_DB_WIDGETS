d9f77eaf4c96bb86ce24a6fae292c60d76963568
harjaapdhillon16 <harjaapdhillon16>
2023-05-15 20:57:11 +0000

    Update allVotingWidget-Mobile-Friendly-v2 by harrydhillon.near at 2023-05-15T20:57:11.706Z

---
+++ harrydhillon.near/allVotingWidget-Mobile-Friendly-v2/source_code.jsx
@@ -0,0 +1,389 @@
+if (!props.isPreview && !props.poll) {
+  return "Property poll not set";
+}
+
+const isPreview = props.isPreview ?? false;
+
+let widgetOwner = "easypoll.near";
+
+// Getting question
+const poll = props.poll;
+
+let defaultVotes = [];
+for (let i = 0; i < poll.value.questions.length; i++) {
+  if (poll.value.questions[i].questionType == "2") {
+    defaultVotes.push([""]);
+  } else {
+    defaultVotes.push("");
+  }
+}
+
+State.init({
+  vote: userVote ?? defaultVotes,
+  answers: {},
+  showErrorsInForm: false,
+  hoveringElement: "",
+});
+
+let bgBlue = "#96C0FF";
+let bgRed = "#FFB4B4";
+let bgYellow = "#FFE999";
+let bgPurple = "#E6C0FF";
+let bgGreen = "#96FFE0";
+let bgPink = "#FF96B9";
+let bgSkyBlue = "#96EAFF";
+let bgIndigo = "#96DCD2";
+
+let allBgColors = [
+  bgBlue,
+  bgRed,
+  bgYellow,
+  bgPurple,
+  bgGreen,
+  bgPink,
+  bgSkyBlue,
+  bgIndigo,
+];
+
+let secondaryBgBlue = "#E6F0FF";
+let secondaryBgRed = "#FFEDED";
+let secondaryBgYellow = "#FFFAE6";
+let secondaryBgPurple = "#F9F0FF";
+let secondaryBgGreen = "#E6FFF7";
+let secondaryBgPink = "#FFE6EE";
+let secondaryBgSkyBlue = "#E6FAFF";
+let secondaryBgIndigo = "#E6F6F4";
+
+let allSecondaryBgColors = [
+  secondaryBgBlue,
+  secondaryBgRed,
+  secondaryBgYellow,
+  secondaryBgPurple,
+  secondaryBgGreen,
+  secondaryBgPink,
+  secondaryBgSkyBlue,
+  secondaryBgIndigo,
+];
+
+let fontColorBlue = "#003E9C";
+let fontColorRed = "#9C2B2B";
+let fontColorYellow = "#9C7B03";
+let fontColorPurple = "#763E9C";
+let fontColorGreen = "#009C6D";
+let fontColorPink = "#9C0034";
+let fontColorSkyBlue = "#007C9C";
+let fontColorIndigo = "#006758";
+
+let allFontColors = [
+  fontColorBlue,
+  fontColorRed,
+  fontColorYellow,
+  fontColorPurple,
+  fontColorGreen,
+  fontColorPink,
+  fontColorSkyBlue,
+  fontColorIndigo,
+];
+
+let secondaryColor = "#E9EBF8";
+
+function getBgColor(index, isPrimary) {
+  let allColorsOfThisType =
+    state.vote != `${index}` && isPrimary ? allBgColors : allSecondaryBgColors;
+
+  return Number.isInteger((index + 1) / allColorsOfThisType.length)
+    ? allColorsOfThisType[0]
+    : allColorsOfThisType[
+        ((index + 1) / allColorsOfThisType.length -
+          Math.trunc((index + 1) / allColorsOfThisType.length)) *
+          allColorsOfThisType.length -
+          1
+      ];
+}
+
+function getFontColor(index) {
+  return Number.isInteger((index + 1) / allFontColors.length)
+    ? allFontColors[0]
+    : allFontColors[
+        ((index + 1) / allFontColors.length -
+          Math.trunc((index + 1) / allFontColors.length)) *
+          allFontColors.length -
+          1
+      ];
+}
+
+function getInputStyles(questionType, questionNumber, optionNumber) {
+  if (questionType == "2") {
+    return state.vote[questionNumber].includes(optionNumber + "")
+      ? {
+          borderColor: "black",
+          backgroundColor: "black",
+          width: "1rem",
+          marginRight: "0.7rem",
+        }
+      : {
+          width: "1rem",
+          marginRight: "0.7rem",
+        };
+  } else {
+    return optionNumber + "" == state.vote[questionNumber]
+      ? {
+          borderColor: "black",
+          backgroundColor: "black",
+          width: "1rem",
+          marginRight: "0.7rem",
+        }
+      : {
+          width: "1rem",
+          marginRight: "0.7rem",
+        };
+  }
+}
+
+// Utility function
+function getBlockTimestamp(blockHeight) {
+  // It is stored in nanoseconds which is 1e-6 miliseconds
+  return Near.block(blockHeight).header.timestamp / 1e6;
+}
+
+// Discards answers that were posted after question's end date
+function getTimeRelatedValidAnswers(answers) {
+  let low = 0;
+  let high = answers.length - 1;
+  const questionEndTimestamp = poll.value.endTimestamp;
+  let endBlockTimestamp = getBlockTimestamp(answers[high].blockHeight);
+  if (endBlockTimestamp < questionEndTimestamp) return answers;
+  // For tries to exceed 50 there should be more than 10e15 answers which will never happen. But if you mess up and make an infinite cycle it will crash. This way it will never be infinite
+  let tries = 10;
+  while (high - low > 1 && tries > 0) {
+    tries--;
+    let curr = Math.floor((high - low) / 2) + low;
+    let currBlockTimestamp = getBlockTimestamp(answers[curr].blockHeight);
+    if (currBlockTimestamp < questionEndTimestamp) {
+      low = curr;
+    } else {
+      high = curr;
+    }
+  }
+  // Slice ignores the index of the last one. Since high - low == 1, high = low + 1
+  return answers.slice(0, high);
+}
+
+function getOptionRelatedValidAnswers(answers) {
+  return answers.filter((a) => {
+    const userAnswers = a.value.answer;
+    return userAnswers.every((an, i) => {
+      // If has choicesOptions, then it's needs validation answer is among the options. If not, any answer is just fine
+      if (poll.value.questions[i].choicesOptions.length > 0) {
+        if (Array.isArray(an)) {
+          return an.every(
+            (ans) =>
+              0 <= Number(ans) &&
+              Number(ans) < poll.value.questions[i].choicesOptions.length
+          );
+        } else {
+          return (
+            0 <= Number(an) &&
+            Number(an) < poll.value.questions[i].choicesOptions.length
+          );
+        }
+      } else {
+        return true;
+      }
+    });
+  });
+}
+
+function getValidAnswers() {
+  let validTimeAnswers = getTimeRelatedValidAnswers(answersToThisPoll);
+  let validOptionAndTimeAnswers =
+    getOptionRelatedValidAnswers(validTimeAnswers);
+  return validOptionAndTimeAnswers;
+}
+
+// Getting valid answers
+const answers = Social.index("poll_question", "answer-v3.1.0");
+
+if (JSON.stringify(answers) != JSON.stringify(state.answers)) {
+  State.update({ answers: answers });
+}
+
+if (!state.answers) {
+  return "Loading";
+}
+const answersToThisPoll = state.answers.filter(
+  (a) => a.value.questionBlockHeight == props.poll.blockHeight
+);
+const validAnswersToThisPoll = getValidAnswers(answersToThisPoll);
+
+let userVote;
+// Getting if user has already voted
+const currAccountId = context.accountId ?? "";
+function userHasVoted() {
+  return (
+    validAnswersToThisPoll.find((a) => a.accountId == currAccountId) !=
+    undefined
+  );
+}
+let hasVoted = userHasVoted();
+const isQuestionOpen =
+  poll.value.startTimestamp < Date.now() &&
+  Date.now() < poll.value.endTimestamp;
+const canVote = !hasVoted && isQuestionOpen;
+
+// Counting votes to display
+function countVotes(questionNumber, questionType) {
+  if (questionType == "3") return;
+  return validAnswersToThisPoll.reduce((acc, curr) => {
+    let ans = curr.value.answer[questionNumber];
+    if (Array.isArray(ans)) {
+      ans.forEach((a) => {
+        acc[Number(a)] += 1;
+      });
+    } else {
+      acc[Number(ans)] += 1;
+    }
+
+    return acc;
+  }, new Array(poll.value.questions[questionNumber].choicesOptions.length).fill(0));
+}
+
+//TODO review this!
+const getPublicationParams = () => {
+  return {
+    index: {
+      poll_question: JSON.stringify(
+        {
+          key: "answer-v3.1.0",
+          value: {
+            answer: state.vote,
+            questionBlockHeight: props.poll.blockHeight,
+          },
+        },
+        undefined,
+        0
+      ),
+    },
+  };
+};
+
+function isVoteValid() {
+  let isValid = state.vote.length == poll.value.questions.length;
+  isValid = isValid && context.accountId;
+  for (let i = 0; i < state.vote.length; i++) {
+    const vote = state.vote[i];
+    // vote should always be a string, but in one case is treated as an array. Replace array with csv
+    if (Array.isArray(vote)) {
+      isValid = isValid && vote.filter((v) => v.trim() != "").length > 0;
+    } else {
+      isValid = isValid && vote.trim() != "";
+    }
+  }
+  return isValid;
+}
+
+function calculatePercentage(votesToThisOption) {
+  if (validAnswersToThisPoll.length == 0) return 0;
+  return ((votesToThisOption / validAnswersToThisPoll.length) * 100).toFixed(2);
+}
+
+function calculatePercentageOfOption(votes, index) {
+  const validAnswers = votes.reduce((acc, curr) => acc + curr, 0);
+
+  if (validAnswers == 0 || votes.length == 0) return 0;
+
+  const votesToThisOption = votes[index];
+  return ((votesToThisOption / validAnswers) * 100).toFixed(2);
+}
+
+function getBorderRadious(questionNumber, optionNumber) {
+  if (optionNumber == 0) {
+    return "12px 12px 4px 4px";
+  } else if (
+    optionNumber ==
+    poll.value.questions[questionNumber].choicesOptions.length - 1
+  ) {
+    return "4px 4px 12px 12px";
+  } else {
+    return "4px";
+  }
+}
+
+const isValidInput = () => {
+  let result = state.vote != "";
+  return result && !isPreview;
+};
+
+const renderAnswers = (questionNumber) => {
+  return (
+    <Widget
+      src={`${widgetOwner}/widget/answer_poll-comment-container`}
+      props={{
+        answers: validAnswersToThisPoll,
+        questionNumber,
+      }}
+    />
+  );
+};
+
+function clickRadioInputHandler(questionNumber, optionNumber) {
+  return () => {
+    let newVote = state.vote;
+
+    newVote[questionNumber] = optionNumber + "";
+    State.update({ vote: newVote });
+  };
+}
+
+function clickCheckboxInputHandler(questionNumber, optionNumber) {
+  return () => {
+    let newVote = state.vote;
+
+    let oldQuestionVotes = newVote[questionNumber];
+    let newQuestionVotes = [];
+
+    if (!oldQuestionVotes.includes(optionNumber + "")) {
+      newQuestionVotes = oldQuestionVotes;
+      newQuestionVotes.push(optionNumber + "");
+    } else {
+      for (let i = 0; i < oldQuestionVotes.length; i++) {
+        if (oldQuestionVotes[i] != optionNumber + "") {
+          newQuestionVotes.push(oldQuestionVotes[i]);
+        }
+      }
+    }
+
+    newVote[questionNumber] = newQuestionVotes.filter((a) => a != "");
+
+    State.update({ votes: newVote });
+  };
+}
+
+return (
+  <Widget
+    src="harrydhillon.near/widget/allVotingWidget-Mobile-Friendly-JSX-V2"
+    props={{
+      state: state,
+      poll: poll,
+      canVote: canVote,
+      getBgColor: getBgColor,
+      getFontColor: getFontColor,
+      countVotes: countVotes,
+      getBorderRadious: getBorderRadious,
+      calculatePercentageOfOption: calculatePercentageOfOption,
+      getBlockTimestamp: getBlockTimestamp,
+      clickRadioInputHandler: clickRadioInputHandler,
+      clickCheckboxInputHandler: clickCheckboxInputHandler,
+      clickRadioInputHandler: clickRadioInputHandler,
+      clickCheckboxInputHandler: clickCheckboxInputHandler,
+      getInputStyles: getInputStyles,
+      getInputStyles: getInputStyles,
+      hasVoted: hasVoted,
+      getPublicationParams: getPublicationParams,
+      stateUpdate: (data) => stateUpdate(data),
+      isQuestionOpen: isQuestionOpen,
+      isVoteValid: isVoteValid,
+      validAnswersToThisPoll: validAnswersToThisPoll,
+    }}
+  />
+);
