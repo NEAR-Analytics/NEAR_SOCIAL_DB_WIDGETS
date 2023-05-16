a76fb1964261507f79017974678568f5cd977992
harjaapdhillon16 <harjaapdhillon16>
2023-05-16 18:24:34 +0000

    Update allVotingWidget-RenderMultiInput-Mobile-Friendly by harrydhillon.near at 2023-05-16T18:24:34.587Z

--- harrydhillon.near/allVotingWidget-RenderMultiInput-Mobile-Friendly/source_code.jsx
+++ harrydhillon.near/allVotingWidget-RenderMultiInput-Mobile-Friendly/source_code.jsx
@@ -60,7 +60,7 @@ const renderMultipleChoiceInput = ({
     </>
   );
 };
-
+console.log(props, "multi");
 return renderMultipleChoiceInput({
   questionNumber: props.questionNumber,
   questionType: props.questionType,
