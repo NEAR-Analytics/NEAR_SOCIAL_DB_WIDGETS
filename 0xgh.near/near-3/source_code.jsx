eebbefe07563388fc4c353352c5155acd7b79c83
0xgh.near <0xgh.near>
2023-05-17 11:54:47 +0000

    Update near-3 by 0xgh.near at 2023-05-17T11:54:47.061Z

--- 0xgh.near/near-3/source_code.jsx
+++ 0xgh.near/near-3/source_code.jsx
@@ -1,6 +1,10 @@
 const Near3Wrapper = styled.div`
   background-color: lightcyan;
   padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 State.init({ msg: "nothing" });
@@ -17,11 +21,15 @@ return (
   <Near3Wrapper>
     <p>Component-3 : near-3.jsx</p>
     <p>State : {state.msg}</p>
-    {/* <input
-      onChange={(e) => {
-        State.update({ msg: e.target.value });
-      }}
-    ></input> */}
     <Widget src={`onboarder.near/widget/Uniswap`} />
+    <button
+      onClick={() => {
+        return Near.call("nearsocialexamples.near", "set_greeting", {
+          message: "Hi Near Social",
+        });
+      }}
+    >
+      Send Transaction to SmartContract
+    </button>
   </Near3Wrapper>
 );
