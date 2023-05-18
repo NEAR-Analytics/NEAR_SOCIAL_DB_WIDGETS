5e1f3da8901d41048925d6b63de5fd8b33f262e3
reup.near <reup.near>
2023-05-18 18:48:30 +0000

    Update Hyperlink by reup.near at 2023-05-18T18:48:30.143Z

--- reup.near/Hyperlink/source_code.jsx
+++ reup.near/Hyperlink/source_code.jsx
@@ -4,7 +4,7 @@ return (
       href="https://www.mintbase.xyz/contract/bonerz.mintbase1.near/nfts/all/0"
       target="_blank"
     >
-      buy bonerz
+      bonerz gallery
     </a>
   </div>
 );
