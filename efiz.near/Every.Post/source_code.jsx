e3271deabd4da785d45151e41415e13d13521274
elliotBraem <elliotBraem>
2023-05-18 18:09:08 +0000

    Update Every.Post by efiz.near at 2023-05-18T18:09:08.263Z

--- efiz.near/Every.Post/source_code.jsx
+++ efiz.near/Every.Post/source_code.jsx
@@ -1,1 +1,46 @@
-return <div>Hello World</div>;
+let index = [];
+
+index = {
+  action: "post",
+  key: "main",
+  options: {
+    limit: 10,
+    order: "desc",
+    accountId: ["efiz.near"],
+  },
+};
+
+const Post = styled.div`
+  border-bottom: 1px solid #eceef0;
+  padding: 24px 0 12px;
+
+  @media (max-width: 1200px) {
+    padding: 12px 0 0;
+  }
+`;
+
+const renderItem = (a) => {
+  if (a.value.type === "md") {
+    return (
+      <Post className="post" key={JSON.stringify(a)}>
+        <Widget
+          src="near/widget/Posts.Post"
+          props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
+        />
+      </Post>
+    );
+  } else {
+    const value = Social.get(`${a.accountId}/post/main`, "final");
+    value = JSON.parse(value);
+    return (
+      <Widget
+        src="efiz.near/widget/Thing"
+        props={{ path: value.path, blockHeight: value.blockHeight }}
+      />
+    );
+  }
+};
+
+return (
+  <Widget src="mob.near/widget/MergedIndexFeed" props={{ index, renderItem }} />
+);
