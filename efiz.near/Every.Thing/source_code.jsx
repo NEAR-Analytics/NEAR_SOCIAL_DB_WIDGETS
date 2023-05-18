33dc0665b8e6bdf54f599e80562ad3a270a4a7b2
elliotBraem <elliotBraem>
2023-05-18 18:21:33 +0000

    Update Every.Thing by efiz.near at 2023-05-18T18:21:33.936Z

--- efiz.near/Every.Thing/source_code.jsx
+++ efiz.near/Every.Thing/source_code.jsx
@@ -1,175 +1,24 @@
-const key = props.key;
-const label = props.label;
-const path = props.path;
-const setPath = props.setPath;
-const history = props.history;
-const setHistory = props.setHistory;
-const isRoot = props.isRoot;
+const creatorId = props.creatorId || context.accountId;
+const blockHeight = props.blockHeight || "final";
+const thingId = props.thingId || "**";
 
-State.init({
-  expanded: false,
-});
+const value = Social.get(`${creatorId}/thing/${thingId}`, blockHeight);
+const keys = Object.keys(value);
 
-function handleExpand() {
-  State.update({ expanded: !state.expanded });
-}
-
-function handleInto() {
-  setPath(path);
-  setHistory([...history, path]);
-}
-
-function handleBack() {
-  const newPath = history[history.length - 2] || "";
-  setPath(newPath);
-  setHistory(history.slice(0, -1));
-}
-
-const Button = styled.button`
-  text-transform: lowercase !important;
-`;
-
-const ButtonRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  gap: 4px;
-`;
-
-function getType(path) {
-  const parts = path.split("/");
-  if (parts.length === 1) {
-    return "account";
-  } else if (parts.length === 2) {
-    return parts[1];
-  } else {
-    const standard = parts[1];
-    if (standard === "thing") {
-      // We're gonna grab the type from the thing itself
-    }
-    return standard;
-  }
-}
-
-const type = getType(path);
-
-// WHEN A NEW ROOT IS SET //
-// GET DATA AT THIS PATH //
-function getNode(path, type) {
-  const parts = path.split("/");
-  let value = {};
-
-  // ACCOUNT //
-  if (type === "account") {
-    if (parts.length > 1) {
-      // GRAPH // FOLLOW // BACK TO ACCOUNT : WORKING
-      //   setRoot(parts[3], "account");
-    } else {
-      if (parts[0] !== "*") {
-        parts.push("**");
-      }
-      value = Social.get(parts.join("/"), "final");
-      return value;
-    }
-    // THING //
-  } else if (type === "thing") {
-    // path: "everything"
-    // type: "thing"
-    return rootNode; // Or should "everything" be "*"?
-    // PROFILE //
-  } else if (type === "profile") {
-    value = Social.get(parts.join("/"), "final");
-    // POST : WIP //
-  } else if (type === "post") {
-    value = path;
-    // NAMETAG : WIP //
-  } else if (type === "nametag") {
-    if (parts.length > 2) {
-      if (parts.length === 3) {
-        // BACK TO ACCOUNT
-        // setRoot(parts[3], "account");
-      } else if (parts.length === 4) {
-        // ALL TAGS BY ACCOUNT
-        value = Social.keys(`${parts[0]}/profile/tags/*`, "final");
-      } else {
-        // THIS TAG
-        value = parts[5];
-      }
-    }
-  } else {
-    parts.push("**");
-    value = Social.get(parts.join("/"), "final");
-    return value;
-  }
-}
-let node = {};
-if (isRoot) {
-  console.log(`getting node for ${path} and ${type}`);
-  node = getNode(path, type);
-}
-
-function renderEdges(edges) {
-  return (
-    <ButtonRow>
-      {edges?.map(([label, path]) => (
-        <Widget
-          src="efiz.near/widget/Every.Node"
-          props={{
-            label: label,
-            path: path,
-            setPath: setPath,
-            history,
-            setHistory: setHistory,
-            isRoot: false,
-          }}
-        />
-      ))}
-    </ButtonRow>
-  );
-}
-
-function renderThing(path, type) {
-  let dataSource;
-  let dataSourceArgs = {};
-  if (type === "account") {
-    dataSource = "SOCIALDB";
-    dataSourceArgs = {
-      arg1: path,
-    };
-  }
+const renderThing = (key) => {
+  const path = `${creatorId}/thing/${key}`;
   return (
     <Widget
-      src="efiz.near/widget/Every.Thing"
-      props={{ dataSource, dataSourceArgs, type, renderEdges: renderEdges }}
+      src="efiz.near/widget/Every.Thing.View"
+      props={{ path, blockHeight }}
     />
   );
-}
+};
 
 return (
   <div>
-    <div>
-      {isRoot ? (
-        <>
-          {/** render root view, is there a way to make the label better? get it from a map? */}
-          <div
-            style={{
-              fontFamily: "Times New Roman",
-              fontSize: "4em",
-              lineHeight: "1.25",
-              fontWeight: 400,
-              cursor: "pointer",
-            }}
-          >
-            {label}
-          </div>
-        </>
-      ) : (
-        <Button onClick={handleInto}>{label}</Button>
-      )}
-      {history.length > 1 && isRoot && (
-        <Button onClick={handleBack}>back</Button>
-      )}
-    </div>
-    {renderThing(path, type)}
+    {keys.map((it) => {
+      return <div>{renderThing(it)}</div>;
+    })}
   </div>
 );
