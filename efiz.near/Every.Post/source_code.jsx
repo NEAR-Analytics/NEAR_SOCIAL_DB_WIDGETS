b9edde52851acd0d66fe0087c2199cf1dece00e8
elliotBraem <elliotBraem>
2023-05-18 19:03:14 +0000

    Update Every.Post by efiz.near at 2023-05-18T19:03:14.185Z

--- efiz.near/Every.Post/source_code.jsx
+++ efiz.near/Every.Post/source_code.jsx
@@ -38,5 +38,8 @@ const renderItem = (a) => {
 };
 
 return (
-  <Widget src="mob.near/widget/MergedIndexFeed" props={{ index, renderItem }} />
+  <Widget
+    src="efiz.near/widget/MergedIndexFeed"
+    props={{ index, renderItem }}
+  />
 );
