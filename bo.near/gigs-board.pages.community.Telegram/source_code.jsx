c6483379962eac36c938391d332b21416700034c
ailisp <ailisp>
2023-05-19 09:23:53 +0000

    Update gigs-board.pages.community.Telegram by bo.near at 2023-05-19T09:23:53.211Z

--- bo.near/gigs-board.pages.community.Telegram/source_code.jsx
+++ bo.near/gigs-board.pages.community.Telegram/source_code.jsx
@@ -113,11 +113,16 @@ const messageIds = groupInfo.body.messageIds;
 const Telegram = (
   <>
     <iframe
-      src={"https://cg-msg-viewer.pages.dev/?group=" + group + '&messageIds=' + messageIds}
+      src={
+        "https://cg-msg-viewer.pages.dev/?group=" +
+        group +
+        "&messageIds=" +
+        messageIds
+      }
       frameborder="0"
       style={{
         width: "100%",
-        height: "100%"
+        height: "100vh",
       }}
     ></iframe>
   </>
