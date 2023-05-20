6a3581c0a8f3ac603782c759f41f72de30aa03f6
elliotBraem <elliotBraem>
2023-05-20 22:55:58 +0000

    Update Form.Input by efiz.near at 2023-05-20T22:55:58.504Z

---
+++ efiz.near/Form.Input/source_code.jsx
@@ -0,0 +1,44 @@
+const section = props.section;
+const key = props.key;
+const index = props.index;
+const value = props.value;
+const modified = props.modified;
+const handleInputChange = props.handleInputChange;
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 8px;
+  border: ${(props) => (props.modified ? "2px solid #f00" : "1px solid #ccc")};
+`;
+
+const Button = styled.button`
+  margin-left: 5px;
+`;
+
+return (
+  <InputContainer>
+    <Input
+      type="text"
+      value={value}
+      modified={modified}
+      onChange={(e) => handleInputChange(section, key, index, e.target.value)}
+    />
+    <Button
+      onClick={() => moveInputUp(section, key, index)}
+      disabled={index === 0}
+    >
+      &uarr;
+    </Button>
+    <Button
+      onClick={() => moveInputDown(section, key, index)}
+      disabled={index === value.length - 1}
+    >
+      &darr;
+    </Button>
+  </InputContainer>
+);
