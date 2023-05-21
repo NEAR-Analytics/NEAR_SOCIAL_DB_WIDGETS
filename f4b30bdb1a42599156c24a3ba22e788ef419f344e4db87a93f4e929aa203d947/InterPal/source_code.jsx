49cf024262fd4bd3a931fa6b6d0f051aba7b9fc4
f4b30bdb1a42599156c24a3ba22e788ef419f344e4db87a93f4e929aa203d947 <f4b30bdb1a42599156c24a3ba22e788ef419f344e4db87a93f4e929aa203d947>
2023-05-21 06:34:39 +0000

    Update InterPal by f4b30bdb1a42599156c24a3ba22e788ef419f344e4db87a93f4e929aa203d947 at 2023-05-21T06:34:39.678Z

---
+++ f4b30bdb1a42599156c24a3ba22e788ef419f344e4db87a93f4e929aa203d947/InterPal/source_code.jsx
@@ -0,0 +1,18 @@
+return (
+  <>
+    <iframe
+      className="container-fluid vh-100 border border-primary rounded"
+      src="https://docs.google.com/presentation/d/e/2PACX-1vRbSysyomzv9vAoiS-vIIvm_I3RzE8KptjfgnhfH9ssayd1ggeTU9oqWQZ2JbvfBmLfixm-fuofwxsn/embed?start=false&loop=false&delayms=3000"
+      frameborder="0"
+      allowfullscreen="true"
+      mozallowfullscreen="true"
+      webkitallowfullscreen="true"
+    ></iframe>
+    <iframe
+      height="100%"
+      width="100%"
+      src="https://user-images.githubusercontent.com/25058545/239722918-38de6e1a-d03f-4dde-9baf-0b86635247ce.mp4"
+      title="InterPal"
+    ></iframe>
+  </>
+);
