f80dd1d5e7e05a41a481eb3643096c24554b0f06
almondjelly.near <almondjelly.near>
2023-05-21 19:36:30 +0000

    Update hearts by almondjelly.near at 2023-05-21T19:36:30.204Z

--- almondjelly.near/hearts/source_code.jsx
+++ almondjelly.near/hearts/source_code.jsx
@@ -1,7 +1,57 @@
 const contract = "almondjelly.near";
 const greeting = Near.view(contract, "get_greeting", {});
 
-return <div>{greeting} World</div>;
+// Use and manipulate state
+State.init({ new_greeting: "" });
+
+const onInputChange = ({ target }) => {
+  State.update({ new_greeting: target.value });
+};
+
+const onBtnClick = () => {
+  if (!state.new_greeting) {
+    return;
+  }
+
+  Near.call(contract, "set_greeting", {
+    greeting: state.new_greeting,
+  });
+};
+
+// Define components
+const greetingForm = (
+  <>
+    <div class="border border-black p-3">
+      <label>Update greeting</label>
+      <input placeholder="Howdy" onChange={onInputChange} />
+      <button class="btn btn-primary mt-2" onClick={onBtnClick}>
+        Save
+      </button>
+    </div>
+  </>
+);
+
+const notLoggedInWarning = (
+  <p class="text-center py-2"> Log in to change the greeting </p>
+);
+
+// Render
+return (
+  <>
+    <div class="container border border-info p-3">
+      <h3 class="text-center">
+        The contract says:
+        <span class="text-decoration-underline"> {greeting} </span>
+      </h3>
+
+      <p class="text-center py-2">
+        Look at that! A greeting stored on the NEAR blockchain.
+      </p>
+
+      {context.accountId ? greetingForm : notLoggedInWarning}
+    </div>
+  </>
+);
 
 return (
   <svg>
