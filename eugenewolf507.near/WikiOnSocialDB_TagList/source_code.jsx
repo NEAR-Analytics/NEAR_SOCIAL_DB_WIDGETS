6524cbf61ff3c276fa21ca0586606133e1433a8e
eugenewolf507.near <eugenewolf507.near>
2023-05-22 15:23:26 +0000

    Update WikiOnSocialDB_TagList by eugenewolf507.near at 2023-05-22T15:23:26.193Z

--- eugenewolf507.near/WikiOnSocialDB_TagList/source_code.jsx
+++ eugenewolf507.near/WikiOnSocialDB_TagList/source_code.jsx
@@ -6,7 +6,7 @@ return (
         <a
           key={tag}
           href={`/#/${authorForWidget}/widget/WikiOnSocialDB_ArticlesByTag?tag=${tag}`}
-          className="me-1 mb-1 text-primary bg-primary bg-opacity-10 position-relative fw-normal badge border border-primary text-decoration-none"
+          className="me-1 text-primary bg-primary bg-opacity-10 position-relative fw-normal badge border border-primary text-decoration-none"
         >
           #{tag}
         </a>
