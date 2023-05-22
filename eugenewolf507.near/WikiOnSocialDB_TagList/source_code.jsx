0e354d40b82e419c41632545f972c9e278fa4254
eugenewolf507.near <eugenewolf507.near>
2023-05-22 14:52:44 +0000

    Update WikiOnSocialDB_TagList by eugenewolf507.near at 2023-05-22T14:52:44.974Z

--- eugenewolf507.near/WikiOnSocialDB_TagList/source_code.jsx
+++ eugenewolf507.near/WikiOnSocialDB_TagList/source_code.jsx
@@ -1,1 +1,15 @@
-return <div>Hello World</div>;
+return (
+  <>
+    {props.tags
+      ? props.tags.map((tag) => (
+          <a
+            key={tag}
+            href={`/#/${authorForWidget}/widget/WikiOnSocialDB_ArticlesByTag?tag=${tag}`}
+            className="me-1 mb-1 text-primary bg-primary bg-opacity-10 position-relative fw-normal badge border border-primary text-decoration-none"
+          >
+            #{tag}
+          </a>
+        ))
+      : "Please, put array of tags as prop inside this component ) "}
+  </>
+);
