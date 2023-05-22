2431a19934157c9b27fe6221716fa22518f232ed
eugenewolf507.near <eugenewolf507.near>
2023-05-22 12:35:50 +0000

    Update WikiOnSocialDB_ArticlesByTag by eugenewolf507.near at 2023-05-22T12:35:50.734Z

--- eugenewolf507.near/WikiOnSocialDB_ArticlesByTag/source_code.jsx
+++ eugenewolf507.near/WikiOnSocialDB_ArticlesByTag/source_code.jsx
@@ -78,6 +78,15 @@ return (
                 {getDateLastEdit(article.timeLastEdit)})
               </small>
             </a>
+            {article.tags.map((tag) => (
+              <a
+                key={tag}
+                href={`/#/${authorForWidget}/widget/WikiOnSocialDB_ArticlesByTag?tag=${tag}`}
+                className="me-1 mb-1 text-primary bg-primary bg-opacity-10 position-relative fw-normal badge border border-primary text-decoration-none"
+              >
+                #{tag}
+              </a>
+            ))}
           </li>
         ))}
     </ol>
