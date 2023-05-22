274ac80c08f5b9adca46fbe2ca577445494244f5
NearSocial <NearSocial>
2023-05-22 02:54:35 +0000

    Update DAO.RemoveMember by hack.near at 2023-05-22T02:54:35.132Z

---
+++ hack.near/DAO.RemoveMember/source_code.jsx
@@ -0,0 +1,36 @@
+const memberId = props.memberId ?? context.accountId;
+const roleId = props.roleId;
+const daoId = props.daoId ?? "multi.sputnik-dao.near";
+
+const policy = Near.view(daoId, "get_policy");
+const deposit = policy.proposal_bond;
+
+const handleProposal = () => {
+  Near.call([
+    {
+      contractName: daoId,
+      methodName: "add_proposal",
+      args: {
+        proposal: {
+          description: "remove DAO member",
+          kind: {
+            RemoveMemberFromRole: {
+              member_id: memberId,
+              role: roleId ?? "council",
+            },
+          },
+        },
+      },
+      gas: 300000000000000,
+      deposit: deposit,
+    },
+  ]);
+};
+
+return (
+  <div>
+    <button className="btn btn-outline-danger" onClick={handleProposal}>
+      Propose Removal
+    </button>
+  </div>
+);
