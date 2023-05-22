a83116360acc50029003c2e782f12fd4314caf55
nathanx.near <nathanx.near>
2023-05-22 13:19:55 +0000

    Update MMC by nathanx.near at 2023-05-22T13:19:55.417Z

---
+++ nathanx.near/MMC/source_code.jsx
@@ -0,0 +1,245 @@
+const accountId = context.accountId;
+
+if (!accountId) {
+  return "Please sign in with a NEAR wallet to use this widget to become a true detective unlocking this case!🕵️‍♂️";
+}
+
+return (
+  <>
+    <div class="container min-vw-80">
+      <h1>{`Hello, ${accountId}!👋`}</h1>
+      <strong>
+        {" "}
+        Welcome to The Murder Mystery Collective Near Social Widget!🕵️‍♂️{" "}
+      </strong>
+      <br></br>
+      <hr />
+      <img
+        src="https://cdn.discordapp.com/attachments/1004449104006488066/1110170938944802887/mmc_discord_profile-image_copy.gif"
+        alt="MMCGIFLOGO"
+        width="195"
+        height="195"
+      ></img>
+      <img
+        src="https://pbs.twimg.com/media/FZZ6MnTX0AEne0J?format=jpg&name=4096x4096"
+        alt="MMCLOGO"
+        width="533"
+        height="195"
+      ></img>
+      <br></br>
+      <strong>📁 NFT - Solve to Earn</strong>
+      <br></br>
+      <br></br>
+      <h3>🕵️‍♂️---The Murder Mystery Collective</h3>
+      <a href="https://playmmc.io">🔗---MMC Website</a>
+      <a href="https://discord.gg/playmmc">🔗---MMC Discord</a>
+      <a href="https://www.mmcmarketplace.xyz/">🔗---MMC Marketplace</a>
+      <a href="https://www.tradeport.xyz/near/collection/mmc.nfts.fewandfar.near?tab=marketview">
+        🔗---MMC Detectives on Tradeport
+      </a>
+      <a href="https://www.tradeport.xyz/near/collection/mmc-pups.nfts.fewandfar.near?tab=marketview">
+        🔗---MMC Pups on Tradeport
+      </a>
+      <br></br>
+      <br></br>
+      <strong>What is The Murder Mystery Collective?</strong>
+      <p>
+        Our Murder Mystery events are played in Discord. The community works
+        together to find clues, solve puzzles, and uncover evidence to crack the
+        case. Participation in the event earns you XP, which can be used to
+        redeem prizes. In our Discord, you'll see a channel to register, and a
+        channel to catch yourself up on the backstory. If you haven't
+        participated in the previous chapters, don't worry, we got you! This is
+        a great time to jump on and help to solve the case.
+      </p>
+      <p>
+        When the event starts, there will be starting clues provided which the
+        community reveals together (reveal parties are a vibe!). Once revealed,
+        other clues will surface to further investigate.
+      </p>
+      <p>
+        After clues are revealed, the community must decide which clues should
+        be combined in order to uncover evidence, puzzles, or more details in
+        order to progress further in the game. (and sometimes traps!)
+      </p>
+      <p>
+        You don't need to be a puzzle wizard or an escape room pro in order to
+        participate, have fun, and earn XP in our events. Simply help the team
+        make decisions, join in on revealing clues, and test your Detective
+        skills at cracking the case.
+      </p>
+      <strong>Holder Benefit - ChatGPT4!</strong>
+      <p>
+        If you are a holder of an Undercover Detective or Pup you are entitled
+        to ChatGPT4!
+      </p>
+      <a href="https://discord.com/channels/1004449103012442132/1100745572429860946">
+        🤖---Ask-AI Discord Channel
+      </a>
+      <br></br>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1200px-ChatGPT_logo.svg.png"
+        alt="CHATGPTLOGO"
+        width="150"
+        height="150"
+      ></img>
+      <br></br>
+      <br></br>
+      <strong>
+        Holder Benefit - Exclusive White Coats and Blue Furs role on Discord!
+      </strong>
+      <p>
+        If you've got a White Coat Detective or Blue Undercover Pup Verify your
+        NFT in the Discord to get your roles! You will unlock exclusive channels
+        to speak to other members in the Discord server.
+      </p>
+      <a href="https://discord.com/channels/1004449103012442132/1101693061962137702">
+        🥼---The White Coats Discord Channel
+      </a>
+      <a href="https://discord.com/channels/1004449103012442132/1109476348524765235">
+        🔷---Blue Furs Discord Channel
+      </a>
+      <br></br>
+      <img
+        src="https://byzantion.mypinata.cloud/ipfs/QmcABQEkMb6sTQ8bzwh4XdtkqHLTUeTkwbwzHSbsUn7mxG/505.jpeg?img-width=850&img-height=850&img-fit=cover&img-quality=80&img-onerror=redirect&img-fit=pad&img-format=webp"
+        alt="MMCDETECTIVE"
+        width="150"
+        height="150"
+      ></img>
+      <img
+        src="https://byzantion.mypinata.cloud/ipfs/QmdwBQGzcYELCCRw3234Un9C4a5EJHncki6nsPywamwnBQ/101.png?img-width=850&img-height=850&img-fit=cover&img-quality=80&img-onerror=redirect&img-fit=pad&img-format=webp"
+        alt="MMCDPUP"
+        width="150"
+        height="150"
+      ></img>
+      <br></br>
+      <br></br>
+      <hr />
+      <h1>Partner Projects🤝</h1>
+      <i>Within the MMC Discord you will find the Partner Project channels!</i>
+      <br></br>
+      <i>
+        Below is a list of the partner projects in the order of the Discord
+        Server.
+      </i>
+      <hr />
+      <h3>💀---B💀NEHEDZ</h3>
+      <i>
+        Here they come emerge from six feet under. back from the afterlife..
+        meet the B💀NEHEDZ...!!
+      </i>
+      <br></br>
+      <img
+        src="https://byzantion.mypinata.cloud/ipfs/bafybeidflrcq2cyku3scw3wbgolpmsutdgmvy4s7k4qx6ixisrqyif6rya?img-width=300&img-height=300&img-fit=cover&img-quality=80&img-onerror=redirect&img-fit=pad&img-format=webp"
+        alt="BONEHEADZ"
+        width="150"
+        height="150"
+      ></img>
+      <img
+        src="https://byzantion.mypinata.cloud/ipfs/bafybeifetknoql6toafbhfy5bsasl5cmbwxfhvjbpnhcw7d7efuqfnjh4a?img-width=850&img-height=850&img-fit=cover&img-quality=80&img-onerror=redirect&img-fit=pad&img-format=webp"
+        alt="BONEHEADZMMCDETECTIVE"
+        width="150"
+        height="150"
+      ></img>
+      <img
+        src="https://byzantion.mypinata.cloud/ipfs/bafybeigp23kc326f6ohkmqqyagurdx3qwwyxalo5ccjp7fi74qqzlgw6im?img-width=850&img-height=850&img-fit=cover&img-quality=80&img-onerror=redirect&img-fit=pad&img-format=webp"
+        alt="BONEHEADZMMCPUP"
+        width="150"
+        height="150"
+      ></img>
+      <img
+        src="https://byzantion.mypinata.cloud/ipfs/bafybeigkqvnqq2vb2wj4jceo2mw4xmsdwarbp7opdegiwdur4xqznutk4e"
+        alt="BONEHEADZMASK"
+        width="150"
+        height="150"
+      ></img>
+      <br></br>
+      <strong>📁 NFT</strong>
+      <a href="https://discord.com/channels/1004449103012442132/1088103393714778122">
+        🔗---B💀NEHEDZ Discord Channel
+      </a>
+      <a href="https://twitter.com/bonehedz_05">🔗---B💀NEHEDZ Twitter</a>
+      <a href="https://www.tradeport.xyz/near/collection/bnehedz-by-bonehedznear?tab=marketview">
+        🔗---B💀NEHEDZ on Tradeport
+      </a>
+      <br></br>
+      <hr />
+      <h3>🫘---Beaning</h3>
+      <i>Beans just beaning around on $NEAR.</i>
+      <br></br>
+      <img
+        src="https://byzantion.mypinata.cloud/ipfs/bafybeic6tgnvxxt644gqhtv6xinyjax4halq3ilhlmany7kcl6ioy3osqq?img-width=900&img-height=900&img-fit=cover&img-quality=80&img-onerror=redirect&img-fit=pad&img-format=webp"
+        alt="BEANINGLOGO"
+        width="150"
+        height="150"
+      ></img>
+      <img
+        src="https://byzantion.mypinata.cloud/ipfs/bafybeibavngmgedkjnqngmqpsvrcjdlu3b22lk4sylel4yjiciubh7gkvy?img-width=850&img-height=850&img-fit=cover&img-quality=80&img-onerror=redirect&img-fit=pad&img-format=webp"
+        alt="BEANINGMMC"
+        width="150"
+        height="150"
+      ></img>
+      <img
+        src="https://byzantion.mypinata.cloud/ipfs/bafybeiagmmns6ecrjaw6cixho2hql4p45fxbjplhwgohassvmbaaa4cjle"
+        alt="BEANINGLJ"
+        width="150"
+        height="150"
+      ></img>
+      <img
+        src="https://byzantion.mypinata.cloud/ipfs/bafybeihbzmlkmvmuma4ocyicm37qlatgivgbpb2tummyddlgmxxb6vtn2m?img-width=850&img-height=850&img-fit=cover&img-quality=80&img-onerror=redirect&img-fit=pad&img-format=webp"
+        alt="BEANINGAV"
+        width="150"
+        height="150"
+      ></img>
+      <br></br>
+      <strong>📁 NFT</strong>
+      <a href="https://discord.com/channels/1004449103012442132/1095325310699982868">
+        🔗---Beaning Discord Channel
+      </a>
+      <a href="https://twitter.com/beaningnear">🔗---Beaning Twitter</a>
+      <a href="https://www.tradeport.xyz/near/collection/beaning-by-beaningnear?tab=marketview">
+        🔗---Beaning on Tradeport
+      </a>
+      <br></br>
+      <hr />
+      <h3>💜---Bonerz</h3>
+      <i>did ash ever become a pokemon master?👀</i>
+      <br></br>
+      <img
+        src="https://www.mintbase.xyz/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fomni-live.appspot.com%2Fo%2Fstore%252Fbonerz.mintbase1.near%253Aprofile%3Falt%3Dmedia%26token%3D6dc3dbab-5ef3-4ca3-b49a-1c30453f693b&w=3840&q=75"
+        alt="BONERZLOGO"
+        width="150"
+        height="150"
+      ></img>
+      <img
+        src="https://arweave.net/97x9LOfhtZyQR5ZMY-pIwe8ynsiBTzTBcUipl8c2haY"
+        alt="BONERZASH"
+        width="150"
+        height="150"
+      ></img>
+      <img
+        src="https://arweave.net/FYMF2wEjxqkWPTfenRKMHHZRJf1Paq9AGdVytibSmLI"
+        alt="BONERZSKATEBOARDINGNY"
+        width="150"
+        height="150"
+      ></img>
+      <img
+        src="https://arweave.net/kD9SUOwsi11rn3PwwhswlqOtk12POpgbKrvkb-SrZdM"
+        alt="BONERZITALIANSTALION"
+        width="150"
+        height="150"
+      ></img>
+      <br></br>
+      <strong>📁 NFT</strong>
+      <a href="https://discord.com/channels/1004449103012442132/1108838928653484212">
+        🔗---Bonerz Discord Channel
+      </a>
+      <a href="https://twitter.com/NearBonerz">🔗---Bonerz Twitter</a>
+      <a href="https://www.tradeport.xyz/near/collection/bonerz.mintbase1.near?tab=marketview">
+        🔗---Bonerz on Tradeport
+      </a>
+      <br></br>
+      <hr />
+    </div>
+  </>
+);
