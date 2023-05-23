5a3221d9dfbe2180b7afbd3bc47b689143fce1b3
simplar.near <simplar.near>
2023-05-23 13:16:48 +0000

    Update CommunityWidgets by simplar.near at 2023-05-23T13:16:48.442Z

--- simplar.near/CommunityWidgets/source_code.jsx
+++ simplar.near/CommunityWidgets/source_code.jsx
@@ -1,57 +1,98 @@
+const componentsUrl = "#/near/widget/ComponentsPage";
+//bozon.near/widget/WidgetHistory
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+const H2 = styled.h2`
+  font-size: 19px;
+  line-height: 22px;
+  color: #11181c;
+  margin: 0;
+`;
+
+const Items = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+`;
+
+const Item = styled.div``;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+`;
+
+const TextLink = styled.a`
+  color: #006adc;
+  outline: none;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 20px;
+
+  &:hover,
+  &:focus {
+    color: #006adc;
+    text-decoration: underline;
+  }
+`;
+
 return (
-    <>
-    <div class="position-relative small">
-      <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item">
-          <a href="#" class="nav-link" aria-current="page">
-            <i class="bi bi-house me-1" />
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link link-dark">
-            <i class="bi bi-mortarboard me-1" />
-            Learn
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link link-dark">
-            <i class="bi bi-calendar-date me-1" />
-            Events
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="nav-link link-dark btn-toggle"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapse"
-            aria-expanded="true"
-          >
-            <i class="bi bi-chat-right-text me-1" />
-            Discuss
-          </a>
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 ps-4 small collapse show"
-              id="collapse" >
-            <li>
-              <a href="#" class="nav-link link-dark">
-                Overview
-              </a>
-            </li>
-            <li>
-              <a href="#" class="nav-link link-dark">
-                Updates
-              </a>
-            </li>
-            <li>
-              <a href="#" class="nav-link link-dark">
-                Reports
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      </div>
-    </>
-  );
-  
+  <Wrapper>
+    <Header>
+      <H2>Featured Components</H2>
+      <TextLink href={componentsUrl}>View All</TextLink>
+    </Header>
+
+    <Items>
+      <Item>
+        <Widget
+          src="near/widget/ComponentCard"
+          props={{ src: "devgovgigs.near/widget/Ideas" }}
+        />
+      </Item>
+      <Item>
+        <Widget
+          src="near/widget/ComponentCard"
+          props={{ src: "let45fc.near/widget/LaserChess3D" }}
+        />
+      </Item>
+      <Item>
+        <Widget
+          src="near/widget/ComponentCard"
+          props={{ src: "zavodil.near/widget/social-avatar-editor" }}
+        />
+      </Item>
+      <Item>
+        <Widget
+          src="near/widget/ComponentCard"
+          props={{ src: "frichard2.near/widget/most-active-contracts" }}
+        />
+      </Item>
+      <Item>
+        <Widget
+          src="near/widget/ComponentCard"
+          props={{ src: "mob.near/widget/Explorer" }}
+        />
+      </Item>
+      <Item>
+        <Widget
+          src="near/widget/ComponentCard"
+          props={{ src: "microchipgnu.near/widget/Game.FlappyBOS" }}
+        />
+      </Item>
+      <Item>
+        <Widget
+          src="near/widget/ComponentCard"
+          props={{ src: "mintbase.near/widget/nft-marketplace" }}
+        />
+      </Item>
+    </Items>
+  </Wrapper>
+);
