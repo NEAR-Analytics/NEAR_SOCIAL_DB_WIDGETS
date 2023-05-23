30c20c82c58c8e0ae2f5ed42af8a2997bd67a9a4
simplar.near <simplar.near>
2023-05-23 16:10:24 +0000

    Update CommunityWidgets by simplar.near at 2023-05-23T16:10:24.892Z

--- simplar.near/CommunityWidgets/source_code.jsx
+++ simplar.near/CommunityWidgets/source_code.jsx
@@ -1,16 +1,9 @@
-const componentsUrl = "#/near/widget/ComponentsPage";
+const widgetList = props.widgetList || [];
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 24px;
-`;
-
-const H2 = styled.h2`
-  font-size: 19px;
-  line-height: 22px;
-  color: #11181c;
-  margin: 0;
 `;
 
 const Items = styled.div`
@@ -21,41 +14,16 @@ const Items = styled.div`
 
 const Item = styled.div``;
 
-const Header = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 12px;
-`;
-
-const TextLink = styled.a`
-  color: #006adc;
-  outline: none;
-  font-weight: 600;
-  font-size: 12px;
-  line-height: 20px;
-
-  &:hover,
-  &:focus {
-    color: #006adc;
-    text-decoration: underline;
-  }
-`;
+if (widgetList.length == 0) { return "Please define widgets for the community" }
 
 return (
   <Wrapper>
     <Items>
-      <Item>
-        <Widget
-          src="mob.near/widget/Explorer"
-        />
-      </Item>
-      <Item>
-        <Widget
-          src="microchipgnu.near/widget/Game.FlappyBOS"
-        />
-      </Item>
-      
+      {
+        widgetList.map(widgetSrc => 
+          <Item> <Widget src={widgetSrc} /> </Item>
+        )
+      }
     </Items>
   </Wrapper>
 );
