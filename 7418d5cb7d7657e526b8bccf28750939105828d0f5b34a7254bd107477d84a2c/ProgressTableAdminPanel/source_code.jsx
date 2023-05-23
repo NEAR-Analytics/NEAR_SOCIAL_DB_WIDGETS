5934414d12ad0552f1ab07b81da7263fdc7acf27
MilanPoznan <MilanPoznan>
2023-05-23 08:37:50 +0000

    Update ProgressTableAdminPanel by 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c at 2023-05-23T08:37:50.682Z

--- 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableAdminPanel/source_code.jsx
+++ 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableAdminPanel/source_code.jsx
@@ -76,12 +76,13 @@ function addItem() {
 }
 
 function removeItemFromState(key) {
-  let newObjState = { ...myState.allItems };
-  delete newObjState[key];
+  // const newObjState = myState.allItems;
+  // newObjState[key] = null;
 
   State.update({
-    allItems: newObjState,
+    allItems: { ...myState.allItems, [key]: null },
   });
+  console.log("newObjState", myState.allItems);
 }
 
 function uploadData() {
@@ -125,24 +126,27 @@ return (
     <h1>Existing Items</h1>
     <ItemsListWrapper>
       {myState.allItems &&
-        Object.entries(myState.allItems).map((item, index) => (
-          <SingleItem>
-            <p>
-              {item[0]} : {item[1]}
-            </p>
-            <RemoveItemBtn onClick={() => removeItemFromState(item[0])}>
-              +
-            </RemoveItemBtn>
-            <svg
-              onClick={() => openModal(item)}
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
-            >
-              <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z" />
-            </svg>
-          </SingleItem>
-        ))}
+        Object.entries(myState.allItems).map(
+          (item, index) =>
+            item[1] !== null && (
+              <SingleItem>
+                <p>
+                  {item[0]} : {item[1]}
+                </p>
+                <RemoveItemBtn onClick={() => removeItemFromState(item[0])}>
+                  +
+                </RemoveItemBtn>
+                <svg
+                  onClick={() => openModal(item)}
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z" />
+                </svg>
+              </SingleItem>
+            )
+        )}
     </ItemsListWrapper>
 
     <FormWrapper>
