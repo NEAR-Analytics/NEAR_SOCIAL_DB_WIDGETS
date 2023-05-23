2d26d9dfdb57d0ccc637a29f74d9c8b940298666
elliotBraem <elliotBraem>
2023-05-23 02:46:07 +0000

    Update Every.Thing by efiz.near at 2023-05-23T02:46:07.524Z

--- efiz.near/Every.Thing/source_code.jsx
+++ efiz.near/Every.Thing/source_code.jsx
@@ -1,23 +1,51 @@
-const creatorId = props.creatorId || context.accountId;
+const path = props.path;
 const blockHeight = props.blockHeight || "final";
-const thingId = props.thingId || "**";
 
-const value = Social.get(`${creatorId}/thing/${thingId}`, blockHeight);
-const keys = Object.keys(value);
+// */thing/**
+const value = Social.get("*/thing/**", blockHeight);
+
+function convertToPaths(obj, parentPath) {
+  parentPath = parentPath || "";
+  var paths = [];
+
+  var keys = Object.keys(obj);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    var path = parentPath;
+    if (path !== "") {
+      path += "/";
+    }
+    path += key;
+
+    paths.push(path);
+
+    if (typeof obj[key] === "object") {
+      var nestedPaths = convertToPaths(obj[key], path);
+      paths = paths.concat(nestedPaths);
+    }
+  }
+
+  return paths;
+}
+
+const paths = convertToPaths(value);
 
 const renderThing = (key) => {
-  const path = `${creatorId}/thing/${key}`;
+  console.log(key);
+
   return (
     <Widget
       src="efiz.near/widget/Every.Thing.View"
-      props={{ path, blockHeight }}
+      props={{ path: key, blockHeight }}
     />
   );
 };
 
+// <Widget src="efiz.near/widget/Every.Thing.Create" />
+// TODO: Infinite Scroll
 return (
   <div>
-    {keys.map((it) => {
+    {paths.map((it) => {
       return <div>{renderThing(it)}</div>;
     })}
   </div>
