d125c4bef25cc22a6c13b13b07c34fc5bab4c9f3
simplar.near <simplar.near>
2023-05-23 13:42:51 +0000

    Update Main by simplar.near at 2023-05-23T13:42:51.239Z

--- simplar.near/Main/source_code.jsx
+++ simplar.near/Main/source_code.jsx
@@ -1,9 +1,9 @@
 return (
   <div class="row">
-    <div class="col-3 border-right">
+    <div class="col-3">
       <Widget src="simplar.near/widget/Menu" />
     </div>
-    <div class="col-6">
+    <div class="col-auto">
       <Widget src="near/widget/NestedDiscussions" props={{identifier: "testo"}} />
     </div>
     <div class="col-3">
