88c0e549d11df81c929e3f65485635f7802bcc5b
simplar.near <simplar.near>
2023-05-23 10:59:48 +0000

    Update Menu by simplar.near at 2023-05-23T10:59:48.875Z

---
+++ simplar.near/Menu/source_code.jsx
@@ -0,0 +1,56 @@
+return (
+  <>
+  <div class="position-relative small">
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link" aria-current="page">
+          <i class="bi bi-house me-1" />
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link link-dark">
+          <i class="bi bi-mortarboard me-1" />
+          Learn
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link link-dark">
+          <i class="bi bi-calendar-date me-1" />
+          Events
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="nav-link link-dark btn-toggle"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapse"
+          aria-expanded="true"
+        >
+          <i class="bi bi-chat-right-text me-1" />
+          Discuss
+        </a>
+        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 ps-4 small collapse show"
+            id="collapse" >
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Overview
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Updates
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Reports
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    </div>
+  </>
+);
