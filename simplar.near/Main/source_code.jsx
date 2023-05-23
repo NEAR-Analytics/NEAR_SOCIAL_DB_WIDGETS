dbc3558d2be80a8fa623d8c8faf15c296105ba45
simplar.near <simplar.near>
2023-05-23 16:10:16 +0000

    Update Main by simplar.near at 2023-05-23T16:10:16.985Z

--- simplar.near/Main/source_code.jsx
+++ simplar.near/Main/source_code.jsx
@@ -1,13 +1,36 @@
+const widgetList = [
+  "mob.near/widget/Explorer",
+  "microchipgnu.near/widget/Game.FlappyBOS"
+]
+
+const homePage = "near/widget/NearOrg.HomePage"
+const topics = ["overview", "updates", "reports"]
+
+State.init({ src: homePage, props: {} });
+
+
+function onSelect(src, prs) {
+  console.log("ya", src, prs);
+  State.update({ src, props: prs });
+}
+
 return (
   <div class="row">
     <div class="col-auto">
-      <Widget src="simplar.near/widget/Menu" />
+      <Widget src="simplar.near/widget/Menu"
+        props={{
+          identifier,
+          homePage,
+          topics,
+          onSelect
+        }}
+      />
     </div>
     <div class="col-7 mx-auto">
-      <Widget src="near/widget/NestedDiscussions" props={{identifier: "testo"}} />
+      <Widget src={state.src} props={state.props} />
     </div>
     <div class="col-3">
-      <Widget src="simplar.near/widget/CommunityWidgets" />
+      <Widget src="simplar.near/widget/CommunityWidgets" props={{ widgetList }} />
     </div>
   </div>
 )
