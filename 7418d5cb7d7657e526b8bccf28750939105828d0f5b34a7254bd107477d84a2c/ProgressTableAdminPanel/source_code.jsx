c7d05a6d43fac2193f5ff02a361d975a2791bac6
MilanPoznan <MilanPoznan>
2023-05-23 14:27:52 +0000

    Update ProgressTableAdminPanel by 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c at 2023-05-23T14:27:52.597Z

--- 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableAdminPanel/source_code.jsx
+++ 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableAdminPanel/source_code.jsx
@@ -14,6 +14,7 @@ const ItemsListWrapper = styled.div`
 const SingleItem = styled.div`
 position: relative;
 display: flex;
+opacity: ${({ isActive }) => (isActive ? 1 : 0.5)};
 p {
   margin-right: 20px;
 }
@@ -32,6 +33,10 @@ const RemoveItemBtn = styled.div`
  
 `;
 
+const EditSvg = styled.svg`
+    cursor: pointer;
+`;
+
 const EditModal = styled.div`
   display: ${({ isOpen }) => (isOpen ? "block" : "none")};
 `;
@@ -47,6 +52,7 @@ const myState = State.init({
   key: "",
   value: "",
   allItems: items,
+  hiddenItems: {},
   editKey: "",
   editValue: "",
   isModalOpen: false,
@@ -110,6 +116,8 @@ function changeItemInState() {
     editValue: "",
   });
 }
+
+console.log("TEST");
 return (
   <DashboardWrapper>
     <EditModal isOpen={myState.isModalOpen}>
@@ -126,27 +134,27 @@ return (
     <h1>Existing Items</h1>
     <ItemsListWrapper>
       {myState.allItems &&
-        Object.entries(myState.allItems).map(
-          (item, index) =>
-            item[1] !== null && (
-              <SingleItem>
-                <p>
-                  {item[0]} : {item[1]}
-                </p>
-                <RemoveItemBtn onClick={() => removeItemFromState(item[0])}>
-                  +
-                </RemoveItemBtn>
-                <svg
-                  onClick={() => openModal(item)}
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z" />
-                </svg>
-              </SingleItem>
-            )
-        )}
+        Object.entries(myState.allItems).map((item, index) => (
+          <SingleItem isActive={item[1] !== null}>
+            <p>
+              {item[0]} : {item[1]}
+            </p>
+            {item[1] !== null && (
+              <RemoveItemBtn onClick={() => removeItemFromState(item[0])}>
+                +
+              </RemoveItemBtn>
+            )}
+
+            <EditSvg
+              onClick={() => openModal(item)}
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+            >
+              <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z" />
+            </EditSvg>
+          </SingleItem>
+        ))}
     </ItemsListWrapper>
 
     <FormWrapper>
@@ -167,7 +175,10 @@ return (
       />
       <div>
         <button onClick={addItem}>Add item</button>
-        <CommitButton data={{ testWidget: myState.allItems }}>
+        <CommitButton
+          data={{ testWidget: myState.allItems }}
+          onCommit={onCommit}
+        >
           Upload data
         </CommitButton>
         <button onClick={() => uploadData()}>Upload data</button>
