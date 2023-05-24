b217c96593b527610052399f73ec0bef5008018d
MilanPoznan <MilanPoznan>
2023-05-24 13:03:33 +0000

    Update ProgressTableRender by 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c at 2023-05-24T13:03:33.548Z

--- 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableRender/source_code.jsx
+++ 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableRender/source_code.jsx
@@ -1,24 +1,27 @@
 const { widgetType, hasBackground } = props;
+let defaultType = 1;
 
 if (widgetType !== 1 && widgetType !== 2 && widgetType !== 3) {
-  return `widgetType prop is mandatory and it should be 1, 2 or 3`;
+  defaultType = 1;
+} else {
+  defaultType = widgetType;
 }
 
 return (
   <div>
-    {widgetType === 1 && (
+    {defaultType === 1 && (
       <Widget
         src="7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressWidgetTable"
         props={{ hasBackground: hasBackground }}
       />
     )}
-    {widgetType === 2 && (
+    {defaultType === 2 && (
       <Widget
         src="7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressWidgetTable-V2"
         props={{ hasBackground: hasBackground }}
       />
     )}
-    {widgetType === 3 && (
+    {defaultType === 3 && (
       <Widget
         src="7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressWidgetTable-V3"
         props={{ hasBackground: hasBackground }}
