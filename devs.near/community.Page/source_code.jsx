0c2ca9a6cb5a110796cc906316b36f4b7d1df757
near <near>
2023-05-24 18:47:42 +0000

    Update community.Page by devs.near at 2023-05-24T18:47:42.154Z

---
+++ devs.near/community.Page/source_code.jsx
@@ -0,0 +1,95 @@
+const communities = {
+  regional: {
+    overviewId: 397,
+    icon: "https://ipfs.near.social/ipfs/bafkreiajwq6ep3n7veddozji2djv5vviyisabhycbweslvpwhsoyuzcwi4",
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreihgxg5kwts2juldaeasveyuddkm6tcabmrat2aaq5u6uyljtyt7lu",
+    title: "Regional",
+  },
+  defi: {
+    overviewId: 412,
+    icon: "https://ipfs.near.social/ipfs/bafkreidpitdafcnhkp4uyomacypdgqvxr35jtfnbxa5s6crby7qjk2nv5a",
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreicg4svzfz5nvllomsahndgm7u62za4sib4mmbygxzhpcl4htqwr4a",
+    title: "DeFi",
+  },
+  nft: {
+    overviewId: 416,
+    icon: "https://ipfs.near.social/ipfs/bafkreie2eaj5czmpfe6pe53kojzcspgozebdsonffwvbxtpuipnwahybvi",
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreiehzr7z2fhoqqmkt3z667wubccbch6sqtsnvd6msodyzpnf72cszy",
+    title: "NFTs",
+  },
+  gaming: {
+    overviewId: 414,
+    icon: "https://ipfs.near.social/ipfs/bafkreiepgdnu7soc6xgbyd4adicbf3eyxiiwqawn6tguaix6aklfpir634",
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreiaowjqxds24fwcliyriintjd4ucciprii2rdxjmxgi7f5dmzuscey",
+    title: "Gaming",
+  },
+};
+
+const Gradient = styled.div`
+   {
+    margin-top: -25px;
+    margin-bottom: 25px;
+    height: 250px;
+    text-align: center;
+    background: radial-gradient(
+      circle,
+      rgba(29, 55, 57, 1) 30%,
+      rgba(24, 24, 24, 1) 80%
+    );
+
+    font-family: Arial, sans-serif;
+  }
+
+  .text-primary-gradient {
+    color: #53fdca;
+    -webkit-text-fill-color: transparent;
+    background-image: linear-gradient(#8e76ba, #1ed2f0);
+    -webkit-background-clip: text;
+    background-clip: text;
+  }
+
+  .subtitle-above {
+    font-size: 18px;
+    letter-spacing: 1px;
+    font-family: Courier, monospace;
+  }
+
+  .subtitle-below {
+    font-size: 16px;
+  }
+
+  .slogan {
+    font-weight: 600;
+    font-size: 60px;
+  }
+`;
+
+return (
+  <>
+    <Gradient className="d-flex flex-column justify-content-center">
+      <div class="subtitle-above text-white opacity-75 mb-2">
+        A decentralized constellation of
+      </div>
+      <h1 class="mb-3 text-white slogan">
+        <span class="text-primary-gradient">NEAR </span>Work Groups
+      </h1>
+      <div class="subtitle-below text-white opacity-75">
+        Share ideas, connect with collaborators, and get more involved!
+      </div>
+    </Gradient>
+    <div class="h5 pb-3">Featured Communities</div>
+    <div class="row">
+      {Object.entries(communities).map(([label, community]) => {
+        return (
+          <div class="col">
+            <Widget src="devgovgigs.near/widget/gigs-board.components.community.FeaturedCommunity" />
+          </div>
+        );
+      })}
+    </div>
+  </>
+);
