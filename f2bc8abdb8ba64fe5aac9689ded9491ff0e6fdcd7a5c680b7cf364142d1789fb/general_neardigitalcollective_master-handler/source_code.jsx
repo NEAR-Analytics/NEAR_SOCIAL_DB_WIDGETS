7f235b7f38527d2095439039faa97d82c7e3727e
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-05-24 00:24:41 +0000

    Update general_neardigitalcollective_master-handler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-05-24T00:24:41.266Z

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/general_neardigitalcollective_master-handler/source_code.jsx
@@ -0,0 +1,412 @@
+/*----------------------------------------------Start get props----------------------------------------------*/
+const dataCallInfo = props.dataCallInfo ?? ["poll_question", "question-v3.1.1"];
+
+const data = Social.index(dataCallInfo[0], dataCallInfo[1]);
+if (!data) {
+  return "Loading datas";
+}
+
+const widgetOwner =
+  props.widgetOwner ??
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+
+const tabs = props.tabs ?? {
+  MY_SCHEDULE: {
+    id: 1,
+    text: "My Schedule",
+    isTab: true,
+    widgetName: "general_neardigitalcollective_cards-container",
+    props: { isOwnAccountId: true, sectionText: "My Schedule", isTab: true },
+  },
+  ALL_SCHEDULE: {
+    id: 2,
+    text: "All Schedules",
+    isTab: true,
+    widgetName: "general_neardigitalcollective_cards-container",
+    props: {
+      isOwnAccountId: false,
+      sectionTtext: "All Schedules",
+      isTab: true,
+    },
+  },
+  NEW_SCHEDULE: {
+    id: 3,
+    text: finalData ? "Edit Schedule" : "Create Schedule",
+    isButtonInNavegation: true,
+    widgetName: "Instance_time_edit",
+    props: { isButtonInNavegation: true },
+  },
+  OPEN_SCHEDULE: {
+    id: 4,
+    text: "",
+    isCardNavigate: true,
+    widgetName: "Instance_time_card",
+    props: { isCardNavigate: true },
+  },
+};
+
+const cardHeaderWidgetName =
+  props.cardHeaderWidgetName ?? "minimalistQuestionHeader";
+const cardContentWidgetName =
+  props.cardContentWidgetName ?? "minimalistQuestionGeneralInfo";
+
+const homePageId = props.homePageId ?? 1;
+
+const abortPopupTexts = props.abortPopupTexts ?? {
+  abortInfoText: "If you leave now, you will lose all your changes",
+  confirmAbortText: "Discard changes",
+  cancelAbortText: "Continue editing",
+};
+
+const bootstrapIcons = props.bootstrapIcons ?? {
+  closeIcon: "bi bi-x-lg",
+  logoIcon: "bi bi-calendar-week-fill",
+  newScheduleButtonInnerIcon: "bi bi-plus-lg",
+};
+
+const logoIconText = props.logoIconText ?? "WeeklySchedule";
+/*-----------------------------------------------End get props-----------------------------------------------*/
+
+/*----------------------------------------------Start styles----------------------------------------------*/
+const colors = props.siteColors ?? {
+  color1: "#010A2D",
+  color2: "white",
+  color3: "rgb(1, 10, 45)",
+  green1: "rgb(217, 252, 239)",
+  green2: "rgb(0, 179, 125)",
+  green3: "#7e7e7e70",
+  red1: "rgb(255, 229, 229)",
+  red2: "rgb(255, 71, 71)",
+  red3: "#FF4747",
+  red4: "#FFE5E5",
+  grey1: "#353A40",
+  grey2: "#767B8E",
+  grey3: "#4B516A",
+  grey4: "#B0B3BE",
+  grey5: "rgb(230, 230, 230)",
+  grey6: "#474D55",
+  grey7: "rgb(53, 58, 64)",
+  grey8: "rgb(225, 233, 240)",
+  grey9: "rgb(71, 77, 85)",
+  grey10: "rgb(118, 123, 142)",
+  lightBlue: "#AAC8F7",
+  lightBlue2: "#E1E9F0",
+};
+
+const standardButtonStyles = props.standardButtonStyles ?? {
+  border: "2px solid transparent",
+  fontWeight: "500",
+  fontSize: "1rem",
+  margin: "0",
+  padding: "0.3rem 1.5rem",
+  backgroundColor: colors.color1,
+  borderRadius: "12px",
+  color: colors.color2,
+};
+
+const standardButtonHoveringStyles = props.standardButtonHoveringStyles ?? {
+  border: "2px solid black",
+  color: "black",
+  backgroundColor: colors.color2,
+  fontWeight: "500",
+  fontSize: "1rem",
+  margin: "0",
+  padding: "0.3rem 1.5rem",
+  borderRadius: "12px",
+};
+
+const thisWidgetInlineStyles = {
+  generalContainer: {
+    backgroundColor: colors.grey5,
+    fontFamily: "Onest",
+    fontStyle: "normal",
+    borderRadius: "20px",
+  },
+  siteHeader: {
+    backgroundColor: colors.color2,
+    boxShadow: "rgba(43, 68, 106, 0.04) 0px 4px 28px",
+  },
+  logoTitle: {
+    margin: "0px 0.5rem",
+    fontWeight: "700",
+    fontSize: "1.3rem",
+    letterSpacing: "0.1px",
+  },
+  openScheduleGeneralContainer: { margin: "0px 4rem" },
+  openScheduleContainer: { marginTop: "0.6rem" },
+  tabContainer: {
+    marginRight: "1.5rem",
+    position: "relative",
+    cursor: "pointer",
+    userSelect: "none",
+  },
+  tabText: {
+    fontWeight: "500",
+    fontSize: "1rem",
+    margin: "0",
+  },
+  decorativeTabUnderline: {
+    height: "0.2rem",
+    width: "50%",
+    position: "absolute",
+    bottom: "-55%",
+    left: "25%",
+    backgroundColor: colors.color1,
+    borderRadius: "8px",
+  },
+  newScheduleButton: standardButtonStyles,
+  newScheduleButtonHovering: standardButtonHoveringStyles,
+  newScheduleButtonInnerIcon: { color: colors.color2 },
+  newScheduleButtonInnerIconHovered: { color: "black" },
+  showUserInfoInHeaderText: { margin: "0px", fontSize: "0.8rem" },
+};
+
+/*-----------------------------------------------End styles-----------------------------------------------*/
+
+/*----------------------------------------------Start functions----------------------------------------------*/
+
+State.init({
+  tab: homePageId,
+  prevTab: homePageId,
+  hoveringElement: "",
+  showAbort: false,
+  abortThroughPage: homePageId,
+});
+
+let sortedData =
+  data && data.length
+    ? data.sort((d1, d2) => d2.blockHeight - d1.blockHeight)
+    : [];
+
+const profile = Social.getr(`${context.accountId}/profile`);
+if (!profile) {
+  return "Loading profile";
+}
+
+let accountIds = ["All"];
+let finalData = undefined;
+for (let i = 0; i < sortedData.length; i++) {
+  if (accountIds.indexOf(sortedData[i].accountId) < 0) {
+    accountIds.push(sortedData[i].accountId);
+    if (sortedData[i].accountId == context.accountId) {
+      finalData = sortedData[i];
+    }
+  }
+}
+
+function handlerStateUpdate(obj) {
+  State.update(obj);
+}
+
+let prevTab = state.prevTab;
+
+function isCardNavigate() {
+  let isCardNavigate;
+  Object.keys(tabs).forEach((tabKey) => {
+    const tab = tabs[tabKey];
+
+    if (state.tab == tab.id) {
+      isCardNavigate = tab.isCardNavigate;
+    }
+  });
+
+  return isCardNavigate;
+}
+
+function isOnNavTab() {
+  let isOnNavTab = false;
+  Object.keys(tabs).forEach((tabKey) => {
+    if (!isOnNavTab) {
+      const tab = tabs[tabKey];
+      if (tab.isTab && state.tab == tab.id) {
+        isOnNavTab = true;
+      }
+    }
+  });
+
+  return isOnNavTab;
+}
+
+/*-----------------------------------------------End functions-----------------------------------------------*/
+
+/*----------------------------------------------Start render components----------------------------------------------*/
+
+const renderAbortPollCreationModal = () => {
+  return (
+    <Widget
+      src={`${widgetOwner}/widget/general_neardigitalcollective_standard-abort-popup`}
+      props={{
+        showAbort: state.showAbort,
+        handlerStateUpdate,
+        abortThroughPage: state.abortThroughPage,
+        abortInfoText: abortPopupTexts.abortInfoText,
+        confirmAbortText: abortPopupTexts.confirmAbortText,
+        cancelAbortText: abortPopupTexts.cancelAbortText,
+        closeIcon: bootstrapIcons.closeIcon,
+        colors,
+      }}
+    />
+  );
+};
+
+const renderNavigationButton = (isMobile) => {
+  return (
+    <button
+      className={isMobile ? "d-md-none d-block mb-3" : ""}
+      onMouseEnter={() => {
+        State.update({ hoveringElement: tab.text });
+      }}
+      onMouseLeave={() => {
+        State.update({ hoveringElement: "" });
+      }}
+      onClick={() => {
+        State.update({ tab: tab.id });
+      }}
+      style={
+        state.hoveringElement == tab.text || state.tab == tab.id
+          ? thisWidgetInlineStyles.newScheduleButtonHovering
+          : thisWidgetInlineStyles.newScheduleButton
+      }
+    >
+      <i
+        className={bootstrapIcons.newScheduleButtonInnerIcon}
+        style={
+          state.hoveringElement == tab.text || state.tab == tab.id
+            ? thisWidgetInlineStyles.newScheduleButtonInnerIconHovered
+            : thisWidgetInlineStyles.newScheduleButtonInnerIcon
+        }
+      ></i>
+      {tab.text}
+    </button>
+  );
+};
+
+const renderTabContent = (tab) => {
+  return (
+    <Widget
+      src={`${widgetOwner}/widget/${tab.widgetName}`}
+      props={{
+        ...tab.props,
+        widgetOwner,
+        accountId: state.userScheduleShown,
+        tabs,
+        prevTab,
+        handlerStateUpdate,
+        data,
+        headerWidgetName,
+        contentWidgetName,
+      }}
+    />
+  );
+};
+/*-----------------------------------------------End render components-----------------------------------------------*/
+return (
+  <div className="pb-5" style={thisWidgetInlineStyles.generalContainer}>
+    <div className="d-flex flex-column">
+      <div
+        className="d-flex flex-column"
+        style={thisWidgetInlineStyles.siteHeader}
+      >
+        <div
+          className="d-flex align-items-center"
+          onClick={() => State.update({ tab: homePageId })}
+        >
+          <i className={bootstrapIcons.logoIcon}></i>
+          {logoIconText && (
+            <h3 style={thisWidgetInlineStyles.logoTitle}>{logoIconText}</h3>
+          )}
+        </div>
+
+        {!isCardNavigate() && (
+          <div
+            className="w-100 d-none d-md-flex justify-content-between"
+            style={thisWidgetInlineStyles.openScheduleGeneralContainer}
+          >
+            <div style={thisWidgetInlineStyles.openScheduleContainer}>
+              <div className="d-flex">
+                {Object.keys(tabs).map((tabKey) => {
+                  const tab = tabs[tabKey];
+                  if (tab.isTab) {
+                    return (
+                      <div style={thisWidgetInlineStyles.tabContainer}>
+                        <p
+                          ariaCurrent="page"
+                          onMouseEnter={() => {
+                            State.update({ hoveringElement: tab.text });
+                          }}
+                          onMouseLeave={() => {
+                            State.update({ hoveringElement: "" });
+                          }}
+                          onClick={() => {
+                            isOnNavTab()
+                              ? State.update({ tab: tab.id })
+                              : State.update({
+                                  showAbort: true,
+                                });
+                          }}
+                          style={thisWidgetInlineStyles.tabText}
+                        >
+                          {tab.text}
+                        </p>
+                        {(state.hoveringElement == tab.text ||
+                          state.tab == tab.id) && (
+                          <div
+                            style={
+                              thisWidgetInlineStyles.decorativeTabUnderline
+                            }
+                          >
+                            {/*Decorative Div, do not delete*/}
+                          </div>
+                        )}
+                      </div>
+                    );
+                  } else {
+                    return <></>;
+                  }
+                })}
+              </div>
+            </div>
+
+            {Object.keys(tabs).map((tabKey) => {
+              const tab = tabs[tabKey];
+              if (tab.isButtonInNavegation) {
+                renderNavigationButton(false);
+              } else {
+                return <></>;
+              }
+            })}
+          </div>
+        )}
+      </div>
+
+      <div className="w-100 d-flex flex-row justify-content-between align-items-center">
+        {/*Decorative div. Do not delete*/}
+      </div>
+      <div style={thisWidgetInlineStyles.newShceduleButtonInMobileContainer}>
+        {Object.keys(tabs).map((tabKey) => {
+          const tab = tabs[tabKey];
+          if (tab.isButtonInNavegation) {
+            renderNavigationButton(true);
+          } else {
+            return <></>;
+          }
+        })}
+      </div>
+    </div>
+
+    <div className="align-items-center">
+      {Object.keys(tabs).map((tabKey) => {
+        let tab = tabs[tabKey];
+        if (
+          (tab.isTab || tab.isButtonInNavegation || tab.isCardNavigate) &&
+          state.tab == tab.id
+        ) {
+          renderTabContent(tab);
+        } else {
+          return <></>;
+        }
+      })}
+    </div>
+    {state.showAbort && renderAbortPollCreationModal()}
+  </div>
+);
