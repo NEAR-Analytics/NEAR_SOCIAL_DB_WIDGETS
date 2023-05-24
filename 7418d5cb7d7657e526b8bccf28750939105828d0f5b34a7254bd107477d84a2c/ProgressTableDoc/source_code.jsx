69f955f89c8f8e5e988a192c0d57685dd10655f5
MilanPoznan <MilanPoznan>
2023-05-24 08:30:53 +0000

    Update ProgressTableDoc by 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c at 2023-05-24T08:30:53.031Z

---
+++ 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableDoc/source_code.jsx
@@ -0,0 +1,52 @@
+const MainWrapper = styled.div`
+
+`;
+return (
+  <MainWrapper>
+    <h1>List and doc for all Progress Table Widgets</h1>
+    <p>
+      <a
+        href="https://near.social/#/7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressTableRender"
+        target="_blank"
+      >
+        ProgressTableRender
+      </a>{" "}
+      widget has 2 props.
+    </p>
+    <p>First one is widgetType and it can be 1, 2 or 3</p>
+    <p>Second one is hasBackground, it is boolean </p>
+
+    <h2>First type</h2>
+    <Widget
+      src="7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressTableRender"
+      props={{ widgetType: 1, hasBackground: true }}
+    />
+    <h3> No Background</h3>
+    <Widget
+      src="7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressTableRender"
+      props={{ widgetType: 1, hasBackground: false }}
+    />
+
+    <h2>Second type </h2>
+    <Widget
+      src="7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressTableRender"
+      props={{ widgetType: 2, hasBackground: true }}
+    />
+    <h3> No Background</h3>
+    <Widget
+      src="7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressTableRender"
+      props={{ widgetType: 2, hasBackground: false }}
+    />
+
+    <h2>Third type </h2>
+    <Widget
+      src="7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressTableRender"
+      props={{ widgetType: 3, hasBackground: true }}
+    />
+    <h3> No Background</h3>
+    <Widget
+      src="7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressTableRender"
+      props={{ widgetType: 3, hasBackground: false }}
+    />
+  </MainWrapper>
+);
