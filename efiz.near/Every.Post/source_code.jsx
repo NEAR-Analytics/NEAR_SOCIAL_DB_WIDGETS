26f18194118c0723c39b7e10424376cf12b86534
elliotBraem <elliotBraem>
2023-05-25 18:34:07 +0000

    Update Every.Post by efiz.near at 2023-05-25T18:34:07.539Z

--- efiz.near/Every.Post/source_code.jsx
+++ efiz.near/Every.Post/source_code.jsx
@@ -1,15 +1,15 @@
 let index = [];
 const typeWhitelist = [
   "md",
+  "social",
   "efiz.near/type/paragraph",
   "efiz.near/type/Image",
   "efiz.near/type/document",
 ];
 const accountFilter =
   (props.accountFilter && [props.accountFilter]) || undefined;
-// const hashtagFilter = ["VNArtistsDAO"];
-
-const key = ""; // this could dictate the version... maybe it's the path? efiz.near/thing/1234
+const hashtagFilter = ["dev"];
+const domainFilter = ["apple123456", "elderberry123456"];
 
 if (hashtagFilter.length) {
   index = hashtagFilter.map((it) => ({
@@ -22,15 +22,15 @@ if (hashtagFilter.length) {
     },
   }));
 } else {
-  index = {
-    action: "post",
-    key: "main",
+  index = domainFilter.map((it) => ({
+    action: it,
+    key: "main", // what should this be used for...
     options: {
       limit: 10,
       order: "desc",
       accountId: accountFilter,
     },
-  };
+  }));
 }
 
 const Post = styled.div`
@@ -45,17 +45,28 @@ const Post = styled.div`
 const renderItem = (a) => {
   let type = a.value.type;
 
-  // return <div>{JSON.stringify(a)}</div>;
+  // PATH NORMALIZATION
+  const path = a.value.path;
+  const domain = "post";
+  if (path === undefined) {
+    // we need to construct paths from hashtags
+    domain = a.action;
+    path = `${a.accountId}/post/${a.key}`;
+  } else {
+    const parts = path.split("/");
+    domain = parts[1];
+    parts[1] = "post";
+    path = parts.join("/");
+  }
 
-  if (typeWhitelist.includes(a.value.type)) {
+  if (typeWhitelist.includes(type) && domainFilter.includes(domain)) {
     return (
       <Post className="post" key={JSON.stringify(a)}>
         <Widget
           src="efiz.near/widget/Every.Post.View"
           props={{
-            accountId: a.accountId,
+            path,
             blockHeight: a.blockHeight,
-            type: a.value.type,
           }}
         />
       </Post>
