6d3369561a27102af5a4ad5fc7d4c9de99a6d8ef
saidulbadhon <saidulbadhon>
2023-05-25 18:34:23 +0000

    Update s3.buildspace.rightSide.card by saidulbadhon.near at 2023-05-25T18:34:23.233Z

---
+++ saidulbadhon.near/s3.buildspace.rightSide.card/source_code.jsx
@@ -0,0 +1,60 @@
+const theme = props.theme;
+const image = props.image;
+
+return (
+  <div
+    key={index}
+    style={{
+      // height: "100%",
+      height: 300,
+      width: "100%",
+      maxWidth: 200,
+      borderRadius: 24,
+      backgroundColor: "red",
+      overflow: "hidden",
+
+      position: "relative",
+
+      display: "flex",
+    }}
+  >
+    <img
+      style={{ width: "100%", height: "100%" }}
+      src={image.imageUri}
+      alt={image.name}
+    />
+
+    <div
+      style={{
+        // backgroundColor: "#FFF",
+        color: "#FFF",
+        position: "absolute",
+        left: "50%",
+        width: "calc(100% - 32px)",
+        transform: "translateX(-50%)",
+        zIndex: 100,
+        bottom: 16,
+        borderRadius: 16,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+
+        backdropFilter: "blur(10px)",
+        border: `1px rgba(256, 256, 256, .25) solid`,
+
+        padding: "8px 16px",
+      }}
+    >
+      <p
+        style={{
+          textAlign: "center",
+          padding: 0,
+          margin: 0,
+          fontWeight: 600,
+        }}
+      >
+        {image.name} {index + 1}
+      </p>
+    </div>
+  </div>
+);
