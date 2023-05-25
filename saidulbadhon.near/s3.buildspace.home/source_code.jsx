ce0246025d32f1ab5c74ffe7adb97d94df88cec8
saidulbadhon <saidulbadhon>
2023-05-25 18:43:39 +0000

    Update s3.buildspace.home by saidulbadhon.near at 2023-05-25T18:43:39.399Z

---
+++ saidulbadhon.near/s3.buildspace.home/source_code.jsx
@@ -0,0 +1,280 @@
+const theme = props.theme;
+
+const images = [
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+  {
+    imageUri:
+      "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    name: "Saidul Badhon",
+  },
+];
+
+// // Calculate the total width of the image container
+// const imageContainer = document.getElementById("image-container");
+// const totalWidth = imageContainer.children.length * 100 + "%";
+// imageContainer.style.width = totalWidth;
+
+// // Clone the images and append them to the image container
+// // const images = imageContainer.getElementsByTagName('img');
+// const imageCount = images.length;
+// for (let i = 0; i < imageCount; i++) {
+//   const clone = images[i].cloneNode(true);
+//   imageContainer.appendChild(clone);
+// }
+
+// // Start the animation
+// imageContainer.style.animationDuration = imageCount * 5 + "s"; // Adjust the speed as needed
+
+return (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <div
+      style={{
+        // height: "calc(100vh - 100px)",
+        height: "100%",
+        maxHeight: "calc(100vh - 85px)",
+        width: "100%",
+        maxWidth: 1250,
+        display: "grid",
+        gridTemplateColumns: "1fr 416px",
+        paddingInline: 20,
+      }}
+    >
+      <Widget
+        src="saidulbadhon.near/widget/s3.buildspace.leftSide"
+        props={{ theme }}
+      />
+
+      <div
+        className="scroll-container"
+        style={{
+          maxHeight: "calc(100vh - 75px)",
+          overflowY: "hidden",
+          display: "flex",
+          gap: 16,
+        }}
+      >
+        <div
+          className="image-container"
+          id="image-container"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "200px",
+            gap: 16,
+            justifyContent: "center",
+            height: "100%",
+            // overflowY: "auto",
+          }}
+        >
+          {images?.map((image, index) => (
+            <Widget
+              key={index}
+              src="saidulbadhon.near/widget/s3.buildspace.rightSide.card"
+              props={{ theme, image }}
+            />
+          ))}
+        </div>
+
+        <div
+          className="image-container reverse"
+          id="image-container"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "200px",
+            gap: 16,
+            justifyContent: "center",
+            height: "100%",
+            // overflowY: "auto",
+          }}
+        >
+          {images?.map((image, index) => (
+            <Widget
+              key={index}
+              src="saidulbadhon.near/widget/s3.buildspace.rightSide.card"
+              props={{ theme, image }}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  </div>
+);
