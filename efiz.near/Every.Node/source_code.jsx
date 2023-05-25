ffa412548ecf6584c6f4d4690fe00aa7fa9ce3c0
elliotBraem <elliotBraem>
2023-05-25 03:32:24 +0000

    Update Every.Node by efiz.near at 2023-05-25T03:32:24.402Z

--- efiz.near/Every.Node/source_code.jsx
+++ efiz.near/Every.Node/source_code.jsx
@@ -51,97 +51,80 @@ function getType(path) {
 
 const type = getType(path);
 
-// // WHEN A NEW ROOT IS SET //
-// // GET DATA AT THIS PATH //
-// function getNode(path, type) {
-//   const parts = path.split("/");
-//   let value = {};
+// WHEN A NEW ROOT IS SET //
+// GET DATA AT THIS PATH //
+function getNode(path, type) {
+  const parts = path.split("/");
+  let value = {};
 
-//   // ACCOUNT //
-//   if (type === "account") {
-//     if (parts.length > 1) {
-//       // GRAPH // FOLLOW // BACK TO ACCOUNT : WORKING
-//       //   setRoot(parts[3], "account");
-//     } else {
-//       if (parts[0] !== "*") {
-//         parts.push("**");
-//       }
-//       value = Social.get(parts.join("/"), "final");
-//       return value;
-//     }
-//     // THING //
-//   } else if (type === "thing") {
-//     // path: "everything"
-//     // type: "thing"
-//     return rootNode; // Or should "everything" be "*"?
-//     // PROFILE //
-//   } else if (type === "profile") {
-//     value = Social.get(parts.join("/"), "final");
-//     // POST : WIP //
-//   } else if (type === "post") {
-//     value = path;
-//     // NAMETAG : WIP //
-//   } else if (type === "nametag") {
-//     if (parts.length > 2) {
-//       if (parts.length === 3) {
-//         // BACK TO ACCOUNT
-//         // setRoot(parts[3], "account");
-//       } else if (parts.length === 4) {
-//         // ALL TAGS BY ACCOUNT
-//         value = Social.keys(`${parts[0]}/profile/tags/*`, "final");
-//       } else {
-//         // THIS TAG
-//         value = parts[5];
-//       }
-//     }
-//   } else {
-//     parts.push("**");
-//     value = Social.get(parts.join("/"), "final");
-//     return value;
-//   }
-// }
-// let node = {};
-// if (isRoot) {
-//   node = getNode(path, type);
-// }
-
-function renderEdges(edges) {
-  console.log(`edges: ${JSON.stringify(edges)}`);
-  return (
-    <ButtonRow>
-      {edges?.map((it) => (
-        <Widget
-          src="efiz.near/widget/Every.Node"
-          props={{
-            label: it.label,
-            path: it.path,
-            setPath: setPath,
-            history,
-            setHistory: setHistory,
-            isRoot: false,
-          }}
-        />
-      ))}
-    </ButtonRow>
-  );
-}
-
-function renderThing(path, type) {
-  let dataSource;
-  let dataSourceArgs = {};
+  // ACCOUNT //
   if (type === "account") {
-    dataSource = "SOCIALDB";
-    dataSourceArgs = {
-      arg1: path,
-    };
+    if (parts.length > 1) {
+      // GRAPH // FOLLOW // BACK TO ACCOUNT : WORKING
+      //   setRoot(parts[3], "account");
+    } else {
+      if (parts[0] !== "*") {
+        parts.push("**");
+      }
+      value = Social.get(parts.join("/"), "final");
+      return value;
+    }
+    // THING //
+  } else if (type === "thing") {
+    // path: "everything"
+    // type: "thing"
+    return rootNode; // Or should "everything" be "*"?
+    // PROFILE //
+  } else if (type === "profile") {
+    value = Social.get(parts.join("/"), "final");
+    // POST : WIP //
+  } else if (type === "post") {
+    value = path;
+    // NAMETAG : WIP //
+  } else if (type === "nametag") {
+    if (parts.length > 2) {
+      if (parts.length === 3) {
+        // BACK TO ACCOUNT
+        // setRoot(parts[3], "account");
+      } else if (parts.length === 4) {
+        // ALL TAGS BY ACCOUNT
+        value = Social.keys(`${parts[0]}/profile/tags/*`, "final");
+      } else {
+        // THIS TAG
+        value = parts[5];
+      }
+    }
+  } else {
+    parts.push("**");
+    value = Social.get(parts.join("/"), "final");
+    return value;
   }
-  return (
-    <Widget
-      src="efiz.near/widget/Every.Thing"
-      props={{ dataSource, dataSourceArgs, type, renderEdges: renderEdges }}
-    />
-  );
 }
+let node = {};
+if (isRoot) {
+  node = getNode(path, type);
+}
+
+// function renderEdges(edges) {
+//   console.log(`edges: ${JSON.stringify(edges)}`);
+//   return (
+//     <ButtonRow>
+//       {edges?.map((it) => (
+//         <Widget
+//           src="efiz.near/widget/Every.Node"
+//           props={{
+//             label: it.label,
+//             path: it.path,
+//             setPath: setPath,
+//             history,
+//             setHistory: setHistory,
+//             isRoot: false,
+//           }}
+//         />
+//       ))}
+//     </ButtonRow>
+//   );
+// }
 
 return (
   <div>
@@ -167,7 +150,28 @@ return (
       {history.length > 1 && isRoot && (
         <Button onClick={handleBack}>back</Button>
       )}
+      <ButtonRow>
+        {/** EDGES */}
+        {node && typeof node === "object"
+          ? Object.entries(node)?.map(([key, val]) => (
+              <Widget
+                src="efiz.near/widget/Node"
+                props={{
+                  key,
+                  label: key,
+                  node: val,
+                  type: getType(),
+                  path: `${path}/${key}`,
+                  setPath: setPath,
+                  history,
+                  setHistory: setHistory,
+                  isRoot: false,
+                }}
+              />
+            ))
+          : null}
+      </ButtonRow>
     </div>
-    {renderThing(path, type)}
+    <Widget src="efiz.near/widget/Every.Thing.View" props={{ path }} />
   </div>
 );
