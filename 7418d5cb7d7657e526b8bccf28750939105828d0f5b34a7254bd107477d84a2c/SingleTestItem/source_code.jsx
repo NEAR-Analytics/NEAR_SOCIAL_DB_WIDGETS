166d4f851b85f7d0c9a1a6abc4981293dce3f8e3
MilanPoznan <MilanPoznan>
2023-05-25 11:04:46 +0000

    Update SingleTestItem by 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c at 2023-05-25T11:04:46.189Z

--- 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/SingleTestItem/source_code.jsx
+++ 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/SingleTestItem/source_code.jsx
@@ -2,8 +2,9 @@ const accountId = context.accountId;
 const authorForWidget =
   "7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c";
 
-console.log(props);
-let item = Social.get(`${accountId}/testPersons/0/**`);
+const { userId } = props;
+
+let item = Social.get(`${accountId}/testPersons/${userId}/**`);
 
 return (
   <div>
