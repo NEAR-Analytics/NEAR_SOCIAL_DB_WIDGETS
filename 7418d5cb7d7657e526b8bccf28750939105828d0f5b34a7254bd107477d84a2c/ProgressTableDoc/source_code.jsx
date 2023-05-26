a0a207479fcae5b7974cd814dbf5f02edcd05960
MilanPoznan <MilanPoznan>
2023-05-26 10:10:06 +0000

    Update ProgressTableDoc by 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c at 2023-05-26T10:10:06.895Z

--- 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableDoc/source_code.jsx
+++ 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableDoc/source_code.jsx
@@ -15,7 +15,16 @@ return (
     </p>
     <p>First one is widgetType and it can be 1, 2 or 3</p>
     <p>Second one is hasBackground, it is boolean </p>
-
+    <p>
+      <a
+        href="https://near.social/#/7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressTableAdminPanel"
+        target="_blank"
+      >
+        Here is the form
+      </a>
+      where you can add,edit or remove items. It is will open to anyone, so feel
+      free to send me accounts for permisions
+    </p>
     <h2>First type</h2>
     <Widget
       src="7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/widget/ProgressTableRender"
