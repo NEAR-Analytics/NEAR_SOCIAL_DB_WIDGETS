9802dc29e99a094eb606fac5a2556323dcdb1665
connectamey <connectamey>
2023-05-26 21:18:48 +0000

    Update Chatbot.iFrame by connectamey.near at 2023-05-26T21:18:48.055Z

--- connectamey.near/Chatbot.iFrame/source_code.jsx
+++ connectamey.near/Chatbot.iFrame/source_code.jsx
@@ -1,3 +1,3 @@
 const website = "http://localhost:3000/";
 
-return <iframe className="w-100" style={style} iframeResizer src={website} />;
+return <iframe className="h-100" style={style} iframeResizer src={website} />;
