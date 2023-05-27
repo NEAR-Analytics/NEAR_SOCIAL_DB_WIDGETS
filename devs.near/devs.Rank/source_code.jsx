6f09db29312c5b29ad66b3c925af82c3fde8e3cf
near <near>
2023-05-27 14:34:56 +0000

    Update devs.Rank by devs.near at 2023-05-27T14:34:56.405Z

--- devs.near/devs.Rank/source_code.jsx
+++ devs.near/devs.Rank/source_code.jsx
@@ -4,7 +4,7 @@ if (!data) {
   return "Loading...";
 }
 let accounts = Object.entries(data);
-const limit = 258;
+const limit = 100;
 
 for (let i = 0; i < accounts.length; ++i) {
   let accountId = accounts[i][0];
@@ -26,7 +26,7 @@ console.log(accountWidgetSort);
 
 return (
   <>
-    <h5>Top Widget Builders</h5>
+    <h3>Top Widget Builders</h3>
     {accountWidgetSort.map((rank, index) => {
       let accountId = rank.accountId;
       return (
@@ -59,7 +59,7 @@ return (
               </span>
             </div>
           </div>
-          <div style={{ width: "20%" }}>
+          <div style={{ width: "10%" }}>
             <Widget src="mob.near/widget/FollowButton" props={{ accountId }} />
           </div>
         </div>
