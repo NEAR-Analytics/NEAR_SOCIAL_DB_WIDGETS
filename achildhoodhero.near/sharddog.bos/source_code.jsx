5e21a5f88f30e3aa7abf7f5d20d4bdc39a038245
achildhoodhero.near <achildhoodhero.near>
2023-05-27 23:45:19 +0000

    Update sharddog.bos by achildhoodhero.near at 2023-05-27T23:45:19.127Z

---
+++ achildhoodhero.near/sharddog.bos/source_code.jsx
@@ -0,0 +1,12 @@
+return (
+  <>
+    <h3>Need a NEAR Account?</h3>
+    <p>Claim your name instantly and for free with ShardDog</p>
+    <img src="https://shard.dog/img/ShardDog.jpg" width="400" height="400" />
+    <br />
+    <br />
+    <a href="https://shard.dog/go?url=https://near.social">
+      <button>Create One For Free</button>
+    </a>
+  </>
+);
