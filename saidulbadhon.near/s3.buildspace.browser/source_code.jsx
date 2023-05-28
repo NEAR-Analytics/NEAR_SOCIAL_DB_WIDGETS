30ef7ddf34e2b24aa875c69dc35838bc8ee7e58f
saidulbadhon <saidulbadhon>
2023-05-28 13:01:57 +0000

    Update s3.buildspace.browser by saidulbadhon.near at 2023-05-28T13:01:57.863Z

--- saidulbadhon.near/s3.buildspace.browser/source_code.jsx
+++ saidulbadhon.near/s3.buildspace.browser/source_code.jsx
@@ -1,13 +1,39 @@
 const theme = props.theme;
+const Card = styled.div`
+  width: 100%;
+  border-radius: 16px;
+  overflow: hidden;
+  border: 1px rgba(256, 256, 256, .25) solid;
+  display: flex;
+  flex-direction: column;
+  filter: brightness(0.8);
+  height: 416px;
+  transition: all .2s ease-in-out;
+
+  &:hover {
+    filter: brightness(1);
+  }
+`;
+const PTag = styled.p`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3; /* number of lines to show */
+  line-clamp: 3;
+  -webkit-box-orient: vertical;
+`;
 
 State.init({
   images: [],
   showBrowser: false,
   selectedHouse: "",
-  users: [],
 });
+// const res = fetch(
+//   "https://t4zr86bzl5.execute-api.us-east-1.amazonaws.com/production/api/v1/buildspace" +
+//     state.selectedHouse
+// );
 const res = fetch(
-  "https://t4zr86bzl5.execute-api.us-east-1.amazonaws.com/production/api/v1/buildspace"
+  `http://localhost:8000/api/v1/buildspace/${state.selectedHouse}`
 );
 
 if (!res.body)
@@ -16,18 +42,10 @@ if (!res.body)
   );
 
 const handleUpdateHouse = (houseName) => {
-  let xUsers = [];
-
-  res.body?.map((i) => {
-    if (i.house === houseName) {
-      xUsers.push(i);
-    }
-  });
+  console.log(houseName);
 
   State.update({
     selectedHouse: state.selectedHouse === houseName ? "" : houseName,
-
-    users: xUsers,
   });
 };
 
@@ -169,8 +187,9 @@ return (
             height: "100%",
           }}
         >
-          {state.selectedHouse.length > 2
-            ? state.users?.map((card, index) => (
+          {/*state.selectedHouse.length > 2
+            ? state.users?.length > 2 &&
+              state.users?.map((card, index) => (
                 <Widget
                   src="saidulbadhon.near/widget/s3.buildspace.browser.card"
                   props={{
@@ -191,7 +210,92 @@ return (
                     }}
                   />
                 </div>
-              ))}
+              ))*/}
+          {res.body?.map((card, index) => (
+            <a
+              href={`/s3/${card.name}`}
+              style={{ textTransform: "none", textDecoration: "none" }}
+            >
+              <Card>
+                <img
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    minHeight: 250,
+                    maxHeight: 250,
+                    objectFit: "cover",
+                    aspectRatio: 1 / 1,
+                    backgroundColor: theme.textColor2,
+                  }}
+                  src={
+                    card.imageUrl
+                      ? `https://ipfs.near.social/ipfs/${card.imageUrl}`
+                      : "https://i.pinimg.com/originals/6b/f6/2c/6bf62c6c123cdcd33d2d693782a46b34.jpg"
+                  }
+                  alt={card.name}
+                />
+
+                <div
+                  style={{
+                    color: "#FFF",
+
+                    bottom: 24,
+                    borderRadius: 16,
+
+                    display: "flex",
+                    justifyContent: "center",
+                    aligncards: "center",
+                    flexDirection: "column",
+                    gap: 8,
+                    padding: "16px 16px 8px 16px",
+                  }}
+                >
+                  <h5
+                    style={{
+                      textAlign: "center",
+                      padding: 0,
+                      margin: 0,
+                      fontWeight: 700,
+                    }}
+                  >
+                    {card.name}
+                  </h5>
+
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    <p
+                      style={{
+                        textAlign: "center",
+                        padding: 0,
+                        margin: 0,
+                        fontWeight: 400,
+
+                        fontSize: "12px",
+
+                        backgroundColor: theme.textColor3 + 66,
+                        color: theme.textColor,
+                        padding: "4px 12px",
+                        borderRadius: 4,
+                      }}
+                    >
+                      #{card.house}
+                    </p>
+                  </div>
+
+                  <PTag
+                    style={{
+                      textAlign: "center",
+                      padding: 0,
+                      margin: 0,
+                      fontWeight: 500,
+                      color: theme.textColor2,
+                    }}
+                  >
+                    {card.message}
+                  </PTag>
+                </div>
+              </Card>
+            </a>
+          ))}
         </div>
       </div>
     </div>
