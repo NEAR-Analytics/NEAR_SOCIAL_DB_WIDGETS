fb4f3d20e83e76b6b5be92d00946bbf0cc2bba09
mattlockyer <mattlockyer>
2023-05-28 00:16:15 +0000

    Update SPACEID-BNB by mattlock.near at 2023-05-28T00:16:15.062Z

---
+++ mattlock.near/SPACEID-BNB/source_code.jsx
@@ -0,0 +1,326 @@
+const toast = (toast) => {
+  State.update({
+    toast,
+  });
+};
+
+// Get token prices
+
+const priceQuery = fetch(
+  `https://galxe-proxy.near.workers.dev?url=https://graphigo.prd.space.id/query`,
+  {
+    method: "POST",
+    body: JSON.stringify({
+      operationName: "tokenUsdPrice",
+      variables: {
+        tokens: ["BNB", "ETH", "WBNB", "WETH"],
+      },
+      query:
+        "query tokenUsdPrice($tokens: [TokenType]!, $date: Int) {\n  tokenUsdPrice(tokens: $tokens, date: $date) {\n    token\n    usdPrice\n    date\n    __typename\n  }\n}",
+    }),
+  }
+);
+
+const prices = JSON.parse(priceQuery.body);
+const priceForName =
+  Math.ceil((10000 * 5) / parseFloat(prices.data.tokenUsdPrice[0].usdPrice)) /
+    10000 +
+  0.001;
+
+console.log("prices ", prices);
+console.log("priceForName", priceForName);
+
+// BNB commit and register
+
+const NAME = "__BNB_COMMIT_NAME";
+const SECRET = "__BNB_COMMIT_SECRET";
+const COMMITMENT = "__BNB_COMMITMENT";
+if (!Storage.get(SECRET)) {
+  Storage.set(SECRET, ethers.utils.formatBytes32String(Date.now().toString()));
+}
+
+// Abi and .bnb registrar setup
+
+const abi = fetch(
+  "https://bafkreidywdcopwkqdp26osfqhk44m4cogqeuzkkq2of4w3nnop6ouvggoe.ipfs.nftstorage.link/"
+);
+const bnb = {
+  address: `0xD9A99AE1f5D173cCf36E19777ACa5B8268B5F291`,
+  resolver: `0x7A18768EdB2619e73c4d5067B90Fd84a71993C1D`,
+  abi: abi.body,
+  iface: new ethers.utils.Interface(abi.body),
+  secret: Storage.get(SECRET),
+};
+
+// .bnb registrar methods
+
+const getNamesForOwner = (owner) => {
+  const res = fetch(
+    `https://galxe-proxy.near.workers.dev?url=https://graphigo.prd.space.id/query`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        operationName: "domains",
+        variables: {
+          input: {
+            owner,
+            orderBy: "LIST_PRICE_ASC",
+            buyNow: 0,
+            domainStatuses: [
+              "REGISTERED",
+              "UNREGISTERED",
+              "GRACE_PERIOD",
+              "PREMIUM",
+            ],
+            first: 30,
+          },
+        },
+        query:
+          "query domains($input: ListDomainsInput!) {\n  domains(input: $input) {\n    exactMatch {\n      name\n      listPrice\n      lastSalePrice\n      tokenId\n      owner\n      network\n      orderSource\n      expirationDate\n      image\n      __typename\n    }\n    list {\n      name\n      listPrice\n      lastSalePrice\n      tokenId\n      owner\n      network\n      orderSource\n      expirationDate\n      image\n      __typename\n    }\n    pageInfo {\n      startCursor\n      endCursor\n      hasNextPage\n      __typename\n    }\n    __typename\n  }\n}",
+      }),
+    }
+  );
+
+  if (!res.body || res.body.error) return;
+
+  const names = JSON.parse(res.body).data.domains.list;
+  console.log("getNamesForOwner", names);
+  return names;
+};
+
+const checkName = (name) => {
+  const queryRes1 = fetch(
+    `https://galxe-proxy.near.workers.dev?url=https://graphigo.prd.space.id/query`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        operationName: "domains",
+        variables: {
+          input: {
+            query: name,
+            orderBy: "LIST_PRICE_ASC",
+            buyNow: 1,
+            network: 1,
+            domainStatuses: ["REGISTERED", "UNREGISTERED"],
+            first: 30,
+          },
+        },
+        query:
+          "query domains($input: ListDomainsInput!) {\n  domains(input: $input) {\n    exactMatch {\n      name\n      listPrice\n      lastSalePrice\n      tokenId\n      owner\n      network\n      orderSource\n      expirationDate\n      image\n      __typename\n    }\n    list {\n      name\n      listPrice\n      lastSalePrice\n      tokenId\n      owner\n      network\n      orderSource\n      expirationDate\n      image\n      __typename\n    }\n    pageInfo {\n      startCursor\n      endCursor\n      hasNextPage\n      __typename\n    }\n    __typename\n  }\n}",
+      }),
+    }
+  );
+
+  console.log("checkName raw data", queryRes1.body);
+
+  const taken =
+    JSON.parse(queryRes1.body).data.domains.exactMatch[0]?.listPrice !== 5;
+  console.log("checkName taken", taken);
+  return taken;
+};
+
+const commit = () => {
+  const name = state.name;
+
+  if (checkName(name)) {
+    toast(`${state.name} is taken. Please try another name.`);
+    return State.update({
+      name: "",
+    });
+  }
+
+  if (!name || name.length < 5) {
+    toast("Enter a valid name. Greater than 7 characters.");
+    return;
+  }
+  Storage.set(NAME, name);
+
+  const encodedData = bnb.iface.encodeFunctionData("makeCommitment", [
+    name,
+    state.address,
+    bnb.secret,
+  ]);
+
+  return Ethers.provider()
+    .call({
+      to: bnb.address,
+      data: encodedData,
+    })
+    .then((commitment) => {
+      if (typeof commitment !== "string") {
+        alert("There was an error committing your name. Please try again!");
+      }
+
+      console.log("commitment", commitment);
+
+      const bnbContract = new ethers.Contract(
+        bnb.address,
+        bnb.abi,
+        Ethers.provider().getSigner()
+      );
+
+      bnbContract
+        .bulkCommit([commitment], {
+          gasLimit: 50000,
+        })
+        .then((res) => {
+          console.log("commitment res", res);
+          toast(
+            "Please wait 10 seconds for transaction to finalize. Register button will be enabled after 10 seconds."
+          );
+          setTimeout(() => Storage.set(COMMITMENT, commitment), 10000);
+        })
+        .catch((e) => {
+          console.log(e);
+          if (e.code === "ACTION_REJECTED") {
+            return toast(
+              "You rejected the name request transaction. Please try again!"
+            );
+          }
+          throw e;
+        });
+    });
+};
+
+const register = () => {
+  const name = Storage.get(NAME);
+  const secret = Storage.get(SECRET);
+
+  if (!name) {
+    return;
+  }
+
+  const bnbContract = new ethers.Contract(
+    bnb.address,
+    bnb.abi,
+    Ethers.provider().getSigner()
+  );
+
+  bnbContract
+    .register(name, state.address, 31556952, secret, {
+      value: ethers.utils.parseEther(priceForName.toString()),
+      gasLimit: 1000000,
+    })
+    .then((transactionHash) => {
+      console.log("transactionHash is ", transactionHash);
+      Storage.set(COMMITMENT, null);
+    });
+};
+
+// CSS
+
+const css = `
+  input, button {
+    margin-bottom: 16px;
+  }
+  .main {
+    margin-top: 32px;
+    width: 100%;
+    text-align: center;
+    position: relative;
+  }
+  .boxes {
+    margin: auto;
+    display: flex;
+    flex-flow: row wrap;
+    max-width: 1000px;
+    > div {
+      border: 1px solid #222;
+      margin: 16px;
+      padding: 8px;
+      text-align: center;
+      width: 256px;
+      > img {
+        width: 256px;
+      }
+      .break {
+        margin: 16px 0 16px 0;
+        height: 1px;
+        background-size: 100%;
+        background-image: url(https://galxe.com/_nuxt/img/bar.57178f2.png);
+      }
+      > button {
+        margin-bottom: 12px;
+      }
+      > h4 {
+        margin: 16px 0 16px 0;
+        font-size: 1.2rem;
+      }
+      > p.not {
+        color: rgb(168, 174, 186);
+      }
+    }
+    
+  }
+`;
+
+if (!css) return "";
+
+if (!state.theme) {
+  State.update({
+    theme: styled.div`
+    box-sizing: border-box;
+    font-family: Sans-Serif;
+    ${css}`,
+  });
+}
+const Theme = state.theme;
+
+const init = () => {
+  console.log("init");
+  const name = Storage.get(NAME);
+  let address;
+  if (state.address === undefined) {
+    const accounts = Ethers.send("eth_requestAccounts", []);
+    if (accounts.length) {
+      address = accounts[0];
+      const names = getNamesForOwner(address);
+      State.update({ address, name, names });
+    }
+  }
+};
+
+init();
+
+if (!state.address)
+  return (
+    <Theme>
+      <button onClick={init}>Get Started</button>
+    </Theme>
+  );
+
+return (
+  <Theme>
+    <Widget
+      src="mattlock.near/widget/toast"
+      props={{
+        toast: state.toast,
+        onClose: () => State.update({ toast: null }),
+      }}
+    />
+
+    <h3>Register a .bnb domain</h3>
+
+    <p>Enter a name and request to register.</p>
+    <input
+      disabled={!state.address}
+      value={state.name}
+      onChange={(e) => State.update({ name: e.target.value })}
+      placeholder="name"
+    />
+    <button onClick={() => commit()}>Step 1. Request</button>
+    <p>After you sign the commitment you can register the name.</p>
+    <button onClick={() => register()} disabled={!Storage.get(COMMITMENT)}>
+      Step 2. Register
+    </button>
+
+    <h3>Names you own:</h3>
+    {state.names && (
+      <ol>
+        {state.names.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ol>
+    )}
+  </Theme>
+);
