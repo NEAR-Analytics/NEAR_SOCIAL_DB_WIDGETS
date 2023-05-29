80a84cb0fe80c7280075f987d2ab9f798d0e5941
sayalot.near <sayalot.near>
2023-05-29 15:55:45 +0000

    Update SayALot by sayalot.near at 2023-05-29T15:55:45.418Z

---
+++ sayalot.near/SayALot/source_code.jsx
@@ -0,0 +1,42 @@
+const addressForArticles = "ndcGigsArticle";
+const authorsWhitelist = [
+  "neardigitalcollective.near",
+  "blaze.near",
+  "jlw.near",
+  "kazanderdad.near",
+  "joep.near",
+  "sarahkornfeld.near",
+  "yuensid.near",
+  "shubham007.near",
+];
+const authorForWidget = "neardigitalcollective.near";
+const accountId = props.accountId ?? context.accountId;
+// if (!accountId) {
+//   return "No account ID";
+// }
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+if (profile === null) {
+  return "Loading";
+}
+
+return (
+  <div
+    className="container-fluid"
+    style={{
+      backgroundColor: "rgb(230, 230, 230)",
+      borderRadius: "20px",
+      padding: "0",
+    }}
+  >
+    <Widget
+      src={`${authorForWidget}/widget/SayALot_MainNavigation`}
+      props={{ currentNavPill: "articles", writersWhiteList }}
+    />
+    <div style={{ margin: "0 auto", width: "90%", minWidth: "360px" }}>
+      <Widget
+        src={`${authorForWidget}/widget/SayALot_AllArticlesList`}
+        props={{ writersWhiteList }}
+      />
+    </div>
+  </div>
+);
