a8994d749e02c176fa324472aae3bee0cd0cad22
sayalot.near <sayalot.near>
2023-05-29 17:47:18 +0000

    Update SayALot_MainNavigation by sayalot.near at 2023-05-29T17:47:18.431Z

--- sayalot.near/SayALot_MainNavigation/source_code.jsx
+++ sayalot.near/SayALot_MainNavigation/source_code.jsx
@@ -68,7 +68,7 @@ return (
               alt: widgetName,
             }}
           />
-          <i>When you have a lot to say...</i>
+          <i>lots to say...</i>
         </div>
       </a>
       <button
