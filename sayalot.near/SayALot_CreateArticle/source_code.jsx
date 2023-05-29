3d930daa1068e9d0fff515f7c0c0dbafa112ffd2
sayalot.near <sayalot.near>
2023-05-29 16:18:23 +0000

    Update SayALot_CreateArticle by sayalot.near at 2023-05-29T16:18:23.811Z

---
+++ sayalot.near/SayALot_CreateArticle/source_code.jsx
@@ -0,0 +1,426 @@
+const addressForArticles = "sayALotArticle";
+const authorForWidget = "sayalot.near";
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+if (profile === null) {
+  return "Loading";
+}
+const initialBody = `# h1 Heading 8-)
+## h2 Heading
+### h3 Heading
+#### h4 Heading
+##### h5 Heading
+###### h6 Heading
+
+
+## Horizontal Rules
+
+___
+
+---
+
+***
+
+
+## Typographic replacements
+
+Enable typographer option to see result.
+
+(c) (C) (r) (R) (tm) (TM) (p) (P) +-
+
+test.. test... test..... test?..... test!....
+
+!!!!!! ???? ,,  -- ---
+
+"Smartypants, double quotes" and 'single quotes'
+
+
+## Emphasis
+
+**This is bold text**
+
+__This is bold text__
+
+*This is italic text*
+
+_This is italic text_
+
+~~Strikethrough~~
+
+
+## Blockquotes
+
+
+> Blockquotes can also be nested...
+>> ...by using additional greater-than signs right next to each other...
+> > > ...or with spaces between arrows.
+
+
+## Lists
+
+Unordered
+
++ Create a list by starting a line with \`+\`, \`-\`, or \`*\`
++ Sub-lists are made by indenting 2 spaces:
+  - Marker character change forces new list start:
+    * Ac tristique libero volutpat at
+    + Facilisis in pretium nisl aliquet
+    - Nulla volutpat aliquam velit
++ Very easy!
+
+Ordered
+
+1. Lorem ipsum dolor sit amet
+2. Consectetur adipiscing elit
+3. Integer molestie lorem at massa
+
+
+1. You can use sequential numbers...
+1. ...or keep all the numbers as \`1.\`
+
+Start numbering with offset:
+
+57. foo
+1. bar
+
+
+## Code
+
+Inline \`code\`
+
+Indented code
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code
+
+
+Block code "fences"
+
+\`\`\`
+Sample text here...
+\`\`\`
+
+Syntax highlighting
+
+\`\`\` js
+var foo = function (bar) {
+  return bar++;
+};
+
+console.log(foo(5));
+\`\`\`
+
+## Tables
+
+| Option | Description |
+| ------ | ----------- |
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+
+Right aligned columns
+
+| Option | Description |
+| ------:| -----------:|
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+
+
+## Links
+
+[link text](http://dev.nodeca.com)
+
+[link with title](http://nodeca.github.io/pica/demo/ "title text!")
+
+Autoconverted link https://github.com/nodeca/pica (enable linkify to see)
+
+
+## Images
+
+![Minion](https://octodex.github.com/images/minion.png)
+![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
+
+Like links, Images also have a footnote style syntax
+
+![Alt text][id]
+
+With a reference later in the document defining the URL location:
+
+[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
+
+### [Emojies](https://github.com/markdown-it/markdown-it-emoji)
+
+> Classic markup: :wink: :crush: :cry: :tear: :laughing: :yum:
+>
+> Shortcuts (emoticons): :-) :-( 8-) ;)
+
+see [how to change output](https://github.com/markdown-it/markdown-it-emoji#change-output) with twemoji.
+
+
+### [Subscript](https://github.com/markdown-it/markdown-it-sub) / [Superscript](https://github.com/markdown-it/markdown-it-sup)
+
+- 19^th^
+- H~2~O
+
+
+### [\<ins>](https://github.com/markdown-it/markdown-it-ins)
+
+++Inserted text++
+
+
+### [\<mark>](https://github.com/markdown-it/markdown-it-mark)
+
+==Marked text==
+
+
+### [Footnotes](https://github.com/markdown-it/markdown-it-footnote)
+
+Footnote 1 link[^first].
+
+Footnote 2 link[^second].
+
+Inline footnote^[Text of inline footnote] definition.
+
+Duplicated footnote reference[^second].
+
+[^first]: Footnote **can have markup**
+
+    and multiple paragraphs.
+
+[^second]: Footnote text.
+
+
+### [Definition lists](https://github.com/markdown-it/markdown-it-deflist)
+
+Term 1
+
+:   Definition 1
+with lazy continuation.
+
+Term 2 with *inline markup*
+
+:   Definition 2
+
+        { some code, part of Definition 2 }
+
+    Third paragraph of definition 2.
+
+_Compact style:_
+
+Term 1
+  ~ Definition 1
+
+Term 2
+  ~ Definition 2a
+  ~ Definition 2b
+`;
+
+const errTextNoBody = "ERROR: no article Body",
+  errTextNoId = "ERROR: no article Id",
+  errTextDublicatedId = "ERROR: there is article with such name";
+
+const initialCreateArticleState = {
+  articleId: "",
+  articleBody: initialBody,
+  errorId: "",
+  errorBody: "",
+  tags: {},
+};
+
+State.init(initialCreateArticleState);
+const tagsArray = state.tags ? Object.keys(state.tags) : undefined;
+
+const getArticleData = () => {
+  const args = {
+    articleId: state.articleId,
+    author: accountId,
+    lastEditor: accountId,
+    timeLastEdit: Date.now(),
+    timeCreate: Date.now(),
+    body: state.articleBody,
+    version: 0,
+    navigation_id: null,
+    tags: tagsArray,
+  };
+  return args;
+};
+
+const composeData = () => {
+  const data = {
+    sayALotArticle: {
+      main: JSON.stringify(getArticleData()),
+    },
+    index: {
+      sayALotArticle: JSON.stringify({
+        key: "main",
+        value: {
+          type: "md",
+        },
+      }),
+    },
+  };
+
+  if (tagsArray.length) {
+    data.index.tag = JSON.stringify(
+      tagsArray.map((tag) => ({
+        key: tag,
+        value: item,
+      }))
+    );
+  }
+
+  return data;
+};
+
+// === SAVE HANDLER ===
+const saveHandler = (e) => {
+  State.update({
+    ...state,
+    errorId: "",
+    errorBody: "",
+  });
+  if (state.articleId && state.articleBody) {
+    // TODO check it automaticle
+    const isArticleIdDublicated = false;
+
+    if (!isArticleIdDublicated) {
+      const newData = composeData();
+      Social.set(newData, { force: true });
+    } else {
+      State.update({
+        ...state,
+        errorId: errTextDublicatedId,
+      });
+    }
+  } else {
+    if (!state.articleId) {
+      State.update({
+        ...state,
+        errorId: errTextNoId,
+      });
+    }
+    if (!state.articleBody) {
+      State.update({ ...state, errorBody: errTextNoBody });
+    }
+  }
+};
+
+const Button = styled.button`
+  margin: 0px 1rem;
+  display: inline-block;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  user-select: none;
+  transition: color 0.15s ease-in-out,background-color 0.15s ease-in-out,border-color 0.15s ease-in-out,box-shadow 0.15s ease-in-out;
+
+  border: 2px solid transparent;
+  font-weight: 500;
+  padding: 0.3rem 0.5rem;
+  background-color: #010A2D;
+  border-radius: 12px;
+  color: white;
+  text-decoration: none;  
+
+  &:hover {
+    color: #010A2D;
+    background-color: white;
+  }
+`;
+
+return (
+  <div
+    className="container-fluid"
+    style={{
+      backgroundColor: "rgb(230, 230, 230)",
+      borderRadius: "20px",
+      padding: "0 0 1rem 0 ",
+    }}
+  >
+    <Widget
+      src={`${authorForWidget}/widget/SayALot_MainNavigation`}
+      props={{ currentNavPill: "create" }}
+    />
+    <div
+      style={{
+        margin: "0 auto",
+        width: "90%",
+        minWidth: "360px",
+        backgroundColor: "white",
+        padding: "1rem",
+        borderRadius: "20px",
+      }}
+    >
+      <h1 className="mb-3"> Create Article</h1>
+      <div>
+        <div>
+          <Button type="submit" onClick={saveHandler}>
+            Save Article
+          </Button>
+        </div>
+        <div class="d-flex flex-column pt-3">
+          <label for="inputArticleId">
+            Input article id (case-sensitive, without spaces):
+          </label>
+          <label for="inputArticleId" class="small text-danger">
+            {state.errorId}
+          </label>
+          <input
+            className="form-control mt-2"
+            id="inputArticleId"
+            value={state.articleId}
+            onChange={(e) => {
+              State.update({
+                ...state,
+                articleId: e.target.value.replace(/\s+/g, ""),
+              });
+            }}
+          />
+        </div>
+        <div class="d-flex flex-column pt-3">
+          <Widget
+            src="mob.near/widget/TagsEditor"
+            props={{
+              initialTagsObject: state.tags,
+              placeholder: "Input tags",
+              setTagsObject: (tags) => {
+                state.tags = tags;
+                State.update();
+              },
+            }}
+          />
+        <div class="d-flex flex-column pt-3">
+          <label for="textareaArticleBody">
+            Input article body (in makrdown format):
+          </label>
+          <label for="textareaArticleBody" class="small text-danger">
+            {state.errorBody}
+          </label>
+          <div className="d-flex gap-2" style={{ minHeight: "300px" }}>
+            <div className="w-50">
+              <Widget
+                src="mob.near/widget/MarkdownEditorIframe"
+                props={{
+                  initialText: initialBody,
+                  onChange: (articleBody) => State.update({ articleBody }),
+                }}
+              />
+            </div>
+            <div className="w-50">
+              <Widget
+                src="mob.near/widget/SocialMarkdown"
+                props={{ text: state.articleBody }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
