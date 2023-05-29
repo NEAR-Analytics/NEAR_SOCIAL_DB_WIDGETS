4b7da31a7373150479f672bd83a36cbb20cc0c44
near <near>
2023-05-29 23:27:00 +0000

    Update Community by devs.near at 2023-05-29T23:27:00.698Z

--- devs.near/Community/source_code.jsx
+++ devs.near/Community/source_code.jsx
@@ -8,8 +8,8 @@ const nftData = Near.view(contractId, "nft_supply_for_owner", {
 const isOwner = nftData > 0;
 
 const Wrapper = styled.div`
-  --section-gap: 42px;
-  padding-top: 42px;
+  --section-gap: 23px;
+  padding-top: 23px;
 
   @media (max-width: 1160px) {
     .line-rounded-corners {
