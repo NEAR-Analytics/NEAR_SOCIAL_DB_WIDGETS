6389f6413f35d8976217db4bc40f6c13c4814c3c
elliotBraem <elliotBraem>
2023-05-30 17:30:54 +0000

    Update Every.Idea.Create by efiz.near at 2023-05-30T17:30:54.222Z

--- efiz.near/Every.Idea.Create/source_code.jsx
+++ efiz.near/Every.Idea.Create/source_code.jsx
@@ -12,20 +12,39 @@ const Button = styled.button`
 
 const Container = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
 `;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 200px;
+  padding: 8px;
+  font-family: monospace;
+`;
+
+const handleTitleChange = (event) => {
+  const inputValue = event.target.value;
+  State.update({ title: inputValue });
+  onChange(state);
+};
+
+const handleDescriptionChange = (event) => {
+  const inputValue = event.target.value;
+  State.update({ description: inputValue });
+  onChange(state);
+};
 
 return (
   <Container>
     <Input
-      onChange={(e) => {
-        State.update({ value: e.target.value });
-        if (onChange) {
-          onChange(e.target.value);
-        }
-      }}
-      value={state.value}
-      placeholder="domain"
+      onChange={handleTitleChange}
+      value={state.title}
+      placeholder="title"
+    />
+    <TextArea
+      value={state.description}
+      onChange={handleDescriptionChange}
+      placeholder="description"
     />
   </Container>
 );
