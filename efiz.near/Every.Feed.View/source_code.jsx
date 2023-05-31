3bc31d0941e09bbe2aaa887ed2fe620b2838b1cb
elliotBraem <elliotBraem>
2023-05-31 20:10:28 +0000

    Update Every.Feed.View by efiz.near at 2023-05-31T20:10:28.426Z

--- efiz.near/Every.Feed.View/source_code.jsx
+++ efiz.near/Every.Feed.View/source_code.jsx
@@ -1,24 +1,15 @@
 const data = props.data;
-let index = [];
-const typeWhitelist = JSON.parse(props.typeWhitelist || "null") || [
-  "efiz.near/type/paragraph",
-  "efiz.near/type/Image",
-  "efiz.near/type/document",
-];
-const accountFilter =
-  (props.accountFilter && [props.accountFilter]) || undefined;
-const hashtagFilter = JSON.parse(props.hashtagFilter || "null") || [];
-const domainFilter = JSON.parse(props.domainFilter || "null") || ["post"];
+const typeWhitelist = JSON.stringify(props.typeWhitelist);
 
-// domain filter will come from the thing itself
 return (
   <>
-    <Widget src="efiz.near/widget/Every.Post.Create" />
+    <Widget
+      src="efiz.near/widget/Every.Post.Create"
+      props={{ typeWhitelist: typeWhitelist, key: "test" }}
+    />
     <Widget
       src="efiz.near/widget/Every.Post"
-      props={{
-        domainFilter: JSON.stringify([data]),
-      }}
+      props={{ typeWhitelist: typeWhitelist, key: "test" }}
     />
   </>
 );
