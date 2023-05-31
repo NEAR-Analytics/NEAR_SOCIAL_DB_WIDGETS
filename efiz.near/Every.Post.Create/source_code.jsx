32407366e17f8bf97c0a243423db587a2c47ddcd
elliotBraem <elliotBraem>
2023-05-31 16:13:54 +0000

    Update Every.Post.Create by efiz.near at 2023-05-31T16:13:54.069Z

--- efiz.near/Every.Post.Create/source_code.jsx
+++ efiz.near/Every.Post.Create/source_code.jsx
@@ -1,20 +1,20 @@
-const type = props.type || "efiz.near/type/document";
+const type = props.type || "md";
 const key = props.key || "main";
 
-function postThing(data) {
+function postThing(data, notifications, hashtags) {
   // get the root thing from data
   const thing = data.index.thing;
-  // get the key (thing Id)
-  const key = JSON.parse(thing).key;
+  // get the thingId
+  const thingId = JSON.parse(thing).key;
   // build the path
-  const path = `${context.accountId}/thing/${key}`;
+  const path = `${context.accountId}/thing/${thingId}`;
   // create a post referencing the thing
   data.post = {
     [key]: JSON.stringify({ path, type }),
   };
   // and tell the indexer to index the post
   data.index.post = JSON.stringify({
-    key,
+    key: key,
     value: {
       path,
       type,
