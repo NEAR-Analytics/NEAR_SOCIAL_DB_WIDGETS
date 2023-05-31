70e172083b6a4ea25fc5dd0f9e6a1a35c5cb9dae
near <near>
2023-05-31 23:17:50 +0000

    Update dev.side by devs.near at 2023-05-31T23:17:50.147Z

--- devs.near/dev.side/source_code.jsx
+++ devs.near/dev.side/source_code.jsx
@@ -11,10 +11,6 @@ const defaultWidgets = [
     src: "devs.near/widget/dev.info",
   },
   {
-    src: "devs.near/widget/dev.start",
-    requiresLogin: true,
-  },
-  {
     src: "devs.near/widget/dev.search",
   },
   {
@@ -40,22 +36,27 @@ const Div = styled.div`
 
 return (
   <Div>
-    {context.accountId && (
-      <a
-        key="edit"
-        href={"#/devs.near/widget/dev.config"}
-        className="edit-link position-absolute top-0 end-0 link-secondary me-2 mt-1"
-      >
-        <i class="bi bi-pencil" /> Customize
-      </a>
-    )}
-    {widgets.map(
-      ({ src, requiresLogin }, i) =>
-        (!requiresLogin || context.accountId) && (
-          <div key={i} className="text-bg-light rounded-4 p-3 mb-3">
-            <Widget src={src} />
-          </div>
-        )
-    )}
+    <div className="mb-3">
+      <h5>Welcome Builders!</h5>
+      {context.accountId && (
+        <a
+          key="edit"
+          href={"#/devs.near/widget/dev.main.config"}
+          className="edit-link position-absolute top-0 end-0 link-secondary me-2 mt-1"
+        >
+          <i class="bi bi-pencil-square" /> Customize
+        </a>
+      )}
+    </div>
+    <div>
+      {widgets.map(
+        ({ src, requiresLogin }, i) =>
+          (!requiresLogin || context.accountId) && (
+            <div key={i} className="text-bg-light rounded-4 p-3 mb-3">
+              <Widget src={src} />
+            </div>
+          )
+      )}
+    </div>
   </Div>
 );
