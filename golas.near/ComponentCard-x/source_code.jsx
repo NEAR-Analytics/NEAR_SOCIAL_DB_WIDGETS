cb1ab9d47cc9809f19d84fa00271f61ad63326e2
golas.near <golas.near>
2023-05-31 20:26:29 +0000

    Update ComponentCard-x by golas.near at 2023-05-31T20:26:29.713Z

--- golas.near/ComponentCard-x/source_code.jsx
+++ golas.near/ComponentCard-x/source_code.jsx
@@ -1,2 +1,9 @@
-const [accountId, widget, widgetName] = props.src.split("/");
+const metadata = Social.get(
+  `${accountId}/widget/${widgetName}/metadata/**`,
+  "final"
+);
+const tags = Object.keys(metadata.tags || {});
+const detailsUrl = `#/near/widget/ComponentDetailsPage?src=${accountId}/widget/${widgetName}`;
+const appUrl = `#/${accountId}/widget/${widgetName}`;
+const accountUrl = `#/near/widget/ProfilePage?accountId=${accountId}`;
 return <div>Hello World</div>;
