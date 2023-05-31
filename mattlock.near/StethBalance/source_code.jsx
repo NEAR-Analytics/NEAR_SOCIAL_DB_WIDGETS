311f056f6b55a53908467c19cf823796b76c3b18
mattlockyer <mattlockyer>
2023-05-31 23:54:16 +0000

    Update StethBalance by mattlock.near at 2023-05-31T23:54:16.242Z

---
+++ mattlock.near/StethBalance/source_code.jsx
@@ -0,0 +1,53 @@
+const sender = props.sender || `0x525521d79134822a342d330bd91da67976569af1`;
+const message = props.message || `Balance is: `;
+const unit = `STETH`;
+
+// FETCH LIDO ABI
+
+const lidoContract = "0xae7ab96520de3a18e5e111b5eaab095312d7fe84";
+const tokenDecimals = 18;
+
+const lidoAbi = fetch(
+  "https://raw.githubusercontent.com/lidofinance/lido-subgraph/master/abis/Lido.json"
+);
+if (!lidoAbi.ok) {
+  return "Loading";
+}
+
+const iface = new ethers.utils.Interface(lidoAbi.body);
+
+// HELPER FUNCTIONS
+
+const getStakedBalance = (receiver) => {
+  const encodedData = iface.encodeFunctionData("balanceOf", [receiver]);
+
+  return Ethers.provider()
+    .call({
+      to: lidoContract,
+      data: encodedData,
+    })
+    .then((rawBalance) => {
+      const receiverBalanceHex = iface.decodeFunctionResult(
+        "balanceOf",
+        rawBalance
+      );
+
+      return Big(receiverBalanceHex.toString())
+        .div(Big(10).pow(tokenDecimals))
+        .toFixed(2)
+        .replace(/\d(?=(\d{3})+\.)/g, "$&,");
+    });
+};
+
+if (state.stakedBalance === undefined && sender) {
+  getStakedBalance(sender).then((stakedBalance) => {
+    State.update({ stakedBalance });
+  });
+}
+
+return (
+  <span>
+    {message}
+    {state.stakedBalance} {unit}
+  </span>
+);
