ecd92749ddfc821064d48bd99355079ea5a0976c
sayalot.near <sayalot.near>
2023-05-31 00:07:15 +0000

    Update SayALot_Comment by sayalot.near at 2023-05-31T00:07:15.596Z

--- sayalot.near/SayALot_Comment/source_code.jsx
+++ sayalot.near/SayALot_Comment/source_code.jsx
@@ -22,11 +22,6 @@ const link = `#/mob.near/widget/MainPage.Comment.Page?accountId=${accountId}&blo
 
 return (
   <>
-    <div>
-      {content}
-      {"..."}
-      {raw}
-    </div>
     <div
       className={`pt-3 border-top pb-2 ${
         highlight ? "bg-warning bg-opacity-10" : ""
