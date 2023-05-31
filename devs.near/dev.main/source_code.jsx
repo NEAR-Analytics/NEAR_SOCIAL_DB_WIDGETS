c6085286fb11a998f1705bc70530dc73122de2b2
near <near>
2023-05-31 22:45:15 +0000

    Update dev.main by devs.near at 2023-05-31T22:45:15.758Z

--- devs.near/dev.main/source_code.jsx
+++ devs.near/dev.main/source_code.jsx
@@ -52,7 +52,7 @@ return (
           href={"#/create.near/widget/Custom.Page.Editor"}
           className="edit-link position-absolute top-0 end-0 link-secondary me-2 mt-1"
         >
-          <i class="bi bi-pencil-square" /> Edit Main
+          <i class="bi bi-pencil-square" /> Edit Page
         </a>
       )}
     </div>
