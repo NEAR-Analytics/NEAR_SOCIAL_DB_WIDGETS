42f8a3949e7475538309524424744eebd68ed76a
near <near>
2023-05-31 21:12:27 +0000

    Update dev.main by devs.near at 2023-05-31T21:12:27.185Z

---
+++ devs.near/dev.main/source_code.jsx
@@ -0,0 +1,74 @@
+const hashtag = props.hashtag ?? "dev";
+
+const main = context.accountId
+  ? Social.get(`${context.accountId}/settings/dev/main`)
+  : undefined;
+
+if (main === null) {
+  return "";
+}
+
+const defaultWidgets = [
+  {
+    src: "devs.near/widget/dev.menu",
+  },
+  {
+    src: "devs.near/widget/dev.profile",
+    requiresLogin: true,
+  },
+  {
+    src: "devs.near/widget/dev.groups",
+  },
+  {
+    src: "devs.near/widget/dev.feed",
+  },
+];
+
+const widgets = (main && JSON.parse(main)) ?? defaultWidgets;
+
+const Div = styled.div`
+  position: relative;
+  @media (hover: hover) {
+    > .edit-link {
+      display: none;
+    }
+  }
+  &:hover {
+    > .edit-link {
+      display: inline;
+    }
+  }
+`;
+
+return (
+  <Div>
+    <div className="mb-3">
+      <Widget
+        src="miraclx.near/widget/Attribution"
+        props={{
+          dep: true,
+          authors: ["devs.near"],
+        }}
+      />
+      {context.accountId && (
+        <a
+          key="edit"
+          href={"#/create.near/widget/Custom.Page.Editor"}
+          className="edit-link position-absolute top-0 end-0 link-secondary me-2 mt-1"
+        >
+          <i class="bi bi-pencil" /> Edit Main
+        </a>
+      )}
+    </div>
+    <div>
+      {widgets.map(
+        ({ src, requiresLogin }, i) =>
+          (!requiresLogin || context.accountId) && (
+            <div key={i} className="text-bg-light rounded-4 p-3 mb-3">
+              <Widget src={src} />
+            </div>
+          )
+      )}
+    </div>
+  </Div>
+);
