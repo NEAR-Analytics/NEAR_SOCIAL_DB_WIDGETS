c86eea9cc1eaaaa4adfd5459dc4b8d2918f194b8
near <near>
2023-05-31 21:43:29 +0000

    Update dev.info by devs.near at 2023-05-31T21:43:29.196Z

---
+++ devs.near/dev.info/source_code.jsx
@@ -0,0 +1,45 @@
+return (
+  <div>
+    <h5>Welcome Builders!</h5>
+    <div className="m-2 d-flex gap-2 flex-wrap">
+      <a className="btn btn-outline-primary" href="#/devs.near/widget/">
+        Apply to Join
+      </a>
+      <a className="btn btn-outline-primary" href="#/hack.near/widget/Academy">
+        Learn More
+      </a>
+    </div>
+    <div className="m-2 d-flex gap-2 flex-wrap">
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="#/mob.near/widget/ProfilePage?accountId=devs.near"
+      >
+        <i className="bi bi-person-circle"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://www.nearbuilders.com"
+      >
+        <i className="bi bi-globe"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://github.com/NEARbuilders"
+      >
+        <i className="bi bi-github"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://calendar.google.com/calendar/u/0?cid=Y182NGYxNGNkMDFiMWY0MmU1OGE4MTY2MGYyOGVmOWFkZjE4NjdhMTJlNDJiM2UyZDNhMTc3ODczOTYwYjAyNTcwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20"
+      >
+        <i className="bi bi-calendar3"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="#/devs.near/widget/dev.config"
+      >
+        <i className="bi bi-gear-fill"></i>
+      </a>
+    </div>
+  </div>
+);
