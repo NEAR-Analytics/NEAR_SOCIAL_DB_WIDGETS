531bca5914379e37b34a0fc3037e3219c338f794
elliotBraem <elliotBraem>
2023-05-31 15:52:07 +0000

    Update Every.Thing.Create by efiz.near at 2023-05-31T15:52:07.615Z

--- efiz.near/Every.Thing.Create/source_code.jsx
+++ efiz.near/Every.Thing.Create/source_code.jsx
@@ -1,13 +1,22 @@
 const type = props.type || "";
 const postThing = props.postThing;
+const availableTypes = JSON.parse(props.availableTypes) || [
+  "efiz.near/type/paragraph",
+  "efiz.near/type/Image",
+  "efiz.near/type/document",
+  "efiz.near/type/feed",
+  "efiz.near/type/idea",
+  "md",
+];
 
 State.init({
   selectedType: type,
+  expanded: false,
 });
 
 const composeData = () => {
   // generate a random id
-  const thingId = Math.random();
+  const thingId = state.thingId || Math.random();
   const data = {
     thing: {
       ...state.extra,
@@ -34,7 +43,6 @@ const composeData = () => {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
 `;
 
 const Row = styled.div`
@@ -44,10 +52,18 @@ const Row = styled.div`
   width: 100%;
 `;
 
-const Select = styled.select`
-  padding: 5px;
-  width: 100%;
-`;
+// const Row = styled.div`
+//   display: flex;
+//   gap: 10px;
+// `;
+
+// const Select = styled.select`
+//   padding: 5px;
+//   width: 100%;
+// `;
+
+// const Input = styled.input`
+// `;
 
 const SwitchButton = styled.button`
   padding: 5px 10px;
@@ -68,20 +84,68 @@ const TextContainer = styled.div`
     margin-left: 4px;
 `;
 
+const FormContainer = styled.div`
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  max-width: 200px;
+  margin-bottom: 10px;
+  height: 30px;
+`;
+
+const Select = styled.select`
+  height: 30px;
+`;
+
+const Button = styled.button`
+  height: 30px;
+`;
+
+const Text = styled.p`
+  display: inline-block;
+  margin-right: 10px;
+`;
+
 const handleTypeChange = (e) => {
   State.update({ selectedType: e.target.value });
 };
 
 if (state.selectedType !== "") {
-  type = JSON.parse(Social.get(state.selectedType, "final") || null);
-  if (type === null) {
-    return <></>;
+  if (state.selectedType !== "md") {
+    type = JSON.parse(Social.get(state.selectedType, "final") || null);
+    if (type === null) {
+      return <></>;
+    }
+  } else {
+    type = "md";
   }
 }
 
 const handleThingData = (value, extra) => {
   State.update({ thing: value, extra });
 };
+
+function RenderTypeCreate() {
+  if (type !== "") {
+    if (type?.widgets?.create !== undefined) {
+      return (
+        <Widget
+          src={type?.widgets?.create}
+          props={{ onChange: handleThingData }} // onChange
+        />
+      );
+    } else {
+      return (
+        <Widget
+          src="efiz.near/widget/every.md.create"
+          props={{ onChange: handleThingData }} // onChange
+        />
+      );
+    }
+  }
+}
 
 return (
   <>
@@ -94,25 +158,31 @@ return (
           <Row>
             <Select value={state.selectedType} onChange={handleTypeChange}>
               <option value="">Select a type</option>
-              <option value="efiz.near/type/paragraph">
-                efiz.near/type/paragraph
-              </option>
-              <option value="efiz.near/type/Image">efiz.near/type/Image</option>
-              <option value="efiz.near/type/document">
-                efiz.near/type/document
-              </option>
+              {availableTypes.map((it) => (
+                <option value={it} key={it}>
+                  {it}
+                </option>
+              ))}
             </Select>
           </Row>
         </>
       ) : null}
 
-      {type?.widgets?.create && (
-        <Widget
-          src={type?.widgets?.create}
-          props={{ onChange: handleThingData }} // onChange
-        />
-      )}
+      <RenderTypeCreate />
     </Container>
+    <Button onClick={() => State.update({ expanded: !state.expanded })}>
+      optional {state.expanded ? "-" : "+"}
+    </Button>
+    <Row>
+      {state.expanded ? (
+        <>
+          <Input
+            onChange={(e) => State.update({ thingId: e.target.value })}
+            placeholder="thing id"
+          />
+        </>
+      ) : null}
+    </Row>
     <CommitButton
       force
       data={composeData()}
