8fb990ce492d507c7a3e4274d33f8e44bfd28a15
near-ndc <near-ndc>
2023-06-01 22:43:49 +0000

    Update EasyPoll.DisplayMultiVote by neardigitalcollective.near at 2023-06-01T22:43:49.888Z

---
+++ neardigitalcollective.near/EasyPoll.DisplayMultiVote/source_code.jsx
@@ -0,0 +1,71 @@
+const widgetOwner = "neardigitalcollective.near"
+
+const renderMultipleChoiceInput = ({
+    questionNumber,
+    questionType,
+    option,
+    optionNumber,
+  }) => {
+    return (
+      <>
+        {!props.canVote ? (
+          <Widget
+            src={`${widgetOwner}/widget/EasyPoll.Percentage`}
+            props={{
+              state: props.state,
+              questionNumber: questionNumber,
+              questionType: questionType,
+              option: option,
+              optionNumber: optionNumber,
+              canVote: props.canVote,
+              getBgColor: props.getBgColor,
+              getFontColor: props.getFontColor,
+              countVotes: props.countVotes,
+              getBorderRadious: props.getBorderRadious,
+              calculatePercentageOfOption: props.calculatePercentageOfOption,
+              getBlockTimestamp: props.getBlockTimestamp,
+              clickRadioInputHandler: props.clickRadioInputHandler,
+              clickCheckboxInputHandler: props.clickCheckboxInputHandler,
+            }}
+          />
+        ) : (
+          <div className="d-flex align-content-center">
+            <input
+              className="form-check-input"
+              id={`${questionNumber}-${optionNumber}`}
+              name={`${questionNumber}-${questionType}`}
+              key={`${questionNumber}-${optionNumber}-${props.state.vote}`}
+              style={props.getInputStyles(
+                questionType,
+                questionNumber,
+                optionNumber
+              )}
+              type={questionType == "2" ? "checkbox" : "radio"}
+              value={optionNumber}
+              checked={
+                questionType == "2"
+                  ? props.state.vote[questionNumber].includes(optionNumber + "")
+                  : props.state.vote[questionNumber] == optionNumber + ""
+              }
+              onClick={
+                questionType != "2" &&
+                props.clickRadioInputHandler(questionNumber, optionNumber)
+              }
+              onChange={
+                questionType == "2" &&
+                props.clickCheckboxInputHandler(questionNumber, optionNumber)
+              }
+            />
+            <label for={`${questionNumber}-${optionNumber}`}>{option}</label>
+          </div>
+        )}
+      </>
+    );
+  };
+  return renderMultipleChoiceInput({
+    questionNumber: props.questionNumber,
+    questionType: props.questionType,
+    option: props.option,
+    optionNumber: props.optionNumber,
+  });
+  
