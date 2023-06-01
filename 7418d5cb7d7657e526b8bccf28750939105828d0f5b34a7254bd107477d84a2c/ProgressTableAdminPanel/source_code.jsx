a034c1c1e46c8bda02a9ae4115b46424c9dcf47a
MilanPoznan <MilanPoznan>
2023-06-01 14:44:30 +0000

    Update ProgressTableAdminPanel by 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c at 2023-06-01T14:44:30.418Z

--- 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableAdminPanel/source_code.jsx
+++ 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableAdminPanel/source_code.jsx
@@ -195,6 +195,8 @@ function changeItemInState() {
   });
 }
 
+console.log(Object.entries(myState.allItems));
+
 return (
   <DashboardWrapper>
     <EditModalWrapper isOpen={myState.isModalOpen}>
@@ -263,27 +265,29 @@ return (
     <ListTitle>Existing Items</ListTitle>
     <ItemsListWrapper>
       {myState.allItems &&
-        Object.entries(myState.allItems).map((item, index) => (
-          <SingleItem isActive={item[1] !== null}>
-            <p>
-              {item[0]} : {item[1].value}
-            </p>
-            {item[1] !== null && (
-              <RemoveItemBtn onClick={() => removeItemFromState(item[0])}>
-                +
-              </RemoveItemBtn>
-            )}
+        Object.entries(myState.allItems)
+          .sort((a, b) => a[0].localeCompare(b[0]))
+          .map((item, index) => (
+            <SingleItem isActive={item[1] !== null}>
+              <p>
+                {item[0]} : {item[1].value}
+              </p>
+              {item[1] !== null && (
+                <RemoveItemBtn onClick={() => removeItemFromState(item[0])}>
+                  +
+                </RemoveItemBtn>
+              )}
 
-            <EditSvg
-              onClick={() => openModal(item)}
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
-            >
-              <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z" />
-            </EditSvg>
-          </SingleItem>
-        ))}
+              <EditSvg
+                onClick={() => openModal(item)}
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z" />
+              </EditSvg>
+            </SingleItem>
+          ))}
     </ItemsListWrapper>
   </DashboardWrapper>
 );
