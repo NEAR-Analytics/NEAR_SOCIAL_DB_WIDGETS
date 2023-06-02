6a16b929f0bfd89a4598953be17c9f00044c588b
near-ndc <near-ndc>
2023-06-02 14:36:09 +0000

    Update EasyPoll.Main by neardigitalcollective.near at 2023-06-02T14:36:09.839Z

--- neardigitalcollective.near/EasyPoll.Main/source_code.jsx
+++ neardigitalcollective.near/EasyPoll.Main/source_code.jsx
@@ -5,7 +5,7 @@ const widgetOwner = "neardigitalcollective.near";
 const getFirstSBTToken = () => {
   const view = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
     account: `${context.accountId}`,
-    issuer: "gooddollar-v1.i-am-human.near",
+    issuer: "fractal.i-am-human.near",
   });
   return view?.[0]?.[1]?.[0];
 };
