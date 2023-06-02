4b09e6558a24e333f8729a991a6fd7558845971b
MilanPoznan <MilanPoznan>
2023-06-02 09:04:55 +0000

    Update ProgressTableAdminPanel by 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c at 2023-06-02T09:04:55.527Z

--- 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableAdminPanel/source_code.jsx
+++ 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/ProgressTableAdminPanel/source_code.jsx
@@ -4,6 +4,37 @@ const DashboardWrapper = styled.div`
     max-width: 600px; 
 `;
 
+const Header = styled.div`
+
+`;
+const AddItemForm = styled.div`
+    margin: 50px auto;
+    width: 90%;
+    max-width: 600px; 
+  
+`;
+
+const AddUserForm = styled.div`
+    margin: 50px auto;
+    width: 90%;
+    max-width: 600px; 
+     
+`;
+
+const UserButtonWrapper = styled.div`
+  width: 100%;
+  height: 80px;
+  display: block;
+   button {
+      margin: 20px 10px 40px;
+      float: right;
+    }
+
+`;
+
+const AdminsList = styled.div`
+`;
+
 const ListTitle = styled.h2`
   margin: 20px 0;
 `;
@@ -89,15 +120,23 @@ const ButtonWapper = styled.div`
 
 //Styles ENDED
 
-const contr_id = "widget-progress-table.near";
-function getNear() {
-  return Near.view("widget-progress-table.near", "get_data", `{}`);
-}
-
-console.log("NEAR", getNear());
-
 const accountId = context.accountId;
 
+const contr_id = "widget-progress-table.near";
+
+function chekcIsUserAdmin() {
+  return Near.view(contr_id, "is_account_admin", {
+    account_id: accountId,
+  });
+}
+
+function getItems() {
+  return Near.view(contr_id, "get_data", `{}`);
+}
+
+function getAdmins() {
+  return Near.view(contr_id, "get_all_admins", `{}`);
+}
 if (!accountId) {
   return "Please sign in with NEAR wallet";
 }
@@ -106,17 +145,21 @@ const myState = State.init({
   key: "",
   value: "",
   linkValue: "",
-  allItems: getNear(),
+  allItems: getItems(),
   editKey: "",
   editValue: "",
   editLink: "",
   isModalOpen: false,
+  itemFormActive: false,
+  userInput: "",
+  admins: getAdmins(),
 });
 
 if (myState.allItems === null) {
   State.update(
     {
-      allItems: getNear(),
+      allItems: getItems(),
+      admins: getAdmins(),
     },
     [items]
   );
@@ -154,23 +197,8 @@ function addItem() {
 
 //Remove item from local state
 function removeItemFromState(key) {
-  //   State.update({
-  //     allItems: { ...myState.allItems, [key]: null },
-  //   });
-
   return Near.call(contr_id, "remove_user_item", { key: key });
 }
-
-//Upload data on chain
-// function uploadData() {
-//   Social.set({
-//     data: {
-//       "7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c": {
-//         testWidget: myState.allItems,
-//       },
-//     },
-//   });
-// }
 
 //Open modal for editing item from local state
 function openModal(item) {
@@ -203,89 +231,138 @@ function changeItemInState() {
   });
 }
 
+//Function related to admin logic
+function addAdmin() {
+  Near.call(contr_id, "add_whitelisted_account", {
+    accountId: myState.userInput,
+  });
+}
+function removeAdmin() {
+  Near.call(contr_id, "", { accountId: myState.userInput });
+}
+
+if (!chekcIsUserAdmin()) {
+  return <h1>You dont have permision</h1>;
+}
 return (
   <DashboardWrapper>
-    <EditModalWrapper isOpen={myState.isModalOpen}>
-      <CloseModal onClick={() => State.update({ isModalOpen: false })}>
-        +
-      </CloseModal>
-      <EditModal>
-        <p>
-          You are editing <strong>{myState.editKey}</strong>
-        </p>
-        <label for="new-value">Value: </label>
-        <input
-          type="number"
-          id="new-value"
-          value={myState.editValue}
-          onChange={(e) => State.update({ editValue: e.target.value })}
-        />
-        <label for="new-link-value">Value: </label>
+    <Header>
+      <button onClick={() => State.update({ itemFormActive: true })}>
+        Add New Item
+      </button>
+      <button onClick={() => State.update({ itemFormActive: false })}>
+        Add New Admin
+      </button>
+    </Header>
+    {state.itemFormActive && (
+      <AddItemForm>
+        <EditModalWrapper isOpen={myState.isModalOpen}>
+          <CloseModal onClick={() => State.update({ isModalOpen: false })}>
+            +
+          </CloseModal>
+          <EditModal>
+            <p>
+              You are editing <strong>{myState.editKey}</strong>
+            </p>
+            <label for="new-value">Value: </label>
+            <input
+              type="number"
+              id="new-value"
+              value={myState.editValue}
+              onChange={(e) => State.update({ editValue: e.target.value })}
+            />
+            <label for="new-link-value">Value: </label>
+            <input
+              type="text"
+              id="new-link-value"
+              value={myState.editLink}
+              onChange={(e) => State.update({ editLink: e.target.value })}
+            />
+            <ButtonWapper>
+              <button onClick={() => changeItemInState()}>Submit</button>
+            </ButtonWapper>
+          </EditModal>
+        </EditModalWrapper>
+        <FormWrapper>
+          <ListTitle>Add new item: </ListTitle>
+          <label for="key">Key: </label>
+          <input
+            type="text"
+            onChange={(e) => State.update({ key: e.target.value })}
+            value={myState.key}
+            id="key"
+          />
+          <label for="value">Value: </label>
+          <input
+            type="number"
+            id="value"
+            value={myState.value}
+            onChange={(e) => State.update({ value: e.target.value })}
+          />
+          <label for="link">Link: </label>
+          <input
+            type="text"
+            id="link"
+            value={myState.linkValue}
+            onChange={(e) => State.update({ linkValue: e.target.value })}
+          />
+          <ButtonWapper>
+            <button onClick={addItem}>Add item</button>
+          </ButtonWapper>
+        </FormWrapper>
+
+        <ListTitle>Existing Items</ListTitle>
+        <ItemsListWrapper>
+          {myState.allItems &&
+            Object.entries(myState.allItems)
+              .sort((a, b) => a[0].localeCompare(b[0]))
+              .map((item, index) => (
+                <SingleItem isActive={item[1] !== null}>
+                  <p>
+                    {item[0]} : {item[1].value}
+                  </p>
+                  {item[1] !== null && (
+                    <RemoveItemBtn onClick={() => removeItemFromState(item[0])}>
+                      +
+                    </RemoveItemBtn>
+                  )}
+
+                  <EditSvg
+                    onClick={() => openModal(item)}
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z" />
+                  </EditSvg>
+                </SingleItem>
+              ))}
+        </ItemsListWrapper>
+      </AddItemForm>
+    )}
+    {!state.itemFormActive && (
+      <AddUserForm>
+        <label for="userInput">Insert Account ID </label>
         <input
           type="text"
-          id="new-link-value"
-          value={myState.editLink}
-          onChange={(e) => State.update({ editLink: e.target.value })}
+          onChange={(e) => State.update({ userInput: e.target.value })}
+          value={myState.userInput}
+          id="userInput"
         />
-        <ButtonWapper>
-          <button onClick={() => changeItemInState()}>Submit</button>
-        </ButtonWapper>
-      </EditModal>
-    </EditModalWrapper>
-    <FormWrapper>
-      <ListTitle>Add new item: </ListTitle>
-      <label for="key">Key: </label>
-      <input
-        type="text"
-        onChange={(e) => State.update({ key: e.target.value })}
-        value={myState.key}
-        id="key"
-      />
-      <label for="value">Value: </label>
-      <input
-        type="number"
-        id="value"
-        value={myState.value}
-        onChange={(e) => State.update({ value: e.target.value })}
-      />
-      <label for="link">Link: </label>
-      <input
-        type="text"
-        id="link"
-        value={myState.linkValue}
-        onChange={(e) => State.update({ linkValue: e.target.value })}
-      />
-      <ButtonWapper>
-        <button onClick={addItem}>Add item</button>
-      </ButtonWapper>
-    </FormWrapper>
+        <UserButtonWrapper>
+          <button class="btn btn-success">Add User</button>
+          <button class="btn btn-danger">Remove User</button>
+        </UserButtonWrapper>
 
-    <ListTitle>Existing Items</ListTitle>
-    <ItemsListWrapper>
-      {myState.allItems &&
-        Object.entries(myState.allItems)
-          .sort((a, b) => a[0].localeCompare(b[0]))
-          .map((item, index) => (
-            <SingleItem isActive={item[1] !== null}>
-              <p>
-                {item[0]} : {item[1].value}
-              </p>
-              {item[1] !== null && (
-                <RemoveItemBtn onClick={() => removeItemFromState(item[0])}>
-                  +
-                </RemoveItemBtn>
-              )}
-
-              <EditSvg
-                onClick={() => openModal(item)}
-                width="18"
-                height="18"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z" />
-              </EditSvg>
-            </SingleItem>
-          ))}
-    </ItemsListWrapper>
+        <AdminsList>
+          <h2> Admins: </h2>
+          <ul>
+            {Object.keys(myState.admins).map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        </AdminsList>
+      </AddUserForm>
+    )}
   </DashboardWrapper>
 );
