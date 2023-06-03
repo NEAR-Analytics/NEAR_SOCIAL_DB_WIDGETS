40c7890508650b0e0c6423c833dc54f0c159cccb
elliotBraem <elliotBraem>
2023-06-03 19:06:05 +0000

    Update create by efiz.near at 2023-06-03T19:06:05.582Z

--- efiz.near/Create/source_code.jsx
+++ efiz.near/Create/source_code.jsx
@@ -1,206 +1,82 @@
-/*
----props---
-
-props.widgetPath?: string,
-
-*/
-
-// How can I access the widget path dynamically?
-const initWidgetPath = "efiz.near/widget/Create";
-
 State.init({
-  widgetPath: initWidgetPath,
-  color: "",
-  brand: "",
-  size: "",
-  material: "",
-  img: null,
-  message: "",
+  created: false,
 });
 
-// START GET THE WIDGET COMMIT
-const historyBlocksRequest = Social.keys(`${initWidgetPath}`, "final", {
-  return_type: "History",
-});
-
-if (historyBlocksRequest === null) return "loading...";
-
-const [widgetAccountId, _, widgetName] = initWidgetPath.split("/");
-
-let blocksChanges =
-  historyBlocksRequest[widgetAccountId]?.["widget"]?.[widgetName];
-
-// Latest commit sits at blocksChanges[0]
-// How do we know this is the one they are using though?
-// END GET THE WIDGET COMMIT
-
-const composeData = () => {
+function composeData() {
   const data = {
     thing: {
-      main: JSON.stringify({
-        color: state.color,
-        brand: state.brand,
-        size: state.size,
-        material: state.material,
-        commit: blocksChanges[0],
-        img: state.img
-          ? `https://ipfs.near.social/ipfs/${state.img.cid}`
-          : null,
-      }),
-    },
-    index: {
-      thing: JSON.stringify({
-        key: "main",
-        value: {
-          type: "thing",
+      core: {
+        data: {
+          name: "",
+          navigation: [
+            {
+              name: "kanban",
+              src: "every.near/thing/kanban",
+            },
+            {
+              name: "social",
+              src: "every.near/thing/feed",
+            },
+          ],
         },
-      }),
+        type: "efiz.near/type/core",
+      },
     },
   };
-
   return data;
-};
+}
 
-const handleImageUpload = (files) => {
-  if (files?.length > 0) {
-    State.update({
-      img: {
-        uploading: true,
-        cid: null,
-      },
-    });
-    const body = files[0];
-    State.update({
-      img: body,
-    });
-    asyncFetch("https://ipfs.near.social/add", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-      },
-      body,
-    }).then((res) => {
-      const cid = res.body.cid;
-
-      State.update({
-        img: {
-          cid,
-        },
-      });
-    });
-  } else {
-    State.update({
-      img: null,
-    });
+function Thing() {
+  // Renders the path according to type (VM doesn't offer switch-case w/ default)
+  if (type === "widget") {
+    // TODO: Verify that props are passed
+    // How to allow user to lock at a specific height?
+    return <Widget src={path} props={props} />;
   }
-};
-
-const createThing = () => {
-  asyncFetch("https://monkfish-app-ginhc.ondigitalocean.app/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: `
-      mutation {
-        things {
-          create(type: { name: { is: "Idea"} }) {
-            message
-          }
-        }
-      }
-  `,
-    }),
-  }).then((res) => {
-    const message = res.body?.message || res.body?.errors[0]?.message;
-
-    State.update({
-      message,
-    });
-  });
-};
+  if (type === "settings") {
+    // Standardize path to {accountId}/settings/**
+    parts.splice(2);
+    parts.push("**");
+    path = parts.join("/");
+    return (
+      <Widget
+        src="efiz.near/widget/Every.Setting"
+        props={{ path, blockHeight }}
+      />
+    );
+  }
+  if (type === "thing") {
+    // get the thing data
+    const thing = Social.get(path, blockHeight);
+    thing = JSON.parse(thing || "null");
+    type = thing.type || null;
+    // get the type data
+    const type = JSON.parse(Social.get(type, blockHeight) || "null");
+    if (type === null) {
+      console.log(`edge case: thing ${path} had an invalid type: ${thingType}`);
+    }
+    // get the widget to render this thing
+    const widgetSrc = type?.widgets?.view;
+    return (
+      <Widget src={widgetSrc} props={{ data: thing.data, path, blockHeight }} />
+    );
+  }
+  // DEFAULT:
+  return <p>The type: {type} is not yet supported.</p>;
+}
 
 return (
-  <div>
-    <h1 class="text-center">Thing</h1>
-    {state.img ? (
-      <img
-        class="rounded w-100 h-100"
-        style={{ objectFit: "cover" }}
-        src={`https://ipfs.near.social/ipfs/${state.img.cid}`}
-        alt="upload preview"
-      />
-    ) : null}
-    <Files
-      multiple={false}
-      accepts={["image/*"]}
-      minFileSize={1}
-      clickable
-      onChange={handleImageUpload}
-      style={{
-        cursor: "pointer",
-      }}
-      class="text-center d-flex justify-content-center align-items-center"
-    >
-      <div class="d-flex m-4 px-2 py-1 rounded bg-black text-white justify-content-center align-items-center">
-        {state.img?.uploading ? (
-          <>...</>
-        ) : state.img?.cid ? (
-          "Replace"
-        ) : (
-          "Take photo"
-        )}
-      </div>
-    </Files>
-    <div class="input-group mb-3">
-      <input
-        class="form-control"
-        placeholder={"color"}
-        onChange={({ target }) => State.update({ color: target.value })}
-      />
-      <input
-        class="form-control"
-        placeholder={"brand"}
-        onChange={({ target }) => State.update({ brand: target.value })}
-      />
-      <input
-        class="form-control"
-        placeholder={"size"}
-        onChange={({ target }) => State.update({ size: target.value })}
-      />
-      <input
-        class="form-control"
-        placeholder={"material"}
-        onChange={({ target }) => State.update({ material: target.value })}
-      />
-    </div>
-    <div>
-      <div className={"list-group-item list-group-item-action "}>
-        #{blocksChanges[0]}
-      </div>
-      <div className={"list-group-item list-group-item-action "}>
-        {state.color}
-      </div>
-      <div className={"list-group-item list-group-item-action "}>
-        {state.brand}
-      </div>
-      <div className={"list-group-item list-group-item-action "}>
-        {state.size}
-      </div>
-      <div className={"list-group-item list-group-item-action "}>
-        {state.material}
-      </div>
-    </div>
-    {state.message}
-    <button onClick={createThing}>Create</button>
-    {/* <CommitButton
-      disabled={false}
-      force
-      className="btn btn-dark rounded-3"
-      data={composeData}
-    >
-      Create
-    </CommitButton> */}
-  </div>
+  <>
+    {state.created ? (
+      <></>
+    ) : (
+      <CommitButton
+        force
+        data={composeData}
+        onCommit={() => console.log("hello")}
+      >
+        create something
+      </CommitButton>
+    )}
+  </>
 );
