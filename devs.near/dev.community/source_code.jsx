ec304e1424f0e061f67ece966aa2610c4a6dae52
devs.near <devs.near>
2023-06-03 22:20:54 +0000

    Update dev.community by devs.near at 2023-06-03T22:20:54.961Z

---
+++ devs.near/dev.community/source_code.jsx
@@ -0,0 +1,272 @@
+const ownerId = props.ownerId ?? "devs.near";
+const accountId = props.accountId ?? context.accountId;
+const daoId = props.daoId ?? "build.sputnik-dao.near";
+const role = props.role ?? "community";
+
+State.init({
+  agreeIsChecked: false,
+  hasRegistered: false,
+  hasApplied: false,
+});
+
+const accountWidgetCount = [];
+let isBuilder = false;
+
+let accounts = Social.keys(`${ownerId}/graph/follow/*`, "final", {
+  return_type: "BlockHeight",
+  values_only: true,
+});
+
+if (accounts === null) {
+  return "Loading...";
+}
+
+accounts = Object.entries(accounts[ownerId].graph.follow || {});
+accounts.sort((a, b) => b[1] - a[1]);
+
+for (let i = 0; i < accounts.length; ++i) {
+  let accountId = accounts[i][0];
+  let widgets = Social.get(`${accountId}/widget/*`, "final", {
+    return_type: "BlockHeight",
+    values_only: true,
+  });
+  let widgetCount = 0;
+  if (widgets) {
+    widgetCount = Object.keys(widgets).length;
+  }
+  accountWidgetCount.push({
+    accountId: accountId,
+    count: widgetCount,
+  });
+  if (widgetCount > 0) {
+    isBuilder = true;
+  }
+}
+
+const accountWidgetSort = accountWidgetCount.sort((a, b) => b.count - a.count);
+const numAccounts = accountWidgetSort.length;
+accountWidgetSort = accountWidgetSort.slice(0, limit);
+console.log(accountWidgetSort);
+
+const handleJoin = () => {
+  const gas = 200000000000000;
+  const deposit = 100000000000000000000000;
+  Near.call([
+    {
+      contractName: daoId,
+      methodName: "add_proposal",
+      args: {
+        proposal: {
+          description: "potential member",
+          kind: {
+            AddMemberToRole: {
+              member_id: accountId,
+              role: role,
+            },
+          },
+        },
+      },
+      gas: gas,
+      deposit: deposit,
+    },
+  ]).then((resp) => {
+    // Storage.privateSet()
+    State.update({ hasApplied: true });
+  });
+};
+
+const Wrapper = styled.div`
+  --section-gap: 23px;
+  padding-top: 42px;
+
+  @media (max-width: 1155px) {
+    .line-rounded-corners {
+      display: none !important;
+    }
+  }
+
+  @media (max-width: 998px) {
+    padding-top: 0;
+  }
+`;
+
+const H1 = styled.h1`
+  font-family: "FK Grotesk", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 90px;
+  line-height: 1;
+  text-align: center;
+  letter-spacing: -0.03em;
+  color: #000;
+  margin: 0;
+  max-width: 700px;
+
+  span {
+    display: inline-block;
+    background: #DC9DFF;
+    border-radius: 20px;
+    position: relative;
+    padding: 0.1em 0.2em 0;
+
+    svg {
+      position: absolute;
+      bottom: -8px;
+      right: -10px;
+      width: 24px;
+    }
+  }
+
+  @media (max-width: 900px) {
+    font-size: 50px;
+
+    span {
+      border-radius: 12px;
+      svg {
+        position: absolute;
+        bottom: -6px;
+        right: -7px;
+        width: 16px;
+      }
+    }
+  }
+`;
+
+const Text = styled.p`
+  font-family: "FK Grotesk", sans-serif;
+  font-size: ${(p) => p.size ?? "18px"};
+  line-height: ${(p) => p.lineHeight ?? "1.5"};
+  font-weight: ${(p) => p.weight ?? "400"};
+  color: ${(p) => p.color ?? "#000"};
+  margin: 0;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: "nowrap";
+
+    @media (max-width: 998px) {
+    flex-direction: column;
+    gap: var(--section-gap);
+    }
+`;
+
+const Container = styled.div`
+  display: flex;
+  max-width: 1080px;
+  margin: 0 auto;
+  gap: var(--section-gap);
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: var(--section-gap) 24px;
+`;
+
+return (
+  <Wrapper>
+    <Container>
+      <Flex>
+        <H1>
+          <span>
+            BOS{" "}
+            <svg viewBox="0 0 26 24" fill="none" aria-hidden="true">
+              <path
+                d="M24.3767 8.06326L1.51965 0.0649912C1.10402 -0.0830767 0.639031 0.026026 0.327308 0.340346C0.0181841 0.657263 -0.0831256 1.12225 0.0701378 1.53788L8.071 23.2519C8.23726 23.7013 8.66587 24 9.14385 24H9.14644C9.62702 24 10.0556 23.6961 10.2167 23.2441L13.734 13.495L24.3325 10.2349C24.8053 10.0895 25.13 9.65824 25.1378 9.16468C25.1482 8.67112 24.8391 8.22691 24.3715 8.06326H24.3767Z"
+                fill="#323330"
+              />
+            </svg>
+          </span>
+          Builders
+        </H1>
+        <Text style={{ maxWidth: "670px" }}>
+          Did you know? The
+          <a href="https://nearbuilders.com/bos">blockchain operating system</a>
+          (BOS) makes it fun and easy to create anything! Join groups, discover
+          projects, and collaborate out loud. Everyone is building a more open
+          web that is greater than the sum of its components.
+        </Text>
+      </Flex>
+      {isBuilder ? (
+        <div>
+          <Text
+            size="18px"
+            weight="600"
+            style={{ textTransform: "uppercase", letterSpacing: "0.17em" }}
+          >
+            Your Adventure Has Begun
+          </Text>
+          <div>
+            <div className="m-2">
+              <Widget
+                src="near/widget/DIG.Button"
+                props={{
+                  href: "https://nearbuilders.com",
+                  label: "Explore Groups",
+                  variant: "outline-success",
+                  size: "large",
+                }}
+              />
+            </div>
+            <div className="m-2">
+              <Widget
+                src="near/widget/DIG.Button"
+                props={{
+                  href: "https://wallet.near.org/linkdrop/v2.keypom.near/2BNMVyPgjXgHtn9xiQkcRLzZYmsh5JyGoeaRV9Tb5rmsbqNrAsuwGPziL6ztsfjxLVzQRwGERA3JGSQ28VEA8NtL",
+                  label: "Get Started",
+                  variant: "outline-secondary",
+                  size: "large",
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Flex>
+          <Text
+            size="18px"
+            weight="600"
+            style={{ textTransform: "uppercase", letterSpacing: "0.17em" }}
+          >
+            Begin a New Adventure
+          </Text>
+          <br />
+          <div>
+            <Widget
+              src="near/widget/DIG.Button"
+              props={{
+                href: "https://wallet.near.org/linkdrop/v2.keypom.near/2BNMVyPgjXgHtn9xiQkcRLzZYmsh5JyGoeaRV9Tb5rmsbqNrAsuwGPziL6ztsfjxLVzQRwGERA3JGSQ28VEA8NtL",
+                label: "Get Started",
+                variant: "outline-secondary",
+                size: "large",
+              }}
+            />
+          </div>
+        </Flex>
+      )}
+      <br />
+    </Container>
+    <hr />
+
+    <Widget src="devs.near/widget/dev.Rank" />
+    <hr />
+    <br />
+    <Flex>
+      <Text
+        size="14px"
+        weight="600"
+        style={{
+          textTransform: "uppercase",
+          letterSpacing: "0.17em",
+          textAlign: "center",
+        }}
+      >
+        Made Possible by Collaboration
+      </Text>
+      <Widget src="hack.near/widget/dev.Badge" />
+    </Flex>
+  </Wrapper>
+);
