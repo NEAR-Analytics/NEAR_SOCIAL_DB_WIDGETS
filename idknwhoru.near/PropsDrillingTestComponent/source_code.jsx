6c7d1118f766b914e78dda10b467750067baebbd
idknwhoru.near <idknwhoru.near>
2023-06-03 22:11:49 +0000

    Update PropsDrillingTestComponent by idknwhoru.near at 2023-06-03T22:11:49.420Z

--- idknwhoru.near/PropsDrillingTestComponent/source_code.jsx
+++ idknwhoru.near/PropsDrillingTestComponent/source_code.jsx
@@ -1,11 +1,18 @@
 const { parentState } = props;
 
-console.log({ props });
+State.init({
+  flag: true,
+});
+
+console.log({ props, state });
 if (parentState) {
   const changeParentState = () => {
     parentState.loading = !parentState.loading;
+    State.update({
+      flag: !state.flag,
+    });
   };
-  return <button onClick={(_) => changeParentState()}></button>;
+  return <button onClick={(_) => changeParentState()}>click me</button>;
 }
 
 return <div>hello</div>;
