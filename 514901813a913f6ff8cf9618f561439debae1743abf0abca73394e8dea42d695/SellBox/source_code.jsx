a94c2d5c81d07c54e621bdfb56e4785c1f863c5d
514901813a913f6ff8cf9618f561439debae1743abf0abca73394e8dea42d695 <514901813a913f6ff8cf9618f561439debae1743abf0abca73394e8dea42d695>
2023-06-03 09:58:23 +0000

    Update SellBox by 514901813a913f6ff8cf9618f561439debae1743abf0abca73394e8dea42d695 at 2023-06-03T09:58:23.319Z

---
+++ 514901813a913f6ff8cf9618f561439debae1743abf0abca73394e8dea42d695/SellBox/source_code.jsx
@@ -0,0 +1,41 @@
+const css = `
+  .flex {
+    display: flex;
+    flexDirection: "row"
+    > div {
+        margin: 32px 0;
+      margin-right: 32px;
+      width: 50%;
+      height: 100%;
+      border: 2px solid rgb(6, 252, 153);
+    }
+    > div:last-of-type {
+      margin-right: 0;
+    }
+  }
+  .btn {
+      margin-bottom: 16px !important;
+  }
+`;
+
+if (!state.theme) {
+  State.update({
+    theme: styled.div`
+    ${css}
+`,
+  });
+}
+const Theme = state.theme;
+
+const { onSellComplete } = props;
+const handleSell = () => {
+  if (typeof onSellComplete === "string") {
+    return;
+  }
+  onSellComplete();
+};
+return (
+  <Theme>
+    <button onClick={handleSell}>Buy it</button>
+  </Theme>
+);
