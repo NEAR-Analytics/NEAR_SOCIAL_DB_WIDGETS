5d1ed2070562368bd2092cc32003a1be9ed4aace
idknwhoru.near <idknwhoru.near>
2023-06-03 22:08:04 +0000

    Update PropsDrillingTestComponent by idknwhoru.near at 2023-06-03T22:08:04.637Z

--- idknwhoru.near/PropsDrillingTestComponent/source_code.jsx
+++ idknwhoru.near/PropsDrillingTestComponent/source_code.jsx
@@ -1,12 +1,13 @@
 const { parentState } = props;
 
+console.log({ props });
 if (parentState) {
   const changeParentState = () => {
     parentState.update({
       isOpen: true,
     });
   };
-  return <button onClick={changeParentState}></button>;
+  return <button onClick={(_) => changeParentState()}></button>;
 }
 
 return <div>hello</div>;
