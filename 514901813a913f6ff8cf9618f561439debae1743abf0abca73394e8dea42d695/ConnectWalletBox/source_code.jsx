a914d24a6c7d5e57f465d3096bb6f2ef360082d7
514901813a913f6ff8cf9618f561439debae1743abf0abca73394e8dea42d695 <514901813a913f6ff8cf9618f561439debae1743abf0abca73394e8dea42d695>
2023-06-03 07:56:45 +0000

    Update ConnectWalletBox by 514901813a913f6ff8cf9618f561439debae1743abf0abca73394e8dea42d695 at 2023-06-03T07:56:45.771Z

---
+++ 514901813a913f6ff8cf9618f561439debae1743abf0abca73394e8dea42d695/ConnectWalletBox/source_code.jsx
@@ -0,0 +1,47 @@
+/**
+ * A simple component to notify the user to connect their wallet
+ *
+ * @remarks
+ * Can inherit a theme but will otherwise default
+ *
+ * props: {
+ *  onConnected: () => undefined;
+ * }
+ */
+
+const sender = Ethers.send("eth_requestAccounts", [])[0];
+if (sender) {
+  console.log(document);
+  State.update({
+    connectBox: {
+      isConnected: true,
+    },
+  });
+  // new Function(props.onConnected); // :)
+  return;
+}
+
+const fontUrl = `https://ipfs.io/ipfs/bafkreicrs3gh7f77yhpw4xiejx35cd56jcczuhvqbwkn77g2ztkrjejopa`;
+const css = {};
+if (!state.theme) {
+  State.update({
+    theme: styled.div`
+    font-family: Pixter;
+    background: black;
+    color: white;
+    padding: 32px;
+    .container
+    ${css}
+`,
+  });
+}
+const Theme = state.theme;
+
+return (
+  <Theme>
+    <div class="container">
+      <h2>Connect Your Wallet</h2>
+      <p>You require your wallet to gamble shitcoins</p>
+    </div>
+  </Theme>
+);
