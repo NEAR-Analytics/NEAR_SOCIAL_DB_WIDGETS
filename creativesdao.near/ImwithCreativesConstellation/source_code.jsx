6ed914f64306c388189e6a332536daceb7ad1baf
creativesdao.near <creativesdao.near>
2023-06-04 08:15:04 +0000

    Update ImwithCreativesConstellation by creativesdao.near at 2023-06-04T08:15:04.792Z

---
+++ creativesdao.near/ImwithCreativesConstellation/source_code.jsx
@@ -0,0 +1,79 @@
+const accountId = props.accountId ?? "*";
+
+const data = Social.keys(`${accountId}/post/iwcc`, "final", {
+  return_type: "History",
+});
+
+if (!data) {
+  return "Loading";
+}
+
+const processData = (data) => {
+  const accounts = Object.entries(data);
+
+  console.log("data", data);
+  const allItems = accounts
+    .map((account) => {
+      const accountId = account[0];
+      const blockHeights = account[1].post.iwcc;
+      return blockHeights.map((blockHeight) => ({
+        accountId,
+        blockHeight,
+      }));
+    })
+    .flat();
+
+  allItems.sort((a, b) => b.blockHeight - a.blockHeight);
+  console.log("allItems", allItems);
+  return allItems;
+};
+
+if (JSON.stringify(data) !== JSON.stringify(state.data || {})) {
+  State.update({
+    data,
+    allItems: processData(data),
+  });
+}
+
+return (
+  <div>
+    <h1>👋 Creatives Constellation Fingerprint Tree</h1>
+    <h3>
+      Press your finger in the Creatives Constellation Fingerprint Tree!!!
+    </h3>
+    <CommitButton
+      data={{ post: { iwcc: "I'm with Creatives Constellation!" } }}
+      onCommit={() => {
+        State.update({
+          // TODO: Feed needs reload?
+        });
+      }}
+    >
+      Print my finger ✌️
+    </CommitButton>
+    <Widget
+      src="williamxx.near/widget/Attribution"
+      props={{
+        dep: true,
+        authors: ["creativesdao.near"],
+      }}
+    />
+    <br />
+    <div>
+      {state.allItems
+        ? state.allItems.map(({ accountId }) => (
+            <div>
+              I'm with Creatives Constellation!
+              <Widget
+                src="williamxx.near/widget/Attribution"
+                props={{
+                  dep: true,
+                  authors: [accountId],
+                }}
+              />
+            </div>
+          ))
+        : "Loading..."}
+    </div>
+  </div>
+);
