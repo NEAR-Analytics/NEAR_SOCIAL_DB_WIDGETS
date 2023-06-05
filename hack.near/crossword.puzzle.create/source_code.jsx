8665d44d85d52d9bea1c55b7e504b068b63b4a68
hack.near <hack.near>
2023-06-05 22:50:07 +0000

    Update crossword.puzzle.create by hack.near at 2023-06-05T22:50:07.232Z

--- hack.near/crossword.puzzle.create/source_code.jsx
+++ hack.near/crossword.puzzle.create/source_code.jsx
@@ -1,12 +1,19 @@
-const createPuzzle = () =>
-  Near.call("crossword.puzzle.near", "new_puzzle", {
-    answer_pk: "",
-    dimensions,
-    answers: "",
-  });
+const accountId = props.accountId ?? context.accountId;
+
+const createPuzzle = () => {
+  Near.call([
+    "crossword.puzzle.near",
+    "new_puzzle",
+    {
+      answer_pk,
+      dimensions,
+      answers,
+    },
+  ]);
+};
 
 return (
-  <>
+  <div>
     <button onClick={createPuzzle}>Create Puzzle</button>
-  </>
+  </div>
 );
