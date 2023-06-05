5e00bf4b806ed87266e9d833411959f8e520faef
jgodwill.near <jgodwill.near>
2023-06-05 15:27:22 +0000

    Update GenaDropHome by jgodwill.near at 2023-06-05T15:27:22.570Z

--- jgodwill.near/GenaDropHome/source_code.jsx
+++ jgodwill.near/GenaDropHome/source_code.jsx
@@ -12,7 +12,7 @@ const Hero = styled.div`
 `;
 const MainHeading = styled.h1`
   font-weight: 700;
-  font-size: 14vw;
+  font-size: 12vw;
   line-height: 1em;
   text-align: center;
   color: #2d3748;
