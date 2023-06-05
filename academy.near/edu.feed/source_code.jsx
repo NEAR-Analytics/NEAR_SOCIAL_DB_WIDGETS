eae1c3808a4803e3b6be1140caef31e7b58e407a
academy.near <academy.near>
2023-06-05 21:45:03 +0000

    Update edu.feed by academy.near at 2023-06-05T21:45:03.483Z

---
+++ academy.near/edu.feed/source_code.jsx
@@ -0,0 +1,42 @@
+const admins = props.admins;
+const daoId = props.daoId;
+
+const index = {
+  action: "question",
+  key: "main",
+  options: {
+    limit: 10,
+    order: "desc",
+    accountId: props.accounts,
+  },
+};
+
+const renderItem = (a) => {
+  if (a.value.type !== "md") {
+    return;
+  }
+
+  const is_hidden = Near.view(adminContract, "is_hidden", {
+    id: { account_id: a.accountId, block_height: a.blockHeight },
+  });
+
+  if (is_hidden) {
+    return;
+  }
+
+  return (
+    <div key={JSON.stringify(a)} className="my-4">
+      <Widget
+        src="academy.near/widget/dev.question.preview"
+        props={{
+          accountId: a.accountId,
+          blockHeight: a.blockHeight,
+          admins,
+          adminContract,
+        }}
+      />
+    </div>
+  );
+};
+
+return <Widget src="mob.near/widget/IndexFeed" props={{ index, renderItem }} />;
