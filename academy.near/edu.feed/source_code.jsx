26064cd7dd3c9ea53a6d6aabb7183ed0a3e0ee89
academy.near <academy.near>
2023-06-05 23:05:29 +0000

    Update edu.feed by academy.near at 2023-06-05T23:05:29.962Z

--- academy.near/edu.feed/source_code.jsx
+++ academy.near/edu.feed/source_code.jsx
@@ -3,7 +3,7 @@ const daoId = props.daoId;
 
 const index = {
   action: "question",
-  key: "main",
+  key: "edu",
   options: {
     limit: 10,
     order: "desc",
@@ -27,7 +27,7 @@ const renderItem = (a) => {
   return (
     <div key={JSON.stringify(a)} className="my-4">
       <Widget
-        src="academy.near/widget/edu.question.preview"
+        src="academy.near/widget/dev.question.preview"
         props={{
           accountId: a.accountId,
           blockHeight: a.blockHeight,
