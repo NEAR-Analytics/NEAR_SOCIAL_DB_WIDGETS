b404ca1a2a986fbd2af8acef8f449d142a474ca4
material-icons.near <material-icons.near>
2023-06-06 12:29:27 +0000

    Update 60FpsSharpIcon by material-icons.near at 2023-06-06T12:29:27.613Z

---
+++ material-icons.near/60FpsSharpIcon/source_code.jsx
@@ -0,0 +1,9 @@
+const { className, width, height, currentColor } = props;return <svg
+      class={className ?? ""}
+      width={width}
+      height={height}
+      aria-hidden="true"
+      focusable="false"
+      tabindex="-1"
+      viewBox="0 0 24 24"
+    ><g><path d="M0,0h24v24H0V0z" fill={fill ?? "currentColor"}/></g><g><g><path d="M19,8v8h-4V8H19 M22,5H12v14h10V5z M10,8V5H2v14h9v-9H5V8H10z M8,13v3H5v-3H8z"/></g></g></svg>;
