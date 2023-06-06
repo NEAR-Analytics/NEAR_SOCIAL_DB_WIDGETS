be4847e1c9a0a171f9f1d6b99c81594e2cc354bf
efiz.near <efiz.near>
2023-06-06 17:48:58 +0000

    Update every.placeholder.view by efiz.near at 2023-06-06T17:48:58.828Z

---
+++ efiz.near/every.placeholder.view/source_code.jsx
@@ -0,0 +1,27 @@
+const data = props.data;
+
+const Container = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: gray;
+  border: 2px solid #000;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 300px;
+`;
+
+const Text = styled.div`
+  font-size: 20px;
+  color: #fff;
+`;
+
+const Button = styled.button`
+`;
+
+return (
+  <Container>
+    <Text>placeholder</Text>
+  </Container>
+);
