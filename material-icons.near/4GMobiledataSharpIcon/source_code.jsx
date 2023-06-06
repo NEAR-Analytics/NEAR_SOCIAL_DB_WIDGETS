1e42a141dea17b6a4917dc4cfa418f5def522f30
material-icons.near <material-icons.near>
2023-06-06 12:28:54 +0000

    Update 4GMobiledataSharpIcon by material-icons.near at 2023-06-06T12:28:54.868Z

---
+++ material-icons.near/4GMobiledataSharpIcon/source_code.jsx
@@ -0,0 +1,9 @@
+const { className, width, height, currentColor } = props;return <svg
+      class={className ?? ""}
+      width={width}
+      height={height}
+      aria-hidden="true"
+      focusable="false"
+      tabindex="-1"
+      viewBox="0 0 24 24"
+    ><g><path d="M0,0h24v24H0V0z" fill={fill ?? "currentColor"}/></g><g><g><path d="M9,7H7v5H5V7H3v7h4v3h2v-3h2v-2H9V7z M17,11v2h2v2h-5V9h7V7h-9v10h9v-6H17z"/></g></g></svg>;
