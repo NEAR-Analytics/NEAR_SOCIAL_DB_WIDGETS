e714411d2c557b5d5ff02626feaee9be49ce923a
material-icons.near <material-icons.near>
2023-06-06 12:29:33 +0000

    Update 6KSharpIcon by material-icons.near at 2023-06-06T12:29:33.820Z

---
+++ material-icons.near/6KSharpIcon/source_code.jsx
@@ -0,0 +1,9 @@
+const { className, width, height, currentColor } = props;return <svg
+      class={className ?? ""}
+      width={width}
+      height={height}
+      aria-hidden="true"
+      focusable="false"
+      tabindex="-1"
+      viewBox="0 0 24 24"
+    ><g><rect fill={fill ?? "currentColor"} height="24" width="24"/></g><g><path d="M8,12.5h1.5V14H8V12.5z M21,3H3v18h18V3z M11,10.5H8v1h3V15H6.5V9H11V10.5z M18,15h-1.75l-1.75-2.25V15H13V9h1.5v2.25 L16.25,9H18l-2.25,3L18,15z"/></g></svg>;
