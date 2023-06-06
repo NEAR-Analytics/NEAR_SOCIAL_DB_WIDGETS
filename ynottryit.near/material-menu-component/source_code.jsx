1e79cb646da33e2f71a5dcdf6a3bd05814dd55e5
ynottryit.near <ynottryit.near>
2023-06-06 09:56:13 +0000

    Update material-menu-component by ynottryit.near at 2023-06-06T09:56:13.968Z

---
+++ ynottryit.near/material-menu-component/source_code.jsx
@@ -0,0 +1,58 @@
+const menustyles = {
+  menuwrap: {
+    backgroundColor: "#1a73e8",
+    padding: "16px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  ul: {
+    margin: "0",
+    listStyleType: "none",
+    display: "flex",
+    padding: "0",
+  },
+  li: {
+    margin: "0px 10px",
+    boxSizing: "border-box",
+  },
+  a: {
+    color: "#fff",
+    fontSize: "16px",
+    padding: "0px 16px",
+    lineHeight: "normal",
+  },
+};
+return (
+  <>
+    <div style={menustyles.menuwrap}>
+      <ul style={menustyles.ul}>
+        <li style={menustyles.li}>
+          <a href="#" style={menustyles.a}>
+            Home
+          </a>
+        </li>
+        <li style={menustyles.li}>
+          <a href="https://docs.near.org/" target="_blank" style={menustyles.a}>
+            About Us
+          </a>
+        </li>
+        <li style={menustyles.li}>
+          <a href="#" style={menustyles.a}>
+            Products
+          </a>
+        </li>
+        <li style={menustyles.li}>
+          <a href="#" style={menustyles.a}>
+            Blog
+          </a>
+        </li>
+        <li style={menustyles.li}>
+          <a href="#" style={menustyles.a}>
+            Contact Us
+          </a>
+        </li>
+      </ul>
+    </div>
+  </>
+);
