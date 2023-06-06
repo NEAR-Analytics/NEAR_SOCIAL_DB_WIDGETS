54d03b1c659edb1693d393a7b80a3ce20413c9e7
gator-fi.near <gator-fi.near>
2023-06-06 05:14:07 +0000

    Update CurrencyInput by gator-fi.near at 2023-06-06T05:14:07.662Z

--- gator-fi.near/CurrencyInput/source_code.jsx
+++ gator-fi.near/CurrencyInput/source_code.jsx
@@ -1,29 +1,33 @@
 State.init({
-    dialogVisible: false,
-    filterInput: "",
-    token: null,
-    modelValue: "",
-})
+  dialogVisible: false,
+  filterInput: "",
+  token: null,
+  modelValue: "",
+});
 
 const token = props.token || state.token;
 const setToken = props.setToken || ((x) => State.update({ token: x }));
 const modelValue = props.modelValue || state.modelValue;
-const setModelValue = props.setModelValue || ((x) => State.update({ modelValue: x }));
+const setModelValue =
+  props.setModelValue || ((x) => State.update({ modelValue: x }));
 const autoFocus = props.autoFocus;
 
-const tokenListJson = fetch(`https://gist.githubusercontent.com/kizzx2/af1f39667c46a07ae83acf25d3791912/raw/c7244e14940cbf6e0977f95e36fe36d981cf3214/ref-list.json`).body;
+const tokenListJson = fetch(
+  `https://gist.githubusercontent.com/kizzx2/af1f39667c46a07ae83acf25d3791912/raw/c7244e14940cbf6e0977f95e36fe36d981cf3214/ref-list.json`
+).body;
 if (!tokenListJson) return "";
 
 const tokenList = JSON.parse(tokenListJson);
 
-const filteredTokenList = tokenList.tokens.filter((x) =>
+const filteredTokenList = tokenList.tokens.filter(
+  (x) =>
     x.name.toLowerCase().includes(state.filterInput.toLowerCase()) ||
     x.symbol.toLowerCase().includes(state.filterInput.toLowerCase())
 );
 
 function handleTokenSelect(x) {
-    State.update({ dialogVisible: false });
-    setToken(x);
+  State.update({ dialogVisible: false });
+  setToken(x);
 }
 
 const Styled = styled.div`
@@ -170,60 +174,69 @@ const Styled = styled.div`
 }
 `;
 
-return <Styled>
-  <Dialog.Root open={state.dialogVisible} onOpenChange={(x) => State.update({ dialogVisible: x })}>
-    <div className="currency-input">
-      <Dialog.Trigger>
-        <div
-          class="currency-button-container"
-        >
-          {token && <>
-            <img src={token.logoURI} alt={token.name} />
-            { token.symbol }
-          </>}
-          {!token && <>Select a token</>}
-          <i class="pi pi-chevron-down" />
-        </div>
-      </Dialog.Trigger>
-      <input
-        placeholder="0.0"
-        v-model="modelValue"
-        value={modelValue}
-        onInput={(e) => setModelValue(e.target.value)}
-        type="number"
-        min="0.0"
-        autoFocus={autoFocus}
-      />
-    </div>
-
-
-    <Dialog.Overlay className="DialogOverlay">
-      <Dialog.Content className="DialogContent">
-        <Dialog.Title>
-          <div style={{width: '100%'}}>
-            <h4 class="text-grey-darken-2 mb-3">Select a Token</h4>
-            <div class="token-search-input-container">
-              <input placeholder="Token Name" autofocus
-                onInput={(e) => State.update({filterInput: e.target.value})} />
-            </div>
+return (
+  <Styled>
+    <Dialog.Root
+      open={state.dialogVisible}
+      onOpenChange={(x) => State.update({ dialogVisible: x })}
+    >
+      <div className="currency-input">
+        <Dialog.Trigger>
+          <div class="currency-button-container">
+            {token && (
+              <>
+                <img src={token.logoURI} alt={token.name} />
+                {token.symbol}
+              </>
+            )}
+            {!token && <>Select a token</>}
+            <i class="pi pi-chevron-down" />
           </div>
-        </Dialog.Title>
-        <ul class="token-list">
-          {filteredTokenList.map((token) => <li
-            class="token-list-row"
-            key={token.address}
-            onClick={() => handleTokenSelect(token)}
-          >
-            <div class="token-list-row-img-container">
-              <img src={token.logoURI} />
+        </Dialog.Trigger>
+        <input
+          placeholder="0.0"
+          v-model="modelValue"
+          value={modelValue}
+          onInput={(e) => setModelValue(e.target.value)}
+          type="number"
+          min="0.0"
+          autoFocus={autoFocus}
+        />
+      </div>
+
+      <Dialog.Overlay className="DialogOverlay">
+        <Dialog.Content className="DialogContent">
+          <Dialog.Title>
+            <div style={{ width: "100%" }}>
+              <h4 class="text-grey-darken-2 mb-3">Select a Token</h4>
+              <div class="token-search-input-container">
+                <input
+                  placeholder="Token Name"
+                  autofocus
+                  onInput={(e) => State.update({ filterInput: e.target.value })}
+                />
+              </div>
             </div>
-            <div class="token-list-row-info">
-              <div class="token-list-row-symbol">{ token.symbol }</div>
-              <div class="token-list-row-name">{ token.name }</div>
-            </div>
-          </li>)}
-        </ul>
-      </Dialog.Content>
-    </Dialog.Overlay>
-  </Dialog.Root>
-</Styled>;
+          </Dialog.Title>
+          <ul class="token-list">
+            {filteredTokenList.map((token) => (
+              <li
+                class="token-list-row"
+                key={token.address}
+                onClick={() => handleTokenSelect(token)}
+              >
+                <div class="token-list-row-img-container">
+                  <img src={token.logoURI} />
+                </div>
+                <div class="token-list-row-info">
+                  <div class="token-list-row-symbol">{token.symbol}</div>
+                  <div class="token-list-row-name">{token.name}</div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Dialog.Content>
+      </Dialog.Overlay>
+    </Dialog.Root>
+  </Styled>
+);
