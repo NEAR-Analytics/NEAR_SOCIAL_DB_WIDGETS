4a2323762aa5be314ae2e5e0c7d33d8d8dfdda76
andyh.near <andyh.near>
2023-06-08 21:21:35 +0000

    Update CallbackRender by andyh.near at 2023-06-08T21:21:35.448Z

--- andyh.near/CallbackRender/source_code.jsx
+++ andyh.near/CallbackRender/source_code.jsx
@@ -1,26 +1,21 @@
 State.init({ j: 0 });
-const renderComponent = (msg) => (
-  <h2>{msg}</h2>
-);
+const renderComponent = (msg) => <h2>{msg}</h2>;
 
-return (
-  <div>
-    {renderComponent('oh hei')}
-    // <Widget
-    //   src="andyh.near/widget/RenderTestGrandchild"
-    //   props={{
-    //     i: props.i,
-    //     j: state.j,
-    //     incrementparent: () => {
-    //       const j = state.j + 1;
-    //       State.update({ j });
-    //       return j;
-    //     },
-    //     setchildincrement: (cb0, number, cb1) => {
-    //       cb0(number, () => /xyz/gi);
-    //       incrementChild = cb0;
-    //     },
-    //   }}
-    // />
-  </div>
-);
+// <Widget
+//   src="andyh.near/widget/RenderTestGrandchild"
+//   props={{
+//     i: props.i,
+//     j: state.j,
+//     incrementparent: () => {
+//       const j = state.j + 1;
+//       State.update({ j });
+//       return j;
+//     },
+//     setchildincrement: (cb0, number, cb1) => {
+//       cb0(number, () => /xyz/gi);
+//       incrementChild = cb0;
+//     },
+//   }}
+// />
+
+return <div>{renderComponent("oh hei")}</div>;
