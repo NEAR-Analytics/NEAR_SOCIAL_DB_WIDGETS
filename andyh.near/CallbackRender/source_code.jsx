c34291f7fe26f3347a5d2d2f9d1cf2a4373dd721
andyh.near <andyh.near>
2023-06-08 21:26:59 +0000

    Update CallbackRender by andyh.near at 2023-06-08T21:26:59.101Z

--- andyh.near/CallbackRender/source_code.jsx
+++ andyh.near/CallbackRender/source_code.jsx
@@ -1,21 +1,13 @@
-State.init({ j: 0 });
 const renderComponent = (msg) => <h2>{msg}</h2>;
 
-// <Widget
-//   src="andyh.near/widget/RenderTestGrandchild"
-//   props={{
-//     i: props.i,
-//     j: state.j,
-//     incrementparent: () => {
-//       const j = state.j + 1;
-//       State.update({ j });
-//       return j;
-//     },
-//     setchildincrement: (cb0, number, cb1) => {
-//       cb0(number, () => /xyz/gi);
-//       incrementChild = cb0;
-//     },
-//   }}
-// />
-
-return <div>{renderComponent("oh hei")}</div>;
+return (
+  <div>
+    {renderComponent("i am da parent")}
+    <Widget
+      src="andyh.near/widget/CallbackRenderChild"
+      props={{
+        renderComponent,
+      }}
+    />
+  </div>
+);
