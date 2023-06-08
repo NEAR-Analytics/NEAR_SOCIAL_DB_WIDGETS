edf0413ced16cb92803d300a73de747d35378338
hack.near <hack.near>
2023-06-08 13:04:10 +0000

    Update creatives.dao by hack.near at 2023-06-08T13:04:10.343Z

--- hack.near/creatives.dao/source_code.jsx
+++ hack.near/creatives.dao/source_code.jsx
@@ -42,6 +42,16 @@ const handleJoin = () => {
 const Wrapper = styled.div`
   --section-gap: 23px;
   padding-top: 42px;
+
+  @media (max-width: 1155px) {
+    .line-rounded-corners {
+      display: none !important;
+    }
+  }
+
+  @media (max-width: 998px) {
+    padding-top: 0;
+  }
 `;
 
 const H1 = styled.h1`
@@ -124,6 +134,7 @@ const FlexContainer = styled.div`
 
 const Container = styled.div`
   display: flex;
+  max-width: 1080px;
   margin: 0 auto;
   gap: var(--section-gap);
   flex-direction: column;
@@ -134,74 +145,76 @@ const Container = styled.div`
 `;
 
 return (
-  <Container>
-    <Flex>
-      <H1>
-        Creatives
-        <span>
-          DAO{" "}
-          <svg viewBox="0 0 26 24" fill="none" aria-hidden="true">
-            <path
-              d="M24.3767 8.06326L1.51965 0.0649912C1.10402 -0.0830767 0.639031 0.026026 0.327308 0.340346C0.0181841 0.657263 -0.0831256 1.12225 0.0701378 1.53788L8.071 23.2519C8.23726 23.7013 8.66587 24 9.14385 24H9.14644C9.62702 24 10.0556 23.6961 10.2167 23.2441L13.734 13.495L24.3325 10.2349C24.8053 10.0895 25.13 9.65824 25.1378 9.16468C25.1482 8.67112 24.8391 8.22691 24.3715 8.06326H24.3767Z"
-              fill="#323330"
-            />
-          </svg>
-        </span>
-      </H1>
-      <div className="mt-3">
-        <Text style={{ maxWidth: "670px" }}>
-          The Creatives DAO is an overarching body for all creative contributors
-          in the NEAR ecosystem and beyond! With support via decentralized
-          autonomous organizations, all creators have the freedom to express
-          themselves and create impactful projects that promote decentralization
-          and sustainability.
-        </Text>
-      </div>
-    </Flex>
-    <Text
-      size="18px"
-      weight="600"
-      style={{ textTransform: "uppercase", letterSpacing: "0.17em" }}
-    >
-      Your Adventure Has Begun
-    </Text>
-    <FlexContainer>
-      <div className="m-1">
-        <Widget
-          src="near/widget/DIG.Button"
-          props={{
-            href: "https://nearbuilders.com",
-            label: "Builder Groups",
-            variant: "outline-success",
-            size: "large",
-          }}
-        />
-      </div>
-      <div className="m-1">
-        <Widget
-          src="near/widget/DIG.Button"
-          props={{
-            href: "#/every.near/widget/browser?path=every.near/thing/test",
-            label: "Create Projects",
-            variant: "outline-secondary",
-            size: "large",
-          }}
-        />
-      </div>
-      <div className="m-1">
-        <Widget
-          src="near/widget/DIG.Button"
-          props={{
-            href: "https://gov.near.org/t/guide-creativesdao-guidelines-and-procedures/32878",
-            label: "How To Apply for Funding",
-            variant: "outline-secondary",
-            size: "large",
-          }}
-        />
-      </div>
-    </FlexContainer>
-    <Widget src="hack.near/widget/creatives.why" />
-    <br />
+  <Wrapper>
+    <Container>
+      <Flex>
+        <H1>
+          Creatives
+          <span>
+            DAO{" "}
+            <svg viewBox="0 0 26 24" fill="none" aria-hidden="true">
+              <path
+                d="M24.3767 8.06326L1.51965 0.0649912C1.10402 -0.0830767 0.639031 0.026026 0.327308 0.340346C0.0181841 0.657263 -0.0831256 1.12225 0.0701378 1.53788L8.071 23.2519C8.23726 23.7013 8.66587 24 9.14385 24H9.14644C9.62702 24 10.0556 23.6961 10.2167 23.2441L13.734 13.495L24.3325 10.2349C24.8053 10.0895 25.13 9.65824 25.1378 9.16468C25.1482 8.67112 24.8391 8.22691 24.3715 8.06326H24.3767Z"
+                fill="#323330"
+              />
+            </svg>
+          </span>
+        </H1>
+        <div className="mt-3">
+          <Text style={{ maxWidth: "670px" }}>
+            The Creatives DAO is an overarching body for all creative
+            contributors in the NEAR ecosystem and beyond! With support via
+            decentralized autonomous organizations, all creators have the
+            freedom to express themselves and create impactful projects that
+            promote decentralization and sustainability.
+          </Text>
+        </div>
+      </Flex>
+      <Text
+        size="18px"
+        weight="600"
+        style={{ textTransform: "uppercase", letterSpacing: "0.17em" }}
+      >
+        Your Adventure Has Begun
+      </Text>
+      <FlexContainer>
+        <div className="m-1">
+          <Widget
+            src="near/widget/DIG.Button"
+            props={{
+              href: "https://nearbuilders.com",
+              label: "Builder Groups",
+              variant: "outline-success",
+              size: "large",
+            }}
+          />
+        </div>
+        <div className="m-1">
+          <Widget
+            src="near/widget/DIG.Button"
+            props={{
+              href: "#/every.near/widget/browser?path=every.near/thing/test",
+              label: "Create Projects",
+              variant: "outline-secondary",
+              size: "large",
+            }}
+          />
+        </div>
+        <div className="m-1">
+          <Widget
+            src="near/widget/DIG.Button"
+            props={{
+              href: "https://gov.near.org/t/guide-creativesdao-guidelines-and-procedures/32878",
+              label: "How To Apply for Funding",
+              variant: "outline-secondary",
+              size: "large",
+            }}
+          />
+        </div>
+      </FlexContainer>
+      <Widget src="hack.near/widget/creatives.why" />
+      <br />
+    </Container>
 
     <Widget src="hack.near/widget/creatives.apps" props={{ tab }} />
     <hr />
@@ -220,5 +233,5 @@ return (
       </Text>
       <Widget src="hack.near/widget/dev.Badge" />
     </Flex>
-  </Container>
+  </Wrapper>
 );
