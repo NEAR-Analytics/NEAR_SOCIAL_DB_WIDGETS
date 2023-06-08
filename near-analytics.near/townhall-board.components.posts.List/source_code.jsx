7b1fd40252759840d5173c46573c490b8c1e026c
near-analytics.near <near-analytics.near>
2023-06-08 18:39:31 +0000

    Update townhall-board.components.posts.List by near-analytics.near at 2023-06-08T18:39:31.152Z

--- near-analytics.near/townhall-board.components.posts.List/source_code.jsx
+++ near-analytics.near/townhall-board.components.posts.List/source_code.jsx
@@ -1,1 +1,27 @@
-return <div>Hello World</div>;
+// This component implementation was forked from [IndexFeed], but it does not fully implement lazy loading.
+// While this component uses InfiniteScroll, it still loads the whole list of Post IDs in one view call.
+// The contract will need to be extended with pagination support, yet, even in the current state the page loads much faster.
+// [IndexFeed]: https://near.social/#/mob.near/widget/WidgetSource?src=mob.near/widget/IndexFeed
+
+return (
+  <>
+    {Head}
+    {state.items.length > 0 ? (
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={makeMoreItems}
+        hasMore={state.displayCount < state.items.length}
+        loader={loader}
+      >
+        {renderedItems}
+      </InfiniteScroll>
+    ) : (
+      <p class="text-secondary">
+        No posts {props.searchResult ? "matches search" : ""}
+        {props.recency == "hot"
+          ? " in " + getPeriodText(state.period).toLowerCase()
+          : ""}
+      </p>
+    )}
+  </>
+);
