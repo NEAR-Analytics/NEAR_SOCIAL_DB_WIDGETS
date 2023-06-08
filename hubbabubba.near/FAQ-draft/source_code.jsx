118f2af5cffdd25003d18d119cf7daed7f54073c
hubbabubba.near <hubbabubba.near>
2023-06-08 19:22:36 +0000

    Update FAQ-draft by hubbabubba.near at 2023-06-08T19:22:36.011Z

---
+++ hubbabubba.near/FAQ-draft/source_code.jsx
@@ -0,0 +1,270 @@
+return (
+  <div>
+    <h2>
+      <strong>Horizon FAQs</strong>
+    </h2>
+    <br />
+    <br />
+
+    <h3>
+      <p>General:</p>
+    </h3>
+    <br />
+    <br />
+    <p>
+      <h4>
+        <strong>What is Horizon?</strong>
+      </h4>
+      <br />
+      <a href="https://youtu.be/Fyl1Hlo-rM0">
+        <img
+          src="https://s3.us-east-2.amazonaws.com/nearhorizon.assets/near-horizon-first-looksv15.gif"
+          alt="horizon first looks"
+        />
+      </a>
+    </p>
+    <p>
+      <p>Horizon is a Web3 startup support platform built on the NEAR BOS.</p>
+    </p>
+    <p>
+      <p>
+        Horizon’s mission is to attract high-quality founders and support their
+        growth by facilitating connections with the right resources all in one
+        place.
+      </p>
+    </p>
+    <br />
+    <br />
+    <br />
+    <h4>
+      <strong>
+        What stage should my project be at before joining Horizon?
+      </strong>
+    </h4>
+    <br />
+    <p>
+      Horizon can help you at any stage of your startup journey. With both
+      guided and self-guided paths, you can connect to the resources and people
+      you need to get to the next step towards a successful launch. To begin,
+      simply create a project profile.
+    </p>
+    <br />
+    <br />
+    <br />
+    <h4>
+      <strong>How do I get started?</strong> <br />
+    </h4>
+    <br />
+
+    <p>
+      1. Create a <a href="https://wallet.near.org/">NEAR account</a> if you
+      don't have one
+      <br />
+      <br />
+      <img
+        src="https://s3.us-east-2.amazonaws.com/nearhorizon.assets/account-create.png"
+        alt="account create"
+      />
+    </p>
+    <br />
+    <br />
+    <p>
+      2. If you are a founder of a project,{" "}
+      <a href="https://near.org/nearhorizon.near/widget/Index?tab=createproject">
+        create a project{" "}
+      </a>
+      in the project page.
+      <br />
+      <br />
+      <img
+        src="https://s3.us-east-2.amazonaws.com/nearhorizon.assets/project-create.png"
+        alt="project create"
+      />
+    </p>
+    <br />
+    <br />
+    <p>
+      3.
+      <a href="https://near.org/nearhorizon.near/widget/Index">
+        {" "}
+        Explore and discover
+      </a>{" "}
+      Horizon
+      <br />
+      <br />
+      <img
+        src="https://s3.us-east-2.amazonaws.com/nearhorizon.assets/nh_app_landing.png"
+        alt="horizon app landing page"
+        width="90%"
+      />
+    </p>
+    <br />
+    <br />
+    <p>
+      4.
+      <a href="https://www.youtube.com/watch?v=EN7TIUr_fkg">
+        Video explainer
+        <br />
+        <br />
+        <img
+          src="https://s3.us-east-2.amazonaws.com/nearhorizon.assets/nh-walkthrough.gif"
+          alt="horizon app walkthrough"
+        />
+      </a>
+    </p>
+    <br />
+    <br />
+    <p>
+      5.{" "}
+      <a href="https://near.org/nearhorizon.near/widget/Index?tab=help">
+        Additional help
+        <br />
+        <br />
+        <img
+          src="https://s3.us-east-2.amazonaws.com/nearhorizon.assets/nh_app_help-pg.png"
+          alt="horizon help page"
+          width="90%"
+        />
+      </a>
+    </p>
+    <br />
+    <h4>
+      <strong>Where do I find help?</strong>
+    </h4>
+    <p>
+      <a href="https://near.org/nearhorizon.near/widget/Index?tab=help">
+        Horizon help page
+      </a>
+    </p>
+    <p>
+      <p>
+        Email Horizon Team:{" "}
+        <a href="mailto:horizon@near.foundation">horizon@near.foundation</a>
+      </p>
+    </p>
+    <p>
+      Email NEAR Foundation Team:
+      <a href="mailto:hello@near.foundation">hello@near.foundation</a>
+    </p>
+    <br />
+    <h4>
+      <strong>How can I get Marketing support?</strong>
+    </h4>
+    <br />
+
+    <p>
+      There are a variety of ways to receive marketing support including
+      connecting to one of the Horizon service providers specializing in
+      marketing help. There are also resources available in the{" "}
+      <a href="https://near.org/nearhorizon.near/widget/Index?tab=learn">
+        Learning section
+      </a>{" "}
+      of Horizon to help you with amplification.
+    </p>
+    <br />
+    <br />
+
+    <h3>Troubleshooting:</h3>
+    <p>
+      <strong>Common issues:</strong>
+    </p>
+    <br />
+    <h4>
+      <strong>How do I find projects?</strong>
+    </h4>
+    <p>
+      You can search all current projects on the NEAR Horizon Application page
+    </p>
+    <img
+      src="https://s3.us-east-2.amazonaws.com/nearhorizon.assets/nh_project_search.jpg"
+      alt="Project search"
+      width="90%"
+    />
+    <br />
+    <br />
+    <br />
+    <h4>
+      <strong>Are there analytics?</strong>
+    </h4>
+    <p>
+      Yes! The main application page will display ecosystem stats from{" "}
+      <a href="https://nearatlas.com/#/">Atlas</a>. We also use{" "}
+      <a href="https://flipsidecrypto.xyz/azerbaijan/near-horizon-activity-vSci5p">
+        Flipside
+      </a>{" "}
+      to collect on-chain data.
+    </p>
+    <br />
+    <img
+      src="https://s3.us-east-2.amazonaws.com/nearhorizon.assets/nh_stats.jpg"
+      alt="atlas stats"
+      width="90%"
+    />
+    <br />
+    <br />
+    <br />
+    <br />
+    <h4>
+      <strong>
+        I tried to create a project but the transaction didn’t go through, I got
+        an error message similar to this:
+      </strong>
+    </h4>
+    <br />
+    <img
+      src="https://s3.us-east-2.amazonaws.com/nearhorizon.assets/error-msg_project-create.png"
+      width="90%"
+      alt="alt_text"
+      title="image_tooltip"
+    />
+    <br />
+    <br />
+    <p>
+      This error is caused when creating a project with an account different
+      than the users NEAR Discovery account that is actively logged in: (e.g.
+      janedoe.near is creating a project for janedoeproject.near)
+    </p>
+    <p>
+      Your project has its own account. In order to add admins on a project,
+      including yourself, you must log in with the project account id, and visit
+      the link to grant appropriate permissions to yourself and your team. Once
+      completed, log back in with your user account (the account you are using
+      now) to complete the setup process.
+    </p>
+    <br />
+    <br />
+    <br />
+    <h4>
+      <strong>I don’t know how to claim a project</strong>
+    </h4>
+    <p>
+      Claim a project can be done in two simple steps:
+      <br />
+      <br />
+      <strong>1.</strong> On the project's profile page, click "Claim Project"
+      <br />
+      <img
+        src="https://s3.us-east-2.amazonaws.com/nearhorizon.assets/nh_claim_project_cta.png"
+        width="90%"
+      />
+      <br />
+      <br />
+      <strong>2.</strong> Complete and submit the form that appears. The Horizon
+      team will review your request and add you as the admin to the project.
+      <img
+        src="https://s3.us-east-2.amazonaws.com/nearhorizon.assets/nh_app_claim_project_form.png"
+        width="90%"
+      />
+      <br />
+    </p>
+    <p>
+      <br />
+      <br />
+      <br />
+    </p>
+    <p>
+      have a question? email us{" "}
+      <a href="mailto:horizon@near.foundation">horizon@near.foundation</a>
+    </p>
+  </div>
+);
