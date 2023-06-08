aab0c5176c975627294e887a9862a72f2089d815
near-analytics.near <near-analytics.near>
2023-06-08 18:40:24 +0000

    Update townhall-board.components.posts.List by near-analytics.near at 2023-06-08T18:40:24.269Z

--- near-analytics.near/townhall-board.components.posts.List/source_code.jsx
+++ near-analytics.near/townhall-board.components.posts.List/source_code.jsx
@@ -2,7 +2,85 @@
 // While this component uses InfiniteScroll, it still loads the whole list of Post IDs in one view call.
 // The contract will need to be extended with pagination support, yet, even in the current state the page loads much faster.
 // [IndexFeed]: https://near.social/#/mob.near/widget/WidgetSource?src=mob.near/widget/IndexFeed
+/* INCLUDE: "common.jsx" */
+const nearNFDevsContractAccountId =
+  props.nearNFDevsContractAccountId ||
+  (context.widgetSrc ?? "near-analytics.near").split("/", 1)[0];
 
+const nearNFDevsWidgetsAccountId =
+  props.nearNFDevsWidgetsAccountId ||
+  (context.widgetSrc ?? "near-analytics.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearNFDevsContractAccountId: props.nearNFDevsContractAccountId,
+    nearNFDevsWidgetsAccountId: props.nearNFDevsWidgetsAccountId,
+    referral: props.referral,
+  };
+
+  return (
+    <Widget
+      src={`${nearNFDevsWidgetsAccountId}/widget/townhall-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+
+  if (props.nearNFDevsContractAccountId) {
+    linkProps.nearNFDevsContractAccountId = props.nearNFDevsContractAccountId;
+  }
+
+  if (props.nearNFDevsWidgetsAccountId) {
+    linkProps.nearNFDevsWidgetsAccountId = props.nearNFDevsWidgetsAccountId;
+  }
+
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+
+  const linkPropsQuery = Object.entries(linkProps)
+    .filter(([_key, nullable]) => (nullable ?? null) !== null)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+
+  return `/#/${nearNFDevsWidgetsAccountId}/widget/townhall-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+/* END_INCLUDE: "common.jsx" */
+
+initState({
+  period: "week",
+});
+
+function defaultRenderItem(postId, additionalProps) {
+  if (!additionalProps) {
+    additionalProps = {};
+  }
+  // It is important to have a non-zero-height element as otherwise InfiniteScroll loads too many items on initial load
+  return (
+    <div style={{ minHeight: "150px" }}>
+      {widget(
+        `components.posts.Post`,
+        {
+          id: postId,
+          expandable: true,
+          defaultExpanded: false,
+          isInList: true,
+          ...additionalProps,
+        },
+        postId
+      )}
+    </div>
+  );
+}
+
+///////////
 return (
   <>
     {Head}
