b2b978e21e5848b987f23087d6ed69ccf5688640
nearvietnamhub.near <nearvietnamhub.near>
2023-06-09 11:13:01 +0000

    Update NEARAPACticket by nearvietnamhub.near at 2023-06-09T11:13:01.659Z

---
+++ nearvietnamhub.near/NEARAPACticket/source_code.jsx
@@ -0,0 +1,28 @@
+return (
+  <>
+    <h3>Register NEAR APAC ticket!</h3>
+    #NEAR APAC - BRINGING UNLIMITED FUTURE ONTO THE BLOCKCHAIN Get ready for
+    Near APAC - the largest event of the NEAR Protocol Ecosystem in the Asia
+    Pacific region! Happening in #Vietnam 🇻🇳 from September 8-12, 2023.{" "}
+    <a href="https://twitter.com/NearVietnamHub/status/1659426817147826176">
+      Learn more
+    </a>
+    <br /> <br />
+    <img
+      src="https://pbs.twimg.com/media/Fwd01HSaYAAt6cc?format=png&name=900x900"
+      width="600"
+      height=""
+    />
+    <br />
+    <br />
+    <p className="d-flex ">Claim your FREE ticket now 👇</p>
+    <a href="https://ticket-nearapac.app/" className="btn btn-lg">
+      <button>😎 Claim VIP ticket (NFT ticket)</button>
+    </a>
+    <a href="https://www.eventbrite.com/e/near-apac-2023-unlimited-future-tickets-634787926777">
+      <button className="btn btn-secondary">
+        😮‍💨 Get ticket with EventBrite
+      </button>
+    </a>
+  </>
+);
