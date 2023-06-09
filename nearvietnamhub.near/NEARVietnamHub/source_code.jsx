38eb823b20a4b91b39d4d52078d32a1b8d0f5293
nearvietnamhub.near <nearvietnamhub.near>
2023-06-09 10:20:42 +0000

    Update NEARVietnamHub by nearvietnamhub.near at 2023-06-09T10:20:42.008Z

--- nearvietnamhub.near/NEARVietnamHub/source_code.jsx
+++ nearvietnamhub.near/NEARVietnamHub/source_code.jsx
@@ -1,36 +1,8 @@
-const css = styled.b`
-html {overflow: auto;}   
-html,
-body,
-div,
-iframe {
-    margin: 0px;
-    padding: 0px;
-    height: 100%;
-    min-height: 3000px;
-    border: none;
-}
-iframe {
-    display: block;
-    width: 100%;
-    border: none;
-    overflow-y: auto;
-    overflow-x: hidden;
-}
-`;
-
 return (
-  <>
-    <css>
-      <iframe
-        src="https://nearvietnamhub.org/"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
-        width="100%"
-        height="100%"
-        scrolling="auto"
-      ></iframe>
-    </css>
-  </>
+  <Widget
+    src="cuongdcdev.near/widget/IframeEmbed"
+    props={{
+      src: "https://nearvietnamhub.org/",
+    }}
+  ></Widget>
 );
