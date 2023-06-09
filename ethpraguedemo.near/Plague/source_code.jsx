cce49d4e4c5b637b3bd0d8c51c6752df14006fe9
ethpraguedemo.near <ethpraguedemo.near>
2023-06-09 21:32:43 +0000

    Update Plague by ethpraguedemo.near at 2023-06-09T21:32:43.464Z

---
+++ ethpraguedemo.near/Plague/source_code.jsx
@@ -0,0 +1,59 @@
+if (context.loading) {
+  return "Loading";
+}
+
+// By default the form to ask a question is hidden
+initState({ askQuestion: false });
+const toggleQuestion = () => {
+  State.update({ askQuestion: !state.askQuestion });
+};
+
+const adminContract = "admin.dev-support.near";
+const admins = Near.view(adminContract, "get_admins", {});
+
+// const sortByCategories = ["Newest", "Popular"];
+// const filterByTopic = ["one", "two", "three"];
+
+const SidebarWrapper = styled.div`
+  border-right: 1px solid #ECEEF0;
+`;
+
+return (
+  <div class="container-fluid py-3 mb-5">
+    <div class="row">
+      {/**
+    <SidebarWrapper className="col-2 pe-5">
+        <Widget
+          src="dima_sheleg.near/widget/DevSupport.Main.Sidebar"
+          props={{ categories: sortByCategories, topics: filterByTopic }}
+        />
+      </SidebarWrapper>
+    */}
+
+      <div class="col-12">
+        <div class="pb-3 border-bottom">
+          <Widget
+            src="dev-support.near/widget/DevSupport.Discussion.Title"
+            props={{ disabled: !context.accountId, onClick: toggleQuestion }}
+          />
+        </div>
+
+        {state.askQuestion && (
+          <div class="mt-4 p-2">
+            <Widget
+              src="dev-support.near/widget/DevSupport.Question.Edit"
+              props={{ onCommit: () => State.update({ askQuestion: false }) }}
+            />
+          </div>
+        )}
+
+        <div class="mt-5">
+          <Widget
+            src="dev-support.near/widget/DevSupport.Feed"
+            props={{ admins, adminContract: adminContract }}
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);
