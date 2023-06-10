3e34c4733e87ff8017522171d5b8c8d3813c881f
hack.near <hack.near>
2023-06-10 20:08:56 +0000

    Update dev.org by hack.near at 2023-06-10T20:08:56.739Z

--- hack.near/dev.org/source_code.jsx
+++ hack.near/dev.org/source_code.jsx
@@ -6,19 +6,23 @@ const tab = props.tab === "following" ? props.tab : "members";
 return (
   <div>
     <ul className="nav nav-pills nav-fill mb-4" role="tablist">
-      <li className="nav-item" role="presentation">
+      <li className="nav-item m-2" role="presentation">
         <a
           href={`#/hack.near/widget/every.one?accountId=${accountId}&tab=members`}
-          className={`btn nav-link ${tab === "members" ? "active" : ""}`}
+          className={`btn m-1 border nav-link ${
+            tab === "members" ? "active" : ""
+          }`}
           role="tab"
         >
           Members
         </a>
       </li>
-      <li className="nav-item" role="presentation">
+      <li className="nav-item m-2" role="presentation">
         <a
           href={`#/hack.near/widget/every.one?accountId=${accountId}&tab=following`}
-          className={`btn nav-link ${tab === "following" ? "active" : ""}`}
+          className={`btn m-1 border nav-link ${
+            tab === "following" ? "active tab-with-border" : ""
+          }`}
           role="tab"
         >
           Collaborators
