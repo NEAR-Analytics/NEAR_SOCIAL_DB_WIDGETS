3ca7441f86fad082f5e3a4488dd9e29d10084b2c
ethpraguedemo.near <ethpraguedemo.near>
2023-06-10 21:02:42 +0000

    Update Progress-Pool-Proposals-Feed by ethpraguedemo.near at 2023-06-10T21:02:42.844Z

--- ethpraguedemo.near/Progress-Pool-Proposals-Feed/source_code.jsx
+++ ethpraguedemo.near/Progress-Pool-Proposals-Feed/source_code.jsx
@@ -174,16 +174,20 @@ if (state.proposalNumber !== undefined && state.proposalNumber > 0) {
     proposalAbi,
     Ethers.provider().getSigner()
   );
-  for (let num = 0; num < state.proposalNumber; num++) {
+
+  const promises = Array.from({ length: state.proposalNumber }, (_, num) =>
     proposals.proposals(num).then((result) => {
       new_pulled_proposals.push({ num, result });
       console.log("result: ", result);
+    })
+  );
+
+  Promise.all(promises).then(() => {
+    State.update({
+      pulled_proposals: [...state.pulled_proposals, ...new_pulled_proposals],
     });
-  }
-  State.update({
-    pulled_proposals: [...state.pulled_proposals, ...new_pulled_proposals],
+    toggleUpdateFlag();
   });
-  toggleUpdateFlag();
 }
 
 // HELPER FUNCTIONS
