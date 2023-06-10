dc69ce2fd672b893c5def5ef99052c05b2ea4ced
ethpraguedemo.near <ethpraguedemo.near>
2023-06-10 23:13:47 +0000

    Update Progress-Pool-Question-Button-Downvote by ethpraguedemo.near at 2023-06-10T23:13:47.525Z

--- ethpraguedemo.near/Progress-Pool-Question-Button-Downvote/source_code.jsx
+++ ethpraguedemo.near/Progress-Pool-Question-Button-Downvote/source_code.jsx
@@ -1,65 +1,7 @@
-const accountId = props.accountId;
-const blockHeight = props.blockHeight;
-
-if (accountId === undefined || blockHeight === undefined) {
-  return;
-}
-
-const item = {
-  accountId,
-  blockHeight,
-};
-
-const likes = Social.index("upvote", item, { subscribe: true });
-
-const dataLoading = likes === null;
-
-let upvotes = 0;
-let downvotes = 0;
-
-(likes || []).forEach((like) => {
-  if (like.value.type === "upvote") {
-    upvotes += 1;
-  }
-  if (like.value.type === "downvote") {
-    downvotes += 1;
-  }
-});
-
-// state.hasLike === true
-
-// const hasLike = context.accountId && !!likesByUsers[context.accountId];
-
-const likeClick = () => {
-  if (state.loading) {
-    return;
-  }
+const dislikeClick = () => {
+  console.log("downvotes");
   State.update({
-    loading: true,
-  });
-  const data = {
-    index: {
-      upvote: JSON.stringify({
-        key: item,
-        value: {
-          type: hasLike ? "downvote" : "upvote",
-        },
-      }),
-    },
-  };
-
-  if (!hasLike && props.notifyAccountId) {
-    data.index.notify = JSON.stringify({
-      key: props.notifyAccountId,
-      value: {
-        type: "upvote",
-        item,
-      },
-    });
-  }
-  Social.set(data, {
-    onCommit: () => State.update({ loading: false, hasLike: !hasLike }),
-    onCancel: () => State.update({ loading: false }),
+    upvote: false,
   });
 };
 
@@ -82,21 +24,8 @@ const Button = styled.button`
 
 return (
   <div className="d-inline-flex align-items-center">
-    <Button
-      disabled={state.loading || dataLoading || !context.accountId}
-      onClick={likeClick}
-      style={{ marginLeft: "2px" }}
-    >
-      {state.loading || dataLoading ? (
-        <span
-          class="spinner-grow spinner-grow-sm p-2"
-          role="status"
-          aria-hidden="true"
-        />
-      ) : (
-        <i class="bi bi-caret-down-fill" />
-      )}
-      {upvotes}
+    <Button onClick={dislikeClick} style={{ marginLeft: "2px" }}>
+      <i class="bi bi-caret-down-fill" />
     </Button>
   </div>
 );
