d1412c7083d252fdf288b56fb075d70f1c714a87
ethpraguedemo.near <ethpraguedemo.near>
2023-06-10 11:32:34 +0000

    Update Progress-Pool-Grantees by ethpraguedemo.near at 2023-06-10T11:32:34.056Z

--- ethpraguedemo.near/Progress-Pool-Grantees/source_code.jsx
+++ ethpraguedemo.near/Progress-Pool-Grantees/source_code.jsx
@@ -1,3 +1,61 @@
+const Content = styled.div`
+  .post {
+    padding-left: 0;
+    padding-right: 0;
+  }
+`;
+
+const Tabs = styled.div`
+  display: flex;
+  height: 48px;
+  border-bottom: 1px solid #eceef0;
+  margin-bottom: 72px;
+  overflow: auto;
+  scroll-behavior: smooth;
+
+  @media (max-width: 1200px) {
+    background: #f8f9fa;
+    border-top: 1px solid #eceef0;
+    margin: 0 -12px 48px;
+
+    > * {
+      flex: 1;
+    }
+  }
+`;
+
+const TabsButton = styled.a`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  font-weight: 600;
+  font-size: 12px;
+  padding: 0 12px;
+  position: relative;
+  color: ${(p) => (p.selected ? "#11181C" : "#687076")};
+  background: none;
+  border: none;
+  outline: none;
+  text-align: center;
+  text-decoration: none !important;
+
+  &:hover {
+    color: #11181c;
+  }
+
+  &::after {
+    content: "";
+    display: ${(p) => (p.selected ? "block" : "none")};
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 3px;
+    background: #59e692;
+  }
+`;
+
 if (
   state.chainId === undefined &&
   ethers !== undefined &&
@@ -176,6 +234,31 @@ const getSender = () => {
 
 return (
   <Theme>
+    <Content>
+      <Tabs>
+        <TabsButton
+          href={`https://near.org/ethpraguedemo.near/widget/Progress-Pool`}
+          selected={state.selectedTab === "overview"}
+        >
+          For Voters
+        </TabsButton>
+
+        <TabsButton
+          href={`https://near.org/ethpraguedemo.near/widget/Progress-Pool-Grantees`}
+          selected={state.selectedTab === "apps"}
+        >
+          For Grantees
+        </TabsButton>
+
+        <TabsButton
+          href={`https://near.org/ethpraguedemo.near/widget/Progress-Pool-Proposals`}
+          selected={state.selectedTab === "nfts"}
+        >
+          Proposals
+        </TabsButton>
+      </Tabs>
+    </Content>
+
     <div class="LidoContainer">
       <div class="Header">Grantees</div>
       <div class="SubHeader">Submit a proposal and get funding.</div>
