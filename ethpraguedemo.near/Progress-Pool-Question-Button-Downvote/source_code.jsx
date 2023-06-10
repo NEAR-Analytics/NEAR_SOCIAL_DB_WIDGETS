5b6a19f6735885ff7c0062c6432f839f2bcc7d1c
ethpraguedemo.near <ethpraguedemo.near>
2023-06-10 15:25:29 +0000

    Update Progress-Pool-Question-Button-Downvote by ethpraguedemo.near at 2023-06-10T15:25:29.049Z

--- ethpraguedemo.near/Progress-Pool-Question-Button-Downvote/source_code.jsx
+++ ethpraguedemo.near/Progress-Pool-Question-Button-Downvote/source_code.jsx
@@ -81,10 +81,11 @@ const Button = styled.button`
 `;
 
 return (
-  <div className="d-inline-flex">
+  <div className="d-inline-flex align-items-center">
     <Button
       disabled={state.loading || dataLoading || !context.accountId}
       onClick={likeClick}
+      style={{ marginLeft: "4px" }}
     >
       {state.loading || dataLoading ? (
         <span
