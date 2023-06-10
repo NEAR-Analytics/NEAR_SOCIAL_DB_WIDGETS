7d57f4f153115062dda0834f83111cf56bde504b
ethpraguedemo.near <ethpraguedemo.near>
2023-06-10 16:25:35 +0000

    Update Progress-Pool-Proposals by ethpraguedemo.near at 2023-06-10T16:25:35.674Z

--- ethpraguedemo.near/Progress-Pool-Proposals/source_code.jsx
+++ ethpraguedemo.near/Progress-Pool-Proposals/source_code.jsx
@@ -152,7 +152,7 @@ return (
       </div>
     </div>
 
-    <div class="col-lg-4 col-sm-12">
+    <div class="col-lg-4 col-sm-4">
       <Button
         class="btn btn-primary"
         disabled={props.disabled}
