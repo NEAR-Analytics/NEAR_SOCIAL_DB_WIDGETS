206e260063e71ac666c91062ec81c3af4458d593
ethpraguedemo.near <ethpraguedemo.near>
2023-06-10 13:27:23 +0000

    Update Progress-Pool-Voters by ethpraguedemo.near at 2023-06-10T13:27:23.932Z

---
+++ ethpraguedemo.near/Progress-Pool-Voters/source_code.jsx
@@ -0,0 +1,504 @@
+if (
+  state.chainId === undefined &&
+  ethers !== undefined &&
+  Ethers.send("eth_requestAccounts", [])[0]
+) {
+  Ethers.provider()
+    .getNetwork()
+    .then((chainIdData) => {
+      if (chainIdData?.chainId) {
+        State.update({ chainId: chainIdData.chainId });
+      }
+    });
+}
+
+// FETCH Token ABI
+
+const voterTokenContract = "0x81602eEdD6C4624150B3F2C76417E0c66411eA30";
+const tokenDecimals = 18;
+
+const tokenAbi = [
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "from", type: "address" },
+      { indexed: true, internalType: "address", name: "to", type: "address" },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [
+      { internalType: "address[]", name: "_addresses", type: "address[]" },
+    ],
+    name: "addToAllowlist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "allowList",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "owner", type: "address" },
+      { internalType: "address", name: "spender", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "account", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "subtractedValue", type: "uint256" },
+    ],
+    name: "decreaseAllowance",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "hasMinted",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "addedValue", type: "uint256" },
+    ],
+    name: "increaseAllowance",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "mint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
+    ],
+    name: "transfer",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "from", type: "address" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
+    ],
+    name: "transferFrom",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+const iface = new ethers.utils.Interface(tokenAbi);
+
+// HELPER FUNCTIONS
+
+const getTokenBalance = () => {
+  const erc20 = new ethers.Contract(
+    voterTokenContract,
+    tokenAbi,
+    Ethers.provider().getSigner()
+  );
+
+  erc20.balanceOf(state.sender).then((rawBalance) => {
+    State.update({
+      balance: rawBalance.toString(),
+    });
+};
+
+const getVote = (_signer) => {
+  const erc20 = new ethers.Contract(
+    voterTokenContract,
+    tokenAbi,
+    Ethers.provider().getSigner()
+  );
+
+  erc20.hasMinted(state.sender).then((_hasMinted) => {
+    if (_hasMinted) {
+      State.update({ hasMinted: true });
+      getTokenBalance();
+    }
+  });
+
+  erc20.mint().then((transactionHash) => {
+    State.update({ hashMinted: true });
+    console.log("transactionHash is " + transactionHash);
+  });
+};
+
+// DETECT SENDER
+
+if (state.sender === undefined) {
+  const accounts = Ethers.send("eth_requestAccounts", []);
+  if (accounts.length) {
+    State.update({ sender: accounts[0] });
+    console.log("set sender", accounts[0]);
+    console.log("2.: ", state);
+  }
+}
+
+//if (!state.sender)  return "Please login first";
+
+// FETCH SENDER BALANCE
+
+if (state.balance === undefined && state.sender) {
+  const erc20 = new ethers.Contract(
+    voterTokenContract,
+    tokenAbi,
+    Ethers.provider().getSigner()
+  );
+
+  erc20.balanceOf(state.sender).then((_balance) => {
+    State.update({
+      balance: _balance.toString(),
+    });
+    console.log("Logging balance: ", state.balance);
+    console.log(state);
+  });
+}
+
+// FETCH TX COST
+
+if (state.txCost === undefined) {
+  const gasEstimate = ethers.BigNumber.from(1875000);
+  const gasPrice = ethers.BigNumber.from(10000000000);
+
+  const gasCostInWei = gasEstimate.mul(gasPrice);
+  const gasCostInEth = ethers.utils.formatEther(gasCostInWei);
+
+  let responseGql = fetch(
+    "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `{
+          bundle(id: "1" ) {
+            ethPrice
+          }
+        }`,
+      }),
+    }
+  );
+
+  if (!responseGql) return "";
+
+  const ethPriceInUsd = responseGql.body.data.bundle.ethPrice;
+
+  const txCost = Number(gasCostInEth) * Number(ethPriceInUsd);
+
+  State.update({ txCost: `$${txCost.toFixed(2)}` });
+}
+
+// FETCH CSS
+
+const cssFont = fetch(
+  "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800"
+).body;
+const css = fetch(
+  "https://pluminite.mypinata.cloud/ipfs/Qmboz8aoSvVXLeP5pZbRtNKtDD3kX5D9DEnfMn2ZGSJWtP"
+).body;
+
+if (!cssFont || !css) return "";
+
+if (!state.theme) {
+  State.update({
+    theme: styled.div`
+    font-family: Manrope, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    ${cssFont}
+    ${css}
+`,
+  });
+}
+const Theme = state.theme;
+
+// OUTPUT UI
+
+const getSender = () => {
+  return !state.sender
+    ? ""
+    : state.sender.substring(0, 6) +
+        "..." +
+        state.sender.substring(state.sender.length - 4, state.sender.length);
+};
+
+return (
+  <Theme>
+    <div class="LidoContainer">
+      <div class="Header">Voters</div>
+      <div class="SubHeader">Claim $PRAGUE and vote on growing projects.</div>
+
+      <div class="LidoForm">
+        {state.sender && (
+          <>
+            <div class="LidoFormTopContainer">
+              <div class="LidoFormTopContainerLeft">
+                <div class="LidoFormTopContainerLeftContent1">
+                  <div class="LidoFormTopContainerLeftContent1Container">
+                    <span>Voting Power</span>
+                    <div class="LidoFormTopContainerLeftContent1Circle" />
+                  </div>
+                </div>
+                <div class="LidoFormTopContainerLeftContent2">
+                  <span>
+                    {state.balance ?? (!state.sender ? "0" : "...")}
+                    &nbsp;$PRAGUE
+                  </span>
+                </div>
+              </div>
+              <div class="LidoFormTopContainerRight">
+                <div class="LidoFormTopContainerRightContent1">
+                  <div class="LidoFormTopContainerRightContent1Text">
+                    <span>{getSender()}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="LidoSplitter" />
+          </>
+        )}
+        <div
+          class={
+            state.sender ? "LidoFormBottomContainer" : "LidoFormTopContainer"
+          }
+        ></div>
+      </div>
+      <div class="LidoStakeForm">
+        <div class="LidoStakeFormInputContainer">
+          <span class="LidoStakeFormInputContainerSpan1">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+              <path
+                opacity="0.6"
+                d="M11.999 3.75v6.098l5.248 2.303-5.248-8.401z"
+              ></path>
+              <path d="M11.999 3.75L6.75 12.151l5.249-2.303V3.75z"></path>
+              <path
+                opacity="0.6"
+                d="M11.999 16.103v4.143l5.251-7.135L12 16.103z"
+              ></path>
+              <path d="M11.999 20.246v-4.144L6.75 13.111l5.249 7.135z"></path>
+              <path
+                opacity="0.2"
+                d="M11.999 15.144l5.248-2.993-5.248-2.301v5.294z"
+              ></path>
+              <path
+                opacity="0.6"
+                d="M6.75 12.151l5.249 2.993V9.85l-5.249 2.3z"
+              ></path>
+            </svg>
+          </span>
+          <span class="LidoStakeFormInputContainerSpan2">
+            <input
+              disabled={!state.sender}
+              class="LidoStakeFormInputContainerSpan2Input"
+              value={state.strEther}
+              onChange={(e) => State.update({ strEther: e.target.value })}
+              placeholder="Amount"
+            />
+          </span>
+          <span
+            class="LidoStakeFormInputContainerSpan3"
+            onClick={() => {
+              State.update({
+                strEther: (state.balance > 0.05
+                  ? parseFloat(state.balance) - 0.05
+                  : 0
+                ).toFixed(2),
+              });
+            }}
+          >
+            <button
+              class="LidoStakeFormInputContainerSpan3Content"
+              disabled={!state.sender}
+            >
+              <span class="LidoStakeFormInputContainerSpan3Max">MAX</span>
+            </button>
+          </span>
+        </div>
+        {!!state.sender ? (
+          state.hasMinted ? (
+            <button
+              class="LidoStakeFormSubmitContainer"
+              onClick={() => console.log("Already minted")}
+            >
+              <span>Already Minted</span>
+            </button>
+          ) : (
+            <button
+              class="LidoStakeFormSubmitContainer"
+              onClick={() => getVote(state.sender)}
+            >
+              <span>Mint Vote</span>
+            </button>
+          )
+        ) : (
+          <Web3Connect
+            className="LidoStakeFormSubmitContainer"
+            connectLabel="Connect with Web3"
+          />
+        )}
+
+        <div class="LidoFooterContainer">
+          {state.sender && (
+            <div class="LidoFooterRaw">
+              <div class="LidoFooterRawLeft">You will receive</div>
+              <div class="LidoFooterRawRight">${state.strEther ?? 0} stETH</div>
+            </div>
+          )}
+          <div class="LidoFooterRaw">
+            <div class="LidoFooterRawLeft">Exchange rate</div>
+            <div class="LidoFooterRawRight">1 ETH = 1 stETH</div>
+          </div>
+          {false && (
+            <div class="LidoFooterRaw">
+              <div class="LidoFooterRawLeft">Transaction cost</div>
+              <div class="LidoFooterRawRight">{state.txCost}</div>
+            </div>
+          )}
+          <div class="LidoFooterRaw">
+            <div class="LidoFooterRawLeft">Reward fee</div>
+            <div class="LidoFooterRawRight">10%</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Theme>
+);
