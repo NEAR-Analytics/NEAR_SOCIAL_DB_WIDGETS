acd754734d7b38fb8b3aab6322ae4532e87f66dd
ethpraguedemo.near <ethpraguedemo.near>
2023-06-10 10:27:26 +0000

    Update Progress-Pool by ethpraguedemo.near at 2023-06-10T10:27:26.119Z

--- ethpraguedemo.near/Progress-Pool/source_code.jsx
+++ ethpraguedemo.near/Progress-Pool/source_code.jsx
@@ -187,6 +187,7 @@ return (
     )}
 
     {currentView === "B" && (
+      // Replace "WidgetB" with the appropriate widget/component for view B
       <Widget
         src="ethpraguedemo.near/widget/Progress-Pool-Grantees"
         props={{}}
@@ -194,6 +195,7 @@ return (
     )}
 
     {currentView === "C" && (
+      // Replace "WidgetC" with the appropriate widget/component for view C
       <Widget
         src="ethpraguedemo.near/widget/Progress-Pool-Proposals"
         props={{}}
