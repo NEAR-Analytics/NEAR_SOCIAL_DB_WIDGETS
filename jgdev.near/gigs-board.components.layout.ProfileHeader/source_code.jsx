dce3d8290a458c8392cebf7fca0deae7b4725843
jgdev.near <jgdev.near>
2023-06-11 04:31:49 +0000

    Update gigs-board.components.layout.ProfileHeader by jgdev.near at 2023-06-11T04:31:49.420Z

---
+++ jgdev.near/gigs-board.components.layout.ProfileHeader/source_code.jsx
@@ -0,0 +1,117 @@
+const nearDevGovGigsContractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
+  (context.widgetSrc ?? "jgdev.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
+    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
+  };
+  return (
+    <Widget
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+  if (props.nearDevGovGigsContractAccountId) {
+    linkProps.nearDevGovGigsContractAccountId =
+      props.nearDevGovGigsContractAccountId;
+  }
+  if (props.nearDevGovGigsWidgetsAccountId) {
+    linkProps.nearDevGovGigsWidgetsAccountId =
+      props.nearDevGovGigsWidgetsAccountId;
+  }
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+  const linkPropsQuery = Object.entries(linkProps)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+  return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+const accountId = props.accountId;
+const link = props.link ?? true;
+const hideAccountId = props.hideAccountId;
+const hideName = props.hideName;
+const hideImage = props.hideImage;
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+const name = profile.name ?? accountId;
+const title = props.title ?? `${name} @${accountId}`;
+const tooltip =
+  props.tooltip && (props.tooltip === true ? title : props.tooltip);
+
+let inner = (
+  <>
+    {!hideImage && (
+      <Widget
+        key="image"
+        src="mob.near/widget/ProfileImage"
+        props={{
+          style: {
+            width: "1.5em",
+            height: "1.5em",
+            marginRight: "0.1em",
+            borderRadius: "50%",
+          },
+          profile,
+          accountId,
+          className: "d-inline-block",
+          imageClassName: "rounded w-100 h-100 align-top",
+        }}
+      />
+    )}
+    {!hideName && <span key="name">{name}</span>}
+    {!hideAccountId && (
+      <span key="accountId" className="text-muted ms-1">
+        @{accountId}
+      </span>
+    )}
+  </>
+);
+
+inner = link ? (
+  <a
+    href={
+      link !== true
+        ? link
+        : `#/mob.near/widget/ProfilePage?accountId=${accountId}`
+    }
+    className="link-dark text-truncate d-inline-flex"
+  >
+    {inner}
+  </a>
+) : (
+  <span className="text-truncate d-inline-flex">{inner}</span>
+);
+
+if (props.tooltip === true) {
+  return (
+    <Widget
+      src="mob.near/widget/Profile.OverlayTrigger"
+      props={{ accountId, children: inner }}
+    />
+  );
+}
+if (tooltip) {
+  inner = (
+    <OverlayTrigger placement="auto" overlay={<Tooltip>{tooltip}</Tooltip>}>
+      {inner}
+    </OverlayTrigger>
+  );
+}
+
+return inner;
