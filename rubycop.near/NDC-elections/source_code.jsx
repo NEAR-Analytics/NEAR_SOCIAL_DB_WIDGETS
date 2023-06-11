e160693c35895c60877616547ec6137574512dd2
rubycop.near <rubycop.near>
2023-06-11 21:14:22 +0000

    Update NDC-elections by rubycop.near at 2023-06-11T21:14:22.870Z

--- rubycop.near/NDC-elections/source_code.jsx
+++ rubycop.near/NDC-elections/source_code.jsx
@@ -8,7 +8,6 @@ const {
   quorum,
   voters_num,
   seats,
-  candidates,
   result,
   voters,
   votes,
@@ -23,6 +22,12 @@ State.init({
   selected: null,
   bookmarked: _bookmarked ? _bookmarked[_bookmarked.length - 1].value : [],
   selectedCandidates: [],
+  candidates: result,
+  filter: {
+    bookmark: false,
+    candidate: false,
+    votes: false,
+  },
 });
 
 const H4 = styled.h4`
@@ -62,24 +67,31 @@ const NominationLink = styled.a`
 `;
 
 const CandidateItem = styled.div`
-  padding: 0 25px;
+  padding: 0 20px;
   height: 48px;
   border-radius: 12px;
   margin-bottom: 8px;
+  border: 1px solid;
   background: ${(props) =>
     props.selected
       ? "linear-gradient(90deg, #9333EA 0%, #4F46E5 100%)"
       : "#F8F8F9"};
+  border-color: ${(props) => (props.selected ? "#4F46E5" : "#F8F8F9")};
   color: ${(props) => (props.selected ? "white" : "inherit")};
 
   &:hover {
     cursor: pointer;
+    border: 1px solid #4F46E5;
+    background: ${(props) =>
+      props.selected
+        ? "linear-gradient(90deg, #9333EA 0%, #4F46E5 100%)"
+        : "linear-gradient(90deg, rgba(147, 51, 234, 0.08) 0%, rgba(79, 70, 229, 0.08) 100%)"};
   }
 `;
 
 const VoterItem = styled.div`
 font-size: 14px;
-  padding: 0 25px;
+  padding: 0 20px;
   height: 48px;
   border-bottom: 1px solid #D0D6D9;
 
@@ -110,7 +122,7 @@ const ActionSelect = styled.div`
 `;
 
 const FilterRow = styled.div`
-  padding: 25px;
+  padding: 15px 20px;
 `;
 
 const PrimaryButton = styled.button`
@@ -121,6 +133,16 @@ const PrimaryButton = styled.button`
   font-weight: 500;
   line-height: 24px;
   border: 0;
+`;
+
+const SecondaryButton = styled.button`
+  padding: 4px 10px;
+  border: 1px solid #FFD50D;
+  background: transparent;
+  border-radius: 10px;
+  font-size: 12px;
+  font-weight: 500;
+  margin-left: 10px;
 `;
 
 const CastVotesSection = styled.div`
@@ -310,30 +332,77 @@ const CandidateList = ({ accountId, votes }) => {
   );
 };
 
-const Filters = () => (
-  <FilterRow className="d-flex align-items-center justify-content-between">
-    <div className="d-flex">
-      <Bookmark>
-        <small className="text-secondary">Bookmark</small>
-      </Bookmark>
-      <AccountLink>
-        <small className="text-secondary">Candidate</small>
-      </AccountLink>
-    </div>
-    <div className="d-flex">
-      <Votes>
-        <small className="text-secondary">Votes</small>
-      </Votes>
-      <ActionSelect />
-    </div>
-  </FilterRow>
-);
+const filterBy = (option) => {
+  if (!state.filter.bookmark && option.bookmark)
+    State.update({
+      candidates: state.candidates.filter(([accountId, _votes], _index) =>
+        state.bookmarked.includes(accountId)
+      ),
+      filter: { bookmark: true },
+    });
+  else if (!state.filter.candidate && option.candidate)
+    State.update({
+      candidates: state.filter.candidate ? state.candidates.sort((a, b) =>
+        state.filter.candidate ? a[1] - b[1] : b[1] - a[1]
+      ),
+      filter: { candidate: !state.filter.candidate },
+    });
+  else if (option.votes) {
+    console.log(state.filter.votes);
+    State.update({
+      candidates: state.candidates.sort((a, b) =>
+        state.filter.votes ? a[1] - b[1] : b[1] - a[1]
+      ),
+      filter: { votes: !state.filter.votes },
+    });
+  } else
+    State.update({
+      candidates: result,
+      filter: { bookmark: false },
+    });
+};
+
+const Filters = () => {
+  return (
+    <FilterRow className="d-flex align-items-center justify-content-between">
+      <div className="d-flex">
+        <Bookmark
+          role="button"
+          className="text-secondary"
+          onClick={() => filterBy({ bookmark: true })}
+        >
+          <small>Bookmark</small>
+          <i className="bi bi-funnel" />
+        </Bookmark>
+        <AccountLink
+          role="button"
+          className="text-secondary"
+          onClick={() => filterBy({ candidate: true })}
+        >
+          <small>Candidate</small>
+          <i className="bi bi-arrow-down" />
+        </AccountLink>
+      </div>
+      <div className="d-flex">
+        <Votes
+          role="button"
+          className="text-secondary"
+          onClick={() => filterBy({ votes: true })}
+        >
+          <small>Votes</small>
+          <i className="bi bi-arrow-down" />
+        </Votes>
+        <ActionSelect />
+      </div>
+    </FilterRow>
+  );
+};
 
 return (
   <Container>
     <h1>{title}</h1>
     <Filters />
-    {result.map(([accountId, votes], index) => (
+    {state.candidates.map(([accountId, votes], index) => (
       <CandidateList accountId={accountId} votes={votes} key={index} />
     ))}
     <CastVotesSection className="d-flex align-items-center justify-content-between">
@@ -342,6 +411,18 @@ return (
         <span>/</span>
         <H4>{votes.total}</H4>
         <span className="text-secondary">votes left</span>
+        {state.selectedCandidates.length > 0 && (
+          <SecondaryButton
+            onClick={() =>
+              State.update({
+                selectedCandidates: [],
+                availableVotes: votes.total,
+              })
+            }
+          >
+            Reset Selection
+          </SecondaryButton>
+        )}
       </div>
       <PrimaryButton
         disabled={!state.selectedCandidates.length}
