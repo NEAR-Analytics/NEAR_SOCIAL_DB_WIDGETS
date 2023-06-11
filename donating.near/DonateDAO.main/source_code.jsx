df8be1d43f3a46c74d2a32b6261137c3836ab30b
donating.near <donating.near>
2023-06-11 03:22:34 +0000

    Update DonateDAO.main by donating.near at 2023-06-11T03:22:34.166Z

--- donating.near/DonateDAO.main/source_code.jsx
+++ donating.near/DonateDAO.main/source_code.jsx
@@ -1,19 +1,59 @@
 // TO-DO
-// hookup to paymster, show transactionr ecipet
-// show balance of USDC of connected wallet
-// put usdc address on ZKSync
-// check connect address instead of deposit
-// configure dropw down reciever
-// show the on chain mimum balance amount
+// hookup to paymster & import abi
+// show balance of ERC20 of connected wallet
+// call in donate
+// NICE TO HAVE
+// - show transactionr reciept of donation
+// configure dropw down receiver
 // add zk account
-if (state.chainId !== undefined && state.chainId !== 280) {
-  return <p>Switch to ZKSync Testnet</p>;
-} // not sure if this is working
+const sender = Ethers.send("eth_requestAccounts", [])[0];
+let networkId = Ethers.provider()._network.chainId;
+
+// const yourAddress = Ethers.provider().getSigner();
 // https://era.zksync.io/docs/dev/building-on-zksync/useful-address.html
+const initalAmount = 1;
 State.init({
-  reciever: "0xDoodoo",
-}); // write now state reciever is in clipboard
+  receiver: "0xPlaceholder",
+  networkId,
+  sender,
+  amount: initalAmount,
+  token: "0x5DBAFaccAADa3E16213de291fA5e39c1996093D5",
+  balance: 0,
+  tokenDecimals: "18",
+});
+if (
+  state.networkId === undefined &&
+  ethers !== undefined &&
+  Ethers.send("eth_requestAccounts", [])[0]
+) {
+  Ethers.provider()
+    .getNetwork()
+    .then((chainIdData) => {
+      if (chainIdData?.chainId) {
+        State.update({ networkId: chainIdData.chainId });
+      }
+    });
+}
+
+console.log("Network ID - " + state.networkId); // not working
+console.log("Your ETH Address - " + state.sender);
+
+// Support ZKSync
+if (state.chainId !== undefined && state.chainId !== 280) {
+  return (
+    <div>
+      <h3>Wrong Network - We currently support the ZKSync Era Testnet</h3>
+    </div>
+  );
+}
+const getEVMAccountId = () => {
+  if (ethers !== undefined) {
+    return Ethers.send("eth_requestAccounts", [])[0] ?? "";
+  }
+  return "";
+};
 // need to have the search result updated and selected in state
+
 // Charity helper functions
 function loadCharities() {
   const res = fetch(
@@ -27,57 +67,91 @@ if (!charityList) {
 }
 
 const PAYMASTER_ADDRESS = "0xaB23B553781757a8ebdFb11A9231825ff4EE4118";
-const TOKEN_ADDRESS = "0x5DBAFaccAADa3E16213de291fA5e39c1996093D5"; // on zk sync testnet
-const { action, amount, USDCbalance } = state;
-const { assets } = deposit;
-// need to get USDC balance form on CHain
+// https://goerli.explorer.zksync.io/address/0xaB23B553781757a8ebdFb11A9231825ff4EE4118
+const TOKEN_ADDRESS = "0x5DBAFaccAADa3E16213de291fA5e39c1996093D5";
+// https://goerli.explorer.zksync.io/address/0x5DBAFaccAADa3E16213de291fA5e39c1996093D5
+const tokenDecimals = 18; // double check on contract
+const erc20Abi = fetch(
+  "https://gist.githubusercontent.com/veox/8800debbf56e24718f9f483e1e40c35c/raw/f853187315486225002ba56e5283c1dba0556e6f/erc20.abi.json"
+);
+const paymasterABI = fetch(
+  "https://raw.githubusercontent.com/codingshot/donatedao-landing/main/data/paymaster-whitelist-abi.json"
+); // updated with whitelist
+//   "https://raw.githubusercontent.com/codingshot/zksync-paymaster/main/paymaster-abi.json"
+if (!paymasterABI.ok) {
+  return "Paymaster ABI Not ok";
+}
+const tokenIFace = new ethers.utils.Interface(erc20Abi.body);
+// get receiver
+console.log("Token Interface is:  -  " + tokenIFace);
+const getTokenBalance = () => {
+  const encodedData = tokenIFace.encodeFunctionData("balanceOf", [
+    state.sender,
+  ]);
 
-const getUSDCBalance = () => {
-  // add to query USDC balance on chain for ZK Testnet account, this is how we will set max
-  State.update({ USDCbalance: USDCbalance }); // not all the way correct
-};
+  return Ethers.provider()
+    .call({
+      to: TOKEN_ADDRESS,
+      data: encodedData,
+    })
+    .then((rawBalance) => {
+      const senderBalanceHex = tokenIFace.decodeFunctionResult(
+        "balanceOf",
+        rawBalance
+      );
 
-const actionTitle = isDeposit ? "Donate" : "Withdraw";
+      console.log(senderBalanceHex.toString());
+      const balanceBig = Big(senderBalanceHex.toString())
+        .div(Big(10).pow(tokenDecimals))
+        .toFixed(2)
+        .replace(/\d(?=(\d{3})+\.)/g, "$&,");
+      State.update({ balance: balanceBig });
+      console.log("Updated Balance: " + state.balance);
+      return Big(senderBalanceHex.toString())
+        .div(Big(10).pow(tokenDecimals))
+        .toFixed(2)
+        .replace(/\d(?=(\d{3})+\.)/g, "$&,");
+    });
+}; // how to update here
+//  const ifaceErc20 = new ethers.utils.Interface(state.erc20Abi);
+// const contract = new ethers.Contract(contractAddress, erc20Abi);
+
+const ifacePaymaster = new ethers.utils.Interface(paymasterABI.body);
 
 const donate = () => {
-  // add payment to charity logic here
+  // add payment to charity logic here // this is where you hit paymaster
+  console.log("Add paymaster donate logic here");
+  getTokenBalance();
 };
-
 const handleMax = () => {
-  State.update({ amount: USDCbalance });
+  State.update({ amount: state.balance });
+  console.log("New Amount: " + state.amount);
 }; // how to get max of selectedasset
 
 const handleAmountChange = (e) => {
   State.update({ amount: e.target.value });
+  console.log("Your ERC20 Amount: " + state.amount);
 };
-const handleRecieverChange = (e) => {
-  State.update({ reciever: e.target.value });
+const handleReceiverChange = (e) => {
+  State.update({ receiver: e.target.value });
+  console.log("Charity Address: " + state.receiver);
 };
-
-// const handleCharityChange = (e) => {
-//   State.update({
-//     reciever: charityList?.find((a) => a.address === e.target.value),
-//   });
-// }; // need to change this around
-
-// Assuming you have a state variable called `state` which contains the `receiver` property
 
 const handleCharityChange = (e) => {
-  // Find the charity object with the selected title
-  const selectedCharity = charityList.find(
-    (charity) => charity.title === e.target.value
+  State.update({
+    receiver: charityList
+      .filter((charity) => charity.title === e.target.value)
+      .map((el) => el.address)[0],
+  });
+  console.log(
+    charityList
+      .filter((charity) => charity.title === e.target.value)
+      .map((el) => el.address)[0]
   );
-
-  if (selectedCharity) {
-    // Update the `receiver` state to the associated address
-    State.update({
-      reciever: selectedCharity.address,
-    });
-  }
-};
+}; // need to change this around
 
 const Container = styled.div`
-    max-width: 90%;
+    max-width: 95%;
     width: 100%;
     margin: 0 auto;
     display: flex;
@@ -91,6 +165,7 @@ const Container = styled.div`
     * {
         font-family: 'Inter custom',sans-serif;
     }
+ 
 
     background: white;
     color: black;
@@ -150,13 +225,6 @@ const Container = styled.div`
     }
 `;
 
-const sender = Ethers.send("eth_requestAccounts", [])[0];
-const erc20Abi = fetch(
-  "https://gist.githubusercontent.com/veox/8800debbf56e24718f9f483e1e40c35c/raw/f853187315486225002ba56e5283c1dba0556e6f/erc20.abi.json"
-);
-const usdcZKtestnet = "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4";
-const paymasterZKtestnet = "";
-
 if (!state.theme) {
   State.update({
     theme: styled.div`
@@ -164,19 +232,30 @@ if (!state.theme) {
 `,
   });
 }
-const Theme = state.theme;
-const paymasterContract = "";
-const tokenDecimals = 18; // double check on contract
-// FETCH Paymaster ABI
+// overlay
+State.init({
+  show: false,
+});
+const handleOnMouseEnter = () => {
+  State.update({ show: true });
+};
+const handleOnMouseLeave = () => {
+  State.update({ show: false });
+};
 
-// see if API3 can be fetched here
-
-const paymasterABI = fetch(
-  "https://raw.githubusercontent.com/codingshot/zksync-paymaster/main/paymaster-abi.json"
+const overlay = (
+  <div
+    className="border m-3 p-3 rounded-4 bg-white shadow"
+    style={{ maxWidth: "24em", zIndex: 1070 }}
+    onMouseEnter={handleOnMouseEnter}
+    onMouseLeave={handleOnMouseLeave}
+  >
+    This is custom ERC 20 Token Deploy on ZK Testnet
+  </div>
 );
-if (!paymasterABI.ok) {
-  return "Loading";
-}
+
+const Theme = state.theme;
+
 // add conditional for charity // this should be emitted as an error from the smart contract
 return (
   <Theme>
@@ -187,21 +266,38 @@ return (
       <div className="border border-secondary border-bottom-0 border-light" />
       <div className="p-2">
         <div className="d-flex justify-content-between">
-          <span>
-            {" "}
-            USDC Balance:
-            {USDCbalance}
-          </span>
+          <OverlayTrigger
+            show={state.show}
+            trigger={["hover", "focus"]}
+            delay={{ show: 250, hide: 300 }}
+            placement="auto"
+            overlay={overlay}
+          >
+            <span>
+              {" "}
+              <a
+                href={
+                  "https://goerli.explorer.zksync.io/address/" + state.token
+                }
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                MOCK USDC
+              </a>{" "}
+              Balance:
+              {state.balance}
+            </span>
+          </OverlayTrigger>
         </div>
         <div className="balance input-group">
           <input
             style={{ maxWidth: "100%" }}
             type="number"
-            min="0"
+            min=".5"
             step="0.1"
-            defaultValue={props.amount}
+            defaultValue={state.amount}
             value={amount}
-            placeholder="0.00"
+            placeholder={state.amount}
             onChange={handleAmountChange}
           />
           <button className="btn btn-light btn-sm max" onClick={handleMax}>
@@ -213,15 +309,19 @@ return (
           <select
             className="form-select"
             aria-label="select asset"
-            onChange={handleCharityChange(e)}
+            onChange={handleCharityChange}
           >
+            <option selected disabled>
+              {" "}
+              Select a charity
+            </option>
             {charityList &&
               charityList.map((charity) => (
                 <option value={charity.title} selected={charity.selected}>
                   {charity.title}
                 </option>
               ))}
-            // add reciever logic here
+            // add receiver logic here
           </select>
         </div>
         <div className="balance input-group">
@@ -229,9 +329,9 @@ return (
             style={{ maxWidth: "100%" }}
             type="string"
             defaultValue={props.amount}
-            value={reciever}
-            placeholder={state.reciever}
-            onChange={handleRecieverChange}
+            value={receiver}
+            placeholder={state.receiver}
+            onChange={handleReceiverChange}
           />
         </div>
       </div>
@@ -240,12 +340,8 @@ return (
       <div className="border border-secondary border-bottom-0 border-light" />
       {!!state.sender ? (
         <div className="p-4 d-grid gap-3">
-          <button
-            className="action btn btn-primary"
-            onClick={donate}
-            disabled={isLoading}
-          >
-            {isLoading ? "Loading..." : actionTitle}
+          <button className="action btn btn-primary" onClick={donate}>
+            Donate
           </button>
           {log && (
             <div className="alert alert-success" role="alert">
@@ -261,23 +357,14 @@ return (
       ) : (
         <div className="row">
           <Web3Connect
-            className="btn btn-secondary col-6"
+            className="btn btn-secondary"
             connectLabel="Connect To ZkSync"
           />
-          <button
-            className=" btn btn-primary col-6"
-            onClick={donate}
-            disabled={isLoading}
-          >
-            {chainId != 280 ? "Connect to Donate" : "Donate"}
+          <button className=" btn btn-primary" onClick={donate}>
+            Donate
           </button>
         </div>
       )}
     </Container>
-    <div>
-      <h1>Debug</h1>
-      <p>Input USDC Amount: {state.amount}</p>
-      <p>Input Reciever Address: {state.reciever}</p>
-    </div>
   </Theme>
 );
