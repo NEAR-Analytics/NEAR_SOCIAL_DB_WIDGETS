7de0ab950b485b842b0cac116df6da4104e44244
bitspresso.near <bitspresso.near>
2023-06-11 08:09:05 +0000

    Update ExtensionSource by bitspresso.near at 2023-06-11T08:09:05.147Z

---
+++ bitspresso.near/ExtensionSource/source_code.jsx
@@ -0,0 +1,13 @@
+return (
+  <>
+    <Widget />
+            <div class="container">
+                <img src=""/>
+                <div>
+                    <span>You have entered incognito mode.
+                    </span>
+                </div>
+            </div>
+    </Widget>
+  </>
+);
