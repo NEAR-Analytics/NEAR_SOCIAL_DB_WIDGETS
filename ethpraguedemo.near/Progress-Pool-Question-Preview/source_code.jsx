02c99b45ac7afed67f66af42a496c0226ca2d199
ethpraguedemo.near <ethpraguedemo.near>
2023-06-11 01:26:58 +0000

    Update Progress-Pool-Question-Preview by ethpraguedemo.near at 2023-06-11T01:26:58.248Z

--- ethpraguedemo.near/Progress-Pool-Question-Preview/source_code.jsx
+++ ethpraguedemo.near/Progress-Pool-Question-Preview/source_code.jsx
@@ -101,7 +101,7 @@ return (
       <div class="row">
         <div class="col">
           <div class="row">
-            <div class="col">
+            <div class="col" style={{ marginLeft: "8px" }}>
               <H2 class="ml-6">
                 <a href={result[0]}>{result[0]}</a>
                 <i class="bi bi-arrow-right" />
