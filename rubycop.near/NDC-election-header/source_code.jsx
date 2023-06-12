ee2dba18eba0a9f50a974e289d314d2d52a842bc
rubycop.near <rubycop.near>
2023-06-12 09:14:02 +0000

    Update NDC-election-header by rubycop.near at 2023-06-12T09:14:02.347Z

--- rubycop.near/NDC-election-header/source_code.jsx
+++ rubycop.near/NDC-election-header/source_code.jsx
@@ -1,45 +1,32 @@
-// props
-// {
-//   "date": "June 20, 2023 1:00:00"
-// }
+const { end } = props;
 
-const countDownDate = new Date(props.date).getTime();
+const countDownDate = new Date(end).getTime();
 
 State.init({
+  days: "-",
   hours: "-",
   minutes: "-",
   seconds: "-",
-  timerType: "0",
 });
 
 const formatTime = (time) => (time < 10 ? `0${time}` : time);
 
 const timer = setInterval(() => {
-  const distance = new Date(props.date).getTime() - new Date().getTime();
+  const diff = new Date(end).getTime() - new Date().getTime();
 
-  const seconds = Math.floor((distance / 1000) % 60);
-  const minutes = Math.floor((distance / 1000 / 60) % 60);
-  const hours = Math.floor(distance / (1000 * 60 * 60));
+  const seconds = Math.floor((diff / 1000) % 60);
+  const minutes = Math.floor((diff / 1000 / 60) % 60);
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
 
-  State.update({
-    hours: hours,
-    minutes: minutes,
-    seconds: seconds,
-  });
+  State.update({ days, hours, minutes, seconds });
 
   clearInterval(timer);
 }, 1000);
 
-const handleChange = (event) =>
-  State.update({
-    timerType: event.target.value,
-  });
-
 const Logo = styled.img`
     width: 70px;
-    z-index: 1;
-    border-radius: 50%;
-    margin-right: 30px;
+    margin-right: 20px;
 `;
 
 const H1 = styled.h1`
@@ -49,49 +36,58 @@ const H1 = styled.h1`
 `;
 
 const H6 = styled.h6`
-  font-size: 14px;
+  font-size: 12px;
   font-weight: 300;
-  margin-right: 10px;
+  margin-right: 32px;
   margin-bottom: 0;
+  line-height: 1.5;
+  align-items: center;
+  letter-spacing: 0.16em;
+  text-transform: uppercase;
 `;
 
-const Timer = styled.h2`
-  font-size: 44px;
-  font-weight: 300;
-  margin-right: 20px;
-  margin-bottom: 0;
+const Timer = styled.div`
+  .time {
+    font-size: 48px;
+    font-weight: 800;
+    color: #FFD50D;
+    width: 100px;
+    line-height: 1;
+  }
+  small {
+    margin-bottom: 0;
+    align-items: center;
+  }
 `;
 
 return (
-  <div className="p-4 bg-light rounded d-flex flex-wrap justify-content-between align-items-center">
-    <div className="d-flex align-items-center w-50">
+  <div className="p-4 bg-black text-white d-flex justify-content-between align-items-center">
+    <div className="d-flex align-items-center">
       <Logo src="https://ipfs.near.social/ipfs/bafkreie4rfa63zedwnpbwm5lglqrwqhahcnf6slllqmq7sh46ngf5y4vsq" />
       <H1>NDC Elections</H1>
     </div>
-    <div className="d-flex align-items-center w-50">
+    <div className="d-flex align-items-center">
       <H6>
-        Time remaining <br /> in current election
+        Time remaining in <br /> current election
       </H6>
-      <div className="w-50">
-        <Timer>
-          {[
-            formatTime(state.hours),
-            formatTime(state.minutes),
-            formatTime(state.seconds),
-          ].join(" : ")}
-        </Timer>
-      </div>
-      <div className="w-25">
-        <select
-          class="form-select"
-          aria-label="Default select example"
-          onChange={handleChange}
-          value={state.timerType}
-        >
-          <option value="0">Congress</option>
-          <option value="1">Time</option>
-        </select>
-      </div>
+      <Timer className="d-flex">
+        <div>
+          <div className="time">{formatTime(state.days)}</div>
+          <small>days</small>
+        </div>
+        <div>
+          <div className="time">{formatTime(state.hours)}</div>
+          <small>hours</small>
+        </div>
+        <div>
+          <div className="time">{formatTime(state.minutes)}</div>
+          <small>minutes</small>
+        </div>
+        <div>
+          <div className="time">{formatTime(state.seconds)}</div>
+          <small>seconds</small>
+        </div>
+      </Timer>
     </div>
   </div>
 );
