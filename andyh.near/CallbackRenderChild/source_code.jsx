7d7c404d9cbf327269be55479a4d34c41e6c9bce
andyh.near <andyh.near>
2023-06-13 21:06:56 +0000

    Update CallbackRenderChild by andyh.near at 2023-06-13T21:06:56.442Z

--- andyh.near/CallbackRenderChild/source_code.jsx
+++ andyh.near/CallbackRenderChild/source_code.jsx
@@ -1,1 +1,6 @@
-return <div>{await props.renderComponent("i am the child")}</div>;
+// return <div>{await props.renderComponent("i am the child")}</div>;
+return (
+  <div>
+    <span>i am the child</span>
+  </div>
+);
