f6b152e5a29e4c53d4929371d2b6868ded262622
kingdeemo.near <kingdeemo.near>
2023-06-13 20:41:57 +0000

    Update aptos_gallery by kingdeemo.near at 2023-06-13T20:41:57.593Z

--- kingdeemo.near/aptos_gallery/source_code.jsx
+++ kingdeemo.near/aptos_gallery/source_code.jsx
@@ -1,16 +1,18 @@
 const apiUrl = "https://api.indexer.xyz/graphql";
 const apiKey = "Krqwh4b.bae381951d6050d351945c0c750f1510";
 const apiUser = "Banyan";
-
-initState({ nfts: [] });
-const headers = {
-  "x-api-key": apiKey,
-  "x-api-user": apiUser,
-  "Content-Type": "application/json",
-};
+initState({
+  collectionData: {},
+  searchTerm: "",
+  nftData: [],
+  filteredNFTData: [],
+  aptosRate: "",
+});
 
 function fetchData() {
-  const response = fetch("https://api.indexer.xyz/graphql", {
+  State.update({ nftData: [] });
+
+  const response = fetch(apiUrl, {
     method: "POST",
     headers: {
       "x-api-key": apiKey,
@@ -19,68 +21,472 @@ function fetchData() {
     },
     body: JSON.stringify({
       query: `query MyQuery {
-      aptos {
-        nfts(limit: 18) {
-          id
-          name
-          owner
-          media_url
-          rarity
-          token_id
-          collection {
-        id
+  aptos {
+    crypto_rates(where: {fiat: {_eq: "USD"}}) {
+      crypto
+      rate
+    }
+    nfts(order_by: {ranking: asc}) {
+      id
+      name
+      media_url
+      ranking
+      rarity
+      token_id
+      listings {
+        price
       }
-        }
-      }
-    }`,
+    }
+  }
+}`,
     }),
   });
 
-  if (response === null) {
-    return "";
-  } else {
-    State.update({ nfts: response.body.data.aptos.nfts });
-    // console.log("Body: ", response.body.data.aptos.nfts);
-    return state.nfts;
-  }
+  response === []
+    ? ""
+    : State.update({
+        nftData: response.body.data.aptos.nfts,
+        aptosRate: response.body.data.aptos.crypto_rates[4].rate,
+      });
+
+  /////////////////////////////////////////////////////////
+  //   console.log("Aptos", response.body.data.aptos.crypto_rates[4].rate);
 }
-
 fetchData();
+const updateInputCollectionSlug = (e) => {
+  State.update({ inputCollectionSlug: e.target.value });
+};
 
+const handleFetchButtonClick = () => {
+  State.update({ collectionSlug: state.inputCollectionSlug });
+  fetchData();
+};
+
+const updateCollectionSlug = (e) => {
+  State.update({ collectionSlug: e.target.value });
+};
+
+const getRarityColor = (rarity) => {
+  if (rarity < 0.1) {
+    return "#ee0000";
+  } else if (rarity < 0.25) {
+    return "#ff9900";
+  } else if (rarity < 0.5) {
+    return "#ffc300";
+  } else {
+    return "#61c700";
+  }
+};
+
+const seachInputHandler = (e) => {
+  const value = e.target.value.toLowerCase();
+  console.log(value);
+  const searched = state.nftData.filter((nft) =>
+    nft.name.toLowerCase().includes(value)
+  );
+  State.update({
+    searchTerm: value,
+    filteredNFTData: searched,
+  });
+};
+
+const Stats = styled.div`
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      width: "100%";
+      max-width: 800px;
+      gap: 20px;
+  `;
+
+const Main = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    margin-bottom: 20px;
+  `;
+
+const PageTitle = styled.h1`
+  text-align:center;
+  font-size: 4vw; 
+  font-weight: bold; 
+  margin-bottom: 20px; 
+  color: #0f1d40;
+  `;
+
+const NFTCard = styled.div`
+   display: flex;
+   flex-flow: column nowrap;
+   -ms-flex-flow:column nowrap;
+   align-items:center;
+   background: #fff;
+   border-radius: 10px;
+   border: 1.41429px solid rgba(28,27,28,.2);
+   box-shadow: 5.65714px 5.65714px 11.3143px rgba(28,27,28,.04);
+   padding: 8px 0px;
+   background-color:#fff;
+   max-width: 350px;
+   margin: 0 auto;
+   &:hover &>div>img{
+     transform:scale(1.05);
+   }
+   button{
+   padding: .75em 2em;
+   border-radius: .7em;
+   color: var(--main-color);
+   border: 1px solid transparent;
+   transition: all .3s;
+   cursor: pointer;
+    color: #fff;
+    background: #0d99ff;
+    &:hover{
+        color: #0d99ff;
+        background:#fff;
+    }
+  @media screen and (max-width: 540px){ 
+    padding: .5em 2em;    
+    }
+    }
+  `;
+
+const NFTCardText = styled.div`
+  width: 100%;
+  // padding: 0px 1rem;
+  `;
+
+const NFTCards = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(270px, 1fr));
+  justify-content: center;
+  background: #e4f1fb;
+  background: linear-gradient(180deg,#e4f1fb 0%, rgba(0,255,0,0) 3%);
+  background: -webkit-linear-gradient(180deg,#e4f1fb 0%, rgba(0,255,0,0) 3%);
+  background: -moz-linear-gradient(270deg,#e4f1fb 0%, rgba(0,255,0,0) 3%);
+  padding: 20px 3rem 1rem 3rem;
+  width:100%;
+`;
+
+const ImageCard = styled.div`
+  height:250px;
+  width: 96%;
+  border-radius: 0.5rem;
+  overflow:hidden;
+  margin-bottom: .4rem;
+  &>img{
+  object-fit: cover;
+  transition: all 0.3s ease-in-out;
+  }
+  &>img:hover{
+    transform:scale(1.05);
+  }
+`;
+const InputContainer = styled.div`
+    width:80%;
+    max-width: 700px;
+    display: flex;
+    align-items: center;
+    justify-content:center;
+    margin: 1rem auto 1rem auto;
+    &>input{
+        outline: none;
+    }
+    &>input:hover, &>input:focus{
+      border: 1px solid #0d99ff;
+      box-shadow: none;
+    }
+`;
+
+const Hero = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  background-image: url(https://www.genadrop.com/static/media/banner-marketplace.e5c03bb6.svg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-positiion: center;
+  width: 100%;
+  padding: 2rem;
+`;
+
+const RankCard = styled.span`
+  background-color: rgba(28,27,28,.06);
+  border-radius: .5rem;
+  color: #000;
+  display: inline-block;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 8px;
+`;
+
+const PriceArea = styled.div`
+  display: flex;
+  align-items: center;
+  color: #0d99ff;
+  &>*{
+  margin: 0px;
+  padding: 0px;
+  }
+  &>h6{
+    font-weight: 700;
+    font-size: 1.2rem;
+  }
+  &>span{
+  font-size: 1.2rem;
+  margin: 0px;
+  }
+`;
+const isPriceValid = typeof nft.listings[0]?.price === "number";
 return (
-  <div style={{ backgroundColor: "black" }}>
-    {state.nfts.length > 0 && (
-      <div className="grid grid-cols-3 gap-4 mt-4">
-        {state.nfts.map((nft) => (
-          <a
-            href={`#/kingdeemo.near/widget/nft_page?id=${nft.id}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-decoration-none color-inherit"
-            key={nft.id}
-          >
-            <div className="bg-black border-white rounded-2xl shadow-md p-4 text-center">
-              <img
-                src={nft.media_url}
-                alt={nft.name}
-                className="w-full h-auto rounded-2xl object-cover mb-4"
-              />
-              <h3 className="text-xl mb-2 text-white">{nft.name}</h3>
-              <div className="mb-2">
-                <span className="bg-blue-500 rounded-full text-white text-xs font-bold py-1 px-2 max-w-32 overflow-hidden overflow-ellipsis whitespace-nowrap">
-                  Owner ID: {nft.owner}
-                </span>
-              </div>
-              <p className="text-sm text-white mb-1 max-w-32 overflow-hidden overflow-ellipsis whitespace-nowrap">
-                Token ID: {nft.token_id}
-              </p>
-              <p className="text-sm text-white max-w-32 overflow-hidden overflow-ellipsis whitespace-nowrap">
-                Collection: {nft.collection.id}
-              </p>
-            </div>
-          </a>
-        ))}
-      </div>
+  <>
+    <Hero className="w-100">
+      <PageTitle>
+        Find, Buy and Sell NEAR NFTs on <br />
+        💧GenaDrop
+      </PageTitle>
+      <InputContainer>
+        <input
+          type="search"
+          value={state.searchTerm}
+          placeholder="Search NFTs"
+          onChange={seachInputHandler}
+        />
+      </InputContainer>
+    </Hero>
+    {state.nftData.length > 0 ? (
+      <NFTCards>
+        {state.searchTerm === "" ? (
+          state.nftData.map((nft) => (
+            <a
+              // href={`https://www.tradeport.xyz/near/collection/${state.collectionData.slug}/${nft.token_id}`}
+              // target="_blank"
+              href={`#/mob.near/widget/MyPage?accountId=${nft.nft_state.owner}`}
+              // rel="noopener noreferrer"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <NFTCard classNmae="card">
+                <ImageCard>
+                  <img
+                    src={nft.media_url}
+                    alt={nft.name}
+                    width="100%"
+                    height="100%"
+                    className="rounded-3"
+                  />
+                </ImageCard>
+                <NFTCardText>
+                  <hr />
+                  <div className="d-flex my-4 justify-content-between w-100 px-2">
+                    <RankCard>Rank: {Math.round(nft.ranking)}</RankCard>
+                    <div>{nft.nft_state_lists[0].list_contract.name}</div>
+                  </div>
+                  <div className="px-2">
+                    <div style={{ color: "#a4a9b6" }}>Name</div>
+                    <h3
+                      style={{
+                        fontSize: "16px",
+                        margin: "0 0 10px",
+                        wordBreak: "break-all",
+                      }}
+                    >
+                      {nft.name}
+                    </h3>
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      marginBottom: "5px",
+                    }}
+                    className="px-2"
+                  >
+                    <div>
+                      <div style={{ color: "#a4a9b6" }}>Token ID</div>
+                      <p
+                        style={{
+                          fontSize: "14px",
+                          marginBottom: "5px",
+                          color: "#0d99ff",
+                        }}
+                      >
+                        {nft.token_id.length > 30
+                          ? `${nft.token_id.slice(0, 30)}...`
+                          : nft.token_id}
+                      </p>
+                    </div>
+                    {nft.nft_state && (
+                      <div>
+                        <div style={{ color: "#a4a9b6" }}>Owner</div>
+                        <p style={{ fontSize: "14px" }}>
+                          {nft.nft_state.owner.length > 12
+                            ? nft.nft_state.owner.slice(0, 12) + "..."
+                            : nft.nft_state.owner}
+                        </p>
+                      </div>
+                    )}
+                  </div>
+                  {/*<p style={{ fontSize: "14px" }} className="px-2">
+                    Collection: {nft.collection.slug}
+                  </p>*/}
+                  <hr />
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      marginBottom: "5px",
+                    }}
+                    className="px-2"
+                  >
+                    <div className="px-2">
+                      <div style={{ color: "#a4a9b6", fontSize: "1.1rem" }}>
+                        Price
+                      </div>
+                      {nft.listings &&
+                      nft.listings[0] &&
+                      typeof nft.listings[0].price === "number" ? (
+                        <PriceArea>
+                          <h6>{`${
+                            nft.listings[0].price.toFixed(2) / 100000000
+                          }APT`}</h6>
+                          <span>{` ($${(
+                            (nft.listings[0].price / 100000000) *
+                            state.aptosRate
+                          ).toFixed(2)})`}</span>
+                        </PriceArea>
+                      ) : (
+                        <div>Not for Sale</div>
+                      )}
+                    </div>
+                    <button>Buy Now </button>
+                  </div>
+                </NFTCardText>
+              </NFTCard>
+            </a>
+          ))
+        ) : state.filteredNFTData.length > 0 ? (
+          state.filteredNFTData.map((nft) => (
+            <a
+              // href={`https://www.tradeport.xyz/near/collection/${state.collectionData.slug}/${nft.token_id}`}
+              // target="_blank"
+              href={`#/mob.near/widget/MyPage?accountId=${nft.nft_state.owner}`}
+              // rel="noopener noreferrer"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <NFTCard classNmae="card">
+                <ImageCard>
+                  <img
+                    src={nft.media_url}
+                    alt={nft.name}
+                    width="100%"
+                    height="100%"
+                    className="rounded-3"
+                  />
+                </ImageCard>
+                <NFTCardText>
+                  <hr />
+                  <div className="d-flex my-4 justify-content-between w-100 px-2">
+                    <RankCard>Rank: {Math.round(nft.ranking)}</RankCard>
+                    <div>{nft.nft_state_lists[0].list_contract.name}</div>
+                  </div>
+                  <div className="px-2">
+                    <div style={{ color: "#a4a9b6" }}>Name</div>
+                    <h3
+                      style={{
+                        fontSize: "16px",
+                        margin: "0 0 10px",
+                        wordBreak: "break-all",
+                      }}
+                    >
+                      {nft.name}
+                    </h3>
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      marginBottom: "5px",
+                    }}
+                    className="px-2"
+                  >
+                    <div>
+                      <div style={{ color: "#a4a9b6" }}>Token ID</div>
+                      <p
+                        style={{
+                          fontSize: "14px",
+                          marginBottom: "5px",
+                          color: "#0d99ff",
+                        }}
+                      >
+                        {nft.token_id.length > 30
+                          ? `${nft.token_id.slice(0, 30)}...`
+                          : nft.token_id}
+                      </p>
+                    </div>
+                    {nft.nft_state && (
+                      <div>
+                        <div style={{ color: "#a4a9b6" }}>Owner</div>
+                        <p style={{ fontSize: "14px" }}>
+                          {nft.nft_state.owner.length > 12
+                            ? nft.nft_state.owner.slice(0, 12) + "..."
+                            : nft.nft_state.owner}
+                        </p>
+                      </div>
+                    )}
+                  </div>
+                  {/*<p style={{ fontSize: "14px" }} className="px-2">
+                    Collection: {nft.collection.slug}
+                  </p>*/}
+                  <hr />
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      marginBottom: "5px",
+                    }}
+                    className="px-2"
+                  >
+                    <div className="px-2">
+                      <div style={{ color: "#a4a9b6", fontSize: "1.1rem" }}>
+                        Price
+                      </div>
+                      {nft.listings &&
+                      nft.listings[0] &&
+                      typeof nft.listings[0].price === "number" ? (
+                        <PriceArea>
+                          <h6>{`${nft.listings[0].price.toFixed(2)}APT`}</h6>
+                          <span>{` ($${(nft.listings[0].price * 1.56).toFixed(
+                            2
+                          )})`}</span>
+                        </PriceArea>
+                      ) : (
+                        <div>Not for Sale</div>
+                      )}
+                    </div>
+                    <button
+                      className={isPriceValid ? "" : "grayed-out"}
+                      disabled={!isPriceValid}
+                    >
+                      Buy Now{" "}
+                    </button>
+                  </div>
+                </NFTCardText>
+              </NFTCard>
+            </a>
+          ))
+        ) : (
+          <div>No results found for "{state.searchTerm}".</div>
+        )}
+      </NFTCards>
+    ) : (
+      <div>No NFTs available.</div>
     )}
-  </div>
+    <Widget src="jgodwill.near/widget/GenaDrop.Footer" />
+  </>
 );
