13c4787763f17e141e0fddc0adbbbb28d1f2d9d6
andyh.near <andyh.near>
2023-06-13 21:59:36 +0000

    Update CallbackRenderChild by andyh.near at 2023-06-13T21:59:36.604Z

--- andyh.near/CallbackRenderChild/source_code.jsx
+++ andyh.near/CallbackRenderChild/source_code.jsx
@@ -1,3 +1,3 @@
 console.log("[NSCOMP:CallbackRenderChild]");
-// return <div>{await props.renderComponent("i am the child")}</div>;
-return <div>i am the child</div>;
+return <div>{await props.renderComponent("i am the child")}</div>;
+// return <div>i am the child</div>;
