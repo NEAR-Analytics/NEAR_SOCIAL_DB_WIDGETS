28d7335a5174f8ff4e391155f4e01738ca7163d5
nearvietnamhub.near <nearvietnamhub.near>
2023-06-14 05:11:37 +0000

    Update nearvietnamhub.logo by nearvietnamhub.near at 2023-06-14T05:11:37.325Z

---
+++ nearvietnamhub.near/nearvietnamhub.logo/source_code.jsx
@@ -0,0 +1,15 @@
+return (
+  <>
+    <a
+      href="https://nearvietnamhub.org/"
+      title="Near Vietnam Hub"
+      target="_blank"
+    >
+      <img
+        class="image"
+        src="https://ipfs.near.social/ipfs/bafkreiaka3spk3f6bizas4472g52tl4v6ms3oykrkzso7uagjqlmd35fy4"
+        alt="Near Vietnam Hub"
+      />
+    </a>
+  </>
+);
