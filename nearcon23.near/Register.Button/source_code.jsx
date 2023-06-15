bde60d94dc13d5b9c54f142a096220dfcb28ea70
nearcon23.near <nearcon23.near>
2023-06-15 16:34:32 +0000

    Update Register.Button by nearcon23.near at 2023-06-15T16:34:32.812Z

---
+++ nearcon23.near/Register.Button/source_code.jsx
@@ -0,0 +1,36 @@
+const ownerId = "nearcon23.near";
+
+const Link = styled.a`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+  width: 10.625em;
+  gap: 0.5em;
+  border: 1px solid #00ec97;
+  border-radius: 50px;
+  background: #00ec97;
+  color: #11181c;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  text-align: center;
+  white-space: nowrap;
+  transition: all 0.3s ease-in-out;
+
+  &:hover,
+  &:focus,
+  &:active {
+    text-decoration: none;
+    background: #11181c;
+    border: 1px solid #11181c;
+    color: #fff;
+  }
+`;
+
+return (
+  <Link href={props.href ?? `/${ownerId}/widget/Index?tab=register`}>
+    Register
+  </Link>
+);
