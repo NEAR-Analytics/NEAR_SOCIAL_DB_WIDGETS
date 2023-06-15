f8b5680a17cab492923264556de9e7cc2250004e
nycdao.near <nycdao.near>
2023-06-15 16:16:06 +0000

    Update project.view by nycdao.near at 2023-06-15T16:16:06.965Z

--- nycdao.near/project.view/source_code.jsx
+++ nycdao.near/project.view/source_code.jsx
@@ -5,20 +5,24 @@ const externalLink = `https://social.near.page/${
   postType === "post" ? "p" : "c"
 }/${accountId}/${blockHeight}`;
 
-const clickbaitPrompt =
-  props.clickbaitPrompt ??
-  `Check out this ${postType} on @NearSocial_\n#NearSocial #NEAR #BOS\n${externalLink}`;
+const clickPrompt =
+  props.clickPrompt ?? `Check out this project on the #BOS!\n${externalLink}`;
 
 const twitterUrl = new URL("https://twitter.com/intent/tweet");
-twitterUrl.searchParams.set("text", clickbaitPrompt);
+twitterUrl.searchParams.set("text", clickPrompt);
 
-const nycProjects = JSON.parse(
-  Social.get(`${accountId}/project/nyc`, blockHeight) ?? "null"
+const mailtoUrl = new URL("mailto:");
+mailtoUrl.searchParams.set("subject", `Check out this project on the #BOS!`);
+mailtoUrl.searchParams.set(
+  "body",
+  `Take a look.
+${externalLink}
+`
 );
 
-if (nycProjects === null) {
-  return "Account does not have any NYC projects.";
-}
+const project = JSON.parse(
+  Social.get(`${accountId}/project/nyc`, blockHeight) ?? "null"
+);
 
 const H2 = styled.h2`
   font-size: 20px;
@@ -135,95 +139,55 @@ return (
         <div class="col">
           <div class="row">
             <div class="col">
-              <H2>
-                <a href={link}>{question.title}</a>
-                <i class="bi bi-arrow-right" />
-              </H2>
+              <H2>{question.title}</H2>
             </div>
             <div class="col-auto">
               <div class="dropdown ms-auto">
-                <Button
-                  className="btn me-1"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                  title="Share"
-                >
-                  <i className="bi fs-4 bi-share" />
-                </Button>
-                <ul class="dropdown-menu">
-                  <li>
-                    <Widget
-                      src="mob.near/widget/CopyButton"
-                      props={{
-                        text: externalLink,
-                        className: "btn btn-outline-dark dropdown-item",
-                        label: `Copy Link`,
-                      }}
-                    />
-                  </li>
-                  <li className="dropdown-item">
-                    <a
-                      className="link-dark text-decoration-none"
-                      href={mailtoUrl.toString()}
-                      target="_blank"
+                {blockHeight !== "now" && (
+                  <span>
+                    <Button
+                      className="btn me-1"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                      title="Share"
                     >
-                      <i className="bi bi-envelope-at" /> Email
-                    </a>
-                  </li>
-                  <li className="dropdown-item">
-                    <a
-                      className="link-dark text-decoration-none"
-                      href={twitterUrl.toString()}
-                      target="_blank"
-                    >
-                      <i className="bi bi-twitter" />
-                      Tweet
-                    </a>
-                  </li>
-                  <li>
-                    <Item>
-                      <Widget
-                        src="mob.near/widget/MainPage.Post.ShareButton"
-                        props={{ accountId, blockHeight, postType: "post" }}
-                      />
-                    </Item>
-                  </li>
-                  <li>
-                    <Item>
-                      {/* Delete widget */}
-                      <Widget
-                        src="dima_sheleg.near/widget/DevSupport.Question.Button.Delete"
-                        props={{
-                          accountId,
-                          blockHeight,
-                          admins,
-                          adminContract,
-                          className: "hide-btn",
-                          text: "Hide",
-                        }}
-                      />
-                    </Item>
-                  </li>
-                </ul>
+                      <i className="bi fs-4 bi-share" />
+                    </Button>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <Widget
+                          src="mob.near/widget/CopyButton"
+                          props={{
+                            text: externalLink,
+                            className: "btn btn-outline-dark dropdown-item",
+                            label: `Copy Link`,
+                          }}
+                        />
+                      </li>
+                      <li className="dropdown-item">
+                        <a
+                          className="link-dark text-decoration-none"
+                          href={mailtoUrl.toString()}
+                          target="_blank"
+                        >
+                          <i className="bi bi-envelope-at" /> Email
+                        </a>
+                      </li>
+                      <li className="dropdown-item">
+                        <a
+                          className="link-dark text-decoration-none"
+                          href={twitterUrl.toString()}
+                          target="_blank"
+                        >
+                          <i className="bi bi-twitter" />
+                          Tweet
+                        </a>
+                      </li>
+                    </ul>
+                  </span>
+                )}
               </div>
             </div>
-          </div>
-          <div class="row">
-            <H6>
-              <div class="d-flex">
-                <Trancate>{accountId}</Trancate>
-                &nbsp;in&nbsp;
-                <TopicName>
-                  <Widget
-                    src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.LabelsDisplay"
-                    props={{ labels: question.labels }}
-                  />
-                </TopicName>
-                &nbsp;&#8226;&nbsp;
-                <Widget src="mob.near/widget/TimeAgo" props={{ blockHeight }} />
-                &nbsp;ago
-              </div>
-            </H6>
           </div>
         </div>
       </div>
