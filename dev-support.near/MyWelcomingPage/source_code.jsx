ac5f48f997441a887c9d39112c242bb38cf75244
dev-support.near <dev-support.near>
2023-06-15 17:25:36 +0000

    Update MyWelcomingPage by dev-support.near at 2023-06-15T17:25:36.404Z

--- dev-support.near/MyWelcomingPage/source_code.jsx
+++ dev-support.near/MyWelcomingPage/source_code.jsx
@@ -1,7 +1,11 @@
+if (!context.accountId) {
+  return "Please login to use this application";
+}
+
 return (
   <>
     <div class="container">
-      <h1> This is my page </h1>
+      <h1> This is my page {context.accountId} </h1>
     </div>
   </>
 );
