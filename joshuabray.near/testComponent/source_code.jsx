971130642520e910f1f96d919c721a26b9a1138c
joshuabray.near <joshuabray.near>
2023-06-15 00:57:26 +0000

    Update testComponent by joshuabray.near at 2023-06-15T00:57:26.746Z

---
+++ joshuabray.near/testComponent/source_code.jsx
@@ -0,0 +1,11 @@
+let greeting = "Have a great day Joshua!";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <h1>Hello world... and {props.name}</h1>
+
+      <p> {greeting} </p>
+    </div>
+  </>
+);
