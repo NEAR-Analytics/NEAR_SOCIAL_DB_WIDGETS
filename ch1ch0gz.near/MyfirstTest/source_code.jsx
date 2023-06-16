577f5a07e3a8c911f96e17b73ac054096dffd607
ch1ch0gz.near <ch1ch0gz.near>
2023-06-16 14:55:35 +0000

    Update MyfirstTest by ch1ch0gz.near at 2023-06-16T14:55:35.420Z

---
+++ ch1ch0gz.near/MyfirstTest/source_code.jsx
@@ -0,0 +1,11 @@
+let greeting = "Have a great day BuildDAO";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <h1>Hi {props.name}</h1>
+
+      <p> {greeting} </p>
+    </div>
+  </>
+);
