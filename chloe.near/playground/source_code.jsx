51aac1a48cae22cb157de65f8759f7dfa701b7db
chloe.near <chloe.near>
2023-06-16 16:24:16 +0000

    Update playground by chloe.near at 2023-06-16T16:24:16.906Z

--- chloe.near/playground/source_code.jsx
+++ chloe.near/playground/source_code.jsx
@@ -1,3 +1,19 @@
+const StyledFetchButton = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+  border: 2px solid palevioletred;
+  border-radius: 10px;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+    background: pink;
+  }
+`;
+
 const fetchDataAndUpdateState = () => {
   asyncFetch("https://rpc.mainnet.near.org/status").then((res) => {
     const data = res.body;
@@ -14,7 +30,9 @@ const fetchDataAndUpdateState = () => {
 
 return (
   <div>
-    <button onClick={fetchDataAndUpdateState}>Fetch Data</button>
+    <StyledFetchButton onClick={fetchDataAndUpdateState}>
+      Fetch Data
+    </StyledFetchButton>
     <p>Block Height: {state.blockHeight}</p>
   </div>
 );
