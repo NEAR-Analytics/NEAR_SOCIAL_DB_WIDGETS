0e34f3bf22b5ad924138b7c0f247bfaa80bb84c8
rubycop.near <rubycop.near>
2023-06-16 19:14:09 +0000

    Update NDC.OG.MintSBT by rubycop.near at 2023-06-16T19:14:09.086Z

---
+++ rubycop.near/NDC.OG.MintSBT/source_code.jsx
@@ -0,0 +1,48 @@
+const conrtactId = props.conrtactId ?? "community-v1.i-am-human.testnet";
+
+const Container = styled.div`
+  padding: 30px 0;
+`;
+
+const InputField = styled.div`
+  margin: 20px 0;
+`;
+
+const PrimaryButton = styled.button`
+  padding: 8px 20px;
+  background: #FFD50D;
+  border-radius: 10px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 24px;
+  border: 0;
+`;
+
+State.init({
+  ogAccountId: "",
+});
+
+const handleMint = () => {
+  Near.call([
+    {
+      contractName: conrtactId,
+      methodName: "sbt_mint",
+      args: { receiver: ogAccountId },
+      gas: "70000000000000",
+      deposit: 0.007,
+    },
+  ]);
+};
+
+return (
+  <Container>
+    <h3>Mint SBT for selected OG member</h3>
+    <InputField>
+      <input
+        type="text"
+        onChange={(e) => State.update({ ogAccountId: e.target.value })}
+      />
+    </InputField>
+    <PrimaryButton onClick={handleMint}>Mint</PrimaryButton>
+  </Container>
+);
