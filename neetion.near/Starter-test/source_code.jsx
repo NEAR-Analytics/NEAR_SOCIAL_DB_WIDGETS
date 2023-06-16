b400aefd2c0c09733df90d9ade26d142c6d524fa
neetion.near <neetion.near>
2023-06-16 17:46:02 +0000

    Update Starter-test by neetion.near at 2023-06-16T17:46:02.388Z

--- neetion.near/Starter-test/source_code.jsx
+++ neetion.near/Starter-test/source_code.jsx
@@ -1,4 +1,32 @@
-const sender = Ethers.send("eth_requestAccounts", [])[0];
-if (!sender) return "Please login first";
+if (
+  state.chainId === undefined &&
+  ethers !== undefined &&
+  Ethers.send("eth_requestAccounts", [])[0]
+) {
+  Ethers.provider()
+    .getNetwork()
+    .then((chainIdData) => {
+      if (chainIdData?.chainId) {
+        State.update({ chainId: chainIdData.chainId });
+      }
+    });
+}
+if (state.chainId !== undefined && state.chainId !== 1) {
+  return <p>Switch to Ethereum Mainnet</p>;
+}
 
-return <p>Account: {sender}</p>;
+// FETCH LIDO ABI
+
+const lidoContract = "0xae7ab96520de3a18e5e111b5eaab095312d7fe84";
+const tokenDecimals = 18;
+
+const lidoAbi = fetch(
+  "https://raw.githubusercontent.com/lidofinance/lido-subgraph/master/abis/Lido.json"
+);
+if (!lidoAbi.ok) {
+  return "Loading";
+}
+
+const iface = new ethers.utils.Interface(lidoAbi.body);
+
+return <p>{JSON.stringify(iface)}</p>;
