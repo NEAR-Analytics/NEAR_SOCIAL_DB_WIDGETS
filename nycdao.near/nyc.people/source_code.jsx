cf6454f3571dc85f17d7166f096d0a921522930d
nycdao.near <nycdao.near>
2023-06-16 15:08:17 +0000

    Update nyc.people by nycdao.near at 2023-06-16T15:08:17.604Z

--- nycdao.near/nyc.people/source_code.jsx
+++ nycdao.near/nyc.people/source_code.jsx
@@ -12,17 +12,6 @@ return (
     <div className="m-2">
       <div className="tab-content">
         <h3>Community</h3>
-        <div className="mt-3 mb-3">
-          <Widget
-            src="near/widget/DIG.Button"
-            props={{
-              href: "#/near/widget/ProfileEditor",
-              label: "Edit Your Profile",
-              variant: "outline-danger",
-              size: "small",
-            }}
-          />
-        </div>
         <div className="tab-pane fade in show active" role="tabpanel">
           <Widget
             src="mob.near/widget/FollowersList"
