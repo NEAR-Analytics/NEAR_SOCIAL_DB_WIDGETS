6dbc2bd70e8122b51a3bcad9b07c065ebad20149
chloe.near <chloe.near>
2023-06-16 16:24:31 +0000

    Update blockheight by chloe.near at 2023-06-16T16:24:31.383Z

--- chloe.near/blockheight/source_code.jsx
+++ chloe.near/blockheight/source_code.jsx
@@ -1,26 +1,38 @@
-// Initialize the state
-State.init({
-  blockHeight: 0,
-});
-
-// Function to retrieve block height and store it in state
-const getBlockHeight = () => {
-  try {
-    const blockData = Near.block("final");
-    const blockHeight = blockData.chunks[0].height_included;
-    // Store the block height in the state
-    State.update({ blockHeight: blockHeight });
-  } catch (error) {
-    console.log("Failed to fetch block height:", error);
+const StyledFetchButton = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+  border: 2px solid palevioletred;
+  border-radius: 10px;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+    background: pink;
   }
+`;
+
+const fetchDataAndUpdateState = () => {
+  asyncFetch("https://rpc.mainnet.near.org/status").then((res) => {
+    const data = res.body;
+    if (res.ok) {
+      State.update({
+        blockHeight: data.sync_info.latest_block_height,
+        data: data,
+      });
+    } else {
+      console.error("Error fetching data: ", data.error);
+    }
+  });
 };
 
-// Fetch block height initially
-getBlockHeight();
-
-// Return block height
 return (
   <div>
-    <span class="text-decoration-underline">{state.blockHeight}</span>
+    <StyledFetchButton onClick={fetchDataAndUpdateState}>
+      Fetch Data
+    </StyledFetchButton>
+    <p>Block Height: {state.blockHeight}</p>
   </div>
 );
