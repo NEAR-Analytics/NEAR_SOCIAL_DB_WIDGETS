336927e7696c22f51fb07392d4db7315d5060785
y3k.near <y3k.near>
2023-06-16 14:32:47 +0000

    Update widgets.statsOverview by y3k.near at 2023-06-16T14:32:47.767Z

---
+++ y3k.near/widgets.statsOverview/source_code.jsx
@@ -0,0 +1,21 @@
+return (
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <Widget src="y3k.near/widget/widgets.dailyCommitStats" props={{}} />
+      </div>
+      <div class="col">
+        <Widget src="y3k.near/widget/widgets.dailyDevStats" props={{}} />
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <Widget src="y3k.near/widget/widgets.monthlyCommitStats" props={{}} />
+      </div>
+      <div class="col">
+        <Widget src="y3k.near/widget/widgets.monthlyDevStats" props={{}} />
+      </div>
+    </div>
+  </div>
+);
