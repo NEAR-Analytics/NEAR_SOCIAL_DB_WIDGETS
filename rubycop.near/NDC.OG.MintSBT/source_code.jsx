eae8d07d701be3906bd4976167addd812a7c2cd9
rubycop.near <rubycop.near>
2023-06-16 19:44:15 +0000

    Update NDC.OG.MintSBT by rubycop.near at 2023-06-16T19:44:15.499Z

--- rubycop.near/NDC.OG.MintSBT/source_code.jsx
+++ rubycop.near/NDC.OG.MintSBT/source_code.jsx
@@ -27,7 +27,7 @@ const handleMint = () => {
     {
       contractName: conrtactId,
       methodName: "sbt_mint",
-      args: { receiver: ogAccountId },
+      args: { receiver: state.ogAccountId },
       gas: "70000000000000",
       deposit: 7000000000000000000000,
     },
