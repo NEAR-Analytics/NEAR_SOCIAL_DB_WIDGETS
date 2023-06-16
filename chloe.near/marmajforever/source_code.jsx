b6d9e53532723de2ece8ff1a67bd49ba37c77122
chloe.near <chloe.near>
2023-06-16 19:55:45 +0000

    Update marmajforever by chloe.near at 2023-06-16T19:55:45.456Z

--- chloe.near/marmajforever/source_code.jsx
+++ chloe.near/marmajforever/source_code.jsx
@@ -1,3 +1,16 @@
+const accountId = props.accountId || context.accountId;
+
+if (!accountId) {
+  return (
+    <div>
+      <p>Please connect your NEAR wallet or create a new one:</p>
+      <a href="https://near.org/signup" target="_blank" rel="noreferrer">
+        <FakeButton>Create NEAR Wallet</FakeButton>
+      </a>
+    </div>
+  );
+}
+
 const GameContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -7,6 +20,8 @@ const GameContainer = styled.div`
   background: #282c34;
   color: white;
   font-size: calc(10px + 2vmin);
+  border: 2px solid white; /* Added border */
+  margin: 20px; /* Added margin */
 `;
 
 const GameButton = styled.button`
@@ -25,28 +40,34 @@ const GameButton = styled.button`
 
 const Title = styled.h1`
   font-weight: bold;
+  padding-top: 10px; // Added top padding
 `;
 
 const Info = styled.p`
   margin-top: 1em;
 `;
 
-State.init({
-  blockHeight: 0,
-  clickCount: 0,
-  love: 0,
-  bossHP: 100,
-  bossLevel: 1,
-  gameStarted: false,
-});
+function initGameState() {
+  return {
+    clickCount: 0,
+    love: 0,
+    bossHP: 100,
+    bossLevel: 1,
+    boostLevel: 1,
+    totalLove: 0,
+    claimLoveCounter: 0,
+    gameStarted: false,
+  };
+}
+
+State.init(initGameState());
 
 function startGame() {
   asyncFetch("https://rpc.mainnet.near.org/status").then((res) => {
     const data = res.body;
     if (res.ok) {
-      let newBlockHeight = data.sync_info.latest_block_height;
       State.update({
-        blockHeight: newBlockHeight,
+        ...initGameState(), // Reset game state
         gameStarted: true,
       });
     } else {
@@ -55,57 +76,141 @@ function startGame() {
   });
 }
 
-function fetchDataAndUpdateState() {
+function claimLove() {
   const oldBlockHeight = state.blockHeight;
   const oldLove = state.love;
-  const oldClickCount = state.clickCount;
-  let bossHP = state.bossHP;
-  let bossLevel = state.bossLevel;
+  const boostLevel = state.boostLevel;
+  let totalLove = state.totalLove;
+  let claimLoveCounter = state.claimLoveCounter;
 
   asyncFetch("https://rpc.mainnet.near.org/status").then((res) => {
     const data = res.body;
     if (res.ok) {
-      let newBlockHeight = data.sync_info.latest_block_height;
-      let love = oldLove;
-      let clickCount = oldClickCount;
+      const newBlockHeight = data.sync_info.latest_block_height;
       if (newBlockHeight > oldBlockHeight) {
-        love += newBlockHeight - oldBlockHeight;
-        clickCount++;
-      }
+        const blockDifference = newBlockHeight - oldBlockHeight;
+        const love = oldLove + blockDifference * boostLevel; // Increase love based on block difference * boost level
 
-      bossHP -= love;
-      love = 0;
-      if (bossHP <= 0) {
-        bossHP = 100;
-        bossLevel++;
-      }
+        // Update totalLove
+        totalLove += blockDifference * boostLevel;
+        claimLoveCounter++;
 
-      State.update({
-        blockHeight: newBlockHeight,
-        clickCount: clickCount,
-        love: love,
-        bossHP: bossHP,
-        bossLevel: bossLevel,
-      });
+        State.update({
+          blockHeight: newBlockHeight,
+          love: love,
+          totalLove: totalLove,
+          claimLoveCounter: claimLoveCounter,
+        });
+      } else {
+        State.update({ blockHeight: newBlockHeight });
+      }
     } else {
       console.error("Error fetching data: ", data.error);
     }
   });
 }
 
+function spreadLove() {
+  const love = state.totalLove; // Use totalLove to spread love
+  let bossHP = state.bossHP;
+  let bossLevel = state.bossLevel;
+  const clickCount = state.clickCount + 1;
+
+  bossHP -= love; // Decrease boss HP by love value
+
+  if (bossHP <= 0) {
+    bossLevel++;
+    bossHP = 100 * bossLevel + clickCount * 2;
+  }
+
+  const difference = state.love - state.totalLove; // Calculate the difference between love and totalLove
+  const totalLove = difference > 0 ? state.totalLove + difference : 0; // Add the difference back to totalLove (minimum 0)
+
+  State.update({
+    bossHP: bossHP,
+    bossLevel: bossLevel,
+    clickCount: clickCount,
+    totalLove: totalLove, // Update totalLove with the new value
+    love: 0, // Reset love to 0
+    claimLoveCounter: 0, // Reset claimLoveCounter to 0
+  });
+}
+
+function boostLove() {
+  const clickCount = state.clickCount + 1;
+  const updatedLove = state.love * clickCount;
+  const updatedBoostLevel = state.boostLevel + 1;
+
+  State.update({
+    love: updatedLove,
+    boostLevel: updatedBoostLevel,
+    clickCount: clickCount,
+  });
+}
+
+function newGame() {
+  State.update(initGameState()); // Reset the game state
+}
+
 return (
   <GameContainer>
-    <Title>Block Battle!</Title>
-    <Info>Block Height: {state.blockHeight}</Info>
-    <Info>Times Attacked: {state.clickCount}</Info>
-    <Info>Love: {state.love}</Info>
-    <Info>Boss HP: {state.bossHP}</Info>
-    <Info>Boss Level: {state.bossLevel}</Info>
+    <Title>Marma J Forever!</Title>
     {!state.gameStarted && (
-      <GameButton onClick={startGame}>Start Game</GameButton>
+      <>
+        <Info>Player ID: {accountId}</Info> {/* Display Player ID */}
+        <GameButton onClick={startGame}>Start Game</GameButton>
+      </>
     )}
     {state.gameStarted && (
-      <GameButton onClick={fetchDataAndUpdateState}>Attack!</GameButton>
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <div>
+            <Info>Total Love: {state.totalLove}</Info>
+            <Info>Boost Level: {state.boostLevel}</Info>{" "}
+            {/* Display Boost Level */}
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <GameButton onClick={claimLove}>Claim Love</GameButton>
+              <GameButton onClick={boostLove}>Boost Love</GameButton>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-end",
+            }}
+          >
+            <Info>Player ID: {accountId}</Info> {/* Display Player ID */}
+            <Info>Boss Level: {state.bossLevel}</Info>{" "}
+            {/* Display Boss Level */}
+            <Info>Boss HP: {state.bossHP}</Info> {/* Display Boss HP */}
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <GameButton
+                onClick={state.claimLoveCounter > 0 ? spreadLove : undefined}
+                disabled={state.claimLoveCounter === 0}
+              >
+                Spread the Love
+              </GameButton>
+              <GameButton onClick={newGame}>New Game</GameButton>
+            </div>
+          </div>
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          {" "}
+          {/* Added margin */}
+          <p>Rules:</p>
+          <p>1. Claim love</p>
+          <p>2. Spread it to attack the boss</p>
+          <p>3. Boost to generate love faster</p>
+          <p>4. New game to reset</p>
+        </div>
+      </div>
     )}
   </GameContainer>
 );
