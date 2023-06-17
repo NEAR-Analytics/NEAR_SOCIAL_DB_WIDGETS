694573d214dd163f87a28a0ce2dd03662c5d4b99
minorityprogrammers.near <minorityprogrammers.near>
2023-06-17 21:34:01 +0000

    Update canny.post.labels by minorityprogrammers.near at 2023-06-17T21:34:01.423Z

---
+++ minorityprogrammers.near/canny.post.labels/source_code.jsx
@@ -0,0 +1,11 @@
+return props.labels ? (
+  <div class="card-title">
+    {props.labels.map((label) => {
+      return (
+        <a href="#">
+          <span class="badge p-0 pe-2 text-primary">{label}</span>
+        </a> // need to add filter logic to routes so it filters backlog feed with tag // try adding to prop logic in features
+      );
+    })}
+  </div>
+) : null;
