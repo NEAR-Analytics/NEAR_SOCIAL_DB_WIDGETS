d3c55313d97ab7032c204f401d1900fb71c3a597
hack.near <hack.near>
2023-06-18 22:32:50 +0000

    Update dao.proposal by hack.near at 2023-06-18T22:32:50.332Z

--- hack.near/dao.proposal/source_code.jsx
+++ hack.near/dao.proposal/source_code.jsx
@@ -8,14 +8,12 @@ const bgClassname =
     : "bg-danger bg-opacity-10";
 
 return (
-  <div className={`border p-2 ${bgClassname}`}>
-    <div className="mb-2">
-      <div>
-        <Widget
-          src="hack.near/widget/profile.human"
-          props={{ accountId: proposal.proposer }}
-        />
-      </div>
+  <div className="mb-2">
+    <div>
+      <Widget
+        src="hack.near/widget/profile.human"
+        props={{ accountId: proposal.proposer }}
+      />
     </div>
   </div>
 );
