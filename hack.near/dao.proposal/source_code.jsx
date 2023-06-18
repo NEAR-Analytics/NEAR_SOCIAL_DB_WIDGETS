6d4abd3191652e3c90af22059c78ef13ca77bfcb
hack.near <hack.near>
2023-06-18 22:25:15 +0000

    Update dao.proposal by hack.near at 2023-06-18T22:25:15.076Z

---
+++ hack.near/dao.proposal/source_code.jsx
@@ -0,0 +1,26 @@
+const { policy, proposal } = props;
+const daoId = "rc-dao.sputnik-dao.near";
+const bgClassname =
+  proposal.status === "InProgress"
+    ? ""
+    : proposal.status === "Approved"
+    ? "bg-success bg-opacity-10"
+    : "bg-danger bg-opacity-10";
+
+return (
+  <div className={`border p-2 ${bgClassname}`}>
+    <div className="mb-2">
+      <div className="float-end">
+        {new Date(parseFloat(proposal.submission_time) / 1e6).toLocaleString()}
+      </div>
+    </div>
+    <div className="mb-2">
+      <div>
+        <Widget
+          src="hack.near/widget/profile.human"
+          props={{ accountId: proposal.proposer }}
+        />
+      </div>
+    </div>
+  </div>
+);
