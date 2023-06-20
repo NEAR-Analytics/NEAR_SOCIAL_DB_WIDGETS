bcf57841fb27681f3076de5f7a9c3fe5ea27ec37
efiz.near <efiz.near>
2023-06-20 12:51:24 +0000

    Update every.array.build by efiz.near at 2023-06-20T12:51:24.984Z

--- efiz.near/every.array.build/source_code.jsx
+++ efiz.near/every.array.build/source_code.jsx
@@ -1,4 +1,5 @@
 const item = props.item;
+const onChange = props.onChange;
 
 const Input = styled.input`
 `;
@@ -11,8 +12,15 @@ const Container = styled.div`
     flex-direction: column;
 `;
 
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+`;
+
 State.init({
-  inputValues: [""],
+  inputValues: [],
+  newInputValue: "",
 });
 
 const handleInputChange = (index, value) => {
@@ -22,19 +30,55 @@ const handleInputChange = (index, value) => {
 };
 
 const handleAddClick = () => {
-  State.update({ inputValues: [...state.inputValues, ""] });
+  if (state.newInputValue.trim() === "") return;
+
+  const newInputValues = [...state.inputValues, state.newInputValue];
+
+  State.update({
+    inputValues: newInputValues,
+    newInputValue: "",
+  });
+
+  if (onChange) {
+    onChange(newInputValues);
+  }
+};
+
+const handleDeleteClick = (index) => {
+  const newInputValues = [...state.inputValues];
+  newInputValues.splice(index, 1);
+  State.update({ inputValues: newInputValues });
+  if (onChange) {
+    onChange(newInputValues);
+  }
 };
 
 return (
   <Container>
     {state.inputValues?.map((inputValue, index) => (
-      <Input
-        key={index}
-        onChange={(e) => handleInputChange(index, e.target.value)}
-        value={inputValue}
-        placeholder={item.name}
-      />
+      <Row>
+        <Input
+          key={index}
+          onChange={(e) => handleInputChange(index, e.target.value)}
+          value={inputValue}
+          placeholder={item.name}
+        />
+        <Button onClick={() => handleDeleteClick(index)}>Delete</Button>
+      </Row>
     ))}
-    <Button onClick={handleAddClick}>Add</Button>
+    <Row>
+      <Input
+        type="text"
+        placeholder=""
+        value={state.newInputValue}
+        onChange={(e) => State.update({ newInputValue: e.target.value })}
+      />
+      <Button
+        onClick={handleAddClick}
+        disabled={state.newInputValue.trim() === ""}
+      >
+        Add
+      </Button>
+    </Row>
   </Container>
 );
