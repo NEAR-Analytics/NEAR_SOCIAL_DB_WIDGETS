04052c232e7559affffed6ef6f5a6a3ae4273cc6
efiz.near <efiz.near>
2023-06-20 21:37:40 +0000

    Update creator by efiz.near at 2023-06-20T21:37:40.054Z

--- efiz.near/creator/source_code.jsx
+++ efiz.near/creator/source_code.jsx
@@ -1,24 +1,133 @@
+const Container = styled.div`
+    display: flex;
+    height: 100%;
+  `;
+
+const SidePanel = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 20px;
+    background-color: #f2f2f2;
+    width: 300px;
+    z-index: 50;
+  `;
+
+const MainContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    padding: 20px;
+  `;
+
+const Header = styled.h2`
+    margin-bottom: 20px;
+  `;
+
+const Footer = styled.div`
+    margin-top: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+  `;
+
 const Button = styled.button`
+    padding: 10px 20px;
+    background-color: #5fba7d;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    width: 100%;
+  `;
+
+const LeftPanelItem = styled.div`
+    padding: 8px;
+    background-color: #ccc;
+    color: white;
+    border-radius: 4px;
+  `;
+
+const Select = styled.select`
+  `;
+
+const Label = styled.label`
 `;
 
-function Content() {
-  switch (state.show) {
-    case "THING":
-      return <Widget src="efiz.near/widget/every.thing.create" props={{}} />;
+const Input = styled.input`
+  `;
 
-    case "TYPE":
-      return <Widget src="efiz.near/widget/every.type.create" props={{}} />;
+State.init({
+  data: {},
+});
 
-    case "POST":
-      return <Widget src="every.near/widget/every.post.create" props={{}} />;
-  }
+const handleOnChange = (value) => {
+  State.update({ data: { ...state.data, ...value } });
+};
+
+const handleApply = () => {
+  State.update({
+    config: state.data,
+    template: state.templateVal,
+  });
+  // set the props for the main content
+};
+
+const handleSave = () => {
+  // create the thing
+};
+
+let availableTypes = [];
+const types = Social.get("efiz.near/type/**", "final");
+if (types !== null) {
+  availableTypes =
+    Object.keys(types)?.map((it) => `efiz.near/type/${it}`) || [];
 }
 
+const handleTypeChange = (e) => {
+  State.update({ selectedType: e.target.value });
+};
+
 return (
-  <>
-    <Button onClick={() => State.update({ show: "THING" })}>thing</Button>
-    <Button onClick={() => State.update({ show: "TYPE" })}>type</Button>
-    <Button onClick={() => State.update({ show: "POST" })}>post</Button>
-    <Content />
-  </>
+  <Container>
+    <SidePanel>
+      <Label>Type</Label>
+      <Select value={state.selectedType} onChange={handleTypeChange}>
+        <option value="">Select a type</option>
+        {availableTypes?.map((it) => (
+          <option value={it} key={it}>
+            {it}
+          </option>
+        ))}
+      </Select>
+      <Label>Template</Label>
+      <Input
+        value={state.templateVal}
+        onChange={(e) => State.update({ templateVal: e.target.value })}
+      />
+      <Widget
+        src="efiz.near/widget/create"
+        props={{
+          item: {
+            type: state.selectedType,
+            value: state.data,
+          },
+          onChange: handleOnChange,
+        }}
+      />
+      <Footer>
+        <Button onClick={() => handleApply()}>apply</Button>
+        <Button onClick={() => handleSave()} disabled={true}>
+          save
+        </Button>
+      </Footer>
+    </SidePanel>
+    <MainContent>
+      <Header></Header>
+      <Widget
+        src="efiz.near/widget/Every.Thing.View"
+        props={{ path: state.template, data: state.config }}
+      />
+    </MainContent>
+  </Container>
 );
