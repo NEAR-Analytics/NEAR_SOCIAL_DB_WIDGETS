49ac6f72e4fa1a2827eb05b001a42f8526450634
nearefi.near <nearefi.near>
2023-06-20 12:53:47 +0000

    Update SupportReFI by nearefi.near at 2023-06-20T12:53:47.524Z

--- nearefi.near/SupportReFI/source_code.jsx
+++ nearefi.near/SupportReFI/source_code.jsx
@@ -7,7 +7,15 @@ const data = Social.keys(`${accountId}/post/refi`, "final", {
 if (!data) {
   return "Loading";
 }
-
+const getFirstSBTToken = () => {
+  const view = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
+    account: `${context.accountId}`,
+    issuer: "fractal.i-am-human.near",
+  });
+  return view?.[0]?.[1]?.[0];
+};
+const hasSBTToken = getFirstSBTToken() !== undefined;
+console.log("Has SBT Token: " + hasSBTToken);
 const processData = (data) => {
   const accounts = Object.entries(data);
 
@@ -37,35 +45,67 @@ if (JSON.stringify(data) !== JSON.stringify(state.data || {})) {
 
 return (
   <div>
-    <h1>🌸 Support ReFI on NEAR</h1>
-    <h3>Show on chain that you support ReFi on NEAR!</h3>
-    <CommitButton
-      data={{ post: { refi: "I Support ReFI on NEAR!" } }}
-      onCommit={() => {
-        State.update({
-          // TODO: Feed needs reload?
-        });
-      }}
-    >
-      Show Support ❤️
-    </CommitButton>
+    {hasSBTToken == true ? (
+      <div>
+        <h1>🌸 Support ReFI on NEAR</h1>
+        <h3>Show on chain that you support ReFi on NEAR!</h3>
+        <CommitButton
+          data={{ post: { refi: "I Support ReFI on NEAR!" } }}
+          onCommit={() => {
+            State.update({
+              // TODO: Feed needs reload?
+            });
+          }}
+        >
+          Show Support ❤️
+        </CommitButton>
 
-    <br />
-    <div>
-      {state.allItems
-        ? state.allItems.map(({ accountId }) => (
-            <div>
-              I support ReFi
-              <Widget
-                src="miraclx.near/widget/Attribution"
-                props={{
-                  dep: true,
-                  authors: [accountId],
-                }}
-              />
-            </div>
-          ))
-        : "Loading..."}
-    </div>
+        <br />
+        <div>
+          {state.allItems
+            ? state.allItems.map(({ accountId }) => (
+                <div>
+                  I support ReFi! 💨
+                  <Widget
+                    src="miraclx.near/widget/Attribution"
+                    props={{
+                      dep: true,
+                      authors: [accountId],
+                    }}
+                  />
+                </div>
+              ))
+            : "Loading with love ..."}
+        </div>
+      </div>
+    ) : (
+      hasSBTToken == false && (
+        <div
+          className="px-4"
+          style={{
+            backgroundColor: "white",
+            margin: "2rem auto 0 auto",
+            width: "100%",
+          }}
+        >
+          <div style={{ padding: "2rem" }}>
+            <h2
+              style={{
+                fontWeight: "700",
+              }}
+            >
+              Get Verified As a Human
+            </h2>
+            <p>
+              Go on{" "}
+              <a href="https://nearefi.org/human" target="_blank">
+                i-am-human.app
+              </a>{" "}
+              and get ✅ faceverified as a human
+            </p>
+          </div>
+        </div>
+      )
+    )}
   </div>
 );
