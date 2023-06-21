3a361bd7b4085b14864f47636dd8c83519446bc3
hack.near <hack.near>
2023-06-21 04:50:45 +0000

    Update event.create by hack.near at 2023-06-21T04:50:45.640Z

---
+++ hack.near/event.create/source_code.jsx
@@ -0,0 +1,121 @@
+const accountId = context.accountId;
+if (!accountId) {
+  return "Please connect your NEAR account :)";
+}
+
+const today = Math.floor((Date.now() + 0) / 86400000) * 86400000;
+const tomorrow = today + 86400000;
+
+State.init({
+  participants: "",
+  date: new Date(tomorrow).toISOString().substring(0, 10),
+  time,
+});
+
+return (
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-6">
+        <h3>
+          <b>create your own event page</b>
+        </h3>
+        <br />
+        <div className="mb-2">
+          <CommitButton data={{ event: state.event }}>save</CommitButton>
+          <a
+            className="btn btn-outline-primary ms-2"
+            href={`#/mob.near/widget/eventPage?accountId=${accountId}`}
+          >
+            view
+          </a>
+          <a
+            className="btn btn-outline-primary ms-2"
+            href={`#/mob.near/widget/eventPage?accountId=${accountId}`}
+          >
+            propose
+          </a>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-6 mb-2">
+            <h5>date</h5>
+            <input
+              type="date"
+              value={state.deadline}
+              onChange={(e) => {
+                State.update({ deadline: e.target.value });
+              }}
+            />
+            {state.error_deadline && <Error>{state.error_deadline}</Error>}
+          </div>
+          <div className="col-6 mb-2">
+            <h5>time</h5>
+            <input
+              type="time"
+              value={state.deadline}
+              onChange={(e) => {
+                State.update({ deadline: e.target.value });
+              }}
+            />
+            {state.error_deadline && <Error>{state.error_deadline}</Error>}
+          </div>
+        </div>
+
+        <div className="mb-3">
+          <h5 className="mt-3">metadata</h5>
+          <Widget
+            src="near/widget/MetadataEditor"
+            props={{
+              initialMetadata: event,
+              onChange: (event) => State.update({ event }),
+              options: {
+                name: { label: "title" },
+                image: { label: "logo" },
+                backgroundImage: { label: "image" },
+                description: { label: "description" },
+                tags: {
+                  label: "tags",
+                  tagsPattern: "*/community/*/tags/*",
+                  placeholder:
+                    "rust, engineer, artist, humanguild, nft, learner, founder",
+                },
+                linktree: {
+                  links: [
+                    {
+                      label: "Twitter",
+                      prefix: "https://twitter.com/",
+                      name: "twitter",
+                    },
+                    {
+                      label: "Github",
+                      prefix: "https://github.com/",
+                      name: "github",
+                    },
+                    {
+                      label: "Telegram",
+                      prefix: "https://t.me/",
+                      name: "telegram",
+                    },
+                    {
+                      label: "Website",
+                      prefix: "https://",
+                      name: "website",
+                    },
+                  ],
+                },
+              },
+            }}
+          />
+        </div>
+      </div>
+      <div className="col-lg-6">
+        <div>
+          <Widget
+            src="hack.near/widget/event.view"
+            props={{ accountId, event: state.event }}
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);
