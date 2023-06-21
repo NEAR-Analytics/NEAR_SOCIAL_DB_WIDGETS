13ff5de2c5f87782c9133a5680309f507194ed50
banyanq2.near <banyanq2.near>
2023-06-21 14:56:55 +0000

    Update dashboard by banyanq2.near at 2023-06-21T14:56:55.319Z

---
+++ banyanq2.near/dashboard/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <div>
+    <Widget
+      src="devs.near/widget/dev.rank"
+      props={{
+        ownerId: "banyanq2.near",
+      }}
+    />
+  </div>
+);
+// need to add profile tracker
