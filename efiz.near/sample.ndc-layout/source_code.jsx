9f0b0151a51e4f660c234e48f2ecd4ba67e61be2
efiz.near <efiz.near>
2023-06-22 19:01:40 +0000

    Update sample.ndc-layout by efiz.near at 2023-06-22T19:01:40.720Z

---
+++ efiz.near/sample.ndc-layout/source_code.jsx
@@ -0,0 +1,78 @@
+const data = props.data;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  background-color: {data.container.backgroundColor};
+  `;
+
+const Header = styled.div`
+  height: {data.header.height};
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: {data.header.backgroundColor};
+`;
+
+const Logo = styled.img`
+  height: 100%
+`;
+
+const Navigation = styled.div`
+  height: 20px;
+  width: 100%;
+  display: flex;
+`;
+
+const Button = styled.button`
+  display: flex;
+  background-color: #FFD50D;
+  padding: 8px 20px;
+  border-radius: 10px;
+  border: none;
+  gap: 10px;
+`;
+
+function Content() {
+  if (state.thingSrc) {
+    return (
+      <>
+        <Widget
+          src={"efiz.near/widget/Every.Thing.View"}
+          props={{ path: state.thingSrc, ...state.p }}
+        />
+      </>
+    );
+  }
+}
+
+return (
+  <Container>
+    <Widget
+      src="efiz.near/widget/marquee"
+      props={{
+        text: data.marquee.text,
+        fontFamily: data.marquee.fontFamily,
+        fontSize: data.marquee.fontSize,
+        textColor: data.marquee.textColor,
+        backgroundColor: data.marquee.backgroundColor,
+        height: data.marquee.height,
+        width: data.marquee.width,
+      }}
+    />
+    <Header>
+      <Logo src={data.logoSrc} />
+    </Header>
+    <Navigation>
+      {data &&
+        data.views?.map((view) => (
+          <Button onClick={() => State.update({ thingSrc: view.src })}>
+            {view.name}
+          </Button>
+        ))}
+    </Navigation>
+    <Content />
+  </Container>
+);
