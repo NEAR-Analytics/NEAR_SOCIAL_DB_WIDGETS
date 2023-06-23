025c9ee56ed9721d9a60029d969672884a2c7d73
rubycop.near <rubycop.near>
2023-06-23 23:02:50 +0000

    Update Kudos.Main by rubycop.near at 2023-06-23T23:02:50.513Z

--- rubycop.near/Kudos.Main/source_code.jsx
+++ rubycop.near/Kudos.Main/source_code.jsx
@@ -1,30 +1,29 @@
-const mockData = [
-  {
-    accountId: "rubycop.near",
-    votes: 5,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quam enim, dignissim sed ante at, convallis maximus enim.",
-    tags: ["ndc", "near"],
-    createdAt: new Date().getTime(),
-  },
-  {
-    accountId: "test.near",
-    votes: 15,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quam enim, dignissim sed ante at, convallis maximus enim.",
-    tags: ["near"],
-    createdAt: new Date().getTime(),
-  },
-];
+let data = Social.index("kudo", "answer");
+if (!data) {
+  return "Loading answers";
+}
+const upvotes = Social.index("kudo", "upvote");
+if (!upvotes) {
+  return "Loading upvotes";
+}
+const blackList = ["webuidl.near"];
+data = data.filter((d) => !blackList.includes(d.accountId));
 
-State.init({
-  kudos: mockData,
-});
+let upvotesMap = {};
+for (let i = 0; i < upvotes.length; i++) {
+  const vote = upvotes[i];
+  const upvoteBlockHeight = vote.value.blockHeight;
+  if (!upvotesMap[upvoteBlockHeight]) {
+    upvotesMap[upvoteBlockHeight] = 0;
+  }
+  upvotesMap[upvoteBlockHeight] += 1;
+}
 
-const fetchKudos = () => {
-  const items = Near.view(conrtactName, contractMethod);
-  State.update({ kudos: items });
-};
+// moving into contract in future
+// const fetchKudos = () => {
+//   const items = Near.view(conrtactName, contractMethod);
+//   State.update({ kudos: items });
+// };
 
 const widgets = {
   header: "rubycop.near/widget/Kudos.Header",
@@ -49,9 +48,21 @@ return (
     <Container>
       <Widget src={widgets.filter} />
     </Container>
-    <Container>
-      {state.kudos.map((kudo, index) => (
-        <Widget key={index} src={widgets.item} props={{ kudo }} />
+    <Container className="d-grid gap-3">
+      {data.map((kudo, index) => (
+        <Widget
+          key={index}
+          src={widgets.item}
+          props={{
+            accountId: kudo.accountId,
+            description: kudo.value.answer,
+            upvotes: upvotesMap[kudo.blockHeight]
+              ? upvotesMap[kudo.blockHeight]
+              : 0,
+            tags: kudo.tags ?? [],
+            createdAt: kudo.createdAt ?? 1687561302337,
+          }}
+        />
       ))}
     </Container>
   </>
