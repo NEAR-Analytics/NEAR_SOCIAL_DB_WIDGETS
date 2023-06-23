ea4ea8afc5753c1cac08e49d33ce9b838d108ac2
rubycop.near <rubycop.near>
2023-06-23 13:31:50 +0000

    Update NDC.StyledComponents by rubycop.near at 2023-06-23T13:31:50.183Z

--- rubycop.near/NDC.StyledComponents/source_code.jsx
+++ rubycop.near/NDC.StyledComponents/source_code.jsx
@@ -8,12 +8,21 @@ const StyledPrimaryButton = styled.button`
   font-weight: 500;
   line-height: 24px;
   border: 0;
+
+  i {
+    margin: 0 0 0 5px;
+    &:before { vertical-align: -0.2rem; }
+  }
 `;
 
 if (PrimaryButton)
   return (
-    <StyledPrimaryButton onClick={PrimaryButton.onClick}>
-      {PrimaryButton.text}
+    <StyledPrimaryButton
+      className="align-items-center d-flex"
+      onClick={PrimaryButton.onClick}
+    >
+      <div>{PrimaryButton.text}</div>
+      {PrimaryButton.icon && <div className="fs-5">{PrimaryButton.icon}</div>}
     </StyledPrimaryButton>
   );
 
