5d8dc86db07970b5fcda5eac26e067e508b82dd7
rubycop.near <rubycop.near>
2023-06-23 14:13:51 +0000

    Update NDC.StyledComponents by rubycop.near at 2023-06-23T14:13:51.399Z

--- rubycop.near/NDC.StyledComponents/source_code.jsx
+++ rubycop.near/NDC.StyledComponents/source_code.jsx
@@ -1,13 +1,21 @@
-const { PrimaryButton } = props;
+const { Button } = props;
 
-const StyledPrimaryButton = styled.button`
+const StyledButton = styled.button`
   padding: 8px 20px;
-  background: #FFD50D;
   border-radius: 10px;
   font-size: 14px;
   font-weight: 500;
   line-height: 24px;
   border: 0;
+
+  &.primary {
+    background: #FFD50D;
+  }
+
+  &.secondary {
+    background: transparent;
+    border: 1px solid #FFD50D;
+  }
 
   i {
     margin: 0 0 0 5px;
@@ -15,16 +23,16 @@ const StyledPrimaryButton = styled.button`
   }
 `;
 
-if (PrimaryButton)
+if (Button)
   return (
-    <StyledPrimaryButton
-      className="align-items-center d-flex"
-      onClick={PrimaryButton.onClick}
-      disabled={PrimaryButton.disabled}
+    <StyledButton
+      className={`align-items-center d-flex ${Button.type}`}
+      onClick={Button.onClick}
+      disabled={Button.disabled}
     >
-      <div>{PrimaryButton.text}</div>
-      {PrimaryButton.icon && <div className="fs-5">{PrimaryButton.icon}</div>}
-    </StyledPrimaryButton>
+      <div>{Button.text}</div>
+      {Button.icon && <div className="fs-5">{Button.icon}</div>}
+    </StyledButton>
   );
 
 return "";
