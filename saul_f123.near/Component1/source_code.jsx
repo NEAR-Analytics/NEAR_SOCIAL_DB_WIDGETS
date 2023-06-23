9884c145660a4f129923b2969f2d5b3b3408595a
saul_f123.near <saul_f123.near>
2023-06-23 00:41:49 +0000

    Update Component1 by saul_f123.near at 2023-06-23T00:41:49.426Z

---
+++ saul_f123.near/Component1/source_code.jsx
@@ -0,0 +1,11 @@
+let greeting = "How are you?";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <h1>Hello {props.name}</h1>
+
+      <p> {greeting} </p>
+    </div>
+  </>
+);
