865125ab5ed5fd93750c8e3fd6935e962195152c
rubycop.near <rubycop.near>
2023-06-23 21:19:43 +0000

    Update Kudos.Header by rubycop.near at 2023-06-23T21:19:43.875Z

--- rubycop.near/Kudos.Header/source_code.jsx
+++ rubycop.near/Kudos.Header/source_code.jsx
@@ -1,5 +1,3 @@
-const logoUrl = "";
-
 const Header = styled.div`
     background: linear-gradient(90deg, #9333EA 0%, #4F46E5 100%);
 `;
@@ -21,13 +19,13 @@ return (
     <Widget
       src="mob.near/widget/Image"
       props={{
-        image: { url: logoUrl },
+        image: {
+          url: "https://bafkreicrlj3lgygabo37j6gelyamwvm5qj4vtd3sid62dlbr7s6wi3qjhm.ipfs.nftstorage.link/",
+        },
         alt: "kudos",
         style: {
-          height: "40px",
+          height: "30px",
           objectFit: "cover",
-          maxHeight: "40px",
-          borderRadius: "50%",
         },
         fallbackUrl:
           "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm",
@@ -38,7 +36,24 @@ return (
       props={{
         Button: {
           text: "Give a Kudo",
-          icon: <i className="bi bi-plus" />,
+          icon: (
+            <Widget
+              src="mob.near/widget/Image"
+              props={{
+                image: {
+                  url: "https://bafkreieynbjyuycbo7naqp5dtiajcsmpiwyt7n2mk35746463nkcjte2yy.ipfs.nftstorage.link/",
+                },
+                alt: "kudos",
+                style: {
+                  height: "20px",
+                  objectFit: "cover",
+                  margin: "0 0 3px 5px",
+                },
+                fallbackUrl:
+                  "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm",
+              }}
+            />
+          ),
           onClick: handleAddKudo,
         },
       }}
