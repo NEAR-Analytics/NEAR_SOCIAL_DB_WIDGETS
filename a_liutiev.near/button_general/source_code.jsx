9dd7c63934f5e94ac5b4016d47a28dac545f6c6e
a_liutiev.near <a_liutiev.near>
2023-06-24 19:43:23 +0000

    Update button_general by a_liutiev.near at 2023-06-24T19:43:23.013Z

--- a_liutiev.near/button_general/source_code.jsx
+++ a_liutiev.near/button_general/source_code.jsx
@@ -3,8 +3,7 @@
 // });
 
 const value = props.value || "No-name button";
-const callback =
-  props.callback || (() => console.log("BUTTON HAS BEEN CLICKED"));
+const callback = props.callback || (() => console.log("button clicked"));
 
 return (
   <button class="btn btn-primary" onClick={callback}>
