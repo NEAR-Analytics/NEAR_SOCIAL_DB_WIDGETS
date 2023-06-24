16471716670f4c9174684e8525024c929560ac9a
gordonjun.near <gordonjun.near>
2023-06-24 15:53:34 +0000

    Update adventures.menu by gordonjun.near at 2023-06-24T15:53:34.586Z

--- gordonjun.near/adventures.menu/source_code.jsx
+++ gordonjun.near/adventures.menu/source_code.jsx
@@ -1,55 +1,65 @@
+const ComposeWrapper = styled.div`
+  border-top: 1px solid #eceef0;
+`;
+
 return (
   <div>
     <h2 className="mb-3">adventures</h2>
-    <div class="card border-secondary">
-      <div class="nav navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a
-                class="nav-link active"
-                href="https://www.therink.sg/public-skating-sessions/"
-              >
-                <i class="bi bi-bicycle"> </i>
-                activities
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="https://www.imdb.com/">
-                <i class="bi bi-film"> </i>
-                entertainment
-              </a>
-            </li>
+    <ComposeWrapper>
+      <br />
+      <div class="card border-secondary">
+        <div class="nav navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  href="https://www.therink.sg/public-skating-sessions/"
+                >
+                  <i class="bi bi-bicycle"> </i>
+                  activities
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="https://www.imdb.com/">
+                  <i class="bi bi-film"> </i>
+                  entertainment
+                </a>
+              </li>
 
-            <li class="nav-item">
-              <a
-                class="nav-link active"
-                href="https://imperiumgroup.asia/kamikazeasiantapasbar/menu"
-              >
-                <i class="bi bi-egg-fried"> </i>
-                makan places
-              </a>
-            </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  href="https://imperiumgroup.asia/kamikazeasiantapasbar/menu"
+                >
+                  <i class="bi bi-egg-fried"> </i>
+                  makan places
+                </a>
+              </li>
 
-            <li class="nav-item">
-              <a
-                class="nav-link active"
-                href="https://www.youtube.com/watch?v=DYTY-0KvZhI"
-              >
-                <i class="bi bi-basket-fill"> </i>
-                cooking session
-              </a>
-            </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  href="https://www.youtube.com/watch?v=DYTY-0KvZhI"
+                >
+                  <i class="bi bi-basket-fill"> </i>
+                  cooking session
+                </a>
+              </li>
 
-            <li class="nav-item">
-              <a class="nav-link active" href="https://store.steampowered.com/">
-                <i class="bi bi-controller"> </i>
-                games
-              </a>
-            </li>
-          </ul>
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  href="https://store.steampowered.com/"
+                >
+                  <i class="bi bi-controller"> </i>
+                  games
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </ComposeWrapper>
   </div>
 );
