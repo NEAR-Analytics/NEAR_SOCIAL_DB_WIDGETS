fdf4e57429b52f0d0ec9497a96d36b9a26f475cf
a_liutiev.near <a_liutiev.near>
2023-06-24 22:21:18 +0000

    Update input_submit by a_liutiev.near at 2023-06-24T22:21:18.033Z

--- a_liutiev.near/input_submit/source_code.jsx
+++ a_liutiev.near/input_submit/source_code.jsx
@@ -1,6 +1,7 @@
 const value = props.value || "no-name button submit";
 const inputSubmitLabel = props.inputSubmitLabel || "no-name default";
-const callback = props.callback || (() => console.log("button clicked"));
+const handleButtonClick =
+  props.handleButtonClick || (() => console.log("button clicked"));
 
 return (
   <div class="text-muted d-flex justify-content-start align-items-center p-3">
@@ -12,7 +13,7 @@ return (
       />
       <Widget
         src="a_liutiev.near/widget/button_general"
-        props={{ value, callback }}
+        props={{ value, handleButtonClick }}
       />
     </div>
   </div>
