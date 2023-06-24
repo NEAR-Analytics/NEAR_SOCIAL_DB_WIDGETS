f35c8d3cba52370bc685f9c439b3cd9d3aeacc23
a_liutiev.near <a_liutiev.near>
2023-06-24 19:43:43 +0000

    Update button_general by a_liutiev.near at 2023-06-24T19:43:43.195Z

--- a_liutiev.near/button_general/source_code.jsx
+++ a_liutiev.near/button_general/source_code.jsx
@@ -1,7 +1,3 @@
-// State.init({
-//   value: "THIS IS A BUTTON",
-// });
-
 const value = props.value || "No-name button";
 const callback = props.callback || (() => console.log("button clicked"));
 
