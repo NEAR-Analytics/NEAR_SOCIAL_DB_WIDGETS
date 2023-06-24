b5ae33c7b046c1f6abb81958f446dc1ca96beb0f
nearjordan.near <nearjordan.near>
2023-06-24 23:42:41 +0000

    Update AIAttestationRequest by nearjordan.near at 2023-06-24T23:42:41.050Z

--- nearjordan.near/AIAttestationRequest/source_code.jsx
+++ nearjordan.near/AIAttestationRequest/source_code.jsx
@@ -1,31 +1,3 @@
-//<script src="https://unpkg.com/ipfs-http-client/dist/index.min.js"></script>
-
-// Once above IPFS library can be imported:
-// const ipfs = window.IpfsHttpClient.create({
-//   host: "localhost",
-//   port: 5001,
-//   protocol: "http",
-// });
-
-// async function addFile(content) {
-//   Once above IPFS library can be imported:
-//   const { path } = await ipfs.add(content);
-//   await ipfs.pin.add(path);
-//   return path;
-// }
-
-// async function getFile(cid) {
-//   Once above IPFS library can be imported:
-//   const stream = ipfs.cat(cid);
-//   let data = "";
-
-//   for await (const chunk of stream) {
-//     data += new TextDecoder().decode(chunk);
-//   }
-
-//   return data;
-// }
-
 const queryGPT3 = () => {
   fetchGPTResponse().then((res) => {
     const data = res.body;
@@ -52,14 +24,14 @@ const fetchGPTResponse = () => {
   });
 };
 
-return (<>
+return (<div>
   <input type="password" placeholder="LLM API KEY" onChange={(e) => State.update({ apiKey: e.target.value })} />
   <textarea
     onChange={(e) => State.update({ question: e.target.value })}
     placeholder="Enter your query"
     rows="10"
     cols="50"></textarea>
-  <button onclick={queryGPT3}>Query GPT-3.5</button>
+  <button onClick={queryGPT3}>Query GPT-3.5</button>
   <div>{state.answer}</div>
   <div>{state.attestationRequestCID}</div>
-</>)
+</div>);
