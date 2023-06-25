e3d2575a06e08ae6479473cb51678c85d707a154
gordonjun.near <gordonjun.near>
2023-06-25 07:12:24 +0000

    Update dev.search.dao.members.only by gordonjun.near at 2023-06-25T07:12:24.942Z

---
+++ gordonjun.near/dev.search.dao.members.only/source_code.jsx
@@ -0,0 +1,52 @@
+const daoId = props.daoId ?? "bbclan.near";
+
+return (
+  <div>
+    <h2 className="mb-3">apps</h2>
+    <div className="mb-2">
+      <Widget
+        src="gordonjun.near/widget/ComponentSearch.dao.members.only"
+        props={{
+          boostedTag: "app",
+          placeholder: "🔍  search for widgets",
+          limit: 10,
+          daoId: daoId,
+          onChange: ({ result }) => {
+            State.update({ apps: result });
+          },
+        }}
+      />
+    </div>
+    {state.apps && (
+      <div className="mb-2">
+        {state.apps.map((app, i) => (
+          <div key={i}>
+            <Widget
+              src="mob.near/widget/ComponentSearch.Item"
+              props={{
+                link: `#/${app.widgetSrc}`,
+                accountId: app.accountId,
+                widgetName: app.widgetName,
+                onHide: () => State.update({ apps: null }),
+                extraButtons: ({ widgetPath }) => (
+                  <a
+                    target="_blank"
+                    className="btn btn-outline-secondary"
+                    href={`#/mob.near/widget/WidgetSource?src=${widgetPath}`}
+                  >
+                    Source
+                  </a>
+                ),
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    )}
+
+    <Widget
+      src="mob.near/widget/WidgetIcons"
+      props={{ tag: "app", limit: 24, daoId: daoId }}
+    />
+  </div>
+);
