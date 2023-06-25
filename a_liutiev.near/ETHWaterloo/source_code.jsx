ea3211f21afa3bdaac42663097fdeeb884f4d18a
a_liutiev.near <a_liutiev.near>
2023-06-25 00:47:33 +0000

    Update ETHWaterloo by a_liutiev.near at 2023-06-25T00:47:33.471Z

--- a_liutiev.near/ETHWaterloo/source_code.jsx
+++ a_liutiev.near/ETHWaterloo/source_code.jsx
@@ -3,26 +3,37 @@ State.init({
   inputSubmitLabel: "Type Message",
   web3connectLabel: "Connect Wallet",
   messageCount: 0,
+  inputValue: "",
 });
 
 const value = state.value || "n/a";
 const web3connectLabel = state.web3connectLabel || "n/a";
 const inputSubmitLabel = state.inputSubmitLabel || "n/a";
-const callback = state.callback || (() => console.log("button clicked"));
+// const callback = state.callback || (() => console.log("button clicked"));
 
-function handleAction() {
+const handleButtonClick = () => {
   State.update({
-    messageCount: messageCount + 1,
+    messageCount: state.messageCount + 1,
   });
-  console.log(messageCount);
-}
+
+  console.log(state.messageCount);
+};
+
+const handleInputChange = (e) => {
+  console.log(e.target.value);
+  // State.uupdate({ inputValue: e.target.value });
+};
+
+function messageAI() {}
 
 return (
   <div class="card">
     <div class="card-header d-flex justify-content-between align-items-center p-3">
       <h5 className="mb-0">Chat messages</h5>
       <div className="d-flex flex-row align-items-center">
-        <span className="badge bg-secondary me-3 p-2">{state.messageCount}</span>
+        <span className="badge bg-secondary me-3 p-2">
+          {state.messageCount}
+        </span>
         <Widget
           src="a_liutiev.near/widget/button_web3connect"
           props={{ web3connectLabel }}
@@ -30,25 +41,18 @@ return (
       </div>
     </div>
 
-    <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
-      <h4 id="fat">@fat</h4>
-      <p>...</p>
-      <h4 id="mdo">@mdo</h4>
-      <p>...</p>
-      <h4 id="one">one</h4>
-      <p>...</p>
-      <h4 id="two">two</h4>
-      <p>...</p>
-      <h4 id="three">three</h4>
-      <p>...</p>
-    </div>
-
+    <div class="card-body">tree</div>
     <br />
 
     <div class="card-footer text-muted justify-content-start align-items-center p-3">
       <Widget
         src="a_liutiev.near/widget/input_submit"
-        props={{ value, inputSubmitLabel, callback={handleAction} }}
+        props={{
+          value,
+          inputSubmitLabel,
+          handleButtonClick,
+          this.handleInputChange,
+        }}
       />
     </div>
 
