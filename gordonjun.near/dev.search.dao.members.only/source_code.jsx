e8cb2dcc675b0f3823e5adf8adeffd7b1e06b838
gordonjun.near <gordonjun.near>
2023-06-25 09:31:35 +0000

    Update dev.search.dao.members.only by gordonjun.near at 2023-06-25T09:31:35.514Z

--- gordonjun.near/dev.search.dao.members.only/source_code.jsx
+++ gordonjun.near/dev.search.dao.members.only/source_code.jsx
@@ -1,52 +1,59 @@
 const daoId = props.daoId ?? "bbclan.near";
 
+const ComposeWrapper = styled.div`
+  border-top: 1px solid #eceef0;
+`;
+
 return (
   <div>
     <h2 className="mb-3">apps</h2>
-    <div className="mb-2">
-      <Widget
-        src="gordonjun.near/widget/ComponentSearch.dao.members.only"
-        props={{
-          boostedTag: "app",
-          placeholder: "🔍  search for widgets",
-          limit: 10,
-          daoId: daoId,
-          onChange: ({ result }) => {
-            State.update({ apps: result });
-          },
-        }}
-      />
-    </div>
-    {state.apps && (
+    <ComposeWrapper>
+      <br />
       <div className="mb-2">
-        {state.apps.map((app, i) => (
-          <div key={i}>
-            <Widget
-              src="mob.near/widget/ComponentSearch.Item"
-              props={{
-                link: `#/${app.widgetSrc}`,
-                accountId: app.accountId,
-                widgetName: app.widgetName,
-                onHide: () => State.update({ apps: null }),
-                extraButtons: ({ widgetPath }) => (
-                  <a
-                    target="_blank"
-                    className="btn btn-outline-secondary"
-                    href={`#/mob.near/widget/WidgetSource?src=${widgetPath}`}
-                  >
-                    Source
-                  </a>
-                ),
-              }}
-            />
-          </div>
-        ))}
+        <Widget
+          src="gordonjun.near/widget/ComponentSearch.dao.members.only"
+          props={{
+            boostedTag: "app",
+            placeholder: "🔍  search for widgets",
+            limit: 10,
+            daoId: daoId,
+            onChange: ({ result }) => {
+              State.update({ apps: result });
+            },
+          }}
+        />
       </div>
-    )}
+      {state.apps && (
+        <div className="mb-2">
+          {state.apps.map((app, i) => (
+            <div key={i}>
+              <Widget
+                src="mob.near/widget/ComponentSearch.Item"
+                props={{
+                  link: `#/${app.widgetSrc}`,
+                  accountId: app.accountId,
+                  widgetName: app.widgetName,
+                  onHide: () => State.update({ apps: null }),
+                  extraButtons: ({ widgetPath }) => (
+                    <a
+                      target="_blank"
+                      className="btn btn-outline-secondary"
+                      href={`#/mob.near/widget/WidgetSource?src=${widgetPath}`}
+                    >
+                      Source
+                    </a>
+                  ),
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      )}
 
-    <Widget
-      src="gordonjun.near/widget/WidgetIcons.dao.members.only"
-      props={{ tag: "app", limit: 24, daoId: daoId }}
-    />
+      <Widget
+        src="gordonjun.near/widget/WidgetIcons.dao.members.only"
+        props={{ tag: "app", limit: 24, daoId: daoId }}
+      />
+    </ComposeWrapper>
   </div>
 );
