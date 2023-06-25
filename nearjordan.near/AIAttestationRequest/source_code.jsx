b0c67b4c3695726aecaab5e59beb09e1e8129907
nearjordan.near <nearjordan.near>
2023-06-25 00:31:20 +0000

    Update AIAttestationRequest by nearjordan.near at 2023-06-25T00:31:20.326Z

--- nearjordan.near/AIAttestationRequest/source_code.jsx
+++ nearjordan.near/AIAttestationRequest/source_code.jsx
@@ -1,36 +1,5 @@
-const queryGPT3 = () => {
-  const res = fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      "{Content-Type": "application/json",
-      "Authorization": "Bearer " + state.apiKey,
-    },
-    body: {
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: state.question }],
-      temperature: 0.0,
-    },
-  });
-console.log(res);
-  const data = res.json();
-console.log("data", data);
-  const a = data.choices[0].message.content;
-  const attestationRequest = { question: state.question, answer: a };
-  State.update({ answer: a, attestationRequestCID: ethers.utils.sha256(ethers.utils.toUtf8Bytes(JSON.stringify(attestationRequest))) });
-}
-
 // const queryGPT3 = () => {
-//   fetchGPTResponse().then((res) => {
-//     const data = res.body;
-// console.log("data", data);
-//     const a = data.choices[0].message.content;
-//     const attestationRequest = { question: state.question, answer: a };
-//     State.update({ answer: a, attestationRequestCID: ethers.utils.sha256(ethers.utils.toUtf8Bytes(JSON.stringify(attestationRequest))) });
-// });
-// };
-
-// const fetchGPTResponse = () => {
-//   return asyncFetch("https://api.openai.com/v1/chat/completions", {
+//   const res = fetch("https://api.openai.com/v1/chat/completions", {
 //     method: "POST",
 //     headers: {
 //       "{Content-Type": "application/json",
@@ -42,7 +11,39 @@ console.log("data", data);
 //       temperature: 0.0,
 //     },
 //   });
-// };
+// console.log(res);
+//   const data = res.json();
+// console.log("data", data);
+//   const a = data.choices[0].message.content;
+//   const attestationRequest = { question: state.question, answer: a };
+//   State.update({ answer: a, attestationRequestCID: ethers.utils.sha256(ethers.utils.toUtf8Bytes(JSON.stringify(attestationRequest))) });
+// }
+
+const queryGPT3 = () => {
+  fetchGPTResponse().then((res) => {
+console.log(res);
+//     const data = res.body;
+// console.log("data", data);
+//     const a = data.choices[0].message.content;
+//     const attestationRequest = { question: state.question, answer: a };
+//     State.update({ answer: a, attestationRequestCID: ethers.utils.sha256(ethers.utils.toUtf8Bytes(JSON.stringify(attestationRequest))) });
+});
+};
+
+const fetchGPTResponse = () => {
+  return asyncFetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "{Content-Type": "application/json",
+      "Authorization": "Bearer " + state.apiKey,
+    },
+    body: {
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: state.question }],
+      temperature: 0.0,
+    },
+  });
+};
 
 console.log("KEY", state.apiKey);
 return (<div>
