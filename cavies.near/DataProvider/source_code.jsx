39b60ea8b12060a952065d47c208dcda64075f1c
cavies.near <cavies.near>
2023-06-26 08:01:07 +0000

    Update DataProvider by cavies.near at 2023-06-26T08:01:07.100Z

--- cavies.near/DataProvider/source_code.jsx
+++ cavies.near/DataProvider/source_code.jsx
@@ -1,5 +1,5 @@
 // onLoad
-const { onLoad } = props;
+const { onLoad, targetState, updateState } = props;
 
 // Define constants value
 const API = "https://prod-pocket-api.hamsterbox.xyz/api";
@@ -8,18 +8,47 @@ const POCKET_ABI_URL =
 const WHITELIST_ENDPOINT = `${API}/whitelist`;
 const LIST_POCKET_ENDPOINT = `${API}/pool/decimals-formatted?limit=&offset=0&chainId=bnb&statuses=POOL_STATUS%3A%3AACTIVE&statuses=POOL_STATUS%3A%3ACLOSED&sortBy=DATE_START_DESC`;
 
+// Define state reducer
+const useState = (stateName, defaultValue) => {
+  if (!targetState[stateName]) {
+    updateState({
+      [stateName]: defaultValue,
+    });
+  }
+
+  return [
+    targetState[stateName] || defaultValue,
+    (value) => {
+      updateState({
+        [stateName]: value,
+      });
+    },
+  ];
+};
+
 // Define methods to be exposed
 const fetchPockets = (ownerAddress) => {
+  const [, setPocket] = useState("pockets", []);
+
   return new Promise((resolve) => {
     asyncFetch(`${LIST_POCKET_ENDPOINT}&ownerAddress=${ownerAddress}`).then(
       (result) => {
+        setPocket(result.body);
         return resolve(result.body);
       }
     );
   });
 };
 
-// expose methods
+// emit data to parents component
 onLoad({
+  useState,
   fetchPockets,
 });
+
+// trigger this to reload DOM
+updateState({
+  [`cavies.near/Widget/DataProvider`]: new Date().getTime(),
+});
+
+console.log("widget reloaded");
