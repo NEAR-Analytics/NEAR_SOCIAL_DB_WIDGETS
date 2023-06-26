8bc89e5b3dbd4a1e64d15e99ece0b96528b9f502
austinballard.near <austinballard.near>
2023-06-26 20:08:42 +0000

    Update Appa by austinballard.near at 2023-06-26T20:08:42.682Z

---
+++ austinballard.near/Appa/source_code.jsx
@@ -0,0 +1,11 @@
+let greeting = "Appa";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <h1>Hello my name is {props.name}</h1>
+
+      <p> {greeting} </p>
+    </div>
+  </>
+);
