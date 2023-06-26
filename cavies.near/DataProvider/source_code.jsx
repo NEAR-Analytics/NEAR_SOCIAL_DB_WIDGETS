13ed00e3dfa8f8fc4a40ebb21ab458f28498b016
cavies.near <cavies.near>
2023-06-26 08:38:44 +0000

    Update DataProvider by cavies.near at 2023-06-26T08:38:44.339Z

--- cavies.near/DataProvider/source_code.jsx
+++ cavies.near/DataProvider/source_code.jsx
@@ -6,7 +6,7 @@ const API = "https://prod-pocket-api.hamsterbox.xyz/api";
 const POCKET_ABI_URL =
   "https://raw.githubusercontent.com/CaviesLabs/hamsterpocket-assets/main/pocketchef.json";
 const WHITELIST_ENDPOINT = `${API}/whitelist`;
-const LIST_POCKET_ENDPOINT = `${API}/pool/decimals-formatted?limit=&offset=0&chainId=bnb&statuses=POOL_STATUS%3A%3AACTIVE&statuses=POOL_STATUS%3A%3ACLOSED&sortBy=DATE_START_DESC`;
+const LIST_POCKET_ENDPOINT = `${API}/pool/decimals-formatted?limit=1000&offset=0&chainId=bnb&statuses=POOL_STATUS%3A%3AACTIVE&statuses=POOL_STATUS%3A%3ACLOSED&sortBy=DATE_START_DESC`;
 
 // Define state reducer
 const context = {
