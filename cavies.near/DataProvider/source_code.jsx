3d3f0429e543c10ee94922ce0db22eede5b4413c
cavies.near <cavies.near>
2023-06-26 07:14:21 +0000

    Update DataProvider by cavies.near at 2023-06-26T07:14:21.501Z

---
+++ cavies.near/DataProvider/source_code.jsx
@@ -0,0 +1,44 @@
+// onLoad
+const { onLoad, targetState, updateState } = props;
+
+// Define constants value
+const API = "https://prod-pocket-api.hamsterbox.xyz/api";
+const POCKET_ABI_URL =
+  "https://raw.githubusercontent.com/CaviesLabs/hamsterpocket-assets/main/pocketchef.json";
+const WHITELIST_ENDPOINT = `${API}/whitelist`;
+const LIST_POCKET_ENDPOINT = `${API}/pool/decimals-formatted?limit=&offset=0&chainId=bnb&statuses=POOL_STATUS%3A%3AACTIVE&statuses=POOL_STATUS%3A%3ACLOSED&sortBy=DATE_START_DESC`;
+
+// Define state reducer
+const useState = (stateName, defaultValue) => {
+  return [
+    targetState[stateName] || defaultValue,
+    (value) => {
+      updateState({
+        [stateName]: value,
+      });
+    },
+  ];
+};
+
+// Define methods to be exposed
+const fetchPockets = (ownerAddress) => {
+  const [, setPocket] = useState("pockets", []);
+
+  return new Promise((resolve) => {
+    asyncFetch(`${LIST_POCKET_ENDPOINT}&ownerAddress=${ownerAddress}`).then(
+      (result) => {
+        setPocket(result.body);
+        return resolve(result.body);
+      }
+    );
+  });
+};
+
+if (onLoad) {
+  onLoad({
+    useState,
+    fetchPockets,
+  });
+}
+
+return <h1>Test</h1>;
