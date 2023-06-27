c00faaf4c609c87ed2d5f4ab5f7206c24d820d16
flowscience.near <flowscience.near>
2023-06-27 18:30:04 +0000

    Update cgd by flowscience.near at 2023-06-27T18:30:04.426Z

--- flowscience.near/cgd/source_code.jsx
+++ flowscience.near/cgd/source_code.jsx
@@ -1,6 +1,6 @@
 const accountId = context.accountId;
 const daoId = props.daoId ?? "cannabis-genome.sputnik-dao.near";
-const role = props.role ?? "council";
+const roleId = props.roleId ?? "Council";
 
 const policy = Near.view(daoId, "get_policy");
 const deposit = policy.proposal_bond;
@@ -38,6 +38,7 @@ const handleJoin = () => {
           kind: {
             AddMemberToRole: {
               member_id: accountId,
+              role: roleId,
             },
           },
         },
@@ -157,7 +158,7 @@ return (
           <svg viewBox="0 0 26 24" fill="none" aria-hidden="true">
             <path
               d="M24.3767 8.06326L1.51965 0.0649912C1.10402 -0.0830767 0.639031 0.026026 0.327308 0.340346C0.0181841 0.657263 -0.0831256 1.12225 0.0701378 1.53788L8.071 23.2519C8.23726 23.7013 8.66587 24 9.14385 24H9.14644C9.62702 24 10.0556 23.6961 10.2167 23.2441L13.734 13.495L24.3325 10.2349C24.8053 10.0895 25.13 9.65824 25.1378 9.16468C25.1482 8.67112 24.8391 8.22691 24.3715 8.06326H24.3767Z"
-              fill="#4D63EC"
+              fill="#88c54b"
             />
           </svg>
         </span>
@@ -182,7 +183,7 @@ return (
       <div>
         <div className="mb-5">
           <Widget
-            src="hack.near/widget/communities.join"
+            src="flowscience.near/widget/communities.join"
             props={{
               memberId: accountId,
               roleId,
