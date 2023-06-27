80971f83575e62bafa48732aed48b2e2707af87c
ndcplug.near <ndcplug.near>
2023-06-27 19:01:59 +0000

    Update DAO.main by ndcplug.near at 2023-06-27T19:01:59.621Z

--- ndcplug.near/DAO.main/source_code.jsx
+++ ndcplug.near/DAO.main/source_code.jsx
@@ -27,8 +27,9 @@ if (props.tab && props.tab !== state.selectedTab) {
   });
 }
 
+const daoPageAccount = "ndcplug.near";
 const profile = props.profile ?? Social.getr(`${daoId}/profile`);
-const accountUrl = `#/sking.near/widget/DAO.Page?daoId=${daoId}`;
+const accountUrl = `#/${daoPageAccount}/widget/DAO.Page?daoId=${daoId}`;
 
 const Wrapper = styled.div`
   padding-bottom: 48px;
