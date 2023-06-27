f975083b9f7cd502364bd7b04e300a816f6e0022
efiz.near <efiz.near>
2023-06-27 22:57:07 +0000

    Update every.edge by efiz.near at 2023-06-27T22:57:07.987Z

--- efiz.near/every.edge/source_code.jsx
+++ efiz.near/every.edge/source_code.jsx
@@ -3,12 +3,14 @@ const blockHeight = props.blockHeight;
 
 const edges = Social.index("edges", path);
 
+// This could be improved so it is cleaner and doesn't need to go through every.thing.view again
+// every.edge.view?
 return (
   <div>
     {edges?.map((it) => {
       return (
         <Widget
-          src="efiz.near/widget/every.thing.view"
+          src="efiz.near/widget/every.edge.view"
           props={{ path: it.value.path, blockHeight }}
         />
       );
