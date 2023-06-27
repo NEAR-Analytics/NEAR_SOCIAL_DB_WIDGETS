59686be12ceae747499b2aeebf9deeac469329ab
efiz.near <efiz.near>
2023-06-27 23:13:44 +0000

    Update every.edge.view by efiz.near at 2023-06-27T23:13:44.893Z

--- efiz.near/every.edge.view/source_code.jsx
+++ efiz.near/every.edge.view/source_code.jsx
@@ -3,4 +3,8 @@ const blockHeight = props.blockHeight;
 
 const thing = JSON.parse(Social.get(path, blockHeight) || "null");
 
+if (!thing) {
+  return <></>;
+}
+
 return <p>{JSON.stringify(thing)}</p>;
