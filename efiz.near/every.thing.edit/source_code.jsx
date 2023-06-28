2b29a807a401ea53cfc308322d7b1cc2d1ae8a75
efiz.near <efiz.near>
2023-06-28 04:19:15 +0000

    Update every.thing.edit by efiz.near at 2023-06-28T04:19:15.745Z

--- efiz.near/every.thing.edit/source_code.jsx
+++ efiz.near/every.thing.edit/source_code.jsx
@@ -1,31 +1,17 @@
 const path = props.path;
 const blockHeight = props.blockHeight;
-
-function handleSubmit(val) {
-  const parts = path.split("/");
-  const creatorId = parts[0];
-  parts.shift(); // Remove the first element
-  const newData = {
-    [parts[0]]: {
-      [parts[1]]: val.replace(/\n/g, ""),
-    },
-  };
-  if (context.accountId === creatorId) {
-    Social.set(newData, {
-      force: true,
-    });
-  }
-}
+const parts = path.split("/");
 
 const thing = JSON.parse(Social.get(path, blockHeight) || "null");
+const type = thing.type;
 
-if (thing) {
-  return (
-    <Widget
-      src="efiz.near/widget/Every.Raw.Edit"
-      props={{ value: thing, handleSubmit: handleSubmit }}
-    />
-  );
-} else {
-  return <p>thing not found</p>;
-}
+return (
+  <Widget
+    src={"efiz.near/widget/creator"}
+    props={{
+      data: thing.data,
+      type: type,
+      thingId: parts[2],
+    }}
+  />
+);
