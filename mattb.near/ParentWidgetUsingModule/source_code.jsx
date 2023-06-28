c403f761b8f46701af725c3dd8e03aab59dd6e8c
mattb.near <mattb.near>
2023-06-28 18:52:07 +0000

    Update ParentWidgetUsingModule by mattb.near at 2023-06-28T18:52:07.122Z

--- mattb.near/ParentWidgetUsingModule/source_code.jsx
+++ mattb.near/ParentWidgetUsingModule/source_code.jsx
@@ -8,18 +8,13 @@ State.init({
 let a = state.a;
 let b = state.b;
 
-let obj = {
-  ...Module,
-};
-
 console.log(a, b, state, obj, Module);
 
 return (
   <>
+    {state.a}
+    {state.b}
     {Module.sum(state.a, state.b)}
     {Module.sum(a, b)}
-    -----
-    {obj.sum(state.a, state.b)}
-    {obj.sum(a, b)}
   </>
 );
