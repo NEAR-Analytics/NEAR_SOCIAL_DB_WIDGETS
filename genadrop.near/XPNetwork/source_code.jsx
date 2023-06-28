0e82a6db0ba52ddd697bf1be9ab6af7c3fb4a297
genadrop.near <genadrop.near>
2023-06-28 15:54:52 +0000

    Update XPNetwork by genadrop.near at 2023-06-28T15:54:52.527Z

--- genadrop.near/XPNetwork/source_code.jsx
+++ genadrop.near/XPNetwork/source_code.jsx
@@ -1,32 +1,10 @@
-const css = styled.b`
-html {overflow: auto;}    
-html,
-body,
-div,
-iframe {
-    margin: 0px;
-    padding: 0px;
-    height: 100%;
-    min-height: 5000px;
-    border: none;
-}
-iframe {
-    display: block;
-    width: 100%;
-    border: none;
-    overflow-y: auto;
-    overflow-x: hidden;
-}
-`;
 return (
   <>
-    <css>
-      <iframe
-        src="https://widget.xp.network/account?widget=true&background=efecec&panelBackground=e6e1e1&modalBackground=efecec&color=c92c6b&fontSize=16px&btnColor=e2d5d5&btnBackground=395FEB&btnRadius=9&fontFamily=Open Sans&chains=Ethereum-BSC-Elrond-Polygon-Avalanche-Fantom-xDai-Fuse-Velas-Tezos-Iotex-Harmony-Aurora-Godwoken-GateChain-VeChain&cardBackground=0d4be7&cardBackgroundBot=16da51&cardColor=d71d1d&cardRadius=&secondaryColor=0f3cf0&accentColor=438a42&borderColor=cc33bf&iconColor=ee446f&tooltipBg=1D212A&tooltipColor=303da1&wallets=MetaMask-WalletConnect-TrustWallet-Maiar-Beacon-TempleWallet-MaiarExtension&bridgeState=undefined&showLink=true&affiliationFees=1"
-        frameborder="0"
-        width="100%"
-        height="800px"
-      ></iframe>
-    </css>
+    <iframe
+      src="https://widget.xp.network/account?widget=true&background=efecec&panelBackground=e6e1e1&modalBackground=efecec&color=c92c6b&fontSize=16px&btnColor=e2d5d5&btnBackground=395FEB&btnRadius=9&fontFamily=Open Sans&chains=Ethereum-BSC-Elrond-Polygon-Avalanche-Fantom-xDai-Fuse-Velas-Tezos-Iotex-Harmony-Aurora-Godwoken-GateChain-VeChain&cardBackground=0d4be7&cardBackgroundBot=16da51&cardColor=d71d1d&cardRadius=&secondaryColor=0f3cf0&accentColor=438a42&borderColor=cc33bf&iconColor=ee446f&tooltipBg=1D212A&tooltipColor=303da1&wallets=MetaMask-WalletConnect-TrustWallet-Maiar-Beacon-TempleWallet-MaiarExtension&bridgeState=undefined&showLink=true&affiliationFees=1"
+      frameborder="0"
+      width="100%"
+      height="800px"
+    ></iframe>
   </>
 );
