bad8f91a540bbef70c33b87594c9b748e82ecef2
efiz.near <efiz.near>
2023-06-28 01:41:51 +0000

    Update every.edge by efiz.near at 2023-06-28T01:41:51.155Z

--- efiz.near/every.edge/source_code.jsx
+++ efiz.near/every.edge/source_code.jsx
@@ -1,3 +1,29 @@
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  gap: 8px;
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`;
+
+const IconLabelButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 8px;
+`;
+
+const Title = styled.h2`
+    margin: 8px;
+`;
+
 const path = props.path;
 const blockHeight = props.blockHeight;
 
@@ -6,6 +32,10 @@ const edges = Social.index("edges", path);
 if (!edges) {
   return <></>;
 }
+
+State.init({
+  view: "VIEW",
+});
 
 const Grid = styled.div`
   display: grid;
@@ -16,15 +46,56 @@ const Grid = styled.div`
   }
 `;
 
-return (
-  <Grid>
-    {edges?.map((it) => {
+function Content() {
+  switch (state.view) {
+    case "VIEW": {
+      return (
+        <Grid>
+          {edges?.map((it) => {
+            return (
+              <Widget
+                src="efiz.near/widget/every.edge.view"
+                props={{ path: it.value.path, blockHeight }}
+              />
+            );
+          })}
+        </Grid>
+      );
+    }
+    case "BUILD": {
       return (
         <Widget
-          src="efiz.near/widget/every.edge.view"
-          props={{ path: it.value.path, blockHeight }}
+          src="efiz.near/widget/every.thing.build"
+          props={{ path, blockHeight }}
         />
       );
-    })}
-  </Grid>
+    }
+    case "CONNECT": {
+      return (
+        <Widget
+          src="efiz.near/widget/every.edge.create"
+          props={{ path, blockHeight }}
+        />
+      );
+    }
+  }
+}
+
+return (
+  <>
+    <Header>
+      <Title>edges</Title>
+      <ButtonRow>
+        <IconLabelButton onClick={() => State.update({ view: "CONNECT" })}>
+          <i class="bi bi-link"></i>
+          <span>connect</span>
+        </IconLabelButton>
+        <IconLabelButton onClick={() => State.update({ view: "BUILD" })}>
+          <i class="bi bi-hammer"></i>
+          <span>build</span>
+        </IconLabelButton>
+      </ButtonRow>
+    </Header>
+    <Content />
+  </>
 );
