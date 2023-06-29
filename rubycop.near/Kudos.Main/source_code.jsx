affdc1227feca592bb048ee36e92a828b1f0ff55
rubycop.near <rubycop.near>
2023-06-29 12:34:52 +0000

    Update Kudos.Main by rubycop.near at 2023-06-29T12:34:52.438Z

--- rubycop.near/Kudos.Main/source_code.jsx
+++ rubycop.near/Kudos.Main/source_code.jsx
@@ -114,7 +114,7 @@ return (
                 src={widgets.item}
                 props={{
                   isIAmHuman,
-                  replyTo: kudo.id,
+                  id: kudo.id,
                   accountId: kudo.accountId,
                   description: kudo.value.answer,
                   upvotes: upvotesMap[kudo.blockHeight]
