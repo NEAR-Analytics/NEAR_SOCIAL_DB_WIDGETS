8c6fa52ff07de9161a13b2d4e3d3159518bd0f7e
rubycop.near <rubycop.near>
2023-06-29 12:29:24 +0000

    Update Kudos.Kudo.Page by rubycop.near at 2023-06-29T12:29:24.441Z

---
+++ rubycop.near/Kudos.Kudo.Page/source_code.jsx
@@ -0,0 +1,56 @@
+const { kudo } = props;
+
+const widgets = {
+  item: "rubycop.near/widget/Kudos.Kudo.Item",
+};
+
+// const comments = Social.index(`kudos/${kudo.id}/comments`);
+const comments = [
+  {
+    accountId: "rubycop.near",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quam enim, dignissim sed ante at, convallis maximus enim.",
+    createdAt: 2323234234,
+  },
+  {
+    accountId: "test.near",
+    description: "Lorem ipsum dolor ",
+    createdAt: 1323254234,
+  },
+];
+
+return (
+  <div className="d-flex flex-wrap">
+    <div className="col col-md-6 p-3">
+      <h4>Kudo</h4>
+      <Widget
+        src={widgets.item}
+        props={{
+          isIAmHuman,
+          accountId: kudo.accountId,
+          description: kudo.value.answer,
+          upvotes: upvotesMap[kudo.blockHeight]
+            ? upvotesMap[kudo.blockHeight]
+            : 0,
+          tags: kudo.tags ?? [],
+          createdAt: kudo.createdAt,
+        }}
+      />
+    </div>
+    <div className="col col-md-6 p-3 d-grid gap-3">
+      <h4>Comments</h4>
+      {comments.map((comment) => (
+        <Widget
+          src={widgets.item}
+          props={{
+            isIAmHuman,
+            accountId: comment.accountId,
+            description: comment.description,
+            createdAt: comment.createdAt,
+            tags: [],
+          }}
+        />
+      ))}
+    </div>
+  </div>
+);
