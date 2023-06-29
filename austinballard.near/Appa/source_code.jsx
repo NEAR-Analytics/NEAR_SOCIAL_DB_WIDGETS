d21fe888dc0c296155ec34736070595deb719b1d
austinballard.near <austinballard.near>
2023-06-29 01:46:05 +0000

    Update Appa by austinballard.near at 2023-06-29T01:46:05.962Z

--- austinballard.near/Appa/source_code.jsx
+++ austinballard.near/Appa/source_code.jsx
@@ -1,9 +1,8 @@
 let greeting = "Appa";
-
 return (
   <>
     <div class="container border border-info p-3 text-center">
-      <h1>Hello my name is {props.name}</h1>
+      <h1>My dog's name is {props.name}</h1>
 
       <p> {greeting} </p>
     </div>
